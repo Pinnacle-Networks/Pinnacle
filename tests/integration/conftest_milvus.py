superduper-io/superduper

Add Milvus support

tests/integration/conftest_milvus.py
@@ -1,0 +1,52 @@
+import pytest
+from typing import Iterator
+
+from tenacity import RetryError, Retrying, stop_after_delay
+
+from superduperdb.misc.config import MilvusConfig
+from superduperdb.vector_search.milvus import MilvusClient
+
+
+@pytest.fixture(scope="session")
+def milvus_config() -> MilvusConfig:
+    return MilvusConfig(
+        host="localhost",
+        port=19530,
+        username="root",
+        password="",
+        consistency_level="Strong",
+    )
+
+
+def wait_for_milvus(config: MilvusConfig, *, timeout_s: float = 30) -> None:
+    try:
+        for attempt in Retrying(stop=stop_after_delay(timeout_s)):
+            with attempt:
+                with MilvusClient(config=config).init():
+                    return
+            print("Waiting for milvus to start...")
+    except RetryError:
+        pytest.fail("Could not connect to milvus")
+
+
+def cleanup_milvus(config: MilvusConfig) -> None:
+    with MilvusClient(config=config).init() as client:
+        client.drop_all_collections()
+
+
+@pytest.fixture(scope="session")
+def _milvus_server(milvus_config: MilvusConfig) -> Iterator[MilvusConfig]:
+    wait_for_milvus(milvus_config)
+    yield milvus_config
+
+
+@pytest.fixture
+def milvus_server(_milvus_server: MilvusConfig) -> Iterator[MilvusConfig]:
+    cleanup_milvus(_milvus_server)
+    yield _milvus_server
+
+
+@pytest.fixture
+def milvus_client(milvus_server: MilvusConfig) -> Iterator[MilvusClient]:
+    with MilvusClient(config=milvus_server).init() as client:
+        yield client

tests/integration/test_milvus.py
@@ -1,0 +1,72 @@
+import pytest
+import numpy
+from typing import Iterator
+
+from superduperdb.misc.config import VectorSearchConfig, MilvusConfig
+from superduperdb.vector_search.base import (
+    VectorIndexItem,
+    VectorIndexManager,
+    VectorIndexItemNotFound,
+)
+from superduperdb.vector_search.milvus import MilvusClient, MilvusVectorIndexManager
+
+
+class TestMilvusClient:
+    def test_list_databases(self, milvus_client: MilvusClient) -> None:
+        dbs = milvus_client.list_databases()
+        assert dbs == ["default"]
+
+
+class TestMilvusVectorIndex:
+    @pytest.fixture
+    def manager(self, milvus_server: MilvusConfig) -> Iterator[VectorIndexManager]:
+        with MilvusVectorIndexManager(
+            config=VectorSearchConfig(milvus=milvus_server)
+        ).init() as manager:
+            yield manager
+
+    def test_find_nearest_from_array(self, manager: MilvusVectorIndexManager) -> None:
+        with manager.get_index("test", dimensions=1) as index:
+            index.add(
+                [
+                    VectorIndexItem(id=str(i), vector=numpy.array([i]))
+                    for i in range(100)
+                ]
+            )
+
+            nearest = index.find_nearest_from_array(numpy.array([15]), limit=8)
+            ids = [int(i) for i in nearest[0].ids]
+            assert len(ids) == 8
+            assert all(5 <= i <= 25 for i in ids)
+
+    def test_find_nearest_from_id(self, manager: MilvusVectorIndexManager) -> None:
+        with manager.get_index("test", dimensions=1) as index:
+            index.add(
+                [
+                    VectorIndexItem(id=str(i), vector=numpy.array([i]))
+                    for i in range(100)
+                ]
+            )
+
+            nearest = index.find_nearest_from_id("15", limit=8)
+            ids = [int(i) for i in nearest[0].ids]
+            assert len(ids) == 8
+            assert all(5 <= i <= 25 for i in ids)
+
+    def test_find_nearest_from_id__not_found(
+        self, manager: MilvusVectorIndexManager
+    ) -> None:
+        with manager.get_index("test", dimensions=1) as index:
+            with pytest.raises(VectorIndexItemNotFound):
+                index.find_nearest_from_id("15")
+
+    def test_add__overwrite(self, manager: MilvusVectorIndexManager) -> None:
+        with manager.get_index("test", dimensions=1) as index:
+            index.add([VectorIndexItem(id="0", vector=numpy.array([0]))])
+            index.add([VectorIndexItem(id="1", vector=numpy.array([1]))])
+
+            index.add([VectorIndexItem(id="1", vector=numpy.array([100]))])
+
+            nearest = index.find_nearest_from_array(numpy.array([99]), limit=1)
+            ids = [int(i) for i in nearest[0].ids]
+            assert ids == [1]

Pinnacle-Networks/Pinnacle

Former-commit-id: 2d299bee12ec4e2c25158bcb412645d2307c367e

tests/integration/conftest_milvus.py
@@ -1,0 +1,52 @@
+import pytest
+from typing import Iterator
+
+from tenacity import RetryError, Retrying, stop_after_delay
+
+from pinnacledb.misc.config import MilvusConfig
+from pinnacledb.vector_search.milvus import MilvusClient
+
+
+@pytest.fixture(scope="session")
+def milvus_config() -> MilvusConfig:
+    return MilvusConfig(
+        host="localhost",
+        port=19530,
+        username="root",
+        password="",
+        consistency_level="Strong",
+    )
+
+
+def wait_for_milvus(config: MilvusConfig, *, timeout_s: float = 30) -> None:
+    try:
+        for attempt in Retrying(stop=stop_after_delay(timeout_s)):
+            with attempt:
+                with MilvusClient(config=config).init():
+                    return
+            print("Waiting for milvus to start...")
+    except RetryError:
+        pytest.fail("Could not connect to milvus")
+
+
+def cleanup_milvus(config: MilvusConfig) -> None:
+    with MilvusClient(config=config).init() as client:
+        client.drop_all_collections()
+
+
+@pytest.fixture(scope="session")
+def _milvus_server(milvus_config: MilvusConfig) -> Iterator[MilvusConfig]:
+    wait_for_milvus(milvus_config)
+    yield milvus_config
+
+
+@pytest.fixture
+def milvus_server(_milvus_server: MilvusConfig) -> Iterator[MilvusConfig]:
+    cleanup_milvus(_milvus_server)
+    yield _milvus_server
+
+
+@pytest.fixture
+def milvus_client(milvus_server: MilvusConfig) -> Iterator[MilvusClient]:
+    with MilvusClient(config=milvus_server).init() as client:
+        yield client

tests/integration/test_milvus.py
@@ -1,0 +1,72 @@
+import pytest
+import numpy
+from typing import Iterator
+
+from pinnacledb.misc.config import VectorSearchConfig, MilvusConfig
+from pinnacledb.vector_search.base import (
+    VectorIndexItem,
+    VectorIndexManager,
+    VectorIndexItemNotFound,
+)
+from pinnacledb.vector_search.milvus import MilvusClient, MilvusVectorIndexManager
+
+
+class TestMilvusClient:
+    def test_list_databases(self, milvus_client: MilvusClient) -> None:
+        dbs = milvus_client.list_databases()
+        assert dbs == ["default"]
+
+
+class TestMilvusVectorIndex:
+    @pytest.fixture
+    def manager(self, milvus_server: MilvusConfig) -> Iterator[VectorIndexManager]:
+        with MilvusVectorIndexManager(
+            config=VectorSearchConfig(milvus=milvus_server)
+        ).init() as manager:
+            yield manager
+
+    def test_find_nearest_from_array(self, manager: MilvusVectorIndexManager) -> None:
+        with manager.get_index("test", dimensions=1) as index:
+            index.add(
+                [
+                    VectorIndexItem(id=str(i), vector=numpy.array([i]))
+                    for i in range(100)
+                ]
+            )
+
+            nearest = index.find_nearest_from_array(numpy.array([15]), limit=8)
+            ids = [int(i) for i in nearest[0].ids]
+            assert len(ids) == 8
+            assert all(5 <= i <= 25 for i in ids)
+
+    def test_find_nearest_from_id(self, manager: MilvusVectorIndexManager) -> None:
+        with manager.get_index("test", dimensions=1) as index:
+            index.add(
+                [
+                    VectorIndexItem(id=str(i), vector=numpy.array([i]))
+                    for i in range(100)
+                ]
+            )
+
+            nearest = index.find_nearest_from_id("15", limit=8)
+            ids = [int(i) for i in nearest[0].ids]
+            assert len(ids) == 8
+            assert all(5 <= i <= 25 for i in ids)
+
+    def test_find_nearest_from_id__not_found(
+        self, manager: MilvusVectorIndexManager
+    ) -> None:
+        with manager.get_index("test", dimensions=1) as index:
+            with pytest.raises(VectorIndexItemNotFound):
+                index.find_nearest_from_id("15")
+
+    def test_add__overwrite(self, manager: MilvusVectorIndexManager) -> None:
+        with manager.get_index("test", dimensions=1) as index:
+            index.add([VectorIndexItem(id="0", vector=numpy.array([0]))])
+            index.add([VectorIndexItem(id="1", vector=numpy.array([1]))])
+
+            index.add([VectorIndexItem(id="1", vector=numpy.array([100]))])
+
+            nearest = index.find_nearest_from_array(numpy.array([99]), limit=1)
+            ids = [int(i) for i in nearest[0].ids]
+            assert ids == [1]