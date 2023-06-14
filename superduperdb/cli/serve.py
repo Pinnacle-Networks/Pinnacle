superduper-io/superduper

Implement server (see #116)

superduperdb/cli/serve.py
@@ -1,18 +1,8 @@
 from . import command
-from typer import Argument, Option
-from typing import List
+from superduperdb.server.server import Server
+from tests.unittests.server.test_server import Object
 
 
-@command(help='serve')
-def serve(
-    arg: str = Argument(
-        'arg',
-        help='First argument',
-    ),
-    argv: List[str] = Argument(
-        None,
-        help='Additional arguments',
-    ),
-    opt: bool = Option(False, '--opt', '-o', help='An option'),
-):
-    pass
+@command(help='Start server')
+def serve():
+    Server().auto_run(Object())

superduperdb/server/registry.py
@@ -1,0 +1,82 @@
+import dataclasses as dc
+import inspect
+import superduperdb as s
+import typing as t
+
+
+class NotSerializableError(ValueError):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class RegistryEntry:
+    name: str
+    parameter_types: t.Sequence[t.Type]
+    result_type: t.Type
+
+
+@dc.dataclass
+class Registry:
+    entries: t.Dict[str, RegistryEntry] = dc.field(default_factory=dict)
+    Parameter: t.Optional[t.Type] = None
+    Result: t.Optional[t.Type] = None
+
+    def register(self, method: t.Callable) -> None:
+        """
+        Registers a method and its signature.
+        """
+        name = method.__name__
+        assert name not in self.entries, f'name={name} already registered'
+
+        sig = inspect.signature(method)
+        params = list(sig.parameters.items())
+        if not inspect.ismethod(method):
+            if not (params and params[0][0] == 'self'):
+                raise NotSerializableError(f'{method} not an instance method')
+            params = params[1:]
+
+        parameter_types = tuple(value.annotation for _, value in params)
+        result_type = sig.return_annotation
+
+        def is_model(t, parent=s.Serializable):
+            return t is parent or any(is_model(c, parent) for c in t.__bases__)
+
+        types = set(parameter_types + (result_type,))
+        if non_models := [t for t in types if not is_model(t)]:
+            raise NotSerializableError(f'Not serializable: {non_models}')
+
+        def unite(a, b):
+            return b if a is None else t.Union[a, b]
+
+        for r in parameter_types:
+            self.Parameter = unite(self.Parameter, r)
+        self.Result = unite(self.Result, result_type)
+        self.entries[name] = RegistryEntry(name, parameter_types, result_type)
+
+    def execute(
+        self, obj: t.Any, method: str, args: t.Sequence[s.Serializable]
+    ) -> s.Serializable:
+        """Executes a query with zero or more parameters"""
+
+        method_impl = getattr(obj, method, None)
+        assert method_impl is not None, f'No database method named {method}'
+
+        entry = self.entries.get(method)
+        assert entry, f'No method named {method}'
+
+        # Going through `execute` destroys most of the type information, so we need to
+        # validate "by hand" here
+        types = entry.parameter_types
+
+        if not types and len(args) == 1 and args[0] is None:
+            # Special case, needed for endpoint /execute-one
+            args = ()
+        elif len(args) != len(types):
+            assert False, f'In {method}, expected {len(args)} objects, got {len(types)}'
+
+        arg_types = enumerate(zip(args, types))
+        if bad := [(i, a, t) for i, (a, t) in arg_types if not isinstance(a, t)]:
+            it = (f'In param {i} of {method}, expected {t}, got {a}' for i, a, t in bad)
+            assert False, '\n'.join(it)
+
+        return method_impl(*args)

superduperdb/server/server.py
@@ -1,0 +1,129 @@
+from .registry import NotSerializableError, Registry
+from enum import Enum
+from fastapi import FastAPI, HTTPException, Response, UploadFile
+from fastapi.responses import PlainTextResponse
+from functools import cached_property
+from http import HTTPStatus as status
+from uvicorn import run
+import dataclasses as dc
+import fastapi
+import superduperdb as s
+import typing as t
+
+MEDIA_TYPE = 'application/octet-stream'
+
+
+@dc.dataclass
+class Server:
+    cfg: s.config.Server = s.CFG.server
+    stats: t.Dict = dc.field(default_factory=dict)
+    artifact_store: t.Dict = dc.field(default_factory=dict)
+    # TODO: this is a non-thread-safe proof of concept
+
+    @cached_property
+    def app(self) -> FastAPI:
+        return FastAPI(**self.cfg.fastapi.dict())
+
+    @cached_property
+    def registry(self) -> Registry:
+        return Registry()
+
+    def register(self, *methods) -> t.Optional[t.Callable]:
+        result = None
+        for result in methods:
+            self.registry.register(result)
+        return result
+
+    def run(self, obj: t.Any) -> t.NoReturn:
+        self.add_endpoints(obj)
+        run(self.app, **self.cfg.web_server.dict())
+        assert False, 'We never get here'
+
+    def auto_register(self, obj: t.Any) -> None:
+        for k, v in vars(obj.__class__).items():
+            if callable(v) and k.islower() and not k.startswith('_'):
+                try:
+                    self.registry.register(v)
+                except NotSerializableError:
+                    pass
+
+    def auto_run(self, obj: t.Any) -> t.NoReturn:
+        self.auto_register(obj)
+        self.run(obj)
+
+    def add_endpoints(self, obj: t.Any):
+        count = 0
+
+        @self.app.get('/', response_class=PlainTextResponse)
+        def root() -> str:
+            return self.cfg.fastapi.title
+
+        count += 1
+
+        @self.app.get('/health', response_class=PlainTextResponse)
+        def health() -> str:
+            return 'ok'
+
+        count += 1
+
+        @self.app.get('/stats')
+        def stats() -> t.Dict[str, int]:
+            return self.stats
+
+        count += 1
+
+        @self.app.get('/download/{artifact_key}', response_class=Response)
+        def download(artifact_key: str) -> Response:
+            if not (content := self.artifact_store.get(artifact_key)):
+                print(self.artifact_store, artifact_key)
+                raise HTTPException(
+                    status.NOT_FOUND, f'Unknown artifact {artifact_key}'
+                )
+
+            elif not isinstance(content, bytes):
+                if not callable(encode := getattr(content, 'encode', None)):
+                    msg = f'Uncodable artifact {artifact_key}'
+                    raise HTTPException(status.NOT_FOUND, msg)
+
+                content = encode()
+
+            return fastapi.Response(
+                content=content, status_code=status.OK, media_type=MEDIA_TYPE
+            )
+
+        count += 1
+
+        @self.app.post('/upload/{artifact_key}')
+        async def upload(file: UploadFile):
+            exists = file.filename in self.artifact_store
+            self.artifact_store[file.filename] = await file.read()
+
+            action = 'replaced' if exists else 'created'
+            return {action: file.filename}
+
+        count += 1
+
+        for method_name in self.registry.entries:
+            method = getattr(obj, method_name)
+            self.app.post('/' + method_name)(method)
+
+            count += 1
+
+        # TODO: unfortunately, mypy won't accept these computed types
+        # even though FastAPI has no issue with them.
+
+        methods = sorted((e, e) for e in self.registry.entries)
+        Method = Enum('Method', methods)  # type: ignore
+        Parameter = self.registry.Parameter
+        Result = self.registry.Result
+
+        @self.app.post('/execute')
+        def execute(
+            method: Method,  # type: ignore
+            args: t.List[Parameter] = (),  # type: ignore
+        ) -> Result:  # type: ignore
+            return self.registry.execute(obj, method.value, args)
+
+        count += 1
+
+        print(count, 'endpoints added')

tests/unittests/server/test_registry.py
@@ -1,0 +1,35 @@
+from superduperdb.server.registry import Registry
+from argparse import Namespace
+import typing as t
+import superduperdb as s
+
+
+def setup_registry(register):
+    class One(s.Serializable):
+        one = 'one'
+
+    class Two(One):
+        two = 'two'
+
+    class Three(One):
+        two = 'three'
+
+    class Object:
+        @register
+        def first(self, one: One) -> Two:
+            return Two(**one.dict())
+
+        @register
+        def second(self, one: One, three: Three) -> One:
+            return one
+
+    return Namespace(**locals())
+
+
+def test_registry():
+    registry = Registry()
+    test = setup_registry(registry.register)
+    assert registry.Parameter == t.Union[test.One, test.Three]
+    assert registry.Result == t.Union[test.One, test.Two]
+
+    return registry

tests/unittests/server/test_server.py
@@ -1,0 +1,114 @@
+from .test_registry import setup_registry
+from fastapi.testclient import TestClient
+from superduperdb.server.server import Server
+import io
+import json
+import superduperdb as s
+
+
+def setup_test_client():
+    server = Server()
+    test = setup_registry(server.register)
+
+    test.client = TestClient(server.app)
+    test.object = test.Object()
+    test.server = server
+
+    server.add_endpoints(test.object)
+
+    return test
+
+
+def test_basics():
+    with setup_test_client().client as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert response.text == s.CFG.server.fastapi.title
+
+        response = client.get('/health')
+        assert response.status_code == 200
+        assert response.text == 'ok'
+
+        response = client.get('/stats')
+        assert response.status_code == 200
+        assert response.json() == {}
+
+
+def test_methods():
+    with setup_test_client().client as client:
+        response = client.post('/first', json={'one': 'three'})
+        assert response.status_code == 200
+        assert response.json() == {'one': 'three', 'two': 'two'}
+
+
+def test_execute():
+    with setup_test_client().client as client:
+        response = client.post('/execute?method=first', json=[{'one': 'three'}])
+        assert response.status_code == 200, json.dumps(response.json(), indent=2)
+        assert response.json() == {'one': 'three', 'two': 'two'}
+
+
+class One(s.Serializable):
+    one = 'one'
+
+
+class Two(One):
+    two = 'two'
+
+
+class Three(One):
+    two = 'three'
+
+
+class Object:
+    def first(self, one: One) -> Two:
+        return Two(**one.dict())
+
+    def second(self, one: One, three: Three) -> One:
+        return one
+
+
+def test_auto_register():
+    obj = Object()
+    server = Server()
+    server.auto_register(obj)
+    server.add_endpoints(obj)
+    with TestClient(server.app) as client:
+        response = client.post('/execute?method=first', json=[{'one': 'three'}])
+        assert response.status_code == 200, json.dumps(response.json(), indent=2)
+        assert response.json() == {'one': 'three', 'two': 'two'}
+
+
+def test_download():
+    test = setup_test_client()
+
+    blob = bytes(range(256))
+    test.server.artifact_store['test-key'] = blob
+
+    with TestClient(test.server.app) as client:
+        response = client.get('/download/test-key')
+        assert response.status_code == 200
+        assert response.content == blob
+
+
+def test_upload():
+    test = setup_test_client()
+
+    blob = bytes(range(256))
+
+    with TestClient(test.server.app) as client:
+        fp = io.BytesIO(blob)
+        files = {'file': ('test-key', fp, 'multipart/form-data')}
+        response = client.post('/upload/test-key', files=files)
+        assert response.status_code == 200, json.dumps(response.json(), indent=2)
+        assert response.json() == {'created': 'test-key'}
+
+        fp = io.BytesIO(blob)
+        files = {'file': ('test-key', fp, 'multipart/form-data')}
+        response = client.post('/upload/test-key', files=files)
+        assert response.status_code == 200, json.dumps(response.json(), indent=2)
+        assert response.json() == {'replaced': 'test-key'}
+
+        response = client.get('/download/test-key')
+        assert response.status_code == 200
+        assert response.content == blob

Pinnacle-Networks/Pinnacle

Former-commit-id: d37321120926b12c594e58cad7b5f06879dbdc2f

superduperdb/cli/serve.py
@@ -1,18 +1,8 @@
 from . import command
-from typer import Argument, Option
-from typing import List
+from pinnacledb.server.server import Server
+from tests.unittests.server.test_server import Object
 
 
-@command(help='serve')
-def serve(
-    arg: str = Argument(
-        'arg',
-        help='First argument',
-    ),
-    argv: List[str] = Argument(
-        None,
-        help='Additional arguments',
-    ),
-    opt: bool = Option(False, '--opt', '-o', help='An option'),
-):
-    pass
+@command(help='Start server')
+def serve():
+    Server().auto_run(Object())

superduperdb/server/registry.py
@@ -1,0 +1,82 @@
+import dataclasses as dc
+import inspect
+import pinnacledb as s
+import typing as t
+
+
+class NotSerializableError(ValueError):
+    pass
+
+
+@dc.dataclass(frozen=True)
+class RegistryEntry:
+    name: str
+    parameter_types: t.Sequence[t.Type]
+    result_type: t.Type
+
+
+@dc.dataclass
+class Registry:
+    entries: t.Dict[str, RegistryEntry] = dc.field(default_factory=dict)
+    Parameter: t.Optional[t.Type] = None
+    Result: t.Optional[t.Type] = None
+
+    def register(self, method: t.Callable) -> None:
+        """
+        Registers a method and its signature.
+        """
+        name = method.__name__
+        assert name not in self.entries, f'name={name} already registered'
+
+        sig = inspect.signature(method)
+        params = list(sig.parameters.items())
+        if not inspect.ismethod(method):
+            if not (params and params[0][0] == 'self'):
+                raise NotSerializableError(f'{method} not an instance method')
+            params = params[1:]
+
+        parameter_types = tuple(value.annotation for _, value in params)
+        result_type = sig.return_annotation
+
+        def is_model(t, parent=s.Serializable):
+            return t is parent or any(is_model(c, parent) for c in t.__bases__)
+
+        types = set(parameter_types + (result_type,))
+        if non_models := [t for t in types if not is_model(t)]:
+            raise NotSerializableError(f'Not serializable: {non_models}')
+
+        def unite(a, b):
+            return b if a is None else t.Union[a, b]
+
+        for r in parameter_types:
+            self.Parameter = unite(self.Parameter, r)
+        self.Result = unite(self.Result, result_type)
+        self.entries[name] = RegistryEntry(name, parameter_types, result_type)
+
+    def execute(
+        self, obj: t.Any, method: str, args: t.Sequence[s.Serializable]
+    ) -> s.Serializable:
+        """Executes a query with zero or more parameters"""
+
+        method_impl = getattr(obj, method, None)
+        assert method_impl is not None, f'No database method named {method}'
+
+        entry = self.entries.get(method)
+        assert entry, f'No method named {method}'
+
+        # Going through `execute` destroys most of the type information, so we need to
+        # validate "by hand" here
+        types = entry.parameter_types
+
+        if not types and len(args) == 1 and args[0] is None:
+            # Special case, needed for endpoint /execute-one
+            args = ()
+        elif len(args) != len(types):
+            assert False, f'In {method}, expected {len(args)} objects, got {len(types)}'
+
+        arg_types = enumerate(zip(args, types))
+        if bad := [(i, a, t) for i, (a, t) in arg_types if not isinstance(a, t)]:
+            it = (f'In param {i} of {method}, expected {t}, got {a}' for i, a, t in bad)
+            assert False, '\n'.join(it)
+
+        return method_impl(*args)

superduperdb/server/server.py
@@ -1,0 +1,129 @@
+from .registry import NotSerializableError, Registry
+from enum import Enum
+from fastapi import FastAPI, HTTPException, Response, UploadFile
+from fastapi.responses import PlainTextResponse
+from functools import cached_property
+from http import HTTPStatus as status
+from uvicorn import run
+import dataclasses as dc
+import fastapi
+import pinnacledb as s
+import typing as t
+
+MEDIA_TYPE = 'application/octet-stream'
+
+
+@dc.dataclass
+class Server:
+    cfg: s.config.Server = s.CFG.server
+    stats: t.Dict = dc.field(default_factory=dict)
+    artifact_store: t.Dict = dc.field(default_factory=dict)
+    # TODO: this is a non-thread-safe proof of concept
+
+    @cached_property
+    def app(self) -> FastAPI:
+        return FastAPI(**self.cfg.fastapi.dict())
+
+    @cached_property
+    def registry(self) -> Registry:
+        return Registry()
+
+    def register(self, *methods) -> t.Optional[t.Callable]:
+        result = None
+        for result in methods:
+            self.registry.register(result)
+        return result
+
+    def run(self, obj: t.Any) -> t.NoReturn:
+        self.add_endpoints(obj)
+        run(self.app, **self.cfg.web_server.dict())
+        assert False, 'We never get here'
+
+    def auto_register(self, obj: t.Any) -> None:
+        for k, v in vars(obj.__class__).items():
+            if callable(v) and k.islower() and not k.startswith('_'):
+                try:
+                    self.registry.register(v)
+                except NotSerializableError:
+                    pass
+
+    def auto_run(self, obj: t.Any) -> t.NoReturn:
+        self.auto_register(obj)
+        self.run(obj)
+
+    def add_endpoints(self, obj: t.Any):
+        count = 0
+
+        @self.app.get('/', response_class=PlainTextResponse)
+        def root() -> str:
+            return self.cfg.fastapi.title
+
+        count += 1
+
+        @self.app.get('/health', response_class=PlainTextResponse)
+        def health() -> str:
+            return 'ok'
+
+        count += 1
+
+        @self.app.get('/stats')
+        def stats() -> t.Dict[str, int]:
+            return self.stats
+
+        count += 1
+
+        @self.app.get('/download/{artifact_key}', response_class=Response)
+        def download(artifact_key: str) -> Response:
+            if not (content := self.artifact_store.get(artifact_key)):
+                print(self.artifact_store, artifact_key)
+                raise HTTPException(
+                    status.NOT_FOUND, f'Unknown artifact {artifact_key}'
+                )
+
+            elif not isinstance(content, bytes):
+                if not callable(encode := getattr(content, 'encode', None)):
+                    msg = f'Uncodable artifact {artifact_key}'
+                    raise HTTPException(status.NOT_FOUND, msg)
+
+                content = encode()
+
+            return fastapi.Response(
+                content=content, status_code=status.OK, media_type=MEDIA_TYPE
+            )
+
+        count += 1
+
+        @self.app.post('/upload/{artifact_key}')
+        async def upload(file: UploadFile):
+            exists = file.filename in self.artifact_store
+            self.artifact_store[file.filename] = await file.read()
+
+            action = 'replaced' if exists else 'created'
+            return {action: file.filename}
+
+        count += 1
+
+        for method_name in self.registry.entries:
+            method = getattr(obj, method_name)
+            self.app.post('/' + method_name)(method)
+
+            count += 1
+
+        # TODO: unfortunately, mypy won't accept these computed types
+        # even though FastAPI has no issue with them.
+
+        methods = sorted((e, e) for e in self.registry.entries)
+        Method = Enum('Method', methods)  # type: ignore
+        Parameter = self.registry.Parameter
+        Result = self.registry.Result
+
+        @self.app.post('/execute')
+        def execute(
+            method: Method,  # type: ignore
+            args: t.List[Parameter] = (),  # type: ignore
+        ) -> Result:  # type: ignore
+            return self.registry.execute(obj, method.value, args)
+
+        count += 1
+
+        print(count, 'endpoints added')

tests/unittests/server/test_registry.py
@@ -1,0 +1,35 @@
+from pinnacledb.server.registry import Registry
+from argparse import Namespace
+import typing as t
+import pinnacledb as s
+
+
+def setup_registry(register):
+    class One(s.Serializable):
+        one = 'one'
+
+    class Two(One):
+        two = 'two'
+
+    class Three(One):
+        two = 'three'
+
+    class Object:
+        @register
+        def first(self, one: One) -> Two:
+            return Two(**one.dict())
+
+        @register
+        def second(self, one: One, three: Three) -> One:
+            return one
+
+    return Namespace(**locals())
+
+
+def test_registry():
+    registry = Registry()
+    test = setup_registry(registry.register)
+    assert registry.Parameter == t.Union[test.One, test.Three]
+    assert registry.Result == t.Union[test.One, test.Two]
+
+    return registry

tests/unittests/server/test_server.py
@@ -1,0 +1,114 @@
+from .test_registry import setup_registry
+from fastapi.testclient import TestClient
+from pinnacledb.server.server import Server
+import io
+import json
+import pinnacledb as s
+
+
+def setup_test_client():
+    server = Server()
+    test = setup_registry(server.register)
+
+    test.client = TestClient(server.app)
+    test.object = test.Object()
+    test.server = server
+
+    server.add_endpoints(test.object)
+
+    return test
+
+
+def test_basics():
+    with setup_test_client().client as client:
+        response = client.get('/')
+        assert response.status_code == 200
+        assert response.text == s.CFG.server.fastapi.title
+
+        response = client.get('/health')
+        assert response.status_code == 200
+        assert response.text == 'ok'
+
+        response = client.get('/stats')
+        assert response.status_code == 200
+        assert response.json() == {}
+
+
+def test_methods():
+    with setup_test_client().client as client:
+        response = client.post('/first', json={'one': 'three'})
+        assert response.status_code == 200
+        assert response.json() == {'one': 'three', 'two': 'two'}
+
+
+def test_execute():
+    with setup_test_client().client as client:
+        response = client.post('/execute?method=first', json=[{'one': 'three'}])
+        assert response.status_code == 200, json.dumps(response.json(), indent=2)
+        assert response.json() == {'one': 'three', 'two': 'two'}
+
+
+class One(s.Serializable):
+    one = 'one'
+
+
+class Two(One):
+    two = 'two'
+
+
+class Three(One):
+    two = 'three'
+
+
+class Object:
+    def first(self, one: One) -> Two:
+        return Two(**one.dict())
+
+    def second(self, one: One, three: Three) -> One:
+        return one
+
+
+def test_auto_register():
+    obj = Object()
+    server = Server()
+    server.auto_register(obj)
+    server.add_endpoints(obj)
+    with TestClient(server.app) as client:
+        response = client.post('/execute?method=first', json=[{'one': 'three'}])
+        assert response.status_code == 200, json.dumps(response.json(), indent=2)
+        assert response.json() == {'one': 'three', 'two': 'two'}
+
+
+def test_download():
+    test = setup_test_client()
+
+    blob = bytes(range(256))
+    test.server.artifact_store['test-key'] = blob
+
+    with TestClient(test.server.app) as client:
+        response = client.get('/download/test-key')
+        assert response.status_code == 200
+        assert response.content == blob
+
+
+def test_upload():
+    test = setup_test_client()
+
+    blob = bytes(range(256))
+
+    with TestClient(test.server.app) as client:
+        fp = io.BytesIO(blob)
+        files = {'file': ('test-key', fp, 'multipart/form-data')}
+        response = client.post('/upload/test-key', files=files)
+        assert response.status_code == 200, json.dumps(response.json(), indent=2)
+        assert response.json() == {'created': 'test-key'}
+
+        fp = io.BytesIO(blob)
+        files = {'file': ('test-key', fp, 'multipart/form-data')}
+        response = client.post('/upload/test-key', files=files)
+        assert response.status_code == 200, json.dumps(response.json(), indent=2)
+        assert response.json() == {'replaced': 'test-key'}
+
+        response = client.get('/download/test-key')
+        assert response.status_code == 200
+        assert response.content == blob