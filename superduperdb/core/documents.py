superduper-io/superduper

Rename Type to Encoder, DataVar to Encodable

superduperdb/core/documents.py
@@ -1,21 +1,21 @@
 import dataclasses as dc
 import typing as t
-from superduperdb.core.type import DataVar
+from superduperdb.core.encoder import Encodable
 
 
 @dc.dataclass
 class Document:
     """
-    A wrapper around an instance of dict or a DataVar which may be used to dump
+    A wrapper around an instance of dict or a Encodable which may be used to dump
     that resource to a mix of jsonable content or `bytes`
     """
 
-    content: t.Union[t.Dict, DataVar]
+    content: t.Union[t.Dict, Encodable]
 
     def _encode(self, r: t.Any):
         if isinstance(r, dict):
             return {k: self._encode(v) for k, v in r.items()}
-        elif isinstance(r, DataVar):
+        elif isinstance(r, Encodable):
             return r.encode()
         return r
 
@@ -50,7 +50,7 @@ class Document:
 
     @classmethod
     def _unpack_datavars(cls, item: t.Any):
-        if isinstance(item, DataVar):
+        if isinstance(item, Encodable):
             return item.x
         elif isinstance(item, dict):
             return {k: cls._unpack_datavars(v) for k, v in item.items()}

superduperdb/core/vector_index.py
@@ -10,8 +10,8 @@ from superduperdb.core.base import (
     DBPlaceholder,
 )
 from superduperdb.core.documents import Document
+from superduperdb.core.encoder import Encodable
 from superduperdb.core.metric import Metric
-from superduperdb.core.type import DataVar
 from superduperdb.core.watcher import Watcher
 from superduperdb.core.model import Model
 from superduperdb.datalayer.base.query import Select
@@ -90,7 +90,7 @@ class VectorIndex(Component):
                     self.indexing_watcher.key,
                     self.indexing_watcher.model.identifier,
                 )
-                if isinstance(h, DataVar):
+                if isinstance(h, Encodable):
                     h = h.x
                 vector_collection.add(
                     [VectorCollectionItem.create(id=str(id), vector=h)]

superduperdb/models/sentence_transformers/wrapper.py
@@ -2,8 +2,8 @@ from typing import Optional, Union
 
 from sentence_transformers import SentenceTransformer as _SentenceTransformer
 
+from superduperdb.core.encoder import Encoder
 from superduperdb.core.model import Model
-from superduperdb.core.type import Type
 
 
 class SentenceTransformer(Model):
@@ -11,7 +11,7 @@ class SentenceTransformer(Model):
         self,
         model_name_or_path: Optional[str] = None,
         identifier: Optional[str] = None,
-        type: Optional[Union[str, Type]] = None,
+        type: Optional[Union[Encoder, str]] = None,
     ):
         if identifier is None:
             identifier = model_name_or_path

superduperdb/models/torch/wrapper.py
@@ -5,7 +5,7 @@ import torch
 from torch.utils import data
 
 from superduperdb.core.documents import Document
-from superduperdb.core.type import DataVar
+from superduperdb.core.encoder import Encodable
 from superduperdb.misc import progress
 from superduperdb.core.model import Model
 from superduperdb.models.torch.utils import device_of, to_device, eval
@@ -177,7 +177,7 @@ class BasicDataset(data.Dataset):
         document = self.documents[item]
         if isinstance(document, Document):
             document = document.unpack()
-        elif isinstance(document, DataVar):
+        elif isinstance(document, Encodable):
             document = document.x
         return self.transform(document)
 

superduperdb/types/numpy/array.py
@@ -1,7 +1,7 @@
 import numpy
 import typing as t
 
-from superduperdb.core.type import Type
+from superduperdb.core.encoder import Encoder
 from superduperdb.types.utils import str_shape
 
 
@@ -23,7 +23,7 @@ class DecodeArray:
 
 
 def array(dtype: str, shape: t.Tuple):
-    return Type(
+    return Encoder(
         identifier=f'numpy.{dtype}[{str_shape(shape)}]',
         encoder=EncodeArray(dtype),
         decoder=DecodeArray(dtype),

superduperdb/types/pillow/image.py
@@ -3,7 +3,7 @@ import PIL.JpegImagePlugin
 import PIL.PngImagePlugin
 import io
 
-from superduperdb.core.type import Type
+from superduperdb.core.encoder import Encoder
 
 
 def encode_pil_image(x):
@@ -16,7 +16,7 @@ def decode_pil_image(bytes):
     return PIL.Image.open(io.BytesIO(bytes))
 
 
-pil_image = Type(
+pil_image = Encoder(
     'pil_image',
     encoder=encode_pil_image,
     decoder=decode_pil_image,

superduperdb/types/torch/tensor.py
@@ -2,7 +2,7 @@ import numpy
 import torch
 import typing as t
 
-from superduperdb.core.type import Type
+from superduperdb.core.encoder import Encoder
 from superduperdb.types.utils import str_shape
 
 
@@ -25,7 +25,7 @@ class DecodeTensor:
 
 
 def tensor(dtype, shape: t.Tuple):
-    return Type(
+    return Encoder(
         identifier=f'{str(dtype)}[{str_shape(shape)}]',
         encoder=EncodeTensor(dtype),
         decoder=DecodeTensor(dtype),

tests/unittests/datalayer/mongodb/test_database.py
@@ -5,9 +5,9 @@ import torch
 
 from superduperdb.core.base import Placeholder
 from superduperdb.core.documents import Document
+from superduperdb.core.encoder import Encoder
 from superduperdb.core.exceptions import ComponentInUseError, ComponentInUseWarning
 from superduperdb.core.learning_task import LearningTask
-from superduperdb.core.type import Type
 from superduperdb.core.watcher import Watcher
 from superduperdb.datalayer.mongodb.query import Select, Insert, Update, Delete
 from superduperdb.models.torch.wrapper import SuperDuperModule
@@ -94,7 +94,7 @@ def test_compound_component(empty):
     assert isinstance(m.type, Placeholder)
 
     m = empty.load(variety='model', identifier='my-test-module', repopulate=True)
-    assert isinstance(m.type, Type)
+    assert isinstance(m.type, Encoder)
 
     with pytest.raises(ComponentInUseError):
         empty.remove('type', 'torch.float32[32]')
@@ -104,7 +104,7 @@ def test_compound_component(empty):
 
     # checks that can reload hidden type if part of another component
     m = empty.load(variety='model', identifier='my-test-module', repopulate=True)
-    assert isinstance(m.type, Type)
+    assert isinstance(m.type, Encoder)
 
     empty.remove('model', 'my-test-module', force=True)
 

Pinnacle-Networks/Pinnacle

Former-commit-id: 407c55a236c1b0981c069baa864d2f2bec0d793e

superduperdb/core/documents.py
@@ -1,21 +1,21 @@
 import dataclasses as dc
 import typing as t
-from pinnacledb.core.type import DataVar
+from pinnacledb.core.encoder import Encodable
 
 
 @dc.dataclass
 class Document:
     """
-    A wrapper around an instance of dict or a DataVar which may be used to dump
+    A wrapper around an instance of dict or a Encodable which may be used to dump
     that resource to a mix of jsonable content or `bytes`
     """
 
-    content: t.Union[t.Dict, DataVar]
+    content: t.Union[t.Dict, Encodable]
 
     def _encode(self, r: t.Any):
         if isinstance(r, dict):
             return {k: self._encode(v) for k, v in r.items()}
-        elif isinstance(r, DataVar):
+        elif isinstance(r, Encodable):
             return r.encode()
         return r
 
@@ -50,7 +50,7 @@ class Document:
 
     @classmethod
     def _unpack_datavars(cls, item: t.Any):
-        if isinstance(item, DataVar):
+        if isinstance(item, Encodable):
             return item.x
         elif isinstance(item, dict):
             return {k: cls._unpack_datavars(v) for k, v in item.items()}

superduperdb/core/vector_index.py
@@ -10,8 +10,8 @@ from pinnacledb.core.base import (
     DBPlaceholder,
 )
 from pinnacledb.core.documents import Document
+from pinnacledb.core.encoder import Encodable
 from pinnacledb.core.metric import Metric
-from pinnacledb.core.type import DataVar
 from pinnacledb.core.watcher import Watcher
 from pinnacledb.core.model import Model
 from pinnacledb.datalayer.base.query import Select
@@ -90,7 +90,7 @@ class VectorIndex(Component):
                     self.indexing_watcher.key,
                     self.indexing_watcher.model.identifier,
                 )
-                if isinstance(h, DataVar):
+                if isinstance(h, Encodable):
                     h = h.x
                 vector_collection.add(
                     [VectorCollectionItem.create(id=str(id), vector=h)]

superduperdb/models/sentence_transformers/wrapper.py
@@ -2,8 +2,8 @@ from typing import Optional, Union
 
 from sentence_transformers import SentenceTransformer as _SentenceTransformer
 
+from pinnacledb.core.encoder import Encoder
 from pinnacledb.core.model import Model
-from pinnacledb.core.type import Type
 
 
 class SentenceTransformer(Model):
@@ -11,7 +11,7 @@ class SentenceTransformer(Model):
         self,
         model_name_or_path: Optional[str] = None,
         identifier: Optional[str] = None,
-        type: Optional[Union[str, Type]] = None,
+        type: Optional[Union[Encoder, str]] = None,
     ):
         if identifier is None:
             identifier = model_name_or_path

superduperdb/models/torch/wrapper.py
@@ -5,7 +5,7 @@ import torch
 from torch.utils import data
 
 from pinnacledb.core.documents import Document
-from pinnacledb.core.type import DataVar
+from pinnacledb.core.encoder import Encodable
 from pinnacledb.misc import progress
 from pinnacledb.core.model import Model
 from pinnacledb.models.torch.utils import device_of, to_device, eval
@@ -177,7 +177,7 @@ class BasicDataset(data.Dataset):
         document = self.documents[item]
         if isinstance(document, Document):
             document = document.unpack()
-        elif isinstance(document, DataVar):
+        elif isinstance(document, Encodable):
             document = document.x
         return self.transform(document)
 

superduperdb/types/numpy/array.py
@@ -1,7 +1,7 @@
 import numpy
 import typing as t
 
-from pinnacledb.core.type import Type
+from pinnacledb.core.encoder import Encoder
 from pinnacledb.types.utils import str_shape
 
 
@@ -23,7 +23,7 @@ class DecodeArray:
 
 
 def array(dtype: str, shape: t.Tuple):
-    return Type(
+    return Encoder(
         identifier=f'numpy.{dtype}[{str_shape(shape)}]',
         encoder=EncodeArray(dtype),
         decoder=DecodeArray(dtype),

superduperdb/types/pillow/image.py
@@ -3,7 +3,7 @@ import PIL.JpegImagePlugin
 import PIL.PngImagePlugin
 import io
 
-from pinnacledb.core.type import Type
+from pinnacledb.core.encoder import Encoder
 
 
 def encode_pil_image(x):
@@ -16,7 +16,7 @@ def decode_pil_image(bytes):
     return PIL.Image.open(io.BytesIO(bytes))
 
 
-pil_image = Type(
+pil_image = Encoder(
     'pil_image',
     encoder=encode_pil_image,
     decoder=decode_pil_image,

superduperdb/types/torch/tensor.py
@@ -2,7 +2,7 @@ import numpy
 import torch
 import typing as t
 
-from pinnacledb.core.type import Type
+from pinnacledb.core.encoder import Encoder
 from pinnacledb.types.utils import str_shape
 
 
@@ -25,7 +25,7 @@ class DecodeTensor:
 
 
 def tensor(dtype, shape: t.Tuple):
-    return Type(
+    return Encoder(
         identifier=f'{str(dtype)}[{str_shape(shape)}]',
         encoder=EncodeTensor(dtype),
         decoder=DecodeTensor(dtype),

tests/unittests/datalayer/mongodb/test_database.py
@@ -5,9 +5,9 @@ import torch
 
 from pinnacledb.core.base import Placeholder
 from pinnacledb.core.documents import Document
+from pinnacledb.core.encoder import Encoder
 from pinnacledb.core.exceptions import ComponentInUseError, ComponentInUseWarning
 from pinnacledb.core.learning_task import LearningTask
-from pinnacledb.core.type import Type
 from pinnacledb.core.watcher import Watcher
 from pinnacledb.datalayer.mongodb.query import Select, Insert, Update, Delete
 from pinnacledb.models.torch.wrapper import SuperDuperModule
@@ -94,7 +94,7 @@ def test_compound_component(empty):
     assert isinstance(m.type, Placeholder)
 
     m = empty.load(variety='model', identifier='my-test-module', repopulate=True)
-    assert isinstance(m.type, Type)
+    assert isinstance(m.type, Encoder)
 
     with pytest.raises(ComponentInUseError):
         empty.remove('type', 'torch.float32[32]')
@@ -104,7 +104,7 @@ def test_compound_component(empty):
 
     # checks that can reload hidden type if part of another component
     m = empty.load(variety='model', identifier='my-test-module', repopulate=True)
-    assert isinstance(m.type, Type)
+    assert isinstance(m.type, Encoder)
 
     empty.remove('model', 'my-test-module', force=True)
 