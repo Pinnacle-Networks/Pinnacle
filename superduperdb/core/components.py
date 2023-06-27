superduper-io/superduper

Decrease circular dependencies

superduperdb/core/components.py
@@ -1,0 +1,17 @@
+from superduperdb.core.learning_task import LearningTask
+from superduperdb.core.metric import Metric
+from superduperdb.core.model import Model
+from superduperdb.core.training_configuration import TrainingConfiguration
+from superduperdb.core.encoder import Encoder
+from superduperdb.core.vector_index import VectorIndex
+from superduperdb.core.watcher import Watcher
+
+components = {
+    'watcher': Watcher,
+    'model': Model,
+    'learning_task': LearningTask,
+    'metric': Metric,
+    'training_configuration': TrainingConfiguration,
+    'type': Encoder,
+    'vector_index': VectorIndex,
+}

superduperdb/datalayer/base/cursor.py
@@ -1,6 +1,6 @@
 import typing as t
 
-from superduperdb.core import Encoder
+from superduperdb.core.encoder import Encoder
 from superduperdb.core.documents import Document
 from superduperdb.misc.special_dicts import MongoStyleDict
 

superduperdb/datalayer/base/data_backend.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 import typing as t
 
-from superduperdb.core import Model
+from superduperdb.core.model import Model
 from superduperdb.core.documents import Document
 from superduperdb.datalayer.base.cursor import SuperDuperCursor
 from superduperdb.datalayer.base.query import Insert, Select, Update, Delete

superduperdb/datalayer/base/database.py
@@ -26,7 +26,7 @@ from superduperdb.fetchers.downloads import gather_uris
 from superduperdb.misc.logger import logging
 from superduperdb.misc.special_dicts import ArgumentDefaultDict
 from superduperdb.vector_search.base import VectorDatabase
-from superduperdb.core import components
+from superduperdb.core.components import components
 
 # TODO:
 # This global variable is a temporary solution to make VectorDatabase available

Pinnacle-Networks/Pinnacle

Former-commit-id: 0bb4842e14693696bc4b43baee021db8e8558f6a

superduperdb/core/components.py
@@ -1,0 +1,17 @@
+from pinnacledb.core.learning_task import LearningTask
+from pinnacledb.core.metric import Metric
+from pinnacledb.core.model import Model
+from pinnacledb.core.training_configuration import TrainingConfiguration
+from pinnacledb.core.encoder import Encoder
+from pinnacledb.core.vector_index import VectorIndex
+from pinnacledb.core.watcher import Watcher
+
+components = {
+    'watcher': Watcher,
+    'model': Model,
+    'learning_task': LearningTask,
+    'metric': Metric,
+    'training_configuration': TrainingConfiguration,
+    'type': Encoder,
+    'vector_index': VectorIndex,
+}

superduperdb/datalayer/base/cursor.py
@@ -1,6 +1,6 @@
 import typing as t
 
-from pinnacledb.core import Encoder
+from pinnacledb.core.encoder import Encoder
 from pinnacledb.core.documents import Document
 from pinnacledb.misc.special_dicts import MongoStyleDict
 

superduperdb/datalayer/base/data_backend.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 import typing as t
 
-from pinnacledb.core import Model
+from pinnacledb.core.model import Model
 from pinnacledb.core.documents import Document
 from pinnacledb.datalayer.base.cursor import SuperDuperCursor
 from pinnacledb.datalayer.base.query import Insert, Select, Update, Delete

superduperdb/datalayer/base/database.py
@@ -26,7 +26,7 @@ from pinnacledb.fetchers.downloads import gather_uris
 from pinnacledb.misc.logger import logging
 from pinnacledb.misc.special_dicts import ArgumentDefaultDict
 from pinnacledb.vector_search.base import VectorDatabase
-from pinnacledb.core import components
+from pinnacledb.core.components import components
 
 # TODO:
 # This global variable is a temporary solution to make VectorDatabase available