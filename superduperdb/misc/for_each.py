superduper-io/superduper

Add misc/for_each.py

superduperdb/misc/for_each.py
@@ -1,0 +1,33 @@
+from superduperdb.misc import dataclasses as dc
+import superduperdb as s
+import typing as t
+
+
+def for_each(
+    fn: t.Callable[[t.Any], t.Any], item: t.Any, depth_first: bool = False
+) -> None:
+    """
+    Recursively applies a function, breadth-first.
+
+    `for_each` recurses through dicts, lists and tuples, and also through
+    members of dataclasses and Pydantic models.
+    """
+    if not depth_first:
+        fn(item)
+
+    it: t.Iterable = ()
+
+    if isinstance(item, (list, tuple)):
+        it = item
+    elif isinstance(item, dict):
+        it = item.values()
+    elif dc.is_dataclass(item):
+        it = (getattr(item, f.name) for f in dc.fields(item))
+    elif isinstance(item, s.JSONable):
+        it = (getattr(item, i) for i in item.schema()['properties'])
+
+    for i in it:
+        for_each(fn, i)
+
+    if depth_first:
+        fn(item)

tests/unittests/misc/test_for_each.py
@@ -1,0 +1,77 @@
+from pydantic import dataclasses as dc
+from superduperdb.misc.for_each import for_each
+import superduperdb as s
+import typing as t
+
+
+@dc.dataclass
+class One:
+    one: t.Dict
+    uno: t.List
+    eins: str = ''
+
+
+class Two(s.JSONable):
+    one: One
+    nine: t.List[One]
+
+
+ONE = One(one={'a': 'b'}, uno=['hello', 'world'], eins='zed')
+TWO = Two(one=ONE, nine=[ONE, One({}, [])])
+
+BREADTH = [
+    TWO,
+    ONE,
+    {'a': 'b'},
+    'b',
+    ['hello', 'world'],
+    'hello',
+    'world',
+    'zed',
+    [ONE, One(one={}, uno=[], eins='')],
+    ONE,
+    {'a': 'b'},
+    'b',
+    ['hello', 'world'],
+    'hello',
+    'world',
+    'zed',
+    One(one={}, uno=[], eins=''),
+    {},
+    [],
+    '',
+]
+DEPTH = [
+    ONE,
+    {'a': 'b'},
+    'b',
+    ['hello', 'world'],
+    'hello',
+    'world',
+    'zed',
+    [ONE, One(one={}, uno=[], eins='')],
+    ONE,
+    {'a': 'b'},
+    'b',
+    ['hello', 'world'],
+    'hello',
+    'world',
+    'zed',
+    One(one={}, uno=[], eins=''),
+    {},
+    [],
+    '',
+    TWO,
+]
+
+
+def test_for_each_breadth():
+    actual = []
+    for_each(actual.append, TWO)
+    assert actual == BREADTH
+
+
+def test_for_each_depth():
+    actual = []
+    for_each(actual.append, TWO, depth_first=True)
+    assert actual == DEPTH

Pinnacle-Networks/Pinnacle

Former-commit-id: a1375e76eee6bea1dfcd77732368b21cd9da19d2

superduperdb/misc/for_each.py
@@ -1,0 +1,33 @@
+from pinnacledb.misc import dataclasses as dc
+import pinnacledb as s
+import typing as t
+
+
+def for_each(
+    fn: t.Callable[[t.Any], t.Any], item: t.Any, depth_first: bool = False
+) -> None:
+    """
+    Recursively applies a function, breadth-first.
+
+    `for_each` recurses through dicts, lists and tuples, and also through
+    members of dataclasses and Pydantic models.
+    """
+    if not depth_first:
+        fn(item)
+
+    it: t.Iterable = ()
+
+    if isinstance(item, (list, tuple)):
+        it = item
+    elif isinstance(item, dict):
+        it = item.values()
+    elif dc.is_dataclass(item):
+        it = (getattr(item, f.name) for f in dc.fields(item))
+    elif isinstance(item, s.JSONable):
+        it = (getattr(item, i) for i in item.schema()['properties'])
+
+    for i in it:
+        for_each(fn, i)
+
+    if depth_first:
+        fn(item)

tests/unittests/misc/test_for_each.py
@@ -1,0 +1,77 @@
+from pydantic import dataclasses as dc
+from pinnacledb.misc.for_each import for_each
+import pinnacledb as s
+import typing as t
+
+
+@dc.dataclass
+class One:
+    one: t.Dict
+    uno: t.List
+    eins: str = ''
+
+
+class Two(s.JSONable):
+    one: One
+    nine: t.List[One]
+
+
+ONE = One(one={'a': 'b'}, uno=['hello', 'world'], eins='zed')
+TWO = Two(one=ONE, nine=[ONE, One({}, [])])
+
+BREADTH = [
+    TWO,
+    ONE,
+    {'a': 'b'},
+    'b',
+    ['hello', 'world'],
+    'hello',
+    'world',
+    'zed',
+    [ONE, One(one={}, uno=[], eins='')],
+    ONE,
+    {'a': 'b'},
+    'b',
+    ['hello', 'world'],
+    'hello',
+    'world',
+    'zed',
+    One(one={}, uno=[], eins=''),
+    {},
+    [],
+    '',
+]
+DEPTH = [
+    ONE,
+    {'a': 'b'},
+    'b',
+    ['hello', 'world'],
+    'hello',
+    'world',
+    'zed',
+    [ONE, One(one={}, uno=[], eins='')],
+    ONE,
+    {'a': 'b'},
+    'b',
+    ['hello', 'world'],
+    'hello',
+    'world',
+    'zed',
+    One(one={}, uno=[], eins=''),
+    {},
+    [],
+    '',
+    TWO,
+]
+
+
+def test_for_each_breadth():
+    actual = []
+    for_each(actual.append, TWO)
+    assert actual == BREADTH
+
+
+def test_for_each_depth():
+    actual = []
+    for_each(actual.append, TWO, depth_first=True)
+    assert actual == DEPTH