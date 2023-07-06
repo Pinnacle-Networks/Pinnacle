superduper-io/superduper

Reduce uncessary subpackages and short-form documentation

superduperdb/core/watcher.py
@@ -3,7 +3,7 @@ import typing as t
 from superduperdb.core.base import Component, Placeholder
 from superduperdb.core.model import Model
 from superduperdb.datalayer.base.query import Select
-from superduperdb.queries.serialization import from_dict, to_dict
+from superduperdb.misc.serialization import from_dict, to_dict
 
 
 class Watcher(Component):

superduperdb/datalayer/base/build.py
@@ -9,6 +9,12 @@ from superduperdb.datalayer.base.database import BaseDatabase
 
 
 def build_datalayer(**connections) -> BaseDatabase:
+    """
+    Build datalayer as per ``db = superduper(db)`` from configuration.
+
+    :param connections: cache of connections to reuse in the build process.
+    """
+
     def build(cfg, stores):
         cls = stores[cfg.cls]
         if connections:

superduperdb/encoders/numpy/array.py
@@ -2,7 +2,7 @@ import numpy
 import typing as t
 
 from superduperdb.core.encoder import Encoder
-from superduperdb.types.utils import str_shape
+from superduperdb.encoders.utils import str_shape
 
 
 class EncodeArray:

superduperdb/encoders/vectors/vector.py
@@ -1,5 +1,5 @@
 from superduperdb.core.encoder import Encoder
-from superduperdb.types.utils import str_shape
+from superduperdb.encoders.utils import str_shape
 
 
 def vector(shape):

tests/unittests/datalayer/mongodb/test_database.py
@@ -10,8 +10,8 @@ from superduperdb.core.encoder import Encoder
 from superduperdb.core.exceptions import ComponentInUseError, ComponentInUseWarning
 from superduperdb.core.watcher import Watcher
 from superduperdb.models.torch.wrapper import TorchModel
-from superduperdb.queries.mongodb.queries import Collection, PreLike
-from superduperdb.types.torch.tensor import tensor
+from superduperdb.datalayer.mongodb.query import Collection, PreLike
+from superduperdb.encoders.torch.tensor import tensor
 
 
 from tests.fixtures.collection import (

tests/unittests/fetchers/test_downloaders.py
@@ -1,4 +1,4 @@
-from superduperdb.fetchers.downloads import Fetcher
+from superduperdb.misc.downloads import Fetcher
 import os
 
 

tests/unittests/models/test_sklearn.py
@@ -8,7 +8,7 @@ from sklearn.base import TransformerMixin
 
 from superduperdb.core.documents import Document
 from superduperdb.models.sklearn.wrapper import Pipeline
-from superduperdb.queries.mongodb.queries import Collection
+from superduperdb.datalayer.mongodb.query import Collection
 
 from tests.fixtures.collection import random_arrays, arrays, empty
 

tests/unittests/training/sklearn/test_trainer.py
@@ -4,7 +4,7 @@ from sklearn.svm import SVC
 from superduperdb.core.fit import Fit
 from superduperdb.models.sklearn.wrapper import Pipeline, SklearnTrainingConfiguration
 from superduperdb.models.vanilla.wrapper import FunctionWrapper
-from superduperdb.queries.mongodb.queries import Collection
+from superduperdb.datalayer.mongodb.query import Collection
 
 from tests.fixtures.collection import random_arrays, arrays, empty
 

Pinnacle-Networks/Pinnacle

Former-commit-id: cd59ae97776495ca24149a7ee96152a632689a5a

superduperdb/core/watcher.py
@@ -3,7 +3,7 @@ import typing as t
 from pinnacledb.core.base import Component, Placeholder
 from pinnacledb.core.model import Model
 from pinnacledb.datalayer.base.query import Select
-from pinnacledb.queries.serialization import from_dict, to_dict
+from pinnacledb.misc.serialization import from_dict, to_dict
 
 
 class Watcher(Component):

superduperdb/datalayer/base/build.py
@@ -9,6 +9,12 @@ from pinnacledb.datalayer.base.database import BaseDatabase
 
 
 def build_datalayer(**connections) -> BaseDatabase:
+    """
+    Build datalayer as per ``db = pinnacle(db)`` from configuration.
+
+    :param connections: cache of connections to reuse in the build process.
+    """
+
     def build(cfg, stores):
         cls = stores[cfg.cls]
         if connections:

superduperdb/encoders/numpy/array.py
@@ -2,7 +2,7 @@ import numpy
 import typing as t
 
 from pinnacledb.core.encoder import Encoder
-from pinnacledb.types.utils import str_shape
+from pinnacledb.encoders.utils import str_shape
 
 
 class EncodeArray:

superduperdb/encoders/vectors/vector.py
@@ -1,5 +1,5 @@
 from pinnacledb.core.encoder import Encoder
-from pinnacledb.types.utils import str_shape
+from pinnacledb.encoders.utils import str_shape
 
 
 def vector(shape):

tests/unittests/datalayer/mongodb/test_database.py
@@ -10,8 +10,8 @@ from pinnacledb.core.encoder import Encoder
 from pinnacledb.core.exceptions import ComponentInUseError, ComponentInUseWarning
 from pinnacledb.core.watcher import Watcher
 from pinnacledb.models.torch.wrapper import TorchModel
-from pinnacledb.queries.mongodb.queries import Collection, PreLike
-from pinnacledb.types.torch.tensor import tensor
+from pinnacledb.datalayer.mongodb.query import Collection, PreLike
+from pinnacledb.encoders.torch.tensor import tensor
 
 
 from tests.fixtures.collection import (

tests/unittests/fetchers/test_downloaders.py
@@ -1,4 +1,4 @@
-from pinnacledb.fetchers.downloads import Fetcher
+from pinnacledb.misc.downloads import Fetcher
 import os
 
 

tests/unittests/models/test_sklearn.py
@@ -8,7 +8,7 @@ from sklearn.base import TransformerMixin
 
 from pinnacledb.core.documents import Document
 from pinnacledb.models.sklearn.wrapper import Pipeline
-from pinnacledb.queries.mongodb.queries import Collection
+from pinnacledb.datalayer.mongodb.query import Collection
 
 from tests.fixtures.collection import random_arrays, arrays, empty
 

tests/unittests/training/sklearn/test_trainer.py
@@ -4,7 +4,7 @@ from sklearn.svm import SVC
 from pinnacledb.core.fit import Fit
 from pinnacledb.models.sklearn.wrapper import Pipeline, SklearnTrainingConfiguration
 from pinnacledb.models.vanilla.wrapper import FunctionWrapper
-from pinnacledb.queries.mongodb.queries import Collection
+from pinnacledb.datalayer.mongodb.query import Collection
 
 from tests.fixtures.collection import random_arrays, arrays, empty
 