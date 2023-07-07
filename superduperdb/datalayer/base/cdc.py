superduper-io/superduper

Add taskworflow execution on cdc
Fix minor bugs and typos

minor refactors

logger to logging

formatting and refactors

superduperdb/datalayer/base/cdc.py
@@ -1,57 +1,112 @@
 import threading
-import typing as T
+import typing as t
 import datetime
 
 from superduperdb.queries.mongodb.queries import Collection
-from superduperdb.misc.logger import logging as logger
+from superduperdb.misc.logger import logging
+from superduperdb.datalayer.base.database import BaseDatabase
 from superduperdb.datalayer.mongodb.cdc import GenericDatabaseWatch
 from superduperdb.datalayer.mongodb.cdc import MongoDatabaseWatcher
 from superduperdb.misc.docs import api
 
-DBWatcherType = T.TypeVar("DBWatcherType", bound=GenericDatabaseWatch)
+DBWatcherType = t.TypeVar('DBWatcherType', bound=MongoDatabaseWatcher)
+
+
+def duck_type_mongodb(item):
+    return hasattr(item, 'list_collection_names')
 
 
 class _DatabaseWatcherThreadScheduler(threading.Thread):
     def __init__(self, watcher) -> None:
-        threading.Thread.__init__(self, daemon=True)
+        threading.Thread.__init__(self, daemon=False)
         self.watcher = watcher
-        logger.info(f"Database Watch service started at {datetime.datetime.now()}")
+        logging.info(f'Database watch service started at {datetime.datetime.now()}')
 
     def run(self) -> None:
         self.watcher.cdc()
 
-    def close(self) -> None:
-        self.watcher.close()
 
+class DatabaseWatcherFactory(t.Generic[DBWatcherType]):
+    """DatabaseWatcherFactory.
+    A Factory class to create instance of DatabaseWatcher corresponding to the
+    `db_type`.
+    """
 
-class DatabaseWatcherFactory(T.Generic[DBWatcherType]):
-    SUPPORTED_WATCHERS: T.List[T.Text] = ['mongodb']
+    SUPPORTED_WATCHERS: t.List[str] = ['mongodb']
 
-    def __init__(self, db_type: T.Text = 'mongodb'):
+    def __init__(self, db_type: str = 'mongodb'):
         if db_type not in self.SUPPORTED_WATCHERS:
-            raise NotImplementedError(f"{db_type} is not supported yet for CDC.")
+            raise ValueError(f'{db_type} is not supported yet for CDC.')
         self.watcher = db_type
 
-    def create(self, *args, **kwargs) -> T.Optional[DBWatcherType]:
-        if self.watcher == "mongo":
+    def create(self, *args, **kwargs) -> t.Optional[DBWatcherType]:
+        if self.watcher == 'mongodb':
             watcher = MongoDatabaseWatcher(*args, **kwargs)
             scheduler = _DatabaseWatcherThreadScheduler(watcher)
             watcher.attach_scheduler(scheduler)
             return watcher
 
 
-@api("alpha")
+@api('alpha')
 class DatabaseWatcher:
+    """
+    Change Data Capture (CDC) is a mechanism used in database systems to track
+    and capture changes made to a table or collection in real-time.
+    It allows applications to stay up-to-date with the latest changes in the database
+    and perform various tasks, such as data synchronization, auditing,
+    or data integration. The ChangeDataCapture class is designed to
+    handle CDC tasksfor a specified table/collection in a database.
+
+    Change streams allow applications to access real-time data changes
+    without the complexity and risk of tailing the oplog.
+    Applications can use change streams to subscribe to all data
+    changes on a single collection,a database, or an entire deployment,
+    and immediately react to them.
+    Because change streams use the aggregation framework, applications
+    can also filter for specific changes or transform the notifications at will.
+
+    ref: https://www.mongodb.com/docs/manual/changeStreams/
+
+    Use this module like this::
+        db = any_arbitary_database.connect(...)
+        db = superduperdb(db)
+        watcher = DatabaseWatcher(db=db, on=Collection('test_collection'))
+        watcher.watch()
+    """
+
     identity_sep = '/'
 
     def __new__(
-        cls, db: 'BaseDatabase', on: Collection = None, *args, **kwargs
-    ) -> DBWatcherType:
-        assert on is not None, "`DatabaseWatcher` needs a source collection to watch."
-        # TODO: check what the db variety
-        db_type = db.variety
-        watcher_type = GenericDatabaseWatch
-        if db_type == "mongodb":
-            watcher_type = MongoDatabaseWatcher
-        db_factory = DatabaseWatcherFactory[watcher_type](db_type=db_type)
-        return db_factory.create(db=db, on=on, *args, **kwargs)
+        cls,
+        db: 'BaseDatabase',
+        on: Collection,
+        identifier: str = '',
+        *args,
+        **kwargs,
+    ) -> t.Optional[GenericDatabaseWatch]:
+        """__new__.
+        A method which creates instance of `GenericDatabaseWatcher` corresponding to the
+        `db`.
+
+        This returns a instance of subclass of `GenericDatabaseWatcher`
+
+        :param db: A superduperdb instance.
+        :type db: 'BaseDatabase'
+        :param on: It defines on which collection/table
+        watcher service should be invoke.
+        :type on: Collection
+        :param identifier: A identity given to the watcher service.
+        :type identifier: str
+        :param args:
+        :param kwargs:
+        :rtype: GenericDatabaseWatch
+        """
+        assert on is not None, '`DatabaseWatcher` needs a source collection to watch.'
+        if duck_type_mongodb(db.db):
+            db_type = 'mongodb'
+            db_factory = DatabaseWatcherFactory[MongoDatabaseWatcher](db_type=db_type)
+            return db_factory.create(
+                db=db, on=on, identifier=identifier, *args, **kwargs
+            )
+        else:
+            raise NotImplementedError('Database not supported yet!')

Pinnacle-Networks/Pinnacle

Former-commit-id: fdb155e6fbf68bbba473f9bb6a8cca72ca22b7ea

superduperdb/datalayer/base/cdc.py
@@ -1,57 +1,112 @@
 import threading
-import typing as T
+import typing as t
 import datetime
 
 from pinnacledb.queries.mongodb.queries import Collection
-from pinnacledb.misc.logger import logging as logger
+from pinnacledb.misc.logger import logging
+from pinnacledb.datalayer.base.database import BaseDatabase
 from pinnacledb.datalayer.mongodb.cdc import GenericDatabaseWatch
 from pinnacledb.datalayer.mongodb.cdc import MongoDatabaseWatcher
 from pinnacledb.misc.docs import api
 
-DBWatcherType = T.TypeVar("DBWatcherType", bound=GenericDatabaseWatch)
+DBWatcherType = t.TypeVar('DBWatcherType', bound=MongoDatabaseWatcher)
+
+
+def duck_type_mongodb(item):
+    return hasattr(item, 'list_collection_names')
 
 
 class _DatabaseWatcherThreadScheduler(threading.Thread):
     def __init__(self, watcher) -> None:
-        threading.Thread.__init__(self, daemon=True)
+        threading.Thread.__init__(self, daemon=False)
         self.watcher = watcher
-        logger.info(f"Database Watch service started at {datetime.datetime.now()}")
+        logging.info(f'Database watch service started at {datetime.datetime.now()}')
 
     def run(self) -> None:
         self.watcher.cdc()
 
-    def close(self) -> None:
-        self.watcher.close()
 
+class DatabaseWatcherFactory(t.Generic[DBWatcherType]):
+    """DatabaseWatcherFactory.
+    A Factory class to create instance of DatabaseWatcher corresponding to the
+    `db_type`.
+    """
 
-class DatabaseWatcherFactory(T.Generic[DBWatcherType]):
-    SUPPORTED_WATCHERS: T.List[T.Text] = ['mongodb']
+    SUPPORTED_WATCHERS: t.List[str] = ['mongodb']
 
-    def __init__(self, db_type: T.Text = 'mongodb'):
+    def __init__(self, db_type: str = 'mongodb'):
         if db_type not in self.SUPPORTED_WATCHERS:
-            raise NotImplementedError(f"{db_type} is not supported yet for CDC.")
+            raise ValueError(f'{db_type} is not supported yet for CDC.')
         self.watcher = db_type
 
-    def create(self, *args, **kwargs) -> T.Optional[DBWatcherType]:
-        if self.watcher == "mongo":
+    def create(self, *args, **kwargs) -> t.Optional[DBWatcherType]:
+        if self.watcher == 'mongodb':
             watcher = MongoDatabaseWatcher(*args, **kwargs)
             scheduler = _DatabaseWatcherThreadScheduler(watcher)
             watcher.attach_scheduler(scheduler)
             return watcher
 
 
-@api("alpha")
+@api('alpha')
 class DatabaseWatcher:
+    """
+    Change Data Capture (CDC) is a mechanism used in database systems to track
+    and capture changes made to a table or collection in real-time.
+    It allows applications to stay up-to-date with the latest changes in the database
+    and perform various tasks, such as data synchronization, auditing,
+    or data integration. The ChangeDataCapture class is designed to
+    handle CDC tasksfor a specified table/collection in a database.
+
+    Change streams allow applications to access real-time data changes
+    without the complexity and risk of tailing the oplog.
+    Applications can use change streams to subscribe to all data
+    changes on a single collection,a database, or an entire deployment,
+    and immediately react to them.
+    Because change streams use the aggregation framework, applications
+    can also filter for specific changes or transform the notifications at will.
+
+    ref: https://www.mongodb.com/docs/manual/changeStreams/
+
+    Use this module like this::
+        db = any_arbitary_database.connect(...)
+        db = pinnacledb(db)
+        watcher = DatabaseWatcher(db=db, on=Collection('test_collection'))
+        watcher.watch()
+    """
+
     identity_sep = '/'
 
     def __new__(
-        cls, db: 'BaseDatabase', on: Collection = None, *args, **kwargs
-    ) -> DBWatcherType:
-        assert on is not None, "`DatabaseWatcher` needs a source collection to watch."
-        # TODO: check what the db variety
-        db_type = db.variety
-        watcher_type = GenericDatabaseWatch
-        if db_type == "mongodb":
-            watcher_type = MongoDatabaseWatcher
-        db_factory = DatabaseWatcherFactory[watcher_type](db_type=db_type)
-        return db_factory.create(db=db, on=on, *args, **kwargs)
+        cls,
+        db: 'BaseDatabase',
+        on: Collection,
+        identifier: str = '',
+        *args,
+        **kwargs,
+    ) -> t.Optional[GenericDatabaseWatch]:
+        """__new__.
+        A method which creates instance of `GenericDatabaseWatcher` corresponding to the
+        `db`.
+
+        This returns a instance of subclass of `GenericDatabaseWatcher`
+
+        :param db: A pinnacledb instance.
+        :type db: 'BaseDatabase'
+        :param on: It defines on which collection/table
+        watcher service should be invoke.
+        :type on: Collection
+        :param identifier: A identity given to the watcher service.
+        :type identifier: str
+        :param args:
+        :param kwargs:
+        :rtype: GenericDatabaseWatch
+        """
+        assert on is not None, '`DatabaseWatcher` needs a source collection to watch.'
+        if duck_type_mongodb(db.db):
+            db_type = 'mongodb'
+            db_factory = DatabaseWatcherFactory[MongoDatabaseWatcher](db_type=db_type)
+            return db_factory.create(
+                db=db, on=on, identifier=identifier, *args, **kwargs
+            )
+        else:
+            raise NotImplementedError('Database not supported yet!')