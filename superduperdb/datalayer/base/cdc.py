superduper-io/superduper

Add batch support for cdc
docs(cdc): Added working example on cdc

added integration tests

refactors

format

superduperdb/datalayer/base/cdc.py
@@ -1,29 +1,33 @@
 import threading
+import time
 import typing as t
 import datetime
 
 from superduperdb.queries.mongodb.queries import Collection
 from superduperdb.misc.logger import logging
 from superduperdb.datalayer.base.database import BaseDatabase
-from superduperdb.datalayer.mongodb.cdc import GenericDatabaseWatch
-from superduperdb.datalayer.mongodb.cdc import MongoDatabaseWatcher
+from superduperdb.datalayer.mongodb import cdc
+from superduperdb.datalayer.base import backends
+
 from superduperdb.misc.docs import api
 
-DBWatcherType = t.TypeVar('DBWatcherType', bound=MongoDatabaseWatcher)
-
-
-def duck_type_mongodb(item):
-    return hasattr(item, 'list_collection_names')
+DBWatcherType = t.TypeVar('DBWatcherType', bound=cdc.GenericDatabaseWatch)
 
 
 class _DatabaseWatcherThreadScheduler(threading.Thread):
-    def __init__(self, watcher) -> None:
+    def __init__(
+        self, watcher: cdc.GenericDatabaseWatch, stop_event: threading.Event
+    ) -> None:
         threading.Thread.__init__(self, daemon=False)
+        self.stop_event = stop_event
         self.watcher = watcher
         logging.info(f'Database watch service started at {datetime.datetime.now()}')
 
     def run(self) -> None:
-        self.watcher.cdc()
+        cdc_stream = self.watcher.setup_cdc()
+        while not self.stop_event.is_set():
+            self.watcher.next_cdc(cdc_stream)
+            time.sleep(0.1)
 
 
 class DatabaseWatcherFactory(t.Generic[DBWatcherType]):
@@ -40,9 +44,10 @@ class DatabaseWatcherFactory(t.Generic[DBWatcherType]):
         self.watcher = db_type
 
     def create(self, *args, **kwargs) -> t.Optional[DBWatcherType]:
+        stop_event = threading.Event()
         if self.watcher == 'mongodb':
-            watcher = MongoDatabaseWatcher(*args, **kwargs)
-            scheduler = _DatabaseWatcherThreadScheduler(watcher)
+            watcher = cdc.MongoDatabaseWatcher(stop_event=stop_event, *args, **kwargs)
+            scheduler = _DatabaseWatcherThreadScheduler(watcher, stop_event=stop_event)
             watcher.attach_scheduler(scheduler)
             return watcher
 
@@ -83,7 +88,7 @@ class DatabaseWatcher:
         identifier: str = '',
         *args,
         **kwargs,
-    ) -> t.Optional[GenericDatabaseWatch]:
+    ) -> t.Optional[cdc.GenericDatabaseWatch]:
         """__new__.
         A method which creates instance of `GenericDatabaseWatcher` corresponding to the
         `db`.
@@ -102,9 +107,15 @@ class DatabaseWatcher:
         :rtype: GenericDatabaseWatch
         """
         assert on is not None, '`DatabaseWatcher` needs a source collection to watch.'
-        if duck_type_mongodb(db.db):
-            db_type = 'mongodb'
-            db_factory = DatabaseWatcherFactory[MongoDatabaseWatcher](db_type=db_type)
+        db_type = [
+            db_type
+            for db_type, db_backend in backends.data_backends.items()
+            if isinstance(db.databackend, db_backend)
+        ][0]
+        if db_type == "mongodb":
+            db_factory = DatabaseWatcherFactory[cdc.MongoDatabaseWatcher](
+                db_type=db_type
+            )
             return db_factory.create(
                 db=db, on=on, identifier=identifier, *args, **kwargs
             )

tests/integration/test_cdc.py
@@ -1,0 +1,100 @@
+import pytest
+import time
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
+from superduperdb.datalayer.base.cdc import DatabaseWatcher
+from superduperdb.queries.mongodb.queries import Collection
+from superduperdb.misc.task_queue import cdc_queue
+
+
+@patch('superduperdb.datalayer.mongodb.cdc.copy_vectors')
+@pytest.fixture()
+def watch_fixture(with_vector_index):
+    watcher = DatabaseWatcher(db=with_vector_index, on=Collection(name='documents'))
+    yield watcher
+    watcher.stop()
+
+
+class TestMongoCDC:
+    def teardown_method(self):
+        with cdc_queue.mutex:
+            cdc_queue.queue.clear()
+
+    def test_smoke(self, watch_fixture):
+        watch_fixture.watch()
+        watch_fixture.stop()
+
+    def test_single_insert(self, watch_fixture, with_vector_index, an_single_insert):
+        watch_fixture._cdc_change_handler = MagicMock()
+        watch_fixture.watch()
+        with_vector_index.execute(
+            Collection(name='documents').insert_many([an_single_insert])
+        )
+        time.sleep(3)
+        info = watch_fixture.info()
+        watch_fixture.stop()
+        assert info['inserts'] == 1
+
+    def test_many_insert(self, watch_fixture, with_vector_index, an_insert):
+        watch_fixture._cdc_change_handler = MagicMock()
+        watch_fixture.watch()
+        with_vector_index.execute(Collection(name='documents').insert_many(an_insert))
+        time.sleep(3)
+        info = watch_fixture.info()
+        watch_fixture.stop()
+        assert info['inserts'] == len(an_insert)
+
+    @patch('superduperdb.datalayer.mongodb.cdc.copy_vectors')
+    def test_task_workflow_on_insert(
+        self, mocked_copy_vectors, watch_fixture, with_vector_index, an_single_insert
+    ):
+        watch_fixture.watch()
+
+        # Adding this so that we ensure the _outputs where not produce
+        # after Insert query refresh.
+        output, _ = with_vector_index.execute(
+            Collection(name='documents').insert_many([an_single_insert], refresh=False)
+        )
+        time.sleep(3)
+        _id = output.inserted_ids[0]
+        time.sleep(8)
+        doc = with_vector_index.db['documents'].find_one({'_id': _id})
+        watch_fixture.stop()
+        assert '_outputs' in doc.keys()
+        assert 'linear_a' in doc['_outputs']['x'].keys()
+        assert 'linear_a' in doc['_outputs']['z'].keys()
+
+    def test_cdc_stop(self, watch_fixture):
+        watch_fixture.watch()
+
+        _prev_states = [
+            watch_fixture._scheduler.is_alive(),
+            watch_fixture._cdc_change_handler.is_alive(),
+        ]
+
+        watch_fixture.stop()
+        time.sleep(4)
+        _post_stop_states = [
+            watch_fixture._scheduler.is_alive(),
+            watch_fixture._cdc_change_handler.is_alive(),
+        ]
+        watch_fixture.stop()
+        assert all(
+            [
+                _prev_s != _post_s
+                for _prev_s, _post_s in zip(_prev_states, _post_stop_states)
+            ]
+        )
+
+    def test_insert_with_cdc(self, watch_fixture, with_vector_index, an_insert):
+        watch_fixture._cdc_change_handler = MagicMock()
+        watch_fixture.watch()
+        output, _ = with_vector_index.execute(
+            Collection(name='documents').insert_many(an_insert, refresh=True)
+        )
+
+        _id = output.inserted_ids[0]
+        doc = with_vector_index.db['documents'].find_one({'_id': _id})
+        watch_fixture.stop()
+        assert '_outputs' not in doc.keys()

Pinnacle-Networks/Pinnacle

Former-commit-id: 30993017dff304ea428e02005448ac86aa16277b

superduperdb/datalayer/base/cdc.py
@@ -1,29 +1,33 @@
 import threading
+import time
 import typing as t
 import datetime
 
 from pinnacledb.queries.mongodb.queries import Collection
 from pinnacledb.misc.logger import logging
 from pinnacledb.datalayer.base.database import BaseDatabase
-from pinnacledb.datalayer.mongodb.cdc import GenericDatabaseWatch
-from pinnacledb.datalayer.mongodb.cdc import MongoDatabaseWatcher
+from pinnacledb.datalayer.mongodb import cdc
+from pinnacledb.datalayer.base import backends
+
 from pinnacledb.misc.docs import api
 
-DBWatcherType = t.TypeVar('DBWatcherType', bound=MongoDatabaseWatcher)
-
-
-def duck_type_mongodb(item):
-    return hasattr(item, 'list_collection_names')
+DBWatcherType = t.TypeVar('DBWatcherType', bound=cdc.GenericDatabaseWatch)
 
 
 class _DatabaseWatcherThreadScheduler(threading.Thread):
-    def __init__(self, watcher) -> None:
+    def __init__(
+        self, watcher: cdc.GenericDatabaseWatch, stop_event: threading.Event
+    ) -> None:
         threading.Thread.__init__(self, daemon=False)
+        self.stop_event = stop_event
         self.watcher = watcher
         logging.info(f'Database watch service started at {datetime.datetime.now()}')
 
     def run(self) -> None:
-        self.watcher.cdc()
+        cdc_stream = self.watcher.setup_cdc()
+        while not self.stop_event.is_set():
+            self.watcher.next_cdc(cdc_stream)
+            time.sleep(0.1)
 
 
 class DatabaseWatcherFactory(t.Generic[DBWatcherType]):
@@ -40,9 +44,10 @@ class DatabaseWatcherFactory(t.Generic[DBWatcherType]):
         self.watcher = db_type
 
     def create(self, *args, **kwargs) -> t.Optional[DBWatcherType]:
+        stop_event = threading.Event()
         if self.watcher == 'mongodb':
-            watcher = MongoDatabaseWatcher(*args, **kwargs)
-            scheduler = _DatabaseWatcherThreadScheduler(watcher)
+            watcher = cdc.MongoDatabaseWatcher(stop_event=stop_event, *args, **kwargs)
+            scheduler = _DatabaseWatcherThreadScheduler(watcher, stop_event=stop_event)
             watcher.attach_scheduler(scheduler)
             return watcher
 
@@ -83,7 +88,7 @@ class DatabaseWatcher:
         identifier: str = '',
         *args,
         **kwargs,
-    ) -> t.Optional[GenericDatabaseWatch]:
+    ) -> t.Optional[cdc.GenericDatabaseWatch]:
         """__new__.
         A method which creates instance of `GenericDatabaseWatcher` corresponding to the
         `db`.
@@ -102,9 +107,15 @@ class DatabaseWatcher:
         :rtype: GenericDatabaseWatch
         """
         assert on is not None, '`DatabaseWatcher` needs a source collection to watch.'
-        if duck_type_mongodb(db.db):
-            db_type = 'mongodb'
-            db_factory = DatabaseWatcherFactory[MongoDatabaseWatcher](db_type=db_type)
+        db_type = [
+            db_type
+            for db_type, db_backend in backends.data_backends.items()
+            if isinstance(db.databackend, db_backend)
+        ][0]
+        if db_type == "mongodb":
+            db_factory = DatabaseWatcherFactory[cdc.MongoDatabaseWatcher](
+                db_type=db_type
+            )
             return db_factory.create(
                 db=db, on=on, identifier=identifier, *args, **kwargs
             )

tests/integration/test_cdc.py
@@ -1,0 +1,100 @@
+import pytest
+import time
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
+from pinnacledb.datalayer.base.cdc import DatabaseWatcher
+from pinnacledb.queries.mongodb.queries import Collection
+from pinnacledb.misc.task_queue import cdc_queue
+
+
+@patch('pinnacledb.datalayer.mongodb.cdc.copy_vectors')
+@pytest.fixture()
+def watch_fixture(with_vector_index):
+    watcher = DatabaseWatcher(db=with_vector_index, on=Collection(name='documents'))
+    yield watcher
+    watcher.stop()
+
+
+class TestMongoCDC:
+    def teardown_method(self):
+        with cdc_queue.mutex:
+            cdc_queue.queue.clear()
+
+    def test_smoke(self, watch_fixture):
+        watch_fixture.watch()
+        watch_fixture.stop()
+
+    def test_single_insert(self, watch_fixture, with_vector_index, an_single_insert):
+        watch_fixture._cdc_change_handler = MagicMock()
+        watch_fixture.watch()
+        with_vector_index.execute(
+            Collection(name='documents').insert_many([an_single_insert])
+        )
+        time.sleep(3)
+        info = watch_fixture.info()
+        watch_fixture.stop()
+        assert info['inserts'] == 1
+
+    def test_many_insert(self, watch_fixture, with_vector_index, an_insert):
+        watch_fixture._cdc_change_handler = MagicMock()
+        watch_fixture.watch()
+        with_vector_index.execute(Collection(name='documents').insert_many(an_insert))
+        time.sleep(3)
+        info = watch_fixture.info()
+        watch_fixture.stop()
+        assert info['inserts'] == len(an_insert)
+
+    @patch('pinnacledb.datalayer.mongodb.cdc.copy_vectors')
+    def test_task_workflow_on_insert(
+        self, mocked_copy_vectors, watch_fixture, with_vector_index, an_single_insert
+    ):
+        watch_fixture.watch()
+
+        # Adding this so that we ensure the _outputs where not produce
+        # after Insert query refresh.
+        output, _ = with_vector_index.execute(
+            Collection(name='documents').insert_many([an_single_insert], refresh=False)
+        )
+        time.sleep(3)
+        _id = output.inserted_ids[0]
+        time.sleep(8)
+        doc = with_vector_index.db['documents'].find_one({'_id': _id})
+        watch_fixture.stop()
+        assert '_outputs' in doc.keys()
+        assert 'linear_a' in doc['_outputs']['x'].keys()
+        assert 'linear_a' in doc['_outputs']['z'].keys()
+
+    def test_cdc_stop(self, watch_fixture):
+        watch_fixture.watch()
+
+        _prev_states = [
+            watch_fixture._scheduler.is_alive(),
+            watch_fixture._cdc_change_handler.is_alive(),
+        ]
+
+        watch_fixture.stop()
+        time.sleep(4)
+        _post_stop_states = [
+            watch_fixture._scheduler.is_alive(),
+            watch_fixture._cdc_change_handler.is_alive(),
+        ]
+        watch_fixture.stop()
+        assert all(
+            [
+                _prev_s != _post_s
+                for _prev_s, _post_s in zip(_prev_states, _post_stop_states)
+            ]
+        )
+
+    def test_insert_with_cdc(self, watch_fixture, with_vector_index, an_insert):
+        watch_fixture._cdc_change_handler = MagicMock()
+        watch_fixture.watch()
+        output, _ = with_vector_index.execute(
+            Collection(name='documents').insert_many(an_insert, refresh=True)
+        )
+
+        _id = output.inserted_ids[0]
+        doc = with_vector_index.db['documents'].find_one({'_id': _id})
+        watch_fixture.stop()
+        assert '_outputs' not in doc.keys()