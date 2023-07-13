superduper-io/superduper

Fix distributed execution of taskgraph on dask

superduperdb/cluster/dask/dask_client.py
@@ -1,10 +1,45 @@
-from dask.distributed import Client
-from superduperdb import CFG
+import uuid
+
+from dask.distributed import Client, wait, fire_and_forget
+
+from superduperdb.misc.logger import logging
 
 
-def dask_client():
-    return Client(
-        address=f'tcp://{CFG.dask.ip}:{CFG.dask.port}',
-        serializers=CFG.dask.serializers,
-        deserializers=CFG.dask.deserializers,
+class DaskClient:
+    def __init__(self, address: str, serializers=None, deserializers=None):
+        self.futures_collection = {}
+        self.client = Client(
+            address=address,
+            serializers=serializers,
+            deserializers=deserializers,
+        )
+
+    def submit(self, function, **kwargs):
+        future = self.client.submit(function, **kwargs)
+        identifier = kwargs.get('identifier', None)
+        if not identifier:
+            logging.warning('Could not get an identifier from submitted function, creating one!')
+            identifier = str(uuid.uuid4())
+        self.futures_collection[identifier] = future
+        return future
+
+    def submit_and_forget(self, function, **kwargs):
+        future = self.submit(function, **kwargs)
+        fire_and_forget(future)
+        return future
+
+    def wait_all_pending_task(self):
+        futures = self.futures_collection.values()
+        wait(futures)
+
+    def get_result(self, identifier: str):
+        future = self.futures_collection[identifier]
+        return self.client.gather(future)
+
+
+def dask_client(cfg):
+    return DaskClient(
+        address=f'tcp://{cfg.ip}:{cfg.port}',
+        serializers=cfg.serializers,
+        deserializers=cfg.deserializers,
     )

superduperdb/datalayer/base/build.py
@@ -1,3 +1,5 @@
+import pymongo
+
 from superduperdb import CFG
 from superduperdb.datalayer.base.backends import (
     data_backends,
@@ -7,6 +9,7 @@ from superduperdb.datalayer.base.backends import (
     connections as default_connections,
 )
 from superduperdb.datalayer.base.database import BaseDatabase
+from superduperdb.cluster.dask.dask_client import dask_client
 
 
 def build_datalayer(**connections) -> BaseDatabase:
@@ -15,10 +18,21 @@ def build_datalayer(**connections) -> BaseDatabase:
 
     :param connections: cache of connections to reuse in the build process.
     """
+    """
+    connection_name = CFG.data_layers.data_backend.connection
+    connection = connections[connection_name] 
+    if isinstance(connection, dict): # This implies db config.
+        connection = pymongo.MongoClient(**connection)
+        connections[connection_name] = connection
+    """
 
     def build_vector_database(cfg, stores):
         cls = stores[cfg.__class__]
         return cls(cfg)
+
+    def build_distributed_client(cfg):
+        if cfg.remote:
+            return dask_client(cfg.dask)
 
     def build(cfg, stores):
         cls = stores[cfg.cls]
@@ -36,4 +50,5 @@ def build_datalayer(**connections) -> BaseDatabase:
         vector_database=build_vector_database(
             CFG.vector_search.type, vector_database_stores
         ),
+        distributed_client=build_distributed_client(CFG)
     )

tests/integration/test_dask.py
@@ -1,0 +1,34 @@
+import time
+from unittest.mock import patch
+
+import pytest
+
+from superduperdb.datalayer.mongodb.query import Collection
+from superduperdb.cluster.dask.dask_client import dask_client
+
+
+def test_taskgraph_futures_with_dask(random_data, a_watcher, an_update):
+    from superduperdb.misc.configs import CONFIG
+    with patch.object(CONFIG.config, "remote", True):
+        random_data.remote = True
+        client = dask_client(CONFIG.config.dask)
+        random_data._distributed_client = client
+        output, graph = random_data.execute(Collection(name='documents').insert_many(an_update))
+
+    client.wait_all_pending_task()
+    time.sleep(3)
+    nodes = graph.G.nodes
+    jobs = [nodes[node]['job'] for node in nodes]
+    assert all([job.future.status == 'finished' for job in jobs])
+
+def test_insert_with_dask(random_data, a_watcher, an_update):
+    from superduperdb.misc.configs import CONFIG
+    with patch.object(CONFIG.config, "remote", True):
+        random_data.remote = True
+        client = dask_client(CONFIG.config.dask)
+        random_data._distributed_client = client
+        random_data.execute(Collection(name='documents').insert_many(an_update))
+        time.sleep(3)
+        r = next(random_data.execute(Collection(name='documents').find({'update': True})))
+        assert 'linear_a' in r['_outputs']['x']
+

Pinnacle-Networks/Pinnacle

Former-commit-id: 3bc36a712c7da66695490b4395b57b30e508ba47

superduperdb/cluster/dask/dask_client.py
@@ -1,10 +1,45 @@
-from dask.distributed import Client
-from pinnacledb import CFG
+import uuid
+
+from dask.distributed import Client, wait, fire_and_forget
+
+from pinnacledb.misc.logger import logging
 
 
-def dask_client():
-    return Client(
-        address=f'tcp://{CFG.dask.ip}:{CFG.dask.port}',
-        serializers=CFG.dask.serializers,
-        deserializers=CFG.dask.deserializers,
+class DaskClient:
+    def __init__(self, address: str, serializers=None, deserializers=None):
+        self.futures_collection = {}
+        self.client = Client(
+            address=address,
+            serializers=serializers,
+            deserializers=deserializers,
+        )
+
+    def submit(self, function, **kwargs):
+        future = self.client.submit(function, **kwargs)
+        identifier = kwargs.get('identifier', None)
+        if not identifier:
+            logging.warning('Could not get an identifier from submitted function, creating one!')
+            identifier = str(uuid.uuid4())
+        self.futures_collection[identifier] = future
+        return future
+
+    def submit_and_forget(self, function, **kwargs):
+        future = self.submit(function, **kwargs)
+        fire_and_forget(future)
+        return future
+
+    def wait_all_pending_task(self):
+        futures = self.futures_collection.values()
+        wait(futures)
+
+    def get_result(self, identifier: str):
+        future = self.futures_collection[identifier]
+        return self.client.gather(future)
+
+
+def dask_client(cfg):
+    return DaskClient(
+        address=f'tcp://{cfg.ip}:{cfg.port}',
+        serializers=cfg.serializers,
+        deserializers=cfg.deserializers,
     )

superduperdb/datalayer/base/build.py
@@ -1,3 +1,5 @@
+import pymongo
+
 from pinnacledb import CFG
 from pinnacledb.datalayer.base.backends import (
     data_backends,
@@ -7,6 +9,7 @@ from pinnacledb.datalayer.base.backends import (
     connections as default_connections,
 )
 from pinnacledb.datalayer.base.database import BaseDatabase
+from pinnacledb.cluster.dask.dask_client import dask_client
 
 
 def build_datalayer(**connections) -> BaseDatabase:
@@ -15,10 +18,21 @@ def build_datalayer(**connections) -> BaseDatabase:
 
     :param connections: cache of connections to reuse in the build process.
     """
+    """
+    connection_name = CFG.data_layers.data_backend.connection
+    connection = connections[connection_name] 
+    if isinstance(connection, dict): # This implies db config.
+        connection = pymongo.MongoClient(**connection)
+        connections[connection_name] = connection
+    """
 
     def build_vector_database(cfg, stores):
         cls = stores[cfg.__class__]
         return cls(cfg)
+
+    def build_distributed_client(cfg):
+        if cfg.remote:
+            return dask_client(cfg.dask)
 
     def build(cfg, stores):
         cls = stores[cfg.cls]
@@ -36,4 +50,5 @@ def build_datalayer(**connections) -> BaseDatabase:
         vector_database=build_vector_database(
             CFG.vector_search.type, vector_database_stores
         ),
+        distributed_client=build_distributed_client(CFG)
     )

tests/integration/test_dask.py
@@ -1,0 +1,34 @@
+import time
+from unittest.mock import patch
+
+import pytest
+
+from pinnacledb.datalayer.mongodb.query import Collection
+from pinnacledb.cluster.dask.dask_client import dask_client
+
+
+def test_taskgraph_futures_with_dask(random_data, a_watcher, an_update):
+    from pinnacledb.misc.configs import CONFIG
+    with patch.object(CONFIG.config, "remote", True):
+        random_data.remote = True
+        client = dask_client(CONFIG.config.dask)
+        random_data._distributed_client = client
+        output, graph = random_data.execute(Collection(name='documents').insert_many(an_update))
+
+    client.wait_all_pending_task()
+    time.sleep(3)
+    nodes = graph.G.nodes
+    jobs = [nodes[node]['job'] for node in nodes]
+    assert all([job.future.status == 'finished' for job in jobs])
+
+def test_insert_with_dask(random_data, a_watcher, an_update):
+    from pinnacledb.misc.configs import CONFIG
+    with patch.object(CONFIG.config, "remote", True):
+        random_data.remote = True
+        client = dask_client(CONFIG.config.dask)
+        random_data._distributed_client = client
+        random_data.execute(Collection(name='documents').insert_many(an_update))
+        time.sleep(3)
+        r = next(random_data.execute(Collection(name='documents').find({'update': True})))
+        assert 'linear_a' in r['_outputs']['x']
+