superduper-io/superduper

Add tests for vector search

tests/unittests/vector_search/test_base.py
@@ -1,0 +1,87 @@
+import numpy as np
+import pytest
+import torch
+from unittest import mock
+
+from superduperdb.vector_search.base import BaseVectorIndex, to_numpy
+
+
+class TestBaseVectorIndex:
+    @pytest.fixture
+    def base_vector_index(self):
+        # Create a sample BaseVectorIndex instance for testing
+        h = np.random.rand(10, 3)  # Sample data
+        index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']  # Sample index
+        measure = 'euclidean'  # Sample measure
+        return BaseVectorIndex(h, index, measure)
+
+    def test_init(self, base_vector_index):
+        assert isinstance(base_vector_index.h, np.ndarray)
+        assert isinstance(base_vector_index.index, list)
+        assert isinstance(base_vector_index.lookup, dict)
+        assert isinstance(base_vector_index.measure, str)
+
+    def test_shape(self, base_vector_index):
+        assert base_vector_index.shape == base_vector_index.h.shape
+
+    @mock.patch('superduperdb.vector_search.base.to_numpy')
+    def test_find_nearest_from_id(self, mock_to_numpy, base_vector_index):
+        # Mock to_numpy function to avoid its actual execution
+        mock_to_numpy.return_value = np.array([[0.1, 0.2, 0.3]])
+
+        _id = 'a'
+        n = 100
+        with pytest.raises(NotImplementedError):
+            result = base_vector_index.find_nearest_from_id(_id, n)
+
+    @mock.patch('superduperdb.vector_search.base.to_numpy')
+    def test_find_nearest_from_ids(self, mock_to_numpy, base_vector_index):
+        # Mock to_numpy function to avoid its actual execution
+        mock_to_numpy.return_value = np.array([[0.1, 0.2, 0.3]])
+
+        _ids = ['a', 'b']
+        n = 100
+        with pytest.raises(NotImplementedError):
+            result = base_vector_index.find_nearest_from_ids(_ids, n)
+
+    @mock.patch('superduperdb.vector_search.base.to_numpy')
+    def test_find_nearest_from_array(self, mock_to_numpy, base_vector_index):
+        # Mock to_numpy function to avoid its actual execution
+        mock_to_numpy.return_value = np.array([[0.1, 0.2, 0.3]])
+
+        h = np.random.rand(1, 3)  # Sample array
+        n = 100
+        with pytest.raises(NotImplementedError):
+            result = base_vector_index.find_nearest_from_array(h, n)
+
+    @mock.patch('superduperdb.vector_search.base.to_numpy')
+    def test_find_nearest_from_arrays(self, mock_to_numpy, base_vector_index):
+        # Mock to_numpy function to avoid its actual execution
+        mock_to_numpy.return_value = np.array([[0.1, 0.2, 0.3]])
+
+        h = np.random.rand(2, 3)  # Sample array
+        n = 100
+        with pytest.raises(NotImplementedError):
+            base_vector_index.find_nearest_from_arrays(h, n)
+
+    def test_getitem(self, base_vector_index):
+        with pytest.raises(NotImplementedError):
+            base_vector_index['item']
+
+
+def test_to_numpy():
+    # Test to_numpy function with different input types
+    x = np.array([[1, 2, 3], [4, 5, 6]])
+    result = to_numpy(x)
+    assert isinstance(result, np.ndarray)
+    assert np.array_equal(result, x)
+
+    x = torch.tensor([[1, 2, 3], [4, 5, 6]])
+    result = to_numpy(x)
+    assert isinstance(result, np.ndarray)
+    assert np.array_equal(result, x.numpy())
+
+    x = [[1, 2, 3], [4, 5, 6]]
+    result = to_numpy(x)
+    assert isinstance(result, np.ndarray)
+    assert np.array_equal(result, np.array(x))

tests/unittests/vector_search/test_faiss.py
@@ -1,0 +1,48 @@
+import os
+import uuid
+
+from scipy.spatial.distance import cdist
+import torch
+import pytest
+
+from superduperdb.vector_search.table_scan import VanillaVectorIndex
+from superduperdb.vector_search.faiss_index import FaissVectorIndex
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+
+@pytest.mark.parametrize('metric', ['l2', 'css', 'dot'])
+def test_faiss_hash_set(metric):
+    x = torch.randn(1000, 32)
+    ids = [uuid.uuid4() for _ in range(x.shape[0])]
+
+    def l2(x, y):
+        return -cdist(x, y)
+
+    h1 = FaissVectorIndex(x, ids, metric)
+    h2 = VanillaVectorIndex(x, ids, metric)
+
+    y = torch.randn(32)
+
+    res1, _ = h1.find_nearest_from_array(y)
+    res2, _ = h2.find_nearest_from_array(y)
+
+    assert res1[0] == res2[0]
+
+@pytest.mark.skip(reason="Faiss doesn't support batched queries")
+def test_faiss_from_arrays():
+    x = torch.randn(1000, 4)
+    ids = [uuid.uuid4() for _ in range(x.shape[0])]
+
+    def l2(x, y):
+        return -cdist(x, y)
+
+    h1 = FaissVectorIndex(x, ids, 'l2')
+    h2 = VanillaVectorIndex(x, ids, l2)
+
+    y = torch.randn(2, 4)
+
+    res1, _ = h1.find_nearest_from_arrays(y, 2)
+    res2, _ = h2.find_nearest_from_arrays(y, 2)
+
+    assert res1[0] == res2[0]

tests/unittests/vector_search/test_vanillaindex.py
@@ -1,0 +1,16 @@
+import uuid
+
+import pytest
+import numpy as np
+
+from superduperdb.vector_search.table_scan import VanillaVectorIndex
+from superduperdb.vector_search.table_scan import l2, dot, cosine
+
+@pytest.mark.parametrize("measure", [l2, dot, cosine])
+def test_vaniila_index(measure):
+    x = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
+    ids = [uuid.uuid4() for _ in range(x.shape[0])]
+    h2 = VanillaVectorIndex(x, ids, measure)
+    y = np.array([0, 0.5, 0.5])
+    res2, _ = h2.find_nearest_from_array(y, 1)
+    assert res2[0] == ids[0]

Pinnacle-Networks/Pinnacle

Former-commit-id: 8e583536c437d4ad0160af55497dd5472a30a5f3

tests/unittests/vector_search/test_base.py
@@ -1,0 +1,87 @@
+import numpy as np
+import pytest
+import torch
+from unittest import mock
+
+from pinnacledb.vector_search.base import BaseVectorIndex, to_numpy
+
+
+class TestBaseVectorIndex:
+    @pytest.fixture
+    def base_vector_index(self):
+        # Create a sample BaseVectorIndex instance for testing
+        h = np.random.rand(10, 3)  # Sample data
+        index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']  # Sample index
+        measure = 'euclidean'  # Sample measure
+        return BaseVectorIndex(h, index, measure)
+
+    def test_init(self, base_vector_index):
+        assert isinstance(base_vector_index.h, np.ndarray)
+        assert isinstance(base_vector_index.index, list)
+        assert isinstance(base_vector_index.lookup, dict)
+        assert isinstance(base_vector_index.measure, str)
+
+    def test_shape(self, base_vector_index):
+        assert base_vector_index.shape == base_vector_index.h.shape
+
+    @mock.patch('pinnacledb.vector_search.base.to_numpy')
+    def test_find_nearest_from_id(self, mock_to_numpy, base_vector_index):
+        # Mock to_numpy function to avoid its actual execution
+        mock_to_numpy.return_value = np.array([[0.1, 0.2, 0.3]])
+
+        _id = 'a'
+        n = 100
+        with pytest.raises(NotImplementedError):
+            result = base_vector_index.find_nearest_from_id(_id, n)
+
+    @mock.patch('pinnacledb.vector_search.base.to_numpy')
+    def test_find_nearest_from_ids(self, mock_to_numpy, base_vector_index):
+        # Mock to_numpy function to avoid its actual execution
+        mock_to_numpy.return_value = np.array([[0.1, 0.2, 0.3]])
+
+        _ids = ['a', 'b']
+        n = 100
+        with pytest.raises(NotImplementedError):
+            result = base_vector_index.find_nearest_from_ids(_ids, n)
+
+    @mock.patch('pinnacledb.vector_search.base.to_numpy')
+    def test_find_nearest_from_array(self, mock_to_numpy, base_vector_index):
+        # Mock to_numpy function to avoid its actual execution
+        mock_to_numpy.return_value = np.array([[0.1, 0.2, 0.3]])
+
+        h = np.random.rand(1, 3)  # Sample array
+        n = 100
+        with pytest.raises(NotImplementedError):
+            result = base_vector_index.find_nearest_from_array(h, n)
+
+    @mock.patch('pinnacledb.vector_search.base.to_numpy')
+    def test_find_nearest_from_arrays(self, mock_to_numpy, base_vector_index):
+        # Mock to_numpy function to avoid its actual execution
+        mock_to_numpy.return_value = np.array([[0.1, 0.2, 0.3]])
+
+        h = np.random.rand(2, 3)  # Sample array
+        n = 100
+        with pytest.raises(NotImplementedError):
+            base_vector_index.find_nearest_from_arrays(h, n)
+
+    def test_getitem(self, base_vector_index):
+        with pytest.raises(NotImplementedError):
+            base_vector_index['item']
+
+
+def test_to_numpy():
+    # Test to_numpy function with different input types
+    x = np.array([[1, 2, 3], [4, 5, 6]])
+    result = to_numpy(x)
+    assert isinstance(result, np.ndarray)
+    assert np.array_equal(result, x)
+
+    x = torch.tensor([[1, 2, 3], [4, 5, 6]])
+    result = to_numpy(x)
+    assert isinstance(result, np.ndarray)
+    assert np.array_equal(result, x.numpy())
+
+    x = [[1, 2, 3], [4, 5, 6]]
+    result = to_numpy(x)
+    assert isinstance(result, np.ndarray)
+    assert np.array_equal(result, np.array(x))

tests/unittests/vector_search/test_faiss.py
@@ -1,0 +1,48 @@
+import os
+import uuid
+
+from scipy.spatial.distance import cdist
+import torch
+import pytest
+
+from pinnacledb.vector_search.table_scan import VanillaVectorIndex
+from pinnacledb.vector_search.faiss_index import FaissVectorIndex
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+
+@pytest.mark.parametrize('metric', ['l2', 'css', 'dot'])
+def test_faiss_hash_set(metric):
+    x = torch.randn(1000, 32)
+    ids = [uuid.uuid4() for _ in range(x.shape[0])]
+
+    def l2(x, y):
+        return -cdist(x, y)
+
+    h1 = FaissVectorIndex(x, ids, metric)
+    h2 = VanillaVectorIndex(x, ids, metric)
+
+    y = torch.randn(32)
+
+    res1, _ = h1.find_nearest_from_array(y)
+    res2, _ = h2.find_nearest_from_array(y)
+
+    assert res1[0] == res2[0]
+
+@pytest.mark.skip(reason="Faiss doesn't support batched queries")
+def test_faiss_from_arrays():
+    x = torch.randn(1000, 4)
+    ids = [uuid.uuid4() for _ in range(x.shape[0])]
+
+    def l2(x, y):
+        return -cdist(x, y)
+
+    h1 = FaissVectorIndex(x, ids, 'l2')
+    h2 = VanillaVectorIndex(x, ids, l2)
+
+    y = torch.randn(2, 4)
+
+    res1, _ = h1.find_nearest_from_arrays(y, 2)
+    res2, _ = h2.find_nearest_from_arrays(y, 2)
+
+    assert res1[0] == res2[0]

tests/unittests/vector_search/test_vanillaindex.py
@@ -1,0 +1,16 @@
+import uuid
+
+import pytest
+import numpy as np
+
+from pinnacledb.vector_search.table_scan import VanillaVectorIndex
+from pinnacledb.vector_search.table_scan import l2, dot, cosine
+
+@pytest.mark.parametrize("measure", [l2, dot, cosine])
+def test_vaniila_index(measure):
+    x = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
+    ids = [uuid.uuid4() for _ in range(x.shape[0])]
+    h2 = VanillaVectorIndex(x, ids, measure)
+    y = np.array([0, 0.5, 0.5])
+    res2, _ = h2.find_nearest_from_array(y, 1)
+    assert res2[0] == ids[0]