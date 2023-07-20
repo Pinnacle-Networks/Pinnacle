superduper-io/superduper

Add unit tests for models

tests/unittests/models/test_langchain.py
@@ -6,7 +6,9 @@ import pytest
 from superduperdb.models.sentence_transformers.wrapper import SentenceTransformer
 from superduperdb.core.watcher import Watcher
 from superduperdb.core.vector_index import VectorIndex
-from superduperdb.models.langchain.retriever import DBQAWithSourcesChain
+from superduperdb.models.langchain.retriever import (
+    DBQAWithSourcesChain,
+)
 from superduperdb.datalayer.mongodb.query import Collection
 from superduperdb.encoders.numpy.array import array
 
@@ -19,6 +21,7 @@ if not SKIP_PAID:
 @pytest.mark.skipif(SKIP_PAID, reason='don\'t test paid API')
 def test_db_qa_with_sources_chain(nursery_rhymes):
     from langchain import OpenAI
+
     nursery_rhymes.add(array(numpy.float32, shape=(1024,)))
     pl = SentenceTransformer(model_name_or_path='all-MiniLM-L6-v2', encoder='array')
     nursery_rhymes.add(pl)

tests/unittests/models/test_torch_utils.py
@@ -1,0 +1,67 @@
+import pytest
+import torch
+from superduperdb.models.torch.utils import device_of, eval, set_device, to_device
+
+
+@pytest.fixture
+def model():
+    return torch.nn.Linear(10, 2)
+
+
+def test_device_of_cpu(model):
+    device = device_of(model)
+    assert device.type == 'cpu'
+
+
+def test_device_of_cuda(model):
+    if torch.cuda.is_available():
+        model.to(torch.device('cuda'))
+        device = device_of(model)
+        assert device == 'cuda'
+
+
+def test_eval_context_manager(model):
+    with eval(model):
+        assert not model.training
+
+
+def test_set_device_context_manager(model):
+    device_before = device_of(model)
+    if torch.cuda.is_available():
+        with set_device(model, torch.device('cuda')):
+            device_after = device_of(model)
+            assert device_after == 'cuda'
+        assert device_of(model) == device_before
+
+
+def test_to_device_tensor(model):
+    tensor = torch.tensor([1, 2, 3])
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    tensor_device = to_device(tensor, device)
+    assert tensor_device.device == device
+
+
+def test_to_device_nested_list(model):
+    nested_list = [
+        torch.tensor([1, 2, 3]),
+        [torch.tensor([4, 5]), torch.tensor([6, 7])],
+    ]
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    nested_list_device = to_device(nested_list, device)
+    for item in nested_list_device:
+        if isinstance(item, list):
+            assert all(i.device == device for i in item)
+        else:
+            assert item.device == device
+
+
+def test_to_device_nested_dict(model):
+    nested_dict = {'a': torch.tensor([1, 2, 3]), 'b': {'c': torch.tensor([4, 5])}}
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    nested_dict_device = to_device(nested_dict, device)
+    for item in nested_dict_device.values():
+        if isinstance(item, dict):
+            for i in item.values():
+                assert i.device == device
+        else:
+            assert item.device == device

tests/unittests/models/test_vanilla.py
@@ -1,0 +1,11 @@
+from superduperdb.models.vanilla.wrapper import Function
+
+
+def test_function_predict_one():
+    function = Function(object=lambda x: x, identifier='test')
+    assert function.predict(1) == 1
+
+
+def test_function_predict():
+    function = Function(object=lambda x: x, identifier='test')
+    assert function.predict([1, 1]) == [1, 1]

tests/unittests/vector_search/test_lancedb.py
@@ -7,6 +7,7 @@ import pytest
 from unittest.mock import MagicMock
 import pyarrow as pa
 
+from superduperdb.vector_search.base import VectorCollectionConfig
 from superduperdb import CFG
 from superduperdb.vector_search.base import VectorCollectionItem
 from superduperdb.misc.config import LanceDB
@@ -72,7 +73,6 @@ def test_create_table_new(lance_client):
     assert table.measure == measure
 
 
-@pytest.mark.skip(reason="`tantivy` package needs to be installed")
 def test_add(lance_table):
     data = [
         {"id": "1", "vector": [2, 3]},
@@ -81,10 +81,11 @@ def test_add(lance_table):
     ]
     data = [VectorCollectionItem(**d) for d in data]
     lance_table.add(data)
-    assert lance_table.get("1") == data[0]
+    data = lance_table.find_nearest_from_array([2, 3])
+    assert data[0].id == 1
 
 
-@pytest.mark.skip(reason="`tantivy` package needs to be installed")
+@pytest.mark.skip(reason="Not implemented")
 def test_find_nearest_from_id(lance_table):
     identifier = "1"
     limit = 100
@@ -114,7 +115,8 @@ def test_find_nearest_from_array(lance_table):
     assert result[0].id == 1
 
 
-def test_create_schema():
+@pytest.mark.parametrize("measure", ["cosine", "euclidean"])
+def test_create_schema(measure):
     dimensions = 3
     expected_schema = pa.schema(
         [
@@ -122,6 +124,17 @@ def test_create_schema():
             pa.field("id", pa.string()),
         ]
     )
-    vector_index = LanceVectorIndex(config=CFG.vector_search.type)
+    vector_index = LanceVectorIndex(config=CFG.vector_search.type, measure=measure)
     schema = vector_index._create_schema(dimensions)
     assert schema.equals(expected_schema)
+
+
+def test_vector_index_get_table():
+    vector_index = LanceVectorIndex(config=CFG.vector_search.type)
+    table = vector_index.get_table(
+        VectorCollectionConfig(
+            id="1",
+            dimensions=3,
+        )
+    )
+    assert isinstance(table, LanceTable)

Pinnacle-Networks/Pinnacle

Former-commit-id: ec887b50f27dfe45429b47529a06e429d9ad43c7

tests/unittests/models/test_langchain.py
@@ -6,7 +6,9 @@ import pytest
 from pinnacledb.models.sentence_transformers.wrapper import SentenceTransformer
 from pinnacledb.core.watcher import Watcher
 from pinnacledb.core.vector_index import VectorIndex
-from pinnacledb.models.langchain.retriever import DBQAWithSourcesChain
+from pinnacledb.models.langchain.retriever import (
+    DBQAWithSourcesChain,
+)
 from pinnacledb.datalayer.mongodb.query import Collection
 from pinnacledb.encoders.numpy.array import array
 
@@ -19,6 +21,7 @@ if not SKIP_PAID:
 @pytest.mark.skipif(SKIP_PAID, reason='don\'t test paid API')
 def test_db_qa_with_sources_chain(nursery_rhymes):
     from langchain import OpenAI
+
     nursery_rhymes.add(array(numpy.float32, shape=(1024,)))
     pl = SentenceTransformer(model_name_or_path='all-MiniLM-L6-v2', encoder='array')
     nursery_rhymes.add(pl)

tests/unittests/models/test_torch_utils.py
@@ -1,0 +1,67 @@
+import pytest
+import torch
+from pinnacledb.models.torch.utils import device_of, eval, set_device, to_device
+
+
+@pytest.fixture
+def model():
+    return torch.nn.Linear(10, 2)
+
+
+def test_device_of_cpu(model):
+    device = device_of(model)
+    assert device.type == 'cpu'
+
+
+def test_device_of_cuda(model):
+    if torch.cuda.is_available():
+        model.to(torch.device('cuda'))
+        device = device_of(model)
+        assert device == 'cuda'
+
+
+def test_eval_context_manager(model):
+    with eval(model):
+        assert not model.training
+
+
+def test_set_device_context_manager(model):
+    device_before = device_of(model)
+    if torch.cuda.is_available():
+        with set_device(model, torch.device('cuda')):
+            device_after = device_of(model)
+            assert device_after == 'cuda'
+        assert device_of(model) == device_before
+
+
+def test_to_device_tensor(model):
+    tensor = torch.tensor([1, 2, 3])
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    tensor_device = to_device(tensor, device)
+    assert tensor_device.device == device
+
+
+def test_to_device_nested_list(model):
+    nested_list = [
+        torch.tensor([1, 2, 3]),
+        [torch.tensor([4, 5]), torch.tensor([6, 7])],
+    ]
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    nested_list_device = to_device(nested_list, device)
+    for item in nested_list_device:
+        if isinstance(item, list):
+            assert all(i.device == device for i in item)
+        else:
+            assert item.device == device
+
+
+def test_to_device_nested_dict(model):
+    nested_dict = {'a': torch.tensor([1, 2, 3]), 'b': {'c': torch.tensor([4, 5])}}
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    nested_dict_device = to_device(nested_dict, device)
+    for item in nested_dict_device.values():
+        if isinstance(item, dict):
+            for i in item.values():
+                assert i.device == device
+        else:
+            assert item.device == device

tests/unittests/models/test_vanilla.py
@@ -1,0 +1,11 @@
+from pinnacledb.models.vanilla.wrapper import Function
+
+
+def test_function_predict_one():
+    function = Function(object=lambda x: x, identifier='test')
+    assert function.predict(1) == 1
+
+
+def test_function_predict():
+    function = Function(object=lambda x: x, identifier='test')
+    assert function.predict([1, 1]) == [1, 1]

tests/unittests/vector_search/test_lancedb.py
@@ -7,6 +7,7 @@ import pytest
 from unittest.mock import MagicMock
 import pyarrow as pa
 
+from pinnacledb.vector_search.base import VectorCollectionConfig
 from pinnacledb import CFG
 from pinnacledb.vector_search.base import VectorCollectionItem
 from pinnacledb.misc.config import LanceDB
@@ -72,7 +73,6 @@ def test_create_table_new(lance_client):
     assert table.measure == measure
 
 
-@pytest.mark.skip(reason="`tantivy` package needs to be installed")
 def test_add(lance_table):
     data = [
         {"id": "1", "vector": [2, 3]},
@@ -81,10 +81,11 @@ def test_add(lance_table):
     ]
     data = [VectorCollectionItem(**d) for d in data]
     lance_table.add(data)
-    assert lance_table.get("1") == data[0]
+    data = lance_table.find_nearest_from_array([2, 3])
+    assert data[0].id == 1
 
 
-@pytest.mark.skip(reason="`tantivy` package needs to be installed")
+@pytest.mark.skip(reason="Not implemented")
 def test_find_nearest_from_id(lance_table):
     identifier = "1"
     limit = 100
@@ -114,7 +115,8 @@ def test_find_nearest_from_array(lance_table):
     assert result[0].id == 1
 
 
-def test_create_schema():
+@pytest.mark.parametrize("measure", ["cosine", "euclidean"])
+def test_create_schema(measure):
     dimensions = 3
     expected_schema = pa.schema(
         [
@@ -122,6 +124,17 @@ def test_create_schema():
             pa.field("id", pa.string()),
         ]
     )
-    vector_index = LanceVectorIndex(config=CFG.vector_search.type)
+    vector_index = LanceVectorIndex(config=CFG.vector_search.type, measure=measure)
     schema = vector_index._create_schema(dimensions)
     assert schema.equals(expected_schema)
+
+
+def test_vector_index_get_table():
+    vector_index = LanceVectorIndex(config=CFG.vector_search.type)
+    table = vector_index.get_table(
+        VectorCollectionConfig(
+            id="1",
+            dimensions=3,
+        )
+    )
+    assert isinstance(table, LanceTable)