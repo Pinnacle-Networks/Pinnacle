superduper-io/superduper

Rename core/documents.py to core/document.py

superduperdb/core/dataset.py
@@ -5,7 +5,7 @@ import numpy
 
 from superduperdb.core.artifact import Artifact
 from superduperdb.core.component import Component
-from superduperdb.core.documents import Document
+from superduperdb.core.document import Document
 from superduperdb.datalayer.mongodb.query import Find
 import dataclasses as dc
 

superduperdb/datalayer/base/cursor.py
@@ -2,7 +2,7 @@ import dataclasses as dc
 import typing as t
 from functools import cached_property
 from pymongo.cursor import Cursor
-from superduperdb.core.documents import Document
+from superduperdb.core.document import Document
 from superduperdb.core.encoder import Encoder
 from superduperdb.misc.special_dicts import MongoStyleDict
 

superduperdb/datalayer/base/database.py
@@ -12,7 +12,7 @@ import networkx
 from superduperdb import CFG
 from superduperdb.core.task_workflow import TaskWorkflow
 from superduperdb.core.component import Component
-from superduperdb.core.documents import Document
+from superduperdb.core.document import Document
 from superduperdb.core.exceptions import ComponentInUseError, ComponentInUseWarning
 from superduperdb.datalayer.base.artifacts import ArtifactStore
 from superduperdb.datalayer.base.data_backend import BaseDataBackend

superduperdb/serve/server.py
@@ -12,7 +12,7 @@ from superduperdb.core.artifact_tree import (
 from superduperdb import CFG
 from superduperdb.misc.serialization import serializers
 
-from superduperdb.core.documents import Document
+from superduperdb.core.document import Document
 from superduperdb.core.serializable import Serializable
 from superduperdb.datalayer.base.build import build_datalayer
 

tests/integration/conftest.py
@@ -11,7 +11,7 @@ from tenacity import RetryError, Retrying, stop_after_delay
 
 from superduperdb import CFG
 from superduperdb.cluster.dask.dask_client import dask_client
-from superduperdb.core.documents import Document
+from superduperdb.core.document import Document
 from superduperdb.datalayer.base.build import build_datalayer
 from superduperdb.encoders.torch.tensor import tensor
 from superduperdb.misc.config import DataLayer, DataLayers

tests/integration/test_server.py
@@ -5,7 +5,7 @@ import torch
 
 from superduperdb.models.torch.wrapper import TorchModel
 from superduperdb import CFG
-from superduperdb.core.documents import Document
+from superduperdb.core.document import Document
 from superduperdb.datalayer.mongodb.query import Collection
 from superduperdb.encoders.torch.tensor import tensor
 from superduperdb.serve.client import Client

tests/unittests/datalayer/mongodb/test_database.py
@@ -2,7 +2,7 @@ import PIL.PngImagePlugin
 import pytest
 import torch
 from superduperdb.core.dataset import Dataset
-from superduperdb.core.documents import Document
+from superduperdb.core.document import Document
 from superduperdb.core.encoder import Encoder
 from superduperdb.core.exceptions import ComponentInUseError, ComponentInUseWarning
 from superduperdb.core.watcher import Watcher

tests/unittests/models/test_transformers.py
@@ -1,7 +1,7 @@
 import pytest
 import tdir
 
-from superduperdb.core.documents import Document as D
+from superduperdb.core.document import Document as D
 from superduperdb.datalayer.mongodb.query import Collection
 
 from superduperdb.models.transformers.wrapper import (

Pinnacle-Networks/Pinnacle

Former-commit-id: 3a056ddbbb04e11cf028312ae11583f817e76f2b

superduperdb/core/dataset.py
@@ -5,7 +5,7 @@ import numpy
 
 from pinnacledb.core.artifact import Artifact
 from pinnacledb.core.component import Component
-from pinnacledb.core.documents import Document
+from pinnacledb.core.document import Document
 from pinnacledb.datalayer.mongodb.query import Find
 import dataclasses as dc
 

superduperdb/datalayer/base/cursor.py
@@ -2,7 +2,7 @@ import dataclasses as dc
 import typing as t
 from functools import cached_property
 from pymongo.cursor import Cursor
-from pinnacledb.core.documents import Document
+from pinnacledb.core.document import Document
 from pinnacledb.core.encoder import Encoder
 from pinnacledb.misc.special_dicts import MongoStyleDict
 

superduperdb/datalayer/base/database.py
@@ -12,7 +12,7 @@ import networkx
 from pinnacledb import CFG
 from pinnacledb.core.task_workflow import TaskWorkflow
 from pinnacledb.core.component import Component
-from pinnacledb.core.documents import Document
+from pinnacledb.core.document import Document
 from pinnacledb.core.exceptions import ComponentInUseError, ComponentInUseWarning
 from pinnacledb.datalayer.base.artifacts import ArtifactStore
 from pinnacledb.datalayer.base.data_backend import BaseDataBackend

superduperdb/serve/server.py
@@ -12,7 +12,7 @@ from pinnacledb.core.artifact_tree import (
 from pinnacledb import CFG
 from pinnacledb.misc.serialization import serializers
 
-from pinnacledb.core.documents import Document
+from pinnacledb.core.document import Document
 from pinnacledb.core.serializable import Serializable
 from pinnacledb.datalayer.base.build import build_datalayer
 

tests/integration/conftest.py
@@ -11,7 +11,7 @@ from tenacity import RetryError, Retrying, stop_after_delay
 
 from pinnacledb import CFG
 from pinnacledb.cluster.dask.dask_client import dask_client
-from pinnacledb.core.documents import Document
+from pinnacledb.core.document import Document
 from pinnacledb.datalayer.base.build import build_datalayer
 from pinnacledb.encoders.torch.tensor import tensor
 from pinnacledb.misc.config import DataLayer, DataLayers

tests/integration/test_server.py
@@ -5,7 +5,7 @@ import torch
 
 from pinnacledb.models.torch.wrapper import TorchModel
 from pinnacledb import CFG
-from pinnacledb.core.documents import Document
+from pinnacledb.core.document import Document
 from pinnacledb.datalayer.mongodb.query import Collection
 from pinnacledb.encoders.torch.tensor import tensor
 from pinnacledb.serve.client import Client

tests/unittests/datalayer/mongodb/test_database.py
@@ -2,7 +2,7 @@ import PIL.PngImagePlugin
 import pytest
 import torch
 from pinnacledb.core.dataset import Dataset
-from pinnacledb.core.documents import Document
+from pinnacledb.core.document import Document
 from pinnacledb.core.encoder import Encoder
 from pinnacledb.core.exceptions import ComponentInUseError, ComponentInUseWarning
 from pinnacledb.core.watcher import Watcher

tests/unittests/models/test_transformers.py
@@ -1,7 +1,7 @@
 import pytest
 import tdir
 
-from pinnacledb.core.documents import Document as D
+from pinnacledb.core.document import Document as D
 from pinnacledb.datalayer.mongodb.query import Collection
 
 from pinnacledb.models.transformers.wrapper import (