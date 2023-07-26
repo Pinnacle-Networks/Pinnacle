superduper-io/superduper

A few refactorizations and transfer-learning notebook

superduperdb/cli/serve.py
@@ -4,8 +4,8 @@ import typing as t
 from superduperdb.datalayer.base.build import build_datalayer
 from superduperdb.datalayer.base.cdc import DatabaseWatcher
 from superduperdb.datalayer.mongodb.query import Collection
-from superduperdb.serve.server import serve as _serve
-from superduperdb.cluster.dask.dask_client import dask_client
+from superduperdb.cluster.server import serve as _serve
+from superduperdb.cluster.dask_client import dask_client
 from superduperdb import CFG
 
 

superduperdb/datalayer/base/build.py
@@ -7,7 +7,7 @@ from superduperdb.datalayer.base.backends import (
     connections as default_connections,
 )
 from superduperdb.datalayer.base.datalayer import Datalayer
-from superduperdb.cluster.dask.dask_client import dask_client
+from superduperdb.cluster.dask_client import dask_client
 import superduperdb as s
 
 

tests/integration/conftest.py
@@ -10,13 +10,13 @@ from pymongo import MongoClient
 from tenacity import RetryError, Retrying, stop_after_delay
 
 from superduperdb import CFG
-from superduperdb.cluster.dask.dask_client import dask_client
+from superduperdb.cluster.dask_client import dask_client
 from superduperdb.core.document import Document
 from superduperdb.datalayer.base.build import build_datalayer
 from superduperdb.encoders.torch.tensor import tensor
 from superduperdb.misc.config import DataLayer, DataLayers
 from superduperdb.models.torch.wrapper import TorchModel
-from superduperdb.serve.server import serve
+from superduperdb.cluster.server import serve
 
 '''
 All pytest fixtures with _package scope_ are defined in this module.

tests/integration/test_server.py
@@ -8,7 +8,7 @@ from superduperdb import CFG
 from superduperdb.core.document import Document
 from superduperdb.datalayer.mongodb.query import Collection
 from superduperdb.encoders.torch.tensor import tensor
-from superduperdb.serve.client import Client
+from superduperdb.cluster.client import Client
 
 
 @pytest.fixture(scope="module")

Pinnacle-Networks/Pinnacle

Former-commit-id: e7e672dba149a198c1b31eedb67270a006916a5e

superduperdb/cli/serve.py
@@ -4,8 +4,8 @@ import typing as t
 from pinnacledb.datalayer.base.build import build_datalayer
 from pinnacledb.datalayer.base.cdc import DatabaseWatcher
 from pinnacledb.datalayer.mongodb.query import Collection
-from pinnacledb.serve.server import serve as _serve
-from pinnacledb.cluster.dask.dask_client import dask_client
+from pinnacledb.cluster.server import serve as _serve
+from pinnacledb.cluster.dask_client import dask_client
 from pinnacledb import CFG
 
 

superduperdb/datalayer/base/build.py
@@ -7,7 +7,7 @@ from pinnacledb.datalayer.base.backends import (
     connections as default_connections,
 )
 from pinnacledb.datalayer.base.datalayer import Datalayer
-from pinnacledb.cluster.dask.dask_client import dask_client
+from pinnacledb.cluster.dask_client import dask_client
 import pinnacledb as s
 
 

tests/integration/conftest.py
@@ -10,13 +10,13 @@ from pymongo import MongoClient
 from tenacity import RetryError, Retrying, stop_after_delay
 
 from pinnacledb import CFG
-from pinnacledb.cluster.dask.dask_client import dask_client
+from pinnacledb.cluster.dask_client import dask_client
 from pinnacledb.core.document import Document
 from pinnacledb.datalayer.base.build import build_datalayer
 from pinnacledb.encoders.torch.tensor import tensor
 from pinnacledb.misc.config import DataLayer, DataLayers
 from pinnacledb.models.torch.wrapper import TorchModel
-from pinnacledb.serve.server import serve
+from pinnacledb.cluster.server import serve
 
 '''
 All pytest fixtures with _package scope_ are defined in this module.

tests/integration/test_server.py
@@ -8,7 +8,7 @@ from pinnacledb import CFG
 from pinnacledb.core.document import Document
 from pinnacledb.datalayer.mongodb.query import Collection
 from pinnacledb.encoders.torch.tensor import tensor
-from pinnacledb.serve.client import Client
+from pinnacledb.cluster.client import Client
 
 
 @pytest.fixture(scope="module")