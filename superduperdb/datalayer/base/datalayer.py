superduper-io/superduper

Add simpler path to contextual question answering

superduperdb/datalayer/base/datalayer.py
@@ -24,6 +24,7 @@ from superduperdb.datalayer.base.query import (
     SelectOne,
     Like,
 )
+from superduperdb.misc.special_dicts import MongoStyleDict
 from .download_content import download_content
 from superduperdb.misc.downloads import Downloader
 from superduperdb.misc.downloads import gather_uris
@@ -161,11 +162,12 @@ class Datalayer:
 
     def predict(
         self,
-        model_identifier: str,
-        input: Document,
-        one: bool = False,
-        select: t.Optional[Select] = None,
-    ) -> t.Union[t.List[Document], Document]:
+        model: str,
+        input: t.Union[Document, t.Any],
+        context_select: t.Optional[Select] = None,
+        context_key: t.Optional[str] = None,
+        **kwargs,
+    ) -> t.Tuple[Document, t.List[Document]]:
         """
         Apply model to input.
 
@@ -173,18 +175,30 @@ class Datalayer:
         :param input: input to be passed to the model.
                       Must be possible to encode with registered encoders
         :param one: if True passed a single document else passed multiple documents
+        :param select: select query object to provide context
         """
-        model = self.models[model_identifier]
-        opts = self.metadata.get_component('model', model_identifier)
-        out = model.predict(input.unpack(), **opts.get('predict_kwargs', {}), one=one)
-        if one:
-            if model.encoder is not None:
-                out = model.encoder(out)
-            return Document(out)
-        else:
-            if model.encoder is not None:
-                out = [model.encoder(x) for x in out]
-            return [Document(x) for x in out]
+        model = self.models[model]
+        context = None
+
+        if context_select is not None:
+            assert model.takes_context, 'model does not take context'
+            context = list(self.execute(context_select))
+            context = [x.unpack() for x in context]
+            if context_key is not None:
+                context = [MongoStyleDict(x)[context_key] for x in context]
+
+        out = model.predict(
+            input.unpack() if isinstance(input, Document) else input,
+            one=True,
+            context=context,
+            **kwargs,
+        )
+        if model.encoder is not None:
+            out = model.encoder(out)
+
+        if context is not None:
+            return Document(out), [Document(x) for x in context]
+        return Document(out), []
 
     def execute(self, query: ExecuteQuery) -> ExecuteResult:
         """

superduperdb/models/openai/wrapper.py
@@ -1,3 +1,4 @@
+import dataclasses as dc
 import os
 import typing as t
 
@@ -11,9 +12,9 @@ from openai.error import Timeout, RateLimitError, TryAgain, ServiceUnavailableEr
 
 import superduperdb as s
 from superduperdb.core.component import Component
+from superduperdb.core.encoder import Encoder
 from superduperdb.core.model import PredictMixin
 from superduperdb.misc.retry import Retry
-from superduperdb.misc import dataclasses as dc
 from superduperdb.misc.compat import cache
 from superduperdb.encoders.vectors.vector import vector
 
@@ -36,13 +37,25 @@ def _available_models():
 @dc.dataclass
 class OpenAI(Component, PredictMixin):
     variety: t.ClassVar[str] = 'model'
-    identifier: str
+    model: str
+    identifier: t.Optional[str] = None
     version: t.Optional[int] = None
+    takes_context: bool = False
+    encoder: t.Union[Encoder, str, None] = None
+
+    @property
+    def child_components(self):
+        if self.encoder is not None:
+            return [('encoder', 'encoder')]
+        return []
 
     def __post_init__(self):
-        if self.identifier not in (mo := _available_models()):
-            msg = f'model {self.identifier} not in OpenAI available models, {mo}'
+        if self.model not in (mo := _available_models()):
+            msg = f'model {self.model} not in OpenAI available models, {mo}'
             raise ValueError(msg)
+
+        if self.identifier is None:
+            self.identifier = self.model
 
         if 'OPENAI_API_KEY' not in os.environ:
             raise ValueError('OPENAI_API_KEY not set')
@@ -78,14 +91,28 @@ class OpenAIEmbedding(OpenAI):
         return out
 
 
+@dc.dataclass
 class OpenAIChatCompletion(OpenAI):
+    takes_context: bool = True
+    prompt: t.Optional[str] = None
+
     @retry
-    def predict_one(self, message, **kwargs):
+    def _predict_one(self, X, context: t.Optional[t.List[str]], **kwargs):
+        if context is not None:
+            prompt = self.prompt.format(context='\n'.join(context))
+            X = prompt + X
         return ChatCompletion.create(
-            messages=[{'role': 'user', 'content': message}],
+            messages=[{'role': 'user', 'content': X}],
             model=self.identifier,
             **kwargs,
         )['choices'][0]['message']['content']
 
-    def predict(self, messages, **kwargs):
-        return [self.predict_one(msg) for msg in messages]  # use asyncio
+    def _predict(
+        self, X, one: bool = True, context: t.Optional[t.List[str]] = None, **kwargs
+    ):
+        if context:
+            assert one, 'context only works with one=True'
+        if one:
+            return self._predict_one(X, context=context, **kwargs)
+
+        return [self.predict_one(msg) for msg in X]  # use asyncio

tests/unittests/models/test_openai.py
@@ -1,0 +1,103 @@
+import json
+import os
+import pytest
+from superduperdb.core.document import Document
+from superduperdb.core.vector_index import VectorIndex
+from superduperdb.core.watcher import Watcher
+from superduperdb.datalayer.mongodb.query import Collection
+
+from superduperdb.models.openai.wrapper import OpenAIChatCompletion, OpenAIEmbedding
+
+
+SKIP_PAID = os.environ.get('OPENAI_API_KEY') is None
+
+
+@pytest.mark.skipif(SKIP_PAID, reason='OPENAI_API_KEY not set')
+def test_open_ai_chat_completion():
+    m = OpenAIChatCompletion('gpt-3.5-turbo')
+
+    result = m.predict('Tell me about yourself?', one=True)
+
+    print(result)
+
+    assert 'openai' in result.lower()
+
+    context = ['Imagine you are a woman from Dallas.', 'You like to ride horses.']
+
+    m = OpenAIChatCompletion(
+        model='gpt-3.5-turbo',
+        prompt=(
+            'Use the following facts to answer this question\n'
+            '{context}\n\n'
+            'Here\'s the question:\n'
+        ),
+    )
+    result = m.predict(
+        'Tell me about yourself?',
+        context=context,
+        one=True,
+    )
+
+    assert 'horse' in result.lower()
+    assert 'dallas' in result.lower()
+
+    print(result)
+
+
+@pytest.fixture
+def open_ai_with_rhymes(empty):
+    with open('tests/material/data/rhymes.json') as f:
+        data = json.load(f)
+    for i, r in enumerate(data):
+        data[i] = Document({'story': r.replace('\n', ' ')})
+    empty.execute(Collection('openai').insert_many(data))
+    yield empty
+    empty.remove('model', 'gpt-3.5-turbo', force=True)
+    empty.remove('model', 'text-embedding-ada-002', force=True)
+
+
+@pytest.mark.skipif(SKIP_PAID, reason='OPENAI_API_KEY not set')
+def test_retrieve_with_similar_context(open_ai_with_rhymes):
+    db = open_ai_with_rhymes
+    m = OpenAIChatCompletion(
+        model='gpt-3.5-turbo',
+        prompt=(
+            'Use the following facts to answer this question\n'
+            '{context}\n\n'
+            'Here\'s the question:\n'
+        ),
+    )
+    db.add(m)
+    vectorizer = OpenAIEmbedding('text-embedding-ada-002')
+    db.add(
+        VectorIndex(
+            identifier='openai-index',
+            indexing_watcher=Watcher(
+                model=vectorizer,
+                key='story',
+                select=Collection('openai').find(),
+            ),
+        )
+    )
+
+    models = db.show('model')
+    assert set(models) == {'text-embedding-ada-002', 'gpt-3.5-turbo'}
+    r = db.execute(Collection('openai').find_one())
+    assert '_outputs' in r.content
+
+    input = 'Is covid a hoax?'
+
+    prediction = db.predict(
+        model='gpt-3.5-turbo',
+        input=input,
+        context_select=Collection('openai')
+        .like({'story': input}, n=1, vector_index='openai-index')
+        .find(),
+        context_key='story',
+    )
+
+    print('PREDICTION IS:\n')
+    print(prediction[0])
+
+    print('\nCONTEXT IS:\n')
+    print(prediction[1])

Pinnacle-Networks/Pinnacle

Former-commit-id: a00875b370950933b75a381222b4524086c6c9f8

superduperdb/datalayer/base/datalayer.py
@@ -24,6 +24,7 @@ from pinnacledb.datalayer.base.query import (
     SelectOne,
     Like,
 )
+from pinnacledb.misc.special_dicts import MongoStyleDict
 from .download_content import download_content
 from pinnacledb.misc.downloads import Downloader
 from pinnacledb.misc.downloads import gather_uris
@@ -161,11 +162,12 @@ class Datalayer:
 
     def predict(
         self,
-        model_identifier: str,
-        input: Document,
-        one: bool = False,
-        select: t.Optional[Select] = None,
-    ) -> t.Union[t.List[Document], Document]:
+        model: str,
+        input: t.Union[Document, t.Any],
+        context_select: t.Optional[Select] = None,
+        context_key: t.Optional[str] = None,
+        **kwargs,
+    ) -> t.Tuple[Document, t.List[Document]]:
         """
         Apply model to input.
 
@@ -173,18 +175,30 @@ class Datalayer:
         :param input: input to be passed to the model.
                       Must be possible to encode with registered encoders
         :param one: if True passed a single document else passed multiple documents
+        :param select: select query object to provide context
         """
-        model = self.models[model_identifier]
-        opts = self.metadata.get_component('model', model_identifier)
-        out = model.predict(input.unpack(), **opts.get('predict_kwargs', {}), one=one)
-        if one:
-            if model.encoder is not None:
-                out = model.encoder(out)
-            return Document(out)
-        else:
-            if model.encoder is not None:
-                out = [model.encoder(x) for x in out]
-            return [Document(x) for x in out]
+        model = self.models[model]
+        context = None
+
+        if context_select is not None:
+            assert model.takes_context, 'model does not take context'
+            context = list(self.execute(context_select))
+            context = [x.unpack() for x in context]
+            if context_key is not None:
+                context = [MongoStyleDict(x)[context_key] for x in context]
+
+        out = model.predict(
+            input.unpack() if isinstance(input, Document) else input,
+            one=True,
+            context=context,
+            **kwargs,
+        )
+        if model.encoder is not None:
+            out = model.encoder(out)
+
+        if context is not None:
+            return Document(out), [Document(x) for x in context]
+        return Document(out), []
 
     def execute(self, query: ExecuteQuery) -> ExecuteResult:
         """

superduperdb/models/openai/wrapper.py
@@ -1,3 +1,4 @@
+import dataclasses as dc
 import os
 import typing as t
 
@@ -11,9 +12,9 @@ from openai.error import Timeout, RateLimitError, TryAgain, ServiceUnavailableEr
 
 import pinnacledb as s
 from pinnacledb.core.component import Component
+from pinnacledb.core.encoder import Encoder
 from pinnacledb.core.model import PredictMixin
 from pinnacledb.misc.retry import Retry
-from pinnacledb.misc import dataclasses as dc
 from pinnacledb.misc.compat import cache
 from pinnacledb.encoders.vectors.vector import vector
 
@@ -36,13 +37,25 @@ def _available_models():
 @dc.dataclass
 class OpenAI(Component, PredictMixin):
     variety: t.ClassVar[str] = 'model'
-    identifier: str
+    model: str
+    identifier: t.Optional[str] = None
     version: t.Optional[int] = None
+    takes_context: bool = False
+    encoder: t.Union[Encoder, str, None] = None
+
+    @property
+    def child_components(self):
+        if self.encoder is not None:
+            return [('encoder', 'encoder')]
+        return []
 
     def __post_init__(self):
-        if self.identifier not in (mo := _available_models()):
-            msg = f'model {self.identifier} not in OpenAI available models, {mo}'
+        if self.model not in (mo := _available_models()):
+            msg = f'model {self.model} not in OpenAI available models, {mo}'
             raise ValueError(msg)
+
+        if self.identifier is None:
+            self.identifier = self.model
 
         if 'OPENAI_API_KEY' not in os.environ:
             raise ValueError('OPENAI_API_KEY not set')
@@ -78,14 +91,28 @@ class OpenAIEmbedding(OpenAI):
         return out
 
 
+@dc.dataclass
 class OpenAIChatCompletion(OpenAI):
+    takes_context: bool = True
+    prompt: t.Optional[str] = None
+
     @retry
-    def predict_one(self, message, **kwargs):
+    def _predict_one(self, X, context: t.Optional[t.List[str]], **kwargs):
+        if context is not None:
+            prompt = self.prompt.format(context='\n'.join(context))
+            X = prompt + X
         return ChatCompletion.create(
-            messages=[{'role': 'user', 'content': message}],
+            messages=[{'role': 'user', 'content': X}],
             model=self.identifier,
             **kwargs,
         )['choices'][0]['message']['content']
 
-    def predict(self, messages, **kwargs):
-        return [self.predict_one(msg) for msg in messages]  # use asyncio
+    def _predict(
+        self, X, one: bool = True, context: t.Optional[t.List[str]] = None, **kwargs
+    ):
+        if context:
+            assert one, 'context only works with one=True'
+        if one:
+            return self._predict_one(X, context=context, **kwargs)
+
+        return [self.predict_one(msg) for msg in X]  # use asyncio

tests/unittests/models/test_openai.py
@@ -1,0 +1,103 @@
+import json
+import os
+import pytest
+from pinnacledb.core.document import Document
+from pinnacledb.core.vector_index import VectorIndex
+from pinnacledb.core.watcher import Watcher
+from pinnacledb.datalayer.mongodb.query import Collection
+
+from pinnacledb.models.openai.wrapper import OpenAIChatCompletion, OpenAIEmbedding
+
+
+SKIP_PAID = os.environ.get('OPENAI_API_KEY') is None
+
+
+@pytest.mark.skipif(SKIP_PAID, reason='OPENAI_API_KEY not set')
+def test_open_ai_chat_completion():
+    m = OpenAIChatCompletion('gpt-3.5-turbo')
+
+    result = m.predict('Tell me about yourself?', one=True)
+
+    print(result)
+
+    assert 'openai' in result.lower()
+
+    context = ['Imagine you are a woman from Dallas.', 'You like to ride horses.']
+
+    m = OpenAIChatCompletion(
+        model='gpt-3.5-turbo',
+        prompt=(
+            'Use the following facts to answer this question\n'
+            '{context}\n\n'
+            'Here\'s the question:\n'
+        ),
+    )
+    result = m.predict(
+        'Tell me about yourself?',
+        context=context,
+        one=True,
+    )
+
+    assert 'horse' in result.lower()
+    assert 'dallas' in result.lower()
+
+    print(result)
+
+
+@pytest.fixture
+def open_ai_with_rhymes(empty):
+    with open('tests/material/data/rhymes.json') as f:
+        data = json.load(f)
+    for i, r in enumerate(data):
+        data[i] = Document({'story': r.replace('\n', ' ')})
+    empty.execute(Collection('openai').insert_many(data))
+    yield empty
+    empty.remove('model', 'gpt-3.5-turbo', force=True)
+    empty.remove('model', 'text-embedding-ada-002', force=True)
+
+
+@pytest.mark.skipif(SKIP_PAID, reason='OPENAI_API_KEY not set')
+def test_retrieve_with_similar_context(open_ai_with_rhymes):
+    db = open_ai_with_rhymes
+    m = OpenAIChatCompletion(
+        model='gpt-3.5-turbo',
+        prompt=(
+            'Use the following facts to answer this question\n'
+            '{context}\n\n'
+            'Here\'s the question:\n'
+        ),
+    )
+    db.add(m)
+    vectorizer = OpenAIEmbedding('text-embedding-ada-002')
+    db.add(
+        VectorIndex(
+            identifier='openai-index',
+            indexing_watcher=Watcher(
+                model=vectorizer,
+                key='story',
+                select=Collection('openai').find(),
+            ),
+        )
+    )
+
+    models = db.show('model')
+    assert set(models) == {'text-embedding-ada-002', 'gpt-3.5-turbo'}
+    r = db.execute(Collection('openai').find_one())
+    assert '_outputs' in r.content
+
+    input = 'Is covid a hoax?'
+
+    prediction = db.predict(
+        model='gpt-3.5-turbo',
+        input=input,
+        context_select=Collection('openai')
+        .like({'story': input}, n=1, vector_index='openai-index')
+        .find(),
+        context_key='story',
+    )
+
+    print('PREDICTION IS:\n')
+    print(prediction[0])
+
+    print('\nCONTEXT IS:\n')
+    print(prediction[1])