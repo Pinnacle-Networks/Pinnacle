superduper-io/superduper

Add edited MonkeyType annotations to core

superduperdb/core/component.py
@@ -7,6 +7,7 @@ import dataclasses as dc
 
 if t.TYPE_CHECKING:
     from superduperdb.datalayer.base.datalayer import Datalayer
+    from superduperdb.datalayer.base.dataset import Dataset
 
 
 @dc.dataclass
@@ -34,23 +35,23 @@ class Component(Serializable):
         pass
 
     @property
-    def child_components(self):
+    def child_components(self) -> t.Sequence[t.Any]:
         return []
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         if self.version is None:
             raise Exception('Version not yet set for component uniqueness')
         return f'{self.variety}/{self.identifier}/{self.version}'
 
-    def dict(self):
+    def dict(self) -> t.Dict[str, t.Any]:
         return dc.asdict(self)
 
     def create_validation_job(
         self,
-        validation_set: t.Union[str, 'Dataset'],  # type: ignore[name-defined]
+        validation_set: t.Union[str, Dataset],
         metrics: t.Sequence[str],
-    ):
+    ) -> ComponentJob:
         return ComponentJob(
             component_identifier=self.identifier,
             method_name='predict',
@@ -62,9 +63,11 @@ class Component(Serializable):
             },
         )
 
-    def schedule_jobs(self, database, dependencies=()):
+    def schedule_jobs(
+        self, database: Datalayer, dependencies: t.Sequence[()] = ()
+    ) -> t.Sequence[t.Any]:
         return []
 
     @classmethod
-    def make_unique_id(cls, variety, identifier, version):
+    def make_unique_id(cls, variety: str, identifier: str, version: int) -> str:
         return f'{variety}/{identifier}/{version}'

superduperdb/core/model.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
-import multiprocessing
-from dask.distributed import Future
-import inspect
 import dataclasses as dc
+import inspect
+import multiprocessing
 import typing as t
 
-from superduperdb.core.dataset import Dataset
-from superduperdb.core.job import Job, ComponentJob
-from superduperdb.core.metric import Metric
+from dask.distributed import Future
+from numpy import ndarray
+from sklearn.pipeline import Pipeline
+
 from superduperdb.core.artifact import Artifact
 from superduperdb.core.component import Component
+from superduperdb.core.dataset import Dataset
 from superduperdb.core.encoder import Encoder
+from superduperdb.core.job import Job, ComponentJob
+from superduperdb.core.metric import Metric
 from superduperdb.core.serializable import Serializable
 from superduperdb.datalayer.base.query import Select
 from superduperdb.misc.special_dicts import MongoStyleDict
@@ -23,10 +26,7 @@ ObjectsArg = t.Sequence[t.Union[t.Any, Artifact]]
 DataArg = t.Optional[t.Union[str, t.Sequence[str]]]
 
 
-def TrainingConfiguration(
-    identifier: str,
-    **kwargs,
-):
+def TrainingConfiguration(identifier: str, **kwargs):
     return _TrainingConfiguration(identifier=identifier, kwargs=kwargs)
 
 
@@ -57,7 +57,7 @@ class _TrainingConfiguration(Component):
 class PredictMixin:
     # ruff: noqa: F821
 
-    def _predict_one(self, X, **kwargs):
+    def _predict_one(self, X: int, **kwargs) -> int:
         if self.preprocess is not None:
             X = self.preprocess.artifact(X)
         output = self.to_call(X, **kwargs)
@@ -65,7 +65,7 @@ class PredictMixin:
             output = self.postprocess.artifact(output)
         return output
 
-    def _forward(self, X, num_workers=0):
+    def _forward(self, X: t.Sequence[int], num_workers: int = 0) -> t.Sequence[int]:
         if self.batch_predict:
             return self.to_call(X)
 
@@ -81,11 +81,13 @@ class PredictMixin:
                 outputs.append(self.to_call(r))
         return outputs
 
-    def _predict(self, X, one: bool = False, **predict_kwargs):
+    def _predict(
+        self, X: t.Any, one: bool = False, **predict_kwargs
+    ) -> t.Union[ndarray, int, t.Sequence[int]]:
         if one:
             return self._predict_one(X)
         if self.preprocess is not None:
-            X = list(map(self.preprocess.artifact, X))
+            X = [self.preprocess.artifact(i) for i in X]
         if self.collate_fn is not None:
             X = self.collate_fn(X)
         outputs = self._forward(X, **predict_kwargs)
@@ -96,16 +98,16 @@ class PredictMixin:
     def predict(
         self,
         X: t.Any,
-        db: 'BaseDatabase' = None,  # type: ignore[name-defined]
+        db: t.Optional[Datalayer] = None,
         select: t.Optional[Select] = None,
         distributed: bool = False,
         ids: t.Optional[t.Sequence[str]] = None,
         max_chunk_size: t.Optional[int] = None,
-        dependencies: t.Sequence[Job] = (),  # type: ignore[assignment]
+        dependencies: t.Sequence[Job] = (),
         watch: bool = False,
         one: bool = False,
         **kwargs,
-    ):
+    ) -> t.Any:
         # TODO this should be separated into sub-procedures
 
         if isinstance(select, dict):
@@ -124,7 +126,7 @@ class PredictMixin:
             )
 
         if db is not None:
-            db.add(self)
+            db.add(self)  # type: ignore[arg-type]
 
         if distributed:
             assert not one
@@ -243,7 +245,7 @@ class Model(Component, PredictMixin):
             self.to_call = getattr(self.object.artifact, self.predict_method)
 
     @property
-    def child_components(self) -> t.List[t.Tuple[str, str]]:
+    def child_components(self) -> t.Sequence[t.Tuple[str, str]]:
         out = []
         if self.encoder is not None:
             out.append(('encoder', 'encoder'))
@@ -252,16 +254,16 @@ class Model(Component, PredictMixin):
         return out
 
     @property
-    def training_keys(self):
+    def training_keys(self) -> t.Sequence[str]:
         out = [self.train_X]
         if self.train_y is not None:
             out.append(self.train_y)
-        return out
+        return out  # type: ignore[return-value]
 
-    def append_metrics(self, d):
+    def append_metrics(self, d: t.Dict[str, float]) -> None:
         for k in d:
-            if k not in self.metric_values:
-                self.metric_values[k] = []
+            if self.metric_values is not None:
+                self.metric_values.setdefault(k, [])
             self.metric_values[k].append(d[k])
 
     def create_predict_job(
@@ -361,7 +363,7 @@ class Model(Component, PredictMixin):
         metrics: t.Optional[t.Sequence[Metric]] = None,
         data_prefetch: bool = False,
         **kwargs,
-    ):
+    ) -> t.Optional[Pipeline]:
         if isinstance(select, dict):
             select = Serializable.deserialize(select)
 

superduperdb/core/task_workflow.py
@@ -1,9 +1,15 @@
+from __future__ import annotations
 import dataclasses as dc
 import typing as t
 
 import networkx
 
 from .job import Job
+from distributed.client import Future
+from superduperdb.core.job import ComponentJob, FunctionJob
+
+if t.TYPE_CHECKING:
+    from superduperdb.datalayer.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -11,16 +17,18 @@ class TaskWorkflow:
     database: t.Any
     G: networkx.DiGraph = dc.field(default_factory=networkx.DiGraph)
 
-    def add_edge(self, node1, node2) -> None:
+    def add_edge(self, node1: str, node2: str) -> None:
         self.G.add_edge(node1, node2)
 
-    def add_node(self, node, job) -> None:
+    def add_node(self, node: str, job: t.Union[FunctionJob, ComponentJob]) -> None:
         self.G.add_node(node, job=job)
 
-    def dependencies(self, node):
+    def dependencies(self, node: str) -> t.List[t.Optional[t.Union[t.Any, Future]]]:
         return [self.G.nodes[a]['job'].future for a in self.G.predecessors(node)]
 
-    def __call__(self, db=None, distributed: bool = False):
+    def __call__(
+        self, db: t.Optional[Datalayer] = None, distributed: bool = False
+    ) -> 'TaskWorkflow':
         current_group = [n for n in self.G.nodes if not networkx.ancestors(self.G, n)]
         done = []
         while current_group:

Pinnacle-Networks/Pinnacle

Former-commit-id: e457f86e56d5dac494519cc3b49c0c894b312e4d

superduperdb/core/component.py
@@ -7,6 +7,7 @@ import dataclasses as dc
 
 if t.TYPE_CHECKING:
     from pinnacledb.datalayer.base.datalayer import Datalayer
+    from pinnacledb.datalayer.base.dataset import Dataset
 
 
 @dc.dataclass
@@ -34,23 +35,23 @@ class Component(Serializable):
         pass
 
     @property
-    def child_components(self):
+    def child_components(self) -> t.Sequence[t.Any]:
         return []
 
     @property
-    def unique_id(self):
+    def unique_id(self) -> str:
         if self.version is None:
             raise Exception('Version not yet set for component uniqueness')
         return f'{self.variety}/{self.identifier}/{self.version}'
 
-    def dict(self):
+    def dict(self) -> t.Dict[str, t.Any]:
         return dc.asdict(self)
 
     def create_validation_job(
         self,
-        validation_set: t.Union[str, 'Dataset'],  # type: ignore[name-defined]
+        validation_set: t.Union[str, Dataset],
         metrics: t.Sequence[str],
-    ):
+    ) -> ComponentJob:
         return ComponentJob(
             component_identifier=self.identifier,
             method_name='predict',
@@ -62,9 +63,11 @@ class Component(Serializable):
             },
         )
 
-    def schedule_jobs(self, database, dependencies=()):
+    def schedule_jobs(
+        self, database: Datalayer, dependencies: t.Sequence[()] = ()
+    ) -> t.Sequence[t.Any]:
         return []
 
     @classmethod
-    def make_unique_id(cls, variety, identifier, version):
+    def make_unique_id(cls, variety: str, identifier: str, version: int) -> str:
         return f'{variety}/{identifier}/{version}'

superduperdb/core/model.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
-import multiprocessing
-from dask.distributed import Future
-import inspect
 import dataclasses as dc
+import inspect
+import multiprocessing
 import typing as t
 
-from pinnacledb.core.dataset import Dataset
-from pinnacledb.core.job import Job, ComponentJob
-from pinnacledb.core.metric import Metric
+from dask.distributed import Future
+from numpy import ndarray
+from sklearn.pipeline import Pipeline
+
 from pinnacledb.core.artifact import Artifact
 from pinnacledb.core.component import Component
+from pinnacledb.core.dataset import Dataset
 from pinnacledb.core.encoder import Encoder
+from pinnacledb.core.job import Job, ComponentJob
+from pinnacledb.core.metric import Metric
 from pinnacledb.core.serializable import Serializable
 from pinnacledb.datalayer.base.query import Select
 from pinnacledb.misc.configs import CFG
@@ -24,10 +27,7 @@ ObjectsArg = t.Sequence[t.Union[t.Any, Artifact]]
 DataArg = t.Optional[t.Union[str, t.Sequence[str]]]
 
 
-def TrainingConfiguration(
-    identifier: str,
-    **kwargs,
-):
+def TrainingConfiguration(identifier: str, **kwargs):
     return _TrainingConfiguration(identifier=identifier, kwargs=kwargs)
 
 
@@ -58,7 +58,7 @@ class _TrainingConfiguration(Component):
 class PredictMixin:
     # ruff: noqa: F821
 
-    def _predict_one(self, X, **kwargs):
+    def _predict_one(self, X: int, **kwargs) -> int:
         if self.preprocess is not None:
             X = self.preprocess.artifact(X)
         output = self.to_call(X, **kwargs)
@@ -66,7 +66,7 @@ class PredictMixin:
             output = self.postprocess.artifact(output)
         return output
 
-    def _forward(self, X, num_workers=0):
+    def _forward(self, X: t.Sequence[int], num_workers: int = 0) -> t.Sequence[int]:
         if self.batch_predict:
             return self.to_call(X)
 
@@ -82,11 +82,13 @@ class PredictMixin:
                 outputs.append(self.to_call(r))
         return outputs
 
-    def _predict(self, X, one: bool = False, **predict_kwargs):
+    def _predict(
+        self, X: t.Any, one: bool = False, **predict_kwargs
+    ) -> t.Union[ndarray, int, t.Sequence[int]]:
         if one:
             return self._predict_one(X)
         if self.preprocess is not None:
-            X = list(map(self.preprocess.artifact, X))
+            X = [self.preprocess.artifact(i) for i in X]
         if self.collate_fn is not None:
             X = self.collate_fn(X)
         outputs = self._forward(X, **predict_kwargs)
@@ -97,17 +99,17 @@ class PredictMixin:
     def predict(
         self,
         X: t.Any,
-        db: 'BaseDatabase' = None,  # type: ignore[name-defined]
+        db: t.Optional[Datalayer] = None,
         select: t.Optional[Select] = None,
         distributed: t.Optional[bool] = None,
         ids: t.Optional[t.Sequence[str]] = None,
         max_chunk_size: t.Optional[int] = None,
-        dependencies: t.Sequence[Job] = (),  # type: ignore[assignment]
+        dependencies: t.Sequence[Job] = (),
         watch: bool = False,
         one: bool = False,
         context: t.Optional[t.Dict] = None,
         **kwargs,
-    ):
+    ) -> t.Any:
         # TODO this should be separated into sub-procedures
 
         if isinstance(select, dict):
@@ -126,7 +128,7 @@ class PredictMixin:
             )
 
         if db is not None:
-            db.add(self)
+            db.add(self)  # type: ignore[arg-type]
 
         if distributed is None:
             distributed = CFG.distributed
@@ -251,7 +253,7 @@ class Model(Component, PredictMixin):
             self.to_call = getattr(self.object.artifact, self.predict_method)
 
     @property
-    def child_components(self) -> t.List[t.Tuple[str, str]]:
+    def child_components(self) -> t.Sequence[t.Tuple[str, str]]:
         out = []
         if self.encoder is not None:
             out.append(('encoder', 'encoder'))
@@ -260,16 +262,16 @@ class Model(Component, PredictMixin):
         return out
 
     @property
-    def training_keys(self):
+    def training_keys(self) -> t.Sequence[str]:
         out = [self.train_X]
         if self.train_y is not None:
             out.append(self.train_y)
-        return out
+        return out  # type: ignore[return-value]
 
-    def append_metrics(self, d):
+    def append_metrics(self, d: t.Dict[str, float]) -> None:
         for k in d:
-            if k not in self.metric_values:
-                self.metric_values[k] = []
+            if self.metric_values is not None:
+                self.metric_values.setdefault(k, [])
             self.metric_values[k].append(d[k])
 
     def create_predict_job(
@@ -369,7 +371,7 @@ class Model(Component, PredictMixin):
         metrics: t.Optional[t.Sequence[Metric]] = None,
         data_prefetch: bool = False,
         **kwargs,
-    ):
+    ) -> t.Optional[Pipeline]:
         if isinstance(select, dict):
             select = Serializable.deserialize(select)
 

superduperdb/core/task_workflow.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import dataclasses as dc
 import typing as t
 
@@ -6,6 +7,11 @@ import networkx
 from pinnacledb.misc.configs import CFG
 
 from .job import Job
+from distributed.client import Future
+from pinnacledb.core.job import ComponentJob, FunctionJob
+
+if t.TYPE_CHECKING:
+    from pinnacledb.datalayer.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -13,21 +19,22 @@ class TaskWorkflow:
     database: t.Any
     G: networkx.DiGraph = dc.field(default_factory=networkx.DiGraph)
 
-    def add_edge(self, node1, node2) -> None:
+    def add_edge(self, node1: str, node2: str) -> None:
         self.G.add_edge(node1, node2)
 
-    def add_node(self, node, job) -> None:
+    def add_node(self, node: str, job: t.Union[FunctionJob, ComponentJob]) -> None:
         self.G.add_node(node, job=job)
 
-    def dependencies(self, node):
+    def dependencies(self, node: str) -> t.List[t.Optional[t.Union[t.Any, Future]]]:
         return [self.G.nodes[a]['job'].future for a in self.G.predecessors(node)]
 
     def watch(self):
         for node in list(networkx.topological_sort(self.G)):
-            job: Job = self.G.nodes[node]['job']
-            job.watch()
+            self.G.nodes[node]['job'].watch()
 
-    def __call__(self, db=None, distributed: t.Optional[bool] = None):
+    def __call__(
+        self, db: t.Optional[Datalayer] = None, distributed: bool = False
+    ) -> 'TaskWorkflow':
         if distributed is None:
             distributed = CFG.distributed
 