superduper-io/superduper

Rename _on_load, _on_create to on_load, on_create

superduperdb/core/component.py
@@ -6,7 +6,7 @@ from superduperdb.core.serializable import Serializable
 import dataclasses as dc
 
 if t.TYPE_CHECKING:
-    pass
+    from superduperdb.datalayer.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -19,10 +19,18 @@ class Component(Serializable):
 
     variety: t.ClassVar[str]
 
-    def _on_load(self, db):
+    def on_create(self, db: Datalayer) -> None:
+        """Called the first time this component is created
+
+        :param db: the datalayer that created the component
+        """
         pass
 
-    def _on_create(self, db):
+    def on_load(self, db: Datalayer) -> None:
+        """Called when this component is loaded from the data store
+
+        :param db: the datalayer that loaded the component
+        """
         pass
 
     @property

superduperdb/core/dataset.py
@@ -1,13 +1,16 @@
+from __future__ import annotations
 from functools import cached_property
-import typing as t
-
+import dataclasses as dc
 import numpy
+import typing as t
 
 from superduperdb.core.artifact import Artifact
 from superduperdb.core.component import Component
 from superduperdb.core.document import Document
 from superduperdb.datalayer.mongodb.query import Find
-import dataclasses as dc
+
+if t.TYPE_CHECKING:
+    from superduperdb.datalayer.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -22,15 +25,23 @@ class Dataset(Component):
     raw_data: t.Optional[t.Union[Artifact, t.Any]] = None
     version: t.Optional[int] = None
 
-    def _on_create(self, db):
+    def on_create(self, db: Datalayer) -> None:
+        """Called the first time this component is created
+
+        :param db: the datalayer that created the component
+        """
         if self.raw_data is None:
-            data = list(db.execute(self.select))
+            data = list(db.execute(self.select))  # type: ignore[arg-type]
             if self.sample_size is not None and self.sample_size < len(data):
                 perm = self.random.permutation(len(data)).tolist()
                 data = [data[perm[i]] for i in range(self.sample_size)]
             self.raw_data = Artifact(artifact=[r.encode() for r in data])
 
-    def _on_load(self, db):
+    def on_load(self, db: Datalayer) -> None:
+        """Called when this component is loaded from the data store
+
+        :param db: the datalayer that loaded the component
+        """
         self.data = [
             Document(Document.decode(r.copy(), encoders=db.encoders))
             for r in self.raw_data.artifact

superduperdb/core/vector_index.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import itertools
 import typing as t
 import dataclasses as dc
@@ -10,6 +11,9 @@ from superduperdb.core.watcher import Watcher
 from superduperdb.misc.logger import logging
 from superduperdb.misc.special_dicts import MongoStyleDict
 from superduperdb.vector_search.base import VectorCollectionConfig, VectorCollectionItem
+
+if t.TYPE_CHECKING:
+    from superduperdb.datalayer.base.datalayer import Datalayer
 
 T = t.TypeVar('T')
 
@@ -51,18 +55,30 @@ class VectorIndex(Component):
     version: t.Optional[int] = None
     metric_values: t.Optional[t.Dict] = dc.field(default_factory=dict)
 
-    def _on_create(self, db):
-        if isinstance(self.indexing_watcher, str):
-            self.indexing_watcher = db.load('watcher', self.indexing_watcher)
-        if isinstance(self.compatible_watcher, str):
-            self.compatible_watcher = db.load('watcher', self.compatible_watcher)
+    def on_create(self, db: Datalayer) -> None:
+        """Called the first time this component is created
 
-    def _on_load(self, db):
-        self.vector_table = db.vector_database.get_table(
+        :param db: the datalayer that created the component
+        """
+        if isinstance(self.indexing_watcher, str):
+            self.indexing_watcher = db.load(
+                'watcher', self.indexing_watcher
+            )  # type: ignore[assignment]
+        if isinstance(self.compatible_watcher, str):
+            self.compatible_watcher = db.load(
+                'watcher', self.compatible_watcher
+            )  # type: ignore[assignment]
+
+    def on_load(self, db: Datalayer) -> None:
+        """Called when this component is loaded from the data store
+
+        :param db: the datalayer that loaded the component
+        """
+        self.vector_table = db.vector_database.get_table(  # type: ignore[call-arg]
             VectorCollectionConfig(
                 id=self.identifier,
                 dimensions=self._dimensions,
-                measure=self.measure,
+                measure=self.measure,  # type: ignore[arg-type]
             ),
             create=True,
         )

Pinnacle-Networks/Pinnacle

Former-commit-id: 2466aad47a8b8c04d12bfaa8e1f5279361523493

superduperdb/core/component.py
@@ -6,7 +6,7 @@ from pinnacledb.core.serializable import Serializable
 import dataclasses as dc
 
 if t.TYPE_CHECKING:
-    pass
+    from pinnacledb.datalayer.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -19,10 +19,18 @@ class Component(Serializable):
 
     variety: t.ClassVar[str]
 
-    def _on_load(self, db):
+    def on_create(self, db: Datalayer) -> None:
+        """Called the first time this component is created
+
+        :param db: the datalayer that created the component
+        """
         pass
 
-    def _on_create(self, db):
+    def on_load(self, db: Datalayer) -> None:
+        """Called when this component is loaded from the data store
+
+        :param db: the datalayer that loaded the component
+        """
         pass
 
     @property

superduperdb/core/dataset.py
@@ -1,13 +1,17 @@
+from __future__ import annotations
+from overrides import override
 from functools import cached_property
-import typing as t
-
+import dataclasses as dc
 import numpy
+import typing as t
 
 from pinnacledb.core.artifact import Artifact
 from pinnacledb.core.component import Component
 from pinnacledb.core.document import Document
 from pinnacledb.datalayer.mongodb.query import Find
-import dataclasses as dc
+
+if t.TYPE_CHECKING:
+    from pinnacledb.datalayer.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -22,15 +26,17 @@ class Dataset(Component):
     raw_data: t.Optional[t.Union[Artifact, t.Any]] = None
     version: t.Optional[int] = None
 
-    def _on_create(self, db):
+    @override
+    def on_create(self, db: Datalayer) -> None:
         if self.raw_data is None:
-            data = list(db.execute(self.select))
+            data = list(db.execute(self.select))  # type: ignore[arg-type]
             if self.sample_size is not None and self.sample_size < len(data):
                 perm = self.random.permutation(len(data)).tolist()
                 data = [data[perm[i]] for i in range(self.sample_size)]
             self.raw_data = Artifact(artifact=[r.encode() for r in data])
 
-    def _on_load(self, db):
+    @override
+    def on_load(self, db: Datalayer) -> None:
         self.data = [
             Document(Document.decode(r.copy(), encoders=db.encoders))
             for r in self.raw_data.artifact

superduperdb/core/vector_index.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+from overrides import override
 import itertools
 import typing as t
 import dataclasses as dc
-
 import pinnacledb as s
+
 from pinnacledb.core.component import Component
 from pinnacledb.core.document import Document
 from pinnacledb.core.encoder import Encodable
@@ -10,6 +12,9 @@ from pinnacledb.core.watcher import Watcher
 from pinnacledb.misc.logger import logging
 from pinnacledb.misc.special_dicts import MongoStyleDict
 from pinnacledb.vector_search.base import VectorCollectionConfig, VectorCollectionItem
+
+if t.TYPE_CHECKING:
+    from pinnacledb.datalayer.base.datalayer import Datalayer
 
 T = t.TypeVar('T')
 
@@ -51,18 +56,24 @@ class VectorIndex(Component):
     version: t.Optional[int] = None
     metric_values: t.Optional[t.Dict] = dc.field(default_factory=dict)
 
-    def _on_create(self, db):
+    @override
+    def on_create(self, db: Datalayer) -> None:
         if isinstance(self.indexing_watcher, str):
-            self.indexing_watcher = db.load('watcher', self.indexing_watcher)
+            self.indexing_watcher = db.load(
+                'watcher', self.indexing_watcher
+            )  # type: ignore[assignment]
         if isinstance(self.compatible_watcher, str):
-            self.compatible_watcher = db.load('watcher', self.compatible_watcher)
+            self.compatible_watcher = db.load(
+                'watcher', self.compatible_watcher
+            )  # type: ignore[assignment]
 
-    def _on_load(self, db):
-        self.vector_table = db.vector_database.get_table(
+    @override
+    def on_load(self, db: Datalayer) -> None:
+        self.vector_table = db.vector_database.get_table(  # type: ignore[call-arg]
             VectorCollectionConfig(
                 id=self.identifier,
                 dimensions=self._dimensions,
-                measure=self.measure,
+                measure=self.measure,  # type: ignore[arg-type]
             ),
             create=True,
         )