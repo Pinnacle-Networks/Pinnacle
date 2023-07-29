superduper-io/superduper

Run isort on each Python file

superduperdb/cli/config.py
@@ -1,6 +1,8 @@
-from . import command
-from superduperdb import CFG
 from typer import Option
+
+from superduperdb import CFG
+
+from . import command
 
 
 @command(help='Print all the SuperDuperDB configs as JSON')

superduperdb/datalayer/base/artifacts.py
@@ -1,8 +1,8 @@
-from abc import ABC, abstractmethod
 import hashlib
 import typing as t
+from abc import ABC, abstractmethod
 
-from superduperdb.misc.serialization import serializers, Info
+from superduperdb.misc.serialization import Info, serializers
 
 
 class ArtifactStore(ABC):

superduperdb/datalayer/mongodb/data_backend.py
@@ -5,8 +5,8 @@ from pymongo import MongoClient
 from superduperdb.core.document import Document
 from superduperdb.core.serializable import Serializable
 from superduperdb.datalayer.base.data_backend import BaseDataBackend
-from superduperdb.misc.special_dicts import MongoStyleDict
 from superduperdb.misc.logger import logging
+from superduperdb.misc.special_dicts import MongoStyleDict
 
 
 class MongoDataBackend(BaseDataBackend):

superduperdb/misc/typed_cache.py
@@ -1,7 +1,9 @@
-from .key_cache import KeyCache
-from threading import Lock
-from superduperdb.misc import dataclasses as dc
 import typing as t
+from threading import Lock
+
+from superduperdb.misc import dataclasses as dc
+
+from .key_cache import KeyCache
 
 SEP = '-'
 

tests/unittests/core/test_model.py
@@ -1,7 +1,7 @@
 import torch
 
-from superduperdb.models.torch.wrapper import TorchModel
 from superduperdb.datalayer.mongodb.query import Collection
+from superduperdb.models.torch.wrapper import TorchModel
 
 
 def test_predict(random_data, float_tensors_32):

tests/unittests/vector_search/test_lancedb.py
@@ -1,13 +1,12 @@
 import os
 import tempfile
+
 import numpy as np
-
-import pytest
 import pyarrow as pa
+import pytest
 
-from superduperdb.vector_search.base import VectorCollectionConfig
-from superduperdb.vector_search.base import VectorCollectionItem
 from superduperdb.misc.config import LanceDB
+from superduperdb.vector_search.base import VectorCollectionConfig, VectorCollectionItem
 from superduperdb.vector_search.lancedb_client import (
     LanceDBClient,
     LanceTable,

Pinnacle-Networks/Pinnacle

Former-commit-id: 8941d6fb5c310967a20a14b7a41602465cdb2c0d

superduperdb/cli/config.py
@@ -1,6 +1,8 @@
-from . import command
-from pinnacledb import CFG
 from typer import Option
+
+from pinnacledb import CFG
+
+from . import command
 
 
 @command(help='Print all the SuperDuperDB configs as JSON')

superduperdb/datalayer/base/artifacts.py
@@ -1,8 +1,8 @@
-from abc import ABC, abstractmethod
 import hashlib
 import typing as t
+from abc import ABC, abstractmethod
 
-from pinnacledb.misc.serialization import serializers, Info
+from pinnacledb.misc.serialization import Info, serializers
 
 
 class ArtifactStore(ABC):

superduperdb/datalayer/mongodb/data_backend.py
@@ -5,8 +5,8 @@ from pymongo import MongoClient
 from pinnacledb.core.document import Document
 from pinnacledb.core.serializable import Serializable
 from pinnacledb.datalayer.base.data_backend import BaseDataBackend
-from pinnacledb.misc.special_dicts import MongoStyleDict
 from pinnacledb.misc.logger import logging
+from pinnacledb.misc.special_dicts import MongoStyleDict
 
 
 class MongoDataBackend(BaseDataBackend):

superduperdb/misc/typed_cache.py
@@ -1,7 +1,9 @@
-from .key_cache import KeyCache
-from threading import Lock
-from pinnacledb.misc import dataclasses as dc
 import typing as t
+from threading import Lock
+
+from pinnacledb.misc import dataclasses as dc
+
+from .key_cache import KeyCache
 
 SEP = '-'
 

tests/unittests/core/test_model.py
@@ -1,7 +1,7 @@
 import torch
 
-from pinnacledb.models.torch.wrapper import TorchModel
 from pinnacledb.datalayer.mongodb.query import Collection
+from pinnacledb.models.torch.wrapper import TorchModel
 
 
 def test_predict(random_data, float_tensors_32):

tests/unittests/vector_search/test_lancedb.py
@@ -1,13 +1,12 @@
 import os
 import tempfile
+
 import numpy as np
-
-import pytest
 import pyarrow as pa
+import pytest
 
-from pinnacledb.vector_search.base import VectorCollectionConfig
-from pinnacledb.vector_search.base import VectorCollectionItem
 from pinnacledb.misc.config import LanceDB
+from pinnacledb.vector_search.base import VectorCollectionConfig, VectorCollectionItem
 from pinnacledb.vector_search.lancedb_client import (
     LanceDBClient,
     LanceTable,