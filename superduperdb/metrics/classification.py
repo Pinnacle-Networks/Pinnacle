superduper-io/superduper

Use cleanmonkey to add a lot of typing

superduperdb/metrics/classification.py
@@ -1,8 +1,22 @@
-def compute_classification_metrics(validation_data, model, metrics):
+from __future__ import annotations
+
+import typing as t
+
+if t.TYPE_CHECKING:
+    from superduperdb.core.metric import Metric
+    from superduperdb.models.torch.wrapper import TorchModel
+
+
+def compute_classification_metrics(
+    validation_data: t.List[t.Dict[str, t.Any]],
+    model: TorchModel,
+    metrics: t.List[Metric],
+) -> t.Dict[str, float]:
     X, y = model.training_keys
     out = {}
     predictions = model.predict([r[X] for r in validation_data])
-    targets = [r[y] for r in validation_data]
+    targets = t.cast(t.List[int], [r[y] for r in validation_data])
+    assert all(isinstance(t, int) for t in targets)
     for m in metrics:
         out[m.identifier] = sum(
             [m(pred, target) for pred, target in zip(predictions, targets)]

superduperdb/misc/configs.py
@@ -5,8 +5,7 @@ from functools import cached_property
 from pathlib import Path
 
 from superduperdb.misc import config_dicts
-
-from . import config
+from superduperdb.misc.config import Config
 
 File = t.Union[Path, str]
 
@@ -54,8 +53,8 @@ class ConfigSettings:
         return self.cls(**config_dicts.combine((*data, environ_dict)))
 
 
-def build_config():
-    CONFIG = ConfigSettings(config.Config, _ALL_CONFIGS, PREFIX)
+def build_config() -> Config:
+    CONFIG = ConfigSettings(Config, _ALL_CONFIGS, PREFIX)
     return CONFIG.config
 
 

superduperdb/misc/serialization.py
@@ -4,10 +4,7 @@ import typing as t
 from abc import ABC
 
 import dill
-import torch
 import typing_extensions as te
-
-from superduperdb.models.torch.utils import device_of
 
 Info = t.Optional[t.Dict[str, t.Any]]
 
@@ -53,6 +50,10 @@ class DillSerializer(Serializer):
 class TorchSerializer(Serializer):
     @staticmethod
     def encode(object: t.Any, info: Info = None) -> bytes:
+        import torch
+
+        from superduperdb.models.torch.utils import device_of
+
         if not isinstance(object, dict):
             was_gpu = str(device_of(object)) == 'cuda'
             object.to('cpu')
@@ -67,6 +68,8 @@ class TorchSerializer(Serializer):
 
     @staticmethod
     def decode(b: bytes, info: Info = None) -> t.Any:
+        import torch
+
         return torch.load(io.BytesIO(b))
 
 

superduperdb/models/torch/utils.py
@@ -1,7 +1,15 @@
+from __future__ import annotations
+
+import typing as t
 from contextlib import contextmanager
 
+if t.TYPE_CHECKING:
+    from torch import device as _device
 
-def device_of(model):
+    from superduperdb.core.model import Model
+
+
+def device_of(model: Model) -> t.Union[_device, str]:
     """
     Get device of a model.
 
@@ -14,7 +22,7 @@ def device_of(model):
 
 
 @contextmanager
-def eval(model):
+def eval(model: Model) -> t.Iterator[None]:
     """
     Temporarily set a model to evaluation mode.
 
@@ -30,7 +38,7 @@ def eval(model):
 
 
 @contextmanager
-def set_device(model, device):
+def set_device(model: Model, device: _device):
     """
     Temporarily set a device of a model.
 
@@ -45,7 +53,10 @@ def set_device(model, device):
         model.to(device_before)
 
 
-def to_device(item, device):
+def to_device(
+    item: t.Any,  # lists or dicts of Tensors
+    device: t.Union[str, _device],
+) -> t.Any:
     """
     Send tensor leaves of nested list/ dictionaries/ tensors to device.
 

Pinnacle-Networks/Pinnacle

Former-commit-id: 433d39470b6faa968b346ee513ea00ce3fe64f8e

superduperdb/metrics/classification.py
@@ -1,8 +1,22 @@
-def compute_classification_metrics(validation_data, model, metrics):
+from __future__ import annotations
+
+import typing as t
+
+if t.TYPE_CHECKING:
+    from pinnacledb.core.metric import Metric
+    from pinnacledb.models.torch.wrapper import TorchModel
+
+
+def compute_classification_metrics(
+    validation_data: t.List[t.Dict[str, t.Any]],
+    model: TorchModel,
+    metrics: t.List[Metric],
+) -> t.Dict[str, float]:
     X, y = model.training_keys
     out = {}
     predictions = model.predict([r[X] for r in validation_data])
-    targets = [r[y] for r in validation_data]
+    targets = t.cast(t.List[int], [r[y] for r in validation_data])
+    assert all(isinstance(t, int) for t in targets)
     for m in metrics:
         out[m.identifier] = sum(
             [m(pred, target) for pred, target in zip(predictions, targets)]

superduperdb/misc/configs.py
@@ -5,8 +5,7 @@ from functools import cached_property
 from pathlib import Path
 
 from pinnacledb.misc import config_dicts
-
-from . import config
+from pinnacledb.misc.config import Config
 
 File = t.Union[Path, str]
 
@@ -54,8 +53,8 @@ class ConfigSettings:
         return self.cls(**config_dicts.combine((*data, environ_dict)))
 
 
-def build_config():
-    CONFIG = ConfigSettings(config.Config, _ALL_CONFIGS, PREFIX)
+def build_config() -> Config:
+    CONFIG = ConfigSettings(Config, _ALL_CONFIGS, PREFIX)
     return CONFIG.config
 
 

superduperdb/misc/serialization.py
@@ -4,10 +4,7 @@ import typing as t
 from abc import ABC
 
 import dill
-import torch
 import typing_extensions as te
-
-from pinnacledb.models.torch.utils import device_of
 
 Info = t.Optional[t.Dict[str, t.Any]]
 
@@ -53,6 +50,10 @@ class DillSerializer(Serializer):
 class TorchSerializer(Serializer):
     @staticmethod
     def encode(object: t.Any, info: Info = None) -> bytes:
+        import torch
+
+        from pinnacledb.models.torch.utils import device_of
+
         if not isinstance(object, dict):
             was_gpu = str(device_of(object)) == 'cuda'
             object.to('cpu')
@@ -67,6 +68,8 @@ class TorchSerializer(Serializer):
 
     @staticmethod
     def decode(b: bytes, info: Info = None) -> t.Any:
+        import torch
+
         return torch.load(io.BytesIO(b))
 
 

superduperdb/models/torch/utils.py
@@ -1,7 +1,15 @@
+from __future__ import annotations
+
+import typing as t
 from contextlib import contextmanager
 
+if t.TYPE_CHECKING:
+    from torch import device as _device
 
-def device_of(model):
+    from pinnacledb.core.model import Model
+
+
+def device_of(model: Model) -> t.Union[_device, str]:
     """
     Get device of a model.
 
@@ -14,7 +22,7 @@ def device_of(model):
 
 
 @contextmanager
-def eval(model):
+def eval(model: Model) -> t.Iterator[None]:
     """
     Temporarily set a model to evaluation mode.
 
@@ -30,7 +38,7 @@ def eval(model):
 
 
 @contextmanager
-def set_device(model, device):
+def set_device(model: Model, device: _device):
     """
     Temporarily set a device of a model.
 
@@ -45,7 +53,10 @@ def set_device(model, device):
         model.to(device_before)
 
 
-def to_device(item, device):
+def to_device(
+    item: t.Any,  # lists or dicts of Tensors
+    device: t.Union[str, _device],
+) -> t.Any:
     """
     Send tensor leaves of nested list/ dictionaries/ tensors to device.
 