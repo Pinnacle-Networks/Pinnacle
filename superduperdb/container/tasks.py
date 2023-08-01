superduper-io/superduper

Rename and reorganize code (fix #450)

superduperdb/container/tasks.py
@@ -1,0 +1,94 @@
+import contextlib
+import inspect
+import traceback
+
+
+def method_job(
+    cfg,
+    variety,
+    identifier,
+    method_name,
+    args,
+    kwargs,
+    job_id,
+    dependencies=(),
+):
+    from superduperdb.db.base.build import build_datalayer
+
+    cfg.distributed = False
+    db = build_datalayer(cfg)
+    component = db.load(variety, identifier)
+    method = getattr(component, method_name)
+    db.metadata.update_job(job_id, 'status', 'running')
+
+    if 'distributed' in inspect.signature(method).parameters:
+        kwargs['distributed'] = False
+    try:
+        handle_function_output(
+            method,
+            db=db,
+            job_id=job_id,
+            args=args,
+            kwargs=kwargs,
+        )
+    except Exception as e:
+        tb = traceback.format_exc()
+        db.metadata.update_job(job_id, 'status', 'failed')
+        db.metadata.update_job(job_id, 'msg', tb)
+        raise e
+    db.metadata.update_job(job_id, 'status', 'success')
+
+
+class Logger:
+    def __init__(self, database, id_, stream='stdout'):
+        self.database = database
+        self.id_ = id_
+        self.stream = stream
+
+    def write(self, message):
+        self.database.metadata.write_output_to_job(
+            self.id_, message, stream=self.stream
+        )
+
+    def flush(self):
+        pass
+
+
+def handle_function_output(function, db, job_id, args, kwargs):
+    with contextlib.redirect_stdout(Logger(db, job_id)):
+        with contextlib.redirect_stderr(Logger(db, job_id, stream='stderr')):
+            return function(db=db, *args, **kwargs)
+
+
+def callable_job(
+    cfg,
+    function_to_call,
+    args,
+    kwargs,
+    job_id,
+    dependencies=(),
+):
+    from superduperdb.db.base.build import build_datalayer
+
+    cfg.distributed = False
+    db = build_datalayer(cfg)
+    db.metadata.update_job(job_id, 'status', 'running')
+    output = None
+    if 'distributed' in inspect.signature(function_to_call).parameters:
+        kwargs['distributed'] = False
+    try:
+        output = handle_function_output(
+            function_to_call,
+            db=db,
+            job_id=job_id,
+            args=args,
+            kwargs=kwargs,
+        )
+    except Exception as e:
+        tb = traceback.format_exc()
+        db.metadata.update_job(job_id, 'status', 'failed')
+        db.metadata.update_job(job_id, 'msg', tb)
+        raise e
+    else:
+        db.metadata.update_job(job_id, 'status', 'success')
+    return output

superduperdb/db/base/download_content.py
@@ -1,0 +1,95 @@
+import typing as t
+
+from superduperdb import logging
+from superduperdb.container.document import Document
+from superduperdb.container.serializable import Serializable
+from superduperdb.db.base.downloads import Downloader, gather_uris
+from superduperdb.db.base.query import Insert, Select
+
+
+def download_content(
+    db,
+    query: t.Union[Select, Insert, t.Dict],
+    ids: t.Optional[t.Sequence[str]] = None,
+    documents: t.Optional[t.Sequence[Document]] = None,
+    timeout: t.Optional[int] = None,
+    raises: bool = True,
+    n_download_workers: t.Optional[int] = None,
+    headers: t.Optional[t.Dict] = None,
+    download_update: t.Optional[t.Callable] = None,
+    **kwargs,
+) -> t.Optional[t.Sequence[Document]]:
+    """
+    Download content contained in uploaded data. Items to be downloaded are identifier
+    via the subdocuments in the form exemplified below. By default items are downloaded
+    to the database, unless a ``download_update`` function is provided.
+
+    >>> d = {"_content": {"uri": "<uri>", "encoder": "<encoder-identifier>"}}
+    >>> def update(key, id, bytes):
+    >>> ... with open(f'/tmp/{key}+{id}', 'wb') as f:
+    >>> ...     f.write(bytes)
+    >>> download_content(None, None, ids=["0"], documents=[d]))
+    ...
+    """
+    logging.debug(query)
+    logging.debug(ids)
+    update_db = False
+    if isinstance(query, dict):
+        query = Serializable.deserialize(query)
+
+    if documents is not None:
+        pass
+    elif isinstance(query, Select):
+        update_db = True
+        if ids is None:
+            documents = list(db.select(query).raw_cursor)
+        else:
+            select = query.select_using_ids(ids)
+            documents = list(db.select(select))
+    else:
+        documents = query.documents  # type: ignore[union-attr]
+
+    uris, keys, place_ids = gather_uris([d.encode() for d in documents])
+    logging.info(f'found {len(uris)} uris')
+    if not uris:
+        return None
+
+    if n_download_workers is None:
+        try:
+            n_download_workers = db.metadata.get_metadata(key='n_download_workers')
+        except TypeError:
+            n_download_workers = 0
+
+    if headers is None:
+        try:
+            headers = db.metadata.get_metadata(key='headers')
+        except TypeError:
+            pass
+
+    if timeout is None:
+        try:
+            timeout = db.metadata.get_metadata(key='download_timeout')
+        except TypeError:
+            pass
+
+    def _download_update(key, id, bytes_):
+        return query.download_update(db=db, key=key, id=id, bytes=bytes_)
+
+    downloader = Downloader(
+        uris=uris,
+        ids=place_ids,
+        keys=keys,
+        update_one=download_update or _download_update,
+        n_workers=n_download_workers,
+        timeout=timeout,
+        headers=headers,
+        raises=raises,
+    )
+    downloader.go()
+    if update_db:
+        return None
+    for id_, key in zip(place_ids, keys):
+        documents[id_] = db.db.set_content_bytes(  # type: ignore[index]
+            documents[id_], key, downloader.results[id_]
+        )
+    return documents

Pinnacle-Networks/Pinnacle

Former-commit-id: 7fbdbf61aa042abc344580b69d6ad7a2bbdd2e0a

superduperdb/container/tasks.py
@@ -1,0 +1,94 @@
+import contextlib
+import inspect
+import traceback
+
+
+def method_job(
+    cfg,
+    variety,
+    identifier,
+    method_name,
+    args,
+    kwargs,
+    job_id,
+    dependencies=(),
+):
+    from pinnacledb.db.base.build import build_datalayer
+
+    cfg.distributed = False
+    db = build_datalayer(cfg)
+    component = db.load(variety, identifier)
+    method = getattr(component, method_name)
+    db.metadata.update_job(job_id, 'status', 'running')
+
+    if 'distributed' in inspect.signature(method).parameters:
+        kwargs['distributed'] = False
+    try:
+        handle_function_output(
+            method,
+            db=db,
+            job_id=job_id,
+            args=args,
+            kwargs=kwargs,
+        )
+    except Exception as e:
+        tb = traceback.format_exc()
+        db.metadata.update_job(job_id, 'status', 'failed')
+        db.metadata.update_job(job_id, 'msg', tb)
+        raise e
+    db.metadata.update_job(job_id, 'status', 'success')
+
+
+class Logger:
+    def __init__(self, database, id_, stream='stdout'):
+        self.database = database
+        self.id_ = id_
+        self.stream = stream
+
+    def write(self, message):
+        self.database.metadata.write_output_to_job(
+            self.id_, message, stream=self.stream
+        )
+
+    def flush(self):
+        pass
+
+
+def handle_function_output(function, db, job_id, args, kwargs):
+    with contextlib.redirect_stdout(Logger(db, job_id)):
+        with contextlib.redirect_stderr(Logger(db, job_id, stream='stderr')):
+            return function(db=db, *args, **kwargs)
+
+
+def callable_job(
+    cfg,
+    function_to_call,
+    args,
+    kwargs,
+    job_id,
+    dependencies=(),
+):
+    from pinnacledb.db.base.build import build_datalayer
+
+    cfg.distributed = False
+    db = build_datalayer(cfg)
+    db.metadata.update_job(job_id, 'status', 'running')
+    output = None
+    if 'distributed' in inspect.signature(function_to_call).parameters:
+        kwargs['distributed'] = False
+    try:
+        output = handle_function_output(
+            function_to_call,
+            db=db,
+            job_id=job_id,
+            args=args,
+            kwargs=kwargs,
+        )
+    except Exception as e:
+        tb = traceback.format_exc()
+        db.metadata.update_job(job_id, 'status', 'failed')
+        db.metadata.update_job(job_id, 'msg', tb)
+        raise e
+    else:
+        db.metadata.update_job(job_id, 'status', 'success')
+    return output

superduperdb/db/base/download_content.py
@@ -1,0 +1,95 @@
+import typing as t
+
+from pinnacledb import logging
+from pinnacledb.container.document import Document
+from pinnacledb.container.serializable import Serializable
+from pinnacledb.db.base.downloads import Downloader, gather_uris
+from pinnacledb.db.base.query import Insert, Select
+
+
+def download_content(
+    db,
+    query: t.Union[Select, Insert, t.Dict],
+    ids: t.Optional[t.Sequence[str]] = None,
+    documents: t.Optional[t.Sequence[Document]] = None,
+    timeout: t.Optional[int] = None,
+    raises: bool = True,
+    n_download_workers: t.Optional[int] = None,
+    headers: t.Optional[t.Dict] = None,
+    download_update: t.Optional[t.Callable] = None,
+    **kwargs,
+) -> t.Optional[t.Sequence[Document]]:
+    """
+    Download content contained in uploaded data. Items to be downloaded are identifier
+    via the subdocuments in the form exemplified below. By default items are downloaded
+    to the database, unless a ``download_update`` function is provided.
+
+    >>> d = {"_content": {"uri": "<uri>", "encoder": "<encoder-identifier>"}}
+    >>> def update(key, id, bytes):
+    >>> ... with open(f'/tmp/{key}+{id}', 'wb') as f:
+    >>> ...     f.write(bytes)
+    >>> download_content(None, None, ids=["0"], documents=[d]))
+    ...
+    """
+    logging.debug(query)
+    logging.debug(ids)
+    update_db = False
+    if isinstance(query, dict):
+        query = Serializable.deserialize(query)
+
+    if documents is not None:
+        pass
+    elif isinstance(query, Select):
+        update_db = True
+        if ids is None:
+            documents = list(db.select(query).raw_cursor)
+        else:
+            select = query.select_using_ids(ids)
+            documents = list(db.select(select))
+    else:
+        documents = query.documents  # type: ignore[union-attr]
+
+    uris, keys, place_ids = gather_uris([d.encode() for d in documents])
+    logging.info(f'found {len(uris)} uris')
+    if not uris:
+        return None
+
+    if n_download_workers is None:
+        try:
+            n_download_workers = db.metadata.get_metadata(key='n_download_workers')
+        except TypeError:
+            n_download_workers = 0
+
+    if headers is None:
+        try:
+            headers = db.metadata.get_metadata(key='headers')
+        except TypeError:
+            pass
+
+    if timeout is None:
+        try:
+            timeout = db.metadata.get_metadata(key='download_timeout')
+        except TypeError:
+            pass
+
+    def _download_update(key, id, bytes_):
+        return query.download_update(db=db, key=key, id=id, bytes=bytes_)
+
+    downloader = Downloader(
+        uris=uris,
+        ids=place_ids,
+        keys=keys,
+        update_one=download_update or _download_update,
+        n_workers=n_download_workers,
+        timeout=timeout,
+        headers=headers,
+        raises=raises,
+    )
+    downloader.go()
+    if update_db:
+        return None
+    for id_, key in zip(place_ids, keys):
+        documents[id_] = db.db.set_content_bytes(  # type: ignore[index]
+            documents[id_], key, downloader.results[id_]
+        )
+    return documents