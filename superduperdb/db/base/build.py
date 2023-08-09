superduper-io/superduper

Add hybrid data uris + records

superduperdb/db/base/build.py
@@ -13,6 +13,12 @@ from superduperdb.server.dask_client import dask_client
 
 
 def build_vector_database(cfg):
+    """
+    Build vector database as per ``vector_database = DB.vector_database``
+    from configuration.
+
+    :param cfg: configuration to use. (See ``superduperdb.CFG.vector_search``)
+    """
     cls = vector_database_stores[cfg.__class__]
     sig = inspect.signature(cls.__init__)
     kwargs = {k: v for k, v in cfg.dict().items() if k in sig.parameters}
@@ -23,6 +29,7 @@ def build_datalayer(cfg=None, **connections) -> DB:
     """
     Build db as per ``db = superduper(db)`` from configuration.
 
+    :param cfg: configuration to use. If None, use ``superduperdb.CFG``.
     :param connections: cache of connections to reuse in the build process.
     """
     cfg = cfg or s.CFG

superduperdb/db/base/cursor.py
@@ -4,13 +4,26 @@ from functools import cached_property
 
 from pymongo.cursor import Cursor
 
+from superduperdb import CFG
 from superduperdb.container.document import Document
 from superduperdb.container.encoder import Encoder
+from superduperdb.misc.files import load_uris
 from superduperdb.misc.special_dicts import MongoStyleDict
 
 
 @dc.dataclass
 class SuperDuperCursor:
+    """
+    A cursor that wraps a cursor and returns ``Document`` wrapping
+    a dict including ``Encodable`` objects.
+
+    :param raw_cursor: the cursor to wrap
+    :param id_field: the field to use as the document id
+    :param encoders: a dict of encoders to use to decode the documents
+    :param features: a dict of features to add to the documents
+    :param scores: a dict of scores to add to the documents
+    """
+
     raw_cursor: Cursor
     id_field: str
     encoders: t.Dict[str, Encoder] = dc.field(default_factory=dict)
@@ -27,6 +40,12 @@ class SuperDuperCursor:
 
     @staticmethod
     def add_features(r, features):
+        """
+        Add features to a document.
+
+        :param r: the document
+        :param features: the features to add
+        """
         r = MongoStyleDict(r)
         for k in features:
             r[k] = r['_outputs'][k][features[k]]
@@ -37,9 +56,15 @@ class SuperDuperCursor:
         return r
 
     def count(self):
+        """
+        Count the number of documents in the cursor.
+        """
         return len(list(self.raw_cursor.clone()))
 
     def limit(self, *args, **kwargs):
+        """
+        Limit the number of documents in the cursor.
+        """
         return SuperDuperCursor(
             raw_cursor=self.raw_cursor.limit(*args, **kwargs),
             id_field=self.id_field,
@@ -50,6 +75,9 @@ class SuperDuperCursor:
 
     @staticmethod
     def wrap_document(r, encoders):
+        """
+        Wrap a document in a ``Document``.
+        """
         return Document(Document.decode(r, encoders))
 
     def __iter__(self):
@@ -68,7 +96,8 @@ class SuperDuperCursor:
             r['_score'] = self.scores[str(r[self.id_field])]
         if self.features is not None and self.features:
             r = self.add_features(r, features=self.features)
-
+        if CFG.downloads.hybrid:
+            load_uris(r, CFG.downloads.root)
         return self.wrap_document(r, self.encoders)
 
     next = __next__

superduperdb/db/base/download.py
@@ -1,3 +1,5 @@
+import hashlib
+import os
 import re
 import signal
 import sys
@@ -6,12 +8,13 @@ import warnings
 from contextlib import contextmanager
 from io import BytesIO
 from multiprocessing.pool import ThreadPool
+from typing import Any
 
 import boto3
 import requests
 from tqdm import tqdm
 
-from superduperdb import logging
+from superduperdb import CFG, logging
 
 
 class TimeoutException(Exception):
@@ -122,6 +125,15 @@ class BaseDownloader:
 
         self._parallel_go(f)
 
+    def _check_exists_if_hybrid(self, uri):
+        if uri.startswith('file://'):
+            file = f'{CFG.downloads.root}/{uri.split("file://")[-1]}'
+        else:
+            file = f'{CFG.downloads.root}/{hashlib.sha1(uri.encode()).hexdigest()}'
+        if os.path.exists(file):
+            return True
+        return False
+
     def _parallel_go(self, f):
         pool = ThreadPool(self.n_workers)
         try:
@@ -140,6 +152,16 @@ class BaseDownloader:
             f(i)
 
 
+class SaveFile:
+    def __init__(self, root: str):
+        self.root = root
+
+    def __call__(self, bytes_: bytearray, uri: str, **kwargs) -> Any:
+        path = f'{self.root}/{hashlib.sha1(uri.encode()).hexdigest()}'
+        with open(path, 'wb') as f:
+            f.write(bytes_)
+
+
 class Downloader(BaseDownloader):
     """
 
@@ -148,10 +170,10 @@ class Downloader(BaseDownloader):
     :param ids: list of ids of rows/ documents to update
     :param keys: list of keys in rows/ documents to insert to
     :param n_workers: number of multiprocessing workers
-    :param raises: raises error ``True``/``False``
     :param headers: dictionary of request headers passed to``requests`` package
     :param skip_existing: if ``True`` then don't bother getting already present data
     :param timeout: set seconds until request times out
+    :param raises: raises error ``True``/``False``
     """
 
     results: t.Dict[int, str]
@@ -183,28 +205,23 @@ class Downloader(BaseDownloader):
         self.fetcher = Fetcher(headers=headers, n_workers=n_workers)
 
     def _download(self, i):
-        uri = self.uris[i]
-        _id = self.ids[i]
-        content = self.fetcher(uri)
-        self.update_one(id=self.ids[i], key=self.keys[i], bytes_=content)
-
-
-class InMemoryDownloader(BaseDownloader):
-    def __init__(self, *args, headers=None, n_workers=0, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.results = {}
-        self.fetcher = Fetcher(headers=headers, n_workers=n_workers)
-
-    def _download(self, i):
+        if CFG.downloads.hybrid:
+            if self._check_exists_if_hybrid(self.uris[i]):
+                return
         content = self.fetcher(self.uris[i])
-        self.results[i] = content
+        self.update_one(
+            id=self.ids[i],
+            key=self.keys[i],
+            bytes_=content,
+            uri=self.uris[i],
+        )
 
 
 def gather_uris(
     documents: t.Sequence[t.Dict], gather_ids: bool = True
 ) -> t.Tuple[t.List[str], t.List[str], t.List[int]]:
     """
-    Get the URLS out of all documents as denoted by ``{"_content": ...}``
+    Get the uris out of all documents as denoted by ``{"_content": ...}``
 
     :param documents: list of dictionaries
     """

superduperdb/db/base/download_content.py
@@ -1,9 +1,9 @@
 import typing as t
 
-from superduperdb import logging
+from superduperdb import CFG, logging
 from superduperdb.container.document import Document
 from superduperdb.container.serializable import Serializable
-from superduperdb.db.base.download import Downloader, gather_uris
+from superduperdb.db.base.download import Downloader, SaveFile, gather_uris
 from superduperdb.db.base.query import Insert, Select
 
 
@@ -42,10 +42,10 @@ def download_content(
     elif isinstance(query, Select):
         update_db = True
         if ids is None:
-            documents = list(db.select(query).raw_cursor)
+            documents = list(db.execute(query).raw_cursor)
         else:
             select = query.select_using_ids(ids)
-            documents = list(db.select(select))
+            documents = list(db.execute(select))
     else:
         documents = query.documents  # type: ignore[union-attr]
 
@@ -72,8 +72,12 @@ def download_content(
         except TypeError:
             pass
 
-    def _download_update(key, id, bytes_):
-        return query.download_update(db=db, key=key, id=id, bytes=bytes_)
+    if CFG.downloads.hybrid:
+        _download_update = SaveFile(CFG.downloads.root)
+    else:
+
+        def _download_update(key, id, bytes_, **kwargs):  # type: ignore[misc]
+            return query.download_update(db=db, key=key, id=id, bytes=bytes_)
 
     downloader = Downloader(
         uris=uris,

test/unittest/db/base/test_downloaders.py
@@ -1,10 +1,63 @@
 import os
+import shutil
 
+import pytest
+
+from superduperdb import CFG
+from superduperdb.container.document import Document
 from superduperdb.db.base.download import Fetcher
+from superduperdb.db.mongodb.query import Collection
+from superduperdb.ext.pillow.image import pil_image
 
 remote = os.environ.get('SUPERDUPERDB_REMOTE_TEST', 'local')
+
+
+IMAGE_URL = 'https://www.superduperdb.com/logos/white.png'
 
 
 def test_s3_and_web():
     if remote == 'remote':
         Fetcher()('s3://superduperdb-bucket/img/black.png')
+
+
+@pytest.fixture
+def make_hybrid_cfg():
+    old = CFG.downloads.hybrid
+    CFG.downloads.hybrid = True
+    root_old = CFG.downloads.root
+    CFG.downloads.root = '.tdir_hybrid'
+    os.makedirs('.tdir_hybrid', exist_ok=True)
+    yield CFG
+    CFG.downloads.hybrid = old
+    CFG.downloads.root = root_old
+    shutil.rmtree('.tdir_hybrid')
+
+
+def test_file_blobs(empty, make_hybrid_cfg):
+    to_insert = [
+        Document(
+            {
+                'item': {
+                    '_content': {
+                        'uri': IMAGE_URL,
+                        'encoder': 'pil_image',
+                    }
+                },
+                'other': {
+                    'item': {
+                        '_content': {
+                            'uri': IMAGE_URL,
+                            'encoder': 'pil_image',
+                        }
+                    }
+                },
+            }
+        )
+        for _ in range(2)
+    ]
+
+    empty.execute(Collection('documents').insert_many(to_insert, encoders=(pil_image,)))
+    empty.execute(Collection('documents').find_one())
+    imgs = os.listdir('.tdir_hybrid/')
+
+    assert imgs

Pinnacle-Networks/Pinnacle

Former-commit-id: 11ec9780e65c87eec9c08956f4a6edac4de8e5ba

superduperdb/db/base/build.py
@@ -13,6 +13,12 @@ from pinnacledb.server.dask_client import dask_client
 
 
 def build_vector_database(cfg):
+    """
+    Build vector database as per ``vector_database = DB.vector_database``
+    from configuration.
+
+    :param cfg: configuration to use. (See ``pinnacledb.CFG.vector_search``)
+    """
     cls = vector_database_stores[cfg.__class__]
     sig = inspect.signature(cls.__init__)
     kwargs = {k: v for k, v in cfg.dict().items() if k in sig.parameters}
@@ -23,6 +29,7 @@ def build_datalayer(cfg=None, **connections) -> DB:
     """
     Build db as per ``db = pinnacle(db)`` from configuration.
 
+    :param cfg: configuration to use. If None, use ``pinnacledb.CFG``.
     :param connections: cache of connections to reuse in the build process.
     """
     cfg = cfg or s.CFG

superduperdb/db/base/cursor.py
@@ -4,13 +4,26 @@ from functools import cached_property
 
 from pymongo.cursor import Cursor
 
+from pinnacledb import CFG
 from pinnacledb.container.document import Document
 from pinnacledb.container.encoder import Encoder
+from pinnacledb.misc.files import load_uris
 from pinnacledb.misc.special_dicts import MongoStyleDict
 
 
 @dc.dataclass
 class SuperDuperCursor:
+    """
+    A cursor that wraps a cursor and returns ``Document`` wrapping
+    a dict including ``Encodable`` objects.
+
+    :param raw_cursor: the cursor to wrap
+    :param id_field: the field to use as the document id
+    :param encoders: a dict of encoders to use to decode the documents
+    :param features: a dict of features to add to the documents
+    :param scores: a dict of scores to add to the documents
+    """
+
     raw_cursor: Cursor
     id_field: str
     encoders: t.Dict[str, Encoder] = dc.field(default_factory=dict)
@@ -27,6 +40,12 @@ class SuperDuperCursor:
 
     @staticmethod
     def add_features(r, features):
+        """
+        Add features to a document.
+
+        :param r: the document
+        :param features: the features to add
+        """
         r = MongoStyleDict(r)
         for k in features:
             r[k] = r['_outputs'][k][features[k]]
@@ -37,9 +56,15 @@ class SuperDuperCursor:
         return r
 
     def count(self):
+        """
+        Count the number of documents in the cursor.
+        """
         return len(list(self.raw_cursor.clone()))
 
     def limit(self, *args, **kwargs):
+        """
+        Limit the number of documents in the cursor.
+        """
         return SuperDuperCursor(
             raw_cursor=self.raw_cursor.limit(*args, **kwargs),
             id_field=self.id_field,
@@ -50,6 +75,9 @@ class SuperDuperCursor:
 
     @staticmethod
     def wrap_document(r, encoders):
+        """
+        Wrap a document in a ``Document``.
+        """
         return Document(Document.decode(r, encoders))
 
     def __iter__(self):
@@ -68,7 +96,8 @@ class SuperDuperCursor:
             r['_score'] = self.scores[str(r[self.id_field])]
         if self.features is not None and self.features:
             r = self.add_features(r, features=self.features)
-
+        if CFG.downloads.hybrid:
+            load_uris(r, CFG.downloads.root)
         return self.wrap_document(r, self.encoders)
 
     next = __next__

superduperdb/db/base/download.py
@@ -1,3 +1,5 @@
+import hashlib
+import os
 import re
 import signal
 import sys
@@ -6,12 +8,13 @@ import warnings
 from contextlib import contextmanager
 from io import BytesIO
 from multiprocessing.pool import ThreadPool
+from typing import Any
 
 import boto3
 import requests
 from tqdm import tqdm
 
-from pinnacledb import logging
+from pinnacledb import CFG, logging
 
 
 class TimeoutException(Exception):
@@ -122,6 +125,15 @@ class BaseDownloader:
 
         self._parallel_go(f)
 
+    def _check_exists_if_hybrid(self, uri):
+        if uri.startswith('file://'):
+            file = f'{CFG.downloads.root}/{uri.split("file://")[-1]}'
+        else:
+            file = f'{CFG.downloads.root}/{hashlib.sha1(uri.encode()).hexdigest()}'
+        if os.path.exists(file):
+            return True
+        return False
+
     def _parallel_go(self, f):
         pool = ThreadPool(self.n_workers)
         try:
@@ -140,6 +152,16 @@ class BaseDownloader:
             f(i)
 
 
+class SaveFile:
+    def __init__(self, root: str):
+        self.root = root
+
+    def __call__(self, bytes_: bytearray, uri: str, **kwargs) -> Any:
+        path = f'{self.root}/{hashlib.sha1(uri.encode()).hexdigest()}'
+        with open(path, 'wb') as f:
+            f.write(bytes_)
+
+
 class Downloader(BaseDownloader):
     """
 
@@ -148,10 +170,10 @@ class Downloader(BaseDownloader):
     :param ids: list of ids of rows/ documents to update
     :param keys: list of keys in rows/ documents to insert to
     :param n_workers: number of multiprocessing workers
-    :param raises: raises error ``True``/``False``
     :param headers: dictionary of request headers passed to``requests`` package
     :param skip_existing: if ``True`` then don't bother getting already present data
     :param timeout: set seconds until request times out
+    :param raises: raises error ``True``/``False``
     """
 
     results: t.Dict[int, str]
@@ -183,28 +205,23 @@ class Downloader(BaseDownloader):
         self.fetcher = Fetcher(headers=headers, n_workers=n_workers)
 
     def _download(self, i):
-        uri = self.uris[i]
-        _id = self.ids[i]
-        content = self.fetcher(uri)
-        self.update_one(id=self.ids[i], key=self.keys[i], bytes_=content)
-
-
-class InMemoryDownloader(BaseDownloader):
-    def __init__(self, *args, headers=None, n_workers=0, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.results = {}
-        self.fetcher = Fetcher(headers=headers, n_workers=n_workers)
-
-    def _download(self, i):
+        if CFG.downloads.hybrid:
+            if self._check_exists_if_hybrid(self.uris[i]):
+                return
         content = self.fetcher(self.uris[i])
-        self.results[i] = content
+        self.update_one(
+            id=self.ids[i],
+            key=self.keys[i],
+            bytes_=content,
+            uri=self.uris[i],
+        )
 
 
 def gather_uris(
     documents: t.Sequence[t.Dict], gather_ids: bool = True
 ) -> t.Tuple[t.List[str], t.List[str], t.List[int]]:
     """
-    Get the URLS out of all documents as denoted by ``{"_content": ...}``
+    Get the uris out of all documents as denoted by ``{"_content": ...}``
 
     :param documents: list of dictionaries
     """

superduperdb/db/base/download_content.py
@@ -1,9 +1,9 @@
 import typing as t
 
-from pinnacledb import logging
+from pinnacledb import CFG, logging
 from pinnacledb.container.document import Document
 from pinnacledb.container.serializable import Serializable
-from pinnacledb.db.base.download import Downloader, gather_uris
+from pinnacledb.db.base.download import Downloader, SaveFile, gather_uris
 from pinnacledb.db.base.query import Insert, Select
 
 
@@ -42,10 +42,10 @@ def download_content(
     elif isinstance(query, Select):
         update_db = True
         if ids is None:
-            documents = list(db.select(query).raw_cursor)
+            documents = list(db.execute(query).raw_cursor)
         else:
             select = query.select_using_ids(ids)
-            documents = list(db.select(select))
+            documents = list(db.execute(select))
     else:
         documents = query.documents  # type: ignore[union-attr]
 
@@ -72,8 +72,12 @@ def download_content(
         except TypeError:
             pass
 
-    def _download_update(key, id, bytes_):
-        return query.download_update(db=db, key=key, id=id, bytes=bytes_)
+    if CFG.downloads.hybrid:
+        _download_update = SaveFile(CFG.downloads.root)
+    else:
+
+        def _download_update(key, id, bytes_, **kwargs):  # type: ignore[misc]
+            return query.download_update(db=db, key=key, id=id, bytes=bytes_)
 
     downloader = Downloader(
         uris=uris,

test/unittest/db/base/test_downloaders.py
@@ -1,10 +1,63 @@
 import os
+import shutil
 
+import pytest
+
+from pinnacledb import CFG
+from pinnacledb.container.document import Document
 from pinnacledb.db.base.download import Fetcher
+from pinnacledb.db.mongodb.query import Collection
+from pinnacledb.ext.pillow.image import pil_image
 
 remote = os.environ.get('SUPERDUPERDB_REMOTE_TEST', 'local')
+
+
+IMAGE_URL = 'https://www.pinnacledb.com/logos/white.png'
 
 
 def test_s3_and_web():
     if remote == 'remote':
         Fetcher()('s3://pinnacledb-bucket/img/black.png')
+
+
+@pytest.fixture
+def make_hybrid_cfg():
+    old = CFG.downloads.hybrid
+    CFG.downloads.hybrid = True
+    root_old = CFG.downloads.root
+    CFG.downloads.root = '.tdir_hybrid'
+    os.makedirs('.tdir_hybrid', exist_ok=True)
+    yield CFG
+    CFG.downloads.hybrid = old
+    CFG.downloads.root = root_old
+    shutil.rmtree('.tdir_hybrid')
+
+
+def test_file_blobs(empty, make_hybrid_cfg):
+    to_insert = [
+        Document(
+            {
+                'item': {
+                    '_content': {
+                        'uri': IMAGE_URL,
+                        'encoder': 'pil_image',
+                    }
+                },
+                'other': {
+                    'item': {
+                        '_content': {
+                            'uri': IMAGE_URL,
+                            'encoder': 'pil_image',
+                        }
+                    }
+                },
+            }
+        )
+        for _ in range(2)
+    ]
+
+    empty.execute(Collection('documents').insert_many(to_insert, encoders=(pil_image,)))
+    empty.execute(Collection('documents').find_one())
+    imgs = os.listdir('.tdir_hybrid/')
+
+    assert imgs