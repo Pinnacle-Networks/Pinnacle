superduper-io/superduper

Refactor QTD app

apps/question-the-docs/backend/ai/artifacts.py
@@ -1,0 +1,20 @@
+from backend.ai.utils.github import save_github_md_files_locally
+from backend.ai.utils.text import chunk_file_contents
+from backend.config import settings
+
+from superduperdb.container.document import Document
+from superduperdb.db.mongodb.query import Collection
+
+
+def load_ai_artifacts(db):
+    artifacts = _create_ai_text_artifacts()
+    documents = [Document({settings.vector_embedding_key: v}) for v in artifacts]
+    db.execute(Collection(settings.mongo_collection_name).insert_many(documents))
+
+
+def _create_ai_text_artifacts():
+    filepaths = save_github_md_files_locally(
+        settings.owner, settings.name, settings.documentation_location
+    )
+    ai_text_artifacts = chunk_file_contents(filepaths)
+    return ai_text_artifacts

apps/question-the-docs/backend/app.py
@@ -1,30 +1,35 @@
-from apps.question-the-docs.backend.ai.components import setup_ai
-from apps.question-the-docs.backend.ai.data import setup_data
+from backend.ai.components import install_ai_components
+from backend.ai.artifacts import load_ai_artifacts
 from backend.config import settings
-from backend.document.routes import document_router
-from backend.ai.data import setup_qa_documentation
+from backend.documents.routes import documents_router
 from fastapi import FastAPI
 from pymongo import MongoClient
 
-from superduperdb.misc import superduper
-from backend.ai.setup import setup as setup_ai_and_data
+from superduperdb import superduper
 
 
 def init_routers(app: FastAPI) -> None:
-    app.include_router(document_router)
+    app.include_router(documents_router)
 
 
 def create_app() -> FastAPI:
-    _app = FastAPI(title="KPI Dashboards")
+    _app = FastAPI(title="Question the Docs")
 
     @_app.on_event("startup")
     def startup_db_client():
-        _app.mongodb_client = MongoClient(settings.DB_URL)
-        _app.superduperdb = superduper(_app.mongodb_client[settings.MONGO_DB_NAME])
+        _app.mongodb_client = MongoClient(settings.mongo_uri)
+        _app.mongodb = _app.mongodb_client[settings.mongo_db_name]
 
-        _app.superduperdb.drop(force=True)
-        setup_data(_app.superduperdb)
-        setup_ai(_app.superduperdb)
+        # We wrap our MongoDB to make it a SuperDuperDB!
+        _app.superduperdb = superduper(_app.mongodb)
+
+        # EXPLAIN ARTIFACTS HERE.
+        load_ai_artifacts(_app.superduperdb)
+
+        # We populate our SuperDuperDB with AI components.
+        # EXPLAIN COMPONENTS HERE.
+        # These will be used later to answer questions on our data.
+        install_ai_components(_app.superduperdb)
 
     @_app.on_event("shutdown")
     def shutdown_db_client():

apps/question-the-docs/backend/config.py
@@ -1,47 +1,30 @@
 from pydantic import BaseSettings
 
 
-PROMPT = '''Use the following descriptions and code-snippets aboout {{ settings.PROJECT_NAME }} to answer this question about SuperDuperDB
-Do not use any other information you might have learned about other python packages
-Only base your answer on the code-snippets retrieved:
+class FastAPISettings(BaseSettings):
+    mongo_uri: str
+    mongo_db_name: str = 'documentation'
+    mongo_collection_name: str = "docs"
+    port: int = 8000
+    host: str = "0.0.0.0"
+    debug_mode: bool = False
 
-{context}
 
-Here's the question:
-'''
+class RepoSettings(FastAPISettings):
+    owner: str = 'SuperDuperDB'
+    name: str = 'superduperdb'
+    documentation_location: str = 'docs'
 
 
-class Settings(BaseSettings):
-    # Mongo connection
-    MONGO_URI: str = 'mongodb://localhost:27017'
-    MONGO_DB_NAME: str = 'documentation'
-    MONGO_COLLECTION_NAME: str = 'docs'
-
-    # Code base to configure
-    PATH_TO_REPO: str = './superduperdb'
-    GIT_URL: str = 'git@github.com:SuperDuperDB/superduperdb.git'
-
-    # Data configurations
-    DOC_FILE_LEVELS: int = 3
-    DOC_FILE_EXT: str = 'md'
-    STRIDE: int = 5
-    WINDOW_SIZE: int = 10
-
+class AISettings(RepoSettings):
     # Model details
-    VECTOR_INDEX_NAME: str = 'documentation_index'
-    VECTOR_EMBEDDING_MODEL: str = 'text-embedding-ada-002'
-    VECTOR_EMBEDDING_KEY: str = 'txt'
-    QA_MODEL: str = 'gpt-3.5-turbo'
-    PROMPT: str = PROMPT
+    vector_index_name: str = 'documentation_index'
+    vector_embedding_model: str = 'text-embedding-ada-002'
+    vector_embedding_key: str = 'txt'
+    qa_model: str = 'gpt-3.5-turbo'
 
-    # FastAPI details
-    PORT: int = 8000
-    HOST: str = "0.0.0.0"
-    DEBUG_MODE: bool = False
-    
-    # Query configurations
-    NEAREST_TO_QUERY: int = 5
+    # Query configuration
+    nearest_to_query: int = 5
 
 
-
-settings = Settings()
+settings = AISettings()

apps/question-the-docs/backend/documents/routes.py
@@ -1,0 +1,35 @@
+from backend.config import settings
+from backend.documents.models import Answer, Query
+from fastapi import APIRouter, Request
+
+from superduperdb.db.mongodb.query import Collection
+
+documents_router = APIRouter(prefix="/documents", tags=["docs"])
+
+
+@documents_router.post(
+    "/query",
+    response_description="Query document database for data to answer prompt",
+)
+async def query_docs(request: Request, query: Query) -> Answer:
+    # Step 1: Build your query
+    # Build your query here combining vector-search "like(...)"
+    # with classical mongodb queries "find(...)"
+    collection = Collection(name=settings.mongo_collection_name)
+    context_select = collection.like(
+        {settings.vector_embedding_key: query.query},
+        n=settings.nearest_to_query,
+        vector_index="documentation_index",
+    ).find()
+
+    # Step 2: Execute your query
+    # INSERT INFORMATION HERE
+    db = request.app.superduperdb
+    db_response, _ = await db.apredict(
+        'gpt-3.5-turbo',
+        input=query.query,
+        context_select=context_select,
+        context_key=settings.vector_embedding_key,
+    )
+
+    return Answer(answer=db_response.unpack())

Pinnacle-Networks/Pinnacle

Former-commit-id: e4aa8d04b65f6efbb9364a4e9d48e1dd2fde7b48

apps/question-the-docs/backend/ai/artifacts.py
@@ -1,0 +1,20 @@
+from backend.ai.utils.github import save_github_md_files_locally
+from backend.ai.utils.text import chunk_file_contents
+from backend.config import settings
+
+from pinnacledb.container.document import Document
+from pinnacledb.db.mongodb.query import Collection
+
+
+def load_ai_artifacts(db):
+    artifacts = _create_ai_text_artifacts()
+    documents = [Document({settings.vector_embedding_key: v}) for v in artifacts]
+    db.execute(Collection(settings.mongo_collection_name).insert_many(documents))
+
+
+def _create_ai_text_artifacts():
+    filepaths = save_github_md_files_locally(
+        settings.owner, settings.name, settings.documentation_location
+    )
+    ai_text_artifacts = chunk_file_contents(filepaths)
+    return ai_text_artifacts

apps/question-the-docs/backend/app.py
@@ -1,30 +1,35 @@
-from apps.question-the-docs.backend.ai.components import setup_ai
-from apps.question-the-docs.backend.ai.data import setup_data
+from backend.ai.components import install_ai_components
+from backend.ai.artifacts import load_ai_artifacts
 from backend.config import settings
-from backend.document.routes import document_router
-from backend.ai.data import setup_qa_documentation
+from backend.documents.routes import documents_router
 from fastapi import FastAPI
 from pymongo import MongoClient
 
-from pinnacledb.misc import pinnacle
-from backend.ai.setup import setup as setup_ai_and_data
+from pinnacledb import pinnacle
 
 
 def init_routers(app: FastAPI) -> None:
-    app.include_router(document_router)
+    app.include_router(documents_router)
 
 
 def create_app() -> FastAPI:
-    _app = FastAPI(title="KPI Dashboards")
+    _app = FastAPI(title="Question the Docs")
 
     @_app.on_event("startup")
     def startup_db_client():
-        _app.mongodb_client = MongoClient(settings.DB_URL)
-        _app.pinnacledb = pinnacle(_app.mongodb_client[settings.MONGO_DB_NAME])
+        _app.mongodb_client = MongoClient(settings.mongo_uri)
+        _app.mongodb = _app.mongodb_client[settings.mongo_db_name]
 
-        _app.pinnacledb.drop(force=True)
-        setup_data(_app.pinnacledb)
-        setup_ai(_app.pinnacledb)
+        # We wrap our MongoDB to make it a SuperDuperDB!
+        _app.pinnacledb = pinnacle(_app.mongodb)
+
+        # EXPLAIN ARTIFACTS HERE.
+        load_ai_artifacts(_app.pinnacledb)
+
+        # We populate our SuperDuperDB with AI components.
+        # EXPLAIN COMPONENTS HERE.
+        # These will be used later to answer questions on our data.
+        install_ai_components(_app.pinnacledb)
 
     @_app.on_event("shutdown")
     def shutdown_db_client():

apps/question-the-docs/backend/config.py
@@ -1,47 +1,30 @@
 from pydantic import BaseSettings
 
 
-PROMPT = '''Use the following descriptions and code-snippets aboout {{ settings.PROJECT_NAME }} to answer this question about SuperDuperDB
-Do not use any other information you might have learned about other python packages
-Only base your answer on the code-snippets retrieved:
+class FastAPISettings(BaseSettings):
+    mongo_uri: str
+    mongo_db_name: str = 'documentation'
+    mongo_collection_name: str = "docs"
+    port: int = 8000
+    host: str = "0.0.0.0"
+    debug_mode: bool = False
 
-{context}
 
-Here's the question:
-'''
+class RepoSettings(FastAPISettings):
+    owner: str = 'SuperDuperDB'
+    name: str = 'pinnacledb'
+    documentation_location: str = 'docs'
 
 
-class Settings(BaseSettings):
-    # Mongo connection
-    MONGO_URI: str = 'mongodb://localhost:27017'
-    MONGO_DB_NAME: str = 'documentation'
-    MONGO_COLLECTION_NAME: str = 'docs'
-
-    # Code base to configure
-    PATH_TO_REPO: str = './pinnacledb'
-    GIT_URL: str = 'git@github.com:SuperDuperDB/pinnacledb.git'
-
-    # Data configurations
-    DOC_FILE_LEVELS: int = 3
-    DOC_FILE_EXT: str = 'md'
-    STRIDE: int = 5
-    WINDOW_SIZE: int = 10
-
+class AISettings(RepoSettings):
     # Model details
-    VECTOR_INDEX_NAME: str = 'documentation_index'
-    VECTOR_EMBEDDING_MODEL: str = 'text-embedding-ada-002'
-    VECTOR_EMBEDDING_KEY: str = 'txt'
-    QA_MODEL: str = 'gpt-3.5-turbo'
-    PROMPT: str = PROMPT
+    vector_index_name: str = 'documentation_index'
+    vector_embedding_model: str = 'text-embedding-ada-002'
+    vector_embedding_key: str = 'txt'
+    qa_model: str = 'gpt-3.5-turbo'
 
-    # FastAPI details
-    PORT: int = 8000
-    HOST: str = "0.0.0.0"
-    DEBUG_MODE: bool = False
-    
-    # Query configurations
-    NEAREST_TO_QUERY: int = 5
+    # Query configuration
+    nearest_to_query: int = 5
 
 
-
-settings = Settings()
+settings = AISettings()

apps/question-the-docs/backend/documents/routes.py
@@ -1,0 +1,35 @@
+from backend.config import settings
+from backend.documents.models import Answer, Query
+from fastapi import APIRouter, Request
+
+from pinnacledb.db.mongodb.query import Collection
+
+documents_router = APIRouter(prefix="/documents", tags=["docs"])
+
+
+@documents_router.post(
+    "/query",
+    response_description="Query document database for data to answer prompt",
+)
+async def query_docs(request: Request, query: Query) -> Answer:
+    # Step 1: Build your query
+    # Build your query here combining vector-search "like(...)"
+    # with classical mongodb queries "find(...)"
+    collection = Collection(name=settings.mongo_collection_name)
+    context_select = collection.like(
+        {settings.vector_embedding_key: query.query},
+        n=settings.nearest_to_query,
+        vector_index="documentation_index",
+    ).find()
+
+    # Step 2: Execute your query
+    # INSERT INFORMATION HERE
+    db = request.app.pinnacledb
+    db_response, _ = await db.apredict(
+        'gpt-3.5-turbo',
+        input=query.query,
+        context_select=context_select,
+        context_key=settings.vector_embedding_key,
+    )
+
+    return Answer(answer=db_response.unpack())