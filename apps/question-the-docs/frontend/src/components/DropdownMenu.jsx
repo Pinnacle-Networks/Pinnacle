NicoleG25/superduperdb

Add frontend to QtD app

apps/question-the-docs/frontend/src/components/DropdownMenu.jsx
@@ -1,0 +1,19 @@
+const DropdownMenu = ({ selectedOption, setSelectedOption }) => {
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <select className="dropdown" value={selectedOption} onChange={handleOptionChange}>
+        <option value="">Choose Documentation</option>
+        <option value="superduperdb">SuperDuperDB</option>
+        <option value="langchain">LangChain</option>
+        <option value="fastchat">FastChat</option>
+      </select>
+    </div>
+  );
+};
+
+  export default DropdownMenu;

apps/question-the-docs/frontend/src/components/Header.jsx
@@ -1,0 +1,14 @@
+import superduperdbLogo from '../assets/superduperdb.svg'
+
+const Header = () => {
+
+    return (
+        <div className='banner'>
+            <a href="https://superduperdb.com" target="_blank">
+                <img src={superduperdbLogo} className="logo" alt="SuperDuperDB logo" />
+            </a>
+        </div>
+    )
+};
+
+export default Header;

Pinnacle-Networks/Pinnacle

Former-commit-id: 296634de2b63d7820390a6aef8e2ea7204b3aa96

apps/question-the-docs/frontend/src/components/DropdownMenu.jsx
@@ -1,0 +1,19 @@
+const DropdownMenu = ({ selectedOption, setSelectedOption }) => {
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <div>
+      <select className="dropdown" value={selectedOption} onChange={handleOptionChange}>
+        <option value="">Choose Documentation</option>
+        <option value="pinnacledb">SuperDuperDB</option>
+        <option value="langchain">LangChain</option>
+        <option value="fastchat">FastChat</option>
+      </select>
+    </div>
+  );
+};
+
+  export default DropdownMenu;

apps/question-the-docs/frontend/src/components/Header.jsx
@@ -1,0 +1,15 @@
+import pinnacledbLogo from '../assets/pinnacledb.svg'
+
+const Header = () => {
+
+    return (
+        <div className='banner'>
+            <a href="https://pinnacledb.com" target="_blank">
+                <img src={pinnacledbLogo} className="logo" alt="SuperDuperDB logo" />
+            </a>
+            <h1 className='title'>Question the Docs</h1>
+        </div>
+    )
+};
+
+export default Header;