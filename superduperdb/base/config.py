superduper-io/superduper

Correct and complete all public doc-strings

superduperdb/base/config.py
@@ -7,7 +7,7 @@ canot contain any other imports from this project.
 import typing as t
 from enum import Enum
 
-from pydantic import Field, root_validator
+from pydantic import Field
 
 from .jsonable import Factory, JSONable
 
@@ -16,7 +16,13 @@ REST_API_VERSION = '0.1.0'
 
 
 class HasPort(JSONable):
-    """A base class for server connections"""
+    """
+    A base class for server connections
+
+    :param password: The password for the connection
+    :param port: The port for the connection
+    :param username: The username for the connection
+    """
 
     password: str = ''
     port: int = 0
@@ -24,19 +30,34 @@ class HasPort(JSONable):
 
 
 class HostPort(HasPort):
-    """Configure server connections with a `host` property"""
+    """
+    Configure server connections with a `host` property
+
+    :param host: The host for the connection
+    """
 
     host: str = 'localhost'
 
 
 class IpPort(HasPort):
-    """Configure server connections with an `ip` property"""
+    """
+    Configure server connections with an `ip` property
+
+    :param ip: The ip for the connection
+    """
 
     ip: str = 'localhost'
 
 
 class Retry(JSONable):
-    """Describes how to retry using the `tenacity` library"""
+    """
+    Describes how to retry using the `tenacity` library
+
+    :param stop_after_attempt: The number of attempts to make
+    :param wait_max: The maximum time to wait between attempts
+    :param wait_min: The minimum time to wait between attempts
+    :param wait_multiplier: The multiplier for the wait time between attempts
+    """
 
     stop_after_attempt: int = 2
     wait_max: float = 10.0
@@ -45,32 +66,41 @@ class Retry(JSONable):
 
 
 class Api(JSONable):
-    """A base class for API connections"""
+    """
+    A base class for API connections
+
+    :param api_key: The API key for the connection
+    """
 
     api_key: str = Field(default=_BAD_KEY, repr=False)
 
 
 class Apis(JSONable):
-    """A container for API connections"""
+    """
+    A container for API connections
+
+    :param providers: A dictionary of API connections
+    :param retry: A ``Retry`` object
+    """
 
     providers: t.Dict[str, Api] = Factory(dict)
     retry: Retry = Factory(Retry)
 
 
 class Dask(IpPort):
-    """Describes a connection to Dask"""
+    """
+    Describes a connection to Dask
+
+    :param deserializers: A list of deserializers
+    :param port: The port for the connection
+    :param serializers: A list of serializers
+    :param local: Whether the connection is local
+    """
 
     deserializers: t.List[str] = Factory(list)
     port: int = 8786
     serializers: t.List[str] = Factory(list)
     local: bool = True
-
-
-class Deployment(JSONable):
-    """(unused)"""
-
-    database: str = ''
-    model: str = ''
 
 
 class LogLevel(str, Enum):
@@ -89,29 +119,42 @@ class LogType(str, Enum):
 
 
 class Logging(JSONable):
-    """Describe how we are going to log. This isn't yet used."""
+    """
+    Describe how we are going to log. This isn't yet used.
+
+    :param level: The log level
+    :param type: The log type
+    :param kwargs: Any additional keyword arguments
+    """
 
     level: LogLevel = LogLevel.INFO
     type: LogType = LogType.STDERR
     kwargs: dict = Factory(dict)
 
 
-class ModelServer(HostPort):
-    """Configure the model server's port"""
-
-    host: str = '127.0.0.1'
-    port: int = 5001
-
-
 class MongoDB(HostPort):
-    """Configure MongoDB's port and credentials"""
+    """
+    Configure MongoDB's port and credentials
+
+    :param host: The host for the connection
+    :param port: The port for the connection
+    """
+
+    # TODO - configure all databases via uri
 
     host: str = 'localhost'
     port: int = 27017
 
 
-class DataLayer(JSONable):
-    """Configure which db or database is being used"""
+class DbComponent(JSONable):
+    """
+    Configure which db or database is being used
+
+    :param cls: The class of the data layer
+    :param connection: The connection for the data layer
+    :param kwargs: Any additional keyword arguments
+    :param name: The name of the data layer
+    """
 
     cls: str = 'mongodb'
     connection: str = 'pymongo'
@@ -119,31 +162,28 @@ class DataLayer(JSONable):
     name: str = 'test_db'
 
 
-class DataLayers(JSONable):
-    """TBD"""
+class DbComponents(JSONable):
+    """
+    A container for the components of ``DB``.
 
-    artifact: DataLayer = Factory(lambda: DataLayer(name='_filesystem:test_db'))
-    data_backend: DataLayer = Factory(DataLayer)
-    metadata: DataLayer = Factory(DataLayer)
+    :param artifact: The artifact data layer
+    :param data_backend: The data backend data layer
+    :param metadata: The metadata data layer
+    """
 
-
-class Notebook(JSONable):
-    """Configure the notebook server connection information"""
-
-    ip: str = '0.0.0.0'
-    password: str = ''
-    port: int = 8888
-    token: str = ''
-
-    @root_validator(skip_on_failure=True)
-    def check_one_or_none(cls, v: t.Dict) -> t.Dict:
-        if v['password'] and v['token']:
-            raise ValueError('At most one of password and token may be set')
-        return v
+    artifact: DbComponent = Factory(lambda: DbComponent(name='_filesystem:test_db'))
+    data_backend: DbComponent = Factory(DbComponent)
+    metadata: DbComponent = Factory(DbComponent)
 
 
 class Server(JSONable):
-    """Configure the SuperDuperDB server connection information"""
+    """
+    Configure the SuperDuperDB server connection information
+
+    :param host: The host for the connection
+    :param port: The port for the connection
+    :param protocol: The protocol for the connection
+    """
 
     host: str = '127.0.0.1'
     port: int = 3223
@@ -155,7 +195,13 @@ class Server(JSONable):
 
 
 class LanceDB(JSONable):
-    """Configure the Lance DB vector search connection information"""
+    """
+    Configure the Lance DB vector search connection information
+
+    :param backfill_batch_size: The batch size for backfilling
+    :param lancedb: Whether LanceDB is being used
+    :param uri: The URI for the connection
+    """
 
     backfill_batch_size: int = 100
     lancedb: bool = True
@@ -163,7 +209,12 @@ class LanceDB(JSONable):
 
 
 class InMemory(JSONable):
-    """Configure the in-memory vector search connection information"""
+    """
+    Configure the in-memory vector search connection information
+
+    :param backfill_batch_size: The batch size for backfilling
+    :param inmemory: Whether in-memory is being used
+    """
 
     backfill_batch_size: int = 100
     inmemory: bool = True
@@ -173,13 +224,24 @@ class SelfHosted(JSONable):
     """A placeholder for the case in which search is self-hosted by data-backend
 
     For example: MongoDB atlas vector-search, Elastic search etc..
+
+    :param selfhosted: Whether self-hosted is being used
     """
 
     selfhosted: bool = True
 
 
 class VectorSearch(JSONable):
-    """Configure the full vector search connection information"""
+    """
+    Configure the full vector search connection information
+
+    :param host: The host for the connection
+    :param password: The password for the connection
+    :param port: The port for the connection
+    :param type: The type of vector search
+    :param backfill_batch_size: The batch size for backfilling
+    :param username: The username for the connection
+    """
 
     host: str = 'localhost'
     password: str = Field(default='', repr=False)
@@ -190,24 +252,42 @@ class VectorSearch(JSONable):
 
 
 class Downloads(JSONable):
+    """
+    Configure how downloads are saved in the database
+    or to hybrid filestorage (references to filesystem from datastore)
+
+    :param hybrid: Whether hybrid is being used
+    :param root: The root for the connection
+    """
+
     hybrid: bool = False
     root: str = 'data/downloads'
 
 
 class Config(JSONable):
-    """The data class containing all configurable superduperdb values"""
+    """
+    The data class containing all configurable superduperdb values
+
+    :param apis: The ``Apis`` object
+    :param cdc: Whether CDC is being used
+    :param dask: The ``Dask`` object
+    :param data_layers: The ``DBComponents`` object
+    :param distributed: Whether distributed is being used
+    :param logging: The ``Logging`` object
+    :param server: The ``Server`` object
+    :param vector_search: The ``VectorSearch`` object
+    :param downloads: The ``Downloads`` object
+    :param verbose: Whether verbose is being used
+    """
 
     apis: Apis = Factory(Apis)
     cdc: bool = False
     dask: Dask = Factory(Dask)
-    data_layers: DataLayers = Factory(DataLayers)
-    distributed: bool = False
+    db_components: DbComponents = Factory(DbComponents)
+    distributed: bool = False  # TODO include this in the dask component
     logging: Logging = Factory(Logging)
-    model_server: ModelServer = Factory(ModelServer)
-    notebook: Notebook = Factory(Notebook)
     server: Server = Factory(Server)
     vector_search: VectorSearch = Factory(VectorSearch)
-    verbose: bool = False
     downloads: Downloads = Factory(Downloads)
 
     class Config(JSONable.Config):

superduperdb/db/base/cdc.py
@@ -61,7 +61,8 @@ def DatabaseListener(
     **kwargs,
 ) -> cdc.BaseDatabaseListener:
     """
-    Create an instance of `BaseDatabaseListener`
+    Create an instance of ``BaseDatabaseListener``.
+    Not to be confused with ``superduperdb.container.listener.Listener``.
 
     :param db: A superduperdb instance.
     :param on: Which collection/table listener service this be invoked on?

test/unittest/conftest.py
@@ -24,7 +24,7 @@ except ImportError:
 from pymongo import MongoClient
 from tenacity import RetryError, Retrying, stop_after_delay
 
-from superduperdb.base.config import DataLayer, DataLayers
+from superduperdb.base.config import DbComponent, DbComponents
 from superduperdb.base.config import MongoDB as MongoDBConfig
 from superduperdb.container.dataset import Dataset
 from superduperdb.container.document import Document
@@ -132,13 +132,13 @@ def test_db(mongodb_server: MongoDBConfig) -> Iterator[DB]:
 @pytest.fixture(autouse=True)
 def config(mongodb_server: MongoDBConfig) -> Iterator[None]:
     kwargs = asdict(TestMongoDBConfig())
-    data_layers_cfg = DataLayers(
-        artifact=DataLayer(name='_filesystem:test_db', kwargs=kwargs),
-        data_backend=DataLayer(name='test_db', kwargs=kwargs),
-        metadata=DataLayer(name='test_db', kwargs=kwargs),
+    db_components_cfg = DbComponents(
+        artifact=DbComponent(name='_filesystem:test_db', kwargs=kwargs),
+        data_backend=DbComponent(name='test_db', kwargs=kwargs),
+        metadata=DbComponent(name='test_db', kwargs=kwargs),
     )
 
-    with mock.patch('superduperdb.CFG.data_layers', data_layers_cfg):
+    with mock.patch('superduperdb.CFG.db_components', db_components_cfg):
         yield
 
 

Pinnacle-Networks/Pinnacle

Former-commit-id: cb392fca70a106ef2f09368b11145a335887c2f7

superduperdb/base/config.py
@@ -7,7 +7,7 @@ canot contain any other imports from this project.
 import typing as t
 from enum import Enum
 
-from pydantic import Field, root_validator
+from pydantic import Field
 
 from .jsonable import Factory, JSONable
 
@@ -16,7 +16,13 @@ REST_API_VERSION = '0.1.0'
 
 
 class HasPort(JSONable):
-    """A base class for server connections"""
+    """
+    A base class for server connections
+
+    :param password: The password for the connection
+    :param port: The port for the connection
+    :param username: The username for the connection
+    """
 
     password: str = ''
     port: int = 0
@@ -24,19 +30,34 @@ class HasPort(JSONable):
 
 
 class HostPort(HasPort):
-    """Configure server connections with a `host` property"""
+    """
+    Configure server connections with a `host` property
+
+    :param host: The host for the connection
+    """
 
     host: str = 'localhost'
 
 
 class IpPort(HasPort):
-    """Configure server connections with an `ip` property"""
+    """
+    Configure server connections with an `ip` property
+
+    :param ip: The ip for the connection
+    """
 
     ip: str = 'localhost'
 
 
 class Retry(JSONable):
-    """Describes how to retry using the `tenacity` library"""
+    """
+    Describes how to retry using the `tenacity` library
+
+    :param stop_after_attempt: The number of attempts to make
+    :param wait_max: The maximum time to wait between attempts
+    :param wait_min: The minimum time to wait between attempts
+    :param wait_multiplier: The multiplier for the wait time between attempts
+    """
 
     stop_after_attempt: int = 2
     wait_max: float = 10.0
@@ -45,32 +66,41 @@ class Retry(JSONable):
 
 
 class Api(JSONable):
-    """A base class for API connections"""
+    """
+    A base class for API connections
+
+    :param api_key: The API key for the connection
+    """
 
     api_key: str = Field(default=_BAD_KEY, repr=False)
 
 
 class Apis(JSONable):
-    """A container for API connections"""
+    """
+    A container for API connections
+
+    :param providers: A dictionary of API connections
+    :param retry: A ``Retry`` object
+    """
 
     providers: t.Dict[str, Api] = Factory(dict)
     retry: Retry = Factory(Retry)
 
 
 class Dask(IpPort):
-    """Describes a connection to Dask"""
+    """
+    Describes a connection to Dask
+
+    :param deserializers: A list of deserializers
+    :param port: The port for the connection
+    :param serializers: A list of serializers
+    :param local: Whether the connection is local
+    """
 
     deserializers: t.List[str] = Factory(list)
     port: int = 8786
     serializers: t.List[str] = Factory(list)
     local: bool = True
-
-
-class Deployment(JSONable):
-    """(unused)"""
-
-    database: str = ''
-    model: str = ''
 
 
 class LogLevel(str, Enum):
@@ -89,29 +119,42 @@ class LogType(str, Enum):
 
 
 class Logging(JSONable):
-    """Describe how we are going to log. This isn't yet used."""
+    """
+    Describe how we are going to log. This isn't yet used.
+
+    :param level: The log level
+    :param type: The log type
+    :param kwargs: Any additional keyword arguments
+    """
 
     level: LogLevel = LogLevel.INFO
     type: LogType = LogType.STDERR
     kwargs: dict = Factory(dict)
 
 
-class ModelServer(HostPort):
-    """Configure the model server's port"""
-
-    host: str = '127.0.0.1'
-    port: int = 5001
-
-
 class MongoDB(HostPort):
-    """Configure MongoDB's port and credentials"""
+    """
+    Configure MongoDB's port and credentials
+
+    :param host: The host for the connection
+    :param port: The port for the connection
+    """
+
+    # TODO - configure all databases via uri
 
     host: str = 'localhost'
     port: int = 27017
 
 
-class DataLayer(JSONable):
-    """Configure which db or database is being used"""
+class DbComponent(JSONable):
+    """
+    Configure which db or database is being used
+
+    :param cls: The class of the data layer
+    :param connection: The connection for the data layer
+    :param kwargs: Any additional keyword arguments
+    :param name: The name of the data layer
+    """
 
     cls: str = 'mongodb'
     connection: str = 'pymongo'
@@ -119,31 +162,28 @@ class DataLayer(JSONable):
     name: str = 'test_db'
 
 
-class DataLayers(JSONable):
-    """TBD"""
+class DbComponents(JSONable):
+    """
+    A container for the components of ``DB``.
 
-    artifact: DataLayer = Factory(lambda: DataLayer(name='_filesystem:test_db'))
-    data_backend: DataLayer = Factory(DataLayer)
-    metadata: DataLayer = Factory(DataLayer)
+    :param artifact: The artifact data layer
+    :param data_backend: The data backend data layer
+    :param metadata: The metadata data layer
+    """
 
-
-class Notebook(JSONable):
-    """Configure the notebook server connection information"""
-
-    ip: str = '0.0.0.0'
-    password: str = ''
-    port: int = 8888
-    token: str = ''
-
-    @root_validator(skip_on_failure=True)
-    def check_one_or_none(cls, v: t.Dict) -> t.Dict:
-        if v['password'] and v['token']:
-            raise ValueError('At most one of password and token may be set')
-        return v
+    artifact: DbComponent = Factory(lambda: DbComponent(name='_filesystem:test_db'))
+    data_backend: DbComponent = Factory(DbComponent)
+    metadata: DbComponent = Factory(DbComponent)
 
 
 class Server(JSONable):
-    """Configure the SuperDuperDB server connection information"""
+    """
+    Configure the SuperDuperDB server connection information
+
+    :param host: The host for the connection
+    :param port: The port for the connection
+    :param protocol: The protocol for the connection
+    """
 
     host: str = '127.0.0.1'
     port: int = 3223
@@ -155,7 +195,13 @@ class Server(JSONable):
 
 
 class LanceDB(JSONable):
-    """Configure the Lance DB vector search connection information"""
+    """
+    Configure the Lance DB vector search connection information
+
+    :param backfill_batch_size: The batch size for backfilling
+    :param lancedb: Whether LanceDB is being used
+    :param uri: The URI for the connection
+    """
 
     backfill_batch_size: int = 100
     lancedb: bool = True
@@ -163,7 +209,12 @@ class LanceDB(JSONable):
 
 
 class InMemory(JSONable):
-    """Configure the in-memory vector search connection information"""
+    """
+    Configure the in-memory vector search connection information
+
+    :param backfill_batch_size: The batch size for backfilling
+    :param inmemory: Whether in-memory is being used
+    """
 
     backfill_batch_size: int = 100
     inmemory: bool = True
@@ -173,13 +224,24 @@ class SelfHosted(JSONable):
     """A placeholder for the case in which search is self-hosted by data-backend
 
     For example: MongoDB atlas vector-search, Elastic search etc..
+
+    :param selfhosted: Whether self-hosted is being used
     """
 
     selfhosted: bool = True
 
 
 class VectorSearch(JSONable):
-    """Configure the full vector search connection information"""
+    """
+    Configure the full vector search connection information
+
+    :param host: The host for the connection
+    :param password: The password for the connection
+    :param port: The port for the connection
+    :param type: The type of vector search
+    :param backfill_batch_size: The batch size for backfilling
+    :param username: The username for the connection
+    """
 
     host: str = 'localhost'
     password: str = Field(default='', repr=False)
@@ -190,24 +252,42 @@ class VectorSearch(JSONable):
 
 
 class Downloads(JSONable):
+    """
+    Configure how downloads are saved in the database
+    or to hybrid filestorage (references to filesystem from datastore)
+
+    :param hybrid: Whether hybrid is being used
+    :param root: The root for the connection
+    """
+
     hybrid: bool = False
     root: str = 'data/downloads'
 
 
 class Config(JSONable):
-    """The data class containing all configurable pinnacledb values"""
+    """
+    The data class containing all configurable pinnacledb values
+
+    :param apis: The ``Apis`` object
+    :param cdc: Whether CDC is being used
+    :param dask: The ``Dask`` object
+    :param data_layers: The ``DBComponents`` object
+    :param distributed: Whether distributed is being used
+    :param logging: The ``Logging`` object
+    :param server: The ``Server`` object
+    :param vector_search: The ``VectorSearch`` object
+    :param downloads: The ``Downloads`` object
+    :param verbose: Whether verbose is being used
+    """
 
     apis: Apis = Factory(Apis)
     cdc: bool = False
     dask: Dask = Factory(Dask)
-    data_layers: DataLayers = Factory(DataLayers)
-    distributed: bool = False
+    db_components: DbComponents = Factory(DbComponents)
+    distributed: bool = False  # TODO include this in the dask component
     logging: Logging = Factory(Logging)
-    model_server: ModelServer = Factory(ModelServer)
-    notebook: Notebook = Factory(Notebook)
     server: Server = Factory(Server)
     vector_search: VectorSearch = Factory(VectorSearch)
-    verbose: bool = False
     downloads: Downloads = Factory(Downloads)
 
     class Config(JSONable.Config):

superduperdb/db/base/cdc.py
@@ -61,7 +61,8 @@ def DatabaseListener(
     **kwargs,
 ) -> cdc.BaseDatabaseListener:
     """
-    Create an instance of `BaseDatabaseListener`
+    Create an instance of ``BaseDatabaseListener``.
+    Not to be confused with ``pinnacledb.container.listener.Listener``.
 
     :param db: A pinnacledb instance.
     :param on: Which collection/table listener service this be invoked on?

test/unittest/conftest.py
@@ -24,7 +24,7 @@ except ImportError:
 from pymongo import MongoClient
 from tenacity import RetryError, Retrying, stop_after_delay
 
-from pinnacledb.base.config import DataLayer, DataLayers
+from pinnacledb.base.config import DbComponent, DbComponents
 from pinnacledb.base.config import MongoDB as MongoDBConfig
 from pinnacledb.container.dataset import Dataset
 from pinnacledb.container.document import Document
@@ -132,13 +132,13 @@ def test_db(mongodb_server: MongoDBConfig) -> Iterator[DB]:
 @pytest.fixture(autouse=True)
 def config(mongodb_server: MongoDBConfig) -> Iterator[None]:
     kwargs = asdict(TestMongoDBConfig())
-    data_layers_cfg = DataLayers(
-        artifact=DataLayer(name='_filesystem:test_db', kwargs=kwargs),
-        data_backend=DataLayer(name='test_db', kwargs=kwargs),
-        metadata=DataLayer(name='test_db', kwargs=kwargs),
+    db_components_cfg = DbComponents(
+        artifact=DbComponent(name='_filesystem:test_db', kwargs=kwargs),
+        data_backend=DbComponent(name='test_db', kwargs=kwargs),
+        metadata=DbComponent(name='test_db', kwargs=kwargs),
     )
 
-    with mock.patch('pinnacledb.CFG.data_layers', data_layers_cfg):
+    with mock.patch('pinnacledb.CFG.db_components', db_components_cfg):
         yield
 
 