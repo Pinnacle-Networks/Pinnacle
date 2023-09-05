superduper-io/superduper

Create schema in superduperdb component hierarchy

superduperdb/container/document.py
@@ -5,11 +5,19 @@ from bson.objectid import ObjectId
 
 import superduperdb as s
 from superduperdb.container.encoder import Encodable
+from superduperdb.container.schema import Schema
 
 ContentType = t.Union[t.Dict, Encodable]
 ItemType = t.Union[t.Dict[str, t.Any], Encodable, ObjectId]
 
 _OUTPUTS_KEY: str = '_outputs'
+
+
+class Row:
+    content: ContentType
+
+    def __init__(self) -> None:
+        pass
 
 
 class Document:
@@ -29,8 +37,10 @@ class Document:
         """Dump this document into BSON and encode as bytes"""
         return bson.encode(self.encode())
 
-    def encode(self) -> t.Any:
+    def encode(self, schema: t.Optional[Schema] = None) -> t.Any:
         """Make a copy of the content with all the Encodables encoded"""
+        if schema is not None:
+            return _encode_with_schema(self.content, schema)
         return _encode(self.content)
 
     def outputs(self, key: str, model: str) -> t.Any:
@@ -120,6 +130,11 @@ def _encode(r: t.Any) -> t.Any:
         return r
     s.log.info(f'Unexpected type {type(r)} in Document.encode')
     return r
+
+
+def _encode_with_schema(r: t.Any, schema: Schema) -> t.Any:
+    if isinstance(r, dict):
+        return {k: _encode_with_schema(v, schema) for k, v in r.items()}
 
 
 def _unpack(item: t.Any) -> t.Any:

superduperdb/container/schema.py
@@ -4,7 +4,20 @@ import typing as t
 
 from superduperdb.container.component import Component
 from superduperdb.container.encoder import Encoder
-from superduperdb.db.base.db import DB
+
+
+"""
+Table with single image column:
+
+```python
+from superduperdb.ext.pillow.image import pil_image
+
+schema = Schema('image_schema', {'img': pil_image})
+t = Table(identifier='my_table', schema=schema)
+t.create(db)
+```
+
+"""
 
 
 @dc.dataclass
@@ -16,14 +29,16 @@ class Schema(Component):
     def trivial(self):
         return any([isinstance(v, Encoder) for v in self.fields.values()])
 
-    def on_create(self, db: DB):
+    # TO DO - move this logic to ibis
+    def on_create(self, db):
         if self.trivial:
             return
-        mapped_names = {
-            k: (f'{k}::_encodable={v.identifier}/{v.version}::' if isinstance(v, Encoder) else v)
-            for k, v in self.fields.items()
+        assert not any(['_encodable' in k for k in self.fields.keys()]), 'Reserved substring: _encodable'
+        schema_for_backend = {
+            f'{k}::_encodable={v.identifier}/{v.version}::' if isinstance(v, Encoder) else k: v
+            for k, v in self.fields.items() if isinstance(v, Encoder)
         }
-        db.databackend.create_schema(self.identifier, self.fields, mapped_names)
+        db.databackend.create_schema(self.identifier, schema_for_backend)
 
     def decode(self, data: t.Mapping[str, t.Any]) -> t.Mapping[str, t.Any]:
         if self.trivial:

superduperdb/db/ibis/query.py
@@ -6,7 +6,10 @@ import typing as t
 import ibis
 
 from superduperdb import CFG
+from superduperdb.container.component import Component
 from superduperdb.container.document import Document
+from superduperdb.container.schema import Schema
+from superduperdb.db.base.db import DB
 from superduperdb.db.ibis.cursor import SuperDuperIbisCursor
 
 PRIMARY_ID: str = 'id'
@@ -118,17 +121,27 @@ class OutputTable:
                 }
         return ibis.schema(schema)
 
+
 @dc.dataclass
-class Table:
-    name: str
-    primary_id: str = 'id'
+class Table(Component):
+    identifier: str
+    schema: Schema
     table: t.Any = None
+    primary_id: str = 'id'
+    #: A unique name for the class
+    type_id: t.ClassVar[str] = 'table'
+
+    def on_create(self, db: DB) -> None:
+        db.databackend.create_table(self.identifier, schema=self.schema)
 
     def get_table(self, conn):
         if self.table is None:
-            self.table = conn.table(self.name)
+            self.table = conn.table(self.identifier)
         return self.table
 
+    @property
+    def name(self):
+        return self.identifier
 
     def __getattr__(self, k):
         if k in self.__dict__:
@@ -159,9 +172,9 @@ class Table:
             'kwargs': {'valid_prob': valid_prob},
             'documents': args[0],
         }
-        kwargs.update({'table_name': self.name})
+        kwargs.update({'table_name': self.identifier})
         decoded_docs = [d.unpack() for d in args[0]]
-        kwargs.update({'obj': decoded_docs })
+        kwargs.update({'obj': decoded_docs})
         args = args[1:]
         
         insert = Query(
@@ -240,11 +253,10 @@ class QueryLinker(LogicalExprMixin):
             parent = member.execute(db, parent, self.collection, table)
         return parent
 
-
     def outputs(self, model, db):
         curr_query = self.build(db)
         model_table = db.db.table(model)
-        query = curr_query.join(model_table, [model_table.id == curr_query.id, model_table.query_id == self.collection.name])
+        query = curr_query.join(model_table, [model_table.id == curr_query.id, model_table.query_id == self.collection.identifier])
         cursor = SuperDuperIbisCursor(query, self.collection.primary_id, encoders=db.encoders)
         return cursor.execute()
 
@@ -260,7 +272,7 @@ class QueryLinker(LogicalExprMixin):
         if not outputs:
             return
 
-        input_table = self.collection.name
+        input_table = self.collection.identifier
 
         table_record = []
         for ix in range(len(outputs)):
@@ -284,8 +296,6 @@ class QueryLinker(LogicalExprMixin):
         for member in self.members:
             parent = member.execute(db, parent, self.collection, ibis_table=ibis_table)
         return parent
-
-
 
 
 class PlaceHolderQuery:
@@ -370,19 +380,20 @@ class Insert:
     namespace: str = 'ibis'
 
     def pre(self, db):
-        valid_prob = self.kwargs.get('valid_prob', 0.05)
+        if self.base_table.identifier not in db.tables:
+            db.add(self.base_table)
 
         for e in self.encoders:
             db.add(e)
-        documents = [r.encode() for r in self.documents]
+
+        documents = [r.encode(self.base_table.schema) for r in self.documents]
         for r in documents:
             if '_fold' in r:
                 continue
-            if random.random() < valid_prob:
+            if random.random() < 0.05:
                 r['_fold'] = 'valid'
             else:
                 r['_fold'] = 'train'
-
         return documents
     
     def post(self, db, output, *args, **kwargs):
@@ -390,7 +401,6 @@ class Insert:
         inserted_ids = [d[PRIMARY_ID] for d in kwargs['kwargs']['obj']]
         if self.refresh and not CFG.cdc:
             '''
-
             graph = db.refresh_after_update_or_insert(
                 query=self,  # type: ignore[arg-type]
                 ids=inserted_ids,

test/integration/test_ibis.py
@@ -1,9 +1,13 @@
 import ibis
-import sqlalchemy
+import PIL.Image
+import pandas
 from sklearn import svm
+import torchvision
 
 from superduperdb import superduper
 from superduperdb import CFG
+from superduperdb.container.encoder import Encoder
+from superduperdb.container.schema import Schema
 from superduperdb.db.base.build import build_vector_database
 from superduperdb.container.document import Document as D
 from superduperdb.db.ibis.db import IbisDB
@@ -11,72 +15,106 @@ from superduperdb.db.filesystem.artifacts import FileSystemArtifactStore
 from superduperdb.db.ibis.data_backend import IbisDataBackend
 from superduperdb.db.sqlalchemy.metadata import SQLAlchemyMetadata
 from superduperdb.db.ibis.query import Table
+from superduperdb.ext.pillow.image import pil_image
+from superduperdb.ext.torch.model import TorchModel
+# from superduperdb.container.schema import Schema
 
 
-
-
-
-
-connection_sql=sqlalchemy.create_engine('sqlite:///./mydb.sqlite')
 connection = ibis.sqlite.connect("mydb.sqlite")
-
-
 
 # Insert some sample data into the table
 schema = ibis.schema({'id': 'int64', 'health': 'int32', 'age': 'int32'})
 # Create the table
 table_name = 'my_table'
-connection.create_table(table_name, schema=schema)
 
-data_to_insert = [
-    (1, 0, 25),
-    (2, 1, 26),
-    (3, 1, 27),
-    (4, 1, 28),
-    (5, 0, 29),
-]
-connection.insert(table_name, data_to_insert)
-
-
+# connection.create_table(table_name, schema=schema)
 
 
 # create data layer
 db = IbisDB(
     databackend=IbisDataBackend(conn=connection, name='ibis'),
-    metadata=SQLAlchemyMetadata(conn=connection_sql, name='ibis'),
+    metadata=SQLAlchemyMetadata(conn=connection.con, name='ibis'),
     artifact_store=FileSystemArtifactStore(
         conn='./.tmp', name='ibis'
     ),
     vector_database=build_vector_database(CFG.vector_search.type),
 )
 
+schema = Schema(
+    identifier='my_table',
+    fields={
+        'id': 'int64',
+        'health': 'int32',
+        'age': 'int32',
+        'image': pil_image,
+    }
+)
+
+im = PIL.Image.open('test/material/data/test-image.jpeg')
+
+data_to_insert = [
+    (1, 0, 25, im),
+    (2, 1, 26, im),
+    (3, 1, 27, im),
+    (4, 1, 28, im),
+    (5, 0, 29, im),
+]
+
+t = Table(identifier='my_table', schema=schema)
+
+t.create(db, schema=...)
+
+def create(self, db, schema):
+    db.add(schema)
+    new_schema = {}
+    for k, v in schema.fields.items():
+        if isinstance(v, Encoder):
+            new_schema[f'{k}::_encodable={v.identifier}/{v.version}::'] = 'binary'
+    self.conn.create_table(self.identifier, schema=schema.fields)
+
+db.add(t)
+
+db.execute(
+    t.insert([D(d) for d in data_to_insert])
+)
+
+# preprocessing function
+preprocess = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(256),
+    torchvision.transforms.CenterCrop(224),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(
+    mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225]
+)])
+
+# create a torchvision model
+resnet = TorchModel(
+    preprocess=preprocess,
+    object=torchvision.models.resnet18(pretrained=True),
+)
+
+# apply the torchvision model
+resnet.predict(X='image', db=db, select=t, max_chunk_size=3000, overwrite=True)
 
 # add a sklearn model
-table = Table(name='my_table')
+
+
 svc = svm.SVC(gamma='scale', class_weight='balanced', C=100, verbose=True)
-svc.fit(data_to_insert, [0, 1, 1, 1, 0])
+svc.fit(pandas.DataFrame(data_to_insert).iloc[:, :3], [0, 1, 1, 1, 0])
 
 model = superduper(
-        svc,
+    svc,
     postprocess=lambda x: int(x),
     preprocess=lambda x: list(x.values()
 ))
-db.add(model)
 
-
-
-# Do prediction
-m = db.load('model', 'svc')
-m.predict(X='_base', db=db, select=table.filter(table.age > 25), max_chunk_size=3000, overwrite=True)    
-
-
-
+model.predict(X='_base', db=db, select=table.filter(table.age > 25), max_chunk_size=3000, overwrite=True)    
 
 #  Query result back
-out_table = connection.table(m.identifier)
-q = table.filter(table.age > 26).outputs(m.identifier, db)
+out_table = connection.table(model.identifier)
+q = table.filter(table.age > 26).outputs(model.identifier, db)
 
 curr = db.execute(q)
-
 for c in curr:
     print(c)

Pinnacle-Networks/Pinnacle

Former-commit-id: b3894314d7896cf461848e73385f96c7cdb7f995

superduperdb/container/document.py
@@ -5,11 +5,19 @@ from bson.objectid import ObjectId
 
 import pinnacledb as s
 from pinnacledb.container.encoder import Encodable
+from pinnacledb.container.schema import Schema
 
 ContentType = t.Union[t.Dict, Encodable]
 ItemType = t.Union[t.Dict[str, t.Any], Encodable, ObjectId]
 
 _OUTPUTS_KEY: str = '_outputs'
+
+
+class Row:
+    content: ContentType
+
+    def __init__(self) -> None:
+        pass
 
 
 class Document:
@@ -31,8 +39,10 @@ class Document:
         """Dump this document into BSON and encode as bytes"""
         return bson.encode(self.encode())
 
-    def encode(self) -> t.Any:
+    def encode(self, schema: t.Optional[Schema] = None) -> t.Any:
         """Make a copy of the content with all the Encodables encoded"""
+        if schema is not None:
+            return _encode_with_schema(self.content, schema)
         return _encode(self.content)
 
     def outputs(self, key: str, model: str) -> t.Any:
@@ -124,6 +134,11 @@ def _encode(r: t.Any) -> t.Any:
         return r
     s.log.info(f'Unexpected type {type(r)} in Document.encode')
     return r
+
+
+def _encode_with_schema(r: t.Any, schema: Schema) -> t.Any:
+    if isinstance(r, dict):
+        return {k: _encode_with_schema(v, schema) for k, v in r.items()}
 
 
 def _unpack(item: t.Any) -> t.Any:

superduperdb/container/schema.py
@@ -4,7 +4,20 @@ import typing as t
 
 from pinnacledb.container.component import Component
 from pinnacledb.container.encoder import Encoder
-from pinnacledb.db.base.db import DB
+
+
+"""
+Table with single image column:
+
+```python
+from pinnacledb.ext.pillow.image import pil_image
+
+schema = Schema('image_schema', {'img': pil_image})
+t = Table(identifier='my_table', schema=schema)
+t.create(db)
+```
+
+"""
 
 
 @dc.dataclass
@@ -16,14 +29,16 @@ class Schema(Component):
     def trivial(self):
         return any([isinstance(v, Encoder) for v in self.fields.values()])
 
-    def on_create(self, db: DB):
+    # TO DO - move this logic to ibis
+    def on_create(self, db):
         if self.trivial:
             return
-        mapped_names = {
-            k: (f'{k}::_encodable={v.identifier}/{v.version}::' if isinstance(v, Encoder) else v)
-            for k, v in self.fields.items()
+        assert not any(['_encodable' in k for k in self.fields.keys()]), 'Reserved substring: _encodable'
+        schema_for_backend = {
+            f'{k}::_encodable={v.identifier}/{v.version}::' if isinstance(v, Encoder) else k: v
+            for k, v in self.fields.items() if isinstance(v, Encoder)
         }
-        db.databackend.create_schema(self.identifier, self.fields, mapped_names)
+        db.databackend.create_schema(self.identifier, schema_for_backend)
 
     def decode(self, data: t.Mapping[str, t.Any]) -> t.Mapping[str, t.Any]:
         if self.trivial:

superduperdb/db/ibis/query.py
@@ -6,7 +6,10 @@ import typing as t
 import ibis
 
 from pinnacledb import CFG
+from pinnacledb.container.component import Component
 from pinnacledb.container.document import Document
+from pinnacledb.container.schema import Schema
+from pinnacledb.db.base.db import DB
 from pinnacledb.db.ibis.cursor import SuperDuperIbisCursor
 from pinnacledb.container.serializable import Serializable
 
@@ -119,17 +122,27 @@ class OutputTable:
                 }
         return ibis.schema(schema)
 
+
 @dc.dataclass
-class Table:
-    name: str
-    primary_id: str = 'id'
+class Table(Component):
+    identifier: str
+    schema: Schema
     table: t.Any = None
+    primary_id: str = 'id'
+    #: A unique name for the class
+    type_id: t.ClassVar[str] = 'table'
+
+    def on_create(self, db: DB) -> None:
+        db.databackend.create_table(self.identifier, schema=self.schema)
 
     def get_table(self, conn):
         if self.table is None:
-            self.table = conn.table(self.name)
+            self.table = conn.table(self.identifier)
         return self.table
 
+    @property
+    def name(self):
+        return self.identifier
 
     def __getattr__(self, k):
         if k in self.__dict__:
@@ -160,9 +173,9 @@ class Table:
             'kwargs': {'valid_prob': valid_prob},
             'documents': args[0],
         }
-        kwargs.update({'table_name': self.name})
+        kwargs.update({'table_name': self.identifier})
         decoded_docs = [d.unpack() for d in args[0]]
-        kwargs.update({'obj': decoded_docs })
+        kwargs.update({'obj': decoded_docs})
         args = args[1:]
         
         insert = Query(
@@ -241,11 +254,10 @@ class QueryLinker(Serializable, LogicalExprMixin):
             parent = member.execute(db, parent, self.collection, table)
         return parent
 
-
     def outputs(self, model, db):
         curr_query = self.build(db)
         model_table = db.db.table(model)
-        query = curr_query.join(model_table, [model_table.id == curr_query.id, model_table.query_id == self.collection.name])
+        query = curr_query.join(model_table, [model_table.id == curr_query.id, model_table.query_id == self.collection.identifier])
         cursor = SuperDuperIbisCursor(query, self.collection.primary_id, encoders=db.encoders)
         return cursor.execute()
 
@@ -261,7 +273,7 @@ class QueryLinker(Serializable, LogicalExprMixin):
         if not outputs:
             return
 
-        input_table = self.collection.name
+        input_table = self.collection.identifier
 
         table_record = []
         for ix in range(len(outputs)):
@@ -285,8 +297,6 @@ class QueryLinker(Serializable, LogicalExprMixin):
         for member in self.members:
             parent = member.execute(db, parent, self.collection, ibis_table=ibis_table)
         return parent
-
-
 
 
 class PlaceHolderQuery:
@@ -371,19 +381,20 @@ class Insert:
     namespace: str = 'ibis'
 
     def pre(self, db):
-        valid_prob = self.kwargs.get('valid_prob', 0.05)
+        if self.base_table.identifier not in db.tables:
+            db.add(self.base_table)
 
         for e in self.encoders:
             db.add(e)
-        documents = [r.encode() for r in self.documents]
+
+        documents = [r.encode(self.base_table.schema) for r in self.documents]
         for r in documents:
             if '_fold' in r:
                 continue
-            if random.random() < valid_prob:
+            if random.random() < 0.05:
                 r['_fold'] = 'valid'
             else:
                 r['_fold'] = 'train'
-
         return documents
     
     def post(self, db, output, *args, **kwargs):
@@ -391,7 +402,6 @@ class Insert:
         inserted_ids = [d[PRIMARY_ID] for d in kwargs['kwargs']['obj']]
         if self.refresh and not CFG.cdc:
             '''
-
             graph = db.refresh_after_update_or_insert(
                 query=self,  # type: ignore[arg-type]
                 ids=inserted_ids,

test/integration/test_ibis.py
@@ -1,9 +1,13 @@
 import ibis
-import sqlalchemy
+import PIL.Image
+import pandas
 from sklearn import svm
+import torchvision
 
 from pinnacledb import pinnacle
 from pinnacledb import CFG
+from pinnacledb.container.encoder import Encoder
+from pinnacledb.container.schema import Schema
 from pinnacledb.db.base.build import build_vector_database
 from pinnacledb.container.document import Document as D
 from pinnacledb.db.ibis.db import IbisDB
@@ -11,72 +15,106 @@ from pinnacledb.db.filesystem.artifacts import FileSystemArtifactStore
 from pinnacledb.db.ibis.data_backend import IbisDataBackend
 from pinnacledb.db.sqlalchemy.metadata import SQLAlchemyMetadata
 from pinnacledb.db.ibis.query import Table
+from pinnacledb.ext.pillow.image import pil_image
+from pinnacledb.ext.torch.model import TorchModel
+# from pinnacledb.container.schema import Schema
 
 
-
-
-
-
-connection_sql=sqlalchemy.create_engine('sqlite:///./mydb.sqlite')
 connection = ibis.sqlite.connect("mydb.sqlite")
-
-
 
 # Insert some sample data into the table
 schema = ibis.schema({'id': 'int64', 'health': 'int32', 'age': 'int32'})
 # Create the table
 table_name = 'my_table'
-connection.create_table(table_name, schema=schema)
 
-data_to_insert = [
-    (1, 0, 25),
-    (2, 1, 26),
-    (3, 1, 27),
-    (4, 1, 28),
-    (5, 0, 29),
-]
-connection.insert(table_name, data_to_insert)
-
-
+# connection.create_table(table_name, schema=schema)
 
 
 # create data layer
 db = IbisDB(
     databackend=IbisDataBackend(conn=connection, name='ibis'),
-    metadata=SQLAlchemyMetadata(conn=connection_sql, name='ibis'),
+    metadata=SQLAlchemyMetadata(conn=connection.con, name='ibis'),
     artifact_store=FileSystemArtifactStore(
         conn='./.tmp', name='ibis'
     ),
     vector_database=build_vector_database(CFG.vector_search.type),
 )
 
+schema = Schema(
+    identifier='my_table',
+    fields={
+        'id': 'int64',
+        'health': 'int32',
+        'age': 'int32',
+        'image': pil_image,
+    }
+)
+
+im = PIL.Image.open('test/material/data/test-image.jpeg')
+
+data_to_insert = [
+    (1, 0, 25, im),
+    (2, 1, 26, im),
+    (3, 1, 27, im),
+    (4, 1, 28, im),
+    (5, 0, 29, im),
+]
+
+t = Table(identifier='my_table', schema=schema)
+
+t.create(db, schema=...)
+
+def create(self, db, schema):
+    db.add(schema)
+    new_schema = {}
+    for k, v in schema.fields.items():
+        if isinstance(v, Encoder):
+            new_schema[f'{k}::_encodable={v.identifier}/{v.version}::'] = 'binary'
+    self.conn.create_table(self.identifier, schema=schema.fields)
+
+db.add(t)
+
+db.execute(
+    t.insert([D(d) for d in data_to_insert])
+)
+
+# preprocessing function
+preprocess = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(256),
+    torchvision.transforms.CenterCrop(224),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize(
+    mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225]
+)])
+
+# create a torchvision model
+resnet = TorchModel(
+    preprocess=preprocess,
+    object=torchvision.models.resnet18(pretrained=True),
+)
+
+# apply the torchvision model
+resnet.predict(X='image', db=db, select=t, max_chunk_size=3000, overwrite=True)
 
 # add a sklearn model
-table = Table(name='my_table')
+
+
 svc = svm.SVC(gamma='scale', class_weight='balanced', C=100, verbose=True)
-svc.fit(data_to_insert, [0, 1, 1, 1, 0])
+svc.fit(pandas.DataFrame(data_to_insert).iloc[:, :3], [0, 1, 1, 1, 0])
 
 model = pinnacle(
-        svc,
+    svc,
     postprocess=lambda x: int(x),
     preprocess=lambda x: list(x.values()
 ))
-db.add(model)
 
-
-
-# Do prediction
-m = db.load('model', 'svc')
-m.predict(X='_base', db=db, select=table.filter(table.age > 25), max_chunk_size=3000, overwrite=True)    
-
-
-
+model.predict(X='_base', db=db, select=table.filter(table.age > 25), max_chunk_size=3000, overwrite=True)    
 
 #  Query result back
-out_table = connection.table(m.identifier)
-q = table.filter(table.age > 26).outputs(m.identifier, db)
+out_table = connection.table(model.identifier)
+q = table.filter(table.age > 26).outputs(model.identifier, db)
 
 curr = db.execute(q)
-
 for c in curr:
     print(c)