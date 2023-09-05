superduper-io/superduper

Make ibis query serializable

superduperdb/db/ibis/query.py
@@ -8,6 +8,7 @@ import ibis
 from superduperdb import CFG
 from superduperdb.container.document import Document
 from superduperdb.db.ibis.cursor import SuperDuperIbisCursor
+from superduperdb.container.serializable import Serializable
 
 PRIMARY_ID: str = 'id'
 
@@ -197,7 +198,7 @@ class LogicalExprMixin:
 
 
 @dc.dataclass
-class QueryLinker(LogicalExprMixin):
+class QueryLinker(Serializable, LogicalExprMixin):
     collection: Table
     query_type: str = 'find'
     args: t.Sequence = dc.field(default_factory=list)

Pinnacle-Networks/Pinnacle

Former-commit-id: 59dab797e22184ca169e6876d5d4e0d9672e9fa2

superduperdb/db/ibis/query.py
@@ -8,6 +8,7 @@ import ibis
 from pinnacledb import CFG
 from pinnacledb.container.document import Document
 from pinnacledb.db.ibis.cursor import SuperDuperIbisCursor
+from pinnacledb.container.serializable import Serializable
 
 PRIMARY_ID: str = 'id'
 
@@ -197,7 +198,7 @@ class LogicalExprMixin:
 
 
 @dc.dataclass
-class QueryLinker(LogicalExprMixin):
+class QueryLinker(Serializable, LogicalExprMixin):
     collection: Table
     query_type: str = 'find'
     args: t.Sequence = dc.field(default_factory=list)