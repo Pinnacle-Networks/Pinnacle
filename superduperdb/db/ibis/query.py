superduper-io/superduper

Add prelike and postlike

superduperdb/db/ibis/query.py
@@ -1,12 +1,28 @@
 import dataclasses as dc
+import enum
 import random
 import typing as t
 
 from superduperdb import CFG
+from superduperdb.container.document import Document
 from superduperdb.db.ibis.cursor import SuperDuperIbisCursor
 
+
+class QueryType(enum.Enum):
+    QUERY = 'query'
+    ATTR = 'attr'
+
+
 class Query:
-    def __init__(self, name, type='query', args=[], kwargs={}, sddb_kwargs={}, connection_parent=False):
+    def __init__(
+        self,
+        name,
+        type=QueryType.QUERY.value,
+        args=[],
+        kwargs={},
+        sddb_kwargs={},
+        connection_parent=False,
+    ):
         self.name = name
         query = query_lookup.get(name, None)
         if query is None:
@@ -18,24 +34,33 @@ class Query:
         self.kwargs = kwargs
         self.connection_parent = connection_parent
 
-    def execute(self, db, parent, table):
-        if self.type == "attr":
-            return getattr(parent, self.query.type_id)
+    def execute(self, db, parent, table, ibis_table=None):
+        if self.type == QueryType.ATTR.value:
+            return getattr(parent, self.query.name)
 
         self.query.pre(db)
         if len(self.args) == 1 and isinstance(self.args[0], QueryLinker):
-            self.args = [self.args[0].execute(db, table)]
-        if self.connection_parent:
-            parent = getattr(db.db, self.query.type_id)(*self.args, **self.kwargs)
-        else:
-            parent = getattr(parent, self.query.type_id)(*self.args, **self.kwargs)
+            self.args = [self.args[0].execute(db, parent, ibis_table)]
 
-        parent = self.query.post(db, parent)
+        if self.query.namespace == 'ibis':
+            if self.connection_parent:
+                parent = getattr(db.db, self.query.name)(*self.args, **self.kwargs)
+            else:
+                parent = getattr(parent, self.query.name)(*self.args, **self.kwargs)
+
+        parent = self.query.post(
+            db,
+            parent,
+            table=table,
+            ibis_table=ibis_table,
+            args=self.args,
+            kwargs=self.kwargs,
+        )
         return parent
 
 
 class QueryChain:
-    def __init__(self, seed=None, type='attr'):
+    def __init__(self, seed=None, type=QueryType.QUERY.value):
         if isinstance(seed, str):
             query = Query(seed, type=type)
         elif isinstance(seed, Query):
@@ -45,7 +70,7 @@ class QueryChain:
 
         self.chain = [query]
 
-    def append(self, data, type='query'):
+    def append(self, data, type=QueryType.QUERY.value):
         query = Query(data, type=type)
         self.chain.append(query)
 
@@ -63,7 +88,7 @@ class QueryChain:
 
     def __iter__(self):
         for query in self.chain:
-            if query.type in ['query', 'attr']:
+            if query.type in [QueryType.QUERY.value, QueryType.ATTR.value]:
                 yield query
 
 
@@ -75,27 +100,44 @@ class Table:
     def __getattr__(self, k):
         if k in self.__dict__:
             return self.__getattr__(k)
-        return QueryLinker(self, query_type=k, members=QueryChain(k, type='attr'))
+        return QueryLinker(
+            self, query_type=k, members=QueryChain(k, type=QueryType.ATTR.value)
+        )
 
-    def like(self):
-        raise NotImplementedError
+    def like(self, r=None, n=10, vector_index=None):
+        k = 'prelike'
+        kwargs = {'r': r, 'n': n, 'vector_index': vector_index}
+        query = Query(k, args=[], sddb_kwargs=kwargs)
+        return QueryLinker(self, query_type=k, members=QueryChain(query))
 
-    def insert(self, 
-           *args,
-            refresh: bool = True,
-            verbose: bool = True,
-            encoders: t.Sequence = [],
-            valid_prob: float = 0.05,
-           **kwargs
-               ):
-        
-        sddb_kwargs = {'refresh': refresh, 'verbose': verbose, 'encoders': encoders, 'kwargs': {'valid_prob': valid_prob},   'documents': args[0]}
+    def insert(
+        self,
+        *args,
+        refresh: bool = True,
+        verbose: bool = True,
+        encoders: t.Sequence = [],
+        valid_prob: float = 0.05,
+        **kwargs,
+    ):
+        sddb_kwargs = {
+            'refresh': refresh,
+            'verbose': verbose,
+            'encoders': encoders,
+            'kwargs': {'valid_prob': valid_prob},
+            'documents': args[0],
+        }
         args = args[1:]
-        insert = Query('insert', type='query', args=args, kwargs=kwargs, sddb_kwargs=sddb_kwargs,  connection_parent=True)
+        insert = Query(
+            'insert',
+            type=QueryType.QUERY.value,
+            args=args,
+            kwargs=kwargs,
+            sddb_kwargs=sddb_kwargs,
+            connection_parent=True,
+        )
 
-        qc =QueryChain(insert)
+        qc = QueryChain(insert)
         return QueryLinker(self, query_type='insert', members=qc)
-
 
 
 class LogicalExprMixin:
@@ -106,15 +148,15 @@ class LogicalExprMixin:
 
     def eq(self, other, members, collection):
         k = '__eq__'
-        self._logical_expr(other, members, collection, k)
+        return self._logical_expr(other, members, collection, k)
 
     def gt(self, other, members, collection):
         k = '__gt__'
-        self._logical_expr(other, members, collection, k)
+        return self._logical_expr(other, members, collection, k)
 
     def lt(self, other, members, collection):
         k = '__lt__'
-        self._logical_expr(other, members, collection, k)
+        return self._logical_expr(other, members, collection, k)
 
 
 @dc.dataclass
@@ -123,7 +165,6 @@ class QueryLinker(LogicalExprMixin):
     query_type: str = 'find'
     args: t.Sequence = dc.field(default_factory=list)
     kwargs: t.Dict = dc.field(default_factory=dict)
-    is_callable: t.Optional[bool] = None
     members: QueryChain = dc.field(default_factory=QueryChain)
 
     def __getattr__(self, k):
@@ -133,13 +174,13 @@ class QueryLinker(LogicalExprMixin):
         return QueryLinker(self.collection, query_type=k, members=self.members)
 
     def __eq__(self, other):
-        self.eq(other, members=self.members, collection=self.collection)
+        return self.eq(other, members=self.members, collection=self.collection)
 
     def __lt__(self, other):
-        self.lt(other, members=self.members, collection=self.collection)
+        return self.lt(other, members=self.members, collection=self.collection)
 
     def __gt__(self, other):
-        self.gt(other, members=self.members, collection=self.collection)
+        return self.gt(other, members=self.members, collection=self.collection)
 
     def select_ids(self):
         k = 'select'
@@ -156,7 +197,7 @@ class QueryLinker(LogicalExprMixin):
         return QueryLinker(self.collection, query_type='filter', members=self.members)
 
     def __call__(self, *args, **kwargs):
-        self.members.update_last_query(args, kwargs, type='query')
+        self.members.update_last_query(args, kwargs, type=QueryType.QUERY.value)
 
         return QueryLinker(
             collection=self.collection,
@@ -166,9 +207,9 @@ class QueryLinker(LogicalExprMixin):
             kwargs=kwargs,
         )
 
-    def execute(self, db, parent):
+    def execute(self, db, parent, ibis_table):
         for member in self.members:
-            parent = member.execute(db, parent, parent)
+            parent = member.execute(db, parent, self.collection, ibis_table=ibis_table)
         return parent
 
 
@@ -179,34 +220,85 @@ class IbisConnection:
     def _execute(self, db, query, parent):
         table = parent
         for member in query.members:
-            parent = member.execute(db, parent, table)
-        return parent.execute()
+            parent = member.execute(
+                db, parent, table=query.collection, ibis_table=table
+            )
+        cursor = SuperDuperIbisCursor(parent, query.collection.primary_id, encoders={})
+        return cursor.execute()
 
     def execute(self, query):
         return self._execute(self.db, query, self.db.table(query.collection.name))
 
+
 class PlaceHolderQuery:
-    def __init__(self, data, *args, **kwargs):
-        self.type_id = data
+    type_id: t.Literal['query'] = 'query'  # type: ignore[annotation-unchecked]
+
+    def __init__(self, name, *args, **kwargs):
+        self.name = name
         self.args = args
         self.kwargs = kwargs
 
-    def pre(self, db):
-        pass
+        self.namespace: str = 'ibis'  # type: ignore[annotation-unchecked]
 
-    def post(self, db, output):
-        pass
+    def pre(self, db):
+        ...
+
+    def post(self, db, output, *args, **kwargs):
+        return output
 
 
 @dc.dataclass
-class Select:
-    id_field: str = '_id'
-    type_id: t.Literal['select'] = 'select'
+class PostLike:
+    type_id: t.Literal['Ibis.PostLike'] = 'Ibis.PostLike'
+    name: str = 'postlike'
+    namespace: str = 'sddb'
+
     def pre(self, db):
         pass
 
-    def post(self, db, cursor):
-        return SuperDuperIbisCursor(raw_cursor=cursor, id_field=self.id_field, encoders={})#db.encoders)
+    def post(self, db, output, table=None, ibis_table=None, args=[], kwargs={}):
+        r = kwargs.get('r', None)
+        assert r is not None, 'r must be provided'
+        n = kwargs.get('n', 10)
+
+        vector_index = kwargs.get('vector_index', 'vector_index')
+        ids = output.select(table.primary_id)
+        ids, scores = db._select_nearest(
+            like=r,
+            vector_index=vector_index,
+            n=n,
+            ids=ids,
+        )
+        return output.filter(  # type: ignore[annotation-unchecked]
+            ibis_table.__getattr__(table.primary_id).isin(ids)
+        )
+
+
+@dc.dataclass
+class PreLike:
+    r: t.Any
+    vector_index: str = 'vector_index'
+    n: int = 10
+    collection: str = 'collection'
+    primary_id: str = 'id'
+    type_id: t.Literal['Ibis.PreLike'] = 'Ibis.PreLike'
+    name: str = 'prelike'
+    namespace: str = 'sddb'
+
+    def pre(self, db):
+        pass
+
+    def post(self, db, output, table=None, ibis_table=None, args=[], kwargs={}):
+        '''
+
+        ids, _ = db._select_nearest(
+            like=self.r, vector_index=self.vector_index, n=self.n
+        )
+        '''
+        ids = [1, 2, 3]
+        f = output.filter(ibis_table.__getattr__(self.primary_id).isin(ids))
+        return f
+
 
 @dc.dataclass
 class Insert:
@@ -215,10 +307,13 @@ class Insert:
     verbose: bool = True
     kwargs: t.Dict = dc.field(default_factory=dict)
     encoders: t.Sequence = dc.field(default_factory=list)
-    type_id: t.Literal['insert'] = 'insert'
+    type_id: t.Literal['Ibis.insert'] = 'Ibis.insert'
+    name: str = 'insert'
+    namespace: str = 'ibis'
 
     def pre(self, db):
         valid_prob = self.kwargs.get('valid_prob', 0.05)
+
         for e in self.encoders:
             db.add(e)
         documents = [r.encode() for r in self.documents]
@@ -232,7 +327,7 @@ class Insert:
 
         return documents
 
-    def post(self, db, output):
+    def post(self, db, output, *args, **kwargs):
         graph = None
         if self.refresh and not CFG.cdc:
             graph = db.refresh_after_update_or_insert(
@@ -240,8 +335,7 @@ class Insert:
                 ids=output.inserted_ids,
                 verbose=self.verbose,
             )
-        return output
+        return graph, output
 
 
-
-query_lookup = {'insert': Insert, 'select': Select}
+query_lookup = {'insert': Insert, 'prelike': PreLike, 'like': PostLike}

Pinnacle-Networks/Pinnacle

Former-commit-id: 2e0b35377642c74554c6dfe4dbde419d0f9e959d

superduperdb/db/ibis/query.py
@@ -1,12 +1,28 @@
 import dataclasses as dc
+import enum
 import random
 import typing as t
 
 from pinnacledb import CFG
+from pinnacledb.container.document import Document
 from pinnacledb.db.ibis.cursor import SuperDuperIbisCursor
 
+
+class QueryType(enum.Enum):
+    QUERY = 'query'
+    ATTR = 'attr'
+
+
 class Query:
-    def __init__(self, name, type='query', args=[], kwargs={}, sddb_kwargs={}, connection_parent=False):
+    def __init__(
+        self,
+        name,
+        type=QueryType.QUERY.value,
+        args=[],
+        kwargs={},
+        sddb_kwargs={},
+        connection_parent=False,
+    ):
         self.name = name
         query = query_lookup.get(name, None)
         if query is None:
@@ -18,24 +34,33 @@ class Query:
         self.kwargs = kwargs
         self.connection_parent = connection_parent
 
-    def execute(self, db, parent, table):
-        if self.type == "attr":
-            return getattr(parent, self.query.type_id)
+    def execute(self, db, parent, table, ibis_table=None):
+        if self.type == QueryType.ATTR.value:
+            return getattr(parent, self.query.name)
 
         self.query.pre(db)
         if len(self.args) == 1 and isinstance(self.args[0], QueryLinker):
-            self.args = [self.args[0].execute(db, table)]
-        if self.connection_parent:
-            parent = getattr(db.db, self.query.type_id)(*self.args, **self.kwargs)
-        else:
-            parent = getattr(parent, self.query.type_id)(*self.args, **self.kwargs)
+            self.args = [self.args[0].execute(db, parent, ibis_table)]
 
-        parent = self.query.post(db, parent)
+        if self.query.namespace == 'ibis':
+            if self.connection_parent:
+                parent = getattr(db.db, self.query.name)(*self.args, **self.kwargs)
+            else:
+                parent = getattr(parent, self.query.name)(*self.args, **self.kwargs)
+
+        parent = self.query.post(
+            db,
+            parent,
+            table=table,
+            ibis_table=ibis_table,
+            args=self.args,
+            kwargs=self.kwargs,
+        )
         return parent
 
 
 class QueryChain:
-    def __init__(self, seed=None, type='attr'):
+    def __init__(self, seed=None, type=QueryType.QUERY.value):
         if isinstance(seed, str):
             query = Query(seed, type=type)
         elif isinstance(seed, Query):
@@ -45,7 +70,7 @@ class QueryChain:
 
         self.chain = [query]
 
-    def append(self, data, type='query'):
+    def append(self, data, type=QueryType.QUERY.value):
         query = Query(data, type=type)
         self.chain.append(query)
 
@@ -63,7 +88,7 @@ class QueryChain:
 
     def __iter__(self):
         for query in self.chain:
-            if query.type in ['query', 'attr']:
+            if query.type in [QueryType.QUERY.value, QueryType.ATTR.value]:
                 yield query
 
 
@@ -75,27 +100,44 @@ class Table:
     def __getattr__(self, k):
         if k in self.__dict__:
             return self.__getattr__(k)
-        return QueryLinker(self, query_type=k, members=QueryChain(k, type='attr'))
+        return QueryLinker(
+            self, query_type=k, members=QueryChain(k, type=QueryType.ATTR.value)
+        )
 
-    def like(self):
-        raise NotImplementedError
+    def like(self, r=None, n=10, vector_index=None):
+        k = 'prelike'
+        kwargs = {'r': r, 'n': n, 'vector_index': vector_index}
+        query = Query(k, args=[], sddb_kwargs=kwargs)
+        return QueryLinker(self, query_type=k, members=QueryChain(query))
 
-    def insert(self, 
-           *args,
-            refresh: bool = True,
-            verbose: bool = True,
-            encoders: t.Sequence = [],
-            valid_prob: float = 0.05,
-           **kwargs
-               ):
-        
-        sddb_kwargs = {'refresh': refresh, 'verbose': verbose, 'encoders': encoders, 'kwargs': {'valid_prob': valid_prob},   'documents': args[0]}
+    def insert(
+        self,
+        *args,
+        refresh: bool = True,
+        verbose: bool = True,
+        encoders: t.Sequence = [],
+        valid_prob: float = 0.05,
+        **kwargs,
+    ):
+        sddb_kwargs = {
+            'refresh': refresh,
+            'verbose': verbose,
+            'encoders': encoders,
+            'kwargs': {'valid_prob': valid_prob},
+            'documents': args[0],
+        }
         args = args[1:]
-        insert = Query('insert', type='query', args=args, kwargs=kwargs, sddb_kwargs=sddb_kwargs,  connection_parent=True)
+        insert = Query(
+            'insert',
+            type=QueryType.QUERY.value,
+            args=args,
+            kwargs=kwargs,
+            sddb_kwargs=sddb_kwargs,
+            connection_parent=True,
+        )
 
-        qc =QueryChain(insert)
+        qc = QueryChain(insert)
         return QueryLinker(self, query_type='insert', members=qc)
-
 
 
 class LogicalExprMixin:
@@ -106,15 +148,15 @@ class LogicalExprMixin:
 
     def eq(self, other, members, collection):
         k = '__eq__'
-        self._logical_expr(other, members, collection, k)
+        return self._logical_expr(other, members, collection, k)
 
     def gt(self, other, members, collection):
         k = '__gt__'
-        self._logical_expr(other, members, collection, k)
+        return self._logical_expr(other, members, collection, k)
 
     def lt(self, other, members, collection):
         k = '__lt__'
-        self._logical_expr(other, members, collection, k)
+        return self._logical_expr(other, members, collection, k)
 
 
 @dc.dataclass
@@ -123,7 +165,6 @@ class QueryLinker(LogicalExprMixin):
     query_type: str = 'find'
     args: t.Sequence = dc.field(default_factory=list)
     kwargs: t.Dict = dc.field(default_factory=dict)
-    is_callable: t.Optional[bool] = None
     members: QueryChain = dc.field(default_factory=QueryChain)
 
     def __getattr__(self, k):
@@ -133,13 +174,13 @@ class QueryLinker(LogicalExprMixin):
         return QueryLinker(self.collection, query_type=k, members=self.members)
 
     def __eq__(self, other):
-        self.eq(other, members=self.members, collection=self.collection)
+        return self.eq(other, members=self.members, collection=self.collection)
 
     def __lt__(self, other):
-        self.lt(other, members=self.members, collection=self.collection)
+        return self.lt(other, members=self.members, collection=self.collection)
 
     def __gt__(self, other):
-        self.gt(other, members=self.members, collection=self.collection)
+        return self.gt(other, members=self.members, collection=self.collection)
 
     def select_ids(self):
         k = 'select'
@@ -156,7 +197,7 @@ class QueryLinker(LogicalExprMixin):
         return QueryLinker(self.collection, query_type='filter', members=self.members)
 
     def __call__(self, *args, **kwargs):
-        self.members.update_last_query(args, kwargs, type='query')
+        self.members.update_last_query(args, kwargs, type=QueryType.QUERY.value)
 
         return QueryLinker(
             collection=self.collection,
@@ -166,9 +207,9 @@ class QueryLinker(LogicalExprMixin):
             kwargs=kwargs,
         )
 
-    def execute(self, db, parent):
+    def execute(self, db, parent, ibis_table):
         for member in self.members:
-            parent = member.execute(db, parent, parent)
+            parent = member.execute(db, parent, self.collection, ibis_table=ibis_table)
         return parent
 
 
@@ -179,34 +220,85 @@ class IbisConnection:
     def _execute(self, db, query, parent):
         table = parent
         for member in query.members:
-            parent = member.execute(db, parent, table)
-        return parent.execute()
+            parent = member.execute(
+                db, parent, table=query.collection, ibis_table=table
+            )
+        cursor = SuperDuperIbisCursor(parent, query.collection.primary_id, encoders={})
+        return cursor.execute()
 
     def execute(self, query):
         return self._execute(self.db, query, self.db.table(query.collection.name))
 
+
 class PlaceHolderQuery:
-    def __init__(self, data, *args, **kwargs):
-        self.type_id = data
+    type_id: t.Literal['query'] = 'query'  # type: ignore[annotation-unchecked]
+
+    def __init__(self, name, *args, **kwargs):
+        self.name = name
         self.args = args
         self.kwargs = kwargs
 
-    def pre(self, db):
-        pass
+        self.namespace: str = 'ibis'  # type: ignore[annotation-unchecked]
 
-    def post(self, db, output):
-        pass
+    def pre(self, db):
+        ...
+
+    def post(self, db, output, *args, **kwargs):
+        return output
 
 
 @dc.dataclass
-class Select:
-    id_field: str = '_id'
-    type_id: t.Literal['select'] = 'select'
+class PostLike:
+    type_id: t.Literal['Ibis.PostLike'] = 'Ibis.PostLike'
+    name: str = 'postlike'
+    namespace: str = 'sddb'
+
     def pre(self, db):
         pass
 
-    def post(self, db, cursor):
-        return SuperDuperIbisCursor(raw_cursor=cursor, id_field=self.id_field, encoders={})#db.encoders)
+    def post(self, db, output, table=None, ibis_table=None, args=[], kwargs={}):
+        r = kwargs.get('r', None)
+        assert r is not None, 'r must be provided'
+        n = kwargs.get('n', 10)
+
+        vector_index = kwargs.get('vector_index', 'vector_index')
+        ids = output.select(table.primary_id)
+        ids, scores = db._select_nearest(
+            like=r,
+            vector_index=vector_index,
+            n=n,
+            ids=ids,
+        )
+        return output.filter(  # type: ignore[annotation-unchecked]
+            ibis_table.__getattr__(table.primary_id).isin(ids)
+        )
+
+
+@dc.dataclass
+class PreLike:
+    r: t.Any
+    vector_index: str = 'vector_index'
+    n: int = 10
+    collection: str = 'collection'
+    primary_id: str = 'id'
+    type_id: t.Literal['Ibis.PreLike'] = 'Ibis.PreLike'
+    name: str = 'prelike'
+    namespace: str = 'sddb'
+
+    def pre(self, db):
+        pass
+
+    def post(self, db, output, table=None, ibis_table=None, args=[], kwargs={}):
+        '''
+
+        ids, _ = db._select_nearest(
+            like=self.r, vector_index=self.vector_index, n=self.n
+        )
+        '''
+        ids = [1, 2, 3]
+        f = output.filter(ibis_table.__getattr__(self.primary_id).isin(ids))
+        return f
+
 
 @dc.dataclass
 class Insert:
@@ -215,10 +307,13 @@ class Insert:
     verbose: bool = True
     kwargs: t.Dict = dc.field(default_factory=dict)
     encoders: t.Sequence = dc.field(default_factory=list)
-    type_id: t.Literal['insert'] = 'insert'
+    type_id: t.Literal['Ibis.insert'] = 'Ibis.insert'
+    name: str = 'insert'
+    namespace: str = 'ibis'
 
     def pre(self, db):
         valid_prob = self.kwargs.get('valid_prob', 0.05)
+
         for e in self.encoders:
             db.add(e)
         documents = [r.encode() for r in self.documents]
@@ -232,7 +327,7 @@ class Insert:
 
         return documents
 
-    def post(self, db, output):
+    def post(self, db, output, *args, **kwargs):
         graph = None
         if self.refresh and not CFG.cdc:
             graph = db.refresh_after_update_or_insert(
@@ -240,8 +335,7 @@ class Insert:
                 ids=output.inserted_ids,
                 verbose=self.verbose,
             )
-        return output
+        return graph, output
 
 
-
-query_lookup = {'insert': Insert, 'select': Select}
+query_lookup = {'insert': Insert, 'prelike': PreLike, 'like': PostLike}