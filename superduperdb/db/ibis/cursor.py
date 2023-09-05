superduper-io/superduper

Added supercursor for ibis

superduperdb/db/ibis/cursor.py
@@ -1,0 +1,19 @@
+import dataclasses as dc
+from superduperdb.db.base.cursor import SuperDuperCursor
+
+@dc.dataclass
+class SuperDuperIbisCursor(SuperDuperCursor):
+    def execute(self):
+        raw_cursor = self.raw_cursor.execute()
+        self.dict_cursor = raw_cursor.to_dict(orient="records")
+        self._n = len(self.dict_cursor)
+        self._index = 0
+        return self
+
+    def cursor_next(self):
+       if self._index < self._n:
+           row = self.dict_cursor[self._index]
+           self._index += 1
+           return row
+       else:
+           raise StopIteration

superduperdb/db/ibis/query.py
@@ -3,6 +3,7 @@ import random
 import typing as t
 
 from superduperdb import CFG
+from superduperdb.db.ibis.cursor import SuperDuperIbisCursor
 
 class Query:
     def __init__(self, name, type='query', args=[], kwargs={}, sddb_kwargs={}, connection_parent=False):
@@ -200,12 +201,12 @@ class PlaceHolderQuery:
 @dc.dataclass
 class Select:
     id_field: str = '_id'
-    type_id: t.Literal['Ibis.select'] = 'Ibis.select'
+    type_id: t.Literal['select'] = 'select'
     def pre(self, db):
         pass
 
     def post(self, db, cursor):
-        return SuperDuperCursor(raw_cursor=cursor, id_field=self.id_field, encoders=db.encoders)
+        return SuperDuperIbisCursor(raw_cursor=cursor, id_field=self.id_field, encoders={})#db.encoders)
 
 @dc.dataclass
 class Insert:
@@ -214,7 +215,7 @@ class Insert:
     verbose: bool = True
     kwargs: t.Dict = dc.field(default_factory=dict)
     encoders: t.Sequence = dc.field(default_factory=list)
-    type_id: t.Literal['Ibis.insert'] = 'Ibis.insert'
+    type_id: t.Literal['insert'] = 'insert'
 
     def pre(self, db):
         valid_prob = self.kwargs.get('valid_prob', 0.05)
@@ -239,8 +240,8 @@ class Insert:
                 ids=output.inserted_ids,
                 verbose=self.verbose,
             )
-        return graph, output
+        return output
 
 
 
-query_lookup = {'insert': Insert}
+query_lookup = {'insert': Insert, 'select': Select}

Pinnacle-Networks/Pinnacle

Former-commit-id: 8c5a71799913a4cc1ff008270192cf56725dcf1c

superduperdb/db/ibis/cursor.py
@@ -1,0 +1,19 @@
+import dataclasses as dc
+from pinnacledb.db.base.cursor import SuperDuperCursor
+
+@dc.dataclass
+class SuperDuperIbisCursor(SuperDuperCursor):
+    def execute(self):
+        raw_cursor = self.raw_cursor.execute()
+        self.dict_cursor = raw_cursor.to_dict(orient="records")
+        self._n = len(self.dict_cursor)
+        self._index = 0
+        return self
+
+    def cursor_next(self):
+       if self._index < self._n:
+           row = self.dict_cursor[self._index]
+           self._index += 1
+           return row
+       else:
+           raise StopIteration

superduperdb/db/ibis/query.py
@@ -3,6 +3,7 @@ import random
 import typing as t
 
 from pinnacledb import CFG
+from pinnacledb.db.ibis.cursor import SuperDuperIbisCursor
 
 class Query:
     def __init__(self, name, type='query', args=[], kwargs={}, sddb_kwargs={}, connection_parent=False):
@@ -200,12 +201,12 @@ class PlaceHolderQuery:
 @dc.dataclass
 class Select:
     id_field: str = '_id'
-    type_id: t.Literal['Ibis.select'] = 'Ibis.select'
+    type_id: t.Literal['select'] = 'select'
     def pre(self, db):
         pass
 
     def post(self, db, cursor):
-        return SuperDuperCursor(raw_cursor=cursor, id_field=self.id_field, encoders=db.encoders)
+        return SuperDuperIbisCursor(raw_cursor=cursor, id_field=self.id_field, encoders={})#db.encoders)
 
 @dc.dataclass
 class Insert:
@@ -214,7 +215,7 @@ class Insert:
     verbose: bool = True
     kwargs: t.Dict = dc.field(default_factory=dict)
     encoders: t.Sequence = dc.field(default_factory=list)
-    type_id: t.Literal['Ibis.insert'] = 'Ibis.insert'
+    type_id: t.Literal['insert'] = 'insert'
 
     def pre(self, db):
         valid_prob = self.kwargs.get('valid_prob', 0.05)
@@ -239,8 +240,8 @@ class Insert:
                 ids=output.inserted_ids,
                 verbose=self.verbose,
             )
-        return graph, output
+        return output
 
 
 
-query_lookup = {'insert': Insert}
+query_lookup = {'insert': Insert, 'select': Select}