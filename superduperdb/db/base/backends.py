superduper-io/superduper

Add outputs support in ibis

superduperdb/db/base/backends.py
@@ -1,17 +1,21 @@
 from pymongo import MongoClient
+from ibis.backends.base import BaseBackend
 
 from superduperdb.base import config
 from superduperdb.db.mongodb.artifacts import MongoArtifactStore
 from superduperdb.db.mongodb.data_backend import MongoDataBackend
+from superduperdb.db.filesystem.artifacts import FileSystemArtifactStore
+from superduperdb.db.sqlalchemy.metadata import  SQLAlchemyMetadata
+from superduperdb.db.ibis.data_backend import IbisDataBackend
 from superduperdb.db.mongodb.metadata import MongoMetaDataStore
 from superduperdb.vector_search.inmemory import InMemoryVectorDatabase
 from superduperdb.vector_search.lancedb_client import LanceVectorIndex
 
-data_backends = {'mongodb': MongoDataBackend}
+data_backends = {'mongodb': MongoDataBackend, 'ibis': IbisDataBackend}
 
-artifact_stores = {'mongodb': MongoArtifactStore}
+artifact_stores = {'mongodb': MongoArtifactStore, 'filesystem': FileSystemArtifactStore}
 
-metadata_stores = {'mongodb': MongoMetaDataStore}
+metadata_stores = {'mongodb': MongoMetaDataStore, 'sqlalchemy': SQLAlchemyMetadata}
 
 vector_database_stores = {
     config.LanceDB: LanceVectorIndex,
@@ -20,4 +24,5 @@ vector_database_stores = {
 
 connections = {
     'pymongo': MongoClient,
+    'ibis': BaseBackend,
 }

superduperdb/db/ibis/data_backend.py
@@ -1,0 +1,28 @@
+import click
+from ibis.backends.base import BaseBackend
+
+from superduperdb.db.base.data_backend import BaseDataBackend
+from superduperdb.misc.colors import Colors
+
+
+class IbisDataBackend(BaseDataBackend):
+
+    def __init__(self, conn: BaseBackend , name:str):
+        super().__init__(conn=conn, name=name)
+        self._db = self.conn
+
+    @property
+    def db(self):
+        return self._db
+
+    def drop(self, force: bool = False):
+        if not force:
+            if not click.confirm(
+                f'{Colors.RED}[!!!WARNING USE WITH CAUTION AS YOU '
+                f'WILL LOSE ALL DATA!!!]{Colors.RESET} '
+                'Are you sure you want to drop the data-backend? ',
+                default=False,
+            ):
+                print('Aborting...')
+        # TODO: Implement drop functionality
+        return

superduperdb/db/ibis/db.py
@@ -1,0 +1,35 @@
+import typing as t
+
+from superduperdb.db.ibis.cursor import SuperDuperIbisCursor
+from superduperdb.db.base.db import DB
+from superduperdb.container.component import Component
+from superduperdb.container.job import Job
+from superduperdb.db.ibis.query import OutputTable
+from superduperdb import logging
+
+class IbisDB(DB):
+    def _execute(self, query, parent):
+        table = parent
+        for member in query.members:
+            parent = member.execute(
+                self, parent, table=query.collection, ibis_table=table
+            )
+        cursor = SuperDuperIbisCursor(parent, query.collection.primary_id, encoders={})
+        return cursor.execute()
+
+    def execute(self, query):
+        table = query.collection.get_table(self.db)
+        return self._execute(query, table)
+
+    def add(self, 
+        object: Component,
+            dependencies: t.Sequence[Job] = ()):
+        super().add(object, dependencies=dependencies)
+
+        try:
+            table = OutputTable(model=object.identifier)
+            table.create(self.db)
+        except Exception as e:
+            logging.error(e)
+        else:
+            logging.debug(f"Created output table for {object.identifier}")

test/integration/test_ibis.py
@@ -1,0 +1,82 @@
+import ibis
+import sqlalchemy
+from sklearn import svm
+
+from superduperdb import superduper
+from superduperdb import CFG
+from superduperdb.db.base.build import build_vector_database
+from superduperdb.container.document import Document as D
+from superduperdb.db.ibis.db import IbisDB
+from superduperdb.db.filesystem.artifacts import FileSystemArtifactStore
+from superduperdb.db.ibis.data_backend import IbisDataBackend
+from superduperdb.db.sqlalchemy.metadata import SQLAlchemyMetadata
+from superduperdb.db.ibis.query import Table
+
+
+
+
+
+
+connection_sql=sqlalchemy.create_engine('sqlite:///./mydb.sqlite')
+connection = ibis.sqlite.connect("mydb.sqlite")
+
+
+
+# Insert some sample data into the table
+schema = ibis.schema({'id': 'int64', 'health': 'int32', 'age': 'int32'})
+# Create the table
+table_name = 'my_table'
+connection.create_table(table_name, schema=schema)
+
+data_to_insert = [
+    (1, 0, 25),
+    (2, 1, 26),
+    (3, 1, 27),
+    (4, 1, 28),
+    (5, 0, 29),
+]
+connection.insert(table_name, data_to_insert)
+
+
+
+
+# create data layer
+db = IbisDB(
+    databackend=IbisDataBackend(conn=connection, name='ibis'),
+    metadata=SQLAlchemyMetadata(conn=connection_sql, name='ibis'),
+    artifact_store=FileSystemArtifactStore(
+        conn='./.tmp', name='ibis'
+    ),
+    vector_database=build_vector_database(CFG.vector_search.type),
+)
+
+
+# add a sklearn model
+table = Table(name='my_table')
+svc = svm.SVC(gamma='scale', class_weight='balanced', C=100, verbose=True)
+svc.fit(data_to_insert, [0, 1, 1, 1, 0])
+
+model = superduper(
+        svc,
+    postprocess=lambda x: int(x),
+    preprocess=lambda x: list(x.values()
+))
+db.add(model)
+
+
+
+# Do prediction
+m = db.load('model', 'svc')
+m.predict(X='_base', db=db, select=table.filter(table.age > 25), max_chunk_size=3000, overwrite=True)    
+
+
+
+
+#  Query result back
+out_table = connection.table(m.identifier)
+q = table.filter(table.age > 26).outputs(m.identifier, db)
+
+curr = db.execute(q)
+
+for c in curr:
+    print(c)

Pinnacle-Networks/Pinnacle

Former-commit-id: 132b3cb11c21927889bfa99b47e3a594b2eededc

superduperdb/db/base/backends.py
@@ -1,17 +1,21 @@
 from pymongo import MongoClient
+from ibis.backends.base import BaseBackend
 
 from pinnacledb.base import config
 from pinnacledb.db.mongodb.artifacts import MongoArtifactStore
 from pinnacledb.db.mongodb.data_backend import MongoDataBackend
+from pinnacledb.db.filesystem.artifacts import FileSystemArtifactStore
+from pinnacledb.db.sqlalchemy.metadata import  SQLAlchemyMetadata
+from pinnacledb.db.ibis.data_backend import IbisDataBackend
 from pinnacledb.db.mongodb.metadata import MongoMetaDataStore
 from pinnacledb.vector_search.inmemory import InMemoryVectorDatabase
 from pinnacledb.vector_search.lancedb_client import LanceVectorIndex
 
-DATA_BACKENDS = {'mongodb': MongoDataBackend}
+data_backends = {'mongodb': MongoDataBackend, 'ibis': IbisDataBackend}
 
-ARTIFACT_STORES = {'mongodb': MongoArtifactStore}
+artifact_stores = {'mongodb': MongoArtifactStore, 'filesystem': FileSystemArtifactStore}
 
-METADATA_STORES = {'mongodb': MongoMetaDataStore}
+metadata_stores = {'mongodb': MongoMetaDataStore, 'sqlalchemy': SQLAlchemyMetadata}
 
 VECTOR_DATA_STORES = {
     config.LanceDB: LanceVectorIndex,
@@ -20,4 +24,5 @@ VECTOR_DATA_STORES = {
 
 CONNECTIONS = {
     'pymongo': MongoClient,
+    'ibis': BaseBackend,
 }

superduperdb/db/ibis/data_backend.py
@@ -1,0 +1,28 @@
+import click
+from ibis.backends.base import BaseBackend
+
+from pinnacledb.db.base.data_backend import BaseDataBackend
+from pinnacledb.misc.colors import Colors
+
+
+class IbisDataBackend(BaseDataBackend):
+
+    def __init__(self, conn: BaseBackend , name:str):
+        super().__init__(conn=conn, name=name)
+        self._db = self.conn
+
+    @property
+    def db(self):
+        return self._db
+
+    def drop(self, force: bool = False):
+        if not force:
+            if not click.confirm(
+                f'{Colors.RED}[!!!WARNING USE WITH CAUTION AS YOU '
+                f'WILL LOSE ALL DATA!!!]{Colors.RESET} '
+                'Are you sure you want to drop the data-backend? ',
+                default=False,
+            ):
+                print('Aborting...')
+        # TODO: Implement drop functionality
+        return

superduperdb/db/ibis/db.py
@@ -1,0 +1,35 @@
+import typing as t
+
+from pinnacledb.db.ibis.cursor import SuperDuperIbisCursor
+from pinnacledb.db.base.db import DB
+from pinnacledb.container.component import Component
+from pinnacledb.container.job import Job
+from pinnacledb.db.ibis.query import OutputTable
+from pinnacledb import logging
+
+class IbisDB(DB):
+    def _execute(self, query, parent):
+        table = parent
+        for member in query.members:
+            parent = member.execute(
+                self, parent, table=query.collection, ibis_table=table
+            )
+        cursor = SuperDuperIbisCursor(parent, query.collection.primary_id, encoders={})
+        return cursor.execute()
+
+    def execute(self, query):
+        table = query.collection.get_table(self.db)
+        return self._execute(query, table)
+
+    def add(self, 
+        object: Component,
+            dependencies: t.Sequence[Job] = ()):
+        super().add(object, dependencies=dependencies)
+
+        try:
+            table = OutputTable(model=object.identifier)
+            table.create(self.db)
+        except Exception as e:
+            logging.error(e)
+        else:
+            logging.debug(f"Created output table for {object.identifier}")

test/integration/test_ibis.py
@@ -1,0 +1,82 @@
+import ibis
+import sqlalchemy
+from sklearn import svm
+
+from pinnacledb import pinnacle
+from pinnacledb import CFG
+from pinnacledb.db.base.build import build_vector_database
+from pinnacledb.container.document import Document as D
+from pinnacledb.db.ibis.db import IbisDB
+from pinnacledb.db.filesystem.artifacts import FileSystemArtifactStore
+from pinnacledb.db.ibis.data_backend import IbisDataBackend
+from pinnacledb.db.sqlalchemy.metadata import SQLAlchemyMetadata
+from pinnacledb.db.ibis.query import Table
+
+
+
+
+
+
+connection_sql=sqlalchemy.create_engine('sqlite:///./mydb.sqlite')
+connection = ibis.sqlite.connect("mydb.sqlite")
+
+
+
+# Insert some sample data into the table
+schema = ibis.schema({'id': 'int64', 'health': 'int32', 'age': 'int32'})
+# Create the table
+table_name = 'my_table'
+connection.create_table(table_name, schema=schema)
+
+data_to_insert = [
+    (1, 0, 25),
+    (2, 1, 26),
+    (3, 1, 27),
+    (4, 1, 28),
+    (5, 0, 29),
+]
+connection.insert(table_name, data_to_insert)
+
+
+
+
+# create data layer
+db = IbisDB(
+    databackend=IbisDataBackend(conn=connection, name='ibis'),
+    metadata=SQLAlchemyMetadata(conn=connection_sql, name='ibis'),
+    artifact_store=FileSystemArtifactStore(
+        conn='./.tmp', name='ibis'
+    ),
+    vector_database=build_vector_database(CFG.vector_search.type),
+)
+
+
+# add a sklearn model
+table = Table(name='my_table')
+svc = svm.SVC(gamma='scale', class_weight='balanced', C=100, verbose=True)
+svc.fit(data_to_insert, [0, 1, 1, 1, 0])
+
+model = pinnacle(
+        svc,
+    postprocess=lambda x: int(x),
+    preprocess=lambda x: list(x.values()
+))
+db.add(model)
+
+
+
+# Do prediction
+m = db.load('model', 'svc')
+m.predict(X='_base', db=db, select=table.filter(table.age > 25), max_chunk_size=3000, overwrite=True)    
+
+
+
+
+#  Query result back
+out_table = connection.table(m.identifier)
+q = table.filter(table.age > 26).outputs(m.identifier, db)
+
+curr = db.execute(q)
+
+for c in curr:
+    print(c)