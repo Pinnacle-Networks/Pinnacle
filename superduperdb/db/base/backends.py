superduper-io/superduper

Refactor model encoder with types

superduperdb/db/base/backends.py
@@ -1,13 +1,13 @@
-from pymongo import MongoClient
 from ibis.backends.base import BaseBackend
+from pymongo import MongoClient
 
 from superduperdb.base import config
+from superduperdb.db.filesystem.artifacts import FileSystemArtifactStore
+from superduperdb.db.ibis.data_backend import IbisDataBackend
 from superduperdb.db.mongodb.artifacts import MongoArtifactStore
 from superduperdb.db.mongodb.data_backend import MongoDataBackend
-from superduperdb.db.filesystem.artifacts import FileSystemArtifactStore
-from superduperdb.db.sqlalchemy.metadata import  SQLAlchemyMetadata
-from superduperdb.db.ibis.data_backend import IbisDataBackend
 from superduperdb.db.mongodb.metadata import MongoMetaDataStore
+from superduperdb.db.sqlalchemy.metadata import SQLAlchemyMetadata
 from superduperdb.vector_search.inmemory import InMemoryVectorDatabase
 from superduperdb.vector_search.lancedb_client import LanceVectorIndex
 

superduperdb/db/ibis/query.py
@@ -3,18 +3,19 @@ import enum
 import typing as t
 
 import ibis
+from ibis.expr.types.relations import Table as IbisTable
 
 from superduperdb import CFG
-from ibis.expr.types.relations import Table as IbisTable
-from superduperdb.container.encoder import Encoder
 from superduperdb.container.component import Component
 from superduperdb.container.document import Document
-from superduperdb.db.ibis.schema import IbisSchema
+from superduperdb.container.encoder import Encoder
+from superduperdb.container.serializable import Serializable
 from superduperdb.db.base.db import DB
 from superduperdb.db.ibis.cursor import SuperDuperIbisCursor
-from superduperdb.container.serializable import Serializable
+from superduperdb.db.ibis.schema import IbisSchema
 
 PRIMARY_ID: str = 'id'
+
 
 class QueryType(enum.Enum):
     QUERY = 'query'
@@ -46,7 +47,7 @@ class Query:
         if self.type == QueryType.ATTR.value:
             return getattr(parent, self.query.name)
 
-        self.query.pre(db, table=table, kwargs = self.kwargs)
+        self.query.pre(db, table=table, kwargs=self.kwargs)
         if len(self.args) == 1 and isinstance(self.args[0], QueryLinker):
             self.args = [self.args[0].execute(db, parent, ibis_table)]
 
@@ -105,11 +106,12 @@ class QueryChain:
     def __repr__(self):
         return str(self.chain)
 
+
 @dc.dataclass
 class OutputTable:
     model: str
     primary_id: str = 'id'
-    table: t.Any = None 
+    table: t.Any = None
     output_type: t.Any = None
 
     def create(self, conn):
@@ -126,13 +128,14 @@ class OutputTable:
             output_type = self.output_type
 
         schema = {
-                'id': 'int32',
-                'input_id': 'int32',
-                'query_id': 'string',
-                'output': output_type,
-                'key': 'string',
-                }
+            'id': 'int32',
+            'input_id': 'int32',
+            'query_id': 'string',
+            'output': output_type,
+            'key': 'string',
+        }
         return ibis.schema(schema)
+
 
 @dc.dataclass
 class InMemoryTable(Component):
@@ -205,7 +208,7 @@ class Table(Component):
                     mutated_args.append(attr)
             else:
                 mutated_args.append(attr)
-                
+
         return mutated_args
 
     def __getattr__(self, k):
@@ -244,7 +247,7 @@ class Table(Component):
         }
         kwargs.update({'table_name': self.identifier})
         args = args[1:]
-        
+
         insert = Query(
             'insert',
             type=QueryType.QUERY.value,
@@ -317,7 +320,6 @@ class QueryLinker(Serializable, LogicalExprMixin):
 
         return QueryLinker(self.collection, query_type='filter', members=members)
 
-
     def build(self, db):
         table = parent = self.collection.table
         for member in self.members:
@@ -327,17 +329,26 @@ class QueryLinker(Serializable, LogicalExprMixin):
     def outputs(self, model, db):
         curr_query = self.build(db)
         model_table = db.db.table(model)
-        query = curr_query.join(model_table, [model_table.id == curr_query.id, model_table.query_id == self.collection.identifier])
-        cursor = SuperDuperIbisCursor(query, self.collection.primary_id, encoders=db.encoders)
+        query = curr_query.join(
+            model_table,
+            [
+                model_table.id == curr_query.id,
+                model_table.query_id == self.collection.identifier,
+            ],
+        )
+        cursor = SuperDuperIbisCursor(
+            query, self.collection.primary_id, encoders=db.encoders
+        )
         return cursor
 
-    def model_update(self, 
+    def model_update(
+        self,
         db,
         ids: t.Sequence[t.Any],
         key: str,
         model: str,
         outputs: t.Sequence[t.Any],
-                     ):
+    ):
         if key.startswith('_outputs'):
             key = key.split('.')[1]
         if not outputs:
@@ -347,10 +358,17 @@ class QueryLinker(Serializable, LogicalExprMixin):
 
         table_record = []
         for ix in range(len(outputs)):
-            d = Document({'id': int(ids[ix]), 'input_id': int(ids[ix]), 'output': outputs[ix], 'query_id': input_table, 'key': key})
+            d = Document(
+                {
+                    'id': int(ids[ix]),
+                    'input_id': int(ids[ix]),
+                    'output': outputs[ix],
+                    'query_id': input_table,
+                    'key': key,
+                }
+            )
             table_record.append(d)
         db.execute(Table(model, schema=None).insert(table_record))
-
 
     def __call__(self, *args, **kwargs):
         args = self.collection.mutate_args(args)
@@ -454,7 +472,6 @@ class Insert:
     namespace: str = 'ibis'
 
     def pre(self, db, table=None, **kwargs):
-
         # TODO: handle adding table later
         '''
         if self.base_table.identifier not in db.tables:
@@ -477,10 +494,10 @@ class Insert:
         '''
 
         # mutate kwargs with documents
-        documents = [tuple(d.values())for d in documents]
+        documents = [tuple(d.values()) for d in documents]
         kwargs['kwargs']['obj'] = documents
-        return 
-    
+        return
+
     def post(self, db, output, *args, **kwargs):
         graph = None
         # inserted_ids = [d[PRIMARY_ID] for d in kwargs['kwargs']['obj']]

superduperdb/db/ibis/schema.py
@@ -3,8 +3,9 @@ import typing as t
 
 import ibis
 
-from superduperdb.container.schema import Schema
 from superduperdb.container.encoder import Encoder
+from superduperdb.container.schema import Schema
+
 
 @dc.dataclass
 class IbisSchema(Schema):
@@ -18,13 +19,17 @@ class IbisSchema(Schema):
     def map(self):
         if self.trivial:
             return
-        assert not any(['_encodable' in k for k in self.fields.keys()]), 'Reserved substring: _encodable'
+        assert not any(
+            ['_encodable' in k for k in self.fields.keys()]
+        ), 'Reserved substring: _encodable'
 
         mapped_schema = {}
         for k, v in self.fields.items():
             if isinstance(v, Encoder):
                 self.encoded_types.append(k)
-                mapped_schema[f'{k}::_encodable={v.identifier}/{v.version}::'] = 'binary'
+                mapped_schema[
+                    f'{k}::_encodable={v.identifier}/{v.version}::'
+                ] = 'binary'
             else:
                 mapped_schema[k] = v
         return ibis.schema(mapped_schema)

test/integration/test_ibis.py
@@ -1,60 +1,64 @@
 import os
+
 import ibis
-import PIL.Image
 import pandas as pd
+import PIL.Image
+import pytest
 import torchvision
 
-import pytest
-
 from superduperdb import CFG
+from superduperdb.container.document import Document as D
 from superduperdb.db import types
 from superduperdb.db.base.build import build_vector_database
-from superduperdb.container.document import Document as D
-from superduperdb.db.ibis.db import IbisDB
 from superduperdb.db.filesystem.artifacts import FileSystemArtifactStore
 from superduperdb.db.ibis.data_backend import IbisDataBackend
+from superduperdb.db.ibis.db import IbisDB
+from superduperdb.db.ibis.query import Table
 from superduperdb.db.ibis.schema import IbisSchema
 from superduperdb.db.sqlalchemy.metadata import SQLAlchemyMetadata
-from superduperdb.db.ibis.query import Table
 from superduperdb.ext.pillow.image import pil_image
 from superduperdb.ext.torch.model import TorchModel
 
+
 @pytest.fixture(scope='session')
-def ibis_db():
+def sqllite_conn():
     connection = ibis.sqlite.connect("mydb.sqlite")
+    yield connection
+    os.remove("mydb.sqlite")
+
+
+@pytest.fixture(scope='session')
+def ibis_db(sqllite_conn):
+    connection = sqllite_conn
 
     # Create data layer
     db = IbisDB(
         databackend=IbisDataBackend(conn=connection, name='ibis'),
         metadata=SQLAlchemyMetadata(conn=connection.con, name='ibis'),
-        artifact_store=FileSystemArtifactStore(
-            conn='./.tmp', name='ibis'
-        ),
+        artifact_store=FileSystemArtifactStore(conn='./.tmp', name='ibis'),
         vector_database=build_vector_database(CFG.vector_search.type),
     )
     yield db
-    os.remove('mydb.sqlite')
+
 
 @pytest.fixture(scope='session')
-def ibis_pandas_db():
-    connection = ibis.sqlite.connect("mydb.sqlite")
+def ibis_pandas_db(sqllite_conn):
+    connection = sqllite_conn
     df = pd.DataFrame(
-    [[1, 0, 25, 'kk'], [2, 1, 26, 'kk'], [3, 0, 27, 'kk'], [4, 1, 28, 'kk']],
-    columns=['id', 'health', 'age', 'name'],
-)
+        [[1, 0, 25, 'kk'], [2, 1, 26, 'kk'], [3, 0, 27, 'kk'], [4, 1, 28, 'kk']],
+        columns=['id', 'health', 'age', 'name'],
+    )
     t = ibis.memtable(df, name="pandas")
 
     # Create data layer
     db = IbisDB(
         databackend=IbisDataBackend(conn=t, name='ibis'),
         metadata=SQLAlchemyMetadata(conn=connection.con, name='ibis'),
-        artifact_store=FileSystemArtifactStore(
-            conn='./.tmp', name='ibis'
-        ),
+        artifact_store=FileSystemArtifactStore(conn='./.tmp', name='ibis'),
         vector_database=build_vector_database(CFG.vector_search.type),
     )
     yield db
-    os.remove('mydb.sqlite')
+
 
 def test_end2end_sql(ibis_db):
     db = ibis_db
@@ -65,7 +69,7 @@ def test_end2end_sql(ibis_db):
             'health': 'int32',
             'age': 'int32',
             'image': pil_image,
-        }
+        },
     )
     im = PIL.Image.open('test/material/data/test-image.jpeg')
     data_to_insert = [
@@ -79,10 +83,20 @@ def test_end2end_sql(ibis_db):
 
     db.add(t)
     db.execute(
-        t.insert([D({'id': d['id'], 'health': d['health'], 'age': d['age'], 'image': d['image']}) for d in data_to_insert])
+        t.insert(
+            [
+                D(
+                    {
+                        'id': d['id'],
+                        'health': d['health'],
+                        'age': d['age'],
+                        'image': d['image'],
+                    }
+                )
+                for d in data_to_insert
+            ]
+        )
     )
-
-
 
     # -------------- retrieve data  from table ----------------
     q = t.select("image", "age", "health")
@@ -93,45 +107,66 @@ def test_end2end_sql(ibis_db):
         assert isinstance(img['age'], int)
         assert isinstance(img['health'], int)
 
-
     # preprocessing function
-    preprocess = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(256),
-        torchvision.transforms.CenterCrop(224),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )])
+    preprocess = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.Resize(256),
+            torchvision.transforms.CenterCrop(224),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),
+        ]
+    )
 
     def postprocess(x):
         return int(x.topk(1)[1].item())
 
     # create a torchvision model
     resnet = TorchModel(
-            identifier='resnet18',
+        identifier='resnet18',
         preprocess=preprocess,
         postprocess=postprocess,
-
         object=torchvision.models.resnet18(pretrained=False),
-        output_type=types.int32,
+        encoder=types.int32,
     )
 
     # Apply the torchvision model
-    resnet.predict(X='image', db=db, select=t.select('id', 'image'), max_chunk_size=3000, overwrite=True)
-
+    resnet.predict(
+        X='image',
+        db=db,
+        select=t.select('id', 'image'),
+        max_chunk_size=3000,
+        overwrite=True,
+    )
 
     # Query the results back
     q = t.select('id', 'image', 'age').filter(t.age > 25).outputs('resnet18', db)
     curr = db.execute(q)
 
     for c in curr:
-        assert all([ k in ['id', 'health', 'age', 'image', 'output', 'query_id', 'key', 'input_id'] for k in c.unpack().keys()])
+        assert all(
+            [
+                k
+                in [
+                    'id',
+                    'health',
+                    'age',
+                    'image',
+                    'output',
+                    'query_id',
+                    'key',
+                    'input_id',
+                ]
+                for k in c.unpack().keys()
+            ]
+        )
+
 
 def test_end2end_pandas(ibis_pandas_db):
     db = ibis_pandas_db
     # -------------- retrieve data  from table ----------------
-    q = db.table.select("id", "age", "health").filter(db.table.age> 25)
+    q = db.table.select("id", "age", "health").filter(db.table.age > 25)
     imgs = db.execute(q)
     for img in imgs:
         img = img.unpack()

Pinnacle-Networks/Pinnacle

Former-commit-id: aba393a16735462520335edd5a663eb2dc1624b3

superduperdb/db/base/backends.py
@@ -1,13 +1,13 @@
-from pymongo import MongoClient
 from ibis.backends.base import BaseBackend
+from pymongo import MongoClient
 
 from pinnacledb.base import config
+from pinnacledb.db.filesystem.artifacts import FileSystemArtifactStore
+from pinnacledb.db.ibis.data_backend import IbisDataBackend
 from pinnacledb.db.mongodb.artifacts import MongoArtifactStore
 from pinnacledb.db.mongodb.data_backend import MongoDataBackend
-from pinnacledb.db.filesystem.artifacts import FileSystemArtifactStore
-from pinnacledb.db.sqlalchemy.metadata import  SQLAlchemyMetadata
-from pinnacledb.db.ibis.data_backend import IbisDataBackend
 from pinnacledb.db.mongodb.metadata import MongoMetaDataStore
+from pinnacledb.db.sqlalchemy.metadata import SQLAlchemyMetadata
 from pinnacledb.vector_search.inmemory import InMemoryVectorDatabase
 from pinnacledb.vector_search.lancedb_client import LanceVectorIndex
 

superduperdb/db/ibis/query.py
@@ -3,18 +3,19 @@ import enum
 import typing as t
 
 import ibis
+from ibis.expr.types.relations import Table as IbisTable
 
 from pinnacledb import CFG
-from ibis.expr.types.relations import Table as IbisTable
-from pinnacledb.container.encoder import Encoder
 from pinnacledb.container.component import Component
 from pinnacledb.container.document import Document
-from pinnacledb.db.ibis.schema import IbisSchema
+from pinnacledb.container.encoder import Encoder
+from pinnacledb.container.serializable import Serializable
 from pinnacledb.db.base.db import DB
 from pinnacledb.db.ibis.cursor import SuperDuperIbisCursor
-from pinnacledb.container.serializable import Serializable
+from pinnacledb.db.ibis.schema import IbisSchema
 
 PRIMARY_ID: str = 'id'
+
 
 class QueryType(enum.Enum):
     QUERY = 'query'
@@ -46,7 +47,7 @@ class Query:
         if self.type == QueryType.ATTR.value:
             return getattr(parent, self.query.name)
 
-        self.query.pre(db, table=table, kwargs = self.kwargs)
+        self.query.pre(db, table=table, kwargs=self.kwargs)
         if len(self.args) == 1 and isinstance(self.args[0], QueryLinker):
             self.args = [self.args[0].execute(db, parent, ibis_table)]
 
@@ -105,11 +106,12 @@ class QueryChain:
     def __repr__(self):
         return str(self.chain)
 
+
 @dc.dataclass
 class OutputTable:
     model: str
     primary_id: str = 'id'
-    table: t.Any = None 
+    table: t.Any = None
     output_type: t.Any = None
 
     def create(self, conn):
@@ -126,13 +128,14 @@ class OutputTable:
             output_type = self.output_type
 
         schema = {
-                'id': 'int32',
-                'input_id': 'int32',
-                'query_id': 'string',
-                'output': output_type,
-                'key': 'string',
-                }
+            'id': 'int32',
+            'input_id': 'int32',
+            'query_id': 'string',
+            'output': output_type,
+            'key': 'string',
+        }
         return ibis.schema(schema)
+
 
 @dc.dataclass
 class InMemoryTable(Component):
@@ -205,7 +208,7 @@ class Table(Component):
                     mutated_args.append(attr)
             else:
                 mutated_args.append(attr)
-                
+
         return mutated_args
 
     def __getattr__(self, k):
@@ -244,7 +247,7 @@ class Table(Component):
         }
         kwargs.update({'table_name': self.identifier})
         args = args[1:]
-        
+
         insert = Query(
             'insert',
             type=QueryType.QUERY.value,
@@ -317,7 +320,6 @@ class QueryLinker(Serializable, LogicalExprMixin):
 
         return QueryLinker(self.collection, query_type='filter', members=members)
 
-
     def build(self, db):
         table = parent = self.collection.table
         for member in self.members:
@@ -327,17 +329,26 @@ class QueryLinker(Serializable, LogicalExprMixin):
     def outputs(self, model, db):
         curr_query = self.build(db)
         model_table = db.db.table(model)
-        query = curr_query.join(model_table, [model_table.id == curr_query.id, model_table.query_id == self.collection.identifier])
-        cursor = SuperDuperIbisCursor(query, self.collection.primary_id, encoders=db.encoders)
+        query = curr_query.join(
+            model_table,
+            [
+                model_table.id == curr_query.id,
+                model_table.query_id == self.collection.identifier,
+            ],
+        )
+        cursor = SuperDuperIbisCursor(
+            query, self.collection.primary_id, encoders=db.encoders
+        )
         return cursor
 
-    def model_update(self, 
+    def model_update(
+        self,
         db,
         ids: t.Sequence[t.Any],
         key: str,
         model: str,
         outputs: t.Sequence[t.Any],
-                     ):
+    ):
         if key.startswith('_outputs'):
             key = key.split('.')[1]
         if not outputs:
@@ -347,10 +358,17 @@ class QueryLinker(Serializable, LogicalExprMixin):
 
         table_record = []
         for ix in range(len(outputs)):
-            d = Document({'id': int(ids[ix]), 'input_id': int(ids[ix]), 'output': outputs[ix], 'query_id': input_table, 'key': key})
+            d = Document(
+                {
+                    'id': int(ids[ix]),
+                    'input_id': int(ids[ix]),
+                    'output': outputs[ix],
+                    'query_id': input_table,
+                    'key': key,
+                }
+            )
             table_record.append(d)
         db.execute(Table(model, schema=None).insert(table_record))
-
 
     def __call__(self, *args, **kwargs):
         args = self.collection.mutate_args(args)
@@ -454,7 +472,6 @@ class Insert:
     namespace: str = 'ibis'
 
     def pre(self, db, table=None, **kwargs):
-
         # TODO: handle adding table later
         '''
         if self.base_table.identifier not in db.tables:
@@ -477,10 +494,10 @@ class Insert:
         '''
 
         # mutate kwargs with documents
-        documents = [tuple(d.values())for d in documents]
+        documents = [tuple(d.values()) for d in documents]
         kwargs['kwargs']['obj'] = documents
-        return 
-    
+        return
+
     def post(self, db, output, *args, **kwargs):
         graph = None
         # inserted_ids = [d[PRIMARY_ID] for d in kwargs['kwargs']['obj']]

superduperdb/db/ibis/schema.py
@@ -3,8 +3,9 @@ import typing as t
 
 import ibis
 
-from pinnacledb.container.schema import Schema
 from pinnacledb.container.encoder import Encoder
+from pinnacledb.container.schema import Schema
+
 
 @dc.dataclass
 class IbisSchema(Schema):
@@ -18,13 +19,17 @@ class IbisSchema(Schema):
     def map(self):
         if self.trivial:
             return
-        assert not any(['_encodable' in k for k in self.fields.keys()]), 'Reserved substring: _encodable'
+        assert not any(
+            ['_encodable' in k for k in self.fields.keys()]
+        ), 'Reserved substring: _encodable'
 
         mapped_schema = {}
         for k, v in self.fields.items():
             if isinstance(v, Encoder):
                 self.encoded_types.append(k)
-                mapped_schema[f'{k}::_encodable={v.identifier}/{v.version}::'] = 'binary'
+                mapped_schema[
+                    f'{k}::_encodable={v.identifier}/{v.version}::'
+                ] = 'binary'
             else:
                 mapped_schema[k] = v
         return ibis.schema(mapped_schema)

test/integration/test_ibis.py
@@ -1,60 +1,64 @@
 import os
+
 import ibis
-import PIL.Image
 import pandas as pd
+import PIL.Image
+import pytest
 import torchvision
 
-import pytest
-
 from pinnacledb import CFG
+from pinnacledb.container.document import Document as D
 from pinnacledb.db import types
 from pinnacledb.db.base.build import build_vector_database
-from pinnacledb.container.document import Document as D
-from pinnacledb.db.ibis.db import IbisDB
 from pinnacledb.db.filesystem.artifacts import FileSystemArtifactStore
 from pinnacledb.db.ibis.data_backend import IbisDataBackend
+from pinnacledb.db.ibis.db import IbisDB
+from pinnacledb.db.ibis.query import Table
 from pinnacledb.db.ibis.schema import IbisSchema
 from pinnacledb.db.sqlalchemy.metadata import SQLAlchemyMetadata
-from pinnacledb.db.ibis.query import Table
 from pinnacledb.ext.pillow.image import pil_image
 from pinnacledb.ext.torch.model import TorchModel
 
+
 @pytest.fixture(scope='session')
-def ibis_db():
+def sqllite_conn():
     connection = ibis.sqlite.connect("mydb.sqlite")
+    yield connection
+    os.remove("mydb.sqlite")
+
+
+@pytest.fixture(scope='session')
+def ibis_db(sqllite_conn):
+    connection = sqllite_conn
 
     # Create data layer
     db = IbisDB(
         databackend=IbisDataBackend(conn=connection, name='ibis'),
         metadata=SQLAlchemyMetadata(conn=connection.con, name='ibis'),
-        artifact_store=FileSystemArtifactStore(
-            conn='./.tmp', name='ibis'
-        ),
+        artifact_store=FileSystemArtifactStore(conn='./.tmp', name='ibis'),
         vector_database=build_vector_database(CFG.vector_search.type),
     )
     yield db
-    os.remove('mydb.sqlite')
+
 
 @pytest.fixture(scope='session')
-def ibis_pandas_db():
-    connection = ibis.sqlite.connect("mydb.sqlite")
+def ibis_pandas_db(sqllite_conn):
+    connection = sqllite_conn
     df = pd.DataFrame(
-    [[1, 0, 25, 'kk'], [2, 1, 26, 'kk'], [3, 0, 27, 'kk'], [4, 1, 28, 'kk']],
-    columns=['id', 'health', 'age', 'name'],
-)
+        [[1, 0, 25, 'kk'], [2, 1, 26, 'kk'], [3, 0, 27, 'kk'], [4, 1, 28, 'kk']],
+        columns=['id', 'health', 'age', 'name'],
+    )
     t = ibis.memtable(df, name="pandas")
 
     # Create data layer
     db = IbisDB(
         databackend=IbisDataBackend(conn=t, name='ibis'),
         metadata=SQLAlchemyMetadata(conn=connection.con, name='ibis'),
-        artifact_store=FileSystemArtifactStore(
-            conn='./.tmp', name='ibis'
-        ),
+        artifact_store=FileSystemArtifactStore(conn='./.tmp', name='ibis'),
         vector_database=build_vector_database(CFG.vector_search.type),
     )
     yield db
-    os.remove('mydb.sqlite')
+
 
 def test_end2end_sql(ibis_db):
     db = ibis_db
@@ -65,7 +69,7 @@ def test_end2end_sql(ibis_db):
             'health': 'int32',
             'age': 'int32',
             'image': pil_image,
-        }
+        },
     )
     im = PIL.Image.open('test/material/data/test-image.jpeg')
     data_to_insert = [
@@ -79,10 +83,20 @@ def test_end2end_sql(ibis_db):
 
     db.add(t)
     db.execute(
-        t.insert([D({'id': d['id'], 'health': d['health'], 'age': d['age'], 'image': d['image']}) for d in data_to_insert])
+        t.insert(
+            [
+                D(
+                    {
+                        'id': d['id'],
+                        'health': d['health'],
+                        'age': d['age'],
+                        'image': d['image'],
+                    }
+                )
+                for d in data_to_insert
+            ]
+        )
     )
-
-
 
     # -------------- retrieve data  from table ----------------
     q = t.select("image", "age", "health")
@@ -93,45 +107,66 @@ def test_end2end_sql(ibis_db):
         assert isinstance(img['age'], int)
         assert isinstance(img['health'], int)
 
-
     # preprocessing function
-    preprocess = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(256),
-        torchvision.transforms.CenterCrop(224),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )])
+    preprocess = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.Resize(256),
+            torchvision.transforms.CenterCrop(224),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),
+        ]
+    )
 
     def postprocess(x):
         return int(x.topk(1)[1].item())
 
     # create a torchvision model
     resnet = TorchModel(
-            identifier='resnet18',
+        identifier='resnet18',
         preprocess=preprocess,
         postprocess=postprocess,
-
         object=torchvision.models.resnet18(pretrained=False),
-        output_type=types.int32,
+        encoder=types.int32,
     )
 
     # Apply the torchvision model
-    resnet.predict(X='image', db=db, select=t.select('id', 'image'), max_chunk_size=3000, overwrite=True)
-
+    resnet.predict(
+        X='image',
+        db=db,
+        select=t.select('id', 'image'),
+        max_chunk_size=3000,
+        overwrite=True,
+    )
 
     # Query the results back
     q = t.select('id', 'image', 'age').filter(t.age > 25).outputs('resnet18', db)
     curr = db.execute(q)
 
     for c in curr:
-        assert all([ k in ['id', 'health', 'age', 'image', 'output', 'query_id', 'key', 'input_id'] for k in c.unpack().keys()])
+        assert all(
+            [
+                k
+                in [
+                    'id',
+                    'health',
+                    'age',
+                    'image',
+                    'output',
+                    'query_id',
+                    'key',
+                    'input_id',
+                ]
+                for k in c.unpack().keys()
+            ]
+        )
+
 
 def test_end2end_pandas(ibis_pandas_db):
     db = ibis_pandas_db
     # -------------- retrieve data  from table ----------------
-    q = db.table.select("id", "age", "health").filter(db.table.age> 25)
+    q = db.table.select("id", "age", "health").filter(db.table.age > 25)
     imgs = db.execute(q)
     for img in imgs:
         img = img.unpack()