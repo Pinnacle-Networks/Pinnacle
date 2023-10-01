superduper-io/superduper

Add OpenAI image endpoint

test/integration/ext/openai/model.py
@@ -10,7 +10,11 @@ from superduperdb.ext.openai.model import (
     OpenAIAudioTranslation,
     OpenAIChatCompletion,
     OpenAIEmbedding,
+    OpenAIImageCreation,
+    OpenAIImageEdit,
 )
+
+PNG_BYTE_SIGNATURE = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR'
 
 CASSETTE_DIR = 'test/integration/ext/openai/cassettes'
 
@@ -18,6 +22,24 @@ if os.getenv('OPENAI_API_KEY') is None:
     mp = pytest.MonkeyPatch()
     mp.setattr(openai, 'api_key', 'sk-TopSecret')
     mp.setenv('OPENAI_API_KEY', 'sk-TopSecret')
+
+
+def _record_only_png_signature_in_response(response):
+    '''
+    VCR filter function to only record the PNG signature in the response.
+
+    This is necessary because the response is a PNG which can be quite large.
+    '''
+    if PNG_BYTE_SIGNATURE in response['body']['string']:
+        response['body']['string'] = PNG_BYTE_SIGNATURE
+
+    if b'b64_json' in response['body']['string']:
+        # "b64_json": base64.b64encode(PNG_BYTE_SIGNATURE)
+        response['body']['string'] = (
+            b'{\n  \"created\": 1695982982,\n  \"data\": '
+            b'[\n    {\n      \"b64_json\": \"iVBORw0KGgoAAAANSUhEUg==\" } ]}'
+        )
+    return response
 
 
 @vcr.use_cassette(
@@ -118,6 +140,197 @@ async def test_batch_chat_async():
 
     assert isinstance(resp, list)
     assert isinstance(resp[0], str)
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_create_url.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+def test_create_url():
+    e = OpenAIImageCreation(
+        model='dall-e', prompt='a close up, studio photographic portrait of a {context}'
+    )
+    resp = e.predict('', one=True, response_format='url', context=['cat'])
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_create_url_batch.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+def test_create_url_batch():
+    e = OpenAIImageCreation(
+        model='dall-e', prompt='a close up, studio photographic portrait of a'
+    )
+    resp = e.predict(['cat', 'dog'], response_format='url')
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_create_async.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_create_async():
+    e = OpenAIImageCreation(
+        model='dall-e', prompt='a close up, studio photographic portrait of a {context}'
+    )
+    resp = await e.apredict('', one=True, context=['cat'])
+
+    assert isinstance(resp, bytes)
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_create_url_async.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_create_url_async():
+    e = OpenAIImageCreation(
+        model='dall-e', prompt='a close up, studio photographic portrait of a {context}'
+    )
+    resp = await e.apredict('', one=True, response_format='url', context=['cat'])
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_create_url_async_batch.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_create_url_async_batch():
+    e = OpenAIImageCreation(
+        model='dall-e', prompt='a close up, studio photographic portrait of a'
+    )
+    resp = await e.apredict(['cat', 'dog'], response_format='url')
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_edit_url.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+def test_edit_url():
+    e = OpenAIImageEdit(
+        model='dall-e', prompt='A celebration party at the launch of {context}'
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    resp = e.predict(buffer, one=True, response_format='url', context=['superduperdb'])
+    buffer.close()
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_edit_url_batch.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+def test_edit_url_batch():
+    e = OpenAIImageEdit(
+        model='dall-e', prompt='A celebration party at the launch of superduperdb'
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_one = io.BytesIO(f.read())
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_two = io.BytesIO(f.read())
+
+    resp = e.predict([buffer_one, buffer_two], response_format='url')
+
+    buffer_one.close()
+    buffer_two.close()
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_edit_async.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_edit_async():
+    e = OpenAIImageEdit(
+        model='dall-e', prompt='A celebration party at the launch of {context}'
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    resp = await e.apredict(buffer, one=True, context=['superduperdb'])
+    buffer.close()
+
+    assert isinstance(resp, bytes)
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_edit_url_async.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_edit_url_async():
+    e = OpenAIImageEdit(
+        model='dall-e', prompt='A celebration party at the launch of {context}'
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    resp = await e.apredict(
+        buffer, one=True, response_format='url', context=['superduperdb']
+    )
+    buffer.close()
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_edit_url_async_batch.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_edit_url_async_batch():
+    e = OpenAIImageEdit(
+        model='dall-e', prompt='A celebration party at the launch of superduperdb'
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_one = io.BytesIO(f.read())
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_two = io.BytesIO(f.read())
+
+    resp = await e.apredict([buffer_one, buffer_two], response_format='url')
+
+    buffer_one.close()
+    buffer_two.close()
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
 
 
 @vcr.use_cassette(

Pinnacle-Networks/Pinnacle

Former-commit-id: c2d3eea964f438171a0ef9b7c8cf903610e11dff

test/integration/ext/openai/model.py
@@ -10,7 +10,11 @@ from pinnacledb.ext.openai.model import (
     OpenAIAudioTranslation,
     OpenAIChatCompletion,
     OpenAIEmbedding,
+    OpenAIImageCreation,
+    OpenAIImageEdit,
 )
+
+PNG_BYTE_SIGNATURE = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR'
 
 CASSETTE_DIR = 'test/integration/ext/openai/cassettes'
 
@@ -18,6 +22,24 @@ if os.getenv('OPENAI_API_KEY') is None:
     mp = pytest.MonkeyPatch()
     mp.setattr(openai, 'api_key', 'sk-TopSecret')
     mp.setenv('OPENAI_API_KEY', 'sk-TopSecret')
+
+
+def _record_only_png_signature_in_response(response):
+    '''
+    VCR filter function to only record the PNG signature in the response.
+
+    This is necessary because the response is a PNG which can be quite large.
+    '''
+    if PNG_BYTE_SIGNATURE in response['body']['string']:
+        response['body']['string'] = PNG_BYTE_SIGNATURE
+
+    if b'b64_json' in response['body']['string']:
+        # "b64_json": base64.b64encode(PNG_BYTE_SIGNATURE)
+        response['body']['string'] = (
+            b'{\n  \"created\": 1695982982,\n  \"data\": '
+            b'[\n    {\n      \"b64_json\": \"iVBORw0KGgoAAAANSUhEUg==\" } ]}'
+        )
+    return response
 
 
 @vcr.use_cassette(
@@ -118,6 +140,197 @@ async def test_batch_chat_async():
 
     assert isinstance(resp, list)
     assert isinstance(resp[0], str)
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_create_url.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+def test_create_url():
+    e = OpenAIImageCreation(
+        model='dall-e', prompt='a close up, studio photographic portrait of a {context}'
+    )
+    resp = e.predict('', one=True, response_format='url', context=['cat'])
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_create_url_batch.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+def test_create_url_batch():
+    e = OpenAIImageCreation(
+        model='dall-e', prompt='a close up, studio photographic portrait of a'
+    )
+    resp = e.predict(['cat', 'dog'], response_format='url')
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_create_async.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_create_async():
+    e = OpenAIImageCreation(
+        model='dall-e', prompt='a close up, studio photographic portrait of a {context}'
+    )
+    resp = await e.apredict('', one=True, context=['cat'])
+
+    assert isinstance(resp, bytes)
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_create_url_async.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_create_url_async():
+    e = OpenAIImageCreation(
+        model='dall-e', prompt='a close up, studio photographic portrait of a {context}'
+    )
+    resp = await e.apredict('', one=True, response_format='url', context=['cat'])
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_create_url_async_batch.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_create_url_async_batch():
+    e = OpenAIImageCreation(
+        model='dall-e', prompt='a close up, studio photographic portrait of a'
+    )
+    resp = await e.apredict(['cat', 'dog'], response_format='url')
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_edit_url.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+def test_edit_url():
+    e = OpenAIImageEdit(
+        model='dall-e', prompt='A celebration party at the launch of {context}'
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    resp = e.predict(buffer, one=True, response_format='url', context=['pinnacledb'])
+    buffer.close()
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_edit_url_batch.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+def test_edit_url_batch():
+    e = OpenAIImageEdit(
+        model='dall-e', prompt='A celebration party at the launch of pinnacledb'
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_one = io.BytesIO(f.read())
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_two = io.BytesIO(f.read())
+
+    resp = e.predict([buffer_one, buffer_two], response_format='url')
+
+    buffer_one.close()
+    buffer_two.close()
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_edit_async.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_edit_async():
+    e = OpenAIImageEdit(
+        model='dall-e', prompt='A celebration party at the launch of {context}'
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    resp = await e.apredict(buffer, one=True, context=['pinnacledb'])
+    buffer.close()
+
+    assert isinstance(resp, bytes)
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_edit_url_async.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_edit_url_async():
+    e = OpenAIImageEdit(
+        model='dall-e', prompt='A celebration party at the launch of {context}'
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    resp = await e.apredict(
+        buffer, one=True, response_format='url', context=['pinnacledb']
+    )
+    buffer.close()
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_edit_url_async_batch.yaml',
+    filter_headers=['authorization'],
+    before_record_response=_record_only_png_signature_in_response,
+)
+@pytest.mark.asyncio
+async def test_edit_url_async_batch():
+    e = OpenAIImageEdit(
+        model='dall-e', prompt='A celebration party at the launch of pinnacledb'
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_one = io.BytesIO(f.read())
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_two = io.BytesIO(f.read())
+
+    resp = await e.apredict([buffer_one, buffer_two], response_format='url')
+
+    buffer_one.close()
+    buffer_two.close()
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
 
 
 @vcr.use_cassette(