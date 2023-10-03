superduper-io/superduper

Add model_update_kwargs for non embedded outputs

superduperdb/ext/openai/model.py
@@ -14,7 +14,7 @@ from openai.error import RateLimitError, ServiceUnavailableError, Timeout, TryAg
 import superduperdb as s
 from superduperdb.container.component import Component
 from superduperdb.container.encoder import Encoder
-from superduperdb.container.model import PredictMixin
+from superduperdb.container.model import DefaultModelUpdateKwargs, PredictMixin
 from superduperdb.ext.vector.encoder import vector
 from superduperdb.misc.compat import cache
 from superduperdb.misc.retry import Retry
@@ -50,6 +50,7 @@ class OpenAI(Component, PredictMixin):
     version: t.Optional[int] = None
     takes_context: bool = False
     encoder: t.Union[Encoder, str, None] = None
+    model_update_kwargs: dict = dc.field(default_factory=DefaultModelUpdateKwargs)
 
     #: A unique name for the class
     type_id: t.ClassVar[str] = 'model'

test/unittest/model/test_vanilla.py
@@ -1,4 +1,20 @@
+import pytest
+
+from superduperdb.container.document import Document
 from superduperdb.container.model import Model
+from superduperdb.db.mongodb.query import Collection
+
+
+@pytest.fixture()
+def data_in_db(empty):
+    X = [1, 2, 3, 4, 5]
+    y = [1, 2, 3, 4, 5]
+    empty.execute(
+        Collection(name='documents').insert_many(
+            [Document({'X': x, 'y': yy}) for x, yy in zip(X, y)]
+        )
+    )
+    yield empty
 
 
 def test_function_predict_one():
@@ -9,3 +25,21 @@ def test_function_predict_one():
 def test_function_predict():
     function = Model(object=lambda x: x, identifier='test')
     assert function.predict([1, 1]) == [1, 1]
+
+
+def test_function_predict_with_document_embedded(data_in_db):
+    function = Model(
+        object=lambda x: x,
+        identifier='test',
+        model_update_kwargs={'document_embedded': False},
+    )
+    function.predict(X='X', db=data_in_db, select=Collection(name='documents').find())
+    out = data_in_db.execute(Collection(name='_outputs.X.test').find({}))
+    assert [o['_outputs'] for o in out] == [1, 2, 3, 4, 5]
+
+
+def test_function_predict_without_document_embedded(data_in_db):
+    function = Model(object=lambda x: x, identifier='test')
+    function.predict(X='X', db=data_in_db, select=Collection(name='documents').find())
+    out = data_in_db.execute(Collection(name='documents').find({}))
+    assert [o['_outputs']['X']['test'] for o in out] == [1, 2, 3, 4, 5]

Pinnacle-Networks/Pinnacle

Former-commit-id: 62ed2f29d992ea4a81304a98866b844e5272b5a3

superduperdb/ext/openai/model.py
@@ -14,7 +14,7 @@ from openai.error import RateLimitError, ServiceUnavailableError, Timeout, TryAg
 import pinnacledb as s
 from pinnacledb.container.component import Component
 from pinnacledb.container.encoder import Encoder
-from pinnacledb.container.model import PredictMixin
+from pinnacledb.container.model import DefaultModelUpdateKwargs, PredictMixin
 from pinnacledb.ext.vector.encoder import vector
 from pinnacledb.misc.compat import cache
 from pinnacledb.misc.retry import Retry
@@ -50,6 +50,7 @@ class OpenAI(Component, PredictMixin):
     version: t.Optional[int] = None
     takes_context: bool = False
     encoder: t.Union[Encoder, str, None] = None
+    model_update_kwargs: dict = dc.field(default_factory=DefaultModelUpdateKwargs)
 
     #: A unique name for the class
     type_id: t.ClassVar[str] = 'model'

test/unittest/model/test_vanilla.py
@@ -1,4 +1,20 @@
+import pytest
+
+from pinnacledb.container.document import Document
 from pinnacledb.container.model import Model
+from pinnacledb.db.mongodb.query import Collection
+
+
+@pytest.fixture()
+def data_in_db(empty):
+    X = [1, 2, 3, 4, 5]
+    y = [1, 2, 3, 4, 5]
+    empty.execute(
+        Collection(name='documents').insert_many(
+            [Document({'X': x, 'y': yy}) for x, yy in zip(X, y)]
+        )
+    )
+    yield empty
 
 
 def test_function_predict_one():
@@ -9,3 +25,21 @@ def test_function_predict_one():
 def test_function_predict():
     function = Model(object=lambda x: x, identifier='test')
     assert function.predict([1, 1]) == [1, 1]
+
+
+def test_function_predict_with_document_embedded(data_in_db):
+    function = Model(
+        object=lambda x: x,
+        identifier='test',
+        model_update_kwargs={'document_embedded': False},
+    )
+    function.predict(X='X', db=data_in_db, select=Collection(name='documents').find())
+    out = data_in_db.execute(Collection(name='_outputs.X.test').find({}))
+    assert [o['_outputs'] for o in out] == [1, 2, 3, 4, 5]
+
+
+def test_function_predict_without_document_embedded(data_in_db):
+    function = Model(object=lambda x: x, identifier='test')
+    function.predict(X='X', db=data_in_db, select=Collection(name='documents').find())
+    out = data_in_db.execute(Collection(name='documents').find({}))
+    assert [o['_outputs']['X']['test'] for o in out] == [1, 2, 3, 4, 5]