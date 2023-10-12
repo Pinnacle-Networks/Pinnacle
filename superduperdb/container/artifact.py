superduper-io/superduper

Refactor artifact store

superduperdb/container/artifact.py
@@ -1,9 +1,12 @@
+import hashlib
 import typing as t
 
 import typing_extensions as te
 
-from superduperdb.db.base.artifact import ArtifactStore
 from superduperdb.misc.serialization import Info, serializers
+
+if t.TYPE_CHECKING:
+    from superduperdb.db.base.artifact import ArtifactStore
 
 ArtifactCache = t.Dict[int, t.Any]
 
@@ -30,7 +33,6 @@ class Artifact:
     object_id: int = 0
     save_method: t.Optional[str] = None
     serializer: str = 'dill'
-    sha1: str = ''
     hash: t.Optional[int] = None
 
     def __init__(
@@ -48,12 +50,21 @@ class Artifact:
         self.info = info
         self.object_id = object_id
         self.serializer = serializer
-        self.sha1 = sha1
         self.hash = hash
+        self._sha1 = sha1
+
+    @property
+    def sha1(self):
+        if not self._sha1:
+            b = self.serialize()
+            self._sha1 = hashlib.sha1(b).hexdigest()
+            return self._sha1
+        return self._sha1
 
     def __hash__(self):
         if self.hash is not None:
             return self.hash
+
         if isinstance(self.artifact, list):
             return hash(str(self.artifact[:100]))
         if isinstance(self.artifact, dict):
@@ -72,14 +83,14 @@ class Artifact:
         serializer = serializers[self.serializer]
         return serializer.encode(self.artifact, self.info)
 
-    def save(self, artifact_store: ArtifactStore) -> t.Dict[str, t.Any]:
+    def save(self, artifact_store: 'ArtifactStore') -> t.Dict[str, t.Any]:
         """Store this artifact, and return a dictionary of the results
 
         :param artifact_store: the store to save the Artifact in
         """
         b = self.serialize()
-        file_id, sha1 = artifact_store.create_artifact(bytes=b)
-        return {'file_id': file_id, 'sha1': sha1, 'serializer': self.serializer}
+        file_id = artifact_store.create(bytes=b)
+        return {'file_id': file_id, 'sha1': self.sha1, 'serializer': self.serializer}
 
 
 class ArtifactDesc(te.TypedDict):

superduperdb/container/component.py
@@ -7,11 +7,10 @@ from __future__ import annotations
 
 import dataclasses as dc
 import typing as t
-from functools import cached_property
 
-from superduperdb.container.artifact_tree import get_artifacts
 from superduperdb.container.job import ComponentJob, Job
 from superduperdb.container.serializable import Serializable
+from superduperdb.db.base.artifact import ArtifactStore
 
 if t.TYPE_CHECKING:
     from superduperdb.container.dataset import Dataset
@@ -47,13 +46,11 @@ class Component(Serializable):
     def child_components(self) -> t.Sequence[t.Any]:
         return []
 
-    @cached_property
+    @property
     def serialized(self):
-        return self.serialize()
-
-    @cached_property
-    def artifacts(self) -> t.Sequence[t.Any]:
-        return list(set(get_artifacts(self.serialized)))
+        serialized = self.serialize()
+        artifacts = tuple(set(ArtifactStore.get_artifacts(serialized)))
+        return serialized, artifacts
 
     @property
     def unique_id(self) -> str:

superduperdb/db/base/db.py
@@ -9,16 +9,11 @@ import networkx
 from dask.distributed import Future
 
 import superduperdb as s
-from superduperdb.container.artifact_tree import (
-    infer_artifacts,
-    load_artifacts_from_store,
-    replace_artifacts,
-)
+from superduperdb.container import serializable
 from superduperdb.container.component import Component
 from superduperdb.container.document import Document
 from superduperdb.container.job import ComponentJob, FunctionJob, Job
 from superduperdb.container.model import Model
-from superduperdb.container.serializable import Serializable
 from superduperdb.container.task_workflow import TaskWorkflow
 from superduperdb.db.base.download import Downloader, gather_uris
 from superduperdb.misc.colors import Colors
@@ -483,8 +478,7 @@ class DB:
             return {
                 k: v
                 for k, v in info['dict'].items()
-                if isinstance(v, dict)
-                and set(v.keys()) == {'type_id', 'identifier', 'version'}
+                if isinstance(v, dict) and serializable.is_component_metadata(v)
             }
 
         def replace_children(r):
@@ -497,9 +491,7 @@ class DB:
             return r
 
         info = replace_children(info)
-        info = load_artifacts_from_store(
-            info, cache={}, artifact_store=self.artifact_store
-        )
+        info = self.artifact_store.load(info)
 
         m = Component.deserialize(info)
         m.on_load(self)
@@ -623,12 +615,9 @@ class DB:
             object.version = 0
 
         if serialized is None:
-            artifact_info = {}
-            for a in object.artifacts:
-                artifact_info[hash(a)] = a.save(self.artifact_store)
-            serialized = t.cast(
-                t.Dict, replace_artifacts(object.serialized, artifact_info)
-            )
+            serialized, artifacts = object.serialized
+            artifact_info = self.artifact_store.save(artifacts)
+            serialized = self.artifact_store.replace(serialized, artifact_info)
 
         else:
             serialized['version'] = object.version
@@ -713,7 +702,7 @@ class DB:
 
             if hasattr(component, 'artifact_attributes'):
                 for a in component.artifact_attributes:
-                    self.artifact_store.delete_artifact(info['dict'][a]['file_id'])
+                    self.artifact_store.delete(info['dict'][a]['file_id'])
             self.metadata.delete_component_version(type_id, identifier, version=version)
 
     def _download_content(  # TODO: duplicated function
@@ -842,7 +831,7 @@ class DB:
         if listener_info is None:
             listener_info = self.metadata.get_component('listener', identifier)
 
-        select = Serializable.deserialize(listener_info['select'])
+        select = serializable.Serializable.deserialize(listener_info['select'])
         if ids is None:
             ids = select.get_ids(self)
         else:
@@ -914,17 +903,7 @@ class DB:
                     object,
                 )
             raise e
-
-        artifact_details = dict()
-        for a in object.artifacts:
-            artifact_details[hash(a)] = a.save(self.artifact_store)
-
-        old_artifacts = tuple(set(infer_artifacts(info)))
-        for oa in old_artifacts:
-            self.artifact_store.delete_artifact(oa)
-
-        new_info = replace_artifacts(object.serialized, artifact_details)
-
+        new_info = self.artifact_store.update(object, metadata_info=info)
         self.metadata.replace_object(
             new_info,
             identifier=object.identifier,

superduperdb/server/client.py
@@ -7,13 +7,10 @@ import uuid
 import click
 import requests
 
-from superduperdb.container.artifact_tree import (
-    load_artifacts,
-    replace_artifacts_with_dict,
-)
 from superduperdb.container.component import Component
 from superduperdb.container.document import Document, dump_bsons, load_bson, load_bsons
 from superduperdb.container.serializable import Serializable
+from superduperdb.db.base.artifact import ArtifactStore
 from superduperdb.db.base.db import ExecuteQuery
 from superduperdb.db.base.query import Delete, Insert, Like, Select, SelectOne, Update
 from superduperdb.misc.serialization import serializers
@@ -197,13 +194,14 @@ class Client:
         :param component: component to be serialized and sent to the server
         """
         request_id = str(uuid.uuid4())
-        lookup = {a: str(uuid.uuid4()) for a in component.artifacts}
-        serializers = {lookup[a]: a.serializer for a in component.artifacts}
-        d = replace_artifacts_with_dict(component.serialized, lookup)
-        for a in component.artifacts:
+        serialized, artifacts = component.serialized
+        lookup = {a.sha1: str(uuid.uuid4()) for a in artifacts}
+        serializers = {lookup[a.sha1]: a.serializer for a in artifacts}
+        d = ArtifactStore.replace_artifacts_with_dict(serialized, lookup)
+        for a in artifacts:
             self._put(
                 request_id=request_id,
-                file_id=lookup[a],
+                file_id=lookup[a.sha1],
                 data=a.serialize(),  # Do we need to serialize again?
             )
         self._make_post_or_put_request(
@@ -303,7 +301,7 @@ class Client:
                 'request_id': request_id,
             },
         ).json()
-        d = load_artifacts(
+        d = ArtifactStore.load_artifacts(
             d, cache={}, getter=lambda x: self._get(request_id=request_id, file_id=x)
         )
         return Serializable.deserialize(d)

superduperdb/server/server.py
@@ -5,12 +5,9 @@ import uuid
 from flask import Flask, jsonify, make_response, request
 
 from superduperdb import CFG
-from superduperdb.container.artifact_tree import (
-    load_artifacts_from_store,
-    replace_artifacts_with_dict,
-)
 from superduperdb.container.document import load_bson, load_bsons
 from superduperdb.container.serializable import Serializable
+from superduperdb.db.base.artifact import ArtifactStore
 from superduperdb.db.base.build import build_datalayer
 from superduperdb.misc.serialization import serializers
 
@@ -137,7 +134,8 @@ def make_endpoints(app, db):
             artifact_cache[a] = serializers[d['serializers'][a]].decode(
                 artifact_cache[a]
             )
-        d = load_artifacts_from_store(d, cache=artifact_cache)
+        d = ArtifactStore.load_from_cache(d, cache=artifact_cache)
+
         object = Serializable.deserialize(d['component'])
         db.add(object)
         del cache[d['request_id']]
@@ -197,13 +195,14 @@ def make_endpoints(app, db):
         if version:
             version = int(version)
         m = db.load(type_id=d['type_id'], identifier=d['identifier'], version=version)
-        lookup = {a: str(uuid.uuid4()) for a in m.artifacts}
-        s_lookup = {lookup[a]: a.serializer for a in m.artifacts}
-        to_send = replace_artifacts_with_dict(m.serialized, lookup)
-        for a in m.artifacts:
-            cache[request_id][lookup[a]] = serializers[s_lookup[lookup[a]]].encode(
-                a.artifact
-            )
+        serialized, artifacts = m.serialized
+        lookup = {a.sha1: str(uuid.uuid4()) for a in artifacts}
+        s_lookup = {lookup[a.sha1]: a.serializer for a in artifacts}
+        to_send = ArtifactStore.replace_artifacts_with_dict(serialized, lookup)
+        for a in artifacts:
+            cache[request_id][lookup[a.sha1]] = serializers[
+                s_lookup[lookup[a.sha1]]
+            ].encode(a.artifact)
         return jsonify(to_send)
 
     @app.route('/remove', methods=['POST'])

Pinnacle-Networks/Pinnacle

Former-commit-id: 15e18de607874d16b31abfe205c33463a5ef1cd7

superduperdb/container/artifact.py
@@ -1,9 +1,12 @@
+import hashlib
 import typing as t
 
 import typing_extensions as te
 
-from pinnacledb.db.base.artifact import ArtifactStore
 from pinnacledb.misc.serialization import Info, serializers
+
+if t.TYPE_CHECKING:
+    from pinnacledb.db.base.artifact import ArtifactStore
 
 ArtifactCache = t.Dict[int, t.Any]
 
@@ -30,7 +33,6 @@ class Artifact:
     object_id: int = 0
     save_method: t.Optional[str] = None
     serializer: str = 'dill'
-    sha1: str = ''
     hash: t.Optional[int] = None
 
     def __init__(
@@ -48,12 +50,21 @@ class Artifact:
         self.info = info
         self.object_id = object_id
         self.serializer = serializer
-        self.sha1 = sha1
         self.hash = hash
+        self._sha1 = sha1
+
+    @property
+    def sha1(self):
+        if not self._sha1:
+            b = self.serialize()
+            self._sha1 = hashlib.sha1(b).hexdigest()
+            return self._sha1
+        return self._sha1
 
     def __hash__(self):
         if self.hash is not None:
             return self.hash
+
         if isinstance(self.artifact, list):
             return hash(str(self.artifact[:100]))
         if isinstance(self.artifact, dict):
@@ -72,14 +83,14 @@ class Artifact:
         serializer = serializers[self.serializer]
         return serializer.encode(self.artifact, self.info)
 
-    def save(self, artifact_store: ArtifactStore) -> t.Dict[str, t.Any]:
+    def save(self, artifact_store: 'ArtifactStore') -> t.Dict[str, t.Any]:
         """Store this artifact, and return a dictionary of the results
 
         :param artifact_store: the store to save the Artifact in
         """
         b = self.serialize()
-        file_id, sha1 = artifact_store.create_artifact(bytes=b)
-        return {'file_id': file_id, 'sha1': sha1, 'serializer': self.serializer}
+        file_id = artifact_store.create(bytes=b)
+        return {'file_id': file_id, 'sha1': self.sha1, 'serializer': self.serializer}
 
 
 class ArtifactDesc(te.TypedDict):

superduperdb/container/component.py
@@ -7,11 +7,10 @@ from __future__ import annotations
 
 import dataclasses as dc
 import typing as t
-from functools import cached_property
 
-from pinnacledb.container.artifact_tree import get_artifacts
 from pinnacledb.container.job import ComponentJob, Job
 from pinnacledb.container.serializable import Serializable
+from pinnacledb.db.base.artifact import ArtifactStore
 
 if t.TYPE_CHECKING:
     from pinnacledb.container.dataset import Dataset
@@ -47,13 +46,11 @@ class Component(Serializable):
     def child_components(self) -> t.Sequence[t.Any]:
         return []
 
-    @cached_property
+    @property
     def serialized(self):
-        return self.serialize()
-
-    @cached_property
-    def artifacts(self) -> t.Sequence[t.Any]:
-        return list(set(get_artifacts(self.serialized)))
+        serialized = self.serialize()
+        artifacts = tuple(set(ArtifactStore.get_artifacts(serialized)))
+        return serialized, artifacts
 
     @property
     def unique_id(self) -> str:

superduperdb/db/base/db.py
@@ -9,16 +9,11 @@ import networkx
 from dask.distributed import Future
 
 import pinnacledb as s
-from pinnacledb.container.artifact_tree import (
-    infer_artifacts,
-    load_artifacts_from_store,
-    replace_artifacts,
-)
+from pinnacledb.container import serializable
 from pinnacledb.container.component import Component
 from pinnacledb.container.document import Document
 from pinnacledb.container.job import ComponentJob, FunctionJob, Job
 from pinnacledb.container.model import Model
-from pinnacledb.container.serializable import Serializable
 from pinnacledb.container.task_workflow import TaskWorkflow
 from pinnacledb.db.base.download import Downloader, gather_uris
 from pinnacledb.misc.colors import Colors
@@ -483,8 +478,7 @@ class DB:
             return {
                 k: v
                 for k, v in info['dict'].items()
-                if isinstance(v, dict)
-                and set(v.keys()) == {'type_id', 'identifier', 'version'}
+                if isinstance(v, dict) and serializable.is_component_metadata(v)
             }
 
         def replace_children(r):
@@ -497,9 +491,7 @@ class DB:
             return r
 
         info = replace_children(info)
-        info = load_artifacts_from_store(
-            info, cache={}, artifact_store=self.artifact_store
-        )
+        info = self.artifact_store.load(info)
 
         m = Component.deserialize(info)
         m.on_load(self)
@@ -623,12 +615,9 @@ class DB:
             object.version = 0
 
         if serialized is None:
-            artifact_info = {}
-            for a in object.artifacts:
-                artifact_info[hash(a)] = a.save(self.artifact_store)
-            serialized = t.cast(
-                t.Dict, replace_artifacts(object.serialized, artifact_info)
-            )
+            serialized, artifacts = object.serialized
+            artifact_info = self.artifact_store.save(artifacts)
+            serialized = self.artifact_store.replace(serialized, artifact_info)
 
         else:
             serialized['version'] = object.version
@@ -713,7 +702,7 @@ class DB:
 
             if hasattr(component, 'artifact_attributes'):
                 for a in component.artifact_attributes:
-                    self.artifact_store.delete_artifact(info['dict'][a]['file_id'])
+                    self.artifact_store.delete(info['dict'][a]['file_id'])
             self.metadata.delete_component_version(type_id, identifier, version=version)
 
     def _download_content(  # TODO: duplicated function
@@ -842,7 +831,7 @@ class DB:
         if listener_info is None:
             listener_info = self.metadata.get_component('listener', identifier)
 
-        select = Serializable.deserialize(listener_info['select'])
+        select = serializable.Serializable.deserialize(listener_info['select'])
         if ids is None:
             ids = select.get_ids(self)
         else:
@@ -914,17 +903,7 @@ class DB:
                     object,
                 )
             raise e
-
-        artifact_details = dict()
-        for a in object.artifacts:
-            artifact_details[hash(a)] = a.save(self.artifact_store)
-
-        old_artifacts = tuple(set(infer_artifacts(info)))
-        for oa in old_artifacts:
-            self.artifact_store.delete_artifact(oa)
-
-        new_info = replace_artifacts(object.serialized, artifact_details)
-
+        new_info = self.artifact_store.update(object, metadata_info=info)
         self.metadata.replace_object(
             new_info,
             identifier=object.identifier,

superduperdb/server/client.py
@@ -7,13 +7,10 @@ import uuid
 import click
 import requests
 
-from pinnacledb.container.artifact_tree import (
-    load_artifacts,
-    replace_artifacts_with_dict,
-)
 from pinnacledb.container.component import Component
 from pinnacledb.container.document import Document, dump_bsons, load_bson, load_bsons
 from pinnacledb.container.serializable import Serializable
+from pinnacledb.db.base.artifact import ArtifactStore
 from pinnacledb.db.base.db import ExecuteQuery
 from pinnacledb.db.base.query import Delete, Insert, Like, Select, SelectOne, Update
 from pinnacledb.misc.serialization import serializers
@@ -197,13 +194,14 @@ class Client:
         :param component: component to be serialized and sent to the server
         """
         request_id = str(uuid.uuid4())
-        lookup = {a: str(uuid.uuid4()) for a in component.artifacts}
-        serializers = {lookup[a]: a.serializer for a in component.artifacts}
-        d = replace_artifacts_with_dict(component.serialized, lookup)
-        for a in component.artifacts:
+        serialized, artifacts = component.serialized
+        lookup = {a.sha1: str(uuid.uuid4()) for a in artifacts}
+        serializers = {lookup[a.sha1]: a.serializer for a in artifacts}
+        d = ArtifactStore.replace_artifacts_with_dict(serialized, lookup)
+        for a in artifacts:
             self._put(
                 request_id=request_id,
-                file_id=lookup[a],
+                file_id=lookup[a.sha1],
                 data=a.serialize(),  # Do we need to serialize again?
             )
         self._make_post_or_put_request(
@@ -303,7 +301,7 @@ class Client:
                 'request_id': request_id,
             },
         ).json()
-        d = load_artifacts(
+        d = ArtifactStore.load_artifacts(
             d, cache={}, getter=lambda x: self._get(request_id=request_id, file_id=x)
         )
         return Serializable.deserialize(d)

superduperdb/server/server.py
@@ -5,12 +5,9 @@ import uuid
 from flask import Flask, jsonify, make_response, request
 
 from pinnacledb import CFG
-from pinnacledb.container.artifact_tree import (
-    load_artifacts_from_store,
-    replace_artifacts_with_dict,
-)
 from pinnacledb.container.document import load_bson, load_bsons
 from pinnacledb.container.serializable import Serializable
+from pinnacledb.db.base.artifact import ArtifactStore
 from pinnacledb.db.base.build import build_datalayer
 from pinnacledb.misc.serialization import serializers
 
@@ -137,7 +134,8 @@ def make_endpoints(app, db):
             artifact_cache[a] = serializers[d['serializers'][a]].decode(
                 artifact_cache[a]
             )
-        d = load_artifacts_from_store(d, cache=artifact_cache)
+        d = ArtifactStore.load_from_cache(d, cache=artifact_cache)
+
         object = Serializable.deserialize(d['component'])
         db.add(object)
         del cache[d['request_id']]
@@ -197,13 +195,14 @@ def make_endpoints(app, db):
         if version:
             version = int(version)
         m = db.load(type_id=d['type_id'], identifier=d['identifier'], version=version)
-        lookup = {a: str(uuid.uuid4()) for a in m.artifacts}
-        s_lookup = {lookup[a]: a.serializer for a in m.artifacts}
-        to_send = replace_artifacts_with_dict(m.serialized, lookup)
-        for a in m.artifacts:
-            cache[request_id][lookup[a]] = serializers[s_lookup[lookup[a]]].encode(
-                a.artifact
-            )
+        serialized, artifacts = m.serialized
+        lookup = {a.sha1: str(uuid.uuid4()) for a in artifacts}
+        s_lookup = {lookup[a.sha1]: a.serializer for a in artifacts}
+        to_send = ArtifactStore.replace_artifacts_with_dict(serialized, lookup)
+        for a in artifacts:
+            cache[request_id][lookup[a.sha1]] = serializers[
+                s_lookup[lookup[a.sha1]]
+            ].encode(a.artifact)
         return jsonify(to_send)
 
     @app.route('/remove', methods=['POST'])