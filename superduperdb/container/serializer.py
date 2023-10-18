superduper-io/superduper

Add serializer for custom serialization

superduperdb/container/serializer.py
@@ -1,0 +1,23 @@
+import dataclasses as dc
+import typing as t
+
+from superduperdb.container.component import Component
+from superduperdb.misc.serialization import serializers
+
+if t.TYPE_CHECKING:
+    from superduperdb.db.base.db import DB
+
+
+@dc.dataclass
+class Serializer(Component):
+    identifier: str
+    object: t.Type
+
+    type_id: t.ClassVar[str] = 'serializer'
+
+    version: t.Optional[int]
+
+    def on_create(self, db: 'DB'):
+        serializers.add(self.identifier, self.object)
+
+        self.object = t.cast(t.Type, self.identifier)

Pinnacle-Networks/Pinnacle

Former-commit-id: 7a90531f491600f89b4d319dc03b4b4e81bfb8fd

superduperdb/container/serializer.py
@@ -1,0 +1,23 @@
+import dataclasses as dc
+import typing as t
+
+from pinnacledb.container.component import Component
+from pinnacledb.misc.serialization import serializers
+
+if t.TYPE_CHECKING:
+    from pinnacledb.db.base.db import DB
+
+
+@dc.dataclass
+class Serializer(Component):
+    identifier: str
+    object: t.Type
+
+    type_id: t.ClassVar[str] = 'serializer'
+
+    version: t.Optional[int]
+
+    def on_create(self, db: 'DB'):
+        serializers.add(self.identifier, self.object)
+
+        self.object = t.cast(t.Type, self.identifier)