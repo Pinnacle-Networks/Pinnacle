superduper-io/superduper

Rename datalayer and backends

superduperdb/backends/mongodb/data_backend.py
@@ -8,9 +8,9 @@ import pymongo
 
 from superduperdb import logging
 from superduperdb.base.serializable import Serializable
-from superduperdb.db.base.data_backend import BaseDataBackend
-from superduperdb.db.mongodb.artifacts import MongoArtifactStore
-from superduperdb.db.mongodb.metadata import MongoMetaDataStore
+from superduperdb.backends.base.data_backend import BaseDataBackend
+from superduperdb.backends.mongodb.artifacts import MongoArtifactStore
+from superduperdb.backends.mongodb.metadata import MongoMetaDataStore
 from superduperdb.misc.colors import Colors
 from superduperdb.misc.special_dicts import MongoStyleDict
 
@@ -40,7 +40,7 @@ class MongoDataBackend(BaseDataBackend):
         from mongomock import MongoClient as MockClient
 
         if isinstance(self.conn, MockClient):
-            from superduperdb.db.filesystem.artifacts import FileSystemArtifactStore
+            from superduperdb.backends.filesystem.artifacts import FileSystemArtifactStore
 
             os.makedirs(f'/tmp/{self.name}', exist_ok=True)
             return FileSystemArtifactStore(f'/tmp/{self.name}')

superduperdb/backends/sqlalchemy/metadata.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
 from superduperdb.base.logger import logging
-from superduperdb.db.base.metadata import MetaDataStore
+from superduperdb.backends.base.metadata import MetaDataStore
 from superduperdb.misc.colors import Colors
 
 Base = declarative_base()

superduperdb/base/artifact.py
@@ -6,7 +6,7 @@ import typing_extensions as te
 from superduperdb.misc.serialization import Info, serializers
 
 if t.TYPE_CHECKING:
-    from superduperdb.db.base.artifact import ArtifactStore
+    from superduperdb.backends.base.artifact import ArtifactStore
 
 ArtifactCache = t.Dict[int, t.Any]
 

superduperdb/components/listener.py
@@ -3,8 +3,8 @@ import typing as t
 
 from overrides import override
 
-from superduperdb.base.db import DB
-from superduperdb.db.base.query import CompoundSelect
+from superduperdb.base.datalayer import Datalayer
+from superduperdb.backends.base.query import CompoundSelect
 
 from .component import Component
 from ..jobs.job import Job
@@ -44,7 +44,7 @@ class Listener(Component):
         return [('model', 'model')]
 
     @override
-    def on_create(self, db: DB) -> None:
+    def on_create(self, db: Datalayer) -> None:
         if isinstance(self.model, str):
             self.model = t.cast(Model, db.load('model', self.model))
 
@@ -81,7 +81,7 @@ class Listener(Component):
     @override
     def schedule_jobs(
         self,
-        database: DB,
+        database: Datalayer,
         dependencies: t.Sequence[Job] = (),
         distributed: bool = False,
         verbose: bool = False,
@@ -107,7 +107,7 @@ class Listener(Component):
             **(self.predict_kwargs or {}),
         )
 
-    def cleanup(self, database: DB) -> None:
+    def cleanup(self, database: Datalayer) -> None:
         """Clean up when the listener is deleted
 
         :param database: The DB instance to process

superduperdb/components/schema.py
@@ -4,7 +4,7 @@ from functools import cached_property
 
 from superduperdb.components.component import Component
 from superduperdb.components.encoder import Encoder
-from superduperdb.db.ibis.field_types import dtype
+from superduperdb.backends.ibis.field_types import dtype
 
 
 @dc.dataclass

superduperdb/components/serializer.py
@@ -5,7 +5,7 @@ from superduperdb.components.component import Component
 from superduperdb.misc.serialization import serializers
 
 if t.TYPE_CHECKING:
-    from superduperdb.base.db import DB
+    from superduperdb.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -17,7 +17,7 @@ class Serializer(Component):
 
     version: t.Optional[int]
 
-    def on_create(self, db: 'DB'):
+    def on_create(self, db: 'Datalayer'):
         serializers.add(self.identifier, self.object)
 
         self.object = t.cast(t.Type, self.identifier)

test/integration/test_dask.py
@@ -13,7 +13,7 @@ from superduperdb import CFG
 from superduperdb.jobs.job import FunctionJob
 from superduperdb.components.listener import Listener
 from superduperdb.jobs.task_workflow import TaskWorkflow
-from superduperdb.db.mongodb.query import Collection
+from superduperdb.backends.mongodb.query import Collection
 
 
 @contextmanager

test/unittest/db/mongodb/test_database.py
@@ -16,7 +16,7 @@ from superduperdb.base.document import Document
 from superduperdb.components.encoder import Encoder
 from superduperdb.components.listener import Listener
 from superduperdb.base.exceptions import ComponentInUseError, ComponentInUseWarning
-from superduperdb.db.mongodb.query import Collection
+from superduperdb.backends.mongodb.query import Collection
 
 n_data_points = 250
 

Pinnacle-Networks/Pinnacle

Former-commit-id: fa2e55fc7be56e94d5b13836b42f5d47c503165f

superduperdb/backends/mongodb/data_backend.py
@@ -8,9 +8,9 @@ import pymongo
 
 from pinnacledb import logging
 from pinnacledb.base.serializable import Serializable
-from pinnacledb.db.base.data_backend import BaseDataBackend
-from pinnacledb.db.mongodb.artifacts import MongoArtifactStore
-from pinnacledb.db.mongodb.metadata import MongoMetaDataStore
+from pinnacledb.backends.base.data_backend import BaseDataBackend
+from pinnacledb.backends.mongodb.artifacts import MongoArtifactStore
+from pinnacledb.backends.mongodb.metadata import MongoMetaDataStore
 from pinnacledb.misc.colors import Colors
 from pinnacledb.misc.special_dicts import MongoStyleDict
 
@@ -40,7 +40,7 @@ class MongoDataBackend(BaseDataBackend):
         from mongomock import MongoClient as MockClient
 
         if isinstance(self.conn, MockClient):
-            from pinnacledb.db.filesystem.artifacts import FileSystemArtifactStore
+            from pinnacledb.backends.filesystem.artifacts import FileSystemArtifactStore
 
             os.makedirs(f'/tmp/{self.name}', exist_ok=True)
             return FileSystemArtifactStore(f'/tmp/{self.name}')

superduperdb/backends/sqlalchemy/metadata.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
 from pinnacledb.base.logger import logging
-from pinnacledb.db.base.metadata import MetaDataStore
+from pinnacledb.backends.base.metadata import MetaDataStore
 from pinnacledb.misc.colors import Colors
 
 Base = declarative_base()

superduperdb/base/artifact.py
@@ -6,7 +6,7 @@ import typing_extensions as te
 from pinnacledb.misc.serialization import Info, serializers
 
 if t.TYPE_CHECKING:
-    from pinnacledb.db.base.artifact import ArtifactStore
+    from pinnacledb.backends.base.artifact import ArtifactStore
 
 ArtifactCache = t.Dict[int, t.Any]
 

superduperdb/components/listener.py
@@ -3,8 +3,8 @@ import typing as t
 
 from overrides import override
 
-from pinnacledb.base.db import DB
-from pinnacledb.db.base.query import CompoundSelect
+from pinnacledb.base.datalayer import Datalayer
+from pinnacledb.backends.base.query import CompoundSelect
 
 from .component import Component
 from ..jobs.job import Job
@@ -44,7 +44,7 @@ class Listener(Component):
         return [('model', 'model')]
 
     @override
-    def on_create(self, db: DB) -> None:
+    def on_create(self, db: Datalayer) -> None:
         if isinstance(self.model, str):
             self.model = t.cast(Model, db.load('model', self.model))
 
@@ -81,7 +81,7 @@ class Listener(Component):
     @override
     def schedule_jobs(
         self,
-        database: DB,
+        database: Datalayer,
         dependencies: t.Sequence[Job] = (),
         distributed: bool = False,
         verbose: bool = False,
@@ -107,7 +107,7 @@ class Listener(Component):
             **(self.predict_kwargs or {}),
         )
 
-    def cleanup(self, database: DB) -> None:
+    def cleanup(self, database: Datalayer) -> None:
         """Clean up when the listener is deleted
 
         :param database: The DB instance to process

superduperdb/components/schema.py
@@ -4,7 +4,7 @@ from functools import cached_property
 
 from pinnacledb.components.component import Component
 from pinnacledb.components.encoder import Encoder
-from pinnacledb.db.ibis.field_types import dtype
+from pinnacledb.backends.ibis.field_types import dtype
 
 
 @dc.dataclass

superduperdb/components/serializer.py
@@ -5,7 +5,7 @@ from pinnacledb.components.component import Component
 from pinnacledb.misc.serialization import serializers
 
 if t.TYPE_CHECKING:
-    from pinnacledb.base.db import DB
+    from pinnacledb.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -17,7 +17,7 @@ class Serializer(Component):
 
     version: t.Optional[int]
 
-    def on_create(self, db: 'DB'):
+    def on_create(self, db: 'Datalayer'):
         serializers.add(self.identifier, self.object)
 
         self.object = t.cast(t.Type, self.identifier)

test/integration/test_dask.py
@@ -13,7 +13,7 @@ from pinnacledb import CFG
 from pinnacledb.jobs.job import FunctionJob
 from pinnacledb.components.listener import Listener
 from pinnacledb.jobs.task_workflow import TaskWorkflow
-from pinnacledb.db.mongodb.query import Collection
+from pinnacledb.backends.mongodb.query import Collection
 
 
 @contextmanager

test/unittest/db/mongodb/test_database.py
@@ -16,7 +16,7 @@ from pinnacledb.base.document import Document
 from pinnacledb.components.encoder import Encoder
 from pinnacledb.components.listener import Listener
 from pinnacledb.base.exceptions import ComponentInUseError, ComponentInUseWarning
-from pinnacledb.db.mongodb.query import Collection
+from pinnacledb.backends.mongodb.query import Collection
 
 n_data_points = 250
 