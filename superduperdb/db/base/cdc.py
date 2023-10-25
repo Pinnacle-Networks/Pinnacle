superduper-io/superduper

Change file structure, delete unused files and no renamings

superduperdb/db/base/cdc.py
@@ -41,8 +41,8 @@ from superduperdb.misc.runnable.queue_chunker import QueueChunker
 from superduperdb.misc.runnable.runnable import Event
 
 if t.TYPE_CHECKING:
-    from superduperdb.container.serializable import Serializable
-    from superduperdb.db.base.db import DB
+    from superduperdb.base.serializable import Serializable
+    from superduperdb.base.db import DB
     from superduperdb.db.base.query import TableOrCollection
 
 

superduperdb/db/ibis/cursor.py
@@ -1,7 +1,7 @@
 import dataclasses as dc
 
-from superduperdb.container.document import Document
-from superduperdb.db.base.cursor import SuperDuperCursor
+from superduperdb.base.document import Document
+from superduperdb.base.cursor import SuperDuperCursor
 
 
 class IbisDocument(Document):

superduperdb/db/ibis/field_types.py
@@ -3,7 +3,7 @@ import typing as t
 
 from ibis.expr.datatypes import DataType, dtype as _dtype
 
-from superduperdb.container.serializable import Serializable
+from superduperdb.base.serializable import Serializable
 
 
 @dc.dataclass

superduperdb/ext/transformers/model.py
@@ -14,10 +14,10 @@ from transformers import (
     pipeline as _pipeline,
 )
 
-from superduperdb.container.artifact import Artifact
+from superduperdb.base.artifact import Artifact
 from superduperdb.container.metric import Metric
 from superduperdb.container.model import Model, _TrainingConfiguration
-from superduperdb.db.base.db import DB
+from superduperdb.base.db import DB
 from superduperdb.db.base.query import Select
 from superduperdb.db.query_dataset import query_dataset_factory
 from superduperdb.misc.special_dicts import MongoStyleDict

superduperdb/server/client.py
@@ -8,10 +8,10 @@ import click
 import requests
 
 from superduperdb.container.component import Component
-from superduperdb.container.document import dump_bsons, load_bsons
-from superduperdb.container.serializable import Serializable
+from superduperdb.base.document import dump_bsons, load_bsons
+from superduperdb.base.serializable import Serializable
 from superduperdb.db.base.artifact import ArtifactStore
-from superduperdb.db.base.db import ExecuteQuery
+from superduperdb.base.db import ExecuteQuery
 from superduperdb.db.base.query import Delete, Insert, Like, Select, Update
 from superduperdb.misc.serialization import serializers
 

superduperdb/vector_search/update_tasks.py
@@ -1,6 +1,6 @@
 import typing as t
 
-from superduperdb.container.serializable import Serializable
+from superduperdb.base.serializable import Serializable
 from superduperdb.db.base.query import CompoundSelect
 from superduperdb.vector_search.base import VectorItem
 

test/unittest/model/test_sklearn.py
@@ -6,7 +6,7 @@ from sklearn.base import TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 
-from superduperdb.container.document import Document
+from superduperdb.base.document import Document
 from superduperdb.db.mongodb.query import Collection
 from superduperdb.ext.sklearn.model import Estimator
 

Pinnacle-Networks/Pinnacle

Former-commit-id: 454a945bc0d904a334fe427967246c4dab0745e5

superduperdb/db/base/cdc.py
@@ -41,8 +41,8 @@ from pinnacledb.misc.runnable.queue_chunker import QueueChunker
 from pinnacledb.misc.runnable.runnable import Event
 
 if t.TYPE_CHECKING:
-    from pinnacledb.container.serializable import Serializable
-    from pinnacledb.db.base.db import DB
+    from pinnacledb.base.serializable import Serializable
+    from pinnacledb.base.db import DB
     from pinnacledb.db.base.query import TableOrCollection
 
 

superduperdb/db/ibis/cursor.py
@@ -1,7 +1,7 @@
 import dataclasses as dc
 
-from pinnacledb.container.document import Document
-from pinnacledb.db.base.cursor import SuperDuperCursor
+from pinnacledb.base.document import Document
+from pinnacledb.base.cursor import SuperDuperCursor
 
 
 class IbisDocument(Document):

superduperdb/db/ibis/field_types.py
@@ -3,7 +3,7 @@ import typing as t
 
 from ibis.expr.datatypes import DataType, dtype as _dtype
 
-from pinnacledb.container.serializable import Serializable
+from pinnacledb.base.serializable import Serializable
 
 
 @dc.dataclass

superduperdb/ext/transformers/model.py
@@ -14,10 +14,10 @@ from transformers import (
     pipeline as _pipeline,
 )
 
-from pinnacledb.container.artifact import Artifact
+from pinnacledb.base.artifact import Artifact
 from pinnacledb.container.metric import Metric
 from pinnacledb.container.model import Model, _TrainingConfiguration
-from pinnacledb.db.base.db import DB
+from pinnacledb.base.db import DB
 from pinnacledb.db.base.query import Select
 from pinnacledb.db.query_dataset import query_dataset_factory
 from pinnacledb.misc.special_dicts import MongoStyleDict

superduperdb/server/client.py
@@ -8,10 +8,10 @@ import click
 import requests
 
 from pinnacledb.container.component import Component
-from pinnacledb.container.document import dump_bsons, load_bsons
-from pinnacledb.container.serializable import Serializable
+from pinnacledb.base.document import dump_bsons, load_bsons
+from pinnacledb.base.serializable import Serializable
 from pinnacledb.db.base.artifact import ArtifactStore
-from pinnacledb.db.base.db import ExecuteQuery
+from pinnacledb.base.db import ExecuteQuery
 from pinnacledb.db.base.query import Delete, Insert, Like, Select, Update
 from pinnacledb.misc.serialization import serializers
 

superduperdb/vector_search/update_tasks.py
@@ -1,6 +1,6 @@
 import typing as t
 
-from pinnacledb.container.serializable import Serializable
+from pinnacledb.base.serializable import Serializable
 from pinnacledb.db.base.query import CompoundSelect
 from pinnacledb.vector_search.base import VectorItem
 

test/unittest/model/test_sklearn.py
@@ -6,7 +6,7 @@ from sklearn.base import TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 
-from pinnacledb.container.document import Document
+from pinnacledb.base.document import Document
 from pinnacledb.db.mongodb.query import Collection
 from pinnacledb.ext.sklearn.model import Estimator
 