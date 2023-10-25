superduper-io/superduper

Rename container to components

superduperdb/components/model.py
@@ -13,12 +13,12 @@ from sklearn.pipeline import Pipeline
 
 import superduperdb as s
 from superduperdb.base.artifact import Artifact
-from superduperdb.container.component import Component
-from superduperdb.container.dataset import Dataset
-from superduperdb.container.encoder import Encoder
+from superduperdb.components.component import Component
+from superduperdb.components.dataset import Dataset
+from superduperdb.components.encoder import Encoder
 from superduperdb.jobs.job import ComponentJob, Job
-from superduperdb.container.metric import Metric
-from superduperdb.container.schema import Schema
+from superduperdb.components.metric import Metric
+from superduperdb.components.schema import Schema
 from superduperdb.base.serializable import Serializable
 from superduperdb.db.base.query import CompoundSelect, Select
 from superduperdb.db.ibis.field_types import FieldType
@@ -263,7 +263,7 @@ class PredictMixin:
         dependencies: t.Sequence[Job] = (),
         **kwargs,
     ):
-        from superduperdb.container.listener import Listener
+        from superduperdb.components.listener import Listener
 
         return db.add(
             Listener(

superduperdb/db/base/data_backend.py
@@ -1,7 +1,7 @@
 import typing as t
 from abc import ABC, abstractmethod
 
-from superduperdb.container.model import Model
+from superduperdb.components.model import Model
 
 
 class BaseDataBackend(ABC):

superduperdb/ext/openai/model.py
@@ -12,9 +12,9 @@ from openai import Audio, ChatCompletion, Embedding, Image, Model as OpenAIModel
 from openai.error import RateLimitError, ServiceUnavailableError, Timeout, TryAgain
 
 import superduperdb as s
-from superduperdb.container.component import Component
-from superduperdb.container.encoder import Encoder
-from superduperdb.container.model import PredictMixin
+from superduperdb.components.component import Component
+from superduperdb.components.encoder import Encoder
+from superduperdb.components.model import PredictMixin
 from superduperdb.ext.vector.encoder import vector
 from superduperdb.misc.compat import cache
 from superduperdb.misc.retry import Retry

superduperdb/ext/torch/model.py
@@ -15,9 +15,9 @@ import superduperdb as s
 from superduperdb import logging
 from superduperdb.base.artifact import Artifact
 from superduperdb.base.document import Document
-from superduperdb.container.encoder import Encodable
-from superduperdb.container.metric import Metric
-from superduperdb.container.model import Model, _TrainingConfiguration
+from superduperdb.components.encoder import Encodable
+from superduperdb.components.metric import Metric
+from superduperdb.components.model import Model, _TrainingConfiguration
 from superduperdb.base.serializable import Serializable
 from superduperdb.base.db import DB
 from superduperdb.db.base.query import Select
@@ -25,7 +25,7 @@ from superduperdb.db.query_dataset import QueryDataset
 from superduperdb.ext.torch.utils import device_of, eval, to_device
 
 if t.TYPE_CHECKING:
-    from superduperdb.container.dataset import Dataset
+    from superduperdb.components.dataset import Dataset
 
 
 class BasicDataset(data.Dataset):

test/integration/test_cdc.py
@@ -11,7 +11,7 @@ except ImportError:
     torch = None
 
 from superduperdb.base.document import Document
-from superduperdb.container.listener import Listener
+from superduperdb.components.listener import Listener
 from superduperdb.db.mongodb.query import Collection
 
 RETRY_TIMEOUT = 1

test/unittest/component/test_vector_index.py
@@ -1,4 +1,4 @@
-from superduperdb.container import vector_index
+from superduperdb.components import vector_index
 
 
 def test_ibatch():

test/unittest/conftest.py
@@ -16,11 +16,11 @@ try:
 except ImportError:
     torch = None
 
-from superduperdb.container.dataset import Dataset
+from superduperdb.components.dataset import Dataset
 from superduperdb.base.document import Document
-from superduperdb.container.listener import Listener
-from superduperdb.container.metric import Metric
-from superduperdb.container.vector_index import VectorIndex
+from superduperdb.components.listener import Listener
+from superduperdb.components.metric import Metric
+from superduperdb.components.vector_index import VectorIndex
 from superduperdb.base.db import DB
 from superduperdb.db.mongodb.query import Collection
 from superduperdb.ext.numpy.array import array

test/unittest/model/test_transformers.py
@@ -6,7 +6,7 @@ try:
 except ImportError:
     torch = None
 
-from superduperdb.container.dataset import Dataset
+from superduperdb.components.dataset import Dataset
 from superduperdb.base.document import Document as D
 from superduperdb.db.mongodb.query import Collection
 from superduperdb.ext.transformers.model import (

Pinnacle-Networks/Pinnacle

Former-commit-id: fb16f132ed6cf6dbef41a3dcba2aeb6d085aea48

superduperdb/components/model.py
@@ -13,12 +13,12 @@ from sklearn.pipeline import Pipeline
 
 import pinnacledb as s
 from pinnacledb.base.artifact import Artifact
-from pinnacledb.container.component import Component
-from pinnacledb.container.dataset import Dataset
-from pinnacledb.container.encoder import Encoder
+from pinnacledb.components.component import Component
+from pinnacledb.components.dataset import Dataset
+from pinnacledb.components.encoder import Encoder
 from pinnacledb.jobs.job import ComponentJob, Job
-from pinnacledb.container.metric import Metric
-from pinnacledb.container.schema import Schema
+from pinnacledb.components.metric import Metric
+from pinnacledb.components.schema import Schema
 from pinnacledb.base.serializable import Serializable
 from pinnacledb.db.base.query import CompoundSelect, Select
 from pinnacledb.db.ibis.field_types import FieldType
@@ -263,7 +263,7 @@ class PredictMixin:
         dependencies: t.Sequence[Job] = (),
         **kwargs,
     ):
-        from pinnacledb.container.listener import Listener
+        from pinnacledb.components.listener import Listener
 
         return db.add(
             Listener(

superduperdb/db/base/data_backend.py
@@ -1,7 +1,7 @@
 import typing as t
 from abc import ABC, abstractmethod
 
-from pinnacledb.container.model import Model
+from pinnacledb.components.model import Model
 
 
 class BaseDataBackend(ABC):

superduperdb/ext/openai/model.py
@@ -12,9 +12,9 @@ from openai import Audio, ChatCompletion, Embedding, Image, Model as OpenAIModel
 from openai.error import RateLimitError, ServiceUnavailableError, Timeout, TryAgain
 
 import pinnacledb as s
-from pinnacledb.container.component import Component
-from pinnacledb.container.encoder import Encoder
-from pinnacledb.container.model import PredictMixin
+from pinnacledb.components.component import Component
+from pinnacledb.components.encoder import Encoder
+from pinnacledb.components.model import PredictMixin
 from pinnacledb.ext.vector.encoder import vector
 from pinnacledb.misc.compat import cache
 from pinnacledb.misc.retry import Retry

superduperdb/ext/torch/model.py
@@ -15,9 +15,9 @@ import pinnacledb as s
 from pinnacledb import logging
 from pinnacledb.base.artifact import Artifact
 from pinnacledb.base.document import Document
-from pinnacledb.container.encoder import Encodable
-from pinnacledb.container.metric import Metric
-from pinnacledb.container.model import Model, _TrainingConfiguration
+from pinnacledb.components.encoder import Encodable
+from pinnacledb.components.metric import Metric
+from pinnacledb.components.model import Model, _TrainingConfiguration
 from pinnacledb.base.serializable import Serializable
 from pinnacledb.base.db import DB
 from pinnacledb.db.base.query import Select
@@ -25,7 +25,7 @@ from pinnacledb.db.query_dataset import QueryDataset
 from pinnacledb.ext.torch.utils import device_of, eval, to_device
 
 if t.TYPE_CHECKING:
-    from pinnacledb.container.dataset import Dataset
+    from pinnacledb.components.dataset import Dataset
 
 
 class BasicDataset(data.Dataset):

test/integration/test_cdc.py
@@ -11,7 +11,7 @@ except ImportError:
     torch = None
 
 from pinnacledb.base.document import Document
-from pinnacledb.container.listener import Listener
+from pinnacledb.components.listener import Listener
 from pinnacledb.db.mongodb.query import Collection
 
 RETRY_TIMEOUT = 1

test/unittest/component/test_vector_index.py
@@ -1,4 +1,4 @@
-from pinnacledb.container import vector_index
+from pinnacledb.components import vector_index
 
 
 def test_ibatch():

test/unittest/conftest.py
@@ -16,11 +16,11 @@ try:
 except ImportError:
     torch = None
 
-from pinnacledb.container.dataset import Dataset
+from pinnacledb.components.dataset import Dataset
 from pinnacledb.base.document import Document
-from pinnacledb.container.listener import Listener
-from pinnacledb.container.metric import Metric
-from pinnacledb.container.vector_index import VectorIndex
+from pinnacledb.components.listener import Listener
+from pinnacledb.components.metric import Metric
+from pinnacledb.components.vector_index import VectorIndex
 from pinnacledb.base.db import DB
 from pinnacledb.db.mongodb.query import Collection
 from pinnacledb.ext.numpy.array import array

test/unittest/model/test_transformers.py
@@ -6,7 +6,7 @@ try:
 except ImportError:
     torch = None
 
-from pinnacledb.container.dataset import Dataset
+from pinnacledb.components.dataset import Dataset
 from pinnacledb.base.document import Document as D
 from pinnacledb.db.mongodb.query import Collection
 from pinnacledb.ext.transformers.model import (