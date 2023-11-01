superduper-io/superduper

change logging mechanism to loguru
Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

superduperdb/backends/base/query.py
@@ -6,9 +6,9 @@ from typing import Any
 
 import pandas as pd
 
+from superduperdb import logging
 from superduperdb.base.cursor import SuperDuperCursor
 from superduperdb.base.document import Document
-from superduperdb.base.logger import logging
 from superduperdb.base.serializable import Serializable
 
 GREEN = '\033[92m'

superduperdb/backends/filesystem/artifacts.py
@@ -5,8 +5,8 @@ import uuid
 
 import click
 
+from superduperdb import logging
 from superduperdb.backends.base.artifact import ArtifactStore
-from superduperdb.base.logger import logging
 from superduperdb.misc.colors import Colors
 
 

superduperdb/backends/ibis/query.py
@@ -11,6 +11,7 @@ import typing as t
 
 import ibis
 
+from superduperdb import logging
 from superduperdb.backends.base.query import (
     CompoundSelect,
     Insert,
@@ -23,7 +24,6 @@ from superduperdb.backends.base.query import (
     _ReprMixin,
 )
 from superduperdb.backends.ibis.cursor import SuperDuperIbisCursor
-from superduperdb.base.logger import logging
 from superduperdb.components.component import Component
 from superduperdb.components.encoder import Encoder
 from superduperdb.components.schema import Schema

superduperdb/backends/mongodb/metadata.py
@@ -4,8 +4,8 @@ import click
 import tenacity
 from pymongo.results import DeleteResult, InsertOneResult, UpdateResult
 
+from superduperdb import logging
 from superduperdb.backends.base.metadata import MetaDataStore
-from superduperdb.base.logger import logging
 from superduperdb.components.component import Component
 from superduperdb.misc.colors import Colors
 

superduperdb/base/datalayer.py
@@ -11,10 +11,10 @@ import tqdm
 from dask.distributed import Future
 
 import superduperdb as s
+from superduperdb import logging
 from superduperdb.backends.base.backends import vector_searcher_implementations
 from superduperdb.base import serializable
 from superduperdb.base.document import Document
-from superduperdb.base.logger import logging
 from superduperdb.components.component import Component
 from superduperdb.components.encoder import Encodable, Encoder
 from superduperdb.components.model import Model
@@ -772,13 +772,13 @@ class Datalayer:
         model=None,
         predict_kwargs=None,
     ):
-        s.log.info('finding documents under filter')
+        s.logging.info('finding documents under filter')
         features = features or {}
         model_identifier = model_info['identifier']
         if features is None:
             features = {}
         documents = list(self.execute(select.select_using_ids(_ids)))
-        s.log.info('done.')
+        s.logging.info('done.')
         documents = [x.unpack() for x in documents]
         if key != '_base' or '_base' in features:
             passed_docs = [r[key] for r in documents]
@@ -804,7 +804,7 @@ class Datalayer:
 
         existing_versions = self.show(object.type_id, object.identifier)
         if isinstance(object.version, int) and object.version in existing_versions:
-            s.log.warn(f'{object.unique_id} already exists - doing nothing')
+            s.logging.warn(f'{object.unique_id} already exists - doing nothing')
             return
 
         if existing_versions:
@@ -932,7 +932,7 @@ class Datalayer:
 
         documents = [x.content for x in documents]
         uris, keys, place_ids = gather_uris(documents)
-        s.log.info(f'found {len(uris)} uris')
+        s.logging.info(f'found {len(uris)} uris')
         if not uris:
             return
 
@@ -1041,7 +1041,7 @@ class Datalayer:
 
         if max_chunk_size is not None:
             for it, i in enumerate(range(0, len(ids), max_chunk_size)):
-                s.log.info(
+                s.logging.info(
                     'computing chunk '
                     f'({it + 1}/{math.ceil(len(ids) / max_chunk_size)})'
                 )

superduperdb/ext/transformers/model.py
@@ -1,6 +1,5 @@
 import dataclasses as dc
 import functools
-import logging
 import random
 import re
 import typing as t
@@ -14,6 +13,7 @@ from transformers import (
     pipeline as _pipeline,
 )
 
+from superduperdb import logging
 from superduperdb.backends.base.query import Select
 from superduperdb.backends.query_dataset import query_dataset_factory
 from superduperdb.base.artifact import Artifact

superduperdb/server/client.py
@@ -1,12 +1,12 @@
 import dataclasses as dc
 import inspect
-import logging
 import typing as t
 import uuid
 
 import click
 import requests
 
+from superduperdb import logging
 from superduperdb.backends.base.artifact import ArtifactStore
 from superduperdb.backends.base.query import Delete, Insert, Like, Select, Update
 from superduperdb.base.datalayer import ExecuteQuery

Pinnacle-Networks/Pinnacle

Former-commit-id: 975c1b423e0ee371849d296db897af24e347b14f

superduperdb/backends/base/query.py
@@ -6,9 +6,9 @@ from typing import Any
 
 import pandas as pd
 
+from pinnacledb import logging
 from pinnacledb.base.cursor import SuperDuperCursor
 from pinnacledb.base.document import Document
-from pinnacledb.base.logger import logging
 from pinnacledb.base.serializable import Serializable
 
 GREEN = '\033[92m'

superduperdb/backends/filesystem/artifacts.py
@@ -5,8 +5,8 @@ import uuid
 
 import click
 
+from pinnacledb import logging
 from pinnacledb.backends.base.artifact import ArtifactStore
-from pinnacledb.base.logger import logging
 from pinnacledb.misc.colors import Colors
 
 

superduperdb/backends/ibis/query.py
@@ -11,6 +11,7 @@ import typing as t
 
 import ibis
 
+from pinnacledb import logging
 from pinnacledb.backends.base.query import (
     CompoundSelect,
     Insert,
@@ -23,7 +24,6 @@ from pinnacledb.backends.base.query import (
     _ReprMixin,
 )
 from pinnacledb.backends.ibis.cursor import SuperDuperIbisCursor
-from pinnacledb.base.logger import logging
 from pinnacledb.components.component import Component
 from pinnacledb.components.encoder import Encoder
 from pinnacledb.components.schema import Schema

superduperdb/backends/mongodb/metadata.py
@@ -4,8 +4,8 @@ import click
 import tenacity
 from pymongo.results import DeleteResult, InsertOneResult, UpdateResult
 
+from pinnacledb import logging
 from pinnacledb.backends.base.metadata import MetaDataStore
-from pinnacledb.base.logger import logging
 from pinnacledb.components.component import Component
 from pinnacledb.misc.colors import Colors
 

superduperdb/base/datalayer.py
@@ -11,10 +11,10 @@ import tqdm
 from dask.distributed import Future
 
 import pinnacledb as s
+from pinnacledb import logging
 from pinnacledb.backends.base.backends import vector_searcher_implementations
 from pinnacledb.base import serializable
 from pinnacledb.base.document import Document
-from pinnacledb.base.logger import logging
 from pinnacledb.components.component import Component
 from pinnacledb.components.encoder import Encodable, Encoder
 from pinnacledb.components.model import Model
@@ -777,13 +777,13 @@ class Datalayer:
         model=None,
         predict_kwargs=None,
     ):
-        s.log.info('finding documents under filter')
+        s.logging.info('finding documents under filter')
         features = features or {}
         model_identifier = model_info['identifier']
         if features is None:
             features = {}
         documents = list(self.execute(select.select_using_ids(_ids)))
-        s.log.info('done.')
+        s.logging.info('done.')
         documents = [x.unpack() for x in documents]
         if key != '_base' or '_base' in features:
             passed_docs = [r[key] for r in documents]
@@ -809,7 +809,7 @@ class Datalayer:
 
         existing_versions = self.show(object.type_id, object.identifier)
         if isinstance(object.version, int) and object.version in existing_versions:
-            s.log.warn(f'{object.unique_id} already exists - doing nothing')
+            s.logging.warn(f'{object.unique_id} already exists - doing nothing')
             return
 
         if existing_versions:
@@ -937,7 +937,7 @@ class Datalayer:
 
         documents = [x.content for x in documents]
         uris, keys, place_ids = gather_uris(documents)
-        s.log.info(f'found {len(uris)} uris')
+        s.logging.info(f'found {len(uris)} uris')
         if not uris:
             return
 
@@ -1046,7 +1046,7 @@ class Datalayer:
 
         if max_chunk_size is not None:
             for it, i in enumerate(range(0, len(ids), max_chunk_size)):
-                s.log.info(
+                s.logging.info(
                     'computing chunk '
                     f'({it + 1}/{math.ceil(len(ids) / max_chunk_size)})'
                 )

superduperdb/ext/transformers/model.py
@@ -1,6 +1,5 @@
 import dataclasses as dc
 import functools
-import logging
 import random
 import re
 import typing as t
@@ -14,6 +13,7 @@ from transformers import (
     pipeline as _pipeline,
 )
 
+from pinnacledb import logging
 from pinnacledb.backends.base.query import Select
 from pinnacledb.backends.query_dataset import query_dataset_factory
 from pinnacledb.base.artifact import Artifact

superduperdb/server/client.py
@@ -1,12 +1,12 @@
 import dataclasses as dc
 import inspect
-import logging
 import typing as t
 import uuid
 
 import click
 import requests
 
+from pinnacledb import logging
 from pinnacledb.backends.base.artifact import ArtifactStore
 from pinnacledb.backends.base.query import Delete, Insert, Like, Select, Update
 from pinnacledb.base.datalayer import ExecuteQuery