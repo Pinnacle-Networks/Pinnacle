superduper-io/superduper

Add vector search service

superduperdb/cli/vector_search.py
@@ -1,0 +1,8 @@
+from superduperdb.vector_search.server.app import app
+
+from . import command
+
+
+@command(help='Start vector search server')
+def vector_search():
+    app.start()

superduperdb/misc/server.py
@@ -1,0 +1,26 @@
+import json
+
+import requests
+
+from superduperdb import CFG
+
+
+def request_server(
+    service: str = 'vector_search', data=None, endpoint='add', args={}, type='post'
+):
+    service_config = getattr(CFG.server, service)
+    url = service_config.host + str(service_config.port) + '/' + endpoint
+    args = '&'.join([f'{k}={v}' for k, v in args.items()])
+    if args:
+        url += '?' + args
+    if type == 'post':
+        response = requests.post(url, json=data)
+        result = json.loads(response.content)
+    else:
+        response = requests.get(url)
+        result = None
+    if response.status_code != 200:
+        error = json.loads(response.content)
+        msg = f'Server error at {service} with {response.status_code} :: {error}'
+        raise Exception(msg)
+    return result

superduperdb/server/app.py
@@ -1,0 +1,81 @@
+
+import uvicorn
+from fastapi import APIRouter, Depends, FastAPI, Request
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from superduperdb.base.build import build_datalayer
+from superduperdb.base.datalayer import Datalayer
+
+app = FastAPI()
+
+
+def create_datalayer() -> Datalayer:
+    return build_datalayer()
+
+
+@app.on_event('startup')
+def startup_db_client():
+    db = create_datalayer()
+    app.state.pool = db
+
+
+@app.on_event('shutdown')
+def shutdown_db_client():
+    try:
+        app.state.pool.close()
+    except AttributeError:
+        raise Exception('Could not close the database properly')
+
+
+# --------------- Create exception handler middleware-----------------
+
+
+class ExceptionHandlerMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        try:
+            return await call_next(request)
+        except Exception as e:
+            return JSONResponse(
+                status_code=500,
+                content={'error': e.__class__.__name__, 'messages': e.args},
+            )
+
+
+app.add_middleware(ExceptionHandlerMiddleware)
+
+
+class SuperDuperApp:
+    def __init__(self, service='vector_search', host='0.0.0.0', port=8000):
+        self.service = service
+        self.host = host
+        self.port = port
+
+        self.router = APIRouter(prefix='/' + self.service)
+
+    def add(self, *args, method='post', **kwargs):
+        def decorator(function):
+            self.router.add_api_route(
+                *args, **kwargs, endpoint=function, methods=[method]
+            )
+            return
+
+        return decorator
+
+    def start(self):
+        app.include_router(self.router)
+
+        uvicorn.run(
+            app,
+            host=self.host,
+            port=self.port,
+            reload=False,
+        )
+
+
+def database(request: Request) -> Datalayer:
+    return request.app.state.pool
+
+
+def DatalayerDependency():
+    return Depends(database)

superduperdb/vector_search/interface.py
@@ -1,0 +1,110 @@
+import typing as t
+
+import numpy as np
+
+from superduperdb import CFG
+from superduperdb.misc.server import request_server
+from superduperdb.vector_search.base import BaseVectorSearcher, VectorItem
+
+
+class FastVectorSearcher(BaseVectorSearcher):
+    def __init__(self, vector_searcher, vector_index: str):
+        self.searcher = vector_searcher
+        self.vector_index = vector_index
+
+        if CFG.mode == 'production':
+            request_server(
+                service='vector_search',
+                endpoint='create/search',
+                args={
+                    'vector_index': self.vector_index,
+                },
+                type='get',
+            )
+
+    def __len__(self):
+        raise self.searcher.__len__()
+
+    def add(self, items: t.Sequence[VectorItem]) -> None:
+        """
+        Add items to the index.
+
+        :param items: t.Sequence of VectorItems
+        """
+        vector_items = [{'vector': i.vector, 'id': i.id} for i in items]
+        if CFG.mode == 'production':
+            request_server(
+                service='vector_search',
+                data=vector_items,
+                endpoint='add/search',
+                args={
+                    'vector_index': self.vector_index,
+                },
+            )
+            return
+
+        return self.searcher.add(items)
+
+    def delete(self, ids: t.Sequence[str]) -> None:
+        """
+        Remove items from the index
+
+        :param ids: t.Sequence of ids of vectors.
+        """
+        if CFG.mode == 'production':
+            request_server(
+                service='vector_search',
+                data=ids,
+                endpoint='delete/search',
+                args={
+                    'vector_index': self.vector_index,
+                },
+            )
+            return
+
+        return self.searcher.delete(ids)
+
+    def find_nearest_from_id(
+        self,
+        _id,
+        n: int = 100,
+        within_ids: t.Sequence[str] = (),
+    ) -> t.Tuple[t.List[str], t.List[float]]:
+        """
+        Find the nearest vectors to the vector with the given id.
+
+        :param _id: id of the vector
+        :param n: number of nearest vectors to return
+        """
+        if CFG.mode == 'production':
+            response = request_server(
+                service='vector_search',
+                endpoint='query/id/search',
+                args={'vector_index': self.vector_index, 'n': n, 'id': _id},
+            )
+            return response['ids'], response['scores']
+
+        return self.searcher.find_nearest_from_id(_id, n=n, within_ids=within_ids)
+
+    def find_nearest_from_array(
+        self,
+        h: np.typing.ArrayLike,
+        n: int = 100,
+        within_ids: t.Sequence[str] = (),
+    ) -> t.Tuple[t.List[str], t.List[float]]:
+        """
+        Find the nearest vectors to the given vector.
+
+        :param h: vector
+        :param n: number of nearest vectors to return
+        """
+        if CFG.mode == 'production':
+            response = request_server(
+                service='vector_search',
+                data=h,
+                endpoint='query/search',
+                args={'vector_index': self.vector_index, 'n': n},
+            )
+            return response['ids'], response['scores']
+
+        return self.searcher.find_nearest_from_array(h=h, n=n, within_ids=within_ids)

superduperdb/vector_search/server/app.py
@@ -1,0 +1,66 @@
+import typing as t
+
+from pydantic import BaseModel
+
+from superduperdb.base.datalayer import Datalayer
+from superduperdb.server.app import DatalayerDependency, SuperDuperApp
+from superduperdb.vector_search.server import service
+
+app = SuperDuperApp('vector_search')
+
+
+class VectorItem(BaseModel):
+    id: str
+    vector: t.List
+
+
+@app.add("/create/search", status_code=200, method='get')
+def create_search(vector_index: str, db: Datalayer = DatalayerDependency()):
+    service.create_search(vector_index=vector_index, db=db)
+    return {'message': 'Vector index created successfuly'}
+
+
+@app.add("/query/id/search", method='post')
+def query_search_by_id(
+    id: str, vector_index: str, n: int = 100, db: Datalayer = DatalayerDependency()
+):
+    ids, scores = service.query_search_from_id(
+        id, vector_index=vector_index, n=n, db=db
+    )
+    return {'ids': ids, 'scores': scores}
+
+
+@app.add("/query/search")
+def query_search_by_array(
+    vector: service.ListVectorType,
+    vector_index: str,
+    n: int = 100,
+    db: Datalayer = DatalayerDependency(),
+):
+    ids, scores = service.query_search_from_array(
+        vector, vector_index=vector_index, n=n, db=db
+    )
+    return {'ids': ids, 'scores': scores}
+
+
+@app.add("/add/search")
+def add_search(
+    vectors: t.List[VectorItem],
+    vector_index: str,
+    db: Datalayer = DatalayerDependency(),
+):
+    service.add_search(vectors, vector_index=vector_index, db=db)
+    return {'message': 'Added vectors successfuly'}
+
+
+@app.add("/delete/search")
+def delete_search(
+    ids: t.List[str], vector_index: str, db: Datalayer = DatalayerDependency()
+):
+    service.delete_search(ids, vector_index=vector_index, db=db)
+    return {'message': 'Ids deleted successfuly'}
+
+
+@app.add("/list/search")
+def list_search(db: Datalayer = DatalayerDependency()):
+    return service.list_search(db)

superduperdb/vector_search/server/service.py
@@ -1,0 +1,64 @@
+import math
+import typing as t
+
+from fastapi import Request
+
+from superduperdb.base.datalayer import Datalayer
+from superduperdb.vector_search.base import VectorItem
+
+ListVectorType = t.List[t.Union[float, int]]
+
+VectorSearchResultType = t.Tuple[t.List[str], t.List[float]]
+
+
+def _vector_search(
+    x: t.Union[str, ListVectorType],
+    n: int,
+    vector_index: str,
+    by_array: bool = True,
+    db=None,
+) -> VectorSearchResultType:
+    vi = db.fast_vector_searchers[vector_index]
+    if by_array:
+        ids, scores = vi.searcher.find_nearest_from_array(x, n=n)
+    else:
+        ids, scores = vi.searcher.find_nearest_from_id(x, n=n)
+    scores = [-1.0 if math.isnan(s) else s for s in scores]
+    return ids, scores
+
+
+def database(request: Request) -> Datalayer:
+    return request.app.state.pool
+
+
+def list_search(db: Datalayer):
+    return db.show('vector_index')
+
+
+def create_search(vector_index: str, db=None):
+    # TODO: add searcher type
+    db.initialize_vector_searcher(vector_index, backfill=True)
+
+
+def query_search_from_array(
+    array: ListVectorType, vector_index: str, n: int = 100, db=None
+) -> VectorSearchResultType:
+    return _vector_search(array, n=n, vector_index=vector_index, db=db)
+
+
+def query_search_from_id(
+    id: str, vector_index: str, n: int = 100, db=None
+) -> VectorSearchResultType:
+    return _vector_search(id, n=n, vector_index=vector_index, db=db, by_array=False)
+
+
+def add_search(vector, vector_index: str, db=None):
+    vector = [VectorItem(id=v.id, vector=v.vector) for v in vector]
+
+    vi = db.fast_vector_searchers[vector_index]
+    vi.searcher.add(vector)
+
+
+def delete_search(ids: t.List[str], vector_index: str, db=None):
+    vi = db.fast_vector_searchers[vector_index]
+    vi.searcher.delete(ids)

test/integration/test_vector_search_server.py
@@ -1,0 +1,37 @@
+import json
+
+import pytest
+from fastapi.testclient import TestClient
+
+
+@pytest.fixture
+def client(database_with_default_encoders_and_model):
+    from superduperdb.vector_search.server.app import app
+
+    app.state.pool = database_with_default_encoders_and_model
+    client = TestClient(app)
+
+    yield client
+
+
+def test_basic_worflow(client):
+    vector_index = 'test_index'
+    data = [{'vector': [100, 100, 100], 'id': '100'}]
+    response = client.post(f"/add/search?vector_index={vector_index}", json=data)
+    assert response.status_code == 200
+    response = json.loads(response.content)
+    assert response['message'] == 'Added vectors successfuly'
+
+    response = client.post(f"/query/id/search?vector_index={vector_index}&id=100")
+    by_id = json.loads(response.content)
+    response = client.post(
+        f"/query/search?vector_index={vector_index}", json=[100, 100, 100]
+    )
+    by_vector = json.loads(response.content)
+    assert by_vector['ids'] == ['100']
+    assert by_id['ids'] == ['100']
+
+    response = client.post(f"/delete/search?vector_index={vector_index}", json=['100'])
+    response = client.post(f"/query/id/search?vector_index={vector_index}&id=100")
+    by_id = json.loads(response.content)
+    assert by_id == {'error': 'KeyError', 'messages': ['100']}

Pinnacle-Networks/Pinnacle

Former-commit-id: d9ab8be0475b87e383784122b0a227b9e2d4f0e2

superduperdb/cli/vector_search.py
@@ -1,0 +1,8 @@
+from pinnacledb.vector_search.server.app import app
+
+from . import command
+
+
+@command(help='Start vector search server')
+def vector_search():
+    app.start()

superduperdb/misc/server.py
@@ -1,0 +1,26 @@
+import json
+
+import requests
+
+from pinnacledb import CFG
+
+
+def request_server(
+    service: str = 'vector_search', data=None, endpoint='add', args={}, type='post'
+):
+    service_config = getattr(CFG.server, service)
+    url = service_config.host + str(service_config.port) + '/' + endpoint
+    args = '&'.join([f'{k}={v}' for k, v in args.items()])
+    if args:
+        url += '?' + args
+    if type == 'post':
+        response = requests.post(url, json=data)
+        result = json.loads(response.content)
+    else:
+        response = requests.get(url)
+        result = None
+    if response.status_code != 200:
+        error = json.loads(response.content)
+        msg = f'Server error at {service} with {response.status_code} :: {error}'
+        raise Exception(msg)
+    return result

superduperdb/server/app.py
@@ -1,0 +1,81 @@
+
+import uvicorn
+from fastapi import APIRouter, Depends, FastAPI, Request
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from pinnacledb.base.build import build_datalayer
+from pinnacledb.base.datalayer import Datalayer
+
+app = FastAPI()
+
+
+def create_datalayer() -> Datalayer:
+    return build_datalayer()
+
+
+@app.on_event('startup')
+def startup_db_client():
+    db = create_datalayer()
+    app.state.pool = db
+
+
+@app.on_event('shutdown')
+def shutdown_db_client():
+    try:
+        app.state.pool.close()
+    except AttributeError:
+        raise Exception('Could not close the database properly')
+
+
+# --------------- Create exception handler middleware-----------------
+
+
+class ExceptionHandlerMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        try:
+            return await call_next(request)
+        except Exception as e:
+            return JSONResponse(
+                status_code=500,
+                content={'error': e.__class__.__name__, 'messages': e.args},
+            )
+
+
+app.add_middleware(ExceptionHandlerMiddleware)
+
+
+class SuperDuperApp:
+    def __init__(self, service='vector_search', host='0.0.0.0', port=8000):
+        self.service = service
+        self.host = host
+        self.port = port
+
+        self.router = APIRouter(prefix='/' + self.service)
+
+    def add(self, *args, method='post', **kwargs):
+        def decorator(function):
+            self.router.add_api_route(
+                *args, **kwargs, endpoint=function, methods=[method]
+            )
+            return
+
+        return decorator
+
+    def start(self):
+        app.include_router(self.router)
+
+        uvicorn.run(
+            app,
+            host=self.host,
+            port=self.port,
+            reload=False,
+        )
+
+
+def database(request: Request) -> Datalayer:
+    return request.app.state.pool
+
+
+def DatalayerDependency():
+    return Depends(database)

superduperdb/vector_search/interface.py
@@ -1,0 +1,110 @@
+import typing as t
+
+import numpy as np
+
+from pinnacledb import CFG
+from pinnacledb.misc.server import request_server
+from pinnacledb.vector_search.base import BaseVectorSearcher, VectorItem
+
+
+class FastVectorSearcher(BaseVectorSearcher):
+    def __init__(self, vector_searcher, vector_index: str):
+        self.searcher = vector_searcher
+        self.vector_index = vector_index
+
+        if CFG.mode == 'production':
+            request_server(
+                service='vector_search',
+                endpoint='create/search',
+                args={
+                    'vector_index': self.vector_index,
+                },
+                type='get',
+            )
+
+    def __len__(self):
+        raise self.searcher.__len__()
+
+    def add(self, items: t.Sequence[VectorItem]) -> None:
+        """
+        Add items to the index.
+
+        :param items: t.Sequence of VectorItems
+        """
+        vector_items = [{'vector': i.vector, 'id': i.id} for i in items]
+        if CFG.mode == 'production':
+            request_server(
+                service='vector_search',
+                data=vector_items,
+                endpoint='add/search',
+                args={
+                    'vector_index': self.vector_index,
+                },
+            )
+            return
+
+        return self.searcher.add(items)
+
+    def delete(self, ids: t.Sequence[str]) -> None:
+        """
+        Remove items from the index
+
+        :param ids: t.Sequence of ids of vectors.
+        """
+        if CFG.mode == 'production':
+            request_server(
+                service='vector_search',
+                data=ids,
+                endpoint='delete/search',
+                args={
+                    'vector_index': self.vector_index,
+                },
+            )
+            return
+
+        return self.searcher.delete(ids)
+
+    def find_nearest_from_id(
+        self,
+        _id,
+        n: int = 100,
+        within_ids: t.Sequence[str] = (),
+    ) -> t.Tuple[t.List[str], t.List[float]]:
+        """
+        Find the nearest vectors to the vector with the given id.
+
+        :param _id: id of the vector
+        :param n: number of nearest vectors to return
+        """
+        if CFG.mode == 'production':
+            response = request_server(
+                service='vector_search',
+                endpoint='query/id/search',
+                args={'vector_index': self.vector_index, 'n': n, 'id': _id},
+            )
+            return response['ids'], response['scores']
+
+        return self.searcher.find_nearest_from_id(_id, n=n, within_ids=within_ids)
+
+    def find_nearest_from_array(
+        self,
+        h: np.typing.ArrayLike,
+        n: int = 100,
+        within_ids: t.Sequence[str] = (),
+    ) -> t.Tuple[t.List[str], t.List[float]]:
+        """
+        Find the nearest vectors to the given vector.
+
+        :param h: vector
+        :param n: number of nearest vectors to return
+        """
+        if CFG.mode == 'production':
+            response = request_server(
+                service='vector_search',
+                data=h,
+                endpoint='query/search',
+                args={'vector_index': self.vector_index, 'n': n},
+            )
+            return response['ids'], response['scores']
+
+        return self.searcher.find_nearest_from_array(h=h, n=n, within_ids=within_ids)

superduperdb/vector_search/server/app.py
@@ -1,0 +1,66 @@
+import typing as t
+
+from pydantic import BaseModel
+
+from pinnacledb.base.datalayer import Datalayer
+from pinnacledb.server.app import DatalayerDependency, SuperDuperApp
+from pinnacledb.vector_search.server import service
+
+app = SuperDuperApp('vector_search')
+
+
+class VectorItem(BaseModel):
+    id: str
+    vector: t.List
+
+
+@app.add("/create/search", status_code=200, method='get')
+def create_search(vector_index: str, db: Datalayer = DatalayerDependency()):
+    service.create_search(vector_index=vector_index, db=db)
+    return {'message': 'Vector index created successfuly'}
+
+
+@app.add("/query/id/search", method='post')
+def query_search_by_id(
+    id: str, vector_index: str, n: int = 100, db: Datalayer = DatalayerDependency()
+):
+    ids, scores = service.query_search_from_id(
+        id, vector_index=vector_index, n=n, db=db
+    )
+    return {'ids': ids, 'scores': scores}
+
+
+@app.add("/query/search")
+def query_search_by_array(
+    vector: service.ListVectorType,
+    vector_index: str,
+    n: int = 100,
+    db: Datalayer = DatalayerDependency(),
+):
+    ids, scores = service.query_search_from_array(
+        vector, vector_index=vector_index, n=n, db=db
+    )
+    return {'ids': ids, 'scores': scores}
+
+
+@app.add("/add/search")
+def add_search(
+    vectors: t.List[VectorItem],
+    vector_index: str,
+    db: Datalayer = DatalayerDependency(),
+):
+    service.add_search(vectors, vector_index=vector_index, db=db)
+    return {'message': 'Added vectors successfuly'}
+
+
+@app.add("/delete/search")
+def delete_search(
+    ids: t.List[str], vector_index: str, db: Datalayer = DatalayerDependency()
+):
+    service.delete_search(ids, vector_index=vector_index, db=db)
+    return {'message': 'Ids deleted successfuly'}
+
+
+@app.add("/list/search")
+def list_search(db: Datalayer = DatalayerDependency()):
+    return service.list_search(db)

superduperdb/vector_search/server/service.py
@@ -1,0 +1,64 @@
+import math
+import typing as t
+
+from fastapi import Request
+
+from pinnacledb.base.datalayer import Datalayer
+from pinnacledb.vector_search.base import VectorItem
+
+ListVectorType = t.List[t.Union[float, int]]
+
+VectorSearchResultType = t.Tuple[t.List[str], t.List[float]]
+
+
+def _vector_search(
+    x: t.Union[str, ListVectorType],
+    n: int,
+    vector_index: str,
+    by_array: bool = True,
+    db=None,
+) -> VectorSearchResultType:
+    vi = db.fast_vector_searchers[vector_index]
+    if by_array:
+        ids, scores = vi.searcher.find_nearest_from_array(x, n=n)
+    else:
+        ids, scores = vi.searcher.find_nearest_from_id(x, n=n)
+    scores = [-1.0 if math.isnan(s) else s for s in scores]
+    return ids, scores
+
+
+def database(request: Request) -> Datalayer:
+    return request.app.state.pool
+
+
+def list_search(db: Datalayer):
+    return db.show('vector_index')
+
+
+def create_search(vector_index: str, db=None):
+    # TODO: add searcher type
+    db.initialize_vector_searcher(vector_index, backfill=True)
+
+
+def query_search_from_array(
+    array: ListVectorType, vector_index: str, n: int = 100, db=None
+) -> VectorSearchResultType:
+    return _vector_search(array, n=n, vector_index=vector_index, db=db)
+
+
+def query_search_from_id(
+    id: str, vector_index: str, n: int = 100, db=None
+) -> VectorSearchResultType:
+    return _vector_search(id, n=n, vector_index=vector_index, db=db, by_array=False)
+
+
+def add_search(vector, vector_index: str, db=None):
+    vector = [VectorItem(id=v.id, vector=v.vector) for v in vector]
+
+    vi = db.fast_vector_searchers[vector_index]
+    vi.searcher.add(vector)
+
+
+def delete_search(ids: t.List[str], vector_index: str, db=None):
+    vi = db.fast_vector_searchers[vector_index]
+    vi.searcher.delete(ids)

test/integration/test_vector_search_server.py
@@ -1,0 +1,37 @@
+import json
+
+import pytest
+from fastapi.testclient import TestClient
+
+
+@pytest.fixture
+def client(database_with_default_encoders_and_model):
+    from pinnacledb.vector_search.server.app import app
+
+    app.state.pool = database_with_default_encoders_and_model
+    client = TestClient(app)
+
+    yield client
+
+
+def test_basic_worflow(client):
+    vector_index = 'test_index'
+    data = [{'vector': [100, 100, 100], 'id': '100'}]
+    response = client.post(f"/add/search?vector_index={vector_index}", json=data)
+    assert response.status_code == 200
+    response = json.loads(response.content)
+    assert response['message'] == 'Added vectors successfuly'
+
+    response = client.post(f"/query/id/search?vector_index={vector_index}&id=100")
+    by_id = json.loads(response.content)
+    response = client.post(
+        f"/query/search?vector_index={vector_index}", json=[100, 100, 100]
+    )
+    by_vector = json.loads(response.content)
+    assert by_vector['ids'] == ['100']
+    assert by_id['ids'] == ['100']
+
+    response = client.post(f"/delete/search?vector_index={vector_index}", json=['100'])
+    response = client.post(f"/query/id/search?vector_index={vector_index}&id=100")
+    by_id = json.loads(response.content)
+    assert by_id == {'error': 'KeyError', 'messages': ['100']}