superduper-io/superduper

Add configuration items about lance home

superduperdb/base/config.py
@@ -5,6 +5,7 @@ canot contain any other imports from this project.
 """
 # TODO further simplify these configurations
 
+import os
 import re
 import typing as t
 from enum import Enum
@@ -143,6 +144,7 @@ class Config(JSONable):
 
     #: The configuration for the vector search
     vector_search: 'str' = 'in_memory'  # "in_memory" / "lance"
+    lance_home: str = os.path.join('.superduperdb', 'vector_indices')
 
     artifact_store: t.Optional[str] = None
     metadata_store: t.Optional[str] = None

superduperdb/vector_search/lance.py
@@ -5,6 +5,7 @@ import lance
 import numpy
 import pyarrow as pa
 
+from superduperdb import CFG
 from superduperdb.vector_search.base import BaseVectorSearcher, VectorItem
 
 
@@ -27,10 +28,7 @@ class LanceVectorSearcher(BaseVectorSearcher):
         index: t.Optional[t.List[str]] = None,
         measure: t.Optional[str] = None,
     ):
-        lance_home = os.environ.get(
-            'SUPERDUPERDB_LANCE_HOME', '.superduperdb/vector_indices'
-        )
-        self.dataset_path = f'{lance_home}/{identifier}.lance'
+        self.dataset_path = os.path.join(CFG.lance_home, f'{identifier}.lance')
         self.dimensions = dimensions
         self._created = False
         self.measure = measure

test/unittest/vector_search/test_vector_search.py
@@ -4,6 +4,7 @@ import uuid
 import numpy as np
 import pytest
 
+from superduperdb import CFG
 from superduperdb.vector_search.base import VectorItem
 from superduperdb.vector_search.in_memory import InMemoryVectorSearcher
 from superduperdb.vector_search.lance import LanceVectorSearcher
@@ -12,7 +13,7 @@ from superduperdb.vector_search.lance import LanceVectorSearcher
 @pytest.fixture
 def index_data(monkeypatch):
     with tempfile.TemporaryDirectory() as unique_dir:
-        monkeypatch.setenv('SUPERDUPERDB_LANCE_HOME', str(unique_dir))
+        monkeypatch.setattr(CFG, 'lance_home', str(unique_dir))
         h = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
         ids = [str(uuid.uuid4()) for _ in range(h.shape[0])]
         yield h, ids

Pinnacle-Networks/Pinnacle

Former-commit-id: dc737523abaf45adaf91f5ea9408b810b2604a92

superduperdb/base/config.py
@@ -5,6 +5,7 @@ canot contain any other imports from this project.
 """
 # TODO further simplify these configurations
 
+import os
 import re
 import typing as t
 from enum import Enum
@@ -143,6 +144,7 @@ class Config(JSONable):
 
     #: The configuration for the vector search
     vector_search: 'str' = 'in_memory'  # "in_memory" / "lance"
+    lance_home: str = os.path.join('.pinnacledb', 'vector_indices')
 
     artifact_store: t.Optional[str] = None
     metadata_store: t.Optional[str] = None

superduperdb/vector_search/lance.py
@@ -5,6 +5,7 @@ import lance
 import numpy
 import pyarrow as pa
 
+from pinnacledb import CFG
 from pinnacledb.vector_search.base import BaseVectorSearcher, VectorItem
 
 
@@ -27,10 +28,7 @@ class LanceVectorSearcher(BaseVectorSearcher):
         index: t.Optional[t.List[str]] = None,
         measure: t.Optional[str] = None,
     ):
-        lance_home = os.environ.get(
-            'SUPERDUPERDB_LANCE_HOME', '.pinnacledb/vector_indices'
-        )
-        self.dataset_path = f'{lance_home}/{identifier}.lance'
+        self.dataset_path = os.path.join(CFG.lance_home, f'{identifier}.lance')
         self.dimensions = dimensions
         self._created = False
         self.measure = measure

test/unittest/vector_search/test_vector_search.py
@@ -4,6 +4,7 @@ import uuid
 import numpy as np
 import pytest
 
+from pinnacledb import CFG
 from pinnacledb.vector_search.base import VectorItem
 from pinnacledb.vector_search.in_memory import InMemoryVectorSearcher
 from pinnacledb.vector_search.lance import LanceVectorSearcher
@@ -12,7 +13,7 @@ from pinnacledb.vector_search.lance import LanceVectorSearcher
 @pytest.fixture
 def index_data(monkeypatch):
     with tempfile.TemporaryDirectory() as unique_dir:
-        monkeypatch.setenv('SUPERDUPERDB_LANCE_HOME', str(unique_dir))
+        monkeypatch.setattr(CFG, 'lance_home', str(unique_dir))
         h = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
         ids = [str(uuid.uuid4()) for _ in range(h.shape[0])]
         yield h, ids