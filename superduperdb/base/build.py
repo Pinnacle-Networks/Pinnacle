superduper-io/superduper

e2e testing for mongo
Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

superduperdb/base/build.py
@@ -1,6 +1,12 @@
 import re
+import sys
+
+import ibis
+import mongomock
+import pymongo
 
 import superduperdb as s
+from superduperdb import logging
 from superduperdb.backends.base.backends import data_backends, metadata_stores
 from superduperdb.backends.filesystem.artifacts import FileSystemArtifactStore
 from superduperdb.backends.mongodb.artifacts import MongoArtifactStore
@@ -26,37 +32,48 @@ def build_artifact_store(cfg):
 
 def build_datalayer(cfg=None, **kwargs) -> Datalayer:
     """
-    Build db as per ``db = superduper(db)`` from configuration.
+    Build a Datalayer object as per ``db = superduper(db)`` from configuration.
 
-    :param cfg: configuration to use. If None, use ``superduperdb.CFG``.
+    :param cfg: Configuration to use. If None, use ``superduperdb.CFG``.
     """
+
+    # Use the provided configuration or fall back to the default configuration.
     cfg = cfg or s.CFG
 
+    # Update configuration with keyword arguments.
     for k, v in kwargs.items():
         setattr(cfg, k, v)
 
+    # Helper function to build a data backend based on the URI.
     def build(uri, mapping):
         if re.match('^mongodb:\/\/|^mongodb\+srv:\/\/', uri) is not None:
             name = uri.split('/')[-1]
-            import pymongo
+            conn = pymongo.MongoClient(
+                uri,
+                serverSelectionTimeoutMS=5000,
+            )
 
-            uri = '/'.join(uri.split('/')[:-1])
-            conn = pymongo.MongoClient(uri, serverSelectionTimeoutMS=5000)
             return mapping['mongodb'](conn, name)
         elif uri.startswith('mongomock://'):
             name = uri.split('/')[-1]
-            import mongomock
-
             conn = mongomock.MongoClient()
             return mapping['mongodb'](conn, name)
         else:
-            import ibis
-
+            name = uri.split('//')[0]
             conn = ibis.connect(uri)
-            return mapping['ibis'](conn, uri.split('//')[0])
+            return mapping['ibis'](conn, name)
 
-    databackend = build(cfg.data_backend, data_backends)
+    # Connect to data backend.
+    try:
+        databackend = build(cfg.data_backend, data_backends)
+        logging.success("Initializing DataBackend Client: ", databackend.conn)
+    except Exception as e:
+        # Exit quickly if a connection fails.
+        logging.error("Error initializing to DataBackend Client:", str(e))
+        sys.exit(1)
 
+
+    # Build a Datalayer object with the specified components.
     db = Datalayer(
         databackend=databackend,
         metadata=(

superduperdb/base/config.py
@@ -142,7 +142,7 @@ class Config(JSONable):
     def self_hosted_vector_search(self) -> bool:
         return re.split('://|\+', self.data_backend)[0] == self.vector_search
 
-    data_backend: str = 'mongodb://localhost:27018'
+    data_backend: str = 'mongodb://superduper:superduper@mongodb/test_db'
 
     #: The configuration for the vector search
     vector_search: 'str' = 'in_memory'  # "in_memory" / "lance"

test/conftest.py
@@ -7,9 +7,9 @@ from typing import Iterator
 
 import fil
 import pytest
-from tenacity import Retrying, stop_after_delay
 
 import superduperdb as s
+from superduperdb import logging
 from superduperdb.base.datalayer import Datalayer
 from superduperdb.misc import superduper
 
@@ -49,7 +49,6 @@ def _write(t):
 
 @pytest.fixture(autouse=SDDB_USE_MONGOMOCK)
 def patch_mongomock(monkeypatch):
-    import gridfs
     import gridfs.grid_file
     import pymongo
     from mongomock import Collection, Database, MongoClient
@@ -73,15 +72,19 @@ def test_db(monkeypatch, request) -> Iterator[Datalayer]:
     # use the below decorator to set the db name, if not using random db_name
     # `@pytest.mark.parametrize('test_db', [db_name], indirect=True)`
     db_name = getattr(request, 'param', uuid.uuid4().hex)
-    data_backend = (
-        f'mongodb://testmongodbuser:testmongodbpassword@localhost:27018/{db_name}'
-    )
+    # data_backend = f'mongodb://superduper:superduper@mongodb:27017/{db_name}'
+
+    data_backend = 'mongodb://superduper:superduper@mongodb:27017/test_db'
+
     monkeypatch.setattr(CFG, 'data_backend', data_backend)
-    for attempt in Retrying(stop=stop_after_delay(15)):
-        with attempt:
-            db = build_datalayer(CFG)
-            db.databackend.conn.is_mongos
-            print("Connected to DB instance with MongoDB!")
+
+    db = build_datalayer(CFG)
+
+    db.databackend.conn.is_mongos
+
     yield db
+
+    logging.info("Dropping database ", {db_name})
+
     db.databackend.conn.drop_database(db_name)
     db.databackend.conn.drop_database(f'_filesystem:{db_name}')

test/material/testenv/mongo-init.sh
@@ -1,37 +1,55 @@
-MONGO_INITDB_USERNAME=testmongodbuser
-MONGO_INITDB_PASSWORD=testmongodbpassword
-MONGO_PORT=27018
+#!/bin/bash
 
-init_replicaset_cmd="\
-	rs.initiate( \
-	{ \
-		_id:\"rs0\", \
-		version: 1, \
-		members: [ \
-		{ _id: 0, host : \"localhost:$MONGO_PORT\" }] \
-	} \
-	); \
-	rs.status(); \
-"
+set -eu
 
-create_user_cmd="\
-    db.getSiblingDB('admin').createUser( \
-        { \
-            user: \"$MONGO_INITDB_USERNAME\", \
-            pwd: \"$MONGO_INITDB_PASSWORD\", \
-            roles: [ { role: \"root\", db: \"admin\" } ] \
-         } \
-    ); \
-    rs.status(); \
-"
 # replicate set initiate
 echo "Checking mongo container..."
-until mongosh --host sddb-mongodb --port $MONGO_PORT --eval "print(\"waited for connection\")"
+until mongosh --host mongodb  --eval "print(\"waited for connection\")"
 do
     sleep 1
 done
 
 echo "Initializing replicaset..."
-mongosh --host sddb-mongodb --port $MONGO_PORT --eval "$init_replicaset_cmd"
+mongosh --host mongodb  <<EOF
+    rs.initiate(
+      {
+          _id: "rs0",
+          version: 1,
+          members: [
+            { _id: 0, host: "mongodb:27017"}
+          ]
+      }
+    )
+    rs.status()
+EOF
 
-mongosh --host sddb-mongodb --port $MONGO_PORT  --eval "$create_user_cmd"
+
+echo "Creating admin user: root@root/admin"
+mongosh --host mongodb  <<EOF
+    db.getSiblingDB('admin').createUser(
+        {
+            user: "root",
+            pwd: "root",
+            roles: [ { role: "root", db: "admin" } ]
+         }
+    )
+
+    rs.status()
+EOF
+
+echo "Creating normal user: superduper:superduper/test_db"
+mongosh --host mongodb  <<EOF
+  use ${SDDB_DATABASE}
+  db.createUser(
+    {
+      user: "${SDDB_USER}",
+      pwd: "${SDDB_PASS}",
+      roles: [ { role: "dbOwner", db: "${SDDB_DATABASE}" } ]
+    }
+  )
+EOF
+
+echo "Confirm everything works properly"
+echo "---------------------------------------"
+mongosh --eval 'rs.status()' "mongodb://${SDDB_USER}:${SDDB_PASS}@mongodb:27017/${SDDB_DATABASE}"
+echo "---------------------------------------"

Pinnacle-Networks/Pinnacle

Former-commit-id: 473917d1ede5baff1d0fadf68ea36003bf818218

superduperdb/base/build.py
@@ -1,6 +1,12 @@
 import re
+import sys
+
+import ibis
+import mongomock
+import pymongo
 
 import pinnacledb as s
+from pinnacledb import logging
 from pinnacledb.backends.base.backends import data_backends, metadata_stores
 from pinnacledb.backends.filesystem.artifacts import FileSystemArtifactStore
 from pinnacledb.backends.mongodb.artifacts import MongoArtifactStore
@@ -26,37 +32,48 @@ def build_artifact_store(cfg):
 
 def build_datalayer(cfg=None, **kwargs) -> Datalayer:
     """
-    Build db as per ``db = pinnacle(db)`` from configuration.
+    Build a Datalayer object as per ``db = pinnacle(db)`` from configuration.
 
-    :param cfg: configuration to use. If None, use ``pinnacledb.CFG``.
+    :param cfg: Configuration to use. If None, use ``pinnacledb.CFG``.
     """
+
+    # Use the provided configuration or fall back to the default configuration.
     cfg = cfg or s.CFG
 
+    # Update configuration with keyword arguments.
     for k, v in kwargs.items():
         setattr(cfg, k, v)
 
+    # Helper function to build a data backend based on the URI.
     def build(uri, mapping):
         if re.match('^mongodb:\/\/|^mongodb\+srv:\/\/', uri) is not None:
             name = uri.split('/')[-1]
-            import pymongo
+            conn = pymongo.MongoClient(
+                uri,
+                serverSelectionTimeoutMS=5000,
+            )
 
-            uri = '/'.join(uri.split('/')[:-1])
-            conn = pymongo.MongoClient(uri, serverSelectionTimeoutMS=5000)
             return mapping['mongodb'](conn, name)
         elif uri.startswith('mongomock://'):
             name = uri.split('/')[-1]
-            import mongomock
-
             conn = mongomock.MongoClient()
             return mapping['mongodb'](conn, name)
         else:
-            import ibis
-
+            name = uri.split('//')[0]
             conn = ibis.connect(uri)
-            return mapping['ibis'](conn, uri.split('//')[0])
+            return mapping['ibis'](conn, name)
 
-    databackend = build(cfg.data_backend, data_backends)
+    # Connect to data backend.
+    try:
+        databackend = build(cfg.data_backend, data_backends)
+        logging.success("Initializing DataBackend Client: ", databackend.conn)
+    except Exception as e:
+        # Exit quickly if a connection fails.
+        logging.error("Error initializing to DataBackend Client:", str(e))
+        sys.exit(1)
 
+
+    # Build a Datalayer object with the specified components.
     db = Datalayer(
         databackend=databackend,
         metadata=(

superduperdb/base/config.py
@@ -141,7 +141,7 @@ class Config(JSONable):
     def self_hosted_vector_search(self) -> bool:
         return self.data_backend == self.vector_search
 
-    data_backend: str = 'mongodb://localhost:27018'
+    data_backend: str = 'mongodb://pinnacle:pinnacle@mongodb/test_db'
 
     #: The configuration for the vector search
     vector_search: 'str' = 'in_memory'  # "in_memory" / "lance"

test/conftest.py
@@ -7,9 +7,9 @@ from typing import Iterator
 
 import fil
 import pytest
-from tenacity import Retrying, stop_after_delay
 
 import pinnacledb as s
+from pinnacledb import logging
 from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.misc import pinnacle
 
@@ -49,7 +49,6 @@ def _write(t):
 
 @pytest.fixture(autouse=SDDB_USE_MONGOMOCK)
 def patch_mongomock(monkeypatch):
-    import gridfs
     import gridfs.grid_file
     import pymongo
     from mongomock import Collection, Database, MongoClient
@@ -73,15 +72,19 @@ def test_db(monkeypatch, request) -> Iterator[Datalayer]:
     # use the below decorator to set the db name, if not using random db_name
     # `@pytest.mark.parametrize('test_db', [db_name], indirect=True)`
     db_name = getattr(request, 'param', uuid.uuid4().hex)
-    data_backend = (
-        f'mongodb://testmongodbuser:testmongodbpassword@localhost:27018/{db_name}'
-    )
+    # data_backend = f'mongodb://pinnacle:pinnacle@mongodb:27017/{db_name}'
+
+    data_backend = 'mongodb://pinnacle:pinnacle@mongodb:27017/test_db'
+
     monkeypatch.setattr(CFG, 'data_backend', data_backend)
-    for attempt in Retrying(stop=stop_after_delay(15)):
-        with attempt:
-            db = build_datalayer(CFG)
-            db.databackend.conn.is_mongos
-            print("Connected to DB instance with MongoDB!")
+
+    db = build_datalayer(CFG)
+
+    db.databackend.conn.is_mongos
+
     yield db
+
+    logging.info("Dropping database ", {db_name})
+
     db.databackend.conn.drop_database(db_name)
     db.databackend.conn.drop_database(f'_filesystem:{db_name}')

test/material/testenv/mongo-init.sh
@@ -1,37 +1,55 @@
-MONGO_INITDB_USERNAME=testmongodbuser
-MONGO_INITDB_PASSWORD=testmongodbpassword
-MONGO_PORT=27018
+#!/bin/bash
 
-init_replicaset_cmd="\
-	rs.initiate( \
-	{ \
-		_id:\"rs0\", \
-		version: 1, \
-		members: [ \
-		{ _id: 0, host : \"localhost:$MONGO_PORT\" }] \
-	} \
-	); \
-	rs.status(); \
-"
+set -eu
 
-create_user_cmd="\
-    db.getSiblingDB('admin').createUser( \
-        { \
-            user: \"$MONGO_INITDB_USERNAME\", \
-            pwd: \"$MONGO_INITDB_PASSWORD\", \
-            roles: [ { role: \"root\", db: \"admin\" } ] \
-         } \
-    ); \
-    rs.status(); \
-"
 # replicate set initiate
 echo "Checking mongo container..."
-until mongosh --host sddb-mongodb --port $MONGO_PORT --eval "print(\"waited for connection\")"
+until mongosh --host mongodb  --eval "print(\"waited for connection\")"
 do
     sleep 1
 done
 
 echo "Initializing replicaset..."
-mongosh --host sddb-mongodb --port $MONGO_PORT --eval "$init_replicaset_cmd"
+mongosh --host mongodb  <<EOF
+    rs.initiate(
+      {
+          _id: "rs0",
+          version: 1,
+          members: [
+            { _id: 0, host: "mongodb:27017"}
+          ]
+      }
+    )
+    rs.status()
+EOF
 
-mongosh --host sddb-mongodb --port $MONGO_PORT  --eval "$create_user_cmd"
+
+echo "Creating admin user: root@root/admin"
+mongosh --host mongodb  <<EOF
+    db.getSiblingDB('admin').createUser(
+        {
+            user: "root",
+            pwd: "root",
+            roles: [ { role: "root", db: "admin" } ]
+         }
+    )
+
+    rs.status()
+EOF
+
+echo "Creating normal user: pinnacle:pinnacle/test_db"
+mongosh --host mongodb  <<EOF
+  use ${SDDB_DATABASE}
+  db.createUser(
+    {
+      user: "${SDDB_USER}",
+      pwd: "${SDDB_PASS}",
+      roles: [ { role: "dbOwner", db: "${SDDB_DATABASE}" } ]
+    }
+  )
+EOF
+
+echo "Confirm everything works properly"
+echo "---------------------------------------"
+mongosh --eval 'rs.status()' "mongodb://${SDDB_USER}:${SDDB_PASS}@mongodb:27017/${SDDB_DATABASE}"
+echo "---------------------------------------"