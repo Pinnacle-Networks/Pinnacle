superduper-io/superduper

fix configuration for database
Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

superduperdb/base/config.py
@@ -142,7 +142,7 @@ class Config(JSONable):
     def self_hosted_vector_search(self) -> bool:
         return re.split('://|\+', self.data_backend)[0] == self.vector_search
 
-    data_backend: str = 'mongodb://superduper:superduper@mongodb/test_db'
+    data_backend: str = 'mongodb://superduper:superduper@mongodb:27017/test_db'
 
     #: The configuration for the vector search
     vector_search: 'str' = 'in_memory'  # "in_memory" / "lance"

test/conftest.py
@@ -1,7 +1,6 @@
 import inspect
 import os
 import time
-import uuid
 from threading import Lock
 from typing import Iterator
 
@@ -69,12 +68,8 @@ def test_db(monkeypatch, request) -> Iterator[Datalayer]:
     from superduperdb import CFG
     from superduperdb.base.build import build_datalayer
 
-    # use the below decorator to set the db name, if not using random db_name
-    # `@pytest.mark.parametrize('test_db', [db_name], indirect=True)`
-    db_name = getattr(request, 'param', uuid.uuid4().hex)
-    # data_backend = f'mongodb://superduper:superduper@mongodb:27017/{db_name}'
-
-    data_backend = 'mongodb://superduper:superduper@mongodb:27017/test_db'
+    db_name = "test_db"
+    data_backend = f'mongodb://superduper:superduper@mongodb:27017/{db_name}'
 
     monkeypatch.setattr(CFG, 'data_backend', data_backend)
 

test/integration/conftest.py
@@ -1,5 +1,4 @@
 import random
-import uuid
 
 import numpy as np
 import pytest
@@ -112,10 +111,9 @@ def fake_updates(database_with_default_encoders_and_model):
 
 @pytest.fixture
 def local_dask_client(monkeypatch, request):
-    db_name = getattr(request, 'param', uuid.uuid4().hex)
-    data_backend = (
-        f'mongodb://testmongodbuser:testmongodbpassword@localhost:27018/{db_name}'
-    )
+    db_name = "test_db"
+    data_backend = f'mongodb://superduper:superduper@mongodb:27017/{db_name}'
+
     monkeypatch.setenv('SUPERDUPERDB_DATA_BACKEND', data_backend)
     client = dask_client(CFG.cluster, local=True)
     yield client

Pinnacle-Networks/Pinnacle

Former-commit-id: 38748d88e0d1721435dccede274d5aa8dadaebde

superduperdb/base/config.py
@@ -141,7 +141,7 @@ class Config(JSONable):
     def self_hosted_vector_search(self) -> bool:
         return self.data_backend == self.vector_search
 
-    data_backend: str = 'mongodb://pinnacle:pinnacle@mongodb/test_db'
+    data_backend: str = 'mongodb://pinnacle:pinnacle@mongodb:27017/test_db'
 
     #: The configuration for the vector search
     vector_search: 'str' = 'in_memory'  # "in_memory" / "lance"

test/conftest.py
@@ -1,7 +1,6 @@
 import inspect
 import os
 import time
-import uuid
 from threading import Lock
 from typing import Iterator
 
@@ -69,12 +68,8 @@ def test_db(monkeypatch, request) -> Iterator[Datalayer]:
     from pinnacledb import CFG
     from pinnacledb.base.build import build_datalayer
 
-    # use the below decorator to set the db name, if not using random db_name
-    # `@pytest.mark.parametrize('test_db', [db_name], indirect=True)`
-    db_name = getattr(request, 'param', uuid.uuid4().hex)
-    # data_backend = f'mongodb://pinnacle:pinnacle@mongodb:27017/{db_name}'
-
-    data_backend = 'mongodb://pinnacle:pinnacle@mongodb:27017/test_db'
+    db_name = "test_db"
+    data_backend = f'mongodb://pinnacle:pinnacle@mongodb:27017/{db_name}'
 
     monkeypatch.setattr(CFG, 'data_backend', data_backend)
 

test/integration/conftest.py
@@ -1,5 +1,4 @@
 import random
-import uuid
 
 import numpy as np
 import pytest
@@ -112,10 +111,9 @@ def fake_updates(database_with_default_encoders_and_model):
 
 @pytest.fixture
 def local_dask_client(monkeypatch, request):
-    db_name = getattr(request, 'param', uuid.uuid4().hex)
-    data_backend = (
-        f'mongodb://testmongodbuser:testmongodbpassword@localhost:27018/{db_name}'
-    )
+    db_name = "test_db"
+    data_backend = f'mongodb://pinnacle:pinnacle@mongodb:27017/{db_name}'
+
     monkeypatch.setenv('SUPERDUPERDB_DATA_BACKEND', data_backend)
     client = dask_client(CFG.cluster, local=True)
     yield client