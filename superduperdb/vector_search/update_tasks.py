superduper-io/superduper

Repair outputs logic

superduperdb/vector_search/update_tasks.py
@@ -2,6 +2,7 @@ import typing as t
 
 from superduperdb.backends.base.query import CompoundSelect
 from superduperdb.base.serializable import Serializable
+from superduperdb.misc.special_dicts import MongoStyleDict
 from superduperdb.vector_search.base import VectorItem
 
 
@@ -47,7 +48,11 @@ def copy_vectors(
     key = vi.indexing_listener.key
     model = vi.indexing_listener.model.identifier
     vectors = [
-        {'vector': doc['_outputs'][key][model], 'id': str(doc['_id'])} for doc in docs
+        {
+            'vector': MongoStyleDict(doc)[f'_outputs.{key}.{model}'],
+            'id': str(doc['_id']),
+        }
+        for doc in docs
     ]
     for r in vectors:
         if hasattr(r['vector'], 'numpy'):

test/unittest/backends/ibis/test_query.py
@@ -1,7 +1,17 @@
+import os
+import shutil
+
+import numpy
+import pandas
+import pytest
+
+from superduperdb import superduper
 from superduperdb.backends.ibis.field_types import dtype
 from superduperdb.backends.ibis.query import Table
 from superduperdb.base.serializable import Serializable
+from superduperdb.components.model import Model
 from superduperdb.components.schema import Schema
+from superduperdb.ext.numpy.encoder import array
 from superduperdb.ext.pillow.encoder import pil_image
 
 
@@ -28,3 +38,43 @@ def test_serialize_table():
     ds = Serializable.deserialize(s)
 
     print(ds)
+
+
+@pytest.fixture
+def duckdb():
+    os.makedirs('.superduperdb', exist_ok=True)
+    db = superduper('duckdb://.superduperdb/test.ddb')
+
+    _, t = db.add(
+        Table(
+            'test',
+            primary_id='id',
+            schema=Schema(
+                'my-schema',
+                fields={'x': dtype(str), 'id': dtype(str)},
+            ),
+        )
+    )
+
+    db.execute(
+        t.insert(pandas.DataFrame([{'x': 'test', 'id': str(i)} for i in range(20)]))
+    )
+
+    model = Model(
+        object=lambda _: numpy.random.randn(32),
+        identifier='test',
+        encoder=array('float64', shape=(32,)),
+    )
+    model.predict('x', select=t, db=db)
+
+    yield db
+
+    shutil.rmtree('.superduperdb')
+
+
+def test_renamings(duckdb):
+    t = duckdb.load('table', 'test')
+
+    data = duckdb.execute(t.outputs(x='test'))
+
+    assert isinstance(data[0]['_outputs.x.test'].x, numpy.ndarray)

Pinnacle-Networks/Pinnacle

Former-commit-id: 4b1274ed4af9e9ed20b28c4f53bd4162aa45dc05

superduperdb/vector_search/update_tasks.py
@@ -2,6 +2,7 @@ import typing as t
 
 from pinnacledb.backends.base.query import CompoundSelect
 from pinnacledb.base.serializable import Serializable
+from pinnacledb.misc.special_dicts import MongoStyleDict
 from pinnacledb.vector_search.base import VectorItem
 
 
@@ -47,7 +48,11 @@ def copy_vectors(
     key = vi.indexing_listener.key
     model = vi.indexing_listener.model.identifier
     vectors = [
-        {'vector': doc['_outputs'][key][model], 'id': str(doc['_id'])} for doc in docs
+        {
+            'vector': MongoStyleDict(doc)[f'_outputs.{key}.{model}'],
+            'id': str(doc['_id']),
+        }
+        for doc in docs
     ]
     for r in vectors:
         if hasattr(r['vector'], 'numpy'):

test/unittest/backends/ibis/test_query.py
@@ -1,7 +1,17 @@
+import os
+import shutil
+
+import numpy
+import pandas
+import pytest
+
+from pinnacledb import pinnacle
 from pinnacledb.backends.ibis.field_types import dtype
 from pinnacledb.backends.ibis.query import Table
 from pinnacledb.base.serializable import Serializable
+from pinnacledb.components.model import Model
 from pinnacledb.components.schema import Schema
+from pinnacledb.ext.numpy.encoder import array
 from pinnacledb.ext.pillow.encoder import pil_image
 
 
@@ -28,3 +38,43 @@ def test_serialize_table():
     ds = Serializable.deserialize(s)
 
     print(ds)
+
+
+@pytest.fixture
+def duckdb():
+    os.makedirs('.pinnacledb', exist_ok=True)
+    db = pinnacle('duckdb://.pinnacledb/test.ddb')
+
+    _, t = db.add(
+        Table(
+            'test',
+            primary_id='id',
+            schema=Schema(
+                'my-schema',
+                fields={'x': dtype(str), 'id': dtype(str)},
+            ),
+        )
+    )
+
+    db.execute(
+        t.insert(pandas.DataFrame([{'x': 'test', 'id': str(i)} for i in range(20)]))
+    )
+
+    model = Model(
+        object=lambda _: numpy.random.randn(32),
+        identifier='test',
+        encoder=array('float64', shape=(32,)),
+    )
+    model.predict('x', select=t, db=db)
+
+    yield db
+
+    shutil.rmtree('.pinnacledb')
+
+
+def test_renamings(duckdb):
+    t = duckdb.load('table', 'test')
+
+    data = duckdb.execute(t.outputs(x='test'))
+
+    assert isinstance(data[0]['_outputs.x.test'].x, numpy.ndarray)