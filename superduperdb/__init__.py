superduper-io/superduper

Make trivial fixes to respect current docs

superduperdb/__init__.py
@@ -1,6 +1,6 @@
 # ruff: noqa: E402
 from .base import config, configs, jsonable, logger
-from .misc.superduper import superduper
+from .base.superduper import superduper
 
 ICON = '🔮'
 CFG = configs.CFG

superduperdb/base/datalayer.py
@@ -16,6 +16,7 @@ from superduperdb.backends.base.backends import vector_searcher_implementations
 from superduperdb.backends.ibis.query import Table
 from superduperdb.base import serializable
 from superduperdb.base.document import Document
+from superduperdb.base.superduper import superduper
 from superduperdb.cdc.cdc import DatabaseChangeDataCapture
 from superduperdb.components.component import Component
 from superduperdb.components.encoder import Encodable, Encoder
@@ -76,7 +77,8 @@ class Datalayer:
         :param metadata: metadata object containing connection to Metadatastore
         :param artifact_store: artifact_store object containing connection to
                                Artifactstore
-        :param distributed_client:
+        :param distributed_client: distributed_client object containing connection to
+                                   ``dask`` cluster (leave alone)
         """
         logging.info("Building Data Layer")
 
@@ -483,7 +485,7 @@ class Datalayer:
 
     def add(
         self,
-        object: t.Union[Component, t.Sequence[Component]],
+        object: t.Union[Component, t.Sequence[Component], t.Any],
         dependencies: t.Sequence[Job] = (),
     ):
         """
@@ -506,9 +508,7 @@ class Datalayer:
         elif isinstance(object, Component):
             return self._add(object=object, dependencies=dependencies)
         else:
-            raise ValueError(
-                'object should be a sequence of `Component` or `Component`'
-            )
+            return self._add(superduper(object))
 
     def remove(
         self,

superduperdb/base/superduper.py
@@ -6,13 +6,18 @@ from superduperdb.base.configs import CFG
 __all__ = ('superduper',)
 
 
-def superduper(item: t.Any, **kwargs) -> t.Any:
+def superduper(item: t.Optional[t.Any] = None, **kwargs) -> t.Any:
     """
     Attempts to automatically wrap an item in a superduperdb container by
     using duck typing to recognize it.
 
     :param item: A database or model
     """
+
+    if item is None:
+        from superduperdb.base.build import build_datalayer
+
+        return build_datalayer()
 
     if isinstance(item, str):
         return _auto_identify_connection_string(item, **kwargs)
@@ -35,7 +40,7 @@ def _auto_identify_connection_string(item: str, **kwargs) -> t.Any:
 
     else:
         if re.match(r'^[a-zA-Z0-9]+://', item) is None:
-            raise NotImplementedError(f'{item} is not a valid connection string')
+            raise ValueError(f'{item} is not a valid connection string')
         CFG.data_backend = item
     return build_datalayer(CFG, **kwargs)
 
@@ -48,7 +53,7 @@ class _DuckTyper:
     def run(item: t.Any, **kwargs) -> t.Any:
         dts = [dt for dt in _DuckTyper._DUCK_TYPES if dt.accept(item)]
         if not dts:
-            raise NotImplementedError(
+            raise ValueError(
                 f'Couldn\'t auto-identify {item}, please wrap explicitly using '
                 '``superduperdb.container.*``'
             )

superduperdb/ext/cohere/model.py
@@ -7,7 +7,7 @@ from cohere.error import CohereAPIError, CohereConnectionError
 
 from superduperdb.components.component import Component
 from superduperdb.components.encoder import Encoder
-from superduperdb.components.model import PredictMixin
+from superduperdb.components.model import Predictor
 from superduperdb.components.vector_index import vector
 from superduperdb.ext.utils import format_prompt, get_key
 from superduperdb.misc.retry import Retry
@@ -18,7 +18,7 @@ KEY_NAME = 'COHERE_API_KEY'
 
 
 @dc.dataclass
-class Cohere(Component, PredictMixin):
+class Cohere(Component, Predictor):
     """Cohere predictor
 
     :param model: The model to use, e.g. ``'base-light'``.

superduperdb/ext/openai/model.py
@@ -18,7 +18,7 @@ from openai import (
 
 from superduperdb.components.component import Component
 from superduperdb.components.encoder import Encoder
-from superduperdb.components.model import PredictMixin
+from superduperdb.components.model import Predictor
 from superduperdb.components.vector_index import vector
 from superduperdb.misc.compat import cache
 from superduperdb.misc.retry import Retry
@@ -32,7 +32,7 @@ def _available_models():
 
 
 @dc.dataclass
-class OpenAI(Component, PredictMixin):
+class OpenAI(Component, Predictor):
     """OpenAI predictor.
 
     :param model: The model to use, e.g. ``'text-embedding-ada-002'``.
@@ -48,9 +48,6 @@ class OpenAI(Component, PredictMixin):
     takes_context: bool = False
     encoder: t.Union[Encoder, str, None] = None
     model_update_kwargs: dict = dc.field(default_factory=dict)
-
-    #: A unique name for the class
-    type_id: t.ClassVar[str] = 'model'
 
     @property
     def child_components(self):

test/conftest.py
@@ -10,7 +10,7 @@ import pytest
 
 import superduperdb as s
 from superduperdb import logging
-from superduperdb.misc import superduper
+from superduperdb.base import superduper
 
 try:
     import torch

test/unittest/misc/test_superduper.py
@@ -6,7 +6,7 @@ except ImportError:
     torch = None
 
 from superduperdb import superduper
-from superduperdb.misc.superduper import SklearnTyper, TorchTyper
+from superduperdb.base.superduper import SklearnTyper, TorchTyper
 
 
 def test_sklearn_typer():
@@ -31,10 +31,10 @@ def test_superduper_model():
 
 
 def test_superduper_raise():
-    with pytest.raises(NotImplementedError):
+    with pytest.raises(ValueError):
         superduper(1)
 
-    with pytest.raises(NotImplementedError):
+    with pytest.raises(ValueError):
         superduper("string")
 
 

Pinnacle-Networks/Pinnacle

Former-commit-id: 7f8ba31ce439d6e7129b5b2136dd85c760920a45

superduperdb/__init__.py
@@ -1,6 +1,6 @@
 # ruff: noqa: E402
 from .base import config, configs, jsonable, logger
-from .misc.pinnacle import pinnacle
+from .base.pinnacle import pinnacle
 
 ICON = '🔮'
 CFG = configs.CFG

superduperdb/base/datalayer.py
@@ -16,6 +16,7 @@ from pinnacledb.backends.base.backends import vector_searcher_implementations
 from pinnacledb.backends.ibis.query import Table
 from pinnacledb.base import serializable
 from pinnacledb.base.document import Document
+from pinnacledb.base.pinnacle import pinnacle
 from pinnacledb.cdc.cdc import DatabaseChangeDataCapture
 from pinnacledb.components.component import Component
 from pinnacledb.components.encoder import Encodable, Encoder
@@ -76,7 +77,8 @@ class Datalayer:
         :param metadata: metadata object containing connection to Metadatastore
         :param artifact_store: artifact_store object containing connection to
                                Artifactstore
-        :param distributed_client:
+        :param distributed_client: distributed_client object containing connection to
+                                   ``dask`` cluster (leave alone)
         """
         logging.info("Building Data Layer")
 
@@ -483,7 +485,7 @@ class Datalayer:
 
     def add(
         self,
-        object: t.Union[Component, t.Sequence[Component]],
+        object: t.Union[Component, t.Sequence[Component], t.Any],
         dependencies: t.Sequence[Job] = (),
     ):
         """
@@ -506,9 +508,7 @@ class Datalayer:
         elif isinstance(object, Component):
             return self._add(object=object, dependencies=dependencies)
         else:
-            raise ValueError(
-                'object should be a sequence of `Component` or `Component`'
-            )
+            return self._add(pinnacle(object))
 
     def remove(
         self,

superduperdb/base/superduper.py
@@ -6,13 +6,18 @@ from pinnacledb.base.configs import CFG
 __all__ = ('pinnacle',)
 
 
-def pinnacle(item: t.Any, **kwargs) -> t.Any:
+def pinnacle(item: t.Optional[t.Any] = None, **kwargs) -> t.Any:
     """
     Attempts to automatically wrap an item in a pinnacledb container by
     using duck typing to recognize it.
 
     :param item: A database or model
     """
+
+    if item is None:
+        from pinnacledb.base.build import build_datalayer
+
+        return build_datalayer()
 
     if isinstance(item, str):
         return _auto_identify_connection_string(item, **kwargs)
@@ -35,7 +40,7 @@ def _auto_identify_connection_string(item: str, **kwargs) -> t.Any:
 
     else:
         if re.match(r'^[a-zA-Z0-9]+://', item) is None:
-            raise NotImplementedError(f'{item} is not a valid connection string')
+            raise ValueError(f'{item} is not a valid connection string')
         CFG.data_backend = item
     return build_datalayer(CFG, **kwargs)
 
@@ -48,7 +53,7 @@ class _DuckTyper:
     def run(item: t.Any, **kwargs) -> t.Any:
         dts = [dt for dt in _DuckTyper._DUCK_TYPES if dt.accept(item)]
         if not dts:
-            raise NotImplementedError(
+            raise ValueError(
                 f'Couldn\'t auto-identify {item}, please wrap explicitly using '
                 '``pinnacledb.container.*``'
             )

superduperdb/ext/cohere/model.py
@@ -7,7 +7,7 @@ from cohere.error import CohereAPIError, CohereConnectionError
 
 from pinnacledb.components.component import Component
 from pinnacledb.components.encoder import Encoder
-from pinnacledb.components.model import PredictMixin
+from pinnacledb.components.model import Predictor
 from pinnacledb.components.vector_index import vector
 from pinnacledb.ext.utils import format_prompt, get_key
 from pinnacledb.misc.retry import Retry
@@ -18,7 +18,7 @@ KEY_NAME = 'COHERE_API_KEY'
 
 
 @dc.dataclass
-class Cohere(Component, PredictMixin):
+class Cohere(Component, Predictor):
     """Cohere predictor
 
     :param model: The model to use, e.g. ``'base-light'``.

superduperdb/ext/openai/model.py
@@ -18,7 +18,7 @@ from openai import (
 
 from pinnacledb.components.component import Component
 from pinnacledb.components.encoder import Encoder
-from pinnacledb.components.model import PredictMixin
+from pinnacledb.components.model import Predictor
 from pinnacledb.components.vector_index import vector
 from pinnacledb.misc.compat import cache
 from pinnacledb.misc.retry import Retry
@@ -32,7 +32,7 @@ def _available_models():
 
 
 @dc.dataclass
-class OpenAI(Component, PredictMixin):
+class OpenAI(Component, Predictor):
     """OpenAI predictor.
 
     :param model: The model to use, e.g. ``'text-embedding-ada-002'``.
@@ -48,9 +48,6 @@ class OpenAI(Component, PredictMixin):
     takes_context: bool = False
     encoder: t.Union[Encoder, str, None] = None
     model_update_kwargs: dict = dc.field(default_factory=dict)
-
-    #: A unique name for the class
-    type_id: t.ClassVar[str] = 'model'
 
     @property
     def child_components(self):

test/conftest.py
@@ -10,7 +10,7 @@ import pytest
 
 import pinnacledb as s
 from pinnacledb import logging
-from pinnacledb.misc import pinnacle
+from pinnacledb.base import pinnacle
 
 try:
     import torch

test/unittest/misc/test_superduper.py
@@ -6,7 +6,7 @@ except ImportError:
     torch = None
 
 from pinnacledb import pinnacle
-from pinnacledb.misc.pinnacle import SklearnTyper, TorchTyper
+from pinnacledb.base.pinnacle import SklearnTyper, TorchTyper
 
 
 def test_sklearn_typer():
@@ -31,10 +31,10 @@ def test_pinnacle_model():
 
 
 def test_pinnacle_raise():
-    with pytest.raises(NotImplementedError):
+    with pytest.raises(ValueError):
         pinnacle(1)
 
-    with pytest.raises(NotImplementedError):
+    with pytest.raises(ValueError):
         pinnacle("string")
 
 