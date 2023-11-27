superduper-io/superduper

Add local cluster

superduperdb/__main__.py
@@ -2,10 +2,10 @@ import sys
 
 import click
 
-from superduperdb.cli import app, config, info, vector_search
-from superduperdb.cli.serve import local_cluster, serve
+from superduperdb.cli import app, config, info
+from superduperdb.cli.serve import cdc, local_cluster, vector_search
 
-__all__ = 'config', 'info', 'local_cluster', 'serve', 'vector_search'
+__all__ = 'config', 'info', 'local_cluster', 'vector_search', 'cdc'
 
 
 def run():

superduperdb/server/cluster.py
@@ -1,0 +1,61 @@
+import uvicorn
+
+
+def _superduper_local_service(name, app, cfg=None):
+    from superduperdb.server.app import Server
+
+    service = getattr(cfg.cluster, name)
+    assert isinstance(service, str)
+    port = int(service.split(':')[-1])
+
+    app.pre_start(cfg)
+    config = uvicorn.Config(app.app, port=port, log_level="info")
+    return Server(config=config)
+
+
+class _Client:
+    def __init__(self, db, services=()):
+        self.db = db
+        self.services = services
+        for service in services:
+            service.run_in_thread()
+
+    def close(self):
+        self.db.compute.shutdown()
+        for service in self.services:
+            service.stop()
+
+
+def local_cluster(db):
+    '''
+    This method is used to create a local cluster consisting of
+    Vector search service, cdc service and dask setup.
+
+    Once this cluster is up, user can offload vector search,
+    cdc on these service.
+    '''
+    from superduperdb import CFG
+
+    # vector search local service
+    CFG.force_set('cluster.vector_search', 'http://localhost:8000')
+    CFG.force_set('cluster.cdc', 'http://localhost:8001')
+    CFG.force_set('cluster.compute', 'dask+thread')
+
+    from superduperdb.vector_search.server.app import app as vector_search_app
+
+    vector_search_server = _superduper_local_service(
+        'vector_search', vector_search_app, CFG
+    )
+
+    # cdc local service
+    from superduperdb.cdc.app import app as cdc_app
+
+    cdc_server = _superduper_local_service('cdc', cdc_app, CFG)
+
+    # local compute
+    from superduperdb.backends.dask.compute import DaskComputeBackend
+
+    local_compute = DaskComputeBackend('', local=True)
+    db.set_compute(local_compute)
+
+    return _Client(db=db, services=(cdc_server, vector_search_server))

test/integration/test_vector_search_server.py
@@ -5,13 +5,19 @@ from fastapi.testclient import TestClient
 
 
 @pytest.fixture
-def client(database_with_default_encoders_and_model):
+def client(monkeypatch, database_with_default_encoders_and_model):
+    from superduperdb import CFG
+
+    vector_search = 'http://localhost:8000'
+    monkeypatch.setattr(CFG.cluster, 'vector_search', vector_search)
+
     from superduperdb.vector_search.server.app import app
 
     app.app.state.pool = database_with_default_encoders_and_model
     client = TestClient(app.app)
 
     yield client
+    monkeypatch.setattr(CFG.cluster, 'vector_search', None)
 
 
 def test_basic_workflow(client):

Pinnacle-Networks/Pinnacle

Former-commit-id: ea8ec01d288a3a86dc9c77d963b4f0bf90fed55d

superduperdb/__main__.py
@@ -2,10 +2,10 @@ import sys
 
 import click
 
-from pinnacledb.cli import app, config, info, vector_search
-from pinnacledb.cli.serve import local_cluster, serve
+from pinnacledb.cli import app, config, info
+from pinnacledb.cli.serve import cdc, local_cluster, vector_search
 
-__all__ = 'config', 'info', 'local_cluster', 'serve', 'vector_search'
+__all__ = 'config', 'info', 'local_cluster', 'vector_search', 'cdc'
 
 
 def run():

superduperdb/server/cluster.py
@@ -1,0 +1,61 @@
+import uvicorn
+
+
+def _pinnacle_local_service(name, app, cfg=None):
+    from pinnacledb.server.app import Server
+
+    service = getattr(cfg.cluster, name)
+    assert isinstance(service, str)
+    port = int(service.split(':')[-1])
+
+    app.pre_start(cfg)
+    config = uvicorn.Config(app.app, port=port, log_level="info")
+    return Server(config=config)
+
+
+class _Client:
+    def __init__(self, db, services=()):
+        self.db = db
+        self.services = services
+        for service in services:
+            service.run_in_thread()
+
+    def close(self):
+        self.db.compute.shutdown()
+        for service in self.services:
+            service.stop()
+
+
+def local_cluster(db):
+    '''
+    This method is used to create a local cluster consisting of
+    Vector search service, cdc service and dask setup.
+
+    Once this cluster is up, user can offload vector search,
+    cdc on these service.
+    '''
+    from pinnacledb import CFG
+
+    # vector search local service
+    CFG.force_set('cluster.vector_search', 'http://localhost:8000')
+    CFG.force_set('cluster.cdc', 'http://localhost:8001')
+    CFG.force_set('cluster.compute', 'dask+thread')
+
+    from pinnacledb.vector_search.server.app import app as vector_search_app
+
+    vector_search_server = _pinnacle_local_service(
+        'vector_search', vector_search_app, CFG
+    )
+
+    # cdc local service
+    from pinnacledb.cdc.app import app as cdc_app
+
+    cdc_server = _pinnacle_local_service('cdc', cdc_app, CFG)
+
+    # local compute
+    from pinnacledb.backends.dask.compute import DaskComputeBackend
+
+    local_compute = DaskComputeBackend('', local=True)
+    db.set_compute(local_compute)
+
+    return _Client(db=db, services=(cdc_server, vector_search_server))

test/integration/test_vector_search_server.py
@@ -5,13 +5,19 @@ from fastapi.testclient import TestClient
 
 
 @pytest.fixture
-def client(database_with_default_encoders_and_model):
+def client(monkeypatch, database_with_default_encoders_and_model):
+    from pinnacledb import CFG
+
+    vector_search = 'http://localhost:8000'
+    monkeypatch.setattr(CFG.cluster, 'vector_search', vector_search)
+
     from pinnacledb.vector_search.server.app import app
 
     app.app.state.pool = database_with_default_encoders_and_model
     client = TestClient(app.app)
 
     yield client
+    monkeypatch.setattr(CFG.cluster, 'vector_search', None)
 
 
 def test_basic_workflow(client):