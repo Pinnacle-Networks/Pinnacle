superduper-io/superduper

Add public api annotations

superduperdb/components/encoder.py
@@ -5,6 +5,7 @@ import typing as t
 
 from superduperdb.base.artifact import Artifact
 from superduperdb.components.component import Component
+from superduperdb.misc.annotations import public_api
 
 Decode = t.Callable[[bytes], t.Any]
 Encode = t.Callable[[t.Any], bytes]
@@ -20,6 +21,7 @@ def _pickle_encoder(x: t.Any) -> bytes:
     return f.getvalue()
 
 
+@public_api(stability='stable')
 @dc.dataclass
 class Encoder(Component):
     """

superduperdb/components/metric.py
@@ -3,8 +3,10 @@ import typing as t
 
 from superduperdb.base.artifact import Artifact
 from superduperdb.components.component import Component
+from superduperdb.misc.annotations import public_api
 
 
+@public_api(stability='beta')
 @dc.dataclass
 class Metric(Component):
     """

superduperdb/components/model.py
@@ -24,6 +24,7 @@ from superduperdb.components.encoder import Encoder
 from superduperdb.components.metric import Metric
 from superduperdb.components.schema import Schema
 from superduperdb.jobs.job import ComponentJob, Job
+from superduperdb.misc.annotations import public_api
 from superduperdb.misc.special_dicts import MongoStyleDict
 
 if t.TYPE_CHECKING:
@@ -68,7 +69,7 @@ class _TrainingConfiguration(Component):
             return self.kwargs.get(k, default)
 
 
-class Predictor:
+class _Predictor:
     """
     Mixin class for components which can predict.
 
@@ -417,8 +418,9 @@ class Predictor:
         )
 
 
+@public_api(stability='stable')
 @dc.dataclass
-class Model(Component, Predictor):
+class Model(Component, _Predictor):
     """Model component which wraps a model to become serializable
 
     :param identifier: Unique identifier of model
@@ -708,8 +710,9 @@ def TrainingConfiguration(identifier: str, **kwargs):
     return _TrainingConfiguration(identifier=identifier, kwargs=kwargs)
 
 
+@public_api(stability='beta')
 @dc.dataclass
-class APIModel(Component, Predictor):
+class APIModel(Component, _Predictor):
     '''
     A Component for representing api models like openai, cohere etc
     :param model: The model to use, e.g. ``'text-embedding-ada-002'``.

superduperdb/components/schema.py
@@ -5,8 +5,10 @@ from functools import cached_property
 from superduperdb.backends.ibis.field_types import dtype
 from superduperdb.components.component import Component
 from superduperdb.components.encoder import Encoder
+from superduperdb.misc.annotations import public_api
 
 
+@public_api(stability='beta')
 @dc.dataclass
 class Schema(Component):
     identifier: str

superduperdb/components/serializer.py
@@ -2,12 +2,14 @@ import dataclasses as dc
 import typing as t
 
 from superduperdb.components.component import Component
+from superduperdb.misc.annotations import public_api
 from superduperdb.misc.serialization import serializers
 
 if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
 
 
+@public_api(stability='beta')
 @dc.dataclass
 class Serializer(Component):
     identifier: str

superduperdb/components/stack.py
@@ -6,6 +6,7 @@ import typing as t
 
 from superduperdb.backends.local.artifacts import FileSystemArtifactStore
 from superduperdb.base.serializable import Serializable
+from superduperdb.misc.annotations import public_api
 
 from .component import Component
 
@@ -17,6 +18,7 @@ ARTIFACTS = 'artifacts'
 _STACK_JSON_FILE = 'stack.json'
 
 
+@public_api(stability='alpha')
 @dc.dataclass
 class Stack(Component):
     """

superduperdb/components/vector_index.py
@@ -11,6 +11,7 @@ from superduperdb.components.component import Component
 from superduperdb.components.encoder import Encoder
 from superduperdb.components.listener import Listener
 from superduperdb.ext.utils import str_shape
+from superduperdb.misc.annotations import public_api
 from superduperdb.misc.special_dicts import MongoStyleDict
 from superduperdb.vector_search.base import VectorIndexMeasureType
 
@@ -18,6 +19,7 @@ if t.TYPE_CHECKING:
     pass
 
 
+@public_api(stability='stable')
 @dc.dataclass
 class VectorIndex(Component):
     """

Pinnacle-Networks/Pinnacle

superduperdb/components/encoder.py
@@ -5,6 +5,7 @@ import typing as t
 
 from pinnacledb.base.artifact import Artifact
 from pinnacledb.components.component import Component
+from pinnacledb.misc.annotations import public_api
 
 Decode = t.Callable[[bytes], t.Any]
 Encode = t.Callable[[t.Any], bytes]
@@ -20,6 +21,7 @@ def _pickle_encoder(x: t.Any) -> bytes:
     return f.getvalue()
 
 
+@public_api(stability='stable')
 @dc.dataclass
 class Encoder(Component):
     """

superduperdb/components/metric.py
@@ -3,8 +3,10 @@ import typing as t
 
 from pinnacledb.base.artifact import Artifact
 from pinnacledb.components.component import Component
+from pinnacledb.misc.annotations import public_api
 
 
+@public_api(stability='beta')
 @dc.dataclass
 class Metric(Component):
     """

superduperdb/components/model.py
@@ -24,6 +24,7 @@ from pinnacledb.components.encoder import Encoder
 from pinnacledb.components.metric import Metric
 from pinnacledb.components.schema import Schema
 from pinnacledb.jobs.job import ComponentJob, Job
+from pinnacledb.misc.annotations import public_api
 from pinnacledb.misc.special_dicts import MongoStyleDict
 
 if t.TYPE_CHECKING:
@@ -68,7 +69,7 @@ class _TrainingConfiguration(Component):
             return self.kwargs.get(k, default)
 
 
-class Predictor:
+class _Predictor:
     """
     Mixin class for components which can predict.
 
@@ -417,8 +418,9 @@ class Predictor:
         )
 
 
+@public_api(stability='stable')
 @dc.dataclass
-class Model(Component, Predictor):
+class Model(Component, _Predictor):
     """Model component which wraps a model to become serializable
 
     :param identifier: Unique identifier of model
@@ -708,8 +710,9 @@ def TrainingConfiguration(identifier: str, **kwargs):
     return _TrainingConfiguration(identifier=identifier, kwargs=kwargs)
 
 
+@public_api(stability='beta')
 @dc.dataclass
-class APIModel(Component, Predictor):
+class APIModel(Component, _Predictor):
     '''
     A Component for representing api models like openai, cohere etc
     :param model: The model to use, e.g. ``'text-embedding-ada-002'``.

superduperdb/components/schema.py
@@ -5,8 +5,10 @@ from functools import cached_property
 from pinnacledb.backends.ibis.field_types import dtype
 from pinnacledb.components.component import Component
 from pinnacledb.components.encoder import Encoder
+from pinnacledb.misc.annotations import public_api
 
 
+@public_api(stability='beta')
 @dc.dataclass
 class Schema(Component):
     identifier: str

superduperdb/components/serializer.py
@@ -2,12 +2,14 @@ import dataclasses as dc
 import typing as t
 
 from pinnacledb.components.component import Component
+from pinnacledb.misc.annotations import public_api
 from pinnacledb.misc.serialization import serializers
 
 if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
 
 
+@public_api(stability='beta')
 @dc.dataclass
 class Serializer(Component):
     identifier: str

superduperdb/components/stack.py
@@ -6,6 +6,7 @@ import typing as t
 
 from pinnacledb.backends.local.artifacts import FileSystemArtifactStore
 from pinnacledb.base.serializable import Serializable
+from pinnacledb.misc.annotations import public_api
 
 from .component import Component
 
@@ -17,6 +18,7 @@ ARTIFACTS = 'artifacts'
 _STACK_JSON_FILE = 'stack.json'
 
 
+@public_api(stability='alpha')
 @dc.dataclass
 class Stack(Component):
     """

superduperdb/components/vector_index.py
@@ -11,6 +11,7 @@ from pinnacledb.components.component import Component
 from pinnacledb.components.encoder import Encoder
 from pinnacledb.components.listener import Listener
 from pinnacledb.ext.utils import str_shape
+from pinnacledb.misc.annotations import public_api
 from pinnacledb.misc.special_dicts import MongoStyleDict
 from pinnacledb.vector_search.base import VectorIndexMeasureType
 
@@ -18,6 +19,7 @@ if t.TYPE_CHECKING:
     pass
 
 
+@public_api(stability='stable')
 @dc.dataclass
 class VectorIndex(Component):
     """