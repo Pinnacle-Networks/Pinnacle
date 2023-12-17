superduper-io/superduper

Add ray compute

superduperdb/backends/ray/compute.py
@@ -1,0 +1,101 @@
+import typing as t
+
+import ray
+
+from superduperdb import logging
+from superduperdb.backends.base.compute import ComputeBackend
+
+
+class RayComputeBackend(ComputeBackend):
+    """
+    A client for interacting with a ray cluster. Initialize the ray client.
+
+    :param address: The address of the ray cluster.
+    :param **kwargs: Additional keyword arguments to be passed to the ray client.
+    """
+
+    def __init__(
+        self,
+        address: t.Optional[str] = None,
+        **kwargs,
+    ):
+        self._futures_collection: t.Dict[str, ray.ObjectRef] = {}
+
+        ray.init(address=address, **kwargs)
+
+    @property
+    def type(self) -> str:
+        return "distributed"
+
+    @property
+    def name(self) -> str:
+        return "ray"
+
+    def submit(self, function: t.Callable, *args, **kwargs) -> ray.ObjectRef:
+        """
+        Submits a function to the ray server for execution.
+
+        :param function: The function to be executed.
+        """
+
+        def _dependable_remote_job(function, *args, **kwargs):
+            if (
+                function.__name__ in ['method_job', 'callable_job']
+                and 'dependencies' in kwargs
+            ):
+                dependencies = kwargs['dependencies']
+                if dependencies:
+                    ray.wait(dependencies)
+            return function(*args, **kwargs)
+
+        remote_function = ray.remote(_dependable_remote_job)
+        future = remote_function.remote(function, *args, **kwargs)
+        self._futures_collection[future.task_id().hex()] = future
+
+        logging.success(f"Job submitted.  function:{function} future:{future}")
+        return future
+
+    @property
+    def tasks(self) -> t.Dict[str, ray.ObjectRef]:
+        """
+        List all pending tasks
+        """
+        return self._futures_collection
+
+    def wait(self, identifier: str) -> None:
+        """
+        Waits for task corresponding to identifier to complete.
+        :param identifier: Future task id to wait
+        """
+        ray.wait([self._futures_collection[identifier]])
+
+    def wait_all(self) -> None:
+        """
+        Waits for all tasks to complete.
+        """
+        ray.wait(
+            list(self._futures_collection.values()),
+            num_returns=len(self._futures_collection),
+        )
+
+    def result(self, identifier: str) -> t.Any:
+        """
+        Retrieves the result of a previously submitted task.
+        Note: This will block until the future is completed.
+
+        :param identifier: The identifier of the submitted task.
+        """
+        future = self._futures_collection[identifier]
+        return ray.get(future)
+
+    def disconnect(self) -> None:
+        """
+        Disconnect the ray client.
+        """
+        ray.shutdown()
+
+    def shutdown(self) -> None:
+        """
+        Shuts down the ray cluster.
+        """
+        ray.shutdown()

superduperdb/base/build.py
@@ -13,6 +13,7 @@ from superduperdb.backends.base.backends import data_backends, metadata_stores
 from superduperdb.backends.local.artifacts import FileSystemArtifactStore
 from superduperdb.backends.local.compute import LocalComputeBackend
 from superduperdb.backends.mongodb.artifacts import MongoArtifactStore
+from superduperdb.backends.ray.compute import RayComputeBackend
 from superduperdb.base.datalayer import Datalayer
 
 
@@ -110,6 +111,9 @@ def build_compute(compute):
 
         uri = compute.split('+')[-1]
         return DaskComputeBackend(uri)
+
+    if compute.split('://')[0] == 'ray':
+        return RayComputeBackend(compute)
 
     return LocalComputeBackend()
 

test/integration/conftest.py
@@ -1,4 +1,5 @@
 import random
+from pathlib import Path
 
 import numpy as np
 import pytest
@@ -116,6 +117,23 @@ def dask_client(monkeypatch, request):
     client = DaskComputeBackend(
         address='tcp://localhost:8786',
         local=False,
+    )
+
+    yield client
+
+    client.disconnect()
+
+
+@pytest.fixture(scope='session')
+def ray_client():
+    # Change the default value
+    from superduperdb.backends.ray.compute import RayComputeBackend
+
+    working_dir = Path(__file__).parents[1]
+
+    client = RayComputeBackend(
+        address='ray://127.0.0.1:10001',
+        runtime_env={"working_dir": working_dir, 'excludes': ['unittest']},
     )
 
     yield client

Pinnacle-Networks/Pinnacle

superduperdb/backends/ray/compute.py
@@ -1,0 +1,101 @@
+import typing as t
+
+import ray
+
+from pinnacledb import logging
+from pinnacledb.backends.base.compute import ComputeBackend
+
+
+class RayComputeBackend(ComputeBackend):
+    """
+    A client for interacting with a ray cluster. Initialize the ray client.
+
+    :param address: The address of the ray cluster.
+    :param **kwargs: Additional keyword arguments to be passed to the ray client.
+    """
+
+    def __init__(
+        self,
+        address: t.Optional[str] = None,
+        **kwargs,
+    ):
+        self._futures_collection: t.Dict[str, ray.ObjectRef] = {}
+
+        ray.init(address=address, **kwargs)
+
+    @property
+    def type(self) -> str:
+        return "distributed"
+
+    @property
+    def name(self) -> str:
+        return "ray"
+
+    def submit(self, function: t.Callable, *args, **kwargs) -> ray.ObjectRef:
+        """
+        Submits a function to the ray server for execution.
+
+        :param function: The function to be executed.
+        """
+
+        def _dependable_remote_job(function, *args, **kwargs):
+            if (
+                function.__name__ in ['method_job', 'callable_job']
+                and 'dependencies' in kwargs
+            ):
+                dependencies = kwargs['dependencies']
+                if dependencies:
+                    ray.wait(dependencies)
+            return function(*args, **kwargs)
+
+        remote_function = ray.remote(_dependable_remote_job)
+        future = remote_function.remote(function, *args, **kwargs)
+        self._futures_collection[future.task_id().hex()] = future
+
+        logging.success(f"Job submitted.  function:{function} future:{future}")
+        return future
+
+    @property
+    def tasks(self) -> t.Dict[str, ray.ObjectRef]:
+        """
+        List all pending tasks
+        """
+        return self._futures_collection
+
+    def wait(self, identifier: str) -> None:
+        """
+        Waits for task corresponding to identifier to complete.
+        :param identifier: Future task id to wait
+        """
+        ray.wait([self._futures_collection[identifier]])
+
+    def wait_all(self) -> None:
+        """
+        Waits for all tasks to complete.
+        """
+        ray.wait(
+            list(self._futures_collection.values()),
+            num_returns=len(self._futures_collection),
+        )
+
+    def result(self, identifier: str) -> t.Any:
+        """
+        Retrieves the result of a previously submitted task.
+        Note: This will block until the future is completed.
+
+        :param identifier: The identifier of the submitted task.
+        """
+        future = self._futures_collection[identifier]
+        return ray.get(future)
+
+    def disconnect(self) -> None:
+        """
+        Disconnect the ray client.
+        """
+        ray.shutdown()
+
+    def shutdown(self) -> None:
+        """
+        Shuts down the ray cluster.
+        """
+        ray.shutdown()

superduperdb/base/build.py
@@ -13,6 +13,7 @@ from pinnacledb.backends.base.backends import data_backends, metadata_stores
 from pinnacledb.backends.local.artifacts import FileSystemArtifactStore
 from pinnacledb.backends.local.compute import LocalComputeBackend
 from pinnacledb.backends.mongodb.artifacts import MongoArtifactStore
+from pinnacledb.backends.ray.compute import RayComputeBackend
 from pinnacledb.base.datalayer import Datalayer
 
 
@@ -110,6 +111,9 @@ def build_compute(compute):
 
         uri = compute.split('+')[-1]
         return DaskComputeBackend(uri)
+
+    if compute.split('://')[0] == 'ray':
+        return RayComputeBackend(compute)
 
     return LocalComputeBackend()
 

test/integration/conftest.py
@@ -1,4 +1,5 @@
 import random
+from pathlib import Path
 
 import numpy as np
 import pytest
@@ -116,6 +117,23 @@ def dask_client(monkeypatch, request):
     client = DaskComputeBackend(
         address='tcp://localhost:8786',
         local=False,
+    )
+
+    yield client
+
+    client.disconnect()
+
+
+@pytest.fixture(scope='session')
+def ray_client():
+    # Change the default value
+    from pinnacledb.backends.ray.compute import RayComputeBackend
+
+    working_dir = Path(__file__).parents[1]
+
+    client = RayComputeBackend(
+        address='ray://127.0.0.1:10001',
+        runtime_env={"working_dir": working_dir, 'excludes': ['unittest']},
     )
 
     yield client