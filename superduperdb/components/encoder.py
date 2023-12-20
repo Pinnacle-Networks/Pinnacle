superduper-io/superduper

Fix snowflake bugs and create use-case

superduperdb/components/encoder.py
@@ -1,9 +1,12 @@
+import base64
 import dataclasses as dc
 import io
 import pickle
 import typing as t
 
+from superduperdb import CFG
 from superduperdb.base.artifact import Artifact
+from superduperdb.base.config import BytesEncoding
 from superduperdb.components.component import Component
 from superduperdb.misc.annotations import public_api
 
@@ -69,12 +72,22 @@ class Encoder(Component):
     ) -> 'Encodable':
         return Encodable(self, x=x, uri=uri)
 
-    def decode(self, b: bytes) -> t.Any:
+    def decode(self, b: t.Union[bytes, str]) -> t.Any:
         assert isinstance(self.decoder, Artifact)
+        if CFG.bytes_encoding == BytesEncoding.BASE64:
+            b = self.from_base64(b)
         return self(self.decoder.artifact(b))
 
     def dump(self, other):
         return self.encoder.artifact(other)
+
+    @staticmethod
+    def to_base64(bytes):
+        return base64.b64encode(bytes).decode('utf-8')
+
+    @staticmethod
+    def from_base64(encoded):
+        return base64.b64decode(encoded)
 
     def encode(
         self,
@@ -83,10 +96,17 @@ class Encoder(Component):
         wrap: bool = True,
     ) -> t.Union[t.Optional[str], t.Dict[str, t.Any]]:
         # TODO clarify what is going on here
+
+        def _encode(x):
+            bytes_ = self.encoder.artifact(x)
+            if CFG.bytes_encoding == BytesEncoding.BASE64:
+                bytes_ = self.to_base64(bytes_)
+            return bytes_
+
         def _wrap_content(x):
             return {
                 '_content': {
-                    'bytes': self.encoder.artifact(x),
+                    'bytes': _encode(x),
                     'encoder': self.identifier,
                 }
             }
@@ -95,7 +115,7 @@ class Encoder(Component):
             if x is not None:
                 if wrap:
                     return _wrap_content(x)
-                return self.encoder.artifact(x)  # type: ignore[union-attr]
+                return _encode(x)
             else:
                 if wrap:
                     return {

superduperdb/components/schema.py
@@ -3,6 +3,7 @@ import typing as t
 from functools import cached_property
 
 from superduperdb.backends.ibis.field_types import dtype
+from superduperdb.base.configs import CFG
 from superduperdb.components.component import Component
 from superduperdb.components.encoder import Encoder
 from superduperdb.misc.annotations import public_api
@@ -40,7 +41,7 @@ class Schema(Component):
     @property
     def raw(self):
         return {
-            k: (v.identifier if not isinstance(v, Encoder) else 'Bytes')
+            k: (v.identifier if not isinstance(v, Encoder) else CFG.bytes_encoding)
             for k, v in self.fields.items()
         }
 

superduperdb/ext/sentence_transformers/model.py
@@ -1,0 +1,27 @@
+import dataclasses as dc
+import typing as t
+
+import sentence_transformers
+
+from superduperdb.base.artifact import Artifact
+from superduperdb.components.model import Model
+
+
+@dc.dataclass(kw_only=True)
+class SentenceTransformer(Model):
+    object: t.Union[Artifact, t.Callable, None] = None
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.object is None:
+            self.object = Artifact(
+                artifact=sentence_transformers.SentenceTransformer(
+                    self.identifier, device=self.device
+                )
+            )
+        self.object.artifact = self.object.artifact.to(self.device)
+        self.model_to_device_method = '_to'
+
+    def _to(self, device):
+        self.object.artifact = self.object.artifact.to(device)
+        self.object.artifact._target_device = device

Pinnacle-Networks/Pinnacle

superduperdb/components/encoder.py
@@ -1,9 +1,12 @@
+import base64
 import dataclasses as dc
 import io
 import pickle
 import typing as t
 
+from pinnacledb import CFG
 from pinnacledb.base.artifact import Artifact
+from pinnacledb.base.config import BytesEncoding
 from pinnacledb.components.component import Component
 from pinnacledb.misc.annotations import public_api
 
@@ -69,12 +72,22 @@ class Encoder(Component):
     ) -> 'Encodable':
         return Encodable(self, x=x, uri=uri)
 
-    def decode(self, b: bytes) -> t.Any:
+    def decode(self, b: t.Union[bytes, str]) -> t.Any:
         assert isinstance(self.decoder, Artifact)
+        if CFG.bytes_encoding == BytesEncoding.BASE64:
+            b = self.from_base64(b)
         return self(self.decoder.artifact(b))
 
     def dump(self, other):
         return self.encoder.artifact(other)
+
+    @staticmethod
+    def to_base64(bytes):
+        return base64.b64encode(bytes).decode('utf-8')
+
+    @staticmethod
+    def from_base64(encoded):
+        return base64.b64decode(encoded)
 
     def encode(
         self,
@@ -83,10 +96,17 @@ class Encoder(Component):
         wrap: bool = True,
     ) -> t.Union[t.Optional[str], t.Dict[str, t.Any]]:
         # TODO clarify what is going on here
+
+        def _encode(x):
+            bytes_ = self.encoder.artifact(x)
+            if CFG.bytes_encoding == BytesEncoding.BASE64:
+                bytes_ = self.to_base64(bytes_)
+            return bytes_
+
         def _wrap_content(x):
             return {
                 '_content': {
-                    'bytes': self.encoder.artifact(x),
+                    'bytes': _encode(x),
                     'encoder': self.identifier,
                 }
             }
@@ -95,7 +115,7 @@ class Encoder(Component):
             if x is not None:
                 if wrap:
                     return _wrap_content(x)
-                return self.encoder.artifact(x)  # type: ignore[union-attr]
+                return _encode(x)
             else:
                 if wrap:
                     return {

superduperdb/components/schema.py
@@ -3,6 +3,7 @@ import typing as t
 from functools import cached_property
 
 from pinnacledb.backends.ibis.field_types import dtype
+from pinnacledb.base.configs import CFG
 from pinnacledb.components.component import Component
 from pinnacledb.components.encoder import Encoder
 from pinnacledb.misc.annotations import public_api
@@ -40,7 +41,7 @@ class Schema(Component):
     @property
     def raw(self):
         return {
-            k: (v.identifier if not isinstance(v, Encoder) else 'Bytes')
+            k: (v.identifier if not isinstance(v, Encoder) else CFG.bytes_encoding)
             for k, v in self.fields.items()
         }
 

superduperdb/ext/sentence_transformers/model.py
@@ -1,0 +1,27 @@
+import dataclasses as dc
+import typing as t
+
+import sentence_transformers
+
+from pinnacledb.base.artifact import Artifact
+from pinnacledb.components.model import Model
+
+
+@dc.dataclass(kw_only=True)
+class SentenceTransformer(Model):
+    object: t.Union[Artifact, t.Callable, None] = None
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.object is None:
+            self.object = Artifact(
+                artifact=sentence_transformers.SentenceTransformer(
+                    self.identifier, device=self.device
+                )
+            )
+        self.object.artifact = self.object.artifact.to(self.device)
+        self.model_to_device_method = '_to'
+
+    def _to(self, device):
+        self.object.artifact = self.object.artifact.to(device)
+        self.object.artifact._target_device = device