superduper-io/superduper

Add UT for llm api: OpenAI, vLLM

superduperdb/ext/llm/base.py
@@ -2,6 +2,7 @@ import abc
 import asyncio
 import dataclasses as dc
 import inspect
+import typing
 from functools import wraps
 from logging import WARNING, getLogger
 from typing import Any, Callable, List, Optional, Union
@@ -10,6 +11,9 @@ from superduperdb import logging
 from superduperdb.components.component import Component
 from superduperdb.components.model import _Predictor
 from superduperdb.ext.utils import format_prompt
+
+if typing.TYPE_CHECKING:
+    from superduperdb.base.datalayer import Datalayer
 
 # Disable httpx info level logging
 getLogger("httpx").setLevel(WARNING)
@@ -51,7 +55,25 @@ class _BaseLLM(Component, _Predictor, metaclass=abc.ABCMeta):
     def __post_init__(self):
         super().__post_init__()
         self.takes_context = True
+        self.identifier = self.identifier.replace("/", "-")
         assert "{input}" in self.prompt_template, "Template must contain {input}"
+
+    def to_call(self, X, *args, **kwargs):
+        raise NotImplementedError
+
+    def post_create(self, db: "Datalayer") -> None:
+        # TODO: Do not make sense to add this logic here,
+        # Need a auto DataType to handle this
+        from superduperdb.backends.ibis.data_backend import IbisDataBackend
+        from superduperdb.backends.ibis.field_types import dtype
+
+        if isinstance(db.databackend, IbisDataBackend) and self.encoder is None:
+            self.encoder = dtype('str')
+
+        # since then the `.add` clause is not necessary
+        output_component = db.databackend.create_model_table_or_collection(self)  # type: ignore[arg-type]
+        if output_component is not None:
+            db.add(output_component)
 
     @abc.abstractmethod
     def init(self):

test/unittest/ext/llm/test_openai.py
@@ -1,0 +1,39 @@
+from test.db_config import DBConfig
+from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predict
+
+import pytest
+import vcr
+
+from superduperdb.ext.llm.openai import OpenAI
+
+CASSETTE_DIR = "test/unittest/ext/cassettes/llm/openai"
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_predict.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_predict(db):
+    """Test chat."""
+    check_predict(db, OpenAI(model_name="gpt-3.5-turbo"))
+    check_predict(
+        db, OpenAI(identifier="chat-llm", model_name="gpt-3.5-turbo", chat=True)
+    )
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_llm_as_listener_model.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_llm_as_listener_model(db):
+    check_llm_as_listener_model(db, OpenAI(model_name="gpt-3.5-turbo"))

test/unittest/ext/llm/test_vllm.py
@@ -1,0 +1,79 @@
+from test.db_config import DBConfig
+from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predict
+
+import pytest
+import vcr
+
+from superduperdb.ext.llm.vllm import VllmAPI, VllmOpenAI
+
+CASSETTE_DIR = "test/unittest/ext/cassettes/llm/vllm"
+
+model_name = "mistralai/Mistral-7B-Instruct-v0.2"
+openai_api_base = "http://ec2-54-208-130-192.compute-1.amazonaws.com:8000/v1"
+api_url = "http://ec2-54-208-130-192.compute-1.amazonaws.com:8000/generate"
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_predict_openai.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_predict_openai(db):
+    """Test chat."""
+    check_predict(
+        db, VllmOpenAI(model_name=model_name, openai_api_base=openai_api_base)
+    )
+    check_predict(
+        db,
+        VllmOpenAI(
+            identifier="chat-llm",
+            model_name=model_name,
+            openai_api_base=openai_api_base,
+            chat=True,
+        ),
+    )
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_llm_as_listener_model_openai.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize("db", [DBConfig.sqldb_empty], indirect=True)
+def test_llm_as_listener_model_openai(db):
+    check_llm_as_listener_model(
+        db,
+        VllmOpenAI(model_name=model_name, openai_api_base=openai_api_base),
+    )
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_predict_api.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_predict_api(db):
+    """Test chat."""
+    check_predict(db, VllmAPI(identifier="llm", api_url=api_url))
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_llm_as_listener_model_openapi.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_llm_as_listener_model_openapi(db):
+    check_llm_as_listener_model(db, VllmAPI(identifier="llm", api_url=api_url))

test/unittest/ext/llm/utils.py
@@ -1,0 +1,62 @@
+"""
+LLM model test cases.
+All the llm model can use the check_xxx func to test the intergration with db.
+"""
+
+from superduperdb.backends.ibis.field_types import dtype
+from superduperdb.backends.ibis.query import Schema, Table
+from superduperdb.backends.mongodb.data_backend import MongoDataBackend
+from superduperdb.backends.mongodb.query import Collection
+from superduperdb.base.document import Document
+from superduperdb.components.listener import Listener
+
+
+def check_predict(db, llm):
+    """Test chat."""
+    db.add(llm)
+    result = db.predict(llm.identifier, "1+1=")[0].content
+    assert isinstance(result, str)
+
+    results = db.predict(llm.identifier, ["1+1=", "2+2="])[0].content
+
+    assert isinstance(results, list)
+    assert len(results) == 2
+
+
+def check_llm_as_listener_model(db, llm):
+    collection_name = "question"
+    datas = [Document({"question": f"1+{i}=", "id": str(i)}) for i in range(10)]
+    if isinstance(db.databackend, MongoDataBackend):
+        db.execute(Collection(collection_name).insert_many(datas))
+        output_select = select = Collection(collection_name).find()
+    else:
+        schema = Schema(
+            identifier=collection_name,
+            fields={
+                "id": dtype("str"),
+                "question": dtype("str"),
+            },
+        )
+        table = Table(identifier=collection_name, schema=schema)
+        db.add(table)
+        db.execute(table.insert(datas))
+        select = table.select("id", "question")
+        output_select = table.select("id", "question").outputs(question=llm.identifier)
+
+    db.add(llm)
+
+    db.add(
+        Listener(
+            select=select,
+            key="question",
+            model=llm.identifier,
+        )
+    )
+
+    results = db.execute(output_select)
+    for result in results:
+        try:
+            output = result.outputs("question", llm.identifier)
+        except KeyError:
+            output = result.unpack()[f'_outputs.question.{llm.identifier}.0']
+        assert isinstance(output, str)

Pinnacle-Networks/Pinnacle

superduperdb/ext/llm/base.py
@@ -2,6 +2,7 @@ import abc
 import asyncio
 import dataclasses as dc
 import inspect
+import typing
 from functools import wraps
 from logging import WARNING, getLogger
 from typing import Any, Callable, List, Optional, Union
@@ -10,6 +11,9 @@ from pinnacledb import logging
 from pinnacledb.components.component import Component
 from pinnacledb.components.model import _Predictor
 from pinnacledb.ext.utils import format_prompt
+
+if typing.TYPE_CHECKING:
+    from pinnacledb.base.datalayer import Datalayer
 
 # Disable httpx info level logging
 getLogger("httpx").setLevel(WARNING)
@@ -51,7 +55,25 @@ class _BaseLLM(Component, _Predictor, metaclass=abc.ABCMeta):
     def __post_init__(self):
         super().__post_init__()
         self.takes_context = True
+        self.identifier = self.identifier.replace("/", "-")
         assert "{input}" in self.prompt_template, "Template must contain {input}"
+
+    def to_call(self, X, *args, **kwargs):
+        raise NotImplementedError
+
+    def post_create(self, db: "Datalayer") -> None:
+        # TODO: Do not make sense to add this logic here,
+        # Need a auto DataType to handle this
+        from pinnacledb.backends.ibis.data_backend import IbisDataBackend
+        from pinnacledb.backends.ibis.field_types import dtype
+
+        if isinstance(db.databackend, IbisDataBackend) and self.encoder is None:
+            self.encoder = dtype('str')
+
+        # since then the `.add` clause is not necessary
+        output_component = db.databackend.create_model_table_or_collection(self)  # type: ignore[arg-type]
+        if output_component is not None:
+            db.add(output_component)
 
     @abc.abstractmethod
     def init(self):

test/unittest/ext/llm/test_openai.py
@@ -1,0 +1,39 @@
+from test.db_config import DBConfig
+from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predict
+
+import pytest
+import vcr
+
+from pinnacledb.ext.llm.openai import OpenAI
+
+CASSETTE_DIR = "test/unittest/ext/cassettes/llm/openai"
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_predict.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_predict(db):
+    """Test chat."""
+    check_predict(db, OpenAI(model_name="gpt-3.5-turbo"))
+    check_predict(
+        db, OpenAI(identifier="chat-llm", model_name="gpt-3.5-turbo", chat=True)
+    )
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_llm_as_listener_model.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_llm_as_listener_model(db):
+    check_llm_as_listener_model(db, OpenAI(model_name="gpt-3.5-turbo"))

test/unittest/ext/llm/test_vllm.py
@@ -1,0 +1,79 @@
+from test.db_config import DBConfig
+from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predict
+
+import pytest
+import vcr
+
+from pinnacledb.ext.llm.vllm import VllmAPI, VllmOpenAI
+
+CASSETTE_DIR = "test/unittest/ext/cassettes/llm/vllm"
+
+model_name = "mistralai/Mistral-7B-Instruct-v0.2"
+openai_api_base = "http://ec2-54-208-130-192.compute-1.amazonaws.com:8000/v1"
+api_url = "http://ec2-54-208-130-192.compute-1.amazonaws.com:8000/generate"
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_predict_openai.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_predict_openai(db):
+    """Test chat."""
+    check_predict(
+        db, VllmOpenAI(model_name=model_name, openai_api_base=openai_api_base)
+    )
+    check_predict(
+        db,
+        VllmOpenAI(
+            identifier="chat-llm",
+            model_name=model_name,
+            openai_api_base=openai_api_base,
+            chat=True,
+        ),
+    )
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_llm_as_listener_model_openai.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize("db", [DBConfig.sqldb_empty], indirect=True)
+def test_llm_as_listener_model_openai(db):
+    check_llm_as_listener_model(
+        db,
+        VllmOpenAI(model_name=model_name, openai_api_base=openai_api_base),
+    )
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_predict_api.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_predict_api(db):
+    """Test chat."""
+    check_predict(db, VllmAPI(identifier="llm", api_url=api_url))
+
+
+@vcr.use_cassette(
+    f"{CASSETTE_DIR}/test_llm_as_listener_model_openapi.yaml",
+    filter_headers=["authorization"],
+    record_on_exception=False,
+    ignore_localhost=True,
+)
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_llm_as_listener_model_openapi(db):
+    check_llm_as_listener_model(db, VllmAPI(identifier="llm", api_url=api_url))

test/unittest/ext/llm/utils.py
@@ -1,0 +1,62 @@
+"""
+LLM model test cases.
+All the llm model can use the check_xxx func to test the intergration with db.
+"""
+
+from pinnacledb.backends.ibis.field_types import dtype
+from pinnacledb.backends.ibis.query import Schema, Table
+from pinnacledb.backends.mongodb.data_backend import MongoDataBackend
+from pinnacledb.backends.mongodb.query import Collection
+from pinnacledb.base.document import Document
+from pinnacledb.components.listener import Listener
+
+
+def check_predict(db, llm):
+    """Test chat."""
+    db.add(llm)
+    result = db.predict(llm.identifier, "1+1=")[0].content
+    assert isinstance(result, str)
+
+    results = db.predict(llm.identifier, ["1+1=", "2+2="])[0].content
+
+    assert isinstance(results, list)
+    assert len(results) == 2
+
+
+def check_llm_as_listener_model(db, llm):
+    collection_name = "question"
+    datas = [Document({"question": f"1+{i}=", "id": str(i)}) for i in range(10)]
+    if isinstance(db.databackend, MongoDataBackend):
+        db.execute(Collection(collection_name).insert_many(datas))
+        output_select = select = Collection(collection_name).find()
+    else:
+        schema = Schema(
+            identifier=collection_name,
+            fields={
+                "id": dtype("str"),
+                "question": dtype("str"),
+            },
+        )
+        table = Table(identifier=collection_name, schema=schema)
+        db.add(table)
+        db.execute(table.insert(datas))
+        select = table.select("id", "question")
+        output_select = table.select("id", "question").outputs(question=llm.identifier)
+
+    db.add(llm)
+
+    db.add(
+        Listener(
+            select=select,
+            key="question",
+            model=llm.identifier,
+        )
+    )
+
+    results = db.execute(output_select)
+    for result in results:
+        try:
+            output = result.outputs("question", llm.identifier)
+        except KeyError:
+            output = result.unpack()[f'_outputs.question.{llm.identifier}.0']
+        assert isinstance(output, str)