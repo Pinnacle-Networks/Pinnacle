superduper-io/superduper

Optimize llm related code
- Support dialog format messages
- Delete the model class in vllm openai format
- Use multi-threading instead of async function for concurrent API requests

superduperdb/ext/llm/__init__.py
@@ -1,9 +1,12 @@
+from superduperdb.ext.llm.base import BaseLLMAPI, BaseLLMModel, BaseOpenAI
 from superduperdb.ext.llm.openai import OpenAI
-from superduperdb.ext.llm.vllm import VllmAPI, VllmModel, VllmOpenAI
+from superduperdb.ext.llm.vllm import VllmAPI, VllmModel
 
 __all__ = [
+    "BaseOpenAI",
+    "BaseLLMModel",
+    "BaseLLMAPI",
     "OpenAI",
     "VllmAPI",
     "VllmModel",
-    "VllmOpenAI",
 ]

superduperdb/ext/llm/vllm.py
@@ -1,12 +1,9 @@
-import asyncio
 import dataclasses as dc
-from typing import Any, List, Optional
+from typing import Any, List
 
-import aiohttp
 import requests
 
-from superduperdb import logging
-from superduperdb.ext.llm.base import BaseLLMAPI, BaseLLMModel, BaseOpenAI
+from superduperdb.ext.llm.base import BaseLLMAPI, BaseLLMModel
 
 VLLM_INFERENCE_PARAMETERS_LIST = [
     "n",
@@ -35,20 +32,6 @@ VLLM_INFERENCE_PARAMETERS_LIST = [
 
 
 @dc.dataclass
-class VllmOpenAI(BaseOpenAI):
-    """
-    Wrapper for requesting the vLLM API service
-    (OpenAI format, started by vllm.entrypoints.openai.api_server)
-
-    {parent_doc}
-    """
-
-    openai_api_key: Optional[str] = "EMPTY"
-
-    __doc__ = __doc__.format(parent_doc=BaseOpenAI.__doc__)
-
-
-@dc.dataclass
 class VllmAPI(BaseLLMAPI):
     """
     Wrapper for requesting the vLLM API service
@@ -65,32 +48,6 @@ class VllmAPI(BaseLLMAPI):
         post_data = self.build_post_data(prompt, **kwargs)
         response = requests.post(self.api_url, json=post_data)
         return response.json()["text"][0]
-
-    def _batch_generate(self, prompts: List[str], **kwargs: Any) -> List[str]:
-        """
-        Use asyncio to batch generate text from a list of prompts.
-        """
-        return asyncio.run(self._async_batch_generate(prompts, **kwargs))
-
-    async def _async_generate(self, session, semaphore, prompt: str, **kwargs) -> str:
-        post_data = self.build_post_data(prompt, **kwargs)
-        async with semaphore:
-            try:
-                async with session.post(self.api_url, json=post_data) as response:
-                    response_json = await response.json()
-                    return response_json["text"][0]
-            except aiohttp.ClientError as e:
-                logging.error(f"HTTP request failed: {e}. Prompt: {prompt}")
-                return ""
-
-    async def _async_batch_generate(self, prompts: List[str], **kwargs) -> List[str]:
-        async with aiohttp.ClientSession() as session:
-            semaphore = asyncio.Semaphore(self.max_batch_size or len(prompts))
-            tasks = [
-                self._async_generate(session, semaphore, prompt, **kwargs)
-                for prompt in prompts
-            ]
-            return await asyncio.gather(*tasks)
 
     def build_post_data(self, prompt: str, **kwargs: dict[str, Any]) -> dict[str, Any]:
         total_kwargs = {}

test/unittest/ext/llm/test_vllm.py
@@ -5,7 +5,7 @@ from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predi
 import pytest
 import vcr
 
-from superduperdb.ext.llm.vllm import VllmAPI, VllmOpenAI
+from superduperdb.ext.llm.vllm import VllmAPI
 
 CASSETTE_DIR = "test/unittest/ext/cassettes/llm/vllm"
 
@@ -18,45 +18,6 @@ api_url = "http://ec2-54-208-130-192.compute-1.amazonaws.com:8000/generate"
 def openai_mock(monkeypatch):
     if os.getenv("OPENAI_API_KEY") is None:
         monkeypatch.setenv("OPENAI_API_KEY", "sk-TopSecret")
-
-
-@vcr.use_cassette(
-    f"{CASSETTE_DIR}/test_predict_openai.yaml",
-    filter_headers=["authorization"],
-    record_on_exception=False,
-    ignore_localhost=True,
-)
-@pytest.mark.parametrize(
-    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
-)
-def test_predict_openai(db, openai_mock):
-    """Test chat."""
-    check_predict(
-        db, VllmOpenAI(model_name=model_name, openai_api_base=openai_api_base)
-    )
-    check_predict(
-        db,
-        VllmOpenAI(
-            identifier="chat-llm",
-            model_name=model_name,
-            openai_api_base=openai_api_base,
-            chat=True,
-        ),
-    )
-
-
-@vcr.use_cassette(
-    f"{CASSETTE_DIR}/test_llm_as_listener_model_openai.yaml",
-    filter_headers=["authorization"],
-    record_on_exception=False,
-    ignore_localhost=True,
-)
-@pytest.mark.parametrize("db", [DBConfig.sqldb_empty], indirect=True)
-def test_llm_as_listener_model_openai(db, openai_mock):
-    check_llm_as_listener_model(
-        db,
-        VllmOpenAI(model_name=model_name, openai_api_base=openai_api_base),
-    )
 
 
 @vcr.use_cassette(
@@ -74,7 +35,7 @@ def test_predict_api(db):
 
 
 @vcr.use_cassette(
-    f"{CASSETTE_DIR}/test_llm_as_listener_model_openapi.yaml",
+    f"{CASSETTE_DIR}/test_llm_as_listener_model_api.yaml",
     filter_headers=["authorization"],
     record_on_exception=False,
     ignore_localhost=True,
@@ -82,5 +43,5 @@ def test_predict_api(db):
 @pytest.mark.parametrize(
     "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
 )
-def test_llm_as_listener_model_openapi(db):
+def test_llm_as_listener_model_api(db):
     check_llm_as_listener_model(db, VllmAPI(identifier="llm", api_url=api_url))

Pinnacle-Networks/Pinnacle

superduperdb/ext/llm/__init__.py
@@ -1,9 +1,12 @@
+from pinnacledb.ext.llm.base import BaseLLMAPI, BaseLLMModel, BaseOpenAI
 from pinnacledb.ext.llm.openai import OpenAI
-from pinnacledb.ext.llm.vllm import VllmAPI, VllmModel, VllmOpenAI
+from pinnacledb.ext.llm.vllm import VllmAPI, VllmModel
 
 __all__ = [
+    "BaseOpenAI",
+    "BaseLLMModel",
+    "BaseLLMAPI",
     "OpenAI",
     "VllmAPI",
     "VllmModel",
-    "VllmOpenAI",
 ]

superduperdb/ext/llm/vllm.py
@@ -1,12 +1,9 @@
-import asyncio
 import dataclasses as dc
-from typing import Any, List, Optional
+from typing import Any, List
 
-import aiohttp
 import requests
 
-from pinnacledb import logging
-from pinnacledb.ext.llm.base import BaseLLMAPI, BaseLLMModel, BaseOpenAI
+from pinnacledb.ext.llm.base import BaseLLMAPI, BaseLLMModel
 
 VLLM_INFERENCE_PARAMETERS_LIST = [
     "n",
@@ -35,20 +32,6 @@ VLLM_INFERENCE_PARAMETERS_LIST = [
 
 
 @dc.dataclass
-class VllmOpenAI(BaseOpenAI):
-    """
-    Wrapper for requesting the vLLM API service
-    (OpenAI format, started by vllm.entrypoints.openai.api_server)
-
-    {parent_doc}
-    """
-
-    openai_api_key: Optional[str] = "EMPTY"
-
-    __doc__ = __doc__.format(parent_doc=BaseOpenAI.__doc__)
-
-
-@dc.dataclass
 class VllmAPI(BaseLLMAPI):
     """
     Wrapper for requesting the vLLM API service
@@ -65,32 +48,6 @@ class VllmAPI(BaseLLMAPI):
         post_data = self.build_post_data(prompt, **kwargs)
         response = requests.post(self.api_url, json=post_data)
         return response.json()["text"][0]
-
-    def _batch_generate(self, prompts: List[str], **kwargs: Any) -> List[str]:
-        """
-        Use asyncio to batch generate text from a list of prompts.
-        """
-        return asyncio.run(self._async_batch_generate(prompts, **kwargs))
-
-    async def _async_generate(self, session, semaphore, prompt: str, **kwargs) -> str:
-        post_data = self.build_post_data(prompt, **kwargs)
-        async with semaphore:
-            try:
-                async with session.post(self.api_url, json=post_data) as response:
-                    response_json = await response.json()
-                    return response_json["text"][0]
-            except aiohttp.ClientError as e:
-                logging.error(f"HTTP request failed: {e}. Prompt: {prompt}")
-                return ""
-
-    async def _async_batch_generate(self, prompts: List[str], **kwargs) -> List[str]:
-        async with aiohttp.ClientSession() as session:
-            semaphore = asyncio.Semaphore(self.max_batch_size or len(prompts))
-            tasks = [
-                self._async_generate(session, semaphore, prompt, **kwargs)
-                for prompt in prompts
-            ]
-            return await asyncio.gather(*tasks)
 
     def build_post_data(self, prompt: str, **kwargs: dict[str, Any]) -> dict[str, Any]:
         total_kwargs = {}

test/unittest/ext/llm/test_vllm.py
@@ -5,7 +5,7 @@ from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predi
 import pytest
 import vcr
 
-from pinnacledb.ext.llm.vllm import VllmAPI, VllmOpenAI
+from pinnacledb.ext.llm.vllm import VllmAPI
 
 CASSETTE_DIR = "test/unittest/ext/cassettes/llm/vllm"
 
@@ -18,45 +18,6 @@ api_url = "http://ec2-54-208-130-192.compute-1.amazonaws.com:8000/generate"
 def openai_mock(monkeypatch):
     if os.getenv("OPENAI_API_KEY") is None:
         monkeypatch.setenv("OPENAI_API_KEY", "sk-TopSecret")
-
-
-@vcr.use_cassette(
-    f"{CASSETTE_DIR}/test_predict_openai.yaml",
-    filter_headers=["authorization"],
-    record_on_exception=False,
-    ignore_localhost=True,
-)
-@pytest.mark.parametrize(
-    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
-)
-def test_predict_openai(db, openai_mock):
-    """Test chat."""
-    check_predict(
-        db, VllmOpenAI(model_name=model_name, openai_api_base=openai_api_base)
-    )
-    check_predict(
-        db,
-        VllmOpenAI(
-            identifier="chat-llm",
-            model_name=model_name,
-            openai_api_base=openai_api_base,
-            chat=True,
-        ),
-    )
-
-
-@vcr.use_cassette(
-    f"{CASSETTE_DIR}/test_llm_as_listener_model_openai.yaml",
-    filter_headers=["authorization"],
-    record_on_exception=False,
-    ignore_localhost=True,
-)
-@pytest.mark.parametrize("db", [DBConfig.sqldb_empty], indirect=True)
-def test_llm_as_listener_model_openai(db, openai_mock):
-    check_llm_as_listener_model(
-        db,
-        VllmOpenAI(model_name=model_name, openai_api_base=openai_api_base),
-    )
 
 
 @vcr.use_cassette(
@@ -74,7 +35,7 @@ def test_predict_api(db):
 
 
 @vcr.use_cassette(
-    f"{CASSETTE_DIR}/test_llm_as_listener_model_openapi.yaml",
+    f"{CASSETTE_DIR}/test_llm_as_listener_model_api.yaml",
     filter_headers=["authorization"],
     record_on_exception=False,
     ignore_localhost=True,
@@ -82,5 +43,5 @@ def test_predict_api(db):
 @pytest.mark.parametrize(
     "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
 )
-def test_llm_as_listener_model_openapi(db):
+def test_llm_as_listener_model_api(db):
     check_llm_as_listener_model(db, VllmAPI(identifier="llm", api_url=api_url))