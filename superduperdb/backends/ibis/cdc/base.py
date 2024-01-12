superduper-io/superduper

Add initial cdc options for sql databases

superduperdb/backends/ibis/cdc/base.py
@@ -1,0 +1,18 @@
+import dataclasses as dc
+import typing as t
+
+from superduperdb.base.serializable import Serializable
+from superduperdb.cdc.cdc import DBEvent, Packet
+
+TokenType = t.Dict[str, str]
+
+
+@dc.dataclass
+class IbisDBPacket(Packet):
+    """
+    A base packet to represent message in task queue.
+    """
+
+    ids: t.List[str]
+    query: t.Optional[Serializable] = None
+    event_type: DBEvent = DBEvent.insert

superduperdb/backends/mongodb/cdc/listener.py
@@ -8,6 +8,7 @@ from pymongo.change_stream import CollectionChangeStream
 
 from superduperdb import logging
 from superduperdb.backends.mongodb import query
+from superduperdb.backends.mongodb.cdc.base import MongoDBPacket
 from superduperdb.cdc import cdc
 from superduperdb.misc.runnable.runnable import Event
 
@@ -106,6 +107,11 @@ class MongoDatabaseListener(cdc.BaseDatabaseListener):
             self.resume_token = resume_token
 
         self._change_pipeline = None
+
+        self.db_type = 'mongodb'
+        self.packet = lambda ids, query, event_type: MongoDBPacket(
+            ids, query, event_type
+        )
 
         super().__init__(
             db=db, on=on, stop_event=stop_event, identifier=identifier, timeout=timeout

superduperdb/cdc/cdc.py
@@ -42,6 +42,7 @@ from superduperdb.misc.runnable.runnable import Event
 
 if t.TYPE_CHECKING:
     from superduperdb.backends.base.query import TableOrCollection
+    from superduperdb.backends.ibis.query import Table
     from superduperdb.base.datalayer import Datalayer
     from superduperdb.base.serializable import Serializable
     from superduperdb.components.listener import Listener
@@ -71,7 +72,10 @@ class Packet:
         Collate a batch of packets into one
         """
         assert packets
-        ids = [packet.ids[0] for packet in packets]
+        ids = []
+        for packet in packets:
+            ids += packet.ids
+
         query = packets[0].query
 
         # TODO: cluster Packet for each event.
@@ -94,7 +98,7 @@ class BaseDatabaseListener(ABC):
     def __init__(
         self,
         db: 'Datalayer',
-        on: 'TableOrCollection',
+        on: t.Union['Table', 'TableOrCollection'],
         stop_event: Event,
         identifier: 'str' = '',
         timeout: t.Optional[float] = None,
@@ -107,18 +111,7 @@ class BaseDatabaseListener(ABC):
         self._startup_event = Event()
         self._scheduler = None
         self.timeout = timeout
-        self.db_type = None
-
-        from superduperdb.backends.base import backends
-        from superduperdb.backends.mongodb.cdc.base import MongoDBPacket
-
-        if isinstance(self.db.databackend, backends.MongoDataBackend):
-            self.db_type = 'mongodb'
-            self.packet = lambda ids, query, event_type: MongoDBPacket(
-                ids, query, event_type
-            )
-        else:
-            raise NotImplementedError(f'{self.db.databackend} not supported yet!')
+        self.db_type: t.Optional[str] = None
 
     @property
     def identity(self) -> str:
@@ -180,7 +173,7 @@ class BaseDatabaseListener(ABC):
         self,
         ids: t.Sequence,
         db: 'Datalayer',
-        table_or_collection: 'TableOrCollection',
+        table_or_collection: t.Union['Table', 'TableOrCollection'],
         event: DBEvent,
     ):
         """
@@ -207,15 +200,15 @@ class BaseDatabaseListener(ABC):
         """
         if event == DBEvent.insert:
             self._change_counters['inserts'] += 1
-            self.on_create(ids, db=self.db, collection=self._on_component)
+            self.on_create(ids, self.db, self._on_component)
 
         elif event == DBEvent.update:
             self._change_counters['updates'] += 1
-            self.on_update(ids, db=self.db, collection=self._on_component)
+            self.on_update(ids, self.db, self._on_component)
 
         elif event == DBEvent.delete:
             self._change_counters['deletes'] += 1
-            self.on_delete(ids, db=self.db, collection=self._on_component)
+            self.on_delete(ids, self.db, self._on_component)
 
 
 class DatabaseListenerThreadScheduler(threading.Thread):
@@ -295,7 +288,7 @@ class DatabaseListenerFactory(t.Generic[DBListenerType]):
     `db_type`.
     """
 
-    SUPPORTED_LISTENERS: t.List[str] = ['mongodb']
+    SUPPORTED_LISTENERS: t.List[str] = ['mongodb', 'ibis']
 
     def __init__(self, db_type: str = 'mongodb'):
         if db_type not in self.SUPPORTED_LISTENERS:
@@ -308,7 +301,16 @@ class DatabaseListenerFactory(t.Generic[DBListenerType]):
         if self.db_type == 'mongodb':
             from superduperdb.backends.mongodb.cdc.listener import MongoDatabaseListener
 
-            listener = MongoDatabaseListener(*args, **kwargs)
+            listener = t.cast(
+                BaseDatabaseListener, MongoDatabaseListener(*args, **kwargs)
+            )
+            return t.cast(DBListenerType, listener)
+        elif self.db_type == 'ibis':
+            from superduperdb.backends.ibis.cdc.listener import IbisDatabaseListener
+
+            listener = t.cast(
+                BaseDatabaseListener, IbisDatabaseListener(*args, **kwargs)
+            )
             return t.cast(DBListenerType, listener)
         else:
             raise NotImplementedError
@@ -333,7 +335,9 @@ class DatabaseChangeDataCapture:
         self.cdc_change_handler: t.Optional[CDCHandler] = None
         self._CDC_LISTENERS: t.Dict[str, BaseDatabaseListener] = {}
         self._running: bool = False
-        self._cdc_existing_collections: t.MutableSequence['TableOrCollection'] = []
+        self._cdc_existing_collections: t.MutableSequence[
+            t.Union['TableOrCollection', 'Table']
+        ] = []
 
         listeners = self.db.show('listeners')
         if listeners:
@@ -354,13 +358,13 @@ class DatabaseChangeDataCapture:
         """
         self._running = True
 
-        # listen to existing collection without cdc enabled
+        # Listen to existing collection without cdc enabled
         for collection in self._cdc_existing_collections:
             self.listen(collection)
 
     def listen(
         self,
-        on: 'TableOrCollection',
+        on: t.Union['Table', 'TableOrCollection'],
         identifier: str = '',
         *args,
         **kwargs,
@@ -377,6 +381,8 @@ class DatabaseChangeDataCapture:
 
         if isinstance(self.db.databackend, backends.MongoDataBackend):
             db_type = 'mongodb'
+        elif isinstance(self.db.databackend, backends.IbisDataBackend):
+            db_type = 'ibis'
         else:
             raise NotImplementedError(f'{self.db.databackend} not supported yet!')
 

superduperdb/vector_search/update_tasks.py
@@ -1,6 +1,8 @@
 import typing as t
 
 from superduperdb.backends.base.query import CompoundSelect
+from superduperdb.backends.ibis.data_backend import IbisDataBackend
+from superduperdb.backends.mongodb.data_backend import MongoDataBackend
 from superduperdb.base.serializable import Serializable
 from superduperdb.misc.special_dicts import MongoStyleDict
 from superduperdb.vector_search.base import VectorItem
@@ -48,13 +50,24 @@ def copy_vectors(
     key = vi.indexing_listener.key
     model = vi.indexing_listener.model.identifier
     version = vi.indexing_listener.model.version
-    vectors = [
-        {
-            'vector': MongoStyleDict(doc)[f'_outputs.{key}.{model}.{version}'],
-            'id': str(doc['_id']),
-        }
-        for doc in docs
-    ]
+    # TODO: Refactor the below logic
+    if isinstance(db.databackend, MongoDataBackend):
+        vectors = [
+            {
+                'vector': MongoStyleDict(doc)[f'_outputs.{key}.{model}.{version}'],
+                'id': str(doc['_id']),
+            }
+            for doc in docs
+        ]
+    elif isinstance(db.databackend, IbisDataBackend):
+        docs = db.execute(select.outputs(**{key: model}))
+        vectors = [
+            {
+                'vector': doc['_outputs.{key}.{model}.{version}'].x,
+                'id': str(doc['input_id']),
+            }
+            for doc in docs
+        ]
     for r in vectors:
         if hasattr(r['vector'], 'numpy'):
             r['vector'] = r['vector'].numpy()

Pinnacle-Networks/Pinnacle

superduperdb/backends/ibis/cdc/base.py
@@ -1,0 +1,18 @@
+import dataclasses as dc
+import typing as t
+
+from pinnacledb.base.serializable import Serializable
+from pinnacledb.cdc.cdc import DBEvent, Packet
+
+TokenType = t.Dict[str, str]
+
+
+@dc.dataclass
+class IbisDBPacket(Packet):
+    """
+    A base packet to represent message in task queue.
+    """
+
+    ids: t.List[str]
+    query: t.Optional[Serializable] = None
+    event_type: DBEvent = DBEvent.insert

superduperdb/backends/mongodb/cdc/listener.py
@@ -8,6 +8,7 @@ from pymongo.change_stream import CollectionChangeStream
 
 from pinnacledb import logging
 from pinnacledb.backends.mongodb import query
+from pinnacledb.backends.mongodb.cdc.base import MongoDBPacket
 from pinnacledb.cdc import cdc
 from pinnacledb.misc.runnable.runnable import Event
 
@@ -106,6 +107,11 @@ class MongoDatabaseListener(cdc.BaseDatabaseListener):
             self.resume_token = resume_token
 
         self._change_pipeline = None
+
+        self.db_type = 'mongodb'
+        self.packet = lambda ids, query, event_type: MongoDBPacket(
+            ids, query, event_type
+        )
 
         super().__init__(
             db=db, on=on, stop_event=stop_event, identifier=identifier, timeout=timeout

superduperdb/cdc/cdc.py
@@ -42,6 +42,7 @@ from pinnacledb.misc.runnable.runnable import Event
 
 if t.TYPE_CHECKING:
     from pinnacledb.backends.base.query import TableOrCollection
+    from pinnacledb.backends.ibis.query import Table
     from pinnacledb.base.datalayer import Datalayer
     from pinnacledb.base.serializable import Serializable
     from pinnacledb.components.listener import Listener
@@ -71,7 +72,10 @@ class Packet:
         Collate a batch of packets into one
         """
         assert packets
-        ids = [packet.ids[0] for packet in packets]
+        ids = []
+        for packet in packets:
+            ids += packet.ids
+
         query = packets[0].query
 
         # TODO: cluster Packet for each event.
@@ -94,7 +98,7 @@ class BaseDatabaseListener(ABC):
     def __init__(
         self,
         db: 'Datalayer',
-        on: 'TableOrCollection',
+        on: t.Union['Table', 'TableOrCollection'],
         stop_event: Event,
         identifier: 'str' = '',
         timeout: t.Optional[float] = None,
@@ -107,18 +111,7 @@ class BaseDatabaseListener(ABC):
         self._startup_event = Event()
         self._scheduler = None
         self.timeout = timeout
-        self.db_type = None
-
-        from pinnacledb.backends.base import backends
-        from pinnacledb.backends.mongodb.cdc.base import MongoDBPacket
-
-        if isinstance(self.db.databackend, backends.MongoDataBackend):
-            self.db_type = 'mongodb'
-            self.packet = lambda ids, query, event_type: MongoDBPacket(
-                ids, query, event_type
-            )
-        else:
-            raise NotImplementedError(f'{self.db.databackend} not supported yet!')
+        self.db_type: t.Optional[str] = None
 
     @property
     def identity(self) -> str:
@@ -180,7 +173,7 @@ class BaseDatabaseListener(ABC):
         self,
         ids: t.Sequence,
         db: 'Datalayer',
-        table_or_collection: 'TableOrCollection',
+        table_or_collection: t.Union['Table', 'TableOrCollection'],
         event: DBEvent,
     ):
         """
@@ -207,15 +200,15 @@ class BaseDatabaseListener(ABC):
         """
         if event == DBEvent.insert:
             self._change_counters['inserts'] += 1
-            self.on_create(ids, db=self.db, collection=self._on_component)
+            self.on_create(ids, self.db, self._on_component)
 
         elif event == DBEvent.update:
             self._change_counters['updates'] += 1
-            self.on_update(ids, db=self.db, collection=self._on_component)
+            self.on_update(ids, self.db, self._on_component)
 
         elif event == DBEvent.delete:
             self._change_counters['deletes'] += 1
-            self.on_delete(ids, db=self.db, collection=self._on_component)
+            self.on_delete(ids, self.db, self._on_component)
 
 
 class DatabaseListenerThreadScheduler(threading.Thread):
@@ -295,7 +288,7 @@ class DatabaseListenerFactory(t.Generic[DBListenerType]):
     `db_type`.
     """
 
-    SUPPORTED_LISTENERS: t.List[str] = ['mongodb']
+    SUPPORTED_LISTENERS: t.List[str] = ['mongodb', 'ibis']
 
     def __init__(self, db_type: str = 'mongodb'):
         if db_type not in self.SUPPORTED_LISTENERS:
@@ -308,7 +301,16 @@ class DatabaseListenerFactory(t.Generic[DBListenerType]):
         if self.db_type == 'mongodb':
             from pinnacledb.backends.mongodb.cdc.listener import MongoDatabaseListener
 
-            listener = MongoDatabaseListener(*args, **kwargs)
+            listener = t.cast(
+                BaseDatabaseListener, MongoDatabaseListener(*args, **kwargs)
+            )
+            return t.cast(DBListenerType, listener)
+        elif self.db_type == 'ibis':
+            from pinnacledb.backends.ibis.cdc.listener import IbisDatabaseListener
+
+            listener = t.cast(
+                BaseDatabaseListener, IbisDatabaseListener(*args, **kwargs)
+            )
             return t.cast(DBListenerType, listener)
         else:
             raise NotImplementedError
@@ -333,7 +335,9 @@ class DatabaseChangeDataCapture:
         self.cdc_change_handler: t.Optional[CDCHandler] = None
         self._CDC_LISTENERS: t.Dict[str, BaseDatabaseListener] = {}
         self._running: bool = False
-        self._cdc_existing_collections: t.MutableSequence['TableOrCollection'] = []
+        self._cdc_existing_collections: t.MutableSequence[
+            t.Union['TableOrCollection', 'Table']
+        ] = []
 
         listeners = self.db.show('listeners')
         if listeners:
@@ -354,13 +358,13 @@ class DatabaseChangeDataCapture:
         """
         self._running = True
 
-        # listen to existing collection without cdc enabled
+        # Listen to existing collection without cdc enabled
         for collection in self._cdc_existing_collections:
             self.listen(collection)
 
     def listen(
         self,
-        on: 'TableOrCollection',
+        on: t.Union['Table', 'TableOrCollection'],
         identifier: str = '',
         *args,
         **kwargs,
@@ -377,6 +381,8 @@ class DatabaseChangeDataCapture:
 
         if isinstance(self.db.databackend, backends.MongoDataBackend):
             db_type = 'mongodb'
+        elif isinstance(self.db.databackend, backends.IbisDataBackend):
+            db_type = 'ibis'
         else:
             raise NotImplementedError(f'{self.db.databackend} not supported yet!')
 

superduperdb/vector_search/update_tasks.py
@@ -1,6 +1,8 @@
 import typing as t
 
 from pinnacledb.backends.base.query import CompoundSelect
+from pinnacledb.backends.ibis.data_backend import IbisDataBackend
+from pinnacledb.backends.mongodb.data_backend import MongoDataBackend
 from pinnacledb.base.serializable import Serializable
 from pinnacledb.misc.special_dicts import MongoStyleDict
 from pinnacledb.vector_search.base import VectorItem
@@ -48,13 +50,24 @@ def copy_vectors(
     key = vi.indexing_listener.key
     model = vi.indexing_listener.model.identifier
     version = vi.indexing_listener.model.version
-    vectors = [
-        {
-            'vector': MongoStyleDict(doc)[f'_outputs.{key}.{model}.{version}'],
-            'id': str(doc['_id']),
-        }
-        for doc in docs
-    ]
+    # TODO: Refactor the below logic
+    if isinstance(db.databackend, MongoDataBackend):
+        vectors = [
+            {
+                'vector': MongoStyleDict(doc)[f'_outputs.{key}.{model}.{version}'],
+                'id': str(doc['_id']),
+            }
+            for doc in docs
+        ]
+    elif isinstance(db.databackend, IbisDataBackend):
+        docs = db.execute(select.outputs(**{key: model}))
+        vectors = [
+            {
+                'vector': doc['_outputs.{key}.{model}.{version}'].x,
+                'id': str(doc['input_id']),
+            }
+            for doc in docs
+        ]
     for r in vectors:
         if hasattr(r['vector'], 'numpy'):
             r['vector'] = r['vector'].numpy()