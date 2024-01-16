superduper-io/superduper

Refactor ibis table import path

superduperdb/backends/ibis/query.py
@@ -7,7 +7,7 @@ import typing as t
 
 import pandas
 
-from superduperdb import Document, logging
+from superduperdb import logging
 from superduperdb.backends.base.query import (
     CompoundSelect,
     Insert,
@@ -21,6 +21,7 @@ from superduperdb.backends.base.query import (
 )
 from superduperdb.backends.ibis.cursor import SuperDuperIbisResult
 from superduperdb.backends.ibis.utils import get_output_table_name
+from superduperdb.base.document import Document
 from superduperdb.base.serializable import Variable
 from superduperdb.components.component import Component
 from superduperdb.components.encoder import Encoder

superduperdb/base/document.py
@@ -6,9 +6,12 @@ from bson.objectid import ObjectId
 from superduperdb import CFG
 from superduperdb.base.config import BytesEncoding
 from superduperdb.components.encoder import Encodable, Encoder
-from superduperdb.components.schema import Schema
 from superduperdb.misc.files import get_file_from_uri
 from superduperdb.misc.special_dicts import MongoStyleDict
+
+if t.TYPE_CHECKING:
+    from superduperdb.components.schema import Schema
+
 
 ContentType = t.Union[t.Dict, Encodable]
 ItemType = t.Union[t.Dict[str, t.Any], Encodable, ObjectId]
@@ -37,7 +40,7 @@ class Document:
 
     def encode(
         self,
-        schema: t.Optional[Schema] = None,
+        schema: t.Optional['Schema'] = None,
         bytes_encoding: t.Optional[BytesEncoding] = None,
     ) -> t.Any:
         """Make a copy of the content with all the Encodables encoded"""
@@ -167,7 +170,7 @@ def _encode(r: t.Any, bytes_encoding: t.Optional[BytesEncoding] = None) -> t.Any
 
 
 def _encode_with_schema(
-    r: t.Any, schema: Schema, bytes_encoding: t.Optional[BytesEncoding] = None
+    r: t.Any, schema: 'Schema', bytes_encoding: t.Optional[BytesEncoding] = None
 ) -> t.Any:
     bytes_encoding = bytes_encoding or CFG.bytes_encoding
     if isinstance(r, dict):

Pinnacle-Networks/Pinnacle

superduperdb/backends/ibis/query.py
@@ -7,7 +7,7 @@ import typing as t
 
 import pandas
 
-from pinnacledb import Document, logging
+from pinnacledb import logging
 from pinnacledb.backends.base.query import (
     CompoundSelect,
     Insert,
@@ -21,6 +21,7 @@ from pinnacledb.backends.base.query import (
 )
 from pinnacledb.backends.ibis.cursor import SuperDuperIbisResult
 from pinnacledb.backends.ibis.utils import get_output_table_name
+from pinnacledb.base.document import Document
 from pinnacledb.base.serializable import Variable
 from pinnacledb.components.component import Component
 from pinnacledb.components.encoder import Encoder

superduperdb/base/document.py
@@ -6,9 +6,12 @@ from bson.objectid import ObjectId
 from pinnacledb import CFG
 from pinnacledb.base.config import BytesEncoding
 from pinnacledb.components.encoder import Encodable, Encoder
-from pinnacledb.components.schema import Schema
 from pinnacledb.misc.files import get_file_from_uri
 from pinnacledb.misc.special_dicts import MongoStyleDict
+
+if t.TYPE_CHECKING:
+    from pinnacledb.components.schema import Schema
+
 
 ContentType = t.Union[t.Dict, Encodable]
 ItemType = t.Union[t.Dict[str, t.Any], Encodable, ObjectId]
@@ -37,7 +40,7 @@ class Document:
 
     def encode(
         self,
-        schema: t.Optional[Schema] = None,
+        schema: t.Optional['Schema'] = None,
         bytes_encoding: t.Optional[BytesEncoding] = None,
     ) -> t.Any:
         """Make a copy of the content with all the Encodables encoded"""
@@ -167,7 +170,7 @@ def _encode(r: t.Any, bytes_encoding: t.Optional[BytesEncoding] = None) -> t.Any
 
 
 def _encode_with_schema(
-    r: t.Any, schema: Schema, bytes_encoding: t.Optional[BytesEncoding] = None
+    r: t.Any, schema: 'Schema', bytes_encoding: t.Optional[BytesEncoding] = None
 ) -> t.Any:
     bytes_encoding = bytes_encoding or CFG.bytes_encoding
     if isinstance(r, dict):