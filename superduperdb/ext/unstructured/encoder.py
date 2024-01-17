superduper-io/superduper

Support unstructured encoder

superduperdb/ext/unstructured/encoder.py
@@ -1,0 +1,70 @@
+import dataclasses as dc
+import pickle
+import typing as t
+
+from unstructured.documents.elements import Element
+from unstructured.partition.auto import partition
+from unstructured.partition.html import partition_html
+
+from superduperdb.components.encoder import Encoder
+
+
+def link2elements(link, unstructure_kwargs):
+    """
+    Convert a link to a list of elements
+    Use unstructured to parse the link
+    param link: str, file path or url
+    param unstructure_kwargs: kwargs for unstructured
+    """
+    if link.startswith("file://"):
+        link = link[7:]
+
+    if link.startswith("http"):
+        # Special handling is required for http
+        elements = partition_html(url=link, **unstructure_kwargs)
+    else:
+        elements = partition(link, **unstructure_kwargs)
+    return elements
+
+
+def create_encoder(unstructure_kwargs):
+    def encoder(x: t.Union[str, t.List[Element]]):
+        if isinstance(x, str):
+            elements = link2elements(x, unstructure_kwargs)
+        elif isinstance(x, list) and isinstance(x[0], Element):
+            elements = x
+        else:
+            raise TypeError(f"Cannot encode {type(x)}, must be str or list of Element")
+        return pickle.dumps(elements)
+
+    return encoder
+
+
+def create_decoder():
+    def decoder(b: bytes):
+        try:
+            return pickle.loads(b)
+        except Exception as e:
+            # TODO: A compatibility issue with unstructured when using uri
+            # When superduperdb download uri and pass the bytes to the decoder,
+            # the file type message is lost, unstructured cannot  automatically parse.
+            raise ValueError(
+                "Cannot parse the bytes from uri data, please use encoder(x=uri)"
+            ) from e
+
+    return decoder
+
+
+@dc.dataclass
+class UnstructuredEncoder(Encoder):
+    unstructure_kwargs: t.Dict[str, t.Any] = dc.field(default_factory=dict)
+
+    def __post_init__(self):
+        self.encoder = create_encoder(self.unstructure_kwargs)
+        self.decoder = create_decoder()
+        super().__post_init__()
+
+
+unstructured_encoder = UnstructuredEncoder(
+    identifier="unstructured",
+)

test/unittest/ext/test_unstructured.py
@@ -1,0 +1,51 @@
+from test.db_config import DBConfig
+
+import pytest
+
+from superduperdb.backends.mongodb.query import Collection
+from superduperdb.base.document import Document
+
+try:
+    import unstructured
+except ImportError:
+    unstructured = None
+
+
+TEST_URL = "https://superduperdb.com"
+TEST_FILE = "file://test/material/data/rhymes.md"
+
+
+@pytest.mark.skipif(not unstructured, reason="unstructured not installed")
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_encoder_auto(db):
+    from unstructured.documents.elements import Element
+
+    from superduperdb.ext.unstructured.encoder import unstructured_encoder
+
+    db.add(unstructured_encoder)
+
+    collection = Collection("documents")
+    to_insert = [Document({"elements": unstructured_encoder(TEST_FILE)})]
+    db.execute(collection.insert_many(to_insert))
+
+    r = db.execute(collection.find_one())
+    for e in r["elements"].x:
+        assert isinstance(e, Element)
+
+
+@pytest.mark.skipif(not unstructured, reason="unstructured not installed")
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_encoder_url(db):
+    from unstructured.documents.elements import Element
+
+    from superduperdb.ext.unstructured.encoder import unstructured_encoder
+
+    db.add(unstructured_encoder)
+
+    collection = Collection("documents")
+    to_insert = [Document({"elements": unstructured_encoder(TEST_URL)})]
+    db.execute(collection.insert_many(to_insert))
+
+    r = db.execute(collection.find_one())
+    for e in r["elements"].x:
+        assert isinstance(e, Element)

Pinnacle-Networks/Pinnacle

superduperdb/ext/unstructured/encoder.py
@@ -1,0 +1,70 @@
+import dataclasses as dc
+import pickle
+import typing as t
+
+from unstructured.documents.elements import Element
+from unstructured.partition.auto import partition
+from unstructured.partition.html import partition_html
+
+from pinnacledb.components.encoder import Encoder
+
+
+def link2elements(link, unstructure_kwargs):
+    """
+    Convert a link to a list of elements
+    Use unstructured to parse the link
+    param link: str, file path or url
+    param unstructure_kwargs: kwargs for unstructured
+    """
+    if link.startswith("file://"):
+        link = link[7:]
+
+    if link.startswith("http"):
+        # Special handling is required for http
+        elements = partition_html(url=link, **unstructure_kwargs)
+    else:
+        elements = partition(link, **unstructure_kwargs)
+    return elements
+
+
+def create_encoder(unstructure_kwargs):
+    def encoder(x: t.Union[str, t.List[Element]]):
+        if isinstance(x, str):
+            elements = link2elements(x, unstructure_kwargs)
+        elif isinstance(x, list) and isinstance(x[0], Element):
+            elements = x
+        else:
+            raise TypeError(f"Cannot encode {type(x)}, must be str or list of Element")
+        return pickle.dumps(elements)
+
+    return encoder
+
+
+def create_decoder():
+    def decoder(b: bytes):
+        try:
+            return pickle.loads(b)
+        except Exception as e:
+            # TODO: A compatibility issue with unstructured when using uri
+            # When pinnacledb download uri and pass the bytes to the decoder,
+            # the file type message is lost, unstructured cannot  automatically parse.
+            raise ValueError(
+                "Cannot parse the bytes from uri data, please use encoder(x=uri)"
+            ) from e
+
+    return decoder
+
+
+@dc.dataclass
+class UnstructuredEncoder(Encoder):
+    unstructure_kwargs: t.Dict[str, t.Any] = dc.field(default_factory=dict)
+
+    def __post_init__(self):
+        self.encoder = create_encoder(self.unstructure_kwargs)
+        self.decoder = create_decoder()
+        super().__post_init__()
+
+
+unstructured_encoder = UnstructuredEncoder(
+    identifier="unstructured",
+)

test/unittest/ext/test_unstructured.py
@@ -1,0 +1,51 @@
+from test.db_config import DBConfig
+
+import pytest
+
+from pinnacledb.backends.mongodb.query import Collection
+from pinnacledb.base.document import Document
+
+try:
+    import unstructured
+except ImportError:
+    unstructured = None
+
+
+TEST_URL = "https://pinnacledb.com"
+TEST_FILE = "file://test/material/data/rhymes.md"
+
+
+@pytest.mark.skipif(not unstructured, reason="unstructured not installed")
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_encoder_auto(db):
+    from unstructured.documents.elements import Element
+
+    from pinnacledb.ext.unstructured.encoder import unstructured_encoder
+
+    db.add(unstructured_encoder)
+
+    collection = Collection("documents")
+    to_insert = [Document({"elements": unstructured_encoder(TEST_FILE)})]
+    db.execute(collection.insert_many(to_insert))
+
+    r = db.execute(collection.find_one())
+    for e in r["elements"].x:
+        assert isinstance(e, Element)
+
+
+@pytest.mark.skipif(not unstructured, reason="unstructured not installed")
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_encoder_url(db):
+    from unstructured.documents.elements import Element
+
+    from pinnacledb.ext.unstructured.encoder import unstructured_encoder
+
+    db.add(unstructured_encoder)
+
+    collection = Collection("documents")
+    to_insert = [Document({"elements": unstructured_encoder(TEST_URL)})]
+    db.execute(collection.insert_many(to_insert))
+
+    r = db.execute(collection.find_one())
+    for e in r["elements"].x:
+        assert isinstance(e, Element)