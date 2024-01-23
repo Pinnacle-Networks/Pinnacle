superduper-io/superduper

Optimize llm compatible prompt logic

superduperdb/ext/llm/base.py
@@ -10,7 +10,8 @@ from typing import Any, Callable, List, Optional, Union
 from superduperdb import logging
 from superduperdb.components.component import Component
 from superduperdb.components.model import _Predictor
-from superduperdb.ext.utils import ensure_initialized, format_prompt
+from superduperdb.ext.llm.utils import Prompter
+from superduperdb.ext.utils import ensure_initialized
 
 if typing.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
@@ -37,6 +38,7 @@ class _BaseLLM(Component, _Predictor, metaclass=abc.ABCMeta):
         super().__post_init__()
         self.takes_context = True
         self.identifier = self.identifier.replace("/", "-")
+        self.prompter = Prompter(self.prompt_template, self.prompt_func)
         assert "{input}" in self.prompt_template, "Template must contain {input}"
 
     def to_call(self, X, *args, **kwargs):
@@ -87,25 +89,11 @@ class _BaseLLM(Component, _Predictor, metaclass=abc.ABCMeta):
             one = isinstance(X[0], dict)
 
         if one:
-            x = self.format_prompt(X, **kwargs)
+            x = self.prompter(X, **kwargs)
             return self._generate(x, **kwargs)
         else:
-            xs = [self.format_prompt(x, **kwargs) for x in X]
+            xs = [self.prompter(x, **kwargs) for x in X]
             return self._batch_generate(xs, **kwargs)
-
-    def format_prompt(self, x, **kwargs):
-        """
-        Format a prompt with the given prompt function or template.
-        :param x: The input to format.
-        :param kwargs: Any additional arguments to pass to the prompt function.
-        """
-        if self.prompt_func is not None:
-            return self.prompt_func(x, **self.get_kwargs(self.prompt_func, kwargs))
-
-        if isinstance(x, str):
-            return format_prompt(x, self.prompt_template, kwargs.get("context", None))
-        else:
-            return x
 
     def get_kwargs(self, func, *kwargs_list):
         """

superduperdb/ext/llm/model.py
@@ -22,6 +22,7 @@ from superduperdb.components.model import (
     Model,
     _TrainingConfiguration,
 )
+from superduperdb.ext.llm.utils import Prompter
 from superduperdb.ext.utils import ensure_initialized
 
 if typing.TYPE_CHECKING:
@@ -96,6 +97,8 @@ class LLM(Model):
         all the kwargs will pass to `transformers.AutoModelForCausalLM.from_pretrained`
     : param tokenizer_kwags: tokenizer kwargs,
         all the kwargs will pass to `transformers.AutoTokenizer.from_pretrained`
+    : param prompt_template: prompt template, default is "{input}"
+    : param prompt_func: prompt function, default is None
     """
 
     identifier: str = ""
@@ -104,6 +107,8 @@ class LLM(Model):
     object: t.Optional[transformers.Trainer] = None
     model_kwargs: t.Dict = dc.field(default_factory=dict)
     tokenizer_kwags: t.Dict = dc.field(default_factory=dict)
+    prompt_template: str = "{input}"
+    prompt_func: t.Optional[t.Callable] = dc.field(default=None)
 
     def __post_init__(self):
         self.identifier = self.identifier or self.model_name_or_path
@@ -140,6 +145,7 @@ class LLM(Model):
         return trainer
 
     def init(self):
+        self.prompter = Prompter(self.prompt_template, self.prompt_func)
         self.model, self.tokenizer = self.init_model_and_tokenizer()
 
     def _fit(
@@ -213,11 +219,21 @@ class LLM(Model):
         return compute_metrics
 
     @ensure_initialized
-    def to_call(self, X: t.Any, **kwargs):
-        """
-        Overwrite `Model.to_call` method to support self.object=None.
-        """
-        return self._generate(X, **kwargs)
+    def _predict(
+        self,
+        X: t.Union[str, t.List[str], t.List[dict[str, str]]],
+        one: bool = False,
+        **kwargs: t.Any,
+    ):
+        # support string and dialog format
+        one = isinstance(X, str)
+        if not one and isinstance(X, list):
+            one = isinstance(X[0], dict)
+
+        xs = [X] if one else X
+        xs = [self.prompter(x, **kwargs) for x in xs]
+        results = self._generate(xs, **kwargs)
+        return results[0] if one else results
 
     def _generate(self, X: t.Any, adapter_name=None, **kwargs):
         """
@@ -235,20 +251,23 @@ class LLM(Model):
 
         elif hasattr(self.model, "disable_adapter"):
             with self.model.disable_adapter():
-                return self.generate(X, **kwargs)
+                return self._base_generate(X, **kwargs)
 
-        return self.generate(X, **kwargs)
+        return self._base_generate(X, **kwargs)
 
-    def generate(self, X: t.Any, **kwargs):
+    def _base_generate(self, X: t.Any, **kwargs):
         """
         Generate text.
         Can overwrite this method to support more inference methods.
         """
-        model_inputs = self.tokenizer(X, return_tensors="pt").to(self.model.device)
+        model_inputs = self.tokenizer(X, return_tensors="pt", padding=True).to(
+            self.model.device
+        )
         kwargs.setdefault("pad_token_id", self.tokenizer.eos_token_id)
         outputs = self.model.generate(**model_inputs, **kwargs)
         texts = self.tokenizer.batch_decode(outputs)
         texts = [text.replace(self.tokenizer.eos_token, "") for text in texts]
+        texts = [text.replace(self.tokenizer.pad_token, "") for text in texts]
         if isinstance(X, str):
             return texts[0]
         return texts

superduperdb/ext/llm/utils.py
@@ -1,0 +1,25 @@
+import dataclasses as dc
+import inspect
+import typing as t
+
+from superduperdb.ext.utils import format_prompt
+
+
+@dc.dataclass
+class Prompter:
+    prompt_template: str = "{input}"
+    prompt_func: t.Optional[t.Callable] = dc.field(default=None)
+
+    def __call__(self, x: t.Any, **kwargs):
+        if self.prompt_func is not None:
+            sig = inspect.signature(self.prompt_func)
+            new_kwargs = {}
+            for k, v in kwargs.items():
+                if k in sig.parameters:
+                    new_kwargs[k] = v
+            return self.prompt_func(x, **new_kwargs)
+
+        if isinstance(x, str):
+            return format_prompt(x, self.prompt_template, kwargs.get("context", None))
+        else:
+            return x

Pinnacle-Networks/Pinnacle

superduperdb/ext/llm/base.py
@@ -10,7 +10,8 @@ from typing import Any, Callable, List, Optional, Union
 from pinnacledb import logging
 from pinnacledb.components.component import Component
 from pinnacledb.components.model import _Predictor
-from pinnacledb.ext.utils import ensure_initialized, format_prompt
+from pinnacledb.ext.llm.utils import Prompter
+from pinnacledb.ext.utils import ensure_initialized
 
 if typing.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
@@ -37,6 +38,7 @@ class _BaseLLM(Component, _Predictor, metaclass=abc.ABCMeta):
         super().__post_init__()
         self.takes_context = True
         self.identifier = self.identifier.replace("/", "-")
+        self.prompter = Prompter(self.prompt_template, self.prompt_func)
         assert "{input}" in self.prompt_template, "Template must contain {input}"
 
     def to_call(self, X, *args, **kwargs):
@@ -87,25 +89,11 @@ class _BaseLLM(Component, _Predictor, metaclass=abc.ABCMeta):
             one = isinstance(X[0], dict)
 
         if one:
-            x = self.format_prompt(X, **kwargs)
+            x = self.prompter(X, **kwargs)
             return self._generate(x, **kwargs)
         else:
-            xs = [self.format_prompt(x, **kwargs) for x in X]
+            xs = [self.prompter(x, **kwargs) for x in X]
             return self._batch_generate(xs, **kwargs)
-
-    def format_prompt(self, x, **kwargs):
-        """
-        Format a prompt with the given prompt function or template.
-        :param x: The input to format.
-        :param kwargs: Any additional arguments to pass to the prompt function.
-        """
-        if self.prompt_func is not None:
-            return self.prompt_func(x, **self.get_kwargs(self.prompt_func, kwargs))
-
-        if isinstance(x, str):
-            return format_prompt(x, self.prompt_template, kwargs.get("context", None))
-        else:
-            return x
 
     def get_kwargs(self, func, *kwargs_list):
         """

superduperdb/ext/llm/model.py
@@ -22,6 +22,7 @@ from pinnacledb.components.model import (
     Model,
     _TrainingConfiguration,
 )
+from pinnacledb.ext.llm.utils import Prompter
 from pinnacledb.ext.utils import ensure_initialized
 
 if typing.TYPE_CHECKING:
@@ -96,6 +97,8 @@ class LLM(Model):
         all the kwargs will pass to `transformers.AutoModelForCausalLM.from_pretrained`
     : param tokenizer_kwags: tokenizer kwargs,
         all the kwargs will pass to `transformers.AutoTokenizer.from_pretrained`
+    : param prompt_template: prompt template, default is "{input}"
+    : param prompt_func: prompt function, default is None
     """
 
     identifier: str = ""
@@ -104,6 +107,8 @@ class LLM(Model):
     object: t.Optional[transformers.Trainer] = None
     model_kwargs: t.Dict = dc.field(default_factory=dict)
     tokenizer_kwags: t.Dict = dc.field(default_factory=dict)
+    prompt_template: str = "{input}"
+    prompt_func: t.Optional[t.Callable] = dc.field(default=None)
 
     def __post_init__(self):
         self.identifier = self.identifier or self.model_name_or_path
@@ -140,6 +145,7 @@ class LLM(Model):
         return trainer
 
     def init(self):
+        self.prompter = Prompter(self.prompt_template, self.prompt_func)
         self.model, self.tokenizer = self.init_model_and_tokenizer()
 
     def _fit(
@@ -213,11 +219,21 @@ class LLM(Model):
         return compute_metrics
 
     @ensure_initialized
-    def to_call(self, X: t.Any, **kwargs):
-        """
-        Overwrite `Model.to_call` method to support self.object=None.
-        """
-        return self._generate(X, **kwargs)
+    def _predict(
+        self,
+        X: t.Union[str, t.List[str], t.List[dict[str, str]]],
+        one: bool = False,
+        **kwargs: t.Any,
+    ):
+        # support string and dialog format
+        one = isinstance(X, str)
+        if not one and isinstance(X, list):
+            one = isinstance(X[0], dict)
+
+        xs = [X] if one else X
+        xs = [self.prompter(x, **kwargs) for x in xs]
+        results = self._generate(xs, **kwargs)
+        return results[0] if one else results
 
     def _generate(self, X: t.Any, adapter_name=None, **kwargs):
         """
@@ -235,20 +251,23 @@ class LLM(Model):
 
         elif hasattr(self.model, "disable_adapter"):
             with self.model.disable_adapter():
-                return self.generate(X, **kwargs)
+                return self._base_generate(X, **kwargs)
 
-        return self.generate(X, **kwargs)
+        return self._base_generate(X, **kwargs)
 
-    def generate(self, X: t.Any, **kwargs):
+    def _base_generate(self, X: t.Any, **kwargs):
         """
         Generate text.
         Can overwrite this method to support more inference methods.
         """
-        model_inputs = self.tokenizer(X, return_tensors="pt").to(self.model.device)
+        model_inputs = self.tokenizer(X, return_tensors="pt", padding=True).to(
+            self.model.device
+        )
         kwargs.setdefault("pad_token_id", self.tokenizer.eos_token_id)
         outputs = self.model.generate(**model_inputs, **kwargs)
         texts = self.tokenizer.batch_decode(outputs)
         texts = [text.replace(self.tokenizer.eos_token, "") for text in texts]
+        texts = [text.replace(self.tokenizer.pad_token, "") for text in texts]
         if isinstance(X, str):
             return texts[0]
         return texts

superduperdb/ext/llm/utils.py
@@ -1,0 +1,25 @@
+import dataclasses as dc
+import inspect
+import typing as t
+
+from pinnacledb.ext.utils import format_prompt
+
+
+@dc.dataclass
+class Prompter:
+    prompt_template: str = "{input}"
+    prompt_func: t.Optional[t.Callable] = dc.field(default=None)
+
+    def __call__(self, x: t.Any, **kwargs):
+        if self.prompt_func is not None:
+            sig = inspect.signature(self.prompt_func)
+            new_kwargs = {}
+            for k, v in kwargs.items():
+                if k in sig.parameters:
+                    new_kwargs[k] = v
+            return self.prompt_func(x, **new_kwargs)
+
+        if isinstance(x, str):
+            return format_prompt(x, self.prompt_template, kwargs.get("context", None))
+        else:
+            return x