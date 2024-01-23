superduper-io/superduper

Optimize metrics, validation_sets, etc. in llm training

examples/llm_finetune.py
@@ -6,7 +6,7 @@ from datasets import load_dataset
 from superduperdb import superduper
 from superduperdb.backends.mongodb import Collection
 from superduperdb.base.document import Document
-from superduperdb.ext.llm import LLM, LLMTrainingConfiguration
+from superduperdb.ext.llm.model import LLM, LLMTrainingConfiguration
 
 prompt_template = (
     "Below is an instruction that describes a task,"
@@ -104,7 +104,7 @@ def inference(db, model_identifier, output_dir):
 
 
 if __name__ == "__main__":
-    db = superduper("mongodb://localhost:27017/llm-finetune")
+    db = superduper("mongomock://llm-finetune")
     model = "facebook/opt-125m"
     output_dir = "outputs/llm-finetune"
 

superduperdb/ext/llm/model.py
@@ -11,11 +11,13 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     BitsAndBytesConfig,
+    TrainerCallback,
     TrainingArguments,
 )
 
 from superduperdb import logging
 from superduperdb.backends.query_dataset import query_dataset_factory
+from superduperdb.components.dataset import Dataset as _Dataset
 from superduperdb.components.model import (
     Model,
     _TrainingConfiguration,
@@ -25,8 +27,15 @@ from superduperdb.ext.utils import ensure_initialized
 if typing.TYPE_CHECKING:
     from superduperdb.backends.base.query import Select
     from superduperdb.base.datalayer import Datalayer
-    from superduperdb.components.dataset import Dataset as _Dataset
     from superduperdb.components.metric import Metric
+
+
+class LLMCallback(TrainerCallback):
+    def __init__(self, llm: "LLM"):
+        self.llm = llm
+
+    def on_evaluate(self, args, state, control, **kwargs):
+        self.llm.append_metrics(state.log_history[-1])
 
 
 @dc.dataclass
@@ -167,27 +176,41 @@ class LLM(Model):
         )
         self._prepare_lora_training(training_args)
 
-        train_dataset, eval_dataset = self.get_datasets(
+        train_dataset, eval_datasets = self.get_datasets(
             X,
             y,
             db,
             select,
+            db_validation_sets=validation_sets,
             data_prefetch=data_prefetch,
-            eval=True,
             prefetch_size=kwargs.pop("prefetch_size", 10000),
         )
 
         # TODO: Defind callbacks about superduperdb side
         trainer = self.create_trainer(
             train_dataset,
-            eval_dataset,
-            compute_metrics=metrics,
+            eval_datasets,
+            compute_metrics=self.get_compute_metrics(metrics),
             training_args=training_args,
             **kwargs,
         )
+        trainer.add_callback(LLMCallback(self))
         trainer.model.config.use_cache = False
         trainer.train()
         trainer.save_state()
+
+    def get_compute_metrics(self, metrics):
+        if not metrics:
+            return None
+
+        def compute_metrics(eval_preds):
+            output = {}
+            logits, labels = eval_preds
+            for metric in metrics:
+                output[metric.identifier] = metric(logits, labels)
+            return output
+
+        return compute_metrics
 
     @ensure_initialized
     def to_call(self, X: t.Any, **kwargs):
@@ -327,8 +350,8 @@ class LLM(Model):
         y,
         db,
         select,
+        db_validation_sets: t.Optional[t.Sequence[t.Union[str, "_Dataset"]]] = None,
         data_prefetch: bool = False,
-        eval: bool = False,
         prefetch_size: int = 10000,
     ):
         keys = [X]
@@ -344,7 +367,11 @@ class LLM(Model):
             transform=self.preprocess,
             prefetch_size=prefetch_size,
         )
-        if eval:
+        train_dataset = Dataset.from_list(list(train_dataset))
+
+        validation_sets = {}
+        if db_validation_sets is None:
+            logging.warn("No validation sets provided, using validation set from db")
             eval_dataset = query_dataset_factory(
                 keys=keys,
                 data_prefetch=data_prefetch,
@@ -354,20 +381,35 @@ class LLM(Model):
                 transform=self.preprocess,
                 prefetch_size=prefetch_size,
             )
+            eval_dataset = Dataset.from_list(list(eval_dataset))
+            validation_sets["_DEFAULT"] = eval_dataset
         else:
-            eval_dataset = None
+            for _, db_dataset in enumerate(db_validation_sets):
+                if isinstance(db_dataset, str):
+                    db_dataset = db.load("dataset", db_dataset)
+
+                assert isinstance(db_dataset, _Dataset), (
+                    "Validation set must be a dataset, "
+                    f"got {type(db_dataset)} instead."
+                )
+
+                datas = []
+                for data in db_dataset.data:
+                    data = data.unpack()
+                    datas.append({key: data[key] for key in keys})
+                dataset = Dataset.from_list(datas)
+                validation_sets[db_dataset.identifier] = dataset
 
         def process_func(example):
             return self.tokenize(example, X, y)
 
-        train_dataset = Dataset.from_list(list(train_dataset))
-        if eval_dataset is not None:
-            eval_dataset = Dataset.from_list(list(eval_dataset))
-
         train_dataset = train_dataset.map(process_func)
-        if eval_dataset is not None:
-            eval_dataset = eval_dataset.map(process_func)
-        return train_dataset, eval_dataset
+        for key, dataset in validation_sets.items():
+            validation_sets[key] = dataset.map(process_func)
+
+        # If no validation sets provided, use the validation set from db
+        validation_sets = validation_sets.get("_DEFAULT", validation_sets)
+        return train_dataset, validation_sets
 
     def tokenize(self, example, X, y):
         prompt = example[X]
@@ -395,8 +437,4 @@ class LLM(Model):
         if isinstance(db.databackend, IbisDataBackend) and self.encoder is None:
             self.encoder = dtype("str")
 
-        # since then the `.add` clause is not necessary
-        output_component = db.databackend.create_model_table_or_collection(self)
-
-        if output_component is not None:
-            db.add(output_component)
+        super().post_create(db)

test/unittest/ext/llm/test_model.py
@@ -1,4 +1,5 @@
 import os
+import random
 from test.db_config import DBConfig
 from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predict
 
@@ -6,6 +7,8 @@ import pytest
 
 from superduperdb.backends.mongodb.query import Collection
 from superduperdb.base.document import Document
+from superduperdb.components.dataset import Dataset
+from superduperdb.components.metric import Metric
 from superduperdb.ext.llm.model import LLM, LLMTrainingConfiguration
 
 TEST_MODEL_NAME = "facebook/opt-125m"
@@ -46,8 +49,9 @@ def test_training(db, tmpdir):
         fold = "train" if i < 32 else "valid"
         datas.append({"text": text, "id": str(i), "fold": fold})
 
-    db.execute(Collection("doc").insert_many(list(map(Document, datas))))
-    select = Collection("doc").find()
+    collection = Collection("doc")
+    db.execute(collection.insert_many(list(map(Document, datas))))
+    select = collection.find()
 
     model = LLM(
         identifier="llm",
@@ -56,7 +60,7 @@ def test_training(db, tmpdir):
     )
     training_configuration = LLMTrainingConfiguration(
         identifier="llm-finetune",
-        output_dir=tmpdir,
+        output_dir=str(tmpdir),
         lora_r=64,
         lora_alpha=16,
         lora_dropout=0.05,
@@ -80,12 +84,35 @@ def test_training(db, tmpdir):
         max_length=64,
     )
 
+    def metric(predictions, targets):
+        return random.random()
+
     model.fit(
         X="text",
         db=db,
         select=select,
         configuration=training_configuration,
         prefetch_size=1000,
+        validation_sets=[
+            Dataset(
+                identifier="dataset_1",
+                select=collection.find({"_fold": "valid"}),
+            ),
+            Dataset(
+                identifier="dataset_2",
+                select=collection.find({"_fold": "valid"}),
+            ),
+        ],
+        metrics=[
+            Metric(
+                identifier="metrics1",
+                object=metric,
+            ),
+            Metric(
+                identifier="metrics2",
+                object=metric,
+            ),
+        ],
     )
 
     checkpoints = os.listdir(tmpdir)

Pinnacle-Networks/Pinnacle

examples/llm_finetune.py
@@ -6,7 +6,7 @@ from datasets import load_dataset
 from pinnacledb import pinnacle
 from pinnacledb.backends.mongodb import Collection
 from pinnacledb.base.document import Document
-from pinnacledb.ext.llm import LLM, LLMTrainingConfiguration
+from pinnacledb.ext.llm.model import LLM, LLMTrainingConfiguration
 
 prompt_template = (
     "Below is an instruction that describes a task,"
@@ -104,7 +104,7 @@ def inference(db, model_identifier, output_dir):
 
 
 if __name__ == "__main__":
-    db = pinnacle("mongodb://localhost:27017/llm-finetune")
+    db = pinnacle("mongomock://llm-finetune")
     model = "facebook/opt-125m"
     output_dir = "outputs/llm-finetune"
 

superduperdb/ext/llm/model.py
@@ -11,11 +11,13 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     BitsAndBytesConfig,
+    TrainerCallback,
     TrainingArguments,
 )
 
 from pinnacledb import logging
 from pinnacledb.backends.query_dataset import query_dataset_factory
+from pinnacledb.components.dataset import Dataset as _Dataset
 from pinnacledb.components.model import (
     Model,
     _TrainingConfiguration,
@@ -25,8 +27,15 @@ from pinnacledb.ext.utils import ensure_initialized
 if typing.TYPE_CHECKING:
     from pinnacledb.backends.base.query import Select
     from pinnacledb.base.datalayer import Datalayer
-    from pinnacledb.components.dataset import Dataset as _Dataset
     from pinnacledb.components.metric import Metric
+
+
+class LLMCallback(TrainerCallback):
+    def __init__(self, llm: "LLM"):
+        self.llm = llm
+
+    def on_evaluate(self, args, state, control, **kwargs):
+        self.llm.append_metrics(state.log_history[-1])
 
 
 @dc.dataclass
@@ -167,27 +176,41 @@ class LLM(Model):
         )
         self._prepare_lora_training(training_args)
 
-        train_dataset, eval_dataset = self.get_datasets(
+        train_dataset, eval_datasets = self.get_datasets(
             X,
             y,
             db,
             select,
+            db_validation_sets=validation_sets,
             data_prefetch=data_prefetch,
-            eval=True,
             prefetch_size=kwargs.pop("prefetch_size", 10000),
         )
 
         # TODO: Defind callbacks about pinnacledb side
         trainer = self.create_trainer(
             train_dataset,
-            eval_dataset,
-            compute_metrics=metrics,
+            eval_datasets,
+            compute_metrics=self.get_compute_metrics(metrics),
             training_args=training_args,
             **kwargs,
         )
+        trainer.add_callback(LLMCallback(self))
         trainer.model.config.use_cache = False
         trainer.train()
         trainer.save_state()
+
+    def get_compute_metrics(self, metrics):
+        if not metrics:
+            return None
+
+        def compute_metrics(eval_preds):
+            output = {}
+            logits, labels = eval_preds
+            for metric in metrics:
+                output[metric.identifier] = metric(logits, labels)
+            return output
+
+        return compute_metrics
 
     @ensure_initialized
     def to_call(self, X: t.Any, **kwargs):
@@ -327,8 +350,8 @@ class LLM(Model):
         y,
         db,
         select,
+        db_validation_sets: t.Optional[t.Sequence[t.Union[str, "_Dataset"]]] = None,
         data_prefetch: bool = False,
-        eval: bool = False,
         prefetch_size: int = 10000,
     ):
         keys = [X]
@@ -344,7 +367,11 @@ class LLM(Model):
             transform=self.preprocess,
             prefetch_size=prefetch_size,
         )
-        if eval:
+        train_dataset = Dataset.from_list(list(train_dataset))
+
+        validation_sets = {}
+        if db_validation_sets is None:
+            logging.warn("No validation sets provided, using validation set from db")
             eval_dataset = query_dataset_factory(
                 keys=keys,
                 data_prefetch=data_prefetch,
@@ -354,20 +381,35 @@ class LLM(Model):
                 transform=self.preprocess,
                 prefetch_size=prefetch_size,
             )
+            eval_dataset = Dataset.from_list(list(eval_dataset))
+            validation_sets["_DEFAULT"] = eval_dataset
         else:
-            eval_dataset = None
+            for _, db_dataset in enumerate(db_validation_sets):
+                if isinstance(db_dataset, str):
+                    db_dataset = db.load("dataset", db_dataset)
+
+                assert isinstance(db_dataset, _Dataset), (
+                    "Validation set must be a dataset, "
+                    f"got {type(db_dataset)} instead."
+                )
+
+                datas = []
+                for data in db_dataset.data:
+                    data = data.unpack()
+                    datas.append({key: data[key] for key in keys})
+                dataset = Dataset.from_list(datas)
+                validation_sets[db_dataset.identifier] = dataset
 
         def process_func(example):
             return self.tokenize(example, X, y)
 
-        train_dataset = Dataset.from_list(list(train_dataset))
-        if eval_dataset is not None:
-            eval_dataset = Dataset.from_list(list(eval_dataset))
-
         train_dataset = train_dataset.map(process_func)
-        if eval_dataset is not None:
-            eval_dataset = eval_dataset.map(process_func)
-        return train_dataset, eval_dataset
+        for key, dataset in validation_sets.items():
+            validation_sets[key] = dataset.map(process_func)
+
+        # If no validation sets provided, use the validation set from db
+        validation_sets = validation_sets.get("_DEFAULT", validation_sets)
+        return train_dataset, validation_sets
 
     def tokenize(self, example, X, y):
         prompt = example[X]
@@ -395,8 +437,4 @@ class LLM(Model):
         if isinstance(db.databackend, IbisDataBackend) and self.encoder is None:
             self.encoder = dtype("str")
 
-        # since then the `.add` clause is not necessary
-        output_component = db.databackend.create_model_table_or_collection(self)
-
-        if output_component is not None:
-            db.add(output_component)
+        super().post_create(db)

test/unittest/ext/llm/test_model.py
@@ -1,4 +1,5 @@
 import os
+import random
 from test.db_config import DBConfig
 from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predict
 
@@ -6,6 +7,8 @@ import pytest
 
 from pinnacledb.backends.mongodb.query import Collection
 from pinnacledb.base.document import Document
+from pinnacledb.components.dataset import Dataset
+from pinnacledb.components.metric import Metric
 from pinnacledb.ext.llm.model import LLM, LLMTrainingConfiguration
 
 TEST_MODEL_NAME = "facebook/opt-125m"
@@ -46,8 +49,9 @@ def test_training(db, tmpdir):
         fold = "train" if i < 32 else "valid"
         datas.append({"text": text, "id": str(i), "fold": fold})
 
-    db.execute(Collection("doc").insert_many(list(map(Document, datas))))
-    select = Collection("doc").find()
+    collection = Collection("doc")
+    db.execute(collection.insert_many(list(map(Document, datas))))
+    select = collection.find()
 
     model = LLM(
         identifier="llm",
@@ -56,7 +60,7 @@ def test_training(db, tmpdir):
     )
     training_configuration = LLMTrainingConfiguration(
         identifier="llm-finetune",
-        output_dir=tmpdir,
+        output_dir=str(tmpdir),
         lora_r=64,
         lora_alpha=16,
         lora_dropout=0.05,
@@ -80,12 +84,35 @@ def test_training(db, tmpdir):
         max_length=64,
     )
 
+    def metric(predictions, targets):
+        return random.random()
+
     model.fit(
         X="text",
         db=db,
         select=select,
         configuration=training_configuration,
         prefetch_size=1000,
+        validation_sets=[
+            Dataset(
+                identifier="dataset_1",
+                select=collection.find({"_fold": "valid"}),
+            ),
+            Dataset(
+                identifier="dataset_2",
+                select=collection.find({"_fold": "valid"}),
+            ),
+        ],
+        metrics=[
+            Metric(
+                identifier="metrics1",
+                object=metric,
+            ),
+            Metric(
+                identifier="metrics2",
+                object=metric,
+            ),
+        ],
     )
 
     checkpoints = os.listdir(tmpdir)