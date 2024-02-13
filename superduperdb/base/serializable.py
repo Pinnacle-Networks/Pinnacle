superduper-io/superduper

Merge Serializer, Datatype and Document

superduperdb/base/serializable.py
@@ -2,93 +2,34 @@ import dataclasses as dc
 import importlib
 import inspect
 import typing as t
+from superduperdb.base.config import BytesEncoding
+from superduperdb.base.leaf import Leaf
 
 from superduperdb.misc.serialization import asdict
 
 
-def is_component_metadata(r: dict) -> bool:
-    COMPONENT_KEYS = {'type_id', 'identifier', 'version'}
-    if COMPONENT_KEYS == set(r):
-        return True
-    return False
-
-
-def is_component(r: dict) -> bool:
-    COMPONENT_KEYS = {'cls', 'dict', 'module'}
-    if COMPONENT_KEYS <= set(r):
-        return True
-    return False
-
-
-def _deserialize(r: t.Any, db: None = None) -> t.Any:
+def _from_dict(r: t.Any, db: None = None) -> t.Any:
+    from superduperdb.base.document import Document
+    if isinstance(r, Document):
+        r = r.unpack()
     if isinstance(r, (list, tuple)):
-        return [_deserialize(i, db=db) for i in r]
-
+        return [_from_dict(i, db=db) for i in r]
     if not isinstance(r, dict):
         return r
-
-    if not is_component(r):
-        return {k: _deserialize(v, db=db) for k, v in r.items()}
-
-    module = importlib.import_module(r['module'])
-    component_cls = getattr(module, r['cls'])
-
-    kwargs = _deserialize(r['dict'])
-    if 'db' in inspect.signature(component_cls.__init__).parameters:
-        kwargs.update(db=db)
-
-    instance = component_cls(**{k: v for k, v in kwargs.items() if k != 'version'})
-
-    # special handling of Component.version
-    from superduperdb.components.component import Component
-
-    if issubclass(component_cls, Component):
-        instance.version = r.get('version', None)
-
-    return instance
-
-
-def _serialize(item: t.Any, serialize_variables: bool = True):
-    if isinstance(item, Serializable):
-        sub_dict = {
-            k: _serialize(getattr(item, k), serialize_variables=serialize_variables)
-            for k in item.dict()
-        }
-        out = {
-            'cls': item.__class__.__name__,
-            'dict': sub_dict,
-            'module': item.__class__.__module__,
-        }
-        from superduperdb.components.component import Component
-
-        if isinstance(item, Component):
-            out.update(
-                {
-                    'type_id': item.type_id,
-                    'identifier': item.identifier,
-                    'version': getattr(
-                        item,
-                        'version',
-                        None,
-                    ),  # type: ignore[dict-item]
-                }
-            )
-        return out
-    if isinstance(item, dict):
-        return {
-            k: _serialize(v, serialize_variables=serialize_variables)
-            for k, v in item.items()
-        }
-    if isinstance(item, (list, tuple)):
-        return [_serialize(x, serialize_variables=serialize_variables) for x in item]
-    if serialize_variables:
-        if isinstance(item, Variable):
-            return {
-                'cls': 'Variable',
-                'dict': {'value': item.value, 'setter_callback': item.setter_callback},
-                'module': 'superduperdb.base.serializable',
-            }
-    return item
+    if '_content' in r:
+        r = r['_content']
+    if 'cls' in r and 'module' in r and 'dict' in r:
+        module = importlib.import_module(r['module'])
+        cls_ = getattr(module, r['cls'])
+        kwargs = _from_dict(r['dict'])
+        kwargs_init = {k: v for k, v in kwargs.items() if k not in cls_.set_post_init}
+        kwargs_post_init = {k: v for k, v in kwargs.items() if k in cls_.set_post_init}
+        instance = cls_(**kwargs_init)
+        for k, v in kwargs_post_init.items():
+            setattr(instance, k, v)
+        return instance
+    else:
+        return {k: _from_dict(v, db=db) for k, v in r.items()}
 
 
 class VariableError(Exception):
@@ -96,22 +37,17 @@ class VariableError(Exception):
 
 
 def _find_variables(r):
-    from .document import Document
-
     if isinstance(r, dict):
         return sum([_find_variables(v) for v in r.values()], [])
     elif isinstance(r, (list, tuple)):
         return sum([_find_variables(v) for v in r], [])
     elif isinstance(r, Variable):
         return [r]
-    elif isinstance(r, Document):
-        return r.variables
     return []
 
 
 def _replace_variables(x, db, **kwargs):
     from .document import Document
-
     if isinstance(x, dict):
         return {
             _replace_variables(k, db, **kwargs): _replace_variables(v, db, **kwargs)
@@ -127,35 +63,48 @@ def _replace_variables(x, db, **kwargs):
 
 
 @dc.dataclass
-class Serializable:
+class Serializable(Leaf):
     """
     Base class for serializable objects. This class is used to serialize and
     deserialize objects to and from JSON + Artifact instances.
     """
 
-    deserialize = staticmethod(_deserialize)
-    serialize = _serialize
+    set_post_init: t.ClassVar[t.Sequence] = ()
+
+    @property
+    def unique_id(self):
+        return str(hash(self.dict().encode()))
 
     @property
     def variables(self) -> t.List['Variable']:
         out = {}
-        v = _find_variables(self.dict())
+        r = self.encode(leaf_types_to_keep=(Variable,))
+        v = _find_variables(r)
         for var in v:
             out[var.value] = var
         return sorted(list(out.values()), key=lambda x: x.value)
 
     def set_variables(self, db, **kwargs) -> 'Serializable':
-        r = self.serialize(
-            serialize_variables=False
-        )  # get serialized version of class instance
-        r = _replace_variables(r, db, **kwargs)  # replace variables with values
-        return self.deserialize(r)  # rebuild new class instance
+        r = self.encode(leaf_types_to_keep=(Variable,))
+        r = _replace_variables(r, db, **kwargs)
+        return self.decode(r)
 
-    def dict(self) -> t.Dict[str, t.Any]:
-        return asdict(self)
+    def encode(self, bytes_encoding: t.Optional[BytesEncoding] = None, leaf_types_to_keep: t.Sequence = ()):
+        r = dict(self.dict().encode(leaf_types_to_keep=leaf_types_to_keep))
+        r['leaf_type'] = 'serializable'
+        return {'_content': r}
+
+    @classmethod
+    def decode(cls, r, db: t.Optional[t.Any] = None, reference: bool = False):
+        return _from_dict(r)
+
+    def dict(self):
+        from superduperdb import Document
+        return Document(asdict(self))
 
 
-class Variable:
+@dc.dataclass
+class Variable(Serializable):
     """
     Mechanism for allowing "free variables" in a serializable object.
     The idea is to allow a variable to be set at runtime, rather than
@@ -167,10 +116,10 @@ class Variable:
                             variable.
     """
 
-    def __init__(
-        self, value: t.Union[str, t.Any], setter_callback: t.Optional[t.Callable] = None
-    ):
-        self.value = value
+    value: t.Any
+    setter_callback: dc.InitVar[t.Optional[t.Callable]] = None
+
+    def __post_init__(self, setter_callback):
         self.setter_callback = setter_callback
 
     def __repr__(self) -> str:

superduperdb/ext/utils.py
@@ -8,7 +8,7 @@ import numpy as np
 from superduperdb import logging
 
 if t.TYPE_CHECKING:
-    from superduperdb.components.encoder import Encoder
+    from superduperdb.components.datatype import DataType
 
 
 def str_shape(shape: t.Sequence[int]) -> str:
@@ -43,19 +43,18 @@ def format_prompt(X: str, prompt: str, context: t.Optional[t.List[str]] = None) 
 def superduperencode(object):
     if isinstance(object, np.ndarray):
         from superduperdb.ext.numpy import array
-
-        encoded = array(dtype=object.dtype, shape=object.shape).encode(object)
+        encoded = array(dtype=object.dtype, shape=object.shape)(object).encode()
         encoded['shape'] = object.shape
         encoded['dtype'] = str(object.dtype)
         return encoded
     return object
 
 
-def superduperdecode(r: t.Any, encoders: t.List['Encoder']):
+def superduperdecode(r: t.Any, encoders: t.List['DataType']):
     if isinstance(r, dict):
-        encoder = encoders[r['_content']['encoder']]
+        encoder = encoders[r['_content']['datatype']]
         b = base64.b64decode(r['_content']['bytes'])
-        return encoder.decode(b).x
+        return encoder.decoder(b)
     return r
 
 

test/unittest/component/test_component.py
@@ -1,0 +1,24 @@
+import shutil
+import pytest
+import os
+
+from superduperdb import Model
+from superduperdb.components.component import Component
+from superduperdb.components.datatype import dill_serializer
+
+
+@pytest.fixture
+def cleanup():
+    yield
+    os.remove('test_export.tar.gz')
+    shutil.rmtree('test_export')
+
+
+def test_compile_decompile(cleanup):
+    m = Model('test_export', object=lambda x: x, datatype=dill_serializer)
+    m.version = 0
+    m.datatype.version = 0
+    m.compile()
+    assert os.path.exists('test_export.tar.gz')
+    m_reload = Component.decompile('test_export.tar.gz')
+    assert isinstance(m_reload, Model)

Pinnacle-Networks/Pinnacle

superduperdb/base/serializable.py
@@ -1,94 +1,35 @@
 import dataclasses as dc
 import importlib
-import inspect
 import typing as t
 
+from pinnacledb.base.config import BytesEncoding
+from pinnacledb.base.leaf import Leaf
 from pinnacledb.misc.serialization import asdict
 
 
-def is_component_metadata(r: dict) -> bool:
-    COMPONENT_KEYS = {'type_id', 'identifier', 'version'}
-    if COMPONENT_KEYS == set(r):
-        return True
-    return False
+def _from_dict(r: t.Any, db: None = None) -> t.Any:
+    from pinnacledb.base.document import Document
 
-
-def is_component(r: dict) -> bool:
-    COMPONENT_KEYS = {'cls', 'dict', 'module'}
-    if COMPONENT_KEYS <= set(r):
-        return True
-    return False
-
-
-def _deserialize(r: t.Any, db: None = None) -> t.Any:
+    if isinstance(r, Document):
+        r = r.unpack()
     if isinstance(r, (list, tuple)):
-        return [_deserialize(i, db=db) for i in r]
-
+        return [_from_dict(i, db=db) for i in r]
     if not isinstance(r, dict):
         return r
-
-    if not is_component(r):
-        return {k: _deserialize(v, db=db) for k, v in r.items()}
-
-    module = importlib.import_module(r['module'])
-    component_cls = getattr(module, r['cls'])
-
-    kwargs = _deserialize(r['dict'])
-    if 'db' in inspect.signature(component_cls.__init__).parameters:
-        kwargs.update(db=db)
-
-    instance = component_cls(**{k: v for k, v in kwargs.items() if k != 'version'})
-
-    # special handling of Component.version
-    from pinnacledb.components.component import Component
-
-    if issubclass(component_cls, Component):
-        instance.version = r.get('version', None)
-
-    return instance
-
-
-def _serialize(item: t.Any, serialize_variables: bool = True):
-    if isinstance(item, Serializable):
-        sub_dict = {
-            k: _serialize(getattr(item, k), serialize_variables=serialize_variables)
-            for k in item.dict()
-        }
-        out = {
-            'cls': item.__class__.__name__,
-            'dict': sub_dict,
-            'module': item.__class__.__module__,
-        }
-        from pinnacledb.components.component import Component
-
-        if isinstance(item, Component):
-            out.update(
-                {
-                    'type_id': item.type_id,
-                    'identifier': item.identifier,
-                    'version': getattr(
-                        item,
-                        'version',
-                        None,
-                    ),  # type: ignore[dict-item]
-                }
-            )
-        return out
-    if isinstance(item, dict):
-        return {
-            k: _serialize(v, serialize_variables=serialize_variables)
-            for k, v in item.items()
-        }
-    if isinstance(item, (list, tuple)):
-        return [_serialize(x, serialize_variables=serialize_variables) for x in item]
-    if serialize_variables:
-        if isinstance(item, Variable):
-            return {
-                'cls': 'Variable',
-                'dict': {'value': item.value, 'setter_callback': item.setter_callback},
-                'module': 'pinnacledb.base.serializable',
-            }
-    return item
+    if '_content' in r:
+        r = r['_content']
+    if 'cls' in r and 'module' in r and 'dict' in r:
+        module = importlib.import_module(r['module'])
+        cls_ = getattr(module, r['cls'])
+        kwargs = _from_dict(r['dict'])
+        kwargs_init = {k: v for k, v in kwargs.items() if k not in cls_.set_post_init}
+        kwargs_post_init = {k: v for k, v in kwargs.items() if k in cls_.set_post_init}
+        instance = cls_(**kwargs_init)
+        for k, v in kwargs_post_init.items():
+            setattr(instance, k, v)
+        return instance
+    else:
+        return {k: _from_dict(v, db=db) for k, v in r.items()}
 
 
 class VariableError(Exception):
@@ -96,16 +37,12 @@ class VariableError(Exception):
 
 
 def _find_variables(r):
-    from .document import Document
-
     if isinstance(r, dict):
         return sum([_find_variables(v) for v in r.values()], [])
     elif isinstance(r, (list, tuple)):
         return sum([_find_variables(v) for v in r], [])
     elif isinstance(r, Variable):
         return [r]
-    elif isinstance(r, Document):
-        return r.variables
     return []
 
 
@@ -127,35 +64,53 @@ def _replace_variables(x, db, **kwargs):
 
 
 @dc.dataclass
-class Serializable:
+class Serializable(Leaf):
     """
     Base class for serializable objects. This class is used to serialize and
     deserialize objects to and from JSON + Artifact instances.
     """
 
-    deserialize = staticmethod(_deserialize)
-    serialize = _serialize
+    set_post_init: t.ClassVar[t.Sequence] = ()
+
+    @property
+    def unique_id(self):
+        return str(hash(self.dict().encode()))
 
     @property
     def variables(self) -> t.List['Variable']:
         out = {}
-        v = _find_variables(self.dict())
+        r = self.encode(leaf_types_to_keep=(Variable,))
+        v = _find_variables(r)
         for var in v:
             out[var.value] = var
         return sorted(list(out.values()), key=lambda x: x.value)
 
     def set_variables(self, db, **kwargs) -> 'Serializable':
-        r = self.serialize(
-            serialize_variables=False
-        )  # get serialized version of class instance
-        r = _replace_variables(r, db, **kwargs)  # replace variables with values
-        return self.deserialize(r)  # rebuild new class instance
+        r = self.encode(leaf_types_to_keep=(Variable,))
+        r = _replace_variables(r, db, **kwargs)
+        return self.decode(r)
 
-    def dict(self) -> t.Dict[str, t.Any]:
-        return asdict(self)
+    def encode(
+        self,
+        bytes_encoding: t.Optional[BytesEncoding] = None,
+        leaf_types_to_keep: t.Sequence = (),
+    ):
+        r = dict(self.dict().encode(leaf_types_to_keep=leaf_types_to_keep))
+        r['leaf_type'] = 'serializable'
+        return {'_content': r}
+
+    @classmethod
+    def decode(cls, r, db: t.Optional[t.Any] = None, reference: bool = False):
+        return _from_dict(r)
+
+    def dict(self):
+        from pinnacledb import Document
+
+        return Document(asdict(self))
 
 
-class Variable:
+@dc.dataclass
+class Variable(Serializable):
     """
     Mechanism for allowing "free variables" in a serializable object.
     The idea is to allow a variable to be set at runtime, rather than
@@ -167,10 +122,10 @@ class Variable:
                             variable.
     """
 
-    def __init__(
-        self, value: t.Union[str, t.Any], setter_callback: t.Optional[t.Callable] = None
-    ):
-        self.value = value
+    value: t.Any
+    setter_callback: dc.InitVar[t.Optional[t.Callable]] = None
+
+    def __post_init__(self, setter_callback):
         self.setter_callback = setter_callback
 
     def __repr__(self) -> str:

superduperdb/ext/utils.py
@@ -8,7 +8,7 @@ import numpy as np
 from pinnacledb import logging
 
 if t.TYPE_CHECKING:
-    from pinnacledb.components.encoder import Encoder
+    from pinnacledb.components.datatype import DataType
 
 
 def str_shape(shape: t.Sequence[int]) -> str:
@@ -44,18 +44,18 @@ def pinnacleencode(object):
     if isinstance(object, np.ndarray):
         from pinnacledb.ext.numpy import array
 
-        encoded = array(dtype=object.dtype, shape=object.shape).encode(object)
+        encoded = array(dtype=object.dtype, shape=object.shape)(object).encode()
         encoded['shape'] = object.shape
         encoded['dtype'] = str(object.dtype)
         return encoded
     return object
 
 
-def pinnacledecode(r: t.Any, encoders: t.List['Encoder']):
+def pinnacledecode(r: t.Any, encoders: t.List['DataType']):
     if isinstance(r, dict):
-        encoder = encoders[r['_content']['encoder']]
+        encoder = encoders[r['_content']['datatype']]
         b = base64.b64decode(r['_content']['bytes'])
-        return encoder.decode(b).x
+        return encoder.decoder(b)
     return r
 
 

test/unittest/component/test_component.py
@@ -1,0 +1,25 @@
+import os
+import shutil
+
+import pytest
+
+from pinnacledb import Model
+from pinnacledb.components.component import Component
+from pinnacledb.components.datatype import dill_serializer
+
+
+@pytest.fixture
+def cleanup():
+    yield
+    os.remove('test_export.tar.gz')
+    shutil.rmtree('test_export')
+
+
+def test_compile_decompile(cleanup):
+    m = Model('test_export', object=lambda x: x, datatype=dill_serializer)
+    m.version = 0
+    m.datatype.version = 0
+    m.export()
+    assert os.path.exists('test_export.tar.gz')
+    m_reload = Component.import_('test_export.tar.gz')
+    assert isinstance(m_reload, Model)