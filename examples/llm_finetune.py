superduper-io/superduper

Optimize the logic of llm infenrence

examples/llm_finetune.py
@@ -1,50 +1,81 @@
-from datasets import load_dataset
-
 from superduperdb import superduper
-from superduperdb.backends.mongodb import Collection
-from superduperdb.base.document import Document
-from superduperdb.ext.llm import LLM
-from superduperdb.ext.llm.model import LLMTrainingConfiguration
 
-model = "facebook/opt-125m"
-dataset_name = "imdb"
-train_dataset = load_dataset("imdb", split="train")
+db = superduper("mongodb://localhost:30000/llm-finetune")
+# db.drop(force=True)
 
-db = superduper("mongomock://test_llm")
+# Training
 
+from datasets import load_dataset
+dataset = load_dataset("philschmid/dolly-15k-oai-style")
+dataset = dataset['train'].train_test_split(test_size=0.05, seed=42)
+train_dataset = dataset['train']
+eval_dataset = dataset['test']
+
+from superduperdb.ext.llm.model import LLM
 llm = LLM(
     identifier="llm-finetune",
-    model_name_or_path=model,
+    model_name_or_path="mistralai/Mistral-7B-v0.1",
 )
 
 
+
+from superduperdb.ext.llm.model import LLMTrainingConfiguration
 training_configuration = LLMTrainingConfiguration(
     identifier="llm-finetune-training-config",
-    output_dir="output/llm-finetune",
+    output_dir="output/dolly-chatml",
+    learning_rate=0.0002,
+    lr_scheduler_type='constant',
+    warmup_ratio=0.003,
+    max_grad_norm=0.3,
     overwrite_output_dir=True,
-    num_train_epochs=1,
+    num_train_epochs=3,
     save_total_limit=5,
-    logging_steps=1,
-    # evaluation_strategy="steps",
-    # eval_steps=10,
-    save_steps=100,
+    logging_steps=10,
+    evaluation_strategy="steps",
+    save_steps=200,
+    eval_steps=200,
     per_device_train_batch_size=1,
     per_device_eval_batch_size=1,
-    gradient_accumulation_steps=4,
+    gradient_accumulation_steps=2,
     max_seq_length=512,
     use_lora=True,
     log_to_db=True,
+    fp16=True,
+    # gradient_checkpointing=True,
+    lora_r=8,
+    lora_alpha=16,
+    lora_dropout=0.0,
+    setup_chat_format=True,
+    bits=4,
 )
+
+
 
 llm.fit(
+    X=None,
     train_dataset=train_dataset,
-    X="text",
+    eval_dataset=eval_dataset,
     configuration=training_configuration,
     db=db,
+    packing=True,
 )
 
 
-prompt = "### Human: Who are you? ### Assistant: "
+## Inference
 
-# Automatically load lora model for prediction, default use the latest checkpoint
-print(llm.predict_one(prompt))
+from superduperdb.ext.llm.model import LLM
+llm = LLM(
+    identifier="llm-finetune",
+    model_name_or_path="mistralai/Mistral-7B-v0.1",
+    adapter_id=db.load('checkpoint', "8ac62d6fca46d145b9cd4039428d7078f1e899e6", version=200),
+    model_kwargs=dict(use_cache=True, device_map="auto", load_in_4bit=True),
+)
+
+
+messages = [
+    {
+        "role": "user",
+        "content": "What is the capital of Germany? Explain why thats the case and if it was different in the past?"
+    }
+]
+print(llm.predict_one(messages, max_new_tokens=200, do_sample=False))

superduperdb/ext/llm/model.py
@@ -7,7 +7,9 @@ import transformers
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
+    pipeline,
 )
+from transformers.pipelines.text_generation import ReturnType
 
 from superduperdb import logging
 from superduperdb.backends.query_dataset import QueryDataset, query_dataset_factory
@@ -21,7 +23,6 @@ from superduperdb.components.model import (
 )
 from superduperdb.ext.llm import training
 from superduperdb.ext.llm.utils import Prompter
-from superduperdb.misc.hash import hash_dict
 
 from .training import Checkpoint
 
@@ -64,7 +65,7 @@ class LLM(_Predictor, _Fittable):
     _encodables: t.ClassVar[t.Sequence[str]] = ("model_kwargs", "tokenizer_kwargs")
 
     identifier: str = ""
-    model_name_or_path: str = "facebook/opt-125m"
+    model_name_or_path: t.Optional[str] = None
     bits: t.Optional[int] = None
     adapter_id: t.Optional[t.Union[str, Checkpoint]] = None
     object: t.Optional[transformers.Trainer] = None
@@ -99,61 +100,85 @@ class LLM(_Predictor, _Fittable):
             self.model_kwargs["load_in_4bit"] = self.bits == 4
             self.model_kwargs["load_in_8bit"] = self.bits == 8
 
+        #  TODO: Compatible with the bug of artifact sha1 equality and will be deleted
+        self.tokenizer_kwargs.setdefault(
+            "pretrained_model_name_or_path", self.model_name_or_path
+        )
+
         super().__post_init__(artifacts)
 
-    def init_model_and_tokenizer(self):
-        model_key = hash_dict(self.model_kwargs)
-        if model_key not in self._model_cache:
-            logging.info(f"Loading model from {self.model_name_or_path}")
-            logging.info(f"model_kwargs: {self.model_kwargs}")
-            self.model_kwargs.setdefault(
-                "pretrained_model_name_or_path", self.model_name_or_path
-            )
-            model = AutoModelForCausalLM.from_pretrained(
-                **self.model_kwargs,
-            )
-            self._model_cache[model_key] = model
+    def init_pipeline(self, adapter_id: t.Optional[str] = None):
+        if adapter_id is not None:
+            self.model_kwargs['pretrained_model_name_or_path'] = adapter_id
+            self.tokenizer_kwargs['pretrained_model_name_or_path'] = adapter_id
+            from peft import AutoPeftModelForCausalLM
+
+            AutoModelCls = AutoPeftModelForCausalLM
+
         else:
-            logging.info("Reuse model from cache")
+            self.model_kwargs["pretrained_model_name_or_path"] = self.model_name_or_path
+            self.tokenizer_kwargs[
+                "pretrained_model_name_or_path"
+            ] = self.model_name_or_path
+            AutoModelCls = AutoModelForCausalLM
 
-        tokenizer_key = hash_dict(self.tokenizer_kwargs)
-        if tokenizer_key not in self._tokenizer_cache:
-            logging.info(f"Loading tokenizer from {self.model_name_or_path}")
-            logging.info(f"tokenizer_kwargs: {self.tokenizer_kwargs}")
-            self.tokenizer_kwargs.setdefault(
-                "pretrained_model_name_or_path", self.model_name_or_path
-            )
-            tokenizer = AutoTokenizer.from_pretrained(
-                **self.tokenizer_kwargs,
-            )
-            self._tokenizer_cache[tokenizer_key] = tokenizer
+        logging.info(f"Loading model from {self.model_name_or_path}")
+        logging.info(f"model_kwargs: {self.model_kwargs}")
+        model = AutoModelCls.from_pretrained(
+            **self.model_kwargs,
+        )
 
-            if tokenizer.pad_token is None:
-                tokenizer.pad_token = tokenizer.eos_token
-        else:
-            logging.info("Reuse tokenizer from cache")
-        return self._model_cache[model_key], self._tokenizer_cache[tokenizer_key]
+        logging.info(f"Loading tokenizer from {self.model_name_or_path}")
+        logging.info(f"tokenizer_kwargs: {self.tokenizer_kwargs}")
+        tokenizer = AutoTokenizer.from_pretrained(
+            **self.tokenizer_kwargs,
+        )
+        if tokenizer.pad_token is None:
+            tokenizer.pad_token = tokenizer.eos_token
 
-    def init(self, db=None):
-        super().init(db)
-        self.prompter = Prompter(self.prompt_template, self.prompt_func)
-        self.model, self.tokenizer = self.init_model_and_tokenizer()
+        return pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+    def init(self):
+        db = self.db
+
+        real_adapter_id = None
         if self.adapter_id is not None:
+            self.handle_chekpoint(db)
             if isinstance(self.adapter_id, Checkpoint):
-                model_id = self.adapter_id.path
-                self.adapter_id = (
-                    self.adapter_id.identifier + "-" + self.adapter_id.path
-                )
+                checkpoint = self.adapter_id
+                self.adapter_id = checkpoint.uri
 
             elif isinstance(self.adapter_id, str):
-                model_id = self.adapter_id
-
+                real_adapter_id = self.adapter_id
+                checkpoint = None
             else:
                 raise ValueError(
-                    "adapter_id must be either a string or Checkpoint object"
+                    "adapter_id must be either a string or Checkpoint object, but got "
+                    f"{type(self.adapter_id)}"
                 )
 
-            self.add_adapter(model_id, self.adapter_id)
+            if checkpoint:
+                db = db or checkpoint.db
+                assert db, "db must be provided when using checkpoint indetiifer"
+                if self.db is None:
+                    self.db = db
+                real_adapter_id = checkpoint.path.unpack(db)
+
+        super().init()
+        self.prompter = Prompter(self.prompt_template, self.prompt_func)
+
+        self.pipeline = self.init_pipeline(real_adapter_id)
+
+    def handle_chekpoint(self, db):
+        if isinstance(self.adapter_id, str):
+            # match checkpoint://<identifier>/<version>
+            if Checkpoint.check_uri(self.adapter_id):
+                assert db, "db must be provided when using checkpoint indetiifer"
+                identifier, version = Checkpoint.parse_uri(self.adapter_id)
+                version = int(version)
+                checkpoint = db.load("checkpoint", identifier, version=version)
+                assert checkpoint, f"Checkpoint {self.adapter_id} not found"
+                self.adapter_id = checkpoint
 
     def _fit(
         self,
@@ -221,79 +246,41 @@ class LLM(_Predictor, _Fittable):
         return compute_metrics
 
     @ensure_initialized
-    def predict_one(self, X):
-        X = self.prompter(X)
-        results = self._generate([X], **self.predict_kwargs)
+    def predict_one(self, X, **kwargs):
+        X = self._process_inputs(X, **kwargs)
+        results = self._generate([X], **kwargs)
         return results[0]
 
     @ensure_initialized
-    def predict(self, dataset: t.Union[t.List, QueryDataset]) -> t.List:
-        X = [self.prompter(dataset[i]) for i in range(len(dataset))]
-        return self._generate(X, **self.predict_kwargs)
+    def predict(self, dataset: t.Union[t.List, QueryDataset], **kwargs) -> t.List:
+        dataset = [
+            self._process_inputs(dataset[i], **kwargs) for i in range(len(dataset))
+        ]
+        return self._generate(dataset, **kwargs)
 
-    def _generate(self, X: t.Any, adapter_name=None, **kwargs):
-        """
-        Private method for `Model.to_call` method.
-        Support inference by multi-lora adapters.
-        """
-        if adapter_name is None and self.adapter_id is not None:
-            adapter_name = self.adapter_id
-        if adapter_name is not None:
-            try:
-                self.model.set_adapter(adapter_name)
-                logging.info(f"Using adapter {adapter_name} for inference")
-            except Exception as e:
-                raise ValueError(
-                    f"Adapter {adapter_name} is not found in the model, "
-                    "please use add_adapter to add it."
-                ) from e
+    def _process_inputs(self, X: t.Any, **kwargs) -> str:
+        if isinstance(X, str):
+            X = self.prompter(X, **kwargs)
+        return X
 
-        elif hasattr(self.model, "disable_adapter"):
-            with self.model.disable_adapter():
-                return self._base_generate(X, **kwargs)
-
-        return self._base_generate(X, **kwargs)
-
-    def _base_generate(self, X: t.Any, **kwargs):
+    def _generate(self, X: list, **kwargs):
         """
         Generate text.
         Can overwrite this method to support more inference methods.
         """
-        model_inputs = self.tokenizer(X, return_tensors="pt", padding=True).to(
-            self.model.device
+        kwargs = kwargs.copy()
+
+        # Set default values, if not will cause bad output
+        kwargs.setdefault("add_special_tokens", True)
+        outputs = self.pipeline(
+            X,
+            return_type=ReturnType.NEW_TEXT,
+            eos_token_id=self.pipeline.tokenizer.eos_token_id,
+            pad_token_id=self.pipeline.tokenizer.eos_token_id,
+            **kwargs,
         )
-        kwargs.setdefault("pad_token_id", self.tokenizer.eos_token_id)
-        outputs = self.model.generate(**model_inputs, **kwargs)
-        texts = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        results = []
-        for text, x in zip(texts, X):
-            text = text[len(x) :]
-            results.append(text)
-        if isinstance(X, str):
-            return results[0]
+        results = [output[0]["generated_text"] for output in outputs]
         return results
-
-    def add_adapter(self, model_id, adapter_name: str):
-        # TODO: Support lora checkpoint from s3
-        try:
-            from peft import PeftModel
-        except Exception as e:
-            raise ImportError("Please install peft to use LoRA training") from e
-
-        logging.info(f"Loading adapter {adapter_name} from {model_id}")
-
-        if not hasattr(self, "model"):
-            self.init()
-
-        if not isinstance(self.model, PeftModel):
-            self.model = PeftModel.from_pretrained(
-                self.model, model_id, adapter_name=adapter_name
-            )
-            # Update cache model
-            self._model_cache[hash_dict(self.model_kwargs)] = self.model
-        else:
-            # TODO where does this come from?
-            self.model.load_adapter(model_id, adapter_name)
 
     def get_datasets(
         self,

Pinnacle-Networks/Pinnacle

examples/llm_finetune.py
@@ -1,50 +1,81 @@
-from datasets import load_dataset
-
 from pinnacledb import pinnacle
-from pinnacledb.backends.mongodb import Collection
-from pinnacledb.base.document import Document
-from pinnacledb.ext.llm import LLM
-from pinnacledb.ext.llm.model import LLMTrainingConfiguration
 
-model = "facebook/opt-125m"
-dataset_name = "imdb"
-train_dataset = load_dataset("imdb", split="train")
+db = pinnacle("mongodb://localhost:30000/llm-finetune")
+# db.drop(force=True)
 
-db = pinnacle("mongomock://test_llm")
+# Training
 
+from datasets import load_dataset
+dataset = load_dataset("philschmid/dolly-15k-oai-style")
+dataset = dataset['train'].train_test_split(test_size=0.05, seed=42)
+train_dataset = dataset['train']
+eval_dataset = dataset['test']
+
+from pinnacledb.ext.llm.model import LLM
 llm = LLM(
     identifier="llm-finetune",
-    model_name_or_path=model,
+    model_name_or_path="mistralai/Mistral-7B-v0.1",
 )
 
 
+
+from pinnacledb.ext.llm.model import LLMTrainingConfiguration
 training_configuration = LLMTrainingConfiguration(
     identifier="llm-finetune-training-config",
-    output_dir="output/llm-finetune",
+    output_dir="output/dolly-chatml",
+    learning_rate=0.0002,
+    lr_scheduler_type='constant',
+    warmup_ratio=0.003,
+    max_grad_norm=0.3,
     overwrite_output_dir=True,
-    num_train_epochs=1,
+    num_train_epochs=3,
     save_total_limit=5,
-    logging_steps=1,
-    # evaluation_strategy="steps",
-    # eval_steps=10,
-    save_steps=100,
+    logging_steps=10,
+    evaluation_strategy="steps",
+    save_steps=200,
+    eval_steps=200,
     per_device_train_batch_size=1,
     per_device_eval_batch_size=1,
-    gradient_accumulation_steps=4,
+    gradient_accumulation_steps=2,
     max_seq_length=512,
     use_lora=True,
     log_to_db=True,
+    fp16=True,
+    # gradient_checkpointing=True,
+    lora_r=8,
+    lora_alpha=16,
+    lora_dropout=0.0,
+    setup_chat_format=True,
+    bits=4,
 )
+
+
 
 llm.fit(
+    X=None,
     train_dataset=train_dataset,
-    X="text",
+    eval_dataset=eval_dataset,
     configuration=training_configuration,
     db=db,
+    packing=True,
 )
 
 
-prompt = "### Human: Who are you? ### Assistant: "
+## Inference
 
-# Automatically load lora model for prediction, default use the latest checkpoint
-print(llm.predict_one(prompt))
+from pinnacledb.ext.llm.model import LLM
+llm = LLM(
+    identifier="llm-finetune",
+    model_name_or_path="mistralai/Mistral-7B-v0.1",
+    adapter_id=db.load('checkpoint', "8ac62d6fca46d145b9cd4039428d7078f1e899e6", version=200),
+    model_kwargs=dict(use_cache=True, device_map="auto", load_in_4bit=True),
+)
+
+
+messages = [
+    {
+        "role": "user",
+        "content": "What is the capital of Germany? Explain why thats the case and if it was different in the past?"
+    }
+]
+print(llm.predict_one(messages, max_new_tokens=200, do_sample=False))

superduperdb/ext/llm/model.py
@@ -7,7 +7,9 @@ import transformers
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
+    pipeline,
 )
+from transformers.pipelines.text_generation import ReturnType
 
 from pinnacledb import logging
 from pinnacledb.backends.query_dataset import QueryDataset, query_dataset_factory
@@ -21,7 +23,6 @@ from pinnacledb.components.model import (
 )
 from pinnacledb.ext.llm import training
 from pinnacledb.ext.llm.utils import Prompter
-from pinnacledb.misc.hash import hash_dict
 
 from .training import Checkpoint
 
@@ -64,8 +65,7 @@ class LLM(_Predictor, _Fittable):
     _encodables: t.ClassVar[t.Sequence[str]] = ("model_kwargs", "tokenizer_kwargs")
 
     identifier: str = ""
-    model_name_or_path: str = "facebook/opt-125m"
-    bits: t.Optional[int] = None
+    model_name_or_path: t.Optional[str] = None
     adapter_id: t.Optional[t.Union[str, Checkpoint]] = None
     object: t.Optional[transformers.Trainer] = None
     model_kwargs: t.Dict = dc.field(default_factory=dict)
@@ -87,73 +87,117 @@ class LLM(_Predictor, _Fittable):
         if not self.identifier:
             self.identifier = self.adapter_id or self.model_name_or_path
 
-        # overwrite model kwargs
-        if self.bits is not None:
-            if (
-                "load_in_4bit" in self.model_kwargs
-                or "load_in_8bit" in self.model_kwargs
-            ):
-                logging.warn(
-                    "The bits is set, will overwrite the load_in_4bit and load_in_8bit"
-                )
-            self.model_kwargs["load_in_4bit"] = self.bits == 4
-            self.model_kwargs["load_in_8bit"] = self.bits == 8
+        #  TODO: Compatible with the bug of artifact sha1 equality and will be deleted
+        self.tokenizer_kwargs.setdefault(
+            "pretrained_model_name_or_path", self.model_name_or_path
+        )
 
         super().__post_init__(artifacts)
 
-    def init_model_and_tokenizer(self):
-        model_key = hash_dict(self.model_kwargs)
-        if model_key not in self._model_cache:
-            logging.info(f"Loading model from {self.model_name_or_path}")
-            logging.info(f"model_kwargs: {self.model_kwargs}")
-            self.model_kwargs.setdefault(
-                "pretrained_model_name_or_path", self.model_name_or_path
-            )
+    def init_pipeline(
+        self, adapter_id: t.Optional[str] = None, load_adapter_directly: bool = False
+    ):
+        # Do not update model state here
+        model_kwargs = self.model_kwargs.copy()
+        tokenizer_kwargs = self.tokenizer_kwargs.copy()
+
+        if self.model_name_or_path and not load_adapter_directly:
+            model_kwargs["pretrained_model_name_or_path"] = self.model_name_or_path
             model = AutoModelForCausalLM.from_pretrained(
-                **self.model_kwargs,
+                **model_kwargs,
             )
-            self._model_cache[model_key] = model
-        else:
-            logging.info("Reuse model from cache")
 
-        tokenizer_key = hash_dict(self.tokenizer_kwargs)
-        if tokenizer_key not in self._tokenizer_cache:
-            logging.info(f"Loading tokenizer from {self.model_name_or_path}")
-            logging.info(f"tokenizer_kwargs: {self.tokenizer_kwargs}")
-            self.tokenizer_kwargs.setdefault(
-                "pretrained_model_name_or_path", self.model_name_or_path
-            )
-            tokenizer = AutoTokenizer.from_pretrained(
-                **self.tokenizer_kwargs,
-            )
-            self._tokenizer_cache[tokenizer_key] = tokenizer
+            if adapter_id is not None:
+                logging.info(f"Loading adapter from {adapter_id}")
 
-            if tokenizer.pad_token is None:
-                tokenizer.pad_token = tokenizer.eos_token
-        else:
-            logging.info("Reuse tokenizer from cache")
-        return self._model_cache[model_key], self._tokenizer_cache[tokenizer_key]
+                from peft import PeftModel
 
-    def init(self, db=None):
-        super().init(db)
-        self.prompter = Prompter(self.prompt_template, self.prompt_func)
-        self.model, self.tokenizer = self.init_model_and_tokenizer()
-        if self.adapter_id is not None:
-            if isinstance(self.adapter_id, Checkpoint):
-                model_id = self.adapter_id.path
-                self.adapter_id = (
-                    self.adapter_id.identifier + "-" + self.adapter_id.path
-                )
+                try:
+                    model = PeftModel.from_pretrained(
+                        model, adapter_id, adapter_name=self.identifier
+                    )
+                except Exception as e:
+                    message = (
+                        f'Failed to add adapter to model, error: {e}\n'
+                        'Try to load adapter directly\n'
+                    )
+                    logging.warn(message)
+                    logging.warn("Try to load adapter directly")
+                    return self.init_pipeline(adapter_id, load_adapter_directly=True)
 
-            elif isinstance(self.adapter_id, str):
-                model_id = self.adapter_id
+                tokenizer_kwargs["pretrained_model_name_or_path"] = adapter_id
 
             else:
+                tokenizer_kwargs[
+                    "pretrained_model_name_or_path"
+                ] = self.model_name_or_path
+
+            tokenizer = AutoTokenizer.from_pretrained(
+                **tokenizer_kwargs,
+            )
+        elif adapter_id is not None:
+            model_kwargs['pretrained_model_name_or_path'] = adapter_id
+            from peft import AutoPeftModelForCausalLM
+
+            model = AutoPeftModelForCausalLM.from_pretrained(
+                **model_kwargs,
+            )
+            tokenizer_kwargs["pretrained_model_name_or_path"] = adapter_id
+            tokenizer = AutoTokenizer.from_pretrained(
+                **tokenizer_kwargs,
+            )
+
+        else:
+            raise ValueError(
+                "model_name_or_path or adapter_id must be provided, "
+                f"got {self.model_name_or_path} and {adapter_id} instead."
+            )
+        if tokenizer.pad_token is None:
+            tokenizer.pad_token = tokenizer.eos_token
+
+        return pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+    def init(self):
+        db = self.db
+
+        real_adapter_id = None
+        if self.adapter_id is not None:
+            self.handle_chekpoint(db)
+            if isinstance(self.adapter_id, Checkpoint):
+                checkpoint = self.adapter_id
+                self.adapter_id = checkpoint.uri
+
+            elif isinstance(self.adapter_id, str):
+                real_adapter_id = self.adapter_id
+                checkpoint = None
+            else:
                 raise ValueError(
-                    "adapter_id must be either a string or Checkpoint object"
+                    "adapter_id must be either a string or Checkpoint object, but got "
+                    f"{type(self.adapter_id)}"
                 )
 
-            self.add_adapter(model_id, self.adapter_id)
+            if checkpoint:
+                db = db or checkpoint.db
+                assert db, "db must be provided when using checkpoint indetiifer"
+                if self.db is None:
+                    self.db = db
+                real_adapter_id = checkpoint.path.unpack(db)
+
+        super().init()
+        self.prompter = Prompter(self.prompt_template, self.prompt_func)
+
+        self.pipeline = self.init_pipeline(real_adapter_id)
+
+    def handle_chekpoint(self, db):
+        if isinstance(self.adapter_id, str):
+            # match checkpoint://<identifier>/<version>
+            if Checkpoint.check_uri(self.adapter_id):
+                assert db, "db must be provided when using checkpoint indetiifer"
+                identifier, version = Checkpoint.parse_uri(self.adapter_id)
+                version = int(version)
+                checkpoint = db.load("checkpoint", identifier, version=version)
+                assert checkpoint, f"Checkpoint {self.adapter_id} not found"
+                self.adapter_id = checkpoint
 
     def _fit(
         self,
@@ -172,7 +216,6 @@ class LLM(_Predictor, _Fittable):
         assert configuration is not None, "configuration must be provided"
 
         training_config = configuration.kwargs or {}
-        training_config["bits"] = training_config.get("bits", self.bits)
 
         if not train_dataset:
             train_dataset, eval_dataset = self.get_datasets(
@@ -187,8 +230,11 @@ class LLM(_Predictor, _Fittable):
 
         assert train_dataset is not None, "train_dataset must be provided"
 
-        model_kwargs = self.model_kwargs
-        tokenizer_kwargs = self.tokenizer_kwargs
+        model_kwargs = self.model_kwargs.copy()
+        tokenizer_kwargs = self.tokenizer_kwargs.copy()
+        assert (
+            self.model_name_or_path
+        ), "model_name_or_path must be provided for training"
         model_kwargs["pretrained_model_name_or_path"] = self.model_name_or_path
         tokenizer_kwargs["pretrained_model_name_or_path"] = self.model_name_or_path
 
@@ -221,79 +267,41 @@ class LLM(_Predictor, _Fittable):
         return compute_metrics
 
     @ensure_initialized
-    def predict_one(self, X):
-        X = self.prompter(X)
-        results = self._generate([X], **self.predict_kwargs)
+    def predict_one(self, X, **kwargs):
+        X = self._process_inputs(X, **kwargs)
+        results = self._generate([X], **kwargs)
         return results[0]
 
     @ensure_initialized
-    def predict(self, dataset: t.Union[t.List, QueryDataset]) -> t.List:
-        X = [self.prompter(dataset[i]) for i in range(len(dataset))]
-        return self._generate(X, **self.predict_kwargs)
+    def predict(self, dataset: t.Union[t.List, QueryDataset], **kwargs) -> t.List:
+        dataset = [
+            self._process_inputs(dataset[i], **kwargs) for i in range(len(dataset))
+        ]
+        return self._generate(dataset, **kwargs)
 
-    def _generate(self, X: t.Any, adapter_name=None, **kwargs):
-        """
-        Private method for `Model.to_call` method.
-        Support inference by multi-lora adapters.
-        """
-        if adapter_name is None and self.adapter_id is not None:
-            adapter_name = self.adapter_id
-        if adapter_name is not None:
-            try:
-                self.model.set_adapter(adapter_name)
-                logging.info(f"Using adapter {adapter_name} for inference")
-            except Exception as e:
-                raise ValueError(
-                    f"Adapter {adapter_name} is not found in the model, "
-                    "please use add_adapter to add it."
-                ) from e
+    def _process_inputs(self, X: t.Any, **kwargs) -> str:
+        if isinstance(X, str):
+            X = self.prompter(X, **kwargs)
+        return X
 
-        elif hasattr(self.model, "disable_adapter"):
-            with self.model.disable_adapter():
-                return self._base_generate(X, **kwargs)
-
-        return self._base_generate(X, **kwargs)
-
-    def _base_generate(self, X: t.Any, **kwargs):
+    def _generate(self, X: list, **kwargs):
         """
         Generate text.
         Can overwrite this method to support more inference methods.
         """
-        model_inputs = self.tokenizer(X, return_tensors="pt", padding=True).to(
-            self.model.device
+        kwargs = kwargs.copy()
+
+        # Set default values, if not will cause bad output
+        kwargs.setdefault("add_special_tokens", True)
+        outputs = self.pipeline(
+            X,
+            return_type=ReturnType.NEW_TEXT,
+            eos_token_id=self.pipeline.tokenizer.eos_token_id,
+            pad_token_id=self.pipeline.tokenizer.eos_token_id,
+            **kwargs,
         )
-        kwargs.setdefault("pad_token_id", self.tokenizer.eos_token_id)
-        outputs = self.model.generate(**model_inputs, **kwargs)
-        texts = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
-        results = []
-        for text, x in zip(texts, X):
-            text = text[len(x) :]
-            results.append(text)
-        if isinstance(X, str):
-            return results[0]
+        results = [output[0]["generated_text"] for output in outputs]
         return results
-
-    def add_adapter(self, model_id, adapter_name: str):
-        # TODO: Support lora checkpoint from s3
-        try:
-            from peft import PeftModel
-        except Exception as e:
-            raise ImportError("Please install peft to use LoRA training") from e
-
-        logging.info(f"Loading adapter {adapter_name} from {model_id}")
-
-        if not hasattr(self, "model"):
-            self.init()
-
-        if not isinstance(self.model, PeftModel):
-            self.model = PeftModel.from_pretrained(
-                self.model, model_id, adapter_name=adapter_name
-            )
-            # Update cache model
-            self._model_cache[hash_dict(self.model_kwargs)] = self.model
-        else:
-            # TODO where does this come from?
-            self.model.load_adapter(model_id, adapter_name)
 
     def get_datasets(
         self,