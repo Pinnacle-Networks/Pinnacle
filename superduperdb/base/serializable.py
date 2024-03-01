superduper-io/superduper

Lazy model initialization project wide

superduperdb/base/serializable.py
@@ -3,16 +3,16 @@ import importlib
 import typing as t
 from copy import deepcopy
 
-from superduperdb.base.config import BytesEncoding
 from superduperdb.base.leaf import Leaf
 from superduperdb.misc.serialization import asdict
 
 
 def _from_dict(r: t.Any, db: None = None) -> t.Any:
     from superduperdb.base.document import Document
+    from superduperdb.components.datatype import File, LazyArtifact
 
     if isinstance(r, Document):
-        r = r.unpack(db)
+        r = r.unpack(db, leaves_to_keep=(LazyArtifact, File))
     if isinstance(r, (list, tuple)):
         return [_from_dict(i, db=db) for i in r]
     if not isinstance(r, dict):
@@ -98,7 +98,6 @@ class Serializable(Leaf):
 
     def encode(
         self,
-        bytes_encoding: t.Optional[BytesEncoding] = None,
         leaf_types_to_keep: t.Sequence = (),
     ):
         r = dict(self.dict().encode(leaf_types_to_keep=leaf_types_to_keep))
@@ -106,7 +105,7 @@ class Serializable(Leaf):
         return {'_content': r}
 
     @classmethod
-    def decode(cls, r, db: t.Optional[t.Any] = None, reference: bool = False):
+    def decode(cls, r, db: t.Optional[t.Any] = None):
         return _from_dict(r, db=db)
 
     def dict(self):

superduperdb/components/component.py
@@ -10,9 +10,10 @@ import os
 import re
 import tempfile
 import typing as t
-from functools import cached_property
+from collections import defaultdict
+from functools import cached_property, wraps
 
-from superduperdb.base.config import BytesEncoding
+from superduperdb import logging
 from superduperdb.base.leaf import Leaf
 from superduperdb.base.serializable import Serializable
 from superduperdb.jobs.job import ComponentJob, Job
@@ -44,22 +45,22 @@ class Component(Serializable, Leaf):
         if not self.identifier:
             raise ValueError('identifier cannot be empty or None')
 
-    def init(self, db=None):
+    def init(self):
         from superduperdb.base.document import Document
         from superduperdb.components.datatype import _BaseEncodable
 
         for f in dc.fields(self):
             item = getattr(self, f.name)
             if isinstance(item, Component):
-                item.init(db=db)
+                item.init()
             if isinstance(item, dict):
-                setattr(self, f.name, Document(item).unpack(db=self.db or db))
+                setattr(self, f.name, Document(item).unpack(db=self.db))
             if isinstance(item, list):
-                unpacked = Document({'_base': item}).unpack(db=self.db or db)
+                unpacked = Document({'_base': item}).unpack(db=self.db)
                 setattr(self, f.name, unpacked)
             if isinstance(item, _BaseEncodable):
-                item.init(db=db)
-                setattr(self, f.name, item.x)
+                item = item.unpack(db=self.db)
+                setattr(self, f.name, item)
 
     @cached_property
     def artifact_schema(self):
@@ -114,11 +115,19 @@ class Component(Serializable, Leaf):
 
     def dict(self) -> 'Document':
         from superduperdb import Document
+        from superduperdb.components.datatype import Artifact, File
 
         r = Document(super().dict())
         s = self.artifact_schema
         for k in s.fields:
-            r[f'dict.{k}'] = s.fields[k](getattr(self, k))  # artifact
+            attr = getattr(self, k)
+            if isinstance(attr, (Artifact, File)):
+                r[f'dict.{k}'] = attr
+            else:
+                if s.fields[k].encodable == 'file':
+                    r[f'dict.{k}'] = s.fields[k](uri=attr)  # artifact or file
+                else:
+                    r[f'dict.{k}'] = s.fields[k](x=attr)  # artifact or file
         r['type_id'] = self.type_id
         r['identifier'] = self.identifier
         r['version'] = self.version
@@ -128,10 +137,9 @@ class Component(Serializable, Leaf):
 
     def encode(
         self,
-        bytes_encoding: BytesEncoding | None = None,
         leaf_types_to_keep: t.Sequence = (),
     ):
-        r = super().encode(bytes_encoding, leaf_types_to_keep=leaf_types_to_keep)
+        r = super().encode(leaf_types_to_keep=leaf_types_to_keep)
         del r['_content']['dict']
         r['_content']['leaf_type'] = 'component'
         return r
@@ -196,14 +204,14 @@ class Component(Serializable, Leaf):
         extracted_path = from_tarball(path)
         with open(os.path.join(extracted_path, 'compiled.json')) as f:
             references = json.load(f)
-        encodables = list(set(os.listdir(extracted_path)) - {'compiled.json'})
 
-        for e in encodables:
-            with open(os.path.join(extracted_path, e), 'rb') as f:
-                r = references['encodable'][e]
-                references['encodable'][e] = serializers[
-                    r['_content']['datatype']
-                ].decoder(f.read())
+        for at in ['artifact', 'lazy_artifact']:
+            for e in references[at]:
+                with open(os.path.join(extracted_path, e), 'rb') as f:
+                    r = references[at][e]
+                    references[at][e] = serializers[
+                        r['_content']['datatype']
+                    ].decode_data(f.read())
 
         # This may get deprecated in favour of inline definitions (inside components)
         for s in references.get('serializable', {}):
@@ -254,9 +262,8 @@ class Component(Serializable, Leaf):
     def export(self):
         from superduperdb.base.document import _encode_with_references
 
-        references = {}
+        references = defaultdict(dict)
         references['component'] = {self.unique_id: self}
-        references['encodable'] = {}
         while True:
             keys = list(references['component'].keys())
             for k in keys[:]:
@@ -268,12 +275,13 @@ class Component(Serializable, Leaf):
             if all(isinstance(v, dict) for v in references['component'].values()):
                 break
         with tempfile.TemporaryDirectory() as td:
-            for k, v in references['encodable'].items():
-                r = v.encode()
-                with open(f'{td}/{k}', 'wb') as f:
-                    f.write(r['_content']['bytes'])
-                del r['_content']['bytes']
-                references['encodable'][k] = r
+            for at in ['artifact', 'lazy_artifact']:
+                for k, v in references[at].items():
+                    r = v.encode()
+                    with open(f'{td}/{k}', 'wb') as f:
+                        f.write(r['_content']['bytes'])
+                    del r['_content']['bytes']
+                    references[at][k] = r
             with open(f'{td}/compiled.json', 'w') as f:
                 json.dump(references, f)
             to_tarball(td, self.identifier)
@@ -313,3 +321,19 @@ def _replace_references(r, lookup, raises=False):
         for i, x in enumerate(r):
             r[i] = _replace_references(x, lookup)
     return r
+
+
+def ensure_initialized(func):
+    """Decorator to ensure that the model is initialized before calling the function"""
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if not hasattr(self, "_is_initialized") or not self._is_initialized:
+            model_message = f"{self.__class__.__name__} : {self.identifier}"
+            logging.info(f"Initializing {model_message}")
+            self.init()
+            self._is_initialized = True
+            logging.info(f"Initialized  {model_message} successfully")
+        return func(self, *args, **kwargs)
+
+    return wrapper

superduperdb/components/dataset.py
@@ -10,7 +10,7 @@ from overrides import override
 from superduperdb.backends.mongodb.query import Select
 from superduperdb.base.datalayer import Datalayer
 from superduperdb.base.document import Document
-from superduperdb.components.component import Component
+from superduperdb.components.component import Component, ensure_initialized
 from superduperdb.components.datatype import (
     DataType,
     dill_serializer,
@@ -45,6 +45,19 @@ class Dataset(Component):
     creation_date: t.Optional[str] = None
     raw_data: t.Optional[t.Sequence[t.Any]] = None
 
+    def __post_init__(self, artifacts):
+        self._data = None
+        return super().__post_init__(artifacts)
+
+    @property
+    @ensure_initialized
+    def data(self):
+        return self._data
+
+    def init(self):
+        super().init()
+        self._data = [Document.decode(r, self.db) for r in pickle_decode(self.raw_data)]
+
     @override
     def pre_create(self, db: 'Datalayer') -> None:
         if self.raw_data is None:
@@ -55,15 +68,6 @@ class Dataset(Component):
                 perm = self.random.permutation(len(data)).tolist()
                 data = [data[perm[i]] for i in range(self.sample_size)]
             self.raw_data = pickle_encode([r.encode() for r in data])
-
-    @override
-    def post_create(self, db: 'Datalayer') -> None:
-        return self.on_load(db)
-
-    @override
-    def on_load(self, db: 'Datalayer') -> None:
-        # ruff: noqa: E501
-        self.data = [Document.decode(r, db) for r in pickle_decode(self.raw_data)]  # type: ignore[arg-type]
 
     @cached_property
     def random(self):

superduperdb/ext/llm/base.py
@@ -9,9 +9,9 @@ from typing import Any, Callable, List, Optional, Sequence, Union
 
 from superduperdb import logging
 from superduperdb.backends.query_dataset import QueryDataset
+from superduperdb.components.component import ensure_initialized
 from superduperdb.components.model import _Predictor
 from superduperdb.ext.llm.utils import Prompter
-from superduperdb.ext.utils import ensure_initialized
 
 if typing.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer

superduperdb/ext/torch/model.py
@@ -16,6 +16,7 @@ from superduperdb.backends.base.query import Select
 from superduperdb.backends.query_dataset import QueryDataset
 from superduperdb.base.datalayer import Datalayer
 from superduperdb.base.serializable import Serializable
+from superduperdb.components.component import ensure_initialized
 from superduperdb.components.dataset import Dataset
 from superduperdb.components.datatype import (
     DataType,
@@ -260,6 +261,7 @@ class TorchModel(_Predictor, _Fittable, _DeviceManaged):
                     io.BytesIO(state.pop('object_bytes'))
                 )
 
+    @ensure_initialized
     def predict_one(self, *args, **kwargs):
         with torch.no_grad(), eval(self.object):
             if self.preprocess is not None:
@@ -277,6 +279,7 @@ class TorchModel(_Predictor, _Fittable, _DeviceManaged):
                 args = self.postprocess(args)
             return args
 
+    @ensure_initialized
     def predict(self, dataset: t.Union[t.List, QueryDataset]) -> t.List:
         with torch.no_grad(), eval(self.object):
             inputs = BasicDataset(
@@ -346,12 +349,13 @@ class TorchModel(_Predictor, _Fittable, _DeviceManaged):
             return True
         return False
 
+    @ensure_initialized
     def _fit(
         self,
         X: t.Any,
         *,
         y: t.Optional[t.Any] = None,
-        configuration: TorchTrainerConfiguration,  # type: ignore[override]
+        configuration: TorchTrainerConfiguration,
         data_prefetch: bool = False,
         db: t.Optional[Datalayer] = None,
         metrics: t.Optional[t.Sequence[Metric]] = None,

test/unittest/component/test_component.py
@@ -2,20 +2,29 @@ import dataclasses as dc
 import os
 import shutil
 import typing as t
+from test.db_config import DBConfig
 
 import pytest
 
 from superduperdb import ObjectModel
 from superduperdb.base.document import Document
 from superduperdb.components.component import Component
-from superduperdb.components.datatype import Encodable, dill_serializer
+from superduperdb.components.datatype import (
+    Artifact,
+    Empty,
+    LazyArtifact,
+    dill_serializer,
+)
 
 
 @pytest.fixture
 def cleanup():
     yield
-    os.remove('test_export.tar.gz')
-    shutil.rmtree('test_export')
+    try:
+        os.remove('test_export.tar.gz')
+        shutil.rmtree('test_export')
+    except FileNotFoundError:
+        pass
 
 
 def test_compile_decompile(cleanup):
@@ -30,6 +39,7 @@ def test_compile_decompile(cleanup):
 
 @dc.dataclass(kw_only=True)
 class MyComponent(Component):
+    type_id: t.ClassVar[str] = 'my_type'
     _lazy_fields: t.ClassVar[t.Sequence[str]] = ('my_dict',)
     my_dict: t.Dict
     nested_list: t.List
@@ -46,8 +56,8 @@ def test_init(monkeypatch):
 
     monkeypatch.setattr(Document, 'unpack', unpack)
 
-    e = Encodable(x=None, file_id='123', datatype=None)
-    a = Encodable(x=None, file_id='456', datatype=None)
+    e = Artifact(x=None, file_id='123', datatype=dill_serializer)
+    a = Artifact(x=None, file_id='456', datatype=dill_serializer)
 
     def side_effect(*args, **kwargs):
         a.x = lambda x: x + 1
@@ -69,3 +79,19 @@ def test_init(monkeypatch):
 
     assert callable(c.nested_list[1])
     assert c.nested_list[1](1) == 3
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb], indirect=True)
+def test_load_lazily(db):
+    m = ObjectModel('lazy_model', object=lambda x: x + 2)
+
+    db.add(m)
+
+    reloaded = db.load('model', m.identifier)
+
+    assert isinstance(reloaded.object, LazyArtifact)
+    assert isinstance(reloaded.object.x, Empty)
+
+    reloaded.init()
+
+    assert callable(reloaded.object)

Pinnacle-Networks/Pinnacle

superduperdb/base/serializable.py
@@ -3,16 +3,16 @@ import importlib
 import typing as t
 from copy import deepcopy
 
-from pinnacledb.base.config import BytesEncoding
 from pinnacledb.base.leaf import Leaf
 from pinnacledb.misc.serialization import asdict
 
 
 def _from_dict(r: t.Any, db: None = None) -> t.Any:
     from pinnacledb.base.document import Document
+    from pinnacledb.components.datatype import File, LazyArtifact
 
     if isinstance(r, Document):
-        r = r.unpack(db)
+        r = r.unpack(db, leaves_to_keep=(LazyArtifact, File))
     if isinstance(r, (list, tuple)):
         return [_from_dict(i, db=db) for i in r]
     if not isinstance(r, dict):
@@ -98,7 +98,6 @@ class Serializable(Leaf):
 
     def encode(
         self,
-        bytes_encoding: t.Optional[BytesEncoding] = None,
         leaf_types_to_keep: t.Sequence = (),
     ):
         r = dict(self.dict().encode(leaf_types_to_keep=leaf_types_to_keep))
@@ -106,7 +105,7 @@ class Serializable(Leaf):
         return {'_content': r}
 
     @classmethod
-    def decode(cls, r, db: t.Optional[t.Any] = None, reference: bool = False):
+    def decode(cls, r, db: t.Optional[t.Any] = None):
         return _from_dict(r, db=db)
 
     def dict(self):

superduperdb/components/component.py
@@ -10,9 +10,10 @@ import os
 import re
 import tempfile
 import typing as t
-from functools import cached_property
+from collections import defaultdict
+from functools import cached_property, wraps
 
-from pinnacledb.base.config import BytesEncoding
+from pinnacledb import logging
 from pinnacledb.base.leaf import Leaf
 from pinnacledb.base.serializable import Serializable
 from pinnacledb.jobs.job import ComponentJob, Job
@@ -44,22 +45,22 @@ class Component(Serializable, Leaf):
         if not self.identifier:
             raise ValueError('identifier cannot be empty or None')
 
-    def init(self, db=None):
+    def init(self):
         from pinnacledb.base.document import Document
         from pinnacledb.components.datatype import _BaseEncodable
 
         for f in dc.fields(self):
             item = getattr(self, f.name)
             if isinstance(item, Component):
-                item.init(db=db)
+                item.init()
             if isinstance(item, dict):
-                setattr(self, f.name, Document(item).unpack(db=self.db or db))
+                setattr(self, f.name, Document(item).unpack(db=self.db))
             if isinstance(item, list):
-                unpacked = Document({'_base': item}).unpack(db=self.db or db)
+                unpacked = Document({'_base': item}).unpack(db=self.db)
                 setattr(self, f.name, unpacked)
             if isinstance(item, _BaseEncodable):
-                item.init(db=db)
-                setattr(self, f.name, item.x)
+                item = item.unpack(db=self.db)
+                setattr(self, f.name, item)
 
     @cached_property
     def artifact_schema(self):
@@ -114,11 +115,19 @@ class Component(Serializable, Leaf):
 
     def dict(self) -> 'Document':
         from pinnacledb import Document
+        from pinnacledb.components.datatype import Artifact, File
 
         r = Document(super().dict())
         s = self.artifact_schema
         for k in s.fields:
-            r[f'dict.{k}'] = s.fields[k](getattr(self, k))  # artifact
+            attr = getattr(self, k)
+            if isinstance(attr, (Artifact, File)):
+                r[f'dict.{k}'] = attr
+            else:
+                if s.fields[k].encodable == 'file':
+                    r[f'dict.{k}'] = s.fields[k](uri=attr)  # artifact or file
+                else:
+                    r[f'dict.{k}'] = s.fields[k](x=attr)  # artifact or file
         r['type_id'] = self.type_id
         r['identifier'] = self.identifier
         r['version'] = self.version
@@ -128,10 +137,9 @@ class Component(Serializable, Leaf):
 
     def encode(
         self,
-        bytes_encoding: BytesEncoding | None = None,
         leaf_types_to_keep: t.Sequence = (),
     ):
-        r = super().encode(bytes_encoding, leaf_types_to_keep=leaf_types_to_keep)
+        r = super().encode(leaf_types_to_keep=leaf_types_to_keep)
         del r['_content']['dict']
         r['_content']['leaf_type'] = 'component'
         return r
@@ -196,14 +204,14 @@ class Component(Serializable, Leaf):
         extracted_path = from_tarball(path)
         with open(os.path.join(extracted_path, 'compiled.json')) as f:
             references = json.load(f)
-        encodables = list(set(os.listdir(extracted_path)) - {'compiled.json'})
 
-        for e in encodables:
-            with open(os.path.join(extracted_path, e), 'rb') as f:
-                r = references['encodable'][e]
-                references['encodable'][e] = serializers[
-                    r['_content']['datatype']
-                ].decoder(f.read())
+        for at in ['artifact', 'lazy_artifact']:
+            for e in references[at]:
+                with open(os.path.join(extracted_path, e), 'rb') as f:
+                    r = references[at][e]
+                    references[at][e] = serializers[
+                        r['_content']['datatype']
+                    ].decode_data(f.read())
 
         # This may get deprecated in favour of inline definitions (inside components)
         for s in references.get('serializable', {}):
@@ -254,9 +262,8 @@ class Component(Serializable, Leaf):
     def export(self):
         from pinnacledb.base.document import _encode_with_references
 
-        references = {}
+        references = defaultdict(dict)
         references['component'] = {self.unique_id: self}
-        references['encodable'] = {}
         while True:
             keys = list(references['component'].keys())
             for k in keys[:]:
@@ -268,12 +275,13 @@ class Component(Serializable, Leaf):
             if all(isinstance(v, dict) for v in references['component'].values()):
                 break
         with tempfile.TemporaryDirectory() as td:
-            for k, v in references['encodable'].items():
-                r = v.encode()
-                with open(f'{td}/{k}', 'wb') as f:
-                    f.write(r['_content']['bytes'])
-                del r['_content']['bytes']
-                references['encodable'][k] = r
+            for at in ['artifact', 'lazy_artifact']:
+                for k, v in references[at].items():
+                    r = v.encode()
+                    with open(f'{td}/{k}', 'wb') as f:
+                        f.write(r['_content']['bytes'])
+                    del r['_content']['bytes']
+                    references[at][k] = r
             with open(f'{td}/compiled.json', 'w') as f:
                 json.dump(references, f)
             to_tarball(td, self.identifier)
@@ -313,3 +321,19 @@ def _replace_references(r, lookup, raises=False):
         for i, x in enumerate(r):
             r[i] = _replace_references(x, lookup)
     return r
+
+
+def ensure_initialized(func):
+    """Decorator to ensure that the model is initialized before calling the function"""
+
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if not hasattr(self, "_is_initialized") or not self._is_initialized:
+            model_message = f"{self.__class__.__name__} : {self.identifier}"
+            logging.info(f"Initializing {model_message}")
+            self.init()
+            self._is_initialized = True
+            logging.info(f"Initialized  {model_message} successfully")
+        return func(self, *args, **kwargs)
+
+    return wrapper

superduperdb/components/dataset.py
@@ -10,7 +10,7 @@ from overrides import override
 from pinnacledb.backends.mongodb.query import Select
 from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.base.document import Document
-from pinnacledb.components.component import Component
+from pinnacledb.components.component import Component, ensure_initialized
 from pinnacledb.components.datatype import (
     DataType,
     dill_serializer,
@@ -45,6 +45,19 @@ class Dataset(Component):
     creation_date: t.Optional[str] = None
     raw_data: t.Optional[t.Sequence[t.Any]] = None
 
+    def __post_init__(self, artifacts):
+        self._data = None
+        return super().__post_init__(artifacts)
+
+    @property
+    @ensure_initialized
+    def data(self):
+        return self._data
+
+    def init(self):
+        super().init()
+        self._data = [Document.decode(r, self.db) for r in pickle_decode(self.raw_data)]
+
     @override
     def pre_create(self, db: 'Datalayer') -> None:
         if self.raw_data is None:
@@ -55,15 +68,6 @@ class Dataset(Component):
                 perm = self.random.permutation(len(data)).tolist()
                 data = [data[perm[i]] for i in range(self.sample_size)]
             self.raw_data = pickle_encode([r.encode() for r in data])
-
-    @override
-    def post_create(self, db: 'Datalayer') -> None:
-        return self.on_load(db)
-
-    @override
-    def on_load(self, db: 'Datalayer') -> None:
-        # ruff: noqa: E501
-        self.data = [Document.decode(r, db) for r in pickle_decode(self.raw_data)]  # type: ignore[arg-type]
 
     @cached_property
     def random(self):

superduperdb/ext/llm/base.py
@@ -9,9 +9,9 @@ from typing import Any, Callable, List, Optional, Sequence, Union
 
 from pinnacledb import logging
 from pinnacledb.backends.query_dataset import QueryDataset
+from pinnacledb.components.component import ensure_initialized
 from pinnacledb.components.model import _Predictor
 from pinnacledb.ext.llm.utils import Prompter
-from pinnacledb.ext.utils import ensure_initialized
 
 if typing.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer

superduperdb/ext/torch/model.py
@@ -16,6 +16,7 @@ from pinnacledb.backends.base.query import Select
 from pinnacledb.backends.query_dataset import QueryDataset
 from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.base.serializable import Serializable
+from pinnacledb.components.component import ensure_initialized
 from pinnacledb.components.dataset import Dataset
 from pinnacledb.components.datatype import (
     DataType,
@@ -260,6 +261,7 @@ class TorchModel(_Predictor, _Fittable, _DeviceManaged):
                     io.BytesIO(state.pop('object_bytes'))
                 )
 
+    @ensure_initialized
     def predict_one(self, *args, **kwargs):
         with torch.no_grad(), eval(self.object):
             if self.preprocess is not None:
@@ -277,6 +279,7 @@ class TorchModel(_Predictor, _Fittable, _DeviceManaged):
                 args = self.postprocess(args)
             return args
 
+    @ensure_initialized
     def predict(self, dataset: t.Union[t.List, QueryDataset]) -> t.List:
         with torch.no_grad(), eval(self.object):
             inputs = BasicDataset(
@@ -346,12 +349,13 @@ class TorchModel(_Predictor, _Fittable, _DeviceManaged):
             return True
         return False
 
+    @ensure_initialized
     def _fit(
         self,
         X: t.Any,
         *,
         y: t.Optional[t.Any] = None,
-        configuration: TorchTrainerConfiguration,  # type: ignore[override]
+        configuration: TorchTrainerConfiguration,
         data_prefetch: bool = False,
         db: t.Optional[Datalayer] = None,
         metrics: t.Optional[t.Sequence[Metric]] = None,

test/unittest/component/test_component.py
@@ -2,20 +2,29 @@ import dataclasses as dc
 import os
 import shutil
 import typing as t
+from test.db_config import DBConfig
 
 import pytest
 
 from pinnacledb import ObjectModel
 from pinnacledb.base.document import Document
 from pinnacledb.components.component import Component
-from pinnacledb.components.datatype import Encodable, dill_serializer
+from pinnacledb.components.datatype import (
+    Artifact,
+    Empty,
+    LazyArtifact,
+    dill_serializer,
+)
 
 
 @pytest.fixture
 def cleanup():
     yield
-    os.remove('test_export.tar.gz')
-    shutil.rmtree('test_export')
+    try:
+        os.remove('test_export.tar.gz')
+        shutil.rmtree('test_export')
+    except FileNotFoundError:
+        pass
 
 
 def test_compile_decompile(cleanup):
@@ -30,6 +39,7 @@ def test_compile_decompile(cleanup):
 
 @dc.dataclass(kw_only=True)
 class MyComponent(Component):
+    type_id: t.ClassVar[str] = 'my_type'
     _lazy_fields: t.ClassVar[t.Sequence[str]] = ('my_dict',)
     my_dict: t.Dict
     nested_list: t.List
@@ -46,8 +56,8 @@ def test_init(monkeypatch):
 
     monkeypatch.setattr(Document, 'unpack', unpack)
 
-    e = Encodable(x=None, file_id='123', datatype=None)
-    a = Encodable(x=None, file_id='456', datatype=None)
+    e = Artifact(x=None, file_id='123', datatype=dill_serializer)
+    a = Artifact(x=None, file_id='456', datatype=dill_serializer)
 
     def side_effect(*args, **kwargs):
         a.x = lambda x: x + 1
@@ -69,3 +79,19 @@ def test_init(monkeypatch):
 
     assert callable(c.nested_list[1])
     assert c.nested_list[1](1) == 3
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb], indirect=True)
+def test_load_lazily(db):
+    m = ObjectModel('lazy_model', object=lambda x: x + 2)
+
+    db.add(m)
+
+    reloaded = db.load('model', m.identifier)
+
+    assert isinstance(reloaded.object, LazyArtifact)
+    assert isinstance(reloaded.object.x, Empty)
+
+    reloaded.init()
+
+    assert callable(reloaded.object)