superduper-io/superduper

Add Checkpoint component to save checkpoint

superduperdb/components/datatype.py
@@ -21,20 +21,10 @@ from superduperdb.base.config import BytesEncoding
 from superduperdb.base.leaf import Leaf
 from superduperdb.components.component import Component, ensure_initialized
 from superduperdb.misc.annotations import public_api
+from superduperdb.misc.hash import random_sha1
 
 Decode = t.Callable[[bytes], t.Any]
 Encode = t.Callable[[t.Any], bytes]
-
-
-def random_sha1():
-    """
-    Generate random sha1 values
-    Can be used to generate file_id and other values
-    """
-    random_data = os.urandom(256)
-    sha1 = hashlib.sha1()
-    sha1.update(random_data)
-    return sha1.hexdigest()
 
 
 def pickle_encode(object: t.Any, info: t.Optional[t.Dict] = None) -> bytes:

superduperdb/ext/llm/model.py
@@ -13,7 +13,7 @@ from superduperdb import logging
 from superduperdb.backends.query_dataset import QueryDataset, query_dataset_factory
 from superduperdb.components.component import ensure_initialized
 from superduperdb.components.dataset import Dataset as _Dataset
-from superduperdb.components.datatype import DataType, dill_serializer, file_serializer
+from superduperdb.components.datatype import DataType, dill_serializer
 from superduperdb.components.model import (
     _Fittable,
     _Predictor,
@@ -22,6 +22,8 @@ from superduperdb.components.model import (
 from superduperdb.ext.llm import training
 from superduperdb.ext.llm.utils import Prompter
 from superduperdb.misc.hash import hash_dict
+
+from .training import Checkpoint
 
 if typing.TYPE_CHECKING:
     from datasets import Dataset
@@ -59,18 +61,18 @@ class LLM(_Predictor, _Fittable):
     : param prompt_func: prompt function, default is None
     """
 
-    _encodables: t.ClassVar[t.Sequence[str]] = ('model_kwargs', 'tokenizer_kwargs')
+    _encodables: t.ClassVar[t.Sequence[str]] = ("model_kwargs", "tokenizer_kwargs")
 
     identifier: str = ""
     model_name_or_path: str = "facebook/opt-125m"
     bits: t.Optional[int] = None
-    adapter_id: t.Optional[str] = None
+    adapter_id: t.Optional[t.Union[str, Checkpoint]] = None
     object: t.Optional[transformers.Trainer] = None
     model_kwargs: t.Dict = dc.field(default_factory=dict)
     tokenizer_kwargs: t.Dict = dc.field(default_factory=dict)
     prompt_template: str = "{input}"
     prompt_func: t.Optional[t.Callable] = None
-    signature: str = 'singleton'  # type: ignore[misc]
+    signature: str = "singleton"  # type: ignore[misc]
 
     # Save models and tokenizers cache for sharing when using multiple models
     _model_cache: t.ClassVar[dict] = {}
@@ -79,7 +81,6 @@ class LLM(_Predictor, _Fittable):
     _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, DataType]]] = (
         ("model_kwargs", dill_serializer),
         ("tokenizer_kwargs", dill_serializer),
-        ('adapter_id', file_serializer),
     )
 
     def __post_init__(self, artifacts):
@@ -97,6 +98,7 @@ class LLM(_Predictor, _Fittable):
                 )
             self.model_kwargs["load_in_4bit"] = self.bits == 4
             self.model_kwargs["load_in_8bit"] = self.bits == 8
+
         super().__post_init__(artifacts)
 
     def init_model_and_tokenizer(self):
@@ -137,7 +139,21 @@ class LLM(_Predictor, _Fittable):
         self.prompter = Prompter(self.prompt_template, self.prompt_func)
         self.model, self.tokenizer = self.init_model_and_tokenizer()
         if self.adapter_id is not None:
-            self.add_adapter(self.adapter_id, self.adapter_id)
+            if isinstance(self.adapter_id, Checkpoint):
+                model_id = self.adapter_id.path
+                self.adapter_id = (
+                    self.adapter_id.identifier + "-" + self.adapter_id.path
+                )
+
+            elif isinstance(self.adapter_id, str):
+                model_id = self.adapter_id
+
+            else:
+                raise ValueError(
+                    "adapter_id must be either a string or Checkpoint object"
+                )
+
+            self.add_adapter(model_id, self.adapter_id)
 
     def _fit(
         self,
@@ -149,7 +165,7 @@ class LLM(_Predictor, _Fittable):
         metrics: t.Optional[t.Sequence["Metric"]] = None,
         select: t.Optional["Select"] = None,
         validation_sets: t.Optional[t.Sequence[t.Union[str, "_Dataset"]]] = None,
-        train_dataset: t.Optional['Dataset'] = None,
+        train_dataset: t.Optional["Dataset"] = None,
         eval_dataset: t.Optional[t.Union["Dataset", t.Dict[str, "Dataset"]]] = None,
         **kwargs,
     ):
@@ -294,8 +310,8 @@ class LLM(_Predictor, _Fittable):
             keys.append(y)
 
         def transform(x):
-            if '_id' in x:
-                x['_id'] = str(x['_id'])
+            if "_id" in x:
+                x["_id"] = str(x["_id"])
             return x
 
         train_dataset = query_dataset_factory(

superduperdb/ext/llm/training.py
@@ -10,7 +10,6 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     BitsAndBytesConfig,
-    Trainer,
     TrainerCallback,
     TrainingArguments,
 )
@@ -19,12 +18,29 @@ from transformers.integrations.deepspeed import is_deepspeed_zero3_enabled
 from superduperdb import logging
 from superduperdb.base.build import build_datalayer
 from superduperdb.base.config import Config
+from superduperdb.components.component import Component
+from superduperdb.components.datatype import DataType, file_serializer
+from superduperdb.misc.hash import random_sha1
 
 if t.TYPE_CHECKING:
     from datasets import Dataset
 
     from superduperdb.base.datalayer import Datalayer
     from superduperdb.ext.llm import LLM
+
+
+@dc.dataclass(kw_only=True)
+class Checkpoint(Component):
+    path: t.Optional[str]
+    step: int
+    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, DataType]]] = (
+        ("path", file_serializer),
+    )
+    type_id: t.ClassVar[str] = "checkpoint"
+
+    def __post_init__(self, artifacts):
+        super().__post_init__(artifacts)
+        self.version = int(self.step)
 
 
 class LLMCallback(TrainerCallback):
@@ -39,6 +55,7 @@ class LLMCallback(TrainerCallback):
         self.identifier = identifier
         self.db = db
         self.llm = llm
+        self.id = random_sha1()
 
         # If we run training on remote, we need to provide identifier and cfg,
         # then can connect to db and load llm
@@ -54,6 +71,8 @@ class LLMCallback(TrainerCallback):
 
     def on_save(self, args, state, control, **kwargs):
         """Event called after a checkpoint save."""
+        if not state.is_world_process_zero:
+            return
 
         self.check_init()
         checkpoint_path = transformers.trainer.get_last_checkpoint(args.output_dir)
@@ -61,18 +80,41 @@ class LLMCallback(TrainerCallback):
             logging.warn("No checkpoint found, skip saving checkpoint")
             return
 
-        # ???
-        # self.llm.adapter_id = Artifact(checkpoint_path, serializer="zip")
-        if checkpoint_path is not None:
-            self.llm.adapter_id = checkpoint_path
-            # self.db.add(self.llm)
-            self.db.replace(self.llm)
+        checkpoint = Checkpoint(
+            identifier=self.id, path=checkpoint_path, step=state.global_step
+        )
+        self.db.add(checkpoint)
+
+    def on_evaluate(self, args, state, control, **kwargs):
+        """Event called after an evaluation."""
+        if not state.is_world_process_zero:
+            return
+
+        self.check_init()
+        self.llm.append_metrics(state.log_history[-1])
+
+    def on_train_end(self, args, state, control, **kwargs):
+        self.check_init()
+        # update the llm to db after training, will save the adapter_id and metrics
+
+        if state.best_model_checkpoint:
+            step = state.best_model_checkpoint.split("-")[-1]
+            checkpoint = Checkpoint(
+                identifier=self.id, path=state.best_model_checkpoint, step=step
+            )
+            self.db.add(checkpoint)
+
+        checkpoint = self.db.load(Checkpoint.type_id, self.id)
+        self.llm.adapter_id = checkpoint
+        self.db.replace(self.llm)
 
     def check_init(self):
         # Rebuild datalayer for the new process
         if self.db is None:
             self.db = build_datalayer(self.cfg)
             self.llm = self.db.load("model", self.identifier)
+
+        assert self.llm is not None
 
 
 @dc.dataclass
@@ -401,7 +443,6 @@ def ray_train(
     """
     import ray
     from ray import train
-    from ray.train import ScalingConfig
     from ray.train.huggingface.transformers import (
         RayTrainReportCallback,
         prepare_trainer,

Pinnacle-Networks/Pinnacle

superduperdb/components/datatype.py
@@ -21,20 +21,10 @@ from pinnacledb.base.config import BytesEncoding
 from pinnacledb.base.leaf import Leaf
 from pinnacledb.components.component import Component, ensure_initialized
 from pinnacledb.misc.annotations import public_api
+from pinnacledb.misc.hash import random_sha1
 
 Decode = t.Callable[[bytes], t.Any]
 Encode = t.Callable[[t.Any], bytes]
-
-
-def random_sha1():
-    """
-    Generate random sha1 values
-    Can be used to generate file_id and other values
-    """
-    random_data = os.urandom(256)
-    sha1 = hashlib.sha1()
-    sha1.update(random_data)
-    return sha1.hexdigest()
 
 
 def pickle_encode(object: t.Any, info: t.Optional[t.Dict] = None) -> bytes:

superduperdb/ext/llm/model.py
@@ -13,7 +13,7 @@ from pinnacledb import logging
 from pinnacledb.backends.query_dataset import QueryDataset, query_dataset_factory
 from pinnacledb.components.component import ensure_initialized
 from pinnacledb.components.dataset import Dataset as _Dataset
-from pinnacledb.components.datatype import DataType, dill_serializer, file_serializer
+from pinnacledb.components.datatype import DataType, dill_serializer
 from pinnacledb.components.model import (
     _Fittable,
     _Predictor,
@@ -22,6 +22,8 @@ from pinnacledb.components.model import (
 from pinnacledb.ext.llm import training
 from pinnacledb.ext.llm.utils import Prompter
 from pinnacledb.misc.hash import hash_dict
+
+from .training import Checkpoint
 
 if typing.TYPE_CHECKING:
     from datasets import Dataset
@@ -59,18 +61,18 @@ class LLM(_Predictor, _Fittable):
     : param prompt_func: prompt function, default is None
     """
 
-    _encodables: t.ClassVar[t.Sequence[str]] = ('model_kwargs', 'tokenizer_kwargs')
+    _encodables: t.ClassVar[t.Sequence[str]] = ("model_kwargs", "tokenizer_kwargs")
 
     identifier: str = ""
     model_name_or_path: str = "facebook/opt-125m"
     bits: t.Optional[int] = None
-    adapter_id: t.Optional[str] = None
+    adapter_id: t.Optional[t.Union[str, Checkpoint]] = None
     object: t.Optional[transformers.Trainer] = None
     model_kwargs: t.Dict = dc.field(default_factory=dict)
     tokenizer_kwargs: t.Dict = dc.field(default_factory=dict)
     prompt_template: str = "{input}"
     prompt_func: t.Optional[t.Callable] = None
-    signature: str = 'singleton'  # type: ignore[misc]
+    signature: str = "singleton"  # type: ignore[misc]
 
     # Save models and tokenizers cache for sharing when using multiple models
     _model_cache: t.ClassVar[dict] = {}
@@ -79,7 +81,6 @@ class LLM(_Predictor, _Fittable):
     _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, DataType]]] = (
         ("model_kwargs", dill_serializer),
         ("tokenizer_kwargs", dill_serializer),
-        ('adapter_id', file_serializer),
     )
 
     def __post_init__(self, artifacts):
@@ -97,6 +98,7 @@ class LLM(_Predictor, _Fittable):
                 )
             self.model_kwargs["load_in_4bit"] = self.bits == 4
             self.model_kwargs["load_in_8bit"] = self.bits == 8
+
         super().__post_init__(artifacts)
 
     def init_model_and_tokenizer(self):
@@ -137,7 +139,21 @@ class LLM(_Predictor, _Fittable):
         self.prompter = Prompter(self.prompt_template, self.prompt_func)
         self.model, self.tokenizer = self.init_model_and_tokenizer()
         if self.adapter_id is not None:
-            self.add_adapter(self.adapter_id, self.adapter_id)
+            if isinstance(self.adapter_id, Checkpoint):
+                model_id = self.adapter_id.path
+                self.adapter_id = (
+                    self.adapter_id.identifier + "-" + self.adapter_id.path
+                )
+
+            elif isinstance(self.adapter_id, str):
+                model_id = self.adapter_id
+
+            else:
+                raise ValueError(
+                    "adapter_id must be either a string or Checkpoint object"
+                )
+
+            self.add_adapter(model_id, self.adapter_id)
 
     def _fit(
         self,
@@ -149,7 +165,7 @@ class LLM(_Predictor, _Fittable):
         metrics: t.Optional[t.Sequence["Metric"]] = None,
         select: t.Optional["Select"] = None,
         validation_sets: t.Optional[t.Sequence[t.Union[str, "_Dataset"]]] = None,
-        train_dataset: t.Optional['Dataset'] = None,
+        train_dataset: t.Optional["Dataset"] = None,
         eval_dataset: t.Optional[t.Union["Dataset", t.Dict[str, "Dataset"]]] = None,
         **kwargs,
     ):
@@ -294,8 +310,8 @@ class LLM(_Predictor, _Fittable):
             keys.append(y)
 
         def transform(x):
-            if '_id' in x:
-                x['_id'] = str(x['_id'])
+            if "_id" in x:
+                x["_id"] = str(x["_id"])
             return x
 
         train_dataset = query_dataset_factory(

superduperdb/ext/llm/training.py
@@ -10,7 +10,6 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     BitsAndBytesConfig,
-    Trainer,
     TrainerCallback,
     TrainingArguments,
 )
@@ -19,12 +18,29 @@ from transformers.integrations.deepspeed import is_deepspeed_zero3_enabled
 from pinnacledb import logging
 from pinnacledb.base.build import build_datalayer
 from pinnacledb.base.config import Config
+from pinnacledb.components.component import Component
+from pinnacledb.components.datatype import DataType, file_serializer
+from pinnacledb.misc.hash import random_sha1
 
 if t.TYPE_CHECKING:
     from datasets import Dataset
 
     from pinnacledb.base.datalayer import Datalayer
     from pinnacledb.ext.llm import LLM
+
+
+@dc.dataclass(kw_only=True)
+class Checkpoint(Component):
+    path: t.Optional[str]
+    step: int
+    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, DataType]]] = (
+        ("path", file_serializer),
+    )
+    type_id: t.ClassVar[str] = "checkpoint"
+
+    def __post_init__(self, artifacts):
+        super().__post_init__(artifacts)
+        self.version = int(self.step)
 
 
 class LLMCallback(TrainerCallback):
@@ -39,6 +55,7 @@ class LLMCallback(TrainerCallback):
         self.identifier = identifier
         self.db = db
         self.llm = llm
+        self.id = random_sha1()
 
         # If we run training on remote, we need to provide identifier and cfg,
         # then can connect to db and load llm
@@ -54,6 +71,8 @@ class LLMCallback(TrainerCallback):
 
     def on_save(self, args, state, control, **kwargs):
         """Event called after a checkpoint save."""
+        if not state.is_world_process_zero:
+            return
 
         self.check_init()
         checkpoint_path = transformers.trainer.get_last_checkpoint(args.output_dir)
@@ -61,18 +80,41 @@ class LLMCallback(TrainerCallback):
             logging.warn("No checkpoint found, skip saving checkpoint")
             return
 
-        # ???
-        # self.llm.adapter_id = Artifact(checkpoint_path, serializer="zip")
-        if checkpoint_path is not None:
-            self.llm.adapter_id = checkpoint_path
-            # self.db.add(self.llm)
-            self.db.replace(self.llm)
+        checkpoint = Checkpoint(
+            identifier=self.id, path=checkpoint_path, step=state.global_step
+        )
+        self.db.add(checkpoint)
+
+    def on_evaluate(self, args, state, control, **kwargs):
+        """Event called after an evaluation."""
+        if not state.is_world_process_zero:
+            return
+
+        self.check_init()
+        self.llm.append_metrics(state.log_history[-1])
+
+    def on_train_end(self, args, state, control, **kwargs):
+        self.check_init()
+        # update the llm to db after training, will save the adapter_id and metrics
+
+        if state.best_model_checkpoint:
+            step = state.best_model_checkpoint.split("-")[-1]
+            checkpoint = Checkpoint(
+                identifier=self.id, path=state.best_model_checkpoint, step=step
+            )
+            self.db.add(checkpoint)
+
+        checkpoint = self.db.load(Checkpoint.type_id, self.id)
+        self.llm.adapter_id = checkpoint
+        self.db.replace(self.llm)
 
     def check_init(self):
         # Rebuild datalayer for the new process
         if self.db is None:
             self.db = build_datalayer(self.cfg)
             self.llm = self.db.load("model", self.identifier)
+
+        assert self.llm is not None
 
 
 @dc.dataclass
@@ -401,7 +443,6 @@ def ray_train(
     """
     import ray
     from ray import train
-    from ray.train import ScalingConfig
     from ray.train.huggingface.transformers import (
         RayTrainReportCallback,
         prepare_trainer,