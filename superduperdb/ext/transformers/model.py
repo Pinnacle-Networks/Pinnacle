superduper-io/superduper

Add requires function for checking module requirement

superduperdb/ext/transformers/model.py
@@ -1,8 +1,13 @@
+# ruff: noqa: E402
 import dataclasses as dc
 import functools
 import re
 import typing as t
 import warnings
+
+from superduperdb.misc.annotations import requires_packages
+
+requires_packages(['transformers', '4.29.1', None])
 
 from transformers import (
     DataCollatorWithPadding,

superduperdb/misc/annotations.py
@@ -1,6 +1,107 @@
+import importlib
+import operator
 import sys
 import warnings
 from typing import Optional
+
+from packaging import version
+
+from superduperdb.base.exceptions import RequiredPackageNotFound
+
+ops = {
+    "<=": operator.le,
+    "==": operator.eq,
+    "!=": operator.ne,
+    ">=": operator.ge,
+    ">": operator.gt,
+    "<": operator.lt,
+}
+
+
+def _compare_versions(op, got_ver, want_ver, requirement, pkg):
+    if not ops[op](version.parse(got_ver), version.parse(want_ver)):
+        raise RequiredPackageNotFound(
+            f"{requirement} is required for a normal"
+            f"functioning of this module, but found {pkg}=={got_ver}"
+        )
+
+
+def check_versioned(module_name, pkg, got_ver):
+    if len(module_name) == 3:
+        op = None
+        if module_name[1] is None:
+            # Upper bounded version check
+            want_ver = module_name[-1]
+            op = '<='
+            _compare_versions(op, got_ver, want_ver, f'{pkg}{op}{want_ver}', pkg)
+        elif module_name[-1] is None:
+            # Lower bounded version check
+            want_ver = module_name[1]
+            op = '>='
+            _compare_versions(op, got_ver, want_ver, f'{pkg}{op}{want_ver}', pkg)
+        else:
+            # Lower and upper bounded version check
+            want_ver_lower = module_name[1]
+            want_ver_upper = module_name[-1]
+            _compare_versions(
+                '>=', got_ver, want_ver_lower, f'{pkg}>={want_ver_lower}', pkg
+            )
+            _compare_versions(
+                '<=', got_ver, want_ver_upper, f'{pkg}<={want_ver_upper}', pkg
+            )
+
+    elif len(module_name) == 2:
+        # Exact version check
+        got_ver = importlib.metadata.version(module_name[0])
+        want_ver = module_name[1]
+        _compare_versions('==', got_ver, want_ver, f'{pkg}<={want_ver}', pkg)
+
+    else:
+        raise ValueError(
+            f'Cannot check the package requirement for the module {module_name}'
+        )
+
+
+def requires_packages(*modules):
+    '''
+    A utility function to check required packages for a module
+    i.e superduperdb.ext
+    '''
+    missing_modules = []
+    missing_module_versions = []
+    for module_name in modules:
+        pkg = module_name[0]
+        if len(module_name) == 1:
+            try:
+                importlib.import_module(module_name[0])
+            except ImportError:
+                missing_modules.append(module_name[0])
+        else:
+            got_ver = None
+            try:
+                got_ver = importlib.metadata.version(module_name[0])
+                check_versioned(module_name, pkg, got_ver)
+            except RequiredPackageNotFound:
+                missing_module_versions.append((module_name, pkg, got_ver))
+            except ImportError:
+                missing_modules.append(module_name[0])
+
+    if missing_modules:
+        raise RequiredPackageNotFound(
+            f"The following modules are required but "
+            "either not present or installed with wrong "
+            f"version: {', '.join(missing_modules)}"
+        )
+
+    if missing_module_versions:
+        msg = []
+        for v in missing_module_versions:
+            msg.append(f'Module: {v[1]} Required: {v[0]} but got {v[1]}=={v[-1]}')
+
+        raise RequiredPackageNotFound(
+            "The following modules are installed with"
+            f"different versions: {', '.join(msg)}"
+        )
 
 
 def public_api(stability: str = 'stable'):

Pinnacle-Networks/Pinnacle

superduperdb/ext/transformers/model.py
@@ -1,8 +1,13 @@
+# ruff: noqa: E402
 import dataclasses as dc
 import functools
 import re
 import typing as t
 import warnings
+
+from pinnacledb.misc.annotations import requires_packages
+
+requires_packages(['transformers', '4.29.1', None])
 
 from transformers import (
     DataCollatorWithPadding,

superduperdb/misc/annotations.py
@@ -1,6 +1,107 @@
+import importlib
+import operator
 import sys
 import warnings
 from typing import Optional
+
+from packaging import version
+
+from pinnacledb.base.exceptions import RequiredPackageNotFound
+
+ops = {
+    "<=": operator.le,
+    "==": operator.eq,
+    "!=": operator.ne,
+    ">=": operator.ge,
+    ">": operator.gt,
+    "<": operator.lt,
+}
+
+
+def _compare_versions(op, got_ver, want_ver, requirement, pkg):
+    if not ops[op](version.parse(got_ver), version.parse(want_ver)):
+        raise RequiredPackageNotFound(
+            f"{requirement} is required for a normal"
+            f"functioning of this module, but found {pkg}=={got_ver}"
+        )
+
+
+def check_versioned(module_name, pkg, got_ver):
+    if len(module_name) == 3:
+        op = None
+        if module_name[1] is None:
+            # Upper bounded version check
+            want_ver = module_name[-1]
+            op = '<='
+            _compare_versions(op, got_ver, want_ver, f'{pkg}{op}{want_ver}', pkg)
+        elif module_name[-1] is None:
+            # Lower bounded version check
+            want_ver = module_name[1]
+            op = '>='
+            _compare_versions(op, got_ver, want_ver, f'{pkg}{op}{want_ver}', pkg)
+        else:
+            # Lower and upper bounded version check
+            want_ver_lower = module_name[1]
+            want_ver_upper = module_name[-1]
+            _compare_versions(
+                '>=', got_ver, want_ver_lower, f'{pkg}>={want_ver_lower}', pkg
+            )
+            _compare_versions(
+                '<=', got_ver, want_ver_upper, f'{pkg}<={want_ver_upper}', pkg
+            )
+
+    elif len(module_name) == 2:
+        # Exact version check
+        got_ver = importlib.metadata.version(module_name[0])
+        want_ver = module_name[1]
+        _compare_versions('==', got_ver, want_ver, f'{pkg}<={want_ver}', pkg)
+
+    else:
+        raise ValueError(
+            f'Cannot check the package requirement for the module {module_name}'
+        )
+
+
+def requires_packages(*modules):
+    '''
+    A utility function to check required packages for a module
+    i.e pinnacledb.ext
+    '''
+    missing_modules = []
+    missing_module_versions = []
+    for module_name in modules:
+        pkg = module_name[0]
+        if len(module_name) == 1:
+            try:
+                importlib.import_module(module_name[0])
+            except ImportError:
+                missing_modules.append(module_name[0])
+        else:
+            got_ver = None
+            try:
+                got_ver = importlib.metadata.version(module_name[0])
+                check_versioned(module_name, pkg, got_ver)
+            except RequiredPackageNotFound:
+                missing_module_versions.append((module_name, pkg, got_ver))
+            except ImportError:
+                missing_modules.append(module_name[0])
+
+    if missing_modules:
+        raise RequiredPackageNotFound(
+            f"The following modules are required but "
+            "either not present or installed with wrong "
+            f"version: {', '.join(missing_modules)}"
+        )
+
+    if missing_module_versions:
+        msg = []
+        for v in missing_module_versions:
+            msg.append(f'Module: {v[1]} Required: {v[0]} but got {v[1]}=={v[-1]}')
+
+        raise RequiredPackageNotFound(
+            "The following modules are installed with"
+            f"different versions: {', '.join(msg)}"
+        )
 
 
 def public_api(stability: str = 'stable'):