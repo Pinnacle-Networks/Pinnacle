superduper-io/superduper

Add requires for all ext

superduperdb/ext/cohere/model.py
@@ -1,6 +1,10 @@
 import dataclasses as dc
 import typing as t
 
+from superduperdb.misc.annotations import requires_packages
+
+requires_packages(['cohere', '4.40'])
+
 import cohere
 import tqdm
 from cohere.error import CohereAPIError, CohereConnectionError

superduperdb/ext/llamacpp/model.py
@@ -3,6 +3,11 @@ import os
 import typing as t
 
 import requests
+
+from superduperdb.misc.annotations import requires_packages
+
+requires_packages(['llama_cpp_python', '0.2.39', None])
+
 from llama_cpp import Llama
 
 from superduperdb.ext.llm.base import _BaseLLM

superduperdb/ext/llm/model.py
@@ -3,6 +3,9 @@ import functools
 import typing
 import typing as t
 
+from superduperdb.misc.annotations import requires_packages
+
+requires_packages(['transformers', '4.29.1', None])
 import transformers
 from transformers import (
     AutoModelForCausalLM,

superduperdb/ext/openai/model.py
@@ -6,6 +6,10 @@ import typing as t
 
 import requests
 import tqdm
+
+from superduperdb.misc.annotations import requires_packages
+
+requires_packages(['openai', '1.1.2', None], ['httpx'])
 from httpx import ResponseNotRead
 from openai import (
     APITimeoutError,

superduperdb/ext/sentence_transformers/model.py
@@ -1,6 +1,10 @@
 import dataclasses as dc
 import typing as t
 
+from superduperdb.misc.annotations import requires_packages
+
+requires_packages(['sentence-transformers', '2.2.2', None])
+
 from overrides import override
 from sentence_transformers import SentenceTransformer as _SentenceTransformer
 

superduperdb/ext/torch/encoder.py
@@ -1,5 +1,9 @@
 import typing as t
 
+from superduperdb.misc.annotations import requires_packages
+
+# TODO: check logic for != version checks
+requires_packages(['torch', '2.0.0', None])
 import numpy
 import torch
 

superduperdb/ext/unstructured/encoder.py
@@ -2,6 +2,9 @@ import pickle
 import typing as t
 
 # TODO add initialization code ensuring a version range for this code
+from superduperdb.misc.annotations import requires_packages
+
+requires_packages(['unstructured', '0.12.5', None])
 from unstructured.documents.elements import Element
 from unstructured.partition.auto import partition
 from unstructured.partition.html import partition_html

test/unittest/misc/annotations.py
@@ -1,0 +1,33 @@
+import pytest
+
+from superduperdb.base.exceptions import RequiredPackageNotFound
+from superduperdb.misc.annotations import requires_packages
+
+
+def reqs_msg(packages):
+    return f'Add following lines to requirements.txt:\n{packages}'
+
+
+def test_basic_requires():
+    with pytest.raises(RequiredPackageNotFound) as excinfo:
+        requires_packages(['some-package'])
+    assert reqs_msg('\n'.join(['some-package'])) in str(excinfo.value)
+
+
+def test_versioned_requires():
+    with pytest.raises(RequiredPackageNotFound) as excinfo:
+        requires_packages(
+            ['numpy', '10.1.0', None],
+            ['requests', None, '0.1.0'],
+            ['numpy', '0.0.1', '0.1.0'],
+            ['requests', '0.1.0'],
+            ['bad-package'],
+        )
+    reqs = [
+        'numpy>=10.1.0',
+        'requests<=0.1.0',
+        'numpy>=0.0.1,<=0.1.0',
+        'requests==0.1.0',
+        'bad-package',
+    ]
+    assert reqs_msg('\n'.join(reqs)) in str(excinfo.value)

Pinnacle-Networks/Pinnacle

superduperdb/ext/cohere/model.py
@@ -1,6 +1,10 @@
 import dataclasses as dc
 import typing as t
 
+from pinnacledb.misc.annotations import requires_packages
+
+requires_packages(['cohere', '4.40'])
+
 import cohere
 import tqdm
 from cohere.error import CohereAPIError, CohereConnectionError

superduperdb/ext/llamacpp/model.py
@@ -3,6 +3,11 @@ import os
 import typing as t
 
 import requests
+
+from pinnacledb.misc.annotations import requires_packages
+
+requires_packages(['llama_cpp_python', '0.2.39', None])
+
 from llama_cpp import Llama
 
 from pinnacledb.ext.llm.base import _BaseLLM

superduperdb/ext/llm/model.py
@@ -3,6 +3,9 @@ import functools
 import typing
 import typing as t
 
+from pinnacledb.misc.annotations import requires_packages
+
+requires_packages(['transformers', '4.29.1', None])
 import transformers
 from transformers import (
     AutoModelForCausalLM,

superduperdb/ext/openai/model.py
@@ -6,6 +6,10 @@ import typing as t
 
 import requests
 import tqdm
+
+from pinnacledb.misc.annotations import requires_packages
+
+requires_packages(['openai', '1.1.2', None], ['httpx'])
 from httpx import ResponseNotRead
 from openai import (
     APITimeoutError,

superduperdb/ext/sentence_transformers/model.py
@@ -1,6 +1,10 @@
 import dataclasses as dc
 import typing as t
 
+from pinnacledb.misc.annotations import requires_packages
+
+requires_packages(['sentence-transformers', '2.2.2', None])
+
 from overrides import override
 from sentence_transformers import SentenceTransformer as _SentenceTransformer
 

superduperdb/ext/torch/encoder.py
@@ -1,5 +1,9 @@
 import typing as t
 
+from pinnacledb.misc.annotations import requires_packages
+
+# TODO: check logic for != version checks
+requires_packages(['torch', '2.0.0', None])
 import numpy
 import torch
 

superduperdb/ext/unstructured/encoder.py
@@ -2,6 +2,9 @@ import pickle
 import typing as t
 
 # TODO add initialization code ensuring a version range for this code
+from pinnacledb.misc.annotations import requires_packages
+
+requires_packages(['unstructured', '0.12.5', None])
 from unstructured.documents.elements import Element
 from unstructured.partition.auto import partition
 from unstructured.partition.html import partition_html

test/unittest/misc/annotations.py
@@ -1,0 +1,33 @@
+import pytest
+
+from pinnacledb.base.exceptions import RequiredPackageNotFound
+from pinnacledb.misc.annotations import requires_packages
+
+
+def reqs_msg(packages):
+    return f'Add following lines to requirements.txt:\n{packages}'
+
+
+def test_basic_requires():
+    with pytest.raises(RequiredPackageNotFound) as excinfo:
+        requires_packages(['some-package'])
+    assert reqs_msg('\n'.join(['some-package'])) in str(excinfo.value)
+
+
+def test_versioned_requires():
+    with pytest.raises(RequiredPackageNotFound) as excinfo:
+        requires_packages(
+            ['numpy', '10.1.0', None],
+            ['requests', None, '0.1.0'],
+            ['numpy', '0.0.1', '0.1.0'],
+            ['requests', '0.1.0'],
+            ['bad-package'],
+        )
+    reqs = [
+        'numpy>=10.1.0',
+        'requests<=0.1.0',
+        'numpy>=0.0.1,<=0.1.0',
+        'requests==0.1.0',
+        'bad-package',
+    ]
+    assert reqs_msg('\n'.join(reqs)) in str(excinfo.value)