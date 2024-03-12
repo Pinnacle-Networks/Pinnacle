superduper-io/superduper

Support Schema option for MongoDB

superduperdb/backends/mongodb/query.py
@@ -23,6 +23,7 @@ from superduperdb.backends.base.query import (
 from superduperdb.base.cursor import SuperDuperCursor
 from superduperdb.base.document import Document
 from superduperdb.base.serializable import Variable
+from superduperdb.components.schema import Schema
 from superduperdb.misc.files import load_uris
 
 
@@ -304,10 +305,12 @@ class Aggregate(Select):
                 self.vector_index,
             )
         )
+        decode_function = _get_decode_function(db, self.table_or_collection.identifier)
         return SuperDuperCursor(
             raw_cursor=cursor,
             id_field='_id',
             db=db,
+            decode_function=decode_function,
         )
 
 
@@ -368,14 +371,17 @@ class MongoCompoundSelect(CompoundSelect):
 
     def execute(self, db, reference=False):
         output, scores = self._execute(db)
+        decode_function = _get_decode_function(db, self.table_or_collection.identifier)
         if isinstance(output, (pymongo.cursor.Cursor, mongomock.collection.Cursor)):
             return SuperDuperCursor(
                 raw_cursor=output,
                 id_field='_id',
                 scores=scores,
                 db=db,
+                decode_function=decode_function,
             )
         elif isinstance(output, dict):
+            output = decode_function(output)
             if reference and CFG.hybrid_storage:
                 load_uris(output, datatypes=db.datatypes)
             return Document.decode(output, db)
@@ -512,7 +518,13 @@ class MongoInsert(Insert):
         collection = db.databackend.get_table_or_collection(
             self.table_or_collection.identifier
         )
-        documents = [r.encode() for r in self.documents]
+        try:
+            schema: Schema = db.load(
+                Schema.type_id, self.table_or_collection.identifier
+            )
+        except FileNotFoundError:
+            schema = None
+        documents = [r.encode(schema) for r in self.documents]
         insert_result = collection.insert_many(documents, **self.kwargs)
         return insert_result.inserted_ids
 
@@ -798,3 +810,20 @@ class Collection(TableOrCollection):
             collection_name = f'_outputs.{key}.{model}'
             collection = db.databackend.get_table_or_collection(collection_name)
             collection.bulk_write(bulk_docs)
+
+
+def _get_decode_function(db, schema_identifier: str) -> t.Callable[[t.Any], t.Any]:
+    try:
+        schema = db.load(Schema.type_id, schema_identifier)
+    except FileNotFoundError:
+        schema = None
+    if schema is None:
+        return lambda x: x
+
+    def decode(output):
+        for k in output.keys():
+            if field := schema.fields.get(k):
+                output[k] = field.decode_data(output[k])
+        return output
+
+    return decode

test/unittest/backends/mongodb/test_query.py
@@ -1,5 +1,98 @@
+import random
+from test.db_config import DBConfig
+
+import numpy as np
+import pytest
+
 from superduperdb.backends.mongodb import query as q
+from superduperdb.backends.mongodb.query import Collection
 from superduperdb.base.document import Document
+from superduperdb.components.schema import Schema
+from superduperdb.ext.numpy.encoder import array
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_mongo_without_schema(db):
+    collection_name = "documents"
+    array_tensor = array("float64", shape=(32,))
+    db.add(array_tensor)
+    data = []
+
+    for id_ in range(5):
+        x = np.random.rand(32)
+        y = int(random.random() > 0.5)
+        z = np.random.randn(32)
+        data.append(
+            Document(
+                {
+                    "id": id_,
+                    "x": array_tensor(x),
+                    "y": y,
+                    "z": array_tensor(z),
+                }
+            )
+        )
+
+    db.execute(
+        Collection(collection_name).insert_many(data),
+    )
+    collection = Collection(collection_name)
+    r = collection.find_one().execute(db)
+    rs = list(collection.find().execute(db))
+
+    rs = sorted(rs, key=lambda x: x['id'])
+
+    assert np.array_equal(r['x'].x, data[0]['x'].x)
+    assert np.array_equal(r['z'].x, data[0]['z'].x)
+
+    assert np.array_equal(rs[0]['x'].x, data[0]['x'].x)
+    assert np.array_equal(rs[0]['z'].x, data[0]['z'].x)
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_mongo_schema(db):
+    collection_name = "documents"
+    array_tensor = array("float64", shape=(32,))
+    data = []
+
+    for id_ in range(5):
+        x = np.random.rand(32)
+        y = int(random.random() > 0.5)
+        z = np.random.randn(32)
+        data.append(
+            Document(
+                {
+                    "id": id_,
+                    "x": x,
+                    "y": y,
+                    "z": z,
+                }
+            )
+        )
+    schema = Schema(
+        identifier=collection_name,
+        fields={
+            "x": array_tensor,
+            "z": array_tensor,
+        },
+    )
+
+    db.add(schema)
+
+    db.execute(
+        Collection(collection_name).insert_many(data),
+    )
+    collection = Collection(collection_name)
+    r = collection.find_one().execute(db)
+    rs = list(collection.find().execute(db))
+
+    rs = sorted(rs, key=lambda x: x['id'])
+
+    assert np.array_equal(r['x'], data[0]['x'])
+    assert np.array_equal(r['z'], data[0]['z'])
+
+    assert np.array_equal(rs[0]['x'], data[0]['x'])
+    assert np.array_equal(rs[0]['z'], data[0]['z'])
 
 
 def test_select_missing_outputs(db):

Pinnacle-Networks/Pinnacle

superduperdb/backends/mongodb/query.py
@@ -23,6 +23,7 @@ from pinnacledb.backends.base.query import (
 from pinnacledb.base.cursor import SuperDuperCursor
 from pinnacledb.base.document import Document
 from pinnacledb.base.serializable import Variable
+from pinnacledb.components.schema import Schema
 from pinnacledb.misc.files import load_uris
 
 
@@ -304,10 +305,12 @@ class Aggregate(Select):
                 self.vector_index,
             )
         )
+        decode_function = _get_decode_function(db, self.table_or_collection.identifier)
         return SuperDuperCursor(
             raw_cursor=cursor,
             id_field='_id',
             db=db,
+            decode_function=decode_function,
         )
 
 
@@ -368,14 +371,17 @@ class MongoCompoundSelect(CompoundSelect):
 
     def execute(self, db, reference=False):
         output, scores = self._execute(db)
+        decode_function = _get_decode_function(db, self.table_or_collection.identifier)
         if isinstance(output, (pymongo.cursor.Cursor, mongomock.collection.Cursor)):
             return SuperDuperCursor(
                 raw_cursor=output,
                 id_field='_id',
                 scores=scores,
                 db=db,
+                decode_function=decode_function,
             )
         elif isinstance(output, dict):
+            output = decode_function(output)
             if reference and CFG.hybrid_storage:
                 load_uris(output, datatypes=db.datatypes)
             return Document.decode(output, db)
@@ -512,7 +518,13 @@ class MongoInsert(Insert):
         collection = db.databackend.get_table_or_collection(
             self.table_or_collection.identifier
         )
-        documents = [r.encode() for r in self.documents]
+        try:
+            schema: Schema = db.load(
+                Schema.type_id, self.table_or_collection.identifier
+            )
+        except FileNotFoundError:
+            schema = None
+        documents = [r.encode(schema) for r in self.documents]
         insert_result = collection.insert_many(documents, **self.kwargs)
         return insert_result.inserted_ids
 
@@ -798,3 +810,20 @@ class Collection(TableOrCollection):
             collection_name = f'_outputs.{key}.{model}'
             collection = db.databackend.get_table_or_collection(collection_name)
             collection.bulk_write(bulk_docs)
+
+
+def _get_decode_function(db, schema_identifier: str) -> t.Callable[[t.Any], t.Any]:
+    try:
+        schema = db.load(Schema.type_id, schema_identifier)
+    except FileNotFoundError:
+        schema = None
+    if schema is None:
+        return lambda x: x
+
+    def decode(output):
+        for k in output.keys():
+            if field := schema.fields.get(k):
+                output[k] = field.decode_data(output[k])
+        return output
+
+    return decode

test/unittest/backends/mongodb/test_query.py
@@ -1,5 +1,98 @@
+import random
+from test.db_config import DBConfig
+
+import numpy as np
+import pytest
+
 from pinnacledb.backends.mongodb import query as q
+from pinnacledb.backends.mongodb.query import Collection
 from pinnacledb.base.document import Document
+from pinnacledb.components.schema import Schema
+from pinnacledb.ext.numpy.encoder import array
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_mongo_without_schema(db):
+    collection_name = "documents"
+    array_tensor = array("float64", shape=(32,))
+    db.add(array_tensor)
+    data = []
+
+    for id_ in range(5):
+        x = np.random.rand(32)
+        y = int(random.random() > 0.5)
+        z = np.random.randn(32)
+        data.append(
+            Document(
+                {
+                    "id": id_,
+                    "x": array_tensor(x),
+                    "y": y,
+                    "z": array_tensor(z),
+                }
+            )
+        )
+
+    db.execute(
+        Collection(collection_name).insert_many(data),
+    )
+    collection = Collection(collection_name)
+    r = collection.find_one().execute(db)
+    rs = list(collection.find().execute(db))
+
+    rs = sorted(rs, key=lambda x: x['id'])
+
+    assert np.array_equal(r['x'].x, data[0]['x'].x)
+    assert np.array_equal(r['z'].x, data[0]['z'].x)
+
+    assert np.array_equal(rs[0]['x'].x, data[0]['x'].x)
+    assert np.array_equal(rs[0]['z'].x, data[0]['z'].x)
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_mongo_schema(db):
+    collection_name = "documents"
+    array_tensor = array("float64", shape=(32,))
+    data = []
+
+    for id_ in range(5):
+        x = np.random.rand(32)
+        y = int(random.random() > 0.5)
+        z = np.random.randn(32)
+        data.append(
+            Document(
+                {
+                    "id": id_,
+                    "x": x,
+                    "y": y,
+                    "z": z,
+                }
+            )
+        )
+    schema = Schema(
+        identifier=collection_name,
+        fields={
+            "x": array_tensor,
+            "z": array_tensor,
+        },
+    )
+
+    db.add(schema)
+
+    db.execute(
+        Collection(collection_name).insert_many(data),
+    )
+    collection = Collection(collection_name)
+    r = collection.find_one().execute(db)
+    rs = list(collection.find().execute(db))
+
+    rs = sorted(rs, key=lambda x: x['id'])
+
+    assert np.array_equal(r['x'], data[0]['x'])
+    assert np.array_equal(r['z'], data[0]['z'])
+
+    assert np.array_equal(rs[0]['x'], data[0]['x'])
+    assert np.array_equal(rs[0]['z'], data[0]['z'])
 
 
 def test_select_missing_outputs(db):