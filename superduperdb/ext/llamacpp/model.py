superduper-io/superduper

Sort out the llm directory structure

superduperdb/ext/llamacpp/model.py
@@ -5,7 +5,7 @@ import typing as t
 import requests
 from llama_cpp import Llama
 
-from superduperdb.ext.llm.base import _BaseLLM
+from superduperdb.ext.llm.base import BaseLLM
 
 
 # TODO use core downloader already implemented
@@ -25,7 +25,7 @@ def download_uri(uri, save_path):
 
 
 @dc.dataclass(kw_only=True)
-class LlamaCpp(_BaseLLM):
+class LlamaCpp(BaseLLM):
     """
     Llama.cpp connector
 

superduperdb/ext/llm/base.py
@@ -2,18 +2,17 @@ import abc
 import concurrent.futures
 import dataclasses as dc
 import inspect
-import typing
+import typing as t
 from functools import reduce
 from logging import WARNING, getLogger
-from typing import Any, Callable, List, Optional, Sequence, Union
 
 from superduperdb import logging
 from superduperdb.backends.query_dataset import QueryDataset
 from superduperdb.components.component import ensure_initialized
 from superduperdb.components.model import _Predictor
-from superduperdb.ext.llm.utils import Prompter
+from superduperdb.ext.llm.prompter import Prompter
 
-if typing.TYPE_CHECKING:
+if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
 
 # Disable httpx info level logging
@@ -21,7 +20,7 @@ getLogger("httpx").setLevel(WARNING)
 
 
 @dc.dataclass(kw_only=True)
-class _BaseLLM(_Predictor, metaclass=abc.ABCMeta):
+class BaseLLM(_Predictor, metaclass=abc.ABCMeta):
     """
     :param prompt_template: The template to use for the prompt.
     :param prompt_func: The function to use for the prompt.
@@ -30,8 +29,9 @@ class _BaseLLM(_Predictor, metaclass=abc.ABCMeta):
     """
 
     prompt_template: str = "{input}"
-    prompt_func: Optional[Callable] = dc.field(default=None)
-    max_batch_size: Optional[int] = 4
+    prompt_func: t.Optional[t.Callable] = dc.field(default=None)
+    max_batch_size: t.Optional[int] = 4
+    signature: t.ClassVar[str] = 'singleton'
 
     def __post_init__(self, artifacts):
         super().__post_init__(artifacts)
@@ -56,10 +56,10 @@ class _BaseLLM(_Predictor, metaclass=abc.ABCMeta):
     def init(self):
         ...
 
-    def _generate(self, prompt: str, **kwargs: Any):
+    def _generate(self, prompt: str, **kwargs: t.Any):
         raise NotImplementedError
 
-    def _batch_generate(self, prompts: List[str], **kwargs) -> List[str]:
+    def _batch_generate(self, prompts: t.List[str], **kwargs) -> t.List[str]:
         """
         Base method to batch generate text from a list of prompts.
         If the model can run batch generation efficiently, pls override this method.
@@ -67,13 +67,13 @@ class _BaseLLM(_Predictor, metaclass=abc.ABCMeta):
         return [self._generate(prompt, **self.predict_kwargs) for prompt in prompts]
 
     @ensure_initialized
-    def predict_one(self, X: Union[str, dict[str, str]], **kwargs):
+    def predict_one(self, X: t.Union[str, dict[str, str]], **kwargs):
         x = self.prompter(X, **kwargs)
         kwargs.pop("context", None)
         return self._generate(x, **kwargs)
 
     @ensure_initialized
-    def predict(self, dataset: Union[List, QueryDataset], **kwargs) -> Sequence:
+    def predict(self, dataset: t.Union[t.List, QueryDataset], **kwargs) -> t.Sequence:
         xs = [self.prompter(dataset[i], **kwargs) for i in range(len(dataset))]
         kwargs.pop("context", None)
         return self._batch_generate(xs, **kwargs)
@@ -99,20 +99,20 @@ class _BaseLLM(_Predictor, metaclass=abc.ABCMeta):
 
 
 @dc.dataclass
-class BaseLLMAPI(_BaseLLM):
+class BaseLLMAPI(BaseLLM):
     """
     :param api_url: The URL for the API.
     {parent_doc}
     """
 
-    __doc__ = __doc__.format(parent_doc=_BaseLLM.__doc__)
+    __doc__ = __doc__.format(parent_doc=BaseLLM.__doc__)
 
     api_url: str = dc.field(default="")
 
     def init(self):
         pass
 
-    def _generate_wrapper(self, prompt: str, **kwargs: Any) -> str:
+    def _generate_wrapper(self, prompt: str, **kwargs: t.Any) -> str:
         """
         Wrapper for the _generate method to handle exceptions.
         """
@@ -122,7 +122,7 @@ class BaseLLMAPI(_BaseLLM):
             logging.error(f"Error generating response for prompt '{prompt}': {e}")
             return ""
 
-    def _batch_generate(self, prompts: List[str], **kwargs: Any) -> List[str]:
+    def _batch_generate(self, prompts: t.List[str], **kwargs: t.Any) -> t.List[str]:
         """
         Base method to batch generate text from a list of prompts using multi-threading.
         Handles exceptions in _generate method.
@@ -137,121 +137,3 @@ class BaseLLMAPI(_BaseLLM):
             )
 
         return results
-
-
-@dc.dataclass
-class BaseOpenAI(BaseLLMAPI):
-    """
-    :param openai_api_base: The base URL for the OpenAI API.
-    :param openai_api_key: The API key to use for the OpenAI API.
-    :param model_name: The name of the model to use.
-    :param chat: Whether to use the chat API or the completion API. Defaults to False.
-    :param system_prompt: The prompt to use for the system.
-    :param user_role: The role to use for the user.
-    :param system_role: The role to use for the system.
-    {parent_doc}
-    """
-
-    __doc__ = __doc__.format(parent_doc=_BaseLLM.__doc__)
-
-    identifier: str = dc.field(default="")
-    openai_api_base: str = "https://api.openai.com/v1"
-    openai_api_key: Optional[str] = None
-    model_name: str = "gpt-3.5-turbo"
-    chat: bool = True
-    system_prompt: Optional[str] = None
-    user_role: str = "user"
-    system_role: str = "system"
-
-    def __post_init__(self, artifacts):
-        self.api_url = self.openai_api_base
-        self.identifier = self.identifier or self.model_name
-        super().__post_init__(artifacts)
-
-    def init(self):
-        try:
-            from openai import OpenAI
-        except ImportError:
-            raise Exception("You must install openai with command 'pip install openai'")
-
-        params = {
-            "api_key": self.openai_api_key,
-            "base_url": self.openai_api_base,
-        }
-
-        self.client = OpenAI(**params)
-        model_set = self.get_model_set()
-        assert (
-            self.model_name in model_set
-        ), f"model_name {self.model_name} is not in model_set {model_set}"
-
-    def get_model_set(self):
-        model_list = self.client.models.list()
-        return sorted({model.id for model in model_list.data})
-
-    def _generate(self, prompt: str, **kwargs: Any) -> str:
-        if self.chat:
-            return self._chat_generate(prompt, **kwargs)
-        else:
-            return self._prompt_generate(prompt, **kwargs)
-
-    def _prompt_generate(self, prompt: str, **kwargs: Any) -> str:
-        """
-        Generate a completion for a given prompt with prompt format.
-        """
-        completion = self.client.completions.create(
-            model=self.model_name,
-            prompt=prompt,
-            **self.get_kwargs(
-                self.client.completions.create, kwargs, self.predict_kwargs
-            ),
-        )
-        return completion.choices[0].text
-
-    def _chat_generate(self, content: str, **kwargs: Any) -> str:
-        """
-        Generate a completion for a given prompt with chat format.
-        :param prompt: The prompt to generate a completion for.
-        :param kwargs: Any additional arguments to pass to the API.
-        """
-        messages = kwargs.get("messages", [])
-
-        if self.system_prompt:
-            messages = [
-                {"role": self.system_role, "content": self.system_prompt}
-            ] + messages
-
-        messages.append({"role": self.user_role, "content": content})
-        completion = self.client.chat.completions.create(
-            messages=messages,
-            model=self.model_name,
-            **self.get_kwargs(
-                self.client.chat.completions.create, kwargs, self.predict_kwargs
-            ),
-        )
-        return completion.choices[0].message.content
-
-
-@dc.dataclass
-class BaseLLMModel(_BaseLLM):
-    """
-    :param model_name: The name of the model to use.
-    :param on_ray: Whether to run the model on Ray.
-    :param ray_config: The Ray config to use.
-    :param ray_addredd: The address of the ray cluster.
-    {parent_doc}
-    """
-
-    __doc__ = __doc__.format(parent_doc=_BaseLLM.__doc__)
-
-    identifier: str = dc.field(default="")
-    model_name: str = dc.field(default="")
-    on_ray: bool = False
-    ray_address: Optional[str] = None
-    ray_config: dict = dc.field(default_factory=dict)
-
-    def __post_init__(self, artifacts):
-        self.identifier = self.identifier or self.model_name
-        assert self.model_name, "model_name can not be empty"
-        self._is_initialized = False
-        super().__post_init__(artifacts)

superduperdb/ext/transformers/llm.py
@@ -19,16 +19,15 @@ from superduperdb.backends.query_dataset import QueryDataset
 from superduperdb.components.component import ensure_initialized
 from superduperdb.components.datatype import DataType, dill_serializer
 from superduperdb.components.model import _Fittable, _Validator
-from superduperdb.ext.llm.base import _BaseLLM
-
-from .training import Checkpoint
+from superduperdb.ext.llm.base import BaseLLM
+from superduperdb.ext.transformers.llm_training import Checkpoint
 
 if typing.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
 
 
 @dc.dataclass(kw_only=True)
-class LLM(_BaseLLM, _Fittable, _Validator):
+class LLM(BaseLLM, _Fittable, _Validator):
     """
     LLM model based on `transformers` library.
 
@@ -232,7 +231,7 @@ class LLM(_BaseLLM, _Fittable, _Validator):
         Generate text.
         Can overwrite this method to support more inference methods.
         """
-        kwargs = kwargs.copy()
+        kwargs = {**self.predict_kwargs, **kwargs}
 
         # Set default values, if not will cause bad output
         kwargs.setdefault("add_special_tokens", True)

superduperdb/ext/vllm/__init__.py
@@ -1,0 +1,3 @@
+from superduperdb.ext.vllm.model import VllmAPI, VllmModel
+
+__all__ = ["VllmAPI", "VllmModel"]

test/unittest/ext/test_vllm.py
@@ -5,13 +5,11 @@ from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predi
 import pytest
 import vcr
 
-from superduperdb.ext.llm.vllm import VllmAPI
+from superduperdb.ext.vllm import VllmAPI
 
-CASSETTE_DIR = "test/unittest/ext/cassettes/llm/vllm"
+CASSETTE_DIR = "test/unittest/ext/cassettes/vllm"
 
-model_name = "mistralai/Mistral-7B-Instruct-v0.2"
-openai_api_base = "http://ec2-54-208-130-192.compute-1.amazonaws.com:8000/v1"
-api_url = "http://ec2-54-208-130-192.compute-1.amazonaws.com:8000/generate"
+api_url = "http://localhost:8000/generate"
 
 
 @pytest.fixture
@@ -24,24 +22,27 @@ def openai_mock(monkeypatch):
     f"{CASSETTE_DIR}/test_predict_api.yaml",
     filter_headers=["authorization"],
     record_on_exception=False,
-    ignore_localhost=True,
+    ignore_localhost=False,
 )
 @pytest.mark.parametrize(
     "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
 )
 def test_predict_api(db):
     """Test chat."""
-    check_predict(db, VllmAPI(identifier="llm", api_url=api_url))
+    check_predict(db, VllmAPI(identifier="llm", api_url=api_url, max_batch_size=1))
 
 
 @vcr.use_cassette(
     f"{CASSETTE_DIR}/test_llm_as_listener_model_api.yaml",
     filter_headers=["authorization"],
     record_on_exception=False,
-    ignore_localhost=True,
+    ignore_localhost=False,
 )
 @pytest.mark.parametrize(
     "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
 )
 def test_llm_as_listener_model_api(db):
-    check_llm_as_listener_model(db, VllmAPI(identifier="llm", api_url=api_url))
+    # vcr does not support multiple requests in a single test, set max_batch_size to 1
+    check_llm_as_listener_model(
+        db, VllmAPI(identifier="llm", api_url=api_url, max_batch_size=1)
+    )

test/unittest/ext/transformers/test_llm.py
@@ -1,0 +1,26 @@
+from test.db_config import DBConfig
+from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predict
+
+import pytest
+
+from superduperdb.ext.transformers import LLM
+
+TEST_MODEL_NAME = "facebook/opt-125m"
+
+
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_predict(db):
+    """Test chat."""
+    model = LLM(identifier="llm", model_name_or_path=TEST_MODEL_NAME)
+
+    check_predict(db, model)
+
+
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_model_as_listener_model(db):
+    model = LLM(identifier="llm", model_name_or_path=TEST_MODEL_NAME)
+    check_llm_as_listener_model(db, model)

test/unittest/ext/transformers/test_llm_training.py
@@ -1,7 +1,6 @@
 import os
 import random
 from test.db_config import DBConfig
-from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predict
 
 import pytest
 
@@ -9,8 +8,8 @@ from superduperdb.backends.mongodb.query import Collection
 from superduperdb.base.document import Document
 from superduperdb.components.dataset import Dataset
 from superduperdb.components.metric import Metric
-from superduperdb.ext.llm.model import LLM
-from superduperdb.ext.llm.training import Checkpoint, LLMTrainer
+from superduperdb.ext.transformers import LLM
+from superduperdb.ext.transformers.llm_training import Checkpoint, LLMTrainer
 
 TEST_MODEL_NAME = "facebook/opt-125m"
 try:
@@ -21,24 +20,6 @@ except ImportError:
     datasets = None
     peft = None
     trl = None
-
-
-@pytest.mark.parametrize(
-    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
-)
-def test_predict(db):
-    """Test chat."""
-    model = LLM(identifier="llm", model_name_or_path=TEST_MODEL_NAME)
-
-    check_predict(db, model)
-
-
-@pytest.mark.parametrize(
-    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
-)
-def test_model_as_listener_model(db):
-    model = LLM(identifier="llm", model_name_or_path=TEST_MODEL_NAME)
-    check_llm_as_listener_model(db, model)
 
 
 @pytest.mark.skipif(

Pinnacle-Networks/Pinnacle

superduperdb/ext/llamacpp/model.py
@@ -5,7 +5,7 @@ import typing as t
 import requests
 from llama_cpp import Llama
 
-from pinnacledb.ext.llm.base import _BaseLLM
+from pinnacledb.ext.llm.base import BaseLLM
 
 
 # TODO use core downloader already implemented
@@ -25,7 +25,7 @@ def download_uri(uri, save_path):
 
 
 @dc.dataclass(kw_only=True)
-class LlamaCpp(_BaseLLM):
+class LlamaCpp(BaseLLM):
     """
     Llama.cpp connector
 

superduperdb/ext/llm/base.py
@@ -2,18 +2,17 @@ import abc
 import concurrent.futures
 import dataclasses as dc
 import inspect
-import typing
+import typing as t
 from functools import reduce
 from logging import WARNING, getLogger
-from typing import Any, Callable, List, Optional, Sequence, Union
 
 from pinnacledb import logging
 from pinnacledb.backends.query_dataset import QueryDataset
 from pinnacledb.components.component import ensure_initialized
 from pinnacledb.components.model import _Predictor
-from pinnacledb.ext.llm.utils import Prompter
+from pinnacledb.ext.llm.prompter import Prompter
 
-if typing.TYPE_CHECKING:
+if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
 
 # Disable httpx info level logging
@@ -21,7 +20,7 @@ getLogger("httpx").setLevel(WARNING)
 
 
 @dc.dataclass(kw_only=True)
-class _BaseLLM(_Predictor, metaclass=abc.ABCMeta):
+class BaseLLM(_Predictor, metaclass=abc.ABCMeta):
     """
     :param prompt_template: The template to use for the prompt.
     :param prompt_func: The function to use for the prompt.
@@ -30,8 +29,9 @@ class _BaseLLM(_Predictor, metaclass=abc.ABCMeta):
     """
 
     prompt_template: str = "{input}"
-    prompt_func: Optional[Callable] = dc.field(default=None)
-    max_batch_size: Optional[int] = 4
+    prompt_func: t.Optional[t.Callable] = dc.field(default=None)
+    max_batch_size: t.Optional[int] = 4
+    signature: t.ClassVar[str] = 'singleton'
 
     def __post_init__(self, artifacts):
         super().__post_init__(artifacts)
@@ -56,10 +56,10 @@ class _BaseLLM(_Predictor, metaclass=abc.ABCMeta):
     def init(self):
         ...
 
-    def _generate(self, prompt: str, **kwargs: Any):
+    def _generate(self, prompt: str, **kwargs: t.Any):
         raise NotImplementedError
 
-    def _batch_generate(self, prompts: List[str], **kwargs) -> List[str]:
+    def _batch_generate(self, prompts: t.List[str], **kwargs) -> t.List[str]:
         """
         Base method to batch generate text from a list of prompts.
         If the model can run batch generation efficiently, pls override this method.
@@ -67,13 +67,13 @@ class _BaseLLM(_Predictor, metaclass=abc.ABCMeta):
         return [self._generate(prompt, **self.predict_kwargs) for prompt in prompts]
 
     @ensure_initialized
-    def predict_one(self, X: Union[str, dict[str, str]], **kwargs):
+    def predict_one(self, X: t.Union[str, dict[str, str]], **kwargs):
         x = self.prompter(X, **kwargs)
         kwargs.pop("context", None)
         return self._generate(x, **kwargs)
 
     @ensure_initialized
-    def predict(self, dataset: Union[List, QueryDataset], **kwargs) -> Sequence:
+    def predict(self, dataset: t.Union[t.List, QueryDataset], **kwargs) -> t.Sequence:
         xs = [self.prompter(dataset[i], **kwargs) for i in range(len(dataset))]
         kwargs.pop("context", None)
         return self._batch_generate(xs, **kwargs)
@@ -99,20 +99,20 @@ class _BaseLLM(_Predictor, metaclass=abc.ABCMeta):
 
 
 @dc.dataclass
-class BaseLLMAPI(_BaseLLM):
+class BaseLLMAPI(BaseLLM):
     """
     :param api_url: The URL for the API.
     {parent_doc}
     """
 
-    __doc__ = __doc__.format(parent_doc=_BaseLLM.__doc__)
+    __doc__ = __doc__.format(parent_doc=BaseLLM.__doc__)
 
     api_url: str = dc.field(default="")
 
     def init(self):
         pass
 
-    def _generate_wrapper(self, prompt: str, **kwargs: Any) -> str:
+    def _generate_wrapper(self, prompt: str, **kwargs: t.Any) -> str:
         """
         Wrapper for the _generate method to handle exceptions.
         """
@@ -122,7 +122,7 @@ class BaseLLMAPI(_BaseLLM):
             logging.error(f"Error generating response for prompt '{prompt}': {e}")
             return ""
 
-    def _batch_generate(self, prompts: List[str], **kwargs: Any) -> List[str]:
+    def _batch_generate(self, prompts: t.List[str], **kwargs: t.Any) -> t.List[str]:
         """
         Base method to batch generate text from a list of prompts using multi-threading.
         Handles exceptions in _generate method.
@@ -137,121 +137,3 @@ class BaseLLMAPI(_BaseLLM):
             )
 
         return results
-
-
-@dc.dataclass
-class BaseOpenAI(BaseLLMAPI):
-    """
-    :param openai_api_base: The base URL for the OpenAI API.
-    :param openai_api_key: The API key to use for the OpenAI API.
-    :param model_name: The name of the model to use.
-    :param chat: Whether to use the chat API or the completion API. Defaults to False.
-    :param system_prompt: The prompt to use for the system.
-    :param user_role: The role to use for the user.
-    :param system_role: The role to use for the system.
-    {parent_doc}
-    """
-
-    __doc__ = __doc__.format(parent_doc=_BaseLLM.__doc__)
-
-    identifier: str = dc.field(default="")
-    openai_api_base: str = "https://api.openai.com/v1"
-    openai_api_key: Optional[str] = None
-    model_name: str = "gpt-3.5-turbo"
-    chat: bool = True
-    system_prompt: Optional[str] = None
-    user_role: str = "user"
-    system_role: str = "system"
-
-    def __post_init__(self, artifacts):
-        self.api_url = self.openai_api_base
-        self.identifier = self.identifier or self.model_name
-        super().__post_init__(artifacts)
-
-    def init(self):
-        try:
-            from openai import OpenAI
-        except ImportError:
-            raise Exception("You must install openai with command 'pip install openai'")
-
-        params = {
-            "api_key": self.openai_api_key,
-            "base_url": self.openai_api_base,
-        }
-
-        self.client = OpenAI(**params)
-        model_set = self.get_model_set()
-        assert (
-            self.model_name in model_set
-        ), f"model_name {self.model_name} is not in model_set {model_set}"
-
-    def get_model_set(self):
-        model_list = self.client.models.list()
-        return sorted({model.id for model in model_list.data})
-
-    def _generate(self, prompt: str, **kwargs: Any) -> str:
-        if self.chat:
-            return self._chat_generate(prompt, **kwargs)
-        else:
-            return self._prompt_generate(prompt, **kwargs)
-
-    def _prompt_generate(self, prompt: str, **kwargs: Any) -> str:
-        """
-        Generate a completion for a given prompt with prompt format.
-        """
-        completion = self.client.completions.create(
-            model=self.model_name,
-            prompt=prompt,
-            **self.get_kwargs(
-                self.client.completions.create, kwargs, self.predict_kwargs
-            ),
-        )
-        return completion.choices[0].text
-
-    def _chat_generate(self, content: str, **kwargs: Any) -> str:
-        """
-        Generate a completion for a given prompt with chat format.
-        :param prompt: The prompt to generate a completion for.
-        :param kwargs: Any additional arguments to pass to the API.
-        """
-        messages = kwargs.get("messages", [])
-
-        if self.system_prompt:
-            messages = [
-                {"role": self.system_role, "content": self.system_prompt}
-            ] + messages
-
-        messages.append({"role": self.user_role, "content": content})
-        completion = self.client.chat.completions.create(
-            messages=messages,
-            model=self.model_name,
-            **self.get_kwargs(
-                self.client.chat.completions.create, kwargs, self.predict_kwargs
-            ),
-        )
-        return completion.choices[0].message.content
-
-
-@dc.dataclass
-class BaseLLMModel(_BaseLLM):
-    """
-    :param model_name: The name of the model to use.
-    :param on_ray: Whether to run the model on Ray.
-    :param ray_config: The Ray config to use.
-    :param ray_addredd: The address of the ray cluster.
-    {parent_doc}
-    """
-
-    __doc__ = __doc__.format(parent_doc=_BaseLLM.__doc__)
-
-    identifier: str = dc.field(default="")
-    model_name: str = dc.field(default="")
-    on_ray: bool = False
-    ray_address: Optional[str] = None
-    ray_config: dict = dc.field(default_factory=dict)
-
-    def __post_init__(self, artifacts):
-        self.identifier = self.identifier or self.model_name
-        assert self.model_name, "model_name can not be empty"
-        self._is_initialized = False
-        super().__post_init__(artifacts)

superduperdb/ext/transformers/llm.py
@@ -19,16 +19,15 @@ from pinnacledb.backends.query_dataset import QueryDataset
 from pinnacledb.components.component import ensure_initialized
 from pinnacledb.components.datatype import DataType, dill_serializer
 from pinnacledb.components.model import _Fittable, _Validator
-from pinnacledb.ext.llm.base import _BaseLLM
-
-from .training import Checkpoint
+from pinnacledb.ext.llm.base import BaseLLM
+from pinnacledb.ext.transformers.llm_training import Checkpoint
 
 if typing.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
 
 
 @dc.dataclass(kw_only=True)
-class LLM(_BaseLLM, _Fittable, _Validator):
+class LLM(BaseLLM, _Fittable, _Validator):
     """
     LLM model based on `transformers` library.
 
@@ -232,7 +231,7 @@ class LLM(_BaseLLM, _Fittable, _Validator):
         Generate text.
         Can overwrite this method to support more inference methods.
         """
-        kwargs = kwargs.copy()
+        kwargs = {**self.predict_kwargs, **kwargs}
 
         # Set default values, if not will cause bad output
         kwargs.setdefault("add_special_tokens", True)

superduperdb/ext/vllm/__init__.py
@@ -1,0 +1,3 @@
+from pinnacledb.ext.vllm.model import VllmAPI, VllmModel
+
+__all__ = ["VllmAPI", "VllmModel"]

test/unittest/ext/test_vllm.py
@@ -5,9 +5,11 @@ from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predi
 import pytest
 import vcr
 
-from pinnacledb.ext.llm.openai import OpenAI
+from pinnacledb.ext.vllm import VllmAPI
 
-CASSETTE_DIR = "test/unittest/ext/cassettes/llm/openai"
+CASSETTE_DIR = "test/unittest/ext/cassettes/vllm"
+
+api_url = "http://localhost:8000/generate"
 
 
 @pytest.fixture
@@ -17,30 +19,30 @@ def openai_mock(monkeypatch):
 
 
 @vcr.use_cassette(
-    f"{CASSETTE_DIR}/test_predict.yaml",
+    f"{CASSETTE_DIR}/test_predict_api.yaml",
     filter_headers=["authorization"],
     record_on_exception=False,
-    ignore_localhost=True,
+    ignore_localhost=False,
 )
 @pytest.mark.parametrize(
     "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
 )
-def test_predict(db, openai_mock):
+def test_predict_api(db):
     """Test chat."""
-    check_predict(db, OpenAI(model_name="gpt-3.5-turbo"))
-    check_predict(
-        db, OpenAI(identifier="chat-llm", model_name="gpt-3.5-turbo", chat=True)
-    )
+    check_predict(db, VllmAPI(identifier="llm", api_url=api_url, max_batch_size=1))
 
 
 @vcr.use_cassette(
-    f"{CASSETTE_DIR}/test_llm_as_listener_model.yaml",
+    f"{CASSETTE_DIR}/test_llm_as_listener_model_api.yaml",
     filter_headers=["authorization"],
     record_on_exception=False,
-    ignore_localhost=True,
+    ignore_localhost=False,
 )
 @pytest.mark.parametrize(
     "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
 )
-def test_llm_as_listener_model(db, openai_mock):
-    check_llm_as_listener_model(db, OpenAI(model_name="gpt-3.5-turbo"))
+def test_llm_as_listener_model_api(db):
+    # vcr does not support multiple requests in a single test, set max_batch_size to 1
+    check_llm_as_listener_model(
+        db, VllmAPI(identifier="llm", api_url=api_url, max_batch_size=1)
+    )

test/unittest/ext/transformers/test_llm.py
@@ -1,0 +1,26 @@
+from test.db_config import DBConfig
+from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predict
+
+import pytest
+
+from pinnacledb.ext.transformers import LLM
+
+TEST_MODEL_NAME = "facebook/opt-125m"
+
+
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_predict(db):
+    """Test chat."""
+    model = LLM(identifier="llm", model_name_or_path=TEST_MODEL_NAME)
+
+    check_predict(db, model)
+
+
+@pytest.mark.parametrize(
+    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
+)
+def test_model_as_listener_model(db):
+    model = LLM(identifier="llm", model_name_or_path=TEST_MODEL_NAME)
+    check_llm_as_listener_model(db, model)

test/unittest/ext/transformers/test_llm_training.py
@@ -1,7 +1,6 @@
 import os
 import random
 from test.db_config import DBConfig
-from test.unittest.ext.llm.utils import check_llm_as_listener_model, check_predict
 
 import pytest
 
@@ -9,8 +8,8 @@ from pinnacledb.backends.mongodb.query import Collection
 from pinnacledb.base.document import Document
 from pinnacledb.components.dataset import Dataset
 from pinnacledb.components.metric import Metric
-from pinnacledb.ext.llm.model import LLM
-from pinnacledb.ext.llm.training import Checkpoint, LLMTrainer
+from pinnacledb.ext.transformers import LLM
+from pinnacledb.ext.transformers.llm_training import Checkpoint, LLMTrainer
 
 TEST_MODEL_NAME = "facebook/opt-125m"
 try:
@@ -21,24 +20,6 @@ except ImportError:
     datasets = None
     peft = None
     trl = None
-
-
-@pytest.mark.parametrize(
-    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
-)
-def test_predict(db):
-    """Test chat."""
-    model = LLM(identifier="llm", model_name_or_path=TEST_MODEL_NAME)
-
-    check_predict(db, model)
-
-
-@pytest.mark.parametrize(
-    "db", [DBConfig.mongodb_empty, DBConfig.sqldb_empty], indirect=True
-)
-def test_model_as_listener_model(db):
-    model = LLM(identifier="llm", model_name_or_path=TEST_MODEL_NAME)
-    check_llm_as_listener_model(db, model)
 
 
 @pytest.mark.skipif(