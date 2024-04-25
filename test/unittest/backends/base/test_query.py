superduper-io/superduper

Fixes for multimodal use case (#1984)
Co-authored-by: JieguangZhou <jieguang_zhou@163.com>
Co-authored-by: Duncan Blythe <duncan@superduperdb.com>
Co-authored-by: fazlulkarimweb <fazlulkarimrocky@gmail.com>

test/unittest/backends/base/test_query.py
@@ -1,8 +1,8 @@
-from superduperdb.backends.base.query import Predict
 from test.db_config import DBConfig
 
 import pytest
 
+from superduperdb.backends.base.query import Predict
 from superduperdb.backends.ibis.field_types import dtype
 from superduperdb.backends.ibis.query import Table
 from superduperdb.backends.mongodb.query import Collection
@@ -137,18 +137,18 @@ def test_model(db):
 
     m = db.load('model', 'linear_a')
 
-    result = m.predict_one(torch.randn(32))
+    m.predict_one(torch.randn(32))
 
     from superduperdb.backends.base.query import model
 
     t = torch.randn(32)
 
-    result1 = m.predict_one(t)
+    m.predict_one(t)
 
     q = model('linear_a').predict_one(t)
 
-    issubclass = isinstance(q, Predict)
+    isinstance(q, Predict)
 
-    result2 = db.execute(q)
+    db.execute(q)
     
     import pdb; pdb.set_trace()

test/unittest/base/test_document.py
@@ -1,10 +1,10 @@
 import dataclasses as dc
 import typing as t
+from test.db_config import DBConfig
 
 import pytest
-from superduperdb.ext.pillow.encoder import pil_image_hybrid_png
 
-from test.db_config import DBConfig
+from superduperdb.ext.pillow.encoder import pil_image_hybrid_png
 
 try:
     import torch

test/unittest/component/test_serialization.py
@@ -11,7 +11,7 @@ from test.db_config import DBConfig
 from sklearn.svm import SVC
 
 from superduperdb.components.model import ObjectModel
-from superduperdb.ext.sklearn.model import Estimator, SklearnTrainer
+from superduperdb.ext.sklearn.model import Estimator
 
 
 @pytest.mark.skipif(not torch, reason='Torch not installed')

test/unittest/ext/test_torch.py
@@ -7,13 +7,13 @@ try:
 except ImportError:
     torch = None
 
-from superduperdb.components.model import Validation
 from test.db_config import DBConfig
 
 from superduperdb.backends.mongodb.data_backend import MongoDataBackend
 from superduperdb.backends.mongodb.query import Collection
 from superduperdb.components.datatype import DataType
 from superduperdb.components.metric import Metric
+from superduperdb.components.model import Validation
 from superduperdb.ext.torch.training import TorchTrainer
 
 

Pinnacle-Networks/Pinnacle

test/unittest/backends/base/test_query.py
@@ -1,8 +1,8 @@
-from pinnacledb.backends.base.query import Predict
 from test.db_config import DBConfig
 
 import pytest
 
+from pinnacledb.backends.base.query import Predict
 from pinnacledb.backends.ibis.field_types import dtype
 from pinnacledb.backends.ibis.query import Table
 from pinnacledb.backends.mongodb.query import Collection
@@ -137,18 +137,18 @@ def test_model(db):
 
     m = db.load('model', 'linear_a')
 
-    result = m.predict_one(torch.randn(32))
+    m.predict_one(torch.randn(32))
 
     from pinnacledb.backends.base.query import model
 
     t = torch.randn(32)
 
-    result1 = m.predict_one(t)
+    m.predict_one(t)
 
     q = model('linear_a').predict_one(t)
 
-    issubclass = isinstance(q, Predict)
+    isinstance(q, Predict)
 
-    result2 = db.execute(q)
+    db.execute(q)
     
     import pdb; pdb.set_trace()

test/unittest/base/test_document.py
@@ -1,10 +1,10 @@
 import dataclasses as dc
 import typing as t
+from test.db_config import DBConfig
 
 import pytest
-from pinnacledb.ext.pillow.encoder import pil_image_hybrid_png
 
-from test.db_config import DBConfig
+from pinnacledb.ext.pillow.encoder import pil_image_hybrid_png
 
 try:
     import torch

test/unittest/component/test_serialization.py
@@ -11,7 +11,7 @@ from test.db_config import DBConfig
 from sklearn.svm import SVC
 
 from pinnacledb.components.model import ObjectModel
-from pinnacledb.ext.sklearn.model import Estimator, SklearnTrainer
+from pinnacledb.ext.sklearn.model import Estimator
 
 
 @pytest.mark.skipif(not torch, reason='Torch not installed')

test/unittest/ext/test_torch.py
@@ -7,13 +7,13 @@ try:
 except ImportError:
     torch = None
 
-from pinnacledb.components.model import Validation
 from test.db_config import DBConfig
 
 from pinnacledb.backends.mongodb.data_backend import MongoDataBackend
 from pinnacledb.backends.mongodb.query import Collection
 from pinnacledb.components.datatype import DataType
 from pinnacledb.components.metric import Metric
+from pinnacledb.components.model import Validation
 from pinnacledb.ext.torch.training import TorchTrainer
 
 