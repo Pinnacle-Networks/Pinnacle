superduper-io/superduper

Fix pandas support

superduperdb/backends/ibis/query.py
@@ -22,6 +22,7 @@ from superduperdb.backends.base.query import (
 from superduperdb.backends.ibis.cursor import SuperDuperIbisResult
 from superduperdb.backends.ibis.field_types import dtype
 from superduperdb.base.document import Document
+from superduperdb.base.exceptions import DatabackendException
 from superduperdb.components.component import Component
 from superduperdb.components.datatype import DataType
 from superduperdb.components.schema import Schema
@@ -100,7 +101,7 @@ def _model_update_impl(
     db.databackend.insert(f'_outputs.{predict_id}', table_records)
 
 
-class IbisBackendError(Exception):
+class IbisBackendError(DatabackendException):
     """
     This error represents ibis query related errors
     i.e when there is an error while executing an ibis query,
@@ -215,7 +216,11 @@ class IbisCompoundSelect(CompoundSelect):
         if tables is None:
             tables = {}
         if table_id not in tables:
-            tables[table_id] = db.databackend.conn.table(table_id)
+            try:
+                tables[table_id] = db.databackend.conn.table(table_id)
+            except KeyError:
+                tables[table_id] = db.databackend.in_memory_tables[table_id]
+
         return self.query_linker.compile(db, tables=tables)
 
     def get_all_tables(self):
@@ -646,8 +651,14 @@ class Table(Component):
         for e in self.schema.encoders:
             db.add(e)
         if db.databackend.in_memory:
-            logging.info(f'Using in-memory tables "{self.identifier}" so doing nothing')
-            return
+            if '_outputs' in self.identifier:
+                db.databackend.in_memory_tables[
+                    self.identifier
+                ] = db.databackend.create_table_and_schema(
+                    self.identifier, self.schema.raw
+                )
+
+                return
 
         try:
             db.databackend.create_table_and_schema(self.identifier, self.schema.raw)

superduperdb/components/model.py
@@ -20,6 +20,7 @@ from superduperdb.backends.ibis.query import IbisCompoundSelect, Table
 from superduperdb.backends.query_dataset import CachedQueryDataset, QueryDataset
 from superduperdb.base.code import Code
 from superduperdb.base.document import Document
+from superduperdb.base.exceptions import DatabackendException
 from superduperdb.base.serializable import Serializable, Variable
 from superduperdb.components.component import Component, ensure_initialized
 from superduperdb.components.datatype import DataType, dill_lazy
@@ -537,9 +538,16 @@ class Model(Component):
             id_field = db.databackend.id_field
         except AttributeError:
             id_field = query.table_or_collection.primary_id
-        for r in tqdm.tqdm(db.execute(query)):
-            predict_ids.append(str(r[id_field]))
 
+        # TODO: Find better solution to support in-memory (pandas)
+        # Since pandas has a bug, it cannot join on empty table.
+        try:
+            id_curr = db.execute(query)
+        except DatabackendException:
+            id_curr = db.execute(select.select(id_field))
+
+        for r in tqdm.tqdm(id_curr):
+            predict_ids.append(str(r[id_field]))
         return predict_ids
 
     def predict_in_db(

test/integration/backends/ibis/test_end_2_end.py
@@ -1,18 +1,13 @@
-import tempfile
+import os
 
-import ibis
 import PIL.Image
 import pytest
 import torch.nn
 import torchvision
 
 from superduperdb import CFG
-from superduperdb.backends.ibis.data_backend import IbisDataBackend
 from superduperdb.backends.ibis.field_types import dtype
 from superduperdb.backends.ibis.query import RawSQL, Table
-from superduperdb.backends.local.artifacts import FileSystemArtifactStore
-from superduperdb.backends.sqlalchemy.metadata import SQLAlchemyMetadata
-from superduperdb.base.datalayer import Datalayer
 from superduperdb.base.document import Document as D
 from superduperdb.components.listener import Listener
 from superduperdb.components.schema import Schema
@@ -23,50 +18,28 @@ from superduperdb.ext.torch.model import TorchModel
 DO_SKIP = CFG.data_backend.startswith('mongo')
 
 
-@pytest.fixture
-def sqllite_conn():
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        tmp_db = f'{tmp_dir}/mydb.sqlite'
-        yield ibis.connect('sqlite://' + str(tmp_db)), tmp_dir
+from superduperdb import superduper
 
 
 @pytest.fixture
-def duckdb_conn():
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        tmp_db = f'{tmp_dir}/mydb.ddb'
-        yield ibis.connect('duckdb://' + str(tmp_db)), tmp_dir
+def clean_cache():
+    directory = './deploy/testenv/cache'
 
-
-@pytest.fixture
-def test_db(sqllite_conn):
-    connection, tmp_dir = sqllite_conn
-    yield make_ibis_db(connection, connection, tmp_dir)
-
-
-@pytest.fixture
-def ibis_duckdb(duckdb_conn):
-    connection, tmp_dir = duckdb_conn
-    yield make_ibis_db(connection, connection, tmp_dir)
-
-
-@pytest.fixture
-def ibis_pandas_db(sqllite_conn):
-    metadata_connection, tmp_dir = sqllite_conn
-    connection = ibis.pandas.connect({})
-    yield make_ibis_db(connection, metadata_connection, tmp_dir, in_memory=True)
-
-
-def make_ibis_db(db_conn, metadata_conn, tmp_dir, in_memory=False):
-    return Datalayer(
-        databackend=IbisDataBackend(conn=db_conn, name='ibis', in_memory=in_memory),
-        metadata=SQLAlchemyMetadata(conn=metadata_conn.con, name='ibis'),
-        artifact_store=FileSystemArtifactStore(conn=tmp_dir, name='ibis'),
-    )
+    for file in os.listdir(directory):
+        file_path = os.path.join(directory, file)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
 
 @pytest.mark.skipif(DO_SKIP, reason="skipping ibis tests if mongodb")
-def test_end_2_end(test_db, memory_table=False):
-    db = test_db
+def test_end_2_end(clean_cache):
+    memory_table = False
+    if CFG.data_backend.startswith('duckdb') or CFG.data_backend.endswith('csv'):
+        memory_table = True
+    _end_2_end(superduper(), memory_table=memory_table)
+
+
+def _end_2_end(db, memory_table=False):
     schema = Schema(
         identifier='my_table',
         fields={
@@ -196,9 +169,13 @@ def test_end_2_end(test_db, memory_table=False):
         ]
 
 
-def test_nested_query(test_db):
-    db = test_db
+@pytest.mark.skipif(DO_SKIP, reason="skipping ibis tests if mongodb")
+def test_nested_query(clean_cache):
+    db = superduper()
 
+    memory_table = False
+    if CFG.data_backend.endswith('csv'):
+        memory_table = True
     schema = Schema(
         identifier='my_table',
         fields={
@@ -217,4 +194,13 @@ def test_nested_query(test_db):
 
     expr_, _ = q.compile(db)
 
-    assert 'SELECT t0.id, t0.health, t0.age, t0.image, t0._fold' in str(expr_.compile())
+    if not memory_table:
+        assert 'SELECT t0.id, t0.health, t0.age, t0.image, t0._fold' in str(
+            expr_.compile()
+        )
+    else:
+        assert 'Selection[r0]\n  predicates:\n    r0.age > 10' in str(expr_.compile())
+        assert (
+            'my_table\n  id     int64\n  health int32\n  age    '
+            'int32\n  image  binary\n  _fold  string' in str(expr_.compile())
+        )

Pinnacle-Networks/Pinnacle

superduperdb/backends/ibis/query.py
@@ -22,6 +22,7 @@ from pinnacledb.backends.base.query import (
 from pinnacledb.backends.ibis.cursor import SuperDuperIbisResult
 from pinnacledb.backends.ibis.field_types import dtype
 from pinnacledb.base.document import Document
+from pinnacledb.base.exceptions import DatabackendException
 from pinnacledb.components.component import Component
 from pinnacledb.components.datatype import DataType
 from pinnacledb.components.schema import Schema
@@ -100,7 +101,7 @@ def _model_update_impl(
     db.databackend.insert(f'_outputs.{predict_id}', table_records)
 
 
-class IbisBackendError(Exception):
+class IbisBackendError(DatabackendException):
     """
     This error represents ibis query related errors
     i.e when there is an error while executing an ibis query,
@@ -215,7 +216,11 @@ class IbisCompoundSelect(CompoundSelect):
         if tables is None:
             tables = {}
         if table_id not in tables:
-            tables[table_id] = db.databackend.conn.table(table_id)
+            try:
+                tables[table_id] = db.databackend.conn.table(table_id)
+            except KeyError:
+                tables[table_id] = db.databackend.in_memory_tables[table_id]
+
         return self.query_linker.compile(db, tables=tables)
 
     def get_all_tables(self):
@@ -646,8 +651,14 @@ class Table(Component):
         for e in self.schema.encoders:
             db.add(e)
         if db.databackend.in_memory:
-            logging.info(f'Using in-memory tables "{self.identifier}" so doing nothing')
-            return
+            if '_outputs' in self.identifier:
+                db.databackend.in_memory_tables[
+                    self.identifier
+                ] = db.databackend.create_table_and_schema(
+                    self.identifier, self.schema.raw
+                )
+
+                return
 
         try:
             db.databackend.create_table_and_schema(self.identifier, self.schema.raw)

superduperdb/components/model.py
@@ -20,6 +20,7 @@ from pinnacledb.backends.ibis.query import IbisCompoundSelect, Table
 from pinnacledb.backends.query_dataset import CachedQueryDataset, QueryDataset
 from pinnacledb.base.code import Code
 from pinnacledb.base.document import Document
+from pinnacledb.base.exceptions import DatabackendException
 from pinnacledb.base.serializable import Serializable, Variable
 from pinnacledb.components.component import Component, ensure_initialized
 from pinnacledb.components.datatype import DataType, dill_lazy
@@ -537,9 +538,16 @@ class Model(Component):
             id_field = db.databackend.id_field
         except AttributeError:
             id_field = query.table_or_collection.primary_id
-        for r in tqdm.tqdm(db.execute(query)):
-            predict_ids.append(str(r[id_field]))
 
+        # TODO: Find better solution to support in-memory (pandas)
+        # Since pandas has a bug, it cannot join on empty table.
+        try:
+            id_curr = db.execute(query)
+        except DatabackendException:
+            id_curr = db.execute(select.select(id_field))
+
+        for r in tqdm.tqdm(id_curr):
+            predict_ids.append(str(r[id_field]))
         return predict_ids
 
     def predict_in_db(

test/integration/backends/ibis/test_end_2_end.py
@@ -1,18 +1,13 @@
-import tempfile
+import os
 
-import ibis
 import PIL.Image
 import pytest
 import torch.nn
 import torchvision
 
 from pinnacledb import CFG
-from pinnacledb.backends.ibis.data_backend import IbisDataBackend
 from pinnacledb.backends.ibis.field_types import dtype
 from pinnacledb.backends.ibis.query import RawSQL, Table
-from pinnacledb.backends.local.artifacts import FileSystemArtifactStore
-from pinnacledb.backends.sqlalchemy.metadata import SQLAlchemyMetadata
-from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.base.document import Document as D
 from pinnacledb.components.listener import Listener
 from pinnacledb.components.schema import Schema
@@ -23,50 +18,28 @@ from pinnacledb.ext.torch.model import TorchModel
 DO_SKIP = CFG.data_backend.startswith('mongo')
 
 
-@pytest.fixture
-def sqllite_conn():
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        tmp_db = f'{tmp_dir}/mydb.sqlite'
-        yield ibis.connect('sqlite://' + str(tmp_db)), tmp_dir
+from pinnacledb import pinnacle
 
 
 @pytest.fixture
-def duckdb_conn():
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        tmp_db = f'{tmp_dir}/mydb.ddb'
-        yield ibis.connect('duckdb://' + str(tmp_db)), tmp_dir
+def clean_cache():
+    directory = './deploy/testenv/cache'
 
-
-@pytest.fixture
-def test_db(sqllite_conn):
-    connection, tmp_dir = sqllite_conn
-    yield make_ibis_db(connection, connection, tmp_dir)
-
-
-@pytest.fixture
-def ibis_duckdb(duckdb_conn):
-    connection, tmp_dir = duckdb_conn
-    yield make_ibis_db(connection, connection, tmp_dir)
-
-
-@pytest.fixture
-def ibis_pandas_db(sqllite_conn):
-    metadata_connection, tmp_dir = sqllite_conn
-    connection = ibis.pandas.connect({})
-    yield make_ibis_db(connection, metadata_connection, tmp_dir, in_memory=True)
-
-
-def make_ibis_db(db_conn, metadata_conn, tmp_dir, in_memory=False):
-    return Datalayer(
-        databackend=IbisDataBackend(conn=db_conn, name='ibis', in_memory=in_memory),
-        metadata=SQLAlchemyMetadata(conn=metadata_conn.con, name='ibis'),
-        artifact_store=FileSystemArtifactStore(conn=tmp_dir, name='ibis'),
-    )
+    for file in os.listdir(directory):
+        file_path = os.path.join(directory, file)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
 
 @pytest.mark.skipif(DO_SKIP, reason="skipping ibis tests if mongodb")
-def test_end_2_end(test_db, memory_table=False):
-    db = test_db
+def test_end_2_end(clean_cache):
+    memory_table = False
+    if CFG.data_backend.startswith('duckdb') or CFG.data_backend.endswith('csv'):
+        memory_table = True
+    _end_2_end(pinnacle(), memory_table=memory_table)
+
+
+def _end_2_end(db, memory_table=False):
     schema = Schema(
         identifier='my_table',
         fields={
@@ -196,9 +169,13 @@ def test_end_2_end(test_db, memory_table=False):
         ]
 
 
-def test_nested_query(test_db):
-    db = test_db
+@pytest.mark.skipif(DO_SKIP, reason="skipping ibis tests if mongodb")
+def test_nested_query(clean_cache):
+    db = pinnacle()
 
+    memory_table = False
+    if CFG.data_backend.endswith('csv'):
+        memory_table = True
     schema = Schema(
         identifier='my_table',
         fields={
@@ -217,4 +194,13 @@ def test_nested_query(test_db):
 
     expr_, _ = q.compile(db)
 
-    assert 'SELECT t0.id, t0.health, t0.age, t0.image, t0._fold' in str(expr_.compile())
+    if not memory_table:
+        assert 'SELECT t0.id, t0.health, t0.age, t0.image, t0._fold' in str(
+            expr_.compile()
+        )
+    else:
+        assert 'Selection[r0]\n  predicates:\n    r0.age > 10' in str(expr_.compile())
+        assert (
+            'my_table\n  id     int64\n  health int32\n  age    '
+            'int32\n  image  binary\n  _fold  string' in str(expr_.compile())
+        )