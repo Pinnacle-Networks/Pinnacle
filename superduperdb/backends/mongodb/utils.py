superduper-io/superduper

Fixed database permission issues when connecting to mongodb

superduperdb/backends/mongodb/utils.py
@@ -1,0 +1,57 @@
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+
+from superduperdb import logging
+from superduperdb.misc.anonymize import anonymize_url
+
+
+def get_avaliable_conn(uri: str, **kwargs):
+    """
+    Get an available connection to the database.
+    This can avoid some issues with database permission verification.
+    1. Try to connect to the database with the given URI.
+    2. Try to connect to the database with the base URI without database name.
+
+    :param uri: The URI of the database.
+    """
+    base_uri, db_name = uri.rsplit("/", 1)
+    kwargs.setdefault("serverSelectionTimeoutMS", 5000)
+
+    raise_e = None
+
+    # Try to connect to the database with the given URI
+    client: MongoClient = MongoClient(uri, **kwargs)
+    try:
+        client[db_name].list_collection_names()
+        return client
+    except ServerSelectionTimeoutError as e:
+        # If the server is not available, raise the exception
+        raise e
+    except Exception as e:
+        uri_mask = anonymize_url(uri)
+        logging.warn(
+            f"Failed to connect to the database with the given URI: {uri_mask}"
+        )
+        logging.error(str(e))
+        raise_e = e
+        client.close()
+
+    # Try to connect to the database with base URI without database name
+    client = MongoClient(base_uri, **kwargs)
+    base_uri_mask = anonymize_url(base_uri)
+    try:
+        logging.info(
+            f"Trying to connect to the database with the base URI: {base_uri_mask}"
+        )
+        client[db_name].list_collection_names()
+        return client
+    except Exception as e:
+        logging.warn(
+            f"Failed to connect to the database with the base URI: {base_uri_mask}"
+        )
+        logging.error(str(e))
+        client.close()
+
+    if raise_e:
+        logging.error("Failed to connect to the database")
+        raise raise_e

superduperdb/base/build.py
@@ -17,6 +17,7 @@ from superduperdb.backends.base.data_backend import BaseDataBackend
 from superduperdb.backends.local.artifacts import FileSystemArtifactStore
 from superduperdb.backends.local.compute import LocalComputeBackend
 from superduperdb.backends.mongodb.artifacts import MongoArtifactStore
+from superduperdb.backends.mongodb.utils import get_avaliable_conn
 from superduperdb.backends.ray.compute import RayComputeBackend
 from superduperdb.base.datalayer import Datalayer
 from superduperdb.misc.anonymize import anonymize_url
@@ -100,12 +101,10 @@ def _build_artifact_store(
 def _build_databackend_impl(uri, mapping, type: str = 'data_backend'):
     logging.debug(f"Parsing data connection URI:{uri}")
 
+    # TODO: Should we move the following code to the DataBackend classes?
     if re.match('^mongodb:\/\/', uri) is not None:
         name = uri.split('/')[-1]
-        conn: pymongo.MongoClient = pymongo.MongoClient(
-            uri,
-            serverSelectionTimeoutMS=5000,
-        )
+        conn = get_avaliable_conn(uri, serverSelectionTimeoutMS=5000)
         return mapping['mongodb'](conn, name)
 
     elif re.match('^mongodb\+srv:\/\/', uri):

Pinnacle-Networks/Pinnacle

superduperdb/backends/mongodb/utils.py
@@ -1,0 +1,57 @@
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
+
+from pinnacledb import logging
+from pinnacledb.misc.anonymize import anonymize_url
+
+
+def get_avaliable_conn(uri: str, **kwargs):
+    """
+    Get an available connection to the database.
+    This can avoid some issues with database permission verification.
+    1. Try to connect to the database with the given URI.
+    2. Try to connect to the database with the base URI without database name.
+
+    :param uri: The URI of the database.
+    """
+    base_uri, db_name = uri.rsplit("/", 1)
+    kwargs.setdefault("serverSelectionTimeoutMS", 5000)
+
+    raise_e = None
+
+    # Try to connect to the database with the given URI
+    client: MongoClient = MongoClient(uri, **kwargs)
+    try:
+        client[db_name].list_collection_names()
+        return client
+    except ServerSelectionTimeoutError as e:
+        # If the server is not available, raise the exception
+        raise e
+    except Exception as e:
+        uri_mask = anonymize_url(uri)
+        logging.warn(
+            f"Failed to connect to the database with the given URI: {uri_mask}"
+        )
+        logging.error(str(e))
+        raise_e = e
+        client.close()
+
+    # Try to connect to the database with base URI without database name
+    client = MongoClient(base_uri, **kwargs)
+    base_uri_mask = anonymize_url(base_uri)
+    try:
+        logging.info(
+            f"Trying to connect to the database with the base URI: {base_uri_mask}"
+        )
+        client[db_name].list_collection_names()
+        return client
+    except Exception as e:
+        logging.warn(
+            f"Failed to connect to the database with the base URI: {base_uri_mask}"
+        )
+        logging.error(str(e))
+        client.close()
+
+    if raise_e:
+        logging.error("Failed to connect to the database")
+        raise raise_e

superduperdb/base/build.py
@@ -17,6 +17,7 @@ from pinnacledb.backends.base.data_backend import BaseDataBackend
 from pinnacledb.backends.local.artifacts import FileSystemArtifactStore
 from pinnacledb.backends.local.compute import LocalComputeBackend
 from pinnacledb.backends.mongodb.artifacts import MongoArtifactStore
+from pinnacledb.backends.mongodb.utils import get_avaliable_conn
 from pinnacledb.backends.ray.compute import RayComputeBackend
 from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.misc.anonymize import anonymize_url
@@ -100,12 +101,10 @@ def _build_artifact_store(
 def _build_databackend_impl(uri, mapping, type: str = 'data_backend'):
     logging.debug(f"Parsing data connection URI:{uri}")
 
+    # TODO: Should we move the following code to the DataBackend classes?
     if re.match('^mongodb:\/\/', uri) is not None:
         name = uri.split('/')[-1]
-        conn: pymongo.MongoClient = pymongo.MongoClient(
-            uri,
-            serverSelectionTimeoutMS=5000,
-        )
+        conn = get_avaliable_conn(uri, serverSelectionTimeoutMS=5000)
         return mapping['mongodb'](conn, name)
 
     elif re.match('^mongodb\+srv:\/\/', uri):