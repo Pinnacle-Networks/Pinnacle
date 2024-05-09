superduper-io/superduper

Auto-infer Schema from data

superduperdb/backends/ibis/query.py
@@ -26,6 +26,7 @@ from superduperdb.base.exceptions import DatabackendException
 from superduperdb.components.component import Component
 from superduperdb.components.datatype import DataType
 from superduperdb.components.schema import Schema
+from superduperdb.misc.auto_schema import infer_schema
 
 if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
@@ -667,6 +668,17 @@ class Table(Component):
                 pass
             else:
                 raise e
+
+    @staticmethod
+    def infer_schema(data: t.Mapping[str, t.Any], identifier: t.Optional[str] = None):
+        """
+        Infer a schema from a given data object
+
+        :param data: The data object
+        :param identifier: The identifier for the schema, if None, it will be generated
+        :return: The inferred schema
+        """
+        return infer_schema(data, identifier=identifier, ibis=True)
 
     @property
     def table_or_collection(self):

superduperdb/backends/mongodb/query.py
@@ -23,6 +23,7 @@ from superduperdb.backends.base.query import (
 from superduperdb.base.cursor import SuperDuperCursor
 from superduperdb.base.document import Document
 from superduperdb.components.schema import Schema
+from superduperdb.misc.auto_schema import infer_schema
 from superduperdb.misc.files import load_uris
 
 SCHEMA_KEY = '_schema'
@@ -955,6 +956,17 @@ class Collection(TableOrCollection):
                 db.execute(
                     collection.insert_many([Document(**doc) for doc in bulk_writes])
                 )
+
+    @staticmethod
+    def infer_schema(data: t.Mapping[str, t.Any], identifier: t.Optional[str] = None):
+        """
+        Infer a schema from a given data object
+
+        :param data: The data object
+        :param identifier: The identifier for the schema, if None, it will be generated
+        :return: The inferred schema
+        """
+        return infer_schema(data, identifier)
 
 
 def _get_decode_function(db) -> t.Callable[[t.Any], t.Any]:

superduperdb/components/datatype.py
@@ -3,6 +3,7 @@ import dataclasses as dc
 import hashlib
 import inspect
 import io
+import json
 import os
 import pickle
 import re
@@ -25,6 +26,19 @@ from superduperdb.misc.hash import random_sha1
 
 Decode = t.Callable[[bytes], t.Any]
 Encode = t.Callable[[t.Any], bytes]
+
+
+class IntermidiaType:
+    BYTES = 'bytes'
+    STRING = 'string'
+
+
+def json_encode(object: t.Any, info: t.Optional[t.Dict] = None) -> str:
+    return json.dumps(object)
+
+
+def json_decode(b: str, info: t.Optional[t.Dict] = None) -> t.Any:
+    return json.loads(b)
 
 
 def pickle_encode(object: t.Any, info: t.Optional[t.Dict] = None) -> bytes:
@@ -116,7 +130,7 @@ def torch_decode(b: bytes, info: t.Optional[t.Dict] = None) -> t.Any:
     return torch.load(io.BytesIO(b))
 
 
-def to_base64(bytes):
+def bytes_to_base64(bytes):
     """
     Converts bytes to base64.
 
@@ -125,7 +139,7 @@ def to_base64(bytes):
     return base64.b64encode(bytes).decode('utf-8')
 
 
-def from_base64(encoded):
+def base64_to_bytes(encoded):
     """
     Decodes a base64 encoded string.
 
@@ -190,6 +204,7 @@ class DataType(Component):
     directory: t.Optional[str] = None
     encodable: str = 'encodable'
     bytes_encoding: t.Optional[str] = CFG.bytes_encoding
+    intermidia_type: t.Optional[str] = IntermidiaType.BYTES
     media_type: t.Optional[str] = None
 
     def __post_init__(self, artifacts):
@@ -252,24 +267,21 @@ class DataType(Component):
         return self.decoder(item, info=info)
 
     def bytes_encoding_after_encode(self, data):
-        """
-        Convert the encoded data to the specified bytes encoding format.
-
-        :param data: The encoded data.
-        """
-        if self.bytes_encoding == BytesEncoding.BASE64:
-            return to_base64(data)
+        # Only encode to base64 if the data is bytes and the intermidia type is bytes
+        if (
+            self.bytes_encoding == BytesEncoding.BASE64
+            and self.intermidia_type == IntermidiaType.BYTES
+        ):
+            return bytes_to_base64(data)
         return data
 
     def bytes_encoding_before_decode(self, data):
-        """
-        Convert the encoded data from the specified bytes encoding format
-        before decoding.
-
-        :param data: The encoded data in the specified bytes encoding format.
-        """
-        if self.bytes_encoding == BytesEncoding.BASE64:
-            return from_base64(data)
+        # Only decode from base64 if the data is bytes and the intermidia type is bytes
+        if (
+            self.bytes_encoding == BytesEncoding.BASE64
+            and self.intermidia_type == IntermidiaType.BYTES
+        ):
+            return base64_to_bytes(data)
         return data
 
 
@@ -507,18 +519,7 @@ class Encodable(_BaseEncodable):
     def _get_object(cls, db, r):
         if r.get('bytes') is None:
             return None
-        if db is None:
-            try:
-                from superduperdb.components.datatype import serializers
-
-                datatype = serializers[r['datatype']]
-            except KeyError:
-                raise Exception(
-                    f'You specified a serializer which doesn\'t have a'
-                    f' default value: {r["datatype"]}'
-                )
-        else:
-            datatype = db.datatypes[r['datatype']]
+        datatype = cls.get_datatype(db, r)
         object = datatype.decode_data(r['bytes'], info=datatype.info)
         return object
 
@@ -574,10 +575,26 @@ class Encodable(_BaseEncodable):
         object = cls._get_object(db, r['_content'])
         return cls(
             x=object,
-            datatype=db.datatypes[r['_content']['datatype']],
+            datatype=cls.get_datatype(db, r['_content']),
             uri=r['_content']['uri'],
             file_id=r['_content'].get('file_id'),
         )
+
+    @classmethod
+    def get_datatype(cls, db, r):
+        if db is None:
+            try:
+                from superduperdb.components.datatype import serializers
+
+                datatype = serializers[r['datatype']]
+            except KeyError:
+                raise Exception(
+                    f'You specified a serializer which doesn\'t have a'
+                    f' default value: {r["datatype"]}'
+                )
+        else:
+            datatype = db.datatypes[r['datatype']]
+        return datatype
 
 
 @dc.dataclass
@@ -907,6 +924,14 @@ _ENCODABLES = {
     'lazy_file': LazyFile,
 }
 
+json_serializer = DataType(
+    'json',
+    encoder=json_encode,
+    decoder=json_decode,
+    encodable='encodable',
+    bytes_encoding=BytesEncoding.BASE64,
+    intermidia_type=IntermidiaType.STRING,
+)
 
 pickle_serializer = DataType(
     'pickle',
@@ -919,6 +944,12 @@ pickle_lazy = DataType(
     encoder=pickle_encode,
     decoder=pickle_decode,
     encodable='lazy_artifact',
+)
+dill_base = DataType(
+    'dill_base',
+    encoder=dill_encode,
+    decoder=dill_decode,
+    encodable='encodable',
 )
 dill_serializer = DataType(
     'dill',
@@ -953,6 +984,7 @@ file_lazy = DataType(
 serializers = {
     'pickle': pickle_serializer,
     'dill': dill_serializer,
+    'dill_base': dill_base,
     'torch': torch_serializer,
     'file': file_serializer,
     'pickle_lazy': pickle_lazy,

superduperdb/misc/auto_schema.py
@@ -1,0 +1,117 @@
+import json
+import typing as t
+
+import numpy as np
+
+from superduperdb import logging
+from superduperdb.components.datatype import (
+    DataType,
+    dill_base,
+    json_serializer,
+    serializers,
+)
+from superduperdb.components.schema import Schema
+from superduperdb.ext.numpy.encoder import array
+
+try:
+    import torch
+
+    torch_cls = (torch.Tensor, torch.nn.Module)
+except ImportError:
+    torch_cls = ()
+
+_BASE_TYPES = (
+    int,
+    str,
+    float,
+    bool,
+    bytes,
+    bytearray,
+)
+
+
+def infer_datatype(data: t.Any, ibis=False) -> t.Optional[DataType]:
+    """
+    Infer the datatype of a given data object
+    If the data object is a base type, return None,
+    Otherwise, return the inferred datatype
+
+    :param data: The data object
+    """
+
+    datatype = dill_base
+
+    if isinstance(data, _BASE_TYPES):
+        if not ibis:
+            datatype = None
+        else:
+            from superduperdb.backends.ibis.field_types import dtype
+
+            datatype = dtype(type(data))
+
+    elif isinstance(data, np.ndarray):
+        dtype = data.dtype.name
+        assert isinstance(dtype, str), f"Expected dtype to be a string, got {dtype}"
+        shape = tuple(data.shape)
+        datatype = array(dtype, shape)
+        serializers[datatype.identifier] = datatype
+
+    elif isinstance(data, torch_cls):
+        import torch
+
+        if isinstance(data, torch.Tensor):
+            from superduperdb.ext.torch.encoder import tensor
+
+            dtype = data.dtype
+            shape = data.shape
+            datatype = tensor(dtype, shape)
+            serializers[datatype.identifier] = datatype
+        else:
+            datatype = serializers["torch"]
+
+    elif isinstance(data, dict):
+        try:
+            json.dumps(data)
+            # Only use json_serializer when using ibis.
+            if ibis:
+                datatype = json_serializer
+                serializers[datatype.identifier] = datatype
+            else:
+                datatype = None
+        except Exception:
+            pass
+
+    if datatype is not None:
+        logging.info(f"Inferred datatype: {datatype} for data: {data}")
+
+    return datatype
+
+
+def infer_schema(
+    data: t.Mapping[str, t.Any],
+    identifier: t.Optional[str] = None,
+    ibis=False,
+) -> Schema:
+    """
+    Infer a schema from a given data object
+
+    :param data: The data object
+    :param identifier: The identifier for the schema, if None, it will be generated
+    :return: The inferred schema
+    """
+
+    assert isinstance(data, dict), "Data must be a dictionary"
+
+    schema_data = {}
+    for k, v in data.items():
+        data_type = infer_datatype(v, ibis)
+        if data_type is not None:
+            schema_data[k] = data_type
+
+    if identifier is None:
+        key_value_pairs = sorted(schema_data.items())
+        id_ = "&".join([f"{k}={v.identifier}" for k, v in key_value_pairs])
+
+        identifier = f"schema_{id_}"
+
+    return Schema(identifier=identifier, fields=schema_data)

test/unittest/misc/test_auto_schema.py
@@ -1,0 +1,129 @@
+from test.db_config import DBConfig
+
+import numpy as np
+import pandas as pd
+import pytest
+import torch
+
+from superduperdb.backends.ibis.query import Table
+from superduperdb.backends.mongodb.query import Collection
+from superduperdb.base.document import Document
+from superduperdb.misc.auto_schema import infer_datatype, infer_schema
+
+
+@pytest.fixture
+def data():
+    data = {
+        "int": 1,
+        "float": 1.0,
+        "str": "1",
+        "bool": True,
+        "bytes": b"1",
+        "np_array": np.array([1, 2, 3]),
+        "torch_tensor": torch.tensor([1, 2, 3]),
+        "dict": {"a": 1, "b": "2", "c": {"d": 3}},
+        "dict_np_array": {"a": np.array([1, 2, 3])},
+        "df": pd.DataFrame({"col1": [1, 2], "col2": [3, 4]}),
+    }
+    return data
+
+
+def test_infer_datatype():
+    assert infer_datatype(1) is None
+    assert infer_datatype(1.0) is None
+    assert infer_datatype("1") is None
+    assert infer_datatype(True) is None
+    assert infer_datatype(b"1") is None
+
+    assert infer_datatype(np.array([1, 2, 3])).identifier == "numpy.int64[3]"
+    assert infer_datatype(torch.tensor([1, 2, 3])).identifier == "torch.int64[3]"
+
+    assert infer_datatype({"a": 1}) is None
+    assert infer_datatype({"a": 1}, ibis=True).identifier == "json"
+
+    assert infer_datatype({"a": np.array([1, 2, 3])}).identifier == "dill_base"
+
+    assert (
+        infer_datatype(pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})).identifier
+        == "dill_base"
+    )
+
+
+def test_infer_schema_mongo(data):
+    schema = infer_schema(data)
+    encode_data = schema(data)
+
+    expected_datatypes = [
+        "np_array",
+        "torch_tensor",
+        "dict_np_array",
+        "df",
+    ]
+    assert sorted(schema.encoded_types) == sorted(expected_datatypes)
+    for key in expected_datatypes:
+        if schema.fields.get(key) is not None:
+            assert str(data[key]) != str(encode_data[key])
+
+    decode_data = Document(schema.decode_data(encode_data)).unpack()
+    for key in data:
+        assert isinstance(data[key], type(decode_data[key]))
+        assert str(data[key]) == str(decode_data[key])
+
+
+def test_infer_schema_ibis(data):
+    schema = infer_schema(data, ibis=True)
+    encode_data = schema(data)
+
+    expected_datatypes = [
+        "np_array",
+        "torch_tensor",
+        "dict",
+        "dict_np_array",
+        "df",
+    ]
+    assert sorted(schema.encoded_types) == sorted(expected_datatypes)
+    for key in expected_datatypes:
+        if schema.fields.get(key) is not None:
+            assert str(data[key]) != str(encode_data[key])
+
+    decode_data = Document(schema.decode_data(encode_data)).unpack()
+    for key in data:
+        assert isinstance(data[key], type(decode_data[key]))
+        assert str(data[key]) == str(decode_data[key])
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_mongo_schema(db, data):
+    collection_name = "documents"
+    schema = Collection.infer_schema(data)
+
+    db.apply(schema)
+
+    db.execute(
+        Collection(collection_name).insert_one(
+            Document(data), schema=schema.identifier
+        ),
+    )
+    collection = Collection(collection_name)
+    decode_data = collection.find_one().execute(db).unpack()
+    for key in data:
+        assert isinstance(data[key], type(decode_data[key]))
+        assert str(data[key]) == str(decode_data[key])
+
+
+@pytest.mark.parametrize("db", [DBConfig.sqldb_empty], indirect=True)
+def test_ibis_schema(db, data):
+    data["id"] = str("1")
+    schema = Table.infer_schema(data)
+
+    t = Table(identifier="my_table", schema=schema)
+
+    db.apply(t)
+
+    db.execute(t.insert([Document(data)]))
+
+    select = t.select(*data.keys()).limit(1)
+    decode_data = db.execute(select).next().unpack()
+    for key in data:
+        assert isinstance(data[key], type(decode_data[key]))
+        assert str(data[key]) == str(decode_data[key])

Pinnacle-Networks/Pinnacle

superduperdb/backends/ibis/query.py
@@ -26,6 +26,7 @@ from pinnacledb.base.exceptions import DatabackendException
 from pinnacledb.components.component import Component
 from pinnacledb.components.datatype import DataType
 from pinnacledb.components.schema import Schema
+from pinnacledb.misc.auto_schema import infer_schema
 
 if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
@@ -667,6 +668,17 @@ class Table(Component):
                 pass
             else:
                 raise e
+
+    @staticmethod
+    def infer_schema(data: t.Mapping[str, t.Any], identifier: t.Optional[str] = None):
+        """
+        Infer a schema from a given data object
+
+        :param data: The data object
+        :param identifier: The identifier for the schema, if None, it will be generated
+        :return: The inferred schema
+        """
+        return infer_schema(data, identifier=identifier, ibis=True)
 
     @property
     def table_or_collection(self):

superduperdb/backends/mongodb/query.py
@@ -23,6 +23,7 @@ from pinnacledb.backends.base.query import (
 from pinnacledb.base.cursor import SuperDuperCursor
 from pinnacledb.base.document import Document
 from pinnacledb.components.schema import Schema
+from pinnacledb.misc.auto_schema import infer_schema
 from pinnacledb.misc.files import load_uris
 
 SCHEMA_KEY = '_schema'
@@ -955,6 +956,17 @@ class Collection(TableOrCollection):
                 db.execute(
                     collection.insert_many([Document(**doc) for doc in bulk_writes])
                 )
+
+    @staticmethod
+    def infer_schema(data: t.Mapping[str, t.Any], identifier: t.Optional[str] = None):
+        """
+        Infer a schema from a given data object
+
+        :param data: The data object
+        :param identifier: The identifier for the schema, if None, it will be generated
+        :return: The inferred schema
+        """
+        return infer_schema(data, identifier)
 
 
 def _get_decode_function(db) -> t.Callable[[t.Any], t.Any]:

superduperdb/components/datatype.py
@@ -3,6 +3,7 @@ import dataclasses as dc
 import hashlib
 import inspect
 import io
+import json
 import os
 import pickle
 import re
@@ -25,6 +26,19 @@ from pinnacledb.misc.hash import random_sha1
 
 Decode = t.Callable[[bytes], t.Any]
 Encode = t.Callable[[t.Any], bytes]
+
+
+class IntermidiaType:
+    BYTES = 'bytes'
+    STRING = 'string'
+
+
+def json_encode(object: t.Any, info: t.Optional[t.Dict] = None) -> str:
+    return json.dumps(object)
+
+
+def json_decode(b: str, info: t.Optional[t.Dict] = None) -> t.Any:
+    return json.loads(b)
 
 
 def pickle_encode(object: t.Any, info: t.Optional[t.Dict] = None) -> bytes:
@@ -116,7 +130,7 @@ def torch_decode(b: bytes, info: t.Optional[t.Dict] = None) -> t.Any:
     return torch.load(io.BytesIO(b))
 
 
-def to_base64(bytes):
+def bytes_to_base64(bytes):
     """
     Converts bytes to base64.
 
@@ -125,7 +139,7 @@ def to_base64(bytes):
     return base64.b64encode(bytes).decode('utf-8')
 
 
-def from_base64(encoded):
+def base64_to_bytes(encoded):
     """
     Decodes a base64 encoded string.
 
@@ -190,6 +204,7 @@ class DataType(Component):
     directory: t.Optional[str] = None
     encodable: str = 'encodable'
     bytes_encoding: t.Optional[str] = CFG.bytes_encoding
+    intermidia_type: t.Optional[str] = IntermidiaType.BYTES
     media_type: t.Optional[str] = None
 
     def __post_init__(self, artifacts):
@@ -252,24 +267,21 @@ class DataType(Component):
         return self.decoder(item, info=info)
 
     def bytes_encoding_after_encode(self, data):
-        """
-        Convert the encoded data to the specified bytes encoding format.
-
-        :param data: The encoded data.
-        """
-        if self.bytes_encoding == BytesEncoding.BASE64:
-            return to_base64(data)
+        # Only encode to base64 if the data is bytes and the intermidia type is bytes
+        if (
+            self.bytes_encoding == BytesEncoding.BASE64
+            and self.intermidia_type == IntermidiaType.BYTES
+        ):
+            return bytes_to_base64(data)
         return data
 
     def bytes_encoding_before_decode(self, data):
-        """
-        Convert the encoded data from the specified bytes encoding format
-        before decoding.
-
-        :param data: The encoded data in the specified bytes encoding format.
-        """
-        if self.bytes_encoding == BytesEncoding.BASE64:
-            return from_base64(data)
+        # Only decode from base64 if the data is bytes and the intermidia type is bytes
+        if (
+            self.bytes_encoding == BytesEncoding.BASE64
+            and self.intermidia_type == IntermidiaType.BYTES
+        ):
+            return base64_to_bytes(data)
         return data
 
 
@@ -507,18 +519,7 @@ class Encodable(_BaseEncodable):
     def _get_object(cls, db, r):
         if r.get('bytes') is None:
             return None
-        if db is None:
-            try:
-                from pinnacledb.components.datatype import serializers
-
-                datatype = serializers[r['datatype']]
-            except KeyError:
-                raise Exception(
-                    f'You specified a serializer which doesn\'t have a'
-                    f' default value: {r["datatype"]}'
-                )
-        else:
-            datatype = db.datatypes[r['datatype']]
+        datatype = cls.get_datatype(db, r)
         object = datatype.decode_data(r['bytes'], info=datatype.info)
         return object
 
@@ -574,10 +575,26 @@ class Encodable(_BaseEncodable):
         object = cls._get_object(db, r['_content'])
         return cls(
             x=object,
-            datatype=db.datatypes[r['_content']['datatype']],
+            datatype=cls.get_datatype(db, r['_content']),
             uri=r['_content']['uri'],
             file_id=r['_content'].get('file_id'),
         )
+
+    @classmethod
+    def get_datatype(cls, db, r):
+        if db is None:
+            try:
+                from pinnacledb.components.datatype import serializers
+
+                datatype = serializers[r['datatype']]
+            except KeyError:
+                raise Exception(
+                    f'You specified a serializer which doesn\'t have a'
+                    f' default value: {r["datatype"]}'
+                )
+        else:
+            datatype = db.datatypes[r['datatype']]
+        return datatype
 
 
 @dc.dataclass
@@ -907,6 +924,14 @@ _ENCODABLES = {
     'lazy_file': LazyFile,
 }
 
+json_serializer = DataType(
+    'json',
+    encoder=json_encode,
+    decoder=json_decode,
+    encodable='encodable',
+    bytes_encoding=BytesEncoding.BASE64,
+    intermidia_type=IntermidiaType.STRING,
+)
 
 pickle_serializer = DataType(
     'pickle',
@@ -919,6 +944,12 @@ pickle_lazy = DataType(
     encoder=pickle_encode,
     decoder=pickle_decode,
     encodable='lazy_artifact',
+)
+dill_base = DataType(
+    'dill_base',
+    encoder=dill_encode,
+    decoder=dill_decode,
+    encodable='encodable',
 )
 dill_serializer = DataType(
     'dill',
@@ -953,6 +984,7 @@ file_lazy = DataType(
 serializers = {
     'pickle': pickle_serializer,
     'dill': dill_serializer,
+    'dill_base': dill_base,
     'torch': torch_serializer,
     'file': file_serializer,
     'pickle_lazy': pickle_lazy,

superduperdb/misc/auto_schema.py
@@ -1,0 +1,117 @@
+import json
+import typing as t
+
+import numpy as np
+
+from pinnacledb import logging
+from pinnacledb.components.datatype import (
+    DataType,
+    dill_base,
+    json_serializer,
+    serializers,
+)
+from pinnacledb.components.schema import Schema
+from pinnacledb.ext.numpy.encoder import array
+
+try:
+    import torch
+
+    torch_cls = (torch.Tensor, torch.nn.Module)
+except ImportError:
+    torch_cls = ()
+
+_BASE_TYPES = (
+    int,
+    str,
+    float,
+    bool,
+    bytes,
+    bytearray,
+)
+
+
+def infer_datatype(data: t.Any, ibis=False) -> t.Optional[DataType]:
+    """
+    Infer the datatype of a given data object
+    If the data object is a base type, return None,
+    Otherwise, return the inferred datatype
+
+    :param data: The data object
+    """
+
+    datatype = dill_base
+
+    if isinstance(data, _BASE_TYPES):
+        if not ibis:
+            datatype = None
+        else:
+            from pinnacledb.backends.ibis.field_types import dtype
+
+            datatype = dtype(type(data))
+
+    elif isinstance(data, np.ndarray):
+        dtype = data.dtype.name
+        assert isinstance(dtype, str), f"Expected dtype to be a string, got {dtype}"
+        shape = tuple(data.shape)
+        datatype = array(dtype, shape)
+        serializers[datatype.identifier] = datatype
+
+    elif isinstance(data, torch_cls):
+        import torch
+
+        if isinstance(data, torch.Tensor):
+            from pinnacledb.ext.torch.encoder import tensor
+
+            dtype = data.dtype
+            shape = data.shape
+            datatype = tensor(dtype, shape)
+            serializers[datatype.identifier] = datatype
+        else:
+            datatype = serializers["torch"]
+
+    elif isinstance(data, dict):
+        try:
+            json.dumps(data)
+            # Only use json_serializer when using ibis.
+            if ibis:
+                datatype = json_serializer
+                serializers[datatype.identifier] = datatype
+            else:
+                datatype = None
+        except Exception:
+            pass
+
+    if datatype is not None:
+        logging.info(f"Inferred datatype: {datatype} for data: {data}")
+
+    return datatype
+
+
+def infer_schema(
+    data: t.Mapping[str, t.Any],
+    identifier: t.Optional[str] = None,
+    ibis=False,
+) -> Schema:
+    """
+    Infer a schema from a given data object
+
+    :param data: The data object
+    :param identifier: The identifier for the schema, if None, it will be generated
+    :return: The inferred schema
+    """
+
+    assert isinstance(data, dict), "Data must be a dictionary"
+
+    schema_data = {}
+    for k, v in data.items():
+        data_type = infer_datatype(v, ibis)
+        if data_type is not None:
+            schema_data[k] = data_type
+
+    if identifier is None:
+        key_value_pairs = sorted(schema_data.items())
+        id_ = "&".join([f"{k}={v.identifier}" for k, v in key_value_pairs])
+
+        identifier = f"schema_{id_}"
+
+    return Schema(identifier=identifier, fields=schema_data)

test/unittest/misc/test_auto_schema.py
@@ -1,0 +1,129 @@
+from test.db_config import DBConfig
+
+import numpy as np
+import pandas as pd
+import pytest
+import torch
+
+from pinnacledb.backends.ibis.query import Table
+from pinnacledb.backends.mongodb.query import Collection
+from pinnacledb.base.document import Document
+from pinnacledb.misc.auto_schema import infer_datatype, infer_schema
+
+
+@pytest.fixture
+def data():
+    data = {
+        "int": 1,
+        "float": 1.0,
+        "str": "1",
+        "bool": True,
+        "bytes": b"1",
+        "np_array": np.array([1, 2, 3]),
+        "torch_tensor": torch.tensor([1, 2, 3]),
+        "dict": {"a": 1, "b": "2", "c": {"d": 3}},
+        "dict_np_array": {"a": np.array([1, 2, 3])},
+        "df": pd.DataFrame({"col1": [1, 2], "col2": [3, 4]}),
+    }
+    return data
+
+
+def test_infer_datatype():
+    assert infer_datatype(1) is None
+    assert infer_datatype(1.0) is None
+    assert infer_datatype("1") is None
+    assert infer_datatype(True) is None
+    assert infer_datatype(b"1") is None
+
+    assert infer_datatype(np.array([1, 2, 3])).identifier == "numpy.int64[3]"
+    assert infer_datatype(torch.tensor([1, 2, 3])).identifier == "torch.int64[3]"
+
+    assert infer_datatype({"a": 1}) is None
+    assert infer_datatype({"a": 1}, ibis=True).identifier == "json"
+
+    assert infer_datatype({"a": np.array([1, 2, 3])}).identifier == "dill_base"
+
+    assert (
+        infer_datatype(pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})).identifier
+        == "dill_base"
+    )
+
+
+def test_infer_schema_mongo(data):
+    schema = infer_schema(data)
+    encode_data = schema(data)
+
+    expected_datatypes = [
+        "np_array",
+        "torch_tensor",
+        "dict_np_array",
+        "df",
+    ]
+    assert sorted(schema.encoded_types) == sorted(expected_datatypes)
+    for key in expected_datatypes:
+        if schema.fields.get(key) is not None:
+            assert str(data[key]) != str(encode_data[key])
+
+    decode_data = Document(schema.decode_data(encode_data)).unpack()
+    for key in data:
+        assert isinstance(data[key], type(decode_data[key]))
+        assert str(data[key]) == str(decode_data[key])
+
+
+def test_infer_schema_ibis(data):
+    schema = infer_schema(data, ibis=True)
+    encode_data = schema(data)
+
+    expected_datatypes = [
+        "np_array",
+        "torch_tensor",
+        "dict",
+        "dict_np_array",
+        "df",
+    ]
+    assert sorted(schema.encoded_types) == sorted(expected_datatypes)
+    for key in expected_datatypes:
+        if schema.fields.get(key) is not None:
+            assert str(data[key]) != str(encode_data[key])
+
+    decode_data = Document(schema.decode_data(encode_data)).unpack()
+    for key in data:
+        assert isinstance(data[key], type(decode_data[key]))
+        assert str(data[key]) == str(decode_data[key])
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_mongo_schema(db, data):
+    collection_name = "documents"
+    schema = Collection.infer_schema(data)
+
+    db.apply(schema)
+
+    db.execute(
+        Collection(collection_name).insert_one(
+            Document(data), schema=schema.identifier
+        ),
+    )
+    collection = Collection(collection_name)
+    decode_data = collection.find_one().execute(db).unpack()
+    for key in data:
+        assert isinstance(data[key], type(decode_data[key]))
+        assert str(data[key]) == str(decode_data[key])
+
+
+@pytest.mark.parametrize("db", [DBConfig.sqldb_empty], indirect=True)
+def test_ibis_schema(db, data):
+    data["id"] = str("1")
+    schema = Table.infer_schema(data)
+
+    t = Table(identifier="my_table", schema=schema)
+
+    db.apply(t)
+
+    db.execute(t.insert([Document(data)]))
+
+    select = t.select(*data.keys()).limit(1)
+    decode_data = db.execute(select).next().unpack()
+    for key in data:
+        assert isinstance(data[key], type(decode_data[key]))
+        assert str(data[key]) == str(decode_data[key])