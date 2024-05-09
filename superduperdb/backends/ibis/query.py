superduper-io/superduper

Add registration mechanism for autoschema.

superduperdb/backends/ibis/query.py
@@ -26,7 +26,6 @@ from superduperdb.base.exceptions import DatabackendException
 from superduperdb.components.component import Component
 from superduperdb.components.datatype import DataType
 from superduperdb.components.schema import Schema
-from superduperdb.misc.auto_schema import infer_schema
 
 if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
@@ -678,6 +677,8 @@ class Table(Component):
         :param identifier: The identifier for the schema, if None, it will be generated
         :return: The inferred schema
         """
+        from superduperdb.misc.auto_schema import infer_schema
+
         return infer_schema(data, identifier=identifier, ibis=True)
 
     @property

superduperdb/backends/mongodb/query.py
@@ -23,7 +23,6 @@ from superduperdb.backends.base.query import (
 from superduperdb.base.cursor import SuperDuperCursor
 from superduperdb.base.document import Document
 from superduperdb.components.schema import Schema
-from superduperdb.misc.auto_schema import infer_schema
 from superduperdb.misc.files import load_uris
 
 SCHEMA_KEY = '_schema'
@@ -966,6 +965,8 @@ class Collection(TableOrCollection):
         :param identifier: The identifier for the schema, if None, it will be generated
         :return: The inferred schema
         """
+        from superduperdb.misc.auto_schema import infer_schema
+
         return infer_schema(data, identifier)
 
 

superduperdb/ext/numpy/encoder.py
@@ -2,7 +2,7 @@ import typing as t
 
 import numpy
 
-from superduperdb.components.datatype import DataType
+from superduperdb.components.datatype import DataType, DataTypeFactory
 from superduperdb.ext.utils import str_shape
 
 
@@ -45,3 +45,21 @@ def array(
         bytes_encoding=bytes_encoding,
         encodable=encodable,
     )
+
+
+class NumpyDataTypeFactory(DataTypeFactory):
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        """
+        Check if the data is a numpy array.
+        It's used for registering the auto schema.
+        """
+        return isinstance(data, numpy.ndarray)
+
+    @staticmethod
+    def create(data: t.Any) -> DataType:
+        """
+        Create a numpy array datatype.
+        It's used for registering the auto schema.
+        """
+        return array(data.dtype, data.shape)

superduperdb/ext/torch/encoder.py
@@ -3,7 +3,7 @@ import typing as t
 import numpy
 import torch
 
-from superduperdb.components.datatype import DataType
+from superduperdb.components.datatype import DataType, DataTypeFactory
 from superduperdb.ext.utils import str_shape
 
 
@@ -13,7 +13,7 @@ class EncodeTensor:
 
     def __call__(self, x, info: t.Optional[t.Dict] = None):
         if x.dtype != self.dtype:
-            raise TypeError(f'dtype was {x.dtype}, expected {self.dtype}')
+            raise TypeError(f"dtype was {x.dtype}, expected {self.dtype}")
         return memoryview(x.numpy()).tobytes()
 
 
@@ -35,9 +35,27 @@ def tensor(dtype, shape: t.Sequence, bytes_encoding: t.Optional[str] = None):
     :param shape: The shape of the tensor.
     """
     return DataType(
-        identifier=f'{str(dtype)}[{str_shape(shape)}]',
+        identifier=f"{str(dtype)}[{str_shape(shape)}]",
         encoder=EncodeTensor(dtype),
         decoder=DecodeTensor(dtype, shape),
         shape=shape,
         bytes_encoding=bytes_encoding,
     )
+
+
+class TorchDataTypeFactory(DataTypeFactory):
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        """
+        Check if the data is a torch tensor.
+        It's used for registering the auto schema.
+        """
+        return isinstance(data, torch.Tensor)
+
+    @staticmethod
+    def create(data: t.Any) -> DataType:
+        """
+        Create a torch tensor datatype.
+        It's used for registering the auto schema.
+        """
+        return tensor(data.dtype, data.shape)

superduperdb/misc/auto_schema.py
@@ -1,26 +1,41 @@
-import json
+import importlib
 import typing as t
-
-import numpy as np
 
 from superduperdb import logging
 from superduperdb.components.datatype import (
     DataType,
-    dill_base,
+    DataTypeFactory,
+    dill_decode,
+    dill_encode,
     json_serializer,
-    serializers,
 )
 from superduperdb.components.schema import Schema
-from superduperdb.ext.numpy.encoder import array
 
-try:
-    import torch
 
-    torch_cls = (torch.Tensor, torch.nn.Module)
-except ImportError:
-    torch_cls = ()
+def register_module(module_name):
+    """
+    Register a module for datatype inference.
+    Only modules with a check and create function will be registered
 
-_BASE_TYPES = (
+    """
+    try:
+        importlib.import_module(module_name)
+    except Exception:
+        logging.debug(f"Could not register module: {module_name}")
+
+
+register_module("superduperdb.ext.numpy.encoder")
+register_module("superduperdb.ext.torch.encoder")
+
+
+DEFAULT_DATATYPE = DataType(
+    "DEFAULT",
+    encoder=dill_encode,
+    decoder=dill_decode,
+    encodable="encodable",
+)
+
+BASE_TYPES = (
     int,
     str,
     float,
@@ -30,7 +45,7 @@ _BASE_TYPES = (
 )
 
 
-def infer_datatype(data: t.Any, ibis=False) -> t.Optional[DataType]:
+def infer_datatype(data: t.Any) -> t.Optional[t.Union[DataType, type]]:
     """
     Infer the datatype of a given data object
     If the data object is a base type, return None,
@@ -39,50 +54,23 @@ def infer_datatype(data: t.Any, ibis=False) -> t.Optional[DataType]:
     :param data: The data object
     """
 
-    datatype = dill_base
+    datatype = None
 
-    if isinstance(data, _BASE_TYPES):
-        if not ibis:
-            datatype = None
-        else:
-            from superduperdb.backends.ibis.field_types import dtype
+    if isinstance(data, BASE_TYPES):
+        datatype = type(data)
+        logging.info(f"Inferred base type: {datatype} for data:", data)
+        return datatype
 
-            datatype = dtype(type(data))
+    for factory in DataTypeFactory.__subclasses__():
+        if factory.check(data):
+            datatype = factory.create(data)
+            assert isinstance(datatype, DataType)
+            logging.info(f"Inferred datatype: {datatype.identifier} for data: {data}")
+            break
 
-    elif isinstance(data, np.ndarray):
-        dtype = data.dtype.name
-        assert isinstance(dtype, str), f"Expected dtype to be a string, got {dtype}"
-        shape = tuple(data.shape)
-        datatype = array(dtype, shape)
-        serializers[datatype.identifier] = datatype
-
-    elif isinstance(data, torch_cls):
-        import torch
-
-        if isinstance(data, torch.Tensor):
-            from superduperdb.ext.torch.encoder import tensor
-
-            dtype = data.dtype
-            shape = data.shape
-            datatype = tensor(dtype, shape)
-            serializers[datatype.identifier] = datatype
-        else:
-            datatype = serializers["torch"]
-
-    elif isinstance(data, dict):
-        try:
-            json.dumps(data)
-            # Only use json_serializer when using ibis.
-            if ibis:
-                datatype = json_serializer
-                serializers[datatype.identifier] = datatype
-            else:
-                datatype = None
-        except Exception:
-            logging.info(f"Could not serialize data: {data}")
-
-    if datatype is not None:
-        logging.info(f"Inferred datatype: {datatype} for data: {data}")
+    if datatype is None:
+        logging.info(f"Could not infer datatype for data: {data}, using default")
+        datatype = DEFAULT_DATATYPE
 
     return datatype
 
@@ -104,12 +92,57 @@ def infer_schema(
 
     schema_data = {}
     for k, v in data.items():
-        data_type = infer_datatype(v, ibis)
+        data_type = infer_datatype(v)
         if data_type is not None:
             schema_data[k] = data_type
 
-    if identifier is None:
-        key_value_pairs = sorted(schema_data.items())
-        identifier = "&".join([f"{k}={v.identifier}" for k, v in key_value_pairs])
+    if ibis:
+        schema_data = updated_schema_data_for_ibis(schema_data)
+    else:
+        schema_data = updated_schema_data_for_mongodb(schema_data)
 
-    return Schema(identifier=identifier, fields=schema_data)
+    if identifier is None:
+        key_value_pairs = []
+        for k, v in sorted(schema_data.items()):
+            if hasattr(v, "identifier"):
+                key_value_pairs.append(f"{k}={v.identifier}")
+            else:
+                key_value_pairs.append(f"{k}={str(v)}")
+        identifier = "&".join(key_value_pairs)
+
+    return Schema(identifier=identifier, fields=schema_data)  # type: ignore
+
+
+def updated_schema_data_for_ibis(
+    schema_data,
+) -> t.Dict[str, DataType]:
+    from superduperdb.backends.ibis.field_types import dtype
+
+    for k, v in schema_data.items():
+        if not isinstance(v, DataType):
+            schema_data[k] = dtype(v)
+
+    return schema_data
+
+
+def updated_schema_data_for_mongodb(schema_data) -> t.Dict[str, DataType]:
+    schema_data = {k: v for k, v in schema_data.items() if isinstance(v, DataType)}
+
+    # MongoDB can store dict directly, so we don't need to serialize it.
+    schema_data = {k: v for k, v in schema_data.items() if v.identifier != "json"}
+
+    return schema_data
+
+
+class JsonDataTypeFactory(DataTypeFactory):
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        try:
+            json_serializer.encode_data(data)
+            return True
+        except Exception:
+            return False
+
+    @staticmethod
+    def create(data: t.Any) -> DataType:
+        return json_serializer

Pinnacle-Networks/Pinnacle

superduperdb/backends/ibis/query.py
@@ -26,7 +26,6 @@ from pinnacledb.base.exceptions import DatabackendException
 from pinnacledb.components.component import Component
 from pinnacledb.components.datatype import DataType
 from pinnacledb.components.schema import Schema
-from pinnacledb.misc.auto_schema import infer_schema
 
 if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
@@ -678,6 +677,8 @@ class Table(Component):
         :param identifier: The identifier for the schema, if None, it will be generated
         :return: The inferred schema
         """
+        from pinnacledb.misc.auto_schema import infer_schema
+
         return infer_schema(data, identifier=identifier, ibis=True)
 
     @property

superduperdb/backends/mongodb/query.py
@@ -23,7 +23,6 @@ from pinnacledb.backends.base.query import (
 from pinnacledb.base.cursor import SuperDuperCursor
 from pinnacledb.base.document import Document
 from pinnacledb.components.schema import Schema
-from pinnacledb.misc.auto_schema import infer_schema
 from pinnacledb.misc.files import load_uris
 
 SCHEMA_KEY = '_schema'
@@ -966,6 +965,8 @@ class Collection(TableOrCollection):
         :param identifier: The identifier for the schema, if None, it will be generated
         :return: The inferred schema
         """
+        from pinnacledb.misc.auto_schema import infer_schema
+
         return infer_schema(data, identifier)
 
 

superduperdb/ext/numpy/encoder.py
@@ -2,7 +2,7 @@ import typing as t
 
 import numpy
 
-from pinnacledb.components.datatype import DataType
+from pinnacledb.components.datatype import DataType, DataTypeFactory
 from pinnacledb.ext.utils import str_shape
 
 
@@ -45,3 +45,21 @@ def array(
         bytes_encoding=bytes_encoding,
         encodable=encodable,
     )
+
+
+class NumpyDataTypeFactory(DataTypeFactory):
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        """
+        Check if the data is a numpy array.
+        It's used for registering the auto schema.
+        """
+        return isinstance(data, numpy.ndarray)
+
+    @staticmethod
+    def create(data: t.Any) -> DataType:
+        """
+        Create a numpy array datatype.
+        It's used for registering the auto schema.
+        """
+        return array(data.dtype, data.shape)

superduperdb/ext/torch/encoder.py
@@ -3,7 +3,7 @@ import typing as t
 import numpy
 import torch
 
-from pinnacledb.components.datatype import DataType
+from pinnacledb.components.datatype import DataType, DataTypeFactory
 from pinnacledb.ext.utils import str_shape
 
 
@@ -13,7 +13,7 @@ class EncodeTensor:
 
     def __call__(self, x, info: t.Optional[t.Dict] = None):
         if x.dtype != self.dtype:
-            raise TypeError(f'dtype was {x.dtype}, expected {self.dtype}')
+            raise TypeError(f"dtype was {x.dtype}, expected {self.dtype}")
         return memoryview(x.numpy()).tobytes()
 
 
@@ -35,9 +35,27 @@ def tensor(dtype, shape: t.Sequence, bytes_encoding: t.Optional[str] = None):
     :param shape: The shape of the tensor.
     """
     return DataType(
-        identifier=f'{str(dtype)}[{str_shape(shape)}]',
+        identifier=f"{str(dtype)}[{str_shape(shape)}]",
         encoder=EncodeTensor(dtype),
         decoder=DecodeTensor(dtype, shape),
         shape=shape,
         bytes_encoding=bytes_encoding,
     )
+
+
+class TorchDataTypeFactory(DataTypeFactory):
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        """
+        Check if the data is a torch tensor.
+        It's used for registering the auto schema.
+        """
+        return isinstance(data, torch.Tensor)
+
+    @staticmethod
+    def create(data: t.Any) -> DataType:
+        """
+        Create a torch tensor datatype.
+        It's used for registering the auto schema.
+        """
+        return tensor(data.dtype, data.shape)

superduperdb/misc/auto_schema.py
@@ -1,26 +1,41 @@
-import json
+import importlib
 import typing as t
-
-import numpy as np
 
 from pinnacledb import logging
 from pinnacledb.components.datatype import (
     DataType,
-    dill_base,
+    DataTypeFactory,
+    dill_decode,
+    dill_encode,
     json_serializer,
-    serializers,
 )
 from pinnacledb.components.schema import Schema
-from pinnacledb.ext.numpy.encoder import array
 
-try:
-    import torch
 
-    torch_cls = (torch.Tensor, torch.nn.Module)
-except ImportError:
-    torch_cls = ()
+def register_module(module_name):
+    """
+    Register a module for datatype inference.
+    Only modules with a check and create function will be registered
 
-_BASE_TYPES = (
+    """
+    try:
+        importlib.import_module(module_name)
+    except Exception:
+        logging.debug(f"Could not register module: {module_name}")
+
+
+register_module("pinnacledb.ext.numpy.encoder")
+register_module("pinnacledb.ext.torch.encoder")
+
+
+DEFAULT_DATATYPE = DataType(
+    "DEFAULT",
+    encoder=dill_encode,
+    decoder=dill_decode,
+    encodable="encodable",
+)
+
+BASE_TYPES = (
     int,
     str,
     float,
@@ -30,7 +45,7 @@ _BASE_TYPES = (
 )
 
 
-def infer_datatype(data: t.Any, ibis=False) -> t.Optional[DataType]:
+def infer_datatype(data: t.Any) -> t.Optional[t.Union[DataType, type]]:
     """
     Infer the datatype of a given data object
     If the data object is a base type, return None,
@@ -39,50 +54,23 @@ def infer_datatype(data: t.Any, ibis=False) -> t.Optional[DataType]:
     :param data: The data object
     """
 
-    datatype = dill_base
+    datatype = None
 
-    if isinstance(data, _BASE_TYPES):
-        if not ibis:
-            datatype = None
-        else:
-            from pinnacledb.backends.ibis.field_types import dtype
+    if isinstance(data, BASE_TYPES):
+        datatype = type(data)
+        logging.info(f"Inferred base type: {datatype} for data:", data)
+        return datatype
 
-            datatype = dtype(type(data))
+    for factory in DataTypeFactory.__subclasses__():
+        if factory.check(data):
+            datatype = factory.create(data)
+            assert isinstance(datatype, DataType)
+            logging.info(f"Inferred datatype: {datatype.identifier} for data: {data}")
+            break
 
-    elif isinstance(data, np.ndarray):
-        dtype = data.dtype.name
-        assert isinstance(dtype, str), f"Expected dtype to be a string, got {dtype}"
-        shape = tuple(data.shape)
-        datatype = array(dtype, shape)
-        serializers[datatype.identifier] = datatype
-
-    elif isinstance(data, torch_cls):
-        import torch
-
-        if isinstance(data, torch.Tensor):
-            from pinnacledb.ext.torch.encoder import tensor
-
-            dtype = data.dtype
-            shape = data.shape
-            datatype = tensor(dtype, shape)
-            serializers[datatype.identifier] = datatype
-        else:
-            datatype = serializers["torch"]
-
-    elif isinstance(data, dict):
-        try:
-            json.dumps(data)
-            # Only use json_serializer when using ibis.
-            if ibis:
-                datatype = json_serializer
-                serializers[datatype.identifier] = datatype
-            else:
-                datatype = None
-        except Exception:
-            logging.info(f"Could not serialize data: {data}")
-
-    if datatype is not None:
-        logging.info(f"Inferred datatype: {datatype} for data: {data}")
+    if datatype is None:
+        logging.info(f"Could not infer datatype for data: {data}, using default")
+        datatype = DEFAULT_DATATYPE
 
     return datatype
 
@@ -104,12 +92,57 @@ def infer_schema(
 
     schema_data = {}
     for k, v in data.items():
-        data_type = infer_datatype(v, ibis)
+        data_type = infer_datatype(v)
         if data_type is not None:
             schema_data[k] = data_type
 
-    if identifier is None:
-        key_value_pairs = sorted(schema_data.items())
-        identifier = "&".join([f"{k}={v.identifier}" for k, v in key_value_pairs])
+    if ibis:
+        schema_data = updated_schema_data_for_ibis(schema_data)
+    else:
+        schema_data = updated_schema_data_for_mongodb(schema_data)
 
-    return Schema(identifier=identifier, fields=schema_data)
+    if identifier is None:
+        key_value_pairs = []
+        for k, v in sorted(schema_data.items()):
+            if hasattr(v, "identifier"):
+                key_value_pairs.append(f"{k}={v.identifier}")
+            else:
+                key_value_pairs.append(f"{k}={str(v)}")
+        identifier = "&".join(key_value_pairs)
+
+    return Schema(identifier=identifier, fields=schema_data)  # type: ignore
+
+
+def updated_schema_data_for_ibis(
+    schema_data,
+) -> t.Dict[str, DataType]:
+    from pinnacledb.backends.ibis.field_types import dtype
+
+    for k, v in schema_data.items():
+        if not isinstance(v, DataType):
+            schema_data[k] = dtype(v)
+
+    return schema_data
+
+
+def updated_schema_data_for_mongodb(schema_data) -> t.Dict[str, DataType]:
+    schema_data = {k: v for k, v in schema_data.items() if isinstance(v, DataType)}
+
+    # MongoDB can store dict directly, so we don't need to serialize it.
+    schema_data = {k: v for k, v in schema_data.items() if v.identifier != "json"}
+
+    return schema_data
+
+
+class JsonDataTypeFactory(DataTypeFactory):
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        try:
+            json_serializer.encode_data(data)
+            return True
+        except Exception:
+            return False
+
+    @staticmethod
+    def create(data: t.Any) -> DataType:
+        return json_serializer