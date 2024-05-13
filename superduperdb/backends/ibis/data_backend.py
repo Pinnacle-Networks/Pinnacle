superduper-io/superduper

Lazy-creation of output tables for ibis to enable auto-inference of output schema

superduperdb/backends/ibis/data_backend.py
@@ -6,6 +6,7 @@ import ibis
 import pandas
 from ibis.backends.base import BaseBackend
 from pandas.core.frame import DataFrame
+from sqlalchemy.exc import NoSuchTableError
 
 from superduperdb.backends.base.data_backend import BaseDataBackend
 from superduperdb.backends.ibis.db_helper import get_db_helper
@@ -13,6 +14,7 @@ from superduperdb.backends.ibis.field_types import FieldType, dtype
 from superduperdb.backends.ibis.query import Table
 from superduperdb.backends.local.artifacts import FileSystemArtifactStore
 from superduperdb.backends.sqlalchemy.metadata import SQLAlchemyMetadata
+from superduperdb.base.enums import DBType
 from superduperdb.components.datatype import DataType
 from superduperdb.components.schema import Schema
 
@@ -21,6 +23,8 @@ INPUT_KEY = '_input_id'
 
 
 class IbisDataBackend(BaseDataBackend):
+    db_type = DBType.SQL
+
     def __init__(self, conn: BaseBackend, name: str, in_memory: bool = False):
         super().__init__(conn=conn, name=name)
         self.in_memory = in_memory
@@ -98,6 +102,13 @@ class IbisDataBackend(BaseDataBackend):
                 schema=Schema(identifier=f'_schema/{predict_id}', fields=fields),
             )
 
+    def check_output_dest(self, predict_id) -> bool:
+        try:
+            self.conn.table(f'_outputs.{predict_id}')
+            return True
+        except NoSuchTableError:
+            return False
+
     def create_table_and_schema(self, identifier: str, mapping: dict):
         """
         Create a schema in the data-backend.
@@ -131,3 +142,16 @@ class IbisDataBackend(BaseDataBackend):
 
     def list_tables_or_collections(self):
         return self.conn.list_tables()
+
+    @staticmethod
+    def infer_schema(data: t.Mapping[str, t.Any], identifier: t.Optional[str] = None):
+        """
+        Infer a schema from a given data object
+
+        :param data: The data object
+        :param identifier: The identifier for the schema, if None, it will be generated
+        :return: The inferred schema
+        """
+        from superduperdb.misc.auto_schema import infer_schema
+
+        return infer_schema(data, identifier=identifier, ibis=True)

superduperdb/backends/mongodb/data_backend.py
@@ -9,6 +9,7 @@ from superduperdb.backends.base.data_backend import BaseDataBackend
 from superduperdb.backends.ibis.field_types import FieldType
 from superduperdb.backends.mongodb.artifacts import MongoArtifactStore
 from superduperdb.backends.mongodb.metadata import MongoMetaDataStore
+from superduperdb.base.enums import DBType
 from superduperdb.base.serializable import Serializable
 from superduperdb.components.datatype import DataType
 from superduperdb.misc.colors import Colors
@@ -22,6 +23,8 @@ class MongoDataBackend(BaseDataBackend):
     :param conn: MongoDB client connection
     :param name: Name of database to host filesystem
     """
+
+    db_type = DBType.MONGODB
 
     id_field = '_id'
 
@@ -129,8 +132,24 @@ class MongoDataBackend(BaseDataBackend):
 
     def create_output_dest(
         self,
-        identifier: str,
+        predict_id: str,
         datatype: t.Union[None, DataType, FieldType],
         flatten: bool = False,
     ):
         pass
+
+    def check_output_dest(self, predict_id) -> bool:
+        return True
+
+    @staticmethod
+    def infer_schema(data: t.Mapping[str, t.Any], identifier: t.Optional[str] = None):
+        """
+        Infer a schema from a given data object
+
+        :param data: The data object
+        :param identifier: The identifier for the schema, if None, it will be generated
+        :return: The inferred schema
+        """
+        from superduperdb.misc.auto_schema import infer_schema
+
+        return infer_schema(data, identifier)

superduperdb/components/listener.py
@@ -5,7 +5,6 @@ from overrides import override
 
 from superduperdb import CFG
 from superduperdb.backends.base.query import CompoundSelect
-from superduperdb.base.datalayer import Datalayer
 from superduperdb.base.document import _OUTPUTS_KEY
 from superduperdb.base.serializable import Variable
 from superduperdb.components.model import Mapping
@@ -16,6 +15,10 @@ from superduperdb.rest.utils import parse_query
 from ..jobs.job import Job
 from .component import Component, ComponentTuple
 from .model import Model, ModelInputType
+
+if t.TYPE_CHECKING:
+    from superduperdb.base.datalayer import Datalayer
+
 
 SELECT_TEMPLATE = {'documents': [], 'query': '<collection_name>.find()'}
 
@@ -113,7 +116,19 @@ class Listener(Component):
         """
         Get query reference to model outputs.
         """
-        return self.select.table_or_collection.outputs(self.predict_id)
+        if self.select.DB_TYPE == "SQL":
+            return self.select.table_or_collection.outputs(self.predict_id)
+
+        else:
+            from superduperdb.backends.mongodb.query import Collection
+
+            model_update_kwargs = self.model.model_update_kwargs or {}
+            if model_update_kwargs.get('document_embedded', True):
+                collection_name = self.select.table_or_collection.identifier
+            else:
+                collection_name = self.outputs
+
+            return Collection(collection_name).find()
 
     @property
     def outputs_key(self):
@@ -126,7 +141,7 @@ class Listener(Component):
             return self.outputs
 
     @override
-    def pre_create(self, db: Datalayer) -> None:
+    def pre_create(self, db: "Datalayer") -> None:
         """
         Pre-create hook.
 
@@ -152,19 +167,13 @@ class Listener(Component):
         return key
 
     @override
-    def post_create(self, db: Datalayer) -> None:
+    def post_create(self, db: "Datalayer") -> None:
         """
         Post-create hook.
 
         :param db: Data layer instance.
         """
-        output_table = db.databackend.create_output_dest(
-            f'{self.identifier}::{self.version}',
-            self.model.datatype,
-            flatten=self.model.flatten,
-        )
-        if output_table is not None:
-            db.add(output_table)
+        self.create_output_dest(db, self.predict_id, self.model)
         if self.select is not None and self.active and not db.server_mode:
             if CFG.cluster.cdc.uri:
                 request_server(
@@ -175,6 +184,25 @@ class Listener(Component):
                 )
             else:
                 db.cdc.add(self)
+
+    @classmethod
+    def create_output_dest(cls, db: "Datalayer", predict_id, model: Model):
+        """
+        Create output destination.
+
+        :param db: Data layer instance.
+        :param predict_id: Predict ID.
+        :param model: Model instance.
+        """
+        if model.datatype is None:
+            return
+        output_table = db.databackend.create_output_dest(
+            predict_id,
+            model.datatype,
+            flatten=model.flatten,
+        )
+        if output_table is not None:
+            db.add(output_table)
 
     @property
     def dependencies(self) -> t.List[ComponentTuple]:
@@ -197,6 +225,18 @@ class Listener(Component):
         Get predict ID.
         """
         return f'{self.identifier}::{self.version}'
+
+    @classmethod
+    def from_predict_id(cls, db: "Datalayer", predict_id) -> 'Listener':
+        """
+        Split predict ID.
+
+        :param db: Data layer instance.
+        :param predict_id: Predict ID.
+        """
+
+        identifier, version = predict_id.rsplit('::', 1)
+        return t.cast(Listener, db.load('listener', identifier, version=int(version)))
 
     @property
     def id_key(self) -> str:
@@ -235,7 +275,10 @@ class Listener(Component):
 
     @override
     def schedule_jobs(
-        self, db: Datalayer, dependencies: t.Sequence[Job] = (), overwrite: bool = False
+        self,
+        db: "Datalayer",
+        dependencies: t.Sequence[Job] = (),
+        overwrite: bool = False,
     ) -> t.Sequence[t.Any]:
         """
         Schedule jobs for the listener.
@@ -251,7 +294,7 @@ class Listener(Component):
             self.model.predict_in_db_job(
                 X=self.key,
                 db=db,
-                predict_id=f'{self.identifier}::{self.version}',
+                predict_id=self.predict_id,
                 select=self.select.copy(),
                 dependencies=dependencies,
                 overwrite=overwrite,
@@ -260,7 +303,7 @@ class Listener(Component):
         ]
         return out
 
-    def cleanup(self, database: Datalayer) -> None:
+    def cleanup(self, database: "Datalayer") -> None:
         """Clean up when the listener is deleted.
 
         :param database: Data layer instance to process.

test/unittest/component/test_model.py
@@ -7,9 +7,9 @@ import numpy as np
 import pytest
 from sklearn.metrics import accuracy_score, f1_score
 
+from superduperdb.backends.base.data_backend import BaseDataBackend
 from superduperdb.backends.base.query import Select
 from superduperdb.backends.ibis.field_types import FieldType
-from superduperdb.backends.local.compute import LocalComputeBackend
 from superduperdb.backends.mongodb.query import Collection
 from superduperdb.base.datalayer import Datalayer
 from superduperdb.base.document import Document
@@ -144,10 +144,10 @@ def test_pm_create_predict_job(mock_job, predict_mixin):
 def test_pm_predict(predict_mixin):
     # Check the logic of predict method, the mock method will be tested below
     db = MagicMock(spec=Datalayer)
-    db.compute = MagicMock(spec=LocalComputeBackend)
-    db.metadata = MagicMock()
+    db.databackend = MagicMock(spec=BaseDataBackend)
     select = MagicMock(spec=Select)
     select.table_or_collection = MagicMock()
+    predict_mixin.db = db
 
     with patch.object(predict_mixin, 'predict') as predict_func:
         predict_mixin.predict_in_db('x', db=db, select=select, predict_id='test')
@@ -164,7 +164,9 @@ def test_pm_predict_with_select_ids(monkeypatch, predict_mixin):
 
     select = MagicMock(spec=Select)
     db = MagicMock(spec=Datalayer)
+    db.databackend = MagicMock(spec=BaseDataBackend)
     db.execute.return_value = docs
+    predict_mixin.db = db
 
     with patch.object(predict_mixin, 'object') as my_object:
         my_object.return_value = 2
@@ -417,6 +419,7 @@ def test_pm_predict_with_select_ids_multikey(monkeypatch, predict_mixin_multikey
 
         select = MagicMock(spec=Select)
         db = MagicMock(spec=Datalayer)
+        db.databackend = MagicMock(spec=BaseDataBackend)
         db.execute.return_value = docs
 
         # Check the base predict function

Pinnacle-Networks/Pinnacle

superduperdb/backends/ibis/data_backend.py
@@ -6,6 +6,7 @@ import ibis
 import pandas
 from ibis.backends.base import BaseBackend
 from pandas.core.frame import DataFrame
+from sqlalchemy.exc import NoSuchTableError
 
 from pinnacledb.backends.base.data_backend import BaseDataBackend
 from pinnacledb.backends.ibis.db_helper import get_db_helper
@@ -13,6 +14,7 @@ from pinnacledb.backends.ibis.field_types import FieldType, dtype
 from pinnacledb.backends.ibis.query import Table
 from pinnacledb.backends.local.artifacts import FileSystemArtifactStore
 from pinnacledb.backends.sqlalchemy.metadata import SQLAlchemyMetadata
+from pinnacledb.base.enums import DBType
 from pinnacledb.components.datatype import DataType
 from pinnacledb.components.schema import Schema
 
@@ -21,6 +23,8 @@ INPUT_KEY = '_input_id'
 
 
 class IbisDataBackend(BaseDataBackend):
+    db_type = DBType.SQL
+
     def __init__(self, conn: BaseBackend, name: str, in_memory: bool = False):
         super().__init__(conn=conn, name=name)
         self.in_memory = in_memory
@@ -98,6 +102,13 @@ class IbisDataBackend(BaseDataBackend):
                 schema=Schema(identifier=f'_schema/{predict_id}', fields=fields),
             )
 
+    def check_output_dest(self, predict_id) -> bool:
+        try:
+            self.conn.table(f'_outputs.{predict_id}')
+            return True
+        except NoSuchTableError:
+            return False
+
     def create_table_and_schema(self, identifier: str, mapping: dict):
         """
         Create a schema in the data-backend.
@@ -131,3 +142,16 @@ class IbisDataBackend(BaseDataBackend):
 
     def list_tables_or_collections(self):
         return self.conn.list_tables()
+
+    @staticmethod
+    def infer_schema(data: t.Mapping[str, t.Any], identifier: t.Optional[str] = None):
+        """
+        Infer a schema from a given data object
+
+        :param data: The data object
+        :param identifier: The identifier for the schema, if None, it will be generated
+        :return: The inferred schema
+        """
+        from pinnacledb.misc.auto_schema import infer_schema
+
+        return infer_schema(data, identifier=identifier, ibis=True)

superduperdb/backends/mongodb/data_backend.py
@@ -9,6 +9,7 @@ from pinnacledb.backends.base.data_backend import BaseDataBackend
 from pinnacledb.backends.ibis.field_types import FieldType
 from pinnacledb.backends.mongodb.artifacts import MongoArtifactStore
 from pinnacledb.backends.mongodb.metadata import MongoMetaDataStore
+from pinnacledb.base.enums import DBType
 from pinnacledb.base.serializable import Serializable
 from pinnacledb.components.datatype import DataType
 from pinnacledb.misc.colors import Colors
@@ -22,6 +23,8 @@ class MongoDataBackend(BaseDataBackend):
     :param conn: MongoDB client connection
     :param name: Name of database to host filesystem
     """
+
+    db_type = DBType.MONGODB
 
     id_field = '_id'
 
@@ -129,8 +132,24 @@ class MongoDataBackend(BaseDataBackend):
 
     def create_output_dest(
         self,
-        identifier: str,
+        predict_id: str,
         datatype: t.Union[None, DataType, FieldType],
         flatten: bool = False,
     ):
         pass
+
+    def check_output_dest(self, predict_id) -> bool:
+        return True
+
+    @staticmethod
+    def infer_schema(data: t.Mapping[str, t.Any], identifier: t.Optional[str] = None):
+        """
+        Infer a schema from a given data object
+
+        :param data: The data object
+        :param identifier: The identifier for the schema, if None, it will be generated
+        :return: The inferred schema
+        """
+        from pinnacledb.misc.auto_schema import infer_schema
+
+        return infer_schema(data, identifier)

superduperdb/components/listener.py
@@ -5,7 +5,6 @@ from overrides import override
 
 from pinnacledb import CFG
 from pinnacledb.backends.base.query import CompoundSelect
-from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.base.document import _OUTPUTS_KEY
 from pinnacledb.base.serializable import Variable
 from pinnacledb.components.model import Mapping
@@ -16,6 +15,10 @@ from pinnacledb.rest.utils import parse_query
 from ..jobs.job import Job
 from .component import Component, ComponentTuple
 from .model import Model, ModelInputType
+
+if t.TYPE_CHECKING:
+    from pinnacledb.base.datalayer import Datalayer
+
 
 SELECT_TEMPLATE = {'documents': [], 'query': '<collection_name>.find()'}
 
@@ -113,7 +116,19 @@ class Listener(Component):
         """
         Get query reference to model outputs.
         """
-        return self.select.table_or_collection.outputs(self.predict_id)
+        if self.select.DB_TYPE == "SQL":
+            return self.select.table_or_collection.outputs(self.predict_id)
+
+        else:
+            from pinnacledb.backends.mongodb.query import Collection
+
+            model_update_kwargs = self.model.model_update_kwargs or {}
+            if model_update_kwargs.get('document_embedded', True):
+                collection_name = self.select.table_or_collection.identifier
+            else:
+                collection_name = self.outputs
+
+            return Collection(collection_name).find()
 
     @property
     def outputs_key(self):
@@ -126,7 +141,7 @@ class Listener(Component):
             return self.outputs
 
     @override
-    def pre_create(self, db: Datalayer) -> None:
+    def pre_create(self, db: "Datalayer") -> None:
         """
         Pre-create hook.
 
@@ -152,19 +167,13 @@ class Listener(Component):
         return key
 
     @override
-    def post_create(self, db: Datalayer) -> None:
+    def post_create(self, db: "Datalayer") -> None:
         """
         Post-create hook.
 
         :param db: Data layer instance.
         """
-        output_table = db.databackend.create_output_dest(
-            f'{self.identifier}::{self.version}',
-            self.model.datatype,
-            flatten=self.model.flatten,
-        )
-        if output_table is not None:
-            db.add(output_table)
+        self.create_output_dest(db, self.predict_id, self.model)
         if self.select is not None and self.active and not db.server_mode:
             if CFG.cluster.cdc.uri:
                 request_server(
@@ -175,6 +184,25 @@ class Listener(Component):
                 )
             else:
                 db.cdc.add(self)
+
+    @classmethod
+    def create_output_dest(cls, db: "Datalayer", predict_id, model: Model):
+        """
+        Create output destination.
+
+        :param db: Data layer instance.
+        :param predict_id: Predict ID.
+        :param model: Model instance.
+        """
+        if model.datatype is None:
+            return
+        output_table = db.databackend.create_output_dest(
+            predict_id,
+            model.datatype,
+            flatten=model.flatten,
+        )
+        if output_table is not None:
+            db.add(output_table)
 
     @property
     def dependencies(self) -> t.List[ComponentTuple]:
@@ -197,6 +225,18 @@ class Listener(Component):
         Get predict ID.
         """
         return f'{self.identifier}::{self.version}'
+
+    @classmethod
+    def from_predict_id(cls, db: "Datalayer", predict_id) -> 'Listener':
+        """
+        Split predict ID.
+
+        :param db: Data layer instance.
+        :param predict_id: Predict ID.
+        """
+
+        identifier, version = predict_id.rsplit('::', 1)
+        return t.cast(Listener, db.load('listener', identifier, version=int(version)))
 
     @property
     def id_key(self) -> str:
@@ -235,7 +275,10 @@ class Listener(Component):
 
     @override
     def schedule_jobs(
-        self, db: Datalayer, dependencies: t.Sequence[Job] = (), overwrite: bool = False
+        self,
+        db: "Datalayer",
+        dependencies: t.Sequence[Job] = (),
+        overwrite: bool = False,
     ) -> t.Sequence[t.Any]:
         """
         Schedule jobs for the listener.
@@ -251,7 +294,7 @@ class Listener(Component):
             self.model.predict_in_db_job(
                 X=self.key,
                 db=db,
-                predict_id=f'{self.identifier}::{self.version}',
+                predict_id=self.predict_id,
                 select=self.select.copy(),
                 dependencies=dependencies,
                 overwrite=overwrite,
@@ -260,7 +303,7 @@ class Listener(Component):
         ]
         return out
 
-    def cleanup(self, database: Datalayer) -> None:
+    def cleanup(self, database: "Datalayer") -> None:
         """Clean up when the listener is deleted.
 
         :param database: Data layer instance to process.

test/unittest/component/test_model.py
@@ -7,9 +7,9 @@ import numpy as np
 import pytest
 from sklearn.metrics import accuracy_score, f1_score
 
+from pinnacledb.backends.base.data_backend import BaseDataBackend
 from pinnacledb.backends.base.query import Select
 from pinnacledb.backends.ibis.field_types import FieldType
-from pinnacledb.backends.local.compute import LocalComputeBackend
 from pinnacledb.backends.mongodb.query import Collection
 from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.base.document import Document
@@ -144,10 +144,10 @@ def test_pm_create_predict_job(mock_job, predict_mixin):
 def test_pm_predict(predict_mixin):
     # Check the logic of predict method, the mock method will be tested below
     db = MagicMock(spec=Datalayer)
-    db.compute = MagicMock(spec=LocalComputeBackend)
-    db.metadata = MagicMock()
+    db.databackend = MagicMock(spec=BaseDataBackend)
     select = MagicMock(spec=Select)
     select.table_or_collection = MagicMock()
+    predict_mixin.db = db
 
     with patch.object(predict_mixin, 'predict') as predict_func:
         predict_mixin.predict_in_db('x', db=db, select=select, predict_id='test')
@@ -164,7 +164,9 @@ def test_pm_predict_with_select_ids(monkeypatch, predict_mixin):
 
     select = MagicMock(spec=Select)
     db = MagicMock(spec=Datalayer)
+    db.databackend = MagicMock(spec=BaseDataBackend)
     db.execute.return_value = docs
+    predict_mixin.db = db
 
     with patch.object(predict_mixin, 'object') as my_object:
         my_object.return_value = 2
@@ -417,6 +419,7 @@ def test_pm_predict_with_select_ids_multikey(monkeypatch, predict_mixin_multikey
 
         select = MagicMock(spec=Select)
         db = MagicMock(spec=Datalayer)
+        db.databackend = MagicMock(spec=BaseDataBackend)
         db.execute.return_value = docs
 
         # Check the base predict function