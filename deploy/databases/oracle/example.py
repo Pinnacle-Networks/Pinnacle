superduper-io/superduper

fix credentials and dependencies
Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

deploy/databases/oracle/example.py
@@ -1,8 +1,23 @@
-from superduperdb import superduper
+import ibis
 
 user = 'superduper'
 password = 'superduper'
 port = 1521
 host = 'localhost'
+database = 'test_db'
 
-db = superduper(f"oracle://{user}:{password}@{host}:{port}")
+# OK
+con = ibis.oracle.connect(
+    user = 'superduper',
+    password = 'superduper',
+    port = 1521,
+    host = 'localhost',
+    database = 'test_db',
+)
+
+# ERROR AttributeError: 'str' object has no attribute 'username'
+#con = ibis.connect(f"oracle://{user}:{password}@{host}:{port}/{database}")
+
+# ERROR ModuleNotFoundError: No module named 'ibis.backends.base'
+# from superduperdb import superduper
+#db = superduper(f"oracle://{user}:{password}@{host}:{port}/{database}")

Pinnacle-Networks/Pinnacle

deploy/databases/oracle/example.py
@@ -1,8 +1,23 @@
-from pinnacledb import pinnacle
+import ibis
 
 user = 'pinnacle'
 password = 'pinnacle'
 port = 1521
 host = 'localhost'
+database = 'test_db'
 
-db = pinnacle(f"oracle://{user}:{password}@{host}:{port}")
+# OK
+con = ibis.oracle.connect(
+    user = 'pinnacle',
+    password = 'pinnacle',
+    port = 1521,
+    host = 'localhost',
+    database = 'test_db',
+)
+
+# ERROR AttributeError: 'str' object has no attribute 'username'
+#con = ibis.connect(f"oracle://{user}:{password}@{host}:{port}/{database}")
+
+# ERROR ModuleNotFoundError: No module named 'ibis.backends.base'
+# from pinnacledb import pinnacle
+#db = pinnacle(f"oracle://{user}:{password}@{host}:{port}/{database}")