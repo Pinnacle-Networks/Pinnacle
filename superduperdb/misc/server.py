superduper-io/superduper

Fix smoke tests and server side requests

superduperdb/misc/server.py
@@ -6,8 +6,9 @@ import requests
 
 from superduperdb import CFG, logging
 from superduperdb.base import exceptions
-from superduperdb.ext.utils import superduperencode
+from superduperdb.misc.auto_schema import DEFAULT_DATATYPE
 
+primitives = (bool, str, int, float, type(None))
 
 @lru_cache(maxsize=None)
 def _handshake(service: str):
@@ -33,16 +34,14 @@ def _request_server(
     logging.debug(f'Trying to connect {service} at {url} method: {type}')
 
     if type == 'post':
-        data = superduperencode(data)
-        if isinstance(data, dict):
-            if '_content' in data:
-                try:
-                    data['_content']['bytes'] = base64.b64encode(
-                        data['_content']['bytes']
-                    ).decode()
-                except Exception as e:
-                    logging.error(str(data))
-                    raise e
+        if data is not None:
+            # TODO: Please use Document.encode with autoschema.
+
+            if not isinstance(data, primitives):
+                data  = DEFAULT_DATATYPE.encoder(data)  
+                data = {'b64data': base64.b64encode(data).decode()}
+                                                 
+
         response = requests.post(url, json=data, params=args)
         result = json.loads(response.content)
     else:

superduperdb/vector_search/server/service.py
@@ -1,10 +1,11 @@
 import math
 import typing as t
+import base64
 
 from fastapi import Request
 
+from superduperdb.misc.auto_schema import DEFAULT_DATATYPE
 from superduperdb.base.datalayer import Datalayer
-from superduperdb.ext.utils import superduperdecode
 from superduperdb.vector_search.base import VectorItem
 
 ListVectorType = t.Union[t.List[t.Union[float, int]], t.Dict]
@@ -21,7 +22,8 @@ def _vector_search(
 ) -> VectorSearchResultType:
     vi = db.fast_vector_searchers[vector_index]
     if by_array:
-        x = superduperdecode(x, db.datatypes)
+        x = x['b64data']
+        x = DEFAULT_DATATYPE.encoder(  base64.b64decode(x))
         ids, scores = vi.searcher.find_nearest_from_array(x, n=n)
     else:
         ids, scores = vi.searcher.find_nearest_from_id(x, n=n)

test/integration/backends/ibis/test_end_2_end.py
@@ -7,7 +7,7 @@ import torchvision
 
 from superduperdb import CFG
 from superduperdb.backends.ibis.field_types import dtype
-from superduperdb.backends.ibis.query import IbisQuery, RawSQL
+from superduperdb.backends.ibis.query import RawSQL
 from superduperdb.base.document import Document as D
 from superduperdb.components.listener import Listener
 from superduperdb.components.schema import Schema
@@ -152,19 +152,15 @@ def _end_2_end(db, memory_table=False):
     assert 'image' in result[0].unpack()
 
     # TODO: Make this work
-    '''
 
     q = t.select('id', 'image', 'age').filter(t.age > 25).outputs(listener2.outputs)
 
     # Get the results
     result = list(db.execute(q))
     assert listener2.outputs in result[0].unpack()
-    '''
 
     # Raw query
     # TODO: Support RawSQL
-    '''
-
     if not memory_table:
         query = RawSQL(query='SELECT id from my_table')
         rows = list(db.execute(query))
@@ -175,7 +171,6 @@ def _end_2_end(db, memory_table=False):
             {'id': '3'},
             {'id': '4'},
         ]
-    '''
 
 
 @pytest.mark.skipif(DO_SKIP, reason="skipping ibis tests if mongodb")
@@ -206,7 +201,7 @@ def test_nested_query(clean_cache):
     expr_ = q.compile(db)
 
     if not memory_table:
-        assert 'SELECT t0._fold, t0.id, t0.health, t0.age, t0.image' in str(
+        assert 'SELECT t0._fold, t0._schema, t0.id, t0.health, t0.age, t0.image' in str(
             expr_
         )
     else:

Pinnacle-Networks/Pinnacle

superduperdb/misc/server.py
@@ -6,8 +6,9 @@ import requests
 
 from pinnacledb import CFG, logging
 from pinnacledb.base import exceptions
-from pinnacledb.ext.utils import pinnacleencode
+from pinnacledb.misc.auto_schema import DEFAULT_DATATYPE
 
+primitives = (bool, str, int, float, type(None))
 
 @lru_cache(maxsize=None)
 def _handshake(service: str):
@@ -33,16 +34,14 @@ def _request_server(
     logging.debug(f'Trying to connect {service} at {url} method: {type}')
 
     if type == 'post':
-        data = pinnacleencode(data)
-        if isinstance(data, dict):
-            if '_content' in data:
-                try:
-                    data['_content']['bytes'] = base64.b64encode(
-                        data['_content']['bytes']
-                    ).decode()
-                except Exception as e:
-                    logging.error(str(data))
-                    raise e
+        if data is not None:
+            # TODO: Please use Document.encode with autoschema.
+
+            if not isinstance(data, primitives):
+                data  = DEFAULT_DATATYPE.encoder(data)  
+                data = {'b64data': base64.b64encode(data).decode()}
+                                                 
+
         response = requests.post(url, json=data, params=args)
         result = json.loads(response.content)
     else:

superduperdb/vector_search/server/service.py
@@ -1,10 +1,11 @@
 import math
 import typing as t
+import base64
 
 from fastapi import Request
 
+from pinnacledb.misc.auto_schema import DEFAULT_DATATYPE
 from pinnacledb.base.datalayer import Datalayer
-from pinnacledb.ext.utils import pinnacledecode
 from pinnacledb.vector_search.base import VectorItem
 
 ListVectorType = t.Union[t.List[t.Union[float, int]], t.Dict]
@@ -21,7 +22,8 @@ def _vector_search(
 ) -> VectorSearchResultType:
     vi = db.fast_vector_searchers[vector_index]
     if by_array:
-        x = pinnacledecode(x, db.datatypes)
+        x = x['b64data']
+        x = DEFAULT_DATATYPE.encoder(  base64.b64decode(x))
         ids, scores = vi.searcher.find_nearest_from_array(x, n=n)
     else:
         ids, scores = vi.searcher.find_nearest_from_id(x, n=n)

test/integration/backends/ibis/test_end_2_end.py
@@ -7,7 +7,7 @@ import torchvision
 
 from pinnacledb import CFG
 from pinnacledb.backends.ibis.field_types import dtype
-from pinnacledb.backends.ibis.query import IbisQuery, RawSQL
+from pinnacledb.backends.ibis.query import RawSQL
 from pinnacledb.base.document import Document as D
 from pinnacledb.components.listener import Listener
 from pinnacledb.components.schema import Schema
@@ -152,19 +152,15 @@ def _end_2_end(db, memory_table=False):
     assert 'image' in result[0].unpack()
 
     # TODO: Make this work
-    '''
 
     q = t.select('id', 'image', 'age').filter(t.age > 25).outputs(listener2.outputs)
 
     # Get the results
     result = list(db.execute(q))
     assert listener2.outputs in result[0].unpack()
-    '''
 
     # Raw query
     # TODO: Support RawSQL
-    '''
-
     if not memory_table:
         query = RawSQL(query='SELECT id from my_table')
         rows = list(db.execute(query))
@@ -175,7 +171,6 @@ def _end_2_end(db, memory_table=False):
             {'id': '3'},
             {'id': '4'},
         ]
-    '''
 
 
 @pytest.mark.skipif(DO_SKIP, reason="skipping ibis tests if mongodb")
@@ -206,7 +201,7 @@ def test_nested_query(clean_cache):
     expr_ = q.compile(db)
 
     if not memory_table:
-        assert 'SELECT t0._fold, t0.id, t0.health, t0.age, t0.image' in str(
+        assert 'SELECT t0._fold, t0._schema, t0.id, t0.health, t0.age, t0.image' in str(
             expr_
         )
     else: