superduper-io/superduper

Fixed some bugs

superduperdb/base/document.py
@@ -11,7 +11,7 @@ from superduperdb.components.datatype import (
     _BaseEncodable,
 )
 from superduperdb.misc.special_dicts import MongoStyleDict, SuperDuperFlatEncode
-from superduperdb.components.schema import Schema
+from superduperdb.components.schema import Schema, get_schema, SCHEMA_KEY
 
 if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
@@ -27,19 +27,6 @@ _LEAF_TYPES = {
 }
 _LEAF_TYPES.update(_ENCODABLES)
 _OUTPUTS_KEY = '_outputs'
-
-
-def get_schema(db, schema: t.Union['Schema', str]) -> 'Schema':
-    """Handle schema caching and loading."""
-    if isinstance(schema, Schema):
-        return schema
-    assert isinstance(schema, str)
-    if db is None:
-        raise ValueError(
-            f'A Datalayer instance is required for encoding with schema {schema}'
-        )
-
-    return db.schemas[schema]
 
 
 class Document(MongoStyleDict):
@@ -60,7 +47,7 @@ class Document(MongoStyleDict):
     ):
         super().__init__(*args, **kwargs)
         self.db = db
-        self.schema= schema
+        self.schema = schema
 
     def _deep_flat_encode(
         self,
@@ -73,12 +60,13 @@ class Document(MongoStyleDict):
         out = dict(self)
         if schema is not None:
             out = schema.deep_flat_encode_data(
-                self,
+                out,
                 cache,
                 blobs,
                 files,
                 leaves_to_keep=leaves_to_keep,
             )
+            pass
         return _deep_flat_encode(
             out, cache, blobs, files, leaves_to_keep=leaves_to_keep, schema=schema
         )
@@ -127,6 +115,8 @@ class Document(MongoStyleDict):
             files = r['_files']
             del r['_files']
 
+        schema = schema or r.get(SCHEMA_KEY)
+        schema = get_schema(db, schema)
         if schema is not None:
             schema.init()
             r = schema.decode_data(r)
@@ -277,7 +267,11 @@ def _deep_flat_decode(r, cache, blobs, files={}, db: t.Optional['Datalayer'] = N
         module = '.'.join(parts[:-1])
         dict_ = {k: v for k, v in r.items() if k != '_path'}
         dict_ = _deep_flat_decode(dict_, cache, blobs, files, db=db)
-        return _import_item(cls=cls, module=module, dict=dict_, db=db)
+        instence = _import_item(cls=cls, module=module, dict=dict_, db=db)
+        # TODO: Auto unpack the instence
+        # instence.unpack()
+        return instence
+
     if isinstance(r, dict):
         return {
             k: _deep_flat_decode(v, cache, blobs, files, db=db) for k, v in r.items()

superduperdb/components/schema.py
@@ -6,15 +6,21 @@ from overrides import override
 
 from superduperdb.components.component import Component
 from superduperdb.components.datatype import DataType
-from superduperdb.misc.special_dicts import SuperDuperFlatEncode
 from superduperdb.misc.annotations import public_api
+from superduperdb.misc.special_dicts import SuperDuperFlatEncode
+
+SCHEMA_KEY = '_schema'
+
 
 class _Native:
     _TYPES = {str: 'str', int: 'int', float: 'float'}
+
     def __init__(self, x):
         if x in self._TYPES:
             x = self._TYPES[x]
-        self.identifier =  x
+        self.identifier = x
+
+
 
 @public_api(stability='beta')
 @dc.dataclass(kw_only=True)
@@ -40,7 +46,6 @@ class Schema(Component):
                 self.fields[k] = _Native(v)
             elif v in (str, bool, int, float):
                 self.fields[k] = _Native(v)
-
 
     @override
     def pre_create(self, db) -> None:
@@ -74,6 +79,7 @@ class Schema(Component):
                 r[k] = encodable._deep_flat_encode(
                     cache, blobs, files, leaves_to_keep=leaves_to_keep, schema=self
                 )
+        r[SCHEMA_KEY] = self.identifier
         return r
 
     @cached_property
@@ -107,7 +113,7 @@ class Schema(Component):
                 # TODO: We need to sort out the logic here
                 # We use encodable_cls to encode the data, but we the decoder here
                 # decoded[k] = field.encodable_cls.decode(data[k])
-                decoded[k] = field.decoder(data[k])
+                decoded[k] = field.decode_data(data[k])
             else:
                 decoded[k] = data[k]
         return decoded
@@ -140,3 +146,18 @@ class Schema(Component):
         encoded_data['_blobs'] = blobs
         encoded_data['_files'] = files
         return SuperDuperFlatEncode(encoded_data)
+
+def get_schema(db, schema: t.Union[Schema, str]) -> t.Optional[Schema]:
+    """Handle schema caching and loading."""
+    if schema is None:
+        return None
+    if isinstance(schema, Schema):
+        return schema
+    assert isinstance(schema, str)
+    if db is None:
+        raise ValueError(
+            f'A Datalayer instance is required for encoding with schema {schema}'
+        )
+
+    return db.schemas[schema]
+

superduperdb/jobs/tasks.py
@@ -1,5 +1,6 @@
 import traceback
 import typing as t
+from superduperdb.base.leaf import Leaf
 
 if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
@@ -45,6 +46,8 @@ def method_job(
         db = build_datalayer(cfg=cfg, cluster__compute__uri=None)
 
     component = db.load(type_id, identifier)
+    component = t.cast(Leaf, component)
+    component.unpack()
     method = getattr(component, method_name)
     db.metadata.update_job(job_id, 'status', 'running')
 

test/unittest/misc/test_downloaders.py
@@ -8,7 +8,7 @@ import pytest
 from superduperdb import CFG
 from superduperdb.backends.mongodb.query import MongoQuery
 from superduperdb.base.document import Document
-from superduperdb.ext.pillow.encoder import pil_image_hybrid
+from superduperdb.ext.pillow.encoder import pil_image
 from superduperdb.misc.download import Fetcher
 
 remote = os.environ.get('SDDB_REMOTE_TEST', 'local')
@@ -30,34 +30,10 @@ def patch_cfg_downloads(monkeypatch):
 # TODO: use table to test the sqldb
 @pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
 def test_file_blobs(db, patch_cfg_downloads, image_url):
-    
-    to_insert = [
-        Document(
-            {
-                'item': {
-                    '_content': {
-                        'uri': image_url,
-                        'datatype': 'pil_image_hybrid',
-                        'leaf_type': 'encodable',
-                    }
-                },
-                'other': {
-                    'item': {
-                        '_content': {
-                            'uri': image_url,
-                            'datatype': 'pil_image_hybrid',
-                            'leaf_type': 'encodable',
-                        }
-                    }
-                },
-            }
-        )
-        for _ in range(2)
-    ]
+    db.apply(pil_image)
+    to_insert = [Document({"item": pil_image(uri=image_url)}) for _ in range(2)]
 
-    db.execute(
-        MongoQuery('documents').insert_many(to_insert), datatypes=(pil_image_hybrid,)
-    )
+    db.execute(MongoQuery('documents').insert_many(to_insert))
     r = db.execute(MongoQuery('documents').find_one())
 
     import PIL.PngImagePlugin

Pinnacle-Networks/Pinnacle

superduperdb/base/document.py
@@ -11,7 +11,7 @@ from pinnacledb.components.datatype import (
     _BaseEncodable,
 )
 from pinnacledb.misc.special_dicts import MongoStyleDict, SuperDuperFlatEncode
-from pinnacledb.components.schema import Schema
+from pinnacledb.components.schema import Schema, get_schema, SCHEMA_KEY
 
 if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
@@ -27,19 +27,6 @@ _LEAF_TYPES = {
 }
 _LEAF_TYPES.update(_ENCODABLES)
 _OUTPUTS_KEY = '_outputs'
-
-
-def get_schema(db, schema: t.Union['Schema', str]) -> 'Schema':
-    """Handle schema caching and loading."""
-    if isinstance(schema, Schema):
-        return schema
-    assert isinstance(schema, str)
-    if db is None:
-        raise ValueError(
-            f'A Datalayer instance is required for encoding with schema {schema}'
-        )
-
-    return db.schemas[schema]
 
 
 class Document(MongoStyleDict):
@@ -60,7 +47,7 @@ class Document(MongoStyleDict):
     ):
         super().__init__(*args, **kwargs)
         self.db = db
-        self.schema= schema
+        self.schema = schema
 
     def _deep_flat_encode(
         self,
@@ -73,12 +60,13 @@ class Document(MongoStyleDict):
         out = dict(self)
         if schema is not None:
             out = schema.deep_flat_encode_data(
-                self,
+                out,
                 cache,
                 blobs,
                 files,
                 leaves_to_keep=leaves_to_keep,
             )
+            pass
         return _deep_flat_encode(
             out, cache, blobs, files, leaves_to_keep=leaves_to_keep, schema=schema
         )
@@ -127,6 +115,8 @@ class Document(MongoStyleDict):
             files = r['_files']
             del r['_files']
 
+        schema = schema or r.get(SCHEMA_KEY)
+        schema = get_schema(db, schema)
         if schema is not None:
             schema.init()
             r = schema.decode_data(r)
@@ -277,7 +267,11 @@ def _deep_flat_decode(r, cache, blobs, files={}, db: t.Optional['Datalayer'] = N
         module = '.'.join(parts[:-1])
         dict_ = {k: v for k, v in r.items() if k != '_path'}
         dict_ = _deep_flat_decode(dict_, cache, blobs, files, db=db)
-        return _import_item(cls=cls, module=module, dict=dict_, db=db)
+        instence = _import_item(cls=cls, module=module, dict=dict_, db=db)
+        # TODO: Auto unpack the instence
+        # instence.unpack()
+        return instence
+
     if isinstance(r, dict):
         return {
             k: _deep_flat_decode(v, cache, blobs, files, db=db) for k, v in r.items()

superduperdb/components/schema.py
@@ -6,15 +6,21 @@ from overrides import override
 
 from pinnacledb.components.component import Component
 from pinnacledb.components.datatype import DataType
-from pinnacledb.misc.special_dicts import SuperDuperFlatEncode
 from pinnacledb.misc.annotations import public_api
+from pinnacledb.misc.special_dicts import SuperDuperFlatEncode
+
+SCHEMA_KEY = '_schema'
+
 
 class _Native:
     _TYPES = {str: 'str', int: 'int', float: 'float'}
+
     def __init__(self, x):
         if x in self._TYPES:
             x = self._TYPES[x]
-        self.identifier =  x
+        self.identifier = x
+
+
 
 @public_api(stability='beta')
 @dc.dataclass(kw_only=True)
@@ -40,7 +46,6 @@ class Schema(Component):
                 self.fields[k] = _Native(v)
             elif v in (str, bool, int, float):
                 self.fields[k] = _Native(v)
-
 
     @override
     def pre_create(self, db) -> None:
@@ -74,6 +79,7 @@ class Schema(Component):
                 r[k] = encodable._deep_flat_encode(
                     cache, blobs, files, leaves_to_keep=leaves_to_keep, schema=self
                 )
+        r[SCHEMA_KEY] = self.identifier
         return r
 
     @cached_property
@@ -107,7 +113,7 @@ class Schema(Component):
                 # TODO: We need to sort out the logic here
                 # We use encodable_cls to encode the data, but we the decoder here
                 # decoded[k] = field.encodable_cls.decode(data[k])
-                decoded[k] = field.decoder(data[k])
+                decoded[k] = field.decode_data(data[k])
             else:
                 decoded[k] = data[k]
         return decoded
@@ -140,3 +146,18 @@ class Schema(Component):
         encoded_data['_blobs'] = blobs
         encoded_data['_files'] = files
         return SuperDuperFlatEncode(encoded_data)
+
+def get_schema(db, schema: t.Union[Schema, str]) -> t.Optional[Schema]:
+    """Handle schema caching and loading."""
+    if schema is None:
+        return None
+    if isinstance(schema, Schema):
+        return schema
+    assert isinstance(schema, str)
+    if db is None:
+        raise ValueError(
+            f'A Datalayer instance is required for encoding with schema {schema}'
+        )
+
+    return db.schemas[schema]
+

superduperdb/jobs/tasks.py
@@ -1,5 +1,6 @@
 import traceback
 import typing as t
+from pinnacledb.base.leaf import Leaf
 
 if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
@@ -45,6 +46,8 @@ def method_job(
         db = build_datalayer(cfg=cfg, cluster__compute__uri=None)
 
     component = db.load(type_id, identifier)
+    component = t.cast(Leaf, component)
+    component.unpack()
     method = getattr(component, method_name)
     db.metadata.update_job(job_id, 'status', 'running')
 

test/unittest/misc/test_downloaders.py
@@ -8,7 +8,7 @@ import pytest
 from pinnacledb import CFG
 from pinnacledb.backends.mongodb.query import MongoQuery
 from pinnacledb.base.document import Document
-from pinnacledb.ext.pillow.encoder import pil_image_hybrid
+from pinnacledb.ext.pillow.encoder import pil_image
 from pinnacledb.misc.download import Fetcher
 
 remote = os.environ.get('SDDB_REMOTE_TEST', 'local')
@@ -30,34 +30,10 @@ def patch_cfg_downloads(monkeypatch):
 # TODO: use table to test the sqldb
 @pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
 def test_file_blobs(db, patch_cfg_downloads, image_url):
-    
-    to_insert = [
-        Document(
-            {
-                'item': {
-                    '_content': {
-                        'uri': image_url,
-                        'datatype': 'pil_image_hybrid',
-                        'leaf_type': 'encodable',
-                    }
-                },
-                'other': {
-                    'item': {
-                        '_content': {
-                            'uri': image_url,
-                            'datatype': 'pil_image_hybrid',
-                            'leaf_type': 'encodable',
-                        }
-                    }
-                },
-            }
-        )
-        for _ in range(2)
-    ]
+    db.apply(pil_image)
+    to_insert = [Document({"item": pil_image(uri=image_url)}) for _ in range(2)]
 
-    db.execute(
-        MongoQuery('documents').insert_many(to_insert), datatypes=(pil_image_hybrid,)
-    )
+    db.execute(MongoQuery('documents').insert_many(to_insert))
     r = db.execute(MongoQuery('documents').find_one())
 
     import PIL.PngImagePlugin