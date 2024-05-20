superduper-io/superduper

Deprecate Serializable

superduperdb/misc/download.py
@@ -14,9 +14,10 @@ import requests
 from tqdm import tqdm
 
 from superduperdb import CFG, logging
-from superduperdb.backends.base.query import Insert, Select
+from superduperdb.backends.base.query import Query
 from superduperdb.base.document import Document
-from superduperdb.base.serializable import Serializable
+from superduperdb.base.leaf import Leaf
+from superduperdb.components.datatype import _BaseEncodable
 
 
 class TimeoutException(Exception):
@@ -322,8 +323,7 @@ def _gather_uris_for_document(r: Document, id_field: str = '_id'):
     uris = []
     keys = []
     datatypes = []
-    leaf_lookup = r.get_leaves('encodable')
-    leaf_lookup.update(r.get_leaves('artifact'))
+    leaf_lookup = r.encode(leaves_to_keep=_BaseEncodable)['_leaves']
     for k in leaf_lookup:
         if leaf_lookup[k].uri is None:
             continue
@@ -335,7 +335,7 @@ def _gather_uris_for_document(r: Document, id_field: str = '_id'):
 
 def download_content(
     db,
-    query: t.Union[Select, Insert, t.Dict],
+    query: t.Union[Query, t.Dict],
     ids: t.Optional[t.Sequence[str]] = None,
     documents: t.Optional[t.List[Document]] = None,
     raises: bool = True,
@@ -367,12 +367,13 @@ def download_content(
     logging.debug(str(query))
     logging.debug(str(ids))
 
+    # TODO handle this in the job runner
     if isinstance(query, dict):
-        query = Serializable.decode(query)
+        query = Document.decode(query).unpack()
 
     if documents is not None:
         pass
-    elif isinstance(query, Select):
+    elif isinstance(query, Query) and query.type == 'select':
         if ids is None:
             # TODO deprecate reference since lazy loading in any case
             documents = list(db.execute(query))
@@ -380,7 +381,7 @@ def download_content(
             select = query.select_using_ids(ids)
             documents = list(db.execute(select))
     else:
-        assert isinstance(query, Insert)
+        assert query.type == 'insert'
         documents = t.cast(t.List[Document], query.documents)
 
     uris, keys, datatypes, place_ids = gather_uris(documents)

test/conftest.py
@@ -11,9 +11,9 @@ import pytest
 import superduperdb as s
 from superduperdb import CFG, logging
 from superduperdb.backends.ibis.field_types import dtype
-from superduperdb.backends.ibis.query import Table
+from superduperdb.components.table import Table
 from superduperdb.backends.mongodb.data_backend import MongoDataBackend
-from superduperdb.backends.mongodb.query import Collection
+from superduperdb.backends.mongodb.query import MongoQuery
 
 # ruff: noqa: E402
 from superduperdb.base import config as _config, superduper
@@ -122,7 +122,7 @@ def test_db(request) -> Iterator[Datalayer]:
 @pytest.fixture
 def valid_dataset(db):
     if isinstance(db.databackend, MongoDataBackend):
-        select = Collection('documents').find({'_fold': 'valid'})
+        select = MongoQuery('documents').find({'_fold': 'valid'})
     else:
         table = db.load('table', 'documents')
         select = table.select('id', 'x', 'y', 'z').filter(table._fold == 'valid')
@@ -139,7 +139,7 @@ def add_random_data_to_sql_db(
     table_name: str = 'documents',
     number_data_points: int = GLOBAL_TEST_N_DATA_POINTS,
 ):
-    float_tensor = db.datatypes['torch.float32[32]']
+    float_tensor = tensor(dtype='float', shape=(32,))
     data = []
 
     schema = Schema(
@@ -154,24 +154,21 @@ def add_random_data_to_sql_db(
     t = Table(identifier=table_name, schema=schema)
     db.add(t)
 
-    for i in range(number_data_points):
-        x = torch.randn(32)
-        y = int(random.random() > 0.5)
-        z = torch.randn(32)
-        data.append(
-            Document(
-                {
-                    'id': str(i),
-                    'x': x,
-                    'y': y,
-                    'z': z,
-                }
-            )
-        )
-    db.execute(
-        t.insert(data),
-        refresh=False,
-    )
+    # for i in range(number_data_points):
+    #     x = torch.randn(32)
+    #     y = int(random.random() > 0.5)
+    #     z = torch.randn(32)
+    #     data.append(
+    #         Document(
+    #             {
+    #                 'id': str(i),
+    #                 'x': x,
+    #                 'y': y,
+    #                 'z': z,
+    #             }
+    #         )
+    #     )
+    # db[table_name].insert(data).execute()
 
 
 def add_random_data_to_mongo_db(
@@ -179,9 +176,9 @@ def add_random_data_to_mongo_db(
     collection_name: str = 'documents',
     number_data_points: int = GLOBAL_TEST_N_DATA_POINTS,
 ):
-    float_tensor = db.datatypes['torch.float32[32]']
     data = []
 
+    float_tensor = tensor(dtype='float', shape=(32,))
     for i in range(number_data_points):
         x = torch.randn(32)
         y = int(random.random() > 0.5)
@@ -197,22 +194,22 @@ def add_random_data_to_mongo_db(
         )
 
     db.execute(
-        Collection(collection_name).insert_many(data),
+        MongoQuery(identifier=collection_name).insert_many(data),
         refresh=False,
     )
 
 
 def add_datatypes(db: Datalayer):
     for n in [8, 16, 32]:
-        db.add(tensor(torch.float, shape=(n,)))
-    db.add(pil_image)
+        db.apply(tensor(dtype='float', shape=(n,)))
+    db.apply(pil_image)
 
 
 def add_models(db: Datalayer):
     # identifier, weight_shape, encoder
     params = [
-        ['linear_a', (32, 16), db.load('datatype', 'torch.float32[16]')],
-        ['linear_b', (16, 8), db.load('datatype', 'torch.float32[8]')],
+        ['linear_a', (32, 16), tensor(dtype='float', shape=(16,))],
+        ['linear_b', (16, 8), tensor(dtype='float', shape=(8,))],
     ]
     for identifier, weight_shape, datatype in params:
         m = TorchModel(
@@ -230,8 +227,8 @@ def add_vector_index(
     # TODO: Support configurable key and mode
     is_mongodb_backend = isinstance(db.databackend, MongoDataBackend)
     if is_mongodb_backend:
-        select_x = Collection(collection_name).find()
-        select_z = Collection(collection_name).find()
+        select_x = MongoQuery(collection_name).find()
+        select_z = MongoQuery(collection_name).find()
     else:
         table = db.load('table', collection_name).to_query()
         select_x = table.select('id', 'x')

test/unittest/backends/mongodb/test_new_query.py
@@ -1,0 +1,18 @@
+from superduperdb.backends.mongodb.new_query import Collection
+
+
+def test_add_fold():
+
+    coll = Collection(identifier='test_coll')
+
+    q = coll.find().limit(5)
+
+    new_q = q.add_fold('valid')
+
+    print(new_q)
+
+    assert str(new_q) == "test_coll.find({'_fold': 'valid'}).limit(5)"
+
+
+def test_add():
+    ...

test/unittest/backends/mongodb/test_query.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 
 from superduperdb.backends.mongodb import query as q
-from superduperdb.backends.mongodb.query import Collection
+from superduperdb.backends.mongodb.query import MongoQuery
 from superduperdb.base.config import BytesEncoding
 from superduperdb.base.document import Document
 from superduperdb.components.schema import Schema
@@ -50,9 +50,9 @@ def test_mongo_without_schema(db):
         )
 
     db.execute(
-        Collection(collection_name).insert_many(data),
+        MongoQuery(collection_name).insert_many(data),
     )
-    collection = Collection(collection_name)
+    collection = MongoQuery(collection_name)
     r = collection.find_one().execute(db)
     rs = list(collection.find().execute(db))
 
@@ -91,9 +91,9 @@ def test_mongo_schema(db, schema):
     db.add(schema)
 
     db.execute(
-        Collection(collection_name).insert_many(data, schema=schema.identifier),
+        MongoQuery(collection_name).insert_many(data, schema=schema.identifier),
     )
-    collection = Collection(collection_name)
+    collection = MongoQuery(collection_name)
     r = collection.find_one().execute(db)
     rs = list(collection.find().execute(db))
 
@@ -107,15 +107,15 @@ def test_mongo_schema(db, schema):
 
 
 def test_select_missing_outputs(db):
-    docs = list(db.execute(q.Collection('documents').find({}, {'_id': 1})))
+    docs = list(db.execute(q.MongoQuery('documents').find({}, {'_id': 1})))
     ids = [r['_id'] for r in docs[: len(docs) // 2]]
     db.execute(
-        q.Collection('documents').update_many(
+        q.MongoQuery('documents').update_many(
             {'_id': {'$in': ids}},
             Document({'$set': {'_outputs.x::test_model_output::0::0': 'test'}}),
         )
     )
-    select = q.Collection('documents').find({}, {'_id': 1})
+    select = q.MongoQuery('documents').find({}, {'_id': 1})
     modified_select = select.select_ids_of_missing_outputs('x::test_model_output::0::0')
     out = list(db.execute(modified_select))
     assert len(out) == (len(docs) - len(ids))

Pinnacle-Networks/Pinnacle

superduperdb/misc/download.py
@@ -14,9 +14,10 @@ import requests
 from tqdm import tqdm
 
 from pinnacledb import CFG, logging
-from pinnacledb.backends.base.query import Insert, Select
+from pinnacledb.backends.base.query import Query
 from pinnacledb.base.document import Document
-from pinnacledb.base.serializable import Serializable
+from pinnacledb.base.leaf import Leaf
+from pinnacledb.components.datatype import _BaseEncodable
 
 
 class TimeoutException(Exception):
@@ -352,8 +353,7 @@ def _gather_uris_for_document(r: Document, id_field: str = '_id'):
     uris = []
     keys = []
     datatypes = []
-    leaf_lookup = r.get_leaves('encodable')
-    leaf_lookup.update(r.get_leaves('artifact'))
+    leaf_lookup = r.encode(leaves_to_keep=_BaseEncodable)['_leaves']
     for k in leaf_lookup:
         if leaf_lookup[k].uri is None:
             continue
@@ -365,7 +365,7 @@ def _gather_uris_for_document(r: Document, id_field: str = '_id'):
 
 def download_content(
     db,
-    query: t.Union[Select, Insert, t.Dict],
+    query: t.Union[Query, t.Dict],
     ids: t.Optional[t.Sequence[str]] = None,
     documents: t.Optional[t.List[Document]] = None,
     raises: bool = True,
@@ -394,12 +394,13 @@ def download_content(
     logging.debug(str(query))
     logging.debug(str(ids))
 
+    # TODO handle this in the job runner
     if isinstance(query, dict):
-        query = Serializable.decode(query)
+        query = Document.decode(query).unpack()
 
     if documents is not None:
         pass
-    elif isinstance(query, Select):
+    elif isinstance(query, Query) and query.type == 'select':
         if ids is None:
             # TODO deprecate reference since lazy loading in any case
             documents = list(db.execute(query))
@@ -407,7 +408,7 @@ def download_content(
             select = query.select_using_ids(ids)
             documents = list(db.execute(select))
     else:
-        assert isinstance(query, Insert)
+        assert query.type == 'insert'
         documents = t.cast(t.List[Document], query.documents)
 
     uris, keys, datatypes, place_ids = gather_uris(documents)

test/conftest.py
@@ -11,9 +11,9 @@ import pytest
 import pinnacledb as s
 from pinnacledb import CFG, logging
 from pinnacledb.backends.ibis.field_types import dtype
-from pinnacledb.backends.ibis.query import Table
+from pinnacledb.components.table import Table
 from pinnacledb.backends.mongodb.data_backend import MongoDataBackend
-from pinnacledb.backends.mongodb.query import Collection
+from pinnacledb.backends.mongodb.query import MongoQuery
 
 # ruff: noqa: E402
 from pinnacledb.base import config as _config, pinnacle
@@ -122,7 +122,7 @@ def test_db(request) -> Iterator[Datalayer]:
 @pytest.fixture
 def valid_dataset(db):
     if isinstance(db.databackend, MongoDataBackend):
-        select = Collection('documents').find({'_fold': 'valid'})
+        select = MongoQuery('documents').find({'_fold': 'valid'})
     else:
         table = db.load('table', 'documents')
         select = table.select('id', 'x', 'y', 'z').filter(table._fold == 'valid')
@@ -139,7 +139,7 @@ def add_random_data_to_sql_db(
     table_name: str = 'documents',
     number_data_points: int = GLOBAL_TEST_N_DATA_POINTS,
 ):
-    float_tensor = db.datatypes['torch.float32[32]']
+    float_tensor = tensor(dtype='float', shape=(32,))
     data = []
 
     schema = Schema(
@@ -154,24 +154,21 @@ def add_random_data_to_sql_db(
     t = Table(identifier=table_name, schema=schema)
     db.add(t)
 
-    for i in range(number_data_points):
-        x = torch.randn(32)
-        y = int(random.random() > 0.5)
-        z = torch.randn(32)
-        data.append(
-            Document(
-                {
-                    'id': str(i),
-                    'x': x,
-                    'y': y,
-                    'z': z,
-                }
-            )
-        )
-    db.execute(
-        t.insert(data),
-        refresh=False,
-    )
+    # for i in range(number_data_points):
+    #     x = torch.randn(32)
+    #     y = int(random.random() > 0.5)
+    #     z = torch.randn(32)
+    #     data.append(
+    #         Document(
+    #             {
+    #                 'id': str(i),
+    #                 'x': x,
+    #                 'y': y,
+    #                 'z': z,
+    #             }
+    #         )
+    #     )
+    # db[table_name].insert(data).execute()
 
 
 def add_random_data_to_mongo_db(
@@ -179,9 +176,9 @@ def add_random_data_to_mongo_db(
     collection_name: str = 'documents',
     number_data_points: int = GLOBAL_TEST_N_DATA_POINTS,
 ):
-    float_tensor = db.datatypes['torch.float32[32]']
     data = []
 
+    float_tensor = tensor(dtype='float', shape=(32,))
     for i in range(number_data_points):
         x = torch.randn(32)
         y = int(random.random() > 0.5)
@@ -197,22 +194,22 @@ def add_random_data_to_mongo_db(
         )
 
     db.execute(
-        Collection(collection_name).insert_many(data),
+        MongoQuery(identifier=collection_name).insert_many(data),
         refresh=False,
     )
 
 
 def add_datatypes(db: Datalayer):
     for n in [8, 16, 32]:
-        db.add(tensor(torch.float, shape=(n,)))
-    db.add(pil_image)
+        db.apply(tensor(dtype='float', shape=(n,)))
+    db.apply(pil_image)
 
 
 def add_models(db: Datalayer):
     # identifier, weight_shape, encoder
     params = [
-        ['linear_a', (32, 16), db.load('datatype', 'torch.float32[16]')],
-        ['linear_b', (16, 8), db.load('datatype', 'torch.float32[8]')],
+        ['linear_a', (32, 16), tensor(dtype='float', shape=(16,))],
+        ['linear_b', (16, 8), tensor(dtype='float', shape=(8,))],
     ]
     for identifier, weight_shape, datatype in params:
         m = TorchModel(
@@ -230,8 +227,8 @@ def add_vector_index(
     # TODO: Support configurable key and mode
     is_mongodb_backend = isinstance(db.databackend, MongoDataBackend)
     if is_mongodb_backend:
-        select_x = Collection(collection_name).find()
-        select_z = Collection(collection_name).find()
+        select_x = MongoQuery(collection_name).find()
+        select_z = MongoQuery(collection_name).find()
     else:
         table = db.load('table', collection_name).to_query()
         select_x = table.select('id', 'x')

test/unittest/backends/mongodb/test_new_query.py
@@ -1,0 +1,18 @@
+from pinnacledb.backends.mongodb.new_query import Collection
+
+
+def test_add_fold():
+
+    coll = Collection(identifier='test_coll')
+
+    q = coll.find().limit(5)
+
+    new_q = q.add_fold('valid')
+
+    print(new_q)
+
+    assert str(new_q) == "test_coll.find({'_fold': 'valid'}).limit(5)"
+
+
+def test_add():
+    ...

test/unittest/backends/mongodb/test_query.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 
 from pinnacledb.backends.mongodb import query as q
-from pinnacledb.backends.mongodb.query import Collection
+from pinnacledb.backends.mongodb.query import MongoQuery
 from pinnacledb.base.config import BytesEncoding
 from pinnacledb.base.document import Document
 from pinnacledb.components.schema import Schema
@@ -50,9 +50,9 @@ def test_mongo_without_schema(db):
         )
 
     db.execute(
-        Collection(collection_name).insert_many(data),
+        MongoQuery(collection_name).insert_many(data),
     )
-    collection = Collection(collection_name)
+    collection = MongoQuery(collection_name)
     r = collection.find_one().execute(db)
     rs = list(collection.find().execute(db))
 
@@ -91,9 +91,9 @@ def test_mongo_schema(db, schema):
     db.add(schema)
 
     db.execute(
-        Collection(collection_name).insert_many(data, schema=schema.identifier),
+        MongoQuery(collection_name).insert_many(data, schema=schema.identifier),
     )
-    collection = Collection(collection_name)
+    collection = MongoQuery(collection_name)
     r = collection.find_one().execute(db)
     rs = list(collection.find().execute(db))
 
@@ -107,15 +107,15 @@ def test_mongo_schema(db, schema):
 
 
 def test_select_missing_outputs(db):
-    docs = list(db.execute(q.Collection('documents').find({}, {'_id': 1})))
+    docs = list(db.execute(q.MongoQuery('documents').find({}, {'_id': 1})))
     ids = [r['_id'] for r in docs[: len(docs) // 2]]
     db.execute(
-        q.Collection('documents').update_many(
+        q.MongoQuery('documents').update_many(
             {'_id': {'$in': ids}},
             Document({'$set': {'_outputs.x::test_model_output::0::0': 'test'}}),
         )
     )
-    select = q.Collection('documents').find({}, {'_id': 1})
+    select = q.MongoQuery('documents').find({}, {'_id': 1})
     modified_select = select.select_ids_of_missing_outputs('x::test_model_output::0::0')
     out = list(db.execute(modified_select))
     assert len(out) == (len(docs) - len(ids))