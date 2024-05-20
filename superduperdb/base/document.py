superduper-io/superduper

Fixed docstrings and type checking

superduperdb/base/document.py
@@ -10,8 +10,8 @@ from superduperdb.components.datatype import (
     Encodable,
     _BaseEncodable,
 )
+from superduperdb.components.schema import SCHEMA_KEY, Schema, get_schema
 from superduperdb.misc.special_dicts import MongoStyleDict, SuperDuperFlatEncode
-from superduperdb.components.schema import Schema, get_schema, SCHEMA_KEY
 
 if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
@@ -32,8 +32,11 @@ _OUTPUTS_KEY = '_outputs'
 class Document(MongoStyleDict):
     """A wrapper around an instance of dict or a Encodable.
 
-    The document data is used to dump that resource to a mix of json-able content, ids and `bytes`
+    The document data is used to dump that resource to
+    a mix of json-able content, ids and `bytes`
 
+    :param schema: The schema to use.
+    :param db: The datalayer to use.
     """
 
     _DEFAULT_ID_KEY: str = '_id'
@@ -76,9 +79,17 @@ class Document(MongoStyleDict):
         schema: t.Optional[t.Union['Schema', str]] = None,
         leaves_to_keep: t.Sequence = (),
     ) -> SuperDuperFlatEncode:
-        cache = {}
-        blobs = {}
-        files = {}
+        """Encode the document to a format that can be used in a database.
+
+        After encoding everything is a vanilla dictionary (JSON + bytes).
+        (Even a model, or artifact etc..)
+
+        :param schema: The schema to use.
+        :param leaves_to_keep: The types of leaves to keep.
+        """
+        cache: t.Dict[str, dict] = {}
+        blobs: t.Dict[str, bytes] = {}
+        files: t.Dict[str, str] = {}
 
         # Get schema from database.
         schema = self.schema or schema
@@ -99,6 +110,12 @@ class Document(MongoStyleDict):
     def decode(
         cls, r, schema: t.Optional['Schema'] = None, db: t.Optional['Datalayer'] = None
     ):
+        """Converts any dictionary into a Document or a Leaf.
+
+        :param r: The encoded data.
+        :param schema: The schema to use.
+        :param db: The datalayer to use.
+        """
         cache = {}
         blobs = {}
         files = {}
@@ -152,7 +169,6 @@ class Document(MongoStyleDict):
     def unpack(self, leaves_to_keep: t.Sequence = ()) -> t.Any:
         """Returns the content, but with any encodables replaced by their contents.
 
-        :param db: The datalayer to use.
         :param leaves_to_keep: The types of leaves to keep.
         """
         out = _unpack(self, leaves_to_keep=leaves_to_keep)
@@ -162,8 +178,18 @@ class Document(MongoStyleDict):
 
 
 class QueryUpdateDocument(Document):
+    """A document that is used to update a document in a database.
+
+    This document is used to update a document in a database.
+    It is a subclass of Document.
+    """
+
     @classmethod
     def from_document(cls, document):
+        """Create a QueryUpdateDocument from a document.
+
+        :param document: The document to create the QueryUpdateDocument from.
+        """
         r = dict(document)
         r = r.get('$set', r)
         return QueryUpdateDocument(r)
@@ -195,6 +221,12 @@ class QueryUpdateDocument(Document):
         schema: t.Optional['Schema'] = None,
         leaves_to_keep: t.Sequence = (),
     ) -> t.Dict:
+        """Encode the document to a format that can be used in a database.
+
+        :param original: The original document.
+        :param schema: The schema to use.
+        :param leaves_to_keep: The types of leaves to keep.
+        """
         r = dict(self)
         r = r.get('$set', r)
         update = super().encode(schema=schema, leaves_to_keep=leaves_to_keep)
@@ -202,7 +234,9 @@ class QueryUpdateDocument(Document):
 
 
 def _unpack(item: t.Any, db=None, leaves_to_keep: t.Sequence = ()) -> t.Any:
-    if isinstance(item, _BaseEncodable) and not any([isinstance(item, l) for l in leaves_to_keep]):  # type: ignore[arg-type]
+    if isinstance(item, _BaseEncodable) and not any(
+        [isinstance(item, leaf) for leaf in leaves_to_keep]
+    ):
         return item.unpack()
     elif isinstance(item, dict):
         return {k: _unpack(v, leaves_to_keep=leaves_to_keep) for k, v in item.items()}
@@ -280,5 +314,7 @@ def _deep_flat_decode(r, cache, blobs, files={}, db: t.Optional['Datalayer'] = N
         return _get_leaf_from_cache(r[1:], cache, blobs, files, db=db)
     if isinstance(r, str) and r.startswith('%'):
         uuid = r.split('/')[-1]
+        if db is None:
+            raise ValueError(f'No database provided to decode {r}')
         return db.load(uuid=uuid)
     return r

superduperdb/components/graph.py
@@ -3,7 +3,7 @@ import typing as t
 
 import networkx as nx
 
-from superduperdb import Schema, Document
+from superduperdb import Schema
 from superduperdb.backends.base.query import Query
 from superduperdb.backends.query_dataset import QueryDataset
 from superduperdb.components.model import Model, Signature, ensure_initialized

superduperdb/components/table.py
@@ -1,10 +1,10 @@
 import dataclasses as dc
 import typing as t
 
+from superduperdb import logging
+from superduperdb.backends.ibis.field_types import dtype
 from superduperdb.components.component import Component
 from superduperdb.components.schema import Schema, _Native
-from superduperdb.backends.ibis.field_types import dtype
-from superduperdb import logging
 
 if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
@@ -14,6 +14,13 @@ DEFAULT_PRIMARY_ID = 'id'
 
 @dc.dataclass(kw_only=True)
 class Table(Component):
+    """
+    A component that represents a table in a database.
+
+    :param schema: The schema of the table
+    :param primary_id: The primary id of the table
+    """
+
     type_id: t.ClassVar[str] = 'table'
     schema: Schema
     primary_id: str = DEFAULT_PRIMARY_ID
@@ -38,6 +45,11 @@ class Table(Component):
         assert self.primary_id != '_input_id', '"_input_id" is a reserved value'
 
     def pre_create(self, db: 'Datalayer'):
+        """
+        Create the table in the database.
+
+        :param db: The datalayer isinstance
+        """
         assert self.schema is not None, "Schema must be set"
         # TODO why? This is done already
         for e in self.schema.encoders:

superduperdb/jobs/tasks.py
@@ -1,6 +1,5 @@
 import traceback
 import typing as t
-from superduperdb.base.leaf import Leaf
 
 if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
@@ -46,7 +45,10 @@ def method_job(
         db = build_datalayer(cfg=cfg, cluster__compute__uri=None)
 
     component = db.load(type_id, identifier)
-    component = t.cast(Leaf, component)
+    # TODO: Move the unpack method to the better place
+    from superduperdb.components.component import Component
+
+    component = t.cast(Component, component)
     component.unpack()
     method = getattr(component, method_name)
     db.metadata.update_job(job_id, 'status', 'running')

test/integration/backends/ibis/test_end_2_end.py
@@ -7,7 +7,7 @@ import torchvision
 
 from superduperdb import CFG
 from superduperdb.backends.ibis.field_types import dtype
-from superduperdb.backends.ibis.query import RawSQL, IbisQuery
+from superduperdb.backends.ibis.query import IbisQuery, RawSQL
 from superduperdb.base.document import Document as D
 from superduperdb.components.listener import Listener
 from superduperdb.components.schema import Schema

test/unittest/backends/base/test_query.py
@@ -4,11 +4,10 @@ import lorem
 import pytest
 
 from superduperdb.backends.ibis.field_types import dtype
-
-from superduperdb.components.table import Table
 from superduperdb.backends.mongodb.query import MongoQuery
 from superduperdb.base.document import Document
 from superduperdb.components.schema import Schema
+from superduperdb.components.table import Table
 
 
 @pytest.fixture

test/unittest/component/test_model.py
@@ -7,13 +7,13 @@ import numpy as np
 import pytest
 from sklearn.metrics import accuracy_score, f1_score
 
+from superduperdb.backends.base.data_backend import BaseDataBackend
 from superduperdb.backends.base.query import Query
 from superduperdb.backends.ibis.field_types import FieldType
 from superduperdb.backends.local.compute import LocalComputeBackend
 from superduperdb.backends.mongodb.query import MongoQuery
 from superduperdb.base.datalayer import Datalayer
 from superduperdb.base.document import Document
-from superduperdb.backends.base.data_backend import BaseDataBackend
 from superduperdb.base.variables import Variable
 from superduperdb.components.dataset import Dataset
 from superduperdb.components.datatype import DataType, pickle_decode, pickle_encode

Pinnacle-Networks/Pinnacle

superduperdb/base/document.py
@@ -10,8 +10,8 @@ from pinnacledb.components.datatype import (
     Encodable,
     _BaseEncodable,
 )
+from pinnacledb.components.schema import SCHEMA_KEY, Schema, get_schema
 from pinnacledb.misc.special_dicts import MongoStyleDict, SuperDuperFlatEncode
-from pinnacledb.components.schema import Schema, get_schema, SCHEMA_KEY
 
 if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
@@ -32,8 +32,11 @@ _OUTPUTS_KEY = '_outputs'
 class Document(MongoStyleDict):
     """A wrapper around an instance of dict or a Encodable.
 
-    The document data is used to dump that resource to a mix of json-able content, ids and `bytes`
+    The document data is used to dump that resource to
+    a mix of json-able content, ids and `bytes`
 
+    :param schema: The schema to use.
+    :param db: The datalayer to use.
     """
 
     _DEFAULT_ID_KEY: str = '_id'
@@ -76,9 +79,17 @@ class Document(MongoStyleDict):
         schema: t.Optional[t.Union['Schema', str]] = None,
         leaves_to_keep: t.Sequence = (),
     ) -> SuperDuperFlatEncode:
-        cache = {}
-        blobs = {}
-        files = {}
+        """Encode the document to a format that can be used in a database.
+
+        After encoding everything is a vanilla dictionary (JSON + bytes).
+        (Even a model, or artifact etc..)
+
+        :param schema: The schema to use.
+        :param leaves_to_keep: The types of leaves to keep.
+        """
+        cache: t.Dict[str, dict] = {}
+        blobs: t.Dict[str, bytes] = {}
+        files: t.Dict[str, str] = {}
 
         # Get schema from database.
         schema = self.schema or schema
@@ -99,6 +110,12 @@ class Document(MongoStyleDict):
     def decode(
         cls, r, schema: t.Optional['Schema'] = None, db: t.Optional['Datalayer'] = None
     ):
+        """Converts any dictionary into a Document or a Leaf.
+
+        :param r: The encoded data.
+        :param schema: The schema to use.
+        :param db: The datalayer to use.
+        """
         cache = {}
         blobs = {}
         files = {}
@@ -152,7 +169,6 @@ class Document(MongoStyleDict):
     def unpack(self, leaves_to_keep: t.Sequence = ()) -> t.Any:
         """Returns the content, but with any encodables replaced by their contents.
 
-        :param db: The datalayer to use.
         :param leaves_to_keep: The types of leaves to keep.
         """
         out = _unpack(self, leaves_to_keep=leaves_to_keep)
@@ -162,8 +178,18 @@ class Document(MongoStyleDict):
 
 
 class QueryUpdateDocument(Document):
+    """A document that is used to update a document in a database.
+
+    This document is used to update a document in a database.
+    It is a subclass of Document.
+    """
+
     @classmethod
     def from_document(cls, document):
+        """Create a QueryUpdateDocument from a document.
+
+        :param document: The document to create the QueryUpdateDocument from.
+        """
         r = dict(document)
         r = r.get('$set', r)
         return QueryUpdateDocument(r)
@@ -195,6 +221,12 @@ class QueryUpdateDocument(Document):
         schema: t.Optional['Schema'] = None,
         leaves_to_keep: t.Sequence = (),
     ) -> t.Dict:
+        """Encode the document to a format that can be used in a database.
+
+        :param original: The original document.
+        :param schema: The schema to use.
+        :param leaves_to_keep: The types of leaves to keep.
+        """
         r = dict(self)
         r = r.get('$set', r)
         update = super().encode(schema=schema, leaves_to_keep=leaves_to_keep)
@@ -202,7 +234,9 @@ class QueryUpdateDocument(Document):
 
 
 def _unpack(item: t.Any, db=None, leaves_to_keep: t.Sequence = ()) -> t.Any:
-    if isinstance(item, _BaseEncodable) and not any([isinstance(item, l) for l in leaves_to_keep]):  # type: ignore[arg-type]
+    if isinstance(item, _BaseEncodable) and not any(
+        [isinstance(item, leaf) for leaf in leaves_to_keep]
+    ):
         return item.unpack()
     elif isinstance(item, dict):
         return {k: _unpack(v, leaves_to_keep=leaves_to_keep) for k, v in item.items()}
@@ -280,5 +314,7 @@ def _deep_flat_decode(r, cache, blobs, files={}, db: t.Optional['Datalayer'] = N
         return _get_leaf_from_cache(r[1:], cache, blobs, files, db=db)
     if isinstance(r, str) and r.startswith('%'):
         uuid = r.split('/')[-1]
+        if db is None:
+            raise ValueError(f'No database provided to decode {r}')
         return db.load(uuid=uuid)
     return r

superduperdb/components/graph.py
@@ -3,7 +3,7 @@ import typing as t
 
 import networkx as nx
 
-from pinnacledb import Schema, Document
+from pinnacledb import Schema
 from pinnacledb.backends.base.query import Query
 from pinnacledb.backends.query_dataset import QueryDataset
 from pinnacledb.components.model import Model, Signature, ensure_initialized

superduperdb/components/table.py
@@ -1,10 +1,10 @@
 import dataclasses as dc
 import typing as t
 
+from pinnacledb import logging
+from pinnacledb.backends.ibis.field_types import dtype
 from pinnacledb.components.component import Component
 from pinnacledb.components.schema import Schema, _Native
-from pinnacledb.backends.ibis.field_types import dtype
-from pinnacledb import logging
 
 if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
@@ -14,6 +14,13 @@ DEFAULT_PRIMARY_ID = 'id'
 
 @dc.dataclass(kw_only=True)
 class Table(Component):
+    """
+    A component that represents a table in a database.
+
+    :param schema: The schema of the table
+    :param primary_id: The primary id of the table
+    """
+
     type_id: t.ClassVar[str] = 'table'
     schema: Schema
     primary_id: str = DEFAULT_PRIMARY_ID
@@ -38,6 +45,11 @@ class Table(Component):
         assert self.primary_id != '_input_id', '"_input_id" is a reserved value'
 
     def pre_create(self, db: 'Datalayer'):
+        """
+        Create the table in the database.
+
+        :param db: The datalayer isinstance
+        """
         assert self.schema is not None, "Schema must be set"
         # TODO why? This is done already
         for e in self.schema.encoders:

superduperdb/jobs/tasks.py
@@ -1,6 +1,5 @@
 import traceback
 import typing as t
-from pinnacledb.base.leaf import Leaf
 
 if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
@@ -46,7 +45,10 @@ def method_job(
         db = build_datalayer(cfg=cfg, cluster__compute__uri=None)
 
     component = db.load(type_id, identifier)
-    component = t.cast(Leaf, component)
+    # TODO: Move the unpack method to the better place
+    from pinnacledb.components.component import Component
+
+    component = t.cast(Component, component)
     component.unpack()
     method = getattr(component, method_name)
     db.metadata.update_job(job_id, 'status', 'running')

test/integration/backends/ibis/test_end_2_end.py
@@ -7,7 +7,7 @@ import torchvision
 
 from pinnacledb import CFG
 from pinnacledb.backends.ibis.field_types import dtype
-from pinnacledb.backends.ibis.query import RawSQL, IbisQuery
+from pinnacledb.backends.ibis.query import IbisQuery, RawSQL
 from pinnacledb.base.document import Document as D
 from pinnacledb.components.listener import Listener
 from pinnacledb.components.schema import Schema

test/unittest/backends/base/test_query.py
@@ -4,11 +4,10 @@ import lorem
 import pytest
 
 from pinnacledb.backends.ibis.field_types import dtype
-
-from pinnacledb.components.table import Table
 from pinnacledb.backends.mongodb.query import MongoQuery
 from pinnacledb.base.document import Document
 from pinnacledb.components.schema import Schema
+from pinnacledb.components.table import Table
 
 
 @pytest.fixture

test/unittest/component/test_model.py
@@ -7,13 +7,13 @@ import numpy as np
 import pytest
 from sklearn.metrics import accuracy_score, f1_score
 
+from pinnacledb.backends.base.data_backend import BaseDataBackend
 from pinnacledb.backends.base.query import Query
 from pinnacledb.backends.ibis.field_types import FieldType
 from pinnacledb.backends.local.compute import LocalComputeBackend
 from pinnacledb.backends.mongodb.query import MongoQuery
 from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.base.document import Document
-from pinnacledb.backends.base.data_backend import BaseDataBackend
 from pinnacledb.base.variables import Variable
 from pinnacledb.components.dataset import Dataset
 from pinnacledb.components.datatype import DataType, pickle_decode, pickle_encode