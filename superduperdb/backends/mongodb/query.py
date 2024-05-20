superduper-io/superduper

Add `.export` & `.read` to `Component`

superduperdb/backends/mongodb/query.py
@@ -1,12 +1,12 @@
-import dataclasses as dc
 import copy
+import dataclasses as dc
 import typing as t
 from collections import defaultdict
-from superduperdb import logging
 
-from bson import ObjectId
 import pymongo
+from bson import ObjectId
 
+from superduperdb import logging
 from superduperdb.backends.base.query import (
     Query,
     applies_to,
@@ -34,6 +34,7 @@ def parse_query(query, documents, db: t.Optional['Datalayer'] = None):
         builder_cls=MongoQuery,
         db=db,
     )
+
 
 @dc.dataclass
 class ChangeStream:
@@ -168,7 +169,6 @@ class MongoQuery(Query):
         result.scores = similar_scores
         return result
 
-
     def _execute_post_like(self, parent):
         assert len(self.parts) == 2
         assert self.parts[0][0] == 'find'
@@ -210,6 +210,7 @@ class MongoQuery(Query):
 
         result.scores = scores
         return result
+
     def _execute_bulk_write(self, parent):
         """Execute the query.
 
@@ -223,7 +224,7 @@ class MongoQuery(Query):
                 raise ValueError(
                     'Please provided update/delete id in args',
                     'all ids selection e.g `\{\}` is not supported',
-                    )
+                )
 
         collection = self.db.databackend.get_table_or_collection(
             self.table_or_collection.identifier
@@ -260,7 +261,6 @@ class MongoQuery(Query):
                 'wont trigger any `model/listeners` unless CDC is active.',
             )
         return bulk_result, bulk_update_ids, bulk_delete_ids
-
 
     def _execute_find(self, parent):
         return self._execute(parent, method='unpack')
@@ -330,9 +330,7 @@ class MongoQuery(Query):
         return ids
 
     def change_stream(self, *args, **kwargs):
-        '''
-        Return a callable Mongodb change stream instance.
-        '''
+        """Return a callable Mongodb change stream instance."""
         return ChangeStream(collection=self.identifier, args=args, kwargs=kwargs)
 
     @applies_to('find')
@@ -526,7 +524,6 @@ class MongoQuery(Query):
                     UpdateOne(
                         filter=mongo_filter,
                         update=update,
-
                     )
                 )
             return self.table_or_collection.bulk_write(bulk_operations)
@@ -627,9 +624,9 @@ def UpdateOne(**kwargs):
     """
     try:
         filter = kwargs['filter']
-    except:
-        raise KeyError('Filter not found in `UpdateOne`')
-    
+    except Exception as e:
+        raise KeyError('Filter not found in `UpdateOne`') from e
+
     id = filter['_id']
     if isinstance(id, ObjectId):
         ids = [id]
@@ -679,11 +676,9 @@ class BulkOp(Leaf):
     @property
     def op(self):
         """Return the operation."""
-
         kwargs = copy.deepcopy(self.kwargs)
         kwargs.pop('arg_ids')
         for k, v in kwargs.items():
             if isinstance(v, Document):
                 kwargs[k] = v.unpack()
         return getattr(pymongo, self.identifier)(**kwargs)
-

superduperdb/components/component.py
@@ -249,49 +249,71 @@ class Component(Leaf):
         cache[self._id] = r
         return f'?{self._id}'
 
-    def _to_dict_and_bytes(self):
-        r = self.deep_flat_encode()
-        bytes = {}
-        for leaf in r['_leaves']:
-            if 'bytes' in leaf['dict']:
-                bytes[leaf['dict']['file_id']] = leaf['dict']['bytes']
-                del leaf['dict']['bytes']
-        return r, bytes
-
-    def export(self, format=None):
-        """Method to export the component in the provided format.
-
-        If format is None, the method exports the component in a dictionary.
-
-        :param format: `json` and `yaml`.
+    @staticmethod
+    def read(path: str):
         """
-        r, bytes = self._to_dict_and_bytes()
+        Read a `Component` instance from a directory created with `.export`.
 
-        if format is None:
-            return r, bytes
+        :param path: Path to the directory containing the component.
 
-        if self.version is None:
-            path = f'_component.{self.type_id}.{self.identifier}'
-        else:
-            path = f'_component.{self.type_id}.{self.identifier}.{self.version}'
+        Expected directory structure:
+        ```
+        |_component.json/yaml
+        |_blobs/*
+        |_files/*
+        ```
+        """
+        try:
+            config = os.path.join(path, 'component.json')
+            with open(config) as f:
+                config_object = json.load(f)
+        except FileNotFoundError:
+            try:
+                config = os.path.join(path, 'component.yaml')
+                with open(config) as f:
+                    config_object = yaml.safe_load(f)
+            except FileNotFoundError as e:
+                raise FileNotFoundError(
+                    'No component.json or component.yaml found'
+                ) from e
 
+        config_object['_blobs'] = {}
+        if os.path.exists(os.path.join(path, 'blobs')):
+            blobs = {}
+            for file_id in os.listdir(os.path.join(path, 'blobs')):
+                with open(os.path.join(path, 'blobs', file_id), 'rb') as f:
+                    blobs[file_id] = f.read()
+            config_object['_blobs'] = blobs
+
+        from superduperdb import Document
+
+        return Document.decode(config_object).unpack()
+
+    def export(self, path: str):
+        """
+        Save `self` to a directory using super-duper protocol.
+
+        :param path: Path to the directory to save the component.
+
+        Created directory structure:
+        ```
+        |_component.json/yaml
+        |_blobs/*
+        |_files/*
+        ```
+        """
+        r = self.encode()
         os.makedirs(path, exist_ok=True)
+        if r.blobs:
+            os.makedirs(os.path.join(path, 'blobs'), exist_ok=True)
+            for file_id, bytestr_ in r.blobs.items():
+                filepath = os.path.join(path, 'blobs', file_id)
+                with open(filepath, 'wb') as f:
+                    f.write(bytestr_)
 
-        for file_id in bytes:
-            with open(f'{path}/{file_id}', 'wb') as f:
-                f.write(bytes[file_id])
-
-        if format == 'json':
-            with open(f'{path}/component.json', 'w') as f:
-                f.write(json.dumps(r, indent=4))
-            return r, bytes
-
-        if format == 'yaml':
-            with open(f'{path}/component.yaml', 'w') as f:
-                f.write(yaml.safe_dump(r))
-            return r, bytes
-
-        raise NotImplementedError(f'Format {format} not supported')
+        r.pop_blobs()
+        with open(os.path.join(path, 'component.json'), 'w') as f:
+            json.dump(r, f, indent=2)
 
     def dict(self) -> 'Document':
         """A dictionary representation of the component."""

test/unittest/component/test_component.py
@@ -1,6 +1,7 @@
 import dataclasses as dc
 import os
 import shutil
+import tempfile
 import typing as t
 from test.db_config import DBConfig
 
@@ -10,10 +11,12 @@ from superduperdb import ObjectModel
 from superduperdb.components.component import Component
 from superduperdb.components.datatype import (
     Artifact,
+    DataType,
     Empty,
     LazyArtifact,
     dill_serializer,
 )
+from superduperdb.ext.torch.encoder import tensor
 
 
 @pytest.fixture
@@ -82,3 +85,23 @@ def test_load_lazily(db):
     reloaded.init()
 
     assert callable(reloaded.object)
+
+
+def test_export_and_read():
+    m = ObjectModel(
+        'test', object=lambda x: x + 2, datatype=tensor(dtype='float', shape=(32,))
+    )
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        save_path = os.path.join(tmpdir, 'tmp_save')
+        m.export(save_path)
+        assert os.path.exists(os.path.join(tmpdir, 'tmp_save', 'blobs'))
+
+        reloaded = Component.read(save_path)
+
+        assert isinstance(reloaded, ObjectModel)
+        assert isinstance(reloaded.datatype, DataType)
+
+    reloaded_from_hr = Component.read('test/material/data/hr_component')
+
+    assert isinstance(reloaded_from_hr, ObjectModel)

Pinnacle-Networks/Pinnacle

superduperdb/backends/mongodb/query.py
@@ -1,12 +1,12 @@
-import dataclasses as dc
 import copy
+import dataclasses as dc
 import typing as t
 from collections import defaultdict
-from pinnacledb import logging
 
-from bson import ObjectId
 import pymongo
+from bson import ObjectId
 
+from pinnacledb import logging
 from pinnacledb.backends.base.query import (
     Query,
     applies_to,
@@ -34,6 +34,7 @@ def parse_query(query, documents, db: t.Optional['Datalayer'] = None):
         builder_cls=MongoQuery,
         db=db,
     )
+
 
 @dc.dataclass
 class ChangeStream:
@@ -168,7 +169,6 @@ class MongoQuery(Query):
         result.scores = similar_scores
         return result
 
-
     def _execute_post_like(self, parent):
         assert len(self.parts) == 2
         assert self.parts[0][0] == 'find'
@@ -210,6 +210,7 @@ class MongoQuery(Query):
 
         result.scores = scores
         return result
+
     def _execute_bulk_write(self, parent):
         """Execute the query.
 
@@ -223,7 +224,7 @@ class MongoQuery(Query):
                 raise ValueError(
                     'Please provided update/delete id in args',
                     'all ids selection e.g `\{\}` is not supported',
-                    )
+                )
 
         collection = self.db.databackend.get_table_or_collection(
             self.table_or_collection.identifier
@@ -260,7 +261,6 @@ class MongoQuery(Query):
                 'wont trigger any `model/listeners` unless CDC is active.',
             )
         return bulk_result, bulk_update_ids, bulk_delete_ids
-
 
     def _execute_find(self, parent):
         return self._execute(parent, method='unpack')
@@ -330,9 +330,7 @@ class MongoQuery(Query):
         return ids
 
     def change_stream(self, *args, **kwargs):
-        '''
-        Return a callable Mongodb change stream instance.
-        '''
+        """Return a callable Mongodb change stream instance."""
         return ChangeStream(collection=self.identifier, args=args, kwargs=kwargs)
 
     @applies_to('find')
@@ -526,7 +524,6 @@ class MongoQuery(Query):
                     UpdateOne(
                         filter=mongo_filter,
                         update=update,
-
                     )
                 )
             return self.table_or_collection.bulk_write(bulk_operations)
@@ -627,9 +624,9 @@ def UpdateOne(**kwargs):
     """
     try:
         filter = kwargs['filter']
-    except:
-        raise KeyError('Filter not found in `UpdateOne`')
-    
+    except Exception as e:
+        raise KeyError('Filter not found in `UpdateOne`') from e
+
     id = filter['_id']
     if isinstance(id, ObjectId):
         ids = [id]
@@ -679,11 +676,9 @@ class BulkOp(Leaf):
     @property
     def op(self):
         """Return the operation."""
-
         kwargs = copy.deepcopy(self.kwargs)
         kwargs.pop('arg_ids')
         for k, v in kwargs.items():
             if isinstance(v, Document):
                 kwargs[k] = v.unpack()
         return getattr(pymongo, self.identifier)(**kwargs)
-

superduperdb/components/component.py
@@ -249,49 +249,71 @@ class Component(Leaf):
         cache[self._id] = r
         return f'?{self._id}'
 
-    def _to_dict_and_bytes(self):
-        r = self.deep_flat_encode()
-        bytes = {}
-        for leaf in r['_leaves']:
-            if 'bytes' in leaf['dict']:
-                bytes[leaf['dict']['file_id']] = leaf['dict']['bytes']
-                del leaf['dict']['bytes']
-        return r, bytes
-
-    def export(self, format=None):
-        """Method to export the component in the provided format.
-
-        If format is None, the method exports the component in a dictionary.
-
-        :param format: `json` and `yaml`.
+    @staticmethod
+    def read(path: str):
         """
-        r, bytes = self._to_dict_and_bytes()
+        Read a `Component` instance from a directory created with `.export`.
 
-        if format is None:
-            return r, bytes
+        :param path: Path to the directory containing the component.
 
-        if self.version is None:
-            path = f'_component.{self.type_id}.{self.identifier}'
-        else:
-            path = f'_component.{self.type_id}.{self.identifier}.{self.version}'
+        Expected directory structure:
+        ```
+        |_component.json/yaml
+        |_blobs/*
+        |_files/*
+        ```
+        """
+        try:
+            config = os.path.join(path, 'component.json')
+            with open(config) as f:
+                config_object = json.load(f)
+        except FileNotFoundError:
+            try:
+                config = os.path.join(path, 'component.yaml')
+                with open(config) as f:
+                    config_object = yaml.safe_load(f)
+            except FileNotFoundError as e:
+                raise FileNotFoundError(
+                    'No component.json or component.yaml found'
+                ) from e
 
+        config_object['_blobs'] = {}
+        if os.path.exists(os.path.join(path, 'blobs')):
+            blobs = {}
+            for file_id in os.listdir(os.path.join(path, 'blobs')):
+                with open(os.path.join(path, 'blobs', file_id), 'rb') as f:
+                    blobs[file_id] = f.read()
+            config_object['_blobs'] = blobs
+
+        from pinnacledb import Document
+
+        return Document.decode(config_object).unpack()
+
+    def export(self, path: str):
+        """
+        Save `self` to a directory using super-duper protocol.
+
+        :param path: Path to the directory to save the component.
+
+        Created directory structure:
+        ```
+        |_component.json/yaml
+        |_blobs/*
+        |_files/*
+        ```
+        """
+        r = self.encode()
         os.makedirs(path, exist_ok=True)
+        if r.blobs:
+            os.makedirs(os.path.join(path, 'blobs'), exist_ok=True)
+            for file_id, bytestr_ in r.blobs.items():
+                filepath = os.path.join(path, 'blobs', file_id)
+                with open(filepath, 'wb') as f:
+                    f.write(bytestr_)
 
-        for file_id in bytes:
-            with open(f'{path}/{file_id}', 'wb') as f:
-                f.write(bytes[file_id])
-
-        if format == 'json':
-            with open(f'{path}/component.json', 'w') as f:
-                f.write(json.dumps(r, indent=4))
-            return r, bytes
-
-        if format == 'yaml':
-            with open(f'{path}/component.yaml', 'w') as f:
-                f.write(yaml.safe_dump(r))
-            return r, bytes
-
-        raise NotImplementedError(f'Format {format} not supported')
+        r.pop_blobs()
+        with open(os.path.join(path, 'component.json'), 'w') as f:
+            json.dump(r, f, indent=2)
 
     def dict(self) -> 'Document':
         """A dictionary representation of the component."""

test/unittest/component/test_component.py
@@ -1,6 +1,7 @@
 import dataclasses as dc
 import os
 import shutil
+import tempfile
 import typing as t
 from test.db_config import DBConfig
 
@@ -10,10 +11,12 @@ from pinnacledb import ObjectModel
 from pinnacledb.components.component import Component
 from pinnacledb.components.datatype import (
     Artifact,
+    DataType,
     Empty,
     LazyArtifact,
     dill_serializer,
 )
+from pinnacledb.ext.torch.encoder import tensor
 
 
 @pytest.fixture
@@ -82,3 +85,23 @@ def test_load_lazily(db):
     reloaded.init()
 
     assert callable(reloaded.object)
+
+
+def test_export_and_read():
+    m = ObjectModel(
+        'test', object=lambda x: x + 2, datatype=tensor(dtype='float', shape=(32,))
+    )
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        save_path = os.path.join(tmpdir, 'tmp_save')
+        m.export(save_path)
+        assert os.path.exists(os.path.join(tmpdir, 'tmp_save', 'blobs'))
+
+        reloaded = Component.read(save_path)
+
+        assert isinstance(reloaded, ObjectModel)
+        assert isinstance(reloaded.datatype, DataType)
+
+    reloaded_from_hr = Component.read('test/material/data/hr_component')
+
+    assert isinstance(reloaded_from_hr, ObjectModel)