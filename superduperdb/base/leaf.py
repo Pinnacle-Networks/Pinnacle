superduper-io/superduper

Complete deep_flat_encode

superduperdb/base/leaf.py
@@ -1,14 +1,45 @@
+import dataclasses as dc
+import importlib
 import inspect
 import typing as t
 from abc import ABC, abstractmethod, abstractproperty
+from superduperdb.misc.serialization import asdict
 
 _CLASS_REGISTRY = {}
 
 
+def _import_item(cls, module, dict):
+    module = importlib.import_module(module)
+    cls = getattr(module, cls)
+    try:
+        return cls(**dict)
+    except TypeError as e:
+        if 'got an unexpected keyword argument' in str(e):
+            if callable(cls):
+                return cls(**{k: v for k, v in dict.items() if k in inspect.signature(cls).parameters})
+            return cls(**{k: v for k, v in dict.items() if k in inspect.signature(cls.__init__).parameters})
+        raise e
+
+
+@dc.dataclass
 class Leaf(ABC):
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         cls._register_class()
+
+    def _deep_flat_encode(self, cache, blobs, files):
+        from superduperdb.base.document import _deep_flat_encode
+        r = dict(self.dict())
+        return _deep_flat_encode(r, cache, blobs, files)
+
+    def dict(self):
+        from superduperdb import Document
+        r = asdict(self)
+        return Document({
+            'module': self.__class__.__module__,
+            'cls': self.__class__.__name__,
+            'dict': r
+        })
 
     @classmethod
     def handle_integration(cls, r):

superduperdb/components/vector_index.py
@@ -12,7 +12,7 @@ from superduperdb.components.listener import Listener
 from superduperdb.components.model import Mapping, ModelInputType
 from superduperdb.ext.utils import str_shape
 from superduperdb.jobs.job import FunctionJob
-from superduperdb.misc.annotations import public_api, ui
+from superduperdb.misc.annotations import public_api, component
 from superduperdb.misc.special_dicts import MongoStyleDict
 from superduperdb.vector_search.base import VectorIndexMeasureType
 from superduperdb.vector_search.update_tasks import copy_vectors
@@ -223,7 +223,7 @@ class DecodeArray:
         return np.frombuffer(bytes, dtype=self.dtype).tolist()
 
 
-@ui(
+@component(
     {'name': 'shape', 'type': 'int'},
     {'name': 'identifier', 'type': 'str'},
 )

superduperdb/ext/pillow/encoder.py
@@ -6,7 +6,7 @@ import PIL.JpegImagePlugin
 import PIL.PngImagePlugin
 
 from superduperdb.components.datatype import DataType
-from superduperdb.misc.annotations import ui
+from superduperdb.misc.annotations import component
 
 BLANK_IMAGE = PIL.Image.new('RGB', (600, 600), (255, 255, 255))
 
@@ -70,7 +70,7 @@ pil_image_hybrid_jpeg = DataType(
 )
 
 
-@ui(
+@component(
     {'name': 'identifier', 'type': 'str'},
     {'name': 'media_type', 'type': 'str', 'default': 'image/png'},
 )

superduperdb/misc/annotations.py
@@ -1,4 +1,5 @@
 import functools
+import hashlib
 import importlib
 import sys
 import typing as t
@@ -10,6 +11,7 @@ from packaging import version
 
 from superduperdb import logging
 from superduperdb.base.exceptions import RequiredPackageVersionsNotFound
+from superduperdb.components.component import Component
 
 
 def _normalize_module(import_module, lower_bound, upper_bound, install_module):
@@ -179,11 +181,29 @@ def _get_indent(docstring: str) -> int:
     return len(non_empty_lines[1]) - len(non_empty_lines[1].lstrip())
 
 
-def ui(*schema: t.Dict, handle_integration: t.Callable = lambda x: x):
-    def decorated(f):
-        f.get_ui_schema = lambda: schema
-        f.build = lambda r: f(**r)
-        f.handle_integration = handle_integration
-        return f
+def component(*schema: t.Dict, handle_integration: t.Callable = lambda x: x):
+    def decorator(f):
 
-    return decorated
+        @functools.wraps(f)
+        def decorated(*args, **kwargs):
+            assert not args, 'Can\'t use *args here'
+            out = f(**kwargs)
+            assert isinstance(out, Component)
+            def _deep_flat_encode(cache, blobs, files):
+                h = hashlib.sha1(str(kwargs).encode()).hexdigest()
+                path = f'{f.__module__}.{f.__name__}'
+                id = f'{path}/{h}'
+                cache[id] = {
+                    'cls': f.__name__,
+                    'module': f.__module__,
+                    'dict': kwargs,
+                }
+                return f'?{id}'
+            out._deep_flat_encode = _deep_flat_encode
+            return out
+        
+        decorated.get_ui_schema = lambda: schema
+        decorated.build = lambda r: f(**r)
+        decorated.handle_integration = handle_integration
+        return decorated
+    return decorator

test/unittest/base/test_document.py
@@ -1,6 +1,8 @@
 import dataclasses as dc
 import pprint
 import typing as t
+from superduperdb.components.datatype import LazyArtifact
+from superduperdb.components.schema import Schema
 from test.db_config import DBConfig
 
 import pytest
@@ -38,63 +40,6 @@ def test_document_encoding(document):
     assert (new_document['x'].x - document['x'].x).sum() == 0
 
 
-def my_function(x):
-    return x + 2
-
-
-def test_encode_decode_flattened(db):
-    m = ObjectModel(
-        identifier='test',
-        object=lambda x: x,
-        datatype=vector(384),
-    )
-
-    r, bytes = m.export(format='json')
-    pprint.pprint(r)
-
-    for file_id in bytes:
-        db.artifact_store.save_artifact({'bytes': bytes[file_id], 'datatype': 'dill'})
-
-    r['_leaves'] = _build_leaves(r['_leaves'], db)[0]
-    pprint.pprint(r['_leaves'][r['_base']])
-
-
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
-def test_build_leaves(db):
-    raw = db.datatypes['dill'].encoder(my_function)
-    file_id = db.artifact_store.save_artifact({'bytes': raw, 'datatype': 'dill'})[
-        'file_id'
-    ]
-    leaf_records = [
-        {
-            'leaf_type': 'artifact',
-            'cls': 'Artifact',
-            'module': 'superduperdb.components.datatype',
-            'dict': {
-                'file_id': file_id,
-                'datatype': 'dill',
-            },
-        },
-        {
-            'leaf_type': 'component',
-            'cls': 'ObjectModel',
-            'module': 'superduperdb.components.model',
-            'dict': {'identifier': 'test', 'object': f'_artifact/{file_id}'},
-        },
-        {
-            'leaf_type': 'component',
-            'cls': 'Stack',
-            'module': 'superduperdb.components.stack',
-            'dict': {
-                'identifier': 'test_stack',
-                'components': ['_component/model/test'],
-            },
-        },
-    ]
-    out = _build_leaves(leaf_records)
-    print(out)
-
-
 def test_flat_query_encoding():
     q = Collection('docs').find({'a': 1}).limit(2)
 
@@ -107,3 +52,66 @@ def test_flat_query_encoding():
     r = q._deep_flat_encode(None)
 
     print(r)
+
+
+def test_encode_decode_flattened_document():
+    from superduperdb.ext.pillow.encoder import image_type
+    import PIL.Image
+
+    schema = Schema('my-schema', fields={'img': image_type(identifier='png', encodable='artifact')})
+
+    img = PIL.Image.open('test/material/data/test.png')
+
+    r = Document(
+        {
+            'x': 2,
+            'img': img,
+        },
+        schema=schema,
+    )
+
+    encoded_r = r.deep_flat_encode()
+
+    import yaml
+    print(yaml.dump({k: v for k, v in encoded_r.items() if k != '_blobs'}))
+
+    assert not isinstance(encoded_r, Document)
+    assert isinstance(encoded_r, dict)
+    assert '_leaves' in encoded_r
+    assert '_blobs' in encoded_r
+    assert isinstance(encoded_r['img'], str)
+    assert encoded_r['img'].startswith('?artifact')
+    assert isinstance(next(iter(encoded_r['_blobs'].values())), bytes)
+
+    decoded_r = Document.deep_flat_decode(encoded_r).unpack()
+
+    pprint.pprint(decoded_r)
+
+    m = ObjectModel(
+        'test',
+        object=lambda x: x + 2,
+    )
+
+    encoded_r = m.deep_flat_encode()
+
+    pprint.pprint(encoded_r)
+
+    decoded_r = Document.deep_flat_decode(encoded_r)
+
+    print(decoded_r)
+
+    m = decoded_r.unpack()
+
+    assert isinstance(m, ObjectModel)
+    assert isinstance(m.object, LazyArtifact)
+
+    pprint.pprint(m)
+
+    r = m.dict()
+
+    assert isinstance(r, Document)
+    assert {'version', 'hidden', 'type_id', 'cls', 'module', 'dict'}.issubset(set(r.keys()))
+
+    print(r)
+
+    pprint.pprint(m.dict().deep_flat_encode())

Pinnacle-Networks/Pinnacle

superduperdb/base/leaf.py
@@ -1,16 +1,47 @@
+import dataclasses as dc
+import importlib
 import inspect
 import typing as t
 from abc import ABC, abstractmethod, abstractproperty
+from pinnacledb.misc.serialization import asdict
 
 _CLASS_REGISTRY = {}
 
 
+def _import_item(cls, module, dict):
+    module = importlib.import_module(module)
+    cls = getattr(module, cls)
+    try:
+        return cls(**dict)
+    except TypeError as e:
+        if 'got an unexpected keyword argument' in str(e):
+            if callable(cls):
+                return cls(**{k: v for k, v in dict.items() if k in inspect.signature(cls).parameters})
+            return cls(**{k: v for k, v in dict.items() if k in inspect.signature(cls.__init__).parameters})
+        raise e
+
+
+@dc.dataclass
 class Leaf(ABC):
     """Base class for all leaf classes."""
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         cls._register_class()
+
+    def _deep_flat_encode(self, cache, blobs, files):
+        from pinnacledb.base.document import _deep_flat_encode
+        r = dict(self.dict())
+        return _deep_flat_encode(r, cache, blobs, files)
+
+    def dict(self):
+        from pinnacledb import Document
+        r = asdict(self)
+        return Document({
+            'module': self.__class__.__module__,
+            'cls': self.__class__.__name__,
+            'dict': r
+        })
 
     @classmethod
     def handle_integration(cls, r):

superduperdb/components/vector_index.py
@@ -12,7 +12,7 @@ from pinnacledb.components.listener import Listener
 from pinnacledb.components.model import Mapping, ModelInputType
 from pinnacledb.ext.utils import str_shape
 from pinnacledb.jobs.job import FunctionJob
-from pinnacledb.misc.annotations import public_api, ui
+from pinnacledb.misc.annotations import public_api, component
 from pinnacledb.misc.special_dicts import MongoStyleDict
 from pinnacledb.vector_search.base import VectorIndexMeasureType
 from pinnacledb.vector_search.update_tasks import copy_vectors
@@ -265,7 +265,7 @@ class DecodeArray:
         return np.frombuffer(bytes, dtype=self.dtype).tolist()
 
 
-@ui(
+@component(
     {'name': 'shape', 'type': 'int'},
     {'name': 'identifier', 'type': 'str'},
 )

superduperdb/ext/pillow/encoder.py
@@ -6,7 +6,7 @@ import PIL.JpegImagePlugin
 import PIL.PngImagePlugin
 
 from pinnacledb.components.datatype import DataType
-from pinnacledb.misc.annotations import ui
+from pinnacledb.misc.annotations import component
 
 BLANK_IMAGE = PIL.Image.new('RGB', (600, 600), (255, 255, 255))
 
@@ -79,7 +79,7 @@ pil_image_hybrid_jpeg = DataType(
 )
 
 
-@ui(
+@component(
     {'name': 'identifier', 'type': 'str'},
     {'name': 'media_type', 'type': 'str', 'default': 'image/png'},
 )

superduperdb/misc/annotations.py
@@ -1,4 +1,5 @@
 import functools
+import hashlib
 import importlib
 import sys
 import typing as t
@@ -10,6 +11,7 @@ from packaging import version
 
 from pinnacledb import logging
 from pinnacledb.base.exceptions import RequiredPackageVersionsNotFound
+from pinnacledb.components.component import Component
 
 
 def _normalize_module(import_module, lower_bound, upper_bound, install_module):
@@ -189,17 +191,29 @@ def _get_indent(docstring: str) -> int:
     return len(non_empty_lines[1]) - len(non_empty_lines[1].lstrip())
 
 
-def ui(*schema: t.Dict, handle_integration: t.Callable = lambda x: x):
-    """Annotation for documenting UI schemas.
+def component(*schema: t.Dict, handle_integration: t.Callable = lambda x: x):
+    def decorator(f):
 
-    :param *schema: list of dictionaries representing the UI schema
-    :param handle_integration: function to handle the integration of the UI schema
-    """
+        @functools.wraps(f)
+        def decorated(*args, **kwargs):
+            assert not args, 'Can\'t use *args here'
+            out = f(**kwargs)
+            assert isinstance(out, Component)
+            def _deep_flat_encode(cache, blobs, files):
+                h = hashlib.sha1(str(kwargs).encode()).hexdigest()
+                path = f'{f.__module__}.{f.__name__}'
+                id = f'{path}/{h}'
+                cache[id] = {
+                    'cls': f.__name__,
+                    'module': f.__module__,
+                    'dict': kwargs,
+                }
+                return f'?{id}'
+            out._deep_flat_encode = _deep_flat_encode
+            return out
 
-    def decorated(f):
-        f.get_ui_schema = lambda: schema
-        f.build = lambda r: f(**r)
-        f.handle_integration = handle_integration
-        return f
-
-    return decorated
+        decorated.get_ui_schema = lambda: schema
+        decorated.build = lambda r: f(**r)
+        decorated.handle_integration = handle_integration
+        return decorated
+    return decorator

test/unittest/base/test_document.py
@@ -1,6 +1,8 @@
 import dataclasses as dc
 import pprint
 import typing as t
+from pinnacledb.components.datatype import LazyArtifact
+from pinnacledb.components.schema import Schema
 from test.db_config import DBConfig
 
 import pytest
@@ -38,63 +40,6 @@ def test_document_encoding(document):
     assert (new_document['x'].x - document['x'].x).sum() == 0
 
 
-def my_function(x):
-    return x + 2
-
-
-def test_encode_decode_flattened(db):
-    m = ObjectModel(
-        identifier='test',
-        object=lambda x: x,
-        datatype=vector(384),
-    )
-
-    r, bytes = m.export(format='json')
-    pprint.pprint(r)
-
-    for file_id in bytes:
-        db.artifact_store.save_artifact({'bytes': bytes[file_id], 'datatype': 'dill'})
-
-    r['_leaves'] = _build_leaves(r['_leaves'], db)[0]
-    pprint.pprint(r['_leaves'][r['_base']])
-
-
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
-def test_build_leaves(db):
-    raw = db.datatypes['dill'].encoder(my_function)
-    file_id = db.artifact_store.save_artifact({'bytes': raw, 'datatype': 'dill'})[
-        'file_id'
-    ]
-    leaf_records = [
-        {
-            'leaf_type': 'artifact',
-            'cls': 'Artifact',
-            'module': 'pinnacledb.components.datatype',
-            'dict': {
-                'file_id': file_id,
-                'datatype': 'dill',
-            },
-        },
-        {
-            'leaf_type': 'component',
-            'cls': 'ObjectModel',
-            'module': 'pinnacledb.components.model',
-            'dict': {'identifier': 'test', 'object': f'_artifact/{file_id}'},
-        },
-        {
-            'leaf_type': 'component',
-            'cls': 'Stack',
-            'module': 'pinnacledb.components.stack',
-            'dict': {
-                'identifier': 'test_stack',
-                'components': ['_component/model/test'],
-            },
-        },
-    ]
-    out = _build_leaves(leaf_records)
-    print(out)
-
-
 def test_flat_query_encoding():
     q = Collection('docs').find({'a': 1}).limit(2)
 
@@ -107,3 +52,66 @@ def test_flat_query_encoding():
     r = q._deep_flat_encode(None)
 
     print(r)
+
+
+def test_encode_decode_flattened_document():
+    from pinnacledb.ext.pillow.encoder import image_type
+    import PIL.Image
+
+    schema = Schema('my-schema', fields={'img': image_type(identifier='png', encodable='artifact')})
+
+    img = PIL.Image.open('test/material/data/test.png')
+
+    r = Document(
+        {
+            'x': 2,
+            'img': img,
+        },
+        schema=schema,
+    )
+
+    encoded_r = r.deep_flat_encode()
+
+    import yaml
+    print(yaml.dump({k: v for k, v in encoded_r.items() if k != '_blobs'}))
+
+    assert not isinstance(encoded_r, Document)
+    assert isinstance(encoded_r, dict)
+    assert '_leaves' in encoded_r
+    assert '_blobs' in encoded_r
+    assert isinstance(encoded_r['img'], str)
+    assert encoded_r['img'].startswith('?artifact')
+    assert isinstance(next(iter(encoded_r['_blobs'].values())), bytes)
+
+    decoded_r = Document.deep_flat_decode(encoded_r).unpack()
+
+    pprint.pprint(decoded_r)
+
+    m = ObjectModel(
+        'test',
+        object=lambda x: x + 2,
+    )
+
+    encoded_r = m.deep_flat_encode()
+
+    pprint.pprint(encoded_r)
+
+    decoded_r = Document.deep_flat_decode(encoded_r)
+
+    print(decoded_r)
+
+    m = decoded_r.unpack()
+
+    assert isinstance(m, ObjectModel)
+    assert isinstance(m.object, LazyArtifact)
+
+    pprint.pprint(m)
+
+    r = m.dict()
+
+    assert isinstance(r, Document)
+    assert {'version', 'hidden', 'type_id', 'cls', 'module', 'dict'}.issubset(set(r.keys()))
+
+    print(r)
+
+    pprint.pprint(m.dict().deep_flat_encode())