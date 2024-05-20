superduper-io/superduper

Fix bugs for mongodb query

superduperdb/backends/mongodb/query.py
@@ -185,7 +185,10 @@ class MongoQuery(Query):
         documents = self.parts[0][1][0]
         trailing_args = self.parts[0][1][1:]
         kwargs = self.parts[0][2]
-        documents = [r.encode() for r in documents]
+        schema = kwargs.pop('schema', None)
+
+        schema = get_schema(self.db, schema) if schema else None
+        documents = [r.encode(schema) for r in self.documents]
         for r in documents:
             if '_blobs' in r:
                 for file_id, bytes in r['_blobs'].items():
@@ -418,3 +421,18 @@ class BulkOp(Leaf):
                 kwargs[k] = v.unpack()
         return getattr(pymongo, self.identifier)(**kwargs)
 
+from superduperdb.components.schema import Schema
+def get_schema(db, schema: t.Union[Schema, str]) -> Schema:
+    """Handle schema caching and loading."""
+    if isinstance(schema, Schema):
+        # If the schema is not in the db, it is added to the db.
+        if schema.identifier not in db.show(Schema.type_id):
+            db.add(schema)
+        schema_identifier = schema.identifier
+
+    else:
+        schema_identifier = schema
+
+    assert isinstance(schema_identifier, str)
+
+    return db.schemas[schema_identifier]

test/unittest/backends/mongodb/test_new_query.py
@@ -1,18 +1,12 @@
-from superduperdb.backends.mongodb.new_query import Collection
+import pytest
+from test.db_config import DBConfig
+from superduperdb import Document
 
-
-def test_add_fold():
-
-    coll = Collection(identifier='test_coll')
-
-    q = coll.find().limit(5)
-
-    new_q = q.add_fold('valid')
-
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_add_fold(db):
+    breakpoint()
+    new_q = db['documents'].like(Document({'text': 'some like'})).limit(n=5)
     print(new_q)
 
-    assert str(new_q) == "test_coll.find({'_fold': 'valid'}).limit(5)"
+    assert str(new_q) == "documents.find({'_fold': 'valid'}).limit(5)"
 
-
-def test_add():
-    ...

Pinnacle-Networks/Pinnacle

superduperdb/backends/mongodb/query.py
@@ -185,7 +185,10 @@ class MongoQuery(Query):
         documents = self.parts[0][1][0]
         trailing_args = self.parts[0][1][1:]
         kwargs = self.parts[0][2]
-        documents = [r.encode() for r in documents]
+        schema = kwargs.pop('schema', None)
+
+        schema = get_schema(self.db, schema) if schema else None
+        documents = [r.encode(schema) for r in self.documents]
         for r in documents:
             if '_blobs' in r:
                 for file_id, bytes in r['_blobs'].items():
@@ -418,3 +421,18 @@ class BulkOp(Leaf):
                 kwargs[k] = v.unpack()
         return getattr(pymongo, self.identifier)(**kwargs)
 
+from pinnacledb.components.schema import Schema
+def get_schema(db, schema: t.Union[Schema, str]) -> Schema:
+    """Handle schema caching and loading."""
+    if isinstance(schema, Schema):
+        # If the schema is not in the db, it is added to the db.
+        if schema.identifier not in db.show(Schema.type_id):
+            db.add(schema)
+        schema_identifier = schema.identifier
+
+    else:
+        schema_identifier = schema
+
+    assert isinstance(schema_identifier, str)
+
+    return db.schemas[schema_identifier]

test/unittest/backends/mongodb/test_new_query.py
@@ -1,18 +1,12 @@
-from pinnacledb.backends.mongodb.new_query import Collection
+import pytest
+from test.db_config import DBConfig
+from pinnacledb import Document
 
-
-def test_add_fold():
-
-    coll = Collection(identifier='test_coll')
-
-    q = coll.find().limit(5)
-
-    new_q = q.add_fold('valid')
-
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_add_fold(db):
+    breakpoint()
+    new_q = db['documents'].like(Document({'text': 'some like'})).limit(n=5)
     print(new_q)
 
-    assert str(new_q) == "test_coll.find({'_fold': 'valid'}).limit(5)"
+    assert str(new_q) == "documents.find({'_fold': 'valid'}).limit(5)"
 
-
-def test_add():
-    ...