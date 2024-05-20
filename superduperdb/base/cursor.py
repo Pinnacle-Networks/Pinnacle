superduper-io/superduper

Fix mongodb queries

superduperdb/base/cursor.py
@@ -1,9 +1,8 @@
 import dataclasses as dc
 import typing as t
 
-from superduperdb import CFG, logging
+from superduperdb import logging
 from superduperdb.base.document import Document
-from superduperdb.misc.files import load_uris
 
 if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
@@ -62,6 +61,7 @@ class SuperDuperCursor:
                 r['score'] = self.scores[str(r[self.id_field])]
             except KeyError:
                 logging.warn(f"No document id found for {r}")
+
         return Document.decode(r, db=self.db, schema=self.schema)
 
     next = __next__

test/unittest/base/test_leaf.py
@@ -5,21 +5,21 @@ from pprint import pprint
 from superduperdb import ObjectModel
 from superduperdb.backends.mongodb.query import MongoQuery
 from superduperdb.base.document import Document
-from superduperdb.base.variables import Variable
 from superduperdb.base.leaf import Leaf
+from superduperdb.base.variables import Variable
 from superduperdb.components.component import Component
 
 
 @dc.dataclass
 class Test(Leaf):
-    a: int
-    b: t.Union[str, Variable]
-    c: t.Union[float, Variable]
+    b: t.Optional[t.Union[str, Variable]] = 'a'
+    c: t.Optional[t.Union[float, Variable]] = 1.0
+    a: t.Optional[int] = 1
 
 
 @dc.dataclass
 class OtherSer(Leaf):
-    d: str
+    d: str = 'd'
 
 
 @dc.dataclass(kw_only=True)

Pinnacle-Networks/Pinnacle

superduperdb/base/cursor.py
@@ -1,9 +1,8 @@
 import dataclasses as dc
 import typing as t
 
-from pinnacledb import CFG, logging
+from pinnacledb import logging
 from pinnacledb.base.document import Document
-from pinnacledb.misc.files import load_uris
 
 if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
@@ -65,6 +64,7 @@ class SuperDuperCursor:
                 r['score'] = self.scores[str(r[self.id_field])]
             except KeyError:
                 logging.warn(f"No document id found for {r}")
+
         return Document.decode(r, db=self.db, schema=self.schema)
 
     next = __next__

test/unittest/base/test_leaf.py
@@ -5,21 +5,21 @@ from pprint import pprint
 from pinnacledb import ObjectModel
 from pinnacledb.backends.mongodb.query import MongoQuery
 from pinnacledb.base.document import Document
-from pinnacledb.base.variables import Variable
 from pinnacledb.base.leaf import Leaf
+from pinnacledb.base.variables import Variable
 from pinnacledb.components.component import Component
 
 
 @dc.dataclass
 class Test(Leaf):
-    a: int
-    b: t.Union[str, Variable]
-    c: t.Union[float, Variable]
+    b: t.Optional[t.Union[str, Variable]] = 'a'
+    c: t.Optional[t.Union[float, Variable]] = 1.0
+    a: t.Optional[int] = 1
 
 
 @dc.dataclass
 class OtherSer(Leaf):
-    d: str
+    d: str = 'd'
 
 
 @dc.dataclass(kw_only=True)