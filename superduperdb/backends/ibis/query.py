superduper-io/superduper

Add SuperDuperEncode special dict

superduperdb/backends/ibis/query.py
@@ -12,6 +12,7 @@ from superduperdb.backends.base.query import (
     parse_query as _parse_query,
 )
 from superduperdb.base.cursor import SuperDuperCursor
+from superduperdb.misc.special_dicts import SuperDuperFlatEncode
 from superduperdb.components.schema import Schema
 
 if t.TYPE_CHECKING:
@@ -65,7 +66,9 @@ def _model_update_impl(
 
     for ix in range(len(outputs)):
         output = outputs[ix]
-        if '_base' in output:
+
+        if isinstance(output, SuperDuperFlatEncode):
+            # TODO: what if it has no _base
             key = output['_base']
             if isinstance(key, str):
                 output = output['_leaves'][key[1:]]['blob']

superduperdb/backends/mongodb/query.py
@@ -10,6 +10,7 @@ from superduperdb.backends.base.query import (
     parse_query as _parse_query,
 )
 from superduperdb.base.cursor import SuperDuperCursor
+from superduperdb.misc.special_dicts import SuperDuperFlatEncode
 from superduperdb.base.document import Document, QueryUpdateDocument
 from superduperdb.base.leaf import Leaf
 
@@ -416,8 +417,8 @@ class MongoQuery(Query):
 
                 output = outputs[i]
 
-                if isinstance(output, dict) and '_base' in output:
-                    output = output['_base']
+                if isinstance(output, SuperDuperFlatEncode):
+                    output = output.get('_base', output)
 
                 update = {f'_outputs.{predict_id}': output}
 
@@ -433,33 +434,54 @@ class MongoQuery(Query):
         else:
             collection = MongoQuery(f'_outputs.{predict_id}')
             documents = []
+            leaves, blobs, files = {}, {}, {}
             if flatten:
                 for i, id in enumerate(ids):
                     _outputs = outputs[i]
                     if isinstance(_outputs, (list, tuple)):
                         for offset, output in enumerate(_outputs):
+                            if isinstance(output, SuperDuperFlatEncode):
+                                leaves, blobs, files = output.pop_leaves(), output.pop_blobs(), output.pop_files()
+                                output = output.get('_base', output)
                             documents.append(
                                 {
                                     f'_outputs': {predict_id: output},
                                     '_source': ObjectId(id),
                                     '_offset': offset,
+                                    '_leaves': leaves,
+                                    '_blobs': blobs,
+                                    '_files': files
                                 }
                             )
                     else:
+                        output = outputs[i]
+                        if isinstance(output, SuperDuperFlatEncode):
+                            leaves, blobs, files = output.pop_leaves(), output.pop_blobs(), output.pop_files()
+                            output = output.get('_base', output)
                         documents.append(
                             {
-                                f'_outputs': {predict_id: outputs[i]},
+                                f'_outputs': {predict_id: output},
                                 '_source': ObjectId(id),
                                 '_offset': 0,
+                                '_leaves': leaves,
+                                '_blobs': blobs,
+                                '_files': files
                             }
                         )
 
             else:
                 for i, id in enumerate(ids):
+                    output = outputs[i]
+                    if isinstance(output, SuperDuperFlatEncode):
+                        leaves, blobs, files = output.pop_leaves(), output.pop_blobs(), output.pop_files()
+                        output = output.get('_base', output)
                     documents.append(
                         {
                             '_id': ObjectId(id),
-                            f'_outputs': {predict_id: outputs[i]},
+                            f'_outputs': {predict_id: output},
+                            '_leaves': leaves,
+                            '_blobs': blobs,
+                            '_files': files
                         }
                     )
             return collection.insert_many(documents)

superduperdb/base/leaf.py
@@ -6,6 +6,7 @@ import uuid
 from abc import ABC
 
 from superduperdb.misc.serialization import asdict
+from superduperdb.misc.special_dicts import SuperDuperFlatEncode
 
 _CLASS_REGISTRY = {}
 
@@ -66,12 +67,12 @@ class Leaf(ABC):
         blobs = {}
         files = {}
         self._deep_flat_encode(cache, blobs, files, leaves_to_keep, schema)
-        return {
+        return SuperDuperFlatEncode({
             '_base': f'?{self._id}',
             '_leaves': cache,
             '_blobs': blobs,
             '_files': files,
-        }
+        })
 
     def set_variables(self, db, **kwargs) -> 'Leaf':
         """Set free variables of self.

superduperdb/components/datatype.py
@@ -20,6 +20,7 @@ from superduperdb.backends.base.artifacts import (
 from superduperdb.base.config import BytesEncoding
 from superduperdb.base.leaf import Leaf
 from superduperdb.components.component import Component, ensure_initialized
+from superduperdb.misc.special_dicts import SuperDuperFlatEncode
 from superduperdb.misc.annotations import component, public_api
 from superduperdb.misc.hash import random_sha1
 
@@ -268,9 +269,10 @@ class DataType(Component):
         :param uri: The optional URI.
         """
         if self._takes_x:
-            return self.encodable_cls(datatype=self, x=x, uri=uri)
+            x = Empty() if x is None else x
+            return self.encodable_cls(datatype=self, x=x, uri=uri, db=self.db)
         else:
-            return self.encodable_cls(datatype=self, uri=uri)
+            return self.encodable_cls(datatype=self, uri=uri, db=self.db)
 
     @ensure_initialized
     def encode_data(self, item, info: t.Optional[t.Dict] = None):
@@ -509,11 +511,11 @@ class Encodable(_BaseEncodable):
         blobs = {}
         files = {}
 
-        return {
+        return SuperDuperFlatEncode({
             '_base': self._deep_flat_encode(cache, blobs, files, (), schema),
             '_leaves': cache,
             '_blobs': blobs,
-        }
+        })
 
     def _deep_flat_encode(self, cache, blobs, files, leaves_to_keep=(), schema=None):
         if isinstance(self, leaves_to_keep):
@@ -652,7 +654,6 @@ class Artifact(_BaseEncodable, _ArtifactSaveMixin):
         if isinstance(self, leaves_to_keep):
             cache[self.id] = self
             return f'?{self.id}'
-
         maybe_bytes, file_id = self._encode()
         self.file_id = file_id
         r = super()._deep_flat_encode(

superduperdb/components/schema.py
@@ -6,6 +6,7 @@ from overrides import override
 
 from superduperdb.components.component import Component
 from superduperdb.components.datatype import DataType
+from superduperdb.misc.special_dicts import SuperDuperFlatEncode
 from superduperdb.misc.annotations import public_api
 
 class _Native:
@@ -120,11 +121,22 @@ class Schema(Component):
             return data
 
         encoded_data = {}
+        cache = {}
+        files = {}
+        blobs = {}
         for k, v in data.items():
             if k in self.fields and isinstance(self.fields[k], DataType):
                 field_encoder = self.fields[k]
                 assert callable(field_encoder)
-                encoded_data.update({k: field_encoder(v).encode()})
+                v = field_encoder(v).encode()
+                base = v['_base']
+                cache.update(v['_leaves'])
+                blobs.update(v.get('_blobs', {}))
+                files.update(v.get('_files', {}))
+                encoded_data.update({k: base})
             else:
                 encoded_data.update({k: v})
-        return encoded_data
+        encoded_data['_leaves'] = cache
+        encoded_data['_blobs'] = blobs
+        encoded_data['_files'] = files
+        return SuperDuperFlatEncode(encoded_data)

test/unittest/component/test_model.py
@@ -13,6 +13,7 @@ from superduperdb.backends.local.compute import LocalComputeBackend
 from superduperdb.backends.mongodb.query import MongoQuery
 from superduperdb.base.datalayer import Datalayer
 from superduperdb.base.document import Document
+from superduperdb.backends.base.data_backend import BaseDataBackend
 from superduperdb.base.variables import Variable
 from superduperdb.components.dataset import Dataset
 from superduperdb.components.datatype import DataType, pickle_decode, pickle_encode
@@ -359,7 +360,7 @@ def test_query_model(db):
     q = (
         MongoQuery(identifier='documents', db=db)
         .like(
-            {'x': Variable(identifier='x', value='X')},
+            {'x': Variable(identifier='X')},
             vector_index='test_vector_search',
             n=3,
         )

Pinnacle-Networks/Pinnacle

superduperdb/backends/ibis/query.py
@@ -12,6 +12,7 @@ from pinnacledb.backends.base.query import (
     parse_query as _parse_query,
 )
 from pinnacledb.base.cursor import SuperDuperCursor
+from pinnacledb.misc.special_dicts import SuperDuperFlatEncode
 from pinnacledb.components.schema import Schema
 
 if t.TYPE_CHECKING:
@@ -65,7 +66,9 @@ def _model_update_impl(
 
     for ix in range(len(outputs)):
         output = outputs[ix]
-        if '_base' in output:
+
+        if isinstance(output, SuperDuperFlatEncode):
+            # TODO: what if it has no _base
             key = output['_base']
             if isinstance(key, str):
                 output = output['_leaves'][key[1:]]['blob']

superduperdb/backends/mongodb/query.py
@@ -10,6 +10,7 @@ from pinnacledb.backends.base.query import (
     parse_query as _parse_query,
 )
 from pinnacledb.base.cursor import SuperDuperCursor
+from pinnacledb.misc.special_dicts import SuperDuperFlatEncode
 from pinnacledb.base.document import Document, QueryUpdateDocument
 from pinnacledb.base.leaf import Leaf
 
@@ -416,8 +417,8 @@ class MongoQuery(Query):
 
                 output = outputs[i]
 
-                if isinstance(output, dict) and '_base' in output:
-                    output = output['_base']
+                if isinstance(output, SuperDuperFlatEncode):
+                    output = output.get('_base', output)
 
                 update = {f'_outputs.{predict_id}': output}
 
@@ -433,33 +434,54 @@ class MongoQuery(Query):
         else:
             collection = MongoQuery(f'_outputs.{predict_id}')
             documents = []
+            leaves, blobs, files = {}, {}, {}
             if flatten:
                 for i, id in enumerate(ids):
                     _outputs = outputs[i]
                     if isinstance(_outputs, (list, tuple)):
                         for offset, output in enumerate(_outputs):
+                            if isinstance(output, SuperDuperFlatEncode):
+                                leaves, blobs, files = output.pop_leaves(), output.pop_blobs(), output.pop_files()
+                                output = output.get('_base', output)
                             documents.append(
                                 {
                                     f'_outputs': {predict_id: output},
                                     '_source': ObjectId(id),
                                     '_offset': offset,
+                                    '_leaves': leaves,
+                                    '_blobs': blobs,
+                                    '_files': files
                                 }
                             )
                     else:
+                        output = outputs[i]
+                        if isinstance(output, SuperDuperFlatEncode):
+                            leaves, blobs, files = output.pop_leaves(), output.pop_blobs(), output.pop_files()
+                            output = output.get('_base', output)
                         documents.append(
                             {
-                                f'_outputs': {predict_id: outputs[i]},
+                                f'_outputs': {predict_id: output},
                                 '_source': ObjectId(id),
                                 '_offset': 0,
+                                '_leaves': leaves,
+                                '_blobs': blobs,
+                                '_files': files
                             }
                         )
 
             else:
                 for i, id in enumerate(ids):
+                    output = outputs[i]
+                    if isinstance(output, SuperDuperFlatEncode):
+                        leaves, blobs, files = output.pop_leaves(), output.pop_blobs(), output.pop_files()
+                        output = output.get('_base', output)
                     documents.append(
                         {
                             '_id': ObjectId(id),
-                            f'_outputs': {predict_id: outputs[i]},
+                            f'_outputs': {predict_id: output},
+                            '_leaves': leaves,
+                            '_blobs': blobs,
+                            '_files': files
                         }
                     )
             return collection.insert_many(documents)

superduperdb/base/leaf.py
@@ -6,6 +6,7 @@ import uuid
 from abc import ABC
 
 from pinnacledb.misc.serialization import asdict
+from pinnacledb.misc.special_dicts import SuperDuperFlatEncode
 
 _CLASS_REGISTRY = {}
 
@@ -66,12 +67,12 @@ class Leaf(ABC):
         blobs = {}
         files = {}
         self._deep_flat_encode(cache, blobs, files, leaves_to_keep, schema)
-        return {
+        return SuperDuperFlatEncode({
             '_base': f'?{self._id}',
             '_leaves': cache,
             '_blobs': blobs,
             '_files': files,
-        }
+        })
 
     def set_variables(self, db, **kwargs) -> 'Leaf':
         """Set free variables of self.

superduperdb/components/datatype.py
@@ -20,6 +20,7 @@ from pinnacledb.backends.base.artifacts import (
 from pinnacledb.base.config import BytesEncoding
 from pinnacledb.base.leaf import Leaf
 from pinnacledb.components.component import Component, ensure_initialized
+from pinnacledb.misc.special_dicts import SuperDuperFlatEncode
 from pinnacledb.misc.annotations import component, public_api
 from pinnacledb.misc.hash import random_sha1
 
@@ -268,9 +269,10 @@ class DataType(Component):
         :param uri: The optional URI.
         """
         if self._takes_x:
-            return self.encodable_cls(datatype=self, x=x, uri=uri)
+            x = Empty() if x is None else x
+            return self.encodable_cls(datatype=self, x=x, uri=uri, db=self.db)
         else:
-            return self.encodable_cls(datatype=self, uri=uri)
+            return self.encodable_cls(datatype=self, uri=uri, db=self.db)
 
     @ensure_initialized
     def encode_data(self, item, info: t.Optional[t.Dict] = None):
@@ -509,11 +511,11 @@ class Encodable(_BaseEncodable):
         blobs = {}
         files = {}
 
-        return {
+        return SuperDuperFlatEncode({
             '_base': self._deep_flat_encode(cache, blobs, files, (), schema),
             '_leaves': cache,
             '_blobs': blobs,
-        }
+        })
 
     def _deep_flat_encode(self, cache, blobs, files, leaves_to_keep=(), schema=None):
         if isinstance(self, leaves_to_keep):
@@ -652,7 +654,6 @@ class Artifact(_BaseEncodable, _ArtifactSaveMixin):
         if isinstance(self, leaves_to_keep):
             cache[self.id] = self
             return f'?{self.id}'
-
         maybe_bytes, file_id = self._encode()
         self.file_id = file_id
         r = super()._deep_flat_encode(

superduperdb/components/schema.py
@@ -6,6 +6,7 @@ from overrides import override
 
 from pinnacledb.components.component import Component
 from pinnacledb.components.datatype import DataType
+from pinnacledb.misc.special_dicts import SuperDuperFlatEncode
 from pinnacledb.misc.annotations import public_api
 
 class _Native:
@@ -120,11 +121,22 @@ class Schema(Component):
             return data
 
         encoded_data = {}
+        cache = {}
+        files = {}
+        blobs = {}
         for k, v in data.items():
             if k in self.fields and isinstance(self.fields[k], DataType):
                 field_encoder = self.fields[k]
                 assert callable(field_encoder)
-                encoded_data.update({k: field_encoder(v).encode()})
+                v = field_encoder(v).encode()
+                base = v['_base']
+                cache.update(v['_leaves'])
+                blobs.update(v.get('_blobs', {}))
+                files.update(v.get('_files', {}))
+                encoded_data.update({k: base})
             else:
                 encoded_data.update({k: v})
-        return encoded_data
+        encoded_data['_leaves'] = cache
+        encoded_data['_blobs'] = blobs
+        encoded_data['_files'] = files
+        return SuperDuperFlatEncode(encoded_data)

test/unittest/component/test_model.py
@@ -13,6 +13,7 @@ from pinnacledb.backends.local.compute import LocalComputeBackend
 from pinnacledb.backends.mongodb.query import MongoQuery
 from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.base.document import Document
+from pinnacledb.backends.base.data_backend import BaseDataBackend
 from pinnacledb.base.variables import Variable
 from pinnacledb.components.dataset import Dataset
 from pinnacledb.components.datatype import DataType, pickle_decode, pickle_encode
@@ -359,7 +360,7 @@ def test_query_model(db):
     q = (
         MongoQuery(identifier='documents', db=db)
         .like(
-            {'x': Variable(identifier='x', value='X')},
+            {'x': Variable(identifier='X')},
             vector_index='test_vector_search',
             n=3,
         )