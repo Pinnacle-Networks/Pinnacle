superduper-io/superduper

Auto-create tables and schemas

superduperdb/base/datalayer.py
@@ -18,6 +18,7 @@ from superduperdb.backends.base.metadata import MetaDataStore
 from superduperdb.backends.base.query import Query
 from superduperdb.backends.local.compute import LocalComputeBackend
 from superduperdb.base import exceptions, variables
+from superduperdb.base.config import Config
 from superduperdb.base.cursor import SuperDuperCursor
 from superduperdb.base.document import Document
 from superduperdb.base.superduper import superduper
@@ -104,6 +105,7 @@ class Datalayer:
 
         self.compute = compute
         self._server_mode = False
+        self._cfg = s.CFG
 
     def __getitem__(self, item):
         return self.databackend.get_query_builder(item)
@@ -324,7 +326,11 @@ class Datalayer:
         return result, None
 
     def _insert(
-        self, insert: Query, refresh: bool = True, datatypes: t.Sequence[DataType] = ()
+        self,
+        insert: Query,
+        refresh: bool = True,
+        datatypes: t.Sequence[DataType] = (),
+        auto_schema: bool = True,
     ) -> InsertResult:
         """
         Insert data into the database.
@@ -341,6 +347,9 @@ class Datalayer:
             if random.random() < s.CFG.fold_probability:
                 r['_fold'] = 'valid'
 
+        if auto_schema and self.cfg.auto_schema:
+            self.auto_create_table_schema(insert)
+
         inserted_ids = insert.do_execute(self)
 
         cdc_status = self.cdc.running or s.CFG.cluster.cdc.uri is not None
@@ -353,6 +362,23 @@ class Datalayer:
                     insert, ids=inserted_ids, verbose=False
                 )
         return inserted_ids, None
+
+    def auto_create_table_schema(self, insert: Query):
+        """Auto create table schema based on insert query.
+
+        :param insert: The insert query object specifying the data to be inserted.
+        """
+        from superduperdb.components.table import Table
+
+        table_name = insert.identifier
+        try:
+            table = self.tables[table_name]
+        except FileNotFoundError:
+            table = None
+        if table is None:
+            schema = self.infer_schema(insert.documents[0])
+            table = Table(identifier=table_name, schema=schema)
+            self.apply(table)
 
     def _select(self, select: Query, reference: bool = True) -> SelectResult:
         """
@@ -409,7 +435,7 @@ class Datalayer:
         task_workflow.run_jobs()
         return task_workflow
 
-    def _write(self, write: Query, refresh: bool = True) -> UpdateResult:
+    def _write(self, write: Query, refresh: bool = True, **kwargs) -> UpdateResult:
         """
         Bulk write data to the database.
 
@@ -445,7 +471,7 @@ class Datalayer:
                 return updated_ids, deleted_ids, jobs
         return updated_ids, deleted_ids, None
 
-    def _update(self, update: Query, refresh: bool = True) -> UpdateResult:
+    def _update(self, update: Query, refresh: bool = True, **kwargs) -> UpdateResult:
         """
         Update data in the database.
 
@@ -1075,6 +1101,16 @@ class Datalayer:
         :return: The inferred schema
         """
         return self.databackend.infer_schema(data, identifier)
+
+    def set_cfg(self, cfg: Config):
+        """Set the configuration object for the datalayer."""
+        assert isinstance(cfg, Config)
+        self._cfg = cfg
+
+    @property
+    def cfg(self) -> Config:
+        """Get the configuration object for the datalayer."""
+        return self._cfg or s.CFG
 
 
 @dc.dataclass

superduperdb/components/table.py
@@ -2,6 +2,7 @@ import dataclasses as dc
 import typing as t
 
 from superduperdb.backends.ibis.field_types import dtype
+from superduperdb.base.enums import DBType
 from superduperdb.components.component import Component
 from superduperdb.components.schema import Schema, _Native
 from superduperdb.misc.annotations import merge_docstrings
@@ -64,6 +65,10 @@ class Table(Component):
                 )
 
                 return
+
+        if self.db.databackend.db_type == DBType.SQL:
+            if self.primary_id not in self.schema.fields:
+                self.schema.fields[self.primary_id] = dtype('str')  # type: ignore
 
         try:
             db.databackend.create_table_and_schema(self.identifier, self.schema.raw)

superduperdb/ext/pillow/encoder.py
@@ -5,7 +5,7 @@ import PIL.Image
 import PIL.JpegImagePlugin
 import PIL.PngImagePlugin
 
-from superduperdb.components.datatype import DataType
+from superduperdb.components.datatype import DataType, DataTypeFactory
 from superduperdb.misc.annotations import component
 
 if t.TYPE_CHECKING:
@@ -87,3 +87,25 @@ pil_image_hybrid = image_type(
     identifier='pil_image_hybrid',
     encodable='artifact',
 )
+
+
+class PilDataTypeFactory(DataTypeFactory):
+    """A factory for pil image # noqa."""
+
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        """Check if the data is a image.
+
+        It's used for registering the auto schema.
+        :param data: The data to check.
+        """
+        return isinstance(data, PIL.Image.Image)
+
+    @staticmethod
+    def create(data: t.Any) -> DataType:
+        """Create a pil_image datatype.
+
+        It's used for registering the auto schema.
+        :param data: The image data.
+        """
+        return pil_image

superduperdb/misc/auto_schema.py
@@ -2,6 +2,7 @@ import importlib
 import typing as t
 
 from superduperdb import logging
+from superduperdb.base.exceptions import UnsupportedDatatype
 from superduperdb.components.datatype import (
     DataType,
     DataTypeFactory,
@@ -68,7 +69,16 @@ def infer_datatype(data: t.Any) -> t.Optional[t.Union[DataType, type]]:
             break
 
     if datatype is None:
-        logging.info(f"Could not infer datatype for data: {data}, using default")
+        try:
+            encoded_data = dill_encode(data)
+            decoded_data = dill_decode(encoded_data)
+            assert isinstance(decoded_data, type(data))
+        except Exception as e:
+            raise UnsupportedDatatype(
+                f"Could not infer datatype for data: {data}"
+            ) from e
+
+        logging.info(f"Inferring default datatype for data: {data}")
         datatype = DEFAULT_DATATYPE
 
     return datatype
@@ -91,7 +101,12 @@ def infer_schema(
 
     schema_data = {}
     for k, v in data.items():
-        data_type = infer_datatype(v)
+        try:
+            data_type = infer_datatype(v)
+        except UnsupportedDatatype as e:
+            raise UnsupportedDatatype(
+                f"Could not infer datatype for key: {k}, value: {v}"
+            ) from e
         if data_type is not None:
             schema_data[k] = data_type
 

test/unittest/misc/test_auto_schema.py
@@ -1,11 +1,14 @@
+import threading
 from test.db_config import DBConfig
 
 import numpy as np
 import pandas as pd
+import PIL
 import pytest
 import torch
 
 from superduperdb.base.document import Document
+from superduperdb.base.exceptions import UnsupportedDatatype
 from superduperdb.components.table import Table
 from superduperdb.misc.auto_schema import infer_datatype, infer_schema
 
@@ -45,6 +48,15 @@ def test_infer_datatype():
         infer_datatype(pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})).identifier
         == "DEFAULT"
     )
+
+    assert (
+        infer_datatype(PIL.Image.open('test/material/data/test.png')).identifier
+        == 'pil_image'
+    )
+
+    with pytest.raises(UnsupportedDatatype):
+        thread = threading.Thread(target=lambda x: x)
+        infer_datatype(thread)
 
 
 def test_infer_schema_mongo(data):

Pinnacle-Networks/Pinnacle

superduperdb/base/datalayer.py
@@ -18,6 +18,7 @@ from pinnacledb.backends.base.metadata import MetaDataStore
 from pinnacledb.backends.base.query import Query
 from pinnacledb.backends.local.compute import LocalComputeBackend
 from pinnacledb.base import exceptions, variables
+from pinnacledb.base.config import Config
 from pinnacledb.base.cursor import SuperDuperCursor
 from pinnacledb.base.document import Document
 from pinnacledb.base.pinnacle import pinnacle
@@ -104,6 +105,7 @@ class Datalayer:
 
         self.compute = compute
         self._server_mode = False
+        self._cfg = s.CFG
 
     def __getitem__(self, item):
         return self.databackend.get_query_builder(item)
@@ -324,7 +326,11 @@ class Datalayer:
         return result, None
 
     def _insert(
-        self, insert: Query, refresh: bool = True, datatypes: t.Sequence[DataType] = ()
+        self,
+        insert: Query,
+        refresh: bool = True,
+        datatypes: t.Sequence[DataType] = (),
+        auto_schema: bool = True,
     ) -> InsertResult:
         """
         Insert data into the database.
@@ -341,6 +347,9 @@ class Datalayer:
             if random.random() < s.CFG.fold_probability:
                 r['_fold'] = 'valid'
 
+        if auto_schema and self.cfg.auto_schema:
+            self.auto_create_table_schema(insert)
+
         inserted_ids = insert.do_execute(self)
 
         cdc_status = self.cdc.running or s.CFG.cluster.cdc.uri is not None
@@ -353,6 +362,23 @@ class Datalayer:
                     insert, ids=inserted_ids, verbose=False
                 )
         return inserted_ids, None
+
+    def auto_create_table_schema(self, insert: Query):
+        """Auto create table schema based on insert query.
+
+        :param insert: The insert query object specifying the data to be inserted.
+        """
+        from pinnacledb.components.table import Table
+
+        table_name = insert.identifier
+        try:
+            table = self.tables[table_name]
+        except FileNotFoundError:
+            table = None
+        if table is None:
+            schema = self.infer_schema(insert.documents[0])
+            table = Table(identifier=table_name, schema=schema)
+            self.apply(table)
 
     def _select(self, select: Query, reference: bool = True) -> SelectResult:
         """
@@ -409,7 +435,7 @@ class Datalayer:
         task_workflow.run_jobs()
         return task_workflow
 
-    def _write(self, write: Query, refresh: bool = True) -> UpdateResult:
+    def _write(self, write: Query, refresh: bool = True, **kwargs) -> UpdateResult:
         """
         Bulk write data to the database.
 
@@ -445,7 +471,7 @@ class Datalayer:
                 return updated_ids, deleted_ids, jobs
         return updated_ids, deleted_ids, None
 
-    def _update(self, update: Query, refresh: bool = True) -> UpdateResult:
+    def _update(self, update: Query, refresh: bool = True, **kwargs) -> UpdateResult:
         """
         Update data in the database.
 
@@ -1075,6 +1101,16 @@ class Datalayer:
         :return: The inferred schema
         """
         return self.databackend.infer_schema(data, identifier)
+
+    def set_cfg(self, cfg: Config):
+        """Set the configuration object for the datalayer."""
+        assert isinstance(cfg, Config)
+        self._cfg = cfg
+
+    @property
+    def cfg(self) -> Config:
+        """Get the configuration object for the datalayer."""
+        return self._cfg or s.CFG
 
 
 @dc.dataclass

superduperdb/components/table.py
@@ -2,6 +2,7 @@ import dataclasses as dc
 import typing as t
 
 from pinnacledb.backends.ibis.field_types import dtype
+from pinnacledb.base.enums import DBType
 from pinnacledb.components.component import Component
 from pinnacledb.components.schema import Schema, _Native
 from pinnacledb.misc.annotations import merge_docstrings
@@ -64,6 +65,10 @@ class Table(Component):
                 )
 
                 return
+
+        if self.db.databackend.db_type == DBType.SQL:
+            if self.primary_id not in self.schema.fields:
+                self.schema.fields[self.primary_id] = dtype('str')  # type: ignore
 
         try:
             db.databackend.create_table_and_schema(self.identifier, self.schema.raw)

superduperdb/ext/pillow/encoder.py
@@ -5,7 +5,7 @@ import PIL.Image
 import PIL.JpegImagePlugin
 import PIL.PngImagePlugin
 
-from pinnacledb.components.datatype import DataType
+from pinnacledb.components.datatype import DataType, DataTypeFactory
 from pinnacledb.misc.annotations import component
 
 if t.TYPE_CHECKING:
@@ -87,3 +87,25 @@ pil_image_hybrid = image_type(
     identifier='pil_image_hybrid',
     encodable='artifact',
 )
+
+
+class PilDataTypeFactory(DataTypeFactory):
+    """A factory for pil image # noqa."""
+
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        """Check if the data is a image.
+
+        It's used for registering the auto schema.
+        :param data: The data to check.
+        """
+        return isinstance(data, PIL.Image.Image)
+
+    @staticmethod
+    def create(data: t.Any) -> DataType:
+        """Create a pil_image datatype.
+
+        It's used for registering the auto schema.
+        :param data: The image data.
+        """
+        return pil_image

superduperdb/misc/auto_schema.py
@@ -2,6 +2,7 @@ import importlib
 import typing as t
 
 from pinnacledb import logging
+from pinnacledb.base.exceptions import UnsupportedDatatype
 from pinnacledb.components.datatype import (
     DataType,
     DataTypeFactory,
@@ -68,7 +69,16 @@ def infer_datatype(data: t.Any) -> t.Optional[t.Union[DataType, type]]:
             break
 
     if datatype is None:
-        logging.info(f"Could not infer datatype for data: {data}, using default")
+        try:
+            encoded_data = dill_encode(data)
+            decoded_data = dill_decode(encoded_data)
+            assert isinstance(decoded_data, type(data))
+        except Exception as e:
+            raise UnsupportedDatatype(
+                f"Could not infer datatype for data: {data}"
+            ) from e
+
+        logging.info(f"Inferring default datatype for data: {data}")
         datatype = DEFAULT_DATATYPE
 
     return datatype
@@ -91,7 +101,12 @@ def infer_schema(
 
     schema_data = {}
     for k, v in data.items():
-        data_type = infer_datatype(v)
+        try:
+            data_type = infer_datatype(v)
+        except UnsupportedDatatype as e:
+            raise UnsupportedDatatype(
+                f"Could not infer datatype for key: {k}, value: {v}"
+            ) from e
         if data_type is not None:
             schema_data[k] = data_type
 

test/unittest/misc/test_auto_schema.py
@@ -1,11 +1,14 @@
+import threading
 from test.db_config import DBConfig
 
 import numpy as np
 import pandas as pd
+import PIL
 import pytest
 import torch
 
 from pinnacledb.base.document import Document
+from pinnacledb.base.exceptions import UnsupportedDatatype
 from pinnacledb.components.table import Table
 from pinnacledb.misc.auto_schema import infer_datatype, infer_schema
 
@@ -45,6 +48,15 @@ def test_infer_datatype():
         infer_datatype(pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})).identifier
         == "DEFAULT"
     )
+
+    assert (
+        infer_datatype(PIL.Image.open('test/material/data/test.png')).identifier
+        == 'pil_image'
+    )
+
+    with pytest.raises(UnsupportedDatatype):
+        thread = threading.Thread(target=lambda x: x)
+        infer_datatype(thread)
 
 
 def test_infer_schema_mongo(data):