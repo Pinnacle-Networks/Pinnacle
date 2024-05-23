superduper-io/superduper

Optimize the logic of auto-schema for different backends.

superduperdb/backends/ibis/data_backend.py
@@ -15,6 +15,7 @@ from superduperdb.backends.ibis.query import IbisQuery
 from superduperdb.backends.local.artifacts import FileSystemArtifactStore
 from superduperdb.backends.sqlalchemy.metadata import SQLAlchemyMetadata
 from superduperdb.base.enums import DBType
+from superduperdb.base.exceptions import SchemaMismatchError
 from superduperdb.components.datatype import DataType
 from superduperdb.components.schema import Schema
 from superduperdb.components.table import Table
@@ -201,3 +202,47 @@ class IbisDataBackend(BaseDataBackend):
         from superduperdb.misc.auto_schema import infer_schema
 
         return infer_schema(data, identifier=identifier, ibis=True)
+
+    def auto_create_table_schema(self, db, table_name, documents):
+        """Auto create table schema.
+
+        For Ibis, we need to create the table schema before inserting the data.
+        The function will infer the schema from the first document and create the table
+        if the table does not exist.
+
+        :param db: The datalayer instanace
+        :param table_name: The table name
+        :param documents: The documents
+        """
+        try:
+            table = db.tables[table_name]
+        except FileNotFoundError:
+            table = None
+
+        schema = self.infer_schema(documents[0])
+        if table is None:
+            # Should we need to check all the documents?
+            table = Table(identifier=table_name, schema=schema)
+            if table.primary_id not in schema.fields:
+                table.schema.fields[table.primary_id] = dtype('str')
+            db.apply(table)
+
+        else:
+            # Only check the shcema built from auto_schema
+            # The schema build from user input should not be checked
+            if not table.schema.identifier.startswith('AUTO:'):
+                return
+            table_schema_identifier = table.schema.identifier.replace("AUTO:", "")
+            table_schema_fields_set = set(table_schema_identifier.split("&"))
+
+            check_schema_identifier = schema.identifier.replace("AUTO:", "")
+            check_schema_fields_set = set(check_schema_identifier.split("&"))
+            # the schema of new documents should be a subset of the existing schema
+            if not table_schema_fields_set.issuperset(check_schema_fields_set):
+                new_fields_set = check_schema_fields_set - table_schema_fields_set
+                error_message = (
+                    f"Cant insert documents with new fields: {new_fields_set} "
+                    f"into the table {table_name}."
+                )
+
+                raise SchemaMismatchError(error_message)

superduperdb/misc/auto_schema.py
@@ -27,6 +27,7 @@ def register_module(module_name):
 
 register_module("superduperdb.ext.numpy.encoder")
 register_module("superduperdb.ext.torch.encoder")
+register_module("superduperdb.ext.pillow.encoder")
 
 
 DEFAULT_DATATYPE = DataType(
@@ -129,6 +130,8 @@ def infer_schema(
 
     if identifier is None:
         raise ValueError("Could not infer schema identifier")
+
+    identifier = "AUTO:" + identifier
 
     return Schema(identifier=identifier, fields=schema_data)  # type: ignore
 

test/unittest/backends/base/test_query.py
@@ -6,6 +6,7 @@ import pytest
 from superduperdb.backends.ibis.field_types import dtype
 from superduperdb.backends.mongodb.query import MongoQuery
 from superduperdb.base.document import Document
+from superduperdb.base.enums import DBType
 from superduperdb.components.schema import Schema
 from superduperdb.components.table import Table
 
@@ -275,8 +276,49 @@ def test_insert_with_schema(db):
     datas = [Document(data)]
 
     table_or_collection.insert(datas).execute()
+    # Make sure multiple insert works
+    table_or_collection.insert(datas).execute()
     datas_from_db = list(table_or_collection.select().execute())
 
     for d, d_db in zip(datas, datas_from_db):
         assert d['img'].size == d_db['img'].size
         assert np.all(d['array'] == d_db['array'])
+
+
+@pytest.mark.parametrize(
+    "db", [DBConfig.sqldb_empty, DBConfig.mongodb_empty], indirect=True
+)
+def test_insert_with_diff_schemas(db):
+    db.cfg.auto_schema = True
+    import numpy as np
+    import PIL.Image
+
+    table_or_collection = db['documents']
+    data = {
+        'array': np.array([1, 2, 3]),
+    }
+    datas = [Document(data)]
+    table_or_collection.insert(datas).execute()
+
+    datas_from_db = list(table_or_collection.select().execute())
+
+    assert np.all(datas[0]['array'] == datas_from_db[0]['array'])
+
+    data = {
+        'img': PIL.Image.open('test/material/data/test.png'),
+    }
+    datas = [Document(data)]
+
+    # Do not support different schema in SQL
+    if db.databackend.db_type == DBType.SQL:
+        from superduperdb.base.exceptions import SchemaMismatchError
+
+        with pytest.raises(SchemaMismatchError) as e:
+            table_or_collection.insert(datas).execute()
+        assert 'img=pil_image' in str(e)
+    # Mongo can support different schema
+    else:
+        table_or_collection.insert(datas).execute()
+        datas_from_db = list(table_or_collection.select().execute())
+
+        assert datas[0]['img'].size == datas_from_db[-1]['img'].size

Pinnacle-Networks/Pinnacle

superduperdb/backends/ibis/data_backend.py
@@ -15,6 +15,7 @@ from pinnacledb.backends.ibis.query import IbisQuery
 from pinnacledb.backends.local.artifacts import FileSystemArtifactStore
 from pinnacledb.backends.sqlalchemy.metadata import SQLAlchemyMetadata
 from pinnacledb.base.enums import DBType
+from pinnacledb.base.exceptions import SchemaMismatchError
 from pinnacledb.components.datatype import DataType
 from pinnacledb.components.schema import Schema
 from pinnacledb.components.table import Table
@@ -201,3 +202,47 @@ class IbisDataBackend(BaseDataBackend):
         from pinnacledb.misc.auto_schema import infer_schema
 
         return infer_schema(data, identifier=identifier, ibis=True)
+
+    def auto_create_table_schema(self, db, table_name, documents):
+        """Auto create table schema.
+
+        For Ibis, we need to create the table schema before inserting the data.
+        The function will infer the schema from the first document and create the table
+        if the table does not exist.
+
+        :param db: The datalayer instanace
+        :param table_name: The table name
+        :param documents: The documents
+        """
+        try:
+            table = db.tables[table_name]
+        except FileNotFoundError:
+            table = None
+
+        schema = self.infer_schema(documents[0])
+        if table is None:
+            # Should we need to check all the documents?
+            table = Table(identifier=table_name, schema=schema)
+            if table.primary_id not in schema.fields:
+                table.schema.fields[table.primary_id] = dtype('str')
+            db.apply(table)
+
+        else:
+            # Only check the shcema built from auto_schema
+            # The schema build from user input should not be checked
+            if not table.schema.identifier.startswith('AUTO:'):
+                return
+            table_schema_identifier = table.schema.identifier.replace("AUTO:", "")
+            table_schema_fields_set = set(table_schema_identifier.split("&"))
+
+            check_schema_identifier = schema.identifier.replace("AUTO:", "")
+            check_schema_fields_set = set(check_schema_identifier.split("&"))
+            # the schema of new documents should be a subset of the existing schema
+            if not table_schema_fields_set.issuperset(check_schema_fields_set):
+                new_fields_set = check_schema_fields_set - table_schema_fields_set
+                error_message = (
+                    f"Cant insert documents with new fields: {new_fields_set} "
+                    f"into the table {table_name}."
+                )
+
+                raise SchemaMismatchError(error_message)

superduperdb/misc/auto_schema.py
@@ -27,6 +27,7 @@ def register_module(module_name):
 
 register_module("pinnacledb.ext.numpy.encoder")
 register_module("pinnacledb.ext.torch.encoder")
+register_module("pinnacledb.ext.pillow.encoder")
 
 
 DEFAULT_DATATYPE = DataType(
@@ -129,6 +130,8 @@ def infer_schema(
 
     if identifier is None:
         raise ValueError("Could not infer schema identifier")
+
+    identifier = "AUTO:" + identifier
 
     return Schema(identifier=identifier, fields=schema_data)  # type: ignore
 

test/unittest/backends/base/test_query.py
@@ -6,6 +6,7 @@ import pytest
 from pinnacledb.backends.ibis.field_types import dtype
 from pinnacledb.backends.mongodb.query import MongoQuery
 from pinnacledb.base.document import Document
+from pinnacledb.base.enums import DBType
 from pinnacledb.components.schema import Schema
 from pinnacledb.components.table import Table
 
@@ -275,8 +276,49 @@ def test_insert_with_schema(db):
     datas = [Document(data)]
 
     table_or_collection.insert(datas).execute()
+    # Make sure multiple insert works
+    table_or_collection.insert(datas).execute()
     datas_from_db = list(table_or_collection.select().execute())
 
     for d, d_db in zip(datas, datas_from_db):
         assert d['img'].size == d_db['img'].size
         assert np.all(d['array'] == d_db['array'])
+
+
+@pytest.mark.parametrize(
+    "db", [DBConfig.sqldb_empty, DBConfig.mongodb_empty], indirect=True
+)
+def test_insert_with_diff_schemas(db):
+    db.cfg.auto_schema = True
+    import numpy as np
+    import PIL.Image
+
+    table_or_collection = db['documents']
+    data = {
+        'array': np.array([1, 2, 3]),
+    }
+    datas = [Document(data)]
+    table_or_collection.insert(datas).execute()
+
+    datas_from_db = list(table_or_collection.select().execute())
+
+    assert np.all(datas[0]['array'] == datas_from_db[0]['array'])
+
+    data = {
+        'img': PIL.Image.open('test/material/data/test.png'),
+    }
+    datas = [Document(data)]
+
+    # Do not support different schema in SQL
+    if db.databackend.db_type == DBType.SQL:
+        from pinnacledb.base.exceptions import SchemaMismatchError
+
+        with pytest.raises(SchemaMismatchError) as e:
+            table_or_collection.insert(datas).execute()
+        assert 'img=pil_image' in str(e)
+    # Mongo can support different schema
+    else:
+        table_or_collection.insert(datas).execute()
+        datas_from_db = list(table_or_collection.select().execute())
+
+        assert datas[0]['img'].size == datas_from_db[-1]['img'].size