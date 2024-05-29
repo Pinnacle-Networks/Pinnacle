superduper-io/superduper

Fixed the bug that activates the downstream listener.

superduperdb/components/schema.py
@@ -5,7 +5,7 @@ from functools import cached_property
 from overrides import override
 
 from superduperdb.components.component import Component
-from superduperdb.components.datatype import DataType
+from superduperdb.components.datatype import DataType, _BaseEncodable
 from superduperdb.misc.annotations import merge_docstrings
 from superduperdb.misc.special_dicts import SuperDuperFlatEncode
 
@@ -75,11 +75,16 @@ class Schema(Component):
         :param files: Files for encoding.
         :param leaves_to_keep: Leaves to keep.
         """
-        for k in self.fields:
+        for k, datatype in self.fields.items():
             if k not in r:
                 continue
-            if isinstance(self.fields[k], DataType):
-                encodable = self.fields[k](r[k])
+            value = r[k]
+            if isinstance(datatype, DataType):
+                if isinstance(value, _BaseEncodable):
+                    assert value.datatype.identifier == datatype.identifier
+                    encodable = value
+                else:
+                    encodable = datatype(value)
                 if isinstance(encodable, leaves_to_keep):
                     continue
                 r[k] = encodable._deep_flat_encode(

test/unittest/jobs/task_workflow.py
@@ -1,0 +1,103 @@
+from test.db_config import DBConfig
+
+import ibis
+
+ibis.options.interactive = True
+
+import numpy as np
+import pytest
+
+from superduperdb import Document
+from superduperdb.components.model import ObjectModel
+
+EMPTY_CASES = [DBConfig.sqldb_empty, DBConfig.mongodb_empty]
+
+
+def assert_output_is_correct(data, output):
+    if isinstance(data, np.ndarray):
+        assert np.allclose(output, data)
+    else:
+        assert output == data
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        2,
+        np.array([[1, 1, 1], [1, 1, 1]]),
+    ],
+)
+@pytest.mark.parametrize("db", EMPTY_CASES, indirect=True)
+@pytest.mark.parametrize("document_embedded", [False, True])
+@pytest.mark.parametrize("flatten", [True, False])
+def test_downstream_task_workflows_are_triggered(db, data, flatten, document_embedded):
+    # Do not support flatten is True and document_embedded is True
+    if flatten is True and document_embedded is True:
+        return
+
+    db.cfg.auto_schema = True
+
+    db.execute(db["test"].insert([{"x": 10}]))
+
+    upstream_model = ObjectModel(
+        "m1",
+        object=lambda x: data * x if not flatten else [data * x] * 10,
+        flatten=flatten,
+        model_update_kwargs={"document_embedded": document_embedded},
+    )
+
+    upstream_listener = upstream_model.to_listener(
+        key="x",
+        select=db["test"].select(),
+        uuid="upstream",
+    )
+
+    db.apply(upstream_listener)
+
+    downstream_model = ObjectModel(
+        "m2",
+        object=lambda x: x / 2,
+    )
+
+    downstream_listener = downstream_model.to_listener(
+        key=upstream_listener.outputs_key,
+        select=upstream_listener.outputs_select,
+        uuid="downstream",
+    )
+
+    db.apply(downstream_listener)
+
+    outputs1 = [
+        Document(d.unpack())[upstream_listener.outputs_key]
+        for d in db.execute(upstream_listener.outputs_select)
+    ]
+
+    outputs2 = [
+        Document(d.unpack())[downstream_listener.outputs_key]
+        for d in db.execute(downstream_listener.outputs_select)
+    ]
+
+    assert len(outputs1) == 1 if not flatten else 10
+    assert len(outputs2) == 1 if not flatten else 10
+
+    assert_output_is_correct(data * 10, outputs1[0])
+    assert_output_is_correct(data * 10 / 2, outputs2[0])
+
+    db.execute(db["test"].insert([{"x": 20}]))
+
+    # Check that the listeners are triggered when data is inserted later
+    outputs1 = [
+        Document(d.unpack())[upstream_listener.outputs_key]
+        for d in db.execute(upstream_listener.outputs_select)
+    ]
+
+    outputs2 = [
+        Document(d.unpack())[downstream_listener.outputs_key]
+        for d in db.execute(downstream_listener.outputs_select)
+    ]
+
+    assert len(outputs1) == 2 if not flatten else 20
+    assert len(outputs2) == 2 if not flatten else 20
+
+    assert_output_is_correct(data * 20, outputs1[-1])
+    assert_output_is_correct(data * 20 / 2, outputs2[-1])

Pinnacle-Networks/Pinnacle

superduperdb/components/schema.py
@@ -5,7 +5,7 @@ from functools import cached_property
 from overrides import override
 
 from pinnacledb.components.component import Component
-from pinnacledb.components.datatype import DataType
+from pinnacledb.components.datatype import DataType, _BaseEncodable
 from pinnacledb.misc.annotations import merge_docstrings
 from pinnacledb.misc.special_dicts import SuperDuperFlatEncode
 
@@ -75,11 +75,16 @@ class Schema(Component):
         :param files: Files for encoding.
         :param leaves_to_keep: Leaves to keep.
         """
-        for k in self.fields:
+        for k, datatype in self.fields.items():
             if k not in r:
                 continue
-            if isinstance(self.fields[k], DataType):
-                encodable = self.fields[k](r[k])
+            value = r[k]
+            if isinstance(datatype, DataType):
+                if isinstance(value, _BaseEncodable):
+                    assert value.datatype.identifier == datatype.identifier
+                    encodable = value
+                else:
+                    encodable = datatype(value)
                 if isinstance(encodable, leaves_to_keep):
                     continue
                 r[k] = encodable._deep_flat_encode(

test/unittest/jobs/task_workflow.py
@@ -1,0 +1,103 @@
+from test.db_config import DBConfig
+
+import ibis
+
+ibis.options.interactive = True
+
+import numpy as np
+import pytest
+
+from pinnacledb import Document
+from pinnacledb.components.model import ObjectModel
+
+EMPTY_CASES = [DBConfig.sqldb_empty, DBConfig.mongodb_empty]
+
+
+def assert_output_is_correct(data, output):
+    if isinstance(data, np.ndarray):
+        assert np.allclose(output, data)
+    else:
+        assert output == data
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        2,
+        np.array([[1, 1, 1], [1, 1, 1]]),
+    ],
+)
+@pytest.mark.parametrize("db", EMPTY_CASES, indirect=True)
+@pytest.mark.parametrize("document_embedded", [False, True])
+@pytest.mark.parametrize("flatten", [True, False])
+def test_downstream_task_workflows_are_triggered(db, data, flatten, document_embedded):
+    # Do not support flatten is True and document_embedded is True
+    if flatten is True and document_embedded is True:
+        return
+
+    db.cfg.auto_schema = True
+
+    db.execute(db["test"].insert([{"x": 10}]))
+
+    upstream_model = ObjectModel(
+        "m1",
+        object=lambda x: data * x if not flatten else [data * x] * 10,
+        flatten=flatten,
+        model_update_kwargs={"document_embedded": document_embedded},
+    )
+
+    upstream_listener = upstream_model.to_listener(
+        key="x",
+        select=db["test"].select(),
+        uuid="upstream",
+    )
+
+    db.apply(upstream_listener)
+
+    downstream_model = ObjectModel(
+        "m2",
+        object=lambda x: x / 2,
+    )
+
+    downstream_listener = downstream_model.to_listener(
+        key=upstream_listener.outputs_key,
+        select=upstream_listener.outputs_select,
+        uuid="downstream",
+    )
+
+    db.apply(downstream_listener)
+
+    outputs1 = [
+        Document(d.unpack())[upstream_listener.outputs_key]
+        for d in db.execute(upstream_listener.outputs_select)
+    ]
+
+    outputs2 = [
+        Document(d.unpack())[downstream_listener.outputs_key]
+        for d in db.execute(downstream_listener.outputs_select)
+    ]
+
+    assert len(outputs1) == 1 if not flatten else 10
+    assert len(outputs2) == 1 if not flatten else 10
+
+    assert_output_is_correct(data * 10, outputs1[0])
+    assert_output_is_correct(data * 10 / 2, outputs2[0])
+
+    db.execute(db["test"].insert([{"x": 20}]))
+
+    # Check that the listeners are triggered when data is inserted later
+    outputs1 = [
+        Document(d.unpack())[upstream_listener.outputs_key]
+        for d in db.execute(upstream_listener.outputs_select)
+    ]
+
+    outputs2 = [
+        Document(d.unpack())[downstream_listener.outputs_key]
+        for d in db.execute(downstream_listener.outputs_select)
+    ]
+
+    assert len(outputs1) == 2 if not flatten else 20
+    assert len(outputs2) == 2 if not flatten else 20
+
+    assert_output_is_correct(data * 20, outputs1[-1])
+    assert_output_is_correct(data * 20 / 2, outputs2[-1])