superduper-io/superduper

Bring docs up-to-date

superduperdb/__init__.py
@@ -16,6 +16,7 @@ from .base.decorators import code
 from .base.document import Document
 from .base.variables import Variable
 from .components.application import Application
+from .components.component import Component
 from .components.dataset import Dataset
 from .components.datatype import DataType, Encoder
 from .components.listener import Listener
@@ -32,6 +33,10 @@ from .components.schema import Schema
 from .components.stack import Stack
 from .components.template import Template
 from .components.vector_index import VectorIndex, vector
+
+REQUIRES = [
+    'superduperdb=={}'.format(__version__),
+]
 
 __all__ = (
     'CFG',
@@ -62,4 +67,5 @@ __all__ = (
     'Template',
     'Application',
     'Variable',
+    'Component',
 )

superduperdb/base/variables.py
@@ -1,7 +1,9 @@
 import dataclasses as dc
 import re
+import typing as t
 
 from superduperdb.base.leaf import Leaf
+from superduperdb.components.schema import Schema
 from superduperdb.misc.annotations import merge_docstrings
 
 
@@ -87,9 +89,23 @@ class Variable(Leaf):
         self.value = self.identifier
 
     @property
+    def _id(self):
+        return f'variable/{self.identifier}'
+
+    @property
     def key(self):
         """Variable key."""
         return f'<var:{str(self.value)}>'
+
+    def _deep_flat_encode(
+        self, cache, blobs, files, leaves_to_keep=(), schema: t.Optional[Schema] = None
+    ):
+        r = super()._deep_flat_encode(cache, blobs, files, leaves_to_keep, schema)
+        if isinstance(self, leaves_to_keep):
+            cache[self._id] = self
+            return f'?{self._id}'
+        cache[self._id] = r
+        return f'?{self._id}'
 
     def __repr__(self) -> str:
         return self.key

superduperdb/rest/app.py
@@ -6,7 +6,6 @@ import yaml
 from fastapi import File, Response
 
 from superduperdb import CFG, logging
-from superduperdb.base.datalayer import Datalayer
 from superduperdb.base.document import Document
 from superduperdb.server import app as superduperapp
 
@@ -15,23 +14,15 @@ assert isinstance(
 ), "cluster.rest.uri should be set with a valid uri"
 port = int(CFG.cluster.rest.uri.split(':')[-1])
 
-assert CFG.cluster.rest.config, "cluster.rest.config should be set with a valid path"
-with open(CFG.cluster.rest.config) as f:
-    CONFIG = yaml.safe_load(f)
+if CFG.cluster.rest.config is not None:
+    try:
+        with open(CFG.cluster.rest.config) as f:
+            CONFIG = yaml.safe_load(f)
+    except FileNotFoundError:
+        logging.warn("cluster.rest.config should be set with a valid path")
+        CONFIG = {}
 
 app = superduperapp.SuperDuperApp('rest', port=port)
-
-
-def _init_hook(db: Datalayer):
-    for type_id in CONFIG['presets']:
-        for leaf in CONFIG['presets'][type_id]:
-            leaf = CONFIG['presets'][type_id][leaf]
-            leaf = Document.decode(leaf).unpack()
-            t = db.type_id_to_cache_mapping[type_id]
-            getattr(db, t)[leaf.identifier] = leaf
-
-
-app.init_hook = _init_hook
 
 
 def build_app(app: superduperapp.SuperDuperApp):
@@ -43,6 +34,20 @@ def build_app(app: superduperapp.SuperDuperApp):
 
     @app.add('/spec/show', method='get')
     def spec_show():
+        template_ids = app.db.show(type_id='template')
+        tmp = {
+            'application': {
+                tid: {
+                    '_path': f'superduperdb/applications/{tid}',
+                    'identifier': {'type': 'str'},
+                    **app.db.show(type_id='template', identifier=tid, version=-1)[
+                        'info'
+                    ],
+                }
+                for tid in template_ids
+            }
+        }
+        CONFIG['leaves'].update(tmp)
         return CONFIG
 
     @app.add('/db/artifact_store/put', method='put')
@@ -78,7 +83,6 @@ def build_app(app: superduperapp.SuperDuperApp):
             type_id=type_id,
             identifier=identifier,
             version=version,
-            include_presets=True,
         )
 
     @app.add('/db/metadata/show_jobs', method='get')
@@ -114,10 +118,9 @@ def build_app(app: superduperapp.SuperDuperApp):
         if isinstance(result, Document):
             result = [result]
 
-        result = [dict(r.encode()) for r in result]
-        for r in result:
-            r['_blobs'] = r['_blobs'].keys()
-        return list(result)
+        result = [r.encode() for r in result]
+        result = [(dict(r), list(r.pop_blobs().keys())) for r in result]
+        return result
 
 
 build_app(app)

test/unittest/test_docstrings.py
@@ -222,9 +222,8 @@ def extract_docstrings():
             continue
         exec(f'import superduperdb.ext.{subpackage}')
         package = eval(f'superduperdb.ext.{subpackage}')
-        members += list_all_members(
-            package=package, prefix=f'superduperdb.ext.{subpackage}'
-        )
+        tmp = list_all_members(package=package, prefix=f'superduperdb.ext.{subpackage}')
+        members.extend(tmp)
     from superduperdb.misc.special_dicts import MongoStyleDict
 
     lookup = MongoStyleDict({})

Pinnacle-Networks/Pinnacle

superduperdb/__init__.py
@@ -16,6 +16,7 @@ from .base.decorators import code
 from .base.document import Document
 from .base.variables import Variable
 from .components.application import Application
+from .components.component import Component
 from .components.dataset import Dataset
 from .components.datatype import DataType, Encoder
 from .components.listener import Listener
@@ -32,6 +33,10 @@ from .components.schema import Schema
 from .components.stack import Stack
 from .components.template import Template
 from .components.vector_index import VectorIndex, vector
+
+REQUIRES = [
+    'pinnacledb=={}'.format(__version__),
+]
 
 __all__ = (
     'CFG',
@@ -62,4 +67,5 @@ __all__ = (
     'Template',
     'Application',
     'Variable',
+    'Component',
 )

superduperdb/base/variables.py
@@ -1,7 +1,9 @@
 import dataclasses as dc
 import re
+import typing as t
 
 from pinnacledb.base.leaf import Leaf
+from pinnacledb.components.schema import Schema
 from pinnacledb.misc.annotations import merge_docstrings
 
 
@@ -87,9 +89,23 @@ class Variable(Leaf):
         self.value = self.identifier
 
     @property
+    def _id(self):
+        return f'variable/{self.identifier}'
+
+    @property
     def key(self):
         """Variable key."""
         return f'<var:{str(self.value)}>'
+
+    def _deep_flat_encode(
+        self, cache, blobs, files, leaves_to_keep=(), schema: t.Optional[Schema] = None
+    ):
+        r = super()._deep_flat_encode(cache, blobs, files, leaves_to_keep, schema)
+        if isinstance(self, leaves_to_keep):
+            cache[self._id] = self
+            return f'?{self._id}'
+        cache[self._id] = r
+        return f'?{self._id}'
 
     def __repr__(self) -> str:
         return self.key

superduperdb/rest/app.py
@@ -6,7 +6,6 @@ import yaml
 from fastapi import File, Response
 
 from pinnacledb import CFG, logging
-from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.base.document import Document
 from pinnacledb.server import app as pinnacleapp
 
@@ -15,23 +14,15 @@ assert isinstance(
 ), "cluster.rest.uri should be set with a valid uri"
 port = int(CFG.cluster.rest.uri.split(':')[-1])
 
-assert CFG.cluster.rest.config, "cluster.rest.config should be set with a valid path"
-with open(CFG.cluster.rest.config) as f:
-    CONFIG = yaml.safe_load(f)
+if CFG.cluster.rest.config is not None:
+    try:
+        with open(CFG.cluster.rest.config) as f:
+            CONFIG = yaml.safe_load(f)
+    except FileNotFoundError:
+        logging.warn("cluster.rest.config should be set with a valid path")
+        CONFIG = {}
 
 app = pinnacleapp.SuperDuperApp('rest', port=port)
-
-
-def _init_hook(db: Datalayer):
-    for type_id in CONFIG['presets']:
-        for leaf in CONFIG['presets'][type_id]:
-            leaf = CONFIG['presets'][type_id][leaf]
-            leaf = Document.decode(leaf).unpack()
-            t = db.type_id_to_cache_mapping[type_id]
-            getattr(db, t)[leaf.identifier] = leaf
-
-
-app.init_hook = _init_hook
 
 
 def build_app(app: pinnacleapp.SuperDuperApp):
@@ -43,6 +34,20 @@ def build_app(app: pinnacleapp.SuperDuperApp):
 
     @app.add('/spec/show', method='get')
     def spec_show():
+        template_ids = app.db.show(type_id='template')
+        tmp = {
+            'application': {
+                tid: {
+                    '_path': f'pinnacledb/applications/{tid}',
+                    'identifier': {'type': 'str'},
+                    **app.db.show(type_id='template', identifier=tid, version=-1)[
+                        'info'
+                    ],
+                }
+                for tid in template_ids
+            }
+        }
+        CONFIG['leaves'].update(tmp)
         return CONFIG
 
     @app.add('/db/artifact_store/put', method='put')
@@ -78,7 +83,6 @@ def build_app(app: pinnacleapp.SuperDuperApp):
             type_id=type_id,
             identifier=identifier,
             version=version,
-            include_presets=True,
         )
 
     @app.add('/db/metadata/show_jobs', method='get')
@@ -114,10 +118,9 @@ def build_app(app: pinnacleapp.SuperDuperApp):
         if isinstance(result, Document):
             result = [result]
 
-        result = [dict(r.encode()) for r in result]
-        for r in result:
-            r['_blobs'] = r['_blobs'].keys()
-        return list(result)
+        result = [r.encode() for r in result]
+        result = [(dict(r), list(r.pop_blobs().keys())) for r in result]
+        return result
 
 
 build_app(app)

test/unittest/test_docstrings.py
@@ -222,9 +222,8 @@ def extract_docstrings():
             continue
         exec(f'import pinnacledb.ext.{subpackage}')
         package = eval(f'pinnacledb.ext.{subpackage}')
-        members += list_all_members(
-            package=package, prefix=f'pinnacledb.ext.{subpackage}'
-        )
+        tmp = list_all_members(package=package, prefix=f'pinnacledb.ext.{subpackage}')
+        members.extend(tmp)
     from pinnacledb.misc.special_dicts import MongoStyleDict
 
     lookup = MongoStyleDict({})