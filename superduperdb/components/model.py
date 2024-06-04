superduper-io/superduper

Fixed some bugs of multimodal vector search use case

superduperdb/components/model.py
@@ -509,6 +509,18 @@ class Model(Component):
         if not self.identifier:
             raise Exception('_Predictor identifier must be non-empty')
 
+    def pre_create(self, db):
+        """Method to run before creating the model in the database.
+
+        :param db: Datalayer instance.
+        """
+        if isinstance(self.datatype, DataType):
+            from superduperdb.misc.datatype_helper import convert_model_datatype
+
+            is_ibis = db.databackend.db_type == DBType.SQL
+            self.datatype = convert_model_datatype(self.datatype, is_ibis)
+        super().pre_create(db)
+
     @property
     def inputs(self) -> Inputs:
         """Instance of `Inputs` to represent model params."""

superduperdb/misc/datatype_helper.py
@@ -1,0 +1,22 @@
+from superduperdb import logging
+from superduperdb.components.datatype import DataType
+
+
+def convert_model_datatype(datatype, ibis=False):
+    """Convert a model datatype to a SQL vector datatype.
+
+    :param datatype: The datatype to convert
+    :param ibis: Whether to use ibis
+    """
+    if not isinstance(datatype, DataType):
+        return datatype
+
+    # If the datatype is a vector, convert it to a SQL vector
+    if ibis and datatype.identifier.startswith('vector['):
+        from superduperdb.components.vector_index import sqlvector
+
+        new_datatype = sqlvector(shape=datatype.shape)
+        logging.warn(f"Detected useing ibis, converting {datatype} to {new_datatype}")
+        return new_datatype
+
+    return datatype

Pinnacle-Networks/Pinnacle

superduperdb/components/model.py
@@ -509,6 +509,18 @@ class Model(Component):
         if not self.identifier:
             raise Exception('_Predictor identifier must be non-empty')
 
+    def pre_create(self, db):
+        """Method to run before creating the model in the database.
+
+        :param db: Datalayer instance.
+        """
+        if isinstance(self.datatype, DataType):
+            from pinnacledb.misc.datatype_helper import convert_model_datatype
+
+            is_ibis = db.databackend.db_type == DBType.SQL
+            self.datatype = convert_model_datatype(self.datatype, is_ibis)
+        super().pre_create(db)
+
     @property
     def inputs(self) -> Inputs:
         """Instance of `Inputs` to represent model params."""

superduperdb/misc/datatype_helper.py
@@ -1,0 +1,22 @@
+from pinnacledb import logging
+from pinnacledb.components.datatype import DataType
+
+
+def convert_model_datatype(datatype, ibis=False):
+    """Convert a model datatype to a SQL vector datatype.
+
+    :param datatype: The datatype to convert
+    :param ibis: Whether to use ibis
+    """
+    if not isinstance(datatype, DataType):
+        return datatype
+
+    # If the datatype is a vector, convert it to a SQL vector
+    if ibis and datatype.identifier.startswith('vector['):
+        from pinnacledb.components.vector_index import sqlvector
+
+        new_datatype = sqlvector(shape=datatype.shape)
+        logging.warn(f"Detected useing ibis, converting {datatype} to {new_datatype}")
+        return new_datatype
+
+    return datatype