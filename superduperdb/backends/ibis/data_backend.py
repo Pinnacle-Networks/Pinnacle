superduper-io/superduper

Fixed saving artifact and some other bugs

superduperdb/backends/ibis/data_backend.py
@@ -13,6 +13,7 @@ from superduperdb.backends.base.data_backend import BaseDataBackend
 from superduperdb.backends.ibis.db_helper import get_db_helper
 from superduperdb.backends.ibis.field_types import FieldType, dtype
 from superduperdb.backends.ibis.query import IbisQuery
+from superduperdb.backends.ibis.utils import convert_schema_to_fields
 from superduperdb.backends.local.artifacts import FileSystemArtifactStore
 from superduperdb.backends.sqlalchemy.metadata import SQLAlchemyMetadata
 from superduperdb.base.enums import DBType
@@ -98,6 +99,7 @@ class IbisDataBackend(BaseDataBackend):
         :param datatype: The data type of the output.
         :param flatten: Whether to flatten the output.
         """
+        # TODO: Support output schema
         msg = (
             "Model must have an encoder to create with the"
             f" {type(self).__name__} backend."
@@ -113,6 +115,7 @@ class IbisDataBackend(BaseDataBackend):
                 INPUT_KEY: dtype('string'),
                 'id': dtype('string'),
                 'output': output_type,
+                '_info': dtype('string'),
             }
             return Table(
                 primary_id='id',
@@ -124,6 +127,7 @@ class IbisDataBackend(BaseDataBackend):
                 INPUT_KEY: dtype('string'),
                 'output': output_type,
                 'id': dtype('string'),
+                '_info': dtype('string'),
             }
             return Table(
                 identifier=f'_outputs.{predict_id}',
@@ -141,14 +145,16 @@ class IbisDataBackend(BaseDataBackend):
         except NoSuchTableError:
             return False
 
-    def create_table_and_schema(self, identifier: str, mapping: dict):
+    def create_table_and_schema(self, identifier: str, schema: Schema):
         """Create a schema in the data-backend.
 
         :param identifier: The identifier of the table.
         :param mapping: The mapping of the schema.
         """
+        mapping = convert_schema_to_fields(schema)
         if 'id' not in mapping:
             mapping['id'] = 'string'
+        mapping['_info'] = 'string'
         try:
             mapping = self.db_helper.process_schema_types(mapping)
             t = self.conn.create_table(identifier, schema=ibis.schema(mapping))

superduperdb/backends/ibis/utils.py
@@ -1,0 +1,30 @@
+from superduperdb.components.datatype import DataType
+from superduperdb.components.schema import Schema
+
+SPECIAL_ENCODABLES_FIELDS = {
+    "file": "String",
+    "lazy_file": "String",
+    'artifact': 'String',
+    'lazy_artifact': 'String',
+}
+
+
+def convert_schema_to_fields(schema: Schema):
+    """Return the raw fields.
+
+    Get a dictionary of fields as keys and datatypes as values.
+    This is used to create ibis tables.
+    :param schema: The schema to convert
+    """
+    fields = {}
+
+    for k, v in schema.fields.items():
+        if not isinstance(v, DataType):
+            fields[k] = v.identifier
+        else:
+            if v.encodable_cls.leaf_type in SPECIAL_ENCODABLES_FIELDS:
+                fields[k] = SPECIAL_ENCODABLES_FIELDS[v.encodable_cls.leaf_type]
+            else:
+                fields[k] = v.bytes_encoding
+
+    return fields

test/unittest/component/datatype/test_file.py
@@ -5,8 +5,7 @@ from test.db_config import DBConfig
 
 import pytest
 
-from superduperdb import Document
-from superduperdb.backends.mongodb import MongoQuery
+from superduperdb import DataType, Schema
 from superduperdb.components.component import Component
 from superduperdb.components.datatype import (
     Empty,
@@ -14,6 +13,7 @@ from superduperdb.components.datatype import (
     file_lazy,
     file_serializer,
 )
+from superduperdb.components.table import Table
 
 
 @dc.dataclass(kw_only=True)
@@ -34,7 +34,7 @@ def random_file(tmpdir):
     return file_name
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
 def test_reference(db, random_file):
     c = SpecialComponent("test", my_file=random_file)
 
@@ -48,7 +48,7 @@ def test_reference(db, random_file):
         assert f.read() == f2.read()
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
 def test_reference_lazy(db, random_file):
     c = SpecialComponent("test", my_file_lazy=random_file)
 
@@ -63,16 +63,17 @@ def test_reference_lazy(db, random_file):
         assert f.read() == f2.read()
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
 def test_file(db, random_file):
-    from superduperdb import DataType
-
     dt = DataType("my-file", encodable="file")
-    db.apply(dt)
-    collection = MongoQuery("my-file", db=db)
-    db.execute(collection.insert_one(Document({"x": dt(random_file)})))
+    schema = Schema(identifier="schema", fields={"x": dt})
+    table_or_collection = Table("documents", schema=schema)
+    db.apply(table_or_collection)
 
-    data = db.execute(collection.find_one()).unpack()
+    collection = db['documents']
+    collection.insert([{"x": random_file}]).execute()
+
+    data = list(db.execute(collection.select()))[0].unpack()
 
     path = data["x"]
     assert os.path.exists(path)
@@ -82,16 +83,19 @@ def test_file(db, random_file):
         assert f.read() == f2.read()
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
 def test_file_lazy(db, random_file):
     from superduperdb import DataType
 
     dt = DataType("my-file", encodable="lazy_file")
-    db.apply(dt)
-    collection = MongoQuery("my-file")
-    db.execute(collection.insert_one(Document({"x": dt(random_file)})))
+    schema = Schema(identifier="schema", fields={"x": dt})
+    table_or_collection = Table("documents", schema=schema)
+    db.apply(table_or_collection)
 
-    data = db.execute(collection.find_one())
+    collection = db['documents']
+    collection.insert([{"x": random_file}]).execute()
+
+    data = list(db.execute(collection.select()))[0]
 
     assert isinstance(data["x"].x, Empty)
     data = data.unpack()

test/unittest/component/datatype/test_pickle.py
@@ -7,9 +7,14 @@ import pytest
 
 from superduperdb.components.component import Component
 from superduperdb.components.datatype import (
+    DataType,
+    Empty,
+    pickle_encoder,
     pickle_lazy,
     pickle_serializer,
 )
+from superduperdb.components.schema import Schema
+from superduperdb.components.table import Table
 
 
 @dc.dataclass(kw_only=True)
@@ -31,7 +36,7 @@ def random_data():
     return df
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
 def test_reference(db, random_data):
     c = SpecialComponent("test", my_data=random_data)
 
@@ -41,3 +46,28 @@ def test_reference(db, random_data):
     reloaded.init()
     assert isinstance(reloaded.my_data, pd.DataFrame)
     assert c.my_data.equals(reloaded.my_data)
+
+
+@pytest.mark.parametrize(
+    "datatype",
+    [
+        pickle_encoder,
+        pickle_serializer,
+        pickle_lazy,
+    ],
+)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
+def test_data(db, datatype: DataType, random_data: pd.DataFrame):
+    schema = Schema(identifier="schema", fields={"x": datatype})
+    table_or_collection = Table("documents", schema=schema)
+    db.apply(table_or_collection)
+
+    collection = db["documents"]
+    collection.insert([{"x": random_data}]).execute()
+
+    loaded_data = list(db.execute(collection.select()))[0]
+    if datatype.encodable_cls.lazy:
+        assert isinstance(loaded_data["x"].x, Empty)
+        assert random_data.equals(loaded_data.unpack()["x"])
+    else:
+        assert random_data.equals(loaded_data.unpack()['x'])

Pinnacle-Networks/Pinnacle

superduperdb/backends/ibis/data_backend.py
@@ -13,6 +13,7 @@ from pinnacledb.backends.base.data_backend import BaseDataBackend
 from pinnacledb.backends.ibis.db_helper import get_db_helper
 from pinnacledb.backends.ibis.field_types import FieldType, dtype
 from pinnacledb.backends.ibis.query import IbisQuery
+from pinnacledb.backends.ibis.utils import convert_schema_to_fields
 from pinnacledb.backends.local.artifacts import FileSystemArtifactStore
 from pinnacledb.backends.sqlalchemy.metadata import SQLAlchemyMetadata
 from pinnacledb.base.enums import DBType
@@ -98,6 +99,7 @@ class IbisDataBackend(BaseDataBackend):
         :param datatype: The data type of the output.
         :param flatten: Whether to flatten the output.
         """
+        # TODO: Support output schema
         msg = (
             "Model must have an encoder to create with the"
             f" {type(self).__name__} backend."
@@ -113,6 +115,7 @@ class IbisDataBackend(BaseDataBackend):
                 INPUT_KEY: dtype('string'),
                 'id': dtype('string'),
                 'output': output_type,
+                '_info': dtype('string'),
             }
             return Table(
                 primary_id='id',
@@ -124,6 +127,7 @@ class IbisDataBackend(BaseDataBackend):
                 INPUT_KEY: dtype('string'),
                 'output': output_type,
                 'id': dtype('string'),
+                '_info': dtype('string'),
             }
             return Table(
                 identifier=f'_outputs.{predict_id}',
@@ -141,14 +145,16 @@ class IbisDataBackend(BaseDataBackend):
         except NoSuchTableError:
             return False
 
-    def create_table_and_schema(self, identifier: str, mapping: dict):
+    def create_table_and_schema(self, identifier: str, schema: Schema):
         """Create a schema in the data-backend.
 
         :param identifier: The identifier of the table.
         :param mapping: The mapping of the schema.
         """
+        mapping = convert_schema_to_fields(schema)
         if 'id' not in mapping:
             mapping['id'] = 'string'
+        mapping['_info'] = 'string'
         try:
             mapping = self.db_helper.process_schema_types(mapping)
             t = self.conn.create_table(identifier, schema=ibis.schema(mapping))

superduperdb/backends/ibis/utils.py
@@ -1,0 +1,30 @@
+from pinnacledb.components.datatype import DataType
+from pinnacledb.components.schema import Schema
+
+SPECIAL_ENCODABLES_FIELDS = {
+    "file": "String",
+    "lazy_file": "String",
+    'artifact': 'String',
+    'lazy_artifact': 'String',
+}
+
+
+def convert_schema_to_fields(schema: Schema):
+    """Return the raw fields.
+
+    Get a dictionary of fields as keys and datatypes as values.
+    This is used to create ibis tables.
+    :param schema: The schema to convert
+    """
+    fields = {}
+
+    for k, v in schema.fields.items():
+        if not isinstance(v, DataType):
+            fields[k] = v.identifier
+        else:
+            if v.encodable_cls.leaf_type in SPECIAL_ENCODABLES_FIELDS:
+                fields[k] = SPECIAL_ENCODABLES_FIELDS[v.encodable_cls.leaf_type]
+            else:
+                fields[k] = v.bytes_encoding
+
+    return fields

test/unittest/component/datatype/test_file.py
@@ -5,8 +5,7 @@ from test.db_config import DBConfig
 
 import pytest
 
-from pinnacledb import Document
-from pinnacledb.backends.mongodb import MongoQuery
+from pinnacledb import DataType, Schema
 from pinnacledb.components.component import Component
 from pinnacledb.components.datatype import (
     Empty,
@@ -14,6 +13,7 @@ from pinnacledb.components.datatype import (
     file_lazy,
     file_serializer,
 )
+from pinnacledb.components.table import Table
 
 
 @dc.dataclass(kw_only=True)
@@ -34,7 +34,7 @@ def random_file(tmpdir):
     return file_name
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
 def test_reference(db, random_file):
     c = SpecialComponent("test", my_file=random_file)
 
@@ -48,7 +48,7 @@ def test_reference(db, random_file):
         assert f.read() == f2.read()
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
 def test_reference_lazy(db, random_file):
     c = SpecialComponent("test", my_file_lazy=random_file)
 
@@ -63,16 +63,17 @@ def test_reference_lazy(db, random_file):
         assert f.read() == f2.read()
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
 def test_file(db, random_file):
-    from pinnacledb import DataType
-
     dt = DataType("my-file", encodable="file")
-    db.apply(dt)
-    collection = MongoQuery("my-file", db=db)
-    db.execute(collection.insert_one(Document({"x": dt(random_file)})))
+    schema = Schema(identifier="schema", fields={"x": dt})
+    table_or_collection = Table("documents", schema=schema)
+    db.apply(table_or_collection)
 
-    data = db.execute(collection.find_one()).unpack()
+    collection = db['documents']
+    collection.insert([{"x": random_file}]).execute()
+
+    data = list(db.execute(collection.select()))[0].unpack()
 
     path = data["x"]
     assert os.path.exists(path)
@@ -82,16 +83,19 @@ def test_file(db, random_file):
         assert f.read() == f2.read()
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
 def test_file_lazy(db, random_file):
     from pinnacledb import DataType
 
     dt = DataType("my-file", encodable="lazy_file")
-    db.apply(dt)
-    collection = MongoQuery("my-file")
-    db.execute(collection.insert_one(Document({"x": dt(random_file)})))
+    schema = Schema(identifier="schema", fields={"x": dt})
+    table_or_collection = Table("documents", schema=schema)
+    db.apply(table_or_collection)
 
-    data = db.execute(collection.find_one())
+    collection = db['documents']
+    collection.insert([{"x": random_file}]).execute()
+
+    data = list(db.execute(collection.select()))[0]
 
     assert isinstance(data["x"].x, Empty)
     data = data.unpack()

test/unittest/component/datatype/test_pickle.py
@@ -7,9 +7,14 @@ import pytest
 
 from pinnacledb.components.component import Component
 from pinnacledb.components.datatype import (
+    DataType,
+    Empty,
+    pickle_encoder,
     pickle_lazy,
     pickle_serializer,
 )
+from pinnacledb.components.schema import Schema
+from pinnacledb.components.table import Table
 
 
 @dc.dataclass(kw_only=True)
@@ -31,7 +36,7 @@ def random_data():
     return df
 
 
-@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
 def test_reference(db, random_data):
     c = SpecialComponent("test", my_data=random_data)
 
@@ -41,3 +46,28 @@ def test_reference(db, random_data):
     reloaded.init()
     assert isinstance(reloaded.my_data, pd.DataFrame)
     assert c.my_data.equals(reloaded.my_data)
+
+
+@pytest.mark.parametrize(
+    "datatype",
+    [
+        pickle_encoder,
+        pickle_serializer,
+        pickle_lazy,
+    ],
+)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
+def test_data(db, datatype: DataType, random_data: pd.DataFrame):
+    schema = Schema(identifier="schema", fields={"x": datatype})
+    table_or_collection = Table("documents", schema=schema)
+    db.apply(table_or_collection)
+
+    collection = db["documents"]
+    collection.insert([{"x": random_data}]).execute()
+
+    loaded_data = list(db.execute(collection.select()))[0]
+    if datatype.encodable_cls.lazy:
+        assert isinstance(loaded_data["x"].x, Empty)
+        assert random_data.equals(loaded_data.unpack()["x"])
+    else:
+        assert random_data.equals(loaded_data.unpack()['x'])