superduper-io/superduper

Update references to components and artifacts.

superduperdb/backends/base/artifacts.py
@@ -107,18 +107,16 @@ class ArtifactStore(ABC):
                 self.put_bytes(blob, file_id=file_id)
             except FileExistsError:
                 continue
-            else:
-                # TODO: Check the following logic.
-                r['_blobs'][file_id] = f'&{file_id}'
 
         for file_id, file_path in files.items():
             try:
                 self.put_file(file_path, file_id=file_id)
             except FileExistsError:
                 continue
-            else:
-                # TODO: Check the following logic.
-                r['_files'][file_id] = f'&{file_id}'
+
+        # After we save the artifacts, we can remove the blobs and files
+        r['_files'] = {}
+        r['_blobs'] = {}
 
         return r
 
@@ -127,11 +125,23 @@ class ArtifactStore(ABC):
 
         :param r: dictionary with mandatory fields
         """
-        for blob in r['_blobs']:
-            self._delete_bytes(blob)
+        from superduperdb.misc.special_dicts import recursive_find
 
-        for file_path in r['_files']:
-            self._delete_bytes(file_path)
+        # find all blobs with `&:blob:` prefix,
+        blobs = recursive_find(
+            r, lambda v: isinstance(v, str) and v.startswith('&:blob:')
+        )
+
+        for blob in blobs:
+            self._delete_bytes(blob.split(':')[-1])
+
+        # find all files with `&:file:` prefix
+        files = recursive_find(
+            r, lambda v: isinstance(v, str) and v.startswith('&:file:')
+        )
+        for file_path in files:
+            # file: &:file:file_name/file_id
+            self._delete_bytes(file_path.split('/')[-1])
 
     @abstractmethod
     def get_bytes(self, file_id: str) -> bytes:

superduperdb/base/datalayer.py
@@ -32,6 +32,7 @@ from superduperdb.misc.annotations import deprecated
 from superduperdb.misc.colors import Colors
 from superduperdb.misc.data import ibatch
 from superduperdb.misc.download import download_content, download_from_one
+from superduperdb.misc.special_dicts import recursive_update
 from superduperdb.vector_search.base import BaseVectorSearcher, VectorItem
 from superduperdb.vector_search.interface import FastVectorSearcher
 from superduperdb.vector_search.update_tasks import copy_vectors, delete_vectors
@@ -891,10 +892,8 @@ class Datalayer:
 
         jobs.extend(self._add_child_components(children, parent=object))
 
-        for k in list(serialized['_leaves'].keys()):
-            if isinstance(serialized['_leaves'][k], Component):
-                serialized['_leaves'].pop(k)
-
+        if children:
+            serialized = self._change_component_reference_prefix(serialized)
         serialized = self.artifact_store.save_artifact(serialized)
 
         self.metadata.create_component(serialized)
@@ -907,6 +906,29 @@ class Datalayer:
         these_jobs = object.schedule_jobs(self, dependencies=dependencies)
         jobs.extend(these_jobs)
         return jobs
+
+    def _change_component_reference_prefix(self, serialized):
+        """Replace '?' to '&' in the serialized object."""
+        references = set()
+        for reference in list(serialized['_leaves'].keys()):
+            if isinstance(serialized['_leaves'][reference], Component):
+                serialized['_leaves'].pop(reference)
+                references.add(reference)
+
+        # Only replace component references
+        if not references:
+            return
+
+        def replace_function(value):
+            # Change value if it is a string and starts with '?'
+            # and the value is in references
+            # ?:xxx: -> &:xxx:
+            if isinstance(value, str) and value[1:] in references:
+                return f'&{value[1:]}'
+            return value
+
+        serialized = recursive_update(serialized, replace_function)
+        return serialized
 
     def _remove_component_version(
         self,
@@ -1005,9 +1027,8 @@ class Datalayer:
             if old_uuid:
                 self.metadata.delete_parent_child(old_uuid, child.uuid)
 
-        for k in list(serialized['_leaves'].keys()):
-            if isinstance(serialized['_leaves'][k], Component):
-                serialized['_leaves'].pop(k)
+        if children:
+            serialized = self._change_component_reference_prefix(serialized)
 
         self.artifact_store.delete_artifact(info)
 

test/unittest/component/datatype/test_file.py
@@ -1,31 +1,14 @@
-import dataclasses as dc
 import os
-import typing as t
 from test.db_config import DBConfig
+from test.unittest.component.datatype import utils
 
 import pytest
 
-from superduperdb import DataType, Schema
-from superduperdb.components.component import Component
-from superduperdb.components.datatype import (
-    Empty,
-    LazyFile,
-    file_lazy,
-    file_serializer,
-)
-from superduperdb.components.table import Table
-
-
-@dc.dataclass(kw_only=True)
-class SpecialComponent(Component):
-    type_id: t.ClassVar[str] = "special"
-    my_file: str | None = None
-    my_file_lazy: str | None = None
-    _artifacts: t.ClassVar = (("my_file", file_serializer), ("my_file_lazy", file_lazy))
+from superduperdb import DataType
 
 
 @pytest.fixture
-def random_file(tmpdir):
+def random_data(tmpdir):
     file_name = os.path.join(tmpdir, "HelloWorld.txt")
 
     with open(file_name, "w") as f:
@@ -34,75 +17,43 @@ def random_file(tmpdir):
     return file_name
 
 
+def dt_file():
+    return DataType("my-file", encodable="file")
+
+
+def dt_file_lazy():
+    return DataType("my-file", encodable="lazy_file")
+
+
+datatypes = [
+    dt_file(),
+    dt_file_lazy(),
+]
+
+
+@pytest.mark.parametrize("datatype", datatypes)
 @pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_reference(db, random_file):
-    c = SpecialComponent("test", my_file=random_file)
-
-    db.add(c)
-
-    reloaded = db.load("special", c.identifier)
-    reloaded.init()
-    assert isinstance(reloaded.my_file, str)
-    assert reloaded.my_file != random_file
-    with open(reloaded.my_file, "r") as f, open(random_file, "r") as f2:
-        assert f.read() == f2.read()
+def test_data_with_schema_and_db(datatype: DataType, random_data, db):
+    utils.check_data_with_schema_and_db(random_data, datatype, db)
 
 
+@pytest.mark.parametrize("datatype", datatypes)
+def test_data_without_schema(datatype: DataType, random_data):
+    utils.check_data_without_schema(random_data, datatype)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES[:1], indirect=True)
+def test_data_without_schema_and_db(datatype: DataType, random_data, db):
+    utils.check_data_without_schema_and_db(random_data, datatype, db)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
+def test_component(random_data, datatype):
+    utils.check_component(random_data, datatype)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
 @pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_reference_lazy(db, random_file):
-    c = SpecialComponent("test", my_file_lazy=random_file)
-
-    db.add(c)
-
-    reloaded = db.load("special", c.identifier)
-    assert isinstance(reloaded.my_file_lazy, LazyFile)
-    assert isinstance(reloaded.my_file_lazy.x, Empty)
-    reloaded.init()
-    assert isinstance(reloaded.my_file_lazy, str)
-    with open(reloaded.my_file_lazy, "r") as f, open(random_file, "r") as f2:
-        assert f.read() == f2.read()
-
-
-@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_file(db, random_file):
-    dt = DataType("my-file", encodable="file")
-    schema = Schema(identifier="schema", fields={"x": dt})
-    table_or_collection = Table("documents", schema=schema)
-    db.apply(table_or_collection)
-
-    collection = db['documents']
-    collection.insert([{"x": random_file}]).execute()
-
-    data = list(db.execute(collection.select()))[0].unpack()
-
-    path = data["x"]
-    assert os.path.exists(path)
-    # Check that the file is the same
-    assert path != random_file
-    with open(path, "r") as f, open(random_file, "r") as f2:
-        assert f.read() == f2.read()
-
-
-@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_file_lazy(db, random_file):
-    from superduperdb import DataType
-
-    dt = DataType("my-file", encodable="lazy_file")
-    schema = Schema(identifier="schema", fields={"x": dt})
-    table_or_collection = Table("documents", schema=schema)
-    db.apply(table_or_collection)
-
-    collection = db['documents']
-    collection.insert([{"x": random_file}]).execute()
-
-    data = list(db.execute(collection.select()))[0]
-
-    assert isinstance(data["x"].x, Empty)
-    data = data.unpack()
-    path = data["x"]
-    assert isinstance(path, str)
-    assert os.path.exists(path)
-    # Check that the file is the same
-    assert path != random_file
-    with open(path, "r") as f, open(random_file, "r") as f2:
-        assert f.read() == f2.read()
+def test_component_with_db(db, random_data, datatype):
+    utils.check_component_with_db(random_data, datatype, db)

test/unittest/component/datatype/test_pickle.py
@@ -1,20 +1,20 @@
 import dataclasses as dc
 import typing as t
 from test.db_config import DBConfig
+from test.unittest.component.datatype import utils
 
+import numpy as np
 import pandas as pd
 import pytest
 
+from superduperdb.base.enums import DBType
 from superduperdb.components.component import Component
 from superduperdb.components.datatype import (
     DataType,
-    Empty,
     pickle_encoder,
     pickle_lazy,
     pickle_serializer,
 )
-from superduperdb.components.schema import Schema
-from superduperdb.components.table import Table
 
 
 @dc.dataclass(kw_only=True)
@@ -30,44 +30,53 @@ class SpecialComponent(Component):
 
 @pytest.fixture
 def random_data():
-    df = pd.DataFrame(
-        [{"a": 1, "b": 2}, {"a": 3, "b": 4}, {"a": 5, "b": 6}, {"a": 7, "b": 8}]
-    )
-    return df
+    array = np.array([1])
+    return array
 
 
+def print_sep():
+    print("\n", "-" * 80, "\n")
+
+
+datatypes = [
+    pickle_encoder,
+    pickle_serializer,
+    pickle_lazy,
+]
+
+
+@pytest.mark.parametrize("datatype", datatypes)
+def test_data_with_schema(datatype: DataType, random_data: pd.DataFrame):
+    utils.check_data_with_schema(random_data, datatype)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
 @pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_reference(db, random_data):
-    c = SpecialComponent("test", my_data=random_data)
-
-    db.add(c)
-
-    reloaded = db.load("special", c.identifier)
-    reloaded.init()
-    assert isinstance(reloaded.my_data, pd.DataFrame)
-    assert c.my_data.equals(reloaded.my_data)
+def test_data_with_schema_and_db(datatype: DataType, random_data: pd.DataFrame, db):
+    utils.check_data_with_schema_and_db(random_data, datatype, db)
 
 
-@pytest.mark.parametrize(
-    "datatype",
-    [
-        pickle_encoder,
-        pickle_serializer,
-        pickle_lazy,
-    ],
-)
+@pytest.mark.parametrize("datatype", datatypes)
+def test_data_without_schema(datatype: DataType, random_data: pd.DataFrame):
+    utils.check_data_without_schema(random_data, datatype)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES[:1], indirect=True)
+def test_data_without_schema_and_db(datatype: DataType, random_data: pd.DataFrame, db):
+    utils.check_data_without_schema_and_db(random_data, datatype, db)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
+def test_component(random_data, datatype):
+    utils.check_component(random_data, datatype)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
 @pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_data(db, datatype: DataType, random_data: pd.DataFrame):
-    schema = Schema(identifier="schema", fields={"x": datatype})
-    table_or_collection = Table("documents", schema=schema)
-    db.apply(table_or_collection)
-
-    collection = db["documents"]
-    collection.insert([{"x": random_data}]).execute()
-
-    loaded_data = list(db.execute(collection.select()))[0]
-    if datatype.encodable_cls.lazy:
-        assert isinstance(loaded_data["x"].x, Empty)
-        assert random_data.equals(loaded_data.unpack()["x"])
-    else:
-        assert random_data.equals(loaded_data.unpack()['x'])
+def test_component_with_db(db, random_data, datatype):
+    # TODO: Need to fix the encodable in component when db is SQL
+    # Some bytes are not serializable, then can't be stored in SQL
+    if datatype.encodable == 'encodable' and db.databackend.db_type == DBType.SQL:
+        return
+    utils.check_component_with_db(random_data, datatype, db)

Pinnacle-Networks/Pinnacle

superduperdb/backends/base/artifacts.py
@@ -107,18 +107,16 @@ class ArtifactStore(ABC):
                 self.put_bytes(blob, file_id=file_id)
             except FileExistsError:
                 continue
-            else:
-                # TODO: Check the following logic.
-                r['_blobs'][file_id] = f'&{file_id}'
 
         for file_id, file_path in files.items():
             try:
                 self.put_file(file_path, file_id=file_id)
             except FileExistsError:
                 continue
-            else:
-                # TODO: Check the following logic.
-                r['_files'][file_id] = f'&{file_id}'
+
+        # After we save the artifacts, we can remove the blobs and files
+        r['_files'] = {}
+        r['_blobs'] = {}
 
         return r
 
@@ -127,11 +125,23 @@ class ArtifactStore(ABC):
 
         :param r: dictionary with mandatory fields
         """
-        for blob in r['_blobs']:
-            self._delete_bytes(blob)
+        from pinnacledb.misc.special_dicts import recursive_find
 
-        for file_path in r['_files']:
-            self._delete_bytes(file_path)
+        # find all blobs with `&:blob:` prefix,
+        blobs = recursive_find(
+            r, lambda v: isinstance(v, str) and v.startswith('&:blob:')
+        )
+
+        for blob in blobs:
+            self._delete_bytes(blob.split(':')[-1])
+
+        # find all files with `&:file:` prefix
+        files = recursive_find(
+            r, lambda v: isinstance(v, str) and v.startswith('&:file:')
+        )
+        for file_path in files:
+            # file: &:file:file_name/file_id
+            self._delete_bytes(file_path.split('/')[-1])
 
     @abstractmethod
     def get_bytes(self, file_id: str) -> bytes:

superduperdb/base/datalayer.py
@@ -32,6 +32,7 @@ from pinnacledb.misc.annotations import deprecated
 from pinnacledb.misc.colors import Colors
 from pinnacledb.misc.data import ibatch
 from pinnacledb.misc.download import download_content, download_from_one
+from pinnacledb.misc.special_dicts import recursive_update
 from pinnacledb.vector_search.base import BaseVectorSearcher, VectorItem
 from pinnacledb.vector_search.interface import FastVectorSearcher
 from pinnacledb.vector_search.update_tasks import copy_vectors, delete_vectors
@@ -891,10 +892,8 @@ class Datalayer:
 
         jobs.extend(self._add_child_components(children, parent=object))
 
-        for k in list(serialized['_leaves'].keys()):
-            if isinstance(serialized['_leaves'][k], Component):
-                serialized['_leaves'].pop(k)
-
+        if children:
+            serialized = self._change_component_reference_prefix(serialized)
         serialized = self.artifact_store.save_artifact(serialized)
 
         self.metadata.create_component(serialized)
@@ -907,6 +906,29 @@ class Datalayer:
         these_jobs = object.schedule_jobs(self, dependencies=dependencies)
         jobs.extend(these_jobs)
         return jobs
+
+    def _change_component_reference_prefix(self, serialized):
+        """Replace '?' to '&' in the serialized object."""
+        references = set()
+        for reference in list(serialized['_leaves'].keys()):
+            if isinstance(serialized['_leaves'][reference], Component):
+                serialized['_leaves'].pop(reference)
+                references.add(reference)
+
+        # Only replace component references
+        if not references:
+            return
+
+        def replace_function(value):
+            # Change value if it is a string and starts with '?'
+            # and the value is in references
+            # ?:xxx: -> &:xxx:
+            if isinstance(value, str) and value[1:] in references:
+                return f'&{value[1:]}'
+            return value
+
+        serialized = recursive_update(serialized, replace_function)
+        return serialized
 
     def _remove_component_version(
         self,
@@ -1005,9 +1027,8 @@ class Datalayer:
             if old_uuid:
                 self.metadata.delete_parent_child(old_uuid, child.uuid)
 
-        for k in list(serialized['_leaves'].keys()):
-            if isinstance(serialized['_leaves'][k], Component):
-                serialized['_leaves'].pop(k)
+        if children:
+            serialized = self._change_component_reference_prefix(serialized)
 
         self.artifact_store.delete_artifact(info)
 

test/unittest/component/datatype/test_file.py
@@ -1,31 +1,14 @@
-import dataclasses as dc
 import os
-import typing as t
 from test.db_config import DBConfig
+from test.unittest.component.datatype import utils
 
 import pytest
 
-from pinnacledb import DataType, Schema
-from pinnacledb.components.component import Component
-from pinnacledb.components.datatype import (
-    Empty,
-    LazyFile,
-    file_lazy,
-    file_serializer,
-)
-from pinnacledb.components.table import Table
-
-
-@dc.dataclass(kw_only=True)
-class SpecialComponent(Component):
-    type_id: t.ClassVar[str] = "special"
-    my_file: str | None = None
-    my_file_lazy: str | None = None
-    _artifacts: t.ClassVar = (("my_file", file_serializer), ("my_file_lazy", file_lazy))
+from pinnacledb import DataType
 
 
 @pytest.fixture
-def random_file(tmpdir):
+def random_data(tmpdir):
     file_name = os.path.join(tmpdir, "HelloWorld.txt")
 
     with open(file_name, "w") as f:
@@ -34,75 +17,43 @@ def random_file(tmpdir):
     return file_name
 
 
+def dt_file():
+    return DataType("my-file", encodable="file")
+
+
+def dt_file_lazy():
+    return DataType("my-file", encodable="lazy_file")
+
+
+datatypes = [
+    dt_file(),
+    dt_file_lazy(),
+]
+
+
+@pytest.mark.parametrize("datatype", datatypes)
 @pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_reference(db, random_file):
-    c = SpecialComponent("test", my_file=random_file)
-
-    db.add(c)
-
-    reloaded = db.load("special", c.identifier)
-    reloaded.init()
-    assert isinstance(reloaded.my_file, str)
-    assert reloaded.my_file != random_file
-    with open(reloaded.my_file, "r") as f, open(random_file, "r") as f2:
-        assert f.read() == f2.read()
+def test_data_with_schema_and_db(datatype: DataType, random_data, db):
+    utils.check_data_with_schema_and_db(random_data, datatype, db)
 
 
+@pytest.mark.parametrize("datatype", datatypes)
+def test_data_without_schema(datatype: DataType, random_data):
+    utils.check_data_without_schema(random_data, datatype)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES[:1], indirect=True)
+def test_data_without_schema_and_db(datatype: DataType, random_data, db):
+    utils.check_data_without_schema_and_db(random_data, datatype, db)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
+def test_component(random_data, datatype):
+    utils.check_component(random_data, datatype)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
 @pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_reference_lazy(db, random_file):
-    c = SpecialComponent("test", my_file_lazy=random_file)
-
-    db.add(c)
-
-    reloaded = db.load("special", c.identifier)
-    assert isinstance(reloaded.my_file_lazy, LazyFile)
-    assert isinstance(reloaded.my_file_lazy.x, Empty)
-    reloaded.init()
-    assert isinstance(reloaded.my_file_lazy, str)
-    with open(reloaded.my_file_lazy, "r") as f, open(random_file, "r") as f2:
-        assert f.read() == f2.read()
-
-
-@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_file(db, random_file):
-    dt = DataType("my-file", encodable="file")
-    schema = Schema(identifier="schema", fields={"x": dt})
-    table_or_collection = Table("documents", schema=schema)
-    db.apply(table_or_collection)
-
-    collection = db['documents']
-    collection.insert([{"x": random_file}]).execute()
-
-    data = list(db.execute(collection.select()))[0].unpack()
-
-    path = data["x"]
-    assert os.path.exists(path)
-    # Check that the file is the same
-    assert path != random_file
-    with open(path, "r") as f, open(random_file, "r") as f2:
-        assert f.read() == f2.read()
-
-
-@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_file_lazy(db, random_file):
-    from pinnacledb import DataType
-
-    dt = DataType("my-file", encodable="lazy_file")
-    schema = Schema(identifier="schema", fields={"x": dt})
-    table_or_collection = Table("documents", schema=schema)
-    db.apply(table_or_collection)
-
-    collection = db['documents']
-    collection.insert([{"x": random_file}]).execute()
-
-    data = list(db.execute(collection.select()))[0]
-
-    assert isinstance(data["x"].x, Empty)
-    data = data.unpack()
-    path = data["x"]
-    assert isinstance(path, str)
-    assert os.path.exists(path)
-    # Check that the file is the same
-    assert path != random_file
-    with open(path, "r") as f, open(random_file, "r") as f2:
-        assert f.read() == f2.read()
+def test_component_with_db(db, random_data, datatype):
+    utils.check_component_with_db(random_data, datatype, db)

test/unittest/component/datatype/test_pickle.py
@@ -1,20 +1,20 @@
 import dataclasses as dc
 import typing as t
 from test.db_config import DBConfig
+from test.unittest.component.datatype import utils
 
+import numpy as np
 import pandas as pd
 import pytest
 
+from pinnacledb.base.enums import DBType
 from pinnacledb.components.component import Component
 from pinnacledb.components.datatype import (
     DataType,
-    Empty,
     pickle_encoder,
     pickle_lazy,
     pickle_serializer,
 )
-from pinnacledb.components.schema import Schema
-from pinnacledb.components.table import Table
 
 
 @dc.dataclass(kw_only=True)
@@ -30,44 +30,53 @@ class SpecialComponent(Component):
 
 @pytest.fixture
 def random_data():
-    df = pd.DataFrame(
-        [{"a": 1, "b": 2}, {"a": 3, "b": 4}, {"a": 5, "b": 6}, {"a": 7, "b": 8}]
-    )
-    return df
+    array = np.array([1])
+    return array
 
 
+def print_sep():
+    print("\n", "-" * 80, "\n")
+
+
+datatypes = [
+    pickle_encoder,
+    pickle_serializer,
+    pickle_lazy,
+]
+
+
+@pytest.mark.parametrize("datatype", datatypes)
+def test_data_with_schema(datatype: DataType, random_data: pd.DataFrame):
+    utils.check_data_with_schema(random_data, datatype)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
 @pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_reference(db, random_data):
-    c = SpecialComponent("test", my_data=random_data)
-
-    db.add(c)
-
-    reloaded = db.load("special", c.identifier)
-    reloaded.init()
-    assert isinstance(reloaded.my_data, pd.DataFrame)
-    assert c.my_data.equals(reloaded.my_data)
+def test_data_with_schema_and_db(datatype: DataType, random_data: pd.DataFrame, db):
+    utils.check_data_with_schema_and_db(random_data, datatype, db)
 
 
-@pytest.mark.parametrize(
-    "datatype",
-    [
-        pickle_encoder,
-        pickle_serializer,
-        pickle_lazy,
-    ],
-)
+@pytest.mark.parametrize("datatype", datatypes)
+def test_data_without_schema(datatype: DataType, random_data: pd.DataFrame):
+    utils.check_data_without_schema(random_data, datatype)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES[:1], indirect=True)
+def test_data_without_schema_and_db(datatype: DataType, random_data: pd.DataFrame, db):
+    utils.check_data_without_schema_and_db(random_data, datatype, db)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
+def test_component(random_data, datatype):
+    utils.check_component(random_data, datatype)
+
+
+@pytest.mark.parametrize("datatype", datatypes)
 @pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
-def test_data(db, datatype: DataType, random_data: pd.DataFrame):
-    schema = Schema(identifier="schema", fields={"x": datatype})
-    table_or_collection = Table("documents", schema=schema)
-    db.apply(table_or_collection)
-
-    collection = db["documents"]
-    collection.insert([{"x": random_data}]).execute()
-
-    loaded_data = list(db.execute(collection.select()))[0]
-    if datatype.encodable_cls.lazy:
-        assert isinstance(loaded_data["x"].x, Empty)
-        assert random_data.equals(loaded_data.unpack()["x"])
-    else:
-        assert random_data.equals(loaded_data.unpack()['x'])
+def test_component_with_db(db, random_data, datatype):
+    # TODO: Need to fix the encodable in component when db is SQL
+    # Some bytes are not serializable, then can't be stored in SQL
+    if datatype.encodable == 'encodable' and db.databackend.db_type == DBType.SQL:
+        return
+    utils.check_component_with_db(random_data, datatype, db)