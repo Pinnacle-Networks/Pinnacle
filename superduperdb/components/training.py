superduper-io/superduper

Optimized the import of dependencies for Large Language Models.

superduperdb/components/training.py
@@ -1,0 +1,21 @@
+import typing as t
+
+from superduperdb.components.component import Component
+from superduperdb.components.datatype import DataType, file_lazy
+
+
+class Checkpoint(Component):
+    """Checkpoint component for saving the model checkpoint.
+
+    :param path: The path to the checkpoint.
+    :param step: The step of the checkpoint.
+    """
+
+    path: t.Optional[str]
+    step: int
+    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, DataType]]] = (("path", file_lazy),)
+    type_id: t.ClassVar[str] = "checkpoint"
+
+    def __post_init__(self, db, artifacts):
+        super().__post_init__(db, artifacts)
+        self.version = int(self.step)

superduperdb/ext/transformers/model.py
@@ -32,8 +32,8 @@ from superduperdb.components.model import (
     _DeviceManaged,
     _Fittable,
 )
+from superduperdb.components.training import Checkpoint
 from superduperdb.ext.llm.model import BaseLLM
-from superduperdb.ext.transformers.training import Checkpoint
 from superduperdb.jobs.job import Job
 
 

superduperdb/ext/transformers/training.py
@@ -1,6 +1,5 @@
 import dataclasses as dc
 import os
-import re
 import typing as t
 from copy import deepcopy
 from functools import wraps
@@ -29,55 +28,13 @@ from superduperdb.backends.query_dataset import QueryDataset
 from superduperdb.base.build import build_datalayer
 from superduperdb.base.config import Config
 from superduperdb.base.datalayer import Datalayer
-from superduperdb.components.component import Component
 from superduperdb.components.dataset import Dataset
-from superduperdb.components.datatype import DataType, file_lazy
 from superduperdb.components.model import Trainer as SuperDuperTrainer
+from superduperdb.components.training import Checkpoint
 from superduperdb.misc.hash import random_sha1
 
 if t.TYPE_CHECKING:
     from superduperdb.ext.transformers.model import LLM
-
-
-class Checkpoint(Component):
-    """Checkpoint component for saving the model checkpoint.
-
-    :param path: The path to the checkpoint.
-    :param step: The step of the checkpoint.
-    """
-
-    path: t.Optional[str]
-    step: int
-    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, DataType]]] = (("path", file_lazy),)
-    type_id: t.ClassVar[str] = "checkpoint"
-
-    def __post_init__(self, db, artifacts):
-        super().__post_init__(db, artifacts)
-        self.version = int(self.step)
-
-    @property
-    def uri(self):
-        """Get the uri of the checkpoint."""
-        return f"checkpoint://{self.identifier}/{self.step}"
-
-    @staticmethod
-    def check_uri(uri):
-        """Check if the uri is a valid checkpoint uri.
-
-        :param uri: The uri to check.
-        """
-        return re.match(r"^checkpoint://.*?/\d+$", uri) is not None
-
-    @staticmethod
-    def parse_uri(uri):
-        """Parse the uri to get the identifier and step.
-
-        :param uri: The uri to parse.
-        """
-        if not Checkpoint.check_uri(uri):
-            raise ValueError(f"Invalid uri: {uri}")
-        *_, identifier, step = uri.split("/")
-        return identifier, int(step)
 
 
 class LLMCallback(TrainerCallback):

test/integration/ext/transformers/test_llm_training.py
@@ -7,13 +7,14 @@ from superduperdb import superduper
 from superduperdb.backends.mongodb import MongoQuery
 from superduperdb.base.document import Document
 from superduperdb.ext.transformers.model import LLM
-from superduperdb.ext.transformers.training import LLMTrainer
 
 try:
     import datasets
     import peft
     import torch
     import trl
+
+    from superduperdb.ext.transformers.training import LLMTrainer
 
     GPU_AVAILABLE = torch.cuda.is_available()
 except ImportError:

test/unittest/ext/transformers/test_llm_training.py
@@ -9,13 +9,14 @@ from superduperdb.base.document import Document
 from superduperdb.components.dataset import Dataset
 from superduperdb.components.metric import Metric
 from superduperdb.ext.transformers import LLM
-from superduperdb.ext.transformers.training import LLMTrainer
 
 TEST_MODEL_NAME = "facebook/opt-125m"
 try:
     import datasets
     import peft
     import trl
+
+    from superduperdb.ext.transformers.training import LLMTrainer
 except ImportError:
     datasets = None
     peft = None

Pinnacle-Networks/Pinnacle

superduperdb/components/training.py
@@ -1,0 +1,21 @@
+import typing as t
+
+from pinnacledb.components.component import Component
+from pinnacledb.components.datatype import DataType, file_lazy
+
+
+class Checkpoint(Component):
+    """Checkpoint component for saving the model checkpoint.
+
+    :param path: The path to the checkpoint.
+    :param step: The step of the checkpoint.
+    """
+
+    path: t.Optional[str]
+    step: int
+    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, DataType]]] = (("path", file_lazy),)
+    type_id: t.ClassVar[str] = "checkpoint"
+
+    def __post_init__(self, db, artifacts):
+        super().__post_init__(db, artifacts)
+        self.version = int(self.step)

superduperdb/ext/transformers/model.py
@@ -32,8 +32,8 @@ from pinnacledb.components.model import (
     _DeviceManaged,
     _Fittable,
 )
+from pinnacledb.components.training import Checkpoint
 from pinnacledb.ext.llm.model import BaseLLM
-from pinnacledb.ext.transformers.training import Checkpoint
 from pinnacledb.jobs.job import Job
 
 

superduperdb/ext/transformers/training.py
@@ -1,6 +1,5 @@
 import dataclasses as dc
 import os
-import re
 import typing as t
 from copy import deepcopy
 from functools import wraps
@@ -29,55 +28,13 @@ from pinnacledb.backends.query_dataset import QueryDataset
 from pinnacledb.base.build import build_datalayer
 from pinnacledb.base.config import Config
 from pinnacledb.base.datalayer import Datalayer
-from pinnacledb.components.component import Component
 from pinnacledb.components.dataset import Dataset
-from pinnacledb.components.datatype import DataType, file_lazy
 from pinnacledb.components.model import Trainer as SuperDuperTrainer
+from pinnacledb.components.training import Checkpoint
 from pinnacledb.misc.hash import random_sha1
 
 if t.TYPE_CHECKING:
     from pinnacledb.ext.transformers.model import LLM
-
-
-class Checkpoint(Component):
-    """Checkpoint component for saving the model checkpoint.
-
-    :param path: The path to the checkpoint.
-    :param step: The step of the checkpoint.
-    """
-
-    path: t.Optional[str]
-    step: int
-    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, DataType]]] = (("path", file_lazy),)
-    type_id: t.ClassVar[str] = "checkpoint"
-
-    def __post_init__(self, db, artifacts):
-        super().__post_init__(db, artifacts)
-        self.version = int(self.step)
-
-    @property
-    def uri(self):
-        """Get the uri of the checkpoint."""
-        return f"checkpoint://{self.identifier}/{self.step}"
-
-    @staticmethod
-    def check_uri(uri):
-        """Check if the uri is a valid checkpoint uri.
-
-        :param uri: The uri to check.
-        """
-        return re.match(r"^checkpoint://.*?/\d+$", uri) is not None
-
-    @staticmethod
-    def parse_uri(uri):
-        """Parse the uri to get the identifier and step.
-
-        :param uri: The uri to parse.
-        """
-        if not Checkpoint.check_uri(uri):
-            raise ValueError(f"Invalid uri: {uri}")
-        *_, identifier, step = uri.split("/")
-        return identifier, int(step)
 
 
 class LLMCallback(TrainerCallback):

test/integration/ext/transformers/test_llm_training.py
@@ -7,13 +7,14 @@ from pinnacledb import pinnacle
 from pinnacledb.backends.mongodb import MongoQuery
 from pinnacledb.base.document import Document
 from pinnacledb.ext.transformers.model import LLM
-from pinnacledb.ext.transformers.training import LLMTrainer
 
 try:
     import datasets
     import peft
     import torch
     import trl
+
+    from pinnacledb.ext.transformers.training import LLMTrainer
 
     GPU_AVAILABLE = torch.cuda.is_available()
 except ImportError:

test/unittest/ext/transformers/test_llm_training.py
@@ -9,13 +9,14 @@ from pinnacledb.base.document import Document
 from pinnacledb.components.dataset import Dataset
 from pinnacledb.components.metric import Metric
 from pinnacledb.ext.transformers import LLM
-from pinnacledb.ext.transformers.training import LLMTrainer
 
 TEST_MODEL_NAME = "facebook/opt-125m"
 try:
     import datasets
     import peft
     import trl
+
+    from pinnacledb.ext.transformers.training import LLMTrainer
 except ImportError:
     datasets = None
     peft = None