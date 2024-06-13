superduper-io/superduper

Update CLI apply command

superduperdb/__main__.py
@@ -2,7 +2,7 @@ import sys
 
 import click
 
-from superduperdb.cli import app, config, info
+from superduperdb.cli import app, info, apply
 from superduperdb.cli.serve import cdc, local_cluster, ray_serve, vector_search
 
 __all__ = 'config', 'info', 'local_cluster', 'vector_search', 'cdc', 'ray_serve'

superduperdb/cli/apply.py
@@ -1,0 +1,37 @@
+from superduperdb import Component, logging, superduper
+
+from . import command
+
+
+@command(help='Apply the component to a `superduperdb` deployment')
+def apply(path: str):
+    """Apply a serialized component.
+
+    :param path: Path to the stack.
+    """
+    _apply(path)
+
+
+@command(help='`superduperdb` deployment')
+def drop(data: bool = False):
+    """Apply a serialized component.
+
+    :param path: Path to the stack.
+    """
+    db = superduper()
+    db.drop(data=data)
+    db.disconnect()
+
+
+def _apply(path: str):
+    try:
+        logging.info('Connecting to superduperdb')
+        db = superduper()
+        logging.info('Reading component')
+        component = Component.read(path)
+        logging.info('Applying component to superduperdb')
+        db.apply(component)
+    except Exception as e:
+        raise e
+    finally:
+        db.disconnect()

Pinnacle-Networks/Pinnacle

superduperdb/__main__.py
@@ -2,7 +2,7 @@ import sys
 
 import click
 
-from pinnacledb.cli import app, config, info
+from pinnacledb.cli import app, info, apply
 from pinnacledb.cli.serve import cdc, local_cluster, ray_serve, vector_search
 
 __all__ = 'config', 'info', 'local_cluster', 'vector_search', 'cdc', 'ray_serve'

superduperdb/cli/apply.py
@@ -1,0 +1,37 @@
+from pinnacledb import Component, logging, pinnacle
+
+from . import command
+
+
+@command(help='Apply the component to a `pinnacledb` deployment')
+def apply(path: str):
+    """Apply a serialized component.
+
+    :param path: Path to the stack.
+    """
+    _apply(path)
+
+
+@command(help='`pinnacledb` deployment')
+def drop(data: bool = False):
+    """Apply a serialized component.
+
+    :param path: Path to the stack.
+    """
+    db = pinnacle()
+    db.drop(data=data)
+    db.disconnect()
+
+
+def _apply(path: str):
+    try:
+        logging.info('Connecting to pinnacledb')
+        db = pinnacle()
+        logging.info('Reading component')
+        component = Component.read(path)
+        logging.info('Applying component to pinnacledb')
+        db.apply(component)
+    except Exception as e:
+        raise e
+    finally:
+        db.disconnect()