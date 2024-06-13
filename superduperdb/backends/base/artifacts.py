superduper-io/superduper

Standardize the references to '_builds', '_blobs', '_files'.

superduperdb/backends/base/artifacts.py
@@ -3,6 +3,8 @@ import os
 import typing as t
 from abc import ABC, abstractmethod
 
+from superduperdb.base.constant import KEY_BLOBS, KEY_FILES
+
 
 def _construct_file_id_from_uri(uri):
     return str(hashlib.sha1(uri.encode()).hexdigest())
@@ -99,8 +101,8 @@ class ArtifactStore(ABC):
 
         :param r: dictionary with mandatory fields
         """
-        blobs = r.get('_blobs', {})
-        files = r.get('_files', {})
+        blobs = r.get(KEY_BLOBS, {})
+        files = r.get(KEY_FILES, {})
 
         for file_id, blob in blobs.items():
             try:
@@ -115,8 +117,8 @@ class ArtifactStore(ABC):
                 continue
 
         # After we save the artifacts, we can remove the blobs and files
-        r['_files'] = {}
-        r['_blobs'] = {}
+        r[KEY_FILES] = {}
+        r[KEY_BLOBS] = {}
 
         return r
 

superduperdb/backends/ibis/query.py
@@ -12,6 +12,7 @@ from superduperdb.backends.base.query import (
     applies_to,
     parse_query as _parse_query,
 )
+from superduperdb.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
 from superduperdb.base.cursor import SuperDuperCursor
 from superduperdb.base.exceptions import DatabackendException
 from superduperdb.components.datatype import Encodable
@@ -265,9 +266,9 @@ class IbisQuery(Query):
     def _execute_insert(self, parent):
         documents = self._prepare_documents()
         for r in documents:
-            r.pop('_leaves')
-            r.pop('_blobs')
-            r.pop('_files')
+            r.pop(KEY_BUILDS)
+            r.pop(KEY_BLOBS)
+            r.pop(KEY_FILES)
             r.pop('_schema')
             if self.primary_id not in r:
                 pid = str(uuid.uuid4())

superduperdb/base/datalayer.py
@@ -19,6 +19,7 @@ from superduperdb.backends.base.query import Query
 from superduperdb.backends.local.compute import LocalComputeBackend
 from superduperdb.base import exceptions
 from superduperdb.base.config import Config
+from superduperdb.base.constant import KEY_BUILDS
 from superduperdb.base.cursor import SuperDuperCursor
 from superduperdb.base.document import Document
 from superduperdb.base.superduper import superduper
@@ -894,14 +895,14 @@ class Datalayer:
 
         serialized = object.dict().encode(leaves_to_keep=(Component,))
 
-        for k, v in serialized['_leaves'].items():
+        for k, v in serialized[KEY_BUILDS].items():
             if isinstance(v, Component) and hasattr(v, 'inline') and v.inline:
                 r = dict(v.dict())
                 del r['identifier']
-                serialized['_leaves'][k] = r
+                serialized[KEY_BUILDS][k] = r
 
         children = [
-            v for v in serialized['_leaves'].values() if isinstance(v, Component)
+            v for v in serialized[KEY_BUILDS].values() if isinstance(v, Component)
         ]
 
         jobs.extend(self._add_child_components(children, parent=object))
@@ -925,10 +926,10 @@ class Datalayer:
     def _change_component_reference_prefix(self, serialized):
         """Replace '?' to '&' in the serialized object."""
         references = {}
-        for reference in list(serialized['_leaves'].keys()):
-            if isinstance(serialized['_leaves'][reference], Component):
-                comp = serialized['_leaves'][reference]
-                serialized['_leaves'].pop(reference)
+        for reference in list(serialized[KEY_BUILDS].keys()):
+            if isinstance(serialized[KEY_BUILDS][reference], Component):
+                comp = serialized[KEY_BUILDS][reference]
+                serialized[KEY_BUILDS].pop(reference)
                 references[reference] = (
                     comp.type_id + ':' + comp.identifier + ':' + comp.uuid
                 )
@@ -1039,14 +1040,14 @@ class Datalayer:
         object.version = info['version']
 
         serialized = object.dict().encode(leaves_to_keep=(Component,))
-        for k, v in serialized['_leaves'].items():
+        for k, v in serialized[KEY_BUILDS].items():
             if isinstance(v, Component) and hasattr(v, 'inline') and v.inline:
                 r = dict(v.dict())
                 del r['identifier']
-                serialized['_leaves'][k] = r
+                serialized[KEY_BUILDS][k] = r
 
         children = [
-            v for v in serialized['_leaves'].values() if isinstance(v, Component)
+            v for v in serialized[KEY_BUILDS].values() if isinstance(v, Component)
         ]
 
         for child in children:

superduperdb/base/leaf.py
@@ -4,6 +4,7 @@ import inspect
 import typing as t
 import uuid
 
+from superduperdb.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
 from superduperdb.misc.annotations import extract_parameters, replace_parameters
 from superduperdb.misc.serialization import asdict
 from superduperdb.misc.special_dicts import SuperDuperFlatEncode
@@ -161,9 +162,9 @@ class Leaf(metaclass=LeafMeta):
         return SuperDuperFlatEncode(
             {
                 '_base': f'?{self.identifier}',
-                '_leaves': builds,
-                '_blobs': blobs,
-                '_files': files,
+                KEY_BUILDS: builds,
+                KEY_BLOBS: blobs,
+                KEY_FILES: files,
             }
         )
 

superduperdb/misc/special_dicts.py
@@ -10,6 +10,8 @@ from rich.panel import Panel
 from rich.text import Text
 from rich.tree import Tree
 
+from superduperdb.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
+
 
 class IndexableDict(OrderedDict):
     """IndexableDict.
@@ -97,8 +99,8 @@ def _print_serialized_object(serialized_object):
 
     # Extract sections of the serialized object
     base_section = serialized_object.get('_base', {})
-    leaves_section = serialized_object.get('_leaves', {})
-    blobs_section = serialized_object.get('_blobs', {})
+    leaves_section = serialized_object.get(KEY_BUILDS, {})
+    blobs_section = serialized_object.get(KEY_BLOBS, {})
 
     # Format base section with additional component info
     base_yaml = _format_base_section(base_section)
@@ -144,38 +146,38 @@ class SuperDuperFlatEncode(t.Dict[str, t.Any]):
     """
 
     @property
-    def leaves(self):
-        """Return the leaves of the dictionary."""
-        return IndexableDict(self.get('_leaves', {}))
+    def builds(self):
+        """Return the builds of the dictionary."""
+        return IndexableDict(self.get(KEY_BUILDS, {}))
 
     @property
     def files(self):
         """Return the files of the dictionary."""
-        return self.get('_files', [])
+        return self.get(KEY_FILES, [])
 
     @property
     def blobs(self):
         """Return the blobs of the dictionary."""
-        return self.get('_blobs', [])
+        return self.get(KEY_BLOBS, [])
 
-    def pop_leaves(self):
-        """Pop the leaves of the dictionary."""
-        return IndexableDict(self.pop('_leaves', {}))
+    def pop_builds(self):
+        """Pop the builds of the dictionary."""
+        return IndexableDict(self.pop(KEY_BUILDS, {}))
 
     def pop_files(self):
         """Pop the files of the dictionary."""
-        return self.pop('_files', {})
+        return self.pop(KEY_FILES, {})
 
     def pop_blobs(self):
         """Pop the blobs of the dictionary."""
-        return self.pop('_blobs', {})
+        return self.pop(KEY_BLOBS, {})
 
     def load_keys_with_blob(self):
         """Load all outer reference keys with actual data blob."""
 
         def _get_blob(output, key):
             if isinstance(key, str) and key[0] == '?':
-                output = output['_leaves'][key[1:]]['blob']
+                output = output[KEY_BUILDS][key[1:]]['blob']
             else:
                 output = key
             return output
@@ -197,8 +199,8 @@ class SuperDuperFlatEncode(t.Dict[str, t.Any]):
         assert isinstance(d, SuperDuperFlatEncode)
         if '_base' in d:
             assert '_base' in self, "Cannot merge differently encoded data"
-        leaves = copy.deepcopy(self.leaves)
-        leaves.update(d.leaves)
+        builds = copy.deepcopy(self.builds)
+        builds.update(d.builds)
 
         blobs = copy.deepcopy(self.blobs)
         blobs = blobs.append(d.blobs)
@@ -207,17 +209,17 @@ class SuperDuperFlatEncode(t.Dict[str, t.Any]):
         files = files.append(d.files)
 
         if inplace:
-            if leaves:
-                self['_leaves'] = leaves
-            self['_blobs'] = blobs
-            self['_files'] = files
+            if builds:
+                self[KEY_BUILDS] = builds
+            self[KEY_BLOBS] = blobs
+            self[KEY_FILES] = files
             return self
         else:
             out = copy.deepcopy(self)
-            if leaves:
-                out['_leaves'] = leaves
-            out['_blobs'] = blobs
-            out['_files'] = files
+            if builds:
+                out[KEY_BUILDS] = builds
+            out[KEY_BLOBS] = blobs
+            out[KEY_FILES] = files
             return out
 
     def info(self):
@@ -321,9 +323,9 @@ def _diff_impl(r1, r2):
 
 
 def _childrens(tree, object, nesting=1):
-    if not object.leaves or not nesting:
+    if not object.builds or not nesting:
         return
-    for name, child in object.leaves.items():
+    for name, child in object.builds.items():
         child_text = f"{name}: {child.__class__}({child.identifier}): {str(child)[:50]}"
         subtree = tree.add(Text(child_text, style="yellow"))
         for key, value in child.__dict__.items():
@@ -345,9 +347,9 @@ def _component_metadata(obj):
 
     def _all_leaves(obj):
         result = []
-        if not obj.leaves:
+        if not obj.builds:
             return result
-        for name, leaf in obj.leaves.items():
+        for name, leaf in obj.builds.items():
             tmp = _all_leaves(leaf)
             result.extend([f'{name}.{x}' for x in tmp])
             result.append(name)
@@ -355,7 +357,7 @@ def _component_metadata(obj):
 
     metadata.append("[yellow]Leaves[/yellow]")
     rleaves = _all_leaves(obj)
-    obj_leaves = list(obj.leaves.keys())
+    obj_leaves = list(obj.builds.keys())
     for leaf in rleaves:
         if leaf in obj_leaves:
             metadata.append(f"[green]{leaf}[/green]")

test/unittest/base/test_document.py
@@ -6,6 +6,7 @@ from test.db_config import DBConfig
 import pytest
 
 from superduperdb.backends.mongodb.query import MongoQuery
+from superduperdb.base.constant import KEY_BLOBS, KEY_BUILDS
 from superduperdb.components.datatype import Artifact, DataType
 from superduperdb.components.model import ObjectModel
 from superduperdb.components.schema import Schema
@@ -79,14 +80,14 @@ def test_encode_decode_flattened_document():
 
     import yaml
 
-    print(yaml.dump({k: v for k, v in encoded_r.items() if k != '_blobs'}))
+    print(yaml.dump({k: v for k, v in encoded_r.items() if k != KEY_BLOBS}))
 
     assert not isinstance(encoded_r, Document)
     assert isinstance(encoded_r, dict)
-    assert '_leaves' in encoded_r
-    assert '_blobs' in encoded_r
+    assert KEY_BUILDS in encoded_r
+    assert KEY_BLOBS in encoded_r
     assert encoded_r['img'].startswith('&:blob:')
-    assert isinstance(next(iter(encoded_r['_blobs'].values())), bytes)
+    assert isinstance(next(iter(encoded_r[KEY_BLOBS].values())), bytes)
 
 
 def test_encode_model():
@@ -100,7 +101,7 @@ def test_encode_model():
     pprint.pprint(encoded_r)
 
     decoded_r = Document.decode(
-        encoded_r, getters={'blob': lambda x: encoded_r['_blobs'][x]}
+        encoded_r, getters={'blob': lambda x: encoded_r[KEY_BLOBS][x]}
     )
 
     print(decoded_r)
@@ -213,7 +214,7 @@ def test_refer_to_system(db):
     db.artifact_store.put_bytes(db.datatypes['image'].encoder(img), file_id='12345')
 
     r = {
-        '_leaves': {
+        '_builds': {
             'my_artifact': {
                 '_path': 'superduperdb/components/datatype/LazyArtifact',
                 'blob': '&:blob:12345',

test/unittest/base/test_leaf.py
@@ -4,6 +4,7 @@ from pprint import pprint
 
 from superduperdb import ObjectModel
 from superduperdb.backends.mongodb.query import MongoQuery
+from superduperdb.base.constant import KEY_BUILDS
 from superduperdb.base.document import Document
 from superduperdb.base.leaf import Leaf
 from superduperdb.base.variables import Variable
@@ -47,7 +48,7 @@ def test_encode_leaf():
         'a': 1,
         'b': 'test_b',
         'c': 1.5,
-        '_leaves': {},
+        '_builds': {},
         '_files': {},
         '_blobs': {},
     }
@@ -67,7 +68,7 @@ def test_encode_leaf_with_children():
         'a': 1,
         'b': 'test_b',
         'c': '?other_ser',
-        '_leaves': {
+        '_builds': {
             'other_ser': {
                 k: v for k, v in obj.c.dict().unpack().items() if k != 'identifier'
             },
@@ -106,12 +107,12 @@ def test_component_with_document():
     print('encoding')
     d = Document(t.dict())
     r = d.encode()
-    leaves = r['_leaves']
+    builds = r[KEY_BUILDS]
 
     pprint(r)
-    assert len(leaves) == 8
+    assert len(builds) == 8
 
-    for leaf in leaves:
+    for leaf in builds:
         print(type(leaf))
 
 

test/unittest/component/test_listener.py
@@ -7,6 +7,7 @@ import pytest
 from superduperdb import Document
 from superduperdb.backends.ibis.field_types import dtype
 from superduperdb.backends.mongodb.query import MongoQuery
+from superduperdb.base.constant import KEY_BLOBS
 from superduperdb.components.listener import Listener
 from superduperdb.components.model import ObjectModel
 from superduperdb.components.schema import Schema
@@ -25,7 +26,7 @@ def test_listener_serializes_properly():
     # check that the result is JSON-able
     import json
 
-    r.pop('_blobs')
+    r.pop(KEY_BLOBS)
     print(json.dumps(r, indent=2))
 
 

Pinnacle-Networks/Pinnacle

superduperdb/backends/base/artifacts.py
@@ -3,6 +3,8 @@ import os
 import typing as t
 from abc import ABC, abstractmethod
 
+from pinnacledb.base.constant import KEY_BLOBS, KEY_FILES
+
 
 def _construct_file_id_from_uri(uri):
     return str(hashlib.sha1(uri.encode()).hexdigest())
@@ -99,8 +101,8 @@ class ArtifactStore(ABC):
 
         :param r: dictionary with mandatory fields
         """
-        blobs = r.get('_blobs', {})
-        files = r.get('_files', {})
+        blobs = r.get(KEY_BLOBS, {})
+        files = r.get(KEY_FILES, {})
 
         for file_id, blob in blobs.items():
             try:
@@ -115,8 +117,8 @@ class ArtifactStore(ABC):
                 continue
 
         # After we save the artifacts, we can remove the blobs and files
-        r['_files'] = {}
-        r['_blobs'] = {}
+        r[KEY_FILES] = {}
+        r[KEY_BLOBS] = {}
 
         return r
 

superduperdb/backends/ibis/query.py
@@ -12,6 +12,7 @@ from pinnacledb.backends.base.query import (
     applies_to,
     parse_query as _parse_query,
 )
+from pinnacledb.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
 from pinnacledb.base.cursor import SuperDuperCursor
 from pinnacledb.base.exceptions import DatabackendException
 from pinnacledb.components.datatype import Encodable
@@ -265,9 +266,9 @@ class IbisQuery(Query):
     def _execute_insert(self, parent):
         documents = self._prepare_documents()
         for r in documents:
-            r.pop('_leaves')
-            r.pop('_blobs')
-            r.pop('_files')
+            r.pop(KEY_BUILDS)
+            r.pop(KEY_BLOBS)
+            r.pop(KEY_FILES)
             r.pop('_schema')
             if self.primary_id not in r:
                 pid = str(uuid.uuid4())

superduperdb/base/datalayer.py
@@ -19,6 +19,7 @@ from pinnacledb.backends.base.query import Query
 from pinnacledb.backends.local.compute import LocalComputeBackend
 from pinnacledb.base import exceptions
 from pinnacledb.base.config import Config
+from pinnacledb.base.constant import KEY_BUILDS
 from pinnacledb.base.cursor import SuperDuperCursor
 from pinnacledb.base.document import Document
 from pinnacledb.base.pinnacle import pinnacle
@@ -894,14 +895,14 @@ class Datalayer:
 
         serialized = object.dict().encode(leaves_to_keep=(Component,))
 
-        for k, v in serialized['_leaves'].items():
+        for k, v in serialized[KEY_BUILDS].items():
             if isinstance(v, Component) and hasattr(v, 'inline') and v.inline:
                 r = dict(v.dict())
                 del r['identifier']
-                serialized['_leaves'][k] = r
+                serialized[KEY_BUILDS][k] = r
 
         children = [
-            v for v in serialized['_leaves'].values() if isinstance(v, Component)
+            v for v in serialized[KEY_BUILDS].values() if isinstance(v, Component)
         ]
 
         jobs.extend(self._add_child_components(children, parent=object))
@@ -925,10 +926,10 @@ class Datalayer:
     def _change_component_reference_prefix(self, serialized):
         """Replace '?' to '&' in the serialized object."""
         references = {}
-        for reference in list(serialized['_leaves'].keys()):
-            if isinstance(serialized['_leaves'][reference], Component):
-                comp = serialized['_leaves'][reference]
-                serialized['_leaves'].pop(reference)
+        for reference in list(serialized[KEY_BUILDS].keys()):
+            if isinstance(serialized[KEY_BUILDS][reference], Component):
+                comp = serialized[KEY_BUILDS][reference]
+                serialized[KEY_BUILDS].pop(reference)
                 references[reference] = (
                     comp.type_id + ':' + comp.identifier + ':' + comp.uuid
                 )
@@ -1039,14 +1040,14 @@ class Datalayer:
         object.version = info['version']
 
         serialized = object.dict().encode(leaves_to_keep=(Component,))
-        for k, v in serialized['_leaves'].items():
+        for k, v in serialized[KEY_BUILDS].items():
             if isinstance(v, Component) and hasattr(v, 'inline') and v.inline:
                 r = dict(v.dict())
                 del r['identifier']
-                serialized['_leaves'][k] = r
+                serialized[KEY_BUILDS][k] = r
 
         children = [
-            v for v in serialized['_leaves'].values() if isinstance(v, Component)
+            v for v in serialized[KEY_BUILDS].values() if isinstance(v, Component)
         ]
 
         for child in children:

superduperdb/base/leaf.py
@@ -4,6 +4,7 @@ import inspect
 import typing as t
 import uuid
 
+from pinnacledb.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
 from pinnacledb.misc.annotations import extract_parameters, replace_parameters
 from pinnacledb.misc.serialization import asdict
 from pinnacledb.misc.special_dicts import SuperDuperFlatEncode
@@ -161,9 +162,9 @@ class Leaf(metaclass=LeafMeta):
         return SuperDuperFlatEncode(
             {
                 '_base': f'?{self.identifier}',
-                '_leaves': builds,
-                '_blobs': blobs,
-                '_files': files,
+                KEY_BUILDS: builds,
+                KEY_BLOBS: blobs,
+                KEY_FILES: files,
             }
         )
 

superduperdb/misc/special_dicts.py
@@ -10,6 +10,8 @@ from rich.panel import Panel
 from rich.text import Text
 from rich.tree import Tree
 
+from pinnacledb.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
+
 
 class IndexableDict(OrderedDict):
     """IndexableDict.
@@ -97,8 +99,8 @@ def _print_serialized_object(serialized_object):
 
     # Extract sections of the serialized object
     base_section = serialized_object.get('_base', {})
-    leaves_section = serialized_object.get('_leaves', {})
-    blobs_section = serialized_object.get('_blobs', {})
+    leaves_section = serialized_object.get(KEY_BUILDS, {})
+    blobs_section = serialized_object.get(KEY_BLOBS, {})
 
     # Format base section with additional component info
     base_yaml = _format_base_section(base_section)
@@ -144,38 +146,38 @@ class SuperDuperFlatEncode(t.Dict[str, t.Any]):
     """
 
     @property
-    def leaves(self):
-        """Return the leaves of the dictionary."""
-        return IndexableDict(self.get('_leaves', {}))
+    def builds(self):
+        """Return the builds of the dictionary."""
+        return IndexableDict(self.get(KEY_BUILDS, {}))
 
     @property
     def files(self):
         """Return the files of the dictionary."""
-        return self.get('_files', [])
+        return self.get(KEY_FILES, [])
 
     @property
     def blobs(self):
         """Return the blobs of the dictionary."""
-        return self.get('_blobs', [])
+        return self.get(KEY_BLOBS, [])
 
-    def pop_leaves(self):
-        """Pop the leaves of the dictionary."""
-        return IndexableDict(self.pop('_leaves', {}))
+    def pop_builds(self):
+        """Pop the builds of the dictionary."""
+        return IndexableDict(self.pop(KEY_BUILDS, {}))
 
     def pop_files(self):
         """Pop the files of the dictionary."""
-        return self.pop('_files', {})
+        return self.pop(KEY_FILES, {})
 
     def pop_blobs(self):
         """Pop the blobs of the dictionary."""
-        return self.pop('_blobs', {})
+        return self.pop(KEY_BLOBS, {})
 
     def load_keys_with_blob(self):
         """Load all outer reference keys with actual data blob."""
 
         def _get_blob(output, key):
             if isinstance(key, str) and key[0] == '?':
-                output = output['_leaves'][key[1:]]['blob']
+                output = output[KEY_BUILDS][key[1:]]['blob']
             else:
                 output = key
             return output
@@ -197,8 +199,8 @@ class SuperDuperFlatEncode(t.Dict[str, t.Any]):
         assert isinstance(d, SuperDuperFlatEncode)
         if '_base' in d:
             assert '_base' in self, "Cannot merge differently encoded data"
-        leaves = copy.deepcopy(self.leaves)
-        leaves.update(d.leaves)
+        builds = copy.deepcopy(self.builds)
+        builds.update(d.builds)
 
         blobs = copy.deepcopy(self.blobs)
         blobs = blobs.append(d.blobs)
@@ -207,17 +209,17 @@ class SuperDuperFlatEncode(t.Dict[str, t.Any]):
         files = files.append(d.files)
 
         if inplace:
-            if leaves:
-                self['_leaves'] = leaves
-            self['_blobs'] = blobs
-            self['_files'] = files
+            if builds:
+                self[KEY_BUILDS] = builds
+            self[KEY_BLOBS] = blobs
+            self[KEY_FILES] = files
             return self
         else:
             out = copy.deepcopy(self)
-            if leaves:
-                out['_leaves'] = leaves
-            out['_blobs'] = blobs
-            out['_files'] = files
+            if builds:
+                out[KEY_BUILDS] = builds
+            out[KEY_BLOBS] = blobs
+            out[KEY_FILES] = files
             return out
 
     def info(self):
@@ -321,9 +323,9 @@ def _diff_impl(r1, r2):
 
 
 def _childrens(tree, object, nesting=1):
-    if not object.leaves or not nesting:
+    if not object.builds or not nesting:
         return
-    for name, child in object.leaves.items():
+    for name, child in object.builds.items():
         child_text = f"{name}: {child.__class__}({child.identifier}): {str(child)[:50]}"
         subtree = tree.add(Text(child_text, style="yellow"))
         for key, value in child.__dict__.items():
@@ -345,9 +347,9 @@ def _component_metadata(obj):
 
     def _all_leaves(obj):
         result = []
-        if not obj.leaves:
+        if not obj.builds:
             return result
-        for name, leaf in obj.leaves.items():
+        for name, leaf in obj.builds.items():
             tmp = _all_leaves(leaf)
             result.extend([f'{name}.{x}' for x in tmp])
             result.append(name)
@@ -355,7 +357,7 @@ def _component_metadata(obj):
 
     metadata.append("[yellow]Leaves[/yellow]")
     rleaves = _all_leaves(obj)
-    obj_leaves = list(obj.leaves.keys())
+    obj_leaves = list(obj.builds.keys())
     for leaf in rleaves:
         if leaf in obj_leaves:
             metadata.append(f"[green]{leaf}[/green]")

test/unittest/base/test_document.py
@@ -6,6 +6,7 @@ from test.db_config import DBConfig
 import pytest
 
 from pinnacledb.backends.mongodb.query import MongoQuery
+from pinnacledb.base.constant import KEY_BLOBS, KEY_BUILDS
 from pinnacledb.components.datatype import Artifact, DataType
 from pinnacledb.components.model import ObjectModel
 from pinnacledb.components.schema import Schema
@@ -79,14 +80,14 @@ def test_encode_decode_flattened_document():
 
     import yaml
 
-    print(yaml.dump({k: v for k, v in encoded_r.items() if k != '_blobs'}))
+    print(yaml.dump({k: v for k, v in encoded_r.items() if k != KEY_BLOBS}))
 
     assert not isinstance(encoded_r, Document)
     assert isinstance(encoded_r, dict)
-    assert '_leaves' in encoded_r
-    assert '_blobs' in encoded_r
+    assert KEY_BUILDS in encoded_r
+    assert KEY_BLOBS in encoded_r
     assert encoded_r['img'].startswith('&:blob:')
-    assert isinstance(next(iter(encoded_r['_blobs'].values())), bytes)
+    assert isinstance(next(iter(encoded_r[KEY_BLOBS].values())), bytes)
 
 
 def test_encode_model():
@@ -100,7 +101,7 @@ def test_encode_model():
     pprint.pprint(encoded_r)
 
     decoded_r = Document.decode(
-        encoded_r, getters={'blob': lambda x: encoded_r['_blobs'][x]}
+        encoded_r, getters={'blob': lambda x: encoded_r[KEY_BLOBS][x]}
     )
 
     print(decoded_r)
@@ -213,7 +214,7 @@ def test_refer_to_system(db):
     db.artifact_store.put_bytes(db.datatypes['image'].encoder(img), file_id='12345')
 
     r = {
-        '_leaves': {
+        '_builds': {
             'my_artifact': {
                 '_path': 'pinnacledb/components/datatype/LazyArtifact',
                 'blob': '&:blob:12345',

test/unittest/base/test_leaf.py
@@ -4,6 +4,7 @@ from pprint import pprint
 
 from pinnacledb import ObjectModel
 from pinnacledb.backends.mongodb.query import MongoQuery
+from pinnacledb.base.constant import KEY_BUILDS
 from pinnacledb.base.document import Document
 from pinnacledb.base.leaf import Leaf
 from pinnacledb.base.variables import Variable
@@ -47,7 +48,7 @@ def test_encode_leaf():
         'a': 1,
         'b': 'test_b',
         'c': 1.5,
-        '_leaves': {},
+        '_builds': {},
         '_files': {},
         '_blobs': {},
     }
@@ -67,7 +68,7 @@ def test_encode_leaf_with_children():
         'a': 1,
         'b': 'test_b',
         'c': '?other_ser',
-        '_leaves': {
+        '_builds': {
             'other_ser': {
                 k: v for k, v in obj.c.dict().unpack().items() if k != 'identifier'
             },
@@ -106,12 +107,12 @@ def test_component_with_document():
     print('encoding')
     d = Document(t.dict())
     r = d.encode()
-    leaves = r['_leaves']
+    builds = r[KEY_BUILDS]
 
     pprint(r)
-    assert len(leaves) == 8
+    assert len(builds) == 8
 
-    for leaf in leaves:
+    for leaf in builds:
         print(type(leaf))
 
 

test/unittest/component/test_listener.py
@@ -7,6 +7,7 @@ import pytest
 from pinnacledb import Document
 from pinnacledb.backends.ibis.field_types import dtype
 from pinnacledb.backends.mongodb.query import MongoQuery
+from pinnacledb.base.constant import KEY_BLOBS
 from pinnacledb.components.listener import Listener
 from pinnacledb.components.model import ObjectModel
 from pinnacledb.components.schema import Schema
@@ -25,7 +26,7 @@ def test_listener_serializes_properly():
     # check that the result is JSON-able
     import json
 
-    r.pop('_blobs')
+    r.pop(KEY_BLOBS)
     print(json.dumps(r, indent=2))
 
 