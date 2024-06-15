superduper-io/superduper

Simplify variables

superduperdb/base/leaf.py
@@ -116,6 +116,7 @@ class Leaf(metaclass=LeafMeta):
     """
 
     set_post_init: t.ClassVar[t.Sequence[str]] = ()
+    literals: t.ClassVar[t.Sequence[str]] = ()
 
     identifier: str
     db: dc.InitVar[t.Optional['Datalayer']] = None
@@ -175,18 +176,17 @@ class Leaf(metaclass=LeafMeta):
         :param kwargs: Keyword arguments to pass to `_replace_variables`.
         """
         from superduperdb import Document
-        from superduperdb.base.variables import Variable, _replace_variables
+        from superduperdb.base.variables import _replace_variables
 
-        r = self.dict().encode(leaves_to_keep=(Variable,))
+        r = self.encode()
         r = _replace_variables(r, **kwargs)
-        return Document.decode(r).unpack()
+        return Document.decode(r)['_base']
 
     @property
     def variables(self) -> t.List[str]:
         """Get list of variables in the object."""
-        from superduperdb.base.variables import Variable, _find_variables
-
-        return list(set(_find_variables(self.encode(leaves_to_keep=Variable))))
+        from superduperdb.base.variables import _find_variables
+        return list(set(_find_variables(self.encode())))
 
     def dict(self):
         """Return dictionary representation of the object."""
@@ -194,6 +194,8 @@ class Leaf(metaclass=LeafMeta):
 
         r = asdict(self)
         r.update(self.metadata)
+        if self.literals:
+            r['_literals'] = list(self.literals)
 
         from superduperdb.components.datatype import Artifact, dill_serializer
 

superduperdb/base/variables.py
@@ -1,24 +1,12 @@
 import re
 
-from superduperdb.base.leaf import Leaf
-
-
-class VariableError(Exception):
-    """
-    Variable error.
-
-    :param args: *args for `Exception`.
-    :param kwargs: **kwargs for `Exception`.
-    """
-
 
 def _find_variables(r):
+    from superduperdb.base.leaf import Leaf
     if isinstance(r, dict):
         return sum([_find_variables(v) for v in r.values()], [])
     if isinstance(r, (list, tuple)):
         return sum([_find_variables(v) for v in r], [])
-    if isinstance(r, Variable):
-        return [r.identifier]
     if isinstance(r, str):
         return re.findall(r'<var:(.*?)>', r)
     if isinstance(r, Leaf):
@@ -26,29 +14,8 @@ def _find_variables(r):
     return []
 
 
-def _find_variables_with_path(r):
-    if isinstance(r, dict):
-        out = []
-        for k, v in r.items():
-            tmp = _find_variables_with_path(v)
-            for p in tmp:
-                out.append({'path': [k] + p['path'], 'variable': p['variable']})
-        return out
-    elif isinstance(r, (list, tuple)):
-        out = []
-        for i, v in enumerate(r):
-            tmp = _find_variables_with_path(v)
-            for p in tmp:
-                out.append({'path': [i] + p['path'], 'variable': p['variable']})
-        return out
-    elif isinstance(r, Variable):
-        return [{'path': [], 'variable': r}]
-    return []
-
-
 def _replace_variables(x, **kwargs):
     from .document import Document
-
     if isinstance(x, dict):
         return {
             _replace_variables(k, **kwargs): _replace_variables(v, **kwargs)
@@ -59,51 +26,13 @@ def _replace_variables(x, **kwargs):
         variables = list(map(lambda v: v.strip(), variables))
         for k, v in kwargs.items():
             if k in variables:
-                x = x.replace(f'<var:{k}>', str(v))
-
+                if isinstance(v, str):
+                    x = x.replace(f'<var:{k}>', v)
+                else:
+                    x = v
         return x
     if isinstance(x, (list, tuple)):
         return [_replace_variables(v, **kwargs) for v in x]
-    if isinstance(x, Variable):
-        return x.set(**kwargs)
     if isinstance(x, Document):
         return x.set_variables(**kwargs)
     return x
-
-
-class Variable(Leaf):
-    """Mechanism for allowing "free variables" in a leaf object.
-
-    The idea is to allow a variable to be set at runtime, rather than
-    at object creation time.
-    """
-
-    def __post_init__(self, db=None):
-        super().__post_init__(db)
-        self.value = self.identifier
-
-    @property
-    def key(self):
-        """Variable key."""
-        return f'<var:{str(self.value)}>'
-
-    def __repr__(self) -> str:
-        return self.key
-
-    def __hash__(self) -> int:
-        return hash(self.value)
-
-    def set(self, **kwargs):
-        """
-        Get the intended value from the values of the global variables.
-
-        :param db: The datalayer instance.
-        :param kwargs: Variables to be used in the setter_callback
-                       or as formatting variables.
-
-        >>> Variable('number').set(db, number=1.5, other='test')
-        1.5
-
-        """
-        assert isinstance(self.value, str)
-        return kwargs.get(self.value, self)

superduperdb/components/graph.py
@@ -100,7 +100,7 @@ class IndexableNode:
         :param identifier: Unique identifier
         """
         from superduperdb.components.listener import Listener
-        from superduperdb.components.stack import Stack
+        from superduperdb.components.application import Application
 
         nodes = list(nx.topological_sort(self.parent_graph))
         input_node = next(
@@ -130,7 +130,7 @@ class IndexableNode:
                 key=key,
                 identifier=node.identifier,
             )
-        return Stack(
+        return Application(
             identifier=identifier,
             components=list(listener_lookup.values()),
         )

superduperdb/components/template.py
@@ -4,8 +4,11 @@ import os
 import typing as t
 
 from superduperdb.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
+from superduperdb.base.datalayer import Datalayer
 from superduperdb.base.document import Document
+from superduperdb.base.variables import _replace_variables
 from superduperdb.components.datatype import DataType, dill_lazy
+from superduperdb.misc.special_dicts import SuperDuperFlatEncode
 
 from .component import Component, ensure_initialized
 
@@ -21,63 +24,41 @@ class Template(Component):
                              use.
     """
 
-    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, "DataType"]]] = (
-        ("_component_blobs", dill_lazy),
+    literals: t.ClassVar[t.Tuple[str]] = ('component',)
+    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, 'DataType']]] = (
+        ('_component_blobs', dill_lazy),
     )
     type_id: t.ClassVar[str] = "template"
 
     component: t.Union[Component, t.Dict]
+    variables: t.Optional[t.List[str]] = None
+    blobs: t.Optional[t.List[str]] = None
     info: t.Optional[t.Dict] = dc.field(default_factory=dict)
-    _component_blobs: t.Optional[t.Union[t.Dict, bytes]] = dc.field(
-        default_factory=dict
-    )
+
+    def pre_create(self, db: Datalayer) -> None:
+        super().pre_create(db)
+        for identifier, blob in self.component[KEY_BLOBS]:
+            db.artifact_store.put_bytes(blob, identifier)
+        self.blobs = list(self.component[KEY_BLOBS].keys())
+        self.component.pop(KEY_BLOBS)
 
     def __post_init__(self, db, artifacts):
-        self._variables = []
         if isinstance(self.component, Component):
-            self._variables = self.component.variables
-            self.component = self.component.dict().encode()
-            if not self._component_blobs:
-                self._component_blobs = self.component.pop_blobs()
+            self.component = self.component.encode()
+        else:
+            self.component = SuperDuperFlatEncode(self.component)
+        if self.variables is None:
+            self.variables = self.component.variables
         return super().__post_init__(db, artifacts)
 
     @ensure_initialized
     def __call__(self, **kwargs):
         """Method to create component from the given template and `kwargs`."""
-        if self.info:
-            assert set(kwargs.keys()) == (set(self.info.keys())), "Invalid variables"
-        t = Document.decode(
-            {**self.component, KEY_BLOBS: self._component_blobs}, db=self.db
-        )
-        t.init(db=self.db)
-        t = t.set_variables(db=self.db, **kwargs)
-        t.init(db=self.db)
-        return t
+        assert set(kwargs.keys()) == set(self.variables)
+        _replace_variables(self.component, **kwargs)
+        return Document.decode(self.component, db=self.db)
 
-    @property
-    def variables(self):
-        """Variables in `Template` property."""
-        return self._variables
-
-    @staticmethod
-    def _append_component_metadata(r, component={}):
-        builds = r.get(KEY_BUILDS, {})
-        files = r.get(KEY_FILES, {})
-        builds.update(component.pop(KEY_BUILDS, {}))
-        files.update(component.pop(KEY_FILES, {}))
-        r[KEY_BUILDS] = builds
-        r[KEY_FILES] = files
-        return r
-
-    def dict(self):
-        """Updates base dict document with `component` metadata."""
-        r = super().dict()
-        component = r["component"]
-        r = self._append_component_metadata(r, component=component)
-
-        return r
-
-    def export(self, path: str, format: str = "json"):
+    def export(self, path: str, format: str = 'json', zip: bool = True):
         """
         Save `self` to a directory using super-duper protocol.
 
@@ -90,35 +71,12 @@ class Template(Component):
         |_files/*
         ```
         """
-        r = self.dict().encode()
-
-        os.makedirs(path, exist_ok=True)
-        if self._component_blobs:
-            os.makedirs(os.path.join(path, "blobs"), exist_ok=True)
-            for file_id, bytestr_ in r.blobs.items():
-                filepath = os.path.join(path, "blobs", file_id)
-                with open(filepath, "wb") as f:
-                    f.write(bytestr_)
-            r.pop_blobs()
-        else:
-            del r[KEY_BLOBS]
-
-        if format == "json":
-            with open(os.path.join(path, "component.json"), "w") as f:
-                json.dump(r, f, indent=2)
-        elif format == "yaml":
-            import yaml
-
-            with open(os.path.join(path, "component.yaml"), "w") as f:
-                yaml.safe_dump(r, f)
-        else:
-            raise ValueError(f"Invalid format: {format}")
-
-    def on_load(self, db):
-        """
-        Datalayer `on_load` hook.
-
-        :param db: Datalayer instance.
-        """
-        super().on_load(db=db)
-        self.init(db=db)
+        super().export(path, format, zip=False)
+        if self.blobs:
+            os.makedirs(os.path.join(path, 'blobs'), exist_ok=True)
+            for identifier in self.blobs:
+                blob = self.db.artifact_store.get_bytes(identifier)
+                with open(path + f'/blobs/{identifier}', 'wb') as f:
+                    f.write(blob)
+        if zip:
+            self.zip_export(path)

superduperdb/misc/special_dicts.py
@@ -11,6 +11,7 @@ from rich.text import Text
 from rich.tree import Tree
 
 from superduperdb.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
+from superduperdb.base.variables import _find_variables
 
 
 class IndexableDict(OrderedDict):
@@ -221,6 +222,10 @@ class SuperDuperFlatEncode(t.Dict[str, t.Any]):
             out[KEY_BLOBS] = blobs
             out[KEY_FILES] = files
             return out
+
+    @property
+    def variables(self):
+        return _find_variables(self)
 
     def info(self):
         """Print the serialized object."""

Pinnacle-Networks/Pinnacle

superduperdb/base/leaf.py
@@ -116,6 +116,7 @@ class Leaf(metaclass=LeafMeta):
     """
 
     set_post_init: t.ClassVar[t.Sequence[str]] = ()
+    literals: t.ClassVar[t.Sequence[str]] = ()
 
     identifier: str
     db: dc.InitVar[t.Optional['Datalayer']] = None
@@ -175,18 +176,17 @@ class Leaf(metaclass=LeafMeta):
         :param kwargs: Keyword arguments to pass to `_replace_variables`.
         """
         from pinnacledb import Document
-        from pinnacledb.base.variables import Variable, _replace_variables
+        from pinnacledb.base.variables import _replace_variables
 
-        r = self.dict().encode(leaves_to_keep=(Variable,))
+        r = self.encode()
         r = _replace_variables(r, **kwargs)
-        return Document.decode(r).unpack()
+        return Document.decode(r)['_base']
 
     @property
     def variables(self) -> t.List[str]:
         """Get list of variables in the object."""
-        from pinnacledb.base.variables import Variable, _find_variables
-
-        return list(set(_find_variables(self.encode(leaves_to_keep=Variable))))
+        from pinnacledb.base.variables import _find_variables
+        return list(set(_find_variables(self.encode())))
 
     def dict(self):
         """Return dictionary representation of the object."""
@@ -194,6 +194,8 @@ class Leaf(metaclass=LeafMeta):
 
         r = asdict(self)
         r.update(self.metadata)
+        if self.literals:
+            r['_literals'] = list(self.literals)
 
         from pinnacledb.components.datatype import Artifact, dill_serializer
 

superduperdb/base/variables.py
@@ -1,24 +1,12 @@
 import re
 
-from pinnacledb.base.leaf import Leaf
-
-
-class VariableError(Exception):
-    """
-    Variable error.
-
-    :param args: *args for `Exception`.
-    :param kwargs: **kwargs for `Exception`.
-    """
-
 
 def _find_variables(r):
+    from pinnacledb.base.leaf import Leaf
     if isinstance(r, dict):
         return sum([_find_variables(v) for v in r.values()], [])
     if isinstance(r, (list, tuple)):
         return sum([_find_variables(v) for v in r], [])
-    if isinstance(r, Variable):
-        return [r.identifier]
     if isinstance(r, str):
         return re.findall(r'<var:(.*?)>', r)
     if isinstance(r, Leaf):
@@ -26,29 +14,8 @@ def _find_variables(r):
     return []
 
 
-def _find_variables_with_path(r):
-    if isinstance(r, dict):
-        out = []
-        for k, v in r.items():
-            tmp = _find_variables_with_path(v)
-            for p in tmp:
-                out.append({'path': [k] + p['path'], 'variable': p['variable']})
-        return out
-    elif isinstance(r, (list, tuple)):
-        out = []
-        for i, v in enumerate(r):
-            tmp = _find_variables_with_path(v)
-            for p in tmp:
-                out.append({'path': [i] + p['path'], 'variable': p['variable']})
-        return out
-    elif isinstance(r, Variable):
-        return [{'path': [], 'variable': r}]
-    return []
-
-
 def _replace_variables(x, **kwargs):
     from .document import Document
-
     if isinstance(x, dict):
         return {
             _replace_variables(k, **kwargs): _replace_variables(v, **kwargs)
@@ -59,51 +26,13 @@ def _replace_variables(x, **kwargs):
         variables = list(map(lambda v: v.strip(), variables))
         for k, v in kwargs.items():
             if k in variables:
-                x = x.replace(f'<var:{k}>', str(v))
-
+                if isinstance(v, str):
+                    x = x.replace(f'<var:{k}>', v)
+                else:
+                    x = v
         return x
     if isinstance(x, (list, tuple)):
         return [_replace_variables(v, **kwargs) for v in x]
-    if isinstance(x, Variable):
-        return x.set(**kwargs)
     if isinstance(x, Document):
         return x.set_variables(**kwargs)
     return x
-
-
-class Variable(Leaf):
-    """Mechanism for allowing "free variables" in a leaf object.
-
-    The idea is to allow a variable to be set at runtime, rather than
-    at object creation time.
-    """
-
-    def __post_init__(self, db=None):
-        super().__post_init__(db)
-        self.value = self.identifier
-
-    @property
-    def key(self):
-        """Variable key."""
-        return f'<var:{str(self.value)}>'
-
-    def __repr__(self) -> str:
-        return self.key
-
-    def __hash__(self) -> int:
-        return hash(self.value)
-
-    def set(self, **kwargs):
-        """
-        Get the intended value from the values of the global variables.
-
-        :param db: The datalayer instance.
-        :param kwargs: Variables to be used in the setter_callback
-                       or as formatting variables.
-
-        >>> Variable('number').set(db, number=1.5, other='test')
-        1.5
-
-        """
-        assert isinstance(self.value, str)
-        return kwargs.get(self.value, self)

superduperdb/components/graph.py
@@ -100,7 +100,7 @@ class IndexableNode:
         :param identifier: Unique identifier
         """
         from pinnacledb.components.listener import Listener
-        from pinnacledb.components.stack import Stack
+        from pinnacledb.components.application import Application
 
         nodes = list(nx.topological_sort(self.parent_graph))
         input_node = next(
@@ -130,7 +130,7 @@ class IndexableNode:
                 key=key,
                 identifier=node.identifier,
             )
-        return Stack(
+        return Application(
             identifier=identifier,
             components=list(listener_lookup.values()),
         )

superduperdb/components/template.py
@@ -4,8 +4,11 @@ import os
 import typing as t
 
 from pinnacledb.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
+from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.base.document import Document
+from pinnacledb.base.variables import _replace_variables
 from pinnacledb.components.datatype import DataType, dill_lazy
+from pinnacledb.misc.special_dicts import SuperDuperFlatEncode
 
 from .component import Component, ensure_initialized
 
@@ -21,63 +24,41 @@ class Template(Component):
                              use.
     """
 
-    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, "DataType"]]] = (
-        ("_component_blobs", dill_lazy),
+    literals: t.ClassVar[t.Tuple[str]] = ('component',)
+    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, 'DataType']]] = (
+        ('_component_blobs', dill_lazy),
     )
     type_id: t.ClassVar[str] = "template"
 
     component: t.Union[Component, t.Dict]
+    variables: t.Optional[t.List[str]] = None
+    blobs: t.Optional[t.List[str]] = None
     info: t.Optional[t.Dict] = dc.field(default_factory=dict)
-    _component_blobs: t.Optional[t.Union[t.Dict, bytes]] = dc.field(
-        default_factory=dict
-    )
+
+    def pre_create(self, db: Datalayer) -> None:
+        super().pre_create(db)
+        for identifier, blob in self.component[KEY_BLOBS]:
+            db.artifact_store.put_bytes(blob, identifier)
+        self.blobs = list(self.component[KEY_BLOBS].keys())
+        self.component.pop(KEY_BLOBS)
 
     def __post_init__(self, db, artifacts):
-        self._variables = []
         if isinstance(self.component, Component):
-            self._variables = self.component.variables
-            self.component = self.component.dict().encode()
-            if not self._component_blobs:
-                self._component_blobs = self.component.pop_blobs()
+            self.component = self.component.encode()
+        else:
+            self.component = SuperDuperFlatEncode(self.component)
+        if self.variables is None:
+            self.variables = self.component.variables
         return super().__post_init__(db, artifacts)
 
     @ensure_initialized
     def __call__(self, **kwargs):
         """Method to create component from the given template and `kwargs`."""
-        if self.info:
-            assert set(kwargs.keys()) == (set(self.info.keys())), "Invalid variables"
-        t = Document.decode(
-            {**self.component, KEY_BLOBS: self._component_blobs}, db=self.db
-        )
-        t.init(db=self.db)
-        t = t.set_variables(db=self.db, **kwargs)
-        t.init(db=self.db)
-        return t
+        assert set(kwargs.keys()) == set(self.variables)
+        _replace_variables(self.component, **kwargs)
+        return Document.decode(self.component, db=self.db)
 
-    @property
-    def variables(self):
-        """Variables in `Template` property."""
-        return self._variables
-
-    @staticmethod
-    def _append_component_metadata(r, component={}):
-        builds = r.get(KEY_BUILDS, {})
-        files = r.get(KEY_FILES, {})
-        builds.update(component.pop(KEY_BUILDS, {}))
-        files.update(component.pop(KEY_FILES, {}))
-        r[KEY_BUILDS] = builds
-        r[KEY_FILES] = files
-        return r
-
-    def dict(self):
-        """Updates base dict document with `component` metadata."""
-        r = super().dict()
-        component = r["component"]
-        r = self._append_component_metadata(r, component=component)
-
-        return r
-
-    def export(self, path: str, format: str = "json"):
+    def export(self, path: str, format: str = 'json', zip: bool = True):
         """
         Save `self` to a directory using super-duper protocol.
 
@@ -90,35 +71,12 @@ class Template(Component):
         |_files/*
         ```
         """
-        r = self.dict().encode()
-
-        os.makedirs(path, exist_ok=True)
-        if self._component_blobs:
-            os.makedirs(os.path.join(path, "blobs"), exist_ok=True)
-            for file_id, bytestr_ in r.blobs.items():
-                filepath = os.path.join(path, "blobs", file_id)
-                with open(filepath, "wb") as f:
-                    f.write(bytestr_)
-            r.pop_blobs()
-        else:
-            del r[KEY_BLOBS]
-
-        if format == "json":
-            with open(os.path.join(path, "component.json"), "w") as f:
-                json.dump(r, f, indent=2)
-        elif format == "yaml":
-            import yaml
-
-            with open(os.path.join(path, "component.yaml"), "w") as f:
-                yaml.safe_dump(r, f)
-        else:
-            raise ValueError(f"Invalid format: {format}")
-
-    def on_load(self, db):
-        """
-        Datalayer `on_load` hook.
-
-        :param db: Datalayer instance.
-        """
-        super().on_load(db=db)
-        self.init(db=db)
+        super().export(path, format, zip=False)
+        if self.blobs:
+            os.makedirs(os.path.join(path, 'blobs'), exist_ok=True)
+            for identifier in self.blobs:
+                blob = self.db.artifact_store.get_bytes(identifier)
+                with open(path + f'/blobs/{identifier}', 'wb') as f:
+                    f.write(blob)
+        if zip:
+            self.zip_export(path)

superduperdb/misc/special_dicts.py
@@ -11,6 +11,7 @@ from rich.text import Text
 from rich.tree import Tree
 
 from pinnacledb.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
+from pinnacledb.base.variables import _find_variables
 
 
 class IndexableDict(OrderedDict):
@@ -221,6 +222,10 @@ class SuperDuperFlatEncode(t.Dict[str, t.Any]):
             out[KEY_BLOBS] = blobs
             out[KEY_FILES] = files
             return out
+
+    @property
+    def variables(self):
+        return _find_variables(self)
 
     def info(self):
         """Print the serialized object."""