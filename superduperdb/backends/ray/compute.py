superduper-io/superduper

Fixed dependencies of ray jobs

superduperdb/backends/ray/compute.py
@@ -2,9 +2,11 @@ import json
 import os
 import typing as t
 import uuid
+import time
 
 import ray
 from ray.job_submission import JobSubmissionClient
+from ray.job_submission import JobStatus
 
 from superduperdb import logging
 from superduperdb.backends.base.compute import ComputeBackend
@@ -75,11 +77,14 @@ class RayComputeBackend(ComputeBackend):
 
         runtime_env = {}
         env_vars = {
-            k: os.environ[k] for k in os.environ if k.startswith('SUPERDUPERDB_')
+            k: os.environ[k]
+            for k in os.environ
+            if k.startswith('SUPERDUPERDB_') and k != 'SUPERDUPERDB_CONFIG'
         }
         if env_vars:
             runtime_env = {'env_vars': env_vars}
         job_id = self.client.submit_job(entrypoint=entrypoint, runtime_env=runtime_env)
+        self._futures_collection[job_id] = job_id
         return job_id
 
     def execute_task(
@@ -94,11 +99,9 @@ class RayComputeBackend(ComputeBackend):
         """
 
         def _dependable_remote_job(function, *args, **kwargs):
-            if 'dependencies' in kwargs:
-                dependencies = kwargs.pop('dependencies', None)
-                if dependencies:
-                    ray.wait(dependencies)
             return function(*args, **kwargs)
+
+        [self.wait_for_job_completion(str(dependency)) for dependency in dependencies]
 
         if compute_kwargs:
             remote_function = ray.remote(**compute_kwargs)(_dependable_remote_job)
@@ -107,7 +110,7 @@ class RayComputeBackend(ComputeBackend):
 
         from superduperdb.jobs.job import remote_task
 
-        future = remote_function.remote(remote_task, job_id, dependencies=dependencies)
+        future = remote_function.remote(remote_task, job_id)
 
         ray.get(future)
         task_id = str(future.task_id().hex())
@@ -118,6 +121,16 @@ class RayComputeBackend(ComputeBackend):
             f"task: {task_id}; job_id: {str(future.job_id())}"
         )
         return future, task_id
+
+    def wait_for_job_completion(self, job_id, poll_interval=3):
+        while True:
+            status = self.client.get_job_status(job_id)
+            logging.info(f"Job {job_id} status: {status}")
+            if status == JobStatus.SUCCEEDED:
+                return True
+            elif status in [JobStatus.FAILED, JobStatus.STOPPED]:
+                raise Exception(f"The dependency job {job_id} failed")
+            time.sleep(poll_interval)
 
     @property
     def tasks(self) -> t.Dict[str, ray.ObjectRef]:

superduperdb/jobs/job.py
@@ -1,10 +1,11 @@
 import datetime
+import json
 import typing as t
 import uuid
 from abc import abstractmethod
 
 import superduperdb as s
-from superduperdb import CFG
+from superduperdb import CFG, logging
 from superduperdb.jobs.tasks import callable_job, method_job
 
 if t.TYPE_CHECKING:
@@ -64,7 +65,7 @@ class Job:
         return self.db.metadata.watch_job(identifier=self.identifier)
 
     @abstractmethod
-    def submit(self, compute, dependencies=()):
+    def submit(self, compute, dependencies=(), update_job=True):
         """Submit job for execution.
 
         :param compute: compute engine
@@ -127,9 +128,11 @@ class FunctionJob(Job):
         :param dependencies: list of dependencies
         """
         self.job_id = self.db.compute.submit(self.identifier, dependencies=dependencies)
+        self.db.metadata.update_job(self.identifier, 'job_id', self.job_id)
+        self.future = self.job_id
         return
 
-    def submit(self, dependencies=()):
+    def submit(self, dependencies=(), update_job=True):
         """Submit job for execution.
 
         :param dependencies: list of dependencies
@@ -144,7 +147,8 @@ class FunctionJob(Job):
             dependencies=dependencies,
             db=self.db if self.db.compute.type == 'local' else None,
         )
-
+        if update_job:
+            self.db.metadata.update_job(self.identifier, 'job_id', self.future)
         return
 
     def __call__(self, db: t.Union['Datalayer', None], dependencies=()):
@@ -225,9 +229,11 @@ class ComponentJob(Job):
             dependencies=dependencies,
             compute_kwargs=self.compute_kwargs,
         )
+        self.db.metadata.update_job(self.identifier, 'job_id', self.job_id)
+        self.future = self.job_id
         return
 
-    def submit(self, dependencies=()):
+    def submit(self, dependencies=(), update_job=True):
         """Submit job for execution.
 
         :param dependencies: list of dependencies
@@ -244,7 +250,8 @@ class ComponentJob(Job):
             dependencies=dependencies,
             db=self.db if self.db.compute.type == 'local' else None,
         )
-        self.db.metadata.update_job(self.identifier, 'job_id', self.job_id)
+        if update_job:
+            self.db.metadata.update_job(self.identifier, 'job_id', self.future)
         return self
 
     def __call__(self, db: t.Union['Datalayer', None] = None, dependencies=()):
@@ -298,6 +305,10 @@ def remote_job(identifier, dependencies=(), compute_kwargs: t.Union[str, t.Dict]
     from superduperdb import CFG
     from superduperdb.base.build import build_compute
 
+    logging.info(f"Running remote job {identifier}")
+    logging.info(f"Dependencies: {dependencies}")
+    logging.info(f"Compute kwargs: {compute_kwargs}")
+
     if isinstance(compute_kwargs, str) and compute_kwargs:
         compute_kwargs = json.loads(compute_kwargs)
 
@@ -329,6 +340,7 @@ def remote_task(identifier, dependencies=()):
     args = info['args']
     kwargs = info['kwargs']
     path = info['_path']
+    logging.info(f"Running remote task: {info}")
 
     if 'ComponentJob' in path:
         component_identifier = info['component_identifier']
@@ -352,5 +364,5 @@ def remote_task(identifier, dependencies=()):
         job = FunctionJob(args=args, kwargs=kwargs, callable=callable, db=db)
     else:
         raise TypeError
-    job.submit(dependencies=dependencies)
+    job.submit(dependencies=dependencies, update_job=True)
     return

Pinnacle-Networks/Pinnacle

superduperdb/backends/ray/compute.py
@@ -1,10 +1,11 @@
 import json
 import os
+import time
 import typing as t
 import uuid
 
 import ray
-from ray.job_submission import JobSubmissionClient
+from ray.job_submission import JobStatus, JobSubmissionClient
 
 from pinnacledb import logging
 from pinnacledb.backends.base.compute import ComputeBackend
@@ -75,11 +76,14 @@ class RayComputeBackend(ComputeBackend):
 
         runtime_env = {}
         env_vars = {
-            k: os.environ[k] for k in os.environ if k.startswith('SUPERDUPERDB_')
+            k: os.environ[k]
+            for k in os.environ
+            if k.startswith('SUPERDUPERDB_') and k != 'SUPERDUPERDB_CONFIG'
         }
         if env_vars:
             runtime_env = {'env_vars': env_vars}
         job_id = self.client.submit_job(entrypoint=entrypoint, runtime_env=runtime_env)
+        self._futures_collection[job_id] = job_id
         return job_id
 
     def execute_task(
@@ -94,11 +98,9 @@ class RayComputeBackend(ComputeBackend):
         """
 
         def _dependable_remote_job(function, *args, **kwargs):
-            if 'dependencies' in kwargs:
-                dependencies = kwargs.pop('dependencies', None)
-                if dependencies:
-                    ray.wait(dependencies)
             return function(*args, **kwargs)
+
+        [self._wait_for_job_completion(str(dependency)) for dependency in dependencies]
 
         if compute_kwargs:
             remote_function = ray.remote(**compute_kwargs)(_dependable_remote_job)
@@ -107,7 +109,7 @@ class RayComputeBackend(ComputeBackend):
 
         from pinnacledb.jobs.job import remote_task
 
-        future = remote_function.remote(remote_task, job_id, dependencies=dependencies)
+        future = remote_function.remote(remote_task, job_id)
 
         ray.get(future)
         task_id = str(future.task_id().hex())
@@ -118,6 +120,16 @@ class RayComputeBackend(ComputeBackend):
             f"task: {task_id}; job_id: {str(future.job_id())}"
         )
         return future, task_id
+
+    def _wait_for_job_completion(self, job_id, poll_interval=3):
+        while True:
+            status = self.client.get_job_status(job_id)
+            logging.info(f"Job {job_id} status: {status}")
+            if status == JobStatus.SUCCEEDED:
+                return True
+            elif status in [JobStatus.FAILED, JobStatus.STOPPED]:
+                raise Exception(f"The dependency job {job_id} failed")
+            time.sleep(poll_interval)
 
     @property
     def tasks(self) -> t.Dict[str, ray.ObjectRef]:

superduperdb/jobs/job.py
@@ -4,7 +4,7 @@ import uuid
 from abc import abstractmethod
 
 import pinnacledb as s
-from pinnacledb import CFG
+from pinnacledb import CFG, logging
 from pinnacledb.jobs.tasks import callable_job, method_job
 
 if t.TYPE_CHECKING:
@@ -64,7 +64,7 @@ class Job:
         return self.db.metadata.watch_job(identifier=self.identifier)
 
     @abstractmethod
-    def submit(self, compute, dependencies=()):
+    def submit(self, compute, dependencies=(), update_job=True):
         """Submit job for execution.
 
         :param compute: compute engine
@@ -127,9 +127,11 @@ class FunctionJob(Job):
         :param dependencies: list of dependencies
         """
         self.job_id = self.db.compute.submit(self.identifier, dependencies=dependencies)
+        self.db.metadata.update_job(self.identifier, 'job_id', self.job_id)
+        self.future = self.job_id
         return
 
-    def submit(self, dependencies=()):
+    def submit(self, dependencies=(), update_job=True):
         """Submit job for execution.
 
         :param dependencies: list of dependencies
@@ -144,7 +146,8 @@ class FunctionJob(Job):
             dependencies=dependencies,
             db=self.db if self.db.compute.type == 'local' else None,
         )
-
+        if update_job:
+            self.db.metadata.update_job(self.identifier, 'job_id', self.future)
         return
 
     def __call__(self, db: t.Union['Datalayer', None], dependencies=()):
@@ -225,9 +228,11 @@ class ComponentJob(Job):
             dependencies=dependencies,
             compute_kwargs=self.compute_kwargs,
         )
+        self.db.metadata.update_job(self.identifier, 'job_id', self.job_id)
+        self.future = self.job_id
         return
 
-    def submit(self, dependencies=()):
+    def submit(self, dependencies=(), update_job=True):
         """Submit job for execution.
 
         :param dependencies: list of dependencies
@@ -244,7 +249,8 @@ class ComponentJob(Job):
             dependencies=dependencies,
             db=self.db if self.db.compute.type == 'local' else None,
         )
-        self.db.metadata.update_job(self.identifier, 'job_id', self.job_id)
+        if update_job:
+            self.db.metadata.update_job(self.identifier, 'job_id', self.future)
         return self
 
     def __call__(self, db: t.Union['Datalayer', None] = None, dependencies=()):
@@ -298,6 +304,10 @@ def remote_job(identifier, dependencies=(), compute_kwargs: t.Union[str, t.Dict]
     from pinnacledb import CFG
     from pinnacledb.base.build import build_compute
 
+    logging.info(f"Running remote job {identifier}")
+    logging.info(f"Dependencies: {dependencies}")
+    logging.info(f"Compute kwargs: {compute_kwargs}")
+
     if isinstance(compute_kwargs, str) and compute_kwargs:
         compute_kwargs = json.loads(compute_kwargs)
 
@@ -329,6 +339,7 @@ def remote_task(identifier, dependencies=()):
     args = info['args']
     kwargs = info['kwargs']
     path = info['_path']
+    logging.info(f"Running remote task: {info}")
 
     if 'ComponentJob' in path:
         component_identifier = info['component_identifier']
@@ -352,5 +363,5 @@ def remote_task(identifier, dependencies=()):
         job = FunctionJob(args=args, kwargs=kwargs, callable=callable, db=db)
     else:
         raise TypeError
-    job.submit(dependencies=dependencies)
+    job.submit(dependencies=dependencies, update_job=True)
     return