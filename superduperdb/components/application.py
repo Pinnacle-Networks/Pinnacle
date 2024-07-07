superduper-io/superduper

Support for packaging application from the database.

superduperdb/components/application.py
@@ -1,8 +1,9 @@
 import typing as t
 
-from superduperdb.base.datalayer import Datalayer
-
 from .component import Component
+
+if t.TYPE_CHECKING:
+    from superduperdb.base.datalayer import Datalayer
 
 
 class Application(Component):
@@ -14,22 +15,22 @@ class Application(Component):
         assist in managing application.
     """
 
-    literals: t.ClassVar[t.Sequence[str]] = ('template',)
-    type_id: t.ClassVar[str] = 'application'
+    literals: t.ClassVar[t.Sequence[str]] = ("template",)
+    type_id: t.ClassVar[str] = "application"
     components: t.Sequence[Component]
     namespace: t.Optional[t.Sequence[t.Tuple[str, str]]] = None
 
-    def pre_create(self, db: Datalayer):
+    def pre_create(self, db: "Datalayer"):
         """Pre-create hook.
 
         :param db: Datalayer instance
         """
         self.namespace = [
-            {'type_id': c.type_id, 'identifier': c.identifier} for c in self.children
+            {"type_id": c.type_id, "identifier": c.identifier} for c in self.children
         ]
         return super().pre_create(db)
 
-    def cleanup(self, db: Datalayer):
+    def cleanup(self, db: "Datalayer"):
         """Cleanup hook.
 
         :param db: Datalayer instance
@@ -38,3 +39,60 @@ class Application(Component):
             for type_id, identifier in self.namespace:
                 db.remove(type_id=type_id, identifier=identifier, force=True)
         return super().cleanup(db)
+
+    @classmethod
+    def build_from_db(cls, identifier, db: "Datalayer"):
+        """Build application from `superduperdb`.
+
+        :param identifier: Identifier of the application.
+        :param db: Datalayer instance
+        """
+        components = []
+        for component_info in db.show():
+            if not all(
+                [
+                    isinstance(component_info, dict),
+                    "type_id" in component_info,
+                    "identifier" in component_info,
+                ]
+            ):
+                raise ValueError("Invalid component info.")
+
+            component = db.load(
+                type_id=component_info["type_id"],
+                identifier=component_info["identifier"],
+            )
+
+            components.append(component)
+
+        # Do not need to include outputs and schema components
+        model_outputs_components = set()
+        for component in components:
+            if any(
+                [
+                    component.type_id == "table"
+                    and component.identifier.startswith("_outputs"),
+                    component.type_id == "schema"
+                    and component.identifier.startswith("_schema/"),
+                ]
+            ):
+                model_outputs_components.add(component.identifier)
+                model_outputs_components.update(
+                    [c.identifier for c in component.children]
+                )
+
+        # Do not need to include components with parent
+        components_with_parent = set()
+        for component in components:
+            components_with_parent.update([c.identifier for c in component.children])
+
+        remove_components = model_outputs_components | components_with_parent
+
+        components = [c for c in components if c.identifier not in remove_components]
+
+        if not components:
+            raise ValueError("No components found.")
+
+        app = cls(identifier=identifier, components=components)
+        app.init(db)
+        return app

test/unittest/component/test_application.py
@@ -1,0 +1,95 @@
+import typing as t
+from pprint import pprint
+from test.db_config import DBConfig
+
+import numpy as np
+import pytest
+
+from superduperdb import ObjectModel, Schema, superduper
+from superduperdb.base.document import Document
+from superduperdb.components.datatype import pickle_encoder
+from superduperdb.components.table import Table
+
+if t.TYPE_CHECKING:
+    from superduperdb.base.datalayer import Datalayer
+
+
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
+def test_wrap_as_application_from_db(db: "Datalayer"):
+    data = {"x": 1, "y": 2, "z": np.array([1, 2, 3])}
+    db.cfg.auto_schema = True
+
+    schema = Schema(
+        identifier="schema",
+        fields={
+            "x": int,
+            "y": int,
+            "z": pickle_encoder,
+        },
+    )
+
+    table = Table("documents", schema=schema)
+
+    db.apply(table)
+
+    db["documents"].insert([data]).execute()
+
+    model1 = ObjectModel(identifier="model1", object=lambda x: x + 1)
+
+    listener1 = model1.to_listener(
+        key="x",
+        select=db["documents"].select(),
+        identifier="listener1",
+    )
+
+    model2 = ObjectModel(identifier="model2", object=lambda y: y + 2)
+
+    listener2 = model2.to_listener(
+        key="y", select=db["documents"].select(), identifier="listener2"
+    )
+
+    model3 = ObjectModel(
+        identifier="model3", object=lambda z: z * 3, datatype=pickle_encoder
+    )
+
+    listener3 = model3.to_listener(
+        key="z", select=db["documents"].select(), identifier="listener3"
+    )
+
+    db.apply(listener1)
+    db.apply(listener2)
+    db.apply(listener3)
+
+    pprint(db.show())
+
+    from superduperdb.components.application import Application
+
+    app = Application.build_from_db(identifier="app", db=db)
+
+    assert app.identifier == "app"
+
+    assert {c.identifier for c in app.components} == {
+        "listener1",
+        "listener2",
+        "listener3",
+        table.identifier,
+    }
+
+    db = superduper(db.databackend.uri)
+
+    db["documents"].insert([data]).execute()
+
+    assert "_outputs" not in list(db["documents"].select().execute())[0]
+    db.apply(app)
+
+    data = list(db["documents"].select().execute())[0]
+    listener1.outputs_select
+
+    def get_listener_output(listener):
+        return Document(list(listener.outputs_select.execute())[0].unpack())[
+            listener.outputs_key
+        ]
+
+    assert get_listener_output(listener1) == 2
+    assert get_listener_output(listener2) == 4
+    assert np.allclose(get_listener_output(listener3), data["z"] * 3)

Pinnacle-Networks/Pinnacle

superduperdb/components/application.py
@@ -1,8 +1,9 @@
 import typing as t
 
-from pinnacledb.base.datalayer import Datalayer
-
 from .component import Component
+
+if t.TYPE_CHECKING:
+    from pinnacledb.base.datalayer import Datalayer
 
 
 class Application(Component):
@@ -14,22 +15,22 @@ class Application(Component):
         assist in managing application.
     """
 
-    literals: t.ClassVar[t.Sequence[str]] = ('template',)
-    type_id: t.ClassVar[str] = 'application'
+    literals: t.ClassVar[t.Sequence[str]] = ("template",)
+    type_id: t.ClassVar[str] = "application"
     components: t.Sequence[Component]
     namespace: t.Optional[t.Sequence[t.Tuple[str, str]]] = None
 
-    def pre_create(self, db: Datalayer):
+    def pre_create(self, db: "Datalayer"):
         """Pre-create hook.
 
         :param db: Datalayer instance
         """
         self.namespace = [
-            {'type_id': c.type_id, 'identifier': c.identifier} for c in self.children
+            {"type_id": c.type_id, "identifier": c.identifier} for c in self.children
         ]
         return super().pre_create(db)
 
-    def cleanup(self, db: Datalayer):
+    def cleanup(self, db: "Datalayer"):
         """Cleanup hook.
 
         :param db: Datalayer instance
@@ -38,3 +39,60 @@ class Application(Component):
             for type_id, identifier in self.namespace:
                 db.remove(type_id=type_id, identifier=identifier, force=True)
         return super().cleanup(db)
+
+    @classmethod
+    def build_from_db(cls, identifier, db: "Datalayer"):
+        """Build application from `pinnacledb`.
+
+        :param identifier: Identifier of the application.
+        :param db: Datalayer instance
+        """
+        components = []
+        for component_info in db.show():
+            if not all(
+                [
+                    isinstance(component_info, dict),
+                    "type_id" in component_info,
+                    "identifier" in component_info,
+                ]
+            ):
+                raise ValueError("Invalid component info.")
+
+            component = db.load(
+                type_id=component_info["type_id"],
+                identifier=component_info["identifier"],
+            )
+
+            components.append(component)
+
+        # Do not need to include outputs and schema components
+        model_outputs_components = set()
+        for component in components:
+            if any(
+                [
+                    component.type_id == "table"
+                    and component.identifier.startswith("_outputs"),
+                    component.type_id == "schema"
+                    and component.identifier.startswith("_schema/"),
+                ]
+            ):
+                model_outputs_components.add(component.identifier)
+                model_outputs_components.update(
+                    [c.identifier for c in component.children]
+                )
+
+        # Do not need to include components with parent
+        components_with_parent = set()
+        for component in components:
+            components_with_parent.update([c.identifier for c in component.children])
+
+        remove_components = model_outputs_components | components_with_parent
+
+        components = [c for c in components if c.identifier not in remove_components]
+
+        if not components:
+            raise ValueError("No components found.")
+
+        app = cls(identifier=identifier, components=components)
+        app.init(db)
+        return app

test/unittest/component/test_application.py
@@ -1,0 +1,95 @@
+import typing as t
+from pprint import pprint
+from test.db_config import DBConfig
+
+import numpy as np
+import pytest
+
+from pinnacledb import ObjectModel, Schema, pinnacle
+from pinnacledb.base.document import Document
+from pinnacledb.components.datatype import pickle_encoder
+from pinnacledb.components.table import Table
+
+if t.TYPE_CHECKING:
+    from pinnacledb.base.datalayer import Datalayer
+
+
+@pytest.mark.parametrize("db", DBConfig.EMPTY_CASES, indirect=True)
+def test_wrap_as_application_from_db(db: "Datalayer"):
+    data = {"x": 1, "y": 2, "z": np.array([1, 2, 3])}
+    db.cfg.auto_schema = True
+
+    schema = Schema(
+        identifier="schema",
+        fields={
+            "x": int,
+            "y": int,
+            "z": pickle_encoder,
+        },
+    )
+
+    table = Table("documents", schema=schema)
+
+    db.apply(table)
+
+    db["documents"].insert([data]).execute()
+
+    model1 = ObjectModel(identifier="model1", object=lambda x: x + 1)
+
+    listener1 = model1.to_listener(
+        key="x",
+        select=db["documents"].select(),
+        identifier="listener1",
+    )
+
+    model2 = ObjectModel(identifier="model2", object=lambda y: y + 2)
+
+    listener2 = model2.to_listener(
+        key="y", select=db["documents"].select(), identifier="listener2"
+    )
+
+    model3 = ObjectModel(
+        identifier="model3", object=lambda z: z * 3, datatype=pickle_encoder
+    )
+
+    listener3 = model3.to_listener(
+        key="z", select=db["documents"].select(), identifier="listener3"
+    )
+
+    db.apply(listener1)
+    db.apply(listener2)
+    db.apply(listener3)
+
+    pprint(db.show())
+
+    from pinnacledb.components.application import Application
+
+    app = Application.build_from_db(identifier="app", db=db)
+
+    assert app.identifier == "app"
+
+    assert {c.identifier for c in app.components} == {
+        "listener1",
+        "listener2",
+        "listener3",
+        table.identifier,
+    }
+
+    db = pinnacle(db.databackend.uri)
+
+    db["documents"].insert([data]).execute()
+
+    assert "_outputs" not in list(db["documents"].select().execute())[0]
+    db.apply(app)
+
+    data = list(db["documents"].select().execute())[0]
+    listener1.outputs_select
+
+    def get_listener_output(listener):
+        return Document(list(listener.outputs_select.execute())[0].unpack())[
+            listener.outputs_key
+        ]
+
+    assert get_listener_output(listener1) == 2
+    assert get_listener_output(listener2) == 4
+    assert np.allclose(get_listener_output(listener3), data["z"] * 3)