SuperDuperDB/superduperdb-staging

Add plugin component

superduperdb/cdc/cdc.py
@@ -410,17 +410,6 @@ class DatabaseChangeDataCapture:
             t.Union['TableOrCollection', 'IbisQuery']
         ] = []
 
-        listeners = self.db.show('listener')
-        if listeners:
-            from superduperdb.components.listener import Listener
-
-            for listener in listeners:
-                listener = db.load(identifier=listener, type_id='listener')
-                assert isinstance(listener, Listener)
-                if listener.select is None:
-                    continue
-                self.add(listener)
-
     @property
     def running(self) -> bool:
         """Check if the cdc service is running."""
@@ -428,10 +417,23 @@ class DatabaseChangeDataCapture:
 
     def start(self):
         """Start the cdc service # noqa."""
+        self._add_listener()
         self._running = True
 
         for collection in self._cdc_existing_collections:
             self.listen(collection)
+
+    def _add_listener(self):
+        listeners = self.db.show('listener')
+        if listeners:
+            from superduperdb.components.listener import Listener
+
+            for listener in listeners:
+                listener = self.db.load(identifier=listener, type_id='listener')
+                assert isinstance(listener, Listener)
+                if listener.select is None:
+                    continue
+                self.add(listener)
 
     def listen(
         self,

superduperdb/components/datatype.py
@@ -20,7 +20,7 @@ from superduperdb.base.config import BytesEncoding
 from superduperdb.base.leaf import Leaf
 from superduperdb.components.component import Component, ensure_initialized
 from superduperdb.misc.annotations import component
-from superduperdb.misc.hash import random_sha1
+from superduperdb.misc.hash import hash_path
 
 Decode = t.Callable[[bytes], t.Any]
 Encode = t.Callable[[t.Any], bytes]
@@ -666,7 +666,7 @@ class File(_BaseEncodable):
 
     def dict(self, metadata: bool = True, defaults: bool = True):
         """Get the dictionary representation of the object."""
-        self.identifier = self.identifier or random_sha1()
+        self.identifier = self.identifier or hash_path(self.x)
         r = super().dict(metadata=metadata, defaults=defaults)
         r['x'] = FileItem(
             identifier=self.identifier, path=self.x, file_name=self._file_name

superduperdb/components/listener.py
@@ -3,7 +3,7 @@ import typing as t
 
 from overrides import override
 
-from superduperdb import CFG
+from superduperdb import CFG, logging
 from superduperdb.backends.base.query import Query
 from superduperdb.base.document import _OUTPUTS_KEY
 from superduperdb.base.enums import DBType
@@ -169,8 +169,14 @@ class Listener(Component):
 
         dependencies_ids = []
         for predict_id in self.dependencies:
-            upstream_listener = db.load(uuid=predict_id)
-            upstream_model = upstream_listener.model
+            try:
+                upstream_listener = db.load(uuid=predict_id)
+                upstream_model = upstream_listener.model
+            except Exception:
+                logging.warn(
+                    f"Could not find the upstream listener with uuid {predict_id}"
+                )
+                continue
             jobs = self.db.metadata.show_jobs(upstream_model.identifier, 'model') or []
             job_ids = [job['job_id'] for job in jobs]
             dependencies_ids.extend(job_ids)

Pinnacle-Networks/Pinnacle

(cherry picked from commit 6c85b9ebd461bf993136de6daae5e3f7edc7069e)
Added UT for plugin component
Support pip install in plugin component
Optimize the import logic of the plugin

superduperdb/cdc/cdc.py
@@ -410,17 +410,6 @@ class DatabaseChangeDataCapture:
             t.Union['TableOrCollection', 'IbisQuery']
         ] = []
 
-        listeners = self.db.show('listener')
-        if listeners:
-            from pinnacledb.components.listener import Listener
-
-            for listener in listeners:
-                listener = db.load(identifier=listener, type_id='listener')
-                assert isinstance(listener, Listener)
-                if listener.select is None:
-                    continue
-                self.add(listener)
-
     @property
     def running(self) -> bool:
         """Check if the cdc service is running."""
@@ -428,10 +417,23 @@ class DatabaseChangeDataCapture:
 
     def start(self):
         """Start the cdc service # noqa."""
+        self._add_listener()
         self._running = True
 
         for collection in self._cdc_existing_collections:
             self.listen(collection)
+
+    def _add_listener(self):
+        listeners = self.db.show('listener')
+        if listeners:
+            from pinnacledb.components.listener import Listener
+
+            for listener in listeners:
+                listener = self.db.load(identifier=listener, type_id='listener')
+                assert isinstance(listener, Listener)
+                if listener.select is None:
+                    continue
+                self.add(listener)
 
     def listen(
         self,

superduperdb/components/datatype.py
@@ -20,7 +20,7 @@ from pinnacledb.base.config import BytesEncoding
 from pinnacledb.base.leaf import Leaf
 from pinnacledb.components.component import Component, ensure_initialized
 from pinnacledb.misc.annotations import component
-from pinnacledb.misc.hash import random_sha1
+from pinnacledb.misc.hash import hash_path
 
 Decode = t.Callable[[bytes], t.Any]
 Encode = t.Callable[[t.Any], bytes]
@@ -666,7 +666,7 @@ class File(_BaseEncodable):
 
     def dict(self, metadata: bool = True, defaults: bool = True):
         """Get the dictionary representation of the object."""
-        self.identifier = self.identifier or random_sha1()
+        self.identifier = self.identifier or hash_path(self.x)
         r = super().dict(metadata=metadata, defaults=defaults)
         r['x'] = FileItem(
             identifier=self.identifier, path=self.x, file_name=self._file_name

superduperdb/components/listener.py
@@ -3,7 +3,7 @@ import typing as t
 
 from overrides import override
 
-from pinnacledb import CFG
+from pinnacledb import CFG, logging
 from pinnacledb.backends.base.query import Query
 from pinnacledb.base.document import _OUTPUTS_KEY
 from pinnacledb.base.enums import DBType
@@ -169,8 +169,14 @@ class Listener(Component):
 
         dependencies_ids = []
         for predict_id in self.dependencies:
-            upstream_listener = db.load(uuid=predict_id)
-            upstream_model = upstream_listener.model
+            try:
+                upstream_listener = db.load(uuid=predict_id)
+                upstream_model = upstream_listener.model
+            except Exception:
+                logging.warn(
+                    f"Could not find the upstream listener with uuid {predict_id}"
+                )
+                continue
             jobs = self.db.metadata.show_jobs(upstream_model.identifier, 'model') or []
             job_ids = [job['job_id'] for job in jobs]
             dependencies_ids.extend(job_ids)