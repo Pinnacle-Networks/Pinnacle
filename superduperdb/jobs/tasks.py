superduper-io/superduper

Add basic services structure

superduperdb/jobs/tasks.py
@@ -2,6 +2,7 @@ import typing as t
 
 if t.TYPE_CHECKING:
     from superduperdb.base.datalayer import Datalayer
+    from superduperdb.components.component import Component
 
 
 def method_job(
@@ -14,6 +15,7 @@ def method_job(
     job_id,
     dependencies=(),
     db: t.Optional['Datalayer'] = None,
+    component: 'Component' = None,
 ):
     """
     Run a method on a component in the database.
@@ -42,13 +44,13 @@ def method_job(
     # Ray cluster would be created inside the job
     if db is None:
         db = build_datalayer(cfg=cfg, cluster__compute___path=None)
+    
+    if not component:
+        component = db.load(type_id, identifier)
+        from superduperdb.components.component import Component
+        component = t.cast(Component, component)
+        component.unpack()
 
-    component = db.load(type_id, identifier)
-    # TODO: Move the unpack method to the better place
-    from superduperdb.components.component import Component
-
-    component = t.cast(Component, component)
-    component.unpack()
     method = getattr(component, method_name)
     db.metadata.update_job(job_id, 'status', 'running')
 

superduperdb/server/scheduler.py
@@ -2,10 +2,7 @@ import typing as t
 
 from pydantic import BaseModel
 
-import json
-
-from superduperdb import CFG
-from superduperdb import CFG, logging
+from superduperdb import logging
 from superduperdb.base.datalayer import Datalayer
 from superduperdb.server.app import DatalayerDependency, SuperDuperApp
 
@@ -18,23 +15,68 @@ class JobSubmit(BaseModel):
     dependencies: t.List[str]
     compute_kwargs: t.Dict[str, t.Any]
 
+
+@app.startup
+def scheduler_startup(db: Datalayer) -> Datalayer:
+    """Start the cdc server.
+
+    :param db: Datalayer instance.
+    """
+    db.compute.connect()
+    return db
+
+
+@app.add("/job/create/component_hook", status_code=200, method='get')
+def component_hook(component_id: str, db: Datalayer = DatalayerDependency()):
+    db.compute.component_hook(component_id, to='create')
+
 @app.add("/job/submit", status_code=200, method='post')
 def submit(job: JobSubmit, db: Datalayer = DatalayerDependency()):
+    from superduperdb.jobs.job import ComponentJob, FunctionJob
     
-
     logging.info(f"Running remote job {job.identifier}")
     logging.info(f"Dependencies: {job.dependencies}")
     logging.info(f"Compute kwargs: {job.compute_kwargs}")
 
 
     assert db.compute.remote is True, "Compute is not a distributed backend type."
+
     deps_future = []
     for dep in job.dependencies:
         if future:=db.compute._futures_collection.get(dep, None):
             deps_future.append(future)
 
-    db.compute.execute_task(
-        job.identifier, dependencies=deps_future, compute_kwargs=job.compute_kwargs
-    )
+    info = db.metadata.get_job(job.identifier)
+
+    args = info['args']
+    kwargs = info['kwargs']
+    path = info['_path']
+    logging.info(f"Running remote task: {info}")
+
+    if 'ComponentJob' in path:
+        component_identifier = info['component_identifier']
+        method_name = info['method_name']
+        type_id = info['type_id']
+        job = ComponentJob(
+            args=args,
+            kwargs=kwargs,
+            method_name=method_name,
+            component_identifier=component_identifier,
+            type_id=type_id,
+            db=db,
+        )
+    elif 'FunctionJob' in path:
+        import importlib
+
+        function = path.split('/')[-1]
+        import_path, function = function.split(';')
+        callable = getattr(importlib.import_module(import_path), function)
+
+        job = FunctionJob(args=args, kwargs=kwargs, callable=callable, db=db)
+    else:
+        raise TypeError
+    job.submit(dependencies=deps_future, update_job=True)
 
     return {'message': 'Job created successfully'}
+
+

Pinnacle-Networks/Pinnacle

superduperdb/jobs/tasks.py
@@ -2,6 +2,7 @@ import typing as t
 
 if t.TYPE_CHECKING:
     from pinnacledb.base.datalayer import Datalayer
+    from pinnacledb.components.component import Component
 
 
 def method_job(
@@ -14,6 +15,7 @@ def method_job(
     job_id,
     dependencies=(),
     db: t.Optional['Datalayer'] = None,
+    component: 'Component' = None,
 ):
     """
     Run a method on a component in the database.
@@ -42,13 +44,13 @@ def method_job(
     # Ray cluster would be created inside the job
     if db is None:
         db = build_datalayer(cfg=cfg, cluster__compute___path=None)
+    
+    if not component:
+        component = db.load(type_id, identifier)
+        from pinnacledb.components.component import Component
+        component = t.cast(Component, component)
+        component.unpack()
 
-    component = db.load(type_id, identifier)
-    # TODO: Move the unpack method to the better place
-    from pinnacledb.components.component import Component
-
-    component = t.cast(Component, component)
-    component.unpack()
     method = getattr(component, method_name)
     db.metadata.update_job(job_id, 'status', 'running')
 

superduperdb/server/scheduler.py
@@ -2,10 +2,7 @@ import typing as t
 
 from pydantic import BaseModel
 
-import json
-
-from pinnacledb import CFG
-from pinnacledb import CFG, logging
+from pinnacledb import logging
 from pinnacledb.base.datalayer import Datalayer
 from pinnacledb.server.app import DatalayerDependency, SuperDuperApp
 
@@ -18,23 +15,68 @@ class JobSubmit(BaseModel):
     dependencies: t.List[str]
     compute_kwargs: t.Dict[str, t.Any]
 
+
+@app.startup
+def scheduler_startup(db: Datalayer) -> Datalayer:
+    """Start the cdc server.
+
+    :param db: Datalayer instance.
+    """
+    db.compute.connect()
+    return db
+
+
+@app.add("/job/create/component_hook", status_code=200, method='get')
+def component_hook(component_id: str, db: Datalayer = DatalayerDependency()):
+    db.compute.component_hook(component_id, to='create')
+
 @app.add("/job/submit", status_code=200, method='post')
 def submit(job: JobSubmit, db: Datalayer = DatalayerDependency()):
+    from pinnacledb.jobs.job import ComponentJob, FunctionJob
     
-
     logging.info(f"Running remote job {job.identifier}")
     logging.info(f"Dependencies: {job.dependencies}")
     logging.info(f"Compute kwargs: {job.compute_kwargs}")
 
 
     assert db.compute.remote is True, "Compute is not a distributed backend type."
+
     deps_future = []
     for dep in job.dependencies:
         if future:=db.compute._futures_collection.get(dep, None):
             deps_future.append(future)
 
-    db.compute.execute_task(
-        job.identifier, dependencies=deps_future, compute_kwargs=job.compute_kwargs
-    )
+    info = db.metadata.get_job(job.identifier)
+
+    args = info['args']
+    kwargs = info['kwargs']
+    path = info['_path']
+    logging.info(f"Running remote task: {info}")
+
+    if 'ComponentJob' in path:
+        component_identifier = info['component_identifier']
+        method_name = info['method_name']
+        type_id = info['type_id']
+        job = ComponentJob(
+            args=args,
+            kwargs=kwargs,
+            method_name=method_name,
+            component_identifier=component_identifier,
+            type_id=type_id,
+            db=db,
+        )
+    elif 'FunctionJob' in path:
+        import importlib
+
+        function = path.split('/')[-1]
+        import_path, function = function.split(';')
+        callable = getattr(importlib.import_module(import_path), function)
+
+        job = FunctionJob(args=args, kwargs=kwargs, callable=callable, db=db)
+    else:
+        raise TypeError
+    job.submit(dependencies=deps_future, update_job=True)
 
     return {'message': 'Job created successfully'}
+
+