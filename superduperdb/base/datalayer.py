superduper-io/superduper

Add Event cls for db events

superduperdb/base/datalayer.py
@@ -2,7 +2,7 @@ import dataclasses as dc
 import random
 import typing as t
 import warnings
-from collections import defaultdict, namedtuple
+from collections import namedtuple
 
 import click
 import networkx
@@ -26,8 +26,7 @@ from superduperdb.cdc.cdc import DatabaseChangeDataCapture
 from superduperdb.components.component import Component
 from superduperdb.components.datatype import DataType, _BaseEncodable
 from superduperdb.components.schema import Schema
-from superduperdb.jobs.job import ComponentJob, FunctionJob, Job
-from superduperdb.jobs.task_workflow import TaskWorkflow
+from superduperdb.jobs.job import Job
 from superduperdb.misc.annotations import deprecated
 from superduperdb.misc.colors import Colors
 from superduperdb.misc.data import ibatch
@@ -36,7 +35,6 @@ from superduperdb.misc.retry import db_retry
 from superduperdb.misc.special_dicts import recursive_update
 from superduperdb.vector_search.base import BaseVectorSearcher, VectorItem
 from superduperdb.vector_search.interface import FastVectorSearcher
-from superduperdb.vector_search.update_tasks import copy_vectors, delete_vectors
 
 DBResult = t.Any
 TaskGraph = t.Any
@@ -48,6 +46,23 @@ UpdateResult = t.Any
 PredictResult = t.Union[Document, t.Sequence[Document]]
 ExecuteResult = t.Union[SelectResult, DeleteResult, UpdateResult, InsertResult]
 
+
+class Event:
+    insert= 'insert'
+    delete= 'delete'
+    update= 'update'
+    upsert= 'upsert'
+
+    @staticmethod
+    def chunk_by_event(lst):
+        chunks = {}
+        for item in lst:
+            item_type = item['type']
+            
+            if item_type not in chunks:
+                chunks[item_type] = []
+            chunks[item_type].append(item)
+        return chunks
 
 class Datalayer:
     """
@@ -107,7 +122,7 @@ class Datalayer:
         self.cdc = DatabaseChangeDataCapture(self)
 
         self.compute = compute
-        self.compute.queue.set_db(self)
+        self.compute.queue.db = self
         self._server_mode = False
         self._cfg = s.CFG
 
@@ -342,7 +357,7 @@ class Datalayer:
         """
         result = delete.do_execute(self)
         if refresh and not self.cdc.running:
-            return result, self.on_db_event(delete, ids=result, type='delete')
+            return result, self.on_event(delete, ids=result, event_type=Event.delete)
         return result, None
 
     def _insert(
@@ -381,7 +396,7 @@ class Datalayer:
             if cdc_status:
                 logging.warn('CDC service is active, skipping model/listener refresh')
             else:
-                return inserted_ids, self.on_db_event(
+                return inserted_ids, self.on_event(
                     insert,
                     ids=inserted_ids,
                 )
@@ -397,12 +412,11 @@ class Datalayer:
         return select.do_execute(db=self)
 
 
-    def on_db_event(
+    def on_event(
         self,
         query: Query,
         ids: t.Sequence[str],
-        overwrite: bool = False,
-        type: str = 'insert'
+        event_type: str = 'insert'
     ):
         """
         Trigger computation jobs after data insertion.
@@ -410,10 +424,9 @@ class Datalayer:
         :param query: The select or update query object that reduces
                       the scope of computations.
         :param ids: IDs that further reduce the scope of computations.
-        :param overwrite: If True, cascade the value to the 'predict_in_db' job.
         """
-        deps = query.listeners()
-        events = [{'identifier': id, 'type': type} for id in ids]
+        deps = query.dependencies()
+        events = [{'identifier': id, 'type': event_type} for id in ids]
         return self.compute.broadcast(events, to=deps)
 
     def _write(self, write: Query, refresh: bool = True) -> UpdateResult:
@@ -437,17 +450,17 @@ class Datalayer:
                     # Overwrite should be true for update operation since updates
                     # could be done on collections with already existing outputs
                     # We need overwrite ouptuts on those select and recompute predict
-                    job_update = self.on_db_event(
-                        query=q, ids=ids, overwrite=True
+                    job_update = self.on_event(
+                        query=q, ids=ids, event_type=Event.upsert
                     )
                     jobs.append(job_update)
 
                     q = d['query']
                     ids = d['ids']
-                    job_update = self.on_db_event(
+                    job_update = self.on_event(
                         query=q,
                         ids=ids,
-                        type='delete'
+                        event_type=Event.delete
                     )
                     jobs.append(job_update)
 
@@ -474,8 +487,8 @@ class Datalayer:
                 # Overwrite should be true since updates could be done on collections
                 # with already existing outputs.
                 # We need overwrite outputs on those select and recompute predict
-                return updated_ids, self.on_db_event(
-                    query=update, ids=updated_ids, overwrite=True, type='insert'
+                return updated_ids, self.on_event(
+                    query=update, ids=updated_ids, event_type=Event.upsert
                 )
         return updated_ids, None
 

superduperdb/components/listener.py
@@ -89,6 +89,8 @@ class Listener(Component):
 
         :param db: Data layer instance.
         """
+
+        from superduperdb.base.datalayer import Event
         self.create_output_dest(db, self.uuid, self.model)
         if self.select is not None and self.active and not db.server_mode:
             if CFG.cluster.cdc.uri:
@@ -100,12 +102,24 @@ class Listener(Component):
                 )
             else:
                 db.cdc.add(self)
+
         db.compute.queue.declare_component(self)
+        
+        ids = db.execute(self.select.select_ids)
+        ids = [id[self.select.primary_id] for id in ids]
+        events = [{'identifier': id, 'type': Event.insert} for id in ids]
+        deps = [{'type_id': 'listener', 'identifier': self.identifier}]
+        return db.compute.broadcast(events, to=deps)
+
 
     def on_db_event(self, db, events):
-        ids = [event['identifier'] for event in events]
-        self.schedule_jobs(db=db, ids=ids)
+        from superduperdb.base.datalayer import Event
 
+        for type, events in Event.chunk_by_event(events).items():
+            overwrite= True if type in [Event.insert, Event.upsert] else False
+
+            ids = [event['identifier'] for event in events]
+            self.schedule_jobs(db=db, ids=ids , overwrite=overwrite)
 
     @classmethod
     def create_output_dest(cls, db: "Datalayer", uuid, model: Model):

superduperdb/components/vector_index.py
@@ -172,17 +172,17 @@ class VectorIndex(Component):
 
     def on_db_event(self, db, events):
         from superduperdb.vector_search.update_tasks import delete_vectors, copy_vectors
-        deleted_ids = []
-        inserted_ids = [  ]
-        for event in events:
-            if event['type'] == 'insert':
-                inserted_ids.append(event['identifier'])
+        from superduperdb.base.datalayer import Event
+        def _schedule_jobs(ids, type=Event.insert):
+            if type in [Event.insert, Event.upsert]:
+
+                callable =  copy_vectors
+            elif type == Event.delete:
+                callable = delete_vectors
             else:
-                deleted_ids.append(event['identifier'])
-        
-        def _schedule_jobs(ids, type='insert'):
+                return
             job=FunctionJob(
-                callable=copy_vectors if type=='insert' else delete_vectors,
+                callable=callable,
                 args=[],
                 kwargs=dict(
                     vector_index=self.identifier,
@@ -191,8 +191,10 @@ class VectorIndex(Component):
                 ),
             )
             job(db=db)
-        _schedule_jobs(inserted_ids)
-        _schedule_jobs(deleted_ids, type='delete')
+        
+        for type, events in Event.chunk_by_event(events).items():
+            ids = [event['identifier'] for event in events]
+            _schedule_jobs(ids, type=type)
 
     @override
     def post_create(self, db: "Datalayer") -> None:

Pinnacle-Networks/Pinnacle

superduperdb/base/datalayer.py
@@ -2,7 +2,7 @@ import dataclasses as dc
 import random
 import typing as t
 import warnings
-from collections import defaultdict, namedtuple
+from collections import namedtuple
 
 import click
 import networkx
@@ -25,8 +25,7 @@ from pinnacledb.base.document import Document
 from pinnacledb.components.component import Component
 from pinnacledb.components.datatype import DataType, _BaseEncodable
 from pinnacledb.components.schema import Schema
-from pinnacledb.jobs.job import ComponentJob, FunctionJob, Job
-from pinnacledb.jobs.task_workflow import TaskWorkflow
+from pinnacledb.jobs.job import Job
 from pinnacledb.misc.annotations import deprecated
 from pinnacledb.misc.colors import Colors
 from pinnacledb.misc.data import ibatch
@@ -35,7 +34,6 @@ from pinnacledb.misc.retry import db_retry
 from pinnacledb.misc.special_dicts import recursive_update
 from pinnacledb.vector_search.base import BaseVectorSearcher, VectorItem
 from pinnacledb.vector_search.interface import FastVectorSearcher
-from pinnacledb.vector_search.update_tasks import copy_vectors, delete_vectors
 
 DBResult = t.Any
 TaskGraph = t.Any
@@ -47,6 +45,23 @@ UpdateResult = t.Any
 PredictResult = t.Union[Document, t.Sequence[Document]]
 ExecuteResult = t.Union[SelectResult, DeleteResult, UpdateResult, InsertResult]
 
+
+class Event:
+    insert= 'insert'
+    delete= 'delete'
+    update= 'update'
+    upsert= 'upsert'
+
+    @staticmethod
+    def chunk_by_event(lst):
+        chunks = {}
+        for item in lst:
+            item_type = item['type']
+            
+            if item_type not in chunks:
+                chunks[item_type] = []
+            chunks[item_type].append(item)
+        return chunks
 
 class Datalayer:
     """
@@ -104,7 +119,7 @@ class Datalayer:
         self.databackend.datalayer = self
 
         self.compute = compute
-        self.compute.queue.set_db(self)
+        self.compute.queue.db = self
         self._server_mode = False
         self._cfg = s.CFG
 
@@ -340,7 +355,7 @@ class Datalayer:
         result = delete.do_execute(self)
         cdc_status = s.CFG.cluster.cdc.uri is not None
         if refresh and not cdc_status:
-            return result, self.on_db_event(delete, ids=result, type='delete')
+            return result, self.on_event(delete, ids=result, event_type=Event.delete)
         return result, None
 
     def _insert(
@@ -379,7 +394,7 @@ class Datalayer:
             if cdc_status:
                 logging.warn('CDC service is active, skipping model/listener refresh')
             else:
-                return inserted_ids, self.on_db_event(
+                return inserted_ids, self.on_event(
                     insert,
                     ids=inserted_ids,
                 )
@@ -395,12 +410,11 @@ class Datalayer:
         return select.do_execute(db=self)
 
 
-    def on_db_event(
+    def on_event(
         self,
         query: Query,
         ids: t.Sequence[str],
-        overwrite: bool = False,
-        type: str = 'insert'
+        event_type: str = 'insert'
     ):
         """
         Trigger computation jobs after data insertion.
@@ -408,10 +422,9 @@ class Datalayer:
         :param query: The select or update query object that reduces
                       the scope of computations.
         :param ids: IDs that further reduce the scope of computations.
-        :param overwrite: If True, cascade the value to the 'predict_in_db' job.
         """
-        deps = query.listeners()
-        events = [{'identifier': id, 'type': type} for id in ids]
+        deps = query.dependencies()
+        events = [{'identifier': id, 'type': event_type} for id in ids]
         return self.compute.broadcast(events, to=deps)
 
     def _write(self, write: Query, refresh: bool = True) -> UpdateResult:
@@ -435,17 +448,17 @@ class Datalayer:
                     # Overwrite should be true for update operation since updates
                     # could be done on collections with already existing outputs
                     # We need overwrite ouptuts on those select and recompute predict
-                    job_update = self.on_db_event(
-                        query=q, ids=ids, overwrite=True
+                    job_update = self.on_event(
+                        query=q, ids=ids, event_type=Event.upsert
                     )
                     jobs.append(job_update)
 
                     q = d['query']
                     ids = d['ids']
-                    job_update = self.on_db_event(
+                    job_update = self.on_event(
                         query=q,
                         ids=ids,
-                        type='delete'
+                        event_type=Event.delete
                     )
                     jobs.append(job_update)
 
@@ -472,8 +485,8 @@ class Datalayer:
                 # Overwrite should be true since updates could be done on collections
                 # with already existing outputs.
                 # We need overwrite outputs on those select and recompute predict
-                return updated_ids, self.on_db_event(
-                    query=update, ids=updated_ids, overwrite=True, type='insert'
+                return updated_ids, self.on_event(
+                    query=update, ids=updated_ids, event_type=Event.upsert
                 )
         return updated_ids, None
 

superduperdb/components/listener.py
@@ -89,6 +89,8 @@ class Listener(Component):
 
         :param db: Data layer instance.
         """
+
+        from pinnacledb.base.datalayer import Event
         self.create_output_dest(db, self.uuid, self.model)
         if self.select is not None and self.active and not db.server_mode:
             if CFG.cluster.cdc.uri:
@@ -100,12 +102,24 @@ class Listener(Component):
                 )
             else:
                 db.cdc.add(self)
+
         db.compute.queue.declare_component(self)
+        
+        ids = db.execute(self.select.select_ids)
+        ids = [id[self.select.primary_id] for id in ids]
+        events = [{'identifier': id, 'type': Event.insert} for id in ids]
+        deps = [{'type_id': 'listener', 'identifier': self.identifier}]
+        return db.compute.broadcast(events, to=deps)
+
 
     def on_db_event(self, db, events):
-        ids = [event['identifier'] for event in events]
-        self.schedule_jobs(db=db, ids=ids)
+        from pinnacledb.base.datalayer import Event
 
+        for type, events in Event.chunk_by_event(events).items():
+            overwrite= True if type in [Event.insert, Event.upsert] else False
+
+            ids = [event['identifier'] for event in events]
+            self.schedule_jobs(db=db, ids=ids , overwrite=overwrite)
 
     @classmethod
     def create_output_dest(cls, db: "Datalayer", uuid, model: Model):

superduperdb/components/vector_index.py
@@ -172,17 +172,17 @@ class VectorIndex(Component):
 
     def on_db_event(self, db, events):
         from pinnacledb.vector_search.update_tasks import delete_vectors, copy_vectors
-        deleted_ids = []
-        inserted_ids = [  ]
-        for event in events:
-            if event['type'] == 'insert':
-                inserted_ids.append(event['identifier'])
+        from pinnacledb.base.datalayer import Event
+        def _schedule_jobs(ids, type=Event.insert):
+            if type in [Event.insert, Event.upsert]:
+
+                callable =  copy_vectors
+            elif type == Event.delete:
+                callable = delete_vectors
             else:
-                deleted_ids.append(event['identifier'])
-        
-        def _schedule_jobs(ids, type='insert'):
+                return
             job=FunctionJob(
-                callable=copy_vectors if type=='insert' else delete_vectors,
+                callable=callable,
                 args=[],
                 kwargs=dict(
                     vector_index=self.identifier,
@@ -191,8 +191,10 @@ class VectorIndex(Component):
                 ),
             )
             job(db=db)
-        _schedule_jobs(inserted_ids)
-        _schedule_jobs(deleted_ids, type='delete')
+        
+        for type, events in Event.chunk_by_event(events).items():
+            ids = [event['identifier'] for event in events]
+            _schedule_jobs(ids, type=type)
 
     @override
     def post_create(self, db: "Datalayer") -> None: