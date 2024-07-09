superduper-io/superduper

Add back integration test

test/integration/backends/mongodb/test_atlas.py
@@ -1,0 +1,97 @@
+import random
+import warnings
+
+import lorem
+import pymongo
+import pytest
+
+import superduperdb as s
+from superduperdb import CFG, superduper
+from superduperdb.backends.mongodb.query import MongoQuery
+from superduperdb.base.document import Document
+from superduperdb.components.listener import Listener
+from superduperdb.components.model import ObjectModel
+from superduperdb.components.vector_index import VectorIndex, vector
+
+try:
+    client = pymongo.MongoClient(CFG.data_backend)
+    build_info = client.admin.command('buildInfo')
+
+    if 'modules' in build_info and 'enterprise' in build_info['modules']:
+        DO_SKIP = False
+    else:
+        DO_SKIP = True
+
+    if CFG.cluster.vector_search.type != 'native':
+        DO_SKIP = True
+
+    if CFG.cluster.data_backend.type == 'native':
+        DO_SKIP = False
+
+except Exception as e:
+    warnings.warn(
+        f'Could not connect to MongoDB: {e} on {CFG.data_backend}; '
+        'skipping Atlas tests.'
+    )
+    DO_SKIP = True
+
+
+def random_vector_model(x):
+    return [random.random() for _ in range(16)]
+
+
+@pytest.fixture()
+def atlas_search_config():
+    previous = s.CFG.vector_search
+    s.CFG.vector_search = s.CFG.data_backend
+    yield
+    s.CFG.vector_search = previous
+
+
+@pytest.mark.skipif(DO_SKIP, reason='Only atlas deployments relevant.')
+def test_setup_atlas_vector_search(atlas_search_config):
+    model = ObjectModel(
+        identifier='test-model', object=random_vector_model, encoder=vector(shape=(16,))
+    )
+    db = superduper()
+    collection = MongoQuery(table='docs')
+
+    vector_indexes = db.data_backend.list_vector_indexes()
+
+    assert not vector_indexes
+
+    db.execute(
+        collection.insert_many(
+            [Document({'text': lorem.sentence()}) for _ in range(50)]
+        )
+    )
+    db.add(
+        VectorIndex(
+            'test-vector-index',
+            indexing_listener=Listener(
+                model=model,
+                key='text',
+                select=collection.find(),
+            ),
+        )
+    )
+
+    assert 'test-vector-index' in db.show('vector_index')
+    assert 'test-vector-index' in db.databackend.list_vector_indexes()
+
+
+@pytest.mark.skipif(DO_SKIP, reason='Only atlas deployments relevant.')
+def test_use_atlas_vector_search(atlas_search_config):
+    db = superduper()
+    collection = MongoQuery(table='docs')
+
+    query = collection.like(
+        Document({'text': 'This is a test'}), n=5, vector_index='test-vector-index'
+    ).find()
+
+    it = 0
+    for r in db.execute(query):
+        print(r)
+        it += 1
+
+    assert it == 5

test/integration/backends/mongodb/test_mongodb_artifacts.py
@@ -1,0 +1,85 @@
+import dataclasses as dc
+import filecmp
+import os
+import typing as t
+
+import pytest
+
+from superduperdb import CFG
+from superduperdb.components.component import Component
+from superduperdb.components.datatype import (
+    DataType,
+    file_serializer,
+)
+
+DO_SKIP = not CFG.data_backend.startswith("mongo")
+
+
+@dc.dataclass(kw_only=True)
+class TestComponent(Component):
+    path: str
+    type_id: t.ClassVar[str] = "TestComponent"
+
+    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, "DataType"]]] = (
+        ("path", file_serializer),
+    )
+
+
+@pytest.fixture
+def random_directory(tmpdir):
+    tmpdir_path = os.path.join(tmpdir, "test_data")
+    os.makedirs(tmpdir_path, exist_ok=True)
+    for i in range(10):
+        file_name = f'{i}.txt'
+        file_path = os.path.join(tmpdir_path, file_name)
+
+        with open(file_path, 'w') as file:
+            file.write(str(i))
+
+        for j in range(10):
+            sub_dir = os.path.join(tmpdir_path, f'subdir_{j}')
+            os.makedirs(sub_dir, exist_ok=True)
+            sub_file_path = os.path.join(sub_dir, file_name)
+            with open(sub_file_path, 'w') as file:
+                file.write(f"{i} {j}")
+
+    return tmpdir_path
+
+
+@pytest.mark.skipif(DO_SKIP, reason="skipping mongodb tests if not mongodb")
+def test_save_and_load_directory(test_db, random_directory):
+    # test save and load directory
+    test_component = TestComponent(path=random_directory, identifier="test")
+    test_db.apply(test_component)
+    test_component_loaded = test_db.load("TestComponent", "test")
+    test_component_loaded.init()
+    # assert that the paths are different
+    assert test_component.path != test_component_loaded.path
+    # assert that the directory names are the same
+    assert (
+        os.path.split(test_component.path)[-1]
+        == os.path.split(test_component_loaded.path)[-1]
+    )
+    # This goes wrong in the container for some reason
+    # assert that the directory sizes are the same
+    # assert os.path.getsize(test_component.path) == os.path.getsize(
+    #     test_component_loaded.path
+    # )
+
+
+def test_save_and_load_file(test_db):
+    # test save and load file
+    file = os.path.abspath(__file__)
+    test_component = TestComponent(path=file, identifier="test")
+    test_db.apply(test_component)
+    test_component_loaded = test_db.load("TestComponent", "test")
+    test_component_loaded.init()
+    # assert that the paths are different
+    assert test_component.path != test_component_loaded.path
+    # assert that the file names are the same
+    assert (
+        os.path.split(test_component.path)[-1]
+        == os.path.split(test_component_loaded.path)[-1]
+    )
+    # assert that the file sizes are the same
+    assert filecmp.cmp(test_component.path, test_component_loaded.path)

test/integration/conftest.py
@@ -1,0 +1,93 @@
+import random
+
+import numpy as np
+import pytest
+
+try:
+    import torch
+
+    from superduperdb.ext.torch.encoder import tensor
+    from superduperdb.ext.torch.model import TorchModel
+except ImportError:
+    torch = None
+
+from superduperdb.backends.mongodb.query import MongoQuery
+from superduperdb.base.document import Document
+from superduperdb.components.listener import Listener
+from superduperdb.components.vector_index import VectorIndex
+
+# Set the seeds
+random.seed(42)
+torch and torch.manual_seed(42)
+np.random.seed(42)
+
+
+def add_models_encoders(test_db):
+    test_db.add(tensor(torch.float, shape=(32,)))
+    _, dt_16 = test_db.add(tensor(torch.float, shape=(16,)))
+    _, model = test_db.add(
+        TorchModel(
+            object=torch.nn.Linear(32, 16),
+            identifier='model_linear_a',
+            datatype=dt_16,
+        )
+    )
+    _, indexing_listener = test_db.add(
+        Listener(
+            select=MongoQuery(table='documents').find(),
+            key='x',
+            model=model,
+        )
+    )
+    _, compatible_listener = test_db.add(
+        Listener(
+            select=MongoQuery(table='documents').find(),
+            key='z',
+            model=model,
+        )
+    )
+    vi = VectorIndex(
+        identifier='test_index',
+        indexing_listener=indexing_listener,
+        compatible_listener=compatible_listener,
+    )
+    test_db.add(vi)
+    return test_db
+
+
+@pytest.fixture
+def database_with_default_encoders_and_model(test_db):
+    yield add_models_encoders(test_db)
+
+
+@pytest.mark.skipif(not torch, reason='Torch not installed')
+def fake_tensor_data(encoder, update: bool = True):
+    data = []
+    for _ in range(10):
+        x = torch.randn(32)
+        y = int(random.random() > 0.5)
+        z = torch.randn(32)
+        data.append(
+            Document(
+                {
+                    'x': encoder(x),
+                    'y': y,
+                    'z': encoder(z),
+                    'update': update,
+                }
+            )
+        )
+
+    return data
+
+
+@pytest.fixture
+def fake_inserts(database_with_default_encoders_and_model):
+    dt = database_with_default_encoders_and_model.datatypes['torch.float32[32]']
+    return fake_tensor_data(dt, update=False)
+
+
+@pytest.fixture
+def fake_updates(database_with_default_encoders_and_model):
+    dt = database_with_default_encoders_and_model.datatypes['torch.float32[32]']
+    return fake_tensor_data(dt, update=True)

test/integration/ext/cohere/test_model_cohere.py
@@ -1,0 +1,67 @@
+import os
+
+import pytest
+import vcr
+
+from superduperdb.ext.cohere import CohereEmbed, CohereGenerate
+
+CASSETTE_DIR = 'test/integration/ext/cohere/cassettes'
+
+
+if os.getenv('COHERE_API_KEY') is None:
+    mp = pytest.MonkeyPatch()
+    mp.setenv('COHERE_API_KEY', 'sk-TopSecret')
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_embed_one.yaml',
+    filter_headers=['authorization'],
+)
+def test_embed_one():
+    embed = CohereEmbed(identifier='embed-english-v2.0')
+    resp = embed.predict('Hello world')
+
+    assert len(resp) == embed.shape[0]
+    assert isinstance(resp, list)
+    assert all(isinstance(x, float) for x in resp)
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_embed_batch.yaml',
+    filter_headers=['authorization'],
+)
+def test_embed_batch():
+    embed = CohereEmbed(identifier='embed-english-v2.0', batch_size=1)
+    resp = embed.predict_batches(['Hello', 'world'])
+
+    assert len(resp) == 2
+    assert len(resp[0]) == embed.shape[0]
+    assert isinstance(resp[0], list)
+    assert all(isinstance(x, float) for x in resp[0])
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_generate.yaml',
+    filter_headers=['authorization'],
+)
+def test_generate():
+    e = CohereGenerate(identifier='base-light', prompt='Hello, {context}')
+    resp = e.predict('', context=['world!'])
+
+    assert isinstance(resp, str)
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_batch_generate.yaml',
+    filter_headers=['authorization'],
+)
+def test_batch_generate():
+    e = CohereGenerate(identifier='base-light')
+    resp = e.predict_batches(
+        [
+            (('Hello, world!',), {}),
+        ]
+    )
+
+    assert isinstance(resp, list)
+    assert isinstance(resp[0], str)

test/integration/ext/jina/test_model_jina.py
@@ -1,0 +1,40 @@
+import os
+
+import pytest
+import vcr
+
+from superduperdb.ext.jina import JinaEmbedding
+
+CASSETTE_DIR = 'test/integration/ext/jina/cassettes'
+
+
+if os.getenv('JINA_API_KEY') is None:
+    mp = pytest.MonkeyPatch()
+    mp.setenv('JINA_API_KEY', 'sk-TopSecret')
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_embed_one.yaml',
+    filter_headers=['Authorization'],
+)
+def test_embed_one():
+    embed = JinaEmbedding(identifier='jina-embeddings-v2-base-en')
+    resp = embed.predict('Hello world')
+
+    assert len(resp) == embed.shape[0]
+    assert isinstance(resp, list)
+    assert all(isinstance(x, float) for x in resp)
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_embed_batch.yaml',
+    filter_headers=['Authorization'],
+)
+def test_embed_batch():
+    embed = JinaEmbedding(identifier='jina-embeddings-v2-base-en', batch_size=3)
+    resp = embed.predict_batches(['Hello', 'world', 'I', 'am', 'here'])
+
+    assert len(resp) == 5
+    assert len(resp[0]) == embed.shape[0]
+    assert isinstance(resp[0], list)
+    assert all(isinstance(x, float) for x in resp[0])

Pinnacle-Networks/Pinnacle

test/integration/backends/mongodb/test_atlas.py
@@ -1,0 +1,97 @@
+import random
+import warnings
+
+import lorem
+import pymongo
+import pytest
+
+import pinnacledb as s
+from pinnacledb import CFG, pinnacle
+from pinnacledb.backends.mongodb.query import MongoQuery
+from pinnacledb.base.document import Document
+from pinnacledb.components.listener import Listener
+from pinnacledb.components.model import ObjectModel
+from pinnacledb.components.vector_index import VectorIndex, vector
+
+try:
+    client = pymongo.MongoClient(CFG.data_backend)
+    build_info = client.admin.command('buildInfo')
+
+    if 'modules' in build_info and 'enterprise' in build_info['modules']:
+        DO_SKIP = False
+    else:
+        DO_SKIP = True
+
+    if CFG.cluster.vector_search.type != 'native':
+        DO_SKIP = True
+
+    if CFG.cluster.data_backend.type == 'native':
+        DO_SKIP = False
+
+except Exception as e:
+    warnings.warn(
+        f'Could not connect to MongoDB: {e} on {CFG.data_backend}; '
+        'skipping Atlas tests.'
+    )
+    DO_SKIP = True
+
+
+def random_vector_model(x):
+    return [random.random() for _ in range(16)]
+
+
+@pytest.fixture()
+def atlas_search_config():
+    previous = s.CFG.vector_search
+    s.CFG.vector_search = s.CFG.data_backend
+    yield
+    s.CFG.vector_search = previous
+
+
+@pytest.mark.skipif(DO_SKIP, reason='Only atlas deployments relevant.')
+def test_setup_atlas_vector_search(atlas_search_config):
+    model = ObjectModel(
+        identifier='test-model', object=random_vector_model, encoder=vector(shape=(16,))
+    )
+    db = pinnacle()
+    collection = MongoQuery(table='docs')
+
+    vector_indexes = db.data_backend.list_vector_indexes()
+
+    assert not vector_indexes
+
+    db.execute(
+        collection.insert_many(
+            [Document({'text': lorem.sentence()}) for _ in range(50)]
+        )
+    )
+    db.add(
+        VectorIndex(
+            'test-vector-index',
+            indexing_listener=Listener(
+                model=model,
+                key='text',
+                select=collection.find(),
+            ),
+        )
+    )
+
+    assert 'test-vector-index' in db.show('vector_index')
+    assert 'test-vector-index' in db.databackend.list_vector_indexes()
+
+
+@pytest.mark.skipif(DO_SKIP, reason='Only atlas deployments relevant.')
+def test_use_atlas_vector_search(atlas_search_config):
+    db = pinnacle()
+    collection = MongoQuery(table='docs')
+
+    query = collection.like(
+        Document({'text': 'This is a test'}), n=5, vector_index='test-vector-index'
+    ).find()
+
+    it = 0
+    for r in db.execute(query):
+        print(r)
+        it += 1
+
+    assert it == 5

test/integration/backends/mongodb/test_mongodb_artifacts.py
@@ -1,0 +1,85 @@
+import dataclasses as dc
+import filecmp
+import os
+import typing as t
+
+import pytest
+
+from pinnacledb import CFG
+from pinnacledb.components.component import Component
+from pinnacledb.components.datatype import (
+    DataType,
+    file_serializer,
+)
+
+DO_SKIP = not CFG.data_backend.startswith("mongo")
+
+
+@dc.dataclass(kw_only=True)
+class TestComponent(Component):
+    path: str
+    type_id: t.ClassVar[str] = "TestComponent"
+
+    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, "DataType"]]] = (
+        ("path", file_serializer),
+    )
+
+
+@pytest.fixture
+def random_directory(tmpdir):
+    tmpdir_path = os.path.join(tmpdir, "test_data")
+    os.makedirs(tmpdir_path, exist_ok=True)
+    for i in range(10):
+        file_name = f'{i}.txt'
+        file_path = os.path.join(tmpdir_path, file_name)
+
+        with open(file_path, 'w') as file:
+            file.write(str(i))
+
+        for j in range(10):
+            sub_dir = os.path.join(tmpdir_path, f'subdir_{j}')
+            os.makedirs(sub_dir, exist_ok=True)
+            sub_file_path = os.path.join(sub_dir, file_name)
+            with open(sub_file_path, 'w') as file:
+                file.write(f"{i} {j}")
+
+    return tmpdir_path
+
+
+@pytest.mark.skipif(DO_SKIP, reason="skipping mongodb tests if not mongodb")
+def test_save_and_load_directory(test_db, random_directory):
+    # test save and load directory
+    test_component = TestComponent(path=random_directory, identifier="test")
+    test_db.apply(test_component)
+    test_component_loaded = test_db.load("TestComponent", "test")
+    test_component_loaded.init()
+    # assert that the paths are different
+    assert test_component.path != test_component_loaded.path
+    # assert that the directory names are the same
+    assert (
+        os.path.split(test_component.path)[-1]
+        == os.path.split(test_component_loaded.path)[-1]
+    )
+    # This goes wrong in the container for some reason
+    # assert that the directory sizes are the same
+    # assert os.path.getsize(test_component.path) == os.path.getsize(
+    #     test_component_loaded.path
+    # )
+
+
+def test_save_and_load_file(test_db):
+    # test save and load file
+    file = os.path.abspath(__file__)
+    test_component = TestComponent(path=file, identifier="test")
+    test_db.apply(test_component)
+    test_component_loaded = test_db.load("TestComponent", "test")
+    test_component_loaded.init()
+    # assert that the paths are different
+    assert test_component.path != test_component_loaded.path
+    # assert that the file names are the same
+    assert (
+        os.path.split(test_component.path)[-1]
+        == os.path.split(test_component_loaded.path)[-1]
+    )
+    # assert that the file sizes are the same
+    assert filecmp.cmp(test_component.path, test_component_loaded.path)

test/integration/conftest.py
@@ -1,0 +1,93 @@
+import random
+
+import numpy as np
+import pytest
+
+try:
+    import torch
+
+    from pinnacledb.ext.torch.encoder import tensor
+    from pinnacledb.ext.torch.model import TorchModel
+except ImportError:
+    torch = None
+
+from pinnacledb.backends.mongodb.query import MongoQuery
+from pinnacledb.base.document import Document
+from pinnacledb.components.listener import Listener
+from pinnacledb.components.vector_index import VectorIndex
+
+# Set the seeds
+random.seed(42)
+torch and torch.manual_seed(42)
+np.random.seed(42)
+
+
+def add_models_encoders(test_db):
+    test_db.add(tensor(torch.float, shape=(32,)))
+    _, dt_16 = test_db.add(tensor(torch.float, shape=(16,)))
+    _, model = test_db.add(
+        TorchModel(
+            object=torch.nn.Linear(32, 16),
+            identifier='model_linear_a',
+            datatype=dt_16,
+        )
+    )
+    _, indexing_listener = test_db.add(
+        Listener(
+            select=MongoQuery(table='documents').find(),
+            key='x',
+            model=model,
+        )
+    )
+    _, compatible_listener = test_db.add(
+        Listener(
+            select=MongoQuery(table='documents').find(),
+            key='z',
+            model=model,
+        )
+    )
+    vi = VectorIndex(
+        identifier='test_index',
+        indexing_listener=indexing_listener,
+        compatible_listener=compatible_listener,
+    )
+    test_db.add(vi)
+    return test_db
+
+
+@pytest.fixture
+def database_with_default_encoders_and_model(test_db):
+    yield add_models_encoders(test_db)
+
+
+@pytest.mark.skipif(not torch, reason='Torch not installed')
+def fake_tensor_data(encoder, update: bool = True):
+    data = []
+    for _ in range(10):
+        x = torch.randn(32)
+        y = int(random.random() > 0.5)
+        z = torch.randn(32)
+        data.append(
+            Document(
+                {
+                    'x': encoder(x),
+                    'y': y,
+                    'z': encoder(z),
+                    'update': update,
+                }
+            )
+        )
+
+    return data
+
+
+@pytest.fixture
+def fake_inserts(database_with_default_encoders_and_model):
+    dt = database_with_default_encoders_and_model.datatypes['torch.float32[32]']
+    return fake_tensor_data(dt, update=False)
+
+
+@pytest.fixture
+def fake_updates(database_with_default_encoders_and_model):
+    dt = database_with_default_encoders_and_model.datatypes['torch.float32[32]']
+    return fake_tensor_data(dt, update=True)

test/integration/ext/cohere/test_model_cohere.py
@@ -1,0 +1,67 @@
+import os
+
+import pytest
+import vcr
+
+from pinnacledb.ext.cohere import CohereEmbed, CohereGenerate
+
+CASSETTE_DIR = 'test/integration/ext/cohere/cassettes'
+
+
+if os.getenv('COHERE_API_KEY') is None:
+    mp = pytest.MonkeyPatch()
+    mp.setenv('COHERE_API_KEY', 'sk-TopSecret')
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_embed_one.yaml',
+    filter_headers=['authorization'],
+)
+def test_embed_one():
+    embed = CohereEmbed(identifier='embed-english-v2.0')
+    resp = embed.predict('Hello world')
+
+    assert len(resp) == embed.shape[0]
+    assert isinstance(resp, list)
+    assert all(isinstance(x, float) for x in resp)
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_embed_batch.yaml',
+    filter_headers=['authorization'],
+)
+def test_embed_batch():
+    embed = CohereEmbed(identifier='embed-english-v2.0', batch_size=1)
+    resp = embed.predict_batches(['Hello', 'world'])
+
+    assert len(resp) == 2
+    assert len(resp[0]) == embed.shape[0]
+    assert isinstance(resp[0], list)
+    assert all(isinstance(x, float) for x in resp[0])
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_generate.yaml',
+    filter_headers=['authorization'],
+)
+def test_generate():
+    e = CohereGenerate(identifier='base-light', prompt='Hello, {context}')
+    resp = e.predict('', context=['world!'])
+
+    assert isinstance(resp, str)
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_batch_generate.yaml',
+    filter_headers=['authorization'],
+)
+def test_batch_generate():
+    e = CohereGenerate(identifier='base-light')
+    resp = e.predict_batches(
+        [
+            (('Hello, world!',), {}),
+        ]
+    )
+
+    assert isinstance(resp, list)
+    assert isinstance(resp[0], str)

test/integration/ext/jina/test_model_jina.py
@@ -1,0 +1,40 @@
+import os
+
+import pytest
+import vcr
+
+from pinnacledb.ext.jina import JinaEmbedding
+
+CASSETTE_DIR = 'test/integration/ext/jina/cassettes'
+
+
+if os.getenv('JINA_API_KEY') is None:
+    mp = pytest.MonkeyPatch()
+    mp.setenv('JINA_API_KEY', 'sk-TopSecret')
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_embed_one.yaml',
+    filter_headers=['Authorization'],
+)
+def test_embed_one():
+    embed = JinaEmbedding(identifier='jina-embeddings-v2-base-en')
+    resp = embed.predict('Hello world')
+
+    assert len(resp) == embed.shape[0]
+    assert isinstance(resp, list)
+    assert all(isinstance(x, float) for x in resp)
+
+
+@vcr.use_cassette(
+    f'{CASSETTE_DIR}/test_embed_batch.yaml',
+    filter_headers=['Authorization'],
+)
+def test_embed_batch():
+    embed = JinaEmbedding(identifier='jina-embeddings-v2-base-en', batch_size=3)
+    resp = embed.predict_batches(['Hello', 'world', 'I', 'am', 'here'])
+
+    assert len(resp) == 5
+    assert len(resp[0]) == embed.shape[0]
+    assert isinstance(resp[0], list)
+    assert all(isinstance(x, float) for x in resp[0])