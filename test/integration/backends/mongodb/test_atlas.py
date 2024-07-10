superduper-io/superduper

Rename superduperdb to superduper

test/integration/backends/mongodb/test_atlas.py
@@ -5,13 +5,13 @@ import lorem
 import pymongo
 import pytest
 
-import superduperdb as s
-from superduperdb import CFG, superduper
-from superduperdb.backends.mongodb.query import MongoQuery
-from superduperdb.base.document import Document
-from superduperdb.components.listener import Listener
-from superduperdb.components.model import ObjectModel
-from superduperdb.components.vector_index import VectorIndex, vector
+import superduper as s
+from superduper import CFG, superduper
+from superduper.backends.mongodb.query import MongoQuery
+from superduper.base.document import Document
+from superduper.components.listener import Listener
+from superduper.components.model import ObjectModel
+from superduper.components.vector_index import VectorIndex, vector
 
 try:
     client = pymongo.MongoClient(CFG.data_backend)

test/integration/backends/mongodb/test_mongodb_artifacts.py
@@ -5,9 +5,9 @@ import typing as t
 
 import pytest
 
-from superduperdb import CFG
-from superduperdb.components.component import Component
-from superduperdb.components.datatype import (
+from superduper import CFG
+from superduper.components.component import Component
+from superduper.components.datatype import (
     DataType,
     file_serializer,
 )

test/unittest/backends/local/test_artifacts.py
@@ -6,9 +6,9 @@ from test.db_config import DBConfig
 
 import pytest
 
-from superduperdb.backends.local.artifacts import FileSystemArtifactStore
-from superduperdb.components.component import Component
-from superduperdb.components.datatype import (
+from superduper.backends.local.artifacts import FileSystemArtifactStore
+from superduper.components.component import Component
+from superduper.components.datatype import (
     DataType,
     file_lazy,
     serializers,

test/unittest/backends/mongodb/test_queries.py
@@ -8,9 +8,9 @@ except ImportError:
 import random
 from test.db_config import DBConfig
 
-from superduperdb.backends.mongodb.query import MongoQuery
-from superduperdb.base.document import Document
-from superduperdb.components.datatype import DataType
+from superduper.backends.mongodb.query import MongoQuery
+from superduper.base.document import Document
+from superduper.components.datatype import DataType
 
 
 def get_new_data(encoder: DataType, n=10, update=False):
@@ -69,7 +69,7 @@ def test_replace(db):
 def test_insert_from_uris(db, image_url):
     import PIL
 
-    from superduperdb.ext.pillow.encoder import pil_image
+    from superduper.ext.pillow.encoder import pil_image
 
     if image_url.startswith('file://'):
         image_url = image_url[7:]
@@ -89,8 +89,8 @@ def test_insert_from_uris(db, image_url):
 def test_insert_from_uris_bytes_encoding(db, image_url):
     import PIL
 
-    from superduperdb.base.config import BytesEncoding
-    from superduperdb.ext.pillow.encoder import pil_image
+    from superduper.base.config import BytesEncoding
+    from superduper.ext.pillow.encoder import pil_image
 
     my_pil_image = DataType(
         'my_pil_image',

test/unittest/component/datatype/test_pickle.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from superduperdb.base.enums import DBType
-from superduperdb.components.datatype import (
+from superduper.base.enums import DBType
+from superduper.components.datatype import (
     DataType,
     pickle_encoder,
     pickle_lazy,

test/unittest/component/test_component.py
@@ -7,17 +7,17 @@ from test.db_config import DBConfig
 
 import pytest
 
-from superduperdb import ObjectModel
-from superduperdb.components.component import Component
-from superduperdb.components.datatype import (
+from superduper import ObjectModel
+from superduper.components.component import Component
+from superduper.components.datatype import (
     Artifact,
     DataType,
     Empty,
     LazyArtifact,
     dill_serializer,
 )
-from superduperdb.components.listener import Listener
-from superduperdb.ext.torch.encoder import tensor
+from superduper.components.listener import Listener
+from superduper.ext.torch.encoder import tensor
 
 
 @pytest.fixture
@@ -114,7 +114,7 @@ def test_set_variables(db):
         select=db['docs'].find(),
     )
 
-    from superduperdb import Document
+    from superduper import Document
 
     e = m.encode()
     recon = Document.decode(e).unpack()

Pinnacle-Networks/Pinnacle

test/integration/backends/mongodb/test_atlas.py
@@ -5,13 +5,13 @@ import lorem
 import pymongo
 import pytest
 
-import pinnacledb as s
-from pinnacledb import CFG, pinnacle
-from pinnacledb.backends.mongodb.query import MongoQuery
-from pinnacledb.base.document import Document
-from pinnacledb.components.listener import Listener
-from pinnacledb.components.model import ObjectModel
-from pinnacledb.components.vector_index import VectorIndex, vector
+import pinnacle as s
+from pinnacle import CFG, pinnacle
+from pinnacle.backends.mongodb.query import MongoQuery
+from pinnacle.base.document import Document
+from pinnacle.components.listener import Listener
+from pinnacle.components.model import ObjectModel
+from pinnacle.components.vector_index import VectorIndex, vector
 
 try:
     client = pymongo.MongoClient(CFG.data_backend)

test/integration/backends/mongodb/test_mongodb_artifacts.py
@@ -5,9 +5,9 @@ import typing as t
 
 import pytest
 
-from pinnacledb import CFG
-from pinnacledb.components.component import Component
-from pinnacledb.components.datatype import (
+from pinnacle import CFG
+from pinnacle.components.component import Component
+from pinnacle.components.datatype import (
     DataType,
     file_serializer,
 )

test/unittest/backends/local/test_artifacts.py
@@ -6,9 +6,9 @@ from test.db_config import DBConfig
 
 import pytest
 
-from pinnacledb.backends.local.artifacts import FileSystemArtifactStore
-from pinnacledb.components.component import Component
-from pinnacledb.components.datatype import (
+from pinnacle.backends.local.artifacts import FileSystemArtifactStore
+from pinnacle.components.component import Component
+from pinnacle.components.datatype import (
     DataType,
     file_lazy,
     serializers,

test/unittest/backends/mongodb/test_queries.py
@@ -8,9 +8,9 @@ except ImportError:
 import random
 from test.db_config import DBConfig
 
-from pinnacledb.backends.mongodb.query import MongoQuery
-from pinnacledb.base.document import Document
-from pinnacledb.components.datatype import DataType
+from pinnacle.backends.mongodb.query import MongoQuery
+from pinnacle.base.document import Document
+from pinnacle.components.datatype import DataType
 
 
 def get_new_data(encoder: DataType, n=10, update=False):
@@ -69,7 +69,7 @@ def test_replace(db):
 def test_insert_from_uris(db, image_url):
     import PIL
 
-    from pinnacledb.ext.pillow.encoder import pil_image
+    from pinnacle.ext.pillow.encoder import pil_image
 
     if image_url.startswith('file://'):
         image_url = image_url[7:]
@@ -89,8 +89,8 @@ def test_insert_from_uris(db, image_url):
 def test_insert_from_uris_bytes_encoding(db, image_url):
     import PIL
 
-    from pinnacledb.base.config import BytesEncoding
-    from pinnacledb.ext.pillow.encoder import pil_image
+    from pinnacle.base.config import BytesEncoding
+    from pinnacle.ext.pillow.encoder import pil_image
 
     my_pil_image = DataType(
         'my_pil_image',

test/unittest/component/datatype/test_pickle.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from pinnacledb.base.enums import DBType
-from pinnacledb.components.datatype import (
+from pinnacle.base.enums import DBType
+from pinnacle.components.datatype import (
     DataType,
     pickle_encoder,
     pickle_lazy,

test/unittest/component/test_component.py
@@ -7,17 +7,17 @@ from test.db_config import DBConfig
 
 import pytest
 
-from pinnacledb import ObjectModel
-from pinnacledb.components.component import Component
-from pinnacledb.components.datatype import (
+from pinnacle import ObjectModel
+from pinnacle.components.component import Component
+from pinnacle.components.datatype import (
     Artifact,
     DataType,
     Empty,
     LazyArtifact,
     dill_serializer,
 )
-from pinnacledb.components.listener import Listener
-from pinnacledb.ext.torch.encoder import tensor
+from pinnacle.components.listener import Listener
+from pinnacle.ext.torch.encoder import tensor
 
 
 @pytest.fixture
@@ -114,7 +114,7 @@ def test_set_variables(db):
         select=db['docs'].find(),
     )
 
-    from pinnacledb import Document
+    from pinnacle import Document
 
     e = m.encode()
     recon = Document.decode(e).unpack()