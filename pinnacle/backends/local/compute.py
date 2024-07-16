superduper-io/superduper

Make queue pluggable config

superduper/backends/local/compute.py
@@ -3,19 +3,25 @@ import uuid
 
 from superduper import logging
 from superduper.backends.base.compute import ComputeBackend
-from superduper.jobs.queue import LocalSequentialQueue
+from superduper.jobs.queue import BaseQueuePublisher, LocalQueuePublisher
 
 
 class LocalComputeBackend(ComputeBackend):
     """
     A mockup backend for running jobs locally.
 
-    :param _uri: Optional uri param.
+    :param uri: Optional uri param.
+    :param queue: Optional pluggable queue.
     """
 
-    def __init__(self, _uri: t.Optional[str] = None):
+    def __init__(
+        self,
+        uri: t.Optional[str] = None,
+        queue: BaseQueuePublisher = LocalQueuePublisher(),
+    ):
         self.__outputs: t.Dict = {}
-        self.queue = LocalSequentialQueue()
+        self.uri = uri
+        self.queue = queue
 
     @property
     def remote(self) -> bool:

superduper/base/build.py
@@ -1,3 +1,4 @@
+import importlib
 import re
 import typing as t
 
@@ -132,23 +133,28 @@ def _build_databackend_impl(uri, mapping, type: str = 'data_backend'):
     return db
 
 
-def build_compute(compute):
+def build_compute(cfg):
     """
     Helper function to build compute backend.
 
-    :param compute: Compute uri.
+    :param cfg: SuperDuper config.
     """
+
+    def _local_component(path, **kwargs):
+        spath = path.split('.')
+        path, cls = '.'.join(spath[:-1]), spath[-1]
+        module = importlib.import_module(path)
+        component_cls = getattr(module, cls)
+        return component_cls(**kwargs)
+
+    compute = cfg.cluster.compute
+    queue = cfg.cluster.queue
     logging.info("Connecting to compute client:", compute)
     path = compute._path or 'superduper.backends.local.compute.LocalComputeBackend'
-    spath = path.split('.')
-    path, cls = '.'.join(spath[:-1]), spath[-1]
+    queue_path = queue._path or 'superduper.jobs.queue.LocalSequentialQueue'
+    queue = _local_component(queue_path, uri=queue.uri)
 
-    import importlib
-
-    module = importlib.import_module(path)
-    compute_cls = getattr(module, cls)
-
-    return compute_cls(compute.uri)
+    return _local_component(path, uri=compute.uri, queue=queue)
 
 
 def build_datalayer(cfg=None, databackend=None, **kwargs) -> Datalayer:
@@ -170,7 +176,7 @@ def build_datalayer(cfg=None, databackend=None, **kwargs) -> Datalayer:
     assert metadata
 
     artifact_store = _build_artifact_store(cfg.artifact_store, databackend)
-    compute = build_compute(cfg.cluster.compute)
+    compute = build_compute(cfg)
 
     datalayer = Datalayer(
         databackend=databackend,

superduper/base/config.py
@@ -167,16 +167,16 @@ class SchedulerConfig(BaseConfig):
 
 
 @dc.dataclass
-class BrokerConfig(BaseConfig):
+class QueueConfig(BaseConfig):
     """Describes the configuration for message broker service.
 
     :param uri: The URI for the scheduler service.
-    :param broker: Message broker currently supporting
+    :param _path: The pluggable message broker currently supporting
                    naive, rabbitmq, etc.
     """
 
     uri: t.Optional[str] = None  # None implies local mode
-    broker: t.Optional[str] = 'naive'
+    _path: t.Optional[str] = 'superduper.jobs.queue.LocalQueuePublisher'
 
 
 @dc.dataclass
@@ -213,7 +213,7 @@ class Cluster(BaseConfig):
                 None: Run cdc on local as a thread.
                 - `f"{http://{host}:{port}"`: Connect a remote cdc service
     :param scheduler: The URI for the scheduler service
-    :param broker: The URI for the message broker service
+    :param queue: The URI for the message broker service
     """
 
     compute: Compute = dc.field(default_factory=Compute)
@@ -222,7 +222,7 @@ class Cluster(BaseConfig):
     rest: Rest = dc.field(default_factory=Rest)
     cdc: CDCConfig = dc.field(default_factory=CDCConfig)
     scheduler: SchedulerConfig = dc.field(default_factory=SchedulerConfig)
-    broker: BrokerConfig = dc.field(default_factory=BrokerConfig)
+    queue: QueueConfig = dc.field(default_factory=QueueConfig)
 
 
 class LogLevel(str, Enum):

superduper/jobs/queue.py
@@ -1,29 +1,74 @@
 import typing as t
+from abc import ABC, abstractmethod
 
 from superduper import logging
 
 DependencyType = t.Union[t.Dict[str, str], t.Sequence[t.Dict[str, str]]]
 
+if t.TYPE_CHECKING:
+    from superduper.base.datalayer import Datalayer
 
-class LocalSequentialQueue:
+
+class BaseQueueConsumer(ABC):
     """
-    LocalSequentialQueue for handling publisher and consumer process.
+    Base class for handling consumer process.
 
-    Local queue which holds listeners, vector indices as queue which
-    consists of events to be consumed by the corresponding components.
+    This class is an implementation of message broker between
+    producers (superduper db client) and consumers i.e listeners.
+
+    :param uri: Uri to connect.
+    :param queue_name: Queue to consume.
+    :param callback: Callback for consumed messages.
     """
 
-    def __init__(self):
-        self.queue = {}
-        self.components = {}
+    def __init__(
+        self,
+        uri: t.Optional[str] = '',
+        queue_name: str = '',
+        callback: t.Optional[t.Callable] = None,
+    ):
+        self.uri = uri
+        self.callback = callback
+        self.queue_name = queue_name
+
+    @abstractmethod
+    def start_consuming(self):
+        """Abstract method to start consuming messages."""
+        pass
+
+    @abstractmethod
+    def close_connection(self):
+        """Abstract method to close connection."""
+        pass
+
+    def consume(self, *args, **kwargs):
+        """Start consuming messages from queue."""
+        logging.info(f"Started consuming on queue: {self.queue_name}")
+        try:
+            self.start_consuming()
+        except KeyboardInterrupt:
+            logging.info("KeyboardInterrupt: Stopping consumer...")
+        finally:
+            self.close_connection()
+            logging.info(f"Stopped consuming on queue: {self.queue_name}")
+
+
+class BaseQueuePublisher(ABC):
+    """
+    Base class for handling publisher and consumer process.
+
+    This class is an implementation of message broker between
+    producers (superduper db client) and consumers i.e listeners.
+
+    :param uri: Uri to connect.
+    """
+
+    def __init__(self, uri: t.Optional[str]):
+        self.queue: t.Dict = {}
+        self.components: t.Dict = {}
+        self.uri: t.Optional[str] = uri
         self._db = None
-        self._component_map = {}
-
-    def declare_component(self, component):
-        """Declare component and add it to queue."""
-        identifier = f'{component.type_id}.{component.identifier}'
-        self.queue[identifier] = []
-        self.components[identifier] = component
+        self._component_map: t.Dict = {}
 
     @property
     def db(self):
@@ -33,6 +78,48 @@ class LocalSequentialQueue:
     @db.setter
     def db(self, db):
         self._db = db
+
+    @abstractmethod
+    def build_consumer(self, **kwargs):
+        """Build a consumer instance."""
+
+    @abstractmethod
+    def publish(self, events: t.List[t.Dict], to: DependencyType):
+        """
+        Publish events to local queue.
+
+        :param events: list of events
+        :param to: Component name for events to be published.
+        """
+
+    @abstractmethod
+    def declare_component(self, component):
+        """Declare component and add it to queue."""
+
+
+class LocalQueuePublisher(BaseQueuePublisher):
+    """
+    LocalQueuePublisher for handling publisher and consumer process.
+
+    Local queue which holds listeners, vector indices as queue which
+    consists of events to be consumed by the corresponding components.
+
+    :param uri: uri to connect.
+    """
+
+    def __init__(self, uri: t.Optional[str] = None):
+        super().__init__(uri=uri)
+        self.consumer = self.build_consumer()
+
+    def build_consumer(self):
+        """Build consumer client."""
+        return LocalQueueConsumer()
+
+    def declare_component(self, component):
+        """Declare component and add it to queue."""
+        identifier = f'{component.type_id}.{component.identifier}'
+        self.queue[identifier] = []
+        self.components[identifier] = component
 
     def publish(self, events: t.List[t.Dict], to: DependencyType):
         """
@@ -63,20 +150,34 @@ class LocalSequentialQueue:
                 _publish(events, dep)
         else:
             _publish(events, to)
-        return self.consume()
+        return self.consumer.consume(
+            db=self.db, queue=self.queue, components=self.components
+        )
 
-    def consume(self):
+
+class LocalQueueConsumer(BaseQueueConsumer):
+    """LocalQueueConsumer for consuming message from queue.
+
+    :param uri: Uri to connect.
+    :param queue_name: Queue to consume.
+    :param callback: Callback for consumed messages.
+    """
+
+    def start_consuming(self):
+        """Start consuming."""
+
+    def consume(self, db: 'Datalayer', queue: t.Dict, components: t.Dict):
         """Consume the current queue and run jobs."""
         from superduper.base.datalayer import Event
 
-        queue_jobs = {}
-        for component_id in self.queue:
-            events = self.queue[component_id]
+        queue_jobs: t.Dict[str, t.List] = {}
+        for component_id in queue:
+            events = queue[component_id]
             if not events:
                 continue
-            self.queue[component_id] = []
+            queue[component_id] = []
 
-            component = self.components[component_id]
+            component = components[component_id]
             jobs = []
 
             for event_type, type_events in Event.chunk_by_event(events).items():
@@ -86,7 +187,7 @@ class LocalSequentialQueue:
                 )
                 logging.info(f'Running jobs for {component_id} with ids: {ids}')
                 job = component.run_jobs(
-                    db=self.db, ids=ids, overwrite=overwrite, event_type=event_type
+                    db=db, ids=ids, overwrite=overwrite, event_type=event_type
                 )
                 jobs.append(job)
 
@@ -96,3 +197,6 @@ class LocalSequentialQueue:
                 queue_jobs[component_id] = jobs
 
         return queue_jobs
+
+    def close_connection(self):
+        """Close connection to queue."""

Pinnacle-Networks/Pinnacle

pinnacle/backends/local/compute.py
@@ -3,19 +3,25 @@ import uuid
 
 from pinnacle import logging
 from pinnacle.backends.base.compute import ComputeBackend
-from pinnacle.jobs.queue import LocalSequentialQueue
+from pinnacle.jobs.queue import BaseQueuePublisher, LocalQueuePublisher
 
 
 class LocalComputeBackend(ComputeBackend):
     """
     A mockup backend for running jobs locally.
 
-    :param _uri: Optional uri param.
+    :param uri: Optional uri param.
+    :param queue: Optional pluggable queue.
     """
 
-    def __init__(self, _uri: t.Optional[str] = None):
+    def __init__(
+        self,
+        uri: t.Optional[str] = None,
+        queue: BaseQueuePublisher = LocalQueuePublisher(),
+    ):
         self.__outputs: t.Dict = {}
-        self.queue = LocalSequentialQueue()
+        self.uri = uri
+        self.queue = queue
 
     @property
     def remote(self) -> bool:

pinnacle/base/build.py
@@ -1,3 +1,4 @@
+import importlib
 import re
 import typing as t
 
@@ -132,23 +133,28 @@ def _build_databackend_impl(uri, mapping, type: str = 'data_backend'):
     return db
 
 
-def build_compute(compute):
+def build_compute(cfg):
     """
     Helper function to build compute backend.
 
-    :param compute: Compute uri.
+    :param cfg: SuperDuper config.
     """
+
+    def _local_component(path, **kwargs):
+        spath = path.split('.')
+        path, cls = '.'.join(spath[:-1]), spath[-1]
+        module = importlib.import_module(path)
+        component_cls = getattr(module, cls)
+        return component_cls(**kwargs)
+
+    compute = cfg.cluster.compute
+    queue = cfg.cluster.queue
     logging.info("Connecting to compute client:", compute)
     path = compute._path or 'pinnacle.backends.local.compute.LocalComputeBackend'
-    spath = path.split('.')
-    path, cls = '.'.join(spath[:-1]), spath[-1]
+    queue_path = queue._path or 'pinnacle.jobs.queue.LocalSequentialQueue'
+    queue = _local_component(queue_path, uri=queue.uri)
 
-    import importlib
-
-    module = importlib.import_module(path)
-    compute_cls = getattr(module, cls)
-
-    return compute_cls(compute.uri)
+    return _local_component(path, uri=compute.uri, queue=queue)
 
 
 def build_datalayer(cfg=None, databackend=None, **kwargs) -> Datalayer:
@@ -170,7 +176,7 @@ def build_datalayer(cfg=None, databackend=None, **kwargs) -> Datalayer:
     assert metadata
 
     artifact_store = _build_artifact_store(cfg.artifact_store, databackend)
-    compute = build_compute(cfg.cluster.compute)
+    compute = build_compute(cfg)
 
     datalayer = Datalayer(
         databackend=databackend,

pinnacle/base/config.py
@@ -167,16 +167,16 @@ class SchedulerConfig(BaseConfig):
 
 
 @dc.dataclass
-class BrokerConfig(BaseConfig):
+class QueueConfig(BaseConfig):
     """Describes the configuration for message broker service.
 
     :param uri: The URI for the scheduler service.
-    :param broker: Message broker currently supporting
+    :param _path: The pluggable message broker currently supporting
                    naive, rabbitmq, etc.
     """
 
     uri: t.Optional[str] = None  # None implies local mode
-    broker: t.Optional[str] = 'naive'
+    _path: t.Optional[str] = 'pinnacle.jobs.queue.LocalQueuePublisher'
 
 
 @dc.dataclass
@@ -213,7 +213,7 @@ class Cluster(BaseConfig):
                 None: Run cdc on local as a thread.
                 - `f"{http://{host}:{port}"`: Connect a remote cdc service
     :param scheduler: The URI for the scheduler service
-    :param broker: The URI for the message broker service
+    :param queue: The URI for the message broker service
     """
 
     compute: Compute = dc.field(default_factory=Compute)
@@ -222,7 +222,7 @@ class Cluster(BaseConfig):
     rest: Rest = dc.field(default_factory=Rest)
     cdc: CDCConfig = dc.field(default_factory=CDCConfig)
     scheduler: SchedulerConfig = dc.field(default_factory=SchedulerConfig)
-    broker: BrokerConfig = dc.field(default_factory=BrokerConfig)
+    queue: QueueConfig = dc.field(default_factory=QueueConfig)
 
 
 class LogLevel(str, Enum):

pinnacle/jobs/queue.py
@@ -1,29 +1,74 @@
 import typing as t
+from abc import ABC, abstractmethod
 
 from pinnacle import logging
 
 DependencyType = t.Union[t.Dict[str, str], t.Sequence[t.Dict[str, str]]]
 
+if t.TYPE_CHECKING:
+    from pinnacle.base.datalayer import Datalayer
 
-class LocalSequentialQueue:
+
+class BaseQueueConsumer(ABC):
     """
-    LocalSequentialQueue for handling publisher and consumer process.
+    Base class for handling consumer process.
 
-    Local queue which holds listeners, vector indices as queue which
-    consists of events to be consumed by the corresponding components.
+    This class is an implementation of message broker between
+    producers (pinnacle db client) and consumers i.e listeners.
+
+    :param uri: Uri to connect.
+    :param queue_name: Queue to consume.
+    :param callback: Callback for consumed messages.
     """
 
-    def __init__(self):
-        self.queue = {}
-        self.components = {}
+    def __init__(
+        self,
+        uri: t.Optional[str] = '',
+        queue_name: str = '',
+        callback: t.Optional[t.Callable] = None,
+    ):
+        self.uri = uri
+        self.callback = callback
+        self.queue_name = queue_name
+
+    @abstractmethod
+    def start_consuming(self):
+        """Abstract method to start consuming messages."""
+        pass
+
+    @abstractmethod
+    def close_connection(self):
+        """Abstract method to close connection."""
+        pass
+
+    def consume(self, *args, **kwargs):
+        """Start consuming messages from queue."""
+        logging.info(f"Started consuming on queue: {self.queue_name}")
+        try:
+            self.start_consuming()
+        except KeyboardInterrupt:
+            logging.info("KeyboardInterrupt: Stopping consumer...")
+        finally:
+            self.close_connection()
+            logging.info(f"Stopped consuming on queue: {self.queue_name}")
+
+
+class BaseQueuePublisher(ABC):
+    """
+    Base class for handling publisher and consumer process.
+
+    This class is an implementation of message broker between
+    producers (pinnacle db client) and consumers i.e listeners.
+
+    :param uri: Uri to connect.
+    """
+
+    def __init__(self, uri: t.Optional[str]):
+        self.queue: t.Dict = {}
+        self.components: t.Dict = {}
+        self.uri: t.Optional[str] = uri
         self._db = None
-        self._component_map = {}
-
-    def declare_component(self, component):
-        """Declare component and add it to queue."""
-        identifier = f'{component.type_id}.{component.identifier}'
-        self.queue[identifier] = []
-        self.components[identifier] = component
+        self._component_map: t.Dict = {}
 
     @property
     def db(self):
@@ -33,6 +78,48 @@ class LocalSequentialQueue:
     @db.setter
     def db(self, db):
         self._db = db
+
+    @abstractmethod
+    def build_consumer(self, **kwargs):
+        """Build a consumer instance."""
+
+    @abstractmethod
+    def publish(self, events: t.List[t.Dict], to: DependencyType):
+        """
+        Publish events to local queue.
+
+        :param events: list of events
+        :param to: Component name for events to be published.
+        """
+
+    @abstractmethod
+    def declare_component(self, component):
+        """Declare component and add it to queue."""
+
+
+class LocalQueuePublisher(BaseQueuePublisher):
+    """
+    LocalQueuePublisher for handling publisher and consumer process.
+
+    Local queue which holds listeners, vector indices as queue which
+    consists of events to be consumed by the corresponding components.
+
+    :param uri: uri to connect.
+    """
+
+    def __init__(self, uri: t.Optional[str] = None):
+        super().__init__(uri=uri)
+        self.consumer = self.build_consumer()
+
+    def build_consumer(self):
+        """Build consumer client."""
+        return LocalQueueConsumer()
+
+    def declare_component(self, component):
+        """Declare component and add it to queue."""
+        identifier = f'{component.type_id}.{component.identifier}'
+        self.queue[identifier] = []
+        self.components[identifier] = component
 
     def publish(self, events: t.List[t.Dict], to: DependencyType):
         """
@@ -63,20 +150,34 @@ class LocalSequentialQueue:
                 _publish(events, dep)
         else:
             _publish(events, to)
-        return self.consume()
+        return self.consumer.consume(
+            db=self.db, queue=self.queue, components=self.components
+        )
 
-    def consume(self):
+
+class LocalQueueConsumer(BaseQueueConsumer):
+    """LocalQueueConsumer for consuming message from queue.
+
+    :param uri: Uri to connect.
+    :param queue_name: Queue to consume.
+    :param callback: Callback for consumed messages.
+    """
+
+    def start_consuming(self):
+        """Start consuming."""
+
+    def consume(self, db: 'Datalayer', queue: t.Dict, components: t.Dict):
         """Consume the current queue and run jobs."""
         from pinnacle.base.datalayer import Event
 
-        queue_jobs = {}
-        for component_id in self.queue:
-            events = self.queue[component_id]
+        queue_jobs: t.Dict[str, t.List] = {}
+        for component_id in queue:
+            events = queue[component_id]
             if not events:
                 continue
-            self.queue[component_id] = []
+            queue[component_id] = []
 
-            component = self.components[component_id]
+            component = components[component_id]
             jobs = []
 
             for event_type, type_events in Event.chunk_by_event(events).items():
@@ -86,7 +187,7 @@ class LocalSequentialQueue:
                 )
                 logging.info(f'Running jobs for {component_id} with ids: {ids}')
                 job = component.run_jobs(
-                    db=self.db, ids=ids, overwrite=overwrite, event_type=event_type
+                    db=db, ids=ids, overwrite=overwrite, event_type=event_type
                 )
                 jobs.append(job)
 
@@ -96,3 +197,6 @@ class LocalSequentialQueue:
                 queue_jobs[component_id] = jobs
 
         return queue_jobs
+
+    def close_connection(self):
+        """Close connection to queue."""