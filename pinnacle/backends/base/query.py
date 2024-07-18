superduper-io/superduper

Add small changes to enable query-templates

superduper/backends/base/query.py
@@ -5,7 +5,7 @@ import uuid
 from abc import abstractmethod
 from functools import wraps
 
-from superduper.base.document import Document
+from superduper.base.document import Document, _unpack
 from superduper.base.leaf import Leaf
 
 if t.TYPE_CHECKING:
@@ -45,6 +45,15 @@ class _BaseQuery(Leaf):
         self._updated_key = None
         if not self.identifier:
             self.identifier = self._build_hr_identifier()
+
+    def unpack(self):
+        parts = _unpack(self.parts)
+        return type(self)(
+            db=self.db,
+            table=self.table,
+            parts=parts,
+            identifier=self.identifier,
+        )
 
     def _build_hr_identifier(self):
         identifier = str(self).split('\n')[-1]
@@ -649,7 +658,16 @@ class Model(_BaseQuery):
         m = self.db.models[self.table]
         method = getattr(m, self.parts[-1][0])
         r = method(*self.parts[-1][1], **self.parts[-1][2])
-        return Document({'_base': r})
+        if self.flatten:
+            return [
+                Document({'_base': res}) if not isinstance(res, dict) else Document(res) 
+                for res in r
+            ]
+        else:
+            if isinstance(r, dict):
+                return Document(r)
+            else:
+                return Document({'_base': r})
 
     def dict(self, metadata: bool = True, defaults: bool = True):
         """Return the query as a dictionary."""

test/unittest/component/datatype/test_init.py
@@ -1,0 +1,49 @@
+from superduper.base.document import Document
+import pytest
+
+from test.db_config import DBConfig
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_load_image_inside_query(db, clean):
+    r = {
+        '_path': 'superduper.backends.base.query.parse_query', 
+        'query': 'image-search.predict(documents[0]["img"])',
+        'documents': [
+            {'img': '?32b6853dd2b2b45de723966dba17e23cece9f35c'}
+        ],
+        '_builds': {
+            'jpg': {
+                '_path': 'superduper.ext.pillow.encoder.image_type',
+                'encodable': 'artifact',
+                'media_type': 'image/jpeg',
+                'type_id': 'datatype',
+            },
+            '32b6853dd2b2b45de723966dba17e23cece9f35c': {
+                '_path': 'superduper.components.datatype.Artifact',
+                'datatype': '?jpg',
+                'uri': None,
+                'blob': '&:blob:32b6853dd2b2b45de723966dba17e23cece9f35c'
+            }
+        }
+    }
+
+    with pytest.raises(FileNotFoundError):  
+        q = Document.decode(r, db=db).unpack()
+
+    with open('test/material/data/test-image.jpeg', 'rb') as f:
+        db.artifact_store.put_bytes(f.read(), '32b6853dd2b2b45de723966dba17e23cece9f35c')
+
+    q = Document.decode(r, db=db).unpack()
+
+    print(q)
+
+
+@pytest.fixture
+def clean():
+    yield
+    import os
+    try:
+        os.remove('/tmp/test_db/32b6853dd2b2b45de723966dba17e23cece9f35c')
+    except FileNotFoundError:
+        pass

Pinnacle-Networks/Pinnacle

pinnacle/backends/base/query.py
@@ -5,7 +5,7 @@ import uuid
 from abc import abstractmethod
 from functools import wraps
 
-from pinnacle.base.document import Document
+from pinnacle.base.document import Document, _unpack
 from pinnacle.base.leaf import Leaf
 
 if t.TYPE_CHECKING:
@@ -45,6 +45,15 @@ class _BaseQuery(Leaf):
         self._updated_key = None
         if not self.identifier:
             self.identifier = self._build_hr_identifier()
+
+    def unpack(self):
+        parts = _unpack(self.parts)
+        return type(self)(
+            db=self.db,
+            table=self.table,
+            parts=parts,
+            identifier=self.identifier,
+        )
 
     def _build_hr_identifier(self):
         identifier = str(self).split('\n')[-1]
@@ -649,7 +658,16 @@ class Model(_BaseQuery):
         m = self.db.models[self.table]
         method = getattr(m, self.parts[-1][0])
         r = method(*self.parts[-1][1], **self.parts[-1][2])
-        return Document({'_base': r})
+        if self.flatten:
+            return [
+                Document({'_base': res}) if not isinstance(res, dict) else Document(res) 
+                for res in r
+            ]
+        else:
+            if isinstance(r, dict):
+                return Document(r)
+            else:
+                return Document({'_base': r})
 
     def dict(self, metadata: bool = True, defaults: bool = True):
         """Return the query as a dictionary."""

test/unittest/component/datatype/test_init.py
@@ -1,0 +1,49 @@
+from pinnacle.base.document import Document
+import pytest
+
+from test.db_config import DBConfig
+
+
+@pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
+def test_load_image_inside_query(db, clean):
+    r = {
+        '_path': 'pinnacle.backends.base.query.parse_query', 
+        'query': 'image-search.predict(documents[0]["img"])',
+        'documents': [
+            {'img': '?32b6853dd2b2b45de723966dba17e23cece9f35c'}
+        ],
+        '_builds': {
+            'jpg': {
+                '_path': 'pinnacle.ext.pillow.encoder.image_type',
+                'encodable': 'artifact',
+                'media_type': 'image/jpeg',
+                'type_id': 'datatype',
+            },
+            '32b6853dd2b2b45de723966dba17e23cece9f35c': {
+                '_path': 'pinnacle.components.datatype.Artifact',
+                'datatype': '?jpg',
+                'uri': None,
+                'blob': '&:blob:32b6853dd2b2b45de723966dba17e23cece9f35c'
+            }
+        }
+    }
+
+    with pytest.raises(FileNotFoundError):  
+        q = Document.decode(r, db=db).unpack()
+
+    with open('test/material/data/test-image.jpeg', 'rb') as f:
+        db.artifact_store.put_bytes(f.read(), '32b6853dd2b2b45de723966dba17e23cece9f35c')
+
+    q = Document.decode(r, db=db).unpack()
+
+    print(q)
+
+
+@pytest.fixture
+def clean():
+    yield
+    import os
+    try:
+        os.remove('/tmp/test_db/32b6853dd2b2b45de723966dba17e23cece9f35c')
+    except FileNotFoundError:
+        pass