superduper-io/superduper

Remove bug in in_memory vector-searcher.add

superduper/base/document.py
@@ -387,7 +387,9 @@ def _deep_flat_encode(
     if isinstance(r, Native):
         return r.x
 
-    if getattr(r, 'importable', False) == True:
+    from superduper.backends.base.query import _BaseQuery
+
+    if not isinstance(r, _BaseQuery) and getattr(r, 'importable', False):
         ref = r.__name__
         r = dict(r.dict(metadata=metadata, defaults=defaults))
         _deep_flat_encode(
@@ -543,6 +545,7 @@ def _get_artifact_callback(db):
     def callback(path):
         def pull_bytes():
             return db.artifact_store.get_bytes(path), path
+
         return pull_bytes
 
     return callback

superduper/jobs/queue.py
@@ -1,6 +1,6 @@
-from collections import defaultdict
 import typing as t
 from abc import ABC, abstractmethod
+from collections import defaultdict
 
 from superduper import logging
 
@@ -22,7 +22,6 @@ class BaseQueueConsumer(ABC):
     :param callback: Callback for consumed messages.
     """
 
-<<<<<<< HEAD
     def __init__(
         self,
         uri: t.Optional[str] = '',
@@ -67,15 +66,15 @@ class BaseQueuePublisher(ABC):
 
     def __init__(self, uri: t.Optional[str]):
         self.uri: t.Optional[str] = uri
-        self.queue = defaultdict(lambda: [])
-        from superduper.misc.special_dicts import ArgumentDefaultDict
-        self.components = ArgumentDefaultDict(default_factory=lambda x: self.db.load(*x))
-        self._db = None
-        self._component_map = {}
+        self.queue: t.Dict = defaultdict(lambda: [])
 
-    def declare_component(self, component):
-        """Declare component and add it to queue."""
-        self.components[component.type_id, component.identifier] = component
+        from superduper.misc.special_dicts import ArgumentDefaultDict
+
+        self.components = ArgumentDefaultDict(
+            default_factory=lambda x: self.db.load(*x)
+        )
+        self._db = None
+        self._component_map: t.Dict = {}
 
     @property
     def db(self):
@@ -124,9 +123,7 @@ class LocalQueuePublisher(BaseQueuePublisher):
 
     def declare_component(self, component):
         """Declare component and add it to queue."""
-        identifier = f'{component.type_id}.{component.identifier}'
-        self.queue[identifier] = []
-        self.components[identifier] = component
+        self.components[component.type_id, component.identifier] = component
 
     def publish(self, events: t.List[t.Dict], to: DependencyType):
         """
@@ -153,6 +150,7 @@ class LocalQueuePublisher(BaseQueuePublisher):
                 _publish(events, dep)
         else:
             _publish(events, to)
+
         return self.consumer.consume(
             db=self.db, queue=self.queue, components=self.components
         )
@@ -174,21 +172,23 @@ class LocalQueueConsumer(BaseQueueConsumer):
         from superduper.base.datalayer import Event
 
         queue_jobs = defaultdict(lambda: [])
-        components = [('listener', x) for x in self.db.show('listener')]
-        components += [('vector_index', x) for x in self.db.show('vector_index')]
-        for type_id, identifier in components:
-            events = self.queue[type_id, identifier]
+        components_to_use = [('listener', x) for x in db.show('listener')]
+        components_to_use += [('vector_index', x) for x in db.show('vector_index')]
+        for type_id, identifier in components_to_use:
+            events = queue[type_id, identifier]
             if not events:
                 continue
-            self.queue[type_id, identifier] = []
-            component = self.components[type_id, identifier]
+            queue[type_id, identifier] = []
+            component = components[type_id, identifier]
             jobs = []
             for event_type, type_events in Event.chunk_by_event(events).items():
                 ids = [event['identifier'] for event in type_events]
                 overwrite = (
                     True if event_type in [Event.insert, Event.upsert] else False
                 )
-                logging.info(f'Running jobs for {component.type_id}::{component.identifier}')
+                logging.info(
+                    f'Running jobs for {component.type_id}::{component.identifier}'
+                )
                 logging.debug(f'Using ids: {ids}')
                 job = component.run_jobs(
                     db=db, ids=ids, overwrite=overwrite, event_type=event_type
@@ -196,6 +196,15 @@ class LocalQueueConsumer(BaseQueueConsumer):
                 jobs.append(job)
             queue_jobs[type_id, identifier].extend(jobs)
         return queue_jobs
+
+    @property
+    def db(self):
+        """Instance of Datalayer."""
+        return self._db
+
+    @db.setter
+    def db(self, db):
+        self._db = db
 
     def close_connection(self):
         """Close connection to queue."""

superduper/misc/annotations.py
@@ -169,19 +169,28 @@ def _get_indent(docstring: str) -> int:
 
 
 def importable(f):
+    """Make a function serializable as an importable.
 
+    :param f: function to make importable, decorated with @importable
+    """
     f.importable = True
 
     if inspect.isclass(f):
-
         # TODO log *args, **kwargs of __init__
         raise NotImplementedError('Classes are not supported yet')
 
     else:
+
         def to_dict(metadata: bool = True, defaults: bool = True):
             path = f'{f.__module__}.{f.__name__}'
             from superduper.base.document import Document
-            r = Document({'_path': 'superduper.misc.annotations.build_importable', 'importable': path})
+
+            r = Document(
+                {
+                    '_path': 'superduper.misc.annotations.build_importable',
+                    'importable': path,
+                }
+            )
             return r
 
         f.dict = to_dict
@@ -189,7 +198,11 @@ def importable(f):
 
 
 def build_importable(*, db=None, importable=None):
-    from superduper.base.leaf import import_item
+    """Build an importable from a path.
+
+    :param db: ``Datalayer`` instance
+    :param importable: importable path
+    """
     attr = importable.split('.')[-1]
     module = '.'.join(importable.split('.')[:-1])
     return getattr(importlib.import_module(module), attr)

test/unittest/component/datatype/test_init.py
@@ -1,17 +1,16 @@
-from superduper.base.document import Document
+from test.db_config import DBConfig
+
 import pytest
 
-from test.db_config import DBConfig
+from superduper.base.document import Document
 
 
 @pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
 def test_load_image_inside_query(db, clean):
     r = {
-        '_path': 'superduper.backends.base.query.parse_query', 
+        '_path': 'superduper.backends.base.query.parse_query',
         'query': 'image-search.predict(documents[0]["img"])',
-        'documents': [
-            {'img': '?32b6853dd2b2b45de723966dba17e23cece9f35c'}
-        ],
+        'documents': [{'img': '?32b6853dd2b2b45de723966dba17e23cece9f35c'}],
         '_builds': {
             'jpg': {
                 '_path': 'superduper.ext.pillow.encoder.image_type',
@@ -23,16 +22,18 @@ def test_load_image_inside_query(db, clean):
                 '_path': 'superduper.components.datatype.Artifact',
                 'datatype': '?jpg',
                 'uri': None,
-                'blob': '&:blob:32b6853dd2b2b45de723966dba17e23cece9f35c'
-            }
-        }
+                'blob': '&:blob:32b6853dd2b2b45de723966dba17e23cece9f35c',
+            },
+        },
     }
 
-    with pytest.raises(FileNotFoundError):  
+    with pytest.raises(FileNotFoundError):
         q = Document.decode(r, db=db).unpack()
 
     with open('test/material/data/test-image.jpeg', 'rb') as f:
-        db.artifact_store.put_bytes(f.read(), '32b6853dd2b2b45de723966dba17e23cece9f35c')
+        db.artifact_store.put_bytes(
+            f.read(), '32b6853dd2b2b45de723966dba17e23cece9f35c'
+        )
 
     q = Document.decode(r, db=db).unpack()
 
@@ -43,6 +44,7 @@ def test_load_image_inside_query(db, clean):
 def clean():
     yield
     import os
+
     try:
         os.remove('/tmp/test_db/32b6853dd2b2b45de723966dba17e23cece9f35c')
     except FileNotFoundError:

Pinnacle-Networks/Pinnacle

pinnacle/base/document.py
@@ -387,7 +387,9 @@ def _deep_flat_encode(
     if isinstance(r, Native):
         return r.x
 
-    if getattr(r, 'importable', False) == True:
+    from pinnacle.backends.base.query import _BaseQuery
+
+    if not isinstance(r, _BaseQuery) and getattr(r, 'importable', False):
         ref = r.__name__
         r = dict(r.dict(metadata=metadata, defaults=defaults))
         _deep_flat_encode(
@@ -543,6 +545,7 @@ def _get_artifact_callback(db):
     def callback(path):
         def pull_bytes():
             return db.artifact_store.get_bytes(path), path
+
         return pull_bytes
 
     return callback

pinnacle/jobs/queue.py
@@ -1,6 +1,6 @@
-from collections import defaultdict
 import typing as t
 from abc import ABC, abstractmethod
+from collections import defaultdict
 
 from pinnacle import logging
 
@@ -22,7 +22,6 @@ class BaseQueueConsumer(ABC):
     :param callback: Callback for consumed messages.
     """
 
-<<<<<<< HEAD
     def __init__(
         self,
         uri: t.Optional[str] = '',
@@ -67,15 +66,15 @@ class BaseQueuePublisher(ABC):
 
     def __init__(self, uri: t.Optional[str]):
         self.uri: t.Optional[str] = uri
-        self.queue = defaultdict(lambda: [])
-        from pinnacle.misc.special_dicts import ArgumentDefaultDict
-        self.components = ArgumentDefaultDict(default_factory=lambda x: self.db.load(*x))
-        self._db = None
-        self._component_map = {}
+        self.queue: t.Dict = defaultdict(lambda: [])
 
-    def declare_component(self, component):
-        """Declare component and add it to queue."""
-        self.components[component.type_id, component.identifier] = component
+        from pinnacle.misc.special_dicts import ArgumentDefaultDict
+
+        self.components = ArgumentDefaultDict(
+            default_factory=lambda x: self.db.load(*x)
+        )
+        self._db = None
+        self._component_map: t.Dict = {}
 
     @property
     def db(self):
@@ -124,9 +123,7 @@ class LocalQueuePublisher(BaseQueuePublisher):
 
     def declare_component(self, component):
         """Declare component and add it to queue."""
-        identifier = f'{component.type_id}.{component.identifier}'
-        self.queue[identifier] = []
-        self.components[identifier] = component
+        self.components[component.type_id, component.identifier] = component
 
     def publish(self, events: t.List[t.Dict], to: DependencyType):
         """
@@ -153,6 +150,7 @@ class LocalQueuePublisher(BaseQueuePublisher):
                 _publish(events, dep)
         else:
             _publish(events, to)
+
         return self.consumer.consume(
             db=self.db, queue=self.queue, components=self.components
         )
@@ -174,21 +172,23 @@ class LocalQueueConsumer(BaseQueueConsumer):
         from pinnacle.base.datalayer import Event
 
         queue_jobs = defaultdict(lambda: [])
-        components = [('listener', x) for x in self.db.show('listener')]
-        components += [('vector_index', x) for x in self.db.show('vector_index')]
-        for type_id, identifier in components:
-            events = self.queue[type_id, identifier]
+        components_to_use = [('listener', x) for x in db.show('listener')]
+        components_to_use += [('vector_index', x) for x in db.show('vector_index')]
+        for type_id, identifier in components_to_use:
+            events = queue[type_id, identifier]
             if not events:
                 continue
-            self.queue[type_id, identifier] = []
-            component = self.components[type_id, identifier]
+            queue[type_id, identifier] = []
+            component = components[type_id, identifier]
             jobs = []
             for event_type, type_events in Event.chunk_by_event(events).items():
                 ids = [event['identifier'] for event in type_events]
                 overwrite = (
                     True if event_type in [Event.insert, Event.upsert] else False
                 )
-                logging.info(f'Running jobs for {component.type_id}::{component.identifier}')
+                logging.info(
+                    f'Running jobs for {component.type_id}::{component.identifier}'
+                )
                 logging.debug(f'Using ids: {ids}')
                 job = component.run_jobs(
                     db=db, ids=ids, overwrite=overwrite, event_type=event_type
@@ -196,6 +196,15 @@ class LocalQueueConsumer(BaseQueueConsumer):
                 jobs.append(job)
             queue_jobs[type_id, identifier].extend(jobs)
         return queue_jobs
+
+    @property
+    def db(self):
+        """Instance of Datalayer."""
+        return self._db
+
+    @db.setter
+    def db(self, db):
+        self._db = db
 
     def close_connection(self):
         """Close connection to queue."""

pinnacle/misc/annotations.py
@@ -169,19 +169,28 @@ def _get_indent(docstring: str) -> int:
 
 
 def importable(f):
+    """Make a function serializable as an importable.
 
+    :param f: function to make importable, decorated with @importable
+    """
     f.importable = True
 
     if inspect.isclass(f):
-
         # TODO log *args, **kwargs of __init__
         raise NotImplementedError('Classes are not supported yet')
 
     else:
+
         def to_dict(metadata: bool = True, defaults: bool = True):
             path = f'{f.__module__}.{f.__name__}'
             from pinnacle.base.document import Document
-            r = Document({'_path': 'pinnacle.misc.annotations.build_importable', 'importable': path})
+
+            r = Document(
+                {
+                    '_path': 'pinnacle.misc.annotations.build_importable',
+                    'importable': path,
+                }
+            )
             return r
 
         f.dict = to_dict
@@ -189,7 +198,11 @@ def importable(f):
 
 
 def build_importable(*, db=None, importable=None):
-    from pinnacle.base.leaf import import_item
+    """Build an importable from a path.
+
+    :param db: ``Datalayer`` instance
+    :param importable: importable path
+    """
     attr = importable.split('.')[-1]
     module = '.'.join(importable.split('.')[:-1])
     return getattr(importlib.import_module(module), attr)

test/unittest/component/datatype/test_init.py
@@ -1,17 +1,16 @@
-from pinnacle.base.document import Document
+from test.db_config import DBConfig
+
 import pytest
 
-from test.db_config import DBConfig
+from pinnacle.base.document import Document
 
 
 @pytest.mark.parametrize("db", [DBConfig.mongodb_empty], indirect=True)
 def test_load_image_inside_query(db, clean):
     r = {
-        '_path': 'pinnacle.backends.base.query.parse_query', 
+        '_path': 'pinnacle.backends.base.query.parse_query',
         'query': 'image-search.predict(documents[0]["img"])',
-        'documents': [
-            {'img': '?32b6853dd2b2b45de723966dba17e23cece9f35c'}
-        ],
+        'documents': [{'img': '?32b6853dd2b2b45de723966dba17e23cece9f35c'}],
         '_builds': {
             'jpg': {
                 '_path': 'pinnacle.ext.pillow.encoder.image_type',
@@ -23,16 +22,18 @@ def test_load_image_inside_query(db, clean):
                 '_path': 'pinnacle.components.datatype.Artifact',
                 'datatype': '?jpg',
                 'uri': None,
-                'blob': '&:blob:32b6853dd2b2b45de723966dba17e23cece9f35c'
-            }
-        }
+                'blob': '&:blob:32b6853dd2b2b45de723966dba17e23cece9f35c',
+            },
+        },
     }
 
-    with pytest.raises(FileNotFoundError):  
+    with pytest.raises(FileNotFoundError):
         q = Document.decode(r, db=db).unpack()
 
     with open('test/material/data/test-image.jpeg', 'rb') as f:
-        db.artifact_store.put_bytes(f.read(), '32b6853dd2b2b45de723966dba17e23cece9f35c')
+        db.artifact_store.put_bytes(
+            f.read(), '32b6853dd2b2b45de723966dba17e23cece9f35c'
+        )
 
     q = Document.decode(r, db=db).unpack()
 
@@ -43,6 +44,7 @@ def test_load_image_inside_query(db, clean):
 def clean():
     yield
     import os
+
     try:
         os.remove('/tmp/test_db/32b6853dd2b2b45de723966dba17e23cece9f35c')
     except FileNotFoundError: