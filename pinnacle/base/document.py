superduper-io/superduper

Fix bugs in vector-index and queue implementation

superduper/base/document.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 from superduper import logging
 from superduper.base.code import Code
 from superduper.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
-from superduper.base.leaf import Leaf, _import_item
+from superduper.base.leaf import Leaf, import_item
 from superduper.base.variables import _replace_variables
 from superduper.components.component import Component
 from superduper.components.datatype import (
@@ -385,6 +385,21 @@ def _deep_flat_encode(
     if isinstance(r, Native):
         return r.x
 
+    if getattr(r, 'importable', False) == True:
+        ref = r.__name__
+        r = dict(r.dict(metadata=metadata, defaults=defaults))
+        _deep_flat_encode(
+            r,
+            builds=builds,
+            blobs=blobs,
+            files=files,
+            leaves_to_keep=leaves_to_keep,
+            metadata=metadata,
+            defaults=defaults,
+        )
+        builds[ref] = r
+        return f'?{ref}'
+
     if isinstance(r, Leaf):
         if r.identifier in builds:
             logging.warn(f'Leaf {r.identifier} already exists')
@@ -480,7 +495,7 @@ def _deep_flat_decode(r, builds, getters: _Getters, db: t.Optional['Datalayer'] 
         module = '.'.join(parts[:-1])
         dict_ = {k: v for k, v in r.items() if k != '_path'}
         dict_ = _deep_flat_decode(dict_, builds, getters=getters, db=db)
-        instance = _import_item(cls=cls, module=module, dict=dict_, db=db)
+        instance = import_item(cls=cls, module=module, dict=dict_, db=db)
         return instance
     if isinstance(r, dict) and '_object' in r:
         dict_ = {k: v for k, v in r.items() if k != '_object'}
@@ -488,7 +503,7 @@ def _deep_flat_decode(r, builds, getters: _Getters, db: t.Optional['Datalayer'] 
         object = _deep_flat_decode(
             builds[r['_object'][1:]], builds, getters=getters, db=db
         )
-        instance = _import_item(object=object.unpack(), dict=dict_, db=db)
+        instance = import_item(object=object.unpack(), dict=dict_, db=db)
         return instance
     if isinstance(r, dict):
         literals = r.get('_literals', [])

superduper/components/listener.py
@@ -37,8 +37,7 @@ class Listener(Component):
 
     key: ModelInputType
     model: Model
-    select: Query
-    active: bool = True
+    select: t.Union[Query, None]
     predict_kwargs: t.Optional[t.Dict] = dc.field(default_factory=dict)
     identifier: str = ''
 
@@ -91,7 +90,7 @@ class Listener(Component):
         :param db: Data layer instance.
         """
         self.create_output_dest(db, self.uuid, self.model)
-        if self.select is not None and self.active and not db.server_mode:
+        if self.select is not None and not db.server_mode:
             if CFG.cluster.cdc.uri:
                 request_server(
                     service='cdc',
@@ -99,7 +98,6 @@ class Listener(Component):
                     args={'name': self.identifier},
                     type='get',
                 )
-
         db.compute.queue.declare_component(self)
         db.compute.component_hook(self.identifier, type_id='listener')
 
@@ -201,11 +199,9 @@ class Listener(Component):
         :param dependencies: A list of dependencies.
         :param overwrite: Overwrite the existing data.
         """
-        from superduper.base.datalayer import Event
-
-        if not self.active or self.select is None:
+        if self.select is None:
             return []
-
+        from superduper.base.datalayer import Event
         ids = db.execute(self.select.select_ids)
         ids = [id[self.select.primary_id] for id in ids]
         events = [{'identifier': id, 'type': Event.insert} for id in ids]
@@ -227,7 +223,7 @@ class Listener(Component):
         :param overwrite: Overwrite the existing data.
         :param event_type: Type of event.
         """
-        if not self.active:
+        if self.select is None:
             return []
         assert not isinstance(self.model, str)
 

superduper/jobs/queue.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import typing as t
 from abc import ABC, abstractmethod
 
@@ -21,6 +22,7 @@ class BaseQueueConsumer(ABC):
     :param callback: Callback for consumed messages.
     """
 
+<<<<<<< HEAD
     def __init__(
         self,
         uri: t.Optional[str] = '',
@@ -64,11 +66,16 @@ class BaseQueuePublisher(ABC):
     """
 
     def __init__(self, uri: t.Optional[str]):
-        self.queue: t.Dict = {}
-        self.components: t.Dict = {}
         self.uri: t.Optional[str] = uri
+        self.queue = defaultdict(lambda: [])
+        from superduper.misc.special_dicts import ArgumentDefaultDict
+        self.components = ArgumentDefaultDict(default_factory=lambda x: self.db.load(*x))
         self._db = None
-        self._component_map: t.Dict = {}
+        self._component_map = {}
+
+    def declare_component(self, component):
+        """Declare component and add it to queue."""
+        self.components[component.type_id, component.identifier] = component
 
     @property
     def db(self):
@@ -130,12 +137,8 @@ class LocalQueuePublisher(BaseQueuePublisher):
         """
 
         def _publish(events, to):
-            identifier = to['identifier']
-            type_id = to['type_id']
             self._component_map.update(to)
-            identifier = f'{type_id}.{identifier}'
-            component = self.components[identifier]
-
+            component = self.components[to['type_id'], to['identifier']]
             ready_ids = component.ready_ids([e['identifier'] for e in events])
             ready_events = []
             for event in events:
@@ -143,7 +146,7 @@ class LocalQueuePublisher(BaseQueuePublisher):
                 if id in ready_ids:
                     ready_events.append(event)
 
-            self.queue[identifier].extend(ready_events)
+            self.queue[to['type_id'], to['identifier']].extend(ready_events)
 
         if isinstance(to, (tuple, list)):
             for dep in to:
@@ -170,32 +173,28 @@ class LocalQueueConsumer(BaseQueueConsumer):
         """Consume the current queue and run jobs."""
         from superduper.base.datalayer import Event
 
-        queue_jobs: t.Dict[str, t.List] = {}
-        for component_id in queue:
-            events = queue[component_id]
+        queue_jobs = defaultdict(lambda: [])
+        components = [('listener', x) for x in self.db.show('listener')]
+        components += [('vector_index', x) for x in self.db.show('vector_index')]
+        for type_id, identifier in components:
+            events = self.queue[type_id, identifier]
             if not events:
                 continue
-            queue[component_id] = []
-
-            component = components[component_id]
+            self.queue[type_id, identifier] = []
+            component = self.components[type_id, identifier]
             jobs = []
-
             for event_type, type_events in Event.chunk_by_event(events).items():
                 ids = [event['identifier'] for event in type_events]
                 overwrite = (
                     True if event_type in [Event.insert, Event.upsert] else False
                 )
-                logging.info(f'Running jobs for {component_id} with ids: {ids}')
+                logging.info(f'Running jobs for {component.type_id}::{component.identifier}')
+                logging.debug(f'Using ids: {ids}')
                 job = component.run_jobs(
                     db=db, ids=ids, overwrite=overwrite, event_type=event_type
                 )
                 jobs.append(job)
-
-            if component_id in queue_jobs:
-                queue_jobs[component_id].extend(jobs)
-            else:
-                queue_jobs[component_id] = jobs
-
+            queue_jobs[type_id, identifier].extend(jobs)
         return queue_jobs
 
     def close_connection(self):

superduper/misc/annotations.py
@@ -168,6 +168,33 @@ def _get_indent(docstring: str) -> int:
     return len(non_empty_lines[1]) - len(non_empty_lines[1].lstrip())
 
 
+def importable(f):
+
+    f.importable = True
+
+    if inspect.isclass(f):
+
+        # TODO log *args, **kwargs of __init__
+        raise NotImplementedError('Classes are not supported yet')
+
+    else:
+        def to_dict(metadata: bool = True, defaults: bool = True):
+            path = f'{f.__module__}.{f.__name__}'
+            from superduper.base.document import Document
+            r = Document({'_path': 'superduper.misc.annotations.build_importable', 'importable': path})
+            return r
+
+        f.dict = to_dict
+        return f
+
+
+def build_importable(*, db=None, importable=None):
+    from superduper.base.leaf import import_item
+    attr = importable.split('.')[-1]
+    module = '.'.join(importable.split('.')[:-1])
+    return getattr(importlib.import_module(module), attr)
+
+
 def component(*schema: t.Dict):
     """Decorator for creating a component.
 

superduper/misc/download.py
@@ -13,9 +13,11 @@ from tqdm import tqdm
 
 from superduper import CFG, logging
 from superduper.backends.base.query import Query
+from superduper.backends.query_dataset import QueryDataset
 from superduper.base.constant import KEY_BUILDS
 from superduper.base.document import Document
 from superduper.components.datatype import _BaseEncodable
+from superduper.components.model import Model
 
 
 class TimeoutException(Exception):
@@ -454,3 +456,30 @@ def download_from_one(r: Document):
         r[key].x = r[key].datatype.decode_data(downloader.results[uri])
 
     return
+
+
+class DownloadFiles(Model):
+    num_workers: int =10,
+    postprocess: t.Optional[t.Callable] = None
+    timeout: t.Optional[int] = None
+    headers: t.Optional[t.Dict] = None
+    raises: bool = True
+    signature: str = 'singleton'
+
+    def predict(self, uri):
+        return self.predict_batches([uri])[0]
+
+    def predict_batches(self, dataset):
+        downloader = BaseDownloader(
+            uris=dataset,
+            n_workers=self.num_workers,
+            timeout=self.timeout,
+            headers=self.headers or {},
+            raises=self.raises,
+        )
+        downloader.go()
+        results = [downloader.results[uri] for uri in dataset]
+        results = [self.datatype.decoder(r) for r in results]
+        if self.postprocess:
+            results = [self.postprocess(r) for r in results]
+        return results

Pinnacle-Networks/Pinnacle

pinnacle/base/document.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 from pinnacle import logging
 from pinnacle.base.code import Code
 from pinnacle.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
-from pinnacle.base.leaf import Leaf, _import_item
+from pinnacle.base.leaf import Leaf, import_item
 from pinnacle.base.variables import _replace_variables
 from pinnacle.components.component import Component
 from pinnacle.components.datatype import (
@@ -385,6 +385,21 @@ def _deep_flat_encode(
     if isinstance(r, Native):
         return r.x
 
+    if getattr(r, 'importable', False) == True:
+        ref = r.__name__
+        r = dict(r.dict(metadata=metadata, defaults=defaults))
+        _deep_flat_encode(
+            r,
+            builds=builds,
+            blobs=blobs,
+            files=files,
+            leaves_to_keep=leaves_to_keep,
+            metadata=metadata,
+            defaults=defaults,
+        )
+        builds[ref] = r
+        return f'?{ref}'
+
     if isinstance(r, Leaf):
         if r.identifier in builds:
             logging.warn(f'Leaf {r.identifier} already exists')
@@ -480,7 +495,7 @@ def _deep_flat_decode(r, builds, getters: _Getters, db: t.Optional['Datalayer'] 
         module = '.'.join(parts[:-1])
         dict_ = {k: v for k, v in r.items() if k != '_path'}
         dict_ = _deep_flat_decode(dict_, builds, getters=getters, db=db)
-        instance = _import_item(cls=cls, module=module, dict=dict_, db=db)
+        instance = import_item(cls=cls, module=module, dict=dict_, db=db)
         return instance
     if isinstance(r, dict) and '_object' in r:
         dict_ = {k: v for k, v in r.items() if k != '_object'}
@@ -488,7 +503,7 @@ def _deep_flat_decode(r, builds, getters: _Getters, db: t.Optional['Datalayer'] 
         object = _deep_flat_decode(
             builds[r['_object'][1:]], builds, getters=getters, db=db
         )
-        instance = _import_item(object=object.unpack(), dict=dict_, db=db)
+        instance = import_item(object=object.unpack(), dict=dict_, db=db)
         return instance
     if isinstance(r, dict):
         literals = r.get('_literals', [])

pinnacle/components/listener.py
@@ -37,8 +37,7 @@ class Listener(Component):
 
     key: ModelInputType
     model: Model
-    select: Query
-    active: bool = True
+    select: t.Union[Query, None]
     predict_kwargs: t.Optional[t.Dict] = dc.field(default_factory=dict)
     identifier: str = ''
 
@@ -91,7 +90,7 @@ class Listener(Component):
         :param db: Data layer instance.
         """
         self.create_output_dest(db, self.uuid, self.model)
-        if self.select is not None and self.active and not db.server_mode:
+        if self.select is not None and not db.server_mode:
             if CFG.cluster.cdc.uri:
                 request_server(
                     service='cdc',
@@ -99,7 +98,6 @@ class Listener(Component):
                     args={'name': self.identifier},
                     type='get',
                 )
-
         db.compute.queue.declare_component(self)
         db.compute.component_hook(self.identifier, type_id='listener')
 
@@ -201,11 +199,9 @@ class Listener(Component):
         :param dependencies: A list of dependencies.
         :param overwrite: Overwrite the existing data.
         """
-        from pinnacle.base.datalayer import Event
-
-        if not self.active or self.select is None:
+        if self.select is None:
             return []
-
+        from pinnacle.base.datalayer import Event
         ids = db.execute(self.select.select_ids)
         ids = [id[self.select.primary_id] for id in ids]
         events = [{'identifier': id, 'type': Event.insert} for id in ids]
@@ -227,7 +223,7 @@ class Listener(Component):
         :param overwrite: Overwrite the existing data.
         :param event_type: Type of event.
         """
-        if not self.active:
+        if self.select is None:
             return []
         assert not isinstance(self.model, str)
 

pinnacle/jobs/queue.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import typing as t
 from abc import ABC, abstractmethod
 
@@ -21,6 +22,7 @@ class BaseQueueConsumer(ABC):
     :param callback: Callback for consumed messages.
     """
 
+<<<<<<< HEAD
     def __init__(
         self,
         uri: t.Optional[str] = '',
@@ -64,11 +66,16 @@ class BaseQueuePublisher(ABC):
     """
 
     def __init__(self, uri: t.Optional[str]):
-        self.queue: t.Dict = {}
-        self.components: t.Dict = {}
         self.uri: t.Optional[str] = uri
+        self.queue = defaultdict(lambda: [])
+        from pinnacle.misc.special_dicts import ArgumentDefaultDict
+        self.components = ArgumentDefaultDict(default_factory=lambda x: self.db.load(*x))
         self._db = None
-        self._component_map: t.Dict = {}
+        self._component_map = {}
+
+    def declare_component(self, component):
+        """Declare component and add it to queue."""
+        self.components[component.type_id, component.identifier] = component
 
     @property
     def db(self):
@@ -130,12 +137,8 @@ class LocalQueuePublisher(BaseQueuePublisher):
         """
 
         def _publish(events, to):
-            identifier = to['identifier']
-            type_id = to['type_id']
             self._component_map.update(to)
-            identifier = f'{type_id}.{identifier}'
-            component = self.components[identifier]
-
+            component = self.components[to['type_id'], to['identifier']]
             ready_ids = component.ready_ids([e['identifier'] for e in events])
             ready_events = []
             for event in events:
@@ -143,7 +146,7 @@ class LocalQueuePublisher(BaseQueuePublisher):
                 if id in ready_ids:
                     ready_events.append(event)
 
-            self.queue[identifier].extend(ready_events)
+            self.queue[to['type_id'], to['identifier']].extend(ready_events)
 
         if isinstance(to, (tuple, list)):
             for dep in to:
@@ -170,32 +173,28 @@ class LocalQueueConsumer(BaseQueueConsumer):
         """Consume the current queue and run jobs."""
         from pinnacle.base.datalayer import Event
 
-        queue_jobs: t.Dict[str, t.List] = {}
-        for component_id in queue:
-            events = queue[component_id]
+        queue_jobs = defaultdict(lambda: [])
+        components = [('listener', x) for x in self.db.show('listener')]
+        components += [('vector_index', x) for x in self.db.show('vector_index')]
+        for type_id, identifier in components:
+            events = self.queue[type_id, identifier]
             if not events:
                 continue
-            queue[component_id] = []
-
-            component = components[component_id]
+            self.queue[type_id, identifier] = []
+            component = self.components[type_id, identifier]
             jobs = []
-
             for event_type, type_events in Event.chunk_by_event(events).items():
                 ids = [event['identifier'] for event in type_events]
                 overwrite = (
                     True if event_type in [Event.insert, Event.upsert] else False
                 )
-                logging.info(f'Running jobs for {component_id} with ids: {ids}')
+                logging.info(f'Running jobs for {component.type_id}::{component.identifier}')
+                logging.debug(f'Using ids: {ids}')
                 job = component.run_jobs(
                     db=db, ids=ids, overwrite=overwrite, event_type=event_type
                 )
                 jobs.append(job)
-
-            if component_id in queue_jobs:
-                queue_jobs[component_id].extend(jobs)
-            else:
-                queue_jobs[component_id] = jobs
-
+            queue_jobs[type_id, identifier].extend(jobs)
         return queue_jobs
 
     def close_connection(self):

pinnacle/misc/annotations.py
@@ -168,6 +168,33 @@ def _get_indent(docstring: str) -> int:
     return len(non_empty_lines[1]) - len(non_empty_lines[1].lstrip())
 
 
+def importable(f):
+
+    f.importable = True
+
+    if inspect.isclass(f):
+
+        # TODO log *args, **kwargs of __init__
+        raise NotImplementedError('Classes are not supported yet')
+
+    else:
+        def to_dict(metadata: bool = True, defaults: bool = True):
+            path = f'{f.__module__}.{f.__name__}'
+            from pinnacle.base.document import Document
+            r = Document({'_path': 'pinnacle.misc.annotations.build_importable', 'importable': path})
+            return r
+
+        f.dict = to_dict
+        return f
+
+
+def build_importable(*, db=None, importable=None):
+    from pinnacle.base.leaf import import_item
+    attr = importable.split('.')[-1]
+    module = '.'.join(importable.split('.')[:-1])
+    return getattr(importlib.import_module(module), attr)
+
+
 def component(*schema: t.Dict):
     """Decorator for creating a component.
 

pinnacle/misc/download.py
@@ -13,9 +13,11 @@ from tqdm import tqdm
 
 from pinnacle import CFG, logging
 from pinnacle.backends.base.query import Query
+from pinnacle.backends.query_dataset import QueryDataset
 from pinnacle.base.constant import KEY_BUILDS
 from pinnacle.base.document import Document
 from pinnacle.components.datatype import _BaseEncodable
+from pinnacle.components.model import Model
 
 
 class TimeoutException(Exception):
@@ -454,3 +456,30 @@ def download_from_one(r: Document):
         r[key].x = r[key].datatype.decode_data(downloader.results[uri])
 
     return
+
+
+class DownloadFiles(Model):
+    num_workers: int =10,
+    postprocess: t.Optional[t.Callable] = None
+    timeout: t.Optional[int] = None
+    headers: t.Optional[t.Dict] = None
+    raises: bool = True
+    signature: str = 'singleton'
+
+    def predict(self, uri):
+        return self.predict_batches([uri])[0]
+
+    def predict_batches(self, dataset):
+        downloader = BaseDownloader(
+            uris=dataset,
+            n_workers=self.num_workers,
+            timeout=self.timeout,
+            headers=self.headers or {},
+            raises=self.raises,
+        )
+        downloader.go()
+        results = [downloader.results[uri] for uri in dataset]
+        results = [self.datatype.decoder(r) for r in results]
+        if self.postprocess:
+            results = [self.postprocess(r) for r in results]
+        return results