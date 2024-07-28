superduper-io/superduper

Remove backfill and server_mode

superduper/base/datalayer.py
@@ -6,7 +6,6 @@ from collections import defaultdict, namedtuple
 
 import click
 import networkx
-import tqdm
 
 import superduper as s
 from superduper import logging
@@ -24,18 +23,16 @@ from superduper.base.cursor import SuperDuperCursor
 from superduper.base.document import Document
 from superduper.base.event import Event
 from superduper.components.component import Component
-from superduper.components.datatype import DataType, _BaseEncodable
+from superduper.components.datatype import DataType
 from superduper.components.schema import Schema
 from superduper.components.table import Table
 from superduper.jobs.job import Job
 from superduper.misc.annotations import deprecated
 from superduper.misc.colors import Colors
-from superduper.misc.data import ibatch
 from superduper.misc.download import download_from_one
 from superduper.misc.retry import db_retry
-from superduper.misc.server import is_csn
 from superduper.misc.special_dicts import recursive_update
-from superduper.vector_search.base import BaseVectorSearcher, VectorItem
+from superduper.vector_search.base import BaseVectorSearcher
 from superduper.vector_search.interface import FastVectorSearcher
 
 DBResult = t.Any
@@ -127,7 +124,6 @@ class Datalayer:
 
         self.compute = compute
         self.compute.queue.db = self
-        self._server_mode = False
         self._cfg = s.CFG
 
     def __getitem__(self, item):
@@ -143,23 +139,8 @@ class Datalayer:
         """CDC property setter."""
         self._cdc = cdc
 
-    @property
-    def server_mode(self):
-        """Property for server mode."""
-        return self._server_mode
-
-    @server_mode.setter
-    def server_mode(self, is_server: bool):
-        """
-        Set server mode property.
-
-        :param is_server: New boolean property.
-        """
-        assert isinstance(is_server, bool)
-        self._server_mode = is_server
-
     def initialize_vector_searcher(
-        self, identifier, searcher_type: t.Optional[str] = None, backfill: bool = False
+        self, identifier, searcher_type: t.Optional[str] = None
     ) -> t.Optional[BaseVectorSearcher]:
         """
         Initialize vector searcher.
@@ -181,68 +162,7 @@ class Datalayer:
         vector_comparison = vector_search_cls.from_component(vi)
 
         assert isinstance(clt.identifier, str), 'clt.identifier must be a string'
-        if backfill:
-            self.backfill_vector_search(vi, vector_comparison)
-
         return FastVectorSearcher(self, vector_comparison, vi.identifier)
-
-    def backfill_vector_search(self, vi, searcher):
-        """
-        Backfill vector search from model outputs of a given vector index.
-
-        :param vi: Identifier of vector index.
-        :param searcher: FastVectorSearch instance to load model outputs as vectors.
-        """
-        if s.CFG.cluster.vector_search.type == 'native':
-            return
-
-        if s.CFG.cluster.vector_search.uri and not is_csn('vector_search'):
-            return
-
-        logging.info(f"Loading vectors of vector-index: '{vi.identifier}'")
-
-        if vi.indexing_listener.select is None:
-            raise ValueError('.select must be set')
-
-        if vi.indexing_listener.select.db is None:
-            vi.indexing_listener.select.db = self
-
-        query = vi.indexing_listener.outputs_select
-
-        logging.info(str(query))
-
-        id_field = query.table_or_collection.primary_id
-
-        progress = tqdm.tqdm(desc='Loading vectors into vector-table...')
-        all_items = []
-        nokeys = 0
-        for record_batch in ibatch(
-            self.execute(query),
-            s.CFG.cluster.vector_search.backfill_batch_size,
-        ):
-            items = []
-            for record in record_batch:
-                id = record[id_field]
-                assert not isinstance(vi.indexing_listener.model, str)
-                try:
-                    h = record[vi.indexing_listener.outputs]
-                except KeyError:
-                    nokeys += 1
-                    continue
-                if isinstance(h, _BaseEncodable):
-                    h = h.unpack()
-                items.append(VectorItem.create(id=str(id), vector=h))
-            searcher.add(items, cache=True)
-            all_items.extend(items)
-            progress.update(len(items))
-        if nokeys:
-            logging.warn(
-                '{nokeys} ids were found without outputs populated yet,',
-                'hence skipped corresponding backfill',
-            )
-        logging.info('Vector search backfill successfully')
-
-        searcher.post_create()
 
     # TODO - needed?
     def set_compute(self, new: ComputeBackend):

superduper/components/listener.py
@@ -7,7 +7,7 @@ from superduper import CFG, logging
 from superduper.backends.base.query import Query
 from superduper.base.document import _OUTPUTS_KEY
 from superduper.components.model import Mapping
-from superduper.misc.server import request_server, is_csn
+from superduper.misc.server import is_csn, request_server
 
 from ..jobs.job import Job
 from .component import Component
@@ -74,6 +74,7 @@ class Listener(Component):
 
     @property
     def cdc_table(self):
+        """Get table for cdc."""
         return self.select.table_or_collection.identifier
 
     @override
@@ -98,10 +99,7 @@ class Listener(Component):
                     'Skipping listener setup on CDC service since no URI is set'
                 )
         else:
-            logging.info(
-                'Skipping listener setup on CDC service'
-                f' since select is {self.select} or server mode is {db.server_mode}'
-            )
+            logging.info('Skipping listener setup on CDC service')
         db.compute.queue.declare_component(self)
 
     @classmethod
@@ -176,7 +174,9 @@ class Listener(Component):
             keys = [self.key]
         elif isinstance(self.key, dict):
             keys = list(self.key.keys())
+        return self._ready_ids(data, keys)
 
+    def _ready_ids(self, data, keys):
         ready_ids = []
         for select in data:
             notfound = 0
@@ -195,14 +195,12 @@ class Listener(Component):
         db: "Datalayer",
         dependencies: t.Sequence[Job] = (),
         overwrite: bool = False,
-        ids: t.Optional[t.List[t.Any]] = None,
     ) -> t.Sequence[t.Any]:
         """Schedule jobs for the listener.
 
         :param db: Data layer instance to process.
         :param dependencies: A list of dependencies.
         :param overwrite: Overwrite the existing data.
-        :param ids: Optional ids to schedule.
         """
         if self.select is None:
             return []
@@ -210,11 +208,14 @@ class Listener(Component):
         from superduper.base.event import Event
 
         events = []
-        if ids is None:
-            ids = db.execute(self.select.select_ids)
-            ids = [id[self.select.primary_id] for id in ids]
+        data = db.execute(self.select)
+        keys = self.key
 
-        # TODO: Check ready ids
+        if isinstance(self.key, str):
+            keys = [self.key]
+        elif isinstance(self.key, dict):
+            keys = list(self.key.keys())
+        ids = self._ready_ids(data, keys)
 
         events = [
             Event(

superduper/components/vector_index.py
@@ -15,10 +15,10 @@ from superduper.components.model import Mapping, ModelInputType
 from superduper.ext.utils import str_shape
 from superduper.jobs.job import FunctionJob
 from superduper.misc.annotations import component
+from superduper.misc.server import is_csn, request_server
 from superduper.misc.special_dicts import MongoStyleDict
 from superduper.vector_search.base import VectorIndexMeasureType
 from superduper.vector_search.update_tasks import copy_vectors, delete_vectors
-from superduper.misc.server import request_server, is_csn
 
 KeyType = t.Union[str, t.List, t.Dict]
 if t.TYPE_CHECKING:
@@ -175,6 +175,7 @@ class VectorIndex(Component):
 
     @property
     def cdc_table(self):
+        """Get table for cdc."""
         return self.indexing_listener.outputs
 
     @override
@@ -233,23 +234,13 @@ class VectorIndex(Component):
         :param query: Query object.
         :param primary_ids: Primary IDs.
         """
-        print('MMMMMMMMMMMMMMMMMMMMMMMMMMM')
-        print('Trigger ids')
         if not isinstance(self.indexing_listener.select, Query):
-            print('NOT PASSED')
             return []
 
         if self.indexing_listener.outputs != query.table:
-            print('NOT PASSED')
             return []
 
-        ids = self._ready_ids(primary_ids)
-        if ids:
-            print('PASSED')
-            return ids
-        else:
-            print('NOT PASSED from Outputs')
-            return []
+        return self._ready_ids(primary_ids)
 
     def _ready_ids(self, ids: t.Sequence):
         select = self.indexing_listener.outputs_select
@@ -281,7 +272,6 @@ class VectorIndex(Component):
         :param ids: List of ids.
         :param event_type: Type of event.
         """
-
         if event_type in [DBEvent.insert, DBEvent.upsert]:
             callable = copy_vectors
         elif type == DBEvent.delete:
@@ -310,23 +300,20 @@ class VectorIndex(Component):
         self,
         db: Datalayer,
         dependencies: t.Sequence['Job'] = (),
-        ids: t.Optional[t.List[t.Any]] = None,
     ) -> t.Sequence[t.Any]:
         """Schedule jobs for the vector index.
 
         :param db: The DB instance to process
         :param dependencies: A list of dependencies
-        :param ids: Optional ids to schedule.
         """
         from superduper.base.event import Event
 
         assert self.indexing_listener.select is not None
 
-        if ids is None:
-            ids = db.execute(self.indexing_listener.select.select_ids)
-            ids = [id[self.indexing_listener.select.primary_id] for id in ids]
+        outputs = db[self.indexing_listener.outputs]
+        ids = db.execute(outputs.select_ids)
+        ids = [id[outputs.primary_id] for id in ids]
 
-        ids = self._ready_ids(ids)
         events = [
             Event(
                 type_id=self.type_id,

Pinnacle-Networks/Pinnacle

pinnacle/base/datalayer.py
@@ -6,7 +6,6 @@ from collections import defaultdict, namedtuple
 
 import click
 import networkx
-import tqdm
 
 import pinnacle as s
 from pinnacle import logging
@@ -24,18 +23,16 @@ from pinnacle.base.cursor import SuperDuperCursor
 from pinnacle.base.document import Document
 from pinnacle.base.event import Event
 from pinnacle.components.component import Component
-from pinnacle.components.datatype import DataType, _BaseEncodable
+from pinnacle.components.datatype import DataType
 from pinnacle.components.schema import Schema
 from pinnacle.components.table import Table
 from pinnacle.jobs.job import Job
 from pinnacle.misc.annotations import deprecated
 from pinnacle.misc.colors import Colors
-from pinnacle.misc.data import ibatch
 from pinnacle.misc.download import download_from_one
 from pinnacle.misc.retry import db_retry
-from pinnacle.misc.server import is_csn
 from pinnacle.misc.special_dicts import recursive_update
-from pinnacle.vector_search.base import BaseVectorSearcher, VectorItem
+from pinnacle.vector_search.base import BaseVectorSearcher
 from pinnacle.vector_search.interface import FastVectorSearcher
 
 DBResult = t.Any
@@ -127,7 +124,6 @@ class Datalayer:
 
         self.compute = compute
         self.compute.queue.db = self
-        self._server_mode = False
         self._cfg = s.CFG
 
     def __getitem__(self, item):
@@ -143,23 +139,8 @@ class Datalayer:
         """CDC property setter."""
         self._cdc = cdc
 
-    @property
-    def server_mode(self):
-        """Property for server mode."""
-        return self._server_mode
-
-    @server_mode.setter
-    def server_mode(self, is_server: bool):
-        """
-        Set server mode property.
-
-        :param is_server: New boolean property.
-        """
-        assert isinstance(is_server, bool)
-        self._server_mode = is_server
-
     def initialize_vector_searcher(
-        self, identifier, searcher_type: t.Optional[str] = None, backfill: bool = False
+        self, identifier, searcher_type: t.Optional[str] = None
     ) -> t.Optional[BaseVectorSearcher]:
         """
         Initialize vector searcher.
@@ -181,68 +162,7 @@ class Datalayer:
         vector_comparison = vector_search_cls.from_component(vi)
 
         assert isinstance(clt.identifier, str), 'clt.identifier must be a string'
-        if backfill:
-            self.backfill_vector_search(vi, vector_comparison)
-
         return FastVectorSearcher(self, vector_comparison, vi.identifier)
-
-    def backfill_vector_search(self, vi, searcher):
-        """
-        Backfill vector search from model outputs of a given vector index.
-
-        :param vi: Identifier of vector index.
-        :param searcher: FastVectorSearch instance to load model outputs as vectors.
-        """
-        if s.CFG.cluster.vector_search.type == 'native':
-            return
-
-        if s.CFG.cluster.vector_search.uri and not is_csn('vector_search'):
-            return
-
-        logging.info(f"Loading vectors of vector-index: '{vi.identifier}'")
-
-        if vi.indexing_listener.select is None:
-            raise ValueError('.select must be set')
-
-        if vi.indexing_listener.select.db is None:
-            vi.indexing_listener.select.db = self
-
-        query = vi.indexing_listener.outputs_select
-
-        logging.info(str(query))
-
-        id_field = query.table_or_collection.primary_id
-
-        progress = tqdm.tqdm(desc='Loading vectors into vector-table...')
-        all_items = []
-        nokeys = 0
-        for record_batch in ibatch(
-            self.execute(query),
-            s.CFG.cluster.vector_search.backfill_batch_size,
-        ):
-            items = []
-            for record in record_batch:
-                id = record[id_field]
-                assert not isinstance(vi.indexing_listener.model, str)
-                try:
-                    h = record[vi.indexing_listener.outputs]
-                except KeyError:
-                    nokeys += 1
-                    continue
-                if isinstance(h, _BaseEncodable):
-                    h = h.unpack()
-                items.append(VectorItem.create(id=str(id), vector=h))
-            searcher.add(items, cache=True)
-            all_items.extend(items)
-            progress.update(len(items))
-        if nokeys:
-            logging.warn(
-                '{nokeys} ids were found without outputs populated yet,',
-                'hence skipped corresponding backfill',
-            )
-        logging.info('Vector search backfill successfully')
-
-        searcher.post_create()
 
     # TODO - needed?
     def set_compute(self, new: ComputeBackend):

pinnacle/components/listener.py
@@ -7,7 +7,7 @@ from pinnacle import CFG, logging
 from pinnacle.backends.base.query import Query
 from pinnacle.base.document import _OUTPUTS_KEY
 from pinnacle.components.model import Mapping
-from pinnacle.misc.server import request_server, is_csn
+from pinnacle.misc.server import is_csn, request_server
 
 from ..jobs.job import Job
 from .component import Component
@@ -74,6 +74,7 @@ class Listener(Component):
 
     @property
     def cdc_table(self):
+        """Get table for cdc."""
         return self.select.table_or_collection.identifier
 
     @override
@@ -98,10 +99,7 @@ class Listener(Component):
                     'Skipping listener setup on CDC service since no URI is set'
                 )
         else:
-            logging.info(
-                'Skipping listener setup on CDC service'
-                f' since select is {self.select} or server mode is {db.server_mode}'
-            )
+            logging.info('Skipping listener setup on CDC service')
         db.compute.queue.declare_component(self)
 
     @classmethod
@@ -176,7 +174,9 @@ class Listener(Component):
             keys = [self.key]
         elif isinstance(self.key, dict):
             keys = list(self.key.keys())
+        return self._ready_ids(data, keys)
 
+    def _ready_ids(self, data, keys):
         ready_ids = []
         for select in data:
             notfound = 0
@@ -195,14 +195,12 @@ class Listener(Component):
         db: "Datalayer",
         dependencies: t.Sequence[Job] = (),
         overwrite: bool = False,
-        ids: t.Optional[t.List[t.Any]] = None,
     ) -> t.Sequence[t.Any]:
         """Schedule jobs for the listener.
 
         :param db: Data layer instance to process.
         :param dependencies: A list of dependencies.
         :param overwrite: Overwrite the existing data.
-        :param ids: Optional ids to schedule.
         """
         if self.select is None:
             return []
@@ -210,11 +208,14 @@ class Listener(Component):
         from pinnacle.base.event import Event
 
         events = []
-        if ids is None:
-            ids = db.execute(self.select.select_ids)
-            ids = [id[self.select.primary_id] for id in ids]
+        data = db.execute(self.select)
+        keys = self.key
 
-        # TODO: Check ready ids
+        if isinstance(self.key, str):
+            keys = [self.key]
+        elif isinstance(self.key, dict):
+            keys = list(self.key.keys())
+        ids = self._ready_ids(data, keys)
 
         events = [
             Event(

pinnacle/components/vector_index.py
@@ -15,10 +15,10 @@ from pinnacle.components.model import Mapping, ModelInputType
 from pinnacle.ext.utils import str_shape
 from pinnacle.jobs.job import FunctionJob
 from pinnacle.misc.annotations import component
+from pinnacle.misc.server import is_csn, request_server
 from pinnacle.misc.special_dicts import MongoStyleDict
 from pinnacle.vector_search.base import VectorIndexMeasureType
 from pinnacle.vector_search.update_tasks import copy_vectors, delete_vectors
-from pinnacle.misc.server import request_server, is_csn
 
 KeyType = t.Union[str, t.List, t.Dict]
 if t.TYPE_CHECKING:
@@ -175,6 +175,7 @@ class VectorIndex(Component):
 
     @property
     def cdc_table(self):
+        """Get table for cdc."""
         return self.indexing_listener.outputs
 
     @override
@@ -233,23 +234,13 @@ class VectorIndex(Component):
         :param query: Query object.
         :param primary_ids: Primary IDs.
         """
-        print('MMMMMMMMMMMMMMMMMMMMMMMMMMM')
-        print('Trigger ids')
         if not isinstance(self.indexing_listener.select, Query):
-            print('NOT PASSED')
             return []
 
         if self.indexing_listener.outputs != query.table:
-            print('NOT PASSED')
             return []
 
-        ids = self._ready_ids(primary_ids)
-        if ids:
-            print('PASSED')
-            return ids
-        else:
-            print('NOT PASSED from Outputs')
-            return []
+        return self._ready_ids(primary_ids)
 
     def _ready_ids(self, ids: t.Sequence):
         select = self.indexing_listener.outputs_select
@@ -281,7 +272,6 @@ class VectorIndex(Component):
         :param ids: List of ids.
         :param event_type: Type of event.
         """
-
         if event_type in [DBEvent.insert, DBEvent.upsert]:
             callable = copy_vectors
         elif type == DBEvent.delete:
@@ -310,23 +300,20 @@ class VectorIndex(Component):
         self,
         db: Datalayer,
         dependencies: t.Sequence['Job'] = (),
-        ids: t.Optional[t.List[t.Any]] = None,
     ) -> t.Sequence[t.Any]:
         """Schedule jobs for the vector index.
 
         :param db: The DB instance to process
         :param dependencies: A list of dependencies
-        :param ids: Optional ids to schedule.
         """
         from pinnacle.base.event import Event
 
         assert self.indexing_listener.select is not None
 
-        if ids is None:
-            ids = db.execute(self.indexing_listener.select.select_ids)
-            ids = [id[self.indexing_listener.select.primary_id] for id in ids]
+        outputs = db[self.indexing_listener.outputs]
+        ids = db.execute(outputs.select_ids)
+        ids = [id[outputs.primary_id] for id in ids]
 
-        ids = self._ready_ids(ids)
         events = [
             Event(
                 type_id=self.type_id,