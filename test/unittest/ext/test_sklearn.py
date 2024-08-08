superduper-io/superduper

Add component test utils

test/unittest/ext/test_sklearn.py
@@ -1,4 +1,5 @@
 import random
+from test.utils.component import utils as component_utils
 
 import numpy
 import pytest
@@ -6,6 +7,7 @@ from sklearn.base import TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 
+from superduper.backends.base.query import Query
 from superduper.base.document import Document
 from superduper.ext.sklearn.model import Estimator, SklearnTrainer
 
@@ -28,7 +30,7 @@ class Lookup(TransformerMixin):
 class TestPipeline:
     @pytest.fixture()
     def dictionary(self):
-        yield ['apple', 'orange', 'banana', 'toast']
+        yield ["apple", "orange", "banana", "toast"]
 
     @pytest.fixture()
     def X(self, dictionary):
@@ -38,7 +40,7 @@ class TestPipeline:
     def data_in_db(self, db, X, y):
         db.cfg.auto_schema = True
         db.execute(
-            db['documents'].insert([Document({'X': x, 'y': yy}) for x, yy in zip(X, y)])
+            db["documents"].insert([Document({"X": x, "y": yy}) for x, yy in zip(X, y)])
         )
         yield db
 
@@ -48,12 +50,34 @@ class TestPipeline:
 
     def test_fit_db(self, dictionary, data_in_db):
         pipeline = Estimator(
-            identifier='my-svc',
-            object=Pipeline([('my-encoding', Lookup(dictionary)), ('my-svc', SVC())]),
+            identifier="my-svc",
+            object=Pipeline([("my-encoding", Lookup(dictionary)), ("my-svc", SVC())]),
             trainer=SklearnTrainer(
-                'my-trainer',
-                key=('X', 'y'),
-                select=data_in_db['documents'].select(),
+                "my-trainer",
+                key=("X", "y"),
+                select=data_in_db["documents"].select(),
             ),
         )
         _ = data_in_db.apply(pipeline)
+
+
+def test_encode_and_decode():
+    model = Estimator(
+        identifier="my-svc",
+        object=Pipeline(
+            [
+                ("my-encoding", Lookup(["apple", "orange", "banana", "toast"])),
+                ("my-svc", SVC()),
+            ]
+        ),
+        trainer=SklearnTrainer(
+            "my-trainer",
+            key=("X", "y"),
+            select=Query(table="document").select(),
+        ),
+    )
+    decode_model = component_utils.test_encode_and_decode(model)
+
+    assert decode_model.trainer.identifier == "my-trainer"
+    assert decode_model.trainer.key == ("X", "y")
+    assert decode_model.trainer.select.table == "document"

test/utils/component/model.py
@@ -1,0 +1,50 @@
+import typing as t
+
+from superduper.components.model import Model
+
+if t.TYPE_CHECKING:
+    from superduper.base.datalayer import Datalayer
+
+
+def test_predict(model: Model, sample_data: t.Any):
+    result = model.predict(sample_data)
+    results = model.predict_batches([sample_data] * 10)
+    assert len(results) == 10
+
+    return result, results
+
+
+def test_predict_in_db(model: Model, sample_data: t.Any, db: "Datalayer"):
+    db.cfg.auto_schema = True
+
+    db["datas"].insert([{"data": sample_data, "i": i} for i in range(10)]).execute()
+
+    model.predict_in_db(
+        X="data",
+        select=db["datas"].select(),
+        db=db,
+        predict_id="test",
+    )
+
+    results = list(db["_outputs__test"].select().execute())
+    assert len(results) == 10
+
+    return results
+
+
+def test_model_as_a_listener(model: Model, sample_data: t.Any, db: "Datalayer"):
+    db.cfg.auto_schema = True
+
+    db["datas"].insert([{"data": sample_data, "i": i} for i in range(10)]).execute()
+
+    listener = model.to_listener(
+        key="data",
+        select=db["datas"].select(),
+        uuid="test",
+    )
+
+    db.apply(listener)
+
+    results = list(db["_outputs__test"].select().execute())
+    assert len(results) == 10
+    return results

test/utils/component/utils.py
@@ -1,0 +1,32 @@
+import base64
+import json
+from copy import deepcopy
+
+from superduper.base.document import Document
+from superduper.components.component import Component
+from superduper.misc.special_dicts import recursive_update
+
+
+def _bytes_to_base64(obj):
+    def replace_func(value):
+        if isinstance(value, bytes):
+            return base64.b64encode(value).decode()
+        return value
+
+    return recursive_update(obj, replace_func)
+
+
+def test_encode_and_decode(component: Component):
+    encode_data = component.encode()
+
+    # Make sure that the data is JSON serializable
+    assert json.dumps(_bytes_to_base64(deepcopy(encode_data)))
+
+    # Make sure that the data can be decoded to the same component
+    load_component = Document.decode(encode_data).unpack()
+    load_component.init()
+
+    assert type(load_component) == type(component)
+    assert load_component.metadata == component.metadata
+
+    return load_component

Pinnacle-Networks/Pinnacle

test/unittest/ext/test_sklearn.py
@@ -1,4 +1,5 @@
 import random
+from test.utils.component import utils as component_utils
 
 import numpy
 import pytest
@@ -6,6 +7,7 @@ from sklearn.base import TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 
+from pinnacle.backends.base.query import Query
 from pinnacle.base.document import Document
 from pinnacle.ext.sklearn.model import Estimator, SklearnTrainer
 
@@ -28,7 +30,7 @@ class Lookup(TransformerMixin):
 class TestPipeline:
     @pytest.fixture()
     def dictionary(self):
-        yield ['apple', 'orange', 'banana', 'toast']
+        yield ["apple", "orange", "banana", "toast"]
 
     @pytest.fixture()
     def X(self, dictionary):
@@ -38,7 +40,7 @@ class TestPipeline:
     def data_in_db(self, db, X, y):
         db.cfg.auto_schema = True
         db.execute(
-            db['documents'].insert([Document({'X': x, 'y': yy}) for x, yy in zip(X, y)])
+            db["documents"].insert([Document({"X": x, "y": yy}) for x, yy in zip(X, y)])
         )
         yield db
 
@@ -48,12 +50,34 @@ class TestPipeline:
 
     def test_fit_db(self, dictionary, data_in_db):
         pipeline = Estimator(
-            identifier='my-svc',
-            object=Pipeline([('my-encoding', Lookup(dictionary)), ('my-svc', SVC())]),
+            identifier="my-svc",
+            object=Pipeline([("my-encoding", Lookup(dictionary)), ("my-svc", SVC())]),
             trainer=SklearnTrainer(
-                'my-trainer',
-                key=('X', 'y'),
-                select=data_in_db['documents'].select(),
+                "my-trainer",
+                key=("X", "y"),
+                select=data_in_db["documents"].select(),
             ),
         )
         _ = data_in_db.apply(pipeline)
+
+
+def test_encode_and_decode():
+    model = Estimator(
+        identifier="my-svc",
+        object=Pipeline(
+            [
+                ("my-encoding", Lookup(["apple", "orange", "banana", "toast"])),
+                ("my-svc", SVC()),
+            ]
+        ),
+        trainer=SklearnTrainer(
+            "my-trainer",
+            key=("X", "y"),
+            select=Query(table="document").select(),
+        ),
+    )
+    decode_model = component_utils.test_encode_and_decode(model)
+
+    assert decode_model.trainer.identifier == "my-trainer"
+    assert decode_model.trainer.key == ("X", "y")
+    assert decode_model.trainer.select.table == "document"

test/utils/component/model.py
@@ -1,0 +1,50 @@
+import typing as t
+
+from pinnacle.components.model import Model
+
+if t.TYPE_CHECKING:
+    from pinnacle.base.datalayer import Datalayer
+
+
+def test_predict(model: Model, sample_data: t.Any):
+    result = model.predict(sample_data)
+    results = model.predict_batches([sample_data] * 10)
+    assert len(results) == 10
+
+    return result, results
+
+
+def test_predict_in_db(model: Model, sample_data: t.Any, db: "Datalayer"):
+    db.cfg.auto_schema = True
+
+    db["datas"].insert([{"data": sample_data, "i": i} for i in range(10)]).execute()
+
+    model.predict_in_db(
+        X="data",
+        select=db["datas"].select(),
+        db=db,
+        predict_id="test",
+    )
+
+    results = list(db["_outputs__test"].select().execute())
+    assert len(results) == 10
+
+    return results
+
+
+def test_model_as_a_listener(model: Model, sample_data: t.Any, db: "Datalayer"):
+    db.cfg.auto_schema = True
+
+    db["datas"].insert([{"data": sample_data, "i": i} for i in range(10)]).execute()
+
+    listener = model.to_listener(
+        key="data",
+        select=db["datas"].select(),
+        uuid="test",
+    )
+
+    db.apply(listener)
+
+    results = list(db["_outputs__test"].select().execute())
+    assert len(results) == 10
+    return results

test/utils/component/utils.py
@@ -1,0 +1,32 @@
+import base64
+import json
+from copy import deepcopy
+
+from pinnacle.base.document import Document
+from pinnacle.components.component import Component
+from pinnacle.misc.special_dicts import recursive_update
+
+
+def _bytes_to_base64(obj):
+    def replace_func(value):
+        if isinstance(value, bytes):
+            return base64.b64encode(value).decode()
+        return value
+
+    return recursive_update(obj, replace_func)
+
+
+def test_encode_and_decode(component: Component):
+    encode_data = component.encode()
+
+    # Make sure that the data is JSON serializable
+    assert json.dumps(_bytes_to_base64(deepcopy(encode_data)))
+
+    # Make sure that the data can be decoded to the same component
+    load_component = Document.decode(encode_data).unpack()
+    load_component.init()
+
+    assert type(load_component) == type(component)
+    assert load_component.metadata == component.metadata
+
+    return load_component