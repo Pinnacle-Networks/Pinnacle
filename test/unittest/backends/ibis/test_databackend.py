superduper-io/superduper

Add database test utils

test/unittest/backends/ibis/test_databackend.py
@@ -1,0 +1,25 @@
+from test.utils.database import databackend as db_utils
+
+import pytest
+
+from superduper import CFG
+from superduper.backends.ibis.data_backend import IbisDataBackend
+
+
+@pytest.fixture
+def databackend():
+    backend = IbisDataBackend(CFG.data_backend)
+    yield backend
+    backend.drop(True)
+
+
+def test_output_dest(databackend):
+    db_utils.test_output_dest(databackend)
+
+
+def test_query_builder(databackend):
+    db_utils.test_query_builder(databackend)
+
+
+def test_list_tables_or_collections(databackend):
+    db_utils.test_list_tables_or_collections(databackend)

test/unittest/backends/local/test_artifact_store.py
@@ -1,0 +1,20 @@
+from test.utils.database import artifact_store as artifact_store_utils
+
+import pytest
+
+from superduper.backends.local.artifacts import FileSystemArtifactStore
+
+
+@pytest.fixture
+def artifact_store(tmpdir):
+    artifact_store = FileSystemArtifactStore(tmpdir)
+    yield artifact_store
+    artifact_store.drop(True)
+
+
+def test_bytes(artifact_store: FileSystemArtifactStore):
+    artifact_store_utils.test_bytes(artifact_store)
+
+
+def test_file(artifact_store: FileSystemArtifactStore):
+    artifact_store_utils.test_file(artifact_store)

test/unittest/backends/mongodb/test_artifact_store.py
@@ -1,0 +1,27 @@
+from test.utils.database import artifact_store as artifact_store_utils
+
+import pytest
+
+from superduper import CFG
+from superduper.backends.mongodb.data_backend import MongoDataBackend
+
+DATABASE_URL = CFG.artifact_store or CFG.data_backend or ""
+
+
+if DATABASE_URL.split(":")[0] not in ["mongodb", "mongodb+srv"]:
+    pytest.skip("MongoDB is not configured, skipping tests...", allow_module_level=True)
+
+
+@pytest.fixture
+def artifact_store():
+    artifact_store = MongoDataBackend(DATABASE_URL).build_artifact_store()
+    yield artifact_store
+    artifact_store.drop(True)
+
+
+def test_bytes(artifact_store):
+    artifact_store_utils.test_bytes(artifact_store)
+
+
+def test_file(artifact_store):
+    artifact_store_utils.test_file(artifact_store)

test/unittest/backends/mongodb/test_databackend.py
@@ -1,0 +1,27 @@
+from test.utils.database import databackend as db_utils
+
+import pytest
+
+from superduper import CFG
+from superduper.backends.mongodb.data_backend import MongoDataBackend
+
+
+@pytest.fixture
+def databackend():
+    backend = MongoDataBackend(CFG.data_backend)
+    yield backend
+    backend.drop(True)
+
+
+@pytest.mark.skip(reason="Open this test after creating a blank table for MongoDB.")
+def test_output_dest(databackend):
+    db_utils.test_output_dest(databackend)
+
+
+def test_query_builder(databackend):
+    db_utils.test_query_builder(databackend)
+
+
+@pytest.mark.skip(reason="Open this test after creating a blank table for MongoDB.")
+def test_list_tables_or_collections(databackend):
+    db_utils.test_list_tables_or_collections(databackend)

test/unittest/backends/sqlalchemy/test_metadata.py
@@ -1,58 +1,35 @@
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+import os
+from test.utils.database import metadata as metadata_utils
 
+import pytest
+
+from superduper import CFG
 from superduper.backends.sqlalchemy.metadata import SQLAlchemyMetadata
 
-Base = declarative_base()
+DATABASE_URL = CFG.metadata_store or "sqlite:///:memory:"
 
-DATABASE_URL = "sqlite:///:memory:"
+
+# TODO: Remove skip when we move all the plugin tests to the new structure
+skip = not os.environ.get('SUPERDUPER_CONFIG', "").endswith('ibis.yaml')
+
+if skip:
+    pytest.skip("Skipping this file for now", allow_module_level=True)
 
 
 @pytest.fixture
 def metadata():
-    engine = create_engine(DATABASE_URL)
     store = SQLAlchemyMetadata(DATABASE_URL)
     yield store
     store.drop(force=True)
-    engine.dispose()
-    Base.metadata.drop_all(engine)
 
 
-def test(metadata):
-    import uuid
+def test_component(metadata):
+    metadata_utils.test_component(metadata)
 
-    metadata.create_component(
-        {
-            'identifier': 'my-model',
-            'type_id': 'model',
-            'version': 0,
-            '_path': 'superduper.container.model.Model',
-            'uuid': str(uuid.uuid4()),
-        }
-    )
 
-    comps = metadata.show_components('model')
-    assert comps == ['my-model']
+def test_parent_child(metadata):
+    metadata_utils.test_parent_child(metadata)
 
-    metadata.create_component(
-        {
-            'identifier': 'other-model',
-            'type_id': 'model',
-            'version': 0,
-            '_path': 'superduper.container.model.Model',
-            'uuid': str(uuid.uuid4()),
-        }
-    )
 
-    comps = metadata.show_components('model')
-
-    r = metadata.get_component(
-        type_id='model',
-        identifier='other-model',
-        version=0,
-        allow_hidden=True,
-    )
-
-    assert r['identifier'] == 'other-model'
-    assert r['version'] == 0
+def test_job(metadata):
+    metadata_utils.test_job(metadata)

test/utils/database/artifact_store.py
@@ -1,0 +1,95 @@
+import os
+import tempfile
+
+from superduper.backends.base.artifacts import ArtifactStore
+
+
+def test_bytes(artifact_store: ArtifactStore):
+    # test put_bytes
+    artifact_store.put_bytes(b"hello", "id")
+
+    # test exists
+    assert artifact_store.exists("id")
+
+    # test get_bytes
+    assert artifact_store.get_bytes("id") == b"hello"
+
+    # test put same file again
+    artifact_store.put_bytes(b"hello_new", "id")
+    assert artifact_store.get_bytes("id") == b"hello_new"
+
+
+def test_file(artifact_store: ArtifactStore):
+    _test_file(artifact_store)
+    _test_directory(artifact_store)
+
+
+def _build_directory(path):
+    tmpdir_path = os.path.join(path, "test_data")
+    os.makedirs(tmpdir_path, exist_ok=True)
+    for i in range(10):
+        file_name = f"{i}.txt"
+        file_path = os.path.join(tmpdir_path, file_name)
+
+        with open(file_path, "w") as file:
+            file.write(str(i))
+
+        for j in range(10):
+            sub_dir = os.path.join(tmpdir_path, f"subdir_{j}")
+            os.makedirs(sub_dir, exist_ok=True)
+            sub_file_path = os.path.join(sub_dir, file_name)
+            with open(sub_file_path, "w") as file:
+                file.write(f"{i} {j}")
+
+    return tmpdir_path
+
+
+def _create_file(path):
+    file_path = os.path.join(path, "test_data.txt")
+    with open(file_path, "w") as file:
+        file.write("hello")
+    return file_path
+
+
+def _test_file(artifact_store: ArtifactStore):
+    with tempfile.TemporaryDirectory() as tempdir:
+        path = _create_file(tempdir)
+        new_file_id = artifact_store.put_file(path, "id-file")
+        loaded_file_path = artifact_store.get_file(new_file_id)
+        with open(loaded_file_path, "r") as file:
+            assert file.read() == "hello"
+
+    assert artifact_store.exists(new_file_id)
+    artifact_store._delete_bytes(new_file_id)
+    assert not artifact_store.exists(new_file_id)
+
+
+def _test_directory(artifact_store: ArtifactStore):
+    import filecmp
+
+    def compare_directories(path1, path2):
+        comparison = filecmp.dircmp(path1, path2)
+        assert not comparison.diff_files
+        for sub_dir in comparison.subdirs:
+            compare_directories(
+                os.path.join(path1, sub_dir), os.path.join(path, sub_dir)
+            )
+
+        for file in comparison.common_files:
+            with open(os.path.join(path1, file), "r") as file1, open(
+                os.path.join(path2, file), "r"
+            ) as file2:
+                if file1.read() != file2.read():
+                    return False
+        return True
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        path = _build_directory(tempdir)
+        new_file_id = artifact_store.put_file(path, "id-dir")
+        loaded_file_path = artifact_store.get_file(new_file_id)
+
+        assert compare_directories(path, loaded_file_path)
+
+    assert artifact_store.exists(new_file_id)
+    artifact_store._delete_bytes(new_file_id)
+    assert not artifact_store.exists(new_file_id)

test/utils/database/databackend.py
@@ -1,0 +1,50 @@
+from superduper import CFG
+from superduper.backends.base.data_backend import BaseDataBackend
+from superduper.backends.base.query import Query
+from superduper.components.datatype import pickle_serializer
+from superduper.components.schema import Schema
+
+
+def test_output_dest(databackend: BaseDataBackend):
+    assert isinstance(databackend, BaseDataBackend)
+    # Create an output destination for the database
+    predict_id = "predict_id"
+
+    assert not databackend.check_output_dest(predict_id)
+
+    table = databackend.create_output_dest(predict_id, pickle_serializer)
+
+    assert table.identifier.startswith(CFG.output_prefix)
+
+    databackend.create_table_and_schema(table.identifier, table.schema)
+
+    assert databackend.check_output_dest(predict_id)
+
+    # Drop the output destination
+    #
+    databackend.drop_outputs()
+
+    assert not databackend.check_output_dest(predict_id)
+
+
+def test_query_builder(databackend: BaseDataBackend):
+    query = databackend.get_query_builder("datas")
+
+    assert isinstance(query, Query)
+    assert query.table == "datas"
+
+
+def test_list_tables_or_collections(databackend: BaseDataBackend):
+    fields = {
+        'a': int,
+    }
+
+    for i in range(10):
+        table_name = f"table_{i}"
+        databackend.create_table_and_schema(
+            table_name, schema=Schema(identifier="schema", fields=fields)
+        )
+
+    tables = databackend.list_tables_or_collections()
+    assert len(tables) == 10
+    assert [f"table_{i}" for i in range(10)] == sorted(tables)

Pinnacle-Networks/Pinnacle

test/unittest/backends/ibis/test_databackend.py
@@ -1,0 +1,25 @@
+from test.utils.database import databackend as db_utils
+
+import pytest
+
+from pinnacle import CFG
+from pinnacle.backends.ibis.data_backend import IbisDataBackend
+
+
+@pytest.fixture
+def databackend():
+    backend = IbisDataBackend(CFG.data_backend)
+    yield backend
+    backend.drop(True)
+
+
+def test_output_dest(databackend):
+    db_utils.test_output_dest(databackend)
+
+
+def test_query_builder(databackend):
+    db_utils.test_query_builder(databackend)
+
+
+def test_list_tables_or_collections(databackend):
+    db_utils.test_list_tables_or_collections(databackend)

test/unittest/backends/local/test_artifact_store.py
@@ -1,0 +1,20 @@
+from test.utils.database import artifact_store as artifact_store_utils
+
+import pytest
+
+from pinnacle.backends.local.artifacts import FileSystemArtifactStore
+
+
+@pytest.fixture
+def artifact_store(tmpdir):
+    artifact_store = FileSystemArtifactStore(tmpdir)
+    yield artifact_store
+    artifact_store.drop(True)
+
+
+def test_bytes(artifact_store: FileSystemArtifactStore):
+    artifact_store_utils.test_bytes(artifact_store)
+
+
+def test_file(artifact_store: FileSystemArtifactStore):
+    artifact_store_utils.test_file(artifact_store)

test/unittest/backends/mongodb/test_artifact_store.py
@@ -1,0 +1,27 @@
+from test.utils.database import artifact_store as artifact_store_utils
+
+import pytest
+
+from pinnacle import CFG
+from pinnacle.backends.mongodb.data_backend import MongoDataBackend
+
+DATABASE_URL = CFG.artifact_store or CFG.data_backend or ""
+
+
+if DATABASE_URL.split(":")[0] not in ["mongodb", "mongodb+srv"]:
+    pytest.skip("MongoDB is not configured, skipping tests...", allow_module_level=True)
+
+
+@pytest.fixture
+def artifact_store():
+    artifact_store = MongoDataBackend(DATABASE_URL).build_artifact_store()
+    yield artifact_store
+    artifact_store.drop(True)
+
+
+def test_bytes(artifact_store):
+    artifact_store_utils.test_bytes(artifact_store)
+
+
+def test_file(artifact_store):
+    artifact_store_utils.test_file(artifact_store)

test/unittest/backends/mongodb/test_databackend.py
@@ -1,0 +1,27 @@
+from test.utils.database import databackend as db_utils
+
+import pytest
+
+from pinnacle import CFG
+from pinnacle.backends.mongodb.data_backend import MongoDataBackend
+
+
+@pytest.fixture
+def databackend():
+    backend = MongoDataBackend(CFG.data_backend)
+    yield backend
+    backend.drop(True)
+
+
+@pytest.mark.skip(reason="Open this test after creating a blank table for MongoDB.")
+def test_output_dest(databackend):
+    db_utils.test_output_dest(databackend)
+
+
+def test_query_builder(databackend):
+    db_utils.test_query_builder(databackend)
+
+
+@pytest.mark.skip(reason="Open this test after creating a blank table for MongoDB.")
+def test_list_tables_or_collections(databackend):
+    db_utils.test_list_tables_or_collections(databackend)

test/unittest/backends/sqlalchemy/test_metadata.py
@@ -1,58 +1,35 @@
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+import os
+from test.utils.database import metadata as metadata_utils
 
+import pytest
+
+from pinnacle import CFG
 from pinnacle.backends.sqlalchemy.metadata import SQLAlchemyMetadata
 
-Base = declarative_base()
+DATABASE_URL = CFG.metadata_store or "sqlite:///:memory:"
 
-DATABASE_URL = "sqlite:///:memory:"
+
+# TODO: Remove skip when we move all the plugin tests to the new structure
+skip = not os.environ.get('SUPERDUPER_CONFIG', "").endswith('ibis.yaml')
+
+if skip:
+    pytest.skip("Skipping this file for now", allow_module_level=True)
 
 
 @pytest.fixture
 def metadata():
-    engine = create_engine(DATABASE_URL)
     store = SQLAlchemyMetadata(DATABASE_URL)
     yield store
     store.drop(force=True)
-    engine.dispose()
-    Base.metadata.drop_all(engine)
 
 
-def test(metadata):
-    import uuid
+def test_component(metadata):
+    metadata_utils.test_component(metadata)
 
-    metadata.create_component(
-        {
-            'identifier': 'my-model',
-            'type_id': 'model',
-            'version': 0,
-            '_path': 'pinnacle.container.model.Model',
-            'uuid': str(uuid.uuid4()),
-        }
-    )
 
-    comps = metadata.show_components('model')
-    assert comps == ['my-model']
+def test_parent_child(metadata):
+    metadata_utils.test_parent_child(metadata)
 
-    metadata.create_component(
-        {
-            'identifier': 'other-model',
-            'type_id': 'model',
-            'version': 0,
-            '_path': 'pinnacle.container.model.Model',
-            'uuid': str(uuid.uuid4()),
-        }
-    )
 
-    comps = metadata.show_components('model')
-
-    r = metadata.get_component(
-        type_id='model',
-        identifier='other-model',
-        version=0,
-        allow_hidden=True,
-    )
-
-    assert r['identifier'] == 'other-model'
-    assert r['version'] == 0
+def test_job(metadata):
+    metadata_utils.test_job(metadata)

test/utils/database/artifact_store.py
@@ -1,0 +1,95 @@
+import os
+import tempfile
+
+from pinnacle.backends.base.artifacts import ArtifactStore
+
+
+def test_bytes(artifact_store: ArtifactStore):
+    # test put_bytes
+    artifact_store.put_bytes(b"hello", "id")
+
+    # test exists
+    assert artifact_store.exists("id")
+
+    # test get_bytes
+    assert artifact_store.get_bytes("id") == b"hello"
+
+    # test put same file again
+    artifact_store.put_bytes(b"hello_new", "id")
+    assert artifact_store.get_bytes("id") == b"hello_new"
+
+
+def test_file(artifact_store: ArtifactStore):
+    _test_file(artifact_store)
+    _test_directory(artifact_store)
+
+
+def _build_directory(path):
+    tmpdir_path = os.path.join(path, "test_data")
+    os.makedirs(tmpdir_path, exist_ok=True)
+    for i in range(10):
+        file_name = f"{i}.txt"
+        file_path = os.path.join(tmpdir_path, file_name)
+
+        with open(file_path, "w") as file:
+            file.write(str(i))
+
+        for j in range(10):
+            sub_dir = os.path.join(tmpdir_path, f"subdir_{j}")
+            os.makedirs(sub_dir, exist_ok=True)
+            sub_file_path = os.path.join(sub_dir, file_name)
+            with open(sub_file_path, "w") as file:
+                file.write(f"{i} {j}")
+
+    return tmpdir_path
+
+
+def _create_file(path):
+    file_path = os.path.join(path, "test_data.txt")
+    with open(file_path, "w") as file:
+        file.write("hello")
+    return file_path
+
+
+def _test_file(artifact_store: ArtifactStore):
+    with tempfile.TemporaryDirectory() as tempdir:
+        path = _create_file(tempdir)
+        new_file_id = artifact_store.put_file(path, "id-file")
+        loaded_file_path = artifact_store.get_file(new_file_id)
+        with open(loaded_file_path, "r") as file:
+            assert file.read() == "hello"
+
+    assert artifact_store.exists(new_file_id)
+    artifact_store._delete_bytes(new_file_id)
+    assert not artifact_store.exists(new_file_id)
+
+
+def _test_directory(artifact_store: ArtifactStore):
+    import filecmp
+
+    def compare_directories(path1, path2):
+        comparison = filecmp.dircmp(path1, path2)
+        assert not comparison.diff_files
+        for sub_dir in comparison.subdirs:
+            compare_directories(
+                os.path.join(path1, sub_dir), os.path.join(path, sub_dir)
+            )
+
+        for file in comparison.common_files:
+            with open(os.path.join(path1, file), "r") as file1, open(
+                os.path.join(path2, file), "r"
+            ) as file2:
+                if file1.read() != file2.read():
+                    return False
+        return True
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        path = _build_directory(tempdir)
+        new_file_id = artifact_store.put_file(path, "id-dir")
+        loaded_file_path = artifact_store.get_file(new_file_id)
+
+        assert compare_directories(path, loaded_file_path)
+
+    assert artifact_store.exists(new_file_id)
+    artifact_store._delete_bytes(new_file_id)
+    assert not artifact_store.exists(new_file_id)

test/utils/database/databackend.py
@@ -1,0 +1,50 @@
+from pinnacle import CFG
+from pinnacle.backends.base.data_backend import BaseDataBackend
+from pinnacle.backends.base.query import Query
+from pinnacle.components.datatype import pickle_serializer
+from pinnacle.components.schema import Schema
+
+
+def test_output_dest(databackend: BaseDataBackend):
+    assert isinstance(databackend, BaseDataBackend)
+    # Create an output destination for the database
+    predict_id = "predict_id"
+
+    assert not databackend.check_output_dest(predict_id)
+
+    table = databackend.create_output_dest(predict_id, pickle_serializer)
+
+    assert table.identifier.startswith(CFG.output_prefix)
+
+    databackend.create_table_and_schema(table.identifier, table.schema)
+
+    assert databackend.check_output_dest(predict_id)
+
+    # Drop the output destination
+    #
+    databackend.drop_outputs()
+
+    assert not databackend.check_output_dest(predict_id)
+
+
+def test_query_builder(databackend: BaseDataBackend):
+    query = databackend.get_query_builder("datas")
+
+    assert isinstance(query, Query)
+    assert query.table == "datas"
+
+
+def test_list_tables_or_collections(databackend: BaseDataBackend):
+    fields = {
+        'a': int,
+    }
+
+    for i in range(10):
+        table_name = f"table_{i}"
+        databackend.create_table_and_schema(
+            table_name, schema=Schema(identifier="schema", fields=fields)
+        )
+
+    tables = databackend.list_tables_or_collections()
+    assert len(tables) == 10
+    assert [f"table_{i}" for i in range(10)] == sorted(tables)