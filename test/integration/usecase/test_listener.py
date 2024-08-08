superduper-io/superduper

Add usecase test utils

test/integration/usecase/test_listener.py
@@ -1,0 +1,15 @@
+import typing as t
+
+if t.TYPE_CHECKING:
+    from superduper.base.datalayer import Datalayer
+
+from test.utils.usecase.chain_listener import build_chain_listener
+from test.utils.usecase.graph_listener import build_graph_listener
+
+
+def test_chain_listener(db: "Datalayer"):
+    build_chain_listener(db)
+
+
+def test_graph_listener(db: "Datalayer"):
+    build_graph_listener(db)

test/integration/usecase/test_vector_index.py
@@ -1,0 +1,52 @@
+import typing as t
+
+if t.TYPE_CHECKING:
+    from superduper.base.datalayer import Datalayer
+
+from test.utils.usecase.vector_search import add_data, build_vector_index
+
+
+def test_vector_index(db: "Datalayer"):
+    def check_result(out, sample_data):
+        scores = out.scores
+
+        ids = [o[primary_id] for o in list(out)]
+        assert len(ids) == 10
+
+        assert sample_data[primary_id] in ids
+        assert scores[str(sample_data[primary_id])] > 0.999999
+
+    build_vector_index(db)
+    vector_index = "vector_index"
+    table = db["documents"]
+    primary_id = table.primary_id
+    sample_data = next(table.select().filter(table['x'] == 50).execute())
+
+    # test indexing vector search
+    out = (
+        table.like({"x": sample_data["x"]}, vector_index=vector_index, n=10)
+        .select()
+        .execute()
+    )
+
+    check_result(out, sample_data)
+
+    # test compatible vector_search
+    out = (
+        table.like({"y": -sample_data["x"]}, vector_index=vector_index, n=10)
+        .select()
+        .execute()
+    )
+    check_result(out, sample_data)
+
+    # test adding new data
+    out = table.like({"x": 150}, vector_index=vector_index, n=10).select().execute()
+
+    assert sum(out.scores.values()) == 0
+
+    add_data(db, 100, 200)
+
+    out = table.like({"x": 150}, vector_index=vector_index, n=1).select().execute()
+    result = next(out)
+    assert result['x'] == 150
+    assert result['score'] > 0.999999

test/utils/usecase/graph_listener.py
@@ -1,0 +1,134 @@
+import typing as t
+
+import numpy as np
+
+from superduper import Document, ObjectModel
+
+if t.TYPE_CHECKING:
+    from superduper.base.datalayer import Datalayer
+
+
+def _check_equal(a, b):
+    if not isinstance(a, type(b)):
+        return False
+
+    if isinstance(a, np.ndarray):
+        return np.array_equal(a, b)
+
+    if isinstance(a, t.Mapping):
+        if set(a.keys()) != set(b.keys()):
+            return False
+
+        for k, v in a.items():
+            if not _check_equal(v, b[k]):
+                return False
+        else:
+            return True
+
+    return a == b
+
+
+def build_graph_listener(db: "Datalayer"):
+    db.cfg.auto_schema = True
+    data = [
+        {"x": 1, "y": "2", "z": np.array([1, 2, 3])},
+        {"x": 2, "y": "3", "z": np.array([4, 5, 6])},
+        {"x": 3, "y": "4", "z": np.array([7, 8, 9])},
+    ]
+
+    db["documents"].insert(data).execute()
+
+    def func_a(x):
+        return {"x": x, "model": "a"}
+
+    primary_id = db["documents"].primary_id
+
+    model_a = ObjectModel(identifier="model_a", object=func_a)
+
+    listener_a = model_a.to_listener(
+        key="x",
+        select=db["documents"].select(),
+        identifier="listener_a",
+        uuid="a",
+        predict_kwargs={"max_chunk_size": 1},
+    )
+
+    def func_b(x, y, o_a):
+        return {"x": x, "y": y, "o_a": o_a, "model": "b"}
+
+    model_b = ObjectModel(identifier="model_b", object=func_b)
+    listener_b = model_b.to_listener(
+        key=("x", "y", "_outputs__a"),
+        select=db["documents"].select(primary_id, "x", "y").outputs("a"),
+        identifier="listener_b",
+        uuid="b",
+        predict_kwargs={"max_chunk_size": 1},
+    )
+
+    def func_c(x, y, z, o_a, o_b):
+        return {"x": x, "y": y, "z": z, "o_a": o_a, "o_b": o_b, "model": "c"}
+
+    model_c = ObjectModel(identifier="model_c", object=func_c)
+    listener_c = model_c.to_listener(
+        key=("x", "y", "z", "_outputs__a", "_outputs__b"),
+        select=db["documents"].select(primary_id, "x", "y", "z").outputs("a", "b"),
+        identifier="listener_c",
+        uuid="c",
+        predict_kwargs={"max_chunk_size": 1},
+    )
+
+    db.apply(listener_a)
+    db.apply(listener_b)
+    db.apply(listener_c)
+
+    data = Document(
+        list(db["documents"].select().outputs("a", "b", "c").execute())[0].unpack()
+    )
+
+    output_a = data["_outputs__a"]
+    output_b = data["_outputs__b"]
+    output_c = data["_outputs__c"]
+
+    assert _check_equal(output_a, {"x": 1, "model": "a"})
+    assert _check_equal(output_b, {"x": 1, "y": "2", "o_a": output_a, "model": "b"})
+    assert _check_equal(
+        output_c,
+        {
+            "x": 1,
+            "y": "2",
+            "z": np.array([1, 2, 3]),
+            "o_a": output_a,
+            "o_b": output_b,
+            "model": "c",
+        },
+    )
+
+    new_data = [
+        {"x": 4, "y": "5", "z": np.array([10, 11, 12])},
+        {"x": 5, "y": "6", "z": np.array([13, 14, 15])},
+        {"x": 6, "y": "7", "z": np.array([16, 17, 18])},
+    ]
+
+    db["documents"].insert(new_data).execute()
+
+    new_data = Document(
+        list(db["documents"].select().outputs("a", "b", "c").execute())[-1].unpack()
+    )
+
+    output_a = new_data["_outputs__a"]
+    output_b = new_data["_outputs__b"]
+    output_c = new_data["_outputs__c"]
+
+    assert _check_equal(output_a, {"x": 6, "model": "a"})
+    assert _check_equal(output_b, {"x": 6, "y": "7", "o_a": output_a, "model": "b"})
+    assert _check_equal(
+        output_c,
+        {
+            "x": 6,
+            "y": "7",
+            "z": np.array([16, 17, 18]),
+            "o_a": output_a,
+            "o_b": output_b,
+            "model": "c",
+        },
+    )

test/utils/usecase/vector_search.py
@@ -1,0 +1,68 @@
+import typing as t
+
+import numpy as np
+
+if t.TYPE_CHECKING:
+    from superduper.base.datalayer import Datalayer
+
+VECTOR_SIZE = 300
+
+
+def add_data(db: "Datalayer", start: int, end: int):
+    data = []
+    for i in range(start, end):
+        data.append(
+            {
+                "x": i,
+                "label": int(i % 2 == 0),
+            }
+        )
+    db["documents"].insert(data).execute()
+
+
+def build_vector_index(db: "Datalayer"):
+    from superduper import ObjectModel, VectorIndex
+
+    db.cfg.auto_schema = True
+
+    add_data(db, 0, 100)
+
+    def predict(x):
+        vector = [0] * VECTOR_SIZE
+        for offset in range(5):
+            if offset + x < VECTOR_SIZE:
+                vector[offset + x] = 1
+
+            if x - offset >= 0:
+                vector[x - offset] = 1
+
+        return np.array(vector)
+
+    indexing_model = ObjectModel(
+        identifier="model",
+        object=predict,
+    )
+
+    indexing_model = indexing_model.to_listener(
+        key="x",
+        select=db["documents"].select(),
+        uuid="vector",
+    )
+
+    compatible_model = ObjectModel(
+        identifier="model-y",
+        object=lambda y: predict(-y),
+    )
+
+    compatible_listener = compatible_model.to_listener(
+        key="y",
+        select=None,
+    )
+
+    vector_index = VectorIndex(
+        identifier="vector_index",
+        indexing_listener=indexing_model,
+        compatible_listener=compatible_listener,
+    )
+
+    db.apply(vector_index)

Pinnacle-Networks/Pinnacle

test/integration/usecase/test_listener.py
@@ -1,0 +1,15 @@
+import typing as t
+
+if t.TYPE_CHECKING:
+    from pinnacle.base.datalayer import Datalayer
+
+from test.utils.usecase.chain_listener import build_chain_listener
+from test.utils.usecase.graph_listener import build_graph_listener
+
+
+def test_chain_listener(db: "Datalayer"):
+    build_chain_listener(db)
+
+
+def test_graph_listener(db: "Datalayer"):
+    build_graph_listener(db)

test/integration/usecase/test_vector_index.py
@@ -1,0 +1,52 @@
+import typing as t
+
+if t.TYPE_CHECKING:
+    from pinnacle.base.datalayer import Datalayer
+
+from test.utils.usecase.vector_search import add_data, build_vector_index
+
+
+def test_vector_index(db: "Datalayer"):
+    def check_result(out, sample_data):
+        scores = out.scores
+
+        ids = [o[primary_id] for o in list(out)]
+        assert len(ids) == 10
+
+        assert sample_data[primary_id] in ids
+        assert scores[str(sample_data[primary_id])] > 0.999999
+
+    build_vector_index(db)
+    vector_index = "vector_index"
+    table = db["documents"]
+    primary_id = table.primary_id
+    sample_data = next(table.select().filter(table['x'] == 50).execute())
+
+    # test indexing vector search
+    out = (
+        table.like({"x": sample_data["x"]}, vector_index=vector_index, n=10)
+        .select()
+        .execute()
+    )
+
+    check_result(out, sample_data)
+
+    # test compatible vector_search
+    out = (
+        table.like({"y": -sample_data["x"]}, vector_index=vector_index, n=10)
+        .select()
+        .execute()
+    )
+    check_result(out, sample_data)
+
+    # test adding new data
+    out = table.like({"x": 150}, vector_index=vector_index, n=10).select().execute()
+
+    assert sum(out.scores.values()) == 0
+
+    add_data(db, 100, 200)
+
+    out = table.like({"x": 150}, vector_index=vector_index, n=1).select().execute()
+    result = next(out)
+    assert result['x'] == 150
+    assert result['score'] > 0.999999

test/utils/usecase/graph_listener.py
@@ -1,0 +1,134 @@
+import typing as t
+
+import numpy as np
+
+from pinnacle import Document, ObjectModel
+
+if t.TYPE_CHECKING:
+    from pinnacle.base.datalayer import Datalayer
+
+
+def _check_equal(a, b):
+    if not isinstance(a, type(b)):
+        return False
+
+    if isinstance(a, np.ndarray):
+        return np.array_equal(a, b)
+
+    if isinstance(a, t.Mapping):
+        if set(a.keys()) != set(b.keys()):
+            return False
+
+        for k, v in a.items():
+            if not _check_equal(v, b[k]):
+                return False
+        else:
+            return True
+
+    return a == b
+
+
+def build_graph_listener(db: "Datalayer"):
+    db.cfg.auto_schema = True
+    data = [
+        {"x": 1, "y": "2", "z": np.array([1, 2, 3])},
+        {"x": 2, "y": "3", "z": np.array([4, 5, 6])},
+        {"x": 3, "y": "4", "z": np.array([7, 8, 9])},
+    ]
+
+    db["documents"].insert(data).execute()
+
+    def func_a(x):
+        return {"x": x, "model": "a"}
+
+    primary_id = db["documents"].primary_id
+
+    model_a = ObjectModel(identifier="model_a", object=func_a)
+
+    listener_a = model_a.to_listener(
+        key="x",
+        select=db["documents"].select(),
+        identifier="listener_a",
+        uuid="a",
+        predict_kwargs={"max_chunk_size": 1},
+    )
+
+    def func_b(x, y, o_a):
+        return {"x": x, "y": y, "o_a": o_a, "model": "b"}
+
+    model_b = ObjectModel(identifier="model_b", object=func_b)
+    listener_b = model_b.to_listener(
+        key=("x", "y", "_outputs__a"),
+        select=db["documents"].select(primary_id, "x", "y").outputs("a"),
+        identifier="listener_b",
+        uuid="b",
+        predict_kwargs={"max_chunk_size": 1},
+    )
+
+    def func_c(x, y, z, o_a, o_b):
+        return {"x": x, "y": y, "z": z, "o_a": o_a, "o_b": o_b, "model": "c"}
+
+    model_c = ObjectModel(identifier="model_c", object=func_c)
+    listener_c = model_c.to_listener(
+        key=("x", "y", "z", "_outputs__a", "_outputs__b"),
+        select=db["documents"].select(primary_id, "x", "y", "z").outputs("a", "b"),
+        identifier="listener_c",
+        uuid="c",
+        predict_kwargs={"max_chunk_size": 1},
+    )
+
+    db.apply(listener_a)
+    db.apply(listener_b)
+    db.apply(listener_c)
+
+    data = Document(
+        list(db["documents"].select().outputs("a", "b", "c").execute())[0].unpack()
+    )
+
+    output_a = data["_outputs__a"]
+    output_b = data["_outputs__b"]
+    output_c = data["_outputs__c"]
+
+    assert _check_equal(output_a, {"x": 1, "model": "a"})
+    assert _check_equal(output_b, {"x": 1, "y": "2", "o_a": output_a, "model": "b"})
+    assert _check_equal(
+        output_c,
+        {
+            "x": 1,
+            "y": "2",
+            "z": np.array([1, 2, 3]),
+            "o_a": output_a,
+            "o_b": output_b,
+            "model": "c",
+        },
+    )
+
+    new_data = [
+        {"x": 4, "y": "5", "z": np.array([10, 11, 12])},
+        {"x": 5, "y": "6", "z": np.array([13, 14, 15])},
+        {"x": 6, "y": "7", "z": np.array([16, 17, 18])},
+    ]
+
+    db["documents"].insert(new_data).execute()
+
+    new_data = Document(
+        list(db["documents"].select().outputs("a", "b", "c").execute())[-1].unpack()
+    )
+
+    output_a = new_data["_outputs__a"]
+    output_b = new_data["_outputs__b"]
+    output_c = new_data["_outputs__c"]
+
+    assert _check_equal(output_a, {"x": 6, "model": "a"})
+    assert _check_equal(output_b, {"x": 6, "y": "7", "o_a": output_a, "model": "b"})
+    assert _check_equal(
+        output_c,
+        {
+            "x": 6,
+            "y": "7",
+            "z": np.array([16, 17, 18]),
+            "o_a": output_a,
+            "o_b": output_b,
+            "model": "c",
+        },
+    )

test/utils/usecase/vector_search.py
@@ -1,0 +1,68 @@
+import typing as t
+
+import numpy as np
+
+if t.TYPE_CHECKING:
+    from pinnacle.base.datalayer import Datalayer
+
+VECTOR_SIZE = 300
+
+
+def add_data(db: "Datalayer", start: int, end: int):
+    data = []
+    for i in range(start, end):
+        data.append(
+            {
+                "x": i,
+                "label": int(i % 2 == 0),
+            }
+        )
+    db["documents"].insert(data).execute()
+
+
+def build_vector_index(db: "Datalayer"):
+    from pinnacle import ObjectModel, VectorIndex
+
+    db.cfg.auto_schema = True
+
+    add_data(db, 0, 100)
+
+    def predict(x):
+        vector = [0] * VECTOR_SIZE
+        for offset in range(5):
+            if offset + x < VECTOR_SIZE:
+                vector[offset + x] = 1
+
+            if x - offset >= 0:
+                vector[x - offset] = 1
+
+        return np.array(vector)
+
+    indexing_model = ObjectModel(
+        identifier="model",
+        object=predict,
+    )
+
+    indexing_model = indexing_model.to_listener(
+        key="x",
+        select=db["documents"].select(),
+        uuid="vector",
+    )
+
+    compatible_model = ObjectModel(
+        identifier="model-y",
+        object=lambda y: predict(-y),
+    )
+
+    compatible_listener = compatible_model.to_listener(
+        key="y",
+        select=None,
+    )
+
+    vector_index = VectorIndex(
+        identifier="vector_index",
+        indexing_listener=indexing_model,
+        compatible_listener=compatible_listener,
+    )
+
+    db.apply(vector_index)