SuperDuperDB/superduper

Fix the tests regarding default.yaml after migration.

superduper/base/document.py
@@ -5,7 +5,12 @@ from bson.objectid import ObjectId
 
 from superduper import logging
 from superduper.base.code import Code
-from superduper.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
+from superduper.base.constant import (
+    KEY_BLOBS,
+    KEY_BUILDS,
+    KEY_FILES,
+    KEY_SCHEMA,
+)
 from superduper.base.leaf import Leaf, import_item
 from superduper.base.variables import _replace_variables
 from superduper.components.component import Component
@@ -18,7 +23,7 @@ from superduper.components.datatype import (
     Native,
     _BaseEncodable,
 )
-from superduper.components.schema import SCHEMA_KEY, Schema, get_schema
+from superduper.components.schema import Schema, get_schema
 from superduper.misc.reference import parse_reference
 from superduper.misc.special_dicts import MongoStyleDict, SuperDuperFlatEncode
 
@@ -156,7 +161,7 @@ class Document(MongoStyleDict):
             r = _replace_variables(
                 {k: v for k, v in r.items() if k != '_variables'}, **r['_variables']
             )
-        schema = schema or r.get(SCHEMA_KEY)
+        schema = schema or r.get(KEY_SCHEMA)
         schema = get_schema(db, schema)
 
         builds = r.get(KEY_BUILDS, {})
@@ -468,7 +473,7 @@ def _schema_decode(
         else:
             decoded[k] = field.decode_data(data[k])
 
-    decoded.pop(SCHEMA_KEY, None)
+    decoded.pop(KEY_SCHEMA, None)
     return decoded
 
 

superduper/components/table.py
@@ -2,7 +2,7 @@ import typing as t
 
 from superduper import CFG
 from superduper.components.component import Component
-from superduper.components.schema import Schema, _Native
+from superduper.components.schema import Schema
 
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
@@ -26,18 +26,10 @@ class Table(Component):
     def __post_init__(self, db, artifacts):
         super().__post_init__(db, artifacts)
         fields = {}
+        fields.update(self.schema.fields)
+
         if '_fold' not in self.schema.fields:
-            # from superduper.backends.ibis.field_types import dtype
-
-            # fields.update({'_fold': dtype('str')})
             fields.update({'_fold': 'str'})
-
-        for k, v in self.schema.fields.items():
-            if isinstance(v, _Native):
-                # fields[k] = dtype(v.identifier)
-                fields[k] = v.identifier
-            else:
-                fields[k] = v
 
         self.schema = Schema(
             self.schema.identifier,

plugins/openai/superduper_openai/model.py
@@ -14,14 +14,10 @@ from openai import (
     RateLimitError,
 )
 from openai._types import NOT_GIVEN
-
-from superduper import CFG
-from superduper.backends.ibis.data_backend import IbisDataBackend
-from superduper.backends.ibis.field_types import dtype
 from superduper.backends.query_dataset import QueryDataset
 from superduper.base.datalayer import Datalayer
 from superduper.components.model import APIBaseModel, Inputs
-from superduper.components.vector_index import sqlvector, vector
+from superduper.components.vector_index import vector
 from superduper.misc.compat import cache
 from superduper.misc.retry import Retry
 
@@ -121,19 +117,12 @@ class OpenAIEmbedding(_OpenAI):
     def pre_create(self, db):
         """Pre creates the model.
 
-        If the datatype is not set and the datalayer is an IbisDataBackend,
-        the datatype is set to ``sqlvector`` or ``vector``.
+        the datatype is set to ``vector``.
 
         :param db: The datalayer instance.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend):
-            if self.datatype is None:
-                self.datatype = sqlvector(
-                    shape=self.shape, bytes_encoding=CFG.bytes_encoding
-                )
-        elif self.datatype is None:
-            self.datatype = vector(shape=self.shape)
+        self.datatype = self.datatype or vector(shape=self.shape)
 
     @retry
     def predict(self, X: str):
@@ -176,14 +165,10 @@ class OpenAIChatCompletion(_OpenAI):
     def pre_create(self, db: Datalayer) -> None:
         """Pre creates the model.
 
-        If the datatype is not set and the datalayer is an IbisDataBackend,
-        the datatype is set to ``dtype('str')``.
-
         :param db: The datalayer instance.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend) and self.datatype is None:
-            self.datatype = dtype('str')
+        self.datatype = self.datatype or 'str'
 
     @retry
     def predict(self, X: str, context: t.Optional[str] = None, **kwargs):
@@ -237,14 +222,10 @@ class OpenAIImageCreation(_OpenAI):
     def pre_create(self, db: Datalayer) -> None:
         """Pre creates the model.
 
-        If the datatype is not set and the datalayer is an IbisDataBackend,
-        the datatype is set to ``dtype('bytes')``.
-
         :param db: The datalayer instance.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend) and self.datatype is None:
-            self.datatype = dtype('bytes')
+        self.datatype = self.datatype or 'bytes'
 
     def _format_prompt(self, context, X):
         prompt = self.prompt.format(context='\n'.join(context))
@@ -311,14 +292,10 @@ class OpenAIImageEdit(_OpenAI):
     def pre_create(self, db: Datalayer) -> None:
         """Pre creates the model.
 
-        If the datatype is not set and the datalayer is an IbisDataBackend,
-        the datatype is set to ``dtype('bytes')``.
-
         :param db: The datalayer instance.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend) and self.datatype is None:
-            self.datatype = dtype('bytes')
+        self.datatype = self.datatype or 'bytes'
 
     @retry
     def predict(
@@ -396,14 +373,10 @@ class OpenAIAudioTranscription(_OpenAI):
     def pre_create(self, db: Datalayer) -> None:
         """Pre creates the model.
 
-        If the datatype is not set and the datalayer is an IbisDataBackend,
-        the datatype is set to ``dtype('str')``.
-
         :param db: The datalayer instance.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend) and self.datatype is None:
-            self.datatype = dtype('str')
+        self.datatype = self.datatype or 'str'
 
     @retry
     def predict(self, file: t.BinaryIO, context: t.Optional[t.List[str]] = None):
@@ -455,8 +428,7 @@ class OpenAIAudioTranslation(_OpenAI):
         :param db: The datalayer to use for the model.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend) and self.datatype is None:
-            self.datatype = dtype('str')
+        self.datatype = self.datatype or 'str'
 
     @retry
     def predict(

test/integration/ext/openai/test_model_openai.py
@@ -4,9 +4,7 @@ import os
 import openai
 import pytest
 import vcr
-from vcr.stubs import httpx_stubs
-
-from superduper.ext.openai.model import (
+from superduper_openai.model import (
     OpenAIAudioTranscription,
     OpenAIAudioTranslation,
     OpenAIChatCompletion,
@@ -15,6 +13,7 @@ from superduper.ext.openai.model import (
     OpenAIImageEdit,
     _available_models,
 )
+from vcr.stubs import httpx_stubs
 
 PNG_BYTE_SIGNATURE = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR'
 

test/unittest/base/test_leaf.py
@@ -3,7 +3,7 @@ import typing as t
 from pprint import pprint
 
 from superduper import ObjectModel
-from superduper.backends.mongodb.query import MongoQuery
+from superduper.backends.base.query import Query
 from superduper.base.constant import KEY_BUILDS
 from superduper.base.document import Document
 from superduper.base.leaf import Leaf
@@ -79,7 +79,7 @@ def test_encode_leaf_with_children():
 
 def test_save_variables_2():
     query = (
-        MongoQuery(table='documents')
+        Query(table='documents')
         .like({'x': '<var:X>'}, vector_index='test')
         .find({'x': {'$regex': '^test/1'}})
     )
@@ -117,18 +117,17 @@ def test_component_with_document():
 
 def test_find_variables():
     from superduper import Document
-    from superduper.backends.mongodb import MongoQuery
 
     r = Document({'txt': '<var:test>'})
 
     assert r.variables == ['test']
 
-    q = MongoQuery(table='test').find_one(Document({'txt': '<var:test>'}))
+    q = Query(table='test').find_one(Document({'txt': '<var:test>'}))
 
     assert q.variables == ['test']
 
     q = (
-        MongoQuery(table='test')
+        Query(table='test')
         .like(Document({'txt': '<var:test>'}), vector_index='test')
         .find()
         .limit(5)

test/unittest/ext/llm/utils.py
@@ -3,12 +3,11 @@ LLM model test cases.
 All the llm model can use the check_xxx func to test the intergration with db.
 """
 
-from superduper.backends.ibis.field_types import dtype
-from superduper.backends.ibis.query import Schema
 from superduper.backends.mongodb.data_backend import MongoDataBackend
 from superduper.backends.mongodb.query import MongoQuery
 from superduper.base.document import Document
 from superduper.components.listener import Listener
+from superduper.components.schema import Schema
 from superduper.components.table import Table
 
 
@@ -22,25 +21,28 @@ def check_predict(db, llm):
 def check_llm_as_listener_model(db, llm):
     """Test whether the model can predict the data in the database normally"""
     collection_name = "question"
+    db.cfg.auto_schema = True
     datas = [
         Document({"question": f"1+{i}=", "id": str(i), '_fold': 'train'})
         for i in range(10)
     ]
-    if isinstance(db.databackend.type, MongoDataBackend):
-        db.execute(MongoQuery(table=collection_name).insert_many(datas))
-        select = MongoQuery(table=collection_name).find()
-    else:
-        schema = Schema(
-            identifier=collection_name,
-            fields={
-                "id": dtype("str"),
-                "question": dtype("str"),
-            },
-        )
-        table = Table(identifier=collection_name, schema=schema)
-        db.add(table)
-        db.execute(db[collection_name].insert(datas))
-        select = db[collection_name].select("id", "question")
+    db[collection_name].insert(datas).execute()
+    select = db[collection_name].select("id", "question")
+    # if isinstance(db.databackend.type, MongoDataBackend):
+    #     db.execute(MongoQuery(table=collection_name).insert_many(datas))
+    #     select = MongoQuery(table=collection_name).find()
+    # else:
+    #     schema = Schema(
+    #         identifier=collection_name,
+    #         fields={
+    #             "id": "str",
+    #             "question": "str",
+    #         },
+    #     )
+    #     table = Table(identifier=collection_name, schema=schema)
+    #     db.add(table)
+    #     db.execute(db[collection_name].insert(datas))
+    #     select = db[collection_name].select("id", "question")
 
     listener = Listener(
         select=select,

Pinnacle-Networks/Pinnacle

pinnacle/base/document.py
@@ -5,7 +5,12 @@ from bson.objectid import ObjectId
 
 from pinnacle import logging
 from pinnacle.base.code import Code
-from pinnacle.base.constant import KEY_BLOBS, KEY_BUILDS, KEY_FILES
+from pinnacle.base.constant import (
+    KEY_BLOBS,
+    KEY_BUILDS,
+    KEY_FILES,
+    KEY_SCHEMA,
+)
 from pinnacle.base.leaf import Leaf, import_item
 from pinnacle.base.variables import _replace_variables
 from pinnacle.components.component import Component
@@ -18,7 +23,7 @@ from pinnacle.components.datatype import (
     Native,
     _BaseEncodable,
 )
-from pinnacle.components.schema import SCHEMA_KEY, Schema, get_schema
+from pinnacle.components.schema import Schema, get_schema
 from pinnacle.misc.reference import parse_reference
 from pinnacle.misc.special_dicts import MongoStyleDict, SuperDuperFlatEncode
 
@@ -156,7 +161,7 @@ class Document(MongoStyleDict):
             r = _replace_variables(
                 {k: v for k, v in r.items() if k != '_variables'}, **r['_variables']
             )
-        schema = schema or r.get(SCHEMA_KEY)
+        schema = schema or r.get(KEY_SCHEMA)
         schema = get_schema(db, schema)
 
         builds = r.get(KEY_BUILDS, {})
@@ -468,7 +473,7 @@ def _schema_decode(
         else:
             decoded[k] = field.decode_data(data[k])
 
-    decoded.pop(SCHEMA_KEY, None)
+    decoded.pop(KEY_SCHEMA, None)
     return decoded
 
 

pinnacle/components/table.py
@@ -2,7 +2,7 @@ import typing as t
 
 from pinnacle import CFG
 from pinnacle.components.component import Component
-from pinnacle.components.schema import Schema, _Native
+from pinnacle.components.schema import Schema
 
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer
@@ -26,18 +26,10 @@ class Table(Component):
     def __post_init__(self, db, artifacts):
         super().__post_init__(db, artifacts)
         fields = {}
+        fields.update(self.schema.fields)
+
         if '_fold' not in self.schema.fields:
-            # from pinnacle.backends.ibis.field_types import dtype
-
-            # fields.update({'_fold': dtype('str')})
             fields.update({'_fold': 'str'})
-
-        for k, v in self.schema.fields.items():
-            if isinstance(v, _Native):
-                # fields[k] = dtype(v.identifier)
-                fields[k] = v.identifier
-            else:
-                fields[k] = v
 
         self.schema = Schema(
             self.schema.identifier,

plugins/openai/superduper_openai/model.py
@@ -14,14 +14,10 @@ from openai import (
     RateLimitError,
 )
 from openai._types import NOT_GIVEN
-
-from pinnacle import CFG
-from pinnacle.backends.ibis.data_backend import IbisDataBackend
-from pinnacle.backends.ibis.field_types import dtype
 from pinnacle.backends.query_dataset import QueryDataset
 from pinnacle.base.datalayer import Datalayer
 from pinnacle.components.model import APIBaseModel, Inputs
-from pinnacle.components.vector_index import sqlvector, vector
+from pinnacle.components.vector_index import vector
 from pinnacle.misc.compat import cache
 from pinnacle.misc.retry import Retry
 
@@ -121,19 +117,12 @@ class OpenAIEmbedding(_OpenAI):
     def pre_create(self, db):
         """Pre creates the model.
 
-        If the datatype is not set and the datalayer is an IbisDataBackend,
-        the datatype is set to ``sqlvector`` or ``vector``.
+        the datatype is set to ``vector``.
 
         :param db: The datalayer instance.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend):
-            if self.datatype is None:
-                self.datatype = sqlvector(
-                    shape=self.shape, bytes_encoding=CFG.bytes_encoding
-                )
-        elif self.datatype is None:
-            self.datatype = vector(shape=self.shape)
+        self.datatype = self.datatype or vector(shape=self.shape)
 
     @retry
     def predict(self, X: str):
@@ -176,14 +165,10 @@ class OpenAIChatCompletion(_OpenAI):
     def pre_create(self, db: Datalayer) -> None:
         """Pre creates the model.
 
-        If the datatype is not set and the datalayer is an IbisDataBackend,
-        the datatype is set to ``dtype('str')``.
-
         :param db: The datalayer instance.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend) and self.datatype is None:
-            self.datatype = dtype('str')
+        self.datatype = self.datatype or 'str'
 
     @retry
     def predict(self, X: str, context: t.Optional[str] = None, **kwargs):
@@ -237,14 +222,10 @@ class OpenAIImageCreation(_OpenAI):
     def pre_create(self, db: Datalayer) -> None:
         """Pre creates the model.
 
-        If the datatype is not set and the datalayer is an IbisDataBackend,
-        the datatype is set to ``dtype('bytes')``.
-
         :param db: The datalayer instance.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend) and self.datatype is None:
-            self.datatype = dtype('bytes')
+        self.datatype = self.datatype or 'bytes'
 
     def _format_prompt(self, context, X):
         prompt = self.prompt.format(context='\n'.join(context))
@@ -311,14 +292,10 @@ class OpenAIImageEdit(_OpenAI):
     def pre_create(self, db: Datalayer) -> None:
         """Pre creates the model.
 
-        If the datatype is not set and the datalayer is an IbisDataBackend,
-        the datatype is set to ``dtype('bytes')``.
-
         :param db: The datalayer instance.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend) and self.datatype is None:
-            self.datatype = dtype('bytes')
+        self.datatype = self.datatype or 'bytes'
 
     @retry
     def predict(
@@ -396,14 +373,10 @@ class OpenAIAudioTranscription(_OpenAI):
     def pre_create(self, db: Datalayer) -> None:
         """Pre creates the model.
 
-        If the datatype is not set and the datalayer is an IbisDataBackend,
-        the datatype is set to ``dtype('str')``.
-
         :param db: The datalayer instance.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend) and self.datatype is None:
-            self.datatype = dtype('str')
+        self.datatype = self.datatype or 'str'
 
     @retry
     def predict(self, file: t.BinaryIO, context: t.Optional[t.List[str]] = None):
@@ -455,8 +428,7 @@ class OpenAIAudioTranslation(_OpenAI):
         :param db: The datalayer to use for the model.
         """
         super().pre_create(db)
-        if isinstance(db.databackend.type, IbisDataBackend) and self.datatype is None:
-            self.datatype = dtype('str')
+        self.datatype = self.datatype or 'str'
 
     @retry
     def predict(

test/integration/ext/openai/test_model_openai.py
@@ -4,9 +4,7 @@ import os
 import openai
 import pytest
 import vcr
-from vcr.stubs import httpx_stubs
-
-from pinnacle.ext.openai.model import (
+from pinnacle_openai.model import (
     OpenAIAudioTranscription,
     OpenAIAudioTranslation,
     OpenAIChatCompletion,
@@ -15,6 +13,7 @@ from pinnacle.ext.openai.model import (
     OpenAIImageEdit,
     _available_models,
 )
+from vcr.stubs import httpx_stubs
 
 PNG_BYTE_SIGNATURE = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR'
 

test/unittest/base/test_leaf.py
@@ -3,7 +3,7 @@ import typing as t
 from pprint import pprint
 
 from pinnacle import ObjectModel
-from pinnacle.backends.mongodb.query import MongoQuery
+from pinnacle.backends.base.query import Query
 from pinnacle.base.constant import KEY_BUILDS
 from pinnacle.base.document import Document
 from pinnacle.base.leaf import Leaf
@@ -79,7 +79,7 @@ def test_encode_leaf_with_children():
 
 def test_save_variables_2():
     query = (
-        MongoQuery(table='documents')
+        Query(table='documents')
         .like({'x': '<var:X>'}, vector_index='test')
         .find({'x': {'$regex': '^test/1'}})
     )
@@ -117,18 +117,17 @@ def test_component_with_document():
 
 def test_find_variables():
     from pinnacle import Document
-    from pinnacle.backends.mongodb import MongoQuery
 
     r = Document({'txt': '<var:test>'})
 
     assert r.variables == ['test']
 
-    q = MongoQuery(table='test').find_one(Document({'txt': '<var:test>'}))
+    q = Query(table='test').find_one(Document({'txt': '<var:test>'}))
 
     assert q.variables == ['test']
 
     q = (
-        MongoQuery(table='test')
+        Query(table='test')
         .like(Document({'txt': '<var:test>'}), vector_index='test')
         .find()
         .limit(5)

test/unittest/ext/llm/utils.py
@@ -3,12 +3,11 @@ LLM model test cases.
 All the llm model can use the check_xxx func to test the intergration with db.
 """
 
-from pinnacle.backends.ibis.field_types import dtype
-from pinnacle.backends.ibis.query import Schema
 from pinnacle.backends.mongodb.data_backend import MongoDataBackend
 from pinnacle.backends.mongodb.query import MongoQuery
 from pinnacle.base.document import Document
 from pinnacle.components.listener import Listener
+from pinnacle.components.schema import Schema
 from pinnacle.components.table import Table
 
 
@@ -22,25 +21,28 @@ def check_predict(db, llm):
 def check_llm_as_listener_model(db, llm):
     """Test whether the model can predict the data in the database normally"""
     collection_name = "question"
+    db.cfg.auto_schema = True
     datas = [
         Document({"question": f"1+{i}=", "id": str(i), '_fold': 'train'})
         for i in range(10)
     ]
-    if isinstance(db.databackend.type, MongoDataBackend):
-        db.execute(MongoQuery(table=collection_name).insert_many(datas))
-        select = MongoQuery(table=collection_name).find()
-    else:
-        schema = Schema(
-            identifier=collection_name,
-            fields={
-                "id": dtype("str"),
-                "question": dtype("str"),
-            },
-        )
-        table = Table(identifier=collection_name, schema=schema)
-        db.add(table)
-        db.execute(db[collection_name].insert(datas))
-        select = db[collection_name].select("id", "question")
+    db[collection_name].insert(datas).execute()
+    select = db[collection_name].select("id", "question")
+    # if isinstance(db.databackend.type, MongoDataBackend):
+    #     db.execute(MongoQuery(table=collection_name).insert_many(datas))
+    #     select = MongoQuery(table=collection_name).find()
+    # else:
+    #     schema = Schema(
+    #         identifier=collection_name,
+    #         fields={
+    #             "id": "str",
+    #             "question": "str",
+    #         },
+    #     )
+    #     table = Table(identifier=collection_name, schema=schema)
+    #     db.add(table)
+    #     db.execute(db[collection_name].insert(datas))
+    #     select = db[collection_name].select("id", "question")
 
     listener = Listener(
         select=select,