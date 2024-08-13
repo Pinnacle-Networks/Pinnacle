jieguangzhou/superduperdb

[PLUGIN ALL] Updated CI

plugins/llamacpp/plugin_test/test_llama_cpp.py
@@ -1,0 +1,45 @@
+from superduper_llamacpp.model import LlamaCpp, LlamaCppEmbedding
+
+
+class _MockedLlama:
+    def create_completion(self, *args, **kwargs):
+        return {'choices': [{'text': 'tested'}]}
+
+    def create_embedding(self, *args, **kwargs):
+        return [1]
+
+
+def test_llama():
+    def mocked_init(self):
+        self._model = _MockedLlama()
+        self.predict_kwargs = {}
+
+    LlamaCpp.init = mocked_init
+
+    llama = LlamaCpp(
+        identifier='myllama',
+        model_name_or_path='some_model',
+        model_kwargs={'vocab_only': True},
+    )
+
+    text = 'testing prompt'
+    output = llama.predict(text)
+    assert output == 'tested'
+
+
+def test_llama_embedding():
+    def mocked_init(self):
+        self._model = _MockedLlama()
+        self.predict_kwargs = {}
+
+    LlamaCppEmbedding.init = mocked_init
+
+    llama = LlamaCppEmbedding(
+        identifier='myllama',
+        model_name_or_path='some_model',
+        model_kwargs={'vocab_only': True},
+    )
+
+    text = 'testing prompt'
+    output = llama.predict(text)
+    assert output == [1]

plugins/openai/plugin_test/test_model_openai.py
@@ -1,0 +1,245 @@
+import io
+import os
+
+import openai
+import pytest
+import vcr
+from vcr.stubs import httpx_stubs
+
+from superduper_openai.model import (
+    OpenAIAudioTranscription,
+    OpenAIAudioTranslation,
+    OpenAIChatCompletion,
+    OpenAIEmbedding,
+    OpenAIImageCreation,
+    OpenAIImageEdit,
+    _available_models,
+)
+
+PNG_BYTE_SIGNATURE = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR'
+
+CASSETTE_DIR = os.path.join(os.path.dirname(__file__), 'cassettes')
+
+if os.getenv('OPENAI_API_KEY') is None:
+    mp = pytest.MonkeyPatch()
+    mp.setattr(openai, 'api_key', 'sk-TopSecret')
+    mp.setenv('OPENAI_API_KEY', 'sk-TopSecret')
+
+
+# monkey patch vcr to make it work with upload binary data
+def _make_vcr_request(httpx_request, **kwargs):
+    from vcr.request import Request as VcrRequest
+
+    try:
+        body = httpx_request.read().decode("utf-8")
+    except UnicodeDecodeError:
+        body = str(httpx_request.read())
+    uri = str(httpx_request.url)
+    headers = dict(httpx_request.headers)
+    return VcrRequest(httpx_request.method, uri, body, headers)
+
+
+httpx_stubs._make_vcr_request = _make_vcr_request
+
+
+def before_record_response(response):
+    """
+    VCR filter function to only record the PNG signature in the response.
+
+    This is necessary because the response is a PNG which can be quite large.
+    """
+    if 'body' not in response:
+        return response
+    if PNG_BYTE_SIGNATURE in response['body']['string']:
+        response['body']['string'] = PNG_BYTE_SIGNATURE
+
+    if b'b64_json' in response['body']['string']:
+        # "b64_json": base64.b64encode(PNG_BYTE_SIGNATURE)
+        response['body']['string'] = (
+            b'{\n  \"created\": 1695982982,\n  \"data\": '
+            b'[\n    {\n      \"b64_json\": \"iVBORw0KGgoAAAANSUhEUg==\" } ]}'
+        )
+    return response
+
+
+def before_record_request(request):
+    # make the cassette simpler
+    if getattr(request, 'body', None) is not None:
+        request.body = 'fake_body'
+    # make the cassette simpler
+    request.headers = {}
+    return request
+
+
+# TODO: Move to top level of test dir, help other api tests
+vcr = vcr.VCR(
+    path_transformer=lambda x: x + '.yaml',
+    match_on=('method', 'path'),
+    filter_headers=['authorization'],
+    cassette_library_dir=CASSETTE_DIR,
+    before_record_request=before_record_request,
+    before_record_response=before_record_response,
+    record_on_exception=False,
+)
+
+
+@pytest.fixture(autouse=True)
+def mock_lru_cache():
+    _available_models.cache_clear()
+
+
+@vcr.use_cassette()
+def test_embed():
+    e = OpenAIEmbedding(identifier='text-embedding-ada-002')
+    resp = e.predict('Hello, world!')
+
+    assert len(resp) == e.shape[0]
+    assert all(isinstance(x, float) for x in resp)
+
+
+@vcr.use_cassette()
+def test_batch_embed():
+    e = OpenAIEmbedding(identifier='text-embedding-ada-002', batch_size=1)
+    resp = e.predict_batches(['Hello', 'world!'])
+
+    assert len(resp) == 2
+    assert all(len(x) == e.shape[0] for x in resp)
+    assert all(isinstance(x, float) for y in resp for x in y)
+
+
+@vcr.use_cassette()
+def test_chat():
+    e = OpenAIChatCompletion(identifier='gpt-3.5-turbo', prompt='Hello, {context}')
+    resp = e.predict('', context=['world!'])
+
+    assert isinstance(resp, str)
+
+
+@vcr.use_cassette()
+def test_batch_chat():
+    e = OpenAIChatCompletion(identifier='gpt-3.5-turbo')
+    resp = e.predict_batches([(('Hello, world!',), {})])
+
+    assert isinstance(resp, list)
+    assert isinstance(resp[0], str)
+
+
+@vcr.use_cassette()
+def test_create_url():
+    e = OpenAIImageCreation(
+        identifier='dall-e',
+        prompt='a close up, studio photographic portrait of a {context}',
+        response_format='url',
+    )
+    resp = e.predict('cat')
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette()
+def test_create_url_batch():
+    e = OpenAIImageCreation(
+        identifier='dall-e',
+        prompt='a close up, studio photographic portrait of a',
+        response_format='url',
+    )
+    resp = e.predict_batches(['cat', 'dog'])
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette()
+def test_edit_url():
+    e = OpenAIImageEdit(
+        identifier='dall-e',
+        prompt='A celebration party at the launch of {context}',
+        response_format='url',
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    resp = e.predict(buffer, context=['superduper'])
+    buffer.close()
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette()
+def test_edit_url_batch():
+    e = OpenAIImageEdit(
+        identifier='dall-e',
+        prompt='A celebration party at the launch of superduper',
+        response_format='url',
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_one = io.BytesIO(f.read())
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_two = io.BytesIO(f.read())
+
+    resp = e.predict_batches(
+        [
+            ((buffer_one,), {}),
+            ((buffer_two,), {}),
+        ]
+    )
+
+    buffer_one.close()
+    buffer_two.close()
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette()
+def test_transcribe():
+    with open('test/material/data/test.wav', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    buffer.name = 'test.wav'
+    prompt = (
+        'i have some advice for you. write all text in lower-case.'
+        'only make an exception for the following words: {context}'
+    )
+    e = OpenAIAudioTranscription(identifier='whisper-1', prompt=prompt)
+    resp = e.predict(buffer, context=['United States'])
+    buffer.close()
+
+    assert 'United States' in resp
+
+
+@vcr.use_cassette()
+def test_translate():
+    with open('test/material/data/german.wav', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    buffer.name = 'test.wav'
+    prompt = (
+        'i have some advice for you. write all text in lower-case.'
+        'only make an exception for the following words: {context}'
+    )
+    e = OpenAIAudioTranslation(identifier='whisper-1', prompt=prompt)
+    resp = e.predict(buffer, context=['Emmerich'])
+    buffer.close()
+
+    assert 'station' in resp
+
+
+@vcr.use_cassette()
+def test_batch_translate():
+    with open('test/material/data/german.wav', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+        buffer.name = 'test.wav'
+
+    with open('test/material/data/german.wav', 'rb') as f:
+        buffer2 = io.BytesIO(f.read())
+        buffer2.name = 'test.wav'
+
+    e = OpenAIAudioTranslation(identifier='whisper-1', batch_size=1)
+    resp = e.predict_batches([buffer, buffer2])
+    buffer.close()
+
+    assert len(resp) == 2
+    assert resp[0] == resp[1]
+    assert 'station' in resp[0]

plugins/pillow/plugin_test/test_init.py
@@ -1,0 +1,58 @@
+from typing import Iterator
+
+import pytest
+from superduper import superduper
+from superduper.base.datalayer import Datalayer
+from superduper.base.document import Document
+
+
+@pytest.fixture
+def db() -> Iterator[Datalayer]:
+    db = superduper("mongomock://test_db")
+
+    yield db
+    db.drop(force=True, data=True)
+
+@pytest.fixture
+def clean():
+    yield
+    import os
+
+    try:
+        os.remove('/tmp/test_db/32b6853dd2b2b45de723966dba17e23cece9f35c')
+    except FileNotFoundError:
+        pass
+
+
+def test_load_image_inside_query(db, clean):
+    r = {
+        '_path': 'superduper.backends.base.query.parse_query',
+        'query': 'image-search.predict(documents[0]["img"])',
+        'documents': [{'img': '?32b6853dd2b2b45de723966dba17e23cece9f35c'}],
+        '_builds': {
+            'jpg': {
+                '_path': 'superduper.ext.pillow.encoder.image_type',
+                'encodable': 'artifact',
+                'media_type': 'image/jpeg',
+                'type_id': 'datatype',
+            },
+            '32b6853dd2b2b45de723966dba17e23cece9f35c': {
+                '_path': 'superduper.components.datatype.Artifact',
+                'datatype': '?jpg',
+                'uri': None,
+                'blob': '&:blob:32b6853dd2b2b45de723966dba17e23cece9f35c',
+            },
+        },
+    }
+
+    with pytest.raises(FileNotFoundError):
+        q = Document.decode(r, db=db).unpack()
+
+    with open('test/material/data/test-image.jpeg', 'rb') as f:
+        db.artifact_store.put_bytes(
+            f.read(), '32b6853dd2b2b45de723966dba17e23cece9f35c'
+        )
+
+    q = Document.decode(r, db=db).unpack()
+
+    print(q)

test/unittest/component/test_serialization.py
@@ -1,42 +1,14 @@
-import pytest
-
-try:
-    import torch
-
-    from superduper.ext.torch.encoder import tensor
-except ImportError:
-    torch = None
-
-from sklearn.svm import SVC
-
+from superduper.components.datatype import pickle_serializer
 from superduper.components.model import ObjectModel
-from superduper.ext.sklearn.model import Estimator
 
 
-@pytest.mark.skipif(not torch, reason='Torch not installed')
 def test_model():
     m = ObjectModel(
         identifier='test',
-        datatype=tensor(dtype='float', shape=(32,)),
-        object=torch.nn.Linear(13, 18),
+        datatype=pickle_serializer,
+        object=lambda x: x + 1,
     )
     m_dict = m.dict()
     assert m_dict['identifier'] == m.identifier
     assert m_dict['object'].x == m.object
-    assert m_dict['datatype'].identifier == 'torch-float32[32]'
-
-
-def test_sklearn(db):
-    m = Estimator(
-        identifier='test',
-        object=SVC(),
-        datatype=tensor(dtype='float', shape=(32,)),
-    )
-    assert 'object' in m.artifact_schema.fields
-    db.apply(m)
-    assert db.show('model') == ['test']
-    assert db.show('datatype') == []
-
-    reloaded = db.load(type_id='model', identifier='test')
-    reloaded.init()
-    assert isinstance(reloaded.object, SVC)
+    assert m_dict['datatype'].identifier == 'pickle'

Pinnacle-Networks/Pinnacle

plugins/llamacpp/plugin_test/test_llama_cpp.py
@@ -1,0 +1,45 @@
+from pinnacle_llamacpp.model import LlamaCpp, LlamaCppEmbedding
+
+
+class _MockedLlama:
+    def create_completion(self, *args, **kwargs):
+        return {'choices': [{'text': 'tested'}]}
+
+    def create_embedding(self, *args, **kwargs):
+        return [1]
+
+
+def test_llama():
+    def mocked_init(self):
+        self._model = _MockedLlama()
+        self.predict_kwargs = {}
+
+    LlamaCpp.init = mocked_init
+
+    llama = LlamaCpp(
+        identifier='myllama',
+        model_name_or_path='some_model',
+        model_kwargs={'vocab_only': True},
+    )
+
+    text = 'testing prompt'
+    output = llama.predict(text)
+    assert output == 'tested'
+
+
+def test_llama_embedding():
+    def mocked_init(self):
+        self._model = _MockedLlama()
+        self.predict_kwargs = {}
+
+    LlamaCppEmbedding.init = mocked_init
+
+    llama = LlamaCppEmbedding(
+        identifier='myllama',
+        model_name_or_path='some_model',
+        model_kwargs={'vocab_only': True},
+    )
+
+    text = 'testing prompt'
+    output = llama.predict(text)
+    assert output == [1]

plugins/openai/plugin_test/test_model_openai.py
@@ -1,0 +1,245 @@
+import io
+import os
+
+import openai
+import pytest
+import vcr
+from vcr.stubs import httpx_stubs
+
+from pinnacle_openai.model import (
+    OpenAIAudioTranscription,
+    OpenAIAudioTranslation,
+    OpenAIChatCompletion,
+    OpenAIEmbedding,
+    OpenAIImageCreation,
+    OpenAIImageEdit,
+    _available_models,
+)
+
+PNG_BYTE_SIGNATURE = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR'
+
+CASSETTE_DIR = os.path.join(os.path.dirname(__file__), 'cassettes')
+
+if os.getenv('OPENAI_API_KEY') is None:
+    mp = pytest.MonkeyPatch()
+    mp.setattr(openai, 'api_key', 'sk-TopSecret')
+    mp.setenv('OPENAI_API_KEY', 'sk-TopSecret')
+
+
+# monkey patch vcr to make it work with upload binary data
+def _make_vcr_request(httpx_request, **kwargs):
+    from vcr.request import Request as VcrRequest
+
+    try:
+        body = httpx_request.read().decode("utf-8")
+    except UnicodeDecodeError:
+        body = str(httpx_request.read())
+    uri = str(httpx_request.url)
+    headers = dict(httpx_request.headers)
+    return VcrRequest(httpx_request.method, uri, body, headers)
+
+
+httpx_stubs._make_vcr_request = _make_vcr_request
+
+
+def before_record_response(response):
+    """
+    VCR filter function to only record the PNG signature in the response.
+
+    This is necessary because the response is a PNG which can be quite large.
+    """
+    if 'body' not in response:
+        return response
+    if PNG_BYTE_SIGNATURE in response['body']['string']:
+        response['body']['string'] = PNG_BYTE_SIGNATURE
+
+    if b'b64_json' in response['body']['string']:
+        # "b64_json": base64.b64encode(PNG_BYTE_SIGNATURE)
+        response['body']['string'] = (
+            b'{\n  \"created\": 1695982982,\n  \"data\": '
+            b'[\n    {\n      \"b64_json\": \"iVBORw0KGgoAAAANSUhEUg==\" } ]}'
+        )
+    return response
+
+
+def before_record_request(request):
+    # make the cassette simpler
+    if getattr(request, 'body', None) is not None:
+        request.body = 'fake_body'
+    # make the cassette simpler
+    request.headers = {}
+    return request
+
+
+# TODO: Move to top level of test dir, help other api tests
+vcr = vcr.VCR(
+    path_transformer=lambda x: x + '.yaml',
+    match_on=('method', 'path'),
+    filter_headers=['authorization'],
+    cassette_library_dir=CASSETTE_DIR,
+    before_record_request=before_record_request,
+    before_record_response=before_record_response,
+    record_on_exception=False,
+)
+
+
+@pytest.fixture(autouse=True)
+def mock_lru_cache():
+    _available_models.cache_clear()
+
+
+@vcr.use_cassette()
+def test_embed():
+    e = OpenAIEmbedding(identifier='text-embedding-ada-002')
+    resp = e.predict('Hello, world!')
+
+    assert len(resp) == e.shape[0]
+    assert all(isinstance(x, float) for x in resp)
+
+
+@vcr.use_cassette()
+def test_batch_embed():
+    e = OpenAIEmbedding(identifier='text-embedding-ada-002', batch_size=1)
+    resp = e.predict_batches(['Hello', 'world!'])
+
+    assert len(resp) == 2
+    assert all(len(x) == e.shape[0] for x in resp)
+    assert all(isinstance(x, float) for y in resp for x in y)
+
+
+@vcr.use_cassette()
+def test_chat():
+    e = OpenAIChatCompletion(identifier='gpt-3.5-turbo', prompt='Hello, {context}')
+    resp = e.predict('', context=['world!'])
+
+    assert isinstance(resp, str)
+
+
+@vcr.use_cassette()
+def test_batch_chat():
+    e = OpenAIChatCompletion(identifier='gpt-3.5-turbo')
+    resp = e.predict_batches([(('Hello, world!',), {})])
+
+    assert isinstance(resp, list)
+    assert isinstance(resp[0], str)
+
+
+@vcr.use_cassette()
+def test_create_url():
+    e = OpenAIImageCreation(
+        identifier='dall-e',
+        prompt='a close up, studio photographic portrait of a {context}',
+        response_format='url',
+    )
+    resp = e.predict('cat')
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette()
+def test_create_url_batch():
+    e = OpenAIImageCreation(
+        identifier='dall-e',
+        prompt='a close up, studio photographic portrait of a',
+        response_format='url',
+    )
+    resp = e.predict_batches(['cat', 'dog'])
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette()
+def test_edit_url():
+    e = OpenAIImageEdit(
+        identifier='dall-e',
+        prompt='A celebration party at the launch of {context}',
+        response_format='url',
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    resp = e.predict(buffer, context=['pinnacle'])
+    buffer.close()
+
+    # PNG 8-byte signature
+    assert resp[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette()
+def test_edit_url_batch():
+    e = OpenAIImageEdit(
+        identifier='dall-e',
+        prompt='A celebration party at the launch of pinnacle',
+        response_format='url',
+    )
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_one = io.BytesIO(f.read())
+    with open('test/material/data/rickroll.png', 'rb') as f:
+        buffer_two = io.BytesIO(f.read())
+
+    resp = e.predict_batches(
+        [
+            ((buffer_one,), {}),
+            ((buffer_two,), {}),
+        ]
+    )
+
+    buffer_one.close()
+    buffer_two.close()
+
+    for img in resp:
+        # PNG 8-byte signature
+        assert img[0:16] == PNG_BYTE_SIGNATURE
+
+
+@vcr.use_cassette()
+def test_transcribe():
+    with open('test/material/data/test.wav', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    buffer.name = 'test.wav'
+    prompt = (
+        'i have some advice for you. write all text in lower-case.'
+        'only make an exception for the following words: {context}'
+    )
+    e = OpenAIAudioTranscription(identifier='whisper-1', prompt=prompt)
+    resp = e.predict(buffer, context=['United States'])
+    buffer.close()
+
+    assert 'United States' in resp
+
+
+@vcr.use_cassette()
+def test_translate():
+    with open('test/material/data/german.wav', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+    buffer.name = 'test.wav'
+    prompt = (
+        'i have some advice for you. write all text in lower-case.'
+        'only make an exception for the following words: {context}'
+    )
+    e = OpenAIAudioTranslation(identifier='whisper-1', prompt=prompt)
+    resp = e.predict(buffer, context=['Emmerich'])
+    buffer.close()
+
+    assert 'station' in resp
+
+
+@vcr.use_cassette()
+def test_batch_translate():
+    with open('test/material/data/german.wav', 'rb') as f:
+        buffer = io.BytesIO(f.read())
+        buffer.name = 'test.wav'
+
+    with open('test/material/data/german.wav', 'rb') as f:
+        buffer2 = io.BytesIO(f.read())
+        buffer2.name = 'test.wav'
+
+    e = OpenAIAudioTranslation(identifier='whisper-1', batch_size=1)
+    resp = e.predict_batches([buffer, buffer2])
+    buffer.close()
+
+    assert len(resp) == 2
+    assert resp[0] == resp[1]
+    assert 'station' in resp[0]

plugins/pillow/plugin_test/test_init.py
@@ -1,0 +1,58 @@
+from typing import Iterator
+
+import pytest
+from pinnacle import pinnacle
+from pinnacle.base.datalayer import Datalayer
+from pinnacle.base.document import Document
+
+
+@pytest.fixture
+def db() -> Iterator[Datalayer]:
+    db = pinnacle("mongomock://test_db")
+
+    yield db
+    db.drop(force=True, data=True)
+
+@pytest.fixture
+def clean():
+    yield
+    import os
+
+    try:
+        os.remove('/tmp/test_db/32b6853dd2b2b45de723966dba17e23cece9f35c')
+    except FileNotFoundError:
+        pass
+
+
+def test_load_image_inside_query(db, clean):
+    r = {
+        '_path': 'pinnacle.backends.base.query.parse_query',
+        'query': 'image-search.predict(documents[0]["img"])',
+        'documents': [{'img': '?32b6853dd2b2b45de723966dba17e23cece9f35c'}],
+        '_builds': {
+            'jpg': {
+                '_path': 'pinnacle.ext.pillow.encoder.image_type',
+                'encodable': 'artifact',
+                'media_type': 'image/jpeg',
+                'type_id': 'datatype',
+            },
+            '32b6853dd2b2b45de723966dba17e23cece9f35c': {
+                '_path': 'pinnacle.components.datatype.Artifact',
+                'datatype': '?jpg',
+                'uri': None,
+                'blob': '&:blob:32b6853dd2b2b45de723966dba17e23cece9f35c',
+            },
+        },
+    }
+
+    with pytest.raises(FileNotFoundError):
+        q = Document.decode(r, db=db).unpack()
+
+    with open('test/material/data/test-image.jpeg', 'rb') as f:
+        db.artifact_store.put_bytes(
+            f.read(), '32b6853dd2b2b45de723966dba17e23cece9f35c'
+        )
+
+    q = Document.decode(r, db=db).unpack()
+
+    print(q)

test/unittest/component/test_serialization.py
@@ -1,42 +1,14 @@
-import pytest
-
-try:
-    import torch
-
-    from pinnacle.ext.torch.encoder import tensor
-except ImportError:
-    torch = None
-
-from sklearn.svm import SVC
-
+from pinnacle.components.datatype import pickle_serializer
 from pinnacle.components.model import ObjectModel
-from pinnacle.ext.sklearn.model import Estimator
 
 
-@pytest.mark.skipif(not torch, reason='Torch not installed')
 def test_model():
     m = ObjectModel(
         identifier='test',
-        datatype=tensor(dtype='float', shape=(32,)),
-        object=torch.nn.Linear(13, 18),
+        datatype=pickle_serializer,
+        object=lambda x: x + 1,
     )
     m_dict = m.dict()
     assert m_dict['identifier'] == m.identifier
     assert m_dict['object'].x == m.object
-    assert m_dict['datatype'].identifier == 'torch-float32[32]'
-
-
-def test_sklearn(db):
-    m = Estimator(
-        identifier='test',
-        object=SVC(),
-        datatype=tensor(dtype='float', shape=(32,)),
-    )
-    assert 'object' in m.artifact_schema.fields
-    db.apply(m)
-    assert db.show('model') == ['test']
-    assert db.show('datatype') == []
-
-    reloaded = db.load(type_id='model', identifier='test')
-    reloaded.init()
-    assert isinstance(reloaded.object, SVC)
+    assert m_dict['datatype'].identifier == 'pickle'