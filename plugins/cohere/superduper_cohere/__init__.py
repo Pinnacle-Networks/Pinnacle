superduper-io/superduper

Migrate plugins

plugins/cohere/superduper_cohere/__init__.py
@@ -1,0 +1,7 @@
+from superduper.misc.annotations import requires_packages
+
+_, requirements = requires_packages(['cohere', '4.40'])
+
+from .model import CohereEmbed, CohereGenerate
+
+__all__ = 'CohereEmbed', 'CohereGenerate'

plugins/jina/superduper_jina/client.py
@@ -1,0 +1,93 @@
+from typing import List, Optional
+
+import aiohttp
+import requests
+from aiohttp import ClientConnectionError, ClientResponseError
+from requests.exceptions import HTTPError
+
+from superduper.ext.utils import get_key
+from superduper.misc.retry import Retry
+
+JINA_API_URL: str = "https://api.jina.ai/v1/embeddings"
+KEY_NAME = 'JINA_API_KEY'
+
+retry = Retry(exception_types=(ClientResponseError, ClientConnectionError, HTTPError))
+
+
+class JinaAPIClient:
+    """A client for the Jina Embedding platform.
+
+    Create a JinaAPIClient to provide an interface to encode using
+        Jina Embedding platform sync and async.
+
+    :param api_key: The Jina API key.
+        It can be explicitly provided or automatically read
+        from the environment variable JINA_API_KEY (recommended).
+    :param model_name: The name of the Jina model to use.
+        Check the list of available models on `https://jina.ai/embeddings/`
+    """
+
+    def __init__(
+        self,
+        api_key: Optional[str] = None,
+        model_name: str = 'jina-embeddings-v2-base-en',
+    ):
+        # if the user does not provide the API key,
+        # check if it is set in the environment variable
+        if api_key is None:
+            api_key = get_key(KEY_NAME)
+
+        self.model_name = model_name
+        self._session = requests.Session()
+        self._headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Accept-Encoding": "identity",
+            "Content-type": "application/json",
+        }
+        self._session.headers.update(self._headers)
+
+    @retry
+    def encode_batch(self, texts: List[str]) -> List[List[float]]:
+        """Encode a batch of texts synchronously.
+
+        :param texts: The list of texts to encode.
+        """
+        response = self._session.post(
+            JINA_API_URL, json={"input": texts, "model": self.model_name}
+        ).json()
+        if "data" not in response:
+            raise RuntimeError(response["detail"])
+
+        # Sort resulting embeddings by index
+        sorted_embeddings = sorted(response["data"], key=lambda e: e["index"])
+        embeddings = [result["embedding"] for result in sorted_embeddings]
+        return embeddings
+
+    @retry
+    async def aencode_batch(self, texts: List[str]) -> List[List[float]]:
+        """Encode a batch of texts asynchronously.
+
+        :param texts: The list of texts to encode.
+        """
+        async with aiohttp.ClientSession() as session:
+            payload = {
+                'model': self.model_name,
+                'input': texts,
+            }
+
+            async with session.post(
+                JINA_API_URL,
+                headers=self._headers,
+                json=payload,
+            ) as response:
+                response.raise_for_status()
+                response_json = await response.json()
+                if "data" not in response_json:
+                    raise RuntimeError(response_json["detail"])
+
+                # Sort resulting embeddings by index
+                sorted_embeddings = sorted(
+                    response_json["data"], key=lambda e: e["index"]
+                )
+                embeddings = [result["embedding"] for result in sorted_embeddings]
+                return embeddings

plugins/pillow/superduper_pillow/encoder.py
@@ -1,0 +1,111 @@
+import io
+import typing as t
+
+import PIL.Image
+import PIL.JpegImagePlugin
+import PIL.PngImagePlugin
+
+from superduper.components.datatype import DataType, DataTypeFactory
+from superduper.misc.annotations import component
+
+if t.TYPE_CHECKING:
+    from superduper.base.datalayer import Datalayer
+
+BLANK_IMAGE = PIL.Image.new('RGB', (600, 600), (255, 255, 255))
+
+
+def encode_pil_image(x, info: t.Optional[t.Dict] = None):
+    """Encode a `PIL.Image` to bytes.
+
+    :param x: The image to encode.
+    :param info: Additional information.
+    """
+    buffer = io.BytesIO()
+    x.save(buffer, 'png')
+    return buffer.getvalue()
+
+
+class DecoderPILImage:
+    """Decoder to convert `bytes` back into a `PIL.Image` class.
+
+    :param handle_exceptions: return a blank image if failure
+    """
+
+    def __init__(self, handle_exceptions: bool = True):
+        self.handle_exceptions = handle_exceptions
+
+    def __call__(self, bytes, info: t.Optional[t.Dict] = None):
+        """Decode a `PIL.Image` from bytes.
+
+        :param bytes: The bytes to decode.
+        :param info: Additional information.
+        """
+        try:
+            return PIL.Image.open(io.BytesIO(bytes))
+        except Exception as e:
+            if self.handle_exceptions:
+                return BLANK_IMAGE
+            else:
+                raise e
+
+
+decode_pil_image = DecoderPILImage()
+
+
+@component(
+    {'name': 'identifier', 'type': 'str'},
+    {'name': 'media_type', 'type': 'str', 'default': 'image/png'},
+)
+def image_type(
+    identifier: str,
+    encodable: str = 'lazy_artifact',
+    media_type: str = 'image/png',
+    db: t.Optional['Datalayer'] = None,
+):
+    """Create a `DataType` for an image.
+
+    :param identifier: The identifier for the data type.
+    :param encodable: The encodable type.
+    :param media_type: The media type.
+    :param db: The datalayer instance.
+    """
+    return DataType(
+        identifier=identifier,
+        encoder=encode_pil_image,
+        decoder=decode_pil_image,
+        encodable=encodable,
+        media_type=media_type,
+    )
+
+
+pil_image = image_type(
+    identifier='pil_image',
+    encodable='encodable',
+)
+
+pil_image_hybrid = image_type(
+    identifier='pil_image_hybrid',
+    encodable='artifact',
+)
+
+
+class PilDataTypeFactory(DataTypeFactory):
+    """A factory for pil image # noqa."""
+
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        """Check if the data is a image.
+
+        It's used for registering the auto schema.
+        :param data: The data to check.
+        """
+        return isinstance(data, PIL.Image.Image)
+
+    @staticmethod
+    def create(data: t.Any) -> DataType:
+        """Create a pil_image datatype.
+
+        It's used for registering the auto schema.
+        :param data: The image data.
+        """
+        return pil_image

plugins/sentence_transformers/superduper_sentence_transformers/model.py
@@ -1,0 +1,124 @@
+import typing as t
+
+from sentence_transformers import SentenceTransformer as _SentenceTransformer
+
+from superduper.backends.query_dataset import QueryDataset
+from superduper.base.enums import DBType
+from superduper.components.component import ensure_initialized
+from superduper.components.datatype import DataType, dill_lazy
+from superduper.components.model import Model, Signature, _DeviceManaged
+
+DEFAULT_PREDICT_KWARGS = {
+    'show_progress_bar': True,
+}
+
+
+class SentenceTransformer(Model, _DeviceManaged):
+    """A model for sentence embeddings using `sentence-transformers`.
+
+    :param object: The SentenceTransformer object to use.
+    :param model: The model name, e.g. 'all-MiniLM-L6-v2'.
+    :param device: The device to use, e.g. 'cpu' or 'cuda'.
+    :param preprocess: The preprocessing function to apply to the input.
+    :param postprocess: The postprocessing function to apply to the output.
+    :param signature: The signature of the model.
+    :param preferred_devices: A list of devices to prefer, in that order.
+    """
+
+    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, 'DataType']]] = (
+        ('object', dill_lazy),
+    )
+
+    object: t.Optional[_SentenceTransformer] = None
+    model: t.Optional[str] = None
+    device: str = 'cpu'
+    preprocess: t.Union[None, t.Callable] = None
+    postprocess: t.Union[None, t.Callable] = None
+    signature: Signature = 'singleton'
+
+    def __post_init__(self, db, artifacts):
+        super().__post_init__(db, artifacts)
+
+        if self.model is None:
+            self.model = self.identifier
+
+        self._default_model = False
+        if self.object is None:
+            self.object = _SentenceTransformer(self.model, device=self.device)
+            self._default_model = True
+
+        if self.datatype is None:
+            sample = self.predict('Test')
+            self.shape = (len(sample),)
+
+    def dict(self, metadata: bool = True, defaults: bool = True):
+        """Serialize as a dictionary."""
+        r = super().dict(metadata=metadata, defaults=defaults)
+        if self._default_model:
+            del r['object']
+        return r
+
+    def init(self, db=None):
+        """Initialize the model."""
+        super().init(db=db)
+        self.to(self.device)
+
+    def to(self, device):
+        """Move the model to a device.
+
+        :param device: The device to move to, e.g. 'cpu' or 'cuda'.
+        """
+        self.object = self.object.to(device)
+        self.object._target_device = device
+
+    @ensure_initialized
+    def predict(self, X, *args, **kwargs):
+        """Predict on a single input.
+
+        :param X: The input to predict on.
+        :param args: Additional positional arguments, which are passed to the model.
+        :param kwargs: Additional keyword arguments, which are passed to the model.
+        """
+        if self.preprocess is not None:
+            X = self.preprocess(X)
+
+        assert self.object is not None
+        result = self.object.encode(X, *args, **{**self.predict_kwargs, **kwargs})
+        if self.postprocess is not None:
+            result = self.postprocess(result)
+        return result
+
+    @t.no_type_check
+    @ensure_initialized
+    def predict_batches(self, dataset: t.Union[t.List, QueryDataset]) -> t.List:
+        """Predict on a dataset.
+
+        :param dataset: The dataset to predict on.
+        """
+        if self.preprocess is not None:
+            dataset = list(map(self.preprocess, dataset))
+        assert self.object is not None
+
+        results = self.object.encode(dataset, **self.predict_kwargs)
+        if self.postprocess is not None:
+            results = self.postprocess(results)
+        return results
+
+    def pre_create(self, db):
+        """Pre creates the model.
+
+        If the datatype is not set and the datalayer is an IbisDataBackend,
+        the datatype is set to ``sqlvector`` or ``vector``.
+
+        :param db: The datalayer instance.
+        """
+        super().pre_create(db)
+        if self.datatype is not None:
+            return
+
+        from superduper.components.vector_index import sqlvector, vector
+
+        if db.databackend.db_type == DBType.SQL:
+            self.datatype = sqlvector(shape=self.shape)
+        else:
+            self.datatype = vector(shape=self.shape)

plugins/vllm/superduper_vllm/__init__.py
@@ -1,0 +1,8 @@
+from superduper.ext.vllm.model import VllmAPI, VllmModel
+from superduper.misc.annotations import requires_packages
+
+__all__ = ["VllmAPI", "VllmModel"]
+
+_, requirements = requires_packages(
+    ['vllm', None, None], ['ray', None, None], warn=True
+)

Pinnacle-Networks/Pinnacle

plugins/cohere/superduper_cohere/__init__.py
@@ -1,0 +1,7 @@
+from pinnacle.misc.annotations import requires_packages
+
+_, requirements = requires_packages(['cohere', '4.40'])
+
+from .model import CohereEmbed, CohereGenerate
+
+__all__ = 'CohereEmbed', 'CohereGenerate'

plugins/jina/superduper_jina/client.py
@@ -1,0 +1,93 @@
+from typing import List, Optional
+
+import aiohttp
+import requests
+from aiohttp import ClientConnectionError, ClientResponseError
+from requests.exceptions import HTTPError
+
+from pinnacle.ext.utils import get_key
+from pinnacle.misc.retry import Retry
+
+JINA_API_URL: str = "https://api.jina.ai/v1/embeddings"
+KEY_NAME = 'JINA_API_KEY'
+
+retry = Retry(exception_types=(ClientResponseError, ClientConnectionError, HTTPError))
+
+
+class JinaAPIClient:
+    """A client for the Jina Embedding platform.
+
+    Create a JinaAPIClient to provide an interface to encode using
+        Jina Embedding platform sync and async.
+
+    :param api_key: The Jina API key.
+        It can be explicitly provided or automatically read
+        from the environment variable JINA_API_KEY (recommended).
+    :param model_name: The name of the Jina model to use.
+        Check the list of available models on `https://jina.ai/embeddings/`
+    """
+
+    def __init__(
+        self,
+        api_key: Optional[str] = None,
+        model_name: str = 'jina-embeddings-v2-base-en',
+    ):
+        # if the user does not provide the API key,
+        # check if it is set in the environment variable
+        if api_key is None:
+            api_key = get_key(KEY_NAME)
+
+        self.model_name = model_name
+        self._session = requests.Session()
+        self._headers = {
+            "Authorization": f"Bearer {api_key}",
+            "Accept-Encoding": "identity",
+            "Content-type": "application/json",
+        }
+        self._session.headers.update(self._headers)
+
+    @retry
+    def encode_batch(self, texts: List[str]) -> List[List[float]]:
+        """Encode a batch of texts synchronously.
+
+        :param texts: The list of texts to encode.
+        """
+        response = self._session.post(
+            JINA_API_URL, json={"input": texts, "model": self.model_name}
+        ).json()
+        if "data" not in response:
+            raise RuntimeError(response["detail"])
+
+        # Sort resulting embeddings by index
+        sorted_embeddings = sorted(response["data"], key=lambda e: e["index"])
+        embeddings = [result["embedding"] for result in sorted_embeddings]
+        return embeddings
+
+    @retry
+    async def aencode_batch(self, texts: List[str]) -> List[List[float]]:
+        """Encode a batch of texts asynchronously.
+
+        :param texts: The list of texts to encode.
+        """
+        async with aiohttp.ClientSession() as session:
+            payload = {
+                'model': self.model_name,
+                'input': texts,
+            }
+
+            async with session.post(
+                JINA_API_URL,
+                headers=self._headers,
+                json=payload,
+            ) as response:
+                response.raise_for_status()
+                response_json = await response.json()
+                if "data" not in response_json:
+                    raise RuntimeError(response_json["detail"])
+
+                # Sort resulting embeddings by index
+                sorted_embeddings = sorted(
+                    response_json["data"], key=lambda e: e["index"]
+                )
+                embeddings = [result["embedding"] for result in sorted_embeddings]
+                return embeddings

plugins/pillow/superduper_pillow/encoder.py
@@ -1,0 +1,111 @@
+import io
+import typing as t
+
+import PIL.Image
+import PIL.JpegImagePlugin
+import PIL.PngImagePlugin
+
+from pinnacle.components.datatype import DataType, DataTypeFactory
+from pinnacle.misc.annotations import component
+
+if t.TYPE_CHECKING:
+    from pinnacle.base.datalayer import Datalayer
+
+BLANK_IMAGE = PIL.Image.new('RGB', (600, 600), (255, 255, 255))
+
+
+def encode_pil_image(x, info: t.Optional[t.Dict] = None):
+    """Encode a `PIL.Image` to bytes.
+
+    :param x: The image to encode.
+    :param info: Additional information.
+    """
+    buffer = io.BytesIO()
+    x.save(buffer, 'png')
+    return buffer.getvalue()
+
+
+class DecoderPILImage:
+    """Decoder to convert `bytes` back into a `PIL.Image` class.
+
+    :param handle_exceptions: return a blank image if failure
+    """
+
+    def __init__(self, handle_exceptions: bool = True):
+        self.handle_exceptions = handle_exceptions
+
+    def __call__(self, bytes, info: t.Optional[t.Dict] = None):
+        """Decode a `PIL.Image` from bytes.
+
+        :param bytes: The bytes to decode.
+        :param info: Additional information.
+        """
+        try:
+            return PIL.Image.open(io.BytesIO(bytes))
+        except Exception as e:
+            if self.handle_exceptions:
+                return BLANK_IMAGE
+            else:
+                raise e
+
+
+decode_pil_image = DecoderPILImage()
+
+
+@component(
+    {'name': 'identifier', 'type': 'str'},
+    {'name': 'media_type', 'type': 'str', 'default': 'image/png'},
+)
+def image_type(
+    identifier: str,
+    encodable: str = 'lazy_artifact',
+    media_type: str = 'image/png',
+    db: t.Optional['Datalayer'] = None,
+):
+    """Create a `DataType` for an image.
+
+    :param identifier: The identifier for the data type.
+    :param encodable: The encodable type.
+    :param media_type: The media type.
+    :param db: The datalayer instance.
+    """
+    return DataType(
+        identifier=identifier,
+        encoder=encode_pil_image,
+        decoder=decode_pil_image,
+        encodable=encodable,
+        media_type=media_type,
+    )
+
+
+pil_image = image_type(
+    identifier='pil_image',
+    encodable='encodable',
+)
+
+pil_image_hybrid = image_type(
+    identifier='pil_image_hybrid',
+    encodable='artifact',
+)
+
+
+class PilDataTypeFactory(DataTypeFactory):
+    """A factory for pil image # noqa."""
+
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        """Check if the data is a image.
+
+        It's used for registering the auto schema.
+        :param data: The data to check.
+        """
+        return isinstance(data, PIL.Image.Image)
+
+    @staticmethod
+    def create(data: t.Any) -> DataType:
+        """Create a pil_image datatype.
+
+        It's used for registering the auto schema.
+        :param data: The image data.
+        """
+        return pil_image

plugins/sentence_transformers/superduper_sentence_transformers/model.py
@@ -1,0 +1,124 @@
+import typing as t
+
+from sentence_transformers import SentenceTransformer as _SentenceTransformer
+
+from pinnacle.backends.query_dataset import QueryDataset
+from pinnacle.base.enums import DBType
+from pinnacle.components.component import ensure_initialized
+from pinnacle.components.datatype import DataType, dill_lazy
+from pinnacle.components.model import Model, Signature, _DeviceManaged
+
+DEFAULT_PREDICT_KWARGS = {
+    'show_progress_bar': True,
+}
+
+
+class SentenceTransformer(Model, _DeviceManaged):
+    """A model for sentence embeddings using `sentence-transformers`.
+
+    :param object: The SentenceTransformer object to use.
+    :param model: The model name, e.g. 'all-MiniLM-L6-v2'.
+    :param device: The device to use, e.g. 'cpu' or 'cuda'.
+    :param preprocess: The preprocessing function to apply to the input.
+    :param postprocess: The postprocessing function to apply to the output.
+    :param signature: The signature of the model.
+    :param preferred_devices: A list of devices to prefer, in that order.
+    """
+
+    _artifacts: t.ClassVar[t.Sequence[t.Tuple[str, 'DataType']]] = (
+        ('object', dill_lazy),
+    )
+
+    object: t.Optional[_SentenceTransformer] = None
+    model: t.Optional[str] = None
+    device: str = 'cpu'
+    preprocess: t.Union[None, t.Callable] = None
+    postprocess: t.Union[None, t.Callable] = None
+    signature: Signature = 'singleton'
+
+    def __post_init__(self, db, artifacts):
+        super().__post_init__(db, artifacts)
+
+        if self.model is None:
+            self.model = self.identifier
+
+        self._default_model = False
+        if self.object is None:
+            self.object = _SentenceTransformer(self.model, device=self.device)
+            self._default_model = True
+
+        if self.datatype is None:
+            sample = self.predict('Test')
+            self.shape = (len(sample),)
+
+    def dict(self, metadata: bool = True, defaults: bool = True):
+        """Serialize as a dictionary."""
+        r = super().dict(metadata=metadata, defaults=defaults)
+        if self._default_model:
+            del r['object']
+        return r
+
+    def init(self, db=None):
+        """Initialize the model."""
+        super().init(db=db)
+        self.to(self.device)
+
+    def to(self, device):
+        """Move the model to a device.
+
+        :param device: The device to move to, e.g. 'cpu' or 'cuda'.
+        """
+        self.object = self.object.to(device)
+        self.object._target_device = device
+
+    @ensure_initialized
+    def predict(self, X, *args, **kwargs):
+        """Predict on a single input.
+
+        :param X: The input to predict on.
+        :param args: Additional positional arguments, which are passed to the model.
+        :param kwargs: Additional keyword arguments, which are passed to the model.
+        """
+        if self.preprocess is not None:
+            X = self.preprocess(X)
+
+        assert self.object is not None
+        result = self.object.encode(X, *args, **{**self.predict_kwargs, **kwargs})
+        if self.postprocess is not None:
+            result = self.postprocess(result)
+        return result
+
+    @t.no_type_check
+    @ensure_initialized
+    def predict_batches(self, dataset: t.Union[t.List, QueryDataset]) -> t.List:
+        """Predict on a dataset.
+
+        :param dataset: The dataset to predict on.
+        """
+        if self.preprocess is not None:
+            dataset = list(map(self.preprocess, dataset))
+        assert self.object is not None
+
+        results = self.object.encode(dataset, **self.predict_kwargs)
+        if self.postprocess is not None:
+            results = self.postprocess(results)
+        return results
+
+    def pre_create(self, db):
+        """Pre creates the model.
+
+        If the datatype is not set and the datalayer is an IbisDataBackend,
+        the datatype is set to ``sqlvector`` or ``vector``.
+
+        :param db: The datalayer instance.
+        """
+        super().pre_create(db)
+        if self.datatype is not None:
+            return
+
+        from pinnacle.components.vector_index import sqlvector, vector
+
+        if db.databackend.db_type == DBType.SQL:
+            self.datatype = sqlvector(shape=self.shape)
+        else:
+            self.datatype = vector(shape=self.shape)

plugins/vllm/superduper_vllm/__init__.py
@@ -1,0 +1,8 @@
+from pinnacle.ext.vllm.model import VllmAPI, VllmModel
+from pinnacle.misc.annotations import requires_packages
+
+__all__ = ["VllmAPI", "VllmModel"]
+
+_, requirements = requires_packages(
+    ['vllm', None, None], ['ray', None, None], warn=True
+)