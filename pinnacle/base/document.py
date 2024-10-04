superduper-io/superduper

Add remote file support

superduper/base/document.py
@@ -1,3 +1,4 @@
+import inspect
 import typing as t
 from collections import defaultdict
 from functools import partial
@@ -45,7 +46,9 @@ def _blob_getter(uri: str, getter: t.Callable):
     from superduper.misc.download import Fetcher
 
     dialect = uri.split('://')[0]
-    if dialect not in Fetcher.DIALECTS:
+
+    is_loader = 'loader' in inspect.signature(getter).parameters
+    if dialect not in Fetcher.DIALECTS or not is_loader:
         return getter(uri)
     else:
         loader = Fetcher()
@@ -85,9 +88,9 @@ class _Getters:
         if name not in self._getters:
             return data
         for getter in self._getters[name]:
-            data = getter(data)
-            if data is not None:
-                break
+            out = getter(data)
+            if out is not None:
+                return out
         return data
 
 
@@ -207,6 +210,10 @@ class Document(MongoStyleDict):
 
         if r.get(KEY_FILES):
             getters.add_getter('file', lambda x: r[KEY_FILES].get(x.split(':')[-1]))
+
+        # Add a remote file getter
+        getters.add_getter('file', _get_file_remote_callback)
+        getters.add_getter('blob', _get_local_blob)
 
         if db is not None:
             getters.add_getter('component', lambda x: _get_component(db, x))
@@ -589,6 +596,27 @@ def _get_artifact_callback(db):
     return callback
 
 
+def _get_file_remote_callback(path):
+    from superduper.misc.download import Fetcher
+
+    fetcher = Fetcher()
+
+    if not fetcher.is_uri(path):
+        return None
+
+    def pull_file():
+        uri = path.split(':file:')[-1]
+        from superduper.misc.files import get_file_from_uri
+
+        content = fetcher(uri)
+        file_id = get_file_from_uri(uri)
+
+        file_path = fetcher.save(uri, content, file_id)
+        return file_path, path
+
+    return pull_file
+
+
 def _get_file_callback(db):
     def callback(path):
         def pull_file():
@@ -598,3 +626,8 @@ def _get_file_callback(db):
         return pull_file
 
     return callback
+
+
+def _get_local_blob(x, loader=None):
+    if x.split('://')[0].startswith('file'):
+        return loader(x)

superduper/misc/download.py
@@ -1,6 +1,8 @@
+import os
 import re
 import signal
 import sys
+import tempfile
 import typing as t
 import warnings
 from contextlib import contextmanager
@@ -81,6 +83,27 @@ class Fetcher:
         """
         return uri.split('://')[0] in self.DIALECTS
 
+    def _download_s3_folder(self, uri):
+        folder_objects = []
+        path = uri.split('s3://')[-1]
+        bucket_name = path.split('/')[0]
+        prefix = uri.split(bucket_name + '/')[-1]
+
+        objects = self.s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+        if 'Contents' in objects:
+            for obj in objects['Contents']:
+                s3_key = obj['Key']
+                if not s3_key.endswith('/'):
+                    data = self._download_s3_object(uri + s3_key)
+                    info = {
+                        'file': s3_key,
+                        'data': data,
+                        'folder': prefix.split('/')[-1],
+                    }
+                    folder_objects.append(info)
+        return folder_objects
+
     def _download_s3_object(self, uri):
         f = BytesIO()
         path = uri.split('s3://')[-1]
@@ -105,11 +128,47 @@ class Fetcher:
         if uri.startswith('file://'):
             return self._download_file(uri)
         elif uri.startswith('s3://'):
+            if uri.endswith('/'):
+                return self._download_s3_folder(uri)
             return self._download_s3_object(uri)
         elif uri.startswith('http://') or uri.startswith('https://'):
             return self._download_from_uri(uri)
         else:
             raise NotImplementedError(f'unknown type of URI "{uri}"')
+
+    def save(self, uri: str, contents: t.Union[t.List, bytes], file_id: str):
+        """Save downloaded bytes to a cached directory."""
+        download_folder = CFG.downloads.folder
+
+        if not download_folder:
+            download_folder = os.path.join(
+                tempfile.gettempdir(), "superduper", "ArtifactStore"
+            )
+
+        save_folder = os.path.join(download_folder, file_id)
+        os.makedirs(save_folder, exist_ok=True)
+
+        if isinstance(contents, list):
+            folder_path = None
+            for content in contents:
+                name = content['file']
+                folder = content['folder']
+                data = content['data']
+                folder_path = os.path.join(save_folder, folder)
+                os.makedirs(folder_path, exist_ok=True)
+
+                path = os.path.join(folder_path, name)
+                with open(path, 'wb') as f:
+                    f.write(data)
+            return folder_path
+
+        else:
+            base_name = uri.split('/')[-1]
+
+            path = os.path.join(save_folder, base_name)
+            with open(path, 'wb') as f:
+                f.write(contents)
+            return path
 
 
 class BaseDownloader:

test/unittest/base/test_document.py
@@ -67,6 +67,39 @@ def test_encode_decode_flattened_document():
     assert isinstance(next(iter(encoded_r[KEY_BLOBS].values())), bytes)
 
 
+def test_encode_model_with_remote_file(db):
+    r = {
+        '_base': '?20d76167d4a6ad7fe00250e8359d0dca',
+        '_builds': {
+            'file': {
+                '_path': 'superduper.components.datatype.get_serializer',
+                'method': 'file',
+                'encodable': 'file',
+                'type_id': 'datatype',
+                'version': None,
+                'uuid': '1ef3aaae626a45aa836b82f493acc874',
+            },
+            '20d76167d4a6ad7fe00250e8359d0dca': {
+                '_path': 'superduper.components.datatype.File',
+                'uuid': '43d651a7990241f3accbd4b67b77b069',
+                'datatype': '?file',
+                'uri': None,
+                'x': '&:file:file://./README.md',
+            },
+        },
+        '_blobs': {},
+        '_files': {},
+    }
+
+    r = Document.decode(r, db=db).unpack()
+    assert os.path.exists(r)
+    with open(r, 'rb') as r:
+        read = r.readlines()
+
+    with open('./README.md', 'rb') as r:
+        assert r.readlines() == read
+
+
 def test_encode_model_with_remote_blob():
     m = ObjectModel(
         identifier='test',
@@ -75,9 +108,9 @@ def test_encode_model_with_remote_blob():
 
     encoded_r = m.encode()
     with tempfile.TemporaryDirectory() as temp_dir:
-        blob_key = [k for k in encoded_r['_builds'] if k not in ('test', 'dill_lazy')][
-            0
-        ]
+        blob_key = [
+            k for k in encoded_r['_builds'] if k not in ('test', 'datatype:dill_lazy')
+        ][0]
 
         blob = encoded_r['_blobs'][blob_key]
         temp_file_path = os.path.join(temp_dir, blob_key)
@@ -85,11 +118,7 @@ def test_encode_model_with_remote_blob():
             f.write(blob)
 
         encoded_r['_builds'][blob_key]['blob'] = f'&:blob:file://{temp_file_path}'
-
-        def local_blob(x, loader=None):
-            return loader(x)
-
-        decoded_r = Document.decode(encoded_r, getters={'blob': local_blob})
+        decoded_r = Document.decode(encoded_r)
 
     m = decoded_r.unpack()
     m.object.init()

Pinnacle-Networks/Pinnacle

pinnacle/base/document.py
@@ -1,3 +1,4 @@
+import inspect
 import typing as t
 from collections import defaultdict
 from functools import partial
@@ -45,7 +46,9 @@ def _blob_getter(uri: str, getter: t.Callable):
     from pinnacle.misc.download import Fetcher
 
     dialect = uri.split('://')[0]
-    if dialect not in Fetcher.DIALECTS:
+
+    is_loader = 'loader' in inspect.signature(getter).parameters
+    if dialect not in Fetcher.DIALECTS or not is_loader:
         return getter(uri)
     else:
         loader = Fetcher()
@@ -85,9 +88,9 @@ class _Getters:
         if name not in self._getters:
             return data
         for getter in self._getters[name]:
-            data = getter(data)
-            if data is not None:
-                break
+            out = getter(data)
+            if out is not None:
+                return out
         return data
 
 
@@ -207,6 +210,10 @@ class Document(MongoStyleDict):
 
         if r.get(KEY_FILES):
             getters.add_getter('file', lambda x: r[KEY_FILES].get(x.split(':')[-1]))
+
+        # Add a remote file getter
+        getters.add_getter('file', _get_file_remote_callback)
+        getters.add_getter('blob', _get_local_blob)
 
         if db is not None:
             getters.add_getter('component', lambda x: _get_component(db, x))
@@ -589,6 +596,27 @@ def _get_artifact_callback(db):
     return callback
 
 
+def _get_file_remote_callback(path):
+    from pinnacle.misc.download import Fetcher
+
+    fetcher = Fetcher()
+
+    if not fetcher.is_uri(path):
+        return None
+
+    def pull_file():
+        uri = path.split(':file:')[-1]
+        from pinnacle.misc.files import get_file_from_uri
+
+        content = fetcher(uri)
+        file_id = get_file_from_uri(uri)
+
+        file_path = fetcher.save(uri, content, file_id)
+        return file_path, path
+
+    return pull_file
+
+
 def _get_file_callback(db):
     def callback(path):
         def pull_file():
@@ -598,3 +626,8 @@ def _get_file_callback(db):
         return pull_file
 
     return callback
+
+
+def _get_local_blob(x, loader=None):
+    if x.split('://')[0].startswith('file'):
+        return loader(x)

pinnacle/misc/download.py
@@ -1,6 +1,8 @@
+import os
 import re
 import signal
 import sys
+import tempfile
 import typing as t
 import warnings
 from contextlib import contextmanager
@@ -81,6 +83,27 @@ class Fetcher:
         """
         return uri.split('://')[0] in self.DIALECTS
 
+    def _download_s3_folder(self, uri):
+        folder_objects = []
+        path = uri.split('s3://')[-1]
+        bucket_name = path.split('/')[0]
+        prefix = uri.split(bucket_name + '/')[-1]
+
+        objects = self.s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+        if 'Contents' in objects:
+            for obj in objects['Contents']:
+                s3_key = obj['Key']
+                if not s3_key.endswith('/'):
+                    data = self._download_s3_object(uri + s3_key)
+                    info = {
+                        'file': s3_key,
+                        'data': data,
+                        'folder': prefix.split('/')[-1],
+                    }
+                    folder_objects.append(info)
+        return folder_objects
+
     def _download_s3_object(self, uri):
         f = BytesIO()
         path = uri.split('s3://')[-1]
@@ -105,11 +128,47 @@ class Fetcher:
         if uri.startswith('file://'):
             return self._download_file(uri)
         elif uri.startswith('s3://'):
+            if uri.endswith('/'):
+                return self._download_s3_folder(uri)
             return self._download_s3_object(uri)
         elif uri.startswith('http://') or uri.startswith('https://'):
             return self._download_from_uri(uri)
         else:
             raise NotImplementedError(f'unknown type of URI "{uri}"')
+
+    def save(self, uri: str, contents: t.Union[t.List, bytes], file_id: str):
+        """Save downloaded bytes to a cached directory."""
+        download_folder = CFG.downloads.folder
+
+        if not download_folder:
+            download_folder = os.path.join(
+                tempfile.gettempdir(), "pinnacle", "ArtifactStore"
+            )
+
+        save_folder = os.path.join(download_folder, file_id)
+        os.makedirs(save_folder, exist_ok=True)
+
+        if isinstance(contents, list):
+            folder_path = None
+            for content in contents:
+                name = content['file']
+                folder = content['folder']
+                data = content['data']
+                folder_path = os.path.join(save_folder, folder)
+                os.makedirs(folder_path, exist_ok=True)
+
+                path = os.path.join(folder_path, name)
+                with open(path, 'wb') as f:
+                    f.write(data)
+            return folder_path
+
+        else:
+            base_name = uri.split('/')[-1]
+
+            path = os.path.join(save_folder, base_name)
+            with open(path, 'wb') as f:
+                f.write(contents)
+            return path
 
 
 class BaseDownloader:

test/unittest/base/test_document.py
@@ -67,6 +67,39 @@ def test_encode_decode_flattened_document():
     assert isinstance(next(iter(encoded_r[KEY_BLOBS].values())), bytes)
 
 
+def test_encode_model_with_remote_file(db):
+    r = {
+        '_base': '?20d76167d4a6ad7fe00250e8359d0dca',
+        '_builds': {
+            'file': {
+                '_path': 'pinnacle.components.datatype.get_serializer',
+                'method': 'file',
+                'encodable': 'file',
+                'type_id': 'datatype',
+                'version': None,
+                'uuid': '1ef3aaae626a45aa836b82f493acc874',
+            },
+            '20d76167d4a6ad7fe00250e8359d0dca': {
+                '_path': 'pinnacle.components.datatype.File',
+                'uuid': '43d651a7990241f3accbd4b67b77b069',
+                'datatype': '?file',
+                'uri': None,
+                'x': '&:file:file://./README.md',
+            },
+        },
+        '_blobs': {},
+        '_files': {},
+    }
+
+    r = Document.decode(r, db=db).unpack()
+    assert os.path.exists(r)
+    with open(r, 'rb') as r:
+        read = r.readlines()
+
+    with open('./README.md', 'rb') as r:
+        assert r.readlines() == read
+
+
 def test_encode_model_with_remote_blob():
     m = ObjectModel(
         identifier='test',
@@ -75,9 +108,9 @@ def test_encode_model_with_remote_blob():
 
     encoded_r = m.encode()
     with tempfile.TemporaryDirectory() as temp_dir:
-        blob_key = [k for k in encoded_r['_builds'] if k not in ('test', 'dill_lazy')][
-            0
-        ]
+        blob_key = [
+            k for k in encoded_r['_builds'] if k not in ('test', 'datatype:dill_lazy')
+        ][0]
 
         blob = encoded_r['_blobs'][blob_key]
         temp_file_path = os.path.join(temp_dir, blob_key)
@@ -85,11 +118,7 @@ def test_encode_model_with_remote_blob():
             f.write(blob)
 
         encoded_r['_builds'][blob_key]['blob'] = f'&:blob:file://{temp_file_path}'
-
-        def local_blob(x, loader=None):
-            return loader(x)
-
-        decoded_r = Document.decode(encoded_r, getters={'blob': local_blob})
+        decoded_r = Document.decode(encoded_r)
 
     m = decoded_r.unpack()
     m.object.init()