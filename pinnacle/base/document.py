superduper-io/superduper

Add support for remote artifacts in component read

superduper/base/document.py
@@ -1,5 +1,6 @@
 import typing as t
 from collections import defaultdict
+from functools import partial
 
 from superduper import logging
 from superduper.base.code import Code
@@ -40,6 +41,21 @@ _LEAF_TYPES = {
 _LEAF_TYPES.update(_ENCODABLES)
 
 
+def _blob_getter(uri: str, getter: t.Callable):
+    from superduper.misc.download import Fetcher
+
+    dialect = uri.split('://')[0]
+    if dialect not in Fetcher.DIALECTS:
+        return getter(uri)
+    else:
+        loader = Fetcher()
+        return getter(uri, loader=loader)
+
+
+def _build_blob_getter(base_getter):
+    return partial(_blob_getter, getter=base_getter)
+
+
 class _Getters:
     """A class to manage getters for decoding documents.
 
@@ -59,7 +75,10 @@ class _Getters:
 
     def add_getter(self, name: str, getter: t.Callable):
         """Add a getter for a reference type."""
-        self._getters[name].append(getter)
+        if name == 'blob':
+            self._getters[name].append(_build_blob_getter(getter))
+        else:
+            self._getters[name].append(getter)
 
     def run(self, name, data):
         """Run the getters one by one until one returns a value."""

superduper/components/component.py
@@ -340,15 +340,31 @@ class Component(Leaf):
         if db is not None:
             for blob in os.listdir(path + '/' + 'blobs'):
                 with open(path + '/blobs/' + blob, 'rb') as f:
-                    bytes = f.read()
-                    db.artifact_store.put_bytes(bytes, blob)
+                    data = f.read()
+                    db.artifact_store.put_bytes(data, blob)
 
             out = Document.decode(config_object, db=db).unpack()
         else:
 
-            def load_blob(blob):
-                with open(path + '/blobs/' + blob, 'rb') as f:
-                    return f.read()
+            def load_blob(blob, loader=None):
+                from superduper.misc.download import hash_uri
+
+                def _read_blob(blob_path):
+                    with open(blob_path, 'rb') as f:
+                        return f.read()
+
+                key = hash_uri(blob)
+                cached_path = path + '/blobs/' + key
+                if os.path.exists(cached_path):
+                    return _read_blob(cached_path)
+                elif loader.is_uri(blob):
+                    with open(path + '/blobs/' + key, 'wb') as f:
+                        data = loader(blob)
+                        f.write(data)
+                        return data
+                else:
+                    blob_path = path + '/blobs/' + blob
+                    return _read_blob(blob_path=blob_path)
 
             getters = {'blob': load_blob}
 

Pinnacle-Networks/Pinnacle

pinnacle/base/document.py
@@ -1,5 +1,6 @@
 import typing as t
 from collections import defaultdict
+from functools import partial
 
 from pinnacle import logging
 from pinnacle.base.code import Code
@@ -40,6 +41,21 @@ _LEAF_TYPES = {
 _LEAF_TYPES.update(_ENCODABLES)
 
 
+def _blob_getter(uri: str, getter: t.Callable):
+    from pinnacle.misc.download import Fetcher
+
+    dialect = uri.split('://')[0]
+    if dialect not in Fetcher.DIALECTS:
+        return getter(uri)
+    else:
+        loader = Fetcher()
+        return getter(uri, loader=loader)
+
+
+def _build_blob_getter(base_getter):
+    return partial(_blob_getter, getter=base_getter)
+
+
 class _Getters:
     """A class to manage getters for decoding documents.
 
@@ -59,7 +75,10 @@ class _Getters:
 
     def add_getter(self, name: str, getter: t.Callable):
         """Add a getter for a reference type."""
-        self._getters[name].append(getter)
+        if name == 'blob':
+            self._getters[name].append(_build_blob_getter(getter))
+        else:
+            self._getters[name].append(getter)
 
     def run(self, name, data):
         """Run the getters one by one until one returns a value."""

pinnacle/components/component.py
@@ -340,15 +340,31 @@ class Component(Leaf):
         if db is not None:
             for blob in os.listdir(path + '/' + 'blobs'):
                 with open(path + '/blobs/' + blob, 'rb') as f:
-                    bytes = f.read()
-                    db.artifact_store.put_bytes(bytes, blob)
+                    data = f.read()
+                    db.artifact_store.put_bytes(data, blob)
 
             out = Document.decode(config_object, db=db).unpack()
         else:
 
-            def load_blob(blob):
-                with open(path + '/blobs/' + blob, 'rb') as f:
-                    return f.read()
+            def load_blob(blob, loader=None):
+                from pinnacle.misc.download import hash_uri
+
+                def _read_blob(blob_path):
+                    with open(blob_path, 'rb') as f:
+                        return f.read()
+
+                key = hash_uri(blob)
+                cached_path = path + '/blobs/' + key
+                if os.path.exists(cached_path):
+                    return _read_blob(cached_path)
+                elif loader.is_uri(blob):
+                    with open(path + '/blobs/' + key, 'wb') as f:
+                        data = loader(blob)
+                        f.write(data)
+                        return data
+                else:
+                    blob_path = path + '/blobs/' + blob
+                    return _read_blob(blob_path=blob_path)
 
             getters = {'blob': load_blob}
 