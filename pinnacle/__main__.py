superduper-io/superduper

Include templates in project

superduper/__main__.py
@@ -2,8 +2,7 @@ import sys
 
 import click
 
-from superduper.cli import app, apply, info
-from superduper.cli.serve import cdc, local_cluster, ray_serve, vector_search
+from superduper.cli import app, apply 
 
 __all__ = (
     'apply',

superduper/cli/apply.py
@@ -1,15 +1,21 @@
+import typing as t
+import json
+import os
+
 from superduper import Component, logging, superduper
+from superduper.components.template import Template
 
 from . import command
 
 
-@command(help='Apply the component to a `superduper` deployment')
-def apply(path: str):
+@command(help='Apply a template or application to a `superduper` deployment')
+def apply(name: str, variables: str | None = None):
     """Apply a serialized component.
 
-    :param path: Path to the stack.
+    :param name: Path or name of the template/ component.
+    :param values: JSON string of values to apply to the template.
     """
-    _apply(path)
+    _apply(name, variables)
 
 
 @command(help='`superduper` deployment')
@@ -23,14 +29,29 @@ def drop(data: bool = False, force: bool = False):
     db.disconnect()
 
 
-def _apply(path: str):
+def _apply(name: str, variables: str | None = None):
+
+    def _build_from_template(t):
+        assert variables is not None, 'Variables must be provided for templates'
+        loaded = json.loads(variables)
+        return t(**loaded)
+
+    if os.path.exists(name):
+        try:
+            t = Template.read(name)
+            c = _build_from_template(t)
+        except Exception as e:
+            if 'Expecting' in str(e):
+                c = Component.read(name)
+    else:
+        from superduper import templates
+        t = getattr(templates, name)
+        c = _build_from_template(t)
+
     try:
         logging.info('Connecting to superduper')
         db = superduper()
-        logging.info('Reading component')
-        component = Component.read(path)
-        logging.info('Applying component to superduper')
-        db.apply(component)
+        db.apply(c)
     except Exception as e:
         raise e
     finally:

Pinnacle-Networks/Pinnacle

pinnacle/__main__.py
@@ -2,8 +2,7 @@ import sys
 
 import click
 
-from pinnacle.cli import app, apply, info
-from pinnacle.cli.serve import cdc, local_cluster, ray_serve, vector_search
+from pinnacle.cli import app, apply 
 
 __all__ = (
     'apply',

pinnacle/cli/apply.py
@@ -1,15 +1,21 @@
+import typing as t
+import json
+import os
+
 from pinnacle import Component, logging, pinnacle
+from pinnacle.components.template import Template
 
 from . import command
 
 
-@command(help='Apply the component to a `pinnacle` deployment')
-def apply(path: str):
+@command(help='Apply a template or application to a `pinnacle` deployment')
+def apply(name: str, variables: str | None = None):
     """Apply a serialized component.
 
-    :param path: Path to the stack.
+    :param name: Path or name of the template/ component.
+    :param values: JSON string of values to apply to the template.
     """
-    _apply(path)
+    _apply(name, variables)
 
 
 @command(help='`pinnacle` deployment')
@@ -23,14 +29,29 @@ def drop(data: bool = False, force: bool = False):
     db.disconnect()
 
 
-def _apply(path: str):
+def _apply(name: str, variables: str | None = None):
+
+    def _build_from_template(t):
+        assert variables is not None, 'Variables must be provided for templates'
+        loaded = json.loads(variables)
+        return t(**loaded)
+
+    if os.path.exists(name):
+        try:
+            t = Template.read(name)
+            c = _build_from_template(t)
+        except Exception as e:
+            if 'Expecting' in str(e):
+                c = Component.read(name)
+    else:
+        from pinnacle import templates
+        t = getattr(templates, name)
+        c = _build_from_template(t)
+
     try:
         logging.info('Connecting to pinnacle')
         db = pinnacle()
-        logging.info('Reading component')
-        component = Component.read(path)
-        logging.info('Applying component to pinnacle')
-        db.apply(component)
+        db.apply(c)
     except Exception as e:
         raise e
     finally: