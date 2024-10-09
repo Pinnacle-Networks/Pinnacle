superduper-io/superduper

Add autocomplete predict-id for user experience optimization

superduper/base/document.py
@@ -1,7 +1,7 @@
 import typing as t
 from collections import defaultdict
 
-from superduper import logging
+from superduper import CFG, logging
 from superduper.base.code import Code
 from superduper.base.constant import (
     KEY_BLOBS,
@@ -140,6 +140,17 @@ class Document(MongoStyleDict):
         out.update({KEY_BUILDS: builds, KEY_FILES: files, KEY_BLOBS: blobs})
         out = SuperDuperFlatEncode(out)
         return out
+
+    def __getitem__(self, key: str) -> t.Any:
+        if CFG.output_prefix not in key:
+            return super().__getitem__(key)
+
+        import re
+        if re.match(f'{CFG.output_prefix}[^_]{1,}__[a-z0-9]{10,}', key):
+            return super().__getitem__(key)
+
+        key = next(k for k in self.keys() if k.startswith(key))
+        return super().__getitem__(key)
 
     @classmethod
     def decode(
@@ -348,6 +359,7 @@ def _deep_flat_encode(
     leaves_to_keep=(),
     metadata: bool = True,
     defaults: bool = True,
+    uuids_to_keys: t.Dict | None = None,
 ):
     if isinstance(r, dict):
         tmp = {}
@@ -360,6 +372,7 @@ def _deep_flat_encode(
                 leaves_to_keep=leaves_to_keep,
                 metadata=metadata,
                 defaults=defaults,
+                uuids_to_keys=uuids_to_keys,
             )
         return tmp
 
@@ -374,6 +387,7 @@ def _deep_flat_encode(
                     leaves_to_keep=leaves_to_keep,
                     metadata=metadata,
                     defaults=defaults,
+                    uuids_to_keys=uuids_to_keys,
                 )
                 for x in r
             ]
@@ -388,6 +402,7 @@ def _deep_flat_encode(
             leaves_to_keep=leaves_to_keep,
             metadata=metadata,
             defaults=defaults,
+            uuids_to_keys=uuids_to_keys,
         )
 
     if isinstance(r, Blob):
@@ -414,6 +429,7 @@ def _deep_flat_encode(
             leaves_to_keep=leaves_to_keep,
             metadata=metadata,
             defaults=defaults,
+            uuids_to_keys=uuids_to_keys,
         )
         builds[ref] = r
         return f'?{ref}'
@@ -422,6 +438,7 @@ def _deep_flat_encode(
         # If the leaf is component, we need to store the type_id
         type_id = r.type_id if isinstance(r, Component) else None
         key = f"{type_id}:{r.identifier}" if type_id else r.identifier
+        uuid = r.uuid
 
         if key in builds:
             logging.warn(f'Leaf {key} already exists')
@@ -441,12 +458,15 @@ def _deep_flat_encode(
             leaves_to_keep=leaves_to_keep,
             metadata=metadata,
             defaults=defaults,
+            uuids_to_keys=uuids_to_keys,
         )
 
         identifier = r.pop('identifier')
         # Rebuild the key with the identifier
         key = f"{type_id}:{identifier}" if type_id else identifier
         builds[key] = r
+        if isinstance(uuids_to_keys, dict):
+            uuids_to_keys[key] = uuid
         return f'?{key}'
 
     return r
@@ -491,18 +511,46 @@ def _schema_decode(
 def _get_leaf_from_cache(k, builds, getters, db: t.Optional['Datalayer'] = None):
     if reference := parse_reference(f'?{k}'):
         if reference.name in getters:
-            return getters[reference.name](reference.path)
+            out = getters[reference.name](reference.path)
+            if reference.attribute is not None:
+                return getattr(out, reference.attribute)
+
+    attribute = None
+    if '.' in k:
+        k, attribute = k.split('.')
 
     if isinstance(builds[k], Leaf):
         leaf = builds[k]
         if not leaf.db:
             leaf.db = db
+        if attribute is not None:
+            return getattr(leaf, attribute)
         return leaf
+
     leaf = _deep_flat_decode(builds[k], builds, getters=getters, db=db)
+
     builds[k] = leaf
+    to_del = []
+    keys = list(builds.keys())
+    for other in keys:
+        import re
+        matches = re.findall(f'.*\?\(({k}\..*)\)', other)
+        old_other = other[:]
+        if matches:
+            for match in matches:
+                got = _get_leaf_from_cache(match, builds, getters, db=db)
+                other = other.replace(f'?({match})', got)
+            builds[other] = leaf
+            to_del.append(old_other)
+
+    for other in to_del:
+        del builds[other]
+        
     if isinstance(leaf, Leaf):
         if not leaf.db:
             leaf.db = db
+    if attribute is not None:
+        return getattr(leaf, attribute)
     return leaf
 
 
@@ -539,8 +587,14 @@ def _deep_flat_decode(r, builds, getters: _Getters, db: t.Optional['Datalayer'] 
             )
             for k, v in r.items()
         }
+    if isinstance(r, str) and '?(' in r:
+        import re
+        matches = re.findall(r'\?\((.*?)\)', r)
+        for match in matches:
+            r = r.replace(f'?({match})', _get_leaf_from_cache(match, builds, getters, db=db))
     if isinstance(r, str) and r.startswith('?'):
-        return _get_leaf_from_cache(r[1:], builds, getters=getters, db=db)
+        out = _get_leaf_from_cache(r[1:], builds, getters=getters, db=db)
+        return out
     if isinstance(r, str) and r.startswith('&'):
         assert getters is not None
         reference = parse_reference(r)

test/unittest/component/test_listener.py
@@ -4,7 +4,7 @@ import random
 import numpy as np
 import pytest
 
-from superduper import Document
+from superduper import Application, Document
 from superduper.backends.base.query import Query
 from superduper.base.constant import KEY_BLOBS
 from superduper.components.listener import Listener
@@ -281,7 +281,7 @@ def test_upstream_serializes(db):
 
 
 # TODO: Need to fix this test case
-@pytest.mark.skip("This test is not working")
+# @pytest.mark.skip("This test is not working")
 def test_predict_id_utils(db):
     db.cfg.auto_schema = True
     table = db["test"]
@@ -307,21 +307,104 @@ def test_predict_id_utils(db):
         identifier="listener1",
     )
 
-    db.add(listener1)
+    db.apply(listener1)
 
     outputs = "_outputs__listener1"
     # Listener identifier is set as the table name
     select = db[outputs].select()
-    docs = list(db.execute(select))
+    docs = select.tolist()
+    # docs = list(db.execute(select))
     assert [doc[listener1.outputs] for doc in docs] == [1, 2, 3]
 
     # Listener identifier is set as the table name and filter is applied
     table = db[outputs].select()
     select = table.filter(table[outputs] > 1)
-    docs = list(db.execute(select))
+    docs = select.tolist()
     assert [doc[listener1.outputs] for doc in docs] == [2, 3]
 
     # Listener identifier is set as the predict_id in outputs()
     select = db["test"].select().outputs('listener1')
-    docs = list(db.execute(select))
+    docs = select.tolist()
     assert [doc[listener1.outputs] for doc in docs] == [1, 2, 3]
+
+
+def test_complete_uuids(db):
+    
+    db.cfg.auto_schema = True
+    
+    m1 = ObjectModel(
+        "m1",
+        object=lambda x: x + 0,
+    )
+
+    q = db['test'].insert(
+        [
+            {"x": 1},
+            {"x": 2},
+            {"x": 3},
+        ]
+    )
+
+    db.execute(q)
+
+    l1 = Listener(
+        model=m1,
+        select=db['test'].select(),
+        key="x",
+        identifier="l1",
+    )
+
+    db.apply(l1)
+    
+    q = db['test'].outputs('l1')
+
+    qq = q.complete_uuids(db)
+
+    assert f'"{l1.predict_id}"' in str(qq)
+
+    results = q.tolist()
+
+    assert results[0]['_outputs__l1'] == results[0][l1.outputs]
+
+
+def test_autofill_data_listener(db):
+
+    db.cfg.auto_schema = True
+    
+    m = ObjectModel(
+        "m1",
+        object=lambda x: x + 2,
+    )
+
+    db['test'].insert(
+        [
+            {"x": 1},
+            {"x": 2},
+            {"x": 3},
+        ]
+    ).execute()
+
+    l1 = m.to_listener(select=db['test'].select(), key='x', identifier='l1')
+    l2 = m.to_listener(select=db['_outputs__l1'].select(), key='_outputs__l1', identifier='l2')
+
+    db.apply(l1)
+    db.apply(l2)
+
+    assert l2.key == l1.outputs
+    assert l1.outputs in str(l2.select)
+
+    app = Application('my-app', components=[l1, l2])
+
+    r = app.encode(metadata=False)
+
+    import pprint
+    pprint.pprint({k: v for k, v in r.items() if k not in {'_blobs', '_files'}})
+
+    out = Document.decode(r, db=db).unpack()
+
+    assert isinstance(out, Application)
+
+    assert isinstance(out.components[0], Listener)
+    assert isinstance(out.components[1], Listener)
+
+    assert isinstance(out.components[0].model, ObjectModel)

Pinnacle-Networks/Pinnacle

pinnacle/base/document.py
@@ -3,7 +3,7 @@ import typing as t
 from collections import defaultdict
 from functools import partial
 
-from pinnacle import logging
+from pinnacle import CFG, logging
 from pinnacle.base.code import Code
 from pinnacle.base.constant import (
     KEY_BLOBS,
@@ -162,6 +162,17 @@ class Document(MongoStyleDict):
         out.update({KEY_BUILDS: builds, KEY_FILES: files, KEY_BLOBS: blobs})
         out = SuperDuperFlatEncode(out)
         return out
+
+    def __getitem__(self, key: str) -> t.Any:
+        if CFG.output_prefix not in key:
+            return super().__getitem__(key)
+
+        import re
+        if re.match(f'{CFG.output_prefix}[^_]{1,}__[a-z0-9]{10,}', key):
+            return super().__getitem__(key)
+
+        key = next(k for k in self.keys() if k.startswith(key))
+        return super().__getitem__(key)
 
     @classmethod
     def decode(
@@ -374,6 +385,7 @@ def _deep_flat_encode(
     leaves_to_keep=(),
     metadata: bool = True,
     defaults: bool = True,
+    uuids_to_keys: t.Dict | None = None,
 ):
     if isinstance(r, dict):
         tmp = {}
@@ -386,6 +398,7 @@ def _deep_flat_encode(
                 leaves_to_keep=leaves_to_keep,
                 metadata=metadata,
                 defaults=defaults,
+                uuids_to_keys=uuids_to_keys,
             )
         return tmp
 
@@ -400,6 +413,7 @@ def _deep_flat_encode(
                     leaves_to_keep=leaves_to_keep,
                     metadata=metadata,
                     defaults=defaults,
+                    uuids_to_keys=uuids_to_keys,
                 )
                 for x in r
             ]
@@ -414,6 +428,7 @@ def _deep_flat_encode(
             leaves_to_keep=leaves_to_keep,
             metadata=metadata,
             defaults=defaults,
+            uuids_to_keys=uuids_to_keys,
         )
 
     if isinstance(r, Blob):
@@ -440,6 +455,7 @@ def _deep_flat_encode(
             leaves_to_keep=leaves_to_keep,
             metadata=metadata,
             defaults=defaults,
+            uuids_to_keys=uuids_to_keys,
         )
         builds[ref] = r
         return f'?{ref}'
@@ -448,6 +464,7 @@ def _deep_flat_encode(
         # If the leaf is component, we need to store the type_id
         type_id = r.type_id if isinstance(r, Component) else None
         key = f"{type_id}:{r.identifier}" if type_id else r.identifier
+        uuid = r.uuid
 
         if key in builds:
             logging.warn(f'Leaf {key} already exists')
@@ -467,12 +484,15 @@ def _deep_flat_encode(
             leaves_to_keep=leaves_to_keep,
             metadata=metadata,
             defaults=defaults,
+            uuids_to_keys=uuids_to_keys,
         )
 
         identifier = r.pop('identifier')
         # Rebuild the key with the identifier
         key = f"{type_id}:{identifier}" if type_id else identifier
         builds[key] = r
+        if isinstance(uuids_to_keys, dict):
+            uuids_to_keys[key] = uuid
         return f'?{key}'
 
     return r
@@ -517,18 +537,46 @@ def _schema_decode(
 def _get_leaf_from_cache(k, builds, getters, db: t.Optional['Datalayer'] = None):
     if reference := parse_reference(f'?{k}'):
         if reference.name in getters:
-            return getters[reference.name](reference.path)
+            out = getters[reference.name](reference.path)
+            if reference.attribute is not None:
+                return getattr(out, reference.attribute)
+
+    attribute = None
+    if '.' in k:
+        k, attribute = k.split('.')
 
     if isinstance(builds[k], Leaf):
         leaf = builds[k]
         if not leaf.db:
             leaf.db = db
+        if attribute is not None:
+            return getattr(leaf, attribute)
         return leaf
+
     leaf = _deep_flat_decode(builds[k], builds, getters=getters, db=db)
+
     builds[k] = leaf
+    to_del = []
+    keys = list(builds.keys())
+    for other in keys:
+        import re
+        matches = re.findall(f'.*\?\(({k}\..*)\)', other)
+        old_other = other[:]
+        if matches:
+            for match in matches:
+                got = _get_leaf_from_cache(match, builds, getters, db=db)
+                other = other.replace(f'?({match})', got)
+            builds[other] = leaf
+            to_del.append(old_other)
+
+    for other in to_del:
+        del builds[other]
+        
     if isinstance(leaf, Leaf):
         if not leaf.db:
             leaf.db = db
+    if attribute is not None:
+        return getattr(leaf, attribute)
     return leaf
 
 
@@ -565,8 +613,14 @@ def _deep_flat_decode(r, builds, getters: _Getters, db: t.Optional['Datalayer'] 
             )
             for k, v in r.items()
         }
+    if isinstance(r, str) and '?(' in r:
+        import re
+        matches = re.findall(r'\?\((.*?)\)', r)
+        for match in matches:
+            r = r.replace(f'?({match})', _get_leaf_from_cache(match, builds, getters, db=db))
     if isinstance(r, str) and r.startswith('?'):
-        return _get_leaf_from_cache(r[1:], builds, getters=getters, db=db)
+        out = _get_leaf_from_cache(r[1:], builds, getters=getters, db=db)
+        return out
     if isinstance(r, str) and r.startswith('&'):
         assert getters is not None
         reference = parse_reference(r)

test/unittest/component/test_listener.py
@@ -4,7 +4,7 @@ import random
 import numpy as np
 import pytest
 
-from pinnacle import Document
+from pinnacle import Application, Document
 from pinnacle.backends.base.query import Query
 from pinnacle.base.constant import KEY_BLOBS
 from pinnacle.components.listener import Listener
@@ -281,7 +281,7 @@ def test_upstream_serializes(db):
 
 
 # TODO: Need to fix this test case
-@pytest.mark.skip("This test is not working")
+# @pytest.mark.skip("This test is not working")
 def test_predict_id_utils(db):
     db.cfg.auto_schema = True
     table = db["test"]
@@ -307,21 +307,104 @@ def test_predict_id_utils(db):
         identifier="listener1",
     )
 
-    db.add(listener1)
+    db.apply(listener1)
 
     outputs = "_outputs__listener1"
     # Listener identifier is set as the table name
     select = db[outputs].select()
-    docs = list(db.execute(select))
+    docs = select.tolist()
+    # docs = list(db.execute(select))
     assert [doc[listener1.outputs] for doc in docs] == [1, 2, 3]
 
     # Listener identifier is set as the table name and filter is applied
     table = db[outputs].select()
     select = table.filter(table[outputs] > 1)
-    docs = list(db.execute(select))
+    docs = select.tolist()
     assert [doc[listener1.outputs] for doc in docs] == [2, 3]
 
     # Listener identifier is set as the predict_id in outputs()
     select = db["test"].select().outputs('listener1')
-    docs = list(db.execute(select))
+    docs = select.tolist()
     assert [doc[listener1.outputs] for doc in docs] == [1, 2, 3]
+
+
+def test_complete_uuids(db):
+    
+    db.cfg.auto_schema = True
+    
+    m1 = ObjectModel(
+        "m1",
+        object=lambda x: x + 0,
+    )
+
+    q = db['test'].insert(
+        [
+            {"x": 1},
+            {"x": 2},
+            {"x": 3},
+        ]
+    )
+
+    db.execute(q)
+
+    l1 = Listener(
+        model=m1,
+        select=db['test'].select(),
+        key="x",
+        identifier="l1",
+    )
+
+    db.apply(l1)
+    
+    q = db['test'].outputs('l1')
+
+    qq = q.complete_uuids(db)
+
+    assert f'"{l1.predict_id}"' in str(qq)
+
+    results = q.tolist()
+
+    assert results[0]['_outputs__l1'] == results[0][l1.outputs]
+
+
+def test_autofill_data_listener(db):
+
+    db.cfg.auto_schema = True
+    
+    m = ObjectModel(
+        "m1",
+        object=lambda x: x + 2,
+    )
+
+    db['test'].insert(
+        [
+            {"x": 1},
+            {"x": 2},
+            {"x": 3},
+        ]
+    ).execute()
+
+    l1 = m.to_listener(select=db['test'].select(), key='x', identifier='l1')
+    l2 = m.to_listener(select=db['_outputs__l1'].select(), key='_outputs__l1', identifier='l2')
+
+    db.apply(l1)
+    db.apply(l2)
+
+    assert l2.key == l1.outputs
+    assert l1.outputs in str(l2.select)
+
+    app = Application('my-app', components=[l1, l2])
+
+    r = app.encode(metadata=False)
+
+    import pprint
+    pprint.pprint({k: v for k, v in r.items() if k not in {'_blobs', '_files'}})
+
+    out = Document.decode(r, db=db).unpack()
+
+    assert isinstance(out, Application)
+
+    assert isinstance(out.components[0], Listener)
+    assert isinstance(out.components[1], Listener)
+
+    assert isinstance(out.components[0].model, ObjectModel)