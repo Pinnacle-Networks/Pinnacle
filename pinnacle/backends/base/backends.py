superduper-io/superduper

Pivot to 1-queue per table structure

superduper/backends/base/backends.py
@@ -1,12 +1,12 @@
 # TODO deprecate the implementations in favour of the plugin paradigm
 from abc import ABC, abstractmethod
 import typing as t
-from superduper.components.component import Component
 from superduper.vector_search.atlas import MongoAtlasVectorSearcher
 from superduper.vector_search.lance import LanceVectorSearcher
 from superduper.vector_search.qdrant import QdrantVectorSearcher
 
 if t.TYPE_CHECKING:
+    from superduper.components.component import Component
     from superduper.base.datalayer import Datalayer
 
 vector_searcher_implementations = {
@@ -46,7 +46,7 @@ class BaseBackend(ABC):
         """To be called on program start."""
         pass
 
-    def put(self, component: Component):
+    def put(self, component: 'Component'):
         # This is to make sure that we only have 1 version 
         # of each component implemented at any given time
         if component.uuid in self.list_uuids():

superduper/components/listener.py
@@ -7,7 +7,7 @@ from superduper import CFG, logging
 from superduper.backends.base.query import Query
 from superduper.base.datalayer import Datalayer
 from superduper.components.model import Mapping
-from superduper.components.trigger import Trigger
+from superduper.components.cdc import CDC
 from superduper.jobs.annotations import trigger
 
 from .model import Model, ModelInputType
@@ -19,7 +19,7 @@ if t.TYPE_CHECKING:
 SELECT_TEMPLATE = {'documents': [], 'query': '<collection_name>.find()'}
 
 
-class Listener(Trigger):
+class Listener(CDC):
     """Listener component.
 
     Listener object which is used to process a column/key of a collection or table,
@@ -28,15 +28,21 @@ class Listener(Trigger):
     :param key: Key to be bound to the model.
     :param model: Model for processing data.
     :param predict_kwargs: Keyword arguments to self.model.predict().
+    :param select: Query to "listen" for input on.
     :param identifier: A string used to identify the listener and it's outputs.
     """
+
+    type_id: t.ClassVar[str] = 'listener'
 
     key: ModelInputType
     model: Model
     predict_kwargs: t.Optional[t.Dict] = dc.field(default_factory=dict)
-    type_id: t.ClassVar[str] = 'listener'
+    select: t.Optional[Query] = None
+    cdc_table: str = ''
 
     def __post_init__(self, db, artifacts):
+        if not self.cdc_table and self.select:
+            self.cdc_table = self.select.table
         deps = self.dependencies
         if deps:
             if not self.upstream:
@@ -59,12 +65,12 @@ class Listener(Trigger):
         """Mapping property."""
         return Mapping(self.key, signature=self.model.signature)
 
-    # TODO - do we need the outputs-prefix?
     @property
     def outputs(self):
         """Get reference to outputs of listener model."""
         return f'{CFG.output_prefix}{self.predict_id}'
 
+    # TODO remove
     @property
     def outputs_key(self):
         """Model outputs key."""
@@ -80,12 +86,6 @@ class Listener(Trigger):
     def outputs_select(self):
         """Get select statement for outputs."""
         return self.db[self.select.table].select().outputs(self.predict_id)
-
-    # TODO do we need this?
-    @property
-    def cdc_table(self):
-        """Get table for cdc."""
-        return self.select.table_or_collection.identifier
 
     @override
     def post_create(self, db: "Datalayer") -> None:
@@ -128,70 +128,72 @@ class Listener(Trigger):
                 out.append(tuple(x[len(CFG.output_prefix) :].split('__')))
         return out
 
-    def trigger_ids(self, query: Query, primary_ids: t.Sequence):
-        """Get trigger IDs.
+    # def triggerz_ids(self, query: Query, primary_ids: t.Sequence):
+    #     """Get trigger IDs.
 
-        Only the ids returned by this function will trigger the listener.
+    #     Only the ids returned by this function will trigger the listener.
 
-        :param query: Query object.
-        :param primary_ids: Primary IDs.
-        """
-        conditions = [
-            # trigger by main table
-            self.select and self.select.table == query.table,
-            # trigger by output table
-            query.table in self.key and query.table != self.outputs,
-        ]
-        if not any(conditions):
-            return []
+    #     :param query: Query object.
+    #     :param primary_ids: Primary IDs.
+    #     """
+    #     conditions = [
+    #         # trigger by main table
+    #         self.select and self.select.table == query.table,
+    #         # trigger by output table
+    #         query.table in self.key and query.table != self.outputs,
+    #     ]
+    #     if not any(conditions):
+    #         return []
 
-        if self.select is None:
-            return []
+    #     if self.select is None:
+    #         return []
 
-        if self.select.table == query.table:
-            trigger_ids = list(primary_ids)
-        else:
-            trigger_ids = [
-                doc['_source'] for doc in query.documents if '_source' in doc
-            ]
+    #     if self.select.table == query.table:
+    #         triggerz_ids = list(primary_ids)
+    #     else:
+    #         triggerz_ids = [
+    #             doc['_source'] for doc in query.documents if '_source' in doc
+    #         ]
 
-        out = self.db.databackend.check_ready_ids(
-            self.select, self._ready_keys, trigger_ids
-        )
-        return out
+    #     out = self.db.databackend.check_ready_ids(
+    #         self.select, self._ready_keys, triggerz_ids
+    #     )
+    #     return out
 
-    @property
-    def _ready_keys(self):
-        keys = self.key
+    # @property
+    # def _ready_keys(self):
+    #     keys = self.key
 
-        if isinstance(self.key, str):
-            keys = [self.key]
-        elif isinstance(self.key, dict):
-            keys = list(self.key.keys())
+    #     if isinstance(self.key, str):
+    #         keys = [self.key]
+    #     elif isinstance(self.key, dict):
+    #         keys = list(self.key.keys())
 
-        # Support multiple levels of nesting
-        clean_keys = []
-        for key in keys:
-            if key.startswith(CFG.output_prefix):
-                key = CFG.output_prefix + key[len(CFG.output_prefix) :].split(".")[0]
-            else:
-                key = key.split(".")[0]
+    #     # Support multiple levels of nesting
+    #     clean_keys = []
+    #     for key in keys:
+    #         if key.startswith(CFG.output_prefix):
+    #             key = CFG.output_prefix + key[len(CFG.output_prefix) :].split(".")[0]
+    #         else:
+    #             key = key.split(".")[0]
 
-            clean_keys.append(key)
+    #         clean_keys.append(key)
 
-        return clean_keys
+    #     return clean_keys
 
     @trigger('apply', 'insert', 'update', requires='select')
-    def run(self, ids: t.Sequence[str] | None):
+    def run(self, ids: t.Sequence[str] | None = None) -> t.List[str]:
         """Run the listener."""
         assert self.select is not None
-        return self.model.predict_in_db(
+        # Returns a list of ids where the outputs were inserted
+        out = self.model.predict_in_db(
             X=self.key,
             predict_id=self.predict_id,
             select=self.select,
             ids=ids,
             **(self.predict_kwargs or {}),
         )
+        return out
 
     def cleanup(self, db: "Datalayer") -> None:
         """Clean up when the listener is deleted.

superduper/jobs/annotations.py
@@ -34,11 +34,13 @@ def trigger(
                 " to be triggered by anything apart from 'apply'"
             )
 
-        def decorated(self, ids: t.List[str] | None = None, job: bool = False):
+        def decorated(self, job: bool = False, **kwargs):
+            # the futures kwargs is a placeholder to allow for pinning 
+            # dependencies according to the readiness of upstream jobs
             if event_types != ('apply',):
                 msg = 'Method must be a `Trigger` instance to take non-applying events'
-                from superduper.components.trigger import Trigger
-                assert isinstance(self, Trigger), msg
+                from superduper.components.cdc import CDC
+                assert isinstance(self, CDC), msg
 
             if job:
                 return Job(
@@ -46,12 +48,12 @@ def trigger(
                     identifier=self.identifier,
                     uuid=self.uuid,
                     method=f.__name__,
-                    args=(ids,) if takes_ids else (),
+                    kwargs=kwargs,
                     db=self.db,
                 )
             else:
                 self.init()
-                return f(self, ids) if takes_ids else f(self)
+                return f(self, **kwargs)
 
         decorated.events = event_types
         decorated.depends = depends

plugins/mongodb/superduper_mongodb/metadata.py
@@ -4,6 +4,7 @@ import click
 from pymongo.results import DeleteResult, InsertOneResult, UpdateResult
 from superduper import logging
 from superduper.backends.base.metadata import MetaDataStore
+from superduper.components.component import Status
 from superduper.misc.colors import Colors
 
 
@@ -170,6 +171,17 @@ class MongoMetaDataStore(MetaDataStore):
         """
         return self.job_collection.update_one(
             {'identifier': identifier}, {'$set': {key: value}}
+        )
+
+    def show_cdc_tables(self):
+        return self.component_collection.distinct('cdc_table')
+
+    def _show_cdcs(self, table):
+        return list(
+            self.component_collection.find(
+                {'cdc_table': table}, 
+                {'identifier': 1, '_id': 0, 'type_id': 1, 'version': 1, 'uuid': 1}
+            )
         )
 
     def _show_components(self, type_id: t.Optional[str] = None):
@@ -384,3 +396,6 @@ class MongoMetaDataStore(MetaDataStore):
         """Disconnect the client."""
 
         # TODO: implement me
+
+    def set_component_status(self, uuid, status: Status):
+        return self.component_collection.update_one({'uuid': uuid}, {'$set': {'status': status}})

Pinnacle-Networks/Pinnacle

pinnacle/backends/base/backends.py
@@ -1,12 +1,12 @@
 # TODO deprecate the implementations in favour of the plugin paradigm
 from abc import ABC, abstractmethod
 import typing as t
-from pinnacle.components.component import Component
 from pinnacle.vector_search.atlas import MongoAtlasVectorSearcher
 from pinnacle.vector_search.lance import LanceVectorSearcher
 from pinnacle.vector_search.qdrant import QdrantVectorSearcher
 
 if t.TYPE_CHECKING:
+    from pinnacle.components.component import Component
     from pinnacle.base.datalayer import Datalayer
 
 vector_searcher_implementations = {
@@ -46,7 +46,7 @@ class BaseBackend(ABC):
         """To be called on program start."""
         pass
 
-    def put(self, component: Component):
+    def put(self, component: 'Component'):
         # This is to make sure that we only have 1 version 
         # of each component implemented at any given time
         if component.uuid in self.list_uuids():

pinnacle/components/listener.py
@@ -7,7 +7,7 @@ from pinnacle import CFG, logging
 from pinnacle.backends.base.query import Query
 from pinnacle.base.datalayer import Datalayer
 from pinnacle.components.model import Mapping
-from pinnacle.components.trigger import Trigger
+from pinnacle.components.cdc import CDC
 from pinnacle.jobs.annotations import trigger
 
 from .model import Model, ModelInputType
@@ -19,7 +19,7 @@ if t.TYPE_CHECKING:
 SELECT_TEMPLATE = {'documents': [], 'query': '<collection_name>.find()'}
 
 
-class Listener(Trigger):
+class Listener(CDC):
     """Listener component.
 
     Listener object which is used to process a column/key of a collection or table,
@@ -28,15 +28,21 @@ class Listener(Trigger):
     :param key: Key to be bound to the model.
     :param model: Model for processing data.
     :param predict_kwargs: Keyword arguments to self.model.predict().
+    :param select: Query to "listen" for input on.
     :param identifier: A string used to identify the listener and it's outputs.
     """
+
+    type_id: t.ClassVar[str] = 'listener'
 
     key: ModelInputType
     model: Model
     predict_kwargs: t.Optional[t.Dict] = dc.field(default_factory=dict)
-    type_id: t.ClassVar[str] = 'listener'
+    select: t.Optional[Query] = None
+    cdc_table: str = ''
 
     def __post_init__(self, db, artifacts):
+        if not self.cdc_table and self.select:
+            self.cdc_table = self.select.table
         deps = self.dependencies
         if deps:
             if not self.upstream:
@@ -59,12 +65,12 @@ class Listener(Trigger):
         """Mapping property."""
         return Mapping(self.key, signature=self.model.signature)
 
-    # TODO - do we need the outputs-prefix?
     @property
     def outputs(self):
         """Get reference to outputs of listener model."""
         return f'{CFG.output_prefix}{self.predict_id}'
 
+    # TODO remove
     @property
     def outputs_key(self):
         """Model outputs key."""
@@ -80,12 +86,6 @@ class Listener(Trigger):
     def outputs_select(self):
         """Get select statement for outputs."""
         return self.db[self.select.table].select().outputs(self.predict_id)
-
-    # TODO do we need this?
-    @property
-    def cdc_table(self):
-        """Get table for cdc."""
-        return self.select.table_or_collection.identifier
 
     @override
     def post_create(self, db: "Datalayer") -> None:
@@ -128,70 +128,72 @@ class Listener(Trigger):
                 out.append(tuple(x[len(CFG.output_prefix) :].split('__')))
         return out
 
-    def trigger_ids(self, query: Query, primary_ids: t.Sequence):
-        """Get trigger IDs.
+    # def triggerz_ids(self, query: Query, primary_ids: t.Sequence):
+    #     """Get trigger IDs.
 
-        Only the ids returned by this function will trigger the listener.
+    #     Only the ids returned by this function will trigger the listener.
 
-        :param query: Query object.
-        :param primary_ids: Primary IDs.
-        """
-        conditions = [
-            # trigger by main table
-            self.select and self.select.table == query.table,
-            # trigger by output table
-            query.table in self.key and query.table != self.outputs,
-        ]
-        if not any(conditions):
-            return []
+    #     :param query: Query object.
+    #     :param primary_ids: Primary IDs.
+    #     """
+    #     conditions = [
+    #         # trigger by main table
+    #         self.select and self.select.table == query.table,
+    #         # trigger by output table
+    #         query.table in self.key and query.table != self.outputs,
+    #     ]
+    #     if not any(conditions):
+    #         return []
 
-        if self.select is None:
-            return []
+    #     if self.select is None:
+    #         return []
 
-        if self.select.table == query.table:
-            trigger_ids = list(primary_ids)
-        else:
-            trigger_ids = [
-                doc['_source'] for doc in query.documents if '_source' in doc
-            ]
+    #     if self.select.table == query.table:
+    #         triggerz_ids = list(primary_ids)
+    #     else:
+    #         triggerz_ids = [
+    #             doc['_source'] for doc in query.documents if '_source' in doc
+    #         ]
 
-        out = self.db.databackend.check_ready_ids(
-            self.select, self._ready_keys, trigger_ids
-        )
-        return out
+    #     out = self.db.databackend.check_ready_ids(
+    #         self.select, self._ready_keys, triggerz_ids
+    #     )
+    #     return out
 
-    @property
-    def _ready_keys(self):
-        keys = self.key
+    # @property
+    # def _ready_keys(self):
+    #     keys = self.key
 
-        if isinstance(self.key, str):
-            keys = [self.key]
-        elif isinstance(self.key, dict):
-            keys = list(self.key.keys())
+    #     if isinstance(self.key, str):
+    #         keys = [self.key]
+    #     elif isinstance(self.key, dict):
+    #         keys = list(self.key.keys())
 
-        # Support multiple levels of nesting
-        clean_keys = []
-        for key in keys:
-            if key.startswith(CFG.output_prefix):
-                key = CFG.output_prefix + key[len(CFG.output_prefix) :].split(".")[0]
-            else:
-                key = key.split(".")[0]
+    #     # Support multiple levels of nesting
+    #     clean_keys = []
+    #     for key in keys:
+    #         if key.startswith(CFG.output_prefix):
+    #             key = CFG.output_prefix + key[len(CFG.output_prefix) :].split(".")[0]
+    #         else:
+    #             key = key.split(".")[0]
 
-            clean_keys.append(key)
+    #         clean_keys.append(key)
 
-        return clean_keys
+    #     return clean_keys
 
     @trigger('apply', 'insert', 'update', requires='select')
-    def run(self, ids: t.Sequence[str] | None):
+    def run(self, ids: t.Sequence[str] | None = None) -> t.List[str]:
         """Run the listener."""
         assert self.select is not None
-        return self.model.predict_in_db(
+        # Returns a list of ids where the outputs were inserted
+        out = self.model.predict_in_db(
             X=self.key,
             predict_id=self.predict_id,
             select=self.select,
             ids=ids,
             **(self.predict_kwargs or {}),
         )
+        return out
 
     def cleanup(self, db: "Datalayer") -> None:
         """Clean up when the listener is deleted.

pinnacle/jobs/annotations.py
@@ -34,11 +34,13 @@ def trigger(
                 " to be triggered by anything apart from 'apply'"
             )
 
-        def decorated(self, ids: t.List[str] | None = None, job: bool = False):
+        def decorated(self, job: bool = False, **kwargs):
+            # the futures kwargs is a placeholder to allow for pinning 
+            # dependencies according to the readiness of upstream jobs
             if event_types != ('apply',):
                 msg = 'Method must be a `Trigger` instance to take non-applying events'
-                from pinnacle.components.trigger import Trigger
-                assert isinstance(self, Trigger), msg
+                from pinnacle.components.cdc import CDC
+                assert isinstance(self, CDC), msg
 
             if job:
                 return Job(
@@ -46,12 +48,12 @@ def trigger(
                     identifier=self.identifier,
                     uuid=self.uuid,
                     method=f.__name__,
-                    args=(ids,) if takes_ids else (),
+                    kwargs=kwargs,
                     db=self.db,
                 )
             else:
                 self.init()
-                return f(self, ids) if takes_ids else f(self)
+                return f(self, **kwargs)
 
         decorated.events = event_types
         decorated.depends = depends

plugins/mongodb/superduper_mongodb/metadata.py
@@ -4,6 +4,7 @@ import click
 from pymongo.results import DeleteResult, InsertOneResult, UpdateResult
 from pinnacle import logging
 from pinnacle.backends.base.metadata import MetaDataStore
+from pinnacle.components.component import Status
 from pinnacle.misc.colors import Colors
 
 
@@ -184,6 +185,17 @@ class MongoMetaDataStore(MetaDataStore):
         """
         return self.job_collection.update_one(
             {'identifier': identifier}, {'$set': {key: value}}
+        )
+
+    def show_cdc_tables(self):
+        return self.component_collection.distinct('cdc_table')
+
+    def _show_cdcs(self, table):
+        return list(
+            self.component_collection.find(
+                {'cdc_table': table}, 
+                {'identifier': 1, '_id': 0, 'type_id': 1, 'version': 1, 'uuid': 1}
+            )
         )
 
     def _show_components(self, type_id: t.Optional[str] = None):
@@ -398,3 +410,6 @@ class MongoMetaDataStore(MetaDataStore):
         """Disconnect the client."""
 
         # TODO: implement me
+
+    def set_component_status(self, uuid, status: Status):
+        return self.component_collection.update_one({'uuid': uuid}, {'$set': {'status': status}})