superduper-io/superduper

Add component initialization in cluster

superduper/backends/base/queue.py
@@ -1,21 +1,18 @@
 import dataclasses as dc
-import networkx as nx
 import typing as t
+import uuid
 from abc import ABC, abstractmethod
 from collections import defaultdict
-import uuid
 
-from superduper import logging, CFG
+import networkx as nx
+
+from superduper import CFG, logging
 from superduper.backends.base.backends import BaseBackend
 from superduper.base.event import Event
 
 DependencyType = t.Union[t.Dict[str, str], t.Sequence[t.Dict[str, str]]]
 
 if t.TYPE_CHECKING:
-    from superduper.components.cdc import CDC
-    from superduper.base.datalayer import Datalayer
-    from superduper.components.component import Component
-    from superduper.components.cdc import CDC
     from superduper.base.datalayer import Datalayer
 
 
@@ -92,12 +89,22 @@ class BaseQueuePublisher(BaseBackend):
         :param to: Component name for events to be published.
         """
 
+    @property
+    def db(self) -> 'Datalayer':
+        return self._db
+
+    @db.setter
+    def db(self, value: 'Datalayer'):
+        self._db = value
+        self.initialize()
+
 
 class JobFutureException(Exception):
     """Exception when futures are not ready.
 
     # noqa
     """
+
     ...
 
 
@@ -182,6 +189,13 @@ def _consume_event_type(event_type, ids, table, db: 'Datalayer'):
 
 
 def consume_events(events, table: str, db=None):
+    """
+    Consume events from table queue.
+
+    :param events: List of events to be consumed.
+    :param table: Queue Table.
+    :param db: Datalayer instance.
+    """
     if table != '_apply':
         consume_streaming_events(events=events, table=table, db=db)
     else:

superduper/base/datalayer.py
@@ -19,7 +19,7 @@ from superduper.base.config import Config
 from superduper.base.constant import KEY_BUILDS
 from superduper.base.cursor import SuperDuperCursor
 from superduper.base.document import Document
-from superduper.base.event import Create, DeploymentPlan, Signal
+from superduper.base.event import Create, Signal
 from superduper.components.component import Component, Status
 from superduper.components.datatype import DataType
 from superduper.components.schema import Schema
@@ -451,7 +451,9 @@ class Datalayer:
         print('\n----------------')
         print('CREATION EVENTS:')
         print('----------------')
-        steps = {c.component['uuid'] : str(i) for i, c in enumerate(unique_create_events)}
+        steps = {
+            c.component['uuid']: str(i) for i, c in enumerate(unique_create_events)
+        }
         for i, c in enumerate(unique_create_events):
             if c.parent:
                 print(f'[{i}]: {c.huuid}: create ~ [{steps[c.parent]}]')
@@ -464,7 +466,9 @@ class Datalayer:
         steps = {j.job_id: str(i) for i, j in enumerate(unique_job_events)}
         for i, j in enumerate(unique_job_events):
             if j.dependencies:
-                print(f'[{i}]: {j.huuid}: {j.method} ~ [{",".join([steps[d] for d in j.dependencies])}]')
+                print(
+                    f'[{i}]: {j.huuid}: {j.method} ~ [{",".join([steps[d] for d in j.dependencies])}]'
+                )
             else:
                 print(f'[{i}]: {j.huuid}: {j.method}')
         print('\n')
@@ -481,8 +485,7 @@ class Datalayer:
                 default=False,
             ):
                 return object
-
-        self.cluster.queue.publish(events=events)
+        self.cluster.queue.publish(events=events[::-1])
         return object
 
     def remove(

Pinnacle-Networks/Pinnacle

pinnacle/backends/base/queue.py
@@ -1,21 +1,18 @@
 import dataclasses as dc
-import networkx as nx
 import typing as t
+import uuid
 from abc import ABC, abstractmethod
 from collections import defaultdict
-import uuid
 
-from pinnacle import logging, CFG
+import networkx as nx
+
+from pinnacle import CFG, logging
 from pinnacle.backends.base.backends import BaseBackend
 from pinnacle.base.event import Event
 
 DependencyType = t.Union[t.Dict[str, str], t.Sequence[t.Dict[str, str]]]
 
 if t.TYPE_CHECKING:
-    from pinnacle.components.cdc import CDC
-    from pinnacle.base.datalayer import Datalayer
-    from pinnacle.components.component import Component
-    from pinnacle.components.cdc import CDC
     from pinnacle.base.datalayer import Datalayer
 
 
@@ -92,12 +89,22 @@ class BaseQueuePublisher(BaseBackend):
         :param to: Component name for events to be published.
         """
 
+    @property
+    def db(self) -> 'Datalayer':
+        return self._db
+
+    @db.setter
+    def db(self, value: 'Datalayer'):
+        self._db = value
+        self.initialize()
+
 
 class JobFutureException(Exception):
     """Exception when futures are not ready.
 
     # noqa
     """
+
     ...
 
 
@@ -182,6 +189,13 @@ def _consume_event_type(event_type, ids, table, db: 'Datalayer'):
 
 
 def consume_events(events, table: str, db=None):
+    """
+    Consume events from table queue.
+
+    :param events: List of events to be consumed.
+    :param table: Queue Table.
+    :param db: Datalayer instance.
+    """
     if table != '_apply':
         consume_streaming_events(events=events, table=table, db=db)
     else:

pinnacle/base/datalayer.py
@@ -19,7 +19,7 @@ from pinnacle.base.config import Config
 from pinnacle.base.constant import KEY_BUILDS
 from pinnacle.base.cursor import SuperDuperCursor
 from pinnacle.base.document import Document
-from pinnacle.base.event import Create, DeploymentPlan, Signal
+from pinnacle.base.event import Create, Signal
 from pinnacle.components.component import Component, Status
 from pinnacle.components.datatype import DataType
 from pinnacle.components.schema import Schema
@@ -451,7 +451,9 @@ class Datalayer:
         print('\n----------------')
         print('CREATION EVENTS:')
         print('----------------')
-        steps = {c.component['uuid'] : str(i) for i, c in enumerate(unique_create_events)}
+        steps = {
+            c.component['uuid']: str(i) for i, c in enumerate(unique_create_events)
+        }
         for i, c in enumerate(unique_create_events):
             if c.parent:
                 print(f'[{i}]: {c.huuid}: create ~ [{steps[c.parent]}]')
@@ -464,7 +466,9 @@ class Datalayer:
         steps = {j.job_id: str(i) for i, j in enumerate(unique_job_events)}
         for i, j in enumerate(unique_job_events):
             if j.dependencies:
-                print(f'[{i}]: {j.huuid}: {j.method} ~ [{",".join([steps[d] for d in j.dependencies])}]')
+                print(
+                    f'[{i}]: {j.huuid}: {j.method} ~ [{",".join([steps[d] for d in j.dependencies])}]'
+                )
             else:
                 print(f'[{i}]: {j.huuid}: {j.method}')
         print('\n')
@@ -481,8 +485,7 @@ class Datalayer:
                 default=False,
             ):
                 return object
-
-        self.cluster.queue.publish(events=events)
+        self.cluster.queue.publish(events=events[::-1])
         return object
 
     def remove(