superduper-io/superduper

Simplify Job class

superduper/components/component.py
@@ -17,7 +17,7 @@ from superduper import logging
 from superduper.base.constant import KEY_BLOBS, KEY_FILES
 from superduper.base.event import EventType
 from superduper.base.leaf import Leaf
-from superduper.jobs.job import ComponentJob, Job
+from superduper.jobs.job import Job
 
 if t.TYPE_CHECKING:
     from superduper import Document
@@ -200,7 +200,7 @@ class Component(Leaf):
         
         :param event_type: EventType.
         """
-        # Get all of the methods in the class which have the `@apply` decorator
+        # Get all of the methods in the class which have the `@trigger` decorator
         # and which match the event type
         d = set(dir(self)) - {'get_triggers', 'triggers'}
         return [
@@ -252,10 +252,10 @@ class Component(Leaf):
                 local_dependencies = [lookup[f] for f in depends] + job_dependencies
 
                 if 'ids' in inspect.signature(attr).parameters:
-                    job: ComponentJob = attr(ids=event.ids, job=True)
+                    job: Job = attr(ids=event.ids, job=True)
                 else:
                     # 'apply'
-                    job: ComponentJob = attr(job=True)
+                    job: Job = attr(job=True)
 
                 # log the dependencies
                 lookup[attr_name] = job.job_id
@@ -454,6 +454,7 @@ class Component(Leaf):
         assert db
         if self.triggers:
             db.compute.queue.declare_component(self)
+            db.compute.declare_component(self)
 
     def on_load(self, db: Datalayer) -> None:
         """Called when this component is loaded from the data store.
@@ -672,29 +673,6 @@ class Component(Leaf):
         r = r['_content']
         assert r['version'] is not None
         return db.load(r['type_id'], r['identifier'], r['version'], allow_hidden=True)
-
-    def create_validation_job(
-        self,
-        validation_set: t.Union[str, Dataset],
-        metrics: t.Sequence[str],
-    ) -> ComponentJob:
-        """Method to create a validation job with `validation_set` and `metrics`.
-
-        :param validation_set: Kwargs for the `predict` method of `Model`.
-        :param metrics: Kwargs for the `predict` method of `Model` to set
-                        metrics for the validation job.
-        """
-        assert self.identifier is not None
-        return ComponentJob(
-            component_identifier=self.identifier,
-            component_uuid=self.uuid,
-            method_name='predict',
-            type_id='model',
-            kwargs={
-                'validation_set': validation_set,
-                'metrics': metrics,
-            },
-        )
 
     def __setattr__(self, k, v):
         if k in dc.fields(self):

superduper/jobs/annotations.py
@@ -2,7 +2,7 @@ import inspect
 import typing as t
 
 from superduper.base.event import EventType
-from superduper.jobs.job import ComponentJob
+from superduper.jobs.job import Job
 
 
 def trigger(*events: t.Sequence[EventType], depends: t.Sequence[str] | str = (), requires: t.Sequence[str] | str = ()):
@@ -22,8 +22,7 @@ def trigger(*events: t.Sequence[EventType], depends: t.Sequence[str] | str = (),
         requires = [requires]
 
     def decorator(f):
-        """Decorator to trigger a method when an event of type.
-        """
+        """Decorator to trigger a method when an event of type."""
         takes_ids = 'ids' in inspect.signature(f).parameters
         if events != ('apply',):
             assert takes_ids, (
@@ -32,15 +31,14 @@ def trigger(*events: t.Sequence[EventType], depends: t.Sequence[str] | str = (),
             )
 
         def decorated(self, ids: t.List[str] | None = None, job: bool = False):
-
             if job:
-                return ComponentJob(
-                    component_identifier=self.identifier,
-                    component_uuid=self.uuid,
+                return Job(
                     type_id=self.type_id,
-                    method_name=f.__name__,
+                    identifier=self.identifier,
+                    uuid=self.uuid,
+                    method=f.__name__,
                     args=(ids,) if takes_ids else (),
-                    db=self.db,
+                    db=self.db
                 )
             else:
                 return f(self, ids) if takes_ids else f(self)

test/unittest/component/test_model.py
@@ -24,7 +24,7 @@ from superduper.components.model import (
     Validation,
 )
 from superduper.components.schema import FieldType
-from superduper.jobs.job import ComponentJob
+from superduper.jobs.job import Job
 
 
 # ------------------------------------------
@@ -285,12 +285,12 @@ def test_model_create_fit_job(db):
     # TODO move these parameters into the `Trainer` (same thing for validation)
     model.trainer = MyTrainer('test', select=db['documents'].select(), key='x')
     db.apply(model)
-    with patch.object(ComponentJob, '__call__') as mock_call:
+    with patch.object(Job, '__call__') as mock_call:
         mock_call.return_value = None
     model.db = db
     job = model.fit_in_db(job=True)
-    assert job.component_identifier == model.identifier
-    assert job.method_name == 'fit_in_db'
+    assert job.identifier == model.identifier
+    assert job.method == 'fit_in_db'
 
 
 def test_model_fit(db):

Pinnacle-Networks/Pinnacle

pinnacle/components/component.py
@@ -17,7 +17,7 @@ from pinnacle import logging
 from pinnacle.base.constant import KEY_BLOBS, KEY_FILES
 from pinnacle.base.event import EventType
 from pinnacle.base.leaf import Leaf
-from pinnacle.jobs.job import ComponentJob, Job
+from pinnacle.jobs.job import Job
 
 if t.TYPE_CHECKING:
     from pinnacle import Document
@@ -200,7 +200,7 @@ class Component(Leaf):
         
         :param event_type: EventType.
         """
-        # Get all of the methods in the class which have the `@apply` decorator
+        # Get all of the methods in the class which have the `@trigger` decorator
         # and which match the event type
         d = set(dir(self)) - {'get_triggers', 'triggers'}
         return [
@@ -252,10 +252,10 @@ class Component(Leaf):
                 local_dependencies = [lookup[f] for f in depends] + job_dependencies
 
                 if 'ids' in inspect.signature(attr).parameters:
-                    job: ComponentJob = attr(ids=event.ids, job=True)
+                    job: Job = attr(ids=event.ids, job=True)
                 else:
                     # 'apply'
-                    job: ComponentJob = attr(job=True)
+                    job: Job = attr(job=True)
 
                 # log the dependencies
                 lookup[attr_name] = job.job_id
@@ -454,6 +454,7 @@ class Component(Leaf):
         assert db
         if self.triggers:
             db.compute.queue.declare_component(self)
+            db.compute.declare_component(self)
 
     def on_load(self, db: Datalayer) -> None:
         """Called when this component is loaded from the data store.
@@ -688,29 +689,6 @@ class Component(Leaf):
         r = r['_content']
         assert r['version'] is not None
         return db.load(r['type_id'], r['identifier'], r['version'], allow_hidden=True)
-
-    def create_validation_job(
-        self,
-        validation_set: t.Union[str, Dataset],
-        metrics: t.Sequence[str],
-    ) -> ComponentJob:
-        """Method to create a validation job with `validation_set` and `metrics`.
-
-        :param validation_set: Kwargs for the `predict` method of `Model`.
-        :param metrics: Kwargs for the `predict` method of `Model` to set
-                        metrics for the validation job.
-        """
-        assert self.identifier is not None
-        return ComponentJob(
-            component_identifier=self.identifier,
-            component_uuid=self.uuid,
-            method_name='predict',
-            type_id='model',
-            kwargs={
-                'validation_set': validation_set,
-                'metrics': metrics,
-            },
-        )
 
     def __setattr__(self, k, v):
         if k in dc.fields(self):

pinnacle/jobs/annotations.py
@@ -2,7 +2,7 @@ import inspect
 import typing as t
 
 from pinnacle.base.event import EventType
-from pinnacle.jobs.job import ComponentJob
+from pinnacle.jobs.job import Job
 
 
 def trigger(*events: t.Sequence[EventType], depends: t.Sequence[str] | str = (), requires: t.Sequence[str] | str = ()):
@@ -22,8 +22,7 @@ def trigger(*events: t.Sequence[EventType], depends: t.Sequence[str] | str = (),
         requires = [requires]
 
     def decorator(f):
-        """Decorator to trigger a method when an event of type.
-        """
+        """Decorator to trigger a method when an event of type."""
         takes_ids = 'ids' in inspect.signature(f).parameters
         if events != ('apply',):
             assert takes_ids, (
@@ -32,15 +31,14 @@ def trigger(*events: t.Sequence[EventType], depends: t.Sequence[str] | str = (),
             )
 
         def decorated(self, ids: t.List[str] | None = None, job: bool = False):
-
             if job:
-                return ComponentJob(
-                    component_identifier=self.identifier,
-                    component_uuid=self.uuid,
+                return Job(
                     type_id=self.type_id,
-                    method_name=f.__name__,
+                    identifier=self.identifier,
+                    uuid=self.uuid,
+                    method=f.__name__,
                     args=(ids,) if takes_ids else (),
-                    db=self.db,
+                    db=self.db
                 )
             else:
                 return f(self, ids) if takes_ids else f(self)

test/unittest/component/test_model.py
@@ -24,7 +24,7 @@ from pinnacle.components.model import (
     Validation,
 )
 from pinnacle.components.schema import FieldType
-from pinnacle.jobs.job import ComponentJob
+from pinnacle.jobs.job import Job
 
 
 # ------------------------------------------
@@ -285,12 +285,12 @@ def test_model_create_fit_job(db):
     # TODO move these parameters into the `Trainer` (same thing for validation)
     model.trainer = MyTrainer('test', select=db['documents'].select(), key='x')
     db.apply(model)
-    with patch.object(ComponentJob, '__call__') as mock_call:
+    with patch.object(Job, '__call__') as mock_call:
         mock_call.return_value = None
     model.db = db
     job = model.fit_in_db(job=True)
-    assert job.component_identifier == model.identifier
-    assert job.method_name == 'fit_in_db'
+    assert job.identifier == model.identifier
+    assert job.method == 'fit_in_db'
 
 
 def test_model_fit(db):