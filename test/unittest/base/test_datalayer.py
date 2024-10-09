superduper-io/superduper

Remove _Fittable

test/unittest/base/test_datalayer.py
@@ -20,12 +20,12 @@ from superduper.components.datatype import (
     pickle_serializer,
 )
 from superduper.components.listener import Listener
-from superduper.components.model import Model, ObjectModel, Trainer, _Fittable
+from superduper.components.model import Model, ObjectModel, Trainer 
 from superduper.components.schema import FieldType, Schema
 from superduper.components.table import Table
 
 
-class FakeModel(Model, _Fittable):
+class FakeModel(Model):
     """Fake model for testing."""
 
     ...
@@ -516,7 +516,7 @@ def test_replace_with_child(db):
         trainer=trainer,
     )
 
-    with patch.object(_Fittable, 'fit'):
+    with patch.object(FakeModel, 'fit'):
         db.apply(model)
 
     model_ids = db.show('model')

test/unittest/component/test_listener.py
@@ -8,7 +8,7 @@ from superduper import Document
 from superduper.backends.base.query import Query
 from superduper.base.constant import KEY_BLOBS
 from superduper.components.listener import Listener
-from superduper.components.model import ObjectModel, Trainer, _Fittable
+from superduper.components.model import ObjectModel, Trainer 
 
 
 class MyTrainer(Trainer):
@@ -19,12 +19,8 @@ class MyTrainer(Trainer):
 
 
 @dc.dataclass
-class _Tmp(_Fittable, ObjectModel):
-    def schedule_jobs(self, *args, **kwargs):
-        return _Fittable.schedule_jobs(self, *args, **kwargs)
-
-    def post_create(self, *args, **kwargs):
-        return _Fittable.post_create(self, *args, **kwargs)
+class _Tmp(ObjectModel):
+    ...
 
 
 def test_listener_serializes_properly():

Pinnacle-Networks/Pinnacle

test/unittest/base/test_datalayer.py
@@ -20,12 +20,12 @@ from pinnacle.components.datatype import (
     pickle_serializer,
 )
 from pinnacle.components.listener import Listener
-from pinnacle.components.model import Model, ObjectModel, Trainer, _Fittable
+from pinnacle.components.model import Model, ObjectModel, Trainer 
 from pinnacle.components.schema import FieldType, Schema
 from pinnacle.components.table import Table
 
 
-class FakeModel(Model, _Fittable):
+class FakeModel(Model):
     """Fake model for testing."""
 
     ...
@@ -516,7 +516,7 @@ def test_replace_with_child(db):
         trainer=trainer,
     )
 
-    with patch.object(_Fittable, 'fit'):
+    with patch.object(FakeModel, 'fit'):
         db.apply(model)
 
     model_ids = db.show('model')

test/unittest/component/test_listener.py
@@ -8,7 +8,7 @@ from pinnacle import Document
 from pinnacle.backends.base.query import Query
 from pinnacle.base.constant import KEY_BLOBS
 from pinnacle.components.listener import Listener
-from pinnacle.components.model import ObjectModel, Trainer, _Fittable
+from pinnacle.components.model import ObjectModel, Trainer 
 
 
 class MyTrainer(Trainer):
@@ -19,12 +19,8 @@ class MyTrainer(Trainer):
 
 
 @dc.dataclass
-class _Tmp(_Fittable, ObjectModel):
-    def schedule_jobs(self, *args, **kwargs):
-        return _Fittable.schedule_jobs(self, *args, **kwargs)
-
-    def post_create(self, *args, **kwargs):
-        return _Fittable.post_create(self, *args, **kwargs)
+class _Tmp(ObjectModel):
+    ...
 
 
 def test_listener_serializes_properly():