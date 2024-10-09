superduper-io/superduper

Execute jobs at the end

superduper/backends/local/queue.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import typing as t
 
+from superduper import logging
 from superduper.backends.base.queue import consume_apply_event, consume_streaming_events
 from superduper.base.event import Event
 from superduper.backends.base.queue import BaseQueueConsumer, BaseQueuePublisher
@@ -71,7 +72,6 @@ class LocalQueuePublisher(BaseQueuePublisher):
                 self.queue['_apply'].append(event)
             else:
                 self.queue[event.source].append(event)
-
         self.consumer.consume(db=self.db, queue=self.queue)
 
 
@@ -96,7 +96,10 @@ class LocalQueueConsumer(BaseQueueConsumer):
                 while queue['_apply']:
                     event = queue['_apply'].pop()
                     if event.msg == 'done':
-                        del self.futures[event.context]
+                        try:
+                            del self.futures[event.context]
+                        except KeyError:
+                            logging.info(f'No futures saved for {event.context}')
                     else:
                         consume_apply_event(event=event, db=db, futures=self.futures[event.source])
 

superduper/base/event.py
@@ -1,8 +1,9 @@
 import dataclasses as dc
 import typing as t
-import uuid
-from collections import defaultdict
 from enum import Enum
+
+if t.TYPE_CHECKING:
+    from superduper.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -27,6 +28,7 @@ class EventType(str, Enum):
     delete = 'delete'
     update = 'update'
     apply = 'apply'
+    db = 'db'
 
 
 @dc.dataclass
@@ -81,3 +83,26 @@ class Event:
         for e in events:
             ids.append(e.uuid)
         return ids
+
+    def execute(self, db: 'Datalayer'):
+        db.cluster.queue.publish([self])
+
+
+class StreamingEvent(Event):
+    """
+    Class for streaming events.
+
+    # noqa
+    """
+    def execute(self):
+        ...
+
+
+class DBEvent(Event):
+    """
+    Class for datalayer events. 
+
+    # noqa
+    """
+    def execute(self, db: 'Datalayer'):
+        return db.metadata.create_component(self.msg)

Pinnacle-Networks/Pinnacle

pinnacle/backends/local/queue.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import typing as t
 
+from pinnacle import logging
 from pinnacle.backends.base.queue import consume_apply_event, consume_streaming_events
 from pinnacle.base.event import Event
 from pinnacle.backends.base.queue import BaseQueueConsumer, BaseQueuePublisher
@@ -71,7 +72,6 @@ class LocalQueuePublisher(BaseQueuePublisher):
                 self.queue['_apply'].append(event)
             else:
                 self.queue[event.source].append(event)
-
         self.consumer.consume(db=self.db, queue=self.queue)
 
 
@@ -96,7 +96,10 @@ class LocalQueueConsumer(BaseQueueConsumer):
                 while queue['_apply']:
                     event = queue['_apply'].pop()
                     if event.msg == 'done':
-                        del self.futures[event.context]
+                        try:
+                            del self.futures[event.context]
+                        except KeyError:
+                            logging.info(f'No futures saved for {event.context}')
                     else:
                         consume_apply_event(event=event, db=db, futures=self.futures[event.source])
 

pinnacle/base/event.py
@@ -1,8 +1,9 @@
 import dataclasses as dc
 import typing as t
-import uuid
-from collections import defaultdict
 from enum import Enum
+
+if t.TYPE_CHECKING:
+    from pinnacle.base.datalayer import Datalayer
 
 
 @dc.dataclass
@@ -27,6 +28,7 @@ class EventType(str, Enum):
     delete = 'delete'
     update = 'update'
     apply = 'apply'
+    db = 'db'
 
 
 @dc.dataclass
@@ -81,3 +83,26 @@ class Event:
         for e in events:
             ids.append(e.uuid)
         return ids
+
+    def execute(self, db: 'Datalayer'):
+        db.cluster.queue.publish([self])
+
+
+class StreamingEvent(Event):
+    """
+    Class for streaming events.
+
+    # noqa
+    """
+    def execute(self):
+        ...
+
+
+class DBEvent(Event):
+    """
+    Class for datalayer events. 
+
+    # noqa
+    """
+    def execute(self, db: 'Datalayer'):
+        return db.metadata.create_component(self.msg)