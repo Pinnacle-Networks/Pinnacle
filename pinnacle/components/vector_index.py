superduper-io/superduper

Add trigger decorator

superduper/components/vector_index.py
@@ -8,14 +8,15 @@ from overrides import override
 
 from superduper import CFG, logging
 from superduper.backends.base.query import Query
-from superduper.base.datalayer import Datalayer, DBEvent
+from superduper.base.datalayer import Datalayer
 from superduper.base.document import Document
-from superduper.base.event import Event
+from superduper.base.event import Event, EventType
 from superduper.components.component import Component
 from superduper.components.datatype import DataType
 from superduper.components.listener import Listener
 from superduper.components.model import Mapping, ModelInputType
 from superduper.ext.utils import str_shape
+from superduper.jobs.annotations import trigger
 from superduper.jobs.job import FunctionJob
 from superduper.misc.annotations import component
 from superduper.misc.special_dicts import MongoStyleDict
@@ -149,6 +150,20 @@ class VectorIndex(Component):
             )
         return False
 
+    @trigger('insert', 'apply', 'update')
+    def copy_vectors(self, ids: t.Sequence[str] | None):
+        if ids is None:
+            primary_id = self.indexing_listener.select.primary_id
+            cur = self.indexing_listener.select.select_ids.execute()
+            ids = [r[primary_id] for r in cur]
+        # TODO copy logic of this into here - not necessary to have both functions
+        return copy_vectors(self.identifier, self.indexing_listener.select, ids, self.db)
+
+    @trigger('delete')
+    def delete_vectors(self, ids: t.Sequence[str] | None):
+        # TODO copy logic of this into here - not necessary to have both functions
+        return delete_vectors(self.identifier, ids, self.db)
+
     def get_vector(
         self,
         like: Document,
@@ -256,14 +271,6 @@ class VectorIndex(Component):
         """
         db.fast_vector_searchers[self.identifier].drop(db)
 
-    @override
-    def post_create(self, db: "Datalayer") -> None:
-        """Post-create hook.
-
-        :param db: Data layer instance.
-        """
-        db.compute.queue.declare_component(self)
-
     @property
     def models_keys(self) -> t.Tuple[t.List[str], t.List[ModelInputType]]:
         """Return a list of model and keys for each listener."""
@@ -324,91 +331,91 @@ class VectorIndex(Component):
         job(db=db, dependencies=deps)
         return [job]
 
-    @override
-    def run_jobs(
-        self,
-        db: Datalayer,
-        dependencies: t.Sequence[str] = (),
-        events: t.List = [],
-        overwrite: bool = False,
-        event_type: str = DBEvent.insert,
-    ) -> t.Sequence[t.Any]:
-        """Run jobs for the vector index.
+    # @override
+    # def run_jobs(
+    #     self,
+    #     db: Datalayer,
+    #     dependencies: t.Sequence[str] = (),
+    #     events: t.List = [],
+    #     overwrite: bool = False,
+    #     event_type: str = EventType.insert,
+    # ) -> t.Sequence[t.Any]:
+    #     """Run jobs for the vector index.
 
-        :param db: The DB instance to process
-        :param dependencies: A list of dependencies
-        :param events: List of events.
-        :param event_type: Type of event.
-        """
-        if event_type in [DBEvent.insert, DBEvent.upsert]:
-            callable = copy_vectors
-        elif type == DBEvent.delete:
-            callable = delete_vectors
-        else:
-            return []
+    #     :param db: The DB instance to process
+    #     :param dependencies: A list of dependencies
+    #     :param events: List of events.
+    #     :param event_type: Type of event.
+    #     """
+    #     if event_type in [EventType.insert, EventType.upsert]:
+    #         callable = copy_vectors
+    #     elif type == EventType.delete:
+    #         callable = delete_vectors
+    #     else:
+    #         return []
 
-        assert self.indexing_listener.select is not None
+    #     assert self.indexing_listener.select is not None
 
-        dependencies = {*self.indexing_listener.model.jobs(db), *dependencies}
+    #     dependencies = {*self.indexing_listener.model.jobs(db), *dependencies}
 
-        component_events, db_events = Event.chunk_by_type(events)
+    #     component_events, db_events = Event.chunk_by_type(events)
 
-        # Create a startup job
-        jobs = []
-        for event in component_events:
-            jobs += [
-                self._create_vector_sync_job(
-                    db=db,
-                    callable=callable,
-                    ids=event.id,
-                    deps=dependencies,
-                    job_id=event.uuid,
-                )
-            ]
+    #     # Create a startup job
+    #     jobs = []
+    #     for event in component_events:
+    #         jobs += [
+    #             self._create_vector_sync_job(
+    #                 db=db,
+    #                 callable=callable,
+    #                 ids=event.id,
+    #                 deps=dependencies,
+    #                 job_id=event.uuid,
+    #             )
+    #         ]
 
-        # Create db events
-        if not db_events:
-            return jobs
+    #     # Create db events
+    #     if not db_events:
+    #         return jobs
 
-        jobs += [
-            self._create_vector_sync_job(
-                db=db,
-                callable=callable,
-                ids=[event.id for event in db_events],
-                deps=dependencies,
-            )
-        ]
-        return jobs
+    #     jobs += [
+    #         self._create_vector_sync_job(
+    #             db=db,
+    #             callable=callable,
+    #             ids=[event.id for event in db_events],
+    #             deps=dependencies,
+    #         )
+    #     ]
+    #     return jobs
 
-    @override
-    def schedule_jobs(
-        self,
-        db: Datalayer,
-        dependencies: t.Sequence[str] = (),
-    ) -> t.Sequence[t.Any]:
-        """Schedule jobs for the vector index.
+    # @override
+    # def schedule_jobs(
+    #     self,
+    #     db: Datalayer,
+    #     dependencies: t.Sequence[str] = (),
+    # ) -> t.Sequence[t.Any]:
+    #     """Schedule jobs for the vector index.
 
-        :param db: The DB instance to process
-        :param dependencies: A list of dependencies
-        """
-        assert self.indexing_listener.select is not None
+    #     :param db: The DB instance to process
+    #     :param dependencies: A list of dependencies
+    #     """
+    #     assert self.indexing_listener.select is not None
 
-        outputs = db[self.indexing_listener.outputs]
-        ids = db.execute(outputs.select_ids)
-        ids = [str(id[outputs.primary_id]) for id in ids]
-        if not ids:
-            return []
+    #     outputs = db[self.indexing_listener.outputs]
+    #     ids = db.execute(outputs.select_ids)
+    #     ids = [str(id[outputs.primary_id]) for id in ids]
+    #     if not ids:
+    #         return []
 
-        event = Event(
-            dest={'type_id': self.type_id, 'identifier': self.identifier},
-            event_type=DBEvent.insert,
-            id=ids,
-            from_type='COMPONENT',
-            dependencies=dependencies,
-        )
+    #     event = Event(
+    #         dest={'type_id': self.type_id, 'identifier': self.identifier},
+    #         event_type=EventType.insert,
+    #         id=ids,
+    #         from_type='COMPONENT',
+    #         dependencies=dependencies,
+    #     )
 
-        db.compute.broadcast([event])
-        return [event.uuid]
+    #     db.compute.broadcast([event])
+    #     return [event.uuid]
 
 
 class EncodeArray:

superduper/jobs/annotations.py
@@ -1,0 +1,53 @@
+import inspect
+import typing as t
+
+from superduper.base.event import EventType
+from superduper.jobs.job import ComponentJob
+
+
+def trigger(*events: t.Sequence[EventType], depends: t.Sequence[str] | str = (), requires: t.Sequence[str] | str = ()):
+    """Decorator to trigger a method when an event of type 
+    {'update', 'insert', 'delete', 'apply'} are called.
+
+    :param events: Event to trigger the method.
+    :param depends: Triggers which should run before this method.
+    :param requires: Dataclass parameters/ attributes which should be 
+                     available to trigger the method
+    """
+
+    if isinstance(depends, str):
+        depends = [depends]
+
+    if isinstance(requires, str):
+        requires = [requires]
+
+    def decorator(f):
+        """Decorator to trigger a method when an event of type.
+        """
+        takes_ids = 'ids' in inspect.signature(f).parameters
+        if events != ('apply',):
+            assert takes_ids, (
+                f"Method {f.__name__} must take an 'ids' argument"
+                " to be triggered by anything apart from 'apply'"
+            )
+
+        def decorated(self, ids: t.List[str] | None = None, job: bool = False):
+
+            if job:
+                return ComponentJob(
+                    component_identifier=self.identifier,
+                    component_uuid=self.uuid,
+                    type_id=self.type_id,
+                    method_name=f.__name__,
+                    args=(ids,) if takes_ids else (),
+                    db=self.db,
+                )
+            else:
+                return f(self, ids) if takes_ids else f(self)
+
+        decorated.events = events
+        decorated.depends = depends
+        decorated.requires = requires
+        return decorated
+
+    return decorator

Pinnacle-Networks/Pinnacle

pinnacle/components/vector_index.py
@@ -8,14 +8,15 @@ from overrides import override
 
 from pinnacle import CFG, logging
 from pinnacle.backends.base.query import Query
-from pinnacle.base.datalayer import Datalayer, DBEvent
+from pinnacle.base.datalayer import Datalayer
 from pinnacle.base.document import Document
-from pinnacle.base.event import Event
+from pinnacle.base.event import Event, EventType
 from pinnacle.components.component import Component
 from pinnacle.components.datatype import DataType
 from pinnacle.components.listener import Listener
 from pinnacle.components.model import Mapping, ModelInputType
 from pinnacle.ext.utils import str_shape
+from pinnacle.jobs.annotations import trigger
 from pinnacle.jobs.job import FunctionJob
 from pinnacle.misc.annotations import component
 from pinnacle.misc.special_dicts import MongoStyleDict
@@ -149,6 +150,20 @@ class VectorIndex(Component):
             )
         return False
 
+    @trigger('insert', 'apply', 'update')
+    def copy_vectors(self, ids: t.Sequence[str] | None):
+        if ids is None:
+            primary_id = self.indexing_listener.select.primary_id
+            cur = self.indexing_listener.select.select_ids.execute()
+            ids = [r[primary_id] for r in cur]
+        # TODO copy logic of this into here - not necessary to have both functions
+        return copy_vectors(self.identifier, self.indexing_listener.select, ids, self.db)
+
+    @trigger('delete')
+    def delete_vectors(self, ids: t.Sequence[str] | None):
+        # TODO copy logic of this into here - not necessary to have both functions
+        return delete_vectors(self.identifier, ids, self.db)
+
     def get_vector(
         self,
         like: Document,
@@ -256,14 +271,6 @@ class VectorIndex(Component):
         """
         db.fast_vector_searchers[self.identifier].drop(db)
 
-    @override
-    def post_create(self, db: "Datalayer") -> None:
-        """Post-create hook.
-
-        :param db: Data layer instance.
-        """
-        db.compute.queue.declare_component(self)
-
     @property
     def models_keys(self) -> t.Tuple[t.List[str], t.List[ModelInputType]]:
         """Return a list of model and keys for each listener."""
@@ -324,91 +331,91 @@ class VectorIndex(Component):
         job(db=db, dependencies=deps)
         return [job]
 
-    @override
-    def run_jobs(
-        self,
-        db: Datalayer,
-        dependencies: t.Sequence[str] = (),
-        events: t.List = [],
-        overwrite: bool = False,
-        event_type: str = DBEvent.insert,
-    ) -> t.Sequence[t.Any]:
-        """Run jobs for the vector index.
+    # @override
+    # def run_jobs(
+    #     self,
+    #     db: Datalayer,
+    #     dependencies: t.Sequence[str] = (),
+    #     events: t.List = [],
+    #     overwrite: bool = False,
+    #     event_type: str = EventType.insert,
+    # ) -> t.Sequence[t.Any]:
+    #     """Run jobs for the vector index.
 
-        :param db: The DB instance to process
-        :param dependencies: A list of dependencies
-        :param events: List of events.
-        :param event_type: Type of event.
-        """
-        if event_type in [DBEvent.insert, DBEvent.upsert]:
-            callable = copy_vectors
-        elif event_type == DBEvent.delete:
-            callable = delete_vectors
-        else:
-            return []
+    #     :param db: The DB instance to process
+    #     :param dependencies: A list of dependencies
+    #     :param events: List of events.
+    #     :param event_type: Type of event.
+    #     """
+    #     if event_type in [EventType.insert, EventType.upsert]:
+    #         callable = copy_vectors
+    #     elif type == EventType.delete:
+    #         callable = delete_vectors
+    #     else:
+    #         return []
 
-        assert self.indexing_listener.select is not None
+    #     assert self.indexing_listener.select is not None
 
-        dependencies = {*self.indexing_listener.model.jobs(db), *dependencies}
+    #     dependencies = {*self.indexing_listener.model.jobs(db), *dependencies}
 
-        component_events, db_events = Event.chunk_by_type(events)
+    #     component_events, db_events = Event.chunk_by_type(events)
 
-        # Create a startup job
-        jobs = []
-        for event in component_events:
-            jobs += [
-                self._create_vector_sync_job(
-                    db=db,
-                    callable=callable,
-                    ids=event.id,
-                    deps=dependencies,
-                    job_id=event.uuid,
-                )
-            ]
+    #     # Create a startup job
+    #     jobs = []
+    #     for event in component_events:
+    #         jobs += [
+    #             self._create_vector_sync_job(
+    #                 db=db,
+    #                 callable=callable,
+    #                 ids=event.id,
+    #                 deps=dependencies,
+    #                 job_id=event.uuid,
+    #             )
+    #         ]
 
-        # Create db events
-        if not db_events:
-            return jobs
+    #     # Create db events
+    #     if not db_events:
+    #         return jobs
 
-        jobs += [
-            self._create_vector_sync_job(
-                db=db,
-                callable=callable,
-                ids=[event.id for event in db_events],
-                deps=dependencies,
-            )
-        ]
-        return jobs
+    #     jobs += [
+    #         self._create_vector_sync_job(
+    #             db=db,
+    #             callable=callable,
+    #             ids=[event.id for event in db_events],
+    #             deps=dependencies,
+    #         )
+    #     ]
+    #     return jobs
 
-    @override
-    def schedule_jobs(
-        self,
-        db: Datalayer,
-        dependencies: t.Sequence[str] = (),
-    ) -> t.Sequence[t.Any]:
-        """Schedule jobs for the vector index.
+    # @override
+    # def schedule_jobs(
+    #     self,
+    #     db: Datalayer,
+    #     dependencies: t.Sequence[str] = (),
+    # ) -> t.Sequence[t.Any]:
+    #     """Schedule jobs for the vector index.
 
-        :param db: The DB instance to process
-        :param dependencies: A list of dependencies
-        """
-        assert self.indexing_listener.select is not None
+    #     :param db: The DB instance to process
+    #     :param dependencies: A list of dependencies
+    #     """
+    #     assert self.indexing_listener.select is not None
 
-        outputs = db[self.indexing_listener.outputs]
-        ids = db.execute(outputs.select_ids)
-        ids = [str(id[outputs.primary_id]) for id in ids]
-        if not ids:
-            return []
+    #     outputs = db[self.indexing_listener.outputs]
+    #     ids = db.execute(outputs.select_ids)
+    #     ids = [str(id[outputs.primary_id]) for id in ids]
+    #     if not ids:
+    #         return []
 
-        event = Event(
-            dest={'type_id': self.type_id, 'identifier': self.identifier},
-            event_type=DBEvent.insert,
-            id=ids,
-            from_type='COMPONENT',
-            dependencies=dependencies,
-        )
+    #     event = Event(
+    #         dest={'type_id': self.type_id, 'identifier': self.identifier},
+    #         event_type=EventType.insert,
+    #         id=ids,
+    #         from_type='COMPONENT',
+    #         dependencies=dependencies,
+    #     )
 
-        db.compute.broadcast([event])
-        return [event.uuid]
+    #     db.compute.broadcast([event])
+    #     return [event.uuid]
 
 
 class EncodeArray:

pinnacle/jobs/annotations.py
@@ -1,0 +1,53 @@
+import inspect
+import typing as t
+
+from pinnacle.base.event import EventType
+from pinnacle.jobs.job import ComponentJob
+
+
+def trigger(*events: t.Sequence[EventType], depends: t.Sequence[str] | str = (), requires: t.Sequence[str] | str = ()):
+    """Decorator to trigger a method when an event of type 
+    {'update', 'insert', 'delete', 'apply'} are called.
+
+    :param events: Event to trigger the method.
+    :param depends: Triggers which should run before this method.
+    :param requires: Dataclass parameters/ attributes which should be 
+                     available to trigger the method
+    """
+
+    if isinstance(depends, str):
+        depends = [depends]
+
+    if isinstance(requires, str):
+        requires = [requires]
+
+    def decorator(f):
+        """Decorator to trigger a method when an event of type.
+        """
+        takes_ids = 'ids' in inspect.signature(f).parameters
+        if events != ('apply',):
+            assert takes_ids, (
+                f"Method {f.__name__} must take an 'ids' argument"
+                " to be triggered by anything apart from 'apply'"
+            )
+
+        def decorated(self, ids: t.List[str] | None = None, job: bool = False):
+
+            if job:
+                return ComponentJob(
+                    component_identifier=self.identifier,
+                    component_uuid=self.uuid,
+                    type_id=self.type_id,
+                    method_name=f.__name__,
+                    args=(ids,) if takes_ids else (),
+                    db=self.db,
+                )
+            else:
+                return f(self, ids) if takes_ids else f(self)
+
+        decorated.events = events
+        decorated.depends = depends
+        decorated.requires = requires
+        return decorated
+
+    return decorator