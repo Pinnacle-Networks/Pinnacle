superduper-io/superduper

Do auto-schema inference client side

superduper/backends/base/vector_search.py
@@ -10,6 +10,7 @@ from superduper.backends.base.backends import BaseBackend
 
 if t.TYPE_CHECKING:
     from superduper.components.vector_index import VectorIndex
+    from superduper.base.datalayer import Datalayer
 
 
 class VectorSearchBackend(BaseBackend):

superduper/components/listener.py
@@ -9,6 +9,7 @@ from superduper.base.annotations import trigger
 from superduper.base.datalayer import Datalayer
 from superduper.components.cdc import CDC
 from superduper.components.model import Mapping
+from superduper.components.table import Table
 
 from .model import Model, ModelInputType
 
@@ -30,6 +31,7 @@ class Listener(CDC):
     :param predict_kwargs: Keyword arguments to self.model.predict().
     :param select: Query to "listen" for input on.
     :param identifier: A string used to identify the listener and it's outputs.
+    :param output_table: Table to store the outputs.
     """
 
     type_id: t.ClassVar[str] = 'listener'
@@ -39,6 +41,7 @@ class Listener(CDC):
     predict_kwargs: t.Optional[t.Dict] = dc.field(default_factory=dict)
     select: t.Optional[Query] = None
     cdc_table: str = ''
+    output_table: t.Optional[Table] = None
 
     def __post_init__(self, db, artifacts):
         if not self.cdc_table and self.select:
@@ -56,9 +59,72 @@ class Listener(CDC):
         """Predict ID property."""
         return f'{self.identifier}__{self.uuid}'
 
-    def pre_create(self, db: Datalayer) -> None:
+    def _pre_create(self, db: Datalayer, startup_cache: t.Dict):
         """Pre-create hook."""
-        return super().pre_create(db)
+        if self.select is None:
+            return
+        if not db.cfg.auto_schema:
+            return
+        if self.output_table is not None:
+            return
+
+        from superduper import Schema
+        if self.model.datatype is not None:
+            schema = Schema(
+                f'_schema/{self.outputs}',
+                fields={self.outputs: self.model.datatype}
+            )
+        elif self.model.output_schema is not None:
+            schema = self.model.output_schema
+        else:
+            if self.model.example is not None:
+                input = self.model.example
+            else:
+                try:
+                    if self.dependencies:
+                        try:
+                            r = next(self.select.limit(1).execute())
+                        except StopIteration as e:
+                            try:
+                                if not self.cdc_table.startswith(CFG.output_prefix):
+                                    r = next(db[self.select.table].select().limit(1).execute())
+                                    r = {**r, **startup_cache}
+                                else:
+                                    r = startup_cache
+                            except StopIteration as e:
+                                raise Exception(
+                                    f'Couldn\'t retrieve outputs to determine schema; {self.cdc_table} returned 0 results.'
+                                ) from e
+                    else:
+                        r = next(self.select.limit(1).execute())
+                except StopIteration as e:
+                    raise Exception(
+                        f'Couldn\'t retrieve outputs to determine schema; {self.select} returned 0 results.'
+                    ) from e
+
+                mapping = Mapping(self.key, self.model.signature)
+                input = mapping(r)
+
+            if self.model.signature == 'singleton':
+                prediction = self.model.predict(input)
+            elif self.model.signature == '*args':
+                prediction = self.model.predict(*input)
+            elif self.model.signature == '**kwargs':
+                prediction = self.model.predict(**input)
+            else:
+                assert self.model.signature == '*args,**kwargs'
+                prediction = self.model.predict(*input[0], **input[1])
+
+            if self.model.flatten:
+                prediction = prediction[0]
+
+            startup_cache[self.outputs] = prediction
+            schema = db.infer_schema({'data': prediction}, identifier=self.outputs + '/schema')
+            datatype = schema.fields['data']
+            self.model.datatype = datatype
+            schema=Schema(f'_schema/{self.outputs}', fields={'_source': 'ID', 'id': 'ID', self.outputs: datatype})
+        
+        self.output_table = Table(self.outputs, schema=schema)
 
     @property
     def mapping(self):
@@ -86,36 +152,6 @@ class Listener(CDC):
     def outputs_select(self):
         """Get select statement for outputs."""
         return self.db[self.select.table].select().outputs(self.predict_id)
-
-    @override
-    def post_create(self, db: "Datalayer") -> None:
-        """Post-create hook.
-
-        :param db: Data layer instance.
-        """
-        self.create_output_dest(db, self.predict_id, self.model)
-        super().post_create(db)
-
-    @classmethod
-    def create_output_dest(cls, db: "Datalayer", predict_id, model: Model):
-        """
-        Create output destination.
-
-        :param db: Data layer instance.
-        :param uuid: UUID of the listener.
-        :param model: Model instance.
-        """
-        if model.datatype is None:
-            return
-        # TODO make this universal over databackends
-        # not special e.g. MongoDB vs. Ibis creating a table or not
-        output_table = db.databackend.create_output_dest(
-            predict_id,
-            model.datatype,
-            flatten=model.flatten,
-        )
-        if output_table is not None:
-            db.apply(output_table)
 
     @property
     def dependencies(self):

superduper/vector_search/qdrant.py
@@ -7,10 +7,11 @@ import numpy as np
 from qdrant_client import QdrantClient, models
 
 from superduper import CFG
-from superduper.vector_search.base import (
+
+from superduper.backends.base.vector_search import (
     BaseVectorSearcher,
-    VectorIndexMeasureType,
     VectorItem,
+    VectorIndexMeasureType,
 )
 
 ID_PAYLOAD_KEY = "_id"
@@ -29,13 +30,10 @@ class QdrantVectorSearcher(BaseVectorSearcher):
 
     def __init__(
         self,
-        identifier: str,
+        uuid: str,
         dimensions: int,
-        h: t.Optional[np.ndarray] = None,
-        index: t.Optional[t.List[str]] = None,
         measure: t.Optional[str] = None,
     ):
-        super().__init__(identifier, dimensions, h, index, measure)
         config_dict = deepcopy(CFG.vector_search_kwargs)
         self.vector_name: t.Optional[str] = config_dict.pop("vector_name", None)
         # Use an in-memory instance by default
@@ -45,7 +43,7 @@ class QdrantVectorSearcher(BaseVectorSearcher):
         self.collection_name = uuid
         self.measure = measure
 
-        self.collection_name = re.sub("\W+", "", identifier)
+        self.collection_name = re.sub("\W+", "", uuid)
         if not self.client.collection_exists(self.collection_name):
             measure = (
                 measure.name if isinstance(measure, VectorIndexMeasureType) else measure
@@ -56,18 +54,8 @@ class QdrantVectorSearcher(BaseVectorSearcher):
                 vectors_config=models.VectorParams(size=dimensions, distance=distance),
             )
 
-        self.initialize(identifier)
-
-        if h is not None and index is not None:
-            self.add(
-                [
-                    VectorItem(
-                        id=_id,
-                        vector=vector,
-                    )
-                    for _id, vector in zip(index, h)
-                ]
-            )
+    def initialize(self, db):
+        pass
 
     def __len__(self):
         return self.client.get_collection(self.collection_name).vectors_count
@@ -91,9 +79,6 @@ class QdrantVectorSearcher(BaseVectorSearcher):
         """
         points = []
         for item in items:
-            vector = (
-                item.vector.tolist() if hasattr(item.vector, "tolist") else item.vector
-            )
             point = models.PointStruct(
                 id=self._convert_id(item.id),
                 vector=(

plugins/mongodb/plugin_test/test_queries.py
@@ -1,6 +1,6 @@
 import random
 from test.utils.setup.fake_data import (
-    add_listener,
+    add_listeners,
     add_models,
     add_random_data,
     add_vector_index,
@@ -95,10 +95,117 @@ def test_update_many(db):
     # )
 
 
+def test_outputs_query_2(db):
+    import numpy
+    from superduper import model
+
+    db.cfg.auto_schema = True
+
+    @model
+    def test(x):
+        return numpy.random.randn(32)
+
+    db['example'].insert([{'x': f'test {i}', 'y': f'other {i}'} for i in range(5)]).execute()
+
+    l1 = test.to_listener(key='x', select=db['example'].select(), identifier='l-x')
+    l2 = test.to_listener(key='y', select=db['example'].select(), identifier='l-y')
+
+    db.apply(l1)
+    db.apply(l2)
+
+    @model(flatten=True)
+    def test_flat(x):
+        return [numpy.random.randn(32) for _ in range(3)]
+
+    l3 = test_flat.to_listener(key='x', select=db['example'].select(), identifier='l-x-flat')
+
+    db.apply(l3)    
+
+    ########
+
+    q = db['example'].outputs(l1.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 5
+
+    #######
+
+    q = db['example'].outputs(l2.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 5
+
+    #######
+
+    q = db['example'].outputs(l1.predict_id, l2.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 5
+
+    #######
+
+    q = db['example'].outputs(l3.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 15
+
+    #######
+
+    q = db['example'].outputs(l1.predict_id, l3.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 15
+
+    #######
+
+    q = db['example'].outputs(l1.predict_id, l2.predict_id, l3.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 15
+
+
+def test_outputs_query(db):
+    db.cfg.auto_schema = True
+
+    add_random_data(db, n=5)
+    add_models(db)
+
+    import numpy
+
+    l1, l2, l1_flat = add_listeners(db)
+    
+    sample1 = db[l1.outputs].find_one().execute()
+    sample2 = db[l2.outputs].find_one().execute()
+
+    outputs_1 = list(db['documents'].outputs(l1.predict_id).execute())
+    assert len(outputs_1) == 5
+    outputs_2 = list(db['documents'].outputs(l2.predict_id).execute())
+    assert len(outputs_2) == 5
+    outputs_1_2 = next(db['documents'].outputs(l1.predict_id, l2.predict_id).execute())
+    assert len(outputs_1_2) == 5
+
+    # outputs_1_flat = list(db['documents'].outputs(l1_flat.predict_id).execute())
+    # # TODO this output seems to be far too big
+    # # the question is what is the expected output when we have multiple listeners?
+    # outputs_1_flat_2 = next(db['documents'].outputs(l1_flat.predict_id, l2.predict_id).execute())
+
+    # assert isinstance(outputs_1[l1.outputs], np.ndarray)
+    # assert isinstance(outputs_2[l2.outputs], np.ndarray)
+
+    # assert isinstance(outputs_1_2[l1.outputs], np.ndarray)
+    # assert isinstance(outputs_1_2[l2.outputs], np.ndarray)
+
+
 def test_insert_many(db):
     add_random_data(db, n=5)
     add_models(db)
-    add_listener(db)
+    add_listeners(db)
     collection = db['documents']
     an_update = get_new_data(10, update=True)
     db.execute(collection.insert(an_update))

test/unittest/component/test_vector_index.py
@@ -1,3 +1,7 @@
+from superduper import model
+from test.utils.usecase.vector_search import add_data
+
+
 def test_vector_index_recovery(db):
     from test.utils.usecase.vector_search import build_vector_index
 
@@ -36,3 +40,27 @@ def test_vector_index_cleanup(db):
     db.remove('vector_index', vector_index, force=True)
 
     assert vector_index, uuid not in db.cluster.vector_search.list()
+
+
+def test_initialize_output_datatype_with_dimensions(db):
+    add_data(db, 0, 100)
+    db.cfg.auto_schema = True
+
+    import numpy
+
+    @model
+    def test(x):
+        return numpy.random.randn(32)
+
+    vector_index = test.to_vector_index(key='x', select=db['documents'].select())
+    vector_index.pre_create(db)
+
+    from superduper import Table
+    assert isinstance(
+        vector_index.indexing_listener.output_table,
+        Table,
+    )
+
+    assert vector_index.dimensions == 32
+
+

test/utils/component/model.py
@@ -1,5 +1,6 @@
 import typing as t
 
+from superduper.components.listener import Listener
 from superduper.components.model import Model
 
 if t.TYPE_CHECKING:
@@ -29,15 +30,20 @@ def test_predict_in_db(model: Model, sample_data: t.Any, db: "Datalayer"):
     db.cfg.auto_schema = True
 
     db["datas"].insert([{"data": sample_data, "i": i} for i in range(10)]).execute()
-    model.db = db
 
-    model.predict_in_db(
-        X="data",
+    rid = random_id()
+
+    listener = Listener(
+        key="data",
+        model=model,
         select=db["datas"].select(),
-        predict_id='test',
+        identifier='test',
+        uuid=rid,
     )
 
-    results = list(db["_outputs__test"].select().execute())
+    db.apply(listener)
+
+    results = list(db[listener.outputs].select().execute())
     assert len(results) == 10
 
     return results

Pinnacle-Networks/Pinnacle

pinnacle/backends/base/vector_search.py
@@ -10,6 +10,7 @@ from pinnacle.backends.base.backends import BaseBackend
 
 if t.TYPE_CHECKING:
     from pinnacle.components.vector_index import VectorIndex
+    from pinnacle.base.datalayer import Datalayer
 
 
 class VectorSearchBackend(BaseBackend):

pinnacle/components/listener.py
@@ -9,6 +9,7 @@ from pinnacle.base.annotations import trigger
 from pinnacle.base.datalayer import Datalayer
 from pinnacle.components.cdc import CDC
 from pinnacle.components.model import Mapping
+from pinnacle.components.table import Table
 
 from .model import Model, ModelInputType
 
@@ -30,6 +31,7 @@ class Listener(CDC):
     :param predict_kwargs: Keyword arguments to self.model.predict().
     :param select: Query to "listen" for input on.
     :param identifier: A string used to identify the listener and it's outputs.
+    :param output_table: Table to store the outputs.
     """
 
     type_id: t.ClassVar[str] = 'listener'
@@ -39,6 +41,7 @@ class Listener(CDC):
     predict_kwargs: t.Optional[t.Dict] = dc.field(default_factory=dict)
     select: t.Optional[Query] = None
     cdc_table: str = ''
+    output_table: t.Optional[Table] = None
 
     def __post_init__(self, db, artifacts):
         if not self.cdc_table and self.select:
@@ -56,9 +59,72 @@ class Listener(CDC):
         """Predict ID property."""
         return f'{self.identifier}__{self.uuid}'
 
-    def pre_create(self, db: Datalayer) -> None:
+    def _pre_create(self, db: Datalayer, startup_cache: t.Dict):
         """Pre-create hook."""
-        return super().pre_create(db)
+        if self.select is None:
+            return
+        if not db.cfg.auto_schema:
+            return
+        if self.output_table is not None:
+            return
+
+        from pinnacle import Schema
+        if self.model.datatype is not None:
+            schema = Schema(
+                f'_schema/{self.outputs}',
+                fields={self.outputs: self.model.datatype}
+            )
+        elif self.model.output_schema is not None:
+            schema = self.model.output_schema
+        else:
+            if self.model.example is not None:
+                input = self.model.example
+            else:
+                try:
+                    if self.dependencies:
+                        try:
+                            r = next(self.select.limit(1).execute())
+                        except StopIteration as e:
+                            try:
+                                if not self.cdc_table.startswith(CFG.output_prefix):
+                                    r = next(db[self.select.table].select().limit(1).execute())
+                                    r = {**r, **startup_cache}
+                                else:
+                                    r = startup_cache
+                            except StopIteration as e:
+                                raise Exception(
+                                    f'Couldn\'t retrieve outputs to determine schema; {self.cdc_table} returned 0 results.'
+                                ) from e
+                    else:
+                        r = next(self.select.limit(1).execute())
+                except StopIteration as e:
+                    raise Exception(
+                        f'Couldn\'t retrieve outputs to determine schema; {self.select} returned 0 results.'
+                    ) from e
+
+                mapping = Mapping(self.key, self.model.signature)
+                input = mapping(r)
+
+            if self.model.signature == 'singleton':
+                prediction = self.model.predict(input)
+            elif self.model.signature == '*args':
+                prediction = self.model.predict(*input)
+            elif self.model.signature == '**kwargs':
+                prediction = self.model.predict(**input)
+            else:
+                assert self.model.signature == '*args,**kwargs'
+                prediction = self.model.predict(*input[0], **input[1])
+
+            if self.model.flatten:
+                prediction = prediction[0]
+
+            startup_cache[self.outputs] = prediction
+            schema = db.infer_schema({'data': prediction}, identifier=self.outputs + '/schema')
+            datatype = schema.fields['data']
+            self.model.datatype = datatype
+            schema=Schema(f'_schema/{self.outputs}', fields={'_source': 'ID', 'id': 'ID', self.outputs: datatype})
+        
+        self.output_table = Table(self.outputs, schema=schema)
 
     @property
     def mapping(self):
@@ -86,36 +152,6 @@ class Listener(CDC):
     def outputs_select(self):
         """Get select statement for outputs."""
         return self.db[self.select.table].select().outputs(self.predict_id)
-
-    @override
-    def post_create(self, db: "Datalayer") -> None:
-        """Post-create hook.
-
-        :param db: Data layer instance.
-        """
-        self.create_output_dest(db, self.predict_id, self.model)
-        super().post_create(db)
-
-    @classmethod
-    def create_output_dest(cls, db: "Datalayer", predict_id, model: Model):
-        """
-        Create output destination.
-
-        :param db: Data layer instance.
-        :param uuid: UUID of the listener.
-        :param model: Model instance.
-        """
-        if model.datatype is None:
-            return
-        # TODO make this universal over databackends
-        # not special e.g. MongoDB vs. Ibis creating a table or not
-        output_table = db.databackend.create_output_dest(
-            predict_id,
-            model.datatype,
-            flatten=model.flatten,
-        )
-        if output_table is not None:
-            db.apply(output_table)
 
     @property
     def dependencies(self):

pinnacle/vector_search/qdrant.py
@@ -7,10 +7,11 @@ import numpy as np
 from qdrant_client import QdrantClient, models
 
 from pinnacle import CFG
-from pinnacle.vector_search.base import (
+
+from pinnacle.backends.base.vector_search import (
     BaseVectorSearcher,
-    VectorIndexMeasureType,
     VectorItem,
+    VectorIndexMeasureType,
 )
 
 ID_PAYLOAD_KEY = "_id"
@@ -29,13 +30,10 @@ class QdrantVectorSearcher(BaseVectorSearcher):
 
     def __init__(
         self,
-        identifier: str,
+        uuid: str,
         dimensions: int,
-        h: t.Optional[np.ndarray] = None,
-        index: t.Optional[t.List[str]] = None,
         measure: t.Optional[str] = None,
     ):
-        super().__init__(identifier, dimensions, h, index, measure)
         config_dict = deepcopy(CFG.vector_search_kwargs)
         self.vector_name: t.Optional[str] = config_dict.pop("vector_name", None)
         # Use an in-memory instance by default
@@ -45,7 +43,7 @@ class QdrantVectorSearcher(BaseVectorSearcher):
         self.collection_name = uuid
         self.measure = measure
 
-        self.collection_name = re.sub("\W+", "", identifier)
+        self.collection_name = re.sub("\W+", "", uuid)
         if not self.client.collection_exists(self.collection_name):
             measure = (
                 measure.name if isinstance(measure, VectorIndexMeasureType) else measure
@@ -56,18 +54,8 @@ class QdrantVectorSearcher(BaseVectorSearcher):
                 vectors_config=models.VectorParams(size=dimensions, distance=distance),
             )
 
-        self.initialize(identifier)
-
-        if h is not None and index is not None:
-            self.add(
-                [
-                    VectorItem(
-                        id=_id,
-                        vector=vector,
-                    )
-                    for _id, vector in zip(index, h)
-                ]
-            )
+    def initialize(self, db):
+        pass
 
     def __len__(self):
         return self.client.get_collection(self.collection_name).vectors_count
@@ -91,9 +79,6 @@ class QdrantVectorSearcher(BaseVectorSearcher):
         """
         points = []
         for item in items:
-            vector = (
-                item.vector.tolist() if hasattr(item.vector, "tolist") else item.vector
-            )
             point = models.PointStruct(
                 id=self._convert_id(item.id),
                 vector=(

plugins/mongodb/plugin_test/test_queries.py
@@ -1,6 +1,6 @@
 import random
 from test.utils.setup.fake_data import (
-    add_listener,
+    add_listeners,
     add_models,
     add_random_data,
     add_vector_index,
@@ -95,10 +95,117 @@ def test_update_many(db):
     # )
 
 
+def test_outputs_query_2(db):
+    import numpy
+    from pinnacle import model
+
+    db.cfg.auto_schema = True
+
+    @model
+    def test(x):
+        return numpy.random.randn(32)
+
+    db['example'].insert([{'x': f'test {i}', 'y': f'other {i}'} for i in range(5)]).execute()
+
+    l1 = test.to_listener(key='x', select=db['example'].select(), identifier='l-x')
+    l2 = test.to_listener(key='y', select=db['example'].select(), identifier='l-y')
+
+    db.apply(l1)
+    db.apply(l2)
+
+    @model(flatten=True)
+    def test_flat(x):
+        return [numpy.random.randn(32) for _ in range(3)]
+
+    l3 = test_flat.to_listener(key='x', select=db['example'].select(), identifier='l-x-flat')
+
+    db.apply(l3)    
+
+    ########
+
+    q = db['example'].outputs(l1.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 5
+
+    #######
+
+    q = db['example'].outputs(l2.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 5
+
+    #######
+
+    q = db['example'].outputs(l1.predict_id, l2.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 5
+
+    #######
+
+    q = db['example'].outputs(l3.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 15
+
+    #######
+
+    q = db['example'].outputs(l1.predict_id, l3.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 15
+
+    #######
+
+    q = db['example'].outputs(l1.predict_id, l2.predict_id, l3.predict_id)
+
+    results = q.execute().tolist()
+
+    assert len(results) == 15
+
+
+def test_outputs_query(db):
+    db.cfg.auto_schema = True
+
+    add_random_data(db, n=5)
+    add_models(db)
+
+    import numpy
+
+    l1, l2, l1_flat = add_listeners(db)
+    
+    sample1 = db[l1.outputs].find_one().execute()
+    sample2 = db[l2.outputs].find_one().execute()
+
+    outputs_1 = list(db['documents'].outputs(l1.predict_id).execute())
+    assert len(outputs_1) == 5
+    outputs_2 = list(db['documents'].outputs(l2.predict_id).execute())
+    assert len(outputs_2) == 5
+    outputs_1_2 = next(db['documents'].outputs(l1.predict_id, l2.predict_id).execute())
+    assert len(outputs_1_2) == 5
+
+    # outputs_1_flat = list(db['documents'].outputs(l1_flat.predict_id).execute())
+    # # TODO this output seems to be far too big
+    # # the question is what is the expected output when we have multiple listeners?
+    # outputs_1_flat_2 = next(db['documents'].outputs(l1_flat.predict_id, l2.predict_id).execute())
+
+    # assert isinstance(outputs_1[l1.outputs], np.ndarray)
+    # assert isinstance(outputs_2[l2.outputs], np.ndarray)
+
+    # assert isinstance(outputs_1_2[l1.outputs], np.ndarray)
+    # assert isinstance(outputs_1_2[l2.outputs], np.ndarray)
+
+
 def test_insert_many(db):
     add_random_data(db, n=5)
     add_models(db)
-    add_listener(db)
+    add_listeners(db)
     collection = db['documents']
     an_update = get_new_data(10, update=True)
     db.execute(collection.insert(an_update))

test/unittest/component/test_vector_index.py
@@ -1,3 +1,7 @@
+from pinnacle import model
+from test.utils.usecase.vector_search import add_data
+
+
 def test_vector_index_recovery(db):
     from test.utils.usecase.vector_search import build_vector_index
 
@@ -36,3 +40,27 @@ def test_vector_index_cleanup(db):
     db.remove('vector_index', vector_index, force=True)
 
     assert vector_index, uuid not in db.cluster.vector_search.list()
+
+
+def test_initialize_output_datatype_with_dimensions(db):
+    add_data(db, 0, 100)
+    db.cfg.auto_schema = True
+
+    import numpy
+
+    @model
+    def test(x):
+        return numpy.random.randn(32)
+
+    vector_index = test.to_vector_index(key='x', select=db['documents'].select())
+    vector_index.pre_create(db)
+
+    from pinnacle import Table
+    assert isinstance(
+        vector_index.indexing_listener.output_table,
+        Table,
+    )
+
+    assert vector_index.dimensions == 32
+
+

test/utils/component/model.py
@@ -1,5 +1,6 @@
 import typing as t
 
+from pinnacle.components.listener import Listener
 from pinnacle.components.model import Model
 
 if t.TYPE_CHECKING:
@@ -29,15 +30,20 @@ def test_predict_in_db(model: Model, sample_data: t.Any, db: "Datalayer"):
     db.cfg.auto_schema = True
 
     db["datas"].insert([{"data": sample_data, "i": i} for i in range(10)]).execute()
-    model.db = db
 
-    model.predict_in_db(
-        X="data",
+    rid = random_id()
+
+    listener = Listener(
+        key="data",
+        model=model,
         select=db["datas"].select(),
-        predict_id='test',
+        identifier='test',
+        uuid=rid,
     )
 
-    results = list(db["_outputs__test"].select().execute())
+    db.apply(listener)
+
+    results = list(db[listener.outputs].select().execute())
     assert len(results) == 10
 
     return results