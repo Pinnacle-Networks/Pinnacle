superduper-io/superduper

Move infrastructure items to cluster object

superduper/base/logger.py
@@ -7,8 +7,7 @@ from loki_logger_handler.loki_logger_handler import LoguruFormatter, LokiLoggerH
 from tqdm import tqdm
 
 from superduper.base.config import LogLevel, LogType
-
-from .configs import CFG
+from superduper.base.configs import CFG
 
 __all__ = ('Logging',)
 

superduper/jobs/annotations.py
@@ -1,11 +1,13 @@
 import inspect
+import sys
 import typing as t
 
+from superduper.base.event import EventType
 from superduper.jobs.job import Job
 
 
 def trigger(
-    *event_types: t.Sequence[str],
+    *event_types: t.Sequence[EventType],
     depends: t.Sequence[str] | str = (),
     requires: t.Sequence[str] | str = (),
 ):
@@ -25,6 +27,7 @@ def trigger(
     def decorator(f):
         """Decorator to trigger a method when an event of type."""
         takes_ids = 'ids' in inspect.signature(f).parameters
+
         if event_types != ('apply',):
             assert takes_ids, (
                 f"Method {f.__name__} must take an 'ids' argument"
@@ -32,6 +35,11 @@ def trigger(
             )
 
         def decorated(self, ids: t.List[str] | None = None, job: bool = False):
+            if event_types != ('apply',):
+                msg = 'Method must be a `Trigger` instance to take non-applying events'
+                from superduper.components.trigger import Trigger
+                assert isinstance(self, Trigger), msg
+
             if job:
                 return Job(
                     type_id=self.type_id,
@@ -42,6 +50,7 @@ def trigger(
                     db=self.db,
                 )
             else:
+                self.init()
                 return f(self, ids) if takes_ids else f(self)
 
         decorated.events = event_types

test/unittest/vector_search/test_vector_search.py
@@ -6,8 +6,8 @@ import pytest
 
 from superduper import CFG
 from superduper.vector_search.base import VectorItem
-from superduper.vector_search.in_memory import InMemoryVectorSearcher
 from superduper.vector_search.lance import LanceVectorSearcher
+from superduper.backends.local.vector_search import InMemoryVectorSearcher
 from superduper.vector_search.qdrant import QdrantVectorSearcher
 
 
@@ -26,10 +26,11 @@ def index_data(monkeypatch):
 )
 @pytest.mark.parametrize("measure", ["l2", "dot", "cosine"])
 def test_index(index_data, measure, vector_index_cls):
-    h, ids, ud = index_data
+    vectors, ids, ud = index_data
     h = vector_index_cls(
-        identifier="my-index", h=h, index=ids, measure=measure, dimensions=3
+        uuid="123456", measure=measure, dimensions=3
     )
+    h.add(items=[VectorItem(id=id_, vector=hh) for hh, id_ in zip(vectors, ids)])
     y = np.array([0, 0, 1])
     res, _ = h.find_nearest_from_array(y, 1)
     assert res[0] == ids[0]

Pinnacle-Networks/Pinnacle

pinnacle/base/logger.py
@@ -7,8 +7,7 @@ from loki_logger_handler.loki_logger_handler import LoguruFormatter, LokiLoggerH
 from tqdm import tqdm
 
 from pinnacle.base.config import LogLevel, LogType
-
-from .configs import CFG
+from pinnacle.base.configs import CFG
 
 __all__ = ('Logging',)
 

pinnacle/jobs/annotations.py
@@ -1,11 +1,13 @@
 import inspect
+import sys
 import typing as t
 
+from pinnacle.base.event import EventType
 from pinnacle.jobs.job import Job
 
 
 def trigger(
-    *event_types: t.Sequence[str],
+    *event_types: t.Sequence[EventType],
     depends: t.Sequence[str] | str = (),
     requires: t.Sequence[str] | str = (),
 ):
@@ -25,6 +27,7 @@ def trigger(
     def decorator(f):
         """Decorator to trigger a method when an event of type."""
         takes_ids = 'ids' in inspect.signature(f).parameters
+
         if event_types != ('apply',):
             assert takes_ids, (
                 f"Method {f.__name__} must take an 'ids' argument"
@@ -32,6 +35,11 @@ def trigger(
             )
 
         def decorated(self, ids: t.List[str] | None = None, job: bool = False):
+            if event_types != ('apply',):
+                msg = 'Method must be a `Trigger` instance to take non-applying events'
+                from pinnacle.components.trigger import Trigger
+                assert isinstance(self, Trigger), msg
+
             if job:
                 return Job(
                     type_id=self.type_id,
@@ -42,6 +50,7 @@ def trigger(
                     db=self.db,
                 )
             else:
+                self.init()
                 return f(self, ids) if takes_ids else f(self)
 
         decorated.events = event_types

test/unittest/vector_search/test_vector_search.py
@@ -6,8 +6,8 @@ import pytest
 
 from pinnacle import CFG
 from pinnacle.vector_search.base import VectorItem
-from pinnacle.vector_search.in_memory import InMemoryVectorSearcher
 from pinnacle.vector_search.lance import LanceVectorSearcher
+from pinnacle.backends.local.vector_search import InMemoryVectorSearcher
 from pinnacle.vector_search.qdrant import QdrantVectorSearcher
 
 
@@ -26,10 +26,11 @@ def index_data(monkeypatch):
 )
 @pytest.mark.parametrize("measure", ["l2", "dot", "cosine"])
 def test_index(index_data, measure, vector_index_cls):
-    h, ids, ud = index_data
+    vectors, ids, ud = index_data
     h = vector_index_cls(
-        identifier="my-index", h=h, index=ids, measure=measure, dimensions=3
+        uuid="123456", measure=measure, dimensions=3
     )
+    h.add(items=[VectorItem(id=id_, vector=hh) for hh, id_ in zip(vectors, ids)])
     y = np.array([0, 0, 1])
     res, _ = h.find_nearest_from_array(y, 1)
     assert res[0] == ids[0]