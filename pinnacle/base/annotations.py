superduper-io/superduper

Compile list of data updates and jobs for submission

superduper/base/annotations.py
@@ -1,13 +1,11 @@
 import inspect
-import sys
 import typing as t
 
-from superduper.base.event import EventType
-from superduper.jobs.job import Job
+from superduper.base.event import Job
 
 
 def trigger(
-    *event_types: t.Sequence[EventType],
+    *event_types: t.Sequence[str],
     depends: t.Sequence[str] | str = (),
     requires: t.Sequence[str] | str = (),
 ):
@@ -34,7 +32,7 @@ def trigger(
                 " to be triggered by anything apart from 'apply'"
             )
 
-        def decorated(self, job: bool = False, **kwargs):
+        def decorated(self, *, context: str = '', job: bool = False, **kwargs):
             # the futures kwargs is a placeholder to allow for pinning 
             # dependencies according to the readiness of upstream jobs
             if event_types != ('apply',):
@@ -49,10 +47,11 @@ def trigger(
                     uuid=self.uuid,
                     method=f.__name__,
                     kwargs=kwargs,
-                    db=self.db,
+                    context=context,
                 )
             else:
                 self.init()
+                kwargs = {k: v for k, v in kwargs.items() if k in inspect.signature(f).parameters}
                 return f(self, **kwargs)
 
         decorated.events = event_types

superduper/components/cdc.py
@@ -1,7 +1,7 @@
+import networkx as nx
 import typing as t
 
-from superduper import Component
-from superduper.backends.base.query import Query
+from superduper import Component, CFG
 
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
@@ -26,3 +26,114 @@ class CDC(Component):
         super().declare_component(cluster)
         self.db.cluster.queue.put(self)
         self.db.cluster.cdc.put(self)
+
+    @property
+    def dependencies(self):
+        """Get dependencies of this component."""
+        if self.cdc_table.startswith(CFG.outputs_prefix):
+            return [tuple(self.cdc_table.split('__'))]
+        return []
+
+
+def _get_parent_cdcs_of_component(component, db: 'Datalayer'):
+    parents = db.metadata.get_component_version_parents(component.uuid)
+    out = []
+    for uuid in parents:
+        r = db.metadata.get_component_by_uuid(uuid)
+        if r.get('cdc_table'):
+            out.append(db.load(uuid=uuid))
+    return {c.uuid: c for c in out}
+
+
+def _get_cdcs_on_table(table, db: 'Datalayer'):
+    from superduper.components.listener import Listener
+    cdcs = db.metadata.show_cdcs(table)
+    out = []
+    for uuid in cdcs:
+        component = db.load(uuid=uuid)
+        if isinstance(component, Listener):
+            if len(component.select.tables) > 1:
+                continue
+            out.append(component)
+        out.append(component)
+    return out
+
+
+def build_streaming_graph(table, db: 'Datalayer'):
+    """Build a streaming graph from a table.
+
+    The graph has as each node a component which 
+    ingests from the table, or ingests from 
+    a component which ingests from the table (etc.).
+    
+    :param table: The table to build the graph from.
+    :param db: Datalayer instance
+    """
+    G = nx.DiGraph()
+    components = _get_cdcs_on_table(table, db)
+    out_cache = {component.huuid: component for component in components}
+    for component in components:
+        G.add_node(component.huuid)
+    while components:
+        new = []
+        for component in components:
+            parents = _get_parent_cdcs_of_component(component, db=db)
+            for parent in parents:
+                deps = [x[1] for x in parents[parent].dependencies]
+                if component.uuid not in deps:
+                    continue
+                G.add_edge(component.huuid, parents[parent].huuid)
+                out_cache[parents[parent].huuid] = parents[parent]
+            new.extend(list(parents.values()))
+        components = list({x.huuid: x for x in new}.values())
+    return G, out_cache
+
+
+if __name__ == '__main__':
+    from superduper.components.model import ObjectModel
+    from superduper import Listener
+    from superduper import superduper
+
+    db = superduper('mongomock://test')
+
+    db['documents'].insert([{'x': i} for i in range(10)]).execute()
+    
+    m = ObjectModel('test', object=lambda x: x)
+    m2 = ObjectModel('test', object=lambda x, y: x)
+
+    l1 = Listener('l1', model=m, select=db['documents'].select(), key='x')
+
+    db.apply(l1)
+
+    l2 = Listener('l2', model=m, key=l1.outputs, select=l1.outputs_select)
+
+    db.apply(l2)
+
+    l3 = Listener('l3', model=m, select=db['documents'].select(), key='x')
+
+    db.apply(l3)
+
+    l4 = Listener('l4', model=m2, select=db['documents'].select().outputs(l1.predict_id, l3.predict_id), key=(l1.outputs, l3.outputs))
+
+    db.apply(l4)
+
+    G, components = build_streaming_graph('documents', db)
+
+    def iterate_upwards(graph, start_nodes):
+        visited = set()
+        nodes_to_explore = list(start_nodes)
+
+        while nodes_to_explore:
+            current_node = nodes_to_explore.pop()
+            
+            for parent in graph.successors(current_node):
+                if parent not in visited:
+                    yield current_node, parent
+                    visited.add(parent)
+                    nodes_to_explore.append(parent)
+
+    root_nodes = [n for n, d in G.in_degree() if d == 0]
+    print(root_nodes)
+
+    for child, parent in iterate_upwards(G, root_nodes):
+        print(f"Step: Moving from {child} to {parent}")

superduper/components/vector_index.py
@@ -16,7 +16,7 @@ from superduper.components.listener import Listener
 from superduper.components.model import Mapping, ModelInputType
 from superduper.components.cdc import CDC
 from superduper.ext.utils import str_shape
-from superduper.jobs.annotations import trigger
+from superduper.base.annotations import trigger
 from superduper.misc.annotations import component
 from superduper.misc.special_dicts import MongoStyleDict
 from superduper.vector_search.base import VectorIndexMeasureType, VectorItem

test/unittest/component/test_component.py
@@ -16,7 +16,7 @@ from superduper.components.datatype import (
     dill_serializer,
 )
 from superduper.components.listener import Listener
-from superduper.jobs.annotations import trigger
+from superduper.base.annotations import trigger
 
 
 @pytest.fixture

test/unittest/component/test_model.py
@@ -1,4 +1,5 @@
 import dataclasses as dc
+from superduper.base.event import Job
 from test.utils.component import model as model_utils
 from unittest.mock import MagicMock, patch
 
@@ -24,7 +25,6 @@ from superduper.components.model import (
     Validation,
 )
 from superduper.components.schema import FieldType
-from superduper.jobs.job import Job
 
 
 # ------------------------------------------
@@ -283,8 +283,6 @@ def test_model_create_fit_job(db):
     # TODO move these parameters into the `Trainer` (same thing for validation)
     model.trainer = MyTrainer('test', select=db['documents'].select(), key='x')
     db.apply(model)
-    with patch.object(Job, '__call__') as mock_call:
-        mock_call.return_value = None
     model.db = db
     job = model.fit_in_db(job=True)
     assert job.identifier == model.identifier

Pinnacle-Networks/Pinnacle

pinnacle/base/annotations.py
@@ -1,13 +1,11 @@
 import inspect
-import sys
 import typing as t
 
-from pinnacle.base.event import EventType
-from pinnacle.jobs.job import Job
+from pinnacle.base.event import Job
 
 
 def trigger(
-    *event_types: t.Sequence[EventType],
+    *event_types: t.Sequence[str],
     depends: t.Sequence[str] | str = (),
     requires: t.Sequence[str] | str = (),
 ):
@@ -34,7 +32,7 @@ def trigger(
                 " to be triggered by anything apart from 'apply'"
             )
 
-        def decorated(self, job: bool = False, **kwargs):
+        def decorated(self, *, context: str = '', job: bool = False, **kwargs):
             # the futures kwargs is a placeholder to allow for pinning 
             # dependencies according to the readiness of upstream jobs
             if event_types != ('apply',):
@@ -49,10 +47,11 @@ def trigger(
                     uuid=self.uuid,
                     method=f.__name__,
                     kwargs=kwargs,
-                    db=self.db,
+                    context=context,
                 )
             else:
                 self.init()
+                kwargs = {k: v for k, v in kwargs.items() if k in inspect.signature(f).parameters}
                 return f(self, **kwargs)
 
         decorated.events = event_types

pinnacle/components/cdc.py
@@ -1,7 +1,7 @@
+import networkx as nx
 import typing as t
 
-from pinnacle import Component
-from pinnacle.backends.base.query import Query
+from pinnacle import Component, CFG
 
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer
@@ -26,3 +26,114 @@ class CDC(Component):
         super().declare_component(cluster)
         self.db.cluster.queue.put(self)
         self.db.cluster.cdc.put(self)
+
+    @property
+    def dependencies(self):
+        """Get dependencies of this component."""
+        if self.cdc_table.startswith(CFG.outputs_prefix):
+            return [tuple(self.cdc_table.split('__'))]
+        return []
+
+
+def _get_parent_cdcs_of_component(component, db: 'Datalayer'):
+    parents = db.metadata.get_component_version_parents(component.uuid)
+    out = []
+    for uuid in parents:
+        r = db.metadata.get_component_by_uuid(uuid)
+        if r.get('cdc_table'):
+            out.append(db.load(uuid=uuid))
+    return {c.uuid: c for c in out}
+
+
+def _get_cdcs_on_table(table, db: 'Datalayer'):
+    from pinnacle.components.listener import Listener
+    cdcs = db.metadata.show_cdcs(table)
+    out = []
+    for uuid in cdcs:
+        component = db.load(uuid=uuid)
+        if isinstance(component, Listener):
+            if len(component.select.tables) > 1:
+                continue
+            out.append(component)
+        out.append(component)
+    return out
+
+
+def build_streaming_graph(table, db: 'Datalayer'):
+    """Build a streaming graph from a table.
+
+    The graph has as each node a component which 
+    ingests from the table, or ingests from 
+    a component which ingests from the table (etc.).
+    
+    :param table: The table to build the graph from.
+    :param db: Datalayer instance
+    """
+    G = nx.DiGraph()
+    components = _get_cdcs_on_table(table, db)
+    out_cache = {component.huuid: component for component in components}
+    for component in components:
+        G.add_node(component.huuid)
+    while components:
+        new = []
+        for component in components:
+            parents = _get_parent_cdcs_of_component(component, db=db)
+            for parent in parents:
+                deps = [x[1] for x in parents[parent].dependencies]
+                if component.uuid not in deps:
+                    continue
+                G.add_edge(component.huuid, parents[parent].huuid)
+                out_cache[parents[parent].huuid] = parents[parent]
+            new.extend(list(parents.values()))
+        components = list({x.huuid: x for x in new}.values())
+    return G, out_cache
+
+
+if __name__ == '__main__':
+    from pinnacle.components.model import ObjectModel
+    from pinnacle import Listener
+    from pinnacle import pinnacle
+
+    db = pinnacle('mongomock://test')
+
+    db['documents'].insert([{'x': i} for i in range(10)]).execute()
+    
+    m = ObjectModel('test', object=lambda x: x)
+    m2 = ObjectModel('test', object=lambda x, y: x)
+
+    l1 = Listener('l1', model=m, select=db['documents'].select(), key='x')
+
+    db.apply(l1)
+
+    l2 = Listener('l2', model=m, key=l1.outputs, select=l1.outputs_select)
+
+    db.apply(l2)
+
+    l3 = Listener('l3', model=m, select=db['documents'].select(), key='x')
+
+    db.apply(l3)
+
+    l4 = Listener('l4', model=m2, select=db['documents'].select().outputs(l1.predict_id, l3.predict_id), key=(l1.outputs, l3.outputs))
+
+    db.apply(l4)
+
+    G, components = build_streaming_graph('documents', db)
+
+    def iterate_upwards(graph, start_nodes):
+        visited = set()
+        nodes_to_explore = list(start_nodes)
+
+        while nodes_to_explore:
+            current_node = nodes_to_explore.pop()
+            
+            for parent in graph.successors(current_node):
+                if parent not in visited:
+                    yield current_node, parent
+                    visited.add(parent)
+                    nodes_to_explore.append(parent)
+
+    root_nodes = [n for n, d in G.in_degree() if d == 0]
+    print(root_nodes)
+
+    for child, parent in iterate_upwards(G, root_nodes):
+        print(f"Step: Moving from {child} to {parent}")

pinnacle/components/vector_index.py
@@ -16,7 +16,7 @@ from pinnacle.components.listener import Listener
 from pinnacle.components.model import Mapping, ModelInputType
 from pinnacle.components.cdc import CDC
 from pinnacle.ext.utils import str_shape
-from pinnacle.jobs.annotations import trigger
+from pinnacle.base.annotations import trigger
 from pinnacle.misc.annotations import component
 from pinnacle.misc.special_dicts import MongoStyleDict
 from pinnacle.vector_search.base import VectorIndexMeasureType, VectorItem

test/unittest/component/test_component.py
@@ -16,7 +16,7 @@ from pinnacle.components.datatype import (
     dill_serializer,
 )
 from pinnacle.components.listener import Listener
-from pinnacle.jobs.annotations import trigger
+from pinnacle.base.annotations import trigger
 
 
 @pytest.fixture

test/unittest/component/test_model.py
@@ -1,4 +1,5 @@
 import dataclasses as dc
+from pinnacle.base.event import Job
 from test.utils.component import model as model_utils
 from unittest.mock import MagicMock, patch
 
@@ -24,7 +25,6 @@ from pinnacle.components.model import (
     Validation,
 )
 from pinnacle.components.schema import FieldType
-from pinnacle.jobs.job import Job
 
 
 # ------------------------------------------
@@ -283,8 +283,6 @@ def test_model_create_fit_job(db):
     # TODO move these parameters into the `Trainer` (same thing for validation)
     model.trainer = MyTrainer('test', select=db['documents'].select(), key='x')
     db.apply(model)
-    with patch.object(Job, '__call__') as mock_call:
-        mock_call.return_value = None
     model.db = db
     job = model.fit_in_db(job=True)
     assert job.identifier == model.identifier