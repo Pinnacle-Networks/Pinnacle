superduper-io/superduper

Simple tests passing

superduper/base/apply.py
@@ -4,7 +4,8 @@ from superduper import logging
 import typing as t
 
 from superduper.base.constant import KEY_BUILDS
-from superduper.base.event import Create, Signal
+from superduper.base.document import Document
+from superduper.base.event import Create, Signal, Update
 from superduper.components.component import Status
 from superduper.misc.special_dicts import recursive_update
 from superduper import Component
@@ -78,13 +79,17 @@ def apply(
     }
     for i, c in enumerate(unique_create_events):
         if c.parent:
-            logging.info(f'[{i}]: {c.huuid}: create ~ [{steps[c.parent]}]')
+            try:
+                logging.info(f'[{i}]: {c.huuid}: {c.genus} ~ [{steps[c.parent]}]')
+            except KeyError:
+                logging.info(f'[{i}]: {c.huuid}: {c.genus}')
         else:
-            logging.info(f'[{i}]: {c.huuid}: create')
+            logging.info(f'[{i}]: {c.huuid}: {c.genus}')
 
     logging.info('JOBS EVENTS:')
     steps = {j.job_id: str(i) for i, j in enumerate(unique_job_events)}
 
+    # TODO why do we need this?
     def uniquify(x):
         return sorted(list(set(x)))
 
@@ -114,51 +119,36 @@ def apply(
 
 
 def _apply(
-    db,
+    db: 'Datalayer',
     object: 'Component',
+    context: str,
+    job_events: t.List['Job'],
     parent: t.Optional[str] = None,
-    artifacts: t.Optional[t.Dict[str, bytes]] = None,
-    context: t.Optional[str] = None,
-    job_events: t.Sequence['Job'] = (),
 ):
-    job_events = list(job_events)
-
     object.db = db
     existing_versions = db.show(object.type_id, object.identifier)
-    already_exists = (
-        isinstance(object.version, int) and object.version in existing_versions
-    )
+    already_exists = bool(existing_versions)
+
+    serialized = object.dict()
+    broken = True
     if already_exists:
-        _update_component(object, parent=parent)
-        return [], []
+        current = db.load(object.type_id, object.identifier)
+        diff = current.dict().diff(serialized)
+        diff = Document({k: v for k, v in diff.items() if k not in {'uuid', 'version'}})
+        if not diff:
+            return [], job_events
 
-    assert hasattr(object, 'identifier')
-    assert hasattr(object, 'version')
+        broken = bool(set(diff.keys()).intersection(object.breaks))
+        if not broken:
+            serialized = diff
 
-    if object.version is None:
-        if existing_versions:
-            object.version = max(existing_versions) + 1
-        else:
-            object.version = 0
-
-    if object.get_triggers('apply') == ['set_status']:
-        object.status = Status.ready
-
-    serialized = object.dict().encode(leaves_to_keep=(Component,))
-
-    for k, v in serialized[KEY_BUILDS].items():
-        # TODO this is from the `@component` decorator.
-        # Can be handled with a single @leaf decorator around a function
-        # or a class
-        if isinstance(v, Component) and hasattr(v, 'inline') and v.inline:
-            r = dict(v.dict())
-            del r['identifier']
-            serialized[KEY_BUILDS][k] = r
+    serialized = serialized.encode(leaves_to_keep=(Component,))
 
     children = [
         v for v in serialized[KEY_BUILDS].values() if isinstance(v, Component)
     ]
-    create_events, j = _add_child_components(
+
+    create_events, j = _apply_child_components(
         db=db,
         components=children,
         parent=object,
@@ -166,26 +156,54 @@ def _apply(
         context=context,
     )
     job_events += j
+
     if children:
         serialized = _change_component_reference_prefix(serialized)
 
     serialized = db._save_artifact(object.uuid, serialized)
-    if artifacts:
-        for file_id, bytes in artifacts.items():
-            db.artifact_store.put_bytes(bytes, file_id)
 
-    assert context is not None
-    event = Create(
-        context=context,
-        component=serialized,
-        parent=parent,
-    )
-    create_events.append(event)
+    if broken:
+        if existing_versions:
+            object.version = max(existing_versions) + 1
+            serialized['version'] = object.version
+        else:
+            serialized['version'] = 0
+            object.version = 0
+        metadata_event = Create(
+            context=context,
+            component=serialized,
+            parent=parent,
+        )
 
-    job_events += object.create_jobs(
-        event_type='apply', jobs=job_events, context=context
-    )
-    return create_events, job_events
+        these_job_events = object.create_jobs(
+            event_type='apply',
+            jobs=job_events,
+            context=context,
+        )
+    else:
+        current_serialized = Document(db.metadata.get_component_by_uuid(uuid=current.uuid))
+        serialized = dict(current_serialized.update(serialized))
+        object.version = current.version
+        object.uuid = current.uuid
+        metadata_event = Update(
+            context=context,
+            component=serialized,
+            parent=parent,
+        )
+
+        these_job_events = object.create_jobs(
+            event_type='apply',
+            jobs=job_events,
+            context=context,
+            requires=list(diff.keys())
+        )
+
+    if not these_job_events:
+        metadata_event.component['status'] = Status.ready
+        object.status = Status.ready
+
+    create_events.append(metadata_event)
+    return create_events, job_events + these_job_events
     
 
 def _change_component_reference_prefix(serialized):
@@ -219,7 +237,7 @@ def _change_component_reference_prefix(serialized):
     return serialized
     
 
-def _add_child_components(db, components, parent, job_events, context):
+def _apply_child_components(db, components, parent, job_events, context):
     # TODO this is a bit of a mess
     # it handles the situation in `Stack` when
     # the components should be added in a certain order

test/unittest/base/test_apply.py
@@ -1,6 +1,15 @@
-import pytest
 import typing as t
 from superduper import Component
+from superduper.base.annotations import trigger
+from superduper.base.datalayer import Datalayer
+
+
+class MyValidator(Component):
+    target: int
+
+    def run(self, parent):
+        return float(parent.b * parent.c > self.target)
+
 
 class MyComponent(Component):
     type_id: t.ClassVar[str] = 'my'
@@ -8,24 +17,34 @@ class MyComponent(Component):
 
     a: str
     b: int
+    c: float = 0.5
     sub: Component | None = None
+    validate_results: bool | None = None
 
+    @trigger('apply', requires='sub')
+    def validate_in_db(self):
+        if isinstance(self.sub, MyValidator):
+            self.validate_results = self.sub.run(self)
+            self.db.replace(self)
+        return self.validate_results
 
-def test_simple_apply(db):
+def test_simple_apply(db: Datalayer):
 
     c = MyComponent('test', a='value', b=1)
 
     db.apply(c)
 
     assert db.show('my') == ['test']
+    assert db.show('my', 'test') == [0]
 
 
-@pytest.mark.skip
-def test_skip_same(db):
+def test_skip_same(db: Datalayer):
 
     c = MyComponent('test', a='value', b=1)
 
     db.apply(c)
+
+    assert db.show()
 
     c = MyComponent('test', a='value', b=1)
 
@@ -37,8 +56,7 @@ def test_skip_same(db):
     assert db.show('my', 'test') == [0]
 
 
-@pytest.mark.skip
-def test_update_component_version(db):
+def test_update_component_version(db: Datalayer):
 
     c = MyComponent('test', a='value', b=1)
 
@@ -61,7 +79,7 @@ def test_update_component_version(db):
     assert reload.a == 'new-value'
 
 
-def test_break_version(db):
+def test_break_version(db: Datalayer):
 
     c = MyComponent('test', a='value', b=1)
 
@@ -84,8 +102,7 @@ def test_break_version(db):
     assert reload.b == 2
 
 
-@pytest.mark.skip
-def test_update_nested(db):
+def test_update_nested(db: Datalayer):
 
     c = MyComponent(
         'test',
@@ -100,7 +117,7 @@ def test_update_nested(db):
 
     db.apply(c)
 
-    assert db.show('my') == ['test', 'sub']
+    assert set(db.show('my')) == {'test', 'sub'}
 
     c = MyComponent(
         'test',
@@ -113,14 +130,20 @@ def test_update_nested(db):
         )
     )
 
+    # Neither the child or the parent
+    # is broken by updating sub.a
+    # that means we don't get a new
+    # version
     db.apply(c)
 
     assert db.show('my', 'test') == [0]
     assert db.show('my', 'sub') == [0]
 
+    # Nonetheless the child is updated
+    assert db.load('my', 'sub').a == 'new-sub-value'
 
-@pytest.mark.skip
-def test_break_nested(db):
+
+def test_break_nested(db: Datalayer):
 
     c = MyComponent(
         'test',
@@ -135,7 +158,7 @@ def test_break_nested(db):
 
     db.apply(c)
 
-    assert db.show('my') == ['test', 'sub']
+    assert set(db.show('my')) == {'test', 'sub'}
 
     c = MyComponent(
         'test',
@@ -155,3 +178,9 @@ def test_break_nested(db):
 
     assert db.show('my', 'test') == [0]
     assert db.show('my', 'sub') == [0, 1]
+
+    assert db.load('my', 'sub').b == 4
+
+
+def test_job_on_update(db: Datalayer):
+    ...

test/unittest/base/test_datalayer.py
@@ -9,6 +9,7 @@ import pytest
 
 from superduper.base.datalayer import Datalayer
 from superduper.base.document import Document
+from superduper.base.leaf import imported_value
 from superduper.components.component import Component
 from superduper.components.dataset import Dataset
 from superduper.components.datatype import (
@@ -39,8 +40,10 @@ mongodb_config = os.environ.get('SUPERDUPER_CONFIG', "").endswith('mongodb.yaml'
 
 @dc.dataclass(kw_only=True)
 class TestComponent(Component):
+    breaks: ClassVar[Sequence] = ('inc',)
     _artifacts: ClassVar[Sequence[str]] = (('artifact', dill_serializer),)
     version: Optional[int] = None
+    inc: int = 0
     type_id: str = 'test-component'
     is_on_create: bool = False
     is_after_create: bool = False
@@ -121,12 +124,12 @@ def test_add_version(db: Datalayer):
     assert db.show('test-component', 'test') == [0]
 
     # Check the version is incremented
-    component = TestComponent(identifier='test')
+    component = TestComponent(identifier='test', inc=1)
     db.apply(component)
     assert component.version == 1
     assert db.show('test-component', 'test') == [0, 1]
 
-    component = TestComponent(identifier='test')
+    component = TestComponent(identifier='test', inc=2)
     db.apply(component)
     assert component.version == 2
     assert db.show('test-component', 'test') == [0, 1, 2]
@@ -216,8 +219,8 @@ def test_add_table(db):
 
 def test_remove_component_version(db):
     for component in [
-        TestComponent(identifier='test', version=0),
-        TestComponent(identifier='test', version=1),
+        TestComponent(identifier='test', inc=0),
+        TestComponent(identifier='test', inc=1),
     ]:
         db.apply(component)
     assert db.show('test-component', 'test') == [0, 1]
@@ -297,8 +300,8 @@ def test_remove_component_with_artifact(db):
 
 def test_remove_one_version(db):
     for component in [
-        TestComponent(identifier='test', version=0),
-        TestComponent(identifier='test', version=1),
+        TestComponent(identifier='test', inc=0),
+        TestComponent(identifier='test', inc=1),
     ]:
         db.apply(component)
 
@@ -336,9 +339,9 @@ def test_show(db):
         TestComponent(identifier='a1'),
         TestComponent(identifier='a2'),
         TestComponent(identifier='a3'),
-        TestComponent(identifier='b'),
-        TestComponent(identifier='b'),
-        TestComponent(identifier='b'),
+        TestComponent(identifier='b', inc=0),
+        TestComponent(identifier='b', inc=1),
+        TestComponent(identifier='b', inc=2),
         DataType(identifier='c1'),
         DataType(identifier='c2'),
     ]:
@@ -469,7 +472,7 @@ def test_update_db(db):
         # assert doc[key] == '100'
 
 
-def test_replace(db):
+def test_replace(db: Datalayer):
     model = ObjectModel(
         object=lambda x: x + 1,
         identifier='m',
@@ -505,7 +508,7 @@ def test_replace(db):
     assert db.load('model', 'm').predict_batches([1]) == [4]
 
 
-def test_replace_with_child(db):
+def test_replace_with_child(db: Datalayer):
     db.apply(Table('docs', schema=Schema('docs', fields={'X': 'int', 'y': 'int'})))
     trainer = Trainer(
         identifier='trainer',
@@ -542,9 +545,12 @@ def test_replace_with_child(db):
     db.metadata.get_component_version_parents(rereloaded.uuid)
 
 
+def my_lambda(x): return x + 1
+
+
 def test_compound_component(db):
     m = ObjectModel(
-        object=lambda x: x + 1,
+        object=imported_value(my_lambda),
         identifier='my-test-module',
         datatype=FieldType(identifier='int'),
     )
@@ -558,7 +564,7 @@ def test_compound_component(db):
 
     db.apply(
         ObjectModel(
-            object=lambda x: x + 1,
+            object=lambda x: x + 2,
             identifier='my-test-module',
             datatype=FieldType(identifier='int'),
         )

Pinnacle-Networks/Pinnacle

pinnacle/base/apply.py
@@ -4,7 +4,8 @@ from pinnacle import logging
 import typing as t
 
 from pinnacle.base.constant import KEY_BUILDS
-from pinnacle.base.event import Create, Signal
+from pinnacle.base.document import Document
+from pinnacle.base.event import Create, Signal, Update
 from pinnacle.components.component import Status
 from pinnacle.misc.special_dicts import recursive_update
 from pinnacle import Component
@@ -78,13 +79,17 @@ def apply(
     }
     for i, c in enumerate(unique_create_events):
         if c.parent:
-            logging.info(f'[{i}]: {c.huuid}: create ~ [{steps[c.parent]}]')
+            try:
+                logging.info(f'[{i}]: {c.huuid}: {c.genus} ~ [{steps[c.parent]}]')
+            except KeyError:
+                logging.info(f'[{i}]: {c.huuid}: {c.genus}')
         else:
-            logging.info(f'[{i}]: {c.huuid}: create')
+            logging.info(f'[{i}]: {c.huuid}: {c.genus}')
 
     logging.info('JOBS EVENTS:')
     steps = {j.job_id: str(i) for i, j in enumerate(unique_job_events)}
 
+    # TODO why do we need this?
     def uniquify(x):
         return sorted(list(set(x)))
 
@@ -114,51 +119,36 @@ def apply(
 
 
 def _apply(
-    db,
+    db: 'Datalayer',
     object: 'Component',
+    context: str,
+    job_events: t.List['Job'],
     parent: t.Optional[str] = None,
-    artifacts: t.Optional[t.Dict[str, bytes]] = None,
-    context: t.Optional[str] = None,
-    job_events: t.Sequence['Job'] = (),
 ):
-    job_events = list(job_events)
-
     object.db = db
     existing_versions = db.show(object.type_id, object.identifier)
-    already_exists = (
-        isinstance(object.version, int) and object.version in existing_versions
-    )
+    already_exists = bool(existing_versions)
+
+    serialized = object.dict()
+    broken = True
     if already_exists:
-        _update_component(object, parent=parent)
-        return [], []
+        current = db.load(object.type_id, object.identifier)
+        diff = current.dict().diff(serialized)
+        diff = Document({k: v for k, v in diff.items() if k not in {'uuid', 'version'}})
+        if not diff:
+            return [], job_events
 
-    assert hasattr(object, 'identifier')
-    assert hasattr(object, 'version')
+        broken = bool(set(diff.keys()).intersection(object.breaks))
+        if not broken:
+            serialized = diff
 
-    if object.version is None:
-        if existing_versions:
-            object.version = max(existing_versions) + 1
-        else:
-            object.version = 0
-
-    if object.get_triggers('apply') == ['set_status']:
-        object.status = Status.ready
-
-    serialized = object.dict().encode(leaves_to_keep=(Component,))
-
-    for k, v in serialized[KEY_BUILDS].items():
-        # TODO this is from the `@component` decorator.
-        # Can be handled with a single @leaf decorator around a function
-        # or a class
-        if isinstance(v, Component) and hasattr(v, 'inline') and v.inline:
-            r = dict(v.dict())
-            del r['identifier']
-            serialized[KEY_BUILDS][k] = r
+    serialized = serialized.encode(leaves_to_keep=(Component,))
 
     children = [
         v for v in serialized[KEY_BUILDS].values() if isinstance(v, Component)
     ]
-    create_events, j = _add_child_components(
+
+    create_events, j = _apply_child_components(
         db=db,
         components=children,
         parent=object,
@@ -166,26 +156,54 @@ def _apply(
         context=context,
     )
     job_events += j
+
     if children:
         serialized = _change_component_reference_prefix(serialized)
 
     serialized = db._save_artifact(object.uuid, serialized)
-    if artifacts:
-        for file_id, bytes in artifacts.items():
-            db.artifact_store.put_bytes(bytes, file_id)
 
-    assert context is not None
-    event = Create(
-        context=context,
-        component=serialized,
-        parent=parent,
-    )
-    create_events.append(event)
+    if broken:
+        if existing_versions:
+            object.version = max(existing_versions) + 1
+            serialized['version'] = object.version
+        else:
+            serialized['version'] = 0
+            object.version = 0
+        metadata_event = Create(
+            context=context,
+            component=serialized,
+            parent=parent,
+        )
 
-    job_events += object.create_jobs(
-        event_type='apply', jobs=job_events, context=context
-    )
-    return create_events, job_events
+        these_job_events = object.create_jobs(
+            event_type='apply',
+            jobs=job_events,
+            context=context,
+        )
+    else:
+        current_serialized = Document(db.metadata.get_component_by_uuid(uuid=current.uuid))
+        serialized = dict(current_serialized.update(serialized))
+        object.version = current.version
+        object.uuid = current.uuid
+        metadata_event = Update(
+            context=context,
+            component=serialized,
+            parent=parent,
+        )
+
+        these_job_events = object.create_jobs(
+            event_type='apply',
+            jobs=job_events,
+            context=context,
+            requires=list(diff.keys())
+        )
+
+    if not these_job_events:
+        metadata_event.component['status'] = Status.ready
+        object.status = Status.ready
+
+    create_events.append(metadata_event)
+    return create_events, job_events + these_job_events
     
 
 def _change_component_reference_prefix(serialized):
@@ -219,7 +237,7 @@ def _change_component_reference_prefix(serialized):
     return serialized
     
 
-def _add_child_components(db, components, parent, job_events, context):
+def _apply_child_components(db, components, parent, job_events, context):
     # TODO this is a bit of a mess
     # it handles the situation in `Stack` when
     # the components should be added in a certain order

test/unittest/base/test_apply.py
@@ -1,6 +1,15 @@
-import pytest
 import typing as t
 from pinnacle import Component
+from pinnacle.base.annotations import trigger
+from pinnacle.base.datalayer import Datalayer
+
+
+class MyValidator(Component):
+    target: int
+
+    def run(self, parent):
+        return float(parent.b * parent.c > self.target)
+
 
 class MyComponent(Component):
     type_id: t.ClassVar[str] = 'my'
@@ -8,24 +17,34 @@ class MyComponent(Component):
 
     a: str
     b: int
+    c: float = 0.5
     sub: Component | None = None
+    validate_results: bool | None = None
 
+    @trigger('apply', requires='sub')
+    def validate_in_db(self):
+        if isinstance(self.sub, MyValidator):
+            self.validate_results = self.sub.run(self)
+            self.db.replace(self)
+        return self.validate_results
 
-def test_simple_apply(db):
+def test_simple_apply(db: Datalayer):
 
     c = MyComponent('test', a='value', b=1)
 
     db.apply(c)
 
     assert db.show('my') == ['test']
+    assert db.show('my', 'test') == [0]
 
 
-@pytest.mark.skip
-def test_skip_same(db):
+def test_skip_same(db: Datalayer):
 
     c = MyComponent('test', a='value', b=1)
 
     db.apply(c)
+
+    assert db.show()
 
     c = MyComponent('test', a='value', b=1)
 
@@ -37,8 +56,7 @@ def test_skip_same(db):
     assert db.show('my', 'test') == [0]
 
 
-@pytest.mark.skip
-def test_update_component_version(db):
+def test_update_component_version(db: Datalayer):
 
     c = MyComponent('test', a='value', b=1)
 
@@ -61,7 +79,7 @@ def test_update_component_version(db):
     assert reload.a == 'new-value'
 
 
-def test_break_version(db):
+def test_break_version(db: Datalayer):
 
     c = MyComponent('test', a='value', b=1)
 
@@ -84,8 +102,7 @@ def test_break_version(db):
     assert reload.b == 2
 
 
-@pytest.mark.skip
-def test_update_nested(db):
+def test_update_nested(db: Datalayer):
 
     c = MyComponent(
         'test',
@@ -100,7 +117,7 @@ def test_update_nested(db):
 
     db.apply(c)
 
-    assert db.show('my') == ['test', 'sub']
+    assert set(db.show('my')) == {'test', 'sub'}
 
     c = MyComponent(
         'test',
@@ -113,14 +130,20 @@ def test_update_nested(db):
         )
     )
 
+    # Neither the child or the parent
+    # is broken by updating sub.a
+    # that means we don't get a new
+    # version
     db.apply(c)
 
     assert db.show('my', 'test') == [0]
     assert db.show('my', 'sub') == [0]
 
+    # Nonetheless the child is updated
+    assert db.load('my', 'sub').a == 'new-sub-value'
 
-@pytest.mark.skip
-def test_break_nested(db):
+
+def test_break_nested(db: Datalayer):
 
     c = MyComponent(
         'test',
@@ -135,7 +158,7 @@ def test_break_nested(db):
 
     db.apply(c)
 
-    assert db.show('my') == ['test', 'sub']
+    assert set(db.show('my')) == {'test', 'sub'}
 
     c = MyComponent(
         'test',
@@ -155,3 +178,9 @@ def test_break_nested(db):
 
     assert db.show('my', 'test') == [0]
     assert db.show('my', 'sub') == [0, 1]
+
+    assert db.load('my', 'sub').b == 4
+
+
+def test_job_on_update(db: Datalayer):
+    ...

test/unittest/base/test_datalayer.py
@@ -9,6 +9,7 @@ import pytest
 
 from pinnacle.base.datalayer import Datalayer
 from pinnacle.base.document import Document
+from pinnacle.base.leaf import imported_value
 from pinnacle.components.component import Component
 from pinnacle.components.dataset import Dataset
 from pinnacle.components.datatype import (
@@ -39,8 +40,10 @@ mongodb_config = os.environ.get('SUPERDUPER_CONFIG', "").endswith('mongodb.yaml'
 
 @dc.dataclass(kw_only=True)
 class TestComponent(Component):
+    breaks: ClassVar[Sequence] = ('inc',)
     _artifacts: ClassVar[Sequence[str]] = (('artifact', dill_serializer),)
     version: Optional[int] = None
+    inc: int = 0
     type_id: str = 'test-component'
     is_on_create: bool = False
     is_after_create: bool = False
@@ -121,12 +124,12 @@ def test_add_version(db: Datalayer):
     assert db.show('test-component', 'test') == [0]
 
     # Check the version is incremented
-    component = TestComponent(identifier='test')
+    component = TestComponent(identifier='test', inc=1)
     db.apply(component)
     assert component.version == 1
     assert db.show('test-component', 'test') == [0, 1]
 
-    component = TestComponent(identifier='test')
+    component = TestComponent(identifier='test', inc=2)
     db.apply(component)
     assert component.version == 2
     assert db.show('test-component', 'test') == [0, 1, 2]
@@ -216,8 +219,8 @@ def test_add_table(db):
 
 def test_remove_component_version(db):
     for component in [
-        TestComponent(identifier='test', version=0),
-        TestComponent(identifier='test', version=1),
+        TestComponent(identifier='test', inc=0),
+        TestComponent(identifier='test', inc=1),
     ]:
         db.apply(component)
     assert db.show('test-component', 'test') == [0, 1]
@@ -297,8 +300,8 @@ def test_remove_component_with_artifact(db):
 
 def test_remove_one_version(db):
     for component in [
-        TestComponent(identifier='test', version=0),
-        TestComponent(identifier='test', version=1),
+        TestComponent(identifier='test', inc=0),
+        TestComponent(identifier='test', inc=1),
     ]:
         db.apply(component)
 
@@ -336,9 +339,9 @@ def test_show(db):
         TestComponent(identifier='a1'),
         TestComponent(identifier='a2'),
         TestComponent(identifier='a3'),
-        TestComponent(identifier='b'),
-        TestComponent(identifier='b'),
-        TestComponent(identifier='b'),
+        TestComponent(identifier='b', inc=0),
+        TestComponent(identifier='b', inc=1),
+        TestComponent(identifier='b', inc=2),
         DataType(identifier='c1'),
         DataType(identifier='c2'),
     ]:
@@ -469,7 +472,7 @@ def test_update_db(db):
         # assert doc[key] == '100'
 
 
-def test_replace(db):
+def test_replace(db: Datalayer):
     model = ObjectModel(
         object=lambda x: x + 1,
         identifier='m',
@@ -505,7 +508,7 @@ def test_replace(db):
     assert db.load('model', 'm').predict_batches([1]) == [4]
 
 
-def test_replace_with_child(db):
+def test_replace_with_child(db: Datalayer):
     db.apply(Table('docs', schema=Schema('docs', fields={'X': 'int', 'y': 'int'})))
     trainer = Trainer(
         identifier='trainer',
@@ -542,9 +545,12 @@ def test_replace_with_child(db):
     db.metadata.get_component_version_parents(rereloaded.uuid)
 
 
+def my_lambda(x): return x + 1
+
+
 def test_compound_component(db):
     m = ObjectModel(
-        object=lambda x: x + 1,
+        object=imported_value(my_lambda),
         identifier='my-test-module',
         datatype=FieldType(identifier='int'),
     )
@@ -558,7 +564,7 @@ def test_compound_component(db):
 
     db.apply(
         ObjectModel(
-            object=lambda x: x + 1,
+            object=lambda x: x + 2,
             identifier='my-test-module',
             datatype=FieldType(identifier='int'),
         )