superduper-io/superduper

Fix SQL tests by adding dtype parameter

superduper/ext/numpy/encoder.py
@@ -2,7 +2,12 @@ import typing as t
 
 import numpy
 
-from superduper.components.datatype import BaseDataType, DataType, DataTypeFactory
+from superduper.components.datatype import (
+    BaseDataType,
+    DataType,
+    DataTypeFactory,
+    Encodable,
+)
 from superduper.ext.utils import str_shape
 from superduper.misc.annotations import component
 
@@ -54,6 +59,10 @@ class Array(BaseDataType):
     """
 
     dtype: str = 'float64'
+
+    def __post_init__(self, db, artifacts):
+        self.encodable_cls = Encodable
+        return super().__post_init__(db, artifacts)
 
     def encode_data(self, item, info=None):
         encoder = EncodeArray(self.dtype)

plugins/ibis/superduper_ibis/data_backend.py
@@ -69,7 +69,7 @@ class IbisDataBackend(BaseDataBackend):
         self.overwrite = False
         self._setup(conn)
 
-        self.datatype_presets = {'vector': 'superduper.ext.numpy.Array'}
+        self.datatype_presets = {'vector': 'superduper.ext.numpy.encoder.Array'}
 
     def _setup(self, conn):
         self.dialect = getattr(conn, "name", "base")
@@ -294,4 +294,4 @@ class IbisDataBackend(BaseDataBackend):
         """
         from superduper.misc.auto_schema import infer_schema
 
-        return infer_schema(data, identifier=identifier, ibis=True)
+        return infer_schema(data, identifier=identifier)

plugins/ibis/superduper_ibis/utils.py
@@ -1,7 +1,8 @@
 from ibis.expr.datatypes import dtype
+from superduper import CFG
 from superduper.components.datatype import (
     Artifact,
-    DataType,
+    BaseDataType,
     File,
     LazyArtifact,
     LazyFile,
@@ -23,7 +24,6 @@ def _convert_field_type_to_ibis_type(field_type: FieldType):
         ibis_type = "String"
     else:
         ibis_type = field_type.identifier
-
     return dtype(ibis_type)
 
 
@@ -39,12 +39,12 @@ def convert_schema_to_fields(schema: Schema):
     for k, v in schema.fields.items():
         if isinstance(v, FieldType):
             fields[k] = _convert_field_type_to_ibis_type(v)
-        elif not isinstance(v, DataType):
+        elif not isinstance(v, BaseDataType):
             fields[k] = v.identifier
         else:
             if v.encodable_cls in SPECIAL_ENCODABLES_FIELDS:
                 fields[k] = dtype(SPECIAL_ENCODABLES_FIELDS[v.encodable_cls])
             else:
-                fields[k] = v.bytes_encoding
+                fields[k] = CFG.bytes_encoding
 
     return fields

plugins/mongodb/plugin_test/test_queries.py
@@ -1,14 +1,14 @@
 import random
-
-import numpy as np
-import pytest
-from superduper.base.document import Document
 from test.utils.setup.fake_data import (
     add_listeners,
     add_models,
     add_random_data,
     add_vector_index,
 )
+
+import numpy as np
+import pytest
+from superduper.base.document import Document
 
 
 def get_new_data(n=10, update=False):

Pinnacle-Networks/Pinnacle

pinnacle/ext/numpy/encoder.py
@@ -2,7 +2,12 @@ import typing as t
 
 import numpy
 
-from pinnacle.components.datatype import BaseDataType, DataType, DataTypeFactory
+from pinnacle.components.datatype import (
+    BaseDataType,
+    DataType,
+    DataTypeFactory,
+    Encodable,
+)
 from pinnacle.ext.utils import str_shape
 from pinnacle.misc.annotations import component
 
@@ -54,6 +59,10 @@ class Array(BaseDataType):
     """
 
     dtype: str = 'float64'
+
+    def __post_init__(self, db, artifacts):
+        self.encodable_cls = Encodable
+        return super().__post_init__(db, artifacts)
 
     def encode_data(self, item, info=None):
         encoder = EncodeArray(self.dtype)

plugins/ibis/superduper_ibis/data_backend.py
@@ -69,7 +69,7 @@ class IbisDataBackend(BaseDataBackend):
         self.overwrite = False
         self._setup(conn)
 
-        self.datatype_presets = {'vector': 'pinnacle.ext.numpy.Array'}
+        self.datatype_presets = {'vector': 'pinnacle.ext.numpy.encoder.Array'}
 
     def _setup(self, conn):
         self.dialect = getattr(conn, "name", "base")
@@ -294,4 +294,4 @@ class IbisDataBackend(BaseDataBackend):
         """
         from pinnacle.misc.auto_schema import infer_schema
 
-        return infer_schema(data, identifier=identifier, ibis=True)
+        return infer_schema(data, identifier=identifier)

plugins/ibis/superduper_ibis/utils.py
@@ -1,7 +1,8 @@
 from ibis.expr.datatypes import dtype
+from pinnacle import CFG
 from pinnacle.components.datatype import (
     Artifact,
-    DataType,
+    BaseDataType,
     File,
     LazyArtifact,
     LazyFile,
@@ -23,7 +24,6 @@ def _convert_field_type_to_ibis_type(field_type: FieldType):
         ibis_type = "String"
     else:
         ibis_type = field_type.identifier
-
     return dtype(ibis_type)
 
 
@@ -39,12 +39,12 @@ def convert_schema_to_fields(schema: Schema):
     for k, v in schema.fields.items():
         if isinstance(v, FieldType):
             fields[k] = _convert_field_type_to_ibis_type(v)
-        elif not isinstance(v, DataType):
+        elif not isinstance(v, BaseDataType):
             fields[k] = v.identifier
         else:
             if v.encodable_cls in SPECIAL_ENCODABLES_FIELDS:
                 fields[k] = dtype(SPECIAL_ENCODABLES_FIELDS[v.encodable_cls])
             else:
-                fields[k] = v.bytes_encoding
+                fields[k] = CFG.bytes_encoding
 
     return fields

plugins/mongodb/plugin_test/test_queries.py
@@ -1,14 +1,14 @@
 import random
-
-import numpy as np
-import pytest
-from pinnacle.base.document import Document
 from test.utils.setup.fake_data import (
     add_listeners,
     add_models,
     add_random_data,
     add_vector_index,
 )
+
+import numpy as np
+import pytest
+from pinnacle.base.document import Document
 
 
 def get_new_data(n=10, update=False):