superduper-io/superduper

Add _BaseDatatype and Vector datatype for auto-schema

superduper/base/config.py
@@ -130,6 +130,10 @@ class RestConfig(BaseConfig):
     config: t.Optional[str] = None
 
 
+class DataTypePresets(BaseConfig):
+    vector: str = 'superduper.ext.numpy.Array'
+
+
 @dc.dataclass
 class Config(BaseConfig):
     """The data class containing all configurable superduper values.
@@ -179,6 +183,8 @@ class Config(BaseConfig):
     log_colorize: bool = True
 
     force_apply: bool = False
+
+    datatype_presets: DataTypePresets = dc.field(default_factory=DataTypePresets)
 
     bytes_encoding: BytesEncoding = BytesEncoding.BYTES
     auto_schema: bool = True

superduper/components/datatype.py
@@ -1,6 +1,7 @@
 import base64
 import dataclasses as dc
 import hashlib
+from importlib import import_module
 import inspect
 import io
 import json
@@ -8,7 +9,7 @@ import os
 import pickle
 import re
 import typing as t
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 import dill
 
@@ -17,7 +18,7 @@ from superduper.backends.base.artifacts import (
     _construct_file_id_from_uri,
 )
 from superduper.base.config import BytesEncoding
-from superduper.base.leaf import Leaf
+from superduper.base.leaf import Leaf, import_item
 from superduper.components.component import Component, ensure_initialized
 from superduper.misc.annotations import component
 from superduper.misc.hash import hash_path
@@ -176,7 +177,34 @@ class DataTypeFactory:
         raise NotImplementedError
 
 
-class DataType(Component):
+class _BaseDatatype(Component):
+    """Base class for datatype.
+
+    :param shape: size of vector
+    """
+
+    type_id: t.ClassVar[str] = 'datatype'
+    # TODO this can just be an integer
+    shape: t.Optional[int] = None
+
+    @abstractmethod
+    def encode_data(self, item, info: t.Optional[t.Dict] = None):
+        """Decode the item as `bytes`
+
+        :param item: The item to decode.
+        :param info: The optional information dictionary.
+        """
+    
+    @abstractmethod
+    def decode_data(self, item, info: t.Optional[t.Dict] = None):
+        """Decode the item from bytes.
+
+        :param item: The item to decode.
+        :param info: The optional information dictionary.
+        """
+
+
+class DataType(_BaseDatatype):
     """A data type component that defines how data is encoded and decoded.
 
     :param encoder: A callable that converts an encodable object of this
@@ -184,7 +212,6 @@ class DataType(Component):
     :param decoder: A callable that converts bytes to an encodable object
                     of this encoder.
     :param info: An optional information dictionary.
-    :param shape: The shape of the data.
     :param directory: The directory to store file types.
     :param encodable: The type of encodable object ('encodable',
                       'lazy_artifact', or 'file').
@@ -194,12 +221,10 @@ class DataType(Component):
     :param media_type: The media type.
     """
 
-    type_id: t.ClassVar[str] = 'datatype'
     encoder: t.Optional[t.Callable] = None  # not necessary if encodable is file
     decoder: t.Optional[t.Callable] = None
-    info: t.Optional[t.Dict] = None
-    shape: t.Optional[t.Sequence] = None
-    directory: t.Optional[str] = None
+    info: t.Optional[t.Dict] = None   # TODO deprecate
+    directory: t.Optional[str] = None   # TODO needed?
     encodable: str = 'encodable'
     bytes_encoding: t.Optional[str] = CFG.bytes_encoding
     intermediate_type: t.Optional[str] = IntermediateType.BYTES
@@ -334,6 +359,7 @@ def encode_torch_state_dict(module, info):
     return buffer.getvalue()
 
 
+# TODO migrate to torch plugin
 class DecodeTorchStateDict:
     """Torch state dictionary decoder.
 
@@ -450,6 +476,8 @@ class Blob(Leaf):
     bytes: bytes
 
 
+# TODO this is no longer stricly needed, since we now encode
+# directly with `Schema`
 class Encodable(_BaseEncodable):
     """Class for encoding non-Python datatypes to the database.
 
@@ -799,3 +827,35 @@ serializers = {
     'dill_lazy': dill_lazy,
     'file_lazy': file_lazy,
 }
+
+
+class Vector(_BaseDatatype):
+    identifier: str = ''
+
+    encodable_cls: t.ClassVar[type] = Encodable
+
+    def __post_init__(self, db, artifacts):
+        type_: str = CFG.datatype_presets.vector
+        module = '.'.join(type_.split('.')[:-1])
+        cls = type_.split('.')[-1]
+        datatype = getattr(import_module(module), cls)
+        if inspect.isclass(datatype):
+            datatype = datatype('tmp')
+        self.datatype = datatype
+        self.identifier = f'vector[{self.shape[0]}]'
+        return super().__post_init__(db, artifacts)
+
+    def encode_data(self, item, info: t.Optional[t.Dict] = None):
+        return self.datatype.encode_data(item=item, info=info)
+
+    def encode_data_with_identifier(self, item, info: t.Optional[t.Dict] = None):
+        b = self.encode_data(item=item, info=info)
+        if isinstance(b, str):
+            return b, hashlib.sha1(b.encode()).hexdigest()
+        else:
+            assert type(b) == bytes
+            return b, hashlib.sha1(b).hexdigest()
+
+    def decode_data(self, item, info: t.Optional[t.Dict] = None):
+        return self.datatype.decode_data(item=item, info=info)
+

superduper/components/schema.py
@@ -3,7 +3,7 @@ from functools import cached_property
 
 from superduper.base.leaf import Leaf
 from superduper.components.component import Component
-from superduper.components.datatype import DataType
+from superduper.components.datatype import _BaseDatatype, DataType
 from superduper.misc.reference import parse_reference
 from superduper.misc.special_dicts import SuperDuperFlatEncode
 
@@ -54,7 +54,7 @@ class Schema(Component):
         super().__post_init__(db, artifacts)
 
         for k, v in self.fields.items():
-            if isinstance(v, (DataType, FieldType)):
+            if isinstance(v, (_BaseDatatype, FieldType)):
                 continue
 
             try:
@@ -99,7 +99,7 @@ class Schema(Component):
         :param files: Files.
         """
         for k, field in self.fields.items():
-            if not isinstance(field, DataType):
+            if not isinstance(field, _BaseDatatype):
                 continue
 
             if k not in out:

superduper/ext/numpy/encoder.py
@@ -2,7 +2,7 @@ import typing as t
 
 import numpy
 
-from superduper.components.datatype import DataType, DataTypeFactory
+from superduper.components.datatype import _BaseDatatype, DataType, DataTypeFactory
 from superduper.ext.utils import str_shape
 from superduper.misc.annotations import component
 
@@ -45,6 +45,21 @@ class DecodeArray:
         :param info: The info of the encoding.
         """
         return numpy.frombuffer(bytes, dtype=self.dtype).reshape(self.shape)
+
+
+class Array(_BaseDatatype):
+    dtype: str = 'float64'
+
+    def encode_data(self, item, info = None):
+        encoder = EncodeArray(self.dtype)
+        return encoder(item)
+
+    def decode_data(self, item, info = None):
+        shape = self.shape
+        if isinstance(shape, int):
+            shape = (self.shape,)
+        decoder = DecodeArray(self.dtype, shape=shape)
+        return decoder(item)
 
 
 @component()

superduper/misc/auto_schema.py
@@ -4,9 +4,11 @@ import typing as t
 from superduper import CFG, logging
 from superduper.base.exceptions import UnsupportedDatatype
 from superduper.components.datatype import (
+    _BaseDatatype,
     DataType,
     DataTypeFactory,
     _BaseEncodable,
+    Vector,
     get_serializer,
     json_serializer,
 )
@@ -24,10 +26,6 @@ def register_module(module_name):
     except Exception:
         logging.debug(f"Could not register module: {module_name}")
 
-
-register_module("superduper.ext.numpy.encoder")
-register_module("superduper.ext.torch.encoder")
-register_module("superduper.ext.pillow.encoder")
 
 DEFAULT_DATATYPE = get_serializer(
     identifier='DEFAULT',
@@ -60,7 +58,6 @@ def infer_datatype(data: t.Any) -> t.Optional[t.Union[DataType, type]]:
 
     try:
         from bson import ObjectId
-
         if isinstance(data, ObjectId):
             return datatype
     except ImportError:
@@ -74,7 +71,7 @@ def infer_datatype(data: t.Any) -> t.Optional[t.Union[DataType, type]]:
     for factory in DataTypeFactory.__subclasses__():
         if factory.check(data):
             datatype = factory.create(data)
-            assert isinstance(datatype, DataType) or isinstance(datatype, FieldType)
+            assert isinstance(datatype, _BaseDatatype) or isinstance(datatype, FieldType)
             logging.debug(f"Inferred datatype: {datatype.identifier} for data: {data}")
             break
 
@@ -97,7 +94,6 @@ def infer_datatype(data: t.Any) -> t.Optional[t.Union[DataType, type]]:
 def infer_schema(
     data: t.Mapping[str, t.Any],
     identifier: t.Optional[str] = None,
-    ibis=False,
 ) -> Schema:
     """Infer a schema from a given data object.
 
@@ -137,6 +133,32 @@ def infer_schema(
     return Schema(identifier=identifier, fields=schema_data)  # type: ignore
 
 
+class VectorTypeFactory(DataTypeFactory):
+    """A factory for Vector datatypes # noqa."""
+
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        """Check if the data is able to be encoded by the JSON serializer.
+
+        :param data: The data object
+        """
+        from numpy import array as native_array
+        try:
+            _ = native_array(data)
+            return True
+        except Exception:
+            return False
+
+    @staticmethod
+    def create(data: t.Any) -> DataType | FieldType:
+        """Create a JSON datatype.
+
+        :param data: The data object
+        """
+        return Vector(shape=(len(data),))
+
+
+
 class JsonDataTypeFactory(DataTypeFactory):
     """A factory for JSON datatypes # noqa."""
 
@@ -161,3 +183,11 @@ class JsonDataTypeFactory(DataTypeFactory):
         if CFG.json_native:
             return FieldType(identifier='json')
         return json_serializer
+
+
+from superduper.ext import numpy
+
+
+register_module("superduper.ext.numpy.encoder")
+register_module("superduper.ext.torch.encoder")
+register_module("superduper.ext.pillow.encoder")

Pinnacle-Networks/Pinnacle

pinnacle/base/config.py
@@ -130,6 +130,10 @@ class RestConfig(BaseConfig):
     config: t.Optional[str] = None
 
 
+class DataTypePresets(BaseConfig):
+    vector: str = 'pinnacle.ext.numpy.Array'
+
+
 @dc.dataclass
 class Config(BaseConfig):
     """The data class containing all configurable pinnacle values.
@@ -179,6 +183,8 @@ class Config(BaseConfig):
     log_colorize: bool = True
 
     force_apply: bool = False
+
+    datatype_presets: DataTypePresets = dc.field(default_factory=DataTypePresets)
 
     bytes_encoding: BytesEncoding = BytesEncoding.BYTES
     auto_schema: bool = True

pinnacle/components/datatype.py
@@ -1,6 +1,7 @@
 import base64
 import dataclasses as dc
 import hashlib
+from importlib import import_module
 import inspect
 import io
 import json
@@ -8,7 +9,7 @@ import os
 import pickle
 import re
 import typing as t
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 import dill
 
@@ -17,7 +18,7 @@ from pinnacle.backends.base.artifacts import (
     _construct_file_id_from_uri,
 )
 from pinnacle.base.config import BytesEncoding
-from pinnacle.base.leaf import Leaf
+from pinnacle.base.leaf import Leaf, import_item
 from pinnacle.components.component import Component, ensure_initialized
 from pinnacle.misc.annotations import component
 from pinnacle.misc.hash import hash_path
@@ -176,7 +177,34 @@ class DataTypeFactory:
         raise NotImplementedError
 
 
-class DataType(Component):
+class _BaseDatatype(Component):
+    """Base class for datatype.
+
+    :param shape: size of vector
+    """
+
+    type_id: t.ClassVar[str] = 'datatype'
+    # TODO this can just be an integer
+    shape: t.Optional[int] = None
+
+    @abstractmethod
+    def encode_data(self, item, info: t.Optional[t.Dict] = None):
+        """Decode the item as `bytes`
+
+        :param item: The item to decode.
+        :param info: The optional information dictionary.
+        """
+    
+    @abstractmethod
+    def decode_data(self, item, info: t.Optional[t.Dict] = None):
+        """Decode the item from bytes.
+
+        :param item: The item to decode.
+        :param info: The optional information dictionary.
+        """
+
+
+class DataType(_BaseDatatype):
     """A data type component that defines how data is encoded and decoded.
 
     :param encoder: A callable that converts an encodable object of this
@@ -184,7 +212,6 @@ class DataType(Component):
     :param decoder: A callable that converts bytes to an encodable object
                     of this encoder.
     :param info: An optional information dictionary.
-    :param shape: The shape of the data.
     :param directory: The directory to store file types.
     :param encodable: The type of encodable object ('encodable',
                       'lazy_artifact', or 'file').
@@ -194,12 +221,10 @@ class DataType(Component):
     :param media_type: The media type.
     """
 
-    type_id: t.ClassVar[str] = 'datatype'
     encoder: t.Optional[t.Callable] = None  # not necessary if encodable is file
     decoder: t.Optional[t.Callable] = None
-    info: t.Optional[t.Dict] = None
-    shape: t.Optional[t.Sequence] = None
-    directory: t.Optional[str] = None
+    info: t.Optional[t.Dict] = None   # TODO deprecate
+    directory: t.Optional[str] = None   # TODO needed?
     encodable: str = 'encodable'
     bytes_encoding: t.Optional[str] = CFG.bytes_encoding
     intermediate_type: t.Optional[str] = IntermediateType.BYTES
@@ -334,6 +359,7 @@ def encode_torch_state_dict(module, info):
     return buffer.getvalue()
 
 
+# TODO migrate to torch plugin
 class DecodeTorchStateDict:
     """Torch state dictionary decoder.
 
@@ -450,6 +476,8 @@ class Blob(Leaf):
     bytes: bytes
 
 
+# TODO this is no longer stricly needed, since we now encode
+# directly with `Schema`
 class Encodable(_BaseEncodable):
     """Class for encoding non-Python datatypes to the database.
 
@@ -799,3 +827,35 @@ serializers = {
     'dill_lazy': dill_lazy,
     'file_lazy': file_lazy,
 }
+
+
+class Vector(_BaseDatatype):
+    identifier: str = ''
+
+    encodable_cls: t.ClassVar[type] = Encodable
+
+    def __post_init__(self, db, artifacts):
+        type_: str = CFG.datatype_presets.vector
+        module = '.'.join(type_.split('.')[:-1])
+        cls = type_.split('.')[-1]
+        datatype = getattr(import_module(module), cls)
+        if inspect.isclass(datatype):
+            datatype = datatype('tmp')
+        self.datatype = datatype
+        self.identifier = f'vector[{self.shape[0]}]'
+        return super().__post_init__(db, artifacts)
+
+    def encode_data(self, item, info: t.Optional[t.Dict] = None):
+        return self.datatype.encode_data(item=item, info=info)
+
+    def encode_data_with_identifier(self, item, info: t.Optional[t.Dict] = None):
+        b = self.encode_data(item=item, info=info)
+        if isinstance(b, str):
+            return b, hashlib.sha1(b.encode()).hexdigest()
+        else:
+            assert type(b) == bytes
+            return b, hashlib.sha1(b).hexdigest()
+
+    def decode_data(self, item, info: t.Optional[t.Dict] = None):
+        return self.datatype.decode_data(item=item, info=info)
+

pinnacle/components/schema.py
@@ -3,7 +3,7 @@ from functools import cached_property
 
 from pinnacle.base.leaf import Leaf
 from pinnacle.components.component import Component
-from pinnacle.components.datatype import DataType
+from pinnacle.components.datatype import _BaseDatatype, DataType
 from pinnacle.misc.reference import parse_reference
 from pinnacle.misc.special_dicts import SuperDuperFlatEncode
 
@@ -54,7 +54,7 @@ class Schema(Component):
         super().__post_init__(db, artifacts)
 
         for k, v in self.fields.items():
-            if isinstance(v, (DataType, FieldType)):
+            if isinstance(v, (_BaseDatatype, FieldType)):
                 continue
 
             try:
@@ -99,7 +99,7 @@ class Schema(Component):
         :param files: Files.
         """
         for k, field in self.fields.items():
-            if not isinstance(field, DataType):
+            if not isinstance(field, _BaseDatatype):
                 continue
 
             if k not in out:

pinnacle/ext/numpy/encoder.py
@@ -2,7 +2,7 @@ import typing as t
 
 import numpy
 
-from pinnacle.components.datatype import DataType, DataTypeFactory
+from pinnacle.components.datatype import _BaseDatatype, DataType, DataTypeFactory
 from pinnacle.ext.utils import str_shape
 from pinnacle.misc.annotations import component
 
@@ -45,6 +45,21 @@ class DecodeArray:
         :param info: The info of the encoding.
         """
         return numpy.frombuffer(bytes, dtype=self.dtype).reshape(self.shape)
+
+
+class Array(_BaseDatatype):
+    dtype: str = 'float64'
+
+    def encode_data(self, item, info = None):
+        encoder = EncodeArray(self.dtype)
+        return encoder(item)
+
+    def decode_data(self, item, info = None):
+        shape = self.shape
+        if isinstance(shape, int):
+            shape = (self.shape,)
+        decoder = DecodeArray(self.dtype, shape=shape)
+        return decoder(item)
 
 
 @component()

pinnacle/misc/auto_schema.py
@@ -4,9 +4,11 @@ import typing as t
 from pinnacle import CFG, logging
 from pinnacle.base.exceptions import UnsupportedDatatype
 from pinnacle.components.datatype import (
+    _BaseDatatype,
     DataType,
     DataTypeFactory,
     _BaseEncodable,
+    Vector,
     get_serializer,
     json_serializer,
 )
@@ -24,10 +26,6 @@ def register_module(module_name):
     except Exception:
         logging.debug(f"Could not register module: {module_name}")
 
-
-register_module("pinnacle.ext.numpy.encoder")
-register_module("pinnacle.ext.torch.encoder")
-register_module("pinnacle.ext.pillow.encoder")
 
 DEFAULT_DATATYPE = get_serializer(
     identifier='DEFAULT',
@@ -60,7 +58,6 @@ def infer_datatype(data: t.Any) -> t.Optional[t.Union[DataType, type]]:
 
     try:
         from bson import ObjectId
-
         if isinstance(data, ObjectId):
             return datatype
     except ImportError:
@@ -74,7 +71,7 @@ def infer_datatype(data: t.Any) -> t.Optional[t.Union[DataType, type]]:
     for factory in DataTypeFactory.__subclasses__():
         if factory.check(data):
             datatype = factory.create(data)
-            assert isinstance(datatype, DataType) or isinstance(datatype, FieldType)
+            assert isinstance(datatype, _BaseDatatype) or isinstance(datatype, FieldType)
             logging.debug(f"Inferred datatype: {datatype.identifier} for data: {data}")
             break
 
@@ -97,7 +94,6 @@ def infer_datatype(data: t.Any) -> t.Optional[t.Union[DataType, type]]:
 def infer_schema(
     data: t.Mapping[str, t.Any],
     identifier: t.Optional[str] = None,
-    ibis=False,
 ) -> Schema:
     """Infer a schema from a given data object.
 
@@ -137,6 +133,32 @@ def infer_schema(
     return Schema(identifier=identifier, fields=schema_data)  # type: ignore
 
 
+class VectorTypeFactory(DataTypeFactory):
+    """A factory for Vector datatypes # noqa."""
+
+    @staticmethod
+    def check(data: t.Any) -> bool:
+        """Check if the data is able to be encoded by the JSON serializer.
+
+        :param data: The data object
+        """
+        from numpy import array as native_array
+        try:
+            _ = native_array(data)
+            return True
+        except Exception:
+            return False
+
+    @staticmethod
+    def create(data: t.Any) -> DataType | FieldType:
+        """Create a JSON datatype.
+
+        :param data: The data object
+        """
+        return Vector(shape=(len(data),))
+
+
+
 class JsonDataTypeFactory(DataTypeFactory):
     """A factory for JSON datatypes # noqa."""
 
@@ -161,3 +183,11 @@ class JsonDataTypeFactory(DataTypeFactory):
         if CFG.json_native:
             return FieldType(identifier='json')
         return json_serializer
+
+
+from pinnacle.ext import numpy
+
+
+register_module("pinnacle.ext.numpy.encoder")
+register_module("pinnacle.ext.torch.encoder")
+register_module("pinnacle.ext.pillow.encoder")