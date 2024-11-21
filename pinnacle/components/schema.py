superduper-io/superduper

Add NativeVector BaseDataType

superduper/components/schema.py
@@ -3,7 +3,7 @@ from functools import cached_property
 
 from superduper.base.leaf import Leaf
 from superduper.components.component import Component
-from superduper.components.datatype import _BaseDatatype, DataType
+from superduper.components.datatype import BaseDataType, DataType
 from superduper.misc.reference import parse_reference
 from superduper.misc.special_dicts import SuperDuperFlatEncode
 
@@ -54,7 +54,7 @@ class Schema(Component):
         super().__post_init__(db, artifacts)
 
         for k, v in self.fields.items():
-            if isinstance(v, (_BaseDatatype, FieldType)):
+            if isinstance(v, (BaseDataType, FieldType)):
                 continue
 
             try:
@@ -99,7 +99,7 @@ class Schema(Component):
         :param files: Files.
         """
         for k, field in self.fields.items():
-            if not isinstance(field, _BaseDatatype):
+            if not isinstance(field, BaseDataType):
                 continue
 
             if k not in out:

superduper/ext/numpy/encoder.py
@@ -2,7 +2,7 @@ import typing as t
 
 import numpy
 
-from superduper.components.datatype import _BaseDatatype, DataType, DataTypeFactory
+from superduper.components.datatype import BaseDataType, DataType, DataTypeFactory
 from superduper.ext.utils import str_shape
 from superduper.misc.annotations import component
 
@@ -47,7 +47,11 @@ class DecodeArray:
         return numpy.frombuffer(bytes, dtype=self.dtype).reshape(self.shape)
 
 
-class Array(_BaseDatatype):
+class Array(BaseDataType):
+    """Encode/ decode a numpy array as bytes.
+
+    :param dtype: numpy native datatype
+    """
     dtype: str = 'float64'
 
     def encode_data(self, item, info = None):

plugins/mongodb/superduper_mongodb/data_backend.py
@@ -41,7 +41,7 @@ class MongoDataBackend(BaseDataBackend):
         self._db = self.conn[self.name]
 
         self.datatype_presets = {
-            'vector': 'superduper.components.datatype.NativeDatatype'
+            'vector': 'superduper.components.datatype.NativeVector'
         }
 
     def reconnect(self):

test/unittest/component/datatype/test_vector.py
@@ -1,9 +1,12 @@
 import numpy
 
+from superduper.components.datatype import Vector
+
 
 def test_auto_detect_vector(db):
     db.cfg.auto_schema = True
     db['vectors'].insert([{'x': numpy.random.randn(7)} for _ in range(3)]).execute()
+
     assert 'vector[7]' in db.show('datatype')
 
     schema = next(iter(db.show('schema')))

Pinnacle-Networks/Pinnacle

pinnacle/components/schema.py
@@ -3,7 +3,7 @@ from functools import cached_property
 
 from pinnacle.base.leaf import Leaf
 from pinnacle.components.component import Component
-from pinnacle.components.datatype import _BaseDatatype, DataType
+from pinnacle.components.datatype import BaseDataType, DataType
 from pinnacle.misc.reference import parse_reference
 from pinnacle.misc.special_dicts import SuperDuperFlatEncode
 
@@ -54,7 +54,7 @@ class Schema(Component):
         super().__post_init__(db, artifacts)
 
         for k, v in self.fields.items():
-            if isinstance(v, (_BaseDatatype, FieldType)):
+            if isinstance(v, (BaseDataType, FieldType)):
                 continue
 
             try:
@@ -99,7 +99,7 @@ class Schema(Component):
         :param files: Files.
         """
         for k, field in self.fields.items():
-            if not isinstance(field, _BaseDatatype):
+            if not isinstance(field, BaseDataType):
                 continue
 
             if k not in out:

pinnacle/ext/numpy/encoder.py
@@ -2,7 +2,7 @@ import typing as t
 
 import numpy
 
-from pinnacle.components.datatype import _BaseDatatype, DataType, DataTypeFactory
+from pinnacle.components.datatype import BaseDataType, DataType, DataTypeFactory
 from pinnacle.ext.utils import str_shape
 from pinnacle.misc.annotations import component
 
@@ -47,7 +47,11 @@ class DecodeArray:
         return numpy.frombuffer(bytes, dtype=self.dtype).reshape(self.shape)
 
 
-class Array(_BaseDatatype):
+class Array(BaseDataType):
+    """Encode/ decode a numpy array as bytes.
+
+    :param dtype: numpy native datatype
+    """
     dtype: str = 'float64'
 
     def encode_data(self, item, info = None):

plugins/mongodb/superduper_mongodb/data_backend.py
@@ -41,7 +41,7 @@ class MongoDataBackend(BaseDataBackend):
         self._db = self.conn[self.name]
 
         self.datatype_presets = {
-            'vector': 'pinnacle.components.datatype.NativeDatatype'
+            'vector': 'pinnacle.components.datatype.NativeVector'
         }
 
     def reconnect(self):

test/unittest/component/datatype/test_vector.py
@@ -1,9 +1,12 @@
 import numpy
 
+from pinnacle.components.datatype import Vector
+
 
 def test_auto_detect_vector(db):
     db.cfg.auto_schema = True
     db['vectors'].insert([{'x': numpy.random.randn(7)} for _ in range(3)]).execute()
+
     assert 'vector[7]' in db.show('datatype')
 
     schema = next(iter(db.show('schema')))