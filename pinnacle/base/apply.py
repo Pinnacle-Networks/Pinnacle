superduper-io/superduper

Fix recursive non-breaking changes

superduper/base/apply.py
@@ -4,6 +4,7 @@ import click
 from rich.console import Console
 
 from superduper import Component, logging
+from superduper.backends.base.query import Query
 from superduper.base.document import Document
 from superduper.base.event import Create, Signal, Update
 from superduper.components.component import Status
@@ -55,6 +56,7 @@ def apply(
         context=object.uuid,
         job_events={},
         global_diff=diff,
+        non_breaking_changes={},
     )
     # this flags that the context is not needed anymore
     if not create_events:
@@ -123,6 +125,7 @@ def apply(
 def _apply(
     db: 'Datalayer',
     object: 'Component',
+    non_breaking_changes: t.Dict,
     context: str | None = None,
     job_events: t.Dict[str, 'Job'] | None = None,
     parent: t.Optional[str] = None,
@@ -164,6 +167,7 @@ def _apply(
             job_events=job_events,
             parent=object.uuid,
             global_diff=global_diff,
+            non_breaking_changes=non_breaking_changes,
         )
 
         job_events.update(j)
@@ -179,6 +183,29 @@ def _apply(
 
     serialized = serialized.map(wrapper, lambda x: isinstance(x, Component))
 
+    def replace_existing(x):
+        if isinstance(x, str):
+            for uuid in non_breaking_changes:
+                x = x.replace(uuid, non_breaking_changes[uuid])
+
+        elif isinstance(x, Query):
+            r = x.dict()
+            for uuid in non_breaking_changes:
+                r['query'] = r['query'].replace(uuid, non_breaking_changes[uuid])
+            for i, doc in enumerate(r['documents']):
+                replace = {}
+                for k in doc:
+                    replace_k = k
+                    for uuid in non_breaking_changes:
+                        replace_k = replace_k.replace(uuid, non_breaking_changes[uuid])
+                    replace[replace_k] = doc[k]
+                r['documents'][i] = replace
+            x = Document.decode(r).unpack()
+
+        else:
+            raise TypeError("Unexpected target of substitution in db.apply")
+        return x
+
     try:
         current = db.load(object.type_id, object.identifier)
 
@@ -186,6 +213,10 @@ def _apply(
         # also only
         current_serialized = current.dict(metadata=False, refs=True)
         del current_serialized['uuid']
+
+        serialized = serialized.map(
+            replace_existing, lambda x: isinstance(x, str) or isinstance(x, Query)
+        )
 
         # finds the fields where there is a difference
         this_diff = Document(current_serialized, schema=current_serialized.schema).diff(
@@ -196,7 +227,11 @@ def _apply(
         if not this_diff:
             # if no change then update the component
             # to have the same info as the "existing" version
+
+            non_breaking_changes[object.uuid] = current.uuid
+
             current.handle_update_or_same(object)
+
             return create_events, job_events
 
         elif set(this_diff.keys(deep=True)).intersection(object.breaks):
@@ -237,6 +272,15 @@ def _apply(
 
         else:
             apply_status = 'update'
+
+            # the non-breaking changes lookup table
+            # allows components which are downstream
+            # from this component via references
+            # (e.g. `Listener` instances which listen to these outputs)
+            # to understand if they are now referring to the "original"
+            # or the "new" version.
+            non_breaking_changes[object.uuid] = current.uuid
+
             current.handle_update_or_same(object)
 
             serialized['version'] = current.version

superduper/components/listener.py
@@ -93,6 +93,26 @@ class Listener(CDC):
                         f'dependency {deps[it]}'
                     )
 
+        if not self.upstream:
+            return
+
+        from collections import defaultdict
+
+        from superduper import Component
+
+        # This is to perform deduplication, in case an upstream
+        # listener has already been provided
+
+        huuids = [x.huuid if isinstance(x, Component) else x for x in self.upstream]
+        huuids = defaultdict(lambda: [])
+        for x in self.upstream:
+            if isinstance(x, Component):
+                huuids['&:component:' + x.huuid].append(x)
+            else:
+                huuids[x].append(x)
+
+        self.upstream = [x[0] for x in huuids.values()]
+
     @property
     def predict_id(self):
         """Predict ID property."""

test/integration/usecase/test_reapply.py
@@ -1,0 +1,50 @@
+from superduper import Model
+
+
+class MyModel(Model):
+    breaks = ('b',)
+    signature: str = 'singleton'
+    a: str
+    b: str
+
+    def predict(self, x):
+        return x + 1
+
+
+def test_reapply(db):
+    # this
+
+    db['docs'].insert([{'x': i} for i in range(10)]).execute()
+
+    def build(name, data):
+        model = MyModel('test', a=name, b=data)
+        listener_1 = model.to_listener(
+            key='x',
+            select=db['docs'].select(),
+            identifier='listener_1',
+        )
+        return model.to_listener(
+            key=listener_1.outputs,
+            select=db[listener_1.outputs].select(),
+            identifier='listener_2',
+            upstream=[listener_1],
+        )
+
+    listener_2 = build('first', '1')
+
+    db.apply(listener_2)
+
+    outputs = db[listener_2.outputs].select().tolist()
+
+    import pprint
+
+    pprint.pprint(outputs)
+
+    assert outputs[0][listener_2.outputs] == 2
+
+    listener_2 = build('second', '1')
+    db.apply(listener_2)
+
+    listener_2_update = build('second', '2')
+
+    db.apply(listener_2_update)

Pinnacle-Networks/Pinnacle

pinnacle/base/apply.py
@@ -4,6 +4,7 @@ import click
 from rich.console import Console
 
 from pinnacle import Component, logging
+from pinnacle.backends.base.query import Query
 from pinnacle.base.document import Document
 from pinnacle.base.event import Create, Signal, Update
 from pinnacle.components.component import Status
@@ -55,6 +56,7 @@ def apply(
         context=object.uuid,
         job_events={},
         global_diff=diff,
+        non_breaking_changes={},
     )
     # this flags that the context is not needed anymore
     if not create_events:
@@ -123,6 +125,7 @@ def apply(
 def _apply(
     db: 'Datalayer',
     object: 'Component',
+    non_breaking_changes: t.Dict,
     context: str | None = None,
     job_events: t.Dict[str, 'Job'] | None = None,
     parent: t.Optional[str] = None,
@@ -164,6 +167,7 @@ def _apply(
             job_events=job_events,
             parent=object.uuid,
             global_diff=global_diff,
+            non_breaking_changes=non_breaking_changes,
         )
 
         job_events.update(j)
@@ -179,6 +183,29 @@ def _apply(
 
     serialized = serialized.map(wrapper, lambda x: isinstance(x, Component))
 
+    def replace_existing(x):
+        if isinstance(x, str):
+            for uuid in non_breaking_changes:
+                x = x.replace(uuid, non_breaking_changes[uuid])
+
+        elif isinstance(x, Query):
+            r = x.dict()
+            for uuid in non_breaking_changes:
+                r['query'] = r['query'].replace(uuid, non_breaking_changes[uuid])
+            for i, doc in enumerate(r['documents']):
+                replace = {}
+                for k in doc:
+                    replace_k = k
+                    for uuid in non_breaking_changes:
+                        replace_k = replace_k.replace(uuid, non_breaking_changes[uuid])
+                    replace[replace_k] = doc[k]
+                r['documents'][i] = replace
+            x = Document.decode(r).unpack()
+
+        else:
+            raise TypeError("Unexpected target of substitution in db.apply")
+        return x
+
     try:
         current = db.load(object.type_id, object.identifier)
 
@@ -186,6 +213,10 @@ def _apply(
         # also only
         current_serialized = current.dict(metadata=False, refs=True)
         del current_serialized['uuid']
+
+        serialized = serialized.map(
+            replace_existing, lambda x: isinstance(x, str) or isinstance(x, Query)
+        )
 
         # finds the fields where there is a difference
         this_diff = Document(current_serialized, schema=current_serialized.schema).diff(
@@ -196,7 +227,11 @@ def _apply(
         if not this_diff:
             # if no change then update the component
             # to have the same info as the "existing" version
+
+            non_breaking_changes[object.uuid] = current.uuid
+
             current.handle_update_or_same(object)
+
             return create_events, job_events
 
         elif set(this_diff.keys(deep=True)).intersection(object.breaks):
@@ -237,6 +272,15 @@ def _apply(
 
         else:
             apply_status = 'update'
+
+            # the non-breaking changes lookup table
+            # allows components which are downstream
+            # from this component via references
+            # (e.g. `Listener` instances which listen to these outputs)
+            # to understand if they are now referring to the "original"
+            # or the "new" version.
+            non_breaking_changes[object.uuid] = current.uuid
+
             current.handle_update_or_same(object)
 
             serialized['version'] = current.version

pinnacle/components/listener.py
@@ -93,6 +93,26 @@ class Listener(CDC):
                         f'dependency {deps[it]}'
                     )
 
+        if not self.upstream:
+            return
+
+        from collections import defaultdict
+
+        from pinnacle import Component
+
+        # This is to perform deduplication, in case an upstream
+        # listener has already been provided
+
+        huuids = [x.huuid if isinstance(x, Component) else x for x in self.upstream]
+        huuids = defaultdict(lambda: [])
+        for x in self.upstream:
+            if isinstance(x, Component):
+                huuids['&:component:' + x.huuid].append(x)
+            else:
+                huuids[x].append(x)
+
+        self.upstream = [x[0] for x in huuids.values()]
+
     @property
     def predict_id(self):
         """Predict ID property."""

test/integration/usecase/test_reapply.py
@@ -1,0 +1,50 @@
+from pinnacle import Model
+
+
+class MyModel(Model):
+    breaks = ('b',)
+    signature: str = 'singleton'
+    a: str
+    b: str
+
+    def predict(self, x):
+        return x + 1
+
+
+def test_reapply(db):
+    db.cfg.auto_schema = True
+
+    db['docs'].insert([{'x': i} for i in range(10)]).execute()
+
+    def build(name, data):
+        model = MyModel('test', a=name, b=data)
+        listener_1 = model.to_listener(
+            key='x',
+            select=db['docs'].select(),
+            identifier='listener_1',
+        )
+        return model.to_listener(
+            key=listener_1.outputs,
+            select=db[listener_1.outputs].select(),
+            identifier='listener_2',
+            upstream=[listener_1],
+        )
+
+    listener_2 = build('first', '1')
+
+    db.apply(listener_2)
+
+    outputs = db[listener_2.outputs].select().tolist()
+
+    import pprint
+
+    pprint.pprint(outputs)
+
+    assert outputs[0][listener_2.outputs] == 2
+
+    listener_2 = build('second', '1')
+    db.apply(listener_2)
+
+    listener_2_update = build('second', '2')
+
+    db.apply(listener_2_update)