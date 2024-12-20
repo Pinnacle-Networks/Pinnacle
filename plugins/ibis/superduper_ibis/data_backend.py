superduper-io/superduper

Add native vector search in datapresets ibis

plugins/ibis/superduper_ibis/data_backend.py
@@ -100,6 +100,9 @@ class IbisDataBackend(BaseDataBackend):
 
         if uri.startswith('snowflake://'):
             self.bytes_encoding = 'base64'
+            self.datatype_presets = {
+                'vector': 'superduper.components.datatype.NativeVector'
+            }
 
     def _setup(self, conn):
         self.dialect = getattr(conn, "name", "base")

plugins/ibis/superduper_ibis/utils.py
@@ -1,8 +1,5 @@
 from ibis.expr.datatypes import dtype
-from superduper.components.datatype import (
-    BaseDataType,
-    File,
-)
+from superduper.components.datatype import BaseDataType, File, Vector
 from superduper.components.schema import ID, FieldType, Schema
 
 SPECIAL_ENCODABLES_FIELDS = {
@@ -39,6 +36,9 @@ def convert_schema_to_fields(schema: Schema):
                     if schema.db.databackend.bytes_encoding == 'base64'
                     else 'bytes'
                 )
+            elif isinstance(v, Vector):
+                fields[k] = dtype('json')
+
             elif v.encodable == 'native':
                 fields[k] = dtype(v.dtype)
 

plugins/snowflake/plugin_test/test_vector_search.py
@@ -2,7 +2,7 @@ import warnings
 
 import pytest
 from superduper import CFG, superduper
-from superduper.components.datatype import NativeVector
+from superduper.components.datatype import Vector
 from test.utils.usecase.vector_search import build_vector_index
 
 from superduper_snowflake.vector_search import SnowflakeVectorSearcher
@@ -21,10 +21,11 @@ except Exception as e:
 @pytest.mark.skipif(DO_SKIP, reason='Only snowflake deployments relevant.')
 def test_basic_snowflake_search():
     CFG.vector_search_engine = 'snowflake'
+    CFG.force_apply = True
 
     db = superduper()
 
-    d1 = NativeVector(identifier='vector[300]', shape=[300])
+    d1 = Vector(shape=[300])
     build_vector_index(db, n=10, list_embeddings=True, vector_datatype=d1, measure='l2')
 
     vector_index = "vector_index"

Pinnacle-Networks/Pinnacle

plugins/ibis/superduper_ibis/data_backend.py
@@ -100,6 +100,9 @@ class IbisDataBackend(BaseDataBackend):
 
         if uri.startswith('snowflake://'):
             self.bytes_encoding = 'base64'
+            self.datatype_presets = {
+                'vector': 'pinnacle.components.datatype.NativeVector'
+            }
 
     def _setup(self, conn):
         self.dialect = getattr(conn, "name", "base")

plugins/ibis/superduper_ibis/utils.py
@@ -1,8 +1,5 @@
 from ibis.expr.datatypes import dtype
-from pinnacle.components.datatype import (
-    BaseDataType,
-    File,
-)
+from pinnacle.components.datatype import BaseDataType, File, Vector
 from pinnacle.components.schema import ID, FieldType, Schema
 
 SPECIAL_ENCODABLES_FIELDS = {
@@ -39,6 +36,9 @@ def convert_schema_to_fields(schema: Schema):
                     if schema.db.databackend.bytes_encoding == 'base64'
                     else 'bytes'
                 )
+            elif isinstance(v, Vector):
+                fields[k] = dtype('json')
+
             elif v.encodable == 'native':
                 fields[k] = dtype(v.dtype)
 

plugins/snowflake/plugin_test/test_vector_search.py
@@ -2,7 +2,7 @@ import warnings
 
 import pytest
 from pinnacle import CFG, pinnacle
-from pinnacle.components.datatype import NativeVector
+from pinnacle.components.datatype import Vector
 from test.utils.usecase.vector_search import build_vector_index
 
 from pinnacle_snowflake.vector_search import SnowflakeVectorSearcher
@@ -21,10 +21,11 @@ except Exception as e:
 @pytest.mark.skipif(DO_SKIP, reason='Only snowflake deployments relevant.')
 def test_basic_snowflake_search():
     CFG.vector_search_engine = 'snowflake'
+    CFG.force_apply = True
 
     db = pinnacle()
 
-    d1 = NativeVector(identifier='vector[300]', shape=[300])
+    d1 = Vector(shape=[300])
     build_vector_index(db, n=10, list_embeddings=True, vector_datatype=d1, measure='l2')
 
     vector_index = "vector_index"