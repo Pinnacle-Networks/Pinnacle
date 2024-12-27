superduper-io/superduper

Add secrets reload withs safe retry

superduper/base/config.py
@@ -174,6 +174,7 @@ class Config(BaseConfig):
     envs: dc.InitVar[t.Optional[t.Dict[str, str]]] = None
 
     data_backend: str = "mongodb://localhost:27017/test_db"
+    secrets_volume: str = os.path.join(".superduper", "/session/secrets")
 
     lance_home: str = os.path.join(".superduper", "vector_indices")
 

superduper/misc/retry.py
@@ -1,3 +1,4 @@
+import time
 import dataclasses as dc
 import functools
 import typing as t
@@ -5,6 +6,8 @@ import typing as t
 import tenacity
 
 import superduper as s
+from superduper import logging
+from superduper.misc.files import load_secrets
 
 ExceptionTypes = t.Union[t.Type[BaseException], t.Tuple[t.Type[BaseException], ...]]
 
@@ -38,6 +41,41 @@ class Retry:
         )
         retrier = tenacity.retry(retry=retry, stop=stop, wait=wait)
         return retrier(f)
+
+
+def safe_retry(exception_to_check, retries=1, delay=1):
+    """
+    A decorator that retries a function if a specified exception is raised.
+
+    :param exception_to_check: The exception or tuple of exceptions to check.
+    :param retries: The maximum number of retries.
+    :param delay: Delay between retries in seconds.
+    :return: The result of the decorated function.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            attempt = 0
+            while attempt < retries:
+                try:
+                    load_secrets()
+                    return func(*args, **kwargs)
+                except exception_to_check as e:
+                    attempt += 1
+                    if attempt >= retries:
+                        logging.error(
+                            f"Function {func.__name__} failed after {retries} retries."
+                        )
+                        raise
+                    logging.warn(
+                        f"Retrying {func.__name__} due to {e}, attempt {attempt} of {retries}..."
+                    )
+                    time.sleep(delay)
+
+        return wrapper
+
+    return decorator
 
 
 def db_retry(connector='databackend'):

plugins/openai/superduper_openai/model.py
@@ -19,7 +19,8 @@ from superduper.backends.query_dataset import QueryDataset
 from superduper.base.datalayer import Datalayer
 from superduper.components.model import APIBaseModel, Inputs
 from superduper.misc.compat import cache
-from superduper.misc.retry import Retry
+from superduper.misc.retry import Retry, safe_retry
+from superduper.base import exceptions
 
 retry = Retry(
     exception_types=(
@@ -61,19 +62,23 @@ class _OpenAI(APIBaseModel):
             self.client_kwargs['base_url'] = self.openai_api_base
             self.client_kwargs['default_headers'] = self.openai_api_base
 
+    @safe_retry(exceptions.MissingSecretsException)
+    def init(self, db=None):
+        """Initialize the model."""
+        super().init()
+
         # dall-e is not currently included in list returned by OpenAI model endpoint
         if self.model not in (
             mo := _available_models(json.dumps(self.client_kwargs))
         ) and self.model not in ('dall-e'):
             msg = f'model {self.model} not in OpenAI available models, {mo}'
             raise ValueError(msg)
-
         self.syncClient = SyncOpenAI(**self.client_kwargs)
 
         if 'OPENAI_API_KEY' not in os.environ and (
             'api_key' not in self.client_kwargs.keys() and self.client_kwargs
         ):
-            raise ValueError(
+            raise exceptions.MissingSecretsException(
                 'OPENAI_API_KEY not available neither in environment vars '
                 'nor in `client_kwargs`'
             )

Pinnacle-Networks/Pinnacle

pinnacle/base/config.py
@@ -174,6 +174,7 @@ class Config(BaseConfig):
     envs: dc.InitVar[t.Optional[t.Dict[str, str]]] = None
 
     data_backend: str = "mongodb://localhost:27017/test_db"
+    secrets_volume: str = os.path.join(".pinnacle", "/session/secrets")
 
     lance_home: str = os.path.join(".pinnacle", "vector_indices")
 

pinnacle/misc/retry.py
@@ -1,3 +1,4 @@
+import time
 import dataclasses as dc
 import functools
 import typing as t
@@ -5,6 +6,8 @@ import typing as t
 import tenacity
 
 import pinnacle as s
+from pinnacle import logging
+from pinnacle.misc.files import load_secrets
 
 ExceptionTypes = t.Union[t.Type[BaseException], t.Tuple[t.Type[BaseException], ...]]
 
@@ -38,6 +41,41 @@ class Retry:
         )
         retrier = tenacity.retry(retry=retry, stop=stop, wait=wait)
         return retrier(f)
+
+
+def safe_retry(exception_to_check, retries=1, delay=1):
+    """
+    A decorator that retries a function if a specified exception is raised.
+
+    :param exception_to_check: The exception or tuple of exceptions to check.
+    :param retries: The maximum number of retries.
+    :param delay: Delay between retries in seconds.
+    :return: The result of the decorated function.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            attempt = 0
+            while attempt < retries:
+                try:
+                    load_secrets()
+                    return func(*args, **kwargs)
+                except exception_to_check as e:
+                    attempt += 1
+                    if attempt >= retries:
+                        logging.error(
+                            f"Function {func.__name__} failed after {retries} retries."
+                        )
+                        raise
+                    logging.warn(
+                        f"Retrying {func.__name__} due to {e}, attempt {attempt} of {retries}..."
+                    )
+                    time.sleep(delay)
+
+        return wrapper
+
+    return decorator
 
 
 def db_retry(connector='databackend'):

plugins/openai/superduper_openai/model.py
@@ -19,7 +19,8 @@ from pinnacle.backends.query_dataset import QueryDataset
 from pinnacle.base.datalayer import Datalayer
 from pinnacle.components.model import APIBaseModel, Inputs
 from pinnacle.misc.compat import cache
-from pinnacle.misc.retry import Retry
+from pinnacle.misc.retry import Retry, safe_retry
+from pinnacle.base import exceptions
 
 retry = Retry(
     exception_types=(
@@ -61,19 +62,23 @@ class _OpenAI(APIBaseModel):
             self.client_kwargs['base_url'] = self.openai_api_base
             self.client_kwargs['default_headers'] = self.openai_api_base
 
+    @safe_retry(exceptions.MissingSecretsException)
+    def init(self, db=None):
+        """Initialize the model."""
+        super().init()
+
         # dall-e is not currently included in list returned by OpenAI model endpoint
         if self.model not in (
             mo := _available_models(json.dumps(self.client_kwargs))
         ) and self.model not in ('dall-e'):
             msg = f'model {self.model} not in OpenAI available models, {mo}'
             raise ValueError(msg)
-
         self.syncClient = SyncOpenAI(**self.client_kwargs)
 
         if 'OPENAI_API_KEY' not in os.environ and (
             'api_key' not in self.client_kwargs.keys() and self.client_kwargs
         ):
-            raise ValueError(
+            raise exceptions.MissingSecretsException(
                 'OPENAI_API_KEY not available neither in environment vars '
                 'nor in `client_kwargs`'
             )