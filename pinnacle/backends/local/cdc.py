superduper-io/superduper

Refactor basebackends cleanup

superduper/backends/local/cdc.py
@@ -1,5 +1,10 @@
+import typing as t
+
 from superduper.backends.base.cdc import CDCBackend
 from superduper.components.cdc import CDC
+
+if t.TYPE_CHECKING:
+    from superduper import Component
 
 
 class LocalCDCBackend(CDCBackend):
@@ -44,7 +49,10 @@ class LocalCDCBackend(CDCBackend):
                 self.put(self.db.load(type_id=type_id, identifier=identifier))
             # TODO consider re-initialzing CDC jobs since potentially failure
 
-    def drop(self):
-        """Drop the CDC."""
+    def drop(self, component: t.Optional['Component'] = None):
+        """Drop the CDC.
+
+        :param component: Component to remove.
+        """
         self.triggers = set()
         self._trigger_uuid_mapping = {}

superduper/backends/local/compute.py
@@ -5,6 +5,9 @@ from superduper import logging
 from superduper.backends.base.compute import ComputeBackend
 from superduper.base.event import Job
 
+if t.TYPE_CHECKING:
+    from superduper import Component
+
 
 class LocalComputeBackend(ComputeBackend):
     """
@@ -101,9 +104,11 @@ class LocalComputeBackend(ComputeBackend):
         """Initialize the compute."""
         pass
 
-    def drop(self):
-        """Drop the compute."""
-        pass
+    def drop(self, component: t.Optional['Component'] = None):
+        """Drop the compute.
+
+        :param component: Component to remove.
+        """
 
     @property
     def tasks(self) -> t.Dict[str, t.Any]:

superduper/backends/local/crontab.py
@@ -1,9 +1,14 @@
+import typing as t
+
 from apscheduler.executors.pool import ThreadPoolExecutor
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from superduper import logging
 from superduper.backends.base.crontab import CrontabBackend
+
+if t.TYPE_CHECKING:
+    from superduper import Component
 
 
 class LocalCrontabBackend(CrontabBackend):
@@ -58,10 +63,23 @@ class LocalCrontabBackend(CrontabBackend):
         """List UUIDs of components."""
         return list(self._job_uuids)
 
-    def drop(self):
-        """Drop the crontab."""
-        for job_id in self._job_uuids:
-            self.scheduler.remove_job(job_id)
+    def drop_component(self, uuid: str):
+        """Drop the crontab.
+
+        :param uuid: Component uuid to remove.
+        """
+        self.scheduler.remove_job(uuid)
+
+    def drop(self, component: t.Optional['Component'] = None):
+        """Drop the crontab.
+
+        :param component: Component to remove.
+        """
+        if component:
+            self.scheduler.remove_job(component.uuid)
+        else:
+            for job_id in self._job_uuids:
+                self.scheduler.remove_job(job_id)
 
     def initialize(self):
         """Initialize the crontab."""

superduper/backends/local/vector_search.py
@@ -10,6 +10,9 @@ from superduper.backends.base.vector_search import (
     measures,
 )
 
+if t.TYPE_CHECKING:
+    from superduper import Component
+
 
 class LocalVectorSearchBackend(VectorSearchBackend):
     """Local vector search backend.
@@ -67,14 +70,17 @@ class LocalVectorSearchBackend(VectorSearchBackend):
     def __getitem__(self, identifier):
         return self._cache[identifier]
 
-    def drop(self, identifier=None):
-        """Drop the vector search."""
+    def drop(self, component: t.Optional['Component'] = None):
+        """Drop the CDC.
+
+        :param component: Component to remove.
+        """
         # TODO: drop actual vector search not the cache
-        if identifier is None:
+        if component is None:
             self._cache = {}
         else:
-            del self._cache[identifier]
-            del self._identifier_uuid_map[identifier]
+            del self._cache[component.identifier]
+            del self._identifier_uuid_map[component.identifier]
 
 
 class InMemoryVectorSearcher(BaseVectorSearcher):

superduper/cli/main.py
@@ -192,7 +192,7 @@ def drop(data: bool = False, force: bool = False, data_backend: str | None = Non
     :param force: Force the drop.
     """
     data_backend = data_backend or CFG.data_backend
-    db = superduper(data_backend)
+    db = superduper(data_backend, initialize_cluster=False)
     db.drop(force=force, data=data)
     db.disconnect()
 

Pinnacle-Networks/Pinnacle

pinnacle/backends/local/cdc.py
@@ -1,5 +1,10 @@
+import typing as t
+
 from pinnacle.backends.base.cdc import CDCBackend
 from pinnacle.components.cdc import CDC
+
+if t.TYPE_CHECKING:
+    from pinnacle import Component
 
 
 class LocalCDCBackend(CDCBackend):
@@ -44,7 +49,10 @@ class LocalCDCBackend(CDCBackend):
                 self.put(self.db.load(type_id=type_id, identifier=identifier))
             # TODO consider re-initialzing CDC jobs since potentially failure
 
-    def drop(self):
-        """Drop the CDC."""
+    def drop(self, component: t.Optional['Component'] = None):
+        """Drop the CDC.
+
+        :param component: Component to remove.
+        """
         self.triggers = set()
         self._trigger_uuid_mapping = {}

pinnacle/backends/local/compute.py
@@ -5,6 +5,9 @@ from pinnacle import logging
 from pinnacle.backends.base.compute import ComputeBackend
 from pinnacle.base.event import Job
 
+if t.TYPE_CHECKING:
+    from pinnacle import Component
+
 
 class LocalComputeBackend(ComputeBackend):
     """
@@ -101,9 +104,11 @@ class LocalComputeBackend(ComputeBackend):
         """Initialize the compute."""
         pass
 
-    def drop(self):
-        """Drop the compute."""
-        pass
+    def drop(self, component: t.Optional['Component'] = None):
+        """Drop the compute.
+
+        :param component: Component to remove.
+        """
 
     @property
     def tasks(self) -> t.Dict[str, t.Any]:

pinnacle/backends/local/crontab.py
@@ -1,9 +1,14 @@
+import typing as t
+
 from apscheduler.executors.pool import ThreadPoolExecutor
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from pinnacle import logging
 from pinnacle.backends.base.crontab import CrontabBackend
+
+if t.TYPE_CHECKING:
+    from pinnacle import Component
 
 
 class LocalCrontabBackend(CrontabBackend):
@@ -58,10 +63,23 @@ class LocalCrontabBackend(CrontabBackend):
         """List UUIDs of components."""
         return list(self._job_uuids)
 
-    def drop(self):
-        """Drop the crontab."""
-        for job_id in self._job_uuids:
-            self.scheduler.remove_job(job_id)
+    def drop_component(self, uuid: str):
+        """Drop the crontab.
+
+        :param uuid: Component uuid to remove.
+        """
+        self.scheduler.remove_job(uuid)
+
+    def drop(self, component: t.Optional['Component'] = None):
+        """Drop the crontab.
+
+        :param component: Component to remove.
+        """
+        if component:
+            self.scheduler.remove_job(component.uuid)
+        else:
+            for job_id in self._job_uuids:
+                self.scheduler.remove_job(job_id)
 
     def initialize(self):
         """Initialize the crontab."""

pinnacle/backends/local/vector_search.py
@@ -10,6 +10,9 @@ from pinnacle.backends.base.vector_search import (
     measures,
 )
 
+if t.TYPE_CHECKING:
+    from pinnacle import Component
+
 
 class LocalVectorSearchBackend(VectorSearchBackend):
     """Local vector search backend.
@@ -67,14 +70,17 @@ class LocalVectorSearchBackend(VectorSearchBackend):
     def __getitem__(self, identifier):
         return self._cache[identifier]
 
-    def drop(self, identifier=None):
-        """Drop the vector search."""
+    def drop(self, component: t.Optional['Component'] = None):
+        """Drop the CDC.
+
+        :param component: Component to remove.
+        """
         # TODO: drop actual vector search not the cache
-        if identifier is None:
+        if component is None:
             self._cache = {}
         else:
-            del self._cache[identifier]
-            del self._identifier_uuid_map[identifier]
+            del self._cache[component.identifier]
+            del self._identifier_uuid_map[component.identifier]
 
 
 class InMemoryVectorSearcher(BaseVectorSearcher):

pinnacle/cli/main.py
@@ -192,7 +192,7 @@ def drop(data: bool = False, force: bool = False, data_backend: str | None = Non
     :param force: Force the drop.
     """
     data_backend = data_backend or CFG.data_backend
-    db = pinnacle(data_backend)
+    db = pinnacle(data_backend, initialize_cluster=False)
     db.drop(force=force, data=data)
     db.disconnect()
 