superduper-io/superduper

Use annotations to declare schema

superduper/base/document.py
@@ -12,7 +12,7 @@ from superduper.base.constant import (
 from superduper.base.leaf import Leaf, import_item
 from superduper.base.variables import _replace_variables
 from superduper.components.component import Component
-from superduper.components.datatype import BaseDataType, Blob, File
+from superduper.components.datatype import BaseDataType, Blob, FileItem
 from superduper.components.schema import Schema, get_schema
 from superduper.misc.reference import parse_reference
 from superduper.misc.special_dicts import MongoStyleDict, SuperDuperFlatEncode
@@ -294,7 +294,7 @@ class Document(MongoStyleDict):
             )
 
         def my_getter(x):
-            return File(path=r[KEY_FILES].get(x.split(':')[-1]), db=db)
+            return FileItem(path=r[KEY_FILES].get(x.split(':')[-1]), db=db)
 
         if r.get(KEY_FILES):
             getters.add_getter('file', my_getter)
@@ -521,7 +521,7 @@ def _deep_flat_encode(
         blobs[r.identifier] = r.bytes
         return '&:blob:' + r.identifier
 
-    if isinstance(r, File):
+    if isinstance(r, FileItem):
         files[r.identifier] = r.path
         return '&:file:' + r.identifier
 
@@ -717,7 +717,7 @@ def _get_component(db, path):
 
 def _get_file_callback(db):
     def callback(ref):
-        return File(identifier=ref, db=db)
+        return FileItem(identifier=ref, db=db)
 
     return callback
 

superduper/components/component.py
@@ -152,6 +152,17 @@ class ComponentMeta(LeafMeta):
                     new_cls._fields[field.name] = 'default'
                 elif annotation is t.Callable or _is_optional_callable(annotation):
                     new_cls._fields[field.name] = 'default'
+                # a hack...
+                elif 'superduper.misc.typing' in str(annotation):
+                    annotation = str(annotation)
+                    import re
+
+                    match1 = re.match('^typing\.Optional\[(.*)\]$', annotation)
+                    match2 = re.match('^t\.Optional\[(.*)\]$', annotation)
+                    match = match1 or match2
+                    if match:
+                        annotation = match.groups()[0]
+                    new_cls._fields[field.name] = annotation.split('.')[-1]
             except KeyError:
                 continue
         return new_cls

superduper/components/datatype.py
@@ -13,10 +13,12 @@ import numpy
 
 from superduper import CFG
 from superduper.base.leaf import Leaf
-from superduper.components.component import Component
+from superduper.components.component import Component, ComponentMeta
 
 Decode = t.Callable[[bytes], t.Any]
 Encode = t.Callable[[t.Any], bytes]
+
+INBUILT_DATATYPES = {}
 
 
 class DataTypeFactory:
@@ -43,7 +45,21 @@ class DataTypeFactory:
         raise NotImplementedError
 
 
-class BaseDataType(Component):
+class DataTypeMeta(ComponentMeta):
+    """Metaclass for the `Model` class and descendants # noqa."""
+
+    def __new__(mcls, name, bases, dct):
+        """Create a new class with merged docstrings # noqa."""
+        cls = super().__new__(mcls, name, bases, dct)
+        try:
+            instance = cls(cls.__name__)
+            INBUILT_DATATYPES[cls.__name__] = instance
+        except TypeError:
+            pass
+        return cls
+
+
+class BaseDataType(Component, metaclass=DataTypeMeta):
     """Base class for datatype."""
 
     type_id: t.ClassVar[str] = 'datatype'
@@ -173,7 +189,7 @@ class _PickleMixin:
         return pickle.loads(item)
 
 
-class PickleSerializer(_Artifact, _PickleMixin, BaseDataType):
+class Pickle(_Artifact, _PickleMixin, BaseDataType):
     """Serializer with pickle."""
 
 
@@ -189,7 +205,7 @@ class _DillMixin:
         return dill.loads(item)
 
 
-class DillSerializer(_Artifact, _DillMixin, BaseDataType):
+class Dill(_Artifact, _DillMixin, BaseDataType):
     """Serializer with dill.
 
     This is also the default serializer.
@@ -197,18 +213,20 @@ class DillSerializer(_Artifact, _DillMixin, BaseDataType):
     """
 
 
-class _DillEncoder(_Encodable, _DillMixin, BaseDataType):
+class DillEncoder(_Encodable, _DillMixin, BaseDataType):
+    """Encoder with dill."""
+
     ...
 
 
-class FileType(BaseDataType):
+class File(BaseDataType):
     """Type for encoding files on disk."""
 
     encodable: t.ClassVar[str] = 'file'
 
     def encode_data(self, item):
         assert os.path.exists(item)
-        return File(path=item)
+        return FileItem(path=item)
 
     def decode_data(self, item):
         return item
@@ -247,7 +265,7 @@ class Saveable(Leaf):
         pass
 
 
-class File(Saveable):
+class FileItem(Saveable):
     """Placeholder for a file.
 
     :param path: Path to file.
@@ -306,25 +324,27 @@ class Blob(Saveable):
 
 json_encoder = JSON('json')
 pickle_encoder = PickleEncoder('pickle_encoder')
-pickle_serializer = PickleSerializer('pickle_serializer')
-dill_encoder = _DillEncoder('dill_encoder')
-dill_serializer = DillSerializer('dill_serializer')
-file = FileType('file')
-
-DEFAULT_ENCODER = PickleEncoder('default_encoder')
-DEFAULT_SERIALIZER = DillSerializer('default')
+pickle_serializer = Pickle('pickle_serializer')
+dill_encoder = DillEncoder('dill_encoder')
+dill_serializer = Dill('dill_serializer')
+file = File('file')
 
 
-INBUILT_DATATYPES = {
-    dt.identifier: dt
-    for dt in [
-        json_encoder,
-        pickle_encoder,
-        pickle_serializer,
-        dill_encoder,
-        dill_serializer,
-        file,
-        DEFAULT_SERIALIZER,
-        DEFAULT_ENCODER,
-    ]
-}
+INBUILT_DATATYPES.update(
+    {
+        dt.identifier: dt
+        for dt in [
+            json_encoder,
+            pickle_encoder,
+            pickle_serializer,
+            dill_encoder,
+            dill_serializer,
+            file,
+        ]
+    }
+)
+
+DEFAULT_ENCODER = INBUILT_DATATYPES['PickleEncoder']
+DEFAULT_SERIALIZER = INBUILT_DATATYPES['Dill']
+INBUILT_DATATYPES['default'] = DEFAULT_SERIALIZER
+INBUILT_DATATYPES['Blob'] = INBUILT_DATATYPES['Pickle']

superduper/components/plugin.py
@@ -6,7 +6,7 @@ import sys
 import typing as t
 
 from superduper import Component, logging
-from superduper.components.datatype import File, file
+from superduper.components.datatype import FileItem, file
 
 
 class Plugin(Component):
@@ -24,7 +24,7 @@ class Plugin(Component):
     cache_path: str = "~/.superduper/plugins"
 
     def __post_init__(self, db):
-        if isinstance(self.path, File):
+        if isinstance(self.path, FileItem):
             self._prepare_plugin()
         else:
             path_name = os.path.basename(self.path.rstrip("/"))
@@ -92,7 +92,7 @@ class Plugin(Component):
 
     def _prepare_plugin(self):
         plugin_name_tag = f"{self.identifier}"
-        assert isinstance(self.path, File)
+        assert isinstance(self.path, FileItem)
         cache_path = os.path.expanduser(self.cache_path)
         uuid_path = os.path.join(cache_path, self.uuid)
         # Check if plugin is already in cache

superduper/misc/typing.py
@@ -1,0 +1,9 @@
+import typing as t
+
+from superduper.components.datatype import *
+
+File = t.NewType('File', t.AnyStr)
+Blob = t.NewType('Blob', t.Callable)
+Dill = t.NewType('Dill', t.Callable)
+Pickle = t.NewType('Pickle', t.Callable)
+JSON = t.NewType('JSON', t.Dict)

Pinnacle-Networks/Pinnacle

pinnacle/base/document.py
@@ -12,7 +12,7 @@ from pinnacle.base.constant import (
 from pinnacle.base.leaf import Leaf, import_item
 from pinnacle.base.variables import _replace_variables
 from pinnacle.components.component import Component
-from pinnacle.components.datatype import BaseDataType, Blob, File
+from pinnacle.components.datatype import BaseDataType, Blob, FileItem
 from pinnacle.components.schema import Schema, get_schema
 from pinnacle.misc.reference import parse_reference
 from pinnacle.misc.special_dicts import MongoStyleDict, SuperDuperFlatEncode
@@ -300,7 +300,7 @@ class Document(MongoStyleDict):
             )
 
         def my_getter(x):
-            return File(path=r[KEY_FILES].get(x.split(':')[-1]), db=db)
+            return FileItem(path=r[KEY_FILES].get(x.split(':')[-1]), db=db)
 
         if r.get(KEY_FILES):
             getters.add_getter('file', my_getter)
@@ -527,7 +527,7 @@ def _deep_flat_encode(
         blobs[r.identifier] = r.bytes
         return '&:blob:' + r.identifier
 
-    if isinstance(r, File):
+    if isinstance(r, FileItem):
         files[r.identifier] = r.path
         return '&:file:' + r.identifier
 
@@ -723,7 +723,7 @@ def _get_component(db, path):
 
 def _get_file_callback(db):
     def callback(ref):
-        return File(identifier=ref, db=db)
+        return FileItem(identifier=ref, db=db)
 
     return callback
 

pinnacle/components/component.py
@@ -153,6 +153,17 @@ class ComponentMeta(LeafMeta):
                     new_cls._fields[field.name] = 'default'
                 elif annotation is t.Callable or _is_optional_callable(annotation):
                     new_cls._fields[field.name] = 'default'
+                # a hack...
+                elif 'pinnacle.misc.typing' in str(annotation):
+                    annotation = str(annotation)
+                    import re
+
+                    match1 = re.match('^typing\.Optional\[(.*)\]$', annotation)
+                    match2 = re.match('^t\.Optional\[(.*)\]$', annotation)
+                    match = match1 or match2
+                    if match:
+                        annotation = match.groups()[0]
+                    new_cls._fields[field.name] = annotation.split('.')[-1]
             except KeyError:
                 continue
         return new_cls

pinnacle/components/datatype.py
@@ -13,10 +13,12 @@ import numpy
 
 from pinnacle import CFG
 from pinnacle.base.leaf import Leaf
-from pinnacle.components.component import Component
+from pinnacle.components.component import Component, ComponentMeta
 
 Decode = t.Callable[[bytes], t.Any]
 Encode = t.Callable[[t.Any], bytes]
+
+INBUILT_DATATYPES = {}
 
 
 class DataTypeFactory:
@@ -43,7 +45,21 @@ class DataTypeFactory:
         raise NotImplementedError
 
 
-class BaseDataType(Component):
+class DataTypeMeta(ComponentMeta):
+    """Metaclass for the `Model` class and descendants # noqa."""
+
+    def __new__(mcls, name, bases, dct):
+        """Create a new class with merged docstrings # noqa."""
+        cls = super().__new__(mcls, name, bases, dct)
+        try:
+            instance = cls(cls.__name__)
+            INBUILT_DATATYPES[cls.__name__] = instance
+        except TypeError:
+            pass
+        return cls
+
+
+class BaseDataType(Component, metaclass=DataTypeMeta):
     """Base class for datatype."""
 
     type_id: t.ClassVar[str] = 'datatype'
@@ -180,7 +196,7 @@ class _PickleMixin:
         return pickle.loads(item)
 
 
-class PickleSerializer(_Artifact, _PickleMixin, BaseDataType):
+class Pickle(_Artifact, _PickleMixin, BaseDataType):
     """Serializer with pickle."""
 
 
@@ -196,7 +212,7 @@ class _DillMixin:
         return dill.loads(item)
 
 
-class DillSerializer(_Artifact, _DillMixin, BaseDataType):
+class Dill(_Artifact, _DillMixin, BaseDataType):
     """Serializer with dill.
 
     This is also the default serializer.
@@ -204,18 +220,20 @@ class DillSerializer(_Artifact, _DillMixin, BaseDataType):
     """
 
 
-class _DillEncoder(_Encodable, _DillMixin, BaseDataType):
+class DillEncoder(_Encodable, _DillMixin, BaseDataType):
+    """Encoder with dill."""
+
     ...
 
 
-class FileType(BaseDataType):
+class File(BaseDataType):
     """Type for encoding files on disk."""
 
     encodable: t.ClassVar[str] = 'file'
 
     def encode_data(self, item):
         assert os.path.exists(item)
-        return File(path=item)
+        return FileItem(path=item)
 
     def decode_data(self, item):
         return item
@@ -254,7 +272,7 @@ class Saveable(Leaf):
         pass
 
 
-class File(Saveable):
+class FileItem(Saveable):
     """Placeholder for a file.
 
     :param path: Path to file.
@@ -313,25 +331,27 @@ class Blob(Saveable):
 
 json_encoder = JSON('json')
 pickle_encoder = PickleEncoder('pickle_encoder')
-pickle_serializer = PickleSerializer('pickle_serializer')
-dill_encoder = _DillEncoder('dill_encoder')
-dill_serializer = DillSerializer('dill_serializer')
-file = FileType('file')
-
-DEFAULT_ENCODER = PickleEncoder('default_encoder')
-DEFAULT_SERIALIZER = DillSerializer('default')
+pickle_serializer = Pickle('pickle_serializer')
+dill_encoder = DillEncoder('dill_encoder')
+dill_serializer = Dill('dill_serializer')
+file = File('file')
 
 
-INBUILT_DATATYPES = {
-    dt.identifier: dt
-    for dt in [
-        json_encoder,
-        pickle_encoder,
-        pickle_serializer,
-        dill_encoder,
-        dill_serializer,
-        file,
-        DEFAULT_SERIALIZER,
-        DEFAULT_ENCODER,
-    ]
-}
+INBUILT_DATATYPES.update(
+    {
+        dt.identifier: dt
+        for dt in [
+            json_encoder,
+            pickle_encoder,
+            pickle_serializer,
+            dill_encoder,
+            dill_serializer,
+            file,
+        ]
+    }
+)
+
+DEFAULT_ENCODER = INBUILT_DATATYPES['PickleEncoder']
+DEFAULT_SERIALIZER = INBUILT_DATATYPES['Dill']
+INBUILT_DATATYPES['default'] = DEFAULT_SERIALIZER
+INBUILT_DATATYPES['Blob'] = INBUILT_DATATYPES['Pickle']

pinnacle/components/plugin.py
@@ -6,7 +6,7 @@ import sys
 import typing as t
 
 from pinnacle import Component, logging
-from pinnacle.components.datatype import File, file
+from pinnacle.components.datatype import FileItem, file
 
 
 class Plugin(Component):
@@ -24,7 +24,7 @@ class Plugin(Component):
     cache_path: str = "~/.pinnacle/plugins"
 
     def __post_init__(self, db):
-        if isinstance(self.path, File):
+        if isinstance(self.path, FileItem):
             self._prepare_plugin()
         else:
             path_name = os.path.basename(self.path.rstrip("/"))
@@ -92,7 +92,7 @@ class Plugin(Component):
 
     def _prepare_plugin(self):
         plugin_name_tag = f"{self.identifier}"
-        assert isinstance(self.path, File)
+        assert isinstance(self.path, FileItem)
         cache_path = os.path.expanduser(self.cache_path)
         uuid_path = os.path.join(cache_path, self.uuid)
         # Check if plugin is already in cache

pinnacle/misc/typing.py
@@ -1,0 +1,9 @@
+import typing as t
+
+from pinnacle.components.datatype import *
+
+File = t.NewType('File', t.AnyStr)
+Blob = t.NewType('Blob', t.Callable)
+Dill = t.NewType('Dill', t.Callable)
+Pickle = t.NewType('Pickle', t.Callable)
+JSON = t.NewType('JSON', t.Dict)