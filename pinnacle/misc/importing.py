blythed/superduper

Cleanup dead code

superduper/misc/importing.py
@@ -1,5 +1,7 @@
 import importlib
 
+from superduper import logging
+
 
 def import_object(path):
     """Import item from path.
@@ -9,3 +11,15 @@ def import_object(path):
     module = '.'.join(path.split('.')[:-1])
     cls = path.split('.')[-1]
     return getattr(importlib.import_module(module), cls)
+
+
+def load_plugin(name: str):
+    """Load a plugin by name.
+
+    :param name: The name of the plugin to load.
+    """
+    if name == 'local':
+        return importlib.import_module('superduper.backends.local')
+    logging.info(f"Loading plugin: {name}")
+    plugin = importlib.import_module(f'superduper_{name}')
+    return plugin

test/unittest/ext/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from superduper.ext.utils import format_prompt, get_key
+from superduper.misc.utils import format_prompt, get_key
 
 
 def test_patch_environment_variable(monkeypatch):

Pinnacle-Networks/Pinnacle

pinnacle/misc/importing.py
@@ -1,5 +1,7 @@
 import importlib
 
+from pinnacle import logging
+
 
 def import_object(path):
     """Import item from path.
@@ -9,3 +11,15 @@ def import_object(path):
     module = '.'.join(path.split('.')[:-1])
     cls = path.split('.')[-1]
     return getattr(importlib.import_module(module), cls)
+
+
+def load_plugin(name: str):
+    """Load a plugin by name.
+
+    :param name: The name of the plugin to load.
+    """
+    if name == 'local':
+        return importlib.import_module('pinnacle.backends.local')
+    logging.info(f"Loading plugin: {name}")
+    plugin = importlib.import_module(f'pinnacle_{name}')
+    return plugin

test/unittest/ext/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pinnacle.ext.utils import format_prompt, get_key
+from pinnacle.misc.utils import format_prompt, get_key
 
 
 def test_patch_environment_variable(monkeypatch):