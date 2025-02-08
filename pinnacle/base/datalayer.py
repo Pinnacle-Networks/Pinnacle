superduper-io/superduper

Refactor encoding based on schemas

superduper/base/datalayer.py
@@ -17,9 +17,9 @@ from superduper.base.config import Config
 from superduper.base.cursor import SuperDuperCursor
 from superduper.base.document import Document
 from superduper.components.component import Component
+from superduper.components.datatype import LeafType
 from superduper.components.schema import Schema
 from superduper.components.table import Table
-from superduper.misc.importing import import_object
 
 DBResult = t.Any
 TaskGraph = t.Any
@@ -469,21 +469,15 @@ class Datalayer:
                 info = self.metadata.get_component_by_uuid(
                     uuid=uuid, allow_hidden=allow_hidden
                 )
-                try:
-                    class_schema = import_object(info['_path']).build_class_schema()
-                except (KeyError, AttributeError):
-                    # if defined in __main__ then the class is directly serialized
-                    assert '_object' in info
-                    from superduper.components.datatype import DEFAULT_SERIALIZER, Blob
+                # to prevent deserialization propagating back to the cache
+                builds = {k: v for k, v in info.get('_builds', {}).items()}
+                for k in builds:
+                    builds[k]['identifier'] = k.split(':')[-1]
 
-                    bytes_ = Blob(
-                        identifier=info['_object'].split(':')[-1], db=self
-                    ).unpack()
-                    object = DEFAULT_SERIALIZER.decode_data(bytes_)
-                    class_schema = object.build_class_schema()
-
-                c = Document.decode(info, db=self, schema=class_schema)
-                c.db = self
+                c = LeafType('leaf_type', db=self).decode_data(
+                    {k: v for k, v in info.items() if k != '_builds'},
+                    builds=builds,
+                )
                 if c.cache:
                     logging.info(f'Adding {c.huuid} to cache')
                     self.cluster.cache.put(c)
@@ -506,9 +500,9 @@ class Datalayer:
                     identifier=identifier,
                     allow_hidden=allow_hidden,
                 )
-                c = Document.decode(info, db=self)
-                c.db = self
-
+                c = LeafType('leaf_type', db=self).decode_data(
+                    info, builds=info.get('_builds', {})
+                )
                 if c.cache:
                     logging.info(f'Adding {c.huuid} to cache')
                     self.cluster.cache.put(c)
@@ -735,7 +729,7 @@ class Datalayer:
         # TODO have a slightly more user-friendly schema
         from superduper.misc.auto_schema import infer_schema
 
-        return infer_schema(data, identifier=identifier)
+        return infer_schema(data, identifier=identifier, db=self)
 
     @property
     def cfg(self) -> Config:

superduper/components/model.py
@@ -193,6 +193,14 @@ class Trainer(Component):
     :param validation: Validation object to measure training performance
     """
 
+    _fields = {
+        'transform': 'default',
+        'select': 'leaf',
+        'metric_values': 'json',
+        'compute_kwargs': 'json',
+        'validation': 'component',
+    }
+
     type_id: t.ClassVar[str] = 'trainer'
     key: ModelInputType
     select: Query
@@ -231,6 +239,8 @@ class Validation(Component):
     :param key: Model input type key
     :param datasets: Sequence of dataset.
     """
+
+    _fields = {'datasets': 'slist', 'metrics': 'slist'}
 
     type_id: t.ClassVar[str] = 'validation'
     metrics: t.Sequence[Metric] = ()
@@ -296,7 +306,13 @@ class Mapping:
                 kwargs[v] = r[k]
         except KeyError:
             raise KeyError(f'Key `{k}` not found in document {r}.')
-        args = Document({'_base': args}).unpack()
+
+        args = list(args)
+        for i, arg in enumerate(args):
+            if isinstance(arg, Leaf):
+                args[i] = arg.unpack()
+        args = tuple(args)
+
         kwargs = Document(kwargs).unpack()
         if self.signature == '**kwargs':
             return kwargs
@@ -387,6 +403,17 @@ class Model(Component, metaclass=ModelMeta):
     :param example: An example to auto-determine the schema/ datatype.
     :param deploy: Creates a standalone class instance on compute cluster.
     """
+
+    _fields = {
+        'datatype': 'leaf',
+        'output_schema': 'component',
+        'model_update_kwargs': 'json',
+        'predict_kwargs': 'json',
+        'compute_kwargs': 'json',
+        'validation': 'component',
+        'metric_values': 'json',
+        'trainer': 'component',
+    }
 
     breaks: t.ClassVar[t.Sequence] = ('trainer',)
     type_id: t.ClassVar[str] = 'model'
@@ -1067,6 +1094,7 @@ class ImportedModel(Model):
 
     """
 
+    _fields = {'object': 'leaf'}
     breaks: t.ClassVar[t.Sequence] = ('object', 'trainer')
     object: Leaf
     method: t.Optional[str] = None
@@ -1447,7 +1475,9 @@ class RAGModel(Model):
 
         :param query: Query string.
         """
-        assert self.db, 'db cannot be None'
+        from superduper.base.datalayer import Datalayer
+
+        assert isinstance(self.db, Datalayer)
         select = self.select.set_variables(db=self.db, query=query)
         results = [r.unpack() for r in select.execute()]
         prompt = self._build_prompt(query, results)

superduper/components/template.py
@@ -7,10 +7,9 @@ from superduper.base.constant import KEY_BLOBS, KEY_FILES
 from superduper.base.datalayer import Datalayer
 from superduper.base.document import Document, QueryUpdateDocument
 from superduper.base.leaf import Leaf
-from superduper.base.variables import _replace_variables
+from superduper.base.variables import _find_variables, _replace_variables
 from superduper.components.component import Component, _build_info_from_path
 from superduper.components.table import Table
-from superduper.misc.special_dicts import SuperDuperFlatEncode
 
 from .component import ensure_initialized
 
@@ -46,7 +45,6 @@ class _BaseTemplate(Component):
         """Post initialization method."""
         if isinstance(self.template, Leaf):
             self.template = self.template.encode(defaults=True, metadata=False)
-        self.template = SuperDuperFlatEncode(self.template)
         if self.substitutions is not None:
             self.substitutions = {
                 self.db.databackend.backend_name: 'databackend',
@@ -58,7 +56,7 @@ class _BaseTemplate(Component):
                 **self.substitutions
             )
         if self.template_variables is None:
-            self.template_variables = self.template.variables
+            self.template_variables = sorted(list(set(_find_variables(self.template))))
 
         super().postinit()
 
@@ -71,7 +69,10 @@ class _BaseTemplate(Component):
             set(self.template_variables) - {'output_prefix', 'databackend'}
         )
         kwargs['output_prefix'] = CFG.output_prefix
-        kwargs['databackend'] = self.db.databackend.backend_name
+        try:
+            kwargs['databackend'] = self.db.databackend.backend_name
+        except AttributeError:
+            pass
         component = _replace_variables(
             self.template,
             **kwargs,

superduper/misc/typing.py
@@ -1,9 +1,13 @@
 import typing as t
 
+from superduper import Component as C
 from superduper.components.datatype import *
 
 File = t.NewType('File', t.AnyStr)
 Blob = t.NewType('Blob', t.Callable)
+Artifact = t.NewType('Artifact', t.Callable)
 Dill = t.NewType('Dill', t.Callable)
 Pickle = t.NewType('Pickle', t.Callable)
-JSON = t.NewType('JSON', t.Dict)
+SDict = t.NewType('SDict', t.Dict)
+SList = t.NewType('SList', t.List)
+Component = t.NewType('Component', C)

Pinnacle-Networks/Pinnacle

pinnacle/base/datalayer.py
@@ -17,9 +17,9 @@ from pinnacle.base.config import Config
 from pinnacle.base.cursor import SuperDuperCursor
 from pinnacle.base.document import Document
 from pinnacle.components.component import Component
+from pinnacle.components.datatype import LeafType
 from pinnacle.components.schema import Schema
 from pinnacle.components.table import Table
-from pinnacle.misc.importing import import_object
 
 DBResult = t.Any
 TaskGraph = t.Any
@@ -469,21 +469,15 @@ class Datalayer:
                 info = self.metadata.get_component_by_uuid(
                     uuid=uuid, allow_hidden=allow_hidden
                 )
-                try:
-                    class_schema = import_object(info['_path']).build_class_schema()
-                except (KeyError, AttributeError):
-                    # if defined in __main__ then the class is directly serialized
-                    assert '_object' in info
-                    from pinnacle.components.datatype import DEFAULT_SERIALIZER, Blob
+                # to prevent deserialization propagating back to the cache
+                builds = {k: v for k, v in info.get('_builds', {}).items()}
+                for k in builds:
+                    builds[k]['identifier'] = k.split(':')[-1]
 
-                    bytes_ = Blob(
-                        identifier=info['_object'].split(':')[-1], db=self
-                    ).unpack()
-                    object = DEFAULT_SERIALIZER.decode_data(bytes_)
-                    class_schema = object.build_class_schema()
-
-                c = Document.decode(info, db=self, schema=class_schema)
-                c.db = self
+                c = LeafType('leaf_type', db=self).decode_data(
+                    {k: v for k, v in info.items() if k != '_builds'},
+                    builds=builds,
+                )
                 if c.cache:
                     logging.info(f'Adding {c.huuid} to cache')
                     self.cluster.cache.put(c)
@@ -506,9 +500,9 @@ class Datalayer:
                     identifier=identifier,
                     allow_hidden=allow_hidden,
                 )
-                c = Document.decode(info, db=self)
-                c.db = self
-
+                c = LeafType('leaf_type', db=self).decode_data(
+                    info, builds=info.get('_builds', {})
+                )
                 if c.cache:
                     logging.info(f'Adding {c.huuid} to cache')
                     self.cluster.cache.put(c)
@@ -735,7 +729,7 @@ class Datalayer:
         # TODO have a slightly more user-friendly schema
         from pinnacle.misc.auto_schema import infer_schema
 
-        return infer_schema(data, identifier=identifier)
+        return infer_schema(data, identifier=identifier, db=self)
 
     @property
     def cfg(self) -> Config:

pinnacle/components/model.py
@@ -193,6 +193,14 @@ class Trainer(Component):
     :param validation: Validation object to measure training performance
     """
 
+    _fields = {
+        'transform': 'default',
+        'select': 'leaf',
+        'metric_values': 'json',
+        'compute_kwargs': 'json',
+        'validation': 'component',
+    }
+
     type_id: t.ClassVar[str] = 'trainer'
     key: ModelInputType
     select: Query
@@ -231,6 +239,8 @@ class Validation(Component):
     :param key: Model input type key
     :param datasets: Sequence of dataset.
     """
+
+    _fields = {'datasets': 'slist', 'metrics': 'slist'}
 
     type_id: t.ClassVar[str] = 'validation'
     metrics: t.Sequence[Metric] = ()
@@ -296,7 +306,13 @@ class Mapping:
                 kwargs[v] = r[k]
         except KeyError:
             raise KeyError(f'Key `{k}` not found in document {r}.')
-        args = Document({'_base': args}).unpack()
+
+        args = list(args)
+        for i, arg in enumerate(args):
+            if isinstance(arg, Leaf):
+                args[i] = arg.unpack()
+        args = tuple(args)
+
         kwargs = Document(kwargs).unpack()
         if self.signature == '**kwargs':
             return kwargs
@@ -387,6 +403,17 @@ class Model(Component, metaclass=ModelMeta):
     :param example: An example to auto-determine the schema/ datatype.
     :param deploy: Creates a standalone class instance on compute cluster.
     """
+
+    _fields = {
+        'datatype': 'leaf',
+        'output_schema': 'component',
+        'model_update_kwargs': 'json',
+        'predict_kwargs': 'json',
+        'compute_kwargs': 'json',
+        'validation': 'component',
+        'metric_values': 'json',
+        'trainer': 'component',
+    }
 
     breaks: t.ClassVar[t.Sequence] = ('trainer',)
     type_id: t.ClassVar[str] = 'model'
@@ -1067,6 +1094,7 @@ class ImportedModel(Model):
 
     """
 
+    _fields = {'object': 'leaf'}
     breaks: t.ClassVar[t.Sequence] = ('object', 'trainer')
     object: Leaf
     method: t.Optional[str] = None
@@ -1447,7 +1475,9 @@ class RAGModel(Model):
 
         :param query: Query string.
         """
-        assert self.db, 'db cannot be None'
+        from pinnacle.base.datalayer import Datalayer
+
+        assert isinstance(self.db, Datalayer)
         select = self.select.set_variables(db=self.db, query=query)
         results = [r.unpack() for r in select.execute()]
         prompt = self._build_prompt(query, results)

pinnacle/components/template.py
@@ -7,10 +7,9 @@ from pinnacle.base.constant import KEY_BLOBS, KEY_FILES
 from pinnacle.base.datalayer import Datalayer
 from pinnacle.base.document import Document, QueryUpdateDocument
 from pinnacle.base.leaf import Leaf
-from pinnacle.base.variables import _replace_variables
+from pinnacle.base.variables import _find_variables, _replace_variables
 from pinnacle.components.component import Component, _build_info_from_path
 from pinnacle.components.table import Table
-from pinnacle.misc.special_dicts import SuperDuperFlatEncode
 
 from .component import ensure_initialized
 
@@ -46,7 +45,6 @@ class _BaseTemplate(Component):
         """Post initialization method."""
         if isinstance(self.template, Leaf):
             self.template = self.template.encode(defaults=True, metadata=False)
-        self.template = SuperDuperFlatEncode(self.template)
         if self.substitutions is not None:
             self.substitutions = {
                 self.db.databackend.backend_name: 'databackend',
@@ -58,7 +56,7 @@ class _BaseTemplate(Component):
                 **self.substitutions
             )
         if self.template_variables is None:
-            self.template_variables = self.template.variables
+            self.template_variables = sorted(list(set(_find_variables(self.template))))
 
         super().postinit()
 
@@ -71,7 +69,10 @@ class _BaseTemplate(Component):
             set(self.template_variables) - {'output_prefix', 'databackend'}
         )
         kwargs['output_prefix'] = CFG.output_prefix
-        kwargs['databackend'] = self.db.databackend.backend_name
+        try:
+            kwargs['databackend'] = self.db.databackend.backend_name
+        except AttributeError:
+            pass
         component = _replace_variables(
             self.template,
             **kwargs,

pinnacle/misc/typing.py
@@ -1,9 +1,13 @@
 import typing as t
 
+from pinnacle import Component as C
 from pinnacle.components.datatype import *
 
 File = t.NewType('File', t.AnyStr)
 Blob = t.NewType('Blob', t.Callable)
+Artifact = t.NewType('Artifact', t.Callable)
 Dill = t.NewType('Dill', t.Callable)
 Pickle = t.NewType('Pickle', t.Callable)
-JSON = t.NewType('JSON', t.Dict)
+SDict = t.NewType('SDict', t.Dict)
+SList = t.NewType('SList', t.List)
+Component = t.NewType('Component', C)