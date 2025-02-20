superduper-io/superduper

Deprecate `auto_schema` and `_object` and inline imports

superduper/base/datalayer.py
@@ -17,7 +17,7 @@ from superduper.backends.base.query import Query
 from superduper.base import apply, exceptions
 from superduper.base.config import Config
 from superduper.base.document import Document
-from superduper.base.leaf import Leaf
+from superduper.base.base import Base
 from superduper.components.component import Component
 from superduper.components.datatype import ComponentType, LeafType
 from superduper.components.table import Table
@@ -68,6 +68,15 @@ class Datalayer:
 
     def __getitem__(self, item):
         return Query(table=item, parts=(), db=self)
+
+    def insert(self, items: t.List[Base]):
+        """
+        Insert data into a table.
+
+        :param items: The instances (`superduper.base.Base`) to insert.
+        """
+        table = self.pre_insert(items)
+        return self[table.identifier].insert([x.dict(path=False) for x in items])
 
     @property
     def cdc(self):
@@ -159,48 +168,19 @@ class Datalayer:
         )
 
     def pre_insert(
-        self, table: str, documents: t.Sequence[t.Dict], auto_schema: bool = True
+        self, items: t.List[Base],
     ):
         """Pre-insert hook for data insertion.
 
-        :param table: The table to insert data into.
-        :param documents: The documents to insert.
-        :param auto_schema: Automatically create a schema for the table.
+        :param items: The items to insert.
         """
+        table = items[0].__class__.__name__
         try:
             table = self.load('Table', table)
             return table
-        except NonExistentMetadataError as e:
-            if auto_schema and self.cfg.auto_schema:
-                logging.info(f"Table {table} does not exist, auto creating...")
-            else:
-                raise e
-        if auto_schema and self.cfg.auto_schema:
-            self._auto_create_table(table, documents).schema
-
-            timeout = 60
-
-            import time
-
-            start = time.time()
-            exists = False
-            while time.time() - start < timeout:
-                try:
-                    assert table in self.show(
-                        'Table'
-                    ), f'{table} not found, retrying...'
-                    exists = True
-                except AssertionError as e:
-                    logging.warn(str(e))
-                    time.sleep(1)
-                    continue
-                break
-
-            if not exists:
-                raise TimeoutError(
-                    f'{table} not found after {timeout} seconds'
-                    ' table auto creation likely has failed or is stalling...'
-                )
+        except NonExistentMetadataError:
+            assert isinstance(items[0], Base)
+            return self.metadata.create(type(items[0]))
 
     def post_insert(self, table: str, ids: t.Sequence[str]):
         """Post-insert hook for data insertion.
@@ -248,7 +228,7 @@ class Datalayer:
         logging.info(f'Publishing {len(events)} events')
         return self.cluster.queue.publish(events)  # type: ignore[arg-type]
 
-    def create(self, object: t.Type[Leaf]):
+    def create(self, object: t.Type[Base]):
         """Create a new type of component/ leaf.
 
         :param object: The object to create.
@@ -517,7 +497,7 @@ class Datalayer:
         except FileNotFoundError:
             pass
 
-        serialized = object.dict(schema=True)
+        serialized = object.dict()
 
         if old_uuid:
 
@@ -634,20 +614,6 @@ class Datalayer:
         """Gracefully shutdown the Datalayer."""
         logging.info("Disconnect from Cluster")
         self.cluster.disconnect()
-
-    def infer_schema(
-        self, data: t.Mapping[str, t.Any], identifier: t.Optional[str] = None
-    ) -> t.Dict:
-        """Infer a schema from a given data object.
-
-        :param data: The data object
-        :param identifier: The identifier for the schema, if None, it will be generated
-        :return: The inferred schema
-        """
-        # TODO have a slightly more user-friendly schema
-        from superduper.misc.auto_schema import infer_schema
-
-        return infer_schema(data, identifier=identifier, db=self)
 
     @property
     def cfg(self) -> Config:

plugins/ibis/plugin_test/test_sanity.py
@@ -1,7 +1,7 @@
-from superduper.base.leaf import Leaf
+from superduper.base.base import Base
 
 
-class New(Leaf):
+class New(Base):
     a: str
 
 

test/integration/usecase/test_reapply.py
@@ -1,4 +1,5 @@
 from superduper import Model
+from superduper.base.base import Base
 from superduper.components.listener import Listener
 
 
@@ -12,8 +13,12 @@ class MyModel(Model):
         return x + 1
 
 
+class docs(Base):
+    x: int
+
+
 def test_reapply(db):
-    db.cfg.auto_schema = True
+    db.create(docs)
 
     db['docs'].insert([{'x': i} for i in range(10)])
 

test/unittest/base/example.py
@@ -1,4 +1,4 @@
-from superduper import imported
+from superduper.base.base import imported
 
 
 @imported

test/unittest/component/test_listener.py
@@ -6,6 +6,7 @@ import numpy as np
 import pytest
 
 from superduper import Application, Document
+from superduper.base.base import Base
 from superduper.base.constant import KEY_BLOBS
 from superduper.components.listener import Listener
 from superduper.components.model import ObjectModel, Trainer
@@ -21,11 +22,20 @@ class MyTrainer(Trainer):
             pass
 
 
-@dc.dataclass
-class _Tmp(ObjectModel): ...
+class _Tmp(ObjectModel):
+    ...
+
+
+class test(Base):
+    x: int
+    y: int
+    _fold: str
 
 
 def test_listener_serializes_properly(db):
+
+    db.create(test)
+
     q = db['test'].select()
     listener = Listener(
         identifier="listener",
@@ -43,7 +53,7 @@ def test_listener_serializes_properly(db):
 
 
 def test_listener_chaining(db):
-    db.cfg.auto_schema = True
+    db.create(test)
     table = db['test']
 
     def insert_random(start=0):
@@ -117,7 +127,7 @@ def test_listener_chaining(db):
 )
 @pytest.mark.parametrize("flatten", [False, True])
 def test_create_output_dest(db, data, flatten):
-    db.cfg.auto_schema = True
+    db.create(test)
     table = db["test"]
 
     data, datatype = data
@@ -165,7 +175,7 @@ def test_create_output_dest(db, data, flatten):
     ],
 )
 def test_listener_cleanup(db, data):
-    db.cfg.auto_schema = True
+    db.create(test)
     table = db["test"]
 
     data, datatype = data
@@ -209,7 +219,7 @@ def cleanup():
 
 
 def test_listener_chaining_with_trainer(db, cleanup):
-    db.cfg.auto_schema = True
+    db.create(test)
     table = db['test']
 
     def insert_random(start=0):
@@ -217,16 +227,10 @@ def test_listener_chaining_with_trainer(db, cleanup):
         for i in range(5):
             y = int(random.random() > 0.5)
             data.append(
-                Document(
-                    {
-                        "x": i + start,
-                        "y": y,
-                        '_fold': 'train' if i < 3 else 'valid',
-                    }
-                )
+                test(x=i + start, y=y, _fold='train' if i < 3 else 'valid')
             )
 
-        table.insert(data)
+        db.insert(data)
 
     # Insert data
     insert_random()
@@ -263,7 +267,6 @@ def test_listener_chaining_with_trainer(db, cleanup):
 
 
 def test_upstream_serializes(db):
-    # db.cfg.auto_schema = True
 
     upstream_component = ObjectModel("upstream", object=lambda x: x)
 
@@ -293,7 +296,8 @@ def test_upstream_serializes(db):
 # TODO: Need to fix this test case
 @pytest.mark.skip("This test is not working")
 def test_predict_id_utils(db):
-    db.cfg.auto_schema = True
+    db.create(test)
+
     table = db["test"]
 
     m1 = ObjectModel(
@@ -339,7 +343,8 @@ def test_predict_id_utils(db):
 
 
 def test_complete_uuids(db):
-    db.cfg.auto_schema = True
+
+    db.create(test)
 
     m1 = ObjectModel(
         "m1",
@@ -375,7 +380,8 @@ def test_complete_uuids(db):
 
 
 def test_autofill_data_listener(db):
-    db.cfg.auto_schema = True
+
+    db.create(test)
 
     def get_model():
         return ObjectModel("m1", object=lambda x: x + 2, datatype='int')

test/utils/usecase/chain_listener.py
@@ -1,14 +1,21 @@
 import typing as t
 
 from superduper import ObjectModel
+from superduper.base.base import Base
 from superduper.components.listener import Listener
 
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
 
 
+class documents(Base):
+    x: int
+
+
 def build_chain_listener(db: "Datalayer"):
-    db.cfg.auto_schema = True
+
+    db.create(documents)
+
     data = [
         {"x": 1},
         {"x": 2},

test/utils/usecase/vector_search.py
@@ -2,6 +2,7 @@ import typing as t
 
 import numpy as np
 
+from superduper.base import Base
 from superduper.components.listener import Listener
 
 if t.TYPE_CHECKING:
@@ -10,34 +11,27 @@ if t.TYPE_CHECKING:
 VECTOR_SIZE = 300
 
 
+class documents(Base):
+    x: int
+    label: int
+
+
 def add_data(db: "Datalayer", start: int, end: int):
     """
     :param db: Datalayer
     :param start: int to start assigning to `x` column
     :param end: int to stop assigning to `x` column
     """
-    db.cfg.auto_schema = True
-    data = []
-    for i in range(start, end):
-        data.append(
-            {
-                "x": i,
-                "label": int(i % 2 == 0),
-            }
-        )
-    db["documents"].insert(data)
+    db.insert([documents(x=i, label=int(i % 2 == 0)) for i in range(start, end)])
 
 
 def build_vector_index(
     db: "Datalayer",
     n: int = 100,
     list_embeddings=False,
-    vector_datatype=None,
     measure=None,
 ):
     from superduper import ObjectModel, VectorIndex
-
-    db.cfg.auto_schema = True
 
     add_data(db, 0, n)
 

Pinnacle-Networks/Pinnacle

pinnacle/base/datalayer.py
@@ -17,7 +17,7 @@ from pinnacle.backends.base.query import Query
 from pinnacle.base import apply, exceptions
 from pinnacle.base.config import Config
 from pinnacle.base.document import Document
-from pinnacle.base.leaf import Leaf
+from pinnacle.base.base import Base
 from pinnacle.components.component import Component
 from pinnacle.components.datatype import ComponentType, LeafType
 from pinnacle.components.table import Table
@@ -68,6 +68,15 @@ class Datalayer:
 
     def __getitem__(self, item):
         return Query(table=item, parts=(), db=self)
+
+    def insert(self, items: t.List[Base]):
+        """
+        Insert data into a table.
+
+        :param items: The instances (`pinnacle.base.Base`) to insert.
+        """
+        table = self.pre_insert(items)
+        return self[table.identifier].insert([x.dict(path=False) for x in items])
 
     @property
     def cdc(self):
@@ -159,48 +168,19 @@ class Datalayer:
         )
 
     def pre_insert(
-        self, table: str, documents: t.Sequence[t.Dict], auto_schema: bool = True
+        self, items: t.List[Base],
     ):
         """Pre-insert hook for data insertion.
 
-        :param table: The table to insert data into.
-        :param documents: The documents to insert.
-        :param auto_schema: Automatically create a schema for the table.
+        :param items: The items to insert.
         """
+        table = items[0].__class__.__name__
         try:
             table = self.load('Table', table)
             return table
-        except NonExistentMetadataError as e:
-            if auto_schema and self.cfg.auto_schema:
-                logging.info(f"Table {table} does not exist, auto creating...")
-            else:
-                raise e
-        if auto_schema and self.cfg.auto_schema:
-            self._auto_create_table(table, documents).schema
-
-            timeout = 60
-
-            import time
-
-            start = time.time()
-            exists = False
-            while time.time() - start < timeout:
-                try:
-                    assert table in self.show(
-                        'Table'
-                    ), f'{table} not found, retrying...'
-                    exists = True
-                except AssertionError as e:
-                    logging.warn(str(e))
-                    time.sleep(1)
-                    continue
-                break
-
-            if not exists:
-                raise TimeoutError(
-                    f'{table} not found after {timeout} seconds'
-                    ' table auto creation likely has failed or is stalling...'
-                )
+        except NonExistentMetadataError:
+            assert isinstance(items[0], Base)
+            return self.metadata.create(type(items[0]))
 
     def post_insert(self, table: str, ids: t.Sequence[str]):
         """Post-insert hook for data insertion.
@@ -248,7 +228,7 @@ class Datalayer:
         logging.info(f'Publishing {len(events)} events')
         return self.cluster.queue.publish(events)  # type: ignore[arg-type]
 
-    def create(self, object: t.Type[Leaf]):
+    def create(self, object: t.Type[Base]):
         """Create a new type of component/ leaf.
 
         :param object: The object to create.
@@ -517,7 +497,7 @@ class Datalayer:
         except FileNotFoundError:
             pass
 
-        serialized = object.dict(schema=True)
+        serialized = object.dict()
 
         if old_uuid:
 
@@ -634,20 +614,6 @@ class Datalayer:
         """Gracefully shutdown the Datalayer."""
         logging.info("Disconnect from Cluster")
         self.cluster.disconnect()
-
-    def infer_schema(
-        self, data: t.Mapping[str, t.Any], identifier: t.Optional[str] = None
-    ) -> t.Dict:
-        """Infer a schema from a given data object.
-
-        :param data: The data object
-        :param identifier: The identifier for the schema, if None, it will be generated
-        :return: The inferred schema
-        """
-        # TODO have a slightly more user-friendly schema
-        from pinnacle.misc.auto_schema import infer_schema
-
-        return infer_schema(data, identifier=identifier, db=self)
 
     @property
     def cfg(self) -> Config:

plugins/ibis/plugin_test/test_sanity.py
@@ -1,7 +1,7 @@
-from pinnacle.base.leaf import Leaf
+from pinnacle.base.base import Base
 
 
-class New(Leaf):
+class New(Base):
     a: str
 
 

test/integration/usecase/test_reapply.py
@@ -1,4 +1,5 @@
 from pinnacle import Model
+from pinnacle.base.base import Base
 from pinnacle.components.listener import Listener
 
 
@@ -12,8 +13,12 @@ class MyModel(Model):
         return x + 1
 
 
+class docs(Base):
+    x: int
+
+
 def test_reapply(db):
-    db.cfg.auto_schema = True
+    db.create(docs)
 
     db['docs'].insert([{'x': i} for i in range(10)])
 

test/unittest/base/example.py
@@ -1,4 +1,4 @@
-from pinnacle import imported
+from pinnacle.base.base import imported
 
 
 @imported

test/unittest/component/test_listener.py
@@ -6,6 +6,7 @@ import numpy as np
 import pytest
 
 from pinnacle import Application, Document
+from pinnacle.base.base import Base
 from pinnacle.base.constant import KEY_BLOBS
 from pinnacle.components.listener import Listener
 from pinnacle.components.model import ObjectModel, Trainer
@@ -21,11 +22,20 @@ class MyTrainer(Trainer):
             pass
 
 
-@dc.dataclass
-class _Tmp(ObjectModel): ...
+class _Tmp(ObjectModel):
+    ...
+
+
+class test(Base):
+    x: int
+    y: int
+    _fold: str
 
 
 def test_listener_serializes_properly(db):
+
+    db.create(test)
+
     q = db['test'].select()
     listener = Listener(
         identifier="listener",
@@ -43,7 +53,7 @@ def test_listener_serializes_properly(db):
 
 
 def test_listener_chaining(db):
-    db.cfg.auto_schema = True
+    db.create(test)
     table = db['test']
 
     def insert_random(start=0):
@@ -117,7 +127,7 @@ def test_listener_chaining(db):
 )
 @pytest.mark.parametrize("flatten", [False, True])
 def test_create_output_dest(db, data, flatten):
-    db.cfg.auto_schema = True
+    db.create(test)
     table = db["test"]
 
     data, datatype = data
@@ -165,7 +175,7 @@ def test_create_output_dest(db, data, flatten):
     ],
 )
 def test_listener_cleanup(db, data):
-    db.cfg.auto_schema = True
+    db.create(test)
     table = db["test"]
 
     data, datatype = data
@@ -209,7 +219,7 @@ def cleanup():
 
 
 def test_listener_chaining_with_trainer(db, cleanup):
-    db.cfg.auto_schema = True
+    db.create(test)
     table = db['test']
 
     def insert_random(start=0):
@@ -217,16 +227,10 @@ def test_listener_chaining_with_trainer(db, cleanup):
         for i in range(5):
             y = int(random.random() > 0.5)
             data.append(
-                Document(
-                    {
-                        "x": i + start,
-                        "y": y,
-                        '_fold': 'train' if i < 3 else 'valid',
-                    }
-                )
+                test(x=i + start, y=y, _fold='train' if i < 3 else 'valid')
             )
 
-        table.insert(data)
+        db.insert(data)
 
     # Insert data
     insert_random()
@@ -263,7 +267,6 @@ def test_listener_chaining_with_trainer(db, cleanup):
 
 
 def test_upstream_serializes(db):
-    # db.cfg.auto_schema = True
 
     upstream_component = ObjectModel("upstream", object=lambda x: x)
 
@@ -293,7 +296,8 @@ def test_upstream_serializes(db):
 # TODO: Need to fix this test case
 @pytest.mark.skip("This test is not working")
 def test_predict_id_utils(db):
-    db.cfg.auto_schema = True
+    db.create(test)
+
     table = db["test"]
 
     m1 = ObjectModel(
@@ -339,7 +343,8 @@ def test_predict_id_utils(db):
 
 
 def test_complete_uuids(db):
-    db.cfg.auto_schema = True
+
+    db.create(test)
 
     m1 = ObjectModel(
         "m1",
@@ -375,7 +380,8 @@ def test_complete_uuids(db):
 
 
 def test_autofill_data_listener(db):
-    db.cfg.auto_schema = True
+
+    db.create(test)
 
     def get_model():
         return ObjectModel("m1", object=lambda x: x + 2, datatype='int')

test/utils/usecase/chain_listener.py
@@ -1,14 +1,21 @@
 import typing as t
 
 from pinnacle import ObjectModel
+from pinnacle.base.base import Base
 from pinnacle.components.listener import Listener
 
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer
 
 
+class documents(Base):
+    x: int
+
+
 def build_chain_listener(db: "Datalayer"):
-    db.cfg.auto_schema = True
+
+    db.create(documents)
+
     data = [
         {"x": 1},
         {"x": 2},

test/utils/usecase/vector_search.py
@@ -2,6 +2,7 @@ import typing as t
 
 import numpy as np
 
+from pinnacle.base import Base
 from pinnacle.components.listener import Listener
 
 if t.TYPE_CHECKING:
@@ -10,34 +11,27 @@ if t.TYPE_CHECKING:
 VECTOR_SIZE = 300
 
 
+class documents(Base):
+    x: int
+    label: int
+
+
 def add_data(db: "Datalayer", start: int, end: int):
     """
     :param db: Datalayer
     :param start: int to start assigning to `x` column
     :param end: int to stop assigning to `x` column
     """
-    db.cfg.auto_schema = True
-    data = []
-    for i in range(start, end):
-        data.append(
-            {
-                "x": i,
-                "label": int(i % 2 == 0),
-            }
-        )
-    db["documents"].insert(data)
+    db.insert([documents(x=i, label=int(i % 2 == 0)) for i in range(start, end)])
 
 
 def build_vector_index(
     db: "Datalayer",
     n: int = 100,
     list_embeddings=False,
-    vector_datatype=None,
     measure=None,
 ):
     from pinnacle import ObjectModel, VectorIndex
-
-    db.cfg.auto_schema = True
 
     add_data(db, 0, n)
 