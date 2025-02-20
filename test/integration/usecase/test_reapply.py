superduper-io/superduper

Deprecate `to_listener` and `to_vector_index`

test/integration/usecase/test_reapply.py
@@ -1,4 +1,5 @@
 from superduper import Model
+from superduper.components.listener import Listener
 
 
 class MyModel(Model):
@@ -18,12 +19,14 @@ def test_reapply(db):
 
     def build(name, data):
         model = MyModel('test', a=name, b=data)
-        listener_1 = model.to_listener(
+        listener_1 = Listener(
+            model=model,
             key='x',
             select=db['docs'].select(),
             identifier='listener_1',
         )
-        return model.to_listener(
+        return Listener(
+            model=model,
             key=listener_1.outputs,
             select=db[listener_1.outputs].select(),
             identifier='listener_2',

test/unittest/base/test_apply.py
@@ -5,6 +5,7 @@ from superduper.base.annotations import trigger
 from superduper.base.apply import _apply
 from superduper.base.datalayer import Datalayer
 from superduper.components.application import Application
+from superduper.components.listener import Listener
 
 
 class MyValidator(Component):
@@ -209,16 +210,24 @@ def test_duplicate_job_submission(db: Datalayer):
     def my_func(x):
         return x + 1
 
-    listener_1 = ObjectModel(
-        'list1',
-        object=my_func,
-    ).to_listener(key='x', select=db['docs'].select())
+    listener_1 = Listener(
+        model=ObjectModel(
+            'list1',
+            object=my_func,
+        ),
+        key='x',
+        select=db['docs'].select(),
+    )
 
-    listener_2 = ObjectModel(
-        'list2',
-        object=my_func,
-        upstream=listener_1,
-    ).to_listener(key=listener_1.outputs, select=db[listener_1.outputs].select())
+    listener_2 = Listener(
+        ObjectModel(
+            'list2',
+            object=my_func,
+            upstream=listener_1,
+        ),
+        key=listener_1.outputs,
+        select=db[listener_1.outputs].select(),
+    )
 
     c = Application(
         'test',

test/unittest/base/test_document.py
@@ -11,6 +11,7 @@ from superduper.components.datatype import (
     pickle_encoder,
     pickle_serializer,
 )
+from superduper.components.listener import Listener
 from superduper.components.model import ObjectModel
 from superduper.components.schema import Schema
 from superduper.components.table import Table
@@ -210,7 +211,7 @@ def test_refer_to_system(db):
 
 def test_encode_same_identifier():
     model = ObjectModel(identifier="a", object=lambda x: x, datatype='str')
-    listener = model.to_listener(identifier="a", key="a", select=None)
+    listener = Listener(model=model, identifier="a", key="a", select=None)
 
     encode_data = listener.encode()
     listener = Document.decode(encode_data).unpack()

test/unittest/component/test_application.py
@@ -7,6 +7,7 @@ import pytest
 from superduper import Application, ObjectModel, Schema, superduper
 from superduper.base.document import Document
 from superduper.components.datatype import pickle_encoder
+from superduper.components.listener import Listener
 from superduper.components.table import Table
 
 if t.TYPE_CHECKING:
@@ -35,7 +36,8 @@ def test_wrap_as_application_from_db(db: "Datalayer"):
 
     model1 = ObjectModel(identifier="model1", object=lambda x: x + 1)
 
-    listener1 = model1.to_listener(
+    listener1 = Listener(
+        model=model1,
         key="x",
         select=db["documents"].select(),
         identifier="listener1",
@@ -43,16 +45,16 @@ def test_wrap_as_application_from_db(db: "Datalayer"):
 
     model2 = ObjectModel(identifier="model2", object=lambda y: y + 2)
 
-    listener2 = model2.to_listener(
-        key="y", select=db["documents"].select(), identifier="listener2"
+    listener2 = Listener(
+        model=model2, key="y", select=db["documents"].select(), identifier="listener2"
     )
 
     model3 = ObjectModel(
         identifier="model3", object=lambda z: z * 3, datatype=pickle_encoder
     )
 
-    listener3 = model3.to_listener(
-        key="z", select=db["documents"].select(), identifier="listener3"
+    listener3 = Listener(
+        model=model3, key="z", select=db["documents"].select(), identifier="listener3"
     )
 
     db.apply(listener1)
@@ -98,8 +100,10 @@ def test_wrap_as_application_from_db(db: "Datalayer"):
 def test_sort_components(db):
     m = ObjectModel('test', object=lambda x: x + 1)
 
-    l1 = m.to_listener(key='x', select=db['docs'].select(), identifier='l1')
-    l2 = m.to_listener(
+    l1 = Listener(model=m, key='x', select=db['docs'].select(), identifier='l1')
+    l1 = Listener(model=m, key='x', select=db['docs'].select(), identifier='l1')
+    l2 = Listener(
+        model=m,
         key=l1.outputs,
         select=db[l1.outputs].select(),
         identifier='l2',

test/unittest/component/test_vector_index.py
@@ -1,6 +1,8 @@
 from test.utils.usecase.vector_search import add_data
 
 from superduper import model
+from superduper.components.listener import Listener
+from superduper.components.vector_index import VectorIndex
 
 
 def test_vector_index_recovery(db):
@@ -53,7 +55,12 @@ def test_initialize_output_datatype_with_dimensions(db):
     def test(x):
         return numpy.random.randn(32)
 
-    vector_index = test.to_vector_index(key='x', select=db['documents'].select())
+    vector_index = VectorIndex(
+        identifier='vector_index',
+        indexing_listener=Listener(
+            'listener', model=test, key='x', select=db['documents'].select()
+        ),
+    )
     vector_index.pre_create(db)
 
     from superduper import Table

test/unittest/jobs/test_task_workflow.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 from superduper import Document
+from superduper.components.listener import Listener
 from superduper.components.model import ObjectModel
 
 
@@ -33,7 +34,8 @@ def test_downstream_task_workflows_are_triggered(db, data, flatten):
         datatype=datatype if not flatten else datatype_flat,
     )
 
-    upstream_listener = upstream_model.to_listener(
+    upstream_listener = Listener(
+        model=upstream_model,
         key="x",
         select=db["test"].select(),
         identifier="upstream",
@@ -48,7 +50,8 @@ def test_downstream_task_workflows_are_triggered(db, data, flatten):
         datatype=datatype if not flatten else datatype_flat,
     )
 
-    downstream_listener = downstream_model.to_listener(
+    downstream_listener = Listener(
+        model=downstream_model,
         key=upstream_listener.outputs,
         select=db[upstream_listener.outputs].select(),
         identifier="downstream",

test/utils/usecase/chain_listener.py
@@ -1,6 +1,7 @@
 import typing as t
 
 from superduper import ObjectModel
+from superduper.components.listener import Listener
 
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
@@ -22,21 +23,24 @@ def build_chain_listener(db: "Datalayer"):
 
     model_c = ObjectModel(identifier="c", object=lambda x: f"{x}->c")
 
-    listener_a = model_a.to_listener(
+    listener_a = Listener(
+        model=model_a,
         select=db["documents"].select(),
         key="x",
         identifier="a",
     )
     db.apply(listener_a)
 
-    listener_b = model_b.to_listener(
+    listener_b = Listener(
+        model=model_b,
         select=db[listener_a.outputs].select(),
         key=listener_a.outputs,
         identifier="b",
     )
     db.apply(listener_b)
 
-    listener_c = model_c.to_listener(
+    listener_c = Listener(
+        model=model_c,
         select=db[listener_b.outputs].select(),
         key=listener_b.outputs,
         identifier="c",

test/utils/usecase/graph_listener.py
@@ -3,6 +3,7 @@ import typing as t
 import numpy as np
 
 from superduper import Document, ObjectModel
+from superduper.components.listener import Listener
 
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
@@ -49,7 +50,8 @@ def build_graph_listener(db: "Datalayer"):
 
     model_a = ObjectModel(identifier="model_a", object=func_a, datatype='pickleencoder')
 
-    listener_a = model_a.to_listener(
+    listener_a = Listener(
+        model=model_a,
         key="x",
         select=db["documents"].select(),
         identifier="a",
@@ -60,7 +62,9 @@ def build_graph_listener(db: "Datalayer"):
         return {"x": x, "y": y, "o_a": o_a, "model": "b"}
 
     model_b = ObjectModel(identifier="model_b", object=func_b, datatype='pickleencoder')
-    listener_b = model_b.to_listener(
+
+    listener_b = Listener(
+        model=model_b,
         key=("x", "y", listener_a.outputs),
         select=db["documents"]
         .select(primary_id, "x", "y")
@@ -73,7 +77,8 @@ def build_graph_listener(db: "Datalayer"):
         return {"x": x, "y": y, "z": z, "o_a": o_a, "o_b": o_b, "model": "c"}
 
     model_c = ObjectModel(identifier="model_c", object=func_c, datatype='pickleencoder')
-    listener_c = model_c.to_listener(
+    listener_c = Listener(
+        model=model_c,
         key=("x", "y", "z", listener_a.outputs, listener_b.outputs),
         select=db["documents"]
         .select(primary_id, "x", "y", "z")

Pinnacle-Networks/Pinnacle

test/integration/usecase/test_reapply.py
@@ -1,4 +1,5 @@
 from pinnacle import Model
+from pinnacle.components.listener import Listener
 
 
 class MyModel(Model):
@@ -18,12 +19,14 @@ def test_reapply(db):
 
     def build(name, data):
         model = MyModel('test', a=name, b=data)
-        listener_1 = model.to_listener(
+        listener_1 = Listener(
+            model=model,
             key='x',
             select=db['docs'].select(),
             identifier='listener_1',
         )
-        return model.to_listener(
+        return Listener(
+            model=model,
             key=listener_1.outputs,
             select=db[listener_1.outputs].select(),
             identifier='listener_2',

test/unittest/base/test_apply.py
@@ -5,6 +5,7 @@ from pinnacle.base.annotations import trigger
 from pinnacle.base.apply import _apply
 from pinnacle.base.datalayer import Datalayer
 from pinnacle.components.application import Application
+from pinnacle.components.listener import Listener
 
 
 class MyValidator(Component):
@@ -209,16 +210,24 @@ def test_duplicate_job_submission(db: Datalayer):
     def my_func(x):
         return x + 1
 
-    listener_1 = ObjectModel(
-        'list1',
-        object=my_func,
-    ).to_listener(key='x', select=db['docs'].select())
+    listener_1 = Listener(
+        model=ObjectModel(
+            'list1',
+            object=my_func,
+        ),
+        key='x',
+        select=db['docs'].select(),
+    )
 
-    listener_2 = ObjectModel(
-        'list2',
-        object=my_func,
-        upstream=listener_1,
-    ).to_listener(key=listener_1.outputs, select=db[listener_1.outputs].select())
+    listener_2 = Listener(
+        ObjectModel(
+            'list2',
+            object=my_func,
+            upstream=listener_1,
+        ),
+        key=listener_1.outputs,
+        select=db[listener_1.outputs].select(),
+    )
 
     c = Application(
         'test',

test/unittest/base/test_document.py
@@ -11,6 +11,7 @@ from pinnacle.components.datatype import (
     pickle_encoder,
     pickle_serializer,
 )
+from pinnacle.components.listener import Listener
 from pinnacle.components.model import ObjectModel
 from pinnacle.components.schema import Schema
 from pinnacle.components.table import Table
@@ -210,7 +211,7 @@ def test_refer_to_system(db):
 
 def test_encode_same_identifier():
     model = ObjectModel(identifier="a", object=lambda x: x, datatype='str')
-    listener = model.to_listener(identifier="a", key="a", select=None)
+    listener = Listener(model=model, identifier="a", key="a", select=None)
 
     encode_data = listener.encode()
     listener = Document.decode(encode_data).unpack()

test/unittest/component/test_application.py
@@ -7,6 +7,7 @@ import pytest
 from pinnacle import Application, ObjectModel, Schema, pinnacle
 from pinnacle.base.document import Document
 from pinnacle.components.datatype import pickle_encoder
+from pinnacle.components.listener import Listener
 from pinnacle.components.table import Table
 
 if t.TYPE_CHECKING:
@@ -35,7 +36,8 @@ def test_wrap_as_application_from_db(db: "Datalayer"):
 
     model1 = ObjectModel(identifier="model1", object=lambda x: x + 1)
 
-    listener1 = model1.to_listener(
+    listener1 = Listener(
+        model=model1,
         key="x",
         select=db["documents"].select(),
         identifier="listener1",
@@ -43,16 +45,16 @@ def test_wrap_as_application_from_db(db: "Datalayer"):
 
     model2 = ObjectModel(identifier="model2", object=lambda y: y + 2)
 
-    listener2 = model2.to_listener(
-        key="y", select=db["documents"].select(), identifier="listener2"
+    listener2 = Listener(
+        model=model2, key="y", select=db["documents"].select(), identifier="listener2"
     )
 
     model3 = ObjectModel(
         identifier="model3", object=lambda z: z * 3, datatype=pickle_encoder
     )
 
-    listener3 = model3.to_listener(
-        key="z", select=db["documents"].select(), identifier="listener3"
+    listener3 = Listener(
+        model=model3, key="z", select=db["documents"].select(), identifier="listener3"
     )
 
     db.apply(listener1)
@@ -98,8 +100,10 @@ def test_wrap_as_application_from_db(db: "Datalayer"):
 def test_sort_components(db):
     m = ObjectModel('test', object=lambda x: x + 1)
 
-    l1 = m.to_listener(key='x', select=db['docs'].select(), identifier='l1')
-    l2 = m.to_listener(
+    l1 = Listener(model=m, key='x', select=db['docs'].select(), identifier='l1')
+    l1 = Listener(model=m, key='x', select=db['docs'].select(), identifier='l1')
+    l2 = Listener(
+        model=m,
         key=l1.outputs,
         select=db[l1.outputs].select(),
         identifier='l2',

test/unittest/component/test_vector_index.py
@@ -1,6 +1,8 @@
 from test.utils.usecase.vector_search import add_data
 
 from pinnacle import model
+from pinnacle.components.listener import Listener
+from pinnacle.components.vector_index import VectorIndex
 
 
 def test_vector_index_recovery(db):
@@ -53,7 +55,12 @@ def test_initialize_output_datatype_with_dimensions(db):
     def test(x):
         return numpy.random.randn(32)
 
-    vector_index = test.to_vector_index(key='x', select=db['documents'].select())
+    vector_index = VectorIndex(
+        identifier='vector_index',
+        indexing_listener=Listener(
+            'listener', model=test, key='x', select=db['documents'].select()
+        ),
+    )
     vector_index.pre_create(db)
 
     from pinnacle import Table

test/unittest/jobs/test_task_workflow.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 from pinnacle import Document
+from pinnacle.components.listener import Listener
 from pinnacle.components.model import ObjectModel
 
 
@@ -33,7 +34,8 @@ def test_downstream_task_workflows_are_triggered(db, data, flatten):
         datatype=datatype if not flatten else datatype_flat,
     )
 
-    upstream_listener = upstream_model.to_listener(
+    upstream_listener = Listener(
+        model=upstream_model,
         key="x",
         select=db["test"].select(),
         identifier="upstream",
@@ -48,7 +50,8 @@ def test_downstream_task_workflows_are_triggered(db, data, flatten):
         datatype=datatype if not flatten else datatype_flat,
     )
 
-    downstream_listener = downstream_model.to_listener(
+    downstream_listener = Listener(
+        model=downstream_model,
         key=upstream_listener.outputs,
         select=db[upstream_listener.outputs].select(),
         identifier="downstream",

test/utils/usecase/chain_listener.py
@@ -1,6 +1,7 @@
 import typing as t
 
 from pinnacle import ObjectModel
+from pinnacle.components.listener import Listener
 
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer
@@ -22,21 +23,24 @@ def build_chain_listener(db: "Datalayer"):
 
     model_c = ObjectModel(identifier="c", object=lambda x: f"{x}->c")
 
-    listener_a = model_a.to_listener(
+    listener_a = Listener(
+        model=model_a,
         select=db["documents"].select(),
         key="x",
         identifier="a",
     )
     db.apply(listener_a)
 
-    listener_b = model_b.to_listener(
+    listener_b = Listener(
+        model=model_b,
         select=db[listener_a.outputs].select(),
         key=listener_a.outputs,
         identifier="b",
     )
     db.apply(listener_b)
 
-    listener_c = model_c.to_listener(
+    listener_c = Listener(
+        model=model_c,
         select=db[listener_b.outputs].select(),
         key=listener_b.outputs,
         identifier="c",

test/utils/usecase/graph_listener.py
@@ -3,6 +3,7 @@ import typing as t
 import numpy as np
 
 from pinnacle import Document, ObjectModel
+from pinnacle.components.listener import Listener
 
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer
@@ -49,7 +50,8 @@ def build_graph_listener(db: "Datalayer"):
 
     model_a = ObjectModel(identifier="model_a", object=func_a, datatype='pickleencoder')
 
-    listener_a = model_a.to_listener(
+    listener_a = Listener(
+        model=model_a,
         key="x",
         select=db["documents"].select(),
         identifier="a",
@@ -60,7 +62,9 @@ def build_graph_listener(db: "Datalayer"):
         return {"x": x, "y": y, "o_a": o_a, "model": "b"}
 
     model_b = ObjectModel(identifier="model_b", object=func_b, datatype='pickleencoder')
-    listener_b = model_b.to_listener(
+
+    listener_b = Listener(
+        model=model_b,
         key=("x", "y", listener_a.outputs),
         select=db["documents"]
         .select(primary_id, "x", "y")
@@ -73,7 +77,8 @@ def build_graph_listener(db: "Datalayer"):
         return {"x": x, "y": y, "z": z, "o_a": o_a, "o_b": o_b, "model": "c"}
 
     model_c = ObjectModel(identifier="model_c", object=func_c, datatype='pickleencoder')
-    listener_c = model_c.to_listener(
+    listener_c = Listener(
+        model=model_c,
         key=("x", "y", "z", listener_a.outputs, listener_b.outputs),
         select=db["documents"]
         .select(primary_id, "x", "y", "z")