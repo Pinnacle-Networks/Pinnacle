superduper-io/superduper

Move schema, datatype, query_dataset, artifact_store, metadata & query to `base`; refactor `.predict_in_db`

superduper/components/vector_index.py
@@ -9,11 +9,9 @@ from superduper.backends.base.vector_search import VectorItem
 from superduper.base.annotations import trigger
 from superduper.base.datalayer import Datalayer
 from superduper.base.document import Document
+from superduper.base.schema import Schema
 from superduper.components.cdc import CDC
-from superduper.components.component import Component
 from superduper.components.listener import Listener
-from superduper.components.model import Mapping
-from superduper.components.schema import Schema
 from superduper.components.table import Table
 from superduper.misc.special_dicts import MongoStyleDict
 
@@ -47,7 +45,7 @@ def backfill_vector_search(db, vi, searcher):
     :param vi: Identifier of vector index.
     :param searcher: FastVectorSearch instance to load model outputs as vectors.
     """
-    from superduper.components.datatype import _BaseEncodable
+    from superduper.base.datatype import _BaseEncodable
 
     logging.info(f"Loading vectors of vector-index: '{vi.identifier}'")
 
@@ -117,17 +115,6 @@ class VectorIndex(CDC):
         """Post-initialization method."""
         self.cdc_table = self.cdc_table or self.indexing_listener.outputs
         super().postinit()
-
-    # TODO why this?
-    def __hash__(self):
-        return hash((self.type_id, self.identifier))
-
-    def __eq__(self, other: t.Any):
-        if isinstance(other, Component):
-            return (
-                self.identifier == other.identifier and self.type_id and other.type_id
-            )
-        return False
 
     def _pre_create(self, db: Datalayer, startup_cache: t.Dict = {}):
         assert isinstance(self.indexing_listener, Listener)
@@ -263,10 +250,9 @@ class VectorIndex(CDC):
                 )
 
         model = models[model_name]
-        data = Mapping(key, model.signature)(document)
-        args, kwargs = model.handle_input_type(data, model.signature)
+        assert model.signature == 'singleton'
         return (
-            model.predict(*args, **kwargs),
+            model.predict(document[key]),
             model.identifier,
             key,
         )

superduper/misc/serialization.py
@@ -42,7 +42,7 @@ def asdict(obj, *, copy_method=copy.copy) -> t.Dict[str, t.Any]:
 
 def _asdict_inner(obj, dict_factory, copy_method, top=False) -> t.Any:
     from superduper.base.base import Base
-    from superduper.components.datatype import Saveable
+    from superduper.base.datatype import Saveable
 
     if type(obj) in _ATOMIC_TYPES:
         return obj

plugins/mongodb/superduper_mongodb/artifacts.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import click
 import gridfs
 from superduper import CFG, logging
-from superduper.backends.base.artifacts import ArtifactStore
+from superduper.base.artifacts import ArtifactStore
 from tqdm import tqdm
 
 from superduper_mongodb.utils import connection_callback

plugins/sklearn/superduper_sklearn/model.py
@@ -4,9 +4,9 @@ import typing as t
 import numpy
 from sklearn.base import BaseEstimator
 from superduper import logging
-from superduper.backends.query_dataset import QueryDataset
 from superduper.base.datalayer import Datalayer
-from superduper.components.datatype import pickle_serializer
+from superduper.base.datatype import pickle_serializer
+from superduper.base.query_dataset import QueryDataset
 from superduper.components.model import (
     Model,
     ModelInputType,

plugins/sqlalchemy/superduper_sqlalchemy/metadata.py
@@ -19,7 +19,7 @@ from sqlalchemy import (
 from sqlalchemy.exc import ProgrammingError
 from sqlalchemy.orm import sessionmaker
 from superduper import logging
-from superduper.backends.base.metadata import MetaDataStore, NonExistentMetadataError
+from superduper.base.metadata import MetaDataStore, NonExistentMetadataError
 from superduper.components.component import Status
 
 from superduper_sqlalchemy.db_helper import get_db_config

test/unittest/backends/local/test_artifact_store.py
@@ -2,7 +2,7 @@ from test.utils.database import artifact_store as artifact_store_utils
 
 import pytest
 
-from superduper.backends.local.artifacts import FileSystemArtifactStore
+from superduper.base.artifacts import FileSystemArtifactStore
 
 
 @pytest.fixture

test/unittest/component/test_application.py
@@ -5,8 +5,8 @@ import numpy as np
 import pytest
 
 from superduper import ObjectModel, Schema, superduper
+from superduper.base.datatype import pickle_encoder
 from superduper.base.document import Document
-from superduper.components.datatype import pickle_encoder
 from superduper.components.listener import Listener
 from superduper.components.table import Table
 

test/utils/database/metadata.py
@@ -2,8 +2,8 @@ import uuid
 
 import pytest
 
-from superduper.backends.base.metadata import MetaDataStore
 from superduper.base.event import Job
+from superduper.base.metadata import MetaDataStore
 
 
 def test_component(metadata: MetaDataStore):

Pinnacle-Networks/Pinnacle

pinnacle/components/vector_index.py
@@ -9,11 +9,9 @@ from pinnacle.backends.base.vector_search import VectorItem
 from pinnacle.base.annotations import trigger
 from pinnacle.base.datalayer import Datalayer
 from pinnacle.base.document import Document
+from pinnacle.base.schema import Schema
 from pinnacle.components.cdc import CDC
-from pinnacle.components.component import Component
 from pinnacle.components.listener import Listener
-from pinnacle.components.model import Mapping
-from pinnacle.components.schema import Schema
 from pinnacle.components.table import Table
 from pinnacle.misc.special_dicts import MongoStyleDict
 
@@ -47,7 +45,7 @@ def backfill_vector_search(db, vi, searcher):
     :param vi: Identifier of vector index.
     :param searcher: FastVectorSearch instance to load model outputs as vectors.
     """
-    from pinnacle.components.datatype import _BaseEncodable
+    from pinnacle.base.datatype import _BaseEncodable
 
     logging.info(f"Loading vectors of vector-index: '{vi.identifier}'")
 
@@ -117,17 +115,6 @@ class VectorIndex(CDC):
         """Post-initialization method."""
         self.cdc_table = self.cdc_table or self.indexing_listener.outputs
         super().postinit()
-
-    # TODO why this?
-    def __hash__(self):
-        return hash((self.type_id, self.identifier))
-
-    def __eq__(self, other: t.Any):
-        if isinstance(other, Component):
-            return (
-                self.identifier == other.identifier and self.type_id and other.type_id
-            )
-        return False
 
     def _pre_create(self, db: Datalayer, startup_cache: t.Dict = {}):
         assert isinstance(self.indexing_listener, Listener)
@@ -263,10 +250,9 @@ class VectorIndex(CDC):
                 )
 
         model = models[model_name]
-        data = Mapping(key, model.signature)(document)
-        args, kwargs = model.handle_input_type(data, model.signature)
+        assert model.signature == 'singleton'
         return (
-            model.predict(*args, **kwargs),
+            model.predict(document[key]),
             model.identifier,
             key,
         )

pinnacle/misc/serialization.py
@@ -42,7 +42,7 @@ def asdict(obj, *, copy_method=copy.copy) -> t.Dict[str, t.Any]:
 
 def _asdict_inner(obj, dict_factory, copy_method, top=False) -> t.Any:
     from pinnacle.base.base import Base
-    from pinnacle.components.datatype import Saveable
+    from pinnacle.base.datatype import Saveable
 
     if type(obj) in _ATOMIC_TYPES:
         return obj

plugins/mongodb/superduper_mongodb/artifacts.py
@@ -6,7 +6,7 @@ from pathlib import Path
 import click
 import gridfs
 from pinnacle import CFG, logging
-from pinnacle.backends.base.artifacts import ArtifactStore
+from pinnacle.base.artifacts import ArtifactStore
 from tqdm import tqdm
 
 from pinnacle_mongodb.utils import connection_callback

plugins/sklearn/superduper_sklearn/model.py
@@ -4,9 +4,9 @@ import typing as t
 import numpy
 from sklearn.base import BaseEstimator
 from pinnacle import logging
-from pinnacle.backends.query_dataset import QueryDataset
 from pinnacle.base.datalayer import Datalayer
-from pinnacle.components.datatype import pickle_serializer
+from pinnacle.base.datatype import pickle_serializer
+from pinnacle.base.query_dataset import QueryDataset
 from pinnacle.components.model import (
     Model,
     ModelInputType,

plugins/sqlalchemy/superduper_sqlalchemy/metadata.py
@@ -19,7 +19,7 @@ from sqlalchemy import (
 from sqlalchemy.exc import ProgrammingError
 from sqlalchemy.orm import sessionmaker
 from pinnacle import logging
-from pinnacle.backends.base.metadata import MetaDataStore, NonExistentMetadataError
+from pinnacle.base.metadata import MetaDataStore, NonExistentMetadataError
 from pinnacle.components.component import Status
 
 from pinnacle_sqlalchemy.db_helper import get_db_config

test/unittest/backends/local/test_artifact_store.py
@@ -2,7 +2,7 @@ from test.utils.database import artifact_store as artifact_store_utils
 
 import pytest
 
-from pinnacle.backends.local.artifacts import FileSystemArtifactStore
+from pinnacle.base.artifacts import FileSystemArtifactStore
 
 
 @pytest.fixture

test/unittest/component/test_application.py
@@ -5,8 +5,8 @@ import numpy as np
 import pytest
 
 from pinnacle import ObjectModel, Schema, pinnacle
+from pinnacle.base.datatype import pickle_encoder
 from pinnacle.base.document import Document
-from pinnacle.components.datatype import pickle_encoder
 from pinnacle.components.listener import Listener
 from pinnacle.components.table import Table
 

test/utils/database/metadata.py
@@ -2,8 +2,8 @@ import uuid
 
 import pytest
 
-from pinnacle.backends.base.metadata import MetaDataStore
 from pinnacle.base.event import Job
+from pinnacle.base.metadata import MetaDataStore
 
 
 def test_component(metadata: MetaDataStore):