superduper-io/superduper

Rewrite services with simple and consistent structure

superduper/backends/base/compute.py
@@ -2,10 +2,11 @@ import typing as t
 from abc import abstractmethod
 
 from superduper.backends.base.backends import BaseBackend
-from superduper.base.event import Job
 
 if t.TYPE_CHECKING:
+    from superduper.components.component import Component
     from superduper.base.datalayer import Datalayer
+    from superduper.base.event import Job
 
 
 class ComputeBackend(BaseBackend):
@@ -18,12 +19,6 @@ class ComputeBackend(BaseBackend):
     # noqa
     """
 
-    @property
-    @abstractmethod
-    def type(self) -> str:
-        """Return the type of compute engine."""
-        pass
-
     @abstractmethod
     def release_futures(self, context: str):
         """Release futures from backend.
@@ -32,18 +27,12 @@ class ComputeBackend(BaseBackend):
         """
         pass
 
-    @property
-    @abstractmethod
-    def name(self) -> str:
-        """Return the name of current compute engine."""
-        pass
-
     def get_local_client(self):
         """Returns a local version of self."""
         pass
 
     @abstractmethod
-    def submit(self, job: Job) -> t.Any:
+    def submit(self, job: 'Job') -> t.Any:
         """
         Submits a function to the server for execution.
 
@@ -59,7 +48,7 @@ class ComputeBackend(BaseBackend):
     def initialize(self):
         """Connect to address."""
 
-    def create_handler(self, *args, **kwargs):
+    def put_component(self, component: 'Component'):
         """Create handler on component declare.
 
         :param args: *args for `create_handler`

superduper/base/event.py
@@ -4,6 +4,8 @@ import typing as t
 import uuid
 from abc import ABC, abstractmethod
 
+from superduper.backends.base.compute import ComputeBackend
+
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
 
@@ -63,6 +65,7 @@ class Signal(Event):
     def execute(
         self,
         db: 'Datalayer',
+        compute: ComputeBackend,
     ):
         """Execute the signal.
 
@@ -70,7 +73,7 @@ class Signal(Event):
 
         """
         if self.msg.lower() == 'done':
-            db.cluster.compute.release_futures(self.context)
+            compute.release_futures(self.context)
 
 
 @dc.dataclass(kw_only=True)
@@ -101,6 +104,7 @@ class Change(Event):
     def execute(
         self,
         db: 'Datalayer',
+        **kwargs,
     ):
         """Execute the change event.
 
@@ -132,7 +136,7 @@ class Create(Event):
     def component(self):
         return self.path.split('.')[-1]
 
-    def execute(self, db: 'Datalayer'):
+    def execute(self, db: 'Datalayer', **kwargs):
         """Execute the create event.
 
         :param db: Datalayer instance.
@@ -190,6 +194,7 @@ class Update(Event):
     def execute(
         self,
         db: 'Datalayer',
+        **kwargs,
     ):
         """Execute the create event.
 
@@ -251,7 +256,7 @@ class Job(Event):
 
         :param futures: dict of futures
         """
-        from superduper.backends.base.queue import Future
+        from superduper.backends.base.scheduler import Future
 
         dependencies = []
         if self.dependencies:
@@ -274,6 +279,7 @@ class Job(Event):
     def execute(
         self,
         db: 'Datalayer',
+        compute: ComputeBackend,
     ):
         """Execute the job event.
 
@@ -281,7 +287,7 @@ class Job(Event):
         """
         meta = {k: v for k, v in self.dict().items() if k not in {'genus', 'queue'}}
         db.metadata.create_job(meta)
-        return db.cluster.compute.submit(self)
+        return compute.submit(self)
 
 
 events = {

Pinnacle-Networks/Pinnacle

pinnacle/backends/base/compute.py
@@ -2,10 +2,11 @@ import typing as t
 from abc import abstractmethod
 
 from pinnacle.backends.base.backends import BaseBackend
-from pinnacle.base.event import Job
 
 if t.TYPE_CHECKING:
+    from pinnacle.components.component import Component
     from pinnacle.base.datalayer import Datalayer
+    from pinnacle.base.event import Job
 
 
 class ComputeBackend(BaseBackend):
@@ -18,12 +19,6 @@ class ComputeBackend(BaseBackend):
     # noqa
     """
 
-    @property
-    @abstractmethod
-    def type(self) -> str:
-        """Return the type of compute engine."""
-        pass
-
     @abstractmethod
     def release_futures(self, context: str):
         """Release futures from backend.
@@ -32,18 +27,12 @@ class ComputeBackend(BaseBackend):
         """
         pass
 
-    @property
-    @abstractmethod
-    def name(self) -> str:
-        """Return the name of current compute engine."""
-        pass
-
     def get_local_client(self):
         """Returns a local version of self."""
         pass
 
     @abstractmethod
-    def submit(self, job: Job) -> t.Any:
+    def submit(self, job: 'Job') -> t.Any:
         """
         Submits a function to the server for execution.
 
@@ -59,7 +48,7 @@ class ComputeBackend(BaseBackend):
     def initialize(self):
         """Connect to address."""
 
-    def create_handler(self, *args, **kwargs):
+    def put_component(self, component: 'Component'):
         """Create handler on component declare.
 
         :param args: *args for `create_handler`

pinnacle/base/event.py
@@ -4,6 +4,8 @@ import typing as t
 import uuid
 from abc import ABC, abstractmethod
 
+from pinnacle.backends.base.compute import ComputeBackend
+
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer
 
@@ -63,6 +65,7 @@ class Signal(Event):
     def execute(
         self,
         db: 'Datalayer',
+        compute: ComputeBackend,
     ):
         """Execute the signal.
 
@@ -70,7 +73,7 @@ class Signal(Event):
 
         """
         if self.msg.lower() == 'done':
-            db.cluster.compute.release_futures(self.context)
+            compute.release_futures(self.context)
 
 
 @dc.dataclass(kw_only=True)
@@ -101,6 +104,7 @@ class Change(Event):
     def execute(
         self,
         db: 'Datalayer',
+        **kwargs,
     ):
         """Execute the change event.
 
@@ -132,7 +136,7 @@ class Create(Event):
     def component(self):
         return self.path.split('.')[-1]
 
-    def execute(self, db: 'Datalayer'):
+    def execute(self, db: 'Datalayer', **kwargs):
         """Execute the create event.
 
         :param db: Datalayer instance.
@@ -190,6 +194,7 @@ class Update(Event):
     def execute(
         self,
         db: 'Datalayer',
+        **kwargs,
     ):
         """Execute the create event.
 
@@ -251,7 +256,7 @@ class Job(Event):
 
         :param futures: dict of futures
         """
-        from pinnacle.backends.base.queue import Future
+        from pinnacle.backends.base.scheduler import Future
 
         dependencies = []
         if self.dependencies:
@@ -274,6 +279,7 @@ class Job(Event):
     def execute(
         self,
         db: 'Datalayer',
+        compute: ComputeBackend,
     ):
         """Execute the job event.
 
@@ -281,7 +287,7 @@ class Job(Event):
         """
         meta = {k: v for k, v in self.dict().items() if k not in {'genus', 'queue'}}
         db.metadata.create_job(meta)
-        return db.cluster.compute.submit(self)
+        return compute.submit(self)
 
 
 events = {