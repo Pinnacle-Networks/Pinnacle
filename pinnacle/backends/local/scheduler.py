blythed/superduper

Fix CDC triggers for update, delete

superduper/backends/local/scheduler.py
@@ -10,6 +10,7 @@ from superduper.backends.base.scheduler import (
 )
 from superduper.base.event import Event
 from superduper.components.cdc import CDC
+from superduper.misc.importing import isreallyinstance
 
 
 class QueueWrapper:
@@ -64,16 +65,15 @@ class LocalScheduler(Bookkeeping, BaseScheduler):
 
     def initialize(self):
         """Initialize the scheduler."""
-        # TODO
-        # self.Q['_apply'] = []
-        # for component_data in self.db.show():
-        #     component = component_data['component']
-        #     identifier = component_data['identifier']
-        #     r = self.db.show(component=component, identifier=identifier, version=-1)
-        #     if r.get('cdc_table'):
-        #         self.put_component(component)
-        #         with self.lock:
-        #             self.Q[component, identifier] = []
+        self.Q['_apply'] = []
+        for component_data in self.db.show():
+            component = component_data['component']
+            identifier = component_data['identifier']
+            c = self.db.load(component=component, identifier=identifier)
+            if isreallyinstance(c, CDC):
+                self.put_component(c)
+                with self.lock:
+                    self.Q[component, identifier] = []
 
     def publish(self, events: t.List[Event]):
         """

superduper/base/base.py
@@ -106,6 +106,20 @@ class Base(metaclass=BaseMeta):
         out = Schema(fields=fields)
         return out
 
+    @lazy_classproperty
+    def table(cls):
+        from superduper.components.table import Table
+        from superduper import Component
+        from superduper.misc.importing import isreallyinstance
+
+        return Table(
+            identifier=cls.__name__,
+            fields=cls.class_schema.fields,
+            path=cls.__module__ + '.' + cls.__name__,
+            primary_id='uuid',
+            component=isreallyinstance(cls, Component),
+        )
+
     @staticmethod
     def get_cls_from_path(path):
         """Get class from a path.

Pinnacle-Networks/Pinnacle

pinnacle/backends/local/scheduler.py
@@ -10,6 +10,7 @@ from pinnacle.backends.base.scheduler import (
 )
 from pinnacle.base.event import Event
 from pinnacle.components.cdc import CDC
+from pinnacle.misc.importing import isreallyinstance
 
 
 class QueueWrapper:
@@ -64,16 +65,15 @@ class LocalScheduler(Bookkeeping, BaseScheduler):
 
     def initialize(self):
         """Initialize the scheduler."""
-        # TODO
-        # self.Q['_apply'] = []
-        # for component_data in self.db.show():
-        #     component = component_data['component']
-        #     identifier = component_data['identifier']
-        #     r = self.db.show(component=component, identifier=identifier, version=-1)
-        #     if r.get('cdc_table'):
-        #         self.put_component(component)
-        #         with self.lock:
-        #             self.Q[component, identifier] = []
+        self.Q['_apply'] = []
+        for component_data in self.db.show():
+            component = component_data['component']
+            identifier = component_data['identifier']
+            c = self.db.load(component=component, identifier=identifier)
+            if isreallyinstance(c, CDC):
+                self.put_component(c)
+                with self.lock:
+                    self.Q[component, identifier] = []
 
     def publish(self, events: t.List[Event]):
         """

pinnacle/base/base.py
@@ -106,6 +106,20 @@ class Base(metaclass=BaseMeta):
         out = Schema(fields=fields)
         return out
 
+    @lazy_classproperty
+    def table(cls):
+        from pinnacle.components.table import Table
+        from pinnacle import Component
+        from pinnacle.misc.importing import isreallyinstance
+
+        return Table(
+            identifier=cls.__name__,
+            fields=cls.class_schema.fields,
+            path=cls.__module__ + '.' + cls.__name__,
+            primary_id='uuid',
+            component=isreallyinstance(cls, Component),
+        )
+
     @staticmethod
     def get_cls_from_path(path):
         """Get class from a path.