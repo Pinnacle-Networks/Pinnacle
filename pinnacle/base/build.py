superduper-io/superduper

Alternative metadata store + inmemory store

superduper/base/build.py
@@ -12,6 +12,7 @@ from superduper.base.artifacts import (
 )
 from superduper.base.config import Config
 from superduper.base.datalayer import Datalayer
+from superduper.base.metadata import MetaDataStore
 from superduper.misc.anonymize import anonymize_url
 from superduper.misc.importing import load_plugin
 
@@ -62,6 +63,7 @@ class _DataBackendLoader(_Loader):
         r'^mssql://': ('sql', 'base'),
         r'^mysql://': ('sql', 'base'),
         r'^redis://': ('redis', 'base'),
+        r'^inmemory://': ('inmemory', 'base'),
     }
 
 
@@ -98,18 +100,30 @@ def build_datalayer(
 
     artifact_store = _build_artifact_store()
 
+    metadata = _build_databackend(cfg.metadata_store or cfg.data_backend)
+
     backend = getattr(load_plugin(cfg.cluster_engine), 'Cluster')
+
     cluster = backend.build(cfg, compute=compute)
+
+    metadata = Datalayer(
+        databackend=metadata,
+        cluster=None,
+        artifact_store=artifact_store,
+        metadata=None,
+    )
 
     datalayer = Datalayer(
         databackend=databackend_obj,
         artifact_store=artifact_store,
         cluster=cluster,
+        metadata=metadata,
     )
     # Keep the real configuration in the datalayer object.
     datalayer.cfg = cfg
 
     if kwargs.get('initialize_cluster', True):
+        assert datalayer.cluster is not None
         datalayer.cluster.initialize()
 
     show_configuration(cfg)
@@ -126,9 +140,9 @@ def show_configuration(cfg):
     table = PrettyTable()
     table.field_names = ["Configuration", "Value"]
     key_values = [
-        ('Data Backend', anonymize_url(cfg.data_backend)),
-        ('Artifact Store', anonymize_url(cfg.artifact_store)),
-        ('Cache', anonymize_url(cfg.cache)),
+        ('DataBackend', anonymize_url(cfg.data_backend)),
+        ('ArtifactStore', anonymize_url(cfg.artifact_store)),
+        ('Metadata', anonymize_url(cfg.metadata_store)),
     ]
     for key, value in key_values:
         if value:

plugins/redis/superduper_redis/data_backend.py
@@ -1,18 +1,12 @@
-import typing as t
-import uuid
+import json
 
 import click
-import numpy
-import pandas
 import redis
-from redis.commands.json import JSON
-from superduper import CFG
-from superduper.backends.base.data_backend import BaseDataBackend
-from superduper.base.query import Query, QueryPart
-from superduper.base.schema import Schema
+from superduper import logging
+from superduper.backends.base.data_backend import KeyedDatabackend
 
 
-class RedisDataBackend(BaseDataBackend):
+class RedisDataBackend(KeyedDatabackend):
     """Redis data backend for SuperDuper.
 
     :param uri: The Redis URI.
@@ -20,284 +14,42 @@ class RedisDataBackend(BaseDataBackend):
     :param flavour: The flavour of the backend.
     """
 
-    def __init__(self, uri: str, plugin: t.Any, flavour: t.Optional[str] = None):
-        super().__init__(uri, plugin, flavour)
+    def __init__(self, uri: str, *args, **kwargs):
+        super().__init__(uri, *args, **kwargs)
         self.reconnect()
 
-    def reconnect(self):
-        """Redis connection."""
-        self.conn = redis.Redis.from_url(self.uri)
-        self.json_client = JSON(self.conn)
-
-    def create_table_and_schema(self, identifier: str, schema: Schema, primary_id: str):
-        """
-        Create a table and schema in Redis.
-
-        :param identifier: The name of the table.
-        :param schema: The schema of the table.
-        :param primary_id: The primary key field.
-        """
-        pass
-
-    def drop_table(self, table: str):
-        """Drop a table in Redis.
-
-        :param table: The name of the table to drop.
-        """
-        keys = self.conn.keys(f"{table}:*")
-        self.conn.delete(*keys)
-
     def drop(self, force: bool = False):
-        """Drop the entire Redis DB.
+        """Drop the in-memory store.
 
-        :param force: If True, drop the DB without confirmation.
+        :param force: Force drop the in-memory store.
         """
-        if not force:
-            if not click.confirm("Are you sure you want to drop the entire Redis DB?"):
-                return
-
-        self.conn.flushall()
-
-    def list_tables(self) -> t.List[str]:
-        """List all tables in Redis."""
-        keys = self.conn.keys('*')
-        return sorted(list(set(x.decode('utf-8').split(':')[0] for x in keys)))
-
-    def get_table(self, identifier):
-        """Get a table from Redis."""
-        pass
-
-    def check_output_dest(self, predict_id: str) -> bool:
-        """Check if the output destination exists in Redis.
-
-        :param predict_id: The ID of the prediction output.
-        """
-        tbl = CFG.output_prefix + predict_id
-        return tbl in self.list_tables()
-
-    def random_id(self) -> str:
-        """Generate a random ID for Redis documents."""
-        return str(uuid.uuid4()).replace('-', '')[:24]
-
-    def primary_id(self, query: Query) -> str:
-        """Get the primary ID field for a query."""
-        return self.id_field
-
-    def insert(self, table: str, documents: t.Sequence[t.Dict]) -> t.List[str]:
-        """Insert documents into a Redis table.
-
-        :param table: The name of the table.
-        """
-        if not documents:
-            return []
-        inserted_ids = []
-        for doc in documents:
-            if self.id_field not in doc:
-                doc[self.id_field] = self.random_id()
-            id = f'{table}:{doc[self.id_field]}'
-            self.json_client.jsonset(id, '$', doc)
-            inserted_ids.append(doc[self.id_field])
-        return inserted_ids
-
-    def replace(self, table: str, condition: t.Dict, r: t.Dict) -> t.List[str]:
-        """Replace documents in a Redis table.
-
-        :param table: The name of the table.
-        :param condition: The condition to match documents.
-        """
-        df = self._get_table_df(table)
-
-        query = []
-        for k in condition:
-            query.append(df[k] == condition[k])
-
-        if len(query) > 1:
-            query_combined = numpy.logical_and(*query)
-        else:
-            query_combined = query[0]
-
-        ids = df[query_combined][self.id_field].tolist()
-
-        for id in ids:
-            s = self.json_client.jsonget(f'{table}:{id}')
-            s.update(r)
-            self.json_client.jsonset(f'{table}:{id}', '$', s)
-
-        return ids
-
-    def update(self, table: str, condition: t.Dict, key: str, value: t.Any):
-        """Update documents in a Redis table.
-
-        :param table: The name of the table.
-        :param condition: The condition to match documents.
-        :param key: The key to update.
-        :param value: The new value for the key.
-        """
-        df = self._get_table_df(table)
-        query = []
-        for k in condition:
-            query.append(df[k] == condition[k])
-
-        if len(query) > 1:
-            query_combined = numpy.logical_and(*query)
-        else:
-            query_combined = query[0]
-
-        ids = df[query_combined][self.id_field].tolist()
-        for id in ids:
-            r = self.json_client.jsonget(f'{table}:{id}')
-            r[key] = value
-            self.json_client.jsonset(f'{table}:{id}', '$', r)
-        return ids
-
-    def delete(self, table: str, condition: t.Dict):
-        """Delete documents from a Redis table.
-
-        :param table: The name of the table.
-        :param condition: The condition to match documents.
-        """
-        df = self._get_table_df(table)
-
-        if df.empty:
+        if not force and not click.confirm(
+            'Are you sure you want to drop the in-memory store?'
+        ):
+            logging.warning('Aborting drop of in-memory store.')
             return
+        self.conn.flushdb()
 
-        query = []
-        for k in condition:
-            query.append(df[k] == condition[k])
+    def keys(self, *pattern: str):
+        """Get the keys in the in-memory store.
 
-        if len(query) > 1:
-            query_combined = numpy.logical_and(*query)
-        else:
-            query_combined = query[0]
+        :param pattern: Pattern to match the keys.
+        """
+        matches = self.conn.keys('/'.join(pattern))
+        return [tuple(x.split('/')) for x in matches]
 
-        ids = df[query_combined][self.id_field].tolist()
-
-        for id in ids:
-            self.conn.delete(f'{table}:{id}')
-
-    def _build_pandas_df(self, query):
-
-        q = self._get_table_df(query.table)
-        pid = None
-        predict_ids = (
-            query.decomposition.outputs.args if query.decomposition.outputs else []
+    def reconnect(self):
+        """Reconnect to the in-memory store."""
+        self.conn = redis.from_url(
+            self.uri,
+            decode_responses=True,
         )
 
-        if q.empty:
-            return q
+    def __delitem__(self, key):
+        self.conn.delete('/'.join(key))
 
-        for part in query.parts:
-            if isinstance(part, QueryPart) and part.name != 'outputs':
-                args = []
-                for a in part.args:
-                    if isinstance(a, Query) and str(a).endswith('.primary_id'):
-                        args.append(self.id_field)
-                    elif isinstance(a, Query):
-                        args.append(self._build_pandas_df(a))
-                    else:
-                        args.append(a)
+    def __setitem__(self, key, value):
+        self.conn.set('/'.join(key), json.dumps(value))
 
-                kwargs = {}
-                for k, v in part.kwargs.items():
-                    if isinstance(a, Query) and str(a).endswith('.primary_id'):
-                        args.append(self.id_field)
-                    elif isinstance(v, Query):
-                        kwargs[k] = self._build_pandas_df(v)
-                    else:
-                        kwargs[k] = v
-
-                if part.name == 'select' and len(args) == 0:
-                    pass
-
-                else:
-                    if part.name == 'select' and predict_ids and args:
-                        args.extend(
-                            [
-                                f'{CFG.output_prefix}{pid}'
-                                for pid in predict_ids
-                                if f'{CFG.output_prefix}{pid}' not in args
-                            ]
-                        )
-                        args = list(set(args))
-
-                    name = part.name
-                    if name == 'limit':
-                        name = 'head'
-
-                    elif name == 'filter':
-                        out = args[0]
-                        if len(args) > 1:
-                            for i in range(1, len(args)):
-                                out = out & args[i]
-                        q = q[out]
-
-                    elif name == 'select':
-                        assert all(isinstance(a, str) for a in args)
-                        if not q.empty:
-                            q = q[args]
-                    else:
-                        q = getattr(q, name)(*args, **kwargs)
-
-            elif isinstance(part, QueryPart) and part.name == 'outputs':
-                if pid is None:
-                    pid = self.id_field
-
-                for predict_id in part.args:
-                    output_t = self._get_table_df(f"{CFG.output_prefix}{predict_id}")[
-                        [f"{CFG.output_prefix}{predict_id}", "_source"]
-                    ]
-                    q = q.merge(output_t, left_on=pid, right_on='_source')
-
-            elif isinstance(part, str):
-                if part == 'primary_id':
-                    if pid is None:
-                        pid = self.id_field
-                    part = pid
-                q = q[part]
-            else:
-                raise ValueError(f'Unknown query part: {part}')
-
-        return q
-
-    def _get_table_df(self, table: str):
-        keys = self.conn.keys(f"{table}:*")
-
-        relevant_set = []
-        for k in keys:
-            relevant_set.append(self.json_client.jsonget(k))
-
-        return pandas.DataFrame(relevant_set)
-
-    def select(self, query: Query) -> t.List[t.Dict]:
-        """Select documents from a Redis table.
-
-        :param query: The query to execute.
-        """
-        df = self._build_pandas_df(query)
-        return df.to_dict(orient='records')
-
-    def missing_outputs(self, query: Query, predict_id: str) -> t.List[str]:
-        """Select documents with missing outputs.
-
-        :param query: The query to execute.
-        :param predict_id: The ID of the prediction output.
-        """
-        pid = self.primary_id(query)
-        df = self._build_pandas_df(query)
-        output_df = self._get_table_df(f'{CFG.output_prefix + predict_id}')
-        columns = output_df.columns
-        columns = [c for c in columns if c != '"id"']
-        output_df = output_df[columns]
-
-        if output_df.empty:
-            return df[pid].tolist()
-
-        # TODO - map to pandas query
-        joined_df = df.join(
-            output_df, df[pid] == output_df['_source'], join_type="left"
-        )
-        return joined_df[joined_df['_source'].isnan()][self.id_field].tolist()
-
-    def execute_native(self, query: str):
-        """Execute a native Redis command (not-implemented)."""
-        raise NotImplementedError
+    def __getitem__(self, key):
+        return json.loads(self.conn.get('/'.join(key)))

Pinnacle-Networks/Pinnacle

pinnacle/base/build.py
@@ -12,6 +12,7 @@ from pinnacle.base.artifacts import (
 )
 from pinnacle.base.config import Config
 from pinnacle.base.datalayer import Datalayer
+from pinnacle.base.metadata import MetaDataStore
 from pinnacle.misc.anonymize import anonymize_url
 from pinnacle.misc.importing import load_plugin
 
@@ -62,6 +63,7 @@ class _DataBackendLoader(_Loader):
         r'^mssql://': ('sql', 'base'),
         r'^mysql://': ('sql', 'base'),
         r'^redis://': ('redis', 'base'),
+        r'^inmemory://': ('inmemory', 'base'),
     }
 
 
@@ -98,18 +100,30 @@ def build_datalayer(
 
     artifact_store = _build_artifact_store()
 
+    metadata = _build_databackend(cfg.metadata_store or cfg.data_backend)
+
     backend = getattr(load_plugin(cfg.cluster_engine), 'Cluster')
+
     cluster = backend.build(cfg, compute=compute)
+
+    metadata = Datalayer(
+        databackend=metadata,
+        cluster=None,
+        artifact_store=artifact_store,
+        metadata=None,
+    )
 
     datalayer = Datalayer(
         databackend=databackend_obj,
         artifact_store=artifact_store,
         cluster=cluster,
+        metadata=metadata,
     )
     # Keep the real configuration in the datalayer object.
     datalayer.cfg = cfg
 
     if kwargs.get('initialize_cluster', True):
+        assert datalayer.cluster is not None
         datalayer.cluster.initialize()
 
     show_configuration(cfg)
@@ -126,9 +140,9 @@ def show_configuration(cfg):
     table = PrettyTable()
     table.field_names = ["Configuration", "Value"]
     key_values = [
-        ('Data Backend', anonymize_url(cfg.data_backend)),
-        ('Artifact Store', anonymize_url(cfg.artifact_store)),
-        ('Cache', anonymize_url(cfg.cache)),
+        ('DataBackend', anonymize_url(cfg.data_backend)),
+        ('ArtifactStore', anonymize_url(cfg.artifact_store)),
+        ('Metadata', anonymize_url(cfg.metadata_store)),
     ]
     for key, value in key_values:
         if value:

plugins/redis/superduper_redis/data_backend.py
@@ -1,18 +1,12 @@
-import typing as t
-import uuid
+import json
 
 import click
-import numpy
-import pandas
 import redis
-from redis.commands.json import JSON
-from pinnacle import CFG
-from pinnacle.backends.base.data_backend import BaseDataBackend
-from pinnacle.base.query import Query, QueryPart
-from pinnacle.base.schema import Schema
+from pinnacle import logging
+from pinnacle.backends.base.data_backend import KeyedDatabackend
 
 
-class RedisDataBackend(BaseDataBackend):
+class RedisDataBackend(KeyedDatabackend):
     """Redis data backend for SuperDuper.
 
     :param uri: The Redis URI.
@@ -20,284 +14,42 @@ class RedisDataBackend(BaseDataBackend):
     :param flavour: The flavour of the backend.
     """
 
-    def __init__(self, uri: str, plugin: t.Any, flavour: t.Optional[str] = None):
-        super().__init__(uri, plugin, flavour)
+    def __init__(self, uri: str, *args, **kwargs):
+        super().__init__(uri, *args, **kwargs)
         self.reconnect()
 
-    def reconnect(self):
-        """Redis connection."""
-        self.conn = redis.Redis.from_url(self.uri)
-        self.json_client = JSON(self.conn)
-
-    def create_table_and_schema(self, identifier: str, schema: Schema, primary_id: str):
-        """
-        Create a table and schema in Redis.
-
-        :param identifier: The name of the table.
-        :param schema: The schema of the table.
-        :param primary_id: The primary key field.
-        """
-        pass
-
-    def drop_table(self, table: str):
-        """Drop a table in Redis.
-
-        :param table: The name of the table to drop.
-        """
-        keys = self.conn.keys(f"{table}:*")
-        self.conn.delete(*keys)
-
     def drop(self, force: bool = False):
-        """Drop the entire Redis DB.
+        """Drop the in-memory store.
 
-        :param force: If True, drop the DB without confirmation.
+        :param force: Force drop the in-memory store.
         """
-        if not force:
-            if not click.confirm("Are you sure you want to drop the entire Redis DB?"):
-                return
-
-        self.conn.flushall()
-
-    def list_tables(self) -> t.List[str]:
-        """List all tables in Redis."""
-        keys = self.conn.keys('*')
-        return sorted(list(set(x.decode('utf-8').split(':')[0] for x in keys)))
-
-    def get_table(self, identifier):
-        """Get a table from Redis."""
-        pass
-
-    def check_output_dest(self, predict_id: str) -> bool:
-        """Check if the output destination exists in Redis.
-
-        :param predict_id: The ID of the prediction output.
-        """
-        tbl = CFG.output_prefix + predict_id
-        return tbl in self.list_tables()
-
-    def random_id(self) -> str:
-        """Generate a random ID for Redis documents."""
-        return str(uuid.uuid4()).replace('-', '')[:24]
-
-    def primary_id(self, query: Query) -> str:
-        """Get the primary ID field for a query."""
-        return self.id_field
-
-    def insert(self, table: str, documents: t.Sequence[t.Dict]) -> t.List[str]:
-        """Insert documents into a Redis table.
-
-        :param table: The name of the table.
-        """
-        if not documents:
-            return []
-        inserted_ids = []
-        for doc in documents:
-            if self.id_field not in doc:
-                doc[self.id_field] = self.random_id()
-            id = f'{table}:{doc[self.id_field]}'
-            self.json_client.jsonset(id, '$', doc)
-            inserted_ids.append(doc[self.id_field])
-        return inserted_ids
-
-    def replace(self, table: str, condition: t.Dict, r: t.Dict) -> t.List[str]:
-        """Replace documents in a Redis table.
-
-        :param table: The name of the table.
-        :param condition: The condition to match documents.
-        """
-        df = self._get_table_df(table)
-
-        query = []
-        for k in condition:
-            query.append(df[k] == condition[k])
-
-        if len(query) > 1:
-            query_combined = numpy.logical_and(*query)
-        else:
-            query_combined = query[0]
-
-        ids = df[query_combined][self.id_field].tolist()
-
-        for id in ids:
-            s = self.json_client.jsonget(f'{table}:{id}')
-            s.update(r)
-            self.json_client.jsonset(f'{table}:{id}', '$', s)
-
-        return ids
-
-    def update(self, table: str, condition: t.Dict, key: str, value: t.Any):
-        """Update documents in a Redis table.
-
-        :param table: The name of the table.
-        :param condition: The condition to match documents.
-        :param key: The key to update.
-        :param value: The new value for the key.
-        """
-        df = self._get_table_df(table)
-        query = []
-        for k in condition:
-            query.append(df[k] == condition[k])
-
-        if len(query) > 1:
-            query_combined = numpy.logical_and(*query)
-        else:
-            query_combined = query[0]
-
-        ids = df[query_combined][self.id_field].tolist()
-        for id in ids:
-            r = self.json_client.jsonget(f'{table}:{id}')
-            r[key] = value
-            self.json_client.jsonset(f'{table}:{id}', '$', r)
-        return ids
-
-    def delete(self, table: str, condition: t.Dict):
-        """Delete documents from a Redis table.
-
-        :param table: The name of the table.
-        :param condition: The condition to match documents.
-        """
-        df = self._get_table_df(table)
-
-        if df.empty:
+        if not force and not click.confirm(
+            'Are you sure you want to drop the in-memory store?'
+        ):
+            logging.warning('Aborting drop of in-memory store.')
             return
+        self.conn.flushdb()
 
-        query = []
-        for k in condition:
-            query.append(df[k] == condition[k])
+    def keys(self, *pattern: str):
+        """Get the keys in the in-memory store.
 
-        if len(query) > 1:
-            query_combined = numpy.logical_and(*query)
-        else:
-            query_combined = query[0]
+        :param pattern: Pattern to match the keys.
+        """
+        matches = self.conn.keys('/'.join(pattern))
+        return [tuple(x.split('/')) for x in matches]
 
-        ids = df[query_combined][self.id_field].tolist()
-
-        for id in ids:
-            self.conn.delete(f'{table}:{id}')
-
-    def _build_pandas_df(self, query):
-
-        q = self._get_table_df(query.table)
-        pid = None
-        predict_ids = (
-            query.decomposition.outputs.args if query.decomposition.outputs else []
+    def reconnect(self):
+        """Reconnect to the in-memory store."""
+        self.conn = redis.from_url(
+            self.uri,
+            decode_responses=True,
         )
 
-        if q.empty:
-            return q
+    def __delitem__(self, key):
+        self.conn.delete('/'.join(key))
 
-        for part in query.parts:
-            if isinstance(part, QueryPart) and part.name != 'outputs':
-                args = []
-                for a in part.args:
-                    if isinstance(a, Query) and str(a).endswith('.primary_id'):
-                        args.append(self.id_field)
-                    elif isinstance(a, Query):
-                        args.append(self._build_pandas_df(a))
-                    else:
-                        args.append(a)
+    def __setitem__(self, key, value):
+        self.conn.set('/'.join(key), json.dumps(value))
 
-                kwargs = {}
-                for k, v in part.kwargs.items():
-                    if isinstance(a, Query) and str(a).endswith('.primary_id'):
-                        args.append(self.id_field)
-                    elif isinstance(v, Query):
-                        kwargs[k] = self._build_pandas_df(v)
-                    else:
-                        kwargs[k] = v
-
-                if part.name == 'select' and len(args) == 0:
-                    pass
-
-                else:
-                    if part.name == 'select' and predict_ids and args:
-                        args.extend(
-                            [
-                                f'{CFG.output_prefix}{pid}'
-                                for pid in predict_ids
-                                if f'{CFG.output_prefix}{pid}' not in args
-                            ]
-                        )
-                        args = list(set(args))
-
-                    name = part.name
-                    if name == 'limit':
-                        name = 'head'
-
-                    elif name == 'filter':
-                        out = args[0]
-                        if len(args) > 1:
-                            for i in range(1, len(args)):
-                                out = out & args[i]
-                        q = q[out]
-
-                    elif name == 'select':
-                        assert all(isinstance(a, str) for a in args)
-                        if not q.empty:
-                            q = q[args]
-                    else:
-                        q = getattr(q, name)(*args, **kwargs)
-
-            elif isinstance(part, QueryPart) and part.name == 'outputs':
-                if pid is None:
-                    pid = self.id_field
-
-                for predict_id in part.args:
-                    output_t = self._get_table_df(f"{CFG.output_prefix}{predict_id}")[
-                        [f"{CFG.output_prefix}{predict_id}", "_source"]
-                    ]
-                    q = q.merge(output_t, left_on=pid, right_on='_source')
-
-            elif isinstance(part, str):
-                if part == 'primary_id':
-                    if pid is None:
-                        pid = self.id_field
-                    part = pid
-                q = q[part]
-            else:
-                raise ValueError(f'Unknown query part: {part}')
-
-        return q
-
-    def _get_table_df(self, table: str):
-        keys = self.conn.keys(f"{table}:*")
-
-        relevant_set = []
-        for k in keys:
-            relevant_set.append(self.json_client.jsonget(k))
-
-        return pandas.DataFrame(relevant_set)
-
-    def select(self, query: Query) -> t.List[t.Dict]:
-        """Select documents from a Redis table.
-
-        :param query: The query to execute.
-        """
-        df = self._build_pandas_df(query)
-        return df.to_dict(orient='records')
-
-    def missing_outputs(self, query: Query, predict_id: str) -> t.List[str]:
-        """Select documents with missing outputs.
-
-        :param query: The query to execute.
-        :param predict_id: The ID of the prediction output.
-        """
-        pid = self.primary_id(query)
-        df = self._build_pandas_df(query)
-        output_df = self._get_table_df(f'{CFG.output_prefix + predict_id}')
-        columns = output_df.columns
-        columns = [c for c in columns if c != '"id"']
-        output_df = output_df[columns]
-
-        if output_df.empty:
-            return df[pid].tolist()
-
-        # TODO - map to pandas query
-        joined_df = df.join(
-            output_df, df[pid] == output_df['_source'], join_type="left"
-        )
-        return joined_df[joined_df['_source'].isnan()][self.id_field].tolist()
-
-    def execute_native(self, query: str):
-        """Execute a native Redis command (not-implemented)."""
-        raise NotImplementedError
+    def __getitem__(self, key):
+        return json.loads(self.conn.get('/'.join(key)))