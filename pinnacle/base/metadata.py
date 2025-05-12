superduper-io/superduper

Improve exception handling
Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

superduper/base/metadata.py
@@ -12,8 +12,13 @@ from superduper import logging
 from superduper.base import exceptions
 from superduper.base.base import Base
 from superduper.base.schema import Schema
-from superduper.base.status import JOB_PHASE_UNINITIALIZED, JOB_PHASE_PENDING, JOB_PHASE_RUNNING, JOB_PHASE_FAILED, \
-    JOB_PHASE_SUCCESS
+from superduper.base.status import (
+    JOB_PHASE_FAILED,
+    JOB_PHASE_PENDING,
+    JOB_PHASE_RUNNING,
+    JOB_PHASE_SUCCESS,
+    JOB_PHASE_UNINITIALIZED,
+)
 from superduper.components.cdc import CDC
 from superduper.components.component import Component, init_status
 from superduper.components.table import Table
@@ -72,11 +77,17 @@ class Job(Base):
 
             # Check if timeout has been exceeded
             if elapsed_time > timeout:
-                if status['phase'] == JOB_PHASE_UNINITIALIZED or status['phase'] == JOB_PHASE_PENDING:
-                    raise Exception(
-                        f'Job {self.job_id} remained in {status["phase"]} state for more than {timeout} seconds')
+                if (
+                    status['phase'] == JOB_PHASE_UNINITIALIZED
+                    or status['phase'] == JOB_PHASE_PENDING
+                ):
+                    # FIXME: should we even raise an error in this case, or just abort the job ?
+                    err_msg = f'Job {self.job_id} remained in {status["phase"]} state for more than {timeout} seconds'
+                    raise exceptions.TimeoutError(err_msg)
                 else:
-                    raise Exception(f'Job {self.job_id} timed out after {timeout} seconds')
+                    # FIXME: should we even raise an error in this case, or just abort the job ?
+                    err_msg = f'Job {self.job_id} timed out after {timeout} seconds'
+                    raise exceptions.TimeoutError(err_msg)
 
             if status['phase'] == JOB_PHASE_UNINITIALIZED:
                 logging.info(f'Job {self.job_id} is uninitialized')
@@ -90,7 +101,9 @@ class Job(Base):
 
         logging.info(f'Job {self.job_id} finished with status: {status}')
         if status['phase'] == JOB_PHASE_FAILED:
-            raise Exception(f'Job {self.job_id} failed: {status}')
+            # FIXME: should we even raise an error in this case, or just abort the job ?
+            err_msg = f'Job {self.job_id} failed: {status}'
+            raise exceptions.InternalError(err_msg)
 
         return status
 
@@ -129,17 +142,18 @@ class Job(Base):
 
         :param db: Datalayer instance
         """
-
-        db.metadata.set_job_status(self.job_id,
-                                   {
-                                       "phase": JOB_PHASE_RUNNING,
-                                       "reason": "RANDOM DOKIMI",
-                                   },
-                                   )
+        db.metadata.set_job_status(
+            self.job_id,
+            {
+                "phase": JOB_PHASE_RUNNING,
+                "reason": "RANDOM DOKIMI",
+            },
+        )
 
         try:
             logging.info(f'Running job {self.job_id}')
-            db.metadata.set_job_status(self.job_id,
+            db.metadata.set_job_status(
+                self.job_id,
                 {
                     "phase": JOB_PHASE_RUNNING,
                     "reason": "job started",
@@ -152,8 +166,11 @@ class Job(Base):
             method = getattr(component, self.method)
             method(*self.args, **self.kwargs)
         except Exception as e:
-            logging.error(f'Error running job {self.huuid}: {e}. Traceback: {traceback.format_exc()}')
-            db.metadata.set_job_status(self.job_id,
+            logging.error(
+                f'Error running job {self.huuid}: {e}. Traceback: {traceback.format_exc()}'
+            )
+            db.metadata.set_job_status(
+                self.job_id,
                 {
                     "phase": JOB_PHASE_FAILED,
                     "reason": f"job failed: {str(e)}",
@@ -548,19 +565,24 @@ class MetaDataStore:
         status = merge_dicts(job['status'], status_update)
 
         children = status.get('children', {})
-        failed_children = [v for v in children.values() if v.get('phase') == JOB_PHASE_FAILED]
+        failed_children = [
+            v for v in children.values() if v.get('phase') == JOB_PHASE_FAILED
+        ]
 
         if failed_children:
             status['phase'] = JOB_PHASE_FAILED
             n_failed = len(failed_children)
-            status['reason'] = f"{n_failed} {'child' if n_failed == 1 else 'children'} failed"
+            status['reason'] = (
+                f"{n_failed} {'child' if n_failed == 1 else 'children'} failed"
+            )
 
         self.db['Job'].update({'job_id': job_id}, 'status', status)
 
         if status['phase'] == JOB_PHASE_FAILED:
             key = f"Job/{job['component']}/{job['identifier']}/{job['uuid']}.{job['method']}"
-            self.set_component_status(job['component'], job['uuid'], {'children': {key: status}})
-
+            self.set_component_status(
+                job['component'], job['uuid'], {'children': {key: status}}
+            )
 
     def set_component_status(self, component: str, uuid: str, status_update: t.Dict):
         """
@@ -574,11 +596,15 @@ class MetaDataStore:
         status = merge_dicts(r['status'], status_update)
 
         if status['children'] and any(
-                v['phase'] == JOB_PHASE_FAILED for v in status['children'].values()
+            v['phase'] == JOB_PHASE_FAILED for v in status['children'].values()
         ):
             status['phase'] = JOB_PHASE_FAILED
             n_failed = len(
-                [v for v in status['children'].values() if v['phase'] == JOB_PHASE_FAILED]
+                [
+                    v
+                    for v in status['children'].values()
+                    if v['phase'] == JOB_PHASE_FAILED
+                ]
             )
             status['reason'] = '{} {} failed'.format(
                 n_failed, 'child' if n_failed == 1 else 'children'
@@ -691,7 +717,6 @@ class MetaDataStore:
 
     def show_cdc_tables(self):
         """List the tables used for CDC."""
-
         metadata = self.db['Table'].execute()
 
         cdc_classes = []

superduper/components/component.py
@@ -17,11 +17,16 @@ from superduper import logging
 from superduper.base.annotations import trigger
 from superduper.base.base import Base, BaseMeta
 from superduper.base.constant import KEY_BLOBS, KEY_FILES, LENGTH_UUID
-from superduper.base.status import init_status, JOB_PHASE_PENDING, JOB_PHASE_FAILED, running_status, JOB_PHASE_RUNNING
+from superduper.base.status import (
+    JOB_PHASE_FAILED,
+    JOB_PHASE_PENDING,
+    JOB_PHASE_RUNNING,
+    init_status,
+    running_status,
+)
 from superduper.misc.annotations import lazy_classproperty
 from superduper.misc.importing import isreallyinstance
 from superduper.misc.utils import hash_item
-
 
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer

test/unittest/component/test_component.py
@@ -14,7 +14,7 @@ from superduper.base.datatype import (
     _Artifact,
     dill_serializer,
 )
-from superduper.base.metadata import Job, JOB_PHASE_FAILED
+from superduper.base.metadata import JOB_PHASE_FAILED, Job
 from superduper.components.component import Component
 from superduper.components.listener import Listener
 

Pinnacle-Networks/Pinnacle

pinnacle/base/metadata.py
@@ -12,8 +12,13 @@ from pinnacle import logging
 from pinnacle.base import exceptions
 from pinnacle.base.base import Base
 from pinnacle.base.schema import Schema
-from pinnacle.base.status import JOB_PHASE_UNINITIALIZED, JOB_PHASE_PENDING, JOB_PHASE_RUNNING, JOB_PHASE_FAILED, \
-    JOB_PHASE_SUCCESS
+from pinnacle.base.status import (
+    JOB_PHASE_FAILED,
+    JOB_PHASE_PENDING,
+    JOB_PHASE_RUNNING,
+    JOB_PHASE_SUCCESS,
+    JOB_PHASE_UNINITIALIZED,
+)
 from pinnacle.components.cdc import CDC
 from pinnacle.components.component import Component, init_status
 from pinnacle.components.table import Table
@@ -72,11 +77,17 @@ class Job(Base):
 
             # Check if timeout has been exceeded
             if elapsed_time > timeout:
-                if status['phase'] == JOB_PHASE_UNINITIALIZED or status['phase'] == JOB_PHASE_PENDING:
-                    raise Exception(
-                        f'Job {self.job_id} remained in {status["phase"]} state for more than {timeout} seconds')
+                if (
+                    status['phase'] == JOB_PHASE_UNINITIALIZED
+                    or status['phase'] == JOB_PHASE_PENDING
+                ):
+                    # FIXME: should we even raise an error in this case, or just abort the job ?
+                    err_msg = f'Job {self.job_id} remained in {status["phase"]} state for more than {timeout} seconds'
+                    raise exceptions.TimeoutError(err_msg)
                 else:
-                    raise Exception(f'Job {self.job_id} timed out after {timeout} seconds')
+                    # FIXME: should we even raise an error in this case, or just abort the job ?
+                    err_msg = f'Job {self.job_id} timed out after {timeout} seconds'
+                    raise exceptions.TimeoutError(err_msg)
 
             if status['phase'] == JOB_PHASE_UNINITIALIZED:
                 logging.info(f'Job {self.job_id} is uninitialized')
@@ -90,7 +101,9 @@ class Job(Base):
 
         logging.info(f'Job {self.job_id} finished with status: {status}')
         if status['phase'] == JOB_PHASE_FAILED:
-            raise Exception(f'Job {self.job_id} failed: {status}')
+            # FIXME: should we even raise an error in this case, or just abort the job ?
+            err_msg = f'Job {self.job_id} failed: {status}'
+            raise exceptions.InternalError(err_msg)
 
         return status
 
@@ -129,17 +142,18 @@ class Job(Base):
 
         :param db: Datalayer instance
         """
-
-        db.metadata.set_job_status(self.job_id,
-                                   {
-                                       "phase": JOB_PHASE_RUNNING,
-                                       "reason": "RANDOM DOKIMI",
-                                   },
-                                   )
+        db.metadata.set_job_status(
+            self.job_id,
+            {
+                "phase": JOB_PHASE_RUNNING,
+                "reason": "RANDOM DOKIMI",
+            },
+        )
 
         try:
             logging.info(f'Running job {self.job_id}')
-            db.metadata.set_job_status(self.job_id,
+            db.metadata.set_job_status(
+                self.job_id,
                 {
                     "phase": JOB_PHASE_RUNNING,
                     "reason": "job started",
@@ -152,8 +166,11 @@ class Job(Base):
             method = getattr(component, self.method)
             method(*self.args, **self.kwargs)
         except Exception as e:
-            logging.error(f'Error running job {self.huuid}: {e}. Traceback: {traceback.format_exc()}')
-            db.metadata.set_job_status(self.job_id,
+            logging.error(
+                f'Error running job {self.huuid}: {e}. Traceback: {traceback.format_exc()}'
+            )
+            db.metadata.set_job_status(
+                self.job_id,
                 {
                     "phase": JOB_PHASE_FAILED,
                     "reason": f"job failed: {str(e)}",
@@ -548,19 +565,24 @@ class MetaDataStore:
         status = merge_dicts(job['status'], status_update)
 
         children = status.get('children', {})
-        failed_children = [v for v in children.values() if v.get('phase') == JOB_PHASE_FAILED]
+        failed_children = [
+            v for v in children.values() if v.get('phase') == JOB_PHASE_FAILED
+        ]
 
         if failed_children:
             status['phase'] = JOB_PHASE_FAILED
             n_failed = len(failed_children)
-            status['reason'] = f"{n_failed} {'child' if n_failed == 1 else 'children'} failed"
+            status['reason'] = (
+                f"{n_failed} {'child' if n_failed == 1 else 'children'} failed"
+            )
 
         self.db['Job'].update({'job_id': job_id}, 'status', status)
 
         if status['phase'] == JOB_PHASE_FAILED:
             key = f"Job/{job['component']}/{job['identifier']}/{job['uuid']}.{job['method']}"
-            self.set_component_status(job['component'], job['uuid'], {'children': {key: status}})
-
+            self.set_component_status(
+                job['component'], job['uuid'], {'children': {key: status}}
+            )
 
     def set_component_status(self, component: str, uuid: str, status_update: t.Dict):
         """
@@ -574,11 +596,15 @@ class MetaDataStore:
         status = merge_dicts(r['status'], status_update)
 
         if status['children'] and any(
-                v['phase'] == JOB_PHASE_FAILED for v in status['children'].values()
+            v['phase'] == JOB_PHASE_FAILED for v in status['children'].values()
         ):
             status['phase'] = JOB_PHASE_FAILED
             n_failed = len(
-                [v for v in status['children'].values() if v['phase'] == JOB_PHASE_FAILED]
+                [
+                    v
+                    for v in status['children'].values()
+                    if v['phase'] == JOB_PHASE_FAILED
+                ]
             )
             status['reason'] = '{} {} failed'.format(
                 n_failed, 'child' if n_failed == 1 else 'children'
@@ -691,7 +717,6 @@ class MetaDataStore:
 
     def show_cdc_tables(self):
         """List the tables used for CDC."""
-
         metadata = self.db['Table'].execute()
 
         cdc_classes = []

pinnacle/components/component.py
@@ -17,11 +17,16 @@ from pinnacle import logging
 from pinnacle.base.annotations import trigger
 from pinnacle.base.base import Base, BaseMeta
 from pinnacle.base.constant import KEY_BLOBS, KEY_FILES, LENGTH_UUID
-from pinnacle.base.status import init_status, JOB_PHASE_PENDING, JOB_PHASE_FAILED, running_status, JOB_PHASE_RUNNING
+from pinnacle.base.status import (
+    JOB_PHASE_FAILED,
+    JOB_PHASE_PENDING,
+    JOB_PHASE_RUNNING,
+    init_status,
+    running_status,
+)
 from pinnacle.misc.annotations import lazy_classproperty
 from pinnacle.misc.importing import isreallyinstance
 from pinnacle.misc.utils import hash_item
-
 
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer

test/unittest/component/test_component.py
@@ -14,7 +14,7 @@ from pinnacle.base.datatype import (
     _Artifact,
     dill_serializer,
 )
-from pinnacle.base.metadata import Job, JOB_PHASE_FAILED
+from pinnacle.base.metadata import JOB_PHASE_FAILED, Job
 from pinnacle.components.component import Component
 from pinnacle.components.listener import Listener
 