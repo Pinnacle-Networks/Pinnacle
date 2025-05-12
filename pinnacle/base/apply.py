superduper-io/superduper

fix status setting
Signed-off-by: Fotis Nikolaidis <nikolaidis.fotis@gmail.com>

# Conflicts:
#	superduper/base/apply.py
#	superduper/base/datalayer.py
#	superduper/base/exceptions.py
#	superduper/base/metadata.py

superduper/base/apply.py
@@ -9,7 +9,8 @@ from superduper import CFG, Component, logging
 from superduper.base import exceptions
 from superduper.base.document import Document
 from superduper.base.event import Create, Signal, Update
-from superduper.components.component import ready_status
+from superduper.base.metadata import NonExistentMetadataError
+from superduper.components.component import running_status
 from superduper.misc.tree import dict_to_tree
 
 if t.TYPE_CHECKING:
@@ -217,7 +218,7 @@ def _apply(
         if current.hash == object.hash:
             apply_status = 'same'
             object.version = current.version
-            object.status = ready_status()
+            object.status = running_status()
         elif current.uuid == object.uuid:
             apply_status = 'update'
             object.version = current.version
@@ -310,11 +311,11 @@ def _apply(
 
     if not these_job_events:
         if not CFG.json_native:
-            metadata_event.data['status'] = json.dumps(ready_status())
+            metadata_event.data['status'] = json.dumps(running_status())
         else:
-            metadata_event.data['status'] = ready_status()
+            metadata_event.data['status'] = running_status()
 
-        object.status = ready_status()
+        object.status = running_status()
 
     create_events[metadata_event.huuid] = metadata_event
     job_events.update({jj.huuid: jj for jj in these_job_events})

superduper/base/event.py
@@ -6,6 +6,7 @@ import pandas
 
 from superduper import logging
 from superduper.base import Base
+from superduper.base.metadata import JOB_PHASE_FAILED
 
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
@@ -130,7 +131,7 @@ class Create(Base):
                 component=self.component,
                 uuid=self.data['uuid'],
                 status_update={
-                    'phase': 'failed',
+                    'phase': JOB_PHASE_FAILED,
                     'reason': f'Failed to create: {str(e)}',
                     'message': format_exc(),
                 },
@@ -201,7 +202,7 @@ class Delete(Base):
                 component=self.component,
                 uuid=self.identifier,
                 status_update={
-                    'phase': 'failed',
+                    'phase': JOB_PHASE_FAILED,
                     'reason': f'Failed to delete: {str(e)}',
                     'message': format_exc(),
                 },
@@ -250,7 +251,7 @@ class Update(Base):
                 component=self.component,
                 uuid=self.data['uuid'],
                 status_update={
-                    'phase': 'failed',
+                    'phase': JOB_PHASE_FAILED,
                     'reason': f'Failed to update: {str(e)}',
                     'message': format_exc(),
                 },

superduper/components/component.py
@@ -17,35 +17,15 @@ from superduper import logging
 from superduper.base.annotations import trigger
 from superduper.base.base import Base, BaseMeta
 from superduper.base.constant import KEY_BLOBS, KEY_FILES, LENGTH_UUID
+from superduper.base.status import init_status, JOB_PHASE_PENDING, JOB_PHASE_FAILED, running_status, JOB_PHASE_RUNNING
 from superduper.misc.annotations import lazy_classproperty
 from superduper.misc.importing import isreallyinstance
 from superduper.misc.utils import hash_item
 
+
 if t.TYPE_CHECKING:
     from superduper.base.datalayer import Datalayer
     from superduper.base.metadata import Job
-
-
-def init_status():
-    """Initialize the status of the component."""
-    return {
-        'phase': 'uninitialized',
-        'reason': 'waiting for initialization',
-        'message': None,
-        'last_change_time': str(datetime.now()),
-        'children': {},
-    }
-
-
-def ready_status():
-    """The status of the component as ready."""
-    return {
-        'phase': 'ready',
-        'reason': 'the component is ready to use',
-        'message': None,
-        'last_change_time': str(datetime.now()),
-        'children': {},
-    }
 
 
 def _build_info_from_path(path: str):
@@ -261,7 +241,7 @@ class Component(Base, metaclass=ComponentMeta):
             if isinstance(r, list):
                 return sum([_find_refs(x) for x in r], [])
             if isinstance(r, Component):
-                if only_initializing and r.status.phase != 'initializing':
+                if only_initializing and r.status.phase != JOB_PHASE_PENDING:
                     return []
                 else:
                     return [r.huuid]
@@ -337,7 +317,7 @@ class Component(Base, metaclass=ComponentMeta):
             component=self.component,
             uuid=self.uuid,
             status_update={
-                'phase': 'failed',
+                'phase': JOB_PHASE_FAILED,
                 'reason': str(exc),
                 'message': format_exc(),
                 'last_change_time': str(datetime.now()),
@@ -351,7 +331,7 @@ class Component(Base, metaclass=ComponentMeta):
         :param status: The status to set the component to.
         """
         return self.db.metadata.set_component_status(
-            self.__class__.__name__, self.uuid, status_update=ready_status()
+            self.__class__.__name__, self.uuid, status_update=running_status()
         )
 
     def create_jobs(
@@ -412,12 +392,12 @@ class Component(Base, metaclass=ComponentMeta):
                         class_name = r['_path'].split('.')[-1]
                         huuid = f'{class_name}:{r["identifier"]}:{r["uuid"]}'
                         if event_type == 'apply':
-                            if r['status'] == 'initializing':
+                            if r['status'] == JOB_PHASE_PENDING:
                                 raise Exception(
                                     f'Component required component '
                                     f'{huuid} still initializing'
                                 )
-                            elif r['status'] == 'ready':
+                            elif r['status'] == JOB_PHASE_RUNNING:
                                 logging.info(
                                     f'Detected a ready component ' f'dependency {huuid}'
                                 )

test/unittest/component/test_component.py
@@ -14,7 +14,7 @@ from superduper.base.datatype import (
     _Artifact,
     dill_serializer,
 )
-from superduper.base.metadata import Job
+from superduper.base.metadata import Job, JOB_PHASE_FAILED
 from superduper.components.component import Component
 from superduper.components.listener import Listener
 
@@ -318,8 +318,8 @@ def test_propagate_failure(db):
     status_model = db.load('ObjectModel', 'model').status
     status_listener = db.load('Listener', 'test').status
 
-    assert status_model['phase'] == 'failed'
-    assert status_listener['phase'] == 'failed'
+    assert status_model['phase'] == JOB_PHASE_FAILED
+    assert status_listener['phase'] == JOB_PHASE_FAILED
     import datetime
 
     job = Job(
@@ -338,12 +338,12 @@ def test_propagate_failure(db):
 
     db.metadata.set_job_status(
         job_id='my-job-id',
-        status_update={'phase': 'failed', 'reason': 'Test exception'},
+        status_update={'phase': JOB_PHASE_FAILED, 'reason': 'Test exception'},
     )
 
     j = db['Job'].get(job_id='my-job-id')
 
-    assert j['status']['phase'] == 'failed'
+    assert j['status']['phase'] == JOB_PHASE_FAILED
     assert j['status']['reason'] == 'Test exception'
 
     list = db.load('Listener', 'test')

Pinnacle-Networks/Pinnacle

pinnacle/base/apply.py
@@ -9,7 +9,8 @@ from pinnacle import CFG, Component, logging
 from pinnacle.base import exceptions
 from pinnacle.base.document import Document
 from pinnacle.base.event import Create, Signal, Update
-from pinnacle.components.component import ready_status
+from pinnacle.base.metadata import NonExistentMetadataError
+from pinnacle.components.component import running_status
 from pinnacle.misc.tree import dict_to_tree
 
 if t.TYPE_CHECKING:
@@ -217,7 +218,7 @@ def _apply(
         if current.hash == object.hash:
             apply_status = 'same'
             object.version = current.version
-            object.status = ready_status()
+            object.status = running_status()
         elif current.uuid == object.uuid:
             apply_status = 'update'
             object.version = current.version
@@ -310,11 +311,11 @@ def _apply(
 
     if not these_job_events:
         if not CFG.json_native:
-            metadata_event.data['status'] = json.dumps(ready_status())
+            metadata_event.data['status'] = json.dumps(running_status())
         else:
-            metadata_event.data['status'] = ready_status()
+            metadata_event.data['status'] = running_status()
 
-        object.status = ready_status()
+        object.status = running_status()
 
     create_events[metadata_event.huuid] = metadata_event
     job_events.update({jj.huuid: jj for jj in these_job_events})

pinnacle/base/event.py
@@ -6,6 +6,7 @@ import pandas
 
 from pinnacle import logging
 from pinnacle.base import Base
+from pinnacle.base.metadata import JOB_PHASE_FAILED
 
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer
@@ -130,7 +131,7 @@ class Create(Base):
                 component=self.component,
                 uuid=self.data['uuid'],
                 status_update={
-                    'phase': 'failed',
+                    'phase': JOB_PHASE_FAILED,
                     'reason': f'Failed to create: {str(e)}',
                     'message': format_exc(),
                 },
@@ -201,7 +202,7 @@ class Delete(Base):
                 component=self.component,
                 uuid=self.identifier,
                 status_update={
-                    'phase': 'failed',
+                    'phase': JOB_PHASE_FAILED,
                     'reason': f'Failed to delete: {str(e)}',
                     'message': format_exc(),
                 },
@@ -250,7 +251,7 @@ class Update(Base):
                 component=self.component,
                 uuid=self.data['uuid'],
                 status_update={
-                    'phase': 'failed',
+                    'phase': JOB_PHASE_FAILED,
                     'reason': f'Failed to update: {str(e)}',
                     'message': format_exc(),
                 },

pinnacle/components/component.py
@@ -17,35 +17,15 @@ from pinnacle import logging
 from pinnacle.base.annotations import trigger
 from pinnacle.base.base import Base, BaseMeta
 from pinnacle.base.constant import KEY_BLOBS, KEY_FILES, LENGTH_UUID
+from pinnacle.base.status import init_status, JOB_PHASE_PENDING, JOB_PHASE_FAILED, running_status, JOB_PHASE_RUNNING
 from pinnacle.misc.annotations import lazy_classproperty
 from pinnacle.misc.importing import isreallyinstance
 from pinnacle.misc.utils import hash_item
 
+
 if t.TYPE_CHECKING:
     from pinnacle.base.datalayer import Datalayer
     from pinnacle.base.metadata import Job
-
-
-def init_status():
-    """Initialize the status of the component."""
-    return {
-        'phase': 'uninitialized',
-        'reason': 'waiting for initialization',
-        'message': None,
-        'last_change_time': str(datetime.now()),
-        'children': {},
-    }
-
-
-def ready_status():
-    """The status of the component as ready."""
-    return {
-        'phase': 'ready',
-        'reason': 'the component is ready to use',
-        'message': None,
-        'last_change_time': str(datetime.now()),
-        'children': {},
-    }
 
 
 def _build_info_from_path(path: str):
@@ -261,7 +241,7 @@ class Component(Base, metaclass=ComponentMeta):
             if isinstance(r, list):
                 return sum([_find_refs(x) for x in r], [])
             if isinstance(r, Component):
-                if only_initializing and r.status.phase != 'initializing':
+                if only_initializing and r.status.phase != JOB_PHASE_PENDING:
                     return []
                 else:
                     return [r.huuid]
@@ -337,7 +317,7 @@ class Component(Base, metaclass=ComponentMeta):
             component=self.component,
             uuid=self.uuid,
             status_update={
-                'phase': 'failed',
+                'phase': JOB_PHASE_FAILED,
                 'reason': str(exc),
                 'message': format_exc(),
                 'last_change_time': str(datetime.now()),
@@ -351,7 +331,7 @@ class Component(Base, metaclass=ComponentMeta):
         :param status: The status to set the component to.
         """
         return self.db.metadata.set_component_status(
-            self.__class__.__name__, self.uuid, status_update=ready_status()
+            self.__class__.__name__, self.uuid, status_update=running_status()
         )
 
     def create_jobs(
@@ -412,12 +392,12 @@ class Component(Base, metaclass=ComponentMeta):
                         class_name = r['_path'].split('.')[-1]
                         huuid = f'{class_name}:{r["identifier"]}:{r["uuid"]}'
                         if event_type == 'apply':
-                            if r['status'] == 'initializing':
+                            if r['status'] == JOB_PHASE_PENDING:
                                 raise Exception(
                                     f'Component required component '
                                     f'{huuid} still initializing'
                                 )
-                            elif r['status'] == 'ready':
+                            elif r['status'] == JOB_PHASE_RUNNING:
                                 logging.info(
                                     f'Detected a ready component ' f'dependency {huuid}'
                                 )

test/unittest/component/test_component.py
@@ -14,7 +14,7 @@ from pinnacle.base.datatype import (
     _Artifact,
     dill_serializer,
 )
-from pinnacle.base.metadata import Job
+from pinnacle.base.metadata import Job, JOB_PHASE_FAILED
 from pinnacle.components.component import Component
 from pinnacle.components.listener import Listener
 
@@ -318,8 +318,8 @@ def test_propagate_failure(db):
     status_model = db.load('ObjectModel', 'model').status
     status_listener = db.load('Listener', 'test').status
 
-    assert status_model['phase'] == 'failed'
-    assert status_listener['phase'] == 'failed'
+    assert status_model['phase'] == JOB_PHASE_FAILED
+    assert status_listener['phase'] == JOB_PHASE_FAILED
     import datetime
 
     job = Job(
@@ -338,12 +338,12 @@ def test_propagate_failure(db):
 
     db.metadata.set_job_status(
         job_id='my-job-id',
-        status_update={'phase': 'failed', 'reason': 'Test exception'},
+        status_update={'phase': JOB_PHASE_FAILED, 'reason': 'Test exception'},
     )
 
     j = db['Job'].get(job_id='my-job-id')
 
-    assert j['status']['phase'] == 'failed'
+    assert j['status']['phase'] == JOB_PHASE_FAILED
     assert j['status']['reason'] == 'Test exception'
 
     list = db.load('Listener', 'test')